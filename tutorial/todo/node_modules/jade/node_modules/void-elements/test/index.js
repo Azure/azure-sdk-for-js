be342b8544e04d600d840d463eeb2efd887f57c3
pacific202 <graham.hunter@gmail.com>
2014-11-17 14:29:57 -0800

    ExpressJS3 (no node_modules)

--- tutorial/todo/node_modules/jade/node_modules/void-elements/test/index.js
+++
@@ -1,5 +0,0 @@
-var assert = require('assert');
-var voidElements = require('../');
-assert(voidElements.indexOf('span') === -1, '<span> is not a void element');
-assert(voidElements.indexOf('img') !== -1, '<img> is a void element');
-console.log('tests passed');
