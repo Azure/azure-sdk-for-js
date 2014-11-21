94555ddd561adb06ef0141c107a5d349724ddf0d
Ryan CrawCour <ryancrawcour@outlook.com>
2014-11-20 17:35:47 -0800

    reorg of sample data

--- samples/config.js
+++ samples/config.js
@@ -1,6 +1,4 @@
-﻿var fs = require('fs');
-
-exports.connection = {
+﻿exports.connection = {
     endpoint: 'https://ryancrawcour.documents.azure.com:443/',
     authKey: 'NC4KZ6Lh5/wukMHMunWlFx6qWfjwMV1MIODmu4WfvL9smr1aT05wT2nD/w4+CKY49ljXHmNK6x8+ejMK3k64SQ=='
 };
@@ -8,9 +6,4 @@ exports.connection = {
 exports.names = {
 	database: 'NodeSamples',
 	collection: 'Families',
-};
-
-exports.documentDefinitions = function () {
-    var data = fs.readFileSync('../Data.json');
-    return JSON.parse(data).Families;
 };
