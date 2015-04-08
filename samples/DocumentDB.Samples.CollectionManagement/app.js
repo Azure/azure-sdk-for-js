d13f4bf5805464df306132e163813a56f21b3bd0
ryancrawcour <ryancrawcour@outlook.com>
2015-04-08 00:40:59 -0700

    updates for OfferTypes & new npm modules

--- samples/DocumentDB.Samples.CollectionManagement/app.js
+++ samples/DocumentDB.Samples.CollectionManagement/app.js
@@ -32,35 +32,105 @@ var client = new DocumentDBClient(host, { masterKey: masterKey });
 // This demo performs a few steps
 // 1. Read or Create a Database (we always need a database to work with)
 // 2. Create a collection in this database
-// 3. List Collections on a database
-// 4. Delete Collection and the database we created
-// 5. finish()
+// 3. Using the collection selfLink, query for the Offer object and verify the OfferType
+// 4. Scale the collection up by changing the Offer it is linked to
+// 5. List Collections on a database
+// 6. Delete Collection and the database we created
+// 7. finish()
 //---------------------------------------------------------------------------------
 
 // 1.
 console.log('Looking for a database named \'' + databaseId + '\'...');
 getOrCreateDatabase(databaseId, function (db) {
+
     // 2.
     console.log('Creating a collection called \'' + collectionId + '\'...');
-    createCollection(db._self, collectionId, function (col) {        
-        // 3.
-        console.log('Listing all collections on this database...');
-        listCollections(db._self, function (cols) {
-            for (var i = 0; i < cols.length; i++) {
-                console.log(cols[i].id);
-            };
+    createCollection(db._self, collectionId, function (col) {
 
+        // 3.
+        console.log('Verifying offertype...');
+        getOfferType(col._self, function (offer) {
+            console.log('Offer type of collection ' + col.id + ' is ' + offer.offerType);
+            
             // 4.
-            console.log('Cleaning up...');
-            deleteCollection(col, function () {
-                deleteDatabase(db, function () {
-                    // 5.
-                    finish();
+            console.log('Changing OfferType of collection...');
+            changeOfferType(col._self, 'S2', function () {
+                console.log('OfferType changed');
+
+                // 5.
+                console.log('Listing all collections on this database...');
+                listCollections(db._self, function (cols) {
+                    for (var i = 0; i < cols.length; i++) {
+                        console.log(cols[i].id);
+                    };
+                    
+                    // 6.
+                    console.log('Cleaning up...');
+                    deleteCollection(col, function () {
+                        deleteDatabase(db, function () {
+                            
+                            // 7.
+                            finish();
+                        });
+                    });
+
                 });
-            });                       
+            });
         });
     });
 });
+
+//Collections and OfferTypes are loosely coupled.
+//An Offer's resource -- Collection's _self property
+//And an Offer's offerResourceId -- Collection's _rid property
+//To find the OfferType for a Collection, query for Offers by resourceLink matching collectionLink
+function getOfferType(collectionLink, callback) {
+    var querySpec = {
+        query: 'SELECT * FROM root r WHERE  r.resource = @link',
+        parameters: [
+            {
+                name: '@link',
+                value: collectionLink
+            }
+        ]
+    };
+    
+    client.queryOffers(querySpec).toArray(function (err, offers) {
+        if (err) handleError(err);
+
+        var offer = offers[0];
+        callback(offer);
+    });
+}
+
+function changeOfferType(collectionLink, newOfferType, callback) {
+    var querySpec = {
+        query: 'SELECT * FROM root r WHERE  r.resource = @link',
+        parameters: [
+            {
+                name: '@link',
+                value: collectionLink
+            }
+        ]
+    };
+    
+    client.queryOffers(querySpec).toArray(function (err, offers) {
+        if (err) handleError(err);
+        
+        var offer = offers[0];
+        offer.offerType = newOfferType;
+        
+        client.replaceOffer(offer._self, offer, function (err, replacedOffer) {
+            if (err) {
+                handleError(err);
+            } else if (replacedOffer.offerType != newOfferType) {
+                throw 'OfferType was not updated';
+            } else {
+                callback(replacedOffer);
+            }
+        })     
+    })
+}
 
 function listCollections(databaseLink, callback) {
     var queryIterator = client.readCollections(databaseLink).toArray(function (err, cols) {
@@ -76,12 +146,14 @@ function listCollections(databaseLink, callback) {
 function createCollection(databaseLink, collectionId, callback) {
     //we're creating a Collection here using the default indexingPolicy, 
     //for more information on using other indexingPolicies please consult the IndexManagement sample
-    var collectionDef = { id: collectionId };
-    
-    client.createCollection(databaseLink, collectionDef, function (err, created) {
-        if (err) {
-            handleError(err);
-        }
+     
+    //we're also setting the OfferType for this new collection to be an "S1"
+    //"S1" is the default, so if a OfferType value is not supplied in the 4th parameter then OfferTyoe of "S1" will apply
+    //for more information on OfferTypes please consult the DocumentDB Documentation on 
+    //http://azure.microsoft.com/en-us/documentation/services/documentdb/
+
+    client.createCollection(databaseLink, { id: collectionId }, { offerType: "S1" }, function (err, created) {
+        if (err) handleError(err);
         
         console.log('Collection \'' + collectionId + '\'created');
         callback(created);
@@ -98,6 +170,7 @@ function getOrCreateDatabase(databaseId, callback){
             }
         ]
     };
+
     client.queryDatabases(querySpec).toArray(function (err, results) {
         if (err) {
             handleError(err);
--- samples/config.js
+++ samples/config.js
@@ -1,6 +1,6 @@
 ﻿exports.connection = {
     endpoint: 'https://ryancrawcour.documents.azure.com:443/',
-    authKey: 'NC4KZ6Lh5/wukMHMunWlFx6qWfjwMV1MIODmu4WfvL9smr1aT05wT2nD/w4+CKY49ljXHmNK6x8+ejMK3k64SQ=='
+    authKey: 'wYIlzQJh2pO8ganvKsGV/GwvpYjPkwPNcqin3sn1ZGOgyU1gwfAqLc6rh84elQjTCJ9jkoBlwnpliMq5+tp0dQ=='
 };
 
 exports.names = {
