5314b9833048142a4c2f8c97773512d047b9ce3c
Ryan CrawCour <ryancraw@microsoft.com>
2015-08-15 12:51:43 -0700

    Update config.js

--- samples/config.js
+++ samples/config.js
@@ -1,9 +1,9 @@
 ﻿exports.connection = {
-    endpoint: 'https://ryancrawcour.documents.azure.com:443/',
-    authKey: 'wYIlzQJh2pO8ganvKsGV/GwvpYjPkwPNcqin3sn1ZGOgyU1gwfAqLc6rh84elQjTCJ9jkoBlwnpliMq5+tp0dQ=='
+    endpoint: '[supply your endpoint]',
+    authKey: '[supply you key]]'
 };
 
 exports.names = {
 	database: 'NodeSamples',
-	collection: 'Families',
+	collection: 'Data',
 };
