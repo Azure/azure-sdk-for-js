6bace13b3f166e4f57955b4953f7fb3a4ff7f255
Armando Trejo <ato9000@outlook.com>
2015-11-08 10:09:04 -0800

    Fix murmurHash (uint multiplications and readBytes)

--- source/lib/Hash/murmurHash.js
+++ source/lib/Hash/murmurHash.js
@@ -26,81 +26,84 @@ SOFTWARE.
 var Base = require("../base");
 
 var MurmurHash = Base.defineClass(
-    undefined, 
+	undefined, 
     undefined,
     {
-        computeHash: function(buffer, seed) {
-            // MurmurHash3 32bit implementation:
-            // https://en.wikipedia.org/wiki/MurmurHash
-            seed = seed || 0;
-
-            var c1 = 0xcc9e2d51;
-            var c2 = 0x1b873593;
-
-            var h1 = seed;
-
-            for (var i = 0; i < buffer.length - 3; i += 4) {
-                var k1 = buffer.readUIntLE(i, 4); // BinaryReader.readUInte32()
-
-				k1 = this._toUint(k1 * c1);
-                k1 = this._rotateLeft(k1, 15);
-                k1 = this._toUint(k1 * c2);
-
-                h1 ^= k1;
-                h1 = this._rotateLeft(h1, 13);
-                h1 = h1 * 5 + 0xe6546b64;
-            }
-
-            // tail
-            var k = 0;
-
-            switch (buffer.length & 3) {
-            case 3:
-                k ^= this._toUint(buffer[buffer.length - 1] << 16);
-                k ^= this._toUint(buffer[buffer.length - 2] << 8);
-                k ^= this._toUint(buffer[buffer.length - 3]);
-                break;
-
-            case 2:
-                k ^= this._toUint(buffer[buffer.length - 1] << 8);
-                k ^= this._toUint(buffer[buffer.length - 2]);
-                break;
-
-            case 1:
-                k ^= this._toUint(buffer[buffer.length - 1]);
-                break;
-            }
-
-            k *= c1;
-            k = this._rotateLeft(k, 15);
-            k *= c2;
-            h1 ^= k;
-
-            // finalization
-            h1 ^= this._toUint(buffer.length);
-            h1 ^= h1 >> 16;
-            h1 *= 0x85ebca6b;
-            h1 ^= h1 >> 13;
-            h1 *= 0xc2b2ae35;
-            h1 ^= h1 >> 16;
-
-            return h1;
-        },
-		_rotateLeft: function (n, numBits) {
-			var a = this._toUint(n << numBits);
-			var b = this._toUint(32 - numBits);
-			var c = this._toUint(n >>> b);
-			var d = this._toUint(a | c);
-            return d;
+		computeHash: function (key, seed) {
+			key = key || '';
+			seed = seed || 0;
+			
+			if (typeof key === "string") {
+				return this._hashString(key, seed);
+			}
+			
+			return this._hashBytes(key, seed);
 		},
-		_toUint: function (value) {
-			return value % 0x100000000;
-        }
-    }
-);
+		_rotateLeft: function (n, numBits) {
+			return (n << numBits) | (n >>> (32 - numBits));
+		},
+		_readUInt32: function (uint32Array, i) {
+			return (uint32Array[i]) | (uint32Array[i + 1] << 8) | (uint32Array[i + 2] << 16) | (uint32Array[i + 3] << 24);
+		},
+		_multiply: function (m, n) {
+			return ((m & 0xffff) * n) + ((((m >>> 16) * n) & 0xffff) << 16);
+		},
+		_hashString: function (key, seed) {
+			return this._hashBytes(new Buffer(key), seed);
+		},
+		_hashBytes: function (bytes, seed) {
+			var h1 = seed;
+			
+			var k1 = 0;
+			
+			var c1 = 0xcc9e2d51;
+			var c2 = 0x1b873593;
+			
+			var reader = new Uint32Array(bytes);
+			{
+				for (var i = 0; i < bytes.length - 3; i += 4) {
+					k1 = this._readUInt32(reader, i);
+					
+					k1 = this._multiply(k1, c1);
+					k1 = this._rotateLeft(k1, 15);
+					k1 = this._multiply(k1, c2);
+					
+					h1 ^= k1;
+					h1 = this._rotateLeft(h1, 13);
+					h1 = this._multiply(h1, 5) + 0xe6546b64;
+				}
+			}
+			
+			k1 = 0;
+			
+			switch (bytes.length & 3) {
+				case 3:
+					k1 ^= reader[i + 2] << 16;
+
+				case 2:
+					k1 ^= reader[i + 1] << 8;
+
+				case 1:
+					k1 ^= reader[i];
+					k1 = this._multiply(k1, c1);
+					k1 = this._rotateLeft(k1, 15);
+					k1 = this._multiply(k1, c2);
+					h1 ^= k1;
+			}
+			
+			h1 ^= bytes.length;
+			h1 ^= h1 >>> 16;
+			h1 = this._multiply(h1, 0x85ebca6b);
+			h1 ^= h1 >>> 13;
+			h1 = this._multiply(h1, 0xc2b2ae35);
+			h1 ^= h1 >>> 16;
+			
+			return h1 >>> 0;
+		}
+	});
 
 //SCRIPT END
 
 if (typeof exports !== "undefined") {
-    exports.MurmurHash = MurmurHash;
+	exports.MurmurHash = MurmurHash;
 }
