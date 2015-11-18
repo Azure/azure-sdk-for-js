99f12b100f9868b9dc898d69655262a752efe2f7
Damon Barry <damonb@microsoft.com>
2015-11-18 12:52:42 -0800

    test for connection string arg

--- tests/test.js
+++ tests/test.js
@@ -11,15 +11,31 @@ chai.should();
 chai.use(chaiAsPromised);
 
 function EventHubClient() {}
-EventHubClient.fromConnectionString = function () {
-	return new EventHubClient();
+EventHubClient.fromConnectionString = function (connectionString) {
+  if (!connectionString) throw Error();
+  return new EventHubClient();
 };
+
+function testFalsyValues(testFn) {
+  [null, undefined, '', 0].forEach(function (value) {
+    testFn(value);
+  });
+}
 
 describe('EventHubClient', function () {
   describe('#fromConnectionString', function () {
-	it('creates an EventHubClient', function () {
-		var client = EventHubClient.fromConnectionString();
-		client.should.be.an.instanceof(EventHubClient);
-	});
+    it('creates an EventHubClient', function () {
+      var client = EventHubClient.fromConnectionString('abc');
+      client.should.be.an.instanceof(EventHubClient);
+    });
+	
+    it('throws when there\'s no connection string', function () {
+      testFalsyValues(function (value) {
+        var test = function () {
+          return EventHubClient.fromConnectionString(value);
+        };
+        test.should.throw(Error);
+      });
+    });
   });
 });
