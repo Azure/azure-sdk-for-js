8fb2acbd12bc59e68f703713676860065ea895ef
Damon Barry <damonb@microsoft.com>
2015-11-20 14:47:39 -0800

    return correct error when server doesn't recognize Event Hub path

--- tests/event_hub_management.js
+++ tests/event_hub_management.js
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+/*jshint expr: true*/
+
 'use strict';
 
 var chai = require('chai');
@@ -10,6 +12,7 @@ chai.should();
 chai.use(chaiAsPromised);
 
 var EventHubClient = require('../lib/client.js');
+var MessagingEntityNotFoundError = require('../lib/errors.js').MessagingEntityNotFoundError;
 
 describe('EventHubClient', function () {
   describe('#getPartitionIds', function () {
@@ -19,11 +22,17 @@ describe('EventHubClient', function () {
       var client = EventHubClient.fromConnectionString(process.env.EVENT_HUB_CONNECTION_STRING, process.env.EVENT_HUB_PATH);
       var partitionIds = client.getPartitionIds();
       return partitionIds.then(function (ids) {
-        ids.length.should.not.equal(0);
+        ids.should.not.be.empty;
 
         var expected = Array.apply(null, Array(ids.length)).map(function (x, i) { return String(i); });
         ids.should.have.members(expected);
       });
+    });
+    
+    it('returns MessagingEntityNotFoundError if the server doesn\'t recognize the Event Hub path', function () {
+      var client = EventHubClient.fromConnectionString(process.env.EVENT_HUB_CONNECTION_STRING, 'bad' + Math.random());
+      var partitionIds = client.getPartitionIds();
+      return partitionIds.should.be.rejectedWith(MessagingEntityNotFoundError);
     });
   });
 });
