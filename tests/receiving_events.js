1184ad8cbdb77357c2736ac109b9fb36cb941d8e
Damon Barry <damonb@microsoft.com>
2015-12-01 16:37:52 -0800

    handle ArgumentOutOfRangeError on receiver

--- tests/receiving_events.js
+++ tests/receiving_events.js
@@ -9,6 +9,7 @@ chai.should();
 var Receiver = require('../lib/receiver.js');
 var EventHubClient = require('../lib/client.js');
 var MessagingEntityNotFoundError = require('../lib/errors.js').MessagingEntityNotFoundError;
+var ArgumentOutOfRangeError = require('../lib/errors.js').ArgumentOutOfRangeError;
 
 describe('EventHubClient', function () {
   describe('#createReceiver', function () {
@@ -21,12 +22,25 @@ describe('EventHubClient', function () {
 });
 
 describe('EventHubReceiver', function () {
+  this.timeout(15000);
+
   it('emits MessagingEntityNotFoundError when the consumer group doesn\'t exist', function (done) {
     var client = EventHubClient.fromConnectionString(process.env.EVENT_HUB_CONNECTION_STRING, process.env.EVENT_HUB_PATH);
     var promise = client.createReceiver('bad', '0');
     promise.then(function (receiver) {
-      receiver.on('errorReceived', function (err) {
+      receiver.once('errorReceived', function (err) {
         err.should.be.instanceOf(MessagingEntityNotFoundError);
+        done();
+      });
+    });
+  });
+
+  it('emits ArgumentOutOfRangeError when the partition ID doesn\'t exist', function (done) {
+    var client = EventHubClient.fromConnectionString(process.env.EVENT_HUB_CONNECTION_STRING, process.env.EVENT_HUB_PATH);
+    var promise = client.createReceiver('$Default', 'bad');
+    promise.then(function (receiver) {
+      receiver.once('errorReceived', function (err) {
+        err.should.be.instanceOf(ArgumentOutOfRangeError);
         done();
       });
     });
