008f18901b1937bdb0efb0bb9691a52cc5e07666
Damon Barry <damonb@microsoft.com>
2015-12-01 16:22:52 -0800

    handle MessagingEntityNotFoundError on receiver

--- tests/receiving_events.js
+++ tests/receiving_events.js
@@ -6,15 +6,29 @@
 var chai = require('chai');
 chai.should();
 
-var EventHubClient = require('../lib/client.js');
 var Receiver = require('../lib/receiver.js');
+var EventHubClient = require('../lib/client.js');
+var MessagingEntityNotFoundError = require('../lib/errors.js').MessagingEntityNotFoundError;
 
 describe('EventHubClient', function () {
   describe('#createReceiver', function () {
     it('returns a Receiver', function () {
       var client = EventHubClient.fromConnectionString(process.env.EVENT_HUB_CONNECTION_STRING, process.env.EVENT_HUB_PATH);
       var receiver = client.createReceiver('$Default', '0');
-      return receiver.should.be.eventually.be.instanceOf(Receiver);
+      return receiver.should.eventually.be.instanceOf(Receiver);
+    });
+  });
+});
+
+describe('EventHubReceiver', function () {
+  it('emits MessagingEntityNotFoundError when the consumer group doesn\'t exist', function (done) {
+    var client = EventHubClient.fromConnectionString(process.env.EVENT_HUB_CONNECTION_STRING, process.env.EVENT_HUB_PATH);
+    var promise = client.createReceiver('bad', '0');
+    promise.then(function (receiver) {
+      receiver.on('errorReceived', function (err) {
+        err.should.be.instanceOf(MessagingEntityNotFoundError);
+        done();
+      });
     });
   });
 });
