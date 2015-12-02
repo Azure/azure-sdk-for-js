11262972bf1499bfcccdfbf13b49cac587292424
Damon Barry <damonb@microsoft.com>
2015-12-01 17:20:23 -0800

    test MessagingEntityNotFoundError mapping in errors.translate()

--- tests/normalizing_errors.js
+++ tests/normalizing_errors.js
@@ -8,12 +8,21 @@ chai.should();
 
 var errors = require('../lib/errors.js');
 
+function AMQPError(conditionStr) {
+  this.condition = { contents: conditionStr };
+}
+
 describe('errors', function () {
   describe('.translate', function () {
     it('acts as a passthrough if the input is not an AMQPError', function () {
       var MyError = function () {};
       var err = new MyError();
       errors.translate(err).should.equal(err);
+    });
+    
+    it('translates \'amqp:not-found\' into MessagingEntityNotFoundError', function () {
+      var err = new AMQPError('amqp:not-found');
+      errors.translate(err).should.be.an.instanceof(errors.MessagingEntityNotFoundError);
     });
   });
 });
