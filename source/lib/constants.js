0df58a42302edebdda772937490852186e5f3b81
Srinath Narayanan <srnara@microsoft.com>
2017-11-10 11:34:03 -0800

    version bump

--- source/lib/constants.js
+++ source/lib/constants.js
@@ -185,7 +185,7 @@ var Constants = {
     CurrentVersion: "2017-11-15",
 
     SDKName: "documentdb-nodejs-sdk",
-    SDKVersion: "1.14.0",
+    SDKVersion: "1.14.1",
 
     DefaultPrecisions: {
         DefaultNumberHashPrecision: 3,
