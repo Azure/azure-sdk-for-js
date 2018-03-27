80ed8578a51e0d61d3aa53102ffaa674f41dd103
Rikki Gibson <rigibson@microsoft.com>
2018-03-27 16:48:56 -0700

    Cleanup syntax for wrapping list

--- lib/serializer.ts
+++ lib/serializer.ts
@@ -355,8 +355,7 @@ export class Serializer {
               parentObject.attributes = parentObject.attributes || {};
               parentObject.attributes[propName] = serializedValue;
             } else if (propertyMapper.xmlIsWrapped) {
-              parentObject[propName] = {};
-              parentObject[propName][propertyMapper.xmlElementName!] = serializedValue;
+              parentObject[propName] = { [propertyMapper.xmlElementName!]: serializedValue };
             } else {
               parentObject[propName] = serializedValue;
             }
