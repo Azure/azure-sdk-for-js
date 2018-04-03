a1f5b6e232819417eb5253c8e65b10bb614257f0
Rikki Gibson <rigibson@microsoft.com>
2018-04-02 17:43:13 -0700

    Update build products

--- typings/lib/msRestAzure.d.ts
+++ typings/lib/msRestAzure.d.ts
@@ -2,4 +2,5 @@ import { AzureServiceClientOptions, AzureServiceClient } from "./azureServiceCli
 import Constants from "./util/constants";
 import { CloudError, CloudErrorMapper } from "./cloudError";
 import { BaseResource, BaseResourceMapper } from "./baseResource";
-export { AzureServiceClient, AzureServiceClientOptions, Constants, CloudError, CloudErrorMapper, BaseResource, BaseResourceMapper };
+import { CognitiveServicesCredentials } from "./credentials/cognitiveServicesCredentials";
+export { AzureServiceClient, AzureServiceClientOptions, Constants, CloudError, CloudErrorMapper, BaseResource, BaseResourceMapper, CognitiveServicesCredentials };
