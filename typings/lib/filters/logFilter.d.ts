b281784b2bb962b2708adec7550c9738adfd32cf
Rikki Gibson <rigibson@microsoft.com>
2018-04-10 15:06:17 -0700

    Remove generated typings from git

--- typings/lib/filters/logFilter.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseFilter } from "./baseFilter";
-import { HttpOperationResponse } from "../httpOperationResponse";
-export declare class LogFilter extends BaseFilter {
-    logger?: any;
-    constructor(logger?: any);
-    after(operationResponse: HttpOperationResponse): Promise<HttpOperationResponse>;
-}
--- typings/lib/filters/redirectFilter.d.ts
+++
@@ -1,8 +0,0 @@
-import { BaseFilter } from "./baseFilter";
-import { HttpOperationResponse } from "../httpOperationResponse";
-export declare class RedirectFilter extends BaseFilter {
-    maximumRetries?: number;
-    constructor(maximumRetries?: number);
-    handleRedirect(operationResponse: HttpOperationResponse, currentRetries: number): Promise<HttpOperationResponse>;
-    after(operationResponse: HttpOperationResponse): Promise<HttpOperationResponse>;
-}
