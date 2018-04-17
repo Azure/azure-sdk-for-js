eea6d0ecf014ce46360200a318f0051a80b064e4
Rikki Gibson <rigibson@microsoft.com>
2018-04-17 14:36:24 -0700

    Browser tests for ms-rest-js

---
+++ webpack.testconfig.ts
@@ -0,0 +1,49 @@
+import * as webpack from 'webpack';
+import * as glob from 'glob';
+import * as path from 'path';
+
+const config: webpack.Configuration = {
+  entry: glob.sync('./test/*.ts'),
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: __dirname
+  },
+  output: {
+    filename: 'testBundle.js',
+    path: __dirname
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "moment": path.resolve('./node_modules/moment/min/moment.min.js')
+    }
+  },
+  externals: {
+    "ms-rest-js": "msRest"
+  },
+  node: {
+    fs: false,
+    net: false,
+    path: false,
+    dns: false,
+    tls: false,
+    tty: false,
+    v8: false,
+    Buffer: true // TODO: find alternative to Buffer for tests
+  }
+};
+
+export default config;
