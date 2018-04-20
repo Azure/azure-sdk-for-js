c8051930d9c94918103bfc628521924b5c478bd2
Amar Zavery <amzavery@microsoft.com>
2018-04-20 16:30:57 -0700

    updated examples.

--- examples/sendReceiveWithSPAuth.ts
+++ examples/sendReceiveWithSPAuth.ts
@@ -15,12 +15,10 @@ const domain = process.env[doma] || "";
 async function main(): Promise<void> {
   const credentials = await msrestAzure.loginWithServicePrincipalSecret(clientId, secret, domain, { tokenAudience: aadEventHubsAudience });
   const client = EventHubClient.createFromAadTokenCredentials(address, path, credentials);
-  const sender = await client.createSender("0");
-  const receiver = client.createReceiver("0", { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
-  sender.send({ body: "Hello awesome world!!" });
-  const datas = await receiver.receive(2, 5);
+  await client.send({ body: "Hello awesome world!!" }, 0);
+  const datas = await client.receiveBatch("0", 2, 5, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
   console.log(">>> EventDataObjects: ", datas);
-  await sender.close();
+  await client.close();
 }
 
 main().catch((err) => {
