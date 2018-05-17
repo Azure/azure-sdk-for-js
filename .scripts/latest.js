d0800f5dd666f1480270fcc9d327fcdf9c36eb84
Dan Schulte <daschult@microsoft.com>
2018-05-17 10:08:22 -0700

    Remove unused dev script functions

--- .scripts/latest.js
+++ .scripts/latest.js
@@ -1,9 +1,3 @@
 const dependencies = require("./dependencies");
 
-const localDependencies = dependencies.getDependenciesWithClonedRepositories();
-for (const localDependency of localDependencies) {
-  const version = dependencies.getNpmPackageVersion(localDependency, "latest");
-  dependencies.updatePackageJsonDependency(localDependency, `^${version}`);
-  dependencies.runLocalRepositoryNPMScript(localDependency, "latest");
-}
 dependencies.updatePackageJsonMain("./dist/lib/msRest.js")
--- .scripts/local.js
+++ .scripts/local.js
@@ -1,8 +1,3 @@
 const dependencies = require("./dependencies");
 
-const localDependencies = dependencies.getDependenciesWithClonedRepositories();
-for (const localDependency of localDependencies) {
-  dependencies.updatePackageJsonDependency(localDependency, `file:../${localDependency}`);
-  dependencies.runLocalRepositoryNPMScript(localDependency, "local");
-}
 dependencies.updatePackageJsonMain("./lib/msRest.ts");
