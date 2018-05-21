3646d393fbd3cbfd406fc379664a975f636a0ab7
Dan Schulte <daschult@microsoft.com>
2018-05-21 09:37:31 -0700

    Add simple test for serializationPolicy

---
+++ test/shared/policies/serializationPolicyTests.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+import * as assert from "assert";
+import { HttpHeaders } from "../../../lib/httpHeaders";
+import { HttpOperationResponse } from "../../../lib/httpOperationResponse";
+import { RequestPolicy, RequestPolicyOptions } from "../../../lib/policies/requestPolicy";
+import { SerializationPolicy } from "../../../lib/policies/serializationPolicy";
+import { Serializer } from "../../../lib/serializer";
+import { WebResource } from "../../../lib/webResource";
+
+describe("serializationPolicy", () => {
+  const mockPolicy: RequestPolicy = {
+    sendRequest(request: WebResource): Promise<HttpOperationResponse> {
+      return Promise.resolve({
+        request: request,
+        status: 200,
+        headers: new HttpHeaders()
+      });
+    }
+  };
+
+  const serializer = new Serializer();
+
+  it(`should not modify a request that has no request body mapper`, async () => {
+    const serializationPolicy = new SerializationPolicy(mockPolicy, new RequestPolicyOptions(), serializer);
+
+    const request = new WebResource();
+    request.body = "hello there!";
+
+    await serializationPolicy.sendRequest(request);
+    assert.strictEqual(request.body, "hello there!");
+  });
+});
