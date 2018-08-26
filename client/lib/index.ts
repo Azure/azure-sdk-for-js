ca5a2a2c46b895cc289c514d991276c7ec9012d1
Amar Zavery <amzavery@microsoft.com>
2018-08-25 21:31:42 -0700

    temp commit: randomNumberFromInterval

--- client/lib/index.ts
+++ client/lib/index.ts
@@ -19,5 +19,5 @@ export {
   delay, Timeout, EventHubConnectionStringModel, parseConnectionString,
   IotHubConnectionStringModel, StorageConnectionStringModel, isIotHubConnectionString,
   ErrorNameConditionMapper, ConditionStatusMapper, ConditionErrorNameMapper, MessagingError,
-  DataTransformer, DefaultDataTransformer, ConnectionConfig
+  DataTransformer, DefaultDataTransformer, ConnectionConfig, randomNumberFromInterval
 } from "./amqp-common";
