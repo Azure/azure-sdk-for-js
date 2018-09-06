860033cf16ac842252a28b170a3763e0c04a99e4
Rikki Gibson <rigibson@microsoft.com>
2018-09-06 16:13:45 -0700

    Add arm-storage sdk with multi api support and update gitignore

---
+++ packages/arm-storage/webpack.config.js
@@ -0,0 +1,41 @@
+// This is a template webpack config file with minimal configuration.
+// Users are free to create their own webpack configuration files in their application.
+const path = require('path');
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+const config = {
+  mode: 'production',
+  entry: './2018-02-01/lib/storageManagementClient.ts',
+  devtool: 'source-map',
+  output: {
+    filename: 'storageManagementClientBundle.js',
+    path: __dirname,
+    libraryTarget: 'var',
+    library: 'storageManagementClient'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: path.join(__dirname, '2018-02-01/tsconfig.esm.json'),
+        }
+      }
+    ]
+  },
+  // "ms-rest-js" and "ms-rest-azure-js" are dependencies of this library.
+  // Customer is expected to import/include this library in browser javascript
+  // (probably using the script tag in their html file).
+  externals: {
+    "ms-rest-js": "msRest",
+    "ms-rest-azure-js": "msRestAzure"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
+
+module.exports = config;
