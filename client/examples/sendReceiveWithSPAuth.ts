ca19417e6996b45ab9491eca6a3ab5fbc175bd07
Amar Zavery <amzavery@microsoft.com>
2018-09-07 21:25:05 -0700

    update examples with license header

--- client/examples/sendReceiveWithSPAuth.ts
+++ client/examples/sendReceiveWithSPAuth.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 import { EventHubClient, aadEventHubsAudience, EventPosition } from "../lib";
 import * as msrestAzure from "ms-rest-azure";
 import * as dotenv from "dotenv";
