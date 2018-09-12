6fc2d6b57c9c6790d4fdc5817e4e329d44c61e34
Amar Zavery <amzavery@microsoft.com>
2018-09-11 22:02:33 -0700

    misc updates

--- processor/lib/util/utils.ts
+++ processor/lib/util/utils.ts
@@ -1,30 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import * as AsyncLock from "async-lock";
 import * as log from "../log";
 import { StorageError } from "azure-storage";
 import { EPHDiagnosticInfo } from "../modelTypes";
-
-export interface AsyncLockOptions {
-  /**
-   * @property {number} [timeout] The max timeout. Default is: 0 (never timeout).
-   */
-  timeout?: number;
-  /**
-   * @property {number} [maxPending] Maximum pending tasks. Default is: 1000.
-   */
-  maxPending?: number;
-  /**
-   * @property {boolean} [domainReentrant] Whether lock can reenter in the same domain.
-   * Default is: false.
-   */
-  domainReentrant?: boolean;
-  /**
-   * @property {any} [Promise] Your implementation of the promise. Default is: global promise.
-   */
-  Promise?: any;
-}
 
 export interface EventHubConnectionStringModel {
   Endpoint: string;
@@ -55,19 +34,6 @@ export function parseConnectionString<T>(connectionString: string): ParsedOutput
     };
   }, {} as any);
 }
-
-/**
- * Gets a new instance of the async lock with desired settings.
- * @param {AsyncLockOptions} [options] The async lock options.
- */
-export function getNewAsyncLock(options?: AsyncLockOptions): AsyncLock {
-  return new AsyncLock(options);
-}
-
-/**
- * @constant {AsyncLock} defaultLock The async lock instance with default settings.
- */
-export const defaultLock: AsyncLock = new AsyncLock();
 
 export class Timeout {
 
