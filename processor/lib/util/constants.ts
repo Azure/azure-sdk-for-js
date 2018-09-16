18d4ccb65db654efe9a65c2e1dd0de48d703877d
Amar Zavery <amzavery@microsoft.com>
2018-09-16 01:28:20 -0700

    Ensure messages are checkpointed in order.

--- processor/lib/util/constants.ts
+++ processor/lib/util/constants.ts
@@ -28,5 +28,5 @@ export const leaseIdMismatchWithBlobOperation = "leaseidmismatchwithbloboperatio
 export const userAgentPrefix = "/js-event-processor-host";
 export const packageInfo = {
   name: "@azure/event-processor-host",
-  version: "1.0.0"
+  version: "1.0.2"
 };
