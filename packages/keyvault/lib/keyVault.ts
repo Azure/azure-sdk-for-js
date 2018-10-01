1392829a12b87f2e9e1b5b0b824ab10425f7859e
Kamil Pajdzik <kamip@microsoft.com>
2018-10-01 10:29:38 -0700

    Add KeyVault convenience layer

--- packages/keyvault/lib/keyVault.ts
+++ packages/keyvault/lib/keyVault.ts
@@ -45,17 +45,19 @@ import { KeyVaultClient as KeyVaultClientBase } from "./keyVaultClient"
  */
 export class KeyVaultClient extends KeyVaultClientBase {
     private _serviceClient: ServiceClient;
+    private _generateClientRequestId: boolean;
 
     constructor(credentials: any, options: any) {
         // convert credentials to KeyVaultCredentials if needed
         let keyVaultCredentials = credentials;
-        if (!(_isKeyVaultCredentials(credentials))) {
+        if (!(KeyVaultClient._isKeyVaultCredentials(credentials))) {
             keyVaultCredentials = new KeyVaultCredentials(credentials);
         }
 
         // ServiceClient constructor adds filter to the pipeline
         super(keyVaultCredentials, options);
         this._serviceClient = new ServiceClient(keyVaultCredentials);
+        this._generateClientRequestId = options.generateClientRequestId;
     }
 
     /**
@@ -70,22 +72,18 @@ export class KeyVaultClient extends KeyVaultClientBase {
      *
      * @param {object} [options.customHeaders] Headers that will be added to the request
      *
-     * @returns {Promise} A promise is returned
+     * @returns {Promise<HttpOperationResponse>} A promise is returned
      *
      * @resolve {HttpOperationResponse<CertificateListResult>} - The deserialized result object.
      *
      * @reject {Error} - The error object.
      */
-    getPendingCertificateSigningRequestWithHttpOperationResponse(vaultBaseUrl: string, certificateName: string, options: any) {
-        let self = this;
+    getPendingCertificateSigningRequestWithHttpOperationResponse(vaultBaseUrl: string, certificateName: string, options: any): Promise<HttpOperationResponse> {
         return new Promise((resolve, reject) => {
-            self._getPendingCertificateSigningRequest(vaultBaseUrl, certificateName, options, (err?: Error, result?: any, request?: WebResource, response?: any) => {
-                let httpOperationResponse: HttpOperationResponse = new HttpOperationResponse(request, response);
-                httpOperationResponse.bodyAsText = result;
-                if (err) { reject(err); }
-                else { resolve(httpOperationResponse); }
-                return;
-            });
+            this.getPendingCertificateSigningRequest(vaultBaseUrl, certificateName, options).then((response: HttpOperationResponse) => {
+                response.bodyAsText = (response.bodyAsText === "" ? response.bodyAsText : undefined);
+                resolve(response);
+            }, (error: Error) => reject(error));
         });
     }
 
@@ -101,192 +99,119 @@ export class KeyVaultClient extends KeyVaultClientBase {
      *
      * @param {object} [options.customHeaders] Headers that will be added to the
      * request
-     *
-     * @param {function} [optionalCallback] - The optional callback.
-     *
-     * @returns {function|Promise} If a callback was passed as the last parameter
-     * then it returns the callback else returns a Promise.
-     *
-     * {Promise} A promise is returned
-     *
-     *                      @resolve {CertificateOperation} - The deserialized result object.
-     *
-     *                      @reject {Error} - The error object.
-     *
-     * {function} optionalCallback(err, result, request, response)
-     *
-     *                      {Error}  err        - The Error object if an error occurred, null otherwise.
-     *
-     *                      {object} [result]   - The deserialized result object if an error did not occur.
-     *                      See {@link CertificateOperation} for more information.
-     *
-     *                      {object} [request]  - The HTTP Request object if an error did not occur.
-     *
-     *                      {stream} [response] - The HTTP Response stream if an error did not occur.
      */
-    getPendingCertificateSigningRequest(vaultBaseUrl: string, certificateName: string, options: any, optionalCallback?: (err?: Error, result?: any, request?: WebResource, response?: any) => void) {
-        if (!optionalCallback && typeof options === 'function') {
-            optionalCallback = options;
-            options = null;
-        }
-        if (!optionalCallback) {
-            return new Promise((resolve, reject) => {
-                this._getPendingCertificateSigningRequest(vaultBaseUrl, certificateName, options, (err?: Error, result?: any, request?: WebResource, response?: any) => {
-                    if (err) { reject(err); }
-                    else { resolve(result); }
-                    return;
-                });
-            });
-        } else {
-            return this._getPendingCertificateSigningRequest(vaultBaseUrl, certificateName, options, optionalCallback);
-        }
-    }
+    getPendingCertificateSigningRequest(vaultBaseUrl: string, certificateName: string, options: any): Promise<HttpOperationResponse> {
+        return new Promise((resolve, reject) => {
+            // Validate
+            try {
+                if (vaultBaseUrl === null || vaultBaseUrl === undefined || typeof vaultBaseUrl.valueOf() !== 'string') {
+                    throw new Error('vaultBaseUrl cannot be null or undefined and it must be of type string.');
+                }
+                if (certificateName === null || certificateName === undefined || typeof certificateName.valueOf() !== 'string') {
+                    throw new Error('certificateName cannot be null or undefined and it must be of type string.');
+                }
+                if (this.apiVersion === null || this.apiVersion === undefined || typeof this.apiVersion.valueOf() !== 'string') {
+                    throw new Error('this.apiVersion cannot be null or undefined and it must be of type string.');
+                }
+                if (this.acceptLanguage !== null && this.acceptLanguage !== undefined && typeof this.acceptLanguage.valueOf() !== 'string') {
+                    throw new Error('this.acceptLanguage must be of type string.');
+                }
+            } catch (error) {
+                return reject(error);
+            }
 
-    /**
-     * Gets the certificate operation response.
-     *
-     * @param {string} vaultBaseUrl The vault name, e.g.
-     * https://myvault.vault.azure.net
-     *
-     * @param {string} certificateName The name of the certificate
-     *
-     * @param {object} [options] Optional Parameters.
-     *
-     * @param {object} [options.customHeaders] Headers that will be added to the
-     * request
-     *
-     * @param {function} callback
-     *
-     * @returns {function} callback(err, result, request, response)
-     *
-     *                      {Error}  err        - The Error object if an error occurred, null otherwise.
-     *
-     *                      {object} [result]   - The deserialized result object.
-     *                      See {@link CertificateOperation} for more information.
-     *
-     *                      {object} [request]  - The HTTP Request object if an error did not occur.
-     *
-     *                      {stream} [response] - The HTTP Response stream if an error did not occur.
-     */
-    private _getPendingCertificateSigningRequest(vaultBaseUrl: string, certificateName: string, options: any, callback: (err?: Error, result?: any, request?: WebResource, response?: any) => void) {
-        if (!callback && typeof options === 'function') {
-            callback = options;
-            options = null;
-        }
-        if (!callback) {
-            throw new Error('callback cannot be null.');
-        }
-        // Validate
-        try {
-            if (vaultBaseUrl === null || vaultBaseUrl === undefined || typeof vaultBaseUrl.valueOf() !== 'string') {
-                throw new Error('vaultBaseUrl cannot be null or undefined and it must be of type string.');
+            // Construct URL
+            let requestUrl = this.baseUri +
+                '//certificates/{certificate-name}/pending';
+            requestUrl = requestUrl.replace('{vaultBaseUrl}', vaultBaseUrl);
+            requestUrl = requestUrl.replace('{certificate-name}', encodeURIComponent(certificateName));
+            let queryParameters = [];
+            queryParameters.push('api-version=' + encodeURIComponent(this.apiVersion));
+            if (queryParameters.length > 0) {
+                requestUrl += '?' + queryParameters.join('&');
             }
-            if (certificateName === null || certificateName === undefined || typeof certificateName.valueOf() !== 'string') {
-                throw new Error('certificateName cannot be null or undefined and it must be of type string.');
-            }
-            if (this.apiVersion === null || this.apiVersion === undefined || typeof this.apiVersion.valueOf() !== 'string') {
-                throw new Error('this.apiVersion cannot be null or undefined and it must be of type string.');
-            }
-            if (this.acceptLanguage !== null && this.acceptLanguage !== undefined && typeof this.acceptLanguage.valueOf() !== 'string') {
-                throw new Error('this.acceptLanguage must be of type string.');
-            }
-        } catch (error) {
-            return callback(error);
-        }
+            // trim all duplicate forward slashes in the url
+            let regex = /([^:]\/)\/+/gi;
+            requestUrl = requestUrl.replace(regex, '$1');
 
-        // Construct URL
-        let requestUrl = this.baseUri +
-            '//certificates/{certificate-name}/pending';
-        requestUrl = requestUrl.replace('{vaultBaseUrl}', vaultBaseUrl);
-        requestUrl = requestUrl.replace('{certificate-name}', encodeURIComponent(certificateName));
-        let queryParameters = [];
-        queryParameters.push('api-version=' + encodeURIComponent(this.apiVersion));
-        if (queryParameters.length > 0) {
-            requestUrl += '?' + queryParameters.join('&');
-        }
-        // trim all duplicate forward slashes in the url
-        let regex = /([^:]\/)\/+/gi;
-        requestUrl = requestUrl.replace(regex, '$1');
-
-        // Create HTTP transport objects
-        let httpRequest = new WebResource();
-        httpRequest.method = 'GET';
-        httpRequest.headers = new HttpHeaders();
-        httpRequest.url = requestUrl;
-        // Set Headers
-        if (this.generateClientRequestId) {
-            httpRequest.headers.set('x-ms-client-request-id', generateUuid());
-        }
-        if (this.acceptLanguage !== undefined && this.acceptLanguage !== null) {
-            httpRequest.headers.set('accept-language', this.acceptLanguage);
-        }
-        if (options) {
-            for (let headerName in options['customHeaders']) {
-                if (options['customHeaders'].hasOwnProperty(headerName)) {
-                    httpRequest.headers.set(headerName, options['customHeaders'][headerName]);
+            // Create HTTP transport objects
+            let httpRequest = new WebResource();
+            httpRequest.method = 'GET';
+            httpRequest.headers = new HttpHeaders();
+            httpRequest.url = requestUrl;
+            // Set Headers
+            if (this._generateClientRequestId) {
+                httpRequest.headers.set('x-ms-client-request-id', generateUuid());
+            }
+            if (this.acceptLanguage !== undefined && this.acceptLanguage !== null) {
+                httpRequest.headers.set('accept-language', this.acceptLanguage);
+            }
+            if (options) {
+                for (let headerName in options['customHeaders']) {
+                    if (options['customHeaders'].hasOwnProperty(headerName)) {
+                        httpRequest.headers.set(headerName, options['customHeaders'][headerName]);
+                    }
                 }
             }
-        }
-        httpRequest.headers.set('Content-Type', 'application/json; charset=utf-8');
-        httpRequest.headers.set('Accept', 'application/pkcs10');
-        httpRequest.body = null;
+            httpRequest.headers.set('Content-Type', 'application/json; charset=utf-8');
+            httpRequest.headers.set('Accept', 'application/pkcs10');
+            httpRequest.body = null;
 
-        this._serviceClient.sendRequest(httpRequest).then(response => {
-            if (response.status !== 200) {
-                const error = parseErrorFromResponse(httpRequest, response);
-                return callback(error);
-            }
+            this._serviceClient.sendRequest(httpRequest).then((response: HttpOperationResponse) => {
+                if (response.status !== 200) {
+                    const error = KeyVaultClient.parseErrorFromResponse(httpRequest, response);
+                    return reject(error);
+                }
 
-            const result = response.bodyAsText === "" ? response.bodyAsText : undefined;
-            return callback(undefined, result, httpRequest, response);
-
-        }).catch(err => callback(err));
+                resolve(response)
+            }).catch((error: Error) => reject(error));
+        })
     }
-}
 
-export function parseErrorFromResponse(request: WebResource,response: HttpOperationResponse): RestError {
-    const responseBody = response.bodyAsText;
+    static parseErrorFromResponse(request: WebResource, response: HttpOperationResponse): RestError {
+        const responseBody = response.bodyAsText;
 
-    let error = new RestError(responseBody!);
-    error.statusCode = response.status;
-    error.request = stripRequest(request);
-    error.response = stripResponse(response);
+        let error = new RestError(responseBody!);
+        error.statusCode = response.status;
+        error.request = stripRequest(request);
+        error.response = stripResponse(response);
 
-    if (!responseBody || responseBody === "") {
+        if (!responseBody || responseBody === "") {
+            return error;
+        }
+
+        let parsedErrorResponse;
+        try {
+            parsedErrorResponse = JSON.parse(responseBody);
+            if (parsedErrorResponse) {
+                let internalError = null;
+                if (parsedErrorResponse.error) internalError = parsedErrorResponse.error;
+                error.code = internalError ? internalError.code : parsedErrorResponse.code;
+                error.message = internalError ? internalError.message : parsedErrorResponse.message;
+            }
+            if (parsedErrorResponse !== null && parsedErrorResponse !== undefined) {
+                //let resultMapper = new this.models['KeyVaultError']().mapper();
+                //let resultMapper = new Mapper()
+                //error.body = this.deserialize(resultMapper, parsedErrorResponse, 'error.body');
+            }
+        } catch (defaultError) {
+            error.message = `Error "${defaultError.message}" occurred in deserializing the responseBody ` +
+                `- "${responseBody}" for the default response.`;
+        }
+
         return error;
     }
 
-    let parsedErrorResponse;
-    try {
-        parsedErrorResponse = JSON.parse(responseBody);
-        if (parsedErrorResponse) {
-            let internalError = null;
-            if (parsedErrorResponse.error) internalError = parsedErrorResponse.error;
-            error.code = internalError ? internalError.code : parsedErrorResponse.code;
-            error.message = internalError ? internalError.message : parsedErrorResponse.message;
-        }
-        if (parsedErrorResponse !== null && parsedErrorResponse !== undefined) {
-            //let resultMapper = new this.models['KeyVaultError']().mapper();
-            //let resultMapper = new Mapper()
-            //error.body = this.deserialize(resultMapper, parsedErrorResponse, 'error.body');
-        }
-    } catch (defaultError) {
-        error.message = `Error "${defaultError.message}" occurred in deserializing the responseBody ` +
-            `- "${responseBody}" for the default response.`;
+    /**
+     * @private
+     *
+     * It is possible for multiple instances of msRestAzure to exist. This method adds a backup check in case "instanceof" returns false.
+     *
+     * @param {object} credentials
+     */
+    private static _isKeyVaultCredentials(credentials: any): boolean {
+        return credentials instanceof KeyVaultCredentials || KeyVaultCredentials.name === credentials.constructor.name;
     }
-
-    return error;
-}
-
-/**
- * @private
- *
- * It is possible for multiple instances of msRestAzure to exist. This method adds a backup check in case "instanceof" returns false.
- *
- * @param {object} credentials
- */
-function _isKeyVaultCredentials(credentials: any) {
-    return credentials instanceof KeyVaultCredentials || KeyVaultCredentials.name === credentials.constructor.name;
 }
 
 /**
