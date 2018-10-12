5fa73d115ff77d1db214b953c4f96ba495a9df80
Dan Schulte <daschult@microsoft.com>
2018-10-12 12:20:26 -0700

    Remove script step that changes package.json main property

--- .scripts/local.ts
+++ .scripts/local.ts
@@ -1,5 +1,4 @@
 import * as dependencies from "./dependencies";
 import * as repository from "./repository";
 
-dependencies.updatePackageJsonMain(dependencies.getThisRepositoryFolderPath(), "./lib/msRest.ts");
 dependencies.updateLocalDependencies(repository.packageFolders, "local", dependencies.getLocalDependencyVersion);
