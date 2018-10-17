39219c54e24f72fac1988a35313fcb2effb6d66d
Amar Zavery <amzavery@microsoft.com>
2018-10-17 12:43:41 -0700

    Add support for servicebus response properties

--- lib/requestResponseLink.ts
+++ lib/requestResponseLink.ts
@@ -89,19 +89,32 @@ export class RequestResponseLink implements ReqResLink {
       options.timeoutInSeconds = 10;
     }
 
-    const sendRequestPromise: Promise<AmqpMessage> = new Promise<AmqpMessage>((resolve: any, reject: any) => {
+    const sendRequestPromise = () => new Promise<AmqpMessage>((resolve: any, reject: any) => {
       let waitTimer: any;
       let timeOver: boolean = false;
+      type NormalizedInfo = {
+        statusCode: number;
+        statusDescription: string;
+        errorCondition: string;
+      };
+
+      // Handle different variations of property names in responses emitted by EventHubs and ServiceBus.
+      const getCodeDescriptionAndError = (props: any): NormalizedInfo => {
+        if (!props) props = {};
+        return {
+          statusCode: (props[Constants.statusCode] || props.statusCode) as number,
+          statusDescription: (props[Constants.statusDescription] || props.statusDescription) as string,
+          errorCondition: (props[Constants.errorCondition] || props.errorCondition) as string
+        };
+      };
 
       const messageCallback = (context: EventContext) => {
         // remove the event listener as this will be registered next time when someone makes a request.
         this.receiver.removeListener(ReceiverEvents.message, messageCallback);
-        const code: number = context.message!.application_properties![Constants.statusCode];
-        const desc: string = context.message!.application_properties![Constants.statusDescription];
-        const errorCondition: string | undefined = context.message!.application_properties![Constants.errorCondition];
+        const info = getCodeDescriptionAndError(context.message!.application_properties);
         const responseCorrelationId = context.message!.correlation_id;
         log.reqres("[%s] %s response: ", this.connection.id, request.to || "$management", context.message);
-        if (code > 199 && code < 300) {
+        if (info.statusCode > 199 && info.statusCode < 300) {
           if (request.message_id === responseCorrelationId || request.correlation_id === responseCorrelationId) {
             if (!timeOver) {
               clearTimeout(waitTimer);
@@ -115,10 +128,10 @@ export class RequestResponseLink implements ReqResLink {
               this.connection.id, request.message_id, responseCorrelationId);
           }
         } else {
-          const condition = errorCondition || ConditionStatusMapper[code] || "amqp:internal-error";
+          const condition = info.errorCondition || ConditionStatusMapper[info.statusCode] || "amqp:internal-error";
           const e: AmqpError = {
             condition: condition,
-            description: desc
+            description: info.statusDescription
           };
           const error = translate(e);
           log.error(error);
@@ -145,7 +158,7 @@ export class RequestResponseLink implements ReqResLink {
       this.sender.send(request);
     });
     const config: RetryConfig<AmqpMessage> = {
-      operation: () => sendRequestPromise,
+      operation: sendRequestPromise,
       connectionId: this.connection.id,
       operationType: request.to && request.to === Constants.cbsEndpoint
         ? RetryOperationType.cbsAuth
