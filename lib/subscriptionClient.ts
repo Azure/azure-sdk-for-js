c4e324c93cf6b314e953d6e130eb8aef63bd32cc
Amar Zavery <amzavery@microsoft.com>
2018-10-30 17:00:25 -0700

    add relevant methods to subscription and topic client and update documentation.

--- lib/subscriptionClient.ts
+++ lib/subscriptionClient.ts
@@ -6,7 +6,7 @@ import { ConnectionContext } from "./connectionContext";
 import { ReceiveOptions, OnError, OnMessage } from ".";
 import { StreamingReceiver, ReceiveHandler, MessageHandlerOptions } from "./streamingReceiver";
 import { BatchingReceiver } from "./batchingReceiver";
-import { ServiceBusMessage } from "./serviceBusMessage";
+import { ServiceBusMessage, ReceivedMessageInfo } from "./serviceBusMessage";
 import { Client } from "./client";
 import { ReceiveMode } from "./messageReceiver";
 
@@ -20,13 +20,6 @@ export interface SubscriptionClientOptions {
    * Default: ReceiveMode.peekLock
    */
   receiveMode?: ReceiveMode;
-  /**
-   * @property {number} [maxConcurrentCalls] he maximum number of messages that should be
-   * processed concurrently while in peek lock mode. Once this limit has been reached, more
-   * messages will not be received until messages currently being processed have been settled.
-   * Default: 1
-   */
-  maxConcurrentCalls?: number;
 }
 
 export class SubscriptionClient extends Client {
@@ -48,10 +41,10 @@ export class SubscriptionClient extends Client {
    * Instantiates a client pointing to the ServiceBus Subscription given by this configuration.
    *
    * @constructor
-   * @param {string} topicPath The Topic path.
-   * @param {string} subscriptionName The Subscription name.
-   * @param {ConnectionContext} context The connection context to create the SubscriptionClient.
-   * @param {SubscriptionClientOptions} [options] The SubscriptionClient options.
+   * @param topicPath - The Topic path.
+   * @param subscriptionName - The Subscription name.
+   * @param context - The connection context to create the SubscriptionClient.
+   * @param [options] - The SubscriptionClient options.
    */
   constructor(topicPath: string, subscriptionName: string, context: ConnectionContext, options?: SubscriptionClientOptions) {
     super(`${topicPath}/Subscriptions/${subscriptionName}`, context);
@@ -64,9 +57,9 @@ export class SubscriptionClient extends Client {
   /**
    * Closes the AMQP connection to the ServiceBus Subscription for this client,
    * returning a promise that will be resolved when disconnection is completed.
-   * @returns {Promise<any>}
+   * @returns Promise<void>
    */
-  async close(): Promise<any> {
+  async close(): Promise<void> {
     try {
       if (this._context.namespace.connection && this._context.namespace.connection.isOpen()) {
         // Close the receiver.
@@ -88,13 +81,11 @@ export class SubscriptionClient extends Client {
    * Messages will be passed to the provided onMessage handler and error will be passed to the
    * provided onError handler.
    *
-   * @param {OnMessage} onMessage          The message handler to receive Message objects.
-   * @param {OnError} onError              The error handler to receive an error that occurs
-   * while receiving messages.
-   * @param {MessageHandlerOptions} [options]     Options for how you'd like to connect.
+   * @param onMessage - The message handler to receive Message objects.
+   * @param onError - The error handler to receive an error that occurs while receiving messages.
+   * @param [options] - Options for how you'd like to connect.
    *
-   * @returns {ReceiveHandler} ReceiveHandler - An object that provides a mechanism to stop
-   * receiving more messages.
+   * @returns ReceiveHandler - An object that provides a mechanism to stop receiving more messages.
    */
   receive(onMessage: OnMessage, onError: OnError, options?: MessageHandlerOptions): ReceiveHandler {
     if (!this._context.streamingReceiver ||
@@ -121,12 +112,12 @@ export class SubscriptionClient extends Client {
   /**
    * Receives a batch of Message objects from a ServiceBus Subscription for a given count and a
    * given max wait time in seconds, whichever happens first.
-   * @param {number} maxMessageCount        The maximum message count. Must be a value greater than 0.
-   * @param {number} [maxWaitTimeInSeconds] The maximum wait time in seconds for which the Receiver
+   * @param maxMessageCount - The maximum message count. Must be a value greater than 0.
+   * @param [maxWaitTimeInSeconds] - The maximum wait time in seconds for which the Receiver
    * should wait to receiver the said amount of messages. If not provided, it defaults to 60 seconds.
-   * @param {ReceiveOptions} [options]      Options for how you'd like to connect.
+   * @param [options] - Options for how you'd like to connect.
    *
-   * @returns {Promise<ServiceBusMessage[]>} A promise that resolves with an array of Message objects.
+   * @returns Promise<ServiceBusMessage[]> - A promise that resolves with an array of Message objects.
    */
   async receiveBatch(maxMessageCount: number, maxWaitTimeInSeconds?: number): Promise<ServiceBusMessage[]> {
     if (!this._context.batchingReceiver ||
@@ -154,5 +145,87 @@ export class SubscriptionClient extends Client {
         `and then call receiveBatch() again.`;
       throw new Error(msg);
     }
+  }
+
+  /**
+   * Fetches the next batch of active messages. The first call to `peek()` fetches the first
+   * active message for this client. Each subsequent call fetches the subsequent message in the
+   * entity.
+   *
+   * Unlike a `received` message, `peeked` message will not have lock token associated with it,
+   * and hence it cannot be `Completed/Abandoned/Deferred/Deadlettered/Renewed`. Also, unlike
+   * `receive() | receiveBatch()` this method will fetch even Deferred messages
+   * (but not Deadlettered message).
+   *
+   * It is especially important to keep in mind when attempting to recover deferred messages from
+   * the queue. A message for which the `expiresAtUtc` instant has passed is no longer eligible for
+   * regular retrieval by any other means, even when it's being returned by `peek()`. Returning
+   * these messages is deliberate, since `peek()` is a diagnostics tool reflecting the current
+   * state of the log.
+   *
+   * @param [messageCount] The number of messages to retrieve. Default value `1`.
+   * @returns Promise<ReceivedSBMessage[]>
+   */
+  async peek(messageCount?: number): Promise<ReceivedMessageInfo[]> {
+    return await this._context.managementClient!.peek(messageCount);
+  }
+
+  /**
+   * Peeks the desired number of messages from the specified sequence number.
+   * @param fromSequenceNumber The sequence number from where to read the message.
+   * @param [messageCount] The number of messages to retrieve. Default value `1`.
+   * @returns Promise<ReceivedSBMessage[]>
+   */
+  async peekBySequenceNumber(fromSequenceNumber: Long, messageCount?: number): Promise<ReceivedMessageInfo[]> {
+    return await this._context.managementClient!.peekBySequenceNumber(fromSequenceNumber, messageCount);
+  }
+
+  /**
+   * Renews the lock on the message. The lock will be renewed based on the setting specified on
+   * the queue.
+   *
+   * When a message is received in `PeekLock` mode, the message is locked on the server for this
+   * receiver instance for a duration as specified during the Queue/Subscription creation
+   * (LockDuration). If processing of the message requires longer than this duration, the
+   * lock needs to be renewed. For each renewal, it resets the time the message is locked by the
+   * LockDuration set on the Entity.
+   *
+   * @param lockTokenOrMessage - Lock token of the message or the message itself.
+   * @returns Promise<Date> - New lock token expiry date and time in UTC format.
+   */
+  async renewLock(lockTokenOrMessage: string | ServiceBusMessage): Promise<Date> {
+    return await this._context.managementClient!.renewLock(lockTokenOrMessage);
+  }
+
+  /**
+   * Receives a specific deferred message identified by `sequenceNumber` of the `Message`.
+   * @param sequenceNumber The sequence number of the message that will be received.
+   * @returns Promise<ServiceBusMessage | undefined>
+   * - Returns `Message` identified by sequence number.
+   * - Returns `undefined` if no such message is found.
+   * - Throws an error if the message has not been deferred.
+   */
+  async receiveDeferredMessage(sequenceNumber: Long): Promise<ServiceBusMessage | undefined> {
+    if (this.receiveMode !== ReceiveMode.peekLock) {
+      throw new Error("The operation is only supported in 'PeekLock' receive mode.");
+    }
+    return await this._context.managementClient!.receiveDeferredMessage(sequenceNumber,
+      this.receiveMode);
+  }
+
+  /**
+   * Receives a list of deferred messages identified by `sequenceNumbers`.
+   * @param sequenceNumbers A list containing the sequence numbers to receive.
+   * @returns Promise<ReceivedSBMessage[]>
+   * - Returns a list of messages identified by the given sequenceNumbers.
+   * - Returns an empty list if no messages are found.
+   * - Throws an error if the messages have not been deferred.
+   */
+  async receiveDeferredMessages(sequenceNumbers: Long[]): Promise<ReceivedMessageInfo[]> {
+    if (this.receiveMode !== ReceiveMode.peekLock) {
+      throw new Error("The operation is only supported in 'PeekLock' receive mode.");
+    }
+    return await this._context.managementClient!.receiveDeferredMessages(sequenceNumbers,
+      this.receiveMode);
   }
 }
--- lib/topicClient.ts
+++ lib/topicClient.ts
@@ -1,24 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+import * as Long from "long";
 import * as log from "./log";
 import { Delivery } from "rhea-promise";
 import { ConnectionContext } from "./connectionContext";
 import { MessageSender } from "./messageSender";
 import { SendableMessageInfo } from "./serviceBusMessage";
 import { Client } from "./client";
+import { ScheduleMessage } from "./managementClient";
 
 
 
-
+/**
+ * Describes the TopicClient that is used to interact with a ServiceBus Topic.
+ * @class TopicClient
+ */
 export class TopicClient extends Client {
   /**
    * Instantiates a client pointing to the ServiceBus Topic given by this configuration.
    *
    * @constructor
-   * @param {string} name The topic name.
-   * @param {ConnectionContext} context The connection context to create the TopicClient.
-   * @param {TopicClientOptions} [options] The TopicClient options.
+   * @param name - The topic name.
+   * @param context - The connection context to create the TopicClient.
+   * @param [options] - The TopicClient options.
    */
   constructor(name: string, context: ConnectionContext) {
     super(name, context);
@@ -49,8 +54,8 @@ export class TopicClient extends Client {
   /**
    * Sends the given message to the ServiceBus Topic.
    *
-   * @param {any} data  Message to send.  Will be sent as UTF8-encoded JSON string.
-   * @returns {Promise<Delivery>} Promise<Delivery>
+   * @param data - Message to send.  Will be sent as UTF8-encoded JSON string.
+   * @returns Promise<Delivery>
    */
   async send(data: SendableMessageInfo): Promise<Delivery> {
     const sender = MessageSender.create(this._context);
@@ -58,17 +63,73 @@ export class TopicClient extends Client {
   }
 
   /**
-   * Send a batch of Message to the ServiceBus Topic. The "message_annotations",
+   * Sends a batch of Message to the ServiceBus Topic. The "message_annotations",
    * "application_properties" and "properties" of the first message will be set as that of
    * the envelope (batch message).
    *
-   * @param {Array<Message>} datas  An array of Message objects to be sent in a Batch
-   * message.
+   * @param datas  An array of Message objects to be sent in a Batch message.
    *
-   * @return {Promise<Delivery>} Promise<Delivery>
+   * @return Promise<Delivery>
    */
   async sendBatch(datas: SendableMessageInfo[]): Promise<Delivery> {
     const sender = MessageSender.create(this._context);
     return await sender.sendBatch(datas);
+  }
+
+  /**
+   * Schedules a message to appear on Service Bus at a later time.
+   *
+   * @param message - The message that needs to be scheduled.
+   * @param scheduledEnqueueTimeUtc - The UTC time at which the message should be available
+   * for processing.
+   * @returns Promise<Long> - The sequence number of the message that was
+   * scheduled. Please save the `Long` type as-is in your application. Do not convert it to a
+   * number as that may cause loss of precision, since JS only supports 53 bit numbers.
+   * `Long` type provides methods for mathematical operations.
+   * If you want to save it to a log file, then save the stringifed form
+   * `const result = Long.toString();`. When deserializing it, please use
+   * `Long.fromString("result");`. This will ensure that precision is preserved.
+   */
+  async scheduleMessage(message: SendableMessageInfo, scheduledEnqueueTimeUtc: Date): Promise<Long> {
+    const scheduleMessages: ScheduleMessage[] = [
+      { message: message, scheduledEnqueueTimeUtc: scheduledEnqueueTimeUtc }
+    ];
+    const result = await this._context.managementClient!.scheduleMessages(scheduleMessages);
+    return result[0];
+  }
+
+  /**
+   * Schedules a message to appear on a servicebus topic at a later time.
+   *
+   * @param message - Message that needs to be scheduled.
+   * @param scheduledEnqueueTimeUtc - The UTC time at which the message should be available
+   * for processing.
+   * @returns Promise<Long[]> - The sequence numbers of messages that were scheduled. Please
+   * save the `Long` type as-is in your application. Do not convert it to a number as that may
+   * cause loss of precision, since JS only supports 53 bit numbers. `Long` type provides methods
+   * for mathematical operations. If you want to save it to a log file, then save the stringifed
+   * form `const result = Long.toString();`. When deserializing it, please use
+   * `Long.fromString("result");`. This will ensure that precision is preserved.
+   */
+  async scheduleMessages(messages: ScheduleMessage[]): Promise<Long[]> {
+    return await this._context.managementClient!.scheduleMessages(messages);
+  }
+
+  /**
+   * Cancels a message that was scheduled to appear on a servicebus topic.
+   * @param sequenceNumber - The sequence number of the message to be cancelled.
+   * @returns Promise<void>
+   */
+  async cancelScheduledMessage(sequenceNumber: Long): Promise<void> {
+    return await this._context.managementClient!.cancelScheduledMessages([sequenceNumber]);
+  }
+
+  /**
+   * Cancels an array of messages that were scheduled to appear on a servicebus topic.
+   * @param sequenceNumbers - An Array of sequence numbers of the message to be cancelled.
+   * @returns Promise<void>
+   */
+  async cancelScheduledMessages(sequenceNumbers: Long[]): Promise<void> {
+    return await this._context.managementClient!.cancelScheduledMessages(sequenceNumbers);
   }
 }
