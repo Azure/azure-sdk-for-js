ab09348000e39307eb0a05d7616e95f716ba88ff
Kamil Pajdzik <41709775+kpajdzik@users.noreply.github.com>
2018-11-01 10:25:35 -0700

    Add missing user-agent export (#75)

--- lib/msRestAzure.ts
+++ lib/msRestAzure.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-export { AzureServiceClient, AzureServiceClientOptions } from "./azureServiceClient";
+export { AzureServiceClient, AzureServiceClientOptions, getDefaultUserAgentValue } from "./azureServiceClient";
 export { BaseResource, BaseResourceMapper } from "./baseResource";
 export { CloudError, CloudErrorMapper } from "./cloudError";
 export { CognitiveServicesCredentials } from "./credentials/cognitiveServicesCredentials";
