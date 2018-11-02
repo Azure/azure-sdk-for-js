4ffa767aa7540bec66f018d9281a3b64ca11316c
Kamil Pajdzik <kamip@microsoft.com>
2018-11-02 18:30:56 +0000

    Regenerated "@azure/arm-containerinstance" SDK.

--- packages/@azure/arm-containerinstance/lib/containerInstanceManagementClientContext.ts
+++ packages/@azure/arm-containerinstance/lib/containerInstanceManagementClientContext.ts
@@ -38,6 +38,11 @@ export class ContainerInstanceManagementClientContext extends msRestAzure.AzureS
     if (!options) {
       options = {};
     }
+    if(!options.userAgent) {
+      const defaultUserAgent = msRestAzure.getDefaultUserAgentValue();
+      options.userAgent = `${packageName}/${packageVersion} ${defaultUserAgent}`;
+    }
+
     super(credentials, options);
 
     this.apiVersion = '2018-10-01';
@@ -48,7 +53,6 @@ export class ContainerInstanceManagementClientContext extends msRestAzure.AzureS
     this.credentials = credentials;
     this.subscriptionId = subscriptionId;
 
-    this.addUserAgentInfo(`${packageName}/${packageVersion}`);
     if(options.acceptLanguage !== null && options.acceptLanguage !== undefined) {
       this.acceptLanguage = options.acceptLanguage;
     }
