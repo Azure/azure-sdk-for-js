e4abb99311bdf8fbea156e723d29aaedfb1339e6
Amar Zavery <amzavery@microsoft.com>
2018-11-05 17:27:58 -0800

    Fix the issue of "Initializers are not allowed in ambient contexts" for 3.0.x version of TS

--- lib/restError.ts
+++ lib/restError.ts
@@ -5,9 +5,9 @@ import { HttpOperationResponse } from "./httpOperationResponse";
 import { WebResource } from "./webResource";
 
 export class RestError extends Error {
-  static readonly REQUEST_SEND_ERROR = "REQUEST_SEND_ERROR";
-  static readonly REQUEST_ABORTED_ERROR = "REQUEST_ABORTED_ERROR";
-  static readonly PARSE_ERROR = "PARSE_ERROR";
+  static readonly REQUEST_SEND_ERROR: string = "REQUEST_SEND_ERROR";
+  static readonly REQUEST_ABORTED_ERROR: string = "REQUEST_ABORTED_ERROR";
+  static readonly PARSE_ERROR: string = "PARSE_ERROR";
 
   code?: string;
   statusCode?: number;
