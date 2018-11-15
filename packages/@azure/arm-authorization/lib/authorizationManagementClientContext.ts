d71692bfdd83a6f455fc53dc30aa31f7b88f36f9
Kamil Pajdzik <kamip@microsoft.com>
2018-11-15 08:27:33 -0800

    Regenerated "@azure/arm-authorization" SDK.

--- packages/@azure/arm-authorization/lib/authorizationManagementClientContext.ts
+++ packages/@azure/arm-authorization/lib/authorizationManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-authorization";
-const packageVersion = "0.1.0";
+const packageVersion = "0.2.0";
 
 export class AuthorizationManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
