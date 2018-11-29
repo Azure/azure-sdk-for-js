4f95ccfea0e4fba7a23c935c64a961202e2b551c
Ramya Achutha Rao <ramyar@microsoft.com>
2018-11-28 18:02:02 -0800

    Add descriptor codes for rules and filters

--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -106,3 +106,9 @@ export const operations = {
   removeRule: "com.microsoft:remove-rule",
   enumerateRules: "com.microsoft:enumerate-rules"
 };
+export const descriptorCodes = {
+  ruleDescriptionList: 1335734829060,
+  sqlFilterList: 83483426822,
+  correlationFilterList: 83483426825,
+  sqlRuleActionList: 1335734829062
+}
