d22884647506c315e47c1692a882008f4b641caa
Ramya Achutha Rao <ramyar@microsoft.com>
2018-11-30 16:04:27 -0800

    Add boolean filters

--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -110,5 +110,7 @@ export const descriptorCodes = {
   ruleDescriptionList: 1335734829060,
   sqlFilterList: 83483426822,
   correlationFilterList: 83483426825,
-  sqlRuleActionList: 1335734829062
-}
+  sqlRuleActionList: 1335734829062,
+  trueFilterList: 83483426823,
+  falseFilterList: 83483426824
+};
