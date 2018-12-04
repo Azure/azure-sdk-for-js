47f45ac8c067d430e03f69cbec741c9ca1072e88
Dan Schulte <daschult@microsoft.com>
2018-12-04 15:26:58 -0800

    Update @azure/arm-logic to 4.2.0

--- packages/@azure/arm-logic/lib/models/integrationAccountPartnersMappers.ts
+++ packages/@azure/arm-logic/lib/models/integrationAccountPartnersMappers.ts
@@ -102,6 +102,10 @@ export {
   ArtifactProperties,
   BatchConfigurationProperties,
   BatchReleaseCriteria,
-  BatchConfiguration
+  BatchConfiguration,
+  RequestHistory,
+  RequestHistoryProperties,
+  Request,
+  Response
 } from "../models/mappers";
 
