75947b3372b7750209253082a6db959d56ca3c79
Sai Harsha Nalluru <sanallur@microsoft.com>
2018-12-11 12:44:59 -0800

    Added template folder from azure-sdk-js

---
+++ template/src/index.ts
@@ -0,0 +1,29 @@
+// This node built-in must be shimmed for the browser.
+import EventEmitter from "events";
+// This is a node dependency that needs to be replaced with a
+// different implementation in the browser.
+import print from "./print";
+export { print };
+
+// this is a utility function from a library that should be external
+// for both node and web
+import { isNode } from "ms-rest-js";
+
+// exporting some value from a dependency
+export { URLBuilder } from "ms-rest-js";
+
+export function createEventEmitter() {
+  // use event emitter
+  const e = new EventEmitter();
+
+  // Dynamic Node and browser-specific code
+  if (isNode) {
+    console.log("Node 👊");
+  } else {
+    console.log("Browser 👊");
+  }
+
+  print("Created event emitter");
+
+  return e;
+}
