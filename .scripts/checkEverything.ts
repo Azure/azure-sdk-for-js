fc82801bcb5c3f85ccc9f8b1401865bfa379c9be
Dan Schulte <daschult@microsoft.com>
2019-01-03 09:39:37 -0800

    Did a little more refactoring

--- .scripts/checkEverything.ts
+++ .scripts/checkEverything.ts
@@ -1,24 +1,15 @@
-import { checkEverything, contains, fileExistsSync, getArgument, getChildFolderPaths, getDefaultLogger, getName, gitDiff, GitDiffResult, gitStatus, GitStatusResult, joinPath, Logger, normalize, resolvePath } from "@ts-common/azure-js-dev-tools";
+import { checkEverything, contains, getArgument, getDefaultLogger, gitDiff, GitDiffResult, gitStatus, GitStatusResult, joinPath, Logger, normalize, resolvePath } from "@ts-common/azure-js-dev-tools";
 import * as path from "path";
+import { getPackageFolderPaths } from "./common";
 
 const logger: Logger = getDefaultLogger();
-
-const headReference: string | undefined = getArgument("head-reference", { environmentVariableName: "headReference" });
-const baseReference: string | undefined = getArgument("base-reference", { environmentVariableName: "baseReference" });
-
 const changedFiles: string[] = [];
 
-let packBaseReference: string | undefined = baseReference;
-if (!packBaseReference) {
-  packBaseReference = "master";
-  logger.logInfo(`No base-reference argument specified on command line or in environment variables. Defaulting to "${packBaseReference}".`);
-}
-
-let packHeadReference: string | undefined = headReference;
-if (!packHeadReference) {
+let headReference: string | undefined = getArgument("head-reference", { environmentVariableName: "headReference" });
+if (!headReference) {
   const statusResult: GitStatusResult = gitStatus();
-  packHeadReference = statusResult.localBranch!;
-  logger.logInfo(`No head-reference argument specified on command line or in environment variables. Defaulting to "${packHeadReference}".`);
+  headReference = statusResult.localBranch!;
+  logger.logInfo(`No head-reference argument specified on command line or in environment variables. Defaulting to "${headReference}".`);
 
   const modifiedFiles: string[] | undefined = statusResult.modifiedFiles;
   if (modifiedFiles) {
@@ -26,11 +17,17 @@ if (!packHeadReference) {
   }
 }
 
-if (packBaseReference !== packHeadReference) {
-  const diffResult: GitDiffResult = gitDiff(packBaseReference, packHeadReference);
+let baseReference: string | undefined = getArgument("base-reference", { environmentVariableName: "baseReference" });
+if (!baseReference) {
+  baseReference = "master";
+  logger.logInfo(`No base-reference argument specified on command line or in environment variables. Defaulting to "${baseReference}".`);
+}
+
+if (baseReference !== headReference) {
+  const diffResult: GitDiffResult = gitDiff(baseReference, headReference);
   changedFiles.push(...diffResult.filesChanged);
   if (!changedFiles || changedFiles.length === 0) {
-    logger.logInfo(`Found no changes between "${packBaseReference}" and "${packHeadReference}".`);
+    logger.logInfo(`Found no changes between "${baseReference}" and "${headReference}".`);
   } else {
     logger.logVerbose(`Found the following changed files`)
     for (const changedFilePath of changedFiles) {
@@ -39,14 +36,9 @@ if (packBaseReference !== packHeadReference) {
   }
 }
 
-const folderNamesToIgnore: string[] = ["node_modules"];
 const repositoryFolderPath: string = resolvePath(__dirname, "..");
 const packagesFolderPath: string = joinPath(repositoryFolderPath, "packages");
-const packageFolderPaths: string[] | undefined = getChildFolderPaths(packagesFolderPath, {
-  recursive: true,
-  condition: (folderPath: string) => fileExistsSync(joinPath(folderPath, "package.json")),
-  folderCondition: (folderPath: string) => !contains(folderNamesToIgnore, getName(folderPath))
-});
+const packageFolderPaths: string[] | undefined = getPackageFolderPaths(packagesFolderPath);
 
 let exitCode: number = 0;
 if (!packageFolderPaths) {
