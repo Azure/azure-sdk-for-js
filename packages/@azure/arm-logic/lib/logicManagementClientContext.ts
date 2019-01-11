7e8d576db5ef845579a71747e5fb776a035a8420
Kamil Pajdzik <kamip@microsoft.com>
2019-01-10 17:59:34 -0800

    Bump the version to 6.0.0

--- packages/@azure/arm-logic/lib/logicManagementClientContext.ts
+++ packages/@azure/arm-logic/lib/logicManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-logic";
-const packageVersion = "5.0.0";
+const packageVersion = "6.0.0";
 
 export class LogicManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
