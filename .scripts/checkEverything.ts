11b35172dd31befc7d0dee7ebacbc5754c3ab43f
Dan Schulte <daschult@microsoft.com>
2019-01-15 12:53:12 -0800

    Add checkConstantsVersion.ts

--- .scripts/checkEverything.ts
+++ .scripts/checkEverything.ts
@@ -1,7 +1,12 @@
 import { checkEverything } from "@ts-common/azure-js-dev-tools";
+import { checkConstantsVersion } from "./checkConstantsVersion";
 
 checkEverything({
   checkForSkipCallsOptions: {
     skipIsWarning: true
+  },
+  additionalChecks: {
+    name: "Constants.ts Version",
+    check: checkConstantsVersion
   }
 });
