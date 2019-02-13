8eb89d186d518eeb97028e771607acf06d5760bc
Jeremy Meng <yumeng@microsoft.com>
2019-02-13 11:25:54 -0800

    Update Copyright header (#197)

--- client/lib/batchingReceiver.ts
+++ client/lib/batchingReceiver.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
+// Licensed under the MIT License.
 
 import { ReceiverEvents, EventContext, OnAmqpEvent, SessionEvents } from "rhea-promise";
 import { translate, Func, Constants, MessagingError } from "@azure/amqp-common";
