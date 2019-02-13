71e79917d49c9bb32fd98bac0f2e23c4d75ca8e2
Jeremy Meng <yumeng@microsoft.com>
2019-02-13 11:25:54 -0800

    Update Copyright header (#197)

--- processor/lib/blobService.ts
+++ processor/lib/blobService.ts
@@ -1,5 +1,5 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 import { Dictionary } from "@azure/event-hubs";
 import { createBlobService, BlobService as StorageBlobService, ServiceResponse } from "azure-storage";
--- testhub/cli.ts
+++ testhub/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 /* tslint:disable */
 
 import * as yargs from "yargs";
