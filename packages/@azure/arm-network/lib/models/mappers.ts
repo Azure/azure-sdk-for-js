fcc9a01e1e4a8239179137b817c82f6ce3b65c93
Kamil Pajdzik <41709775+kpajdzik@users.noreply.github.com>
2019-02-22 16:15:19 -0800

    Regenerate @azure/arm-network package (#1327)

--- packages/@azure/arm-network/lib/models/mappers.ts
+++ packages/@azure/arm-network/lib/models/mappers.ts
@@ -3031,6 +3031,40 @@ export const ApplicationGatewayRequestRoutingRule: msRest.CompositeMapper = {
   }
 };
 
+export const ApplicationGatewayRewriteRuleCondition: msRest.CompositeMapper = {
+  serializedName: "ApplicationGatewayRewriteRuleCondition",
+  type: {
+    name: "Composite",
+    className: "ApplicationGatewayRewriteRuleCondition",
+    modelProperties: {
+      variable: {
+        serializedName: "variable",
+        type: {
+          name: "String"
+        }
+      },
+      pattern: {
+        serializedName: "pattern",
+        type: {
+          name: "String"
+        }
+      },
+      ignoreCase: {
+        serializedName: "ignoreCase",
+        type: {
+          name: "Boolean"
+        }
+      },
+      negate: {
+        serializedName: "negate",
+        type: {
+          name: "Boolean"
+        }
+      }
+    }
+  }
+};
+
 export const ApplicationGatewayHeaderConfiguration: msRest.CompositeMapper = {
   serializedName: "ApplicationGatewayHeaderConfiguration",
   type: {
@@ -3097,6 +3131,24 @@ export const ApplicationGatewayRewriteRule: msRest.CompositeMapper = {
         serializedName: "name",
         type: {
           name: "String"
+        }
+      },
+      ruleSequence: {
+        serializedName: "ruleSequence",
+        type: {
+          name: "Number"
+        }
+      },
+      conditions: {
+        serializedName: "conditions",
+        type: {
+          name: "Sequence",
+          element: {
+            type: {
+              name: "Composite",
+              className: "ApplicationGatewayRewriteRuleCondition"
+            }
+          }
         }
       },
       actionSet: {
@@ -3491,6 +3543,15 @@ export const ApplicationGatewayAutoscaleConfiguration: msRest.CompositeMapper = 
         required: true,
         serializedName: "minCapacity",
         constraints: {
+          InclusiveMinimum: 0
+        },
+        type: {
+          name: "Number"
+        }
+      },
+      maxCapacity: {
+        serializedName: "maxCapacity",
+        constraints: {
           InclusiveMinimum: 2
         },
         type: {
@@ -3841,69 +3902,6 @@ export const ApplicationGateway: msRest.CompositeMapper = {
         type: {
           name: "Composite",
           className: "ManagedServiceIdentity"
-        }
-      }
-    }
-  }
-};
-
-export const ApplicationGatewayAvailableServerVariablesResult: msRest.CompositeMapper = {
-  serializedName: "ApplicationGatewayAvailableServerVariablesResult",
-  type: {
-    name: "Composite",
-    className: "ApplicationGatewayAvailableServerVariablesResult",
-    modelProperties: {
-      value: {
-        serializedName: "value",
-        type: {
-          name: "Sequence",
-          element: {
-            type: {
-              name: "String"
-            }
-          }
-        }
-      }
-    }
-  }
-};
-
-export const ApplicationGatewayAvailableRequestHeadersResult: msRest.CompositeMapper = {
-  serializedName: "ApplicationGatewayAvailableRequestHeadersResult",
-  type: {
-    name: "Composite",
-    className: "ApplicationGatewayAvailableRequestHeadersResult",
-    modelProperties: {
-      value: {
-        serializedName: "value",
-        type: {
-          name: "Sequence",
-          element: {
-            type: {
-              name: "String"
-            }
-          }
-        }
-      }
-    }
-  }
-};
-
-export const ApplicationGatewayAvailableResponseHeadersResult: msRest.CompositeMapper = {
-  serializedName: "ApplicationGatewayAvailableResponseHeadersResult",
-  type: {
-    name: "Composite",
-    className: "ApplicationGatewayAvailableResponseHeadersResult",
-    modelProperties: {
-      value: {
-        serializedName: "value",
-        type: {
-          name: "Sequence",
-          element: {
-            type: {
-              name: "String"
-            }
-          }
         }
       }
     }
@@ -4814,6 +4812,12 @@ export const AzureFirewall: msRest.CompositeMapper = {
       },
       provisioningState: {
         serializedName: "properties.provisioningState",
+        type: {
+          name: "String"
+        }
+      },
+      threatIntelMode: {
+        serializedName: "properties.threatIntelMode",
         type: {
           name: "String"
         }
@@ -11297,6 +11301,80 @@ export const VirtualNetworkUsage: msRest.CompositeMapper = {
         serializedName: "unit",
         type: {
           name: "String"
+        }
+      }
+    }
+  }
+};
+
+export const NetworkIntentPolicy: msRest.CompositeMapper = {
+  serializedName: "NetworkIntentPolicy",
+  type: {
+    name: "Composite",
+    className: "NetworkIntentPolicy",
+    modelProperties: {
+      ...Resource.type.modelProperties,
+      etag: {
+        serializedName: "etag",
+        type: {
+          name: "String"
+        }
+      }
+    }
+  }
+};
+
+export const NetworkIntentPolicyConfiguration: msRest.CompositeMapper = {
+  serializedName: "NetworkIntentPolicyConfiguration",
+  type: {
+    name: "Composite",
+    className: "NetworkIntentPolicyConfiguration",
+    modelProperties: {
+      networkIntentPolicyName: {
+        serializedName: "networkIntentPolicyName",
+        type: {
+          name: "String"
+        }
+      },
+      sourceNetworkIntentPolicy: {
+        serializedName: "sourceNetworkIntentPolicy",
+        type: {
+          name: "Composite",
+          className: "NetworkIntentPolicy"
+        }
+      }
+    }
+  }
+};
+
+export const PrepareNetworkPoliciesRequest: msRest.CompositeMapper = {
+  serializedName: "PrepareNetworkPoliciesRequest",
+  type: {
+    name: "Composite",
+    className: "PrepareNetworkPoliciesRequest",
+    modelProperties: {
+      serviceName: {
+        serializedName: "serviceName",
+        type: {
+          name: "String"
+        }
+      },
+      resourceGroupName: {
+        serializedName: "resourceGroupName",
+        type: {
+          name: "String"
+        }
+      },
+      networkIntentPolicyConfigurations: {
+        serializedName: "networkIntentPolicyConfigurations",
+        type: {
+          name: "Sequence",
+          element: {
+            type: {
+              name: "Composite",
+              className: "NetworkIntentPolicyConfiguration"
+            }
+          }
         }
       }
     }
