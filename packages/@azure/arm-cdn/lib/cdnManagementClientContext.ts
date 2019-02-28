f467c37b70b4730c79986e15e7615a0d4c79a6df
Kamil Pajdzik <kamip@microsoft.com>
2019-02-28 13:27:29 -0800

    Update the version in the context

--- packages/@azure/arm-cdn/lib/cdnManagementClientContext.ts
+++ packages/@azure/arm-cdn/lib/cdnManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-cdn";
-const packageVersion = "4.1.0";
+const packageVersion = "4.2.0";
 
 export class CdnManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
