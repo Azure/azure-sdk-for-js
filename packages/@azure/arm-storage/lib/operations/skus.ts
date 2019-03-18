9ba1c94b500dc7865bc7654e2cc10c89a3ffbd7a
Kamil Pajdzik <kamip@microsoft.com>
2019-03-18 10:21:36 -0700

    Regenerate @azure/arm-storage package

--- packages/@azure/arm-storage/lib/operations/skus.ts
+++ packages/@azure/arm-storage/lib/operations/skus.ts
@@ -60,7 +60,7 @@ const listOperationSpec: msRest.OperationSpec = {
     Parameters.subscriptionId
   ],
   queryParameters: [
-    Parameters.apiVersion0
+    Parameters.apiVersion
   ],
   headerParameters: [
     Parameters.acceptLanguage
