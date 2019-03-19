c7e7de38926860fb72e78da357b934f3e81caf4d
Kamil Pajdzik <41709775+kpajdzik@users.noreply.github.com>
2019-03-19 12:33:27 -0700

    Generate @azure/arm-network package (#1640)
    * Generate @azure/arm-network package
    
    * Update version to 12.1.0

--- packages/@azure/arm-network/lib/models/expressRouteCircuitsMappers.ts
+++ packages/@azure/arm-network/lib/models/expressRouteCircuitsMappers.ts
@@ -124,6 +124,8 @@ export {
   LocalNetworkGateway,
   ManagedServiceIdentity,
   ManagedServiceIdentityUserAssignedIdentitiesValue,
+  MatchCondition,
+  MatchVariable,
   NetworkIntentPolicy,
   NetworkInterface,
   NetworkInterfaceDnsSettings,
@@ -141,6 +143,8 @@ export {
   P2SVpnServerConfigVpnClientRootCertificate,
   PatchRouteFilter,
   PatchRouteFilterRule,
+  PeerExpressRouteCircuitConnection,
+  PolicySettings,
   Probe,
   ProtocolCustomSettingsFormat,
   PublicIPAddress,
@@ -184,5 +188,7 @@ export {
   VpnClientRootCertificate,
   VpnConnection,
   VpnGateway,
-  VpnSite
+  VpnSite,
+  WebApplicationFirewallCustomRule,
+  WebApplicationFirewallPolicy
 } from "../models/mappers";
