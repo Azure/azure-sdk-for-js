cfd8205662102c1f3ce2c0efe9914792a35f0f80
Jeremy Meng <yumeng@microsoft.com>
2019-03-21 15:16:39 -0700

    Mark url and util as external

--- packages/@azure/keyvault/rollup.config.js
+++ packages/@azure/keyvault/rollup.config.js
@@ -1,5 +1,4 @@
 import nodeResolve from "rollup-plugin-node-resolve";
-import builtins from "rollup-plugin-node-builtins"
 /**
  * @type {import('rollup').RollupFileOptions}
  */
@@ -17,7 +16,7 @@ const banner = [
 
 const config = {
   input: './dist-esm/lib/index.js',
-  external: ["@azure/ms-rest-js", "@azure/ms-rest-azure-js"],
+  external: ["@azure/ms-rest-js", "@azure/ms-rest-azure-js", "url", "util"],
   output: {
     file: "./dist/index.js",
     format: "umd",
@@ -25,12 +24,13 @@ const config = {
     sourcemap: true,
     globals: {
       "@azure/ms-rest-js": "msRest",
-      "@azure/ms-rest-azure-js": "msRestAzure"
+      "@azure/ms-rest-azure-js": "msRestAzure",
+      "url": "url",
+      "util": "util"
     },
     banner: banner
   },
   plugins: [
-    builtins(),
     nodeResolve({ module: true })
   ]
 };
