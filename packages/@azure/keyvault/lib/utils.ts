11f20a50bfe9e1d521136d7ead5164bc1485f50a
Jeremy Meng <yumeng@microsoft.com>
2019-03-21 21:14:31 -0700

    Replace util.format with template strings

--- packages/@azure/keyvault/lib/utils.ts
+++ packages/@azure/keyvault/lib/utils.ts
@@ -1,6 +1,5 @@
 import { ParsedKeyVaultEntityIdentifier } from "./secretsModels";
 import * as url from "url";
-import * as util from "util";
 
 export function parseKeyvaultIdentifier(
   collection: string,
@@ -19,7 +18,7 @@ export function parseKeyvaultIdentifier(
     baseUri = url.parse(identifier, true, true);
   } catch (e) {
     throw new Error(
-      util.format("Invalid %s identifier: %s. Not a valid URI", collection, identifier)
+      `Invalid ${collection} identifier: ${identifier}. Not a valid URI`
     );
   }
 
@@ -27,28 +26,17 @@ export function parseKeyvaultIdentifier(
   var segments = (baseUri.pathname || "").split("/");
   if (segments.length !== 3 && segments.length !== 4) {
     throw new Error(
-      util.format(
-        "Invalid %s identifier: %s. Bad number of segments: %d",
-        collection,
-        identifier,
-        segments.length
-      )
+      `Invalid ${collection} identifier: ${identifier}. Bad number of segments: ${segments.length}`
     );
   }
 
   if (collection !== segments[1]) {
     throw new Error(
-      util.format(
-        'Invalid %s identifier: %s. segment [1] should be "%s", found "%s"',
-        collection,
-        identifier,
-        collection,
-        segments[1]
-      )
+      `Invalid ${collection} identifier: ${identifier}. segment [1] should be "${collection}", found "${segments[1]}"`,
     );
   }
 
-  var vaultUrl = util.format("%s//%s", baseUri.protocol, baseUri.host);
+  var vaultUrl = `${baseUri.protocol}//${baseUri.host}`;
   var name = segments[2];
   var version = segments.length === 4 ? segments[3] : undefined;
   return {
