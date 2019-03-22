ca7f25b43affb8f217af5a5ed073bcb6df481330
Jeremy Meng <yumeng@microsoft.com>
2019-03-21 21:13:47 -0700

    Export RestError directly

--- packages/@azure/keyvault/lib/index.ts
+++ packages/@azure/keyvault/lib/index.ts
@@ -1,6 +1,5 @@
-import { RestError } from '@azure/ms-rest-js';
+export { RestError } from '@azure/ms-rest-js';
 
 export * from "./secretsClient";
 export * from "./secretsModels";
 export * from "./clientOptions";
-export { RestError };
