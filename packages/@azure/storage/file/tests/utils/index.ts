f213be172bd45560eb1aefa249b4d1e80ce6a971
Mike Harder <mharder@microsoft.com>
2019-03-28 15:01:46 -0700

    Ensure promise does not resolve until write stream is closed (#1783)
    - Improves reliability of test "bloburl.download should download data failed when exceeding max stream retry requests"

--- packages/@azure/storage/file/tests/utils/index.ts
+++ packages/@azure/storage/file/tests/utils/index.ts
@@ -109,15 +109,59 @@ export async function createRandomLocalFile(
   });
 }
 
+// Returns a Promise which is completed after the file handle is closed.
+// If Promise is rejected, the reason will be set to the first error raised by either the
+// ReadableStream or the fs.WriteStream.
 export async function readStreamToLocalFile(
   rs: NodeJS.ReadableStream,
   file: string
 ) {
   return new Promise<void>((resolve, reject) => {
     const ws = fs.createWriteStream(file);
+
+    // Set STREAM_DEBUG env var to log stream events while running tests
+    if (process.env.STREAM_DEBUG) {
+      rs.on("close", () => console.log("rs.close"));
+      rs.on("data", () => console.log("rs.data"));
+      rs.on("end", () => console.log("rs.end"));
+      rs.on("error", () => console.log("rs.error"));
+
+      ws.on("close", () => console.log("ws.close"));
+      ws.on("drain", () => console.log("ws.drain"));
+      ws.on("error", () => console.log("ws.error"));
+      ws.on("finish", () => console.log("ws.finish"));
+      ws.on("pipe", () => console.log("ws.pipe"));
+      ws.on("unpipe", () => console.log("ws.unpipe"));
+    }
+
+    let error : Error;
+
+    rs.on("error", (err: Error) => {
+      // First error wins
+      if (!error) {
+        error = err;
+      }
+
+      // When rs.error is raised, rs.end will never be raised automatically, so it must be raised manually
+      // to ensure ws.close is eventually raised.
+      rs.emit("end");
+    });
+
+    ws.on("error", (err: Error) => {
+      // First error wins
+      if (!error) {
+        error = err;
+      }
+    });
+
+    ws.on("close", () => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+
     rs.pipe(ws);
-    rs.on("error", reject);
-    ws.on("error", reject);
-    ws.on("finish", resolve);
   });
 }
