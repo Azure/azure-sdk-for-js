ea9337fc762c04f9301b1698d97cc6ccd162bbaa
Kamil Pajdzik <kamip@microsoft.com>
2019-03-28 21:33:52 -0700

    Bump the version to 6.2.0

--- packages/@azure/arm-storagesync/lib/storageSyncManagementClientContext.ts
+++ packages/@azure/arm-storagesync/lib/storageSyncManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-storagesync";
-const packageVersion = "6.1.0";
+const packageVersion = "6.2.0";
 
 export class StorageSyncManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
