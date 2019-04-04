7e7e7118ba7cdbbb3021a3b2a4f31f429747b252
Ramya Achutha Rao <ramyar@microsoft.com>
2019-04-03 23:19:33 -0700

    Add async iterator using receiveBatch

--- sdk/servicebus/service-bus/src/util/constants.ts
+++ sdk/servicebus/service-bus/src/util/constants.ts
@@ -2,10 +2,12 @@
 // Licensed under the MIT License.
 
 export const packageJsonInfo = {
-  name: "@azure/service-bus",
-  version: "1.0.0-preview.1"
+	name: "@azure/service-bus",
+	version: "1.0.0-preview.1"
 };
 
 export const messageDispositionTimeout = 20000;
 
 export const max32BitNumber = Math.pow(2, 31) - 1;
+
+export const prefetchSizeInIterator = 100;
