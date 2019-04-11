07d9ada29ccd92887c29ac9a7093dcf2374ab9c6
Steve Faulkner <southpolesteve@gmail.com>
2019-04-11 17:31:55 -0400

    Remove semaphore from PartitionKeyRangeCache (#305)

--- src/routing/partitionKeyRangeCache.ts
+++ src/routing/partitionKeyRangeCache.ts
@@ -1,4 +1,3 @@
-import semaphore from "semaphore";
 import { ClientContext } from "../ClientContext";
 import { getIdFromLink } from "../common/helper";
 import { createCompleteRoutingMap } from "./CollectionRoutingMapFactory";
@@ -8,64 +7,27 @@ import { QueryRange } from "./QueryRange";
 /** @hidden */
 export class PartitionKeyRangeCache {
   private collectionRoutingMapByCollectionId: {
-    [key: string]: InMemoryCollectionRoutingMap;
+    [key: string]: Promise<InMemoryCollectionRoutingMap>;
   };
-  private sem: semaphore.Semaphore;
 
   constructor(private clientContext: ClientContext) {
     this.collectionRoutingMapByCollectionId = {};
-    this.sem = semaphore(1);
   }
   /**
-   * Finds or Instantiates the requested Collection Routing Map and invokes callback
-   * @param {callback} callback                - Function to execute for the collection routing map.
-   *                                             the function takes two parameters error, collectionRoutingMap.
+   * Finds or Instantiates the requested Collection Routing Map
    * @param {string} collectionLink            - Requested collectionLink
    * @ignore
    */
   public async onCollectionRoutingMap(collectionLink: string): Promise<InMemoryCollectionRoutingMap> {
     const collectionId = getIdFromLink(collectionLink);
-
-    let collectionRoutingMap = this.collectionRoutingMapByCollectionId[collectionId];
-    if (collectionRoutingMap === undefined) {
-      // attempt to consturct collection routing map
-      collectionRoutingMap = await new Promise<InMemoryCollectionRoutingMap>((resolve, reject) => {
-        const semaphorizedFuncCollectionMapInstantiator = async () => {
-          let crm: InMemoryCollectionRoutingMap = this.collectionRoutingMapByCollectionId[collectionId];
-          if (crm === undefined) {
-            try {
-              const { resources } = await this.clientContext.queryPartitionKeyRanges(collectionLink).fetchAll();
-
-              crm = createCompleteRoutingMap(resources.map(r => [r, true]));
-
-              this.collectionRoutingMapByCollectionId[collectionId] = crm;
-              this.sem.leave();
-              resolve(crm);
-            } catch (err) {
-              this.sem.leave();
-              reject(err);
-            }
-          } else {
-            // sanity gaurd
-            this.sem.leave();
-            // TODO: it looks like this code should never be reached...
-            // return resolve(collectionRoutingMap.getOverlappingRanges(partitionKeyRanges));
-            reject(new Error("Not yet implemented"));
-          }
-        };
-
-        // We want only one attempt to construct collectionRoutingMap
-        // so we pass the consturction in the semaphore take
-        this.sem.take(semaphorizedFuncCollectionMapInstantiator);
-      });
+    if (this.collectionRoutingMapByCollectionId[collectionId] === undefined) {
+      this.collectionRoutingMapByCollectionId[collectionId] = this.requestCollectionRoutingMap(collectionLink);
     }
-    return collectionRoutingMap;
+    return this.collectionRoutingMapByCollectionId[collectionId];
   }
 
   /**
    * Given the query ranges and a collection, invokes the callback on the list of overlapping partition key ranges
-   * @param {callback} callback - Function execute on the overlapping partition key ranges result,
-   *                                  takes two parameters error, partition key ranges
    * @param collectionLink
    * @param queryRanges
    * @ignore
@@ -73,5 +35,10 @@ export class PartitionKeyRangeCache {
   public async getOverlappingRanges(collectionLink: string, queryRanges: QueryRange) {
     const crm = await this.onCollectionRoutingMap(collectionLink);
     return crm.getOverlappingRanges(queryRanges);
+  }
+
+  private async requestCollectionRoutingMap(collectionLink: string) {
+    const { resources } = await this.clientContext.queryPartitionKeyRanges(collectionLink).fetchAll();
+    return createCompleteRoutingMap(resources.map(r => [r, true]));
   }
 }
