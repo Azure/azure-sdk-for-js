acbd69a267d240de372392911c17b5c18a62b15c
Kamil Pajdzik <kamip@microsoft.com>
2019-04-15 20:02:32 -0700

    Bump the version

--- sdk/apimanagement/arm-apimanagement/src/apiManagementClientContext.ts
+++ sdk/apimanagement/arm-apimanagement/src/apiManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-apimanagement";
-const packageVersion = "4.4.0";
+const packageVersion = "5.0.0";
 
 export class ApiManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
