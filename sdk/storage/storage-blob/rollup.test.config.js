154af83bbb4744e67ba7f86d6c98c382be49b26b
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-04-16 15:34:09 -0700

    remove commented code

--- sdk/storage/storage-blob/rollup.test.config.js
+++ sdk/storage/storage-blob/rollup.test.config.js
@@ -1,17 +1,3 @@
-// import multi from "rollup-plugin-multi-entry";
-// import baseConfig from "./rollup.config";
-// import sourcemaps from "rollup-plugin-sourcemaps";
-// const [browser] = baseConfig;
-
-// browser.input = ["dist-esm/test/*.js", "dist-esm/test/browser/*.js"];
-// browser.output.sourcemap = "inline";
-// browser.output.file = "dist-test/index.browser.js";
-// browser.plugins.unshift(multi());
-// browser.plugins.unshift(sourcemaps());
-// browser.context = "null";
-
-// export default [browser];
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
