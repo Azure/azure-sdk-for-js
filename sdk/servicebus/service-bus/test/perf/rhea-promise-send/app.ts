40f0bf25eeda78a23a470ac981c08603e8ccc188
Ramya Achutha Rao <ramyar@microsoft.com>
2019-04-18 18:58:01 -0700

    Delete README that fails checks

--- sdk/servicebus/service-bus/test/perf/rhea-promise-send/app.ts
+++ sdk/servicebus/service-bus/test/perf/rhea-promise-send/app.ts
@@ -1,3 +1,15 @@
+/* 
+# Overview
+Measures the maximum throughput of `sender.send()` in package `rhea-promise`.
+
+# Instructions
+1. Create a Service Bus namespace with `Tier=Premium` and `Messaging Units=4`.  It is recommended to use the largest possible namespace to allow maximum client throughput.
+2. Create a queue inside the namespace.
+3. Set env vars `SERVICE_BUS_CONNECTION_STRING` and `SERVICE_BUS_QUEUE_NAME`.
+4. `ts-node app.ts [maxInflightMessages] [totalMessages]`
+5. Example: `ts-node app.ts 1000 1000000`
+ */
+
 import { Connection } from "rhea-promise";
 import delay from "delay";
 import moment from "moment";
