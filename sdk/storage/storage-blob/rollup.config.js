7bddadf59707e4788daf2f1c2a0f636d46551d4a
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-04-22 15:48:14 -0700

    Update rollup to generate min and unmin versions

--- sdk/storage/storage-blob/rollup.config.js
+++ sdk/storage/storage-blob/rollup.config.js
@@ -12,6 +12,7 @@ if (!process.env.ONLY_BROWSER) {
 // Disable this until we are ready to run rollup for the browser.
 if (!process.env.ONLY_NODE) {
   inputs.push(base.browserConfig());
+  inputs.push(base.browserConfig(false, true));
 }
 
 export default inputs;
