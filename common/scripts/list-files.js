10bd28a4cb42b9e5a9635f4a50b695b2b8a1d56b
Brandon Siegel <brandon.siegel@microsoft.com>
2019-05-02 17:59:55 -0700

    Add list-files command
    This is especially useful when piped to grep to find specific filenames
    within all projects in the Rush workspace, in order to open those files
    in your editor.

---
+++ common/scripts/list-files.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+function read(filename) {
+  const txt = fs
+    .readFileSync(filename, "utf8")
+    .replace(/\r/gm, "")
+    .replace(/\n/gm, "«")
+    .replace(/\/\*.*?\*\//gm, "")
+    .replace(/«/gm, "\n")
+    .replace(/\s+\/\/.*/g, "");
+  return JSON.parse(txt);
+}
+
+function walk(dir) {
+  var list = fs.readdirSync(dir);
+  for (const fileName of list) {
+    const filePath = path.join(dir, fileName);
+    if (fileName == "node_modules") {
+      continue;
+    }
+
+    const stat = fs.statSync(filePath);
+    if (stat && stat.isDirectory()) { 
+      walk(filePath);
+    } else {
+      console.log(path.resolve(filePath));
+    }
+  }
+}
+
+const rush = read(`${__dirname}/../../rush.json`);
+for (const each of rush.projects) {
+  walk(each.projectFolder);
+}
