f1b988b05dde22fd4fe07b5502b70fe139be2dc4
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-05-02 20:06:28 -0700

    Perf tests - receive test for rhea-promise

---
+++ sdk/servicebus/service-bus/test/perf/rhea-promise/receive.ts
@@ -0,0 +1,162 @@
+/*
+# Overview
+Measures the maximum throughput of `receiver.receive()` in package `rhea-promise`.
+
+# Instructions
+1. Create a Service Bus namespace with `Tier=Premium` and `Messaging Units=4`.  It is recommended to use the largest possible namespace to allow maximum client throughput.
+2. Create a queue inside the namespace.
+3. Set env vars `SERVICE_BUS_CONNECTION_STRING` and `SERVICE_BUS_QUEUE_NAME`.
+4. This test presumes that there are messages in the queue.
+5. `ts-node receive.ts [totalMessages]`
+6. Example: `ts-node receive.ts 1000000`
+ */
+
+import { Connection, ConnectionOptions, Receiver, ReceiverEvents } from "rhea-promise";
+import delay from "delay";
+import moment from "moment";
+
+const _start = moment();
+
+let _messages = 0;
+
+/**
+ * Maximum wait duration for the expected event to happen = `10000 ms`(default value is 10 seconds)(= maxWaitTimeInMilliseconds)
+ * Keep checking whether the predicate is true after every `1000 ms`(default value is 1 second) (= delayBetweenRetriesInMilliseconds)
+ */
+async function checkWithTimeout(
+  predicate: () => boolean,
+  maxWaitTimeInMilliseconds: number = 10000,
+  delayBetweenRetriesInMilliseconds: number = 1000
+): Promise<boolean> {
+  const maxTime = Date.now() + maxWaitTimeInMilliseconds;
+  while (Date.now() < maxTime) {
+    if (predicate()) return true;
+    await delay(delayBetweenRetriesInMilliseconds);
+  }
+  return false;
+}
+
+async function main(): Promise<void> {
+  // Endpoint=sb://<your-namespace>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=<shared-access-key>
+  const connectionString = process.env.SERVICE_BUS_CONNECTION_STRING as string;
+  const entityPath = process.env.SERVICE_BUS_QUEUE_NAME as string;
+  const allowUnauthorized = process.env.SERVICE_BUS_ALLOW_UNAUTHORIZED ? true : false;
+
+  // <your-namespace>.servicebus.windows.net
+  const host = connectionString.match(/\/\/([^\/]*)\//)![1];
+
+  // SharedAccessKeyName (usually  "RootManageSharedAccessKey")
+  const username = connectionString.match(/SharedAccessKeyName=(.*);/)![1];
+
+  // SharedAccessKey
+  const password = connectionString.match(/SharedAccessKey=(.*)/)![1];
+
+  const messages = process.argv.length > 2 ? parseInt(process.argv[2]) : 10;
+  log(`Total messages: ${messages}`);
+
+  const writeResultsPromise = WriteResults(messages);
+
+  await RunTest(host, username, password, allowUnauthorized, entityPath, messages);
+
+  await writeResultsPromise;
+}
+
+async function RunTest(
+  host: string,
+  username: string,
+  password: string,
+  allowUnauthorized: boolean,
+  entityPath: string,
+  messages: number
+): Promise<void> {
+  const port = 5671;
+
+  const connection = new Connection({
+    transport: "tls",
+    host: host,
+    hostname: host,
+    username: username,
+    password: password,
+    port: port,
+    reconnect: false,
+    rejectUnauthorized: !allowUnauthorized
+  } as ConnectionOptions);
+  await connection.open();
+
+  const receiver = await connection.createReceiver({
+    name: "receiver-1",
+    source: {
+      address: entityPath
+    }
+  });
+  ExecuteReceivesAsync(receiver);
+
+  await checkWithTimeout(() => _messages >= messages, messages * 1000);
+
+  await receiver.close();
+  await connection.close();
+}
+
+function ExecuteReceivesAsync(receiver: Receiver) {
+  receiver.on(ReceiverEvents.message, (context) => {
+    //console.log("Received message: %O", context.message);
+    _messages++;
+  });
+}
+
+async function WriteResults(messages: number): Promise<void> {
+  let lastMessages = 0;
+  let lastElapsed = 0;
+  let maxMessages = 0;
+  let maxElapsed = Number.MAX_SAFE_INTEGER;
+
+  do {
+    await delay(1000);
+
+    const receivedMessages = _messages;
+    const currentMessages = receivedMessages - lastMessages;
+    lastMessages = receivedMessages;
+
+    const elapsed = moment().diff(_start);
+    const currentElapsed = elapsed - lastElapsed;
+    lastElapsed = elapsed;
+
+    if (currentMessages / currentElapsed > maxMessages / maxElapsed) {
+      maxMessages = currentMessages;
+      maxElapsed = currentElapsed;
+    }
+
+    WriteResult(
+      receivedMessages,
+      elapsed,
+      currentMessages,
+      currentElapsed,
+      maxMessages,
+      maxElapsed
+    );
+  } while (_messages < messages);
+}
+
+function WriteResult(
+  totalMessages: number,
+  totalElapsed: number,
+  currentMessages: number,
+  currentElapsed: number,
+  maxMessages: number,
+  maxElapsed: number
+): void {
+  log(
+    `\tTot Msg\t${totalMessages}` +
+      `\tCur MPS\t${Math.round((currentMessages * 1000) / currentElapsed)}` +
+      `\tAvg MPS\t${Math.round((totalMessages * 1000) / totalElapsed)}` +
+      `\tMax MPS\t${Math.round((maxMessages * 1000) / maxElapsed)}`
+  );
+}
+
+function log(message: string): void {
+  console.log(`[${moment().format("hh:mm:ss.SSS")}] ${message}`);
+}
+
+main().catch((err) => {
+  log(`Error occurred: ${err}`);
+});
