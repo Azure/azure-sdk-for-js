1692e2acbe3ace3bcdfbd8cc8983c84f8c5f4af6
Shivangi Reja <shreja@microsoft.com>
2019-05-07 11:19:43 -0700

    update sendBatch Sample

--- sdk/eventhub/event-processor-host/samples/sendBatch.ts
+++ sdk/eventhub/event-processor-host/samples/sendBatch.ts
@@ -2,39 +2,34 @@
   This sample demonstrates how the sendBatch() function can be used to send messages to Event Hubs.
 */
 
-import { EventHubClient, EventData, delay } from "@azure/event-hubs";
+import { EventHubClient, EventData } from "@azure/event-hubs";
 
-// Define connection string and related Event Hubs entity name here
+// Define connection string and related Event Hub entity name here
 const connectionString = "";
-const eventHubsName = "";
+const eventHubName = "";
 
 async function main(): Promise<void> {
-  const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName);
+  const client = EventHubClient.createFromConnectionString(connectionString, eventHubName);
   const partitionIds = await client.getPartitionIds();
   const messageCount = 300;
 
-  const data: EventData[] = [];
-  for (let i = 0; i < messageCount; i++) {
-    const obj: EventData = { body: `Hello foo ${i}` };
-    data.push(obj);
-  }
-  console.log("Sending batch message...");
+  const events: EventData[] = [];
   // NOTE: For receiving events from Azure Stream Analytics, please send Events to an EventHub
   // where the body is a JSON object/array.
-  // const datas = [
+  // const events = [
   //   { body: { "message": "Hello World 1" }, applicationProperties: { id: "Some id" }, partitionKey: "pk786" },
   //   { body: { "message": "Hello World 2" } },
   //   { body: { "message": "Hello World 3" } }
   // ];
-  const sendPromises: Promise<any>[] = [];
-  for (const id of partitionIds) {
-    sendPromises.push(client.sendBatch(data, id));
+  for (let i = 0; i < messageCount; i++) {
+    events.push({ body: `Hello foo ${i}` });
   }
+  console.log("Sending batch message...");
 
   // Will concurrently send batched messages to all the partitions.
+  const sendPromises = partitionIds.map(id => client.sendBatch(events, id));
+
   await Promise.all(sendPromises);
-  // Giving some more time, just in case.
-  await delay(5000);
   await client.close();
 }
 
