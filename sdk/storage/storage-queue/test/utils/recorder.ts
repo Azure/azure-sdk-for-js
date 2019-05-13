6d72a39aba0415a2331beab3bd89e1528c16fcae
--show-origin <camaiaor@microsoft.com>
2019-05-13 06:28:39 -0700

    recording for browser implemented using nise => can't write file using 'fs'

--- sdk/storage/storage-queue/test/utils/recorder.ts
+++ sdk/storage/storage-queue/test/utils/recorder.ts
@@ -1,8 +1,18 @@
 import fs from "fs";
-import nock from "nock";
+import nise from "nise";
 import { getUniqueName } from "../utils";
+import { isBrowser } from "./testutils.common";
 import * as dotenv from "dotenv";
 dotenv.config({ path: "../../.env" });
+
+let nock: any;
+if (!isBrowser()) {
+  nock = require("nock");
+}
+
+function writeFileInBrowser(_filename: string, _obj: any) {
+  // TODO
+}
 
 /**
  * Possible reasons for skipping a test:
@@ -41,11 +51,11 @@ function nockRecorder(folderpath: string, testTitle: string) {
       });
     },
     playback: function() {
-      uniqueTestInfo = require("../../recordings/" + fp).testInfo;
+      uniqueTestInfo = require("../../recordings/node/" + fp).testInfo;
     },
     stop: function() {
       let fixtures = nock.recorder.play();
-      let file = fs.createWriteStream("./recordings/" + fp, { flags: "w" });
+      let file = fs.createWriteStream("./recordings/node/" + fp, { flags: "w" });
       file.on("error", err => console.log(err));
       file.write(importNock + "\n" + "module.exports.testInfo = " + JSON.stringify(uniqueTestInfo) + "\n");
       for (let i = 0; i < fixtures.length; i++) {
@@ -58,10 +68,92 @@ function nockRecorder(folderpath: string, testTitle: string) {
   };
 }
 
+function niseRecorder(folderpath: string, testTitle: string) {
+  const filename = "recording_" + testTitle;
+  const fp = formatPath(folderpath) + "/" + formatPath(filename) + ".json";
+  let uniqueTestInfo: any = {};
+  let recordings: any[] = [];
+  let xhr: nise.FakeXMLHttpRequestStatic;
+
+  return {
+    skip: skip.includes(fp),
+    getUniqueTestInfoHandle: () => uniqueTestInfo,
+    record: function() {
+      xhr = nise.fakeXhr.useFakeXMLHttpRequest();
+      xhr.useFilters = true;
+      xhr.addFilter(() => true);
+
+      xhr.onCreate = function(req: any) {
+        const reqOpen = req.open;
+        req.open = function() {
+          reqOpen.apply(req, arguments);
+
+          const requestHeaders: any = {};
+          const reqSetReqHeader = req.setRequestHeader;
+          req.setRequestHeader = function(header: string, value: any) {
+            requestHeaders[header] = value;
+            reqSetReqHeader.apply(req, arguments);
+          }
+
+          const reqSend = req.send;
+          req.send = function() {
+            const reqStateChange = req.onreadystatechange;
+            req.onreadystatechange = function() {
+              if (req.readyState === 4) {
+                recordRequest(req, requestHeaders);
+              }
+              reqStateChange && reqStateChange.apply(null, arguments);
+            }
+            reqSend.apply(req, arguments);
+          }
+        }
+      }
+
+      function recordRequest(req: any, requestHeaders: any[]) {
+        const responseHeaders: any = {};
+        const responseHeadersPairs = req.getAllResponseHeaders().split("\r\n");
+        for (const pair of responseHeadersPairs) {
+          const [key, value] = pair.split(": ");
+          responseHeaders[key] = value;
+        }
+
+        recordings.push({
+          method: req.method,
+          url: req.url,
+          requestBody: req.requestBody,
+          requestHeaders: requestHeaders,
+          status: req.status,
+          response: req.response,
+          responseHeaders: responseHeaders
+        });
+      }
+    },
+    playback: function() {
+    },
+    stop: function() {
+      writeFileInBrowser("./recordings/browser/" + fp, {
+        recordings: recordings,
+        uniqueTestInfo: uniqueTestInfo
+      });
+      xhr.restore();
+    }
+  };
+}
+
 export function record(this: any, folderpath: string, testTitle?: string) {
-  const isRecording = (process.env.TEST_MODE === "record");
-  const isPlayingBack = (process.env.TEST_MODE === "playback");
-  const recorder = nockRecorder(folderpath, testTitle || this.currentTest.title);
+  let recorder: any;
+  let isRecording: boolean;
+  let isPlayingBack: boolean;
+
+  if (isBrowser()) {
+    recorder = niseRecorder(folderpath, testTitle || this.currentTest.title);
+    isRecording = ((window as any).__env__.TEST_MODE === "record");
+    isPlayingBack = ((window as any).__env__.TEST_MODE === "playback");
+  } else {
+    recorder = nockRecorder(folderpath, testTitle || this.currentTest.title);
+    isRecording = (process.env.TEST_MODE === "record");
+    isPlayingBack = (process.env.TEST_MODE === "playback");
+  }
 
   if (recorder.skip && (isRecording || isPlayingBack)) {
     this.skip();
