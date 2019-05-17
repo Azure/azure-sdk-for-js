cb6dcd0955ab627b8882194f717258176f95fcb5
Jonathan Turner <joturner@microsoft.com>
2019-05-17 15:03:23 -0700

    Add support for async iterable to index

--- sdk/keyvault/keyvault/src/secrets/index.ts
+++ sdk/keyvault/keyvault/src/secrets/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+/// <reference lib="esnext.asynciterable">
+
 import {
   HttpClient as IHttpClient,
   HttpPipelineLogger as IHttpPipelineLogger,
