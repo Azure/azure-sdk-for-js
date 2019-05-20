2ac05a39abad3fbd14930980c86e087e4f62bee5
Jonathan Turner <joturner@microsoft.com>
2019-05-20 12:12:35 -0700

    WIP

--- sdk/keyvault/keyvault/src/index.ts
+++ sdk/keyvault/keyvault/src/index.ts
@@ -1,7 +1,5 @@
 export { RestError } from '@azure/ms-rest-js';
 
-export * from "./secretsClient";
-export * from "./secretsModels";
-export * from "./keysClient";
-export * from "./keysModels";
+export * from "./secrets";
+export * from "./keys";
 export * from "./clientOptions";
