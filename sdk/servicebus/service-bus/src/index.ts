8a0efe32416a254ecc8cb88260915c8c6c6ea84e
Karishma Ghiya <kaghiya@microsoft.com>
2019-05-21 13:13:18 -0700

    fixing the triple-slash reference and eslint spaced-comments rule

--- sdk/servicebus/service-bus/src/index.ts
+++ sdk/servicebus/service-bus/src/index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// / <reference lib="es2015" />
-// / <reference lib="esnext.asynciterable" />
+/// <reference lib="es2015" />
+/// <reference lib="esnext.asynciterable" />
 
 export { ServiceBusClient, ServiceBusClientOptions } from "./serviceBusClient";
 export {
