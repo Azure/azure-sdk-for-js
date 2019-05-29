40d8dae088d44a6ca2f27d1dd6ee400ee016018b
Daniel Rodríguez <sadasant@gmail.com>
2019-05-29 21:21:02 +0000

    first build

---
+++ sdk/keyvault/core-keyvault/types/src/utils/index.d.ts
@@ -0,0 +1,4 @@
+import { ParsedKeyVaultEntityIdentifier } from "../keyVaultBase";
+export * from "./constants";
+export declare function parseKeyvaultIdentifier(collection: string, identifier: string | undefined): ParsedKeyVaultEntityIdentifier;
+//# sourceMappingURL=index.d.ts.map
