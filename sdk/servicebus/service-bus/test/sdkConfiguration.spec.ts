f74a93e32246d00f6b2ed579fe10b6a44c169126
ramya0820 <ramya.raja0820@gmail.com>
2019-05-30 10:40:31 -0700

    Test case for consistency of package name and version values

---
+++ sdk/servicebus/service-bus/test/sdkConfiguration.spec.ts
@@ -0,0 +1,30 @@
+import chai from "chai";
+const should = chai.should();
+import fs from "fs";
+import path from "path";
+import { packageJsonInfo } from "../src/util/constants";
+
+// Since we currently hardcode package name and version in `constants.ts` file,
+// following test is in place to ensure the values in package.json and in this file are consistent
+describe("Ensure package name and version are consistent in SDK and package.json", function(): void {
+  it("Ensure constants.ts file is consistent with package.json", () => {
+    const packageJsonFilePath = path.join(__dirname, "../package.json");
+    console.log("path : ", packageJsonFilePath);
+    const fileContents = fs.readFileSync(packageJsonFilePath, { encoding: "utf-8" });
+    const packageJson = JSON.parse(fileContents);
+
+    const name = packageJson.name;
+    const version = packageJson.version;
+
+    should.equal(
+      packageJsonInfo.name,
+      name,
+      `${name} from package.json is not same as 'name' used in constants.ts`
+    );
+    should.equal(
+      packageJsonInfo.version,
+      version,
+      `${version} from package.json is not same as 'version' used in constants.ts`
+    );
+  });
+});
