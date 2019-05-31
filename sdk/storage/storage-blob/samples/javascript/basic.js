71ebb89681bd4e078555125ab174d147ebec266f
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-05-31 14:22:43 -0700

    ../../dist-esm/src -> "../../src"

--- sdk/storage/storage-blob/samples/javascript/basic.js
+++ sdk/storage/storage-blob/samples/javascript/basic.js
@@ -2,12 +2,7 @@
  Setup: Enter your storage account name and shared key in main()
 */
 
-const {
-  BlobServiceClient,
-  StorageClient,
-  SharedKeyCredential,
-  TokenCredential
-} = require("../../dist-esm/src"); // Change to "@azure/storage-blob" in your package
+const { BlobServiceClient, StorageClient, SharedKeyCredential, TokenCredential } = require("../.."); // Change to "@azure/storage-blob" in your package
 
 async function main() {
   // Enter your storage account name and shared key
