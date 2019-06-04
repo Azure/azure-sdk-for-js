cfee621b2431ac2011d37275a476d1d6c5db7f8e
Kamil Pajdzik <kamip@microsoft.com>
2019-06-04 09:16:08 -0700

    Update versions

--- sdk/resources/arm-resources/src/resourceManagementClientContext.ts
+++ sdk/resources/arm-resources/src/resourceManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-resources";
-const packageVersion = "1.0.1";
+const packageVersion = "1.1.0";
 
 export class ResourceManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
