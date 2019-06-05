dbaee993d9ea4e948df75f2e491cdcfa634252bb
David Wilson <daviwil@microsoft.com>
2019-06-05 15:13:05 -0700

    Add missing copyright headers in source files

--- sdk/identity/identity/test/credentials/authTestUtils.ts
+++ sdk/identity/identity/test/credentials/authTestUtils.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 import assert from "assert";
 import { IdentityClientOptions } from '../../src';
 import { HttpHeaders, HttpOperationResponse, WebResource, HttpClient } from '@azure/core-http'
--- sdk/identity/identity/test/credentials/azureCredential.spec.ts
+++ sdk/identity/identity/test/credentials/azureCredential.spec.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 import assert from "assert";
 import { AccessToken } from '../../src';
 import { AzureCredential, TokenRefreshBufferMs } from '../../src/credentials/azureCredential'
