7f63375eb5aaf57bcb12e675db9d097f1fffa3ed
Daniel Rodríguez <sadasant@gmail.com>
2019-06-06 18:03:23 -0400

    better catches

--- sdk/keyvault/keyvault-secrets/tests/secrets.test.ts
+++ sdk/keyvault/keyvault-secrets/tests/secrets.test.ts
@@ -33,18 +33,21 @@ describe("Secret client", () => {
       client = new SecretsClient(url, credential);
       version = "";
     } catch (e) {
-      console.error("00001", e);
+      console.error("00001", e, e.stack);
     }
   });
 
   it("can add a secret", async () => {
-    try {
-      const secretName = getUniqueName("secret");
-      const secretValue = getUniqueName("value");
-      after(async () => {
+    const secretName = getUniqueName("secret");
+    const secretValue = getUniqueName("value");
+    after(async () => {
+      try {
         await client.deleteSecret(secretName);
-      });
-
+      } catch (e) {
+        console.error("00002 after", e, e.stack);
+      }
+    });
+    try {
       const result = await client.setSecret(secretName, secretValue);
 
       assert.equal(
@@ -58,17 +61,21 @@ describe("Secret client", () => {
         "Unexpected secret value in result from setSecret()."
       );
     } catch (e) {
-      console.error("00002", e);
+      console.error("00002", e, e.stack);
     }
   });
 
   it("can retrieve the latest version of a secret value", async () => {
-    try {
-      const secretName = getUniqueName("secret");
-      const secretValue = getUniqueName("value");
-      after(async () => {
+    const secretName = getUniqueName("secret");
+    const secretValue = getUniqueName("value");
+    after(async () => {
+      try {
         await client.deleteSecret(secretName);
-      });
+      } catch (e) {
+        console.error("00003 after", e, e.stack);
+      }
+    });
+    try {
       await client.setSecret(secretName, secretValue);
 
       const result = await client.getSecret(secretName);
@@ -84,18 +91,22 @@ describe("Secret client", () => {
         "Unexpected secret value in result from setSecret()."
       );
     } catch (e) {
-      console.error("00003", e);
+      console.error("00003", e, e.stack);
     }
   });
 
   it("can set a secret with attributes", async () => {
-    try {
-      const secretName = getUniqueName("secret");
-      const secretValue = getUniqueName("value");
-      const expiryDate = new Date("3000-01-01");
-      after(async () => {
+    const secretName = getUniqueName("secret");
+    const secretValue = getUniqueName("value");
+    const expiryDate = new Date("3000-01-01");
+    after(async () => {
+      try {
         await client.deleteSecret(secretName);
-      });
+      } catch (e) {
+        console.error("00004 after", e, e.stack);
+      }
+    });
+    try {
       await client.setSecret(secretName, secretValue, { expires: expiryDate });
 
       const updated = await client.getSecret(secretName);
@@ -109,18 +120,22 @@ describe("Secret client", () => {
         "Expect attribute 'expires' to be updated."
       );
     } catch (e) {
-      console.error("00004", e);
+      console.error("00004", e, e.stack);
     }
   });
 
   it("can update a secret", async () => {
-    try {
-      const secretName = getUniqueName("secret");
-      const secretValue = getUniqueName("value");
-      const expiryDate = new Date("3000-01-01");
-      after(async () => {
+    const secretName = getUniqueName("secret");
+    const secretValue = getUniqueName("value");
+    const expiryDate = new Date("3000-01-01");
+    after(async () => {
+      try {
         await client.deleteSecret(secretName);
-      });
+      } catch (e) {
+        console.error("00005 after", e, e.stack);
+      }
+    });
+    try {
       await client.setSecret(secretName, secretValue);
 
       await client.updateSecretAttributes(secretName, version, {
@@ -138,43 +153,42 @@ describe("Secret client", () => {
         "Expect attribute 'expires' to be updated."
       );
     } catch (e) {
-      console.error("00005", e);
+      console.error("00005", e, e.stack);
     }
   });
 
   it("can delete a secret", async () => {
+    const secretName = getUniqueName("secret");
+    const secretValue1 = getUniqueName("value");
+    await client.setSecret(secretName, secretValue1);
+
+    await client.deleteSecret(secretName);
+
     try {
-      const secretName = getUniqueName("secret");
-      const secretValue1 = getUniqueName("value");
-      await client.setSecret(secretName, secretValue1);
-
-      await client.deleteSecret(secretName);
-
-      try {
-        await client.getSecret(secretName);
-        throw Error("Expecting an error but not catching one.");
-      } catch (e) {
-        if (e.statusCode === 404) {
-          assert.equal(e.message, `Secret not found: ${secretName}`);
-        } else {
-          throw e;
-        }
-      }
+      await client.getSecret(secretName);
+      throw Error("Expecting an error but not catching one.");
     } catch (e) {
-      console.error("00006", e);
+      if (e.statusCode === 404) {
+        assert.equal(e.message, `Secret not found: ${secretName}`);
+      } else {
+        throw e;
+      }
     }
   });
 
   it("can retrieve all versions of a secret", async () => {
-    try {
-      const secretName = getUniqueName("secret");
-      const secretValue1 = getUniqueName("value");
-      const secretValue2 = getUniqueName("value");
-      const secretValue3 = getUniqueName("value");
-      after(async () => {
+    const secretName = getUniqueName("secret");
+    const secretValue1 = getUniqueName("value");
+    const secretValue2 = getUniqueName("value");
+    const secretValue3 = getUniqueName("value");
+    after(async () => {
+      try {
         await client.deleteSecret(secretName);
-      });
-
+      } catch (e) {
+        console.error("00007 after", e, e.stack);
+      }
+    });
+    try {
       const secretValues = [secretValue1, secretValue2, secretValue3];
       interface versionValuePair {
         version: string;
@@ -200,7 +214,7 @@ describe("Secret client", () => {
 
       expect(results).to.deep.equal(versions);
     } catch (e) {
-      console.error("00007", e);
+      console.error("00007", e, e.stack);
     }
   });
 });
