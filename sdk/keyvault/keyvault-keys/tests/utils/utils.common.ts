16c1b1bbe3c2ecaf94da23c904ebd2222dbf56bb
Daniel Rodríguez <sadasant@users.noreply.github.com>
2019-06-17 21:15:00 -0400

    Update utils.common.ts

--- sdk/keyvault/keyvault-keys/tests/utils/utils.common.ts
+++ sdk/keyvault/keyvault-keys/tests/utils/utils.common.ts
@@ -15,22 +15,3 @@ export function getKeyvaultName(): string {
 
   return keyVaultName;
 }
-
-function padStart(currentString: string, targetLength: number, padString: string = " "): string {
-  // To run the tests under ts-node the compiler option { module: "commonjs" } is needed,
-  // which prevent the usage of `padStart()`.
-  // if (String.prototype.padStart) {
-  //   return currentString.padStart(targetLength, padString);
-  // }
-
-  padString = padString || " ";
-  if (currentString.length > targetLength) {
-    return currentString;
-  } else {
-    targetLength = targetLength - currentString.length;
-    if (targetLength > padString.length) {
-      padString += padString.repeat(targetLength / padString.length);
-    }
-    return padString.slice(0, targetLength) + currentString;
-  }
-}
