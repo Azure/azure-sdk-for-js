5a8ce86ba128c4edfb059fa7b5583ba13015e8d1
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-06-18 14:43:54 -0700

    simplified next() method

--- sdk/core/core-paging/types/corePaging.d.ts
+++ sdk/core/core-paging/types/corePaging.d.ts
@@ -6,34 +6,31 @@ export interface PageSettings {
   /**
    * @member {string} [continuationToken] The token that keeps track of where to continue the iterator
    */
-  continuationToken?: string,
+  continuationToken?: string;
   /**
    * @member {number} [pageSize] The size of the page during paged iteration
    */
-  maxPageSize?: number,
+  maxPageSize?: number;
 }
 
 /**
  * @interface
  * An interface that allows async iterable iteration both to completion and by page.
  */
-export interface PagedAsyncIterableIterator<T> {
+export interface PagedAsyncIterableIterator<T, PageT = never> {
   /**
    * @member {Promise} [next] The next method, part of the iteration protocol
    */
   next(): Promise<{
     done: boolean;
     value: T;
-  } | {
-    done: boolean;
-    value: undefined;
   }>;
   /**
    * @member {Symbol} [asyncIterator] The connection to the async iterator, part of the iteration protocol
    */
-  [Symbol.asyncIterator](): PagedAsyncIterableIterator<T>;
+  [Symbol.asyncIterator](): PagedAsyncIterableIterator<T, PageT>;
   /**
    * @member {Function} [byPage] Return an AsyncIterableIterator that works a page at a time
    */
-  byPage: (settings?: PageSettings) => AsyncIterableIterator<T[]>;
+  byPage: (settings?: PageSettings) => AsyncIterableIterator<PageT extends never ? T[] : PageT>;
 }
