8970044b004616820c793d5539dfeb511b9d8b5d
David Wilson <daviwil@microsoft.com>
2019-06-19 17:25:56 -0700

    Clarify comment with URL on ErrorResponse type

--- sdk/identity/identity/src/client/errors.ts
+++ sdk/identity/identity/src/client/errors.ts
@@ -5,6 +5,9 @@
  * See the official documentation for more details:
  *
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v1-protocols-oauth-code#error-response-1
+ * 
+ * NOTE: This documentation is for v1 OAuth support but the same error
+ * response details still apply to v2.
  */
 export interface ErrorResponse {
   error: string;
