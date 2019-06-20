d16cf6f391ff90e53e6bea32ba79cafa169eb785
David Wilson <daviwil@microsoft.com>
2019-06-19 21:23:28 -0700

    [Identity] Throw AuthenticationError when any authentication request fails (#3826)
    * Throw AuthenticationError when any authentication request fails
    
    * Improve test reliability on CI
    
    * Bump tsc output to ES6 and remove object prototype hack
    
    * Rename AggregateCredential tests for ChainedTokenCredential
    
    * Clarify comment with URL on ErrorResponse type
    
    * Move assertRejects to authTestUtils and fix linter warnings

--- sdk/identity/identity/src/client/identityClient.ts
+++ sdk/identity/identity/src/client/identityClient.ts
@@ -12,6 +12,7 @@ import {
   WebResource,
   RequestPrepareOptions
 } from "@azure/core-http";
+import { AuthenticationError } from "./errors";
 
 const SelfSignedJwtLifetimeMins = 10;
 const DefaultAuthorityHost = "https://login.microsoftonline.com";
@@ -42,9 +43,9 @@ export class IdentityClient extends ServiceClient {
         token: response.parsedBody.access_token,
         expiresOnTimestamp: Date.now() + response.parsedBody.expires_in * 1000
       };
+    } else {
+      throw new AuthenticationError(response.status, response.bodyAsText);
     }
-
-    return null;
   }
 
   private mapScopesToResource(scopes: string | string[]): string {
@@ -66,7 +67,7 @@ export class IdentityClient extends ServiceClient {
     return scope.substr(0, scope.lastIndexOf(DefaultScopeSuffix));
   }
 
-  private dateInSeconds(date: Date) {
+  private dateInSeconds(date: Date): number {
     return Math.floor(date.getTime() / 1000);
   }
 
