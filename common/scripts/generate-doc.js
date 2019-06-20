ba36ec5da26b22b41a2675a009ee173fca67ac88
Karishma Ghiya <kaghiya@microsoft.com>
2019-06-20 11:03:27 -0700

    script for docGen with all the checks and flag for output to docGen folder

--- common/scripts/generate-doc.js
+++ common/scripts/generate-doc.js
@@ -2,25 +2,41 @@ const fs = require("fs");
 const path = require("path");
 const childProcess = require("child_process");
 
-function walk(dir, isRush) {
+function walk(dir, checks) {
   var list = fs.readdirSync(dir);
   for (const fileName of list) {
     const filePath = path.join(dir, fileName);
     if (fileName == "node_modules") {
-      isRush = true;
-      return isRush;
+      checks.isRush = true;
+      continue;
     }
+    if (fileName == "src") {
+      checks.srcPresent = true;
+    }
+    if (fileName == "package.json") {
+      let data = fs.readFileSync(filePath, "utf8");
+      let settings = JSON.parse(data);
+      if (settings["private"] === true) {
+        checks.isPrivate = true;
+      }
+    }
+
     const stat = fs.statSync(filePath);
     if (stat && stat.isDirectory()) {
-      isRush = walk(filePath, isRush);
+      checks = walk(filePath, checks);
     } else {
       // console.log(path.resolve(filePath));
     }
   }
-  return isRush;
+  return checks;
 }
 
-const [scriptPath /* /repo/common/scripts/generate-doc.js */] = process.argv;
+const [
+  nodePath /* Ex: /bin/node */,
+  scriptPath /* /repo/common/scripts/generate-doc.js */,
+  docGen /* -dg */
+] = process.argv;
+let docOutputFolder = "--out ./dist/docs ./src";
 
 console.log("process.cwd = " + process.cwd());
 try {
@@ -49,35 +65,81 @@ for (const eachService of serviceFolders) {
     var packageList = fs.readdirSync(eachServicePath);
 
     for (const eachPackage of packageList) {
-      let isRush = false;
+      let checks = { isRush: false, isPrivate: false, srcPresent: false };
+      console.log(
+        "checks before walk: checks.isRush = " +
+          checks.isRush +
+          " , checks.isPrivate = " +
+          checks.isPrivate +
+          ", checks.srcPresent = " +
+          checks.srcPresent
+      );
       eachPackagePath = path.join(eachServicePath, eachPackage);
-      isRush = walk(eachPackagePath, isRush);
-      console.log("Path: " + eachPackagePath);
-      if (!isRush) {
-        try {
-          const result2 = childProcess.spawnSync("npm", ["install"], {
-            stdio: "inherit",
-            cwd: eachPackagePath,
-            shell: true
-          });
-          console.log('result2.output for "npm install":' + result2.output);
-        } catch (e) {
-          console.error(`\n\n${e.toString()}\n\n`);
-          process.exit(1);
-        }
-      }
+      const packageStat = fs.statSync(eachPackagePath);
+      if (packageStat && packageStat.isDirectory()) {
+        checks = walk(eachPackagePath, checks);
 
-      try {
-        const result3 = childProcess.spawnSync("rushx", ["generate-doc"], {
-          cwd: eachPackagePath,
-          shell: true
-        });
         console.log(
-          'result3.output for "rushx generate-doc":' + result3.output
+          "checks after walk: checks.isRush = " +
+            checks.isRush +
+            " , checks.isPrivate = " +
+            checks.isPrivate +
+            ", checks.srcPresent = " +
+            checks.srcPresent
         );
-      } catch (e) {
-        console.error(`\n\n${e.toString()}\n\n`);
-        process.exit(1);
+        console.log("Path: " + eachPackagePath);
+        if (!checks.isPrivate) {
+          if (checks.srcPresent) {
+            if (!checks.isRush) {
+              try {
+                const result2 = childProcess.spawnSync("npm", ["install"], {
+                  stdio: "inherit",
+                  cwd: eachPackagePath,
+                  shell: true
+                });
+                console.log(
+                  'result2.output for "npm install":' + result2.output
+                );
+              } catch (e) {
+                console.error(`\n\n${e.toString()}\n\n`);
+                process.exit(1);
+              }
+            }
+            if (docGen === "-dg") {
+              docOutputFolder =
+                "--out ../../../docGen/" + eachPackage + " ./src";
+            }
+
+            try {
+              const result3 = childProcess.spawnSync(
+                "typedoc",
+                [
+                  "--excludePrivate",
+                  '--exclude "node_modules/**/*"',
+                  "--mode modules",
+                  docOutputFolder
+                ],
+                {
+                  cwd: eachPackagePath,
+                  shell: true
+                }
+              );
+              console.log(
+                'result3.output for "typedoc ----excludePrivate  --exclude "node_modules/**/*" --mode modules ' +
+                  docOutputFolder +
+                  ' ":' +
+                  result3.output
+              );
+            } catch (e) {
+              console.error(`\n\n${e.toString()}\n\n`);
+              process.exit(1);
+            }
+          } else {
+            console.log("...SKIPPING Since src folder could not be found.....");
+          }
+        } else {
+          console.log("...SKIPPING Since package marked as private...");
+        }
       }
     }
   }
