0d333f35c1d555a0481e8bc49f33ffb35bdb4489
Daniel Rodríguez <sadasant@gmail.com>
2019-06-24 19:33:34 +0000

    retry utility on keyvault

--- sdk/keyvault/keyvault-secrets/tests/utils/utils.common.ts
+++ sdk/keyvault/keyvault-secrets/tests/utils/utils.common.ts
@@ -2,9 +2,10 @@
 // Licensed under the MIT License.
 
 import { env } from "./recorder";
+import { delay } from "@azure/core-http";
 
 // Async iterator's polyfill for Node 8
-if (!Symbol || !Symbol.asyncIterator) {
+if (!Symbol || !(Symbol as any).asyncIterator) {
   (Symbol as any).asyncIterator = Symbol.for("Symbol.asyncIterator");
 }
 
@@ -17,4 +18,40 @@ export function getKeyvaultName(): string {
   }
 
   return keyVaultName;
+}
+
+export type RetryFunction = () => Promise<any>;
+export interface RetryOptions {
+  retries: number;
+  factor: number;
+  minTimeout: number;
+  maxTimeout: number;
+}
+
+/**
+ * A simple abstraction to retry, and exponentially de-escalate retrying, a
+ * given async function until it is fulfileld.
+ * @param {RetryFunction} target The async function you want to retry.
+ * @param {RetryOptions} options An object with configuration values.
+ * @returns {Promise<any>} Resolved promise
+ */
+export async function retry(
+  target: RetryFunction,
+  { retries = 10, factor = 2, minTimeout = 1000, maxTimeout = Infinity }: RetryOptions
+): Promise<any> {
+  let timeout = minTimeout;
+  let error: any;
+
+  while (retries > 0 && timeout < maxTimeout) {
+    try {
+      return await target();
+    } catch (e) {
+      error = e;
+    }
+    if (retries) await delay(timeout);
+    retries--;
+    timeout *= factor;
+  }
+
+  if (error) throw error;
 }
