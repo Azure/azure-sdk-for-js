762e03b49ba3e80bc1268b1de5fff8e241fce4e6
David Wilson <daviwil@microsoft.com>
2019-06-25 15:30:26 -0700

    [Identity] Update @azure/identity version, license headers, and… (#4048)
    * Update source code license headers
    
    * Set @azure/identity version to 1.0.0-preview.1
    
    * Remove 'private: true' from package.json
    
    * Update README.md with standard information
    
    * Update README.md based on feedback
    
    * Add JSDoc comments to exported classes

--- sdk/identity/identity/src/credentials/clientSecretCredential.ts
+++ sdk/identity/identity/src/credentials/clientSecretCredential.ts
@@ -1,15 +1,33 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 import { TokenCredential, GetTokenOptions, AccessToken } from "@azure/core-http";
 import { IdentityClientOptions, IdentityClient } from "../client/identityClient";
 
+/**
+ * Enables authentication to Azure Active Directory using a client secret
+ * that was generated for an App Registration.  More information on how
+ * to configure a client secret can be found here:
+ * 
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-configure-app-access-web-apis#add-credentials-to-your-web-application
+ *
+ */
 export class ClientSecretCredential implements TokenCredential {
   private identityClient: IdentityClient;
   private _tenantId: string;
   private _clientId: string;
   private _clientSecret: string;
 
+  /**
+   * Creates an instance of the ClientSecretCredential with the details
+   * needed to authenticate against Azure Active Directory with a client
+   * secret.
+   * 
+   * @param tenantId The Azure Active Directory tenant (directory) ID.
+   * @param clientId The client (application) ID of an App Registration in the tenant.
+   * @param clientSecret A client secret that was generated for the App Registration.
+   * @param options Options for configuring the client which makes the authentication request.
+   */
   constructor(
     tenantId: string,
     clientId: string,
@@ -22,6 +40,16 @@ export class ClientSecretCredential implements TokenCredential {
     this._clientSecret = clientSecret;
   }
 
+  /**
+   * Authenticates with Azure Active Directory and returns an {@link AccessToken} if
+   * successful.  If authentication cannot be performed at this time, this method may
+   * return null.  If an error occurs during authentication, an {@link AuthenticationError}
+   * containing failure details will be thrown.
+   * 
+   * @param scopes The list of scopes for which the token will have access.
+   * @param options The options used to configure any requests this
+   *                TokenCredential implementation might make.
+   */
   public getToken(
     scopes: string | string[],
     options?: GetTokenOptions
