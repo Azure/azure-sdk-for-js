9bf0c765233e6bafc641a2ab79bbf2a0a683e791
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-06-28 12:01:53 -0700

    JS samples - in sync with TS samples - storage-file

--- sdk/storage/storage-file/samples/javascript/basic.js
+++ sdk/storage/storage-file/samples/javascript/basic.js
@@ -2,12 +2,7 @@
  Setup: Enter your storage account name and shared key in main()
 */
 
-const {
-  newPipeline,
-  FileServiceClient,
-  SharedKeyCredential,
-  AnonymousCredential
-} = require("../.."); // Change to "@azure/storage-file" in your package
+import { FileServiceClient, SharedKeyCredential } from "../../src"; // Change to "@azure/storage-file" in your package
 
 async function main() {
   // Enter your storage account name and shared key
@@ -19,16 +14,13 @@ async function main() {
   const sharedKeyCredential = new SharedKeyCredential(account, accountKey);
 
   // Use AnonymousCredential when url already includes a SAS signature
-  const anonymousCredential = new AnonymousCredential();
-
-  // Use sharedKeyCredential or anonymousCredential to create a pipeline
-  const pipeline = newPipeline(sharedKeyCredential);
+  // const anonymousCredential = new AnonymousCredential();
 
   // List shares
   const serviceClient = new FileServiceClient(
     // When using AnonymousCredential, following url should include a valid SAS
     `https://${account}.file.core.windows.net`,
-    pipeline
+    sharedKeyCredential
   );
 
   console.log(`List shares`);
---
+++ sdk/storage/storage-file/samples/javascript/proxyAuth.js
@@ -0,0 +1,43 @@
+/* 
+ Setup: Enter your storage account name and shared key in main()
+*/
+
+import { SharedKeyCredential, FileServiceClient } from "../../src"; // Change to "@azure/storage-blob" in your package
+
+async function main() {
+  // Enter your storage account name and shared key
+  const account = "";
+  const accountKey = "";
+
+  // Use SharedKeyCredential with storage account and account key
+  // SharedKeyCredential is only avaiable in Node.js runtime, not in browsers
+  const sharedKeyCredential = new SharedKeyCredential(account, accountKey);
+
+  const serviceClient = new FileServiceClient(
+    `https://${account}.file.core.windows.net`,
+    sharedKeyCredential,
+    {
+      // Proxy is supported in Node.js environment only, not in browsers
+      proxy: { url: "http://localhost:3128" }
+    }
+  );
+
+  // Create a share
+  const shareName = `newshare${new Date().getTime()}`;
+  const shareClient = serviceClient.getShareClient(shareName);
+  await shareClient.create();
+  console.log(`Create share ${shareName} successfully`);
+
+  // Delete share
+  await shareClient.delete();
+  console.log(`deleted share ${shareName}`);
+}
+
+// An async method returns a Promise object, which is compatible with then().catch() coding style.
+main()
+  .then(() => {
+    console.log("Successfully executed the sample.");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
