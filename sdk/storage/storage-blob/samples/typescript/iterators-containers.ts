eb894e5fce41f629fb399654ebf6b24d4e3b5158
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-06-27 21:52:35 -0700

    SharedKeyCred is only avaiable in Node.js runtime

--- sdk/storage/storage-blob/samples/typescript/iterators-containers.ts
+++ sdk/storage/storage-blob/samples/typescript/iterators-containers.ts
@@ -10,6 +10,7 @@ async function main() {
   const accountKey = "";
 
   // Use SharedKeyCredential with storage account and account key
+  // SharedKeyCredential is only avaiable in Node.js runtime, not in browsers
   const sharedKeyCredential = new SharedKeyCredential(account, accountKey);
 
   // List containers
