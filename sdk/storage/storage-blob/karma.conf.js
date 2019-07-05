d87fb8648b78dab905740f0956a709d9ee5563b0
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-07-05 15:23:47 -0700

    resolve console.logs issue in recordings

--- sdk/storage/storage-blob/karma.conf.js
+++ sdk/storage/storage-blob/karma.conf.js
@@ -84,22 +84,31 @@ module.exports = function(config) {
 
     jsonToFileReporter: {
       filter: function(obj) {
-        if (obj.writeFile) {
-          const fs = require("fs-extra");
-          // Create the directories recursively incase they don't exist
-          try {
-            // Stripping away the filename from the file path and retaining the directory structure
-            fs.ensureDirSync(obj.path.substring(0, obj.path.lastIndexOf("/") + 1));
-          } catch (err) {
-            if (err.code !== "EEXIST") throw err;
-          }
-          fs.writeFile(obj.path, JSON.stringify(obj.content, null, " "), (err) => {
-            if (err) {
-              throw err;
+        // - jsonToFileReporter filters the JSON strings in console.logs.
+        // - Console logs with `.writeFile` property are captured and are written to a file(recordings).
+        // - The other console statements are captured and printed normally.
+        // - Example - console.warn("hello"); -> console.log({ warn: "hello" });
+        // - Example - console.log("hello"); -> console.log({ log: "hello" });
+        if (process.env.TEST_MODE === "record") {
+          if (obj.writeFile) {
+            const fs = require("fs-extra");
+            // Create the directories recursively incase they don't exist
+            try {
+              // Stripping away the filename from the file path and retaining the directory structure
+              fs.ensureDirSync(obj.path.substring(0, obj.path.lastIndexOf("/") + 1));
+            } catch (err) {
+              if (err.code !== "EEXIST") throw err;
             }
-          });
+            fs.writeFile(obj.path, JSON.stringify(obj.content, null, " "), (err) => {
+              if (err) {
+                throw err;
+              }
+            });
+          } else {
+            console.log(obj);
+          }
+          return false;
         }
-        return false;
       },
       outputPath: "."
     },
