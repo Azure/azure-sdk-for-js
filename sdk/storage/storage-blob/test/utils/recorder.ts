4a330f77fe9519216970f299126ea3d9f9260a0b
Harsha Nalluru <sanallur@microsoft.com>
2019-07-08 15:05:05 -0700

    [Storage] Record and playback - console.logs issue for browser tests (#3711)
    * resolve console.logs issue in recordings
    
    * update RecordAndPlayback docs regarding console.logs in browser
    
    * crisis averted - comment no longer needed
    
    * Add hasOwnProperty check

--- sdk/storage/storage-blob/test/utils/recorder.ts
+++ sdk/storage/storage-blob/test/utils/recorder.ts
@@ -9,7 +9,63 @@ dotenv.config({ path: "../.env" });
 
 let nock: any;
 if (!isBrowser()) {
-  nock = require("nock");
+  if (process.env.TEST_MODE === "record" || process.env.TEST_MODE === "playback") {
+    nock = require("nock");
+  }
+} else if ((window as any).__env__.TEST_MODE === "record") {
+  // Converting content corresponding to all the console statements
+  //  into (JSON.stringify)-ed content in record mode for browser tests.
+  //
+  // In browser, once the content to be recorded is ready, recordings
+  //  are supposed to be sent to the appropriate karma reporter(jsonToFileReporter)
+  //  in order to generate the corresponding recording file.
+  // The way to do this is by printing the recordings as JSON strings to `console.log()`.
+  // As a result, the console gets filled with lots of prints while recording.
+  //
+  // We solve this issue by
+  // - disabling the console.logs from karma and
+  // - by adding a custom console.log() which converts all the console statements into
+  //   console.log() with stringified JSON objects.
+  // - Handle all the console.logs with stringified JSON objects in karma.conf.js
+  //   as explained below.
+  //
+  // Karma.conf.js
+  // - jsonToFileReporter in karma.conf.js filters the JSON strings in console.logs.
+  // - Console logs with `.writeFile` property are captured and are written to a file(recordings).
+  // - The other console statements are captured and printed normally.
+  // - Example - console.warn("hello"); -> console.log({ warn: "hello" });
+  // - Example - console.log("hello"); -> console.log({ log: "hello" });
+  const consoleLog = window.console.log;
+  for (const method in window.console) {
+    if (
+      window.console.hasOwnProperty(method) &&
+      typeof (window.console as any)[method] === "function"
+    ) {
+      (window.console as any)[method] = function(obj: any) {
+        try {
+          if (!JSON.parse(obj).writeFile) {
+            // If the JSON string doesn't contain `.writeFile` property,
+            // we wrap the object as a JSON object and apply JSON.stringify()
+            // Example - console.warn("hello"); -> console.log({ warn: "hello" });
+            const newObj: any = {};
+            newObj[method] = obj;
+            consoleLog(JSON.stringify(newObj));
+          } else {
+            // If the JSON strings contain `.writeFile` property,
+            // use the console.log as it is.
+            consoleLog(obj);
+          }
+        } catch (error) {
+          // If the object is not a JSON string, the try block fails and
+          // we wrap the object as a JSON object and apply JSON.stringify()
+          // (same as the if block in try)
+          const newObj: any = {};
+          newObj[method] = obj;
+          consoleLog(JSON.stringify(newObj));
+        }
+      };
+    }
+  }
 }
 
 const env = isBrowser() ? (window as any).__env__ : process.env;
