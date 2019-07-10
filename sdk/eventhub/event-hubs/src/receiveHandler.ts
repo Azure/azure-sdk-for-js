e18ef840417e21d923cfa0a1496c638c3637d88b
chradek <chradek@microsoft.com>
2019-07-10 13:57:07 -0700

    adds copyright to receiveHandler.ts

--- sdk/eventhub/event-hubs/src/receiveHandler.ts
+++ sdk/eventhub/event-hubs/src/receiveHandler.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import * as log from "./log";
 import { EventHubReceiver } from "./eventHubReceiver";
 
