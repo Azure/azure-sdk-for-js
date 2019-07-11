0639d0190c450d0a4c6021bda2006be5bf1753f0
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-07-11 14:27:07 -0700

    remove delay from utils.ts file

--- sdk/test-utils/recorder/src/utils.ts
+++ sdk/test-utils/recorder/src/utils.ts
@@ -1,13 +1,3 @@
-/**
- * A wrapper for setTimeout that resolves a promise after t milliseconds.
- * @param {number} t The number of milliseconds to be delayed.
- * @param {T} value The value to be resolved with after a timeout of t milliseconds.
- * @returns {Promise<T>} Resolved promise
- */
-export function restDelay<T>(t: number, value?: T): Promise<T> {
-  return new Promise((resolve) => setTimeout(() => resolve(value), t));
-}
-
 export async function blobToString(blob: Blob): Promise<string> {
   const fileReader = new FileReader();
   return new Promise<string>((resolve, reject) => {
