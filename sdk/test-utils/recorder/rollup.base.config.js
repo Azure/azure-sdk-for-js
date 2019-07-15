92bedd870aafd87e335a4bff1948c3d8fdb1f44e
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-07-15 14:49:27 -0700

    mark nock as external for browser

--- sdk/test-utils/recorder/rollup.base.config.js
+++ sdk/test-utils/recorder/rollup.base.config.js
@@ -55,7 +55,7 @@ export function nodeConfig(test = false) {
 export function browserConfig(test = false, production = false) {
   const baseConfig = {
     input: input,
-    external: ["fs-extra"],
+    external: ["fs-extra", "nock"],
     output: {
       file: "browser/azure-test-utils-recorder.js",
       format: "umd",
