d618931f37591b8a1e998414bd9a2aff176c8d58
Shivangi Reja <shreja@microsoft.com>
2019-07-16 16:09:31 -0700

    Add extra line

--- sdk/eventhub/event-processor-host/src/util/constants.ts
+++ sdk/eventhub/event-processor-host/src/util/constants.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 import { name, version } from "../../package.json";
 
 export const minLeaseDurationInSeconds = 15;
