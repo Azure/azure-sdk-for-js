776ab360f4489180b9924e661f4990a34cc18e0c
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2019-07-19 15:14:15 -0700

    Use of Prettier

--- samples/SmokeTest/KeyVaultTest.ts
+++ samples/SmokeTest/KeyVaultTest.ts
@@ -1,13 +1,13 @@
 import { EnvironmentCredential } from "@azure/identity";
 import { SecretsClient } from "@azure/keyvault-secrets";
 
-export class KeyVaultSecrets{
-    private static client : SecretsClient;
-    private static secretName : string;
-    private static secretValue : string;
-    
-    static async Run() {
-        console.log(`
+export class KeyVaultSecrets {
+  private static client: SecretsClient;
+  private static secretName: string;
+  private static secretValue: string;
+
+  static async Run() {
+    console.log(`
         ------------------------
         Key Vault - Secrets
         Identity - Credential
@@ -16,51 +16,56 @@ export class KeyVaultSecrets{
         2) Get that secret
         3) Delete that secret (Clean up the resource)
         `);
-        
-        // EnvironmentCredential expects the following three environment variables:
-        // * AZURE_TENANT_ID: The tenant ID in Azure Active Directory
-        // * AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
-        // * AZURE_CLIENT_SECRET: The client secret for the registered application
-        const credential = new EnvironmentCredential();
-        const url = process.env["AZURE_PROJECT_URL"] || "<YourProjectURL>";
 
-        KeyVaultSecrets.client = new SecretsClient(url,credential);
+    // EnvironmentCredential expects the following three environment variables:
+    // * AZURE_TENANT_ID: The tenant ID in Azure Active Directory
+    // * AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
+    // * AZURE_CLIENT_SECRET: The client secret for the registered application
+    const credential = new EnvironmentCredential();
+    const url = process.env["AZURE_PROJECT_URL"] || "<YourProjectURL>";
 
-        KeyVaultSecrets.secretName = "MySecretName";
-        KeyVaultSecrets.secretValue = "MySecretValue";
+    KeyVaultSecrets.client = new SecretsClient(url, credential);
 
-        try {
-            await KeyVaultSecrets.setSecret();
-            await KeyVaultSecrets.getSecret();
-        } catch (err) {
-            throw err;
-        } finally {
-            await KeyVaultSecrets.deleteSecret();
-        }
+    KeyVaultSecrets.secretName = "MySecretName";
+    KeyVaultSecrets.secretValue = "MySecretValue";
+
+    try {
+      await KeyVaultSecrets.setSecret();
+      await KeyVaultSecrets.getSecret();
+    } catch (err) {
+      throw err;
+    } finally {
+      await KeyVaultSecrets.deleteSecret();
+    }
+  }
+
+  private static async setSecret() {
+    console.log("\tSetting a secret...");
+    const result = await KeyVaultSecrets.client.setSecret(
+      KeyVaultSecrets.secretName,
+      KeyVaultSecrets.secretValue
+    );
+    console.log("\t\tSecret = (" + result.name + "," + result.value + ")");
+    console.log("\t\tdone");
+  }
+
+  private static async getSecret() {
+    console.log("\tGetting that secret...");
+    const result = await KeyVaultSecrets.client.getSecret(KeyVaultSecrets.secretName);
+
+    if (
+      result.name !== KeyVaultSecrets.secretName ||
+      result.value !== KeyVaultSecrets.secretValue
+    ) {
+      throw "Error, a secret was obtained but is not the one that was setted before";
     }
 
-    private static async setSecret(){
-        console.log("\tSetting a secret...");
-        const result = await KeyVaultSecrets.client.setSecret(KeyVaultSecrets.secretName, KeyVaultSecrets.secretValue);
-        console.log("\t\tSecret = (" + result.name +","+ result.value + ")");
-        console.log("\t\tdone");
-    }
+    console.log("\t\tdone");
+  }
 
-    private static async getSecret(){
-        console.log("\tGetting that secret...");
-        const result = await KeyVaultSecrets.client.getSecret(KeyVaultSecrets.secretName);
-
-        if((result.name !== KeyVaultSecrets.secretName)||(result.value !== KeyVaultSecrets.secretValue)){
-            throw "Error, a secret was obtained but is not the one that was setted before";
-        }
-
-        console.log("\t\tdone");
-    }
-
-    private static async deleteSecret(){
-        console.log("\tDeleting that secret...");
-        await KeyVaultSecrets.client.deleteSecret(KeyVaultSecrets.secretName);
-        console.log("\t\tdone");
-    }
-
-} 
+  private static async deleteSecret() {
+    console.log("\tDeleting that secret...");
+    await KeyVaultSecrets.client.deleteSecret(KeyVaultSecrets.secretName);
+    console.log("\t\tdone");
+  }
+}
--- samples/SmokeTest/app.ts
+++ samples/SmokeTest/app.ts
@@ -1,23 +1,23 @@
-import {KeyVaultSecrets} from "./KeyVaultTest";
-import {EventHubs} from "./EventHubsTest";
-import {BlobStorage} from "./BlobStorage";
-import {CosmosDB} from "./CosmosDB";
+import { KeyVaultSecrets } from "./KeyVaultTest";
+import { EventHubs } from "./EventHubsTest";
+import { BlobStorage } from "./BlobStorage";
+import { CosmosDB } from "./CosmosDB";
 
-function welcomeMessage(){
-    console.log(`
-=============================================
-Smoke Test Samples for SDK Track 2 libraries
-============================================
-`);
-}
+function welcomeMessage() {
+  console.log(`
+    =============================================
+    Smoke Test Samples for SDK Track 2 libraries
+    ============================================
+    `);
+    }
 
-async function main(){
-    welcomeMessage();
+async function main() {
+  welcomeMessage();
 
-    await KeyVaultSecrets.Run();
-    await BlobStorage.Run();
-    await EventHubs.Run();
-    await CosmosDB.Run(); 
+  await KeyVaultSecrets.Run();
+  await BlobStorage.Run();
+  await EventHubs.Run();
+  await CosmosDB.Run();
 }
 
 main();
