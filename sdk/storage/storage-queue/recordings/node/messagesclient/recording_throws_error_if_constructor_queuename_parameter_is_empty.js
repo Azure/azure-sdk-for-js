174cf8bb26e9dca3bf96e0386f95c4a1ec1556bb
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-07-19 23:45:09 -0700

    moving a test from node to both node and browser - since sas conn string supports both

--- sdk/storage/storage-queue/recordings/node/messagesclient/recording_throws_error_if_constructor_queuename_parameter_is_empty.js
+++ sdk/storage/storage-queue/recordings/node/messagesclient/recording_throws_error_if_constructor_queuename_parameter_is_empty.js
@@ -1,37 +1,37 @@
 let nock = require('nock');
 
-module.exports.testInfo = {"queue":"queue156149556016601109"}
+module.exports.testInfo = {"queue":"queue156360508842502963"}
 
 nock('https://fakestorageaccount.queue.core.windows.net:443', {"encodedQueryParams":true})
-  .put('/queue156149556016601109')
+  .put('/queue156360508842502963')
   .query(true)
   .reply(201, "", [ 'Content-Length',
   '0',
   'Server',
   'Windows-Azure-Queue/1.0 Microsoft-HTTPAPI/2.0',
   'x-ms-request-id',
-  '8cd0b0d0-9003-0009-2896-2b601f000000',
+  '041af35a-c003-0011-71c6-3e4d8a000000',
   'x-ms-version',
   '2018-03-28',
   'Date',
-  'Tue, 25 Jun 2019 20:46:00 GMT',
+  'Sat, 20 Jul 2019 06:44:48 GMT',
   'Connection',
   'close' ]);
 
 
 nock('https://fakestorageaccount.queue.core.windows.net:443', {"encodedQueryParams":true})
-  .delete('/queue156149556016601109')
+  .delete('/queue156360508842502963')
   .query(true)
   .reply(204, "", [ 'Content-Length',
   '0',
   'Server',
   'Windows-Azure-Queue/1.0 Microsoft-HTTPAPI/2.0',
   'x-ms-request-id',
-  'bd7f53c1-0003-00ad-0696-2b5afb000000',
+  '7e8696f2-7003-0008-3ac6-3e61e2000000',
   'x-ms-version',
   '2018-03-28',
   'Date',
-  'Tue, 25 Jun 2019 20:45:59 GMT',
+  'Sat, 20 Jul 2019 06:44:48 GMT',
   'Connection',
   'close' ]);
 
