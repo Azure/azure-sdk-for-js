5378d62b552c213391433269c3dd7e07c3dafa9a
Mike Harder <mharder@microsoft.com>
2019-07-22 17:06:23 -0700

    Format pnpmfile.js

--- common/config/rush/pnpmfile.js
+++ common/config/rush/pnpmfile.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * When using the PNPM package manager, you can use pnpmfile.js to workaround
@@ -28,7 +28,6 @@ module.exports = {
  * The return value is the updated object.
  */
 function readPackage(packageJson, context) {
-
   // // The karma types have a missing dependency on typings from the log4js package.
   // if (packageJson.name === '@types/karma') {
   //  context.log('Fixed up dependencies for @types/karma');
