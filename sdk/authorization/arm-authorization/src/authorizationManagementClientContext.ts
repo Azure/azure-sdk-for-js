112eb09b5136d5743290a0469f9ff365b70c0f77
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-07-24 00:17:04 -0700

    PR Comments

--- sdk/authorization/arm-authorization/src/authorizationManagementClientContext.ts
+++ sdk/authorization/arm-authorization/src/authorizationManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-authorization";
-const packageVersion = "8.3.1";
+const packageVersion = "8.3.2";
 
 export class AuthorizationManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
