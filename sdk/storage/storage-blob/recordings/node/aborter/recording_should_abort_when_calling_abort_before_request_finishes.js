3e5ea138934fba13cba19a8ee9316d4ec1dcca50
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-07-29 13:25:56 -0700

    delete recordings

--- sdk/storage/storage-blob/recordings/node/aborter/recording_should_abort_when_calling_abort_before_request_finishes.js
+++
@@ -1,3 +0,0 @@
-let nock = require('nock');
-
-module.exports.testInfo = {"container":"container156150780657507778"}
--- sdk/storage/storage-blob/recordings/node/aborter/recording_should_set_value_and_get_value_successfully.js
+++
@@ -1,3 +0,0 @@
-let nock = require('nock');
-
-module.exports.testInfo = {"container":"container156150780603601216"}
