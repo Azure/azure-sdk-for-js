334d71207c11d8968785c2e4a7b008194e0a7bef
bsiegel <96068+bsiegel@users.noreply.github.com>
2019-07-31 12:43:33 -0700

    Add dependency analysis script (#4548)
    * Initial port of template to Handlebars
    
    * Collect dependency info from package.jsons
    
    * Generate inconsistent dependency info
    
    * Remove lockfile processing
    
    * eslint and prettier
    
    * Sort everything in the HTML report
    
    * Exclude from docwarden

---
+++ eng/tools/analyze-deps/index.js
@@ -0,0 +1,237 @@
+const Buffer = require("buffer").Buffer;
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const util = require("util");
+
+const argparse = require("argparse");
+const Handlebars = require("handlebars");
+const jju = require("jju");
+const tar = require("tar");
+
+const readdir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const appendPackageData = (data, pkgSrc, pkgJson) => {
+  data[pkgJson.name] = {
+    src: pkgSrc,
+    ver: pkgJson.version,
+    run: pkgJson.dependencies,
+    dev: pkgJson.devDependencies,
+    peer: pkgJson.peerDependencies
+  };
+};
+
+const getRushPackages = async (rushPath) => {
+  const baseDir = path.dirname(rushPath);
+  const rushJson = jju.parse(await readFile(rushPath, "utf8"));
+  const packageData = {};
+
+  for (const proj of rushJson.projects) {
+    const projDir = path.join(baseDir, proj.projectFolder);
+    const packageJson = jju.parse(await readFile(path.join(projDir, "package.json"), "utf8"));
+    appendPackageData(packageData, projDir, packageJson);
+  }
+
+  return packageData;
+};
+
+const readCompressedFile = async (archivePath, filePath, encoding) => {
+  const data = [];
+  let processed = false;
+
+  await tar.t(
+    {
+      file: archivePath,
+      onentry: (entry) => {
+        if (!processed) {
+          processed = true;
+          entry.on("data", (c) => {
+            data.push(c);
+          });
+        }
+      }
+    },
+    [filePath]
+  );
+
+  if (data) {
+    return Buffer.concat(data).toString(encoding);
+  } else {
+    return undefined;
+  }
+};
+
+const getTarballPackages = async (tarballDir) => {
+  const files = await readdir(tarballDir);
+  const packageData = {};
+
+  for (const file of files) {
+    const filePath = path.join(tarballDir, file);
+    if (path.extname(filePath).toLowerCase() === ".tgz") {
+      const packageJson = jju.parse(
+        await readCompressedFile(filePath, "package/package.json", "utf8")
+      );
+      appendPackageData(packageData, filePath, packageJson);
+    }
+  }
+  return packageData;
+};
+
+const render = async (context, dest) => {
+  context.repo_name = "azure-sdk-for-js";
+  context.branch =
+    process.env.SYSTEM_PULLREQUEST_SOURCEBRANCH || process.env.BUILD_SOURCEBRANCHNAME;
+  context.build = process.env.BUILD_BUILDNUMBER;
+  context.build_url = `${process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}${process.env.SYSTEM_TEAMPROJECT}/_build/results?buildId=${process.env.BUILD_BUILDID}`;
+  context.commit = process.env.BUILD_SOURCEVERSION;
+  context.isfork = process.env.SYSTEM_PULLREQUEST_ISFORK === "True";
+  context.rel_url = process.env.RELEASE_RELEASEWEBURL;
+  context.release = process.env.RELEASE_RELEASENAME;
+  context.repo = context.isfork ? process.env.BUILD_REPOSITORY_NAME : `Azure/${context.repo_name}`;
+  context.curtime = new Date().toISOString();
+
+  Handlebars.registerHelper({
+    and: (a, b) => a && b,
+    capitalize: (s) => new Handlebars.SafeString(s ? s.charAt(0).toUpperCase() + s.slice(1) : ""),
+    contains: (c, i) => (typeof c.includes === "function" ? c.includes(i) : i in c),
+    default: (s, def) => new Handlebars.SafeString(s ? s : def),
+    dep_type: (p) => {
+      const ret = [];
+      if (context.inconsistent.includes(p)) {
+        ret.push("inconsistent");
+      }
+      if (context.external.includes(p)) {
+        ret.push("external");
+      } else {
+        ret.push("internal");
+      }
+      return ret.join(" ");
+    },
+    len: (c) => (typeof c.length === "number" ? c.length : Object.keys(c).length),
+    ne: (a, b) => a !== b,
+    or: (a, b) => a || b,
+    pluralize: (num, singular, plural) => new Handlebars.SafeString(num === 1 ? singular : plural),
+    sorted: (c) => (typeof c.sort === "function" ? c.sort() : Object.entries(c).sort()),
+    sub: (a, b) => a - b,
+    title: (s) => new Handlebars.SafeString(s ? s.replace(/\b\S/g, (t) => t.toUpperCase()) : ""),
+    truncate: (s, len) => new Handlebars.SafeString(s.substr(0, len))
+  });
+
+  const template = await readFile("deps.html.hbs", "utf8");
+  return writeFile(dest, Handlebars.compile(template)(context));
+};
+
+const appendDependencyData = (dependencies, dep, spec, package, depType) => {
+  if (!dependencies[dep]) {
+    dependencies[dep] = {};
+  }
+  if (!dependencies[dep][spec]) {
+    dependencies[dep][spec] = [];
+  }
+  dependencies[dep][spec].push([package, depType]);
+};
+
+const constructDeps = (pkgs) => {
+  const dependencies = {};
+
+  for (const [name, data] of Object.entries(pkgs)) {
+    for (const [dep, spec] of Object.entries(data.run || {})) {
+      appendDependencyData(dependencies, dep, spec, name, "runtime");
+    }
+    for (const [dep, spec] of Object.entries(data.dev || {})) {
+      appendDependencyData(dependencies, dep, spec, name, "dev");
+    }
+    for (const [dep, spec] of Object.entries(data.peer || {})) {
+      appendDependencyData(dependencies, dep, spec, name, "peer");
+    }
+  }
+
+  return dependencies;
+};
+
+const main = async () => {
+  const parser = new argparse.ArgumentParser({
+    prog: "analyze-deps",
+    description: "Analyze dependencies in NodeJS packages."
+  });
+  parser.addArgument("--verbose", { help: "verbose output", action: "storeTrue" });
+  parser.addArgument("--out", { metavar: "FILE", help: "write HTML-formatted report to FILE" });
+  parser.addArgument("--packdir", {
+    metavar: "DIR",
+    help: "analyze packed tarballs in DIR rather than source packages in this repository"
+  });
+  const args = parser.parseArgs();
+
+  const context = {
+    packages: {},
+    dependencies: {},
+    external: [],
+    inconsistent: []
+  };
+
+  const rushPackages = await getRushPackages(path.resolve(`${__dirname}/../../../rush.json`));
+  context.packages = args.packdir
+    ? await getTarballPackages(path.resolve(args.packdir))
+    : rushPackages;
+  context.dependencies = constructDeps(context.packages);
+  context.external = Object.keys(context.dependencies).filter((p) => !(p in rushPackages));
+  context.inconsistent = Object.keys(context.dependencies).filter(
+    (p) => Object.keys(context.dependencies[p]).length > 1
+  );
+
+  if (args.verbose) {
+    console.log("Packages analyzed:");
+    for (const package of Object.keys(context.packages).sort()) {
+      const info = context.packages[package];
+      console.log(`${package} ${info.ver}`);
+      console.log(`  from ${info.src}`);
+    }
+
+    console.log("\nDependencies discovered:");
+    for (const dep of Object.keys(context.dependencies).sort()) {
+      const info = context.dependencies[dep];
+      console.log(`${dep}`);
+      for (const ver of Object.keys(info).sort()) {
+        const pkgs = info[ver];
+        console.log(`${ver}`);
+        for (const pkg of pkgs.sort()) {
+          console.log(`  * ${pkg[0]} (${pkg[1]})`);
+        }
+      }
+      console.log("");
+    }
+
+    for (const inc of context.inconsistent) {
+      const info = context.dependencies[inc];
+      const vers = Object.keys(info).sort();
+      console.log(`\nDependency '${inc}' has ${vers.length} unique specifiers:`);
+      for (const ver of vers.sort()) {
+        const pkgs = info[ver];
+        console.log(`'${ver}'`);
+        console.log(`${"-".repeat(ver.length + 2)}`);
+        for (const pkg of pkgs.sort()) {
+          console.log(`  * ${pkg[0]} (${pkg[1]})`);
+        }
+        console.log("");
+      }
+    }
+  }
+
+  if (context.inconsistent.length > 0) {
+    if (!args.verbose) {
+      console.log(
+        "Incompatible dependency versions detected in libraries, run this script with --verbose for details"
+      );
+    }
+  } else {
+    console.log("All library dependencies verified, no incompatible versions detected");
+  }
+
+  if (args.out) {
+    await render(context, args.out);
+  }
+};
+
+main();
