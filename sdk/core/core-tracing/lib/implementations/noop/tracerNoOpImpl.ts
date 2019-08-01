6c3de84fd7f7b7cbd3c251a390d05f1cba6fe252
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-08-01 13:48:19 -0700

    Add NoOp Impl

--- sdk/core/core-tracing/lib/implementations/noop/tracerNoOpImpl.ts
+++ sdk/core/core-tracing/lib/implementations/noop/tracerNoOpImpl.ts
@@ -1,13 +1,14 @@
 import { Tracer } from "../../interfaces/tracer";
 import { SpanOptions } from "../../interfaces/SpanOptions";
 import { Span } from "../../interfaces/span";
+import { SpanNoOpImpl } from "./spanNoOpImpl";
 
 export class TracerNoOpImpl implements Tracer {
   getCurrentSpan(): Span {
     throw new Error("Method not implemented.");
   }
   startSpan(name: string, options?: SpanOptions | undefined): Span {
-    throw new Error("Method not implemented.");
+    return new SpanNoOpImpl();
   }
   withSpan<T extends (...args: unknown[]) => unknown>(span: Span, fn: T): ReturnType<T> {
     throw new Error("Method not implemented.");
