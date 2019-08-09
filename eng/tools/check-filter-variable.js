9696d83c581ea602cef1cade21fa8dec2b14c6a7
Karishma Ghiya <kaghiya@microsoft.com>
2019-08-08 17:39:25 -0700

    deleted filter variable script

--- eng/tools/check-filter-variable.js
+++
@@ -1,5 +0,0 @@
-let globFilter = process.argv[2];
-if(globFilter === undefined || globFilter ===""){
-globFilter = "**";
-}
-console.log(`##vso[task.setvariable variable=packageFilter]${globFilter}`);
