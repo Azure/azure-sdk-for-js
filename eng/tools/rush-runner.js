e1a12126036ad6b51a29ad87ed806f3df834aa2c
Brandon Siegel <brandon.siegel@microsoft.com>
2019-08-09 17:55:07 -0700

    Refactor rush-runner

--- eng/tools/rush-runner.js
+++ eng/tools/rush-runner.js
@@ -1,176 +1,112 @@
-// See the @microsoft/rush package's LICENSE file for license information.
-
-// This script searches for package.json files recursively under a given path
-// excluding packages under a node_modules folder and opens them to determine
-// whether they contain a "sdk-archetype" field with the value of "client".
-//
-// If they do, then the package name is recorded and then after all package.json files
-// have been scanned a single string is output using the Azure Pipelines Commands special
-// syntax which creates a variable for use within a later stage of the pipeine.
-
 const fs = require('fs');
-let path = require('path');
+const path = require('path');
+const process = require('process');
 const { spawn } = require('child_process');
 
+const parseArgs = () => {
+  if (process.argv.length < 3 || process.argv.some(a => ['-h', '--help'].includes(a.toLowerCase()))) {
+    console.error('Usage: rush-runner.js <action> [<servicename>...] [args...]');
+    console.error('Example: rush-runner.js build keyvault storage --verbose');
+    process.exit(1);
+  }
+
+  let inFlags = false;
+  const services = [], flags = [];
+  const [scriptPath, action, ...givenArgs] = process.argv.slice(1);
+  const baseDir = path.resolve(`${path.dirname(scriptPath)}/../..`);
+
+  for (const arg of givenArgs) {
+    if (!inFlags && arg.startsWith('-')) {
+      inFlags = true;
+    }
+
+    if (inFlags) {
+      flags.push(arg);
+    } else {
+      if (arg !== '*') { // exclude special value "*" meaning all libraries
+        services.push(arg);
+      }
+    }
+  }
+  return [baseDir, action, services, flags];
+};
+
 const getPackageJsons = (searchDir) => {
-  console.log("searchDir=" + searchDir);
-  const packageJsons = fs.readdirSync(searchDir)
+  return fs.readdirSync(searchDir)
     .filter(f => !f.startsWith('arm-')) // exclude libraries starting with "arm-"
     .map(f => path.join(searchDir, f, 'package.json')) // turn potential directory names into package.json paths
     .filter(f => fs.existsSync(f)); // only keep paths for files that actually exist
-  
-  return packageJsons;
-}
-
-let logLineCounter = 0;
-const log = message => {
-  console.log(`select-packages(${logLineCounter++}): ${message}`);
 };
 
-log(`Working directory is "${process.cwd()}".`);
-
-let [action, ...givenArgs] = process.argv.slice(2);
-let pos = 0, i = 0;
-let serviceDirList = [];
-for(arg of givenArgs){
-  if(arg == "--"){
-    pos = i; 
-    break;
-  }
-
-  serviceDirList.push(arg);
-  i++;
-}
-let rushParams = [];
-
-for(arg of givenArgs){
-  
-  if(arg == "--"){
-    continue;
-  }
-  if(arg.startsWith("--") || arg.startsWith("-")){
-    rushParams.push(arg);
-  }
-}
-
-let packageJsons = [];
-if (serviceDirList.length > 0) {
-  for(serviceDir of serviceDirList){
-    const searchDir = path.resolve(`../../sdk/${serviceDir}`);
-    let listJsons = getPackageJsons(searchDir);
-    packageJsons = packageJsons.concat(listJsons);
-  }
-} else {
-  const sdkDir = path.resolve(`../../sdk`);
-  const serviceDirs = fs.readdirSync(sdkDir)
-    .map(f => path.join(sdkDir,f)) // turn directory names into paths
-    .filter(f => fs.lstatSync(f).isDirectory()); // only keep those which are actually directories (not files)
-
-  for (const dir of serviceDirs) {
-    packageJsons.concat(getPackageJsons(dir));
-  }
-}
-
-const packageNames = [];
-const packageDirs = [];
-for (const filePath of packageJsons) {
-  const contents = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  if (contents['sdk-type'] === 'client') {
-    packageNames.push(contents.name);
-    packageDirs.push(path.dirname(filePath));
-  }
-}
-
-if (!rushParams) {
-  rushParams = [];
-}
-let rushArgs = [];
-let args = [];
-let params = [];
-switch (action.toLowerCase()) {
-  case 'build':
-      params = packageNames.map(p => [`--to`,p,`--from`,p]);
-      args = ['../../common/scripts/install-run-rush.js','build'];
-      rushArgs = [].concat(args, ...params, rushParams);
-
-      const build = spawn('node', rushArgs);
-      build.stdout.on('data', (data) => {
-        log(`stdout: ${data}`);
-        });
-
-      build.stderr.on('data', (data) => {
-        log(`stderr: ${data}`);
-      });
-
-      build.on('close', (code) => {
-        log(`child process exited with code ${code}`);
-      });
-    
-    break;
-
-  case 'test':
-    params = packageNames.map(p => [`--from`,p]);
-    args = ['../../common/scripts/install-run-rush.js','test'];
-    rushArgs = [].concat(args, ...params, rushParams);
-    const test = spawn('node', rushArgs);
-    test.stdout.on('data', (data) => {
-        log(`stdout: ${data}`);
-      });
-
-    test.stderr.on('data', (data) => {
-      log(`stderr: ${data}`);
-    });
-
-    test.on('close', (code) => {
-      log(`child process exited with code ${code}`);
-    });
-
-    break;
-
-  case 'lint':
-  console.log(rushParams);
-    if (serviceDir) {
-      for (const dir of packageDirs) {
-
-        // rushx doesn't accept rushParams - ????
-        const defaults = {
-                          cwd: dir,
-                          env: process.env
-                        };
-        args = ['../../../common/scripts/install-run-rushx.js','lint'];
-
-        rushArgs = [].concat(args, ...params, rushParams);
-        const lintr = spawn('node', rushArgs,defaults);
-        lintr.stdout.on('data', (data) => {
-          log(`stdout: ${data}`);
-        });
-
-        lintr.stderr.on('data', (data) => {
-          log(`stderr: ${data}`);
-        });
-
-        lintr.on('close', (code) => {
-          log(`child process exited with code ${code}`);
-        });
+const getServicePackages = (baseDir, serviceDirs) => {
+  const packageNames = [], packageDirs = [];
+  for (const serviceDir of serviceDirs) {
+    const searchDir = path.resolve(path.join(baseDir, 'sdk', serviceDir));
+    const packageJsons = getPackageJsons(searchDir);
+    for (const filePath of packageJsons) {
+      const contents = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+      if (contents['sdk-type'] === 'client') {
+        packageNames.push(contents.name);
+        packageDirs.push(path.dirname(filePath));
       }
-    } else {
-        ///run('rush', 'lint', rushParams)
-        args = ['../../common/scripts/install-run-rush.js','lint'];
-        rushArgs = [].concat(args, ...params, rushParams);
-        const lint = spawn('node', rushArgs);
-
-        lint.stdout.on('data', (data) => {
-          log(`stdout: ${data}`);
-        });
-
-        lint.stderr.on('data', (data) => {
-          log(`stderr: ${data}`);
-        });
-
-        lint.on('close', (code) => {
-          log(`child process exited with code ${code}`);
-        });
-
     }
-    break;
+  }
+
+  return [packageNames, packageDirs];
+};
+
+const spawnNode = (cwd, ...args) => {
+  const y = `\u001B[33m`;
+  const x = `\u001B[39m`;
+
+  console.log(`Executing: "node ${args.join(' ')}" in ${cwd}\n\n`);
+  const proc = spawn('node', args, { cwd });
+
+  proc.stdout.on('data', (data) => {
+    console.log(`${data}`);
+  });
+
+  proc.stderr.on('data', (data) => {
+    console.log(`${y}${data}${x}`);
+  });
+
+  proc.on('close', (code) => {
+    console.log(`\n\nNode process exited with code ${code}`);
+  });
+}
+
+const flatMap = (arr, f) => {
+  const result = arr.map(f);
+  return [].concat(...result);
+}
+
+const [baseDir, action, serviceDirs, rushParams] = parseArgs();
+const [packageNames, packageDirs] = getServicePackages(baseDir, serviceDirs);
+
+if (serviceDirs.length === 0) {
+  spawnNode(baseDir, 'common/scripts/install-run-rush.js', action, ...rushParams);
+} else {
+  let params = [];
+  switch (action.toLowerCase()) {
+    case 'build':
+      params = flatMap(packageNames, (p) => [`--to`, p, `--from`, p]);
+      spawnNode(baseDir, 'common/scripts/install-run-rush.js', action, ...params, ...rushParams);
+      break;
+
+    case 'test':
+      params = flatMap(packageNames, (p) => [`--from`, p]);
+      spawnNode(baseDir, 'common/scripts/install-run-rush.js', action, ...params, ...rushParams);
+      break;
+
+    case 'lint':
+      for (const packageDir of packageDirs) {
+        spawnNode(packageDir, '../../../common/scripts/install-run-rushx.js', action);
+      }
+      break;
+
+    default:
+      params = flatMap(packageNames, (p) => [`--to`, p]);
+      spawnNode(baseDir, 'common/scripts/install-run-rush.js', action, ...params, ...rushParams);
+      break;
+  }
 }
