ab1fb62972d5deb6e54974a8faeb607ca9eded88
Daniel Rodríguez <sadasant@gmail.com>
2019-08-24 23:00:19 +0000

    WIP

---
+++ sdk/core/core-lro/src/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+import * as lroPoller from "./lroPoller"
+import * as lroPollStrategy from "./lroPollStrategy"
+
+export {
+  ...lroPoller,
+  ...lroPollStrategy
+};
