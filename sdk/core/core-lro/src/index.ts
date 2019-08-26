81f72afdebdcc3bccaba8db396cfb78b3a33e532
Daniel Rodríguez <sadasant@gmail.com>
2019-08-26 20:56:13 +0000

    some of the feedback by Brian

--- sdk/core/core-lro/src/index.ts
+++ sdk/core/core-lro/src/index.ts
@@ -1,33 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import {
-  LROPoller,
-  createLROPollerFromInitialResponse,
-  createLROPollerFromPollState,
-} from "./lroPoller";
-
-import {
-  LROPollStrategyType,
-  LROPollState,
-  LROPollStrategy,
-  getDelayInSeconds,
-  isFinished,
-  longRunningOperationStatesEqual,
-  createLROPollStrategyFromInitialResponse,
-  createLROPollStrategyFromPollState,
-} from "./lroPollStrategy";
-
-export {
-  LROPoller,
-  createLROPollerFromInitialResponse,
-  createLROPollerFromPollState, 
-  LROPollStrategyType,
-  LROPollState,
-  LROPollStrategy,
-  getDelayInSeconds,
-  isFinished,
-  longRunningOperationStatesEqual,
-  createLROPollStrategyFromInitialResponse,
-  createLROPollStrategyFromPollState,
-};
+export * from "./lroPoller";
+export * from "./lroPollStrategy";
