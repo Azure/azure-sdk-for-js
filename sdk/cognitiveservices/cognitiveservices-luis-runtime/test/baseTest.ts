debac7cce45b888ac518188e6daa55039f3a65aa
Ahmed Abuamra <ah.abuamra@gmail.com>
2019-08-28 11:41:08 +0200

    resolved comments

--- sdk/cognitiveservices/cognitiveservices-luis-runtime/test/baseTest.ts
+++ sdk/cognitiveservices/cognitiveservices-luis-runtime/test/baseTest.ts
@@ -2,13 +2,13 @@ import { LUISRuntimeClient } from "../src/lUISRuntimeClient";
 import { CognitiveServicesCredentials } from "@azure/ms-rest-azure-js";
 
 export class BaseTest {
-  static GlobalAppId = "0894d430-8f00-4bcd-8153-45e06a1feca1";
+  static GlobalAppId = process.env['global_app_id'];
   static GlobalVersionId = "0.1";
-  static GlobalAppIdError = "86226c53-b7a6-416f-876b-226b2b5ab07d";
-  static GlobalNoneId = "73ca6bdd-e7fa-420e-b5b7-22fe281e3a64";
-  static AuthoringKey = "b15ebe3a1ec446a08f8021fe6f95f0f6";
+  static GlobalAppIdError = process.env['global_app_id_error'];
+  static GlobalNoneId = process.env['global_none_id'];
+  static AuthoringKey = process.env['authoring_key'];
   static EmptyId = "00000000-0000-0000-0000-000000000000";
-  static OwenerEmail = "a-ahabu@microsoft.com";
+  static OwnerEmail = process.env['owner_email'];
 
   static async useClientFor(test) {
     let client = new LUISRuntimeClient(
