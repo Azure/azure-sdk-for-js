db8dc2050f00d8cf3f0d5f8ed2c2b9d15d66a085
Karishma Ghiya <kaghiya@microsoft.com>
2019-08-28 17:06:33 -0700

    add a trailing / to the diff path

--- eng/tools/compare-packages.ps1
+++ eng/tools/compare-packages.ps1
@@ -31,7 +31,7 @@ try{
   }
   echo "Finished unzipping of all .tgz packages"
 
-  $diffFile = $pathForDiffFile+"Change_"+$dailyDevBuildNo + ".diff"
+  $diffFile = $pathForDiffFile+"/Change_"+$dailyDevBuildNo + ".diff"
   echo "created filename variable $diffFile"
 
   git diff $pathToMasterPkg $pathToCurrentPkg > $diffFile
