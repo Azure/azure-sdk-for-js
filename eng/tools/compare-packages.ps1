e52779b09ea781f7e621f30e9b2395535f8860b7
Karishma Ghiya <kaghiya@microsoft.com>
2019-08-29 19:47:50 -0700

    extracting and diffing another subfolder

--- eng/tools/compare-packages.ps1
+++ eng/tools/compare-packages.ps1
@@ -7,36 +7,55 @@ param (
 
 function ExtractTGZPackages($pathToPkg){
   $regExp = "-\d+\.\d+\.\d+(-\w*\.\d*)?(-dev-$dailyDevBuildNo)?"
+
+
+
+  ## $parentCurrent = Split-Path -Path $pathToPkg -Parent
+  ## $parentExtractName = (Split-Path -Path $pathToPkg -Leaf)+"-contents"  
+  ## $parentExtractDir = Join-Path $parentCurrent -ChildPath $parentExtractName
+
+
+  $parentExtractDir = Join-Path $pathToPkg -ChildPath "all-contents"
+  mkdir "$parentExtractDir"
   foreach ($p in $(dir $pathToPkg -r -i *.tgz)){
-    $extractDir = (get-item $p.FullName).Directoryname + "/" + $p.BaseName
+    $extractDir = Join-Path $parentExtractDir -ChildPath $p.BaseName
+    #$extractDir = Join-Path $pathToPkg -ChildPath $p.BaseName
+
     if($p.BaseName -match $regExp){
       $newDirName = ($p.BaseName -split $Matches.0)[0]
-      $extractDir = (get-item $p.FullName).Directoryname + "/" + $newDirName
+      $extractDir = Join-Path $parentExtractDir $newDirName
+      #$extractDir = Join-Path $pathToPkg $newDirName
     }
     mkdir "$extractDir"
     pushd "$extractDir"
     tar -xzf $p.FullName
     popd
   }
+  #return $pathToPkg
+  return $parentExtractDir
 }
 try{
-  ExtractTGZPackages($pathToMasterPkg)
+  $extractMasterDir = ExtractTGZPackages($pathToMasterPkg)
+  echo $extractMasterDir
   if($LastExitCode -ne 0){
     Write-Host "error >> ExtractTGZPackages($pathToMasterPkg) failed with exit code $LastExitCode"
   }
 
-  ExtractTGZPackages($pathToCurrentPkg)
+  $extractCurrentDir = ExtractTGZPackages($pathToCurrentPkg)
+  echo $extractCurrentDir 
   if($LastExitCode -ne 0){
     Write-Host "error >> ExtractTGZPackages($pathToCurrentPkg) failed with exit code $LastExitCode"
   }
   echo "Finished unzipping of all .tgz packages"
 
-  $diffFile = $pathForDiffFile+"/Change_"+$dailyDevBuildNo + ".diff"
+  $diffFile = Join-Path $pathForDiffFile "Change_$dailyDevBuildNo.diff"
   echo "created filename variable $diffFile"
-
-  git diff $pathToMasterPkg $pathToCurrentPkg | tee $diffFile
+  echo "extractMasterDir - $extractMasterDir"
+  echo "extractCurrentDir - $extractCurrentDir"
+  git diff $extractMasterDir $extractCurrentDir | tee $diffFile
+  #git diff $pathToMasterPkg $pathToCurrentPkg | tee $diffFile
   if($LastExitCode -ne 0){
-    Write-Host "error >> git diff $pathToMasterPkg $pathToCurrentPkg | tee $diffFile executes with exit code $LastExitCode"
+    Write-Host "error >> git diff $extractMasterDir $extractCurrentDir | tee $diffFile executes with exit code $LastExitCode"
   }
   echo "created the diff file - $diffFile"
   exit 0
@@ -45,3 +64,5 @@ catch{
   Write-Host "An error occurred:"
   Write-Host $_
 }
+#git diff C:\Users\kaghiya\Downloads\package_master C:\Users\kaghiya\Downloads\package_PR
+#git diff C:\Users\kaghiya\Downloads\package_master\all-contents\ C:\Users\kaghiya\Downloads\package_PR\all-contents\
