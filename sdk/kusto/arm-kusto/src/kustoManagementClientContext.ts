6d6319d48ab0490b24bda66f124cd7f7cabda9cb
Zhiqing Qiu <zhqqi@microsoft.com>
2019-09-02 12:50:51 +0800

    generate mgmt package: kusto

--- sdk/kusto/arm-kusto/src/kustoManagementClientContext.ts
+++ sdk/kusto/arm-kusto/src/kustoManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-kusto";
-const packageVersion = "3.0.0";
+const packageVersion = "3.0.1";
 
 export class KustoManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
