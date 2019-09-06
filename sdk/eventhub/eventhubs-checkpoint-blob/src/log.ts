e541f098ec0731589e7830939005cccc16e035d3
Shivangi Reja <shreja@microsoft.com>
2019-09-06 15:53:18 -0700

    Address comments

--- sdk/eventhub/eventhubs-checkpoint-blob/src/log.ts
+++ sdk/eventhub/eventhubs-checkpoint-blob/src/log.ts
@@ -6,9 +6,9 @@ import debugModule from "debug";
  * @ignore
  * log statements for error
  */
-export const error = debugModule("azure:event-hubs:error");
+export const error = debugModule("azure:eventhubs-checkpoint-blob");
 /**
  * @ignore
  * log statements for blobPartitionManager
  */
-export const blobPartitionManager = debugModule("azure:eventhubs-checkpoint-blob:blobPartitionManager");
+export const blobPartitionManager = debugModule("azure:eventhubs-checkpoint-blob:info");
