b202a15451959cc900333cf9c749ba17ce5b9f92
HarshaNalluru <harsha.nalluru1997@gmail.com>
2019-09-08 19:23:24 -0700

    emulator tests - update getGenericQSU function

--- sdk/storage/storage-queue/test/utils/index.ts
+++ sdk/storage/storage-queue/test/utils/index.ts
@@ -9,39 +9,46 @@ import {
   AccountSASServices
 } from "../../src";
 import { extractConnectionStringParts } from "../../src/utils/utils.common";
+import { env } from "./testutils.common";
 
 // Uncomment if need to enable logger when debugging
 // import {HttpPipelineLogLevel} from "../../src"
 // import {ConsoleHttpPipelineLogger} from "./testutils.common"
 
-export * from "./testutils.common";
-
 export function getGenericQSU(
   accountType: string,
   accountNameSuffix: string = ""
 ): QueueServiceClient {
-  const accountNameEnvVar = `${accountType}ACCOUNT_NAME`;
-  const accountKeyEnvVar = `${accountType}ACCOUNT_KEY`;
+  if (env.TEST_MODE === "emulator-tests") {
+    // Expected environment variables to run tests with the emulator
+    // [Azurite - Extension for VS Code](https://marketplace.visualstudio.com/items?itemName=Azurite.azurite)
+    // STORAGE_CONNECTION_STRING=UseDevelopmentStorage=true
+    // TEST_MODE=emulator-tests
+    return QueueServiceClient.fromConnectionString(getConnectionStringFromEnvironment());
+  } else {
+    const accountNameEnvVar = `${accountType}ACCOUNT_NAME`;
+    const accountKeyEnvVar = `${accountType}ACCOUNT_KEY`;
 
-  let accountName: string | undefined;
-  let accountKey: string | undefined;
+    let accountName: string | undefined;
+    let accountKey: string | undefined;
 
-  accountName = process.env[accountNameEnvVar];
-  accountKey = process.env[accountKeyEnvVar];
+    accountName = process.env[accountNameEnvVar];
+    accountKey = process.env[accountKeyEnvVar];
 
-  if (!accountName || !accountKey || accountName === "" || accountKey === "") {
-    throw new Error(
-      `${accountNameEnvVar} and/or ${accountKeyEnvVar} environment variables not specified.`
-    );
+    if (!accountName || !accountKey || accountName === "" || accountKey === "") {
+      throw new Error(
+        `${accountNameEnvVar} and/or ${accountKeyEnvVar} environment variables not specified.`
+      );
+    }
+
+    const credentials = new SharedKeyCredential(accountName, accountKey);
+    const pipeline = newPipeline(credentials, {
+      // Enable logger when debugging
+      // logger: new ConsoleHttpPipelineLogger(HttpPipelineLogLevel.INFO)
+    });
+    const queuePrimaryURL = `https://${accountName}${accountNameSuffix}.queue.core.windows.net/`;
+    return new QueueServiceClient(queuePrimaryURL, pipeline);
   }
-
-  const credentials = new SharedKeyCredential(accountName, accountKey);
-  const pipeline = newPipeline(credentials, {
-    // Enable logger when debugging
-    // logger: new ConsoleHttpPipelineLogger(HttpPipelineLogLevel.INFO)
-  });
-  const queuePrimaryURL = `https://${accountName}${accountNameSuffix}.queue.core.windows.net/`;
-  return new QueueServiceClient(queuePrimaryURL, pipeline);
 }
 
 export function getQSU(): QueueServiceClient {
