77bec38feb78ae9cd8caa7c04aee789a5d72de03
Ramya Raja <ramya.raja@microsoft.com>
2019-09-11 16:06:01 -0700

    Fix rollup

--- sdk/core/amqp-common/rollup.config.js
+++ sdk/core/amqp-common/rollup.config.js
@@ -11,6 +11,11 @@ if (!process.env.ONLY_BROWSER) {
 
 if (!process.env.ONLY_NODE) {
   inputs.push(base.browserConfig());
+  inputs.push(base.browserConfig({ production: true }));
+}
+
+if (process.env.BROWSER_TEST) {
+  inputs.push(base.browserConfig({ test: true }));
 }
 
 export default inputs;
