464cf77fa43f59f07a6744687a2943646b74d009
Daniel Rodríguez <sadasant@gmail.com>
2019-09-13 16:02:28 +0000

    prevent manual polling if automatic and resuming a poller from a serialized copy

--- sdk/core/core-lro/test/fakeClient.test.ts
+++ sdk/core/core-lro/test/fakeClient.test.ts
@@ -34,7 +34,7 @@ describe("Long Running Operations - custom client", function () {
       status: 204,
     }]);
     const poller = await client.startLRO({ automatic: false });
-    await poller.retry();
+    await poller.retry(); // Manual polling
     assert.equal(poller.getState(), "Succeeded");
     poller.forget();
   });
@@ -74,7 +74,7 @@ describe("Long Running Operations - custom client", function () {
   //   assert.equal(e.message, "Cancellation not supported");
   // });
 
-  it("allows polling to stop", async function () {
+  it("allows polling to stop (forget to continue polling)", async function () {
     const client = new FakeClient(new SimpleTokenCredential("my-fake-token"));
     const responses = Array(20).fill({
       ...basicResponseStructure,
@@ -82,33 +82,29 @@ describe("Long Running Operations - custom client", function () {
     });
     client.setResponses(responses);
     const poller = await client.startLRO();
-    console.log({ retries: poller.retries });
     assert.equal(poller.retries, 0);
-    console.log({ retries: poller.retries });
+    await delay(100);
     assert.equal(poller.retries, 9);
     poller.forget();
-    console.log({ retries: poller.retries });
+    await delay(100);
     assert.equal(poller.retries, 10);
   });
 
-  // it("allows manual polling", async function () {
-  //   const poller = await client.startLRO({ automatic: false });
-  //   await delay(100);
-  //   assert.equal(poller.retries, 0);
-  //   await poller.retry();
-  //   assert.equal(poller.retries, 1);
-  // });
-
-  // it("prevents manual polling if automatic", async function () {
-  //   const poller = await client.startLRO();
-  //   let error: any;
-  //   try {
-  //     await poller.retry();
-  //   } catch(e) {
-  //     error = e;
-  //   }
-  //   assert.equal(e.message, "Manual retries are disabled on an automatic poller");
-  // });
+  it("prevents manual polling if automatic", async function () {
+    const client = new FakeClient(new SimpleTokenCredential("my-fake-token"));
+    client.setResponses([{
+      ...basicResponseStructure,
+      status: 204,
+    }]);
+    const poller = await client.startLRO();
+    let error: any;
+    try {
+      await poller.retry();
+    } catch(e) {
+      error = e;
+    }
+    assert.equal(error.message, "Manual retries are disabled on an automatic poller");
+  });
 
   // it("documents progress", async function () {
   //   const poller = await client.startLRO();
@@ -116,6 +112,32 @@ describe("Long Running Operations - custom client", function () {
   //   assert.equal(poller.progress, 0.5); // 0.5 of 1. 50%
   // });
 
-  // it("can reuse one poller state to instantiate another poller", async function () {
-  // });
+  it("can reuse one poller state to instantiate another poller", async function () {
+    // Let's start with the forgetful test
+    const client = new FakeClient(new SimpleTokenCredential("my-fake-token"));
+    const responses = Array(20).fill({
+      ...basicResponseStructure,
+      status: 202,
+    });
+    client.setResponses(responses);
+    const poller = await client.startLRO();
+    assert.equal(poller.retries, 0);
+    await delay(100);
+    assert.equal(poller.retries, 9);
+    poller.forget();
+    await delay(100);
+    assert.equal(poller.retries, 10);
+    // Let's try to resume this with a new poller.
+    const serialized = poller.serialize();
+    const client2 = new FakeClient(new SimpleTokenCredential("my-fake-token"));
+    client2.setResponses(responses);
+    const poller2 = await client2.startLRO({
+      ...JSON.parse(serialized),
+      automatic: true,
+    });
+    assert.equal(poller2.retries, 10);
+    await delay(100);
+    assert.equal(poller2.retries, 19);
+    poller2.forget();
+  });
 });
