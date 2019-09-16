98e1368dd65291e154da72c254429122112a79cc
HarshaNalluru <harsha.nalluru1997@gmail.com>
2019-09-15 21:54:53 -0700

    add DevelopmentConnectionString to constants.ts

--- sdk/storage/storage-queue/src/utils/constants.ts
+++ sdk/storage/storage-queue/src/utils/constants.ts
@@ -43,3 +43,7 @@ export const HeaderConstants = {
 
 export const ETagNone = "";
 export const ETagAny = "*";
+
+export const DevelopmentConnectionString = `DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;
+  AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;
+  QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;`;
