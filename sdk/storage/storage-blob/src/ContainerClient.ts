81fdfae0b1b2937714fa10156e2177c6b6dba4bc
Jeremy Meng <yumeng@microsoft.com>
2019-09-20 15:22:45 -0700

    [Storage] Only iterate with an undefined marker or a non-empty… (#5168)
    When using the paged iterator next() function and the continuation token, we get
    an next marker of empty string. The service happily takes the empty string and
    iterates from the beginning.
    
    This change fixes it by only iterating when the marker is non-empty (more items
    available) or undefined (starting from the beginning).
    
    Samples are updated to check for `done`.

--- sdk/storage/storage-blob/src/ContainerClient.ts
+++ sdk/storage/storage-blob/src/ContainerClient.ts
@@ -989,11 +989,13 @@ export class ContainerClient extends StorageClient {
     options: ContainerListBlobsSegmentOptions = {}
   ): AsyncIterableIterator<Models.ContainerListBlobFlatSegmentResponse> {
     let listBlobsFlatSegmentResponse;
-    do {
-      listBlobsFlatSegmentResponse = await this.listBlobFlatSegment(marker, options);
-      marker = listBlobsFlatSegmentResponse.nextMarker;
-      yield await listBlobsFlatSegmentResponse;
-    } while (marker);
+    if (!!marker || marker === undefined) {
+      do {
+        listBlobsFlatSegmentResponse = await this.listBlobFlatSegment(marker, options);
+        marker = listBlobsFlatSegmentResponse.nextMarker;
+        yield await listBlobsFlatSegmentResponse;
+      } while (marker);
+    }
   }
 
   /**
@@ -1126,15 +1128,17 @@ export class ContainerClient extends StorageClient {
     options: ContainerListBlobsSegmentOptions = {}
   ): AsyncIterableIterator<Models.ContainerListBlobHierarchySegmentResponse> {
     let listBlobsHierarchySegmentResponse;
-    do {
-      listBlobsHierarchySegmentResponse = await this.listBlobHierarchySegment(
-        delimiter,
-        marker,
-        options
-      );
-      marker = listBlobsHierarchySegmentResponse.nextMarker;
-      yield await listBlobsHierarchySegmentResponse;
-    } while (marker);
+    if (!!marker || marker === undefined) {
+      do {
+        listBlobsHierarchySegmentResponse = await this.listBlobHierarchySegment(
+          delimiter,
+          marker,
+          options
+        );
+        marker = listBlobsHierarchySegmentResponse.nextMarker;
+        yield await listBlobsHierarchySegmentResponse;
+      } while (marker);
+    }
   }
 
   /**
