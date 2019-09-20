29cacff8331a765be4eb5a3481354f79a79851ad
Jeff Fisher <jeffish@microsoft.com>
2019-09-20 12:30:49 -0700

    Simplify TracerProxy

--- sdk/core/core-tracing/lib/tracerProxy.ts
+++ sdk/core/core-tracing/lib/tracerProxy.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { SupportedPlugins } from "./utils/supportedPlugins";
-import { OpenCensusTracePlugin } from "./plugins/opencensus/openCensusTracePlugin";
+
 import { NoOpTracePlugin } from "./plugins/noop/noOpTracePlugin";
 import { Plugin } from "./interfaces/plugin";
 
@@ -9,16 +8,12 @@ import { Plugin } from "./interfaces/plugin";
 let _tracerPlugin: Plugin;
 
 export interface ITracerProxy {
-  setTracer(tracer: any, tracerPluginType: SupportedPlugins): void;
+  setTracer(tracer: Plugin): void;
   getTracer(): Plugin;
 }
 
-export function setTracer(tracer: any, tracerPluginType: SupportedPlugins) {
-  if (tracerPluginType === SupportedPlugins.OPENCENSUS) {
-    _tracerPlugin = new OpenCensusTracePlugin(tracer);
-  } else {
-    _tracerPlugin = new NoOpTracePlugin();
-  }
+export function setTracer(tracer: Plugin) {
+  _tracerPlugin = tracer;
 }
 
 export function getTracer() {
--- sdk/core/core-tracing/review/core-tracing.api.md
+++ sdk/core/core-tracing/review/core-tracing.api.md
@@ -57,7 +57,7 @@ export interface ITracerProxy {
     // (undocumented)
     getTracer(): Plugin;
     // (undocumented)
-    setTracer(tracer: any, tracerPluginType: SupportedPlugins): void;
+    setTracer(tracer: Plugin): void;
 }
 
 // @public
