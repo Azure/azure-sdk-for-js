0ce9d3363f280f83a9c82b97afeab29b126cb9b2
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-09-25 14:28:07 -0700

    add external in rollup - cosmos

--- sdk/cosmosdb/cosmos/rollup.config.js
+++ sdk/cosmosdb/cosmos/rollup.config.js
@@ -2,6 +2,21 @@ import resolve from "rollup-plugin-local-resolve";
 export default [
   {
     input: "dist-esm/index.js",
+    external: [
+      "tslib",
+      "@azure/cosmos-sign",
+      "universal-user-agent",
+      "uuid/v4",
+      "debug",
+      "node-abort-controller",
+      "node-fetch",
+      "atob",
+      "binary-search-bounds",
+      "priorityqueuejs",
+      "semaphore",
+      "crypto-hash",
+      "fast-json-stable-stringify"
+    ],
     output: {
       file: "dist/index.js",
       format: "umd",
