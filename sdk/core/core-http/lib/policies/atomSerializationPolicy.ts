01f3f9b90b130e18b183156c84f164dc362d5805
Ramya Raja <ramya.raja@microsoft.com>
2019-09-30 16:43:32 -0700

    Update error handling

--- sdk/core/core-http/lib/policies/atomSerializationPolicy.ts
+++ sdk/core/core-http/lib/policies/atomSerializationPolicy.ts
@@ -60,7 +60,10 @@ export class AtomSerializationPolicy extends BaseRequestPolicy {
       }
     } catch (e) {
       const error = new RestError("ResponseNotInAtomXMLFormat", RestError.PARSE_ERROR);
-      error.statusCode = 400;
+      error.statusCode =
+        response.status && (response.status < 200 || response.status >= 300)
+          ? response.status
+          : 400;
       error.request = utils.stripRequest(response.request);
       error.response = utils.stripResponse(response);
       throw error;
