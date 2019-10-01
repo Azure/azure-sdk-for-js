c5ca49b1c8f825acf1d41c515f4e9663d054d802
chradek <chradek@microsoft.com>
2019-10-01 15:09:43 -0700

    [logger] checks if process is defined before accessing it

--- sdk/core/logger/src/logger.ts
+++ sdk/core/logger/src/logger.ts
@@ -1,86 +1,91 @@
-import debug from 'debug';
+import debug from "debug";
 
 const registeredLoggers = new Set<AzureDebugger>();
-let azureLogLevel: AzureLogLevel | undefined = (process.env.AZURE_LOG_LEVEL as AzureLogLevel) || null;
-export const AzureLogger = debug('azure') as AzureDebugger;
-AzureLogger.log = (...args) => { debug.log(...args) };
+let azureLogLevel: AzureLogLevel | undefined =
+  (typeof process !== "undefined" &&
+    process.env &&
+    (process.env.AZURE_LOG_LEVEL as AzureLogLevel)) ||
+  undefined;
+export const AzureLogger = debug("azure") as AzureDebugger;
+AzureLogger.log = (...args) => {
+  debug.log(...args);
+};
 
-export type AzureLogLevel = 'verbose' | 'info' | 'warning' | 'error';
+export type AzureLogLevel = "verbose" | "info" | "warning" | "error";
 
 type AzureDebugger = debug.Debugger & { level: number };
 
-
-if (process.env.AZURE_LOG_LEVEL) {
-    setLogLevel(process.env.AZURE_LOG_LEVEL as any);
+if (typeof process !== "undefined" && process.env && process.env.AZURE_LOG_LEVEL) {
+  setLogLevel(process.env.AZURE_LOG_LEVEL as any);
 }
 
 export function setLogLevel(level?: AzureLogLevel) {
-    azureLogLevel = level;
+  azureLogLevel = level;
 
-    const enabledNamespaces = [];
-    for (const logger of registeredLoggers) {
-        if (shouldEnable(logger)) {
-            enabledNamespaces.push(logger.namespace);
-        }
+  const enabledNamespaces = [];
+  for (const logger of registeredLoggers) {
+    if (shouldEnable(logger)) {
+      enabledNamespaces.push(logger.namespace);
     }
+  }
 
-    debug.enable(enabledNamespaces.join(','));
+  debug.enable(enabledNamespaces.join(","));
 }
 
 export function getLogLevel() {
-    return azureLogLevel;
+  return azureLogLevel;
 }
 
 const levelMap = {
-    'verbose': 400,
-    'info': 300,
-    'warning': 200,
-    'error': 100
-}
+  verbose: 400,
+  info: 300,
+  warning: 200,
+  error: 100
+};
 
 export interface AzureLogger {
-    error: debug.Debugger;
-    warning: debug.Debugger;
-    info: debug.Debugger;
-    verbose: debug.Debugger;
+  error: debug.Debugger;
+  warning: debug.Debugger;
+  info: debug.Debugger;
+  verbose: debug.Debugger;
 }
 
 export function createClientLogger(namespace: string): AzureLogger {
-    const clientRootLogger = createLogger(AzureLogger, namespace);
-    return {
-        error: createLogger(clientRootLogger, 'error'),
-        warning: createLogger(clientRootLogger, 'warning'),
-        info: createLogger(clientRootLogger, 'info'),
-        verbose: createLogger(clientRootLogger, 'verbose')
-    }
+  const clientRootLogger = createLogger(AzureLogger, namespace);
+  return {
+    error: createLogger(clientRootLogger, "error"),
+    warning: createLogger(clientRootLogger, "warning"),
+    info: createLogger(clientRootLogger, "info"),
+    verbose: createLogger(clientRootLogger, "verbose")
+  };
 }
 
 function createLogger(parent: AzureDebugger, levelOrNamespace: string) {
-    const logger: AzureDebugger = parent.extend(levelOrNamespace) as any;
-    if (levelOrNamespace !== undefined) {
-        logger.level = levelMap[levelOrNamespace as AzureLogLevel];
-    }
+  const logger: AzureDebugger = parent.extend(levelOrNamespace) as any;
+  if (levelOrNamespace !== undefined) {
+    logger.level = levelMap[levelOrNamespace as AzureLogLevel];
+  }
 
-    if (parent) {
-        logger.log = (...args) => {
-            parent.log(...args);
-        }
-    }
+  if (parent) {
+    logger.log = (...args) => {
+      parent.log(...args);
+    };
+  }
 
-    if(shouldEnable(logger)) {
-        const enabled = debug.disable();
-        debug.enable(enabled + ',' + logger.namespace);
-    }
+  if (shouldEnable(logger)) {
+    const enabled = debug.disable();
+    debug.enable(enabled + "," + logger.namespace);
+  }
 
-    registeredLoggers.add(logger);
+  registeredLoggers.add(logger);
 
-    return logger;
+  return logger;
 }
 
 function shouldEnable(logger: AzureDebugger) {
-    if (azureLogLevel && logger.level <= levelMap[azureLogLevel]) {
-        return true;
-    } else {
-        return false;
-    }
+  if (azureLogLevel && logger.level <= levelMap[azureLogLevel]) {
+    return true;
+  } else {
+    return false;
+  }
 }
