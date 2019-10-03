c3676eb691f5944833ac453bfb80a2cbd68e8c33
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-10-03 16:44:16 -0700

    checkpoint - 1

--- sdk/storage/storage-blob/execute-samples.js
+++ sdk/storage/storage-blob/execute-samples.js
@@ -2,13 +2,32 @@ const execa = require("execa");
 const fs = require("fs");
 require("dotenv").config({ path: "../.env" });
 
-async function exec(cwd, cmd) {
-  fs.readdir(cwd, function(err, items) {
+async function getCommand(cwd, fn) {
+  let cmd = "";
+
+  fs.readdir(cwd, async function(err, items) {
     for (var i = 0; i < items.length; i++) {
       console.log(items[i]);
+      const thisSample = "ts-node " + items[i] + " && ";
+      cmd = cmd + thisSample;
     }
+    cmd = cmd + "echo done";
+    console.log(".0 - ", cmd);
+    fn(cmd);
   });
-  const command = execa(cmd, { cwd, shell: true });
+}
+
+async function exec(cwd) {
+  let cmd = "";
+  await getCommand(cwd, function(location) {
+    console.log(".3 - " + location); // this is where you get the return value
+    cmd = location;
+  });
+  console.log(".2 - ", cmd);
+  let command = execa("ts-node basic.ts", {
+    cwd,
+    shell: true
+  });
   command.stderr.pipe(process.stderr);
   command.stdout.pipe(process.stdout);
   return command;
@@ -17,10 +36,10 @@ async function exec(cwd, cmd) {
 (async () => {
   try {
     console.log("Running typescript samples...");
-    await exec("./samples/typescript", `ts-node basic.ts`);
+    await exec("./samples/typescript");
     process.exit(0);
   } catch (error) {
-    console.log("Typescript test failed!");
+    console.log("Typescript samples failed!");
     console.log(error);
     process.exit(1);
   }
