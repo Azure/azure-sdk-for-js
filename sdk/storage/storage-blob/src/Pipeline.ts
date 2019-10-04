39656c6f1b743eb0ba4b7546346cd8491dc37e4b
Jeff Fisher <jeffish@microsoft.com>
2019-10-03 17:04:01 -0700

    Revert "Add tracing policy from core-http"
    This reverts commit db3031303e8341feed44bcea0ac5ec7e9f06e947.
    
    Breaks browser tests

--- sdk/storage/storage-blob/src/Pipeline.ts
+++ sdk/storage/storage-blob/src/Pipeline.ts
@@ -21,8 +21,7 @@ import {
   TokenCredential,
   isTokenCredential,
   bearerTokenAuthenticationPolicy,
-  ProxySettings,
-  tracingPolicy
+  ProxySettings
 } from "@azure/core-http";
 
 import { KeepAliveOptions, KeepAlivePolicyFactory } from "./KeepAlivePolicyFactory";
@@ -192,7 +191,6 @@ export function newPipeline(
   // The credential's policy factory must appear close to the wire so it can sign any
   // changes made by other factories (like UniqueRequestIDPolicyFactory)
   const factories: RequestPolicyFactory[] = [
-    tracingPolicy(),
     new KeepAlivePolicyFactory(pipelineOptions.keepAliveOptions),
     new TelemetryPolicyFactory(pipelineOptions.telemetry),
     new UniqueRequestIDPolicyFactory(),
