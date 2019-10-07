c3355c84402cefa918420fe690879fabe525c7ee
Richard Park <richard.park@microsoft.com>
2019-10-07 10:10:30 -0700

    Update to use a less-rollup-issue-causing version.

--- sdk/appconfiguration/app-configuration/test/package.spec.ts
+++ sdk/appconfiguration/app-configuration/test/package.spec.ts
@@ -1,9 +1,14 @@
-const packageJson = require("../package.json");
 import { packageVersion } from "../src/generated/src/appConfigurationContext";
+import { join } from "path";
+import { readFileSync } from "fs";
 import * as assert from "assert";
 
 describe("packagejson related tests", () => {
   it("user agent string", () => {
-    assert.equal(packageJson.version, packageVersion);
+    const packageJsonFilePath = join(__dirname, "../package.json");
+    const rawFileContents = readFileSync(packageJsonFilePath, { encoding: "utf-8" });
+    const packageJsonContents = JSON.parse(rawFileContents);
+
+    assert.equal(packageJsonContents.version, packageVersion);
   });
 });
