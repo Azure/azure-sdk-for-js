560b14f20ef0c4583968be018dfb41feaf3e45b6
Steve Faulkner <southpolesteve@gmail.com>
2019-10-08 15:36:48 -0500

    [Cosmos] Remove binary-search-bounds (#5417)

--- sdk/cosmosdb/cosmos/rollup.config.js
+++ sdk/cosmosdb/cosmos/rollup.config.js
@@ -10,8 +10,6 @@ export default [
       "debug",
       "@azure/abort-controller",
       "node-fetch",
-      "atob",
-      "binary-search-bounds",
       "priorityqueuejs",
       "semaphore",
       "crypto-hash",
@@ -25,7 +23,6 @@ export default [
       globals: {
         "universal-user-agent": "universalUserAgent",
         "@azure/cosmos-sign": "cosmosSign",
-        "binary-search-bounds": "bs",
         "crypto-hash": "cryptoHash",
         "fast-json-stable-stringify": "stableStringify",
         "uuid/v4": "uuid",
@@ -34,8 +31,7 @@ export default [
         tslib: "tslib_1",
         debug: "debugLib",
         priorityqueuejs: "PriorityQueue",
-        semaphore: "semaphore",
-        atob: "atob"
+        semaphore: "semaphore"
       }
     },
     plugins: [resolve()]
