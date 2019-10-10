a4ca8b989aba8bdf163ef995089823c7d401d4d4
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2019-10-10 12:14:13 -0700

    Drop dependency on @azure/cosmos-sign in favor of local header signature

--- sdk/cosmosdb/cosmos/src/utils/encode.ts
+++ sdk/cosmosdb/cosmos/src/utils/encode.ts
@@ -5,3 +5,17 @@ export function encodeUTF8(str: string): Uint8Array {
   }
   return bytes;
 }
+
+export function encodeBase64(value: ArrayBuffer): string {
+  if ("function" !== typeof btoa) {
+    throw new Error("Your browser environment is missing the global `btoa` function");
+  }
+
+  let binary = "";
+  const bytes = new Uint8Array(value);
+  const len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
