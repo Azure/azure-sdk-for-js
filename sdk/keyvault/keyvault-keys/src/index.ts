708c1c66c3f59958568918649aa4563d468fed66
David Wilson <daviwil@microsoft.com>
2019-10-16 15:13:55 -0700

    Add logPolicy configuration to @azure/keyvault-keys

--- sdk/keyvault/keyvault-keys/src/index.ts
+++ sdk/keyvault/keyvault-keys/src/index.ts
@@ -19,10 +19,12 @@ import {
   isNode,
   userAgentPolicy,
   RequestOptionsBase,
-  tracingPolicy
+  tracingPolicy,
+  logPolicy
 } from "@azure/core-http";
 
 import { getTracer, Span } from "@azure/core-tracing";
+import { logger } from "./log";
 
 import "@azure/core-paging";
 import { PageSettings, PagedAsyncIterableIterator } from "@azure/core-paging";
@@ -117,7 +119,8 @@ export {
   UnwrapResult,
   UpdateKeyOptions,
   VerifyResult,
-  WrapResult
+  WrapResult,
+  logger
 };
 
 export { ProxyOptions, TelemetryOptions, RetryOptions };
@@ -171,7 +174,16 @@ export class KeyClient {
       redirectPolicy(),
       isTokenCredential(credential)
         ? challengeBasedAuthenticationPolicy(credential)
-        : signingPolicy(credential)
+        : signingPolicy(credential),
+      logPolicy(
+        logger.info, {
+          allowedHeaderNames: [
+            "x-ms-keyvault-region",
+            "x-ms-keyvault-network-info",
+            "x-ms-keyvault-service-version"
+          ],
+          allowedQueryParameters: ["api-version"]
+      })
     ]);
 
     return {
