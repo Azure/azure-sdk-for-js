dbd61a0591ba109d6d856369be682dc330f9f1ad
Daniel Jurek <djurek@microsoft.com>
2019-10-16 10:14:51 -0700

    add versioning utilities

---
+++ eng/tools/versioning/set-dev.js
@@ -0,0 +1,44 @@
+let argv = require("yargs")
+  .options({
+    "build-id": {
+      type: "string",
+      describe: "build ID suffix to give the package (e.g. usually YYYYMMDD.r)",
+      demandOption: true
+    },
+    "repo-root": {
+      type: "string",
+      describe: "root of the repository (e.g. ../../../)",
+      demandOption: true
+    }
+  })
+  .help().argv;
+
+const path = require("path");
+const versionUtils = require("./VersionUtils");
+
+async function main(argv) {
+  const buildId = argv["build-id"];
+  const repoRoot = argv["repo-root"];
+
+  const rushPath = path.resolve(`${repoRoot}/rush.json`);
+  const rushSpec = await versionUtils.readFileJson(rushPath);
+
+  console.log(`Updating packages with Build ID ${buildId}`);
+
+  const targetPackageFiles = rushSpec.projects.map(project =>
+    path.resolve(`${repoRoot}/${project.projectFolder}/package.json`)
+  );
+
+  for (const targetFile of targetPackageFiles) {
+    const targetFileContents = await versionUtils.readFileJson(targetFile);
+    const newVersion = `${targetFileContents.version}-dev-${buildId}`;
+    console.log(`File ${targetFile} version updated to ${newVersion}`);
+    const updatedPackageSpec = {
+      ...targetFileContents,
+      version: newVersion
+    };
+    await versionUtils.writePackageJson(targetFile, updatedPackageSpec);
+  }
+}
+
+main(argv);
