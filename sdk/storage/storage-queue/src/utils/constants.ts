a0a6f240f0f9225ffba401f7628122170105f334
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-10-17 16:22:25 -0700

    update versions to 12.0.0-preview.5

--- sdk/storage/storage-queue/src/utils/constants.ts
+++ sdk/storage/storage-queue/src/utils/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export const SDK_VERSION: string = "12.0.0-preview.4";
+export const SDK_VERSION: string = "12.0.0-preview.5";
 export const SERVICE_VERSION: string = "2019-02-02";
 
 export const StorageOAuthScopes: string | string[] = "https://storage.azure.com/.default";
