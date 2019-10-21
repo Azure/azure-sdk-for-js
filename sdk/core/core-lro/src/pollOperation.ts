4682b478b78322397b08085ae4425c67c35256ae
Will Temple <witemple@microsoft.com>
2019-10-21 09:04:08 -0700

    Corrected copyright headers in core-lro

--- sdk/core/core-lro/src/pollOperation.ts
+++ sdk/core/core-lro/src/pollOperation.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { AbortSignalLike } from "@azure/abort-controller";
 
 export interface PollOperationState<TResult> {
--- sdk/core/core-lro/src/poller.ts
+++ sdk/core/core-lro/src/poller.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { PollOperation, PollOperationState } from "./pollOperation";
 import { AbortSignalLike } from "@azure/abort-controller";
 
