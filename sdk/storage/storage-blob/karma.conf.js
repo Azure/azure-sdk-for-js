cdab97d44072554198b708450e79675cf8bd81db
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-10-22 14:45:33 -0700

    IE -> ChromeHeadless

--- sdk/storage/storage-blob/karma.conf.js
+++ sdk/storage/storage-blob/karma.conf.js
@@ -129,7 +129,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // 'ChromeHeadless', 'Chrome', 'Firefox', 'Edge', 'IE'
-    browsers: ["IE"],
+    browsers: ["ChromeHeadless"],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
