d44cf7c1338bc032d87330bc0ba4ec11aa420d9d
Jonathan Turner <joturner@microsoft.com>
2019-10-24 10:00:11 +1300

    Check for the env first

--- sdk/core/core-tracing/lib/tracerProxy.ts
+++ sdk/core/core-tracing/lib/tracerProxy.ts
@@ -23,7 +23,7 @@ export function getTracer() {
   if (!cache.tracer) {
     cache.tracer = new NoOpTracer();
   }
-  if (process.env.AZURE_TRACING_DISABLED) {
+  if (process && process.env && process.env.AZURE_TRACING_DISABLED) {
     cache.tracer = new NoOpTracer();
   }
   return cache.tracer;
