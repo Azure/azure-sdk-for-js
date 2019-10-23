13c028179b770e205606a13a4f4540e305c7b58c
Ramya Achutha Rao <ramyar@microsoft.com>
2019-10-22 19:58:06 -0700

    Format files using prettier

--- sdk/appconfiguration/app-configuration/test/helpers.spec.ts
+++ sdk/appconfiguration/app-configuration/test/helpers.spec.ts
@@ -1,113 +1,130 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { checkAndFormatIfAndIfNoneMatch, formatWildcards, extractAfterTokenFromNextLink, quoteETag, makeConfigurationSettingEmpty, transformKeyValue, transformKeyValueResponseWithStatusCode, transformKeyValueResponse } from "../src/internal/helpers"
+import {
+  checkAndFormatIfAndIfNoneMatch,
+  formatWildcards,
+  extractAfterTokenFromNextLink,
+  quoteETag,
+  makeConfigurationSettingEmpty,
+  transformKeyValue,
+  transformKeyValueResponseWithStatusCode,
+  transformKeyValueResponse
+} from "../src/internal/helpers";
 import * as assert from "assert";
-import { ConfigurationSetting, HttpResponseField, HttpResponseFields } from '../src';
-import { HttpHeaders } from '@azure/core-http';
+import { ConfigurationSetting, HttpResponseField, HttpResponseFields } from "../src";
+import { HttpHeaders } from "@azure/core-http";
 
 describe("helper methods", () => {
-  it("checkAndFormatIfAndIfNoneMatch", () => {    
+  it("checkAndFormatIfAndIfNoneMatch", () => {
     const key = "ignored";
 
-    assert.deepEqual({
-      ifMatch: undefined,
-      ifNoneMatch: undefined
-    }, checkAndFormatIfAndIfNoneMatch({ key }, {}));
+    assert.deepEqual(
+      {
+        ifMatch: undefined,
+        ifNoneMatch: undefined
+      },
+      checkAndFormatIfAndIfNoneMatch({ key }, {})
+    );
 
-    assert.deepEqual({
-      ifMatch: "\"hello\"",
-      ifNoneMatch: undefined
-    }, checkAndFormatIfAndIfNoneMatch({ key, etag: "hello" }, {
-      onlyIfUnchanged: true
-    }));
+    assert.deepEqual(
+      {
+        ifMatch: '"hello"',
+        ifNoneMatch: undefined
+      },
+      checkAndFormatIfAndIfNoneMatch(
+        { key, etag: "hello" },
+        {
+          onlyIfUnchanged: true
+        }
+      )
+    );
 
-    assert.deepEqual({
-      ifNoneMatch: "\"hello\"",
-      ifMatch: undefined
-    }, checkAndFormatIfAndIfNoneMatch({ key, etag: "hello" }, {
-      onlyIfChanged: true
-    }));
+    assert.deepEqual(
+      {
+        ifNoneMatch: '"hello"',
+        ifMatch: undefined
+      },
+      checkAndFormatIfAndIfNoneMatch(
+        { key, etag: "hello" },
+        {
+          onlyIfChanged: true
+        }
+      )
+    );
   });
 
   it("checkAndFormatIfAndIfNoneMatch - mutually exclusive", () => {
     const key = "ignored";
 
-    assert.throws(() => checkAndFormatIfAndIfNoneMatch({ key, etag: "won't get used"}, {
-      onlyIfChanged: true,
-      onlyIfUnchanged: true
-    }), /onlyIfChanged and onlyIfUnchanged are mutually-exclusive/);    
+    assert.throws(
+      () =>
+        checkAndFormatIfAndIfNoneMatch(
+          { key, etag: "won't get used" },
+          {
+            onlyIfChanged: true,
+            onlyIfUnchanged: true
+          }
+        ),
+      /onlyIfChanged and onlyIfUnchanged are mutually-exclusive/
+    );
   });
 
   describe("quoteETag", () => {
     it("undefined", () => {
-      assert.equal(
-        undefined,
-        quoteETag(undefined)
-      );
-  
-      assert.equal(
-        '"etagishere"',
-        quoteETag("etagishere")
-      );
-  
-      assert.equal(
-        "'etagishere'",
-        quoteETag("'etagishere'")
-      );
-  
-      assert.equal(
-        "*",
-        quoteETag("*")
-      );
+      assert.equal(undefined, quoteETag(undefined));
+
+      assert.equal('"etagishere"', quoteETag("etagishere"));
+
+      assert.equal("'etagishere'", quoteETag("'etagishere'"));
+
+      assert.equal("*", quoteETag("*"));
     });
   });
-  
+
   describe("formatWildcards", () => {
     it("undefined", () => {
       const result = formatWildcards({
         keys: undefined,
         labels: undefined
       });
-  
+
       assert.ok(!result.key);
       assert.ok(!result.label);
     });
-  
+
     it("single values only", () => {
       const result = formatWildcards({
         keys: ["key1"],
         labels: ["label1"]
       });
-  
+
       assert.equal("key1", result.key);
       assert.equal("label1", result.label);
     });
-  
+
     it("multiple values", () => {
       const result = formatWildcards({
         keys: ["key1", "key2"],
         labels: ["label1", "label2"]
       });
-  
+
       assert.equal("key1,key2", result.key);
       assert.equal("label1,label2", result.label);
     });
 
     it("fields map properly", () => {
       const result = formatWildcards({
-        fields: [ "readOnly", "value" ]
+        fields: ["readOnly", "value"]
       });
-      
-      assert.deepEqual([ "locked", "value" ], result.select);
+
+      assert.deepEqual(["locked", "value"], result.select);
     });
   });
-  
+
   describe("extractAfterTokenFromNextLink", () => {
     it("token is extracted and properly unescaped", () => {
-      let token = extractAfterTokenFromNextLink(
-        "/kv?key=someKey&api-version=1.0&after=bGlah%3D"
-      );
+      let token = extractAfterTokenFromNextLink("/kv?key=someKey&api-version=1.0&after=bGlah%3D");
       assert.equal("bGlah=", token);
     });
   });
@@ -118,7 +135,7 @@ describe("helper methods", () => {
       statusCode: 204,
       readOnly: false,
       ...fakeHttp204Response
-    };;
+    };
 
     makeConfigurationSettingEmpty(response);
 
@@ -130,7 +147,7 @@ describe("helper methods", () => {
     }
 
     // These point is these properties are untouched and won't throw
-    // since they're the only properties the user is allowed to touch on these 
+    // since they're the only properties the user is allowed to touch on these
     // "body empty" objects.
     assert.equal(204, response._response.status);
     assert.equal(204, response.statusCode);
@@ -142,12 +159,15 @@ describe("helper methods", () => {
       locked: true
     });
 
-    assert.deepEqual({
-      // the 'locked' property should not be present in the object since
-      // it should be 'renamed' to readOnly
-      readOnly: true,
-      key: "hello"
-    }, configurationSetting);
+    assert.deepEqual(
+      {
+        // the 'locked' property should not be present in the object since
+        // it should be 'renamed' to readOnly
+        readOnly: true,
+        key: "hello"
+      },
+      configurationSetting
+    );
   });
 
   it("transformKeyValueResponseWithStatusCode", () => {
@@ -160,19 +180,22 @@ describe("helper methods", () => {
     const actualKeys = Object.keys(configurationSetting).sort();
 
     // _response is explictly set to not enumerate, even in our copied object.
-    assert.deepEqual(['key', 'readOnly', 'statusCode'], actualKeys);
+    assert.deepEqual(["key", "readOnly", "statusCode"], actualKeys);
 
     // now make it enumerable so we can do our comparison
-    Object.defineProperty(configurationSetting, '_response', {
+    Object.defineProperty(configurationSetting, "_response", {
       enumerable: true
     });
 
-    assert.deepEqual({
-      readOnly: true,
-      key: "hello",
-      statusCode: 204,
-      _response: fakeHttp204Response._response
-    }, configurationSetting);
+    assert.deepEqual(
+      {
+        readOnly: true,
+        key: "hello",
+        statusCode: 204,
+        _response: fakeHttp204Response._response
+      },
+      configurationSetting
+    );
   });
 
   it("transformKeyValueResponse", () => {
@@ -185,21 +208,24 @@ describe("helper methods", () => {
     const actualKeys = Object.keys(configurationSetting).sort();
 
     // _response is explictly set to not enumerate, even in our copied object.
-    assert.deepEqual(['key', 'readOnly'], actualKeys);
+    assert.deepEqual(["key", "readOnly"], actualKeys);
 
     // now make it enumerable so we can do our comparison
-    Object.defineProperty(configurationSetting, '_response', {
+    Object.defineProperty(configurationSetting, "_response", {
       enumerable: true
     });
 
-    assert.deepEqual({
-      readOnly: true,
-      key: "hello",
-      _response: fakeHttp204Response._response
-    }, configurationSetting);
+    assert.deepEqual(
+      {
+        readOnly: true,
+        key: "hello",
+        _response: fakeHttp204Response._response
+      },
+      configurationSetting
+    );
   });
 
-  function getAllConfigurationSettingFields() : (Exclude<keyof ConfigurationSetting, 'key'>)[] {
+  function getAllConfigurationSettingFields(): (Exclude<keyof ConfigurationSetting, "key">)[] {
     const configObjectWithAllFieldsRequired: Required<ConfigurationSetting> = {
       contentType: "",
       etag: "",
@@ -211,8 +237,8 @@ describe("helper methods", () => {
       value: ""
     };
 
-    const keys = Object.keys(configObjectWithAllFieldsRequired).filter(key => key !== "key");
-    return keys as (Exclude<keyof ConfigurationSetting, 'key'>[]);
+    const keys = Object.keys(configObjectWithAllFieldsRequired).filter((key) => key !== "key");
+    return keys as (Exclude<keyof ConfigurationSetting, "key">[]);
   }
 
   const fakeHttp204Response: HttpResponseField<any> = {
@@ -221,16 +247,20 @@ describe("helper methods", () => {
         url: "unused",
         abortSignal: {
           aborted: true,
-          addEventListener: () => { },
-          removeEventListener: () => { }
+          addEventListener: () => {},
+          removeEventListener: () => {}
         },
         method: "GET",
         withCredentials: false,
         headers: new HttpHeaders(),
         timeout: 0,
-        clone: function () { return this; },
-        validateRequestProperties: () => { },
-        prepare: function (options) { return this; },
+        clone: function() {
+          return this;
+        },
+        validateRequestProperties: () => {},
+        prepare: function(options) {
+          return this;
+        }
       },
       status: 204,
       headers: new HttpHeaders(),
@@ -238,5 +268,4 @@ describe("helper methods", () => {
       parsedHeaders: {}
     }
   };
-})
-
+});
