f0707cb64d6768ae4c6cf9f98ecfafde137ce47d
ramya0820 <45977823+ramya0820@users.noreply.github.com>
2019-10-24 11:54:10 -0700

    Update sdk/servicebus/service-bus/src/serializers/subscriptionResourceSerializer.ts
    Co-Authored-By: Ramya Rao <ramya.rao.a@outlook.com>

--- sdk/servicebus/service-bus/src/serializers/subscriptionResourceSerializer.ts
+++ sdk/servicebus/service-bus/src/serializers/subscriptionResourceSerializer.ts
@@ -18,7 +18,9 @@ import {
 
 /**
  * @ignore
- * Builds the subscription options object
+ * Builds the subscription options object from the user provided options. 
+ * Handles the differences in casing for the property names, converts values to string and ensures the 
+ * right order as expected by the service
  * @param subscriptionOptions
  */
 export function buildSubscriptionOptions(
