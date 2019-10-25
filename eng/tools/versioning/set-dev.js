2f00d9fa4f77f0bc6e23295231e7e21decd9df24
Daniel Jurek <djurek@microsoft.com>
2019-10-25 13:35:05 -0700

    update dev package procedure and add ability to scope dev version setting

--- eng/tools/versioning/set-dev.js
+++ eng/tools/versioning/set-dev.js
@@ -9,22 +9,42 @@ let argv = require("yargs")
       type: "string",
       describe: "root of the repository (e.g. ../../../)",
       demandOption: true
+    },
+    service: {
+      type: "string",
+      describe:
+        "service directory whose packages should be updated (if not set updates all directories)"
     }
   })
   .help().argv;
 
 const path = require("path");
+const semver = require("semver");
 const versionUtils = require("./VersionUtils");
+
+function getDevVersion(currentVersion, buildId) {
+  const prerelease = semver.prerelease(currentVersion);
+  if (prerelease) {
+    return `${currentVersion}.${buildId}`;
+  }
+
+  return `${currentVersion}-dev.${buildId}`;
+}
 
 async function main(argv) {
   const buildId = argv["build-id"];
   const repoRoot = argv["repo-root"];
+  const service = argv["service"];
 
   const rushSpec = await versionUtils.getRushSpec(repoRoot);
 
   console.log(`Updating packages with Build ID ${buildId}`);
 
-  const targetPackages = rushSpec.projects.map(project =>
+  const scopedPackages = rushSpec.projects.filter(package =>
+    service ? package.projectFolder.startsWith(`sdk/${service}`) : true
+  );
+
+  let targetPackages = scopedPackages.map(project =>
     path.resolve(path.join(repoRoot, project.projectFolder))
   );
 
@@ -33,7 +53,7 @@ async function main(argv) {
     const packageJsonContents = await versionUtils.readFileJson(
       targetPackageJson
     );
-    const newVersion = `${packageJsonContents.version}-dev-${buildId}`;
+    const newVersion = getDevVersion(packageJsonContents.version, buildId);
     console.log(`File ${targetPackageJson} version updated to ${newVersion}`);
     const updatedPackageSpec = {
       ...packageJsonContents,
