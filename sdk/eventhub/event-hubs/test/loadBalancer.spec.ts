2c81d1a88e9c1008da5c9427d5130bb109221929
Richard Park <richard.park@microsoft.com>
2019-10-29 14:34:17 -0700

    Add test for the new "load balancer"

---
+++ sdk/eventhub/event-hubs/test/loadBalancer.spec.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { GreedyPartitionLoadBalancer } from "../src/partitionLoadBalancer";
+import { PartitionOwnership } from '../src/eventProcessor';
+
+describe.only("PartitionLoadBalancer", () => {
+  describe("GreedyPartitionLoadBalancer", () => {
+    it("all", () => {
+      const m = new Map<string, PartitionOwnership>();
+      const lb = new GreedyPartitionLoadBalancer();
+
+      lb.loadBalance(m, ["1", "2", "3"]).should.deep.eq(["1", "2", "3"]);
+      m.should.be.empty;
+    });
+
+    it("filtered", () => {
+      const m = new Map<string, PartitionOwnership>();
+      const lb = new GreedyPartitionLoadBalancer(["2"]);
+
+      lb.loadBalance(m, ["1", "2", "3"]).should.deep.eq(["2"]);
+      m.should.be.empty;
+    });    
+  });
+});
