016c49541239d603e89ba8e0714155699614c8ee
Richard Park <richard.park@microsoft.com>
2019-10-30 08:26:06 -0700

    LIcense

--- sdk/eventhub/event-hubs/src/eventHubConsumerClient.ts
+++ sdk/eventhub/event-hubs/src/eventHubConsumerClient.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import { EventHubClientOptions, EventHubClient } from "./eventHubClient";
 import { PartitionProcessor } from "./partitionProcessor";
 import { ReceivedEventData } from "./eventData";
