1d5d3ab198a8b9110d2bb7ba1649b781136b2150
Jonathan Turner <joturner@microsoft.com>
2019-10-31 13:35:05 +1300

    Fix typo in KV JS sample

--- sdk/keyvault/keyvault-secrets/samples/javascript/backupAndRestore.js
+++ sdk/keyvault/keyvault-secrets/samples/javascript/backupAndRestore.js
@@ -33,7 +33,7 @@ async function main() {
 
   const vaultName = process.env["KEYVAULT_NAME"] || "<keyvault-name>";
   const url = `https://${vaultName}.vault.azure.net`;
-  const client = new SecretsClient(url, credential);
+  const client = new SecretClient(url, credential);
 
   const secretName = "StorageAccountPassword19312312";
 
