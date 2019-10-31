60e9317b3a22aff39cdb8d0e34166fe0f877e7a3
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2019-10-31 11:10:42 +0800

    eventgrid-package-2020-01 (#5942)

--- sdk/eventgrid/arm-eventgrid/src/eventGridManagementClientContext.ts
+++ sdk/eventgrid/arm-eventgrid/src/eventGridManagementClientContext.ts
@@ -45,7 +45,7 @@ export class EventGridManagementClientContext extends msRestAzure.AzureServiceCl
 
     super(credentials, options);
 
-    this.apiVersion = '2019-06-01';
+    this.apiVersion = '2020-01-01-preview';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
