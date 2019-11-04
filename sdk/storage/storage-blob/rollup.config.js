6bb86c2eb5cf548c81b8307e2336f25820953a56
Will Temple <witemple@microsoft.com>
2019-11-04 14:27:04 -0800

    [storage-blob] Removed residual configurations from browser build configs
    - Removed browser build from rollup configuration for non-test builds
    - Removed gulpfile and dependency on gulp, as well as gulp-based
    commands from package.json
    - Removed ThirdPartyNotices.txt

--- sdk/storage/storage-blob/rollup.config.js
+++ sdk/storage/storage-blob/rollup.config.js
@@ -9,10 +9,4 @@ if (!process.env.ONLY_BROWSER) {
   inputs.push(base.nodeConfig());
 }
 
-// Disable this until we are ready to run rollup for the browser.
-if (!process.env.ONLY_NODE) {
-  inputs.push(base.browserConfig());
-  inputs.push(base.browserConfig(false, true));
-}
-
 export default inputs;
