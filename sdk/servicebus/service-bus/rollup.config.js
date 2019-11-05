a3b05d4eabc89469c96e04f399195997bdbea977
Brian Terlson <brian.terlson@microsoft.com>
2019-11-05 14:25:07 -0800

    another attempt

--- sdk/servicebus/service-bus/rollup.config.js
+++ sdk/servicebus/service-bus/rollup.config.js
@@ -5,13 +5,6 @@ import * as base from "./rollup.base.config";
 
 const inputs = [];
 
-if (!process.env.ONLY_BROWSER) {
-  inputs.push(base.nodeConfig());
-}
-
-if (!process.env.ONLY_NODE) {
-  inputs.push(base.browserConfig());
-  inputs.push(base.browserConfig({ production: true }));
-}
+inputs.push(base.browserConfig());
 
 export default inputs;
