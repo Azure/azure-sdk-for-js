3fc9a280484798abbebadc6f24d061486e8bc9d1
Will Temple <witemple@microsoft.com>
2019-11-05 09:45:31 -0800

    Re-added rollup config from master to storage-blob, so that we can standardize all the rollup configs together.

--- sdk/storage/storage-blob/rollup.config.js
+++ sdk/storage/storage-blob/rollup.config.js
@@ -9,4 +9,10 @@ if (!process.env.ONLY_BROWSER) {
   inputs.push(base.nodeConfig());
 }
 
+// Disable this until we are ready to run rollup for the browser.
+if (!process.env.ONLY_NODE) {
+  inputs.push(base.browserConfig());
+  inputs.push(base.browserConfig(false, true));
+}
+
 export default inputs;
