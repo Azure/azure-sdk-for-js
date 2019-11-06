b9c1310078d4544a5fd69d035e868a825510bd5c
Qiaoqiao Zhang <qiaozha#microsoft.com>
2019-11-06 13:37:12 +0800

    small changes for package eventgrid 2020-01 preview

--- sdk/eventgrid/arm-eventgrid/src/eventGridManagementClientContext.ts
+++ sdk/eventgrid/arm-eventgrid/src/eventGridManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-eventgrid";
-const packageVersion = "4.0.0";
+const packageVersion = "4.1.0";
 
 export class EventGridManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
