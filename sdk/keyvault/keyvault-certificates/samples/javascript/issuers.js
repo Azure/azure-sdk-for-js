680e345ea49bf5429f02f12f27f626fdea4a1967
Jonathan Turner <joturner@microsoft.com>
2019-11-07 11:56:42 +1300

    Rename setIssuer to createIssuer

--- sdk/keyvault/keyvault-certificates/samples/javascript/issuers.js
+++ sdk/keyvault/keyvault-certificates/samples/javascript/issuers.js
@@ -19,7 +19,7 @@ async function main() {
   const issuerName = "issuerName";
 
   // Create
-  await client.setIssuer(issuerName, "Test", {
+  await client.createIssuer(issuerName, "Test", {
     credentials: {
       accountId: "keyvaultuser"
     },
