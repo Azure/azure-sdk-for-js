9bed82d0f99edb80b8aa6eb2f0a646dbd29134d9
Jonathan Turner <jonathandturner@users.noreply.github.com>
2019-11-11 08:50:19 +1300

    Update index.ts

--- sdk/keyvault/keyvault-certificates/src/index.ts
+++ sdk/keyvault/keyvault-certificates/src/index.ts
@@ -178,7 +178,7 @@ export {
   LifetimeAction,
   ListPropertiesOfCertificatesOptions,
   ListPropertiesOfCertificateVersionsOptions,
-  ListPropertiesOfIssuersOptions as ListIssuersOptions,
+  ListPropertiesOfIssuersOptions,
   ListDeletedCertificatesOptions,
   MergeCertificateOptions,
   OrganizationDetails,
