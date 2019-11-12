6f41945f11e8b8f9aac6a0f44a83e26c78fcf7dd
Jeff Fisher <jeffish@microsoft.com>
2019-11-11 16:50:57 -0800

    Alias old constant

--- sdk/cosmosdb/cosmos/src/common/constants.ts
+++ sdk/cosmosdb/cosmos/src/common/constants.ts
@@ -77,6 +77,8 @@ export const Constants = {
     XDate: "x-ms-date",
     CollectionPartitionInfo: "x-ms-collection-partition-info",
     CollectionServiceInfo: "x-ms-collection-service-info",
+    // Deprecated, use RetryAfterInMs instead.
+    RetryAfterInMilliseconds: "x-ms-retry-after-ms",
     RetryAfterInMs: "x-ms-retry-after-ms",
     IsFeedUnfiltered: "x-ms-is-feed-unfiltered",
     ResourceTokenExpiry: "x-ms-documentdb-expiry-seconds",
