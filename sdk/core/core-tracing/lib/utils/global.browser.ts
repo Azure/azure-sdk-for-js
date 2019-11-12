8fb1656a04d2debb9fdef3b14703af1d8a4bd8e4
Jeff Fisher <jeffish@microsoft.com>
2019-11-12 12:40:42 -0800

    Fix build break

--- sdk/core/core-tracing/lib/utils/global.browser.ts
+++ sdk/core/core-tracing/lib/utils/global.browser.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+/// <reference lib="dom"/>
+
 export function getGlobalObject(): any {
   return self;
 }
