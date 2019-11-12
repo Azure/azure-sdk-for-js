37435f8973dec78d4a02cc77e2ccc868e8e0141c
Karishma Ghiya <kaghiya@microsoft.com>
2019-11-12 12:04:39 -0800

    versioning updating dependencies

--- eng/tools/versioning/set-dev-dependencies.js
+++ eng/tools/versioning/set-dev-dependencies.js
@@ -23,27 +23,77 @@ const path = require("path");
 const semver = require("semver");
 const versionUtils = require("./VersionUtils");
 
-function getDevVersion(currentVersion, buildId) {
-  const parsedVersion = semver.parse(currentVersion);
-  return `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch}-dev.${buildId}`;
-
-
-const getRushPackages = async (rushPath) => {
-  const baseDir = path.dirname(rushPath);
-  const rushJson = parse(await readFile(rushPath, "utf8"));
-  const packageData = {};
-
-  for (const proj of rushJson.projects) {
-    const filePath = path.join(baseDir, proj.projectFolder, "package.json");
-    const packageJson = parse(await readFile(filePath, "utf8"));
-    packageData[packageJson.name] = {
-      src: filePath,
-      json: packageJson,
-      newVer: undefined
-    };
+const commitChanges = async (rushPackages, package) => {
+  // Commit the new version to the JSON document
+  if(rushPackages[package].newVer){
+    rushPackages[package].json.version = rushPackages[package].newVer;
   }
+  try {
+    // Write out the JSON document to disk
+    await versionUtils.writePackageJson(
+      rushPackages[package].src,
+      rushPackages[package].json
+    );
+    console.info(
+      "File " +
+        rushPackages[package].src +
+        " created successfully with Node.js v10 fs/promises!"
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
 
-  return packageData;
+const updatePackageVersion = (rushPackages, package, buildId) => {
+  const currentVersion = rushPackages[package].json.version;
+  const parsedVersion = semver.parse(currentVersion);
+  rushPackages[package].newVer = `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch}-dev.${buildId}`;
+  console.log(`version updated for ${package}`);
+  return rushPackages;
+};
+
+const updateDependencySection = (rushPackages, dependencySection) => {
+  console.log(dependencySection);
+  if(dependencySection){
+    for (const [depName, depVersionRange] of Object.entries(dependencySection)) {
+      console.log(`checking ${depName}:${depVersionRange}...`);
+
+      // If the dependency isn't part of the Rush workspace, skip it
+      if (!rushPackages[depName]) {
+        continue;
+      }
+
+      // Compare the dependency version range with the package's current version
+      const packageVersion = rushPackages[depName].json.version;
+
+      console.log(`version in package's dep = ${depVersionRange}`);
+      console.log(`dep's version = ${packageVersion}`);
+
+      // If the dependency range is satisfied by the package's current version,
+      // replace it with an exact match to the package's new version
+      if (semver.satisfies(packageVersion, depVersionRange)) {
+        // Commit the dependency version to the JSON document
+        if(rushPackages[depName].newVer !== undefined){
+          dependencySection[depName] = rushPackages[depName].newVer;
+        }
+      }
+    }
+  }
+  return rushPackages;
+};
+
+const updateInternalDependencyVersions = (rushPackages, package) => {
+  console.log(package);
+  console.log("checking dependencies ..");
+  rushPackages = updateDependencySection(rushPackages,rushPackages[package].json.dependencies);
+
+  console.log("checking devDependencies ..");
+  rushPackages = updateDependencySection(rushPackages,rushPackages[package].json.devDependencies);
+
+  console.log("checking peerDependencies ..");
+  rushPackages = updateDependencySection(rushPackages,rushPackages[package].json.peerDependencies);
+
+  return rushPackages;
 };
 
 
@@ -52,55 +102,31 @@ async function main(argv) {
   const repoRoot = argv["repo-root"];
   const service = argv["service"];
 
-  var rushSpec = await versionUtils.getRushSpec(repoRoot);
+  var rushPackages = await versionUtils.getRushPackageJsons(repoRoot);
 
-  console.log(`Updating packages with Build ID ${buildId}`);
+  // Set all the new versions
+  console.log(`Updating packages with build ID ${buildId}`);
+  for (const package of Object.keys(rushPackages)) {
+    if ((rushPackages[package].versionPolicy == "client"||rushPackages[package].versionPolicy == "core") && (rushPackages[package].projectFolder.startsWith(`sdk/${service}`))){
+      console.log("package updated = ");
+      console.log(package);
+      rushPackages = updatePackageVersion(rushPackages, package, buildId);
+      console.log(rushPackages[package].newVer);
+    }
+  };
 
-  const scopedPackages = rushSpec.projects.filter(package =>
-    service ? package.projectFolder.startsWith(`sdk/${service}`) : true
-  );
-
-  let appropriateVersionedPackages = scopedPackages.filter(
-    package =>
-      package.versionPolicyName == "core" ||
-      package.versionPolicyName == "client"
-  );
-
-  console.log(appropriateVersionedPackages);
-
-  let targetPackages = appropriateVersionedPackages.map(project =>
-    path.resolve(path.join(repoRoot, project.projectFolder))
-  );
-
-  const packageData = {};
-
-  for (const targetFolder of targetPackages) {
-    const targetPackageJson = path.join(targetFolder, "package.json");
-    const packageJsonContents = await versionUtils.readFileJson(
-      targetPackageJson
-    );
-
-    packageData[packageJson.name] = {
-      src: targetPackageJson,
-      json: packageJsonContents,
-      newVer: undefined
-    };
-
-    const newVersion = getDevVersion(packageJsonContents.version, buildId);
-    console.log(`File ${targetPackageJson} version updated to ${newVersion}`);
-    var updatedPackageSpec = {
-      ...packageJsonContents,
-      version: newVersion
-    };
-
-    await versionUtils.writePackageJson(targetPackageJson, updatedPackageSpec);
-
-    await versionUtils.updatePackageConstants(
-      targetFolder,
-      packageJsonContents,
-      newVersion
-    );
+  // Update any references to internal projects with the new versions
+  for (const package of Object.keys(rushPackages)) {
+    if ((rushPackages[package].versionPolicy == "client"||rushPackages[package].versionPolicy == "core")){
+       rushPackages = updateInternalDependencyVersions(rushPackages, package);
+    }
   }
-}
+
+  for (const package of Object.keys(rushPackages)) {
+    if ((rushPackages[package].versionPolicy == "client"||rushPackages[package].versionPolicy == "core")){
+      await commitChanges(rushPackages, package);
+    }
+  }
+};
 
 main(argv);
