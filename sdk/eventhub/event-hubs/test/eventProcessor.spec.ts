571156938733b7ad72ea14dceb19f9b786e34f5a
Richard Park (WDG/ES) <ripark@microsoft.com>
2019-11-16 12:57:09 -0800

    Move the subscription test handler out.
    Consider...merging/removing the received messages tester which is similar.

--- sdk/eventhub/event-hubs/test/eventProcessor.spec.ts
+++ sdk/eventhub/event-hubs/test/eventProcessor.spec.ts
@@ -14,9 +14,7 @@ import {
   ReceivedEventData,
   LastEnqueuedEventProperties,
   SubscriptionEventHandlers,
-  EventPosition,
-  EventHubProducerClient,
-  EventHubConsumerClient
+  EventPosition
 } from "../src";
 import { EventHubClient } from "../src/impl/eventHubClient";
 import { EnvVarKeys, getEnvVars, loopUntil } from "./utils/testUtils";
@@ -27,6 +25,7 @@ import { delay } from '@azure/core-amqp';
 import { InitializationContext, PartitionContext } from '../src/eventHubConsumerClientModels';
 import { InMemoryPartitionManager } from '../src/inMemoryPartitionManager';
 import { loggerForTest } from './utils/logHelpers';
+import { SubscriptionHandlerForTests, sendOneMessagePerPartition } from './utils/subscriptionHandlerForTests';
 const env = getEnvVars();
 
 describe("Event Processor", function (): void {
@@ -150,8 +149,6 @@ describe("Event Processor", function (): void {
     );
 
     processor.start();
-
-    await subscriptionEventHandler.waitUntilInitialized(partitionIds);
 
     const expectedMessages = await sendOneMessagePerPartition(partitionIds, client);
     const receivedEvents = await subscriptionEventHandler.waitForEvents(partitionIds);
@@ -679,143 +676,3 @@ describe("Event Processor", function (): void {
   });
 }).timeout(90000);
 
-type SequenceNumberMap = Map<string, number>;
-
-class SubscriptionHandlerForTests implements Required<SubscriptionEventHandlers> {
-  maxTimeToWaitSeconds = 120;
-
-  constructor(private _initialSequenceNumbers?: SequenceNumberMap) {
-  }
-
-  static async startingFromHere(client: EventHubClient | EventHubProducerClient | EventHubConsumerClient) {
-    const partitionIds = await client.getPartitionIds({});
-
-    const sequenceNumbers = new Map<string, number>();
-
-    for (const partitionId of partitionIds) {
-      const props = await client.getPartitionProperties(partitionId);
-      sequenceNumbers.set(props.partitionId, props.lastEnqueuedSequenceNumber);
-    }
-
-    return new SubscriptionHandlerForTests(sequenceNumbers);
-  }
-
-  public data: Map<string, {
-    closeReason?: CloseReason;
-    error?: Error;
-  }> = new Map();
-
-  public events: { partitionId: string, body: string }[] = [];
-
-  async processInitialize(context: InitializationContext) {
-    this.data.set(context.partitionId, {});
-
-    let startPosition = EventPosition.latest();
-
-    if (this._initialSequenceNumbers && this._initialSequenceNumbers.get(context.partitionId)) {
-      const sequenceNumber = this._initialSequenceNumbers.get(context.partitionId)!;
-      loggerForTest(`Overriding initial event position for partition ${context.partitionId} with ${sequenceNumber}`);
-      startPosition = EventPosition.fromSequenceNumber(sequenceNumber, false);
-    }
-
-    context.setStartPosition(startPosition);
-  }
-
-  async processClose(reason: CloseReason, context: PartitionContext) {
-    this.data.get(context.partitionId)!.closeReason = reason;
-  }
-
-  async processEvent(event: ReceivedEventData, context: PartitionContext) {
-    // by default we don't fill out the lastEnqueuedEventInfo field (they have to enable it
-    // explicitly in the options for the processor).
-    should.not.exist(context.lastEnqueuedEventProperties);
-
-    this.events.push({
-      body: event.body,
-      partitionId: context.partitionId
-    });
-  }
-  async processError(err: Error, context: PartitionContextError) {
-    should.exist(context.partitionId, `Non-partition level errors should definitely not happen : ${err}`);
-
-    if (context.partitionId) {
-      this.data.get(context.partitionId)!.error = err;
-    }
-  }
-
-  async waitUntilInitialized(partitionIds: string[]): Promise<void> {
-    await loopUntil({
-      name: "waiting until initialized",
-      maxTimes: 60,
-      timeBetweenRunsMs: 1000,
-      until: async () => { return this.data.size === partitionIds.length }
-    });
-  }
-
-  async waitForEvents(partitionIds: string[]): Promise<{ partitionId: string, body: string }[]>{
-    const startTime = Date.now();
-
-    // wait until all partitions have received at least 1 event
-    while (true) {
-      if (this.events.length !== partitionIds.length && !this.hasErrors) {
-        await delay(100);
-
-        if ((Date.now() - startTime) > this.maxTimeToWaitSeconds * 1000) {
-          throw new Error("Waiting _way_ too long in initialize");
-        }
-      } else {
-        // sort for simpler comparisons in our tests
-        this.events.sort((a, b) => {
-          const akey = `${a.partitionId}:${a.body}`;
-          const bkey = `${b.partitionId}:${b.body}`;
-          return akey.localeCompare(bkey);
-        })
-
-        return this.events;
-      }
-    }
-  }
-
-  clear() {
-    this.data = new Map();
-    this.events = [];
-  }
-
-  hasErrors(partitionIds: string[]): boolean {
-    for (const partitionId of partitionIds) {
-      if (this.data.get(partitionId)!.error) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  allShutdown(partitionIds: string[]): boolean {
-    for (const partitionId of partitionIds) {
-      if (this.data.get(partitionId)!.closeReason !== CloseReason.Shutdown) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-}
-
-async function sendOneMessagePerPartition(partitionIds: string[], client: EventHubClient): Promise<{ body: string, partitionId: string}[]>{
-  const expectedMessagePrefix = "EventProcessor test - multiple partitions - ";
-  const sentMessages = [];
-
-  for (const partitionId of partitionIds) {
-    const producer = client.createProducer({ partitionId });
-    const body = expectedMessagePrefix + partitionId;
-    await producer.send({ body });
-    await producer.close();
-    sentMessages.push({
-      body,
-      partitionId
-    });
-  }
-
-  return sentMessages;
-}
