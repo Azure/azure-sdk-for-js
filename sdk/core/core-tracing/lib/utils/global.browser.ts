1c72142166e5de84934cf6bd05066ba50498a61e
Jeff Fisher <jeffish@microsoft.com>
2019-11-19 17:30:43 -0800

    Remove dom lib reference

--- sdk/core/core-tracing/lib/utils/global.browser.ts
+++ sdk/core/core-tracing/lib/utils/global.browser.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-/// <reference lib="dom"/>
-
 export function getGlobalObject(): any {
   return self;
 }
