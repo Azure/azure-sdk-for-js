82ef9beec0ef728d64f8c56e64f30cc37941ce09
Daniel Imms <tyriar@tyriar.com>
2019-11-23 10:19:12 -0800

    [Cosmos] Add jsdoc for partitionKey (#6319)
    Description came from the create a collection article on docs.ms

--- sdk/cosmosdb/cosmos/src/documents/PartitionKeyDefinition.ts
+++ sdk/cosmosdb/cosmos/src/documents/PartitionKeyDefinition.ts
@@ -1,7 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 export interface PartitionKeyDefinition {
+  /**
+   * An array of paths for which data within the collection can be partitioned. Paths must not contain a wildcard or
+   * a trailing slash. For example, the JSON property “AccountNumber” is specified as “/AccountNumber”. The array must
+   * contain only a single value.
+   */
   paths: string[];
+  /**
+   * An optional field, if not specified the default value is 1. To use the large partition key set the version to 2.
+   * To learn about large partition keys, see [how to create containers with large partition key](https://docs.microsoft.com/en-us/azure/cosmos-db/large-partition-keys) article.
+   */
   version?: number;
   systemKey?: boolean;
 }
