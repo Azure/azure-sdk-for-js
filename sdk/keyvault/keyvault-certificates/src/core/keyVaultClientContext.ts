e0662a2228aed208594a3da9811ea8d80d458d7e
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2019-12-03 21:59:37 +0000

    agent strings

--- sdk/keyvault/keyvault-certificates/src/core/keyVaultClientContext.ts
+++ sdk/keyvault/keyvault-certificates/src/core/keyVaultClientContext.ts
@@ -11,7 +11,7 @@
 import * as coreHttp from "@azure/core-http";
 
 const packageName = "@azure/keyvault-certificates";
-const packageVersion = "4.0.0-preview.10";
+const packageVersion = "4.0.0-preview.11";
 
 export class KeyVaultClientContext extends coreHttp.ServiceClient {
   apiVersion: string;
