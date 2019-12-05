Azure/azure-sdk-for-js

commit 7e2c2ac7edde5babfe2de47c27d22c8af9a73ba3

[core-http] Report progress after pushing data (#5298)
We were reporting the progress before data is pushed. This leads to customer
reported issue like

https://github.com/Azure/azure-sdk-for-js/issues/4719

where the uploaded progress events are triggered before data chunks are
completely uploaded.

This change introduces a `ReportStream` that provides the correct `_transform`
behavior.

sdk/core/core-http/lib/fetchHttpClient.ts
@@ -5,7 +5,7 @@ import { AbortController } from "@azure/abort-controller";
 import FormData from "form-data";
 
 import { HttpClient } from "./httpClient";
-import { WebResource } from "./webResource";
+import { TransferProgressEvent, WebResource } from "./webResource";
 import { HttpOperationResponse } from "./httpOperationResponse";
 import { HttpHeaders } from "./httpHeaders";
 import { RestError } from "./restError";
@@ -15,6 +15,20 @@ interface FetchError extends Error {
   code?: string;
   errno?: string;
   type?: string;
+}
+
+class ReportTransform extends Transform {
+  private loadedBytes: number = 0;
+  _transform(chunk: string | Buffer, _encoding: string, callback: Function) {
+    this.push(chunk);
+    this.loadedBytes += chunk.length;
+    this.progressCallback!({ loadedBytes: this.loadedBytes });
+    callback(undefined, chunk);
+  }
+
+  constructor(private progressCallback: ((progress: TransferProgressEvent) => void)) {
+    super();
+  }
 }
 
 export abstract class FetchHttpClient implements HttpClient {
@@ -84,15 +98,8 @@ export abstract class FetchHttpClient implements HttpClient {
             ? (typeof httpRequest.body === "function" ? httpRequest.body() : httpRequest.body)
             : undefined;
     if (httpRequest.onUploadProgress && httpRequest.body) {
-      let loadedBytes = 0;
-      const uploadReportStream = new Transform({
-        transform: (chunk: string | Buffer, _encoding, callback) => {
-          loadedBytes += chunk.length;
-          httpRequest.onUploadProgress!({ loadedBytes });
-          callback(undefined, chunk);
-        }
-      });
-
+      const onUploadProgress = httpRequest.onUploadProgress;
+      const uploadReportStream = new ReportTransform(onUploadProgress);
       if (isReadableStream(body)) {
         body.pipe(uploadReportStream);
       } else {
@@ -129,14 +136,7 @@ export abstract class FetchHttpClient implements HttpClient {
         const responseBody: ReadableStream<Uint8Array> | undefined = response.body || undefined;
 
         if (isReadableStream(responseBody)) {
-          let loadedBytes = 0;
-          const downloadReportStream = new Transform({
-            transform: (chunk: string | Buffer, _encoding, callback) => {
-              loadedBytes += chunk.length;
-              onDownloadProgress({ loadedBytes });
-              callback(undefined, chunk);
-            }
-          });
+          const downloadReportStream = new ReportTransform(onDownloadProgress);
           responseBody.pipe(downloadReportStream);
           operationResponse.readableStreamBody = downloadReportStream;
         } else {

sdk/storage/storage-file/test/utils/recorder.ts
@@ -51,6 +51,8 @@ const skip = [
   "browsers/fileurl/recording_download_should_update_progress_and_abort_successfully.json",
   // Unknown reason (recording throws an error, but file is generated and playback works)
   "browsers/fileurl/recording_uploadrange_with_progress_event.json",
+  // Nock issue: https://github.com/Azure/azure-sdk-for-js/issues/5229
+  "node/fileurl/recording_uploadrange_with_progress_event.js",
   // Abort. Nock doesn't record aborted request, should investigate
   "node/highlevel_nodejs_only/recording_uploadfiletoazurefile_should_abort_for_large_data.js",
   // Abort. Nock doesn't record aborted request, should investigate

commit c38f0377508d2f9b5d9996b46711ee36e85544cf

[Core-http] Fix issue of data being pushed twice when reporting progress
The regression was introduced in #5298. If a second argument `data`
is passed to `callback()` in `_transform()`, `data` will be pushed
again.  I misread the doc when adding the report transform.

This change removes the second argument from `callback()`.

Fixes #6352.

sdk/core/core-http/lib/fetchHttpClient.ts
@@ -17,13 +17,13 @@ interface FetchError extends Error {
   type?: string;
 }
 
-class ReportTransform extends Transform {
+export class ReportTransform extends Transform {
   private loadedBytes: number = 0;
   _transform(chunk: string | Buffer, _encoding: string, callback: Function) {
     this.push(chunk);
     this.loadedBytes += chunk.length;
     this.progressCallback!({ loadedBytes: this.loadedBytes });
-    callback(undefined, chunk);
+    callback(undefined);
   }
 
   constructor(private progressCallback: (progress: TransferProgressEvent) => void) {

sdk/core/core-http/test/defaultHttpClientTests.ts
@@ -13,6 +13,8 @@ import { isNode } from "../lib/util/utils";
 import { WebResource, HttpRequestBody, TransferProgressEvent } from "../lib/webResource";
 import { getHttpMock, HttpMockFacade } from "./mockHttp";
 import { TestFunction } from "mocha";
+import { PassThrough } from 'stream';
+import { ReportTransform } from '../lib/fetchHttpClient';
 
 const nodeIt = (isNode ? it : it.skip) as TestFunction;
 
@@ -370,5 +372,18 @@ describe("defaultHttpClient", function() {
     assert.strictEqual(responseBody, responseContent);
 
     httpMock.teardown();
+  });
+});
+
+describe("ReportTransform", function() {
+  it("should not modify the stream data", function() {
+    const a = new PassThrough();
+    const b = new PassThrough();
+    const callback = () => {};
+    const report = new ReportTransform(callback);
+    a.pipe(report, { end: false }).pipe(b, { end: false });
+    a.write("hello");
+    const transformed = b.read();
+    transformed.toString().should.equal("hello");
   });
 });