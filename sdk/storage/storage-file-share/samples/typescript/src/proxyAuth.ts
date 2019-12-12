77c602d2463a279c803a6a18c7492bdc70f8e235
Will Temple <witemple@microsoft.com>
2019-12-12 12:19:12 -0800

    [storage] changed proxyAuth samples to use HTTP[s]_PROXY vars

--- sdk/storage/storage-file-share/samples/typescript/src/proxyAuth.ts
+++ sdk/storage/storage-file-share/samples/typescript/src/proxyAuth.ts
@@ -14,29 +14,31 @@ export async function main() {
   const account = process.env.ACCOUNT_NAME || "";
   const accountKey = process.env.ACCOUNT_KEY || "";
 
-  // Enter your proxy information
-  const proxyOptions = {
-    host: process.env.PROXY_HOST || "",
-    port: parseInt(process.env.PROXY_PORT || "3128"),
-    username: process.env.PROXY_USERNAME || "",
-    password: process.env.PROXY_PASSWORD || ""
-  };
-
-  if (proxyOptions.host === "") {
-    console.warn("Proxy information not provided, but it is required to run this sample. Exiting.");
-    return;
-  }
-
   // Use StorageSharedKeyCredential with storage account and account key
   // StorageSharedKeyCredential is only avaiable in Node.js runtime, not in browsers
   const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 
+  // To use the manual proxyOptions below, remove this block
+  if (!process.env.HTTP_PROXY || !process.env.HTTPS_PROXY) {
+    console.warn("Proxy information not provided, but it is required to run this sample. Exiting.");
+    return;
+  }
+
   const serviceClient = new ShareServiceClient(
     `https://${account}.file.core.windows.net`,
     sharedKeyCredential,
+    // The library tries to load the proxy settings from the environment variables like HTTP_PROXY
+    // Alternatively, the service client accepts the following `proxyOptions` as part of its options:
     {
-      // if proxy is undefined, the library tries to load the proxy settings from the environment variables like HTTP_PROXY
-      proxyOptions
+      /*
+      proxyOptions : {
+        // To use these options, remove the section above that checks for HTTP_PROXY or HTTPS_PROXY
+        host: "http://localhost",
+        port: 3128,
+        username: "<username>",
+        password: "<password>"
+      }
+      */
     }
   );
 
