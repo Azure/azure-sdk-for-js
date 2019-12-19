882ae93da0bbca805879d3162584521b9093a2cb
SDK Automation <sdkautomation@microsoft.com>
2019-12-19 05:30:40 +0000

    Generated from 20ad0a73604a7fc6fe91fb337b4dc330461c4d7c
    ARM review comments taken

--- sdk/recoveryservicessiterecovery/arm-recoveryservices-siterecovery/src/siteRecoveryManagementClientContext.ts
+++ sdk/recoveryservicessiterecovery/arm-recoveryservices-siterecovery/src/siteRecoveryManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-recoveryservices-siterecovery";
-const packageVersion = "0.1.0";
+const packageVersion = "3.2.0";
 
 export class SiteRecoveryManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
@@ -55,7 +55,7 @@ export class SiteRecoveryManagementClientContext extends msRestAzure.AzureServic
 
     super(credentials, options);
 
-    this.apiVersion = '2018-01-10';
+    this.apiVersion = '2018-07-10';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
