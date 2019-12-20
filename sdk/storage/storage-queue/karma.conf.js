138cbee8db726918cc40c040e1a0a99de50216a1
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-12-20 10:10:51 -0800

    keep only the json reporter in coverageReporter

--- sdk/storage/storage-queue/karma.conf.js
+++ sdk/storage/storage-queue/karma.conf.js
@@ -60,19 +60,7 @@ module.exports = function(config) {
     coverageReporter: {
       // specify a common output directory
       dir: "coverage-browser/",
-      reporters: [
-        { type: "in-memory" },
-        // reporters not supporting the `file` property
-        { type: "html", subdir: "report-html" },
-        { type: "lcov", subdir: "report-lcov" },
-        // reporters supporting the `file` property, use `subdir` to directly
-        // output them in the `dir` directory
-        { type: "json", subdir: ".", file: "coverage.json" },
-        { type: "lcovonly", subdir: ".", file: "report-lcovonly.txt" },
-        { type: "teamcity", subdir: ".", file: "teamcity.txt" },
-        { type: "text", subdir: ".", file: "text.txt" },
-        { type: "text-summary", subdir: ".", file: "text-summary.txt" }
-      ]
+      reporters: [{ type: "json", subdir: ".", file: "coverage.json" }]
     },
 
     remapIstanbulReporter: {
