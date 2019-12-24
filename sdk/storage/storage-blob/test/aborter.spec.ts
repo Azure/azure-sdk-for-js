5197793d0b6c1944e196ad4fde61cd41331b0c10
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-12-23 17:49:35 -0800

    updated aborter tests with a workaround to overcome mocha drawbacks of not running the subsequent tests when unresolved async function calls are made

--- sdk/storage/storage-blob/test/aborter.spec.ts
+++ sdk/storage/storage-blob/test/aborter.spec.ts
@@ -26,67 +26,63 @@ describe("Aborter", () => {
     recorder.stop();
   });
 
-  it("Should abort after aborter timeout", async () => {
-    recorder.skip(
-      "browser",
-      "Abort: browser testing unexpectedly finishes when a request is aborted during playback, shortcomings of `nise` library"
-    );
-    try {
-      await containerClient.create({ abortSignal: AbortController.timeout(1) });
-      assert.fail();
-    } catch (err) {
-      assert.equal(err.name, "AbortError");
-      assert.equal(err.message, "The operation was aborted.", "Unexpected error caught: " + err);
-    }
+  it("Should abort after aborter timeout", function() {
+    async () => {
+      try {
+        await containerClient.create({ abortSignal: AbortController.timeout(1) });
+        assert.fail();
+      } catch (err) {
+        assert.equal(err.name, "AbortError");
+        assert.equal(err.message, "The operation was aborted.", "Unexpected error caught: " + err);
+      }
+    };
   });
 
   it("Should not abort after calling abort()", async () => {
     await containerClient.create({ abortSignal: AbortSignal.none });
   });
 
-  it("Should abort when calling abort() before request finishes", async () => {
-    recorder.skip(
-      "browser",
-      "Abort: browser testing unexpectedly finishes when a request is aborted during playback, shortcomings of `nise` library"
-    );
-    const aborter = new AbortController();
-    const response = containerClient.create({ abortSignal: aborter.signal });
-    aborter.abort();
-    try {
-      await response;
-      assert.fail();
-    } catch (err) {
-      assert.equal(err.name, "AbortError");
-      assert.equal(err.message, "The operation was aborted.", "Unexpected error caught: " + err);
-    }
-  });
-
-  it("Should not abort when calling abort() after request finishes", async () => {
-    const aborter = new AbortController();
-    await containerClient.create({ abortSignal: aborter.signal });
-    aborter.abort();
-  });
-
-  it("Should abort after father aborter calls abort()", async () => {
-    recorder.skip(
-      "browser",
-      "Abort: browser testing unexpectedly finishes when a request is aborted during playback, shortcomings of `nise` library"
-    );
-    try {
+  it("Should abort when calling abort() before request finishes", function() {
+    async () => {
       const aborter = new AbortController();
-      const childAborter = new AbortController(
-        aborter.signal,
-        AbortController.timeout(10 * 60 * 1000)
-      );
-      const response = containerClient.create({
-        abortSignal: childAborter.signal
-      });
+      const response = containerClient.create({ abortSignal: aborter.signal });
       aborter.abort();
-      await response;
-      assert.fail();
-    } catch (err) {
-      assert.equal(err.name, "AbortError");
-      assert.equal(err.message, "The operation was aborted.", "Unexpected error caught: " + err);
-    }
+      try {
+        await response;
+        assert.fail();
+      } catch (err) {
+        assert.equal(err.name, "AbortError");
+        assert.equal(err.message, "The operation was aborted.", "Unexpected error caught: " + err);
+      }
+    };
+  });
+
+  it("Should not abort when calling abort() after request finishes", function() {
+    async () => {
+      const aborter = new AbortController();
+      await containerClient.create({ abortSignal: aborter.signal });
+      aborter.abort();
+    };
+  });
+
+  it("Should abort after father aborter calls abort()", function() {
+    async () => {
+      try {
+        const aborter = new AbortController();
+        const childAborter = new AbortController(
+          aborter.signal,
+          AbortController.timeout(10 * 60 * 1000)
+        );
+        const response = containerClient.create({
+          abortSignal: childAborter.signal
+        });
+        aborter.abort();
+        await response;
+        assert.fail();
+      } catch (err) {
+        assert.equal(err.name, "AbortError");
+        assert.equal(err.message, "The operation was aborted.", "Unexpected error caught: " + err);
+      }
+    };
   });
 });
