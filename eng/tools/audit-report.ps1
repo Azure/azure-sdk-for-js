62f3290085f0fcd60dffe577dcafb86a5dfe55a1
Karishma Ghiya <kaghiya@microsoft.com>
2019-12-30 16:08:56 -0800

    added script for npm audit to not fail on enoaudit

--- eng/tools/audit-report.ps1
+++ eng/tools/audit-report.ps1
@@ -1,1 +1,14 @@
-try{ }
+try{
+  $cmdOutput=& npm audit 2>&1 | Out-String
+  if($cmdOutput.contains('npm ERR! code ENOAUDIT')){
+    $cmdOutput
+    exit 0
+  }
+  else{
+    npm audit
+  }
+ }
+ catch{
+  Write-Host "An error occurred:"
+  Write-Host $_
+ }
