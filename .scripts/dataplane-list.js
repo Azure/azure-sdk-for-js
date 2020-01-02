ce4dcb54ee71b5a7d31f1716538d042ba89a56ca
Karishma Ghiya <kaghiya@microsoft.com>
2020-01-02 13:16:20 -0800

    updated the script for listing dataplane packages

--- .scripts/dataplane-list.js
+++ .scripts/dataplane-list.js
@@ -1,14 +1,36 @@
 const versionUtils = require("../eng/tools/versioning/VersionUtils");
+const process = require("process");
+
+const parseArgs = () => {
+  if (
+    process.argv.length < 4 ||
+    process.argv.some(a => ["-h", "--help"].includes(a.toLowerCase()))
+  ) {
+    console.error("Usage: dataplane-list.js <pathToRepo> <pathToFileListName>");
+    console.error("Example: dataplane-list.js . list-dataplane.json");
+    process.exit(1);
+  }
+  var pathToRepo = process.argv[2];
+  var pathToFileListName = process.argv[3];
+  return [pathToRepo, pathToFileListName];
+};
+
+const listDataplanePackages = async pathToFileListName => {
+  dataplane = await versionUtils.readFile(pathToFileListName);
+  listDataplane = dataplane.split(",");
+  return listDataplane;
+};
 
 async function main() {
-  var rushPackages = await versionUtils.getRushPackageJsons(".");
-  //console.log(Object.keys(rushPackages));
+  const [pathToRepo, pathToFileListName] = parseArgs();
+  //const pathToFileListName = "list-dataplane.json";
+  var rushPackages = await versionUtils.getRushPackageJsons(pathToRepo);
+
   await versionUtils.writeFile(
-    "list-dataplane.json",
+    pathToFileListName,
     Object.keys(rushPackages).toString()
   );
-  dataplane = await versionUtils.readFile("list-dataplane.json");
-  listDataplane = dataplane.split(",");
-  console.log(listDataplane);
+  var res = await listDataplanePackages(pathToFileListName);
+  console.log(res[0]);
 }
 main();
