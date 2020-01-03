db1ce22b2a567cc4f489f7ca6b2632faa663966f
Steve Faulkner <southpolesteve@gmail.com>
2020-01-03 12:18:34 -0600

    Check for MIN_VERSION constant

--- sdk/cosmosdb/cosmos/src/request/defaultAgent.ts
+++ sdk/cosmosdb/cosmos/src/request/defaultAgent.ts
@@ -15,10 +15,22 @@ export let defaultHttpsAgent: Agent;
 if (isNode) {
   // tslint:disable-next-line:no-var-requires
   const https = require("https");
-  defaultHttpsAgent = new https.Agent({
-    keepAlive: true,
-    secureProtocol: "TLSv1_2_method"
-  });
+  // tslint:disable-next-line:no-var-requires
+  const tls = require("tls");
+
+  // minVersion only available in Node 10+
+  if (tls.DEFAULT_MIN_VERSION) {
+    defaultHttpsAgent = new https.Agent({
+      keepAlive: true,
+      minVersion: "TLSv1.2"
+    });
+  } else {
+    // Remove when Node 8 support has been dropped
+    defaultHttpsAgent = new https.Agent({
+      keepAlive: true,
+      secureProtocol: "TLSv1_2_method"
+    });
+  }
   // tslint:disable-next-line:no-var-requires
   const http = require("http");
   defaultHttpAgent = new http.Agent({
