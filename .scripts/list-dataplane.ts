37d662379452fcc451fa4e280bb9911c3d4e895d
Karishma Ghiya <kaghiya@microsoft.com>
2020-01-06 18:00:51 -0800

    try

---
+++ .scripts/list-dataplane.ts
@@ -0,0 +1,29 @@
+const versionUtils = require("../eng/tools/versioning/VersionUtils");
+const process = require("process");
+const dataplaneListFilename = "list-dataplane.json";
+
+const parseArgs = () => {
+  if (
+    process.argv.length < 3 ||
+    process.argv.some(a => ["-h", "--help"].includes(a.toLowerCase()))
+  ) {
+    console.error("Usage: dataplane-list.js <pathToRepo>");
+    console.error("Example: dataplane-list.js .");
+    process.exit(1);
+  }
+  var pathToRepo = process.argv[2];
+  return [pathToRepo];
+};
+
+async function main() {
+  const [pathToRepo] = parseArgs();
+  const pathToFileListName = dataplaneListFilename;
+  var rushPackages = await versionUtils.getRushPackageJsons(pathToRepo);
+
+  await versionUtils.writeFile(
+    pathToFileListName,
+    Object.keys(rushPackages).toString()
+  );
+}
+
+main();
