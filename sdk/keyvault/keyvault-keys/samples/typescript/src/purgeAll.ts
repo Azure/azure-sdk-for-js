a8d7d1c4b4f4867e923c18ba94eb7b1b4b7953ca
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-01-08 20:04:49 +0000

    the work for secrets, and the purge sample

---
+++ sdk/keyvault/keyvault-keys/samples/typescript/src/purgeAll.ts
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft corporation.
+// Licensed under the MIT license.
+
+// purgeAll.ts
+// helps remove any existing resources from the KeyVault.
+
+import { KeyClient } from "@azure/keyvault-keys";
+import { DefaultAzureCredential } from "@azure/identity";
+
+// Load the .env file if it exists
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export async function main(): Promise<void> {
+  // DefaultAzureCredential expects the following three environment variables:
+  // - AZURE_TENANT_ID: The tenant ID in Azure Active Directory
+  // - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
+  // - AZURE_CLIENT_SECRET: The client secret for the registered application
+  const credential = new DefaultAzureCredential();
+
+  const vaultName = process.env["KEYVAULT_NAME"] || "<keyvault-name>";
+  const url = `https://${vaultName}.vault.azure.net`;
+  const client = new KeyClient(url, credential);
+
+  for await (const properties of client.listPropertiesOfKeys()) {
+    try {
+      await client.flushKey(properties.name);
+    } catch (e) {}
+  }
+  for await (const deletedKey of client.listDeletedKeys()) {
+    try {
+      await client.purgeKey(deletedKey.name);
+    } catch (e) {}
+  }
+}
+
+main().catch((err) => {
+  console.log("error code: ", err.code);
+  console.log("error message: ", err.message);
+  console.log("error stack: ", err.stack);
+});
