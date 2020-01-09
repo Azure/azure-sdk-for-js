c0900a683de89155b947f6c9ffc4941b2794b4da
Karishma Ghiya <kaghiya@microsoft.com>
2020-01-08 17:16:39 -0800

    add fs module reference

--- .scripts/generateDataplaneList.ts
+++ .scripts/generateDataplaneList.ts
@@ -1,4 +1,5 @@
 import * as path from "path";
+import * as fs from "fs";
 const parse = require("../common/lib/jju/parse").parse;
 
 export type PackageData = { packageList: string[]; folderList: string[] };
