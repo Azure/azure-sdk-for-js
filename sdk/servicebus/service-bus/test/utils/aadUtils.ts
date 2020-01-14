c4a2267409c17d4cd089b60c7480a87a030b2877
Ramya Raja <ramya.raja@microsoft.com>
2020-01-14 13:48:23 -0800

    Remove unused util

--- sdk/servicebus/service-bus/test/utils/aadUtils.ts
+++ sdk/servicebus/service-bus/test/utils/aadUtils.ts
@@ -1,10 +1,4 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { EnvironmentCredential } from "@azure/identity";
-
-export async function getTokenCredentialsFromAAD() {
-  return new EnvironmentCredential();
-}
-
 export { EnvironmentCredential } from "@azure/identity";
