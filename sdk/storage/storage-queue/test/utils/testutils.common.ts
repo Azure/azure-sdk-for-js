2b0c7787d7cd25a823ccd8ad5f4ff2b72ccd3966
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2020-01-23 15:33:26 -0800

    import RecorderEnvironmentSetup in storage-queue

--- sdk/storage/storage-queue/test/utils/testutils.common.ts
+++ sdk/storage/storage-queue/test/utils/testutils.common.ts
@@ -1,21 +1,15 @@
 import { padStart } from "../../src/utils/utils.common";
-import {
-  setReplaceableVariables,
-  skipQueryParams,
-  env,
-  setReplacements
-} from "@azure/test-utils-recorder";
+import { env, RecorderEnvironmentSetup } from "@azure/test-utils-recorder";
 
 export function isBrowser(): boolean {
   return typeof window !== "undefined";
 }
 
-export function setupEnvironment() {
-  // setReplaceableVariables()
-  // 1. The key-value pairs will be used as the environment variables in playback
-  // 2. If the env variales are present in the recordings as plain strings, they will be replaced with the provided values
-  setReplaceableVariables({
-    // Providing dummy values
+export const recorderEnvSetup: RecorderEnvironmentSetup = {
+  replaceableVariables: {
+    // Used in record and playback modes
+    // 1. The key-value pairs will be used as the environment variables in playback mode
+    // 2. If the env variales are present in the recordings as plain strings, they will be replaced with the provided values in record mode
     ACCOUNT_NAME: "fakestorageaccount",
     ACCOUNT_KEY: "aaaaa",
     ACCOUNT_SAS: "aaaaa",
@@ -23,19 +17,27 @@ export function setupEnvironment() {
     // Comment following line to skip user delegation key/SAS related cases in record and play
     // which depends on this environment variable
     ACCOUNT_TOKEN: "aaaaa"
-  });
-
-  // Array of callback functions can be passed to `setReplacements` to customize the generated recordings
-  // `sig` param of SAS Token is being filtered here
-  setReplacements([
+  },
+  replaceInRecordings: [
+    // Used in record mode
+    // Array of callback functions can be provided to customize the generated recordings in record mode
+    // `sig` param of SAS Token is being filtered here
     (recording: string): string =>
       recording.replace(new RegExp(env.ACCOUNT_SAS.match("(.*)&sig=(.*)")[2], "g"), "aaaaa")
-  ]);
-
+  ],
   // SAS token may contain sensitive information
-  // skipQueryParams() method will filter out the plain parameter info from the recordings
-  skipQueryParams(["se", "sig", "sp", "spr", "srt", "ss", "st", "sv"]);
-}
+  queryParametersToSkip: [
+    // Used in record and playback modes
+    "se",
+    "sig",
+    "sp",
+    "spr",
+    "srt",
+    "ss",
+    "st",
+    "sv"
+  ]
+};
 
 export function getUniqueName(prefix: string): string {
   return `${prefix}${new Date().getTime()}${padStart(
