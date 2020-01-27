a729f24313990ad59f5cf3f0b6ab970d09a932c0
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-01-27 21:52:13 +0000

    undoing the src/core changes

--- sdk/keyvault/keyvault-keys/src/core/challengeBasedAuthenticationPolicy.ts
+++ sdk/keyvault/keyvault-keys/src/core/challengeBasedAuthenticationPolicy.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. See license.txt in the project root for license information.
+// Licensed under the MIT License. See License.txt in the project root for license information.
 
 import { TokenCredential } from "@azure/core-http";
 import { BaseRequestPolicy, RequestPolicy, RequestPolicyOptions, RequestPolicyFactory } from "@azure/core-http";
