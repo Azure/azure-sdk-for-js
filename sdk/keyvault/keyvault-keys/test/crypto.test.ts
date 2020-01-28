2914995781d26eb037506ce5a9ae0e3fdf10ecb0
Daniel Rodríguez <sadasant@users.noreply.github.com>
2020-01-28 12:05:10 -0500

    [KeyVault] Small cleanup after checking the eslint errors (many files changed) (#7126)
    Originally from: https://github.com/Azure/azure-sdk-for-js/pull/7120/files
    
    Eslint rules require a considerable cleanup before we can run them.
    
    This PR is mostly about an update to the copyright headers of the files. Other changes include changing from let to const and deleting some types that weren't needed anymore.

--- sdk/keyvault/keyvault-keys/test/crypto.test.ts
+++ sdk/keyvault/keyvault-keys/test/crypto.test.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+// Licensed under the MIT license.
 
 import * as assert from "assert";
 import { createHash, publicEncrypt } from "crypto";
