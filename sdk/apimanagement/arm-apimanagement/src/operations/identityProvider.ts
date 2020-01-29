730b5abcdd6dcac543144cf3959c7679647ba5be
SDK Automation <sdkautomation@microsoft.com>
2020-01-29 22:07:37 +0000

    Generated from 87c99392a06f3fbfde0507d9c802c17aac48414a
    ApiManagement - long running operations - 200 - payload

--- sdk/apimanagement/arm-apimanagement/src/operations/identityProvider.ts
+++ sdk/apimanagement/arm-apimanagement/src/operations/identityProvider.ts
@@ -277,6 +277,45 @@ export class IdentityProvider {
   }
 
   /**
+   * Gets the client secret details of the Identity Provider.
+   * @param resourceGroupName The name of the resource group.
+   * @param serviceName The name of the API Management service.
+   * @param identityProviderName Identity Provider Type identifier. Possible values include:
+   * 'facebook', 'google', 'microsoft', 'twitter', 'aad', 'aadB2C'
+   * @param [options] The optional parameters
+   * @returns Promise<Models.IdentityProviderListSecretsResponse>
+   */
+  listSecrets(resourceGroupName: string, serviceName: string, identityProviderName: Models.IdentityProviderType, options?: msRest.RequestOptionsBase): Promise<Models.IdentityProviderListSecretsResponse>;
+  /**
+   * @param resourceGroupName The name of the resource group.
+   * @param serviceName The name of the API Management service.
+   * @param identityProviderName Identity Provider Type identifier. Possible values include:
+   * 'facebook', 'google', 'microsoft', 'twitter', 'aad', 'aadB2C'
+   * @param callback The callback
+   */
+  listSecrets(resourceGroupName: string, serviceName: string, identityProviderName: Models.IdentityProviderType, callback: msRest.ServiceCallback<Models.ClientSecretContract>): void;
+  /**
+   * @param resourceGroupName The name of the resource group.
+   * @param serviceName The name of the API Management service.
+   * @param identityProviderName Identity Provider Type identifier. Possible values include:
+   * 'facebook', 'google', 'microsoft', 'twitter', 'aad', 'aadB2C'
+   * @param options The optional parameters
+   * @param callback The callback
+   */
+  listSecrets(resourceGroupName: string, serviceName: string, identityProviderName: Models.IdentityProviderType, options: msRest.RequestOptionsBase, callback: msRest.ServiceCallback<Models.ClientSecretContract>): void;
+  listSecrets(resourceGroupName: string, serviceName: string, identityProviderName: Models.IdentityProviderType, options?: msRest.RequestOptionsBase | msRest.ServiceCallback<Models.ClientSecretContract>, callback?: msRest.ServiceCallback<Models.ClientSecretContract>): Promise<Models.IdentityProviderListSecretsResponse> {
+    return this.client.sendOperationRequest(
+      {
+        resourceGroupName,
+        serviceName,
+        identityProviderName,
+        options
+      },
+      listSecretsOperationSpec,
+      callback) as Promise<Models.IdentityProviderListSecretsResponse>;
+  }
+
+  /**
    * Lists a collection of Identity Provider configured in the specified service instance.
    * @param nextPageLink The NextLink from the previous successful call to List operation.
    * @param [options] The optional parameters
@@ -475,6 +514,32 @@ const deleteMethodOperationSpec: msRest.OperationSpec = {
   responses: {
     200: {},
     204: {},
+    default: {
+      bodyMapper: Mappers.ErrorResponse
+    }
+  },
+  serializer
+};
+
+const listSecretsOperationSpec: msRest.OperationSpec = {
+  httpMethod: "POST",
+  path: "subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ApiManagement/service/{serviceName}/identityProviders/{identityProviderName}/listSecrets",
+  urlParameters: [
+    Parameters.resourceGroupName,
+    Parameters.serviceName,
+    Parameters.identityProviderName,
+    Parameters.subscriptionId
+  ],
+  queryParameters: [
+    Parameters.apiVersion
+  ],
+  headerParameters: [
+    Parameters.acceptLanguage
+  ],
+  responses: {
+    200: {
+      bodyMapper: Mappers.ClientSecretContract
+    },
     default: {
       bodyMapper: Mappers.ErrorResponse
     }
