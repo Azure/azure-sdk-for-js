0b4d2f0bb858f33ddf8955a98980d607b8ab5877
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2020-01-30 19:55:22 -0800

    document the error

--- sdk/core/core-http/lib/webResource.ts
+++ sdk/core/core-http/lib/webResource.ts
@@ -503,6 +503,7 @@ export interface RequestOptionsBase {
 
   /**
    * The number of milliseconds a request can take before automatically being terminated.
+   * If the request is terminated, an abort error is thrown with the message - "The operation was aborted."
    */
   timeout?: number;
 
