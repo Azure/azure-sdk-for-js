c9fa14d0cea84a3e30a047740dd4755ba493557c
Jeff Fisher <jeffish@microsoft.com>
2020-01-31 13:21:43 -0800

    Fix bad queueClient test

--- sdk/storage/storage-queue/test/queueclient.spec.ts
+++ sdk/storage/storage-queue/test/queueclient.spec.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 import { QueueClient } from "../src";
 import { TestTracer, setTracer, SpanGraph } from "@azure/core-tracing";
 import { setupEnvironment } from "./utils/testutils.common";
-import { URLBuilder } from "@azure/core-http";
+import { URLBuilder, RestError } from "@azure/core-http";
 dotenv.config({ path: "../.env" });
 
 describe("QueueClient", () => {
@@ -52,18 +52,18 @@ describe("QueueClient", () => {
   it("getProperties negative", async () => {
     const queueName2 = recorder.getUniqueName("queue", "queue2");
     const queueClient2 = queueServiceClient.getQueueClient(queueName2);
-    let error;
+    let error: RestError | undefined;
     try {
       await queueClient2.getProperties();
     } catch (err) {
       error = err;
     }
     assert.ok(error);
-    assert.ok(error.statusCode);
-    assert.deepEqual(error.statusCode, 404);
-    assert.ok(error.response);
-    assert.ok(error.response.body);
-    assert.ok(error.response.body.includes("QueueNotFound"));
+    assert.ok(error!.statusCode);
+    assert.deepEqual(error!.statusCode, 404);
+    assert.ok(error!.response);
+    assert.ok(error!.response!.bodyAsText);
+    assert.ok(error!.response!.bodyAsText!.includes("QueueNotFound"));
   });
 
   it("create with default parameters", (done) => {
