f6aa58ce1ed6851f1e831e2c194e36303197f348
Daniel Jurek <djurek@microsoft.com>
2020-01-31 14:59:23 -0800

    add comment about the need for the post-deploy script

--- sdk/storage/test-resources-post.ps1
+++ sdk/storage/test-resources-post.ps1
@@ -2,6 +2,9 @@
 # Licensed under the MIT License.
 
 # This script is used to set up CORS rules for queues after storage account creation by New-TestResources.ps1
+# There are no documented approaches to specifying CORS rules using ARM, this is a workaround until
+# support for setting CORS rules is added to ARM for Queues
+
 # It is invoked by the https://github.com/Azure/azure-sdk-for-js/blob/master/eng/New-TestResources.ps1
 # script after the ARM template, defined in https://github.com/Azure/azure-sdk-for-js/blob/master/sdk/storage/test-resources.json,
 # is finished being deployed. The ARM template is responsible for creating the Storage accounts needed for live tests.
