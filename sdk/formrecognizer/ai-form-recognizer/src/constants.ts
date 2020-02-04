bf875d240ef76376b3b8fcfc3633e4866485773d
Jeremy Meng <jeremy.ymeng@gmail.com>
2020-02-04 23:32:38 +0000

    Adding more source files

---
+++ sdk/formrecognizer/ai-form-recognizer/src/constants.ts
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export const SDK_VERSION: string = "0.2.0-preview.1";
---
+++ sdk/formrecognizer/ai-form-recognizer/src/formRecognizerClient.ts
@@ -0,0 +1,140 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  PipelineOptions,
+  createPipelineFromOptions,
+  signingPolicy,
+  InternalPipelineOptions,
+  isTokenCredential,
+  bearerTokenAuthenticationPolicy,
+  operationOptionsToRequestOptionsBase,
+  OperationOptions
+} from "@azure/core-http";
+import { TokenCredential } from "@azure/identity";
+import { SDK_VERSION } from "./constants";
+import { logger } from "./logger";
+import {
+  GetCustomModelsResponse
+} from "./generated/models";
+import { createSpan } from "./tracing";
+import { CanonicalCode } from "@opentelemetry/types";
+
+import { FormRecognizerClient as GeneratedClient } from "./generated/formRecognizerClient";
+import { FormRecognizerApiKeyCredential } from "./formRecognizerApiKeyCredential";
+
+const DEFAULT_COGNITIVE_SCOPE = "https://cognitiveservices.azure.com/.default";
+
+
+/**
+ * Client options used to configure FormRecognizer API requests.
+ */
+export interface FormRecognizerClientOptions extends PipelineOptions {
+
+}
+
+/**
+ * Options common to all form recognizer operations.
+ */
+export interface FormRecognizerOperationOptions extends OperationOptions {
+}
+
+/**
+ * Options for the detect languages operation.
+ */
+export type ListCustomModelsOptions = FormRecognizerOperationOptions
+
+/**
+ * Client class for interacting with Azure Form Recognizer.
+ */
+export class FormRecognizerClient {
+  /**
+   * The URL to the FormRecognizer endpoint
+   */
+  public readonly endpointUrl: string;
+
+  /**
+   * @internal
+   * @ignore
+   * A reference to the auto-generated FormRecognizer HTTP client.
+   */
+  private readonly client: GeneratedClient;
+
+  /**
+   * Creates an instance of FormRecognizerClient.
+   *
+   * Example usage:
+   * ```ts
+   * import { FormRecognizerClient, FormRecognizerApiKeyCredential } from "@azure/ai-form-recognizer";
+   *
+   * const client = new FormRecognizerClient(
+   *    "<service endpoint>",
+   *    new FormRecognizerApiKeyCredential("<api key>")
+   * );
+   * ```
+   * @param {string} endpointUrl The URL to the FormRecognizer endpoint
+   * @param {TokenCredential | FormRecognizerApiKeyCredential} credential Used to authenticate requests to the service.
+   * @param {FormRecognizerClientOptions} [options] Used to configure the FormRecognizer client.
+   */
+  constructor(
+    endpointUrl: string,
+    credential: TokenCredential | FormRecognizerApiKeyCredential,
+    options: FormRecognizerClientOptions = {}
+  ) {
+    this.endpointUrl = endpointUrl;
+    const { ...pipelineOptions } = options;
+
+    const libInfo = `azsdk-js-ai-formrecognizer/${SDK_VERSION}`;
+    if (!pipelineOptions.userAgentOptions) {
+      pipelineOptions.userAgentOptions = {};
+    }
+    if (pipelineOptions.userAgentOptions.userAgentPrefix) {
+      pipelineOptions.userAgentOptions.userAgentPrefix = `${pipelineOptions.userAgentOptions.userAgentPrefix} ${libInfo}`;
+    } else {
+      pipelineOptions.userAgentOptions.userAgentPrefix = libInfo;
+    }
+
+    const authPolicy = isTokenCredential(credential)
+      ? bearerTokenAuthenticationPolicy(credential, DEFAULT_COGNITIVE_SCOPE)
+      : signingPolicy(credential);
+
+    const internalPipelineOptions: InternalPipelineOptions = {
+      ...pipelineOptions,
+      ...{
+        loggingOptions: {
+          logger: logger.info,
+          allowedHeaderNames: ["x-ms-correlation-request-id", "x-ms-request-id"]
+        }
+      }
+    };
+
+    const pipeline = createPipelineFromOptions(internalPipelineOptions, authPolicy);
+    this.client = new GeneratedClient(credential, this.endpointUrl, pipeline);
+  }
+
+  public async listCustomModels(includingFullList: boolean = true, options?: ListCustomModelsOptions): Promise<GetCustomModelsResponse> {
+
+    const realOptions: ListCustomModelsOptions = options || {};
+    const { span, updatedOptions: finalOptions } = createSpan(
+      "FormRecognizerClient-listCustomModels",
+      realOptions
+    );
+
+    try {
+      const result = await this.client.getCustomModels({
+        ...operationOptionsToRequestOptionsBase(finalOptions),
+        op: includingFullList? "full" : "summary"
+      });
+
+      return result;
+    } catch (e) {
+      span.setStatus({
+        code: CanonicalCode.UNKNOWN,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
+  }
+}
