0493a564f759492286db11996352b558d3406af3
Karishma Ghiya <kaghiya@microsoft.com>
2020-02-05 15:07:14 -0800

    export changelog update function

--- eng/tools/versioning/VersionUtils.js
+++ eng/tools/versioning/VersionUtils.js
@@ -119,3 +119,4 @@ module.exports.writePackageJson = writePackageJson;
 module.exports.getRushSpec = getRushSpec;
 module.exports.updatePackageConstants = updatePackageConstants;
 module.exports.getRushPackageJsons = getRushPackageJsons;
+module.exports.updateChangelog = updateChangelog;
