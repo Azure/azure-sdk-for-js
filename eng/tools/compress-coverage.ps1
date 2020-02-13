d7c22fd734527fc2ccca4e875f9e93ccdd9ba96c
Karishma Ghiya <kaghiya@microsoft.com>
2020-02-13 13:23:10 -0800

    mkdir added

--- eng/tools/compress-coverage.ps1
+++ eng/tools/compress-coverage.ps1
@@ -2,7 +2,13 @@ param (
   $pathToDir,
   $pathToDest
 )
+
+if((-Not (Test-Path $pathToDest))){
+  mkdir $pathToDest
+}
+
 $list = Get-ChildItem $pathToDir -Recurse -Depth 3
+Write-Host "list of code coverage reports = $list"
 Foreach ($q in $list) {
   $dir=(Split-Path (Split-Path $q) -Leaf)
   $dest = Join-path -path $pathToDest -ChildPath "$($dir)-$($q.name).zip"
