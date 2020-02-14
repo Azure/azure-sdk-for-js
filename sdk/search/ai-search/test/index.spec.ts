d19be4fceebd5cae290778bcbbde9e682c537e23
Jeff Fisher <jeffish@microsoft.com>
2020-02-14 14:40:47 -0800

    Add tracing tests

--- sdk/search/ai-search/test/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { assert } from "chai";
-import { SearchIndexClient, SearchApiKeyCredential } from "../src/index";
-
-describe("Hello world", () => {
-  it("should say hello world", () => {
-    const indexClient = new SearchIndexClient("test", "test", new SearchApiKeyCredential("test"));
-    assert.isNotNull(indexClient);
-  });
-});
