b80acdf52d91dfd2e3d902bbb2cfd493bd93f442
Will Temple <witemple@microsoft.com>
2020-02-14 12:12:32 -0800

    Shimmed dom types for @azure/template

--- sdk/template/template/src/print.browser.ts
+++ sdk/template/template/src/print.browser.ts
@@ -1,7 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-/// <reference lib="dom"/>
+// This shim just declares "document" to be defined globally.
+// If you really need access to browser features with strong
+// type-checking, extend the shim to the extent you require or
+// use:
+//
+// /// <reference lib="dom" />
+declare global {
+  const document: any;
+}
 
 export default function(str: string): void {
   document.write(str);
