14afc0f2912eae5ba865f8673bc9f52a0fc8a408
SDK Automation <sdkautomation@microsoft.com>
2020-02-17 02:18:58 +0000

    Generated from fe6355b1f076ccb3a6a36b66bf358fb596881f3c
    remove unneeded params

--- sdk/compute/arm-compute/src/models/parameters.ts
+++ sdk/compute/arm-compute/src/models/parameters.ts
@@ -50,6 +50,18 @@ export const apiVersion2: msRest.OperationQueryParameter = {
     required: true,
     isConstant: true,
     serializedName: "api-version",
+    defaultValue: '2019-12-01',
+    type: {
+      name: "String"
+    }
+  }
+};
+export const apiVersion3: msRest.OperationQueryParameter = {
+  parameterPath: "apiVersion",
+  mapper: {
+    required: true,
+    isConstant: true,
+    serializedName: "api-version",
     defaultValue: '2017-01-31',
     type: {
       name: "String"
