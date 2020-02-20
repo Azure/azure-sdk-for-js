dc652954a95ccefa2f4ea252bfdffa934c9f34ae
Steve Faulkner <southpolesteve@gmail.com>
2020-02-20 11:09:46 -0600

    global -> globalThis

--- sdk/cosmosdb/cosmos/src/utils/digest.browser.ts
+++ sdk/cosmosdb/cosmos/src/utils/digest.browser.ts
@@ -1,14 +1,14 @@
 import { encodeUTF8 } from "./encode";
 
-const _global = typeof self === "undefined" ? window : self;
+const globalThis = typeof self === "undefined" ? window : self;
 
 export async function digest(str: string) {
-  if (!_global || !_global.crypto || !_global.crypto.subtle) {
+  if (!globalThis || !globalThis.crypto || !globalThis.crypto.subtle) {
     throw new Error("Browser does not support cryptography functions");
   }
 
   const data = encodeUTF8(str);
-  const hash = await _global.crypto.subtle.digest("SHA-256", data);
+  const hash = await globalThis.crypto.subtle.digest("SHA-256", data);
   return bufferToHex(hash);
 }
 
--- sdk/cosmosdb/cosmos/src/utils/hmac.browser.ts
+++ sdk/cosmosdb/cosmos/src/utils/hmac.browser.ts
@@ -1,10 +1,10 @@
 import { encodeUTF8, encodeBase64 } from "./encode";
 import atob from "./atob";
 
-const _global = typeof self === "undefined" ? window : self;
+const globalThis = typeof self === "undefined" ? window : self;
 
 export async function hmac(key: string, message: string) {
-  if (!_global || !_global.crypto || !_global.crypto.subtle) {
+  if (!globalThis || !globalThis.crypto || !globalThis.crypto.subtle) {
     throw new Error("Browser does not support cryptography functions");
   }
   const importParams: HmacImportParams = { name: "HMAC", hash: { name: "SHA-256" } };
@@ -12,10 +12,14 @@ export async function hmac(key: string, message: string) {
     [...unescape(encodeURIComponent(message))].map((c) => c.charCodeAt(0))
   );
   const encodedKey = encodeUTF8(atob(key));
-  const cryptoKey = await _global.crypto.subtle.importKey("raw", encodedKey, importParams, false, [
-    "sign"
-  ]);
-  const signature = await _global.crypto.subtle.sign(importParams, cryptoKey, encodedMessage);
+  const cryptoKey = await globalThis.crypto.subtle.importKey(
+    "raw",
+    encodedKey,
+    importParams,
+    false,
+    ["sign"]
+  );
+  const signature = await globalThis.crypto.subtle.sign(importParams, cryptoKey, encodedMessage);
 
   return encodeBase64(signature);
 }
