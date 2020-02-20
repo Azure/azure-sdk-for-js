7a6092ae9cda97e7275aa7dc9f78c8178ec9438b
Daniel Rodríguez <sadasant@users.noreply.github.com>
2020-02-20 17:01:20 -0500

    Update identifier.test.ts

--- sdk/keyvault/keyvault-secrets/test/identifier.test.ts
+++ sdk/keyvault/keyvault-secrets/test/identifier.test.ts
@@ -38,21 +38,4 @@ describe("KeyVault Secrets Identifier", () => {
       })
     );
   });
-
-  it("It should work with a URI of a secret's issuer", async function() {
-    const uri = "https://keyvault-name.vault.azure.net/secrets/issuers/secret-issuer-name";
-    const identifier = new KeyVaultSecretsIdentifier(uri);
-
-    // Assert doesn't have deepEqual on the browser.
-    assert.equal(
-      JSON.stringify(identifier),
-      JSON.stringify({
-        collection: "secrets",
-        id: "https://keyvault-name.vault.azure.net/secrets/issuers/secret-issuer-name",
-        vaultUrl: "https://keyvault-name.vault.azure.net",
-        version: "secret-issuer-name",
-        name: "issuers"
-      })
-    );
-  });
 });
