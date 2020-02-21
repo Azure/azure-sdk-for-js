99c1af6414cc8fe4d20f8191452c9dcc214dcc66
Richard Park <richard.park@microsoft.com>
2020-02-21 09:41:40 -0800

    type union idea

---
+++ sdk/servicebus/service-bus/src/track2/receiverClientConstructorsUsingTypeUnions.ts
@@ -0,0 +1,64 @@
+import { ServiceBusClientReceiverOptions } from "../receiverClient";
+import { NonSessionReceiverClient } from "./receiverClient";
+import { TokenCredential } from "@azure/identity";
+
+export type QueueAuths =
+  | {
+      connectionString: string;
+      queueName: string;
+    }
+  | {
+      queueConnectionString: string;
+    }
+  | {
+      tokenCredential: TokenCredential;
+      host: string;
+    };
+
+export type SubscriptionAuths =
+  | {
+      connectionString: string;
+      topicName: string;
+      subscriptionName: string;
+    }
+  | {
+      topicConnectionString: string;
+      subscriptionName: string;
+    }
+  | {
+      tokenCredential: TokenCredential;
+      host: string;
+      topicName: string;
+      subscriptionName: string;
+    };
+
+export interface SubscriptionRuleManager {
+  addRule(): Promise<void>;
+  removeRule(): Promise<void>;
+}
+
+export interface ReceiverClientWithTypeUnions {
+  // queue constructors
+  new (
+    queueAuthentication: QueueAuths,
+    receiveMode?: "PeekLock",
+    options?: ServiceBusClientReceiverOptions
+  ): NonSessionReceiverClient<"PeekLock">;
+  new (
+    queueAuthentication: QueueAuths,
+    receiveMode: "ReceiveAndDelete",
+    options?: ServiceBusClientReceiverOptions
+  ): NonSessionReceiverClient<"ReceiveAndDelete">;
+
+  // subscription constructors
+  new (
+    subscriptionAuthentication: SubscriptionAuths,
+    receiveMode: "ReceiveAndDelete",
+    options?: ServiceBusClientReceiverOptions
+  ): NonSessionReceiverClient<"ReceiveAndDelete"> & SubscriptionRuleManager;
+  new (
+    subscriptionAuthentication: SubscriptionAuths,
+    receiveMode: "ReceiveAndDelete",
+    options?: ServiceBusClientReceiverOptions
+  ): NonSessionReceiverClient<"ReceiveAndDelete"> & SubscriptionRuleManager;
+}
