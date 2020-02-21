115df18ea89f211017aa5f530757beeef07889ab
SDK Automation <sdkautomation@microsoft.com>
2020-02-21 10:47:58 +0000

    Generated from cce4898730132b8905bcef0bf5230dc55aeb23ad
    Revert "Salesforce add apiVersion and set securityToken optional"
    
    This reverts commit 233cd93f43a9e4e995834b608560316bbab54fbf.

--- sdk/datafactory/arm-datafactory/src/models/pipelinesMappers.ts
+++ sdk/datafactory/arm-datafactory/src/models/pipelinesMappers.ts
@@ -375,7 +375,6 @@ export {
   RedshiftUnloadSettings,
   RelationalSource,
   RelationalTableDataset,
-  RerunTriggerResource,
   RerunTumblingWindowTrigger,
   Resource,
   ResponsysLinkedService,
