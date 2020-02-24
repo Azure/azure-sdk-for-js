85a55ab75734e01b1b12f9112eb7611b03a794c1
Harsha Nalluru <sanallur@microsoft.com>
2020-02-24 13:51:38 -0800

    Code coverage in browsers - leftovers (#7523)

--- sdk/eventhub/event-hubs/karma.conf.js
+++ sdk/eventhub/event-hubs/karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["mocha", "coverage", "remap-coverage", "junit"],
+    reporters: ["mocha", "coverage", "karma-remap-istanbul", "junit"],
 
     coverageReporter: {
       // specify a common output directory
