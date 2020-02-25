f21f994390bf0d6fc6eb4347977e7669c032ee0f
Dongwei Wang <wangdongwei1203@gmail.com>
2020-02-26 07:37:08 +0800

    Update sdk/synapse/synapse/src/synapseClientContext.ts
    Co-Authored-By: chradek <51000525+chradek@users.noreply.github.com>

--- sdk/synapse/synapse/src/synapseClientContext.ts
+++ sdk/synapse/synapse/src/synapseClientContext.ts
@@ -9,7 +9,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/synapse";
-const packageVersion = "1.0.0";
+const packageVersion = "1.0.0-preview.1";
 
 export class SynapseClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
