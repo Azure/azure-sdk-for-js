37ac25c08da5135d1f2781a6dbd2db3aa59ba747
SDK Automation <sdkautomation@microsoft.com>
2020-03-06 16:14:14 +0000

    Generated from 141cce2c654fd9011e7b57664cf8f845c527074c
    change clicommon to cli

--- sdk/keyvault/arm-keyvault/src/keyVaultManagementClientContext.ts
+++ sdk/keyvault/arm-keyvault/src/keyVaultManagementClientContext.ts
@@ -45,7 +45,7 @@ export class KeyVaultManagementClientContext extends msRestAzure.AzureServiceCli
 
     super(credentials, options);
 
-    this.apiVersion = '2018-02-14';
+    this.apiVersion = '2019-09-01';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
