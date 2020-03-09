df1b1bfa73a724973afd013358c5de91d2ff4407
azure-sdk <azuresdk@microsoft.com>
2020-03-09 21:22:15 +0000

    Increment package version after release of azure-search

--- sdk/search/search/src/constants.ts
+++ sdk/search/search/src/constants.ts
@@ -1,4 +1,4 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const SDK_VERSION: string = "11.0.0-preview.1";
+export const SDK_VERSION: string = "11.0.0-preview.2";
