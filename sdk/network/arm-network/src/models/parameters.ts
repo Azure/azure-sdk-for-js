8823ae1a619518c3a36ed8094b35fd3f9a63ad6a
SDK Automation <sdkautomation@microsoft.com>
2020-03-24 04:36:13 +0000

    Generated from d0f7a23c47cf836f0fac554521aea64f800c09b0
    Add 2019-12 api-version of network to go SDK generation

--- sdk/network/arm-network/src/models/parameters.ts
+++ sdk/network/arm-network/src/models/parameters.ts
@@ -774,6 +774,16 @@ export const virtualHubName: msRest.OperationURLParameter = {
     }
   }
 };
+export const virtualmachineIndex: msRest.OperationURLParameter = {
+  parameterPath: "virtualmachineIndex",
+  mapper: {
+    required: true,
+    serializedName: "virtualmachineIndex",
+    type: {
+      name: "String"
+    }
+  }
+};
 export const virtualMachineScaleSetName: msRest.OperationURLParameter = {
   parameterPath: "virtualMachineScaleSetName",
   mapper: {
@@ -849,16 +859,6 @@ export const virtualWANName1: msRest.OperationURLParameter = {
   mapper: {
     required: true,
     serializedName: "VirtualWANName",
-    type: {
-      name: "String"
-    }
-  }
-};
-export const virtualmachineIndex: msRest.OperationURLParameter = {
-  parameterPath: "virtualmachineIndex",
-  mapper: {
-    required: true,
-    serializedName: "virtualmachineIndex",
     type: {
       name: "String"
     }
