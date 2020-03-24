cdb8eb5f8fe8f7b3c69521d58e89d8cc28a57083
Richard Park <richard.park@microsoft.com>
2020-03-23 18:01:29 -0700

    Everything uses DefaultAzureCredential now and we only need a single sample for that.

--- sdk/servicebus/service-bus/samples/javascript/servicePrincipalLogin.js
+++ sdk/servicebus/service-bus/samples/javascript/servicePrincipalLogin.js
@@ -1,25 +1,25 @@
 /*
-Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the MIT Licence.
+  Copyright (c) Microsoft Corporation. All rights reserved.
+  Licensed under the MIT Licence.
 
-This sample demonstrates how to create a namespace using AAD token credentials
-obtained from using Service Principal Secrets.
+  This sample demonstrates how to create a namespace using AAD token credentials
+  obtained from using Service Principal Secrets.
 
-Setup :
-Please ensure that your Azure Service Bus resource is in US East, US East 2, or West Europe
-region. AAD Role Based Access Control is not supported in other regions yet.
+  Setup :
+    Please ensure that your Azure Service Bus resource is in US East, US East 2, or West Europe
+    region. AAD Role Based Access Control is not supported in other regions yet.
 
-Register a new application in AAD and assign the "Azure Service Bus Data Owner" role to it
-- See https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app
-to register a new application in the Azure Active Directory.
-- Note down the CLIENT_ID and TENANT_ID from the above step.
-- In the "Certificates & Secrets" tab, create a secret and note that down.
-- In the Azure portal, go to your Service Bus resource and click on the Access control (IAM)
-tab. Here, assign "Azure Service Bus Data Owner" role to the registered application.
+    Register a new application in AAD and assign the "Azure Service Bus Data Owner" role to it
+     - See https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app
+       to register a new application in the Azure Active Directory.
+     - Note down the CLIENT_ID and TENANT_ID from the above step.
+     - In the "Certificates & Secrets" tab, create a secret and note that down.
+     - In the Azure portal, go to your Service Bus resource and click on the Access control (IAM)
+       tab. Here, assign "Azure Service Bus Data Owner" role to the registered application.
 */
 
 const { ServiceBusClient } = require("@azure/service-bus");
-const { loginWithServicePrincipalSecret } = require("@azure/ms-rest-nodeauth");
+const { DefaultAzureCredential } = require("@azure/identity");
 
 // Load the .env file if it exists
 require("dotenv").config();
@@ -32,17 +32,14 @@ const serviceBusEndpoint =
 const clientId = process.env.AZURE_TENANT_ID || "<azure tenant id>";
 const clientSecret = process.env.AZURE_CLIENT_SECRET || "<azure client secret>";
 const tenantId = process.env.AZURE_CLIENT_ID || "<azure client id>";
-
 async function main() {
-  const tokenCreds = await loginWithServicePrincipalSecret(clientId, clientSecret, tenantId, {
-    tokenAudience: "https://servicebus.azure.net/"
-  });
+  const tokenCreds = new DefaultAzureCredential();
 
-  const sbClient = ServiceBusClient.createFromAadTokenCredentials(serviceBusEndpoint, tokenCreds);
+  const sbClient = new ServiceBusClient(serviceBusEndpoint, tokenCreds);
   /*
-Refer to other samples, and place your code here
-to create queue clients, and send/receive messages
-*/
+     Refer to other samples, and place your code here
+     to create queue clients, and send/receive messages
+    */
   await sbClient.close();
 }
 
