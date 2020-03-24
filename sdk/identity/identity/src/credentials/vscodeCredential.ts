0c4a0c24355de74f86b384fcbb8b39b2b6577f16
Jonathan Turner <joturner@microsoft.com>
2020-03-25 12:08:22 +1300

    Finish up VSCode credential and add to DAC. Add user-defined managed to DAC.

--- sdk/identity/identity/src/credentials/vscodeCredential.ts
+++ sdk/identity/identity/src/credentials/vscodeCredential.ts
@@ -4,23 +4,44 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { TokenCredential, GetTokenOptions, AccessToken } from "@azure/core-http";
-import { TokenCredentialOptions } from '../client/identityClient';
+import { TokenCredentialOptions, IdentityClient } from '../client/identityClient';
 import * as keytar from 'keytar';
 
+const commonTenantId = 'common';
+const clientId = 'aebc6443-996d-45c2-90f0-388ff96faa56'; // VSC: 'aebc6443-996d-45c2-90f0-388ff96faa56'
+
 export class VSCodeCredential implements TokenCredential {
+  private identityClient: IdentityClient;
+
   constructor(
     options?: TokenCredentialOptions
   ) {
-    //throw BrowserNotSupportedError;
+    this.identityClient = new IdentityClient();
   }
 
   public async getToken(
     scopes: string | string[],
     options?: GetTokenOptions
   ): Promise<AccessToken | null> {
-    let token = await keytar.findPassword("VS Code Azure");
-    if (token) {
-      return { token, expiresOnTimestamp: Date.now() + 2 * 60 * 1000 }
+    let scopeString = typeof scopes === "string" ? scopes : scopes.join(" ");
+    if (scopeString.indexOf("offline_access") < 0) {
+      scopeString += " offline_access";
+    }    
+    let refreshToken = await keytar.findPassword("VS Code Azure");
+    if (refreshToken) {
+      let tokenResponse = await this.identityClient.refreshAccessToken(
+          commonTenantId,
+          clientId,
+          scopeString,
+          refreshToken,
+          undefined
+        );
+      
+      if (tokenResponse) {
+        return tokenResponse.accessToken;
+      } else {
+        return null;
+      }
     } else {
       return null;
     }
