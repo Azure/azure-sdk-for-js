8eb19fceb3fa3b953d1782891b807d19ece6ad2a
HarshaNalluru <sanallur@microsoft.com>
2020-03-25 14:22:02 -0700

    add getRetryAttemptTimeoutInMs in core-amqp index.ts

--- sdk/core/core-amqp/src/index.ts
+++ sdk/core/core-amqp/src/index.ts
@@ -4,7 +4,14 @@
 /// <reference lib="es2015" />
 
 export { RequestResponseLink, SendRequestOptions } from "./requestResponseLink";
-export { retry, RetryOptions, RetryConfig, RetryOperationType, RetryMode } from "./retry";
+export {
+  retry,
+  RetryOptions,
+  RetryConfig,
+  RetryOperationType,
+  RetryMode,
+  getRetryAttemptTimeoutInMs
+} from "./retry";
 export { DataTransformer, DefaultDataTransformer } from "./dataTransformer";
 export { TokenType } from "./auth/token";
 export { AccessToken, TokenCredential, isTokenCredential } from "@azure/core-auth";
