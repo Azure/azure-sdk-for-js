d69a78bca4b4b7e811d567bb8d12dd816e6c03d6
HarshaNalluru <sanallur@microsoft.com>
2020-03-25 15:52:13 -0700

    remove .only - revert changes to the tests

--- sdk/servicebus/service-bus/test/sendBatch.spec.ts
+++ sdk/servicebus/service-bus/test/sendBatch.spec.ts
@@ -125,7 +125,7 @@ describe("Send Batch", () => {
     });
   });
 
-  describe.only("Send multiple homogeneous messages - Multiple Sessions - size > max_batch_size_allowed", function(): void {
+  describe("Send multiple homogeneous messages - Multiple Sessions - size > max_batch_size_allowed", function(): void {
     afterEach(async () => {
       await afterEachTest();
     });
@@ -181,23 +181,15 @@ describe("Send Batch", () => {
       await testSendBatch(true);
     });
     */
-    for (let iter = 0; iter < 100; iter++) {
-      it(`Iteration #${iter} - Unpartitioned Queue with Sessions: SendBatch`, async function(): Promise<
-        void
-      > {
-        await beforeEachTest(TestClientType.UnpartitionedQueueWithSessions);
-        await testSendBatch(true);
-      });
-    }
+    it("Unpartitioned Queue with Sessions: SendBatch", async function(): Promise<void> {
+      await beforeEachTest(TestClientType.UnpartitionedQueueWithSessions);
+      await testSendBatch(true);
+    });
 
-    for (let iter = 0; iter < 100; iter++) {
-      it(`Iteration #${iter} - Unpartitioned Topic with Sessions: SendBatch`, async function(): Promise<
-        void
-      > {
-        await beforeEachTest(TestClientType.UnpartitionedSubscriptionWithSessions);
-        await testSendBatch(true);
-      });
-    }
+    it("Unpartitioned Topic with Sessions: SendBatch", async function(): Promise<void> {
+      await beforeEachTest(TestClientType.UnpartitionedSubscriptionWithSessions);
+      await testSendBatch(true);
+    });
   });
 
   describe("Send multiple homogeneous messages - size < max_batch_size_allowed", function(): void {
