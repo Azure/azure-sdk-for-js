2a69118901c582ab4851693e80868db1e49447f9
SDK Automation <sdkautomation@microsoft.com>
2020-03-26 11:28:31 +0000

    Generated from f31b3094d636cd11b16647f38137faef5fff7524
    Really fix prettier errors.

--- sdk/storage/arm-storage/src/storageManagementClient.ts
+++ sdk/storage/arm-storage/src/storageManagementClient.ts
@@ -24,6 +24,7 @@ class StorageManagementClient extends StorageManagementClientContext {
   managementPolicies: operations.ManagementPolicies;
   privateEndpointConnections: operations.PrivateEndpointConnections;
   privateLinkResources: operations.PrivateLinkResources;
+  encryptionScopes: operations.EncryptionScopes;
   blobServices: operations.BlobServices;
   blobContainers: operations.BlobContainers;
   fileServices: operations.FileServices;
@@ -44,6 +45,7 @@ class StorageManagementClient extends StorageManagementClientContext {
     this.managementPolicies = new operations.ManagementPolicies(this);
     this.privateEndpointConnections = new operations.PrivateEndpointConnections(this);
     this.privateLinkResources = new operations.PrivateLinkResources(this);
+    this.encryptionScopes = new operations.EncryptionScopes(this);
     this.blobServices = new operations.BlobServices(this);
     this.blobContainers = new operations.BlobContainers(this);
     this.fileServices = new operations.FileServices(this);
