50d2aeba002e8b0824f3e511a4c160ca4ab09e1f
Nourhan Farid <a-nofari@microsoft.com>
2020-04-06 15:01:31 +0200

    Adding tests for (import luis app  v2 , luis app in lu format ) and (export luis app in lu format)

--- sdk/cognitiveservices/cognitiveservices-luis-authoring/test/Luis/import_export.test.ts
+++ sdk/cognitiveservices/cognitiveservices-luis-authoring/test/Luis/import_export.test.ts
@@ -27,6 +27,22 @@ describe("Import and Export Tests", () => {
     });
   });
 
+  it("should export version Lu format", async () => {
+      await BaseTest.useClientFor(async (client: LUISAuthoringClient) => {
+        const appId = await client.apps.add({
+          name: "LUIS App to be exported in Lu Format",
+          description: "New LUIS App to be exported in Lu",
+          culture: "en-us",
+          domain: "Comics",
+          usageScenario: "IoT"
+        });
+        const luisApp = await client.versions.exportLuFormat(appId.body, "0.1");
+        await client.apps.deleteMethod(appId.body);
+        chai.expect(luisApp).to.exist;
+        chai.expect(luisApp.readableStreamBody.read.toString).to.contains("LUIS App to be exported in Lu Format");
+      });
+    });
+
   it("should import version", async () => {
     await BaseTest.useClientFor(async (client: LUISAuthoringClient) => {
       fs.readFile("test/SessionRecords/ImportApp.json", "utf-8", async (err, data) => {
@@ -61,6 +77,79 @@ describe("Import and Export Tests", () => {
         var testApp = await client.apps.get(testAppId.body);
         await client.apps.deleteMethod(testAppId.body);
         chai.expect(testApp).to.exist;
+      });
+    });
+  });
+
+  it("should import application in Lu format", async () => {
+    await BaseTest.useClientFor(async (client: LUISAuthoringClient) => {
+      fs.readFile("test/SessionRecords/ImportLuApp.json", "utf-8", async (err, app) => {
+        if (err) {
+          throw err;
+        }     
+        var testAppId = await client.apps.importLuFromat(app, { appName: "Test Import LUIS App in Lu format" });
+        var testApp = await client.apps.get(testAppId.body);
+        await client.apps.deleteMethod(testAppId.body);
+        chai.expect(testApp).to.exist;
+      });
+    });
+  });
+   
+  it("should import version in Lu format", async () => {
+    await BaseTest.useClientFor(async (client: LUISAuthoringClient) => {
+      fs.readFile("test/SessionRecords/ImportLuApp.json", "utf-8", async (err, app) => {
+        if (err) {
+          throw err;
+        }
+        const testAppId = await client.apps.add({
+          name: "LUIS App to be exported",
+          description: "New LUIS App",
+          culture: "en-us",
+          domain: "Comics",
+          usageScenario: "IoT"
+        });
+
+        const newVersion = await client.versions.importLuFormat(testAppId.body, app, { versionId: "0.2" });
+        await client.apps.deleteMethod(testAppId.body);
+
+        chai.expect(newVersion.body, "0.2");
+      });
+    });
+  });
+
+  it("should import v2 application", async () => {
+    await BaseTest.useClientFor(async (client: LUISAuthoringClient) => {
+      fs.readFile("test/SessionRecords/ImportV2App.json", "utf-8", async (err, data) => {
+        if (err) {
+          throw err;
+        }     
+        let app = JSON.parse(data);
+        var testAppId = await client.apps.importV2App(app, { appName: "Test Import V2 LUIS App" });
+        var testApp = await client.apps.get(testAppId.body);
+        await client.apps.deleteMethod(testAppId.body);
+        chai.expect(testApp).to.exist;
+      });
+    });
+  });
+
+  it("should import v2 version", async () => {
+    await BaseTest.useClientFor(async (client: LUISAuthoringClient) => {
+      fs.readFile("test/SessionRecords/ImportV2App.json", "utf-8", async (err, data) => {
+        if (err) {
+          throw err;
+        }     
+        var testAppId = await client.apps.add({
+          name: "LUIS App to be exported",
+          description: "New LUIS App",
+          culture: "en-us",
+          domain: "Comics",
+          usageScenario: "IoT"
+        });
+        let app = JSON.parse(data);
+        const newVersion = await client.versions.importV2App(testAppId.body, app, { appName: "Test Import V2 LUIS App" });
+        var newVersionApp = await client.apps.get(newVersion.body);
+        await client.apps.deleteMethod(newVersion.body);
+        chai.expect(newVersionApp).to.exist;
       });
     });
   });
