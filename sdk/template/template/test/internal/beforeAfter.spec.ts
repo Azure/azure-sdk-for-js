98a0f10f8ef8bb81b48763feba37836f9a95e75f
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-04-09 17:08:12 +0000

    Removed discouraged example

--- sdk/template/template/test/internal/beforeAfter.spec.ts
+++ sdk/template/template/test/internal/beforeAfter.spec.ts
@@ -9,34 +9,6 @@ describe("before, beforeEach, after and afterEach examples", function() {
   // We recommend using `beforeEach` rather than `before`,
   // just as much as we recommend using `afterEach` rather than `after`.
   // The idea is that each test case should not depend on a state that is shared with other tests.
-  describe.skip("Discouraged example of `before` and `after`", function() {
-    let state: {
-      properties?: any;
-    } = {};
-    let client: InternalClass;
-
-    before(() => {
-      state = {
-        properties: {}
-      };
-
-      // The recorder needs to be initialized before the clients are created,
-      // so assume the recorder is being initialized here.
-
-      client = new InternalClass();
-      // And other global setups...
-    });
-
-    after(() => {
-      delete state.properties;
-      // And other global cleanups...
-    });
-
-    it("A test for the discouraged example of `before`", function() {
-      assert.exists(state.properties);
-      assert.exists(client);
-    });
-  });
 
   // Use `beforeEach` to execute tasks that will prepare the resources needed
   // for each test to run cleanly, and `afterEach` to tear down or clean
