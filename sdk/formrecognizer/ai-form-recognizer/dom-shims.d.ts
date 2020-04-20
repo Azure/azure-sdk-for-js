92f778f66743178acddeb476be5345b05c815ee2
Jeremy Meng <jeremy.ymeng@gmail.com>
2020-04-20 12:01:21 -0700

    Apply fix from Will

--- sdk/formrecognizer/ai-form-recognizer/dom-shims.d.ts
+++ sdk/formrecognizer/ai-form-recognizer/dom-shims.d.ts
@@ -6,24 +6,4 @@
 // d.ts shims provide types for things we use internally but are not part
 // of this package's surface area.
 
-interface RequestInit {}
-
-interface RequestInfo {}
-
-interface Response {}
-
-interface Headers {}
-
-interface FileReader {
-  onloadend: ((this: FileReader, ev: any) => any) | null;
-  readonly result: string | ArrayBuffer | null;
-  readAsArrayBuffer(blob: Blob): void;
-}
-
-declare var FileReader: {
-  prototype: FileReader;
-  new (): FileReader;
-  readonly DONE: number;
-  readonly EMPTY: number;
-  readonly LOADING: number;
-};
+interface FileReader {}
