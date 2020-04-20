578f469c778be1a762721819e83a78ea21153ad0
Zach foster <notzachfoster@gmail.com>
2020-04-20 15:52:05 -0400

    Adds shim comment re: TS 3.5 omit

--- sdk/cosmosdb/cosmos/src/client/Container/ContainerRequest.ts
+++ sdk/cosmosdb/cosmos/src/client/Container/ContainerRequest.ts
@@ -5,7 +5,7 @@ import { PartitionKeyDefinition } from "../../documents";
 import { VerboseOmit } from "../../utils/types";
 
 export interface ContainerRequest extends VerboseOmit<ContainerDefinition, "partitionKey"> {
-  /** Throughput for this container. */
+  /* Throughput for this container. */
   throughput?: number;
   partitionKey?: string | PartitionKeyDefinition;
 }
