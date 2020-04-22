3ce7fe678b17c30bcbd4f948ee35631edcff795b
Mike Harder <mharder@microsoft.com>
2020-04-22 16:33:02 -0700

    Remove whitespace

--- sdk/test-utils/perfstress/test/sleep.spec.ts
+++ sdk/test-utils/perfstress/test/sleep.spec.ts
@@ -6,7 +6,6 @@ import { delay } from "@azure/core-http";
 
 // Used for verifying the perf framework correctly computes average throughput across parallel tests of different speed
 export class SleepTest extends PerfStressTest<string> {
-
   private static instanceCount: number = 0;
   private secondsPerOperation: number = 0;
 
