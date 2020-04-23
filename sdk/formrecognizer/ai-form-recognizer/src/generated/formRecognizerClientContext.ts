42313d23c8cc37d3e0a3cb2ef8bf7407742b04d2
azure-sdk <azuresdk@microsoft.com>
2020-04-23 19:34:02 +0000

    Increment package version after release of azure-ai-form-recognizer

--- sdk/formrecognizer/ai-form-recognizer/src/generated/formRecognizerClientContext.ts
+++ sdk/formrecognizer/ai-form-recognizer/src/generated/formRecognizerClientContext.ts
@@ -10,7 +10,7 @@ import * as coreHttp from "@azure/core-http";
 import * as Models from "./models";
 
 const packageName = "@azure/ai-form-recognizer";
-const packageVersion = "1.0.0-preview.1";
+const packageVersion = "1.0.0-preview.2";
 
 export class FormRecognizerClientContext extends coreHttp.ServiceClient {
   endpoint: string;
