7ed253e459d4bb383f03abd172598852793c91a6
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-04-24 21:59:57 +0000

    [KeyVault-Keys] Eslint should be good now

--- sdk/keyvault/keyvault-keys/test/utils/testAuthentication.ts
+++ sdk/keyvault/keyvault-keys/test/utils/testAuthentication.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { ClientSecretCredential } from "@azure/identity";
 import { getKeyvaultName } from "./utils.common";
 import { KeyClient } from "../../src";
