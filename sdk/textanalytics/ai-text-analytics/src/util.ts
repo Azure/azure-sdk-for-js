7b00d60ab664c07456e6edd4e1396f18aa8fd307
Will Temple <witemple@microsoft.com>
2020-05-01 09:56:20 -0700

    Rework sorting logic

--- sdk/textanalytics/ai-text-analytics/src/util.ts
+++ sdk/textanalytics/ai-text-analytics/src/util.ts
@@ -1,8 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { TextAnalyticsError } from "./generated/models";
+
+import { logger } from "./logger";
+
 export interface IdObject {
   id: string;
+}
+
+function isGenericError(o: IdObject): o is IdObject & { error: TextAnalyticsError } {
+  return o.id === "";
 }
 
 /**
@@ -16,13 +24,20 @@ export function sortByPreviousIdOrder<T extends IdObject, U extends IdObject>(
   sortedArray: T[],
   unsortedArray: U[]
 ): U[] {
-  if (sortedArray.length !== unsortedArray.length) {
-    throw new Error("Can't use this sort unless you have two arrays with the same length!");
-  }
-
   const unsortedMap = new Map<string, U>();
   for (const item of unsortedArray) {
+    if (isGenericError(item)) {
+      // This item is a generic error, so we will throw it
+      throw item.error;
+    }
     unsortedMap.set(item.id, item);
+  }
+
+  if (unsortedArray.length !== sortedArray.length) {
+    const ordinal = unsortedArray.length > sortedArray.length ? "more" : "fewer";
+    logger.warning(
+      `The service returned ${ordinal} responses than inputs. Some errors may be treated as fatal. Proceeding.`
+    );
   }
 
   return sortedArray.map((item) => unsortedMap.get(item.id)!);
