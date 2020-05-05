e37ef4a8236f9e2e9452d056a170c29804da8b61
Jeremy Meng <yumeng@microsoft.com>
2020-05-05 14:02:50 -0700

    [FormRecognizer] Fix TypeError for missing fields (#8703)
    and several minor issues.
    
    There are cases where the custom form recognition result from the
    service contains fields with null value:
    
    - forms optional fields
    
    - when passing a form document that has a type different from training
    form documents.
    
    This change handles missing fields properly by create a field with
    only `name` field in the recognized forms.
    
    This PR also fixes several minor issues for preview.2
    
    - include a version update that were missed in `build:autorest`
    script. The generated code was already using correct version of
    `1.0.0-preview.2`.
    
    - Use correct form type for models from supervised training
    
    * Minor fixes
    
    * Add `name` property for array/object for consistency
    
    * run `rushx format`

--- sdk/formrecognizer/ai-form-recognizer/test/node/formtrainingclient.spec.ts
+++ sdk/formrecognizer/ai-form-recognizer/test/node/formtrainingclient.spec.ts
@@ -8,7 +8,10 @@ import path from "path";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { createRecordedTrainingClient, createRecordedRecognizerClient } from "../util/recordedClients";
+import {
+  createRecordedTrainingClient,
+  createRecordedRecognizerClient
+} from "../util/recordedClients";
 import {
   FormRecognizerClient,
   AzureKeyCredential,
@@ -27,7 +30,7 @@ describe("FormTrainingClient NodeJS only", () => {
   let recorder: Recorder;
 
   beforeEach(function() {
-    ({recorder, client: trainingClient} = createRecordedTrainingClient(this));
+    ({ recorder, client: trainingClient } = createRecordedTrainingClient(this));
     trainingClient = new FormTrainingClient(
       env.FORM_RECOGNIZER_ENDPOINT,
       new AzureKeyCredential(env.FORM_RECOGNIZER_API_KEY)
@@ -38,7 +41,7 @@ describe("FormTrainingClient NodeJS only", () => {
     if (recorder) {
       recorder.stop();
     }
-  })
+  });
 
   const expectedDocumentInfo: TrainingDocumentInfo = {
     documentName: "Form_1.jpg",
@@ -85,7 +88,7 @@ describe("FormTrainingClient NodeJS only", () => {
 
     assert.ok(response!.models && response!.models.length > 0, "Expected non empty sub model list");
     const model = response!.models![0];
-    assert.equal(model.formType, "TBD");
+    assert.equal(model.formType, `form-${response!.modelId}`);
     assert.equal(model.accuracy, 0.973);
     assert.ok(model.fields["Signature"], "Expecting field with name 'Signature' to be valid");
 
@@ -208,14 +211,14 @@ describe("FormRecognizerClient form recognition NodeJS", () => {
   let recorder: Recorder;
 
   beforeEach(function() {
-    ({recorder, client: recognizerClient} = createRecordedRecognizerClient(this));
+    ({ recorder, client: recognizerClient } = createRecordedRecognizerClient(this));
   });
 
   afterEach(function() {
     if (recorder) {
       recorder.stop();
     }
-  })
+  });
 
   it("recognizes form jpeg unlabeled model", async () => {
     const filePath = path.join(ASSET_PATH, "forms", "Form_1.jpg");
@@ -338,5 +341,56 @@ describe("FormRecognizerClient form recognition NodeJS", () => {
     assert.ok(form.fields["CompanyPhoneNumber"]);
     assert.ok(form.fields["CompanyName"]);
     assert.ok(form.fields["Signature"]);
+  });
+
+  it("handles missing fields", async () => {
+    const filePath = path.join(ASSET_PATH, "forms", "Invoice_1.pdf");
+    const stream = fs.createReadStream(filePath);
+
+    assert.ok(labeledModelId, "Expecting valid model id from training without labels");
+    const poller = await recognizerClient.beginRecognizeForms(labeledModelId!, stream);
+    await poller.pollUntilDone();
+    const response = poller.getResult();
+
+    assert.ok(response, "Expect valid response object");
+    assert.equal(response!.status, "succeeded");
+    assert.ok(
+      response!.forms && response!.forms.length > 0,
+      `Expect no-empty pages but got ${response!.forms}`
+    );
+    const form = response!.forms![0];
+    assert.equal(form.formType, "custom:form");
+    assert.deepStrictEqual(form.pageRange, {
+      firstPageNumber: 1,
+      lastPageNumber: 1
+    });
+    assert.ok(form.pages.length > 0, "Expecting at least one page in the first recognized form");
+    assert.ok(form.fields);
+    assert.ok(form.fields["Merchant"], "Expecting valid 'Merchant' field");
+    assert.equal(
+      form.fields["Merchant"].confidence,
+      undefined,
+      "Expecting 'Merchant' field has undefined confidence"
+    );
+    assert.equal(
+      form.fields["Merchant"].fieldLabel,
+      undefined,
+      "Expecting 'Merchant' field has undefined fieldLabel"
+    );
+    assert.equal(
+      form.fields["Merchant"].value,
+      undefined,
+      "Expecting 'Merchant' field has undefined value"
+    );
+    assert.equal(
+      form.fields["Merchant"].valueText,
+      undefined,
+      "Expecting 'Merchant' field has undefined valueText"
+    );
+    assert.equal(
+      form.fields["Merchant"].valueType,
+      undefined,
+      "Expecting 'Merchant' field has undefined valueType"
+    );
   });
 }).timeout(60000);
