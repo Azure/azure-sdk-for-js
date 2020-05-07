3fadf07bd7d643e1fd0b2dbe1ced121689b84ae9
Steve Faulkner <southpolesteve@gmail.com>
2020-05-07 11:26:14 -0500

    Casing

--- sdk/cosmosdb/cosmos/src/utils/globalCrypto.ts
+++ sdk/cosmosdb/cosmos/src/utils/globalCrypto.ts
@@ -1,10 +1,10 @@
-const _globalThis: any = typeof self === "undefined" ? window : self;
+const globalRef: any = typeof self === "undefined" ? window : self;
 
 if (!globalThis) {
   throw new Error("Could not find global");
 }
 
-const globalCrypto: Crypto = _globalThis.crypto || _globalThis.msCrypto;
+const globalCrypto: Crypto = globalRef.crypto || globalRef.msCrypto;
 
 if (!globalCrypto || globalCrypto.subtle) {
   throw new Error("Browser does not support cryptography functions");
