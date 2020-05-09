ab15890e0a17705cc000a3197e5ab98afad91396
Jeff Fisher <jeffish@microsoft.com>
2020-05-08 17:58:56 -0700

    [core-http] [v2] Initial implementation of new Pipeline model (#8712)
    Add the new Pipeline and some basic tests.
    
    Next step: Make it able to create http requests and start porting over policies!

--- sdk/core/core-https/src/index.ts
+++ sdk/core/core-https/src/index.ts
@@ -1,6 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export function helloWorld(): string {
-  return "Hello world!";
-}
+export { HttpsClient, PipelineRequest, PipelineResponse, SendRequest } from "./interfaces";
+export {
+  AddPolicyOptions as AddPipelineOptions,
+  PipelinePhase,
+  PipelinePolicy,
+  Pipeline
+} from "./pipeline";
