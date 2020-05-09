2adbb0e817385ffe3e4fccf6e062ef0a2e2bf8fe
Lin Jian <ljian3377@gmail.com>
2020-05-09 21:58:16 +0800

    IReadable

---
+++ sdk/storage/storage-internal-avro/src/AvroReader.ts
@@ -0,0 +1,144 @@
+import { IReadable } from "./IReadable";
+
+interface Metadata {
+  /**
+   * A name-value pair.
+   */
+  [propertyName: string]: string;
+}
+
+export class AvroReader {
+  private readonly _dataStream: IReadable;
+
+  private readonly _headerStream: IReadable;
+
+  private _syncMarker: Uint8Array | undefined;
+
+  private _metadata: Metadata | undefined;
+
+  private _itemType: AvroType | undefined;
+
+  private _itemsRemainingInBlock: number | undefined;
+
+  private _blockOffset: number;
+  public get blockOffset(): number {
+    return this._blockOffset;
+  }
+
+  private _objectIndex: number;
+  public get objectIndex(): number {
+    return this._objectIndex;
+  }
+
+  private _initialized: boolean;
+
+  constructor(
+    dataStream: Readable
+  );
+
+  constructor(
+    dataStream: Readable,
+    headerStream: Readable,
+    currentBlockOffset: number,
+    indexWithinCurrentBlock: number
+  );
+
+  constructor(
+    dataStream: Readable,
+    headerStream?: Readable,
+    currentBlockOffset?: number,
+    indexWithinCurrentBlock?: number
+  ) {
+    this._dataStream = dataStream;
+    this._headerStream = headerStream || dataStream;
+    this._initialized = false;
+    this._blockOffset = currentBlockOffset || 0;
+    this._objectIndex = indexWithinCurrentBlock || 0;
+  }
+
+  // TODO: cancellation / aborter?
+  private async initialize() {
+    const header = await AvroParser.readFixedBytes(this._headerStream, AvroConstants.InitBytes.Length);
+    if (header == AvroConstants.InitBytes) {
+      throw new Error("Stream is not an Avro file.");
+    }
+
+    // File metadata is written as if defined by the following map schema:
+    // { "type": "map", "values": "bytes"}
+    this._metadata = await AvroParser.readMap(this._headerStream, AvroParser.readString);
+
+    // Validate codec
+    const codec = this._metadata![AvroConstants.CodecKey];
+    if (!(codec == undefined || codec == "null")) {
+      throw new Error("Codecs are not supported");
+    }
+
+    // The 16-byte, randomly-generated sync marker for this file.
+    this._syncMarker = await AvroParser.readFixedBytes(this._headerStream, AvroConstants.SyncMarkerSize);
+
+    // Parse the schema
+    const schema = JSON.parse(this._metadata![AvroConstants.SchemaKey]);
+    this._itemType = AvroType.fromSchema(schema.RootElement);
+
+    if (this._blockOffset == 0) {
+      this._blockOffset = this._dataStream.
+    }
+
+    // Populate _itemsRemainingInCurrentBlock
+    this._itemsRemainingInBlock = await AvroParser.readLong(this._dataStream);
+
+    // skip block length
+    await AvroParser.readLong(this._dataStream);
+
+    this._initialized = true;
+
+    if (this._objectIndex && this._objectIndex > 0) {
+      for (let i = 0; i < this._objectIndex; i++) {
+        await this._itemType.Read(this._dataStream);
+        this._itemsRemainingInBlock!--;
+      }
+    }
+  }
+
+  public hasNext(): boolean {
+    return !this._initialized || this._itemsRemainingInBlock! > 0;
+  }
+
+  public async *parseObjects(): AsyncIterableIterator<Object> {
+    if (!this._initialized) {
+      await this.initialize();
+    }
+
+    while (this.hasNext) {
+      const result = await this._itemType.read(this._dataStream);
+
+      this._itemsRemainingInBlock!--;
+      this._objectIndex!++;
+
+      if (this._itemsRemainingInBlock == 0) {
+        const marker = await AvroParser.readFixedBytes(this._dataStream, 16);
+
+        BlockOffset = _dataStream.Position;
+        this._objectIndex = 0;
+
+        if (!this._syncMarker == marker) {
+          throw new Error("Stream is not a valid Avro file.");
+        }
+
+        try {
+          this._itemsRemainingInBlock = await AvroParser.readLong(this._dataStream);
+        }
+        catch (err) {
+          // We hit the end of the stream.
+          this._itemsRemainingInBlock = 0;
+        }
+
+        if (this._itemsRemainingInBlock! > 0) {
+          // Ignore block size
+          await AvroParser.readLong(this._dataStream);
+        }
+      }
+      yield result;
+    }
+  }
+}
---
+++ sdk/storage/storage-internal-avro/src/IReadable.ts
@@ -0,0 +1,92 @@
+export abstract class IReadable {
+  public abstract get position(): number;
+  public abstract async read(size: number): Promise<Uint8Array>;
+}
+
+
+import { Readable } from "stream";
+export class ReadableFromStream extends IReadable {
+  private _position: number;
+  private _readable: Readable;
+  // private _stillReadable: boolean;
+
+  constructor(readable: Readable) {
+    super();
+    this._readable = readable;
+    this._position = 0;
+
+    // workaround due to Readable.readable only availabe after Node.js v11.4
+    // this._stillReadable = true;
+    // this._readable.on("end", () => {
+    //   this._stillReadable = false;
+    // });
+    // this._readable.on("error", () => {
+    //   this._stillReadable = false;
+    // });
+  }
+
+  public get position(): number {
+    return this._position;
+  }
+
+  public async read(size: number): Promise<Uint8Array> {
+    // readable is true if it is safe to call readable.read(), which means the stream has not been destroyed or emitted 'error' or 'end'.
+    // if (!this._stillReadable || this._readable.destroyed) {
+    if (!this._readable.readable) {
+      throw Error("Stream no longer readable.");
+    }
+
+    // See if there is already enough data, note that "Only after readable.read() returns null, 'readable' will be emitted."
+    let chunk = this._readable.read(size);
+    if (chunk) {
+      this._position += chunk.length;
+      // chunk.lenght maybe less than desired size if the stream ends.
+      return chunk;
+    } else {
+      // register callback to wait for enough data to read
+      return new Promise((resolve, reject) => {
+        const callback = () => {
+          let chunk = this._readable.read(size);
+          if (chunk) {
+            this._position += chunk.length;
+            // chunk.lenght maybe less than desired size if the stream ends.
+            resolve(chunk);
+            this._readable.removeListener('readable', callback);
+          }
+        }
+        this._readable.on('readable', callback);
+        this._readable.once('error', reject);
+        this._readable.once('end', reject);
+        this._readable.once('close', reject);
+      });
+    }
+  }
+}
+
+
+/* TEST CODE */
+import * as fs from "fs";
+
+async function main() {
+  let rs = fs.createReadStream("README.md");
+
+  let rfs = new ReadableFromStream(rs);
+  console.log(rfs.position);
+
+  const buf = await rfs.read(10);
+  console.log(buf.toString());
+  console.log(rs.readable);
+  console.log(rs.readableLength);
+
+  const buf2 = await rfs.read(100000);
+  console.log(buf2.toString());
+  console.log(rfs.position);
+
+  const buf3 = await rfs.read(10);
+  console.log(buf3.toString());
+  console.log(rfs.position);
+}
+
+main().catch((err) => {
+  console.error("Error running test:", err.message);
+});
