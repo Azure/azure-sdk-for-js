a7c693b6aa303462bf9940fbce534bf8d2560edf
SDK Automation <sdkautomation@microsoft.com>
2020-05-13 23:01:30 +0000

    Generated from e9bf0a211c3138939cc841883fc5a506c0017ea1
    Fixed invalid reference

--- sdk/applicationinsights/arm-appinsights/src/operations/index.ts
+++ sdk/applicationinsights/arm-appinsights/src/operations/index.ts
@@ -9,18 +9,3 @@
  */
 
 export * from "./operations";
-export * from "./annotations";
-export * from "./aPIKeys";
-export * from "./exportConfigurations";
-export * from "./componentCurrentBillingFeatures";
-export * from "./componentQuotaStatus";
-export * from "./componentFeatureCapabilities";
-export * from "./componentAvailableFeatures";
-export * from "./proactiveDetectionConfigurations";
-export * from "./components";
-export * from "./workItemConfigurations";
-export * from "./favorites";
-export * from "./webTestLocations";
-export * from "./webTests";
-export * from "./analyticsItems";
-export * from "./workbooks";
