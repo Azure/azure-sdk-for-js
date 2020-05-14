6925ccaf2ff81286066210b15e3047783d1cdbb4
azure-sdk <azuresdk@microsoft.com>
2020-05-14 00:03:57 +0000

    Increment package version after release of azure-keyvault-keys

--- sdk/keyvault/keyvault-keys/src/core/keyVaultClientContext.ts
+++ sdk/keyvault/keyvault-keys/src/core/keyVaultClientContext.ts
@@ -11,7 +11,7 @@
 import * as coreHttp from "@azure/core-http";
 
 const packageName = "@azure/keyvault-keys";
-const packageVersion = "4.0.3";
+const packageVersion = "4.0.4";
 
 export class KeyVaultClientContext extends coreHttp.ServiceClient {
   apiVersion: string;
