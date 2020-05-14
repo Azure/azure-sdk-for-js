4ac0b8fad2761987f2fe44cb9fc4bb9d7aab018c
Zachary Foster <zfoster@users.noreply.github.com>
2020-05-14 14:16:31 -0400

    Update sdk/cosmosdb/cosmos/src/client/Container/ContainerRequest.ts
    Co-authored-by: Steve Faulkner <southpolesteve@gmail.com>

--- sdk/cosmosdb/cosmos/src/client/Container/ContainerRequest.ts
+++ sdk/cosmosdb/cosmos/src/client/Container/ContainerRequest.ts
@@ -7,7 +7,7 @@ import { VerboseOmit } from "../../utils/types";
 export interface ContainerRequest extends VerboseOmit<ContainerDefinition, "partitionKey"> {
   /* Throughput for this container. Cannot use with maxThroughput */
   throughput?: number;
-  /* Max throughput for this container. Specify to use Autopilot */
+  /* Max throughput for this container. Specify to use Autoscale*/
   maxThroughput?: number;
   /* Autoscale scaling defined by throughput increment percent */
   autoUpgradePolicy?: {
