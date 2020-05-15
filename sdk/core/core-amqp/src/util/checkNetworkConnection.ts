0a75156f0fb5f8e079c24d6a4bb906110f9a99db
Daniel Rodríguez <sadasant@users.noreply.github.com>
2020-05-15 17:35:15 -0400

    Update sdk/core/core-amqp/src/util/checkNetworkConnection.ts
    Co-authored-by: chradek <51000525+chradek@users.noreply.github.com>

--- sdk/core/core-amqp/src/util/checkNetworkConnection.ts
+++ sdk/core/core-amqp/src/util/checkNetworkConnection.ts
@@ -20,7 +20,7 @@ export function checkNetworkConnection(host: string): Promise<boolean> {
           err
         );
         // List of possible DNS error codes: https://nodejs.org/dist/latest-v12.x/docs/api/dns.html#dns_error_codes
-        // We only resolve with `false` when dnsResolve fails with a network disconnection.
+        // We only resolve with `false` when dnsResolve fails with an error we expect to see when the network is down.
         if (err.code === CONNREFUSED || err.code === TIMEOUT) {
           return resolve(false);
         }
