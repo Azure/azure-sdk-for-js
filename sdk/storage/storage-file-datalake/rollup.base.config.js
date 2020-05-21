ec125d6961a502961256db61d840afb6e74f05cb
HarshaNalluru <sanallur@microsoft.com>
2020-05-21 15:20:18 -0700

    port circular reference fixes from #8642

--- sdk/storage/storage-file-datalake/rollup.base.config.js
+++ sdk/storage/storage-file-datalake/rollup.base.config.js
@@ -22,6 +22,26 @@ const pkg = require("./package.json");
 const depNames = Object.keys(pkg.dependencies);
 const production = process.env.NODE_ENV === "production";
 
+// This method throws an error if any circular references are caught,
+// this also whitelists non-@azure libraries since such circular references are out of our control.
+// More Info - https://github.com/Azure/azure-sdk-for-js/pull/8642#issuecomment-622201377
+const overrideOnwarnForTests = (warning, warn) => {
+  if (warning.code === "CIRCULAR_DEPENDENCY") {
+    let throwError = true;
+    if (
+      warning.message.includes("node_modules") &&
+      !(
+        warning.message.includes("node_modules/@azure") ||
+        warning.message.includes("node_modules\\@azure")
+      )
+    ) {
+      throwError = false;
+    }
+    if (throwError) throw new Error(warning.message);
+  }
+  warn(warning);
+};
+
 export function nodeConfig(test = false) {
   const externalNodeBuiltins = [
     "@azure/core-http",
@@ -91,6 +111,7 @@ export function nodeConfig(test = false) {
     baseConfig.external.push("assert", "fs", "path");
 
     baseConfig.context = "null";
+    baseConfig.onwarn = overrideOnwarnForTests;
 
     // Disable tree-shaking of test code.  In rollup-plugin-node-resolve@5.0.0, rollup started respecting
     // the "sideEffects" field in package.json.  Since our package.json sets "sideEffects=false", this also
@@ -177,6 +198,7 @@ export function browserConfig(test = false) {
     baseConfig.external = ["fs-extra"];
 
     baseConfig.context = "null";
+    baseConfig.onwarn = overrideOnwarnForTests;
 
     // Disable tree-shaking of test code.  In rollup-plugin-node-resolve@5.0.0, rollup started respecting
     // the "sideEffects" field in package.json.  Since our package.json sets "sideEffects=false", this also
