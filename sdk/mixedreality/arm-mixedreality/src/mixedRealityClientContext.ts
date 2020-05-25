3e0bd9ca544bb4449eac6c834c8ae1dcb7cd9790
SDK Automation <sdkautomation@microsoft.com>
2020-05-25 05:56:29 +0000

    Generated from 32e275e150a32c8007a929937fd717819807f4a9
    feat: update java readme

--- sdk/mixedreality/arm-mixedreality/src/mixedRealityClientContext.ts
+++ sdk/mixedreality/arm-mixedreality/src/mixedRealityClientContext.ts
@@ -17,13 +17,13 @@ const packageVersion = "1.0.0";
 
 export class MixedRealityClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
-  apiVersion?: string;
   subscriptionId: string;
 
   /**
    * Initializes a new instance of the MixedRealityClient class.
    * @param credentials Credentials needed for the client to connect to Azure.
-   * @param subscriptionId Azure subscription ID.
+   * @param subscriptionId The Azure subscription ID. This is a GUID-formatted string (e.g.
+   * 00000000-0000-0000-0000-000000000000)
    * @param [options] The parameter options
    */
   constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.MixedRealityClientOptions) {
@@ -44,7 +44,6 @@ export class MixedRealityClientContext extends msRestAzure.AzureServiceClient {
 
     super(credentials, options);
 
-    this.apiVersion = '2019-02-28-preview';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
