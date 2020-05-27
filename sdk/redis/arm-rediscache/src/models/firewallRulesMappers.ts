542d4a63642caffd47422bf1897bb2462f0ab811
SDK Automation <sdkautomation@microsoft.com>
2020-05-27 01:54:32 +0000

    Generated from e514da4f9fe066615fd8dc83c96b1951ffd1cbbf
    Fix prettier errors.

--- sdk/redis/arm-rediscache/src/models/firewallRulesMappers.ts
+++ sdk/redis/arm-rediscache/src/models/firewallRulesMappers.ts
@@ -14,6 +14,7 @@ export {
   RedisFirewallRule,
   RedisFirewallRuleCreateParameters,
   RedisFirewallRuleListResult,
+  RedisInstanceDetails,
   RedisLinkedServer,
   RedisLinkedServerWithProperties,
   RedisPatchSchedule,
