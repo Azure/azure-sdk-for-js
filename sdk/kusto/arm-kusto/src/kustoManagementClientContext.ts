d5f331119b7482f1df0d854b93eb311edd5d690f
Changlong Liu <changlong.liu@microsoft.com>
2020-05-31 07:04:27 +0800

    upgrade major version

--- sdk/kusto/arm-kusto/src/kustoManagementClientContext.ts
+++ sdk/kusto/arm-kusto/src/kustoManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-kusto";
-const packageVersion = "4.0.1";
+const packageVersion = "5.0.0";
 
 export class KustoManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
