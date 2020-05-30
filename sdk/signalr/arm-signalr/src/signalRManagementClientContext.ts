b1841ef9bef59adf51f9bc9ee4b9fd5367ffa4ab
Changlong Liu <changlong.liu@microsoft.com>
2020-05-31 07:10:33 +0800

    bump version

--- sdk/signalr/arm-signalr/src/signalRManagementClientContext.ts
+++ sdk/signalr/arm-signalr/src/signalRManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-signalr";
-const packageVersion = "2.1.0";
+const packageVersion = "3.0.0";
 
 export class SignalRManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
