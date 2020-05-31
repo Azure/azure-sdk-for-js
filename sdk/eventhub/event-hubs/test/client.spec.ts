7fd1920757e3a312897b86f7cdea05dc0ca14e1a
Ramya Achutha Rao <ramyar@microsoft.com>
2020-05-31 16:42:16 -0700

    Update user agent string tests as well

--- sdk/eventhub/event-hubs/test/client.spec.ts
+++ sdk/eventhub/event-hubs/test/client.spec.ts
@@ -11,10 +11,10 @@ chai.use(chaiString);
 import debugModule from "debug";
 const debug = debugModule("azure:event-hubs:client-spec");
 import { TokenCredential, EventHubProducerClient, EventHubConsumerClient } from "../src";
-import { EventHubClient } from "../src/impl/eventHubClient";
 import { packageJsonInfo } from "../src/util/constants";
 import { EnvVarKeys, getEnvVars, isNode } from "./utils/testUtils";
 import { delay } from "@azure/core-amqp";
+import { ConnectionContext } from "../src/connectionContext";
 const env = getEnvVars();
 
 describe("Create EventHubConsumerClient", function(): void {
@@ -430,68 +430,92 @@ describe("EventHubProducerClient with non existent event hub", function(): void 
   });
 });
 
-describe("User Agent on EventHubClient on", function(): void {
-  let client: EventHubClient;
-
+describe("EventHubConsumerClient User Agent String", function(): void {
   beforeEach(() => {
     should.exist(
       env[EnvVarKeys.EVENTHUB_CONNECTION_STRING],
       "define EVENTHUB_CONNECTION_STRING in your environment before running integration tests."
     );
+
     should.exist(
       env[EnvVarKeys.EVENTHUB_NAME],
       "define EVENTHUB_NAME in your environment before running integration tests."
     );
   });
 
-  afterEach(() => {
-    return client.close();
-  });
-
-  it("should correctly populate the default user agent", function(done: Mocha.Done): void {
-    client = new EventHubClient(
+  it("should correctly populate the default user agent", async function(): Promise<void> {
+    const consumerClient = new EventHubConsumerClient(
+      "$Default",
       env[EnvVarKeys.EVENTHUB_CONNECTION_STRING],
       env[EnvVarKeys.EVENTHUB_NAME]
     );
-    const packageVersion = packageJsonInfo.version;
-    const properties = client["_context"].connection.options.properties;
-    properties!["user-agent"].should.startWith(`azsdk-js-azureeventhubs/${packageVersion}`);
-    should.equal(properties!.product, "MSJSClient");
-    should.equal(properties!.version, packageVersion);
-    if (isNode) {
-      should.equal(properties!.framework, `Node/${process.version}`);
-    } else {
-      should.equal(properties!.framework.startsWith("Browser/"), true);
-    }
-    should.equal(properties!.platform, `(${os.arch()}-${os.type()}-${os.release()})`);
-    done();
+    testUserAgentString(consumerClient["_eventHubClient"]["_context"]);
+    await consumerClient.close();
   });
 
-  it("should correctly populate the custom user agent", function(done: Mocha.Done): void {
-    const customua = "/js-event-processor-host=0.2.0";
-
-    client = new EventHubClient(
+  it("should correctly populate the custom user agent", async function(): Promise<void> {
+    const customUserAgent = "boo";
+    const consumerClient = new EventHubConsumerClient(
+      "$Default",
       env[EnvVarKeys.EVENTHUB_CONNECTION_STRING],
       env[EnvVarKeys.EVENTHUB_NAME],
-      {
-        userAgent: customua
-      }
+      { userAgent: customUserAgent }
     );
-    const packageVersion = packageJsonInfo.version;
-    const properties = client["_context"].connection.options.properties;
-    properties!["user-agent"].should.startWith(`azsdk-js-azureeventhubs/${packageVersion}`);
-    properties!["user-agent"].should.endWith(customua);
-    should.equal(properties!.product, "MSJSClient");
-    should.equal(properties!.version, packageVersion);
-    if (isNode) {
-      should.equal(properties!.framework, `Node/${process.version}`);
-    } else {
-      should.equal(properties!.framework.startsWith("Browser/"), true);
-    }
-    should.equal(properties!.platform, `(${os.arch()}-${os.type()}-${os.release()})`);
-    done();
+    testUserAgentString(consumerClient["_eventHubClient"]["_context"], customUserAgent);
+    await consumerClient.close();
   });
 });
+
+describe("EventHubProducerClient User Agent String", function(): void {
+  beforeEach(() => {
+    should.exist(
+      env[EnvVarKeys.EVENTHUB_CONNECTION_STRING],
+      "define EVENTHUB_CONNECTION_STRING in your environment before running integration tests."
+    );
+
+    should.exist(
+      env[EnvVarKeys.EVENTHUB_NAME],
+      "define EVENTHUB_NAME in your environment before running integration tests."
+    );
+  });
+
+  it("should correctly populate the default user agent", async function(): Promise<void> {
+    const producerClient = new EventHubProducerClient(
+      env[EnvVarKeys.EVENTHUB_CONNECTION_STRING],
+      env[EnvVarKeys.EVENTHUB_NAME]
+    );
+    testUserAgentString(producerClient["_client"]["_context"]);
+    await producerClient.close();
+  });
+
+  it("should correctly populate the custom user agent", async function(): Promise<void> {
+    const customUserAgent = "boo";
+    const producerClient = new EventHubProducerClient(
+      env[EnvVarKeys.EVENTHUB_CONNECTION_STRING],
+      env[EnvVarKeys.EVENTHUB_NAME],
+      { userAgent: customUserAgent }
+    );
+    testUserAgentString(producerClient["_client"]["_context"], customUserAgent);
+    await producerClient.close();
+  });
+});
+
+function testUserAgentString(context: ConnectionContext, customValue?: string) {
+  const packageVersion = packageJsonInfo.version;
+  const properties = context.connection.options.properties;
+  properties!["user-agent"].should.startWith(`azsdk-js-azureeventhubs/${packageVersion}`);
+  should.equal(properties!.product, "MSJSClient");
+  should.equal(properties!.version, packageVersion);
+  if (isNode) {
+    should.equal(properties!.framework, `Node/${process.version}`);
+  } else {
+    should.equal(properties!.framework.startsWith("Browser/"), true);
+  }
+  should.equal(properties!.platform, `(${os.arch()}-${os.type()}-${os.release()})`);
+  if (customValue) {
+    properties!["user-agent"].should.endWith(customValue);
+  }
+}
 
 describe("EventHubConsumerClient after close()", function(): void {
   let client: EventHubConsumerClient;
@@ -518,7 +542,6 @@ describe("EventHubConsumerClient after close()", function(): void {
     // close(), so that we can then test the resulting error.
     await client.close();
   }
-
 
   it("should throw connection closed error for getEventHubProperties", async function(): Promise<
     void
