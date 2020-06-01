129e7102c2da5f3231d11a78c31ecc0e216bee2e
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-06-01 13:37:23 +0000

    [KeyVault-Secrets] Proposed test folders

--- sdk/keyvault/keyvault-secrets/test/internal/challengeBasedAuthenticationPolicy.spec.ts
+++ sdk/keyvault/keyvault-secrets/test/internal/challengeBasedAuthenticationPolicy.spec.ts
@@ -2,14 +2,14 @@
 // Licensed under the MIT license.
 
 import * as assert from "assert";
-import { SecretClient } from "../src";
+import { SecretClient } from "../../src";
 import { env, Recorder } from "@azure/test-utils-recorder";
-import { authenticate } from "./utils/testAuthentication";
-import TestClient from "./utils/testClient";
+import { authenticate } from "../utils/testAuthentication";
+import TestClient from "../utils/testClient";
 import {
   AuthenticationChallengeCache,
   AuthenticationChallenge
-} from "../src/core/challengeBasedAuthenticationPolicy";
+} from "../../src/core/challengeBasedAuthenticationPolicy";
 
 // Following the philosophy of not testing the insides if we can test the outsides...
 // I present you with this "Get Out of Jail Free" card (in reference to Monopoly).
--- sdk/keyvault/keyvault-secrets/test/public/CRUD.spec.ts
+++ sdk/keyvault/keyvault-secrets/test/public/CRUD.spec.ts
@@ -2,13 +2,13 @@
 // Licensed under the MIT license.
 
 import * as assert from "assert";
-import { SecretClient } from "../src";
-import { testPollerProperties } from "./utils/recorderUtils";
+import { SecretClient } from "../../src";
+import { testPollerProperties } from "../utils/recorderUtils";
 import { env, Recorder } from "@azure/test-utils-recorder";
-import { authenticate } from "./utils/testAuthentication";
-import TestClient from "./utils/testClient";
+import { authenticate } from "../utils/testAuthentication";
+import TestClient from "../utils/testClient";
 import { AbortController } from "@azure/abort-controller";
-import { assertThrowsAbortError } from "./utils/utils.common";
+import { assertThrowsAbortError } from "../utils/utils.common";
 
 describe("Secret client - create, read, update and delete operations", () => {
   const secretValue = "SECRET_VALUE";
