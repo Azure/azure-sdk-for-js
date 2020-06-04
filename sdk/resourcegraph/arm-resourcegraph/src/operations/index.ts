b2aa25b1f7f07f509f4596cfd2517a7ed6fd8a63
SDK Automation <sdkautomation@microsoft.com>
2020-06-04 07:59:34 +0000

    Generated from 2ec08dbf0024615aef0c2b18ef70e9a1c7520e2f
    set location readOnly false

--- sdk/resourcegraph/arm-resourcegraph/src/operations/index.ts
+++ sdk/resourcegraph/arm-resourcegraph/src/operations/index.ts
@@ -8,4 +8,6 @@
  * regenerated.
  */
 
+export * from "./list";
+export * from "./get";
 export * from "./operations";
--- sdk/resourcegraph/arm-resourcegraph/src/resourceGraphClientContext.ts
+++ sdk/resourcegraph/arm-resourcegraph/src/resourceGraphClientContext.ts
@@ -39,7 +39,7 @@ export class ResourceGraphClientContext extends msRestAzure.AzureServiceClient {
 
     super(credentials, options);
 
-    this.apiVersion = '2019-04-01';
+    this.apiVersion = '2020-04-01-preview';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
