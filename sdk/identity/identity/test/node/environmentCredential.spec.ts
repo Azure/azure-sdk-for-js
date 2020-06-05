4986a3d6704b3de16485dd56609e6d851b9bb39f
Scott Schaab <sschaab@microsoft.com>
2020-06-05 16:51:01 -0700

    exporting CredentialUnavailable and fixing tests

--- sdk/identity/identity/test/node/environmentCredential.spec.ts
+++ sdk/identity/identity/test/node/environmentCredential.spec.ts
@@ -3,7 +3,7 @@
 
 import assert from "assert";
 import path from "path";
-import { EnvironmentCredential, AuthenticationError } from "../../src";
+import { EnvironmentCredential, AuthenticationError, CredentialUnavailable } from "../../src";
 import {
   MockAuthHttpClient,
   assertClientCredentials,
@@ -136,14 +136,14 @@ describe("EnvironmentCredential", function() {
     assert.strictEqual(tracer.getActiveSpans().length, 0, "All spans should have had end called");
   });
 
-  it("throws an AuthenticationError when getToken is called and no credential was configured", async () => {
+  it("throws an CredentialUnavailable when getToken is called and no credential was configured", async () => {
     const mockHttpClient = new MockAuthHttpClient();
 
     const credential = new EnvironmentCredential(mockHttpClient.tokenCredentialOptions);
     await assertRejects(
       credential.getToken("scope"),
-      (error: AuthenticationError) =>
-        error.errorResponse.errorDescription.indexOf(AllSupportedEnvironmentVariables.join("\n")) >
+      (error: CredentialUnavailable) =>
+        error.message.indexOf(AllSupportedEnvironmentVariables.join("\n")) >
         -1
     );
 
