d69c7609f01f318383651d1b0d245a698f7592f3
azure-sdk <azuresdk@microsoft.com>
2020-06-08 19:06:27 +0000

    Increment package version after release of azure-search-documents

--- sdk/search/search-documents/src/generated/data/searchClientContext.ts
+++ sdk/search/search-documents/src/generated/data/searchClientContext.ts
@@ -11,7 +11,7 @@
 import * as coreHttp from "@azure/core-http";
 
 const packageName = "@azure/search-documents";
-const packageVersion = "1.0.0-preview.4";
+const packageVersion = "1.0.0-preview.5";
 
 export class SearchClientContext extends coreHttp.ServiceClient {
   apiVersion: string;
