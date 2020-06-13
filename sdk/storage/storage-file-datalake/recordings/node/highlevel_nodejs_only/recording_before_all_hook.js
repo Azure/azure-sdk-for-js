aa1f9627033f1dd73a7ef7d2ba85486e077a8ab7
Lin Jian <ljian3377@gmail.com>
2020-06-13 19:54:45 +0800

    record skipped test for Jumbo

--- sdk/storage/storage-file-datalake/recordings/node/highlevel_nodejs_only/recording_before_all_hook.js
+++ sdk/storage/storage-file-datalake/recordings/node/highlevel_nodejs_only/recording_before_all_hook.js
@@ -1,5 +1,5 @@
 let nock = require('nock');
 
-module.exports.hash = "eb7536c54793ecdfc978a993963c0ba7";
+module.exports.hash = "806e9a38359ed552a548cc5564d21b5b";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
