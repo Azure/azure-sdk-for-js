fd9ba89e8ec1b9ce987d76e41fb83e5e0ea410ae
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-06-24 14:04:35 +0000

    documentation on the key paramter of the CryptographyClient constructor

--- sdk/keyvault/keyvault-keys/src/cryptographyClient.ts
+++ sdk/keyvault/keyvault-keys/src/cryptographyClient.ts
@@ -665,14 +665,14 @@ export class CryptographyClient {
    * // or
    * let client = new CryptographyClient(keyVaultKey, credentials);
    * ```
-   * @param key The key to use during cryptography tasks.
+   * @param key The key to use during cryptography tasks. It can be either a Key Vault Key identifier (or URL) or a full KeyVaultKey.
    * @param {TokenCredential} credential An object that implements the `TokenCredential` interface used to authenticate requests to the service. Use the @azure/identity package to create a credential that suits your needs.
    * @param {PipelineOptions} [pipelineOptions={}] Optional. Pipeline options used to configure Key Vault API requests.
    *                                                         Omit this parameter to use the default pipeline configuration.
    * @memberof CryptographyClient
    */
   constructor(
-    key: string | KeyVaultKey, // keyUrl or KeyVaultKey
+    key: string | KeyVaultKey,
     credential: TokenCredential,
     pipelineOptions: CryptographyClientOptions = {}
   ) {
