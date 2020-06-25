0b33aabe68fedd739ad247b58d7902a73dbc1d82
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-06-25 12:36:55 +0000

    keyvault-keys node recordings

--- sdk/keyvault/keyvault-keys/recordings/node/keys_client__list_keys_in_various_ways/recording_list_0_versions_of_a_nonexisting_key.js
+++ sdk/keyvault/keyvault-keys/recordings/node/keys_client__list_keys_in_various_ways/recording_list_0_versions_of_a_nonexisting_key.js
@@ -1,11 +1,11 @@
 let nock = require('nock');
 
-module.exports.hash = "8aee362f9cb8644344dd0116b739ded1";
+module.exports.hash = "d5c4f7ac5263214da90fd3ee27a06b62";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
 nock('https://keyvault_name.vault.azure.net:443', {"encodedQueryParams":true})
-  .get('/keys/recoverKeyName-list0versionsofanon-existingkey-/versions')
+  .get('/keys/listKeyName-list0versionsofanon-existingkey-/versions')
   .query(true)
   .reply(401, {"error":{"code":"Unauthorized","message":"Request is missing a Bearer or PoP token."}}, [
   'Cache-Control',
@@ -18,18 +18,16 @@ nock('https://keyvault_name.vault.azure.net:443', {"encodedQueryParams":true})
   'application/json; charset=utf-8',
   'Expires',
   '-1',
-  'Server',
-  'Microsoft-IIS/10.0',
   'WWW-Authenticate',
   'Bearer authorization="https://login.windows.net/azure_tenant_id", resource="https://vault.azure.net"',
   'x-ms-keyvault-region',
   'westus',
   'x-ms-request-id',
-  '174c8c66-a1d5-4465-b612-04a74cd726b8',
+  '9a189743-64ef-499e-9198-cb2bff3d14b8',
   'x-ms-keyvault-service-version',
-  '1.1.0.898',
+  '1.1.6.0',
   'x-ms-keyvault-network-info',
-  'addr=52.247.203.156;act_addr_fam=InterNetwork;',
+  'conn_type=Ipv4;addr=51.141.175.151;act_addr_fam=InterNetwork;',
   'X-AspNet-Version',
   '4.0.30319',
   'X-Powered-By',
@@ -39,7 +37,7 @@ nock('https://keyvault_name.vault.azure.net:443', {"encodedQueryParams":true})
   'X-Content-Type-Options',
   'nosniff',
   'Date',
-  'Tue, 28 Apr 2020 01:21:27 GMT'
+  'Thu, 25 Jun 2020 12:00:56 GMT'
 ]);
 
 nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
@@ -58,25 +56,25 @@ nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
   'X-Content-Type-Options',
   'nosniff',
   'x-ms-request-id',
-  '729088f1-b443-491a-8245-58c2aef47100',
+  '2cdd5278-77a8-4425-95b2-ac36ba7ff700',
   'x-ms-ests-server',
-  '2.1.10433.14 - EUS ProdSlices',
+  '2.1.10732.8 - NCUS ProdSlices',
   'P3P',
   'CP="DSP CUR OTPi IND OTRi ONL FIN"',
   'Set-Cookie',
-  'fpc=AvRe1uZsOqRCqM4gGHWlCcQ_aSJHAQAAABh8OdYOAAAA; expires=Thu, 28-May-2020 01:21:28 GMT; path=/; secure; HttpOnly; SameSite=None',
+  'fpc=AtenyQFe8_pFtTk3RjJ0lok_aSJHAQAAAPiIhtYOAAAA; expires=Sat, 25-Jul-2020 12:00:57 GMT; path=/; secure; HttpOnly; SameSite=None',
   'Set-Cookie',
   'x-ms-gateway-slice=estsfd; path=/; SameSite=None; secure; HttpOnly',
   'Set-Cookie',
   'stsservicecookie=estsfd; path=/; SameSite=None; secure; HttpOnly',
   'Date',
-  'Tue, 28 Apr 2020 01:21:28 GMT',
+  'Thu, 25 Jun 2020 12:00:56 GMT',
   'Content-Length',
   '1315'
 ]);
 
 nock('https://keyvault_name.vault.azure.net:443', {"encodedQueryParams":true})
-  .get('/keys/recoverKeyName-list0versionsofanon-existingkey-/versions')
+  .get('/keys/listKeyName-list0versionsofanon-existingkey-/versions')
   .query(true)
   .reply(200, {"value":[],"nextLink":null}, [
   'Cache-Control',
@@ -87,16 +85,14 @@ nock('https://keyvault_name.vault.azure.net:443', {"encodedQueryParams":true})
   'application/json; charset=utf-8',
   'Expires',
   '-1',
-  'Server',
-  'Microsoft-IIS/10.0',
   'x-ms-keyvault-region',
   'westus',
   'x-ms-request-id',
-  'f3794e99-9a62-467c-b38c-b34c4e81dc5d',
+  '7ade4539-4bbd-4ba0-bb9a-81923775b588',
   'x-ms-keyvault-service-version',
-  '1.1.0.898',
+  '1.1.6.0',
   'x-ms-keyvault-network-info',
-  'addr=52.247.203.156;act_addr_fam=InterNetwork;',
+  'conn_type=Ipv4;addr=51.141.175.151;act_addr_fam=InterNetwork;',
   'X-AspNet-Version',
   '4.0.30319',
   'X-Powered-By',
@@ -106,7 +102,7 @@ nock('https://keyvault_name.vault.azure.net:443', {"encodedQueryParams":true})
   'X-Content-Type-Options',
   'nosniff',
   'Date',
-  'Tue, 28 Apr 2020 01:21:27 GMT',
+  'Thu, 25 Jun 2020 12:00:56 GMT',
   'Content-Length',
   '28'
 ]);
