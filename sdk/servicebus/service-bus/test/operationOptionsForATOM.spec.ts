80a44102e72aea432103839b434d16f768ce1ab4
HarshaNalluru <sanallur@microsoft.com>
2020-06-26 14:54:37 -0700

    tests

--- sdk/servicebus/service-bus/test/operationOptionsForATOM.spec.ts
+++ sdk/servicebus/service-bus/test/operationOptionsForATOM.spec.ts
@@ -21,27 +21,221 @@ const serviceBusAtomManagementClient: ServiceBusManagementClient = new ServiceBu
   env[EnvVarNames.SERVICEBUS_CONNECTION_STRING]
 );
 
-describe.only("Operation Options", () => {
+describe("Operation Options", () => {
+  const enitityName1 = "random-name";
+  const enitityName2 = "random-name-2";
+  // This describe block ensures that abort signal works as expected and
+  // the OperationOptions are plugged in for all the methods
   describe("Abort Signal", () => {
-    const queueName = "random-name";
-    it("abortSignal should work", async () => {
+    async function verifyAbortError(func: Function) {
       try {
-        await serviceBusAtomManagementClient.createQueue(queueName, {
-          abortSignal: AbortController.timeout(1)
-        });
+        await func();
         assert.fail();
       } catch (err) {
         assert.equal(err.name, "AbortError");
         assert.equal(err.message, "The operation was aborted.", "Unexpected error caught: " + err);
       }
+    }
+
+    it("getNamespaceProperties", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getNamespaceProperties({
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("createQueue", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.createQueue(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getQueue", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getQueue(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("updateQueue", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.updateQueue(
+            { name: enitityName1 },
+            {
+              abortSignal: AbortController.timeout(1)
+            }
+          )
+      );
+    });
+    it("deleteQueue", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.deleteQueue(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getQueueRuntimeinfo", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getQueueRuntimeInfo(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getQueues", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getQueues({
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getQueuesRuntimeinfo", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getQueuesRuntimeInfo({
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("createTopic", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.createTopic(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getTopic", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getTopic(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("updateTopic", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.updateTopic(
+            { name: enitityName1 },
+            {
+              abortSignal: AbortController.timeout(1)
+            }
+          )
+      );
+    });
+    it("deleteTopic", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.deleteTopic(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getTopicRuntimeinfo", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getTopicRuntimeInfo(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getTopics", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getTopics({
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getTopicsRuntimeinfo", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getTopicsRuntimeInfo({
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("createSubscription", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.createSubscription(
+            { topicName: enitityName1, subscriptionName: enitityName2 },
+            {
+              abortSignal: AbortController.timeout(1)
+            }
+          )
+      );
+    });
+    it("getSubscription", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getSubscription(enitityName1, enitityName2, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("updateSubscription", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.updateSubscription(
+            { topicName: enitityName1, subscriptionName: enitityName2 },
+            {
+              abortSignal: AbortController.timeout(1)
+            }
+          )
+      );
+    });
+    it("deleteSubscription", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.deleteSubscription(enitityName1, enitityName2, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getSubscriptionRuntimeinfo", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getSubscriptionRuntimeInfo(
+            enitityName1,
+            enitityName2,
+            {
+              abortSignal: AbortController.timeout(1)
+            }
+          )
+      );
+    });
+    it("getSubscriptions", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getSubscriptions(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
+    });
+    it("getSubscriptionsRuntimeinfo", async () => {
+      await verifyAbortError(
+        async () =>
+          await serviceBusAtomManagementClient.getSubscriptionsRuntimeInfo(enitityName1, {
+            abortSignal: AbortController.timeout(1)
+          })
+      );
     });
   });
 
   describe("RequestOptions timeout", () => {
-    const queueName = "random-name";
+    // This test makes sure the timeout is plumbed as expected
     it("requestOptions.timeout should work", async () => {
       try {
-        await serviceBusAtomManagementClient.createQueue(queueName, {
+        await serviceBusAtomManagementClient.createQueue(enitityName1, {
           requestOptions: { timeout: 1 }
         });
         assert.fail();
@@ -52,17 +246,17 @@ describe.only("Operation Options", () => {
     });
   });
 
-  describe("RequestOptions custom headers", () => {
-    const queueName = "random-name";
+  describe.skip("RequestOptions custom headers", () => {
+    // Response doesn't contain the custom headers,
+    // Custom headers are being plumbed properly at the SDK level, might be a problem at the core-http or at the service
+    // Needs more investigation
     it("requestOptions.customHeaders should work", async () => {
-      console.log(
-        (
-          await serviceBusAtomManagementClient.createQueue(queueName, {
-            requestOptions: { customHeaders: { hello: "world" } }
-          })
-        )._response.headers
-      );
-      await serviceBusAtomManagementClient.deleteQueue(queueName);
+      await serviceBusAtomManagementClient.createQueue(enitityName1, {
+        requestOptions: {
+          customHeaders: { state: "WA" }
+        }
+      });
+      await serviceBusAtomManagementClient.deleteQueue(enitityName1);
     });
   });
 });
