669c0e44f27edd6d8a34547db9e126b6cf37717e
Jeremy Meng <yumeng@microsoft.com>
2020-06-26 14:40:39 -0700

    Remove some creating polling state from recording
    to shorten playback test run duration.

--- sdk/formrecognizer/ai-form-recognizer/recordings/node/formtrainingclient_nodejs_only/recording_trains_model_with_forms_and_labels.js
+++ sdk/formrecognizer/ai-form-recognizer/recordings/node/formtrainingclient_nodejs_only/recording_trains_model_with_forms_and_labels.js
@@ -1,6 +1,6 @@
 let nock = require('nock');
 
-module.exports.hash = "5a98b0b4f208c33690bb9023a8a45c33";
+module.exports.hash = "cad1a0d190f552be0293f81c20b80044";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
@@ -10,95 +10,61 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Content-Length',
   '0',
   'Location',
-  'https://endpoint/formrecognizer/v2.0-preview/custom/models/7f954280-8212-4c61-9c33-8fea2e47d279',
+  'https://endpoint/formrecognizer/v2.0-preview/custom/models/8868c5b9-2248-4650-b906-a30bd14196bb',
   'x-envoy-upstream-service-time',
-  '43',
+  '5093',
   'apim-request-id',
-  '34098037-97a2-406f-a674-f56b31b7f1db',
+  '866a1217-6c0b-491f-bbbe-f5b666017852',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Sat, 02 May 2020 20:00:04 GMT'
+  'Fri, 26 Jun 2020 21:23:26 GMT',
+  'Connection',
+  'close'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/7f954280-8212-4c61-9c33-8fea2e47d279')
+  .get('/formrecognizer/v2.0-preview/custom/models/8868c5b9-2248-4650-b906-a30bd14196bb')
   .query(true)
-  .reply(200, {"modelInfo":{"modelId":"7f954280-8212-4c61-9c33-8fea2e47d279","status":"creating","createdDateTime":"2020-05-02T20:00:04Z","lastUpdatedDateTime":"2020-05-02T20:00:04Z"}}, [
+  .reply(200, {"modelInfo":{"modelId":"8868c5b9-2248-4650-b906-a30bd14196bb","status":"creating","createdDateTime":"2020-06-26T21:23:22Z","lastUpdatedDateTime":"2020-06-26T21:23:22Z"}}, [
   'Transfer-Encoding',
   'chunked',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-envoy-upstream-service-time',
-  '31',
+  '23',
   'apim-request-id',
-  '5e601f0c-7b99-4928-8cd8-46863156553d',
+  '2b4f9d53-b8c9-4aab-a13c-e4e1f28bbe0a',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Sat, 02 May 2020 20:00:04 GMT'
+  'Fri, 26 Jun 2020 21:23:32 GMT',
+  'Connection',
+  'close'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/7f954280-8212-4c61-9c33-8fea2e47d279')
+  .get('/formrecognizer/v2.0-preview/custom/models/8868c5b9-2248-4650-b906-a30bd14196bb')
   .query(true)
-  .reply(200, {"modelInfo":{"modelId":"7f954280-8212-4c61-9c33-8fea2e47d279","status":"creating","createdDateTime":"2020-05-02T20:00:04Z","lastUpdatedDateTime":"2020-05-02T20:00:04Z"}}, [
+  .reply(200, {"modelInfo":{"modelId":"8868c5b9-2248-4650-b906-a30bd14196bb","status":"ready","createdDateTime":"2020-06-26T21:23:22Z","lastUpdatedDateTime":"2020-06-26T21:23:36Z"},"trainResult":{"averageModelAccuracy":0.973,"trainingDocuments":[{"documentName":"Form_1.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_2.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_3.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_4.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_5.jpg","pages":1,"status":"succeeded"}],"fields":[{"fieldName":"CompanyAddress","accuracy":0.8},{"fieldName":"CompanyName","accuracy":1},{"fieldName":"CompanyPhoneNumber","accuracy":1},{"fieldName":"DatedAs","accuracy":1},{"fieldName":"Email","accuracy":0.8},{"fieldName":"Merchant","accuracy":1},{"fieldName":"PhoneNumber","accuracy":1},{"fieldName":"PurchaseOrderNumber","accuracy":1},{"fieldName":"Quantity","accuracy":1},{"fieldName":"Signature","accuracy":1},{"fieldName":"Subtotal","accuracy":1},{"fieldName":"Tax","accuracy":1},{"fieldName":"Total","accuracy":1},{"fieldName":"VendorName","accuracy":1},{"fieldName":"Website","accuracy":1}],"errors":[]}}, [
   'Transfer-Encoding',
   'chunked',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-envoy-upstream-service-time',
-  '48',
+  '18',
   'apim-request-id',
-  'b139f1d7-3a2e-43ef-ae74-396dea5c1388',
+  'b5892068-1533-4b07-911f-0dc9abdb9429',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Sat, 02 May 2020 20:00:04 GMT'
-]);
-
-nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/7f954280-8212-4c61-9c33-8fea2e47d279')
-  .query(true)
-  .reply(200, {"modelInfo":{"modelId":"7f954280-8212-4c61-9c33-8fea2e47d279","status":"creating","createdDateTime":"2020-05-02T20:00:04Z","lastUpdatedDateTime":"2020-05-02T20:00:04Z"}}, [
-  'Transfer-Encoding',
-  'chunked',
-  'Content-Type',
-  'application/json; charset=utf-8',
-  'x-envoy-upstream-service-time',
-  '27',
-  'apim-request-id',
-  '370aac86-e5a3-4069-ad0b-6726f0e905af',
-  'Strict-Transport-Security',
-  'max-age=31536000; includeSubDomains; preload',
-  'x-content-type-options',
-  'nosniff',
-  'Date',
-  'Sat, 02 May 2020 20:00:09 GMT'
-]);
-
-nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/7f954280-8212-4c61-9c33-8fea2e47d279')
-  .query(true)
-  .reply(200, {"modelInfo":{"modelId":"7f954280-8212-4c61-9c33-8fea2e47d279","status":"ready","createdDateTime":"2020-05-02T20:00:04Z","lastUpdatedDateTime":"2020-05-02T20:00:12Z"},"trainResult":{"averageModelAccuracy":0.973,"trainingDocuments":[{"documentName":"Form_1.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_2.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_3.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_4.jpg","pages":1,"status":"succeeded"},{"documentName":"Form_5.jpg","pages":1,"status":"succeeded"}],"fields":[{"fieldName":"CompanyAddress","accuracy":0.8},{"fieldName":"CompanyName","accuracy":1},{"fieldName":"CompanyPhoneNumber","accuracy":1},{"fieldName":"DatedAs","accuracy":1},{"fieldName":"Email","accuracy":0.8},{"fieldName":"Merchant","accuracy":1},{"fieldName":"PhoneNumber","accuracy":1},{"fieldName":"PurchaseOrderNumber","accuracy":1},{"fieldName":"Quantity","accuracy":1},{"fieldName":"Signature","accuracy":1},{"fieldName":"Subtotal","accuracy":1},{"fieldName":"Tax","accuracy":1},{"fieldName":"Total","accuracy":1},{"fieldName":"VendorName","accuracy":1},{"fieldName":"Website","accuracy":1}],"errors":[]}}, [
-  'Transfer-Encoding',
-  'chunked',
-  'Content-Type',
-  'application/json; charset=utf-8',
-  'x-envoy-upstream-service-time',
-  '29',
-  'apim-request-id',
-  '2c23e3c3-4c7b-4304-b058-1b26feba5c61',
-  'Strict-Transport-Security',
-  'max-age=31536000; includeSubDomains; preload',
-  'x-content-type-options',
-  'nosniff',
-  'Date',
-  'Sat, 02 May 2020 20:00:15 GMT'
+  'Fri, 26 Jun 2020 21:23:37 GMT',
+  'Connection',
+  'close'
 ]);
--- sdk/formrecognizer/ai-form-recognizer/recordings/node/formtrainingclient_nodejs_only/recording_trains_model_with_forms_and_no_labels.js
+++ sdk/formrecognizer/ai-form-recognizer/recordings/node/formtrainingclient_nodejs_only/recording_trains_model_with_forms_and_no_labels.js
@@ -1,6 +1,6 @@
 let nock = require('nock');
 
-module.exports.hash = "bb482db3fb10d0ee44e322df4f19ed76";
+module.exports.hash = "c8c4a8dfbb5b81bc885b80a28093ea14";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
@@ -10,95 +10,61 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Content-Length',
   '0',
   'Location',
-  'https://endpoint/formrecognizer/v2.0-preview/custom/models/80d87355-5d05-40b3-9dc3-1a7c384ff710',
+  'https://endpoint/formrecognizer/v2.0-preview/custom/models/97ffff4a-c78c-48e5-8a60-5644bf3f3bf0',
   'x-envoy-upstream-service-time',
-  '49',
+  '514',
   'apim-request-id',
-  'bc1f9d21-38e8-4db7-b38c-43615e93ffe2',
+  '510d9a9c-b393-4fc8-b84d-ef01a6c3fd2c',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Sat, 02 May 2020 19:59:54 GMT'
+  'Fri, 26 Jun 2020 21:23:09 GMT',
+  'Connection',
+  'close'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/80d87355-5d05-40b3-9dc3-1a7c384ff710')
+  .get('/formrecognizer/v2.0-preview/custom/models/97ffff4a-c78c-48e5-8a60-5644bf3f3bf0')
   .query(true)
-  .reply(200, {"modelInfo":{"modelId":"80d87355-5d05-40b3-9dc3-1a7c384ff710","status":"creating","createdDateTime":"2020-05-02T19:59:54Z","lastUpdatedDateTime":"2020-05-02T19:59:54Z"}}, [
+  .reply(200, {"modelInfo":{"modelId":"97ffff4a-c78c-48e5-8a60-5644bf3f3bf0","status":"creating","createdDateTime":"2020-06-26T21:23:09Z","lastUpdatedDateTime":"2020-06-26T21:23:09Z"}}, [
   'Transfer-Encoding',
   'chunked',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-envoy-upstream-service-time',
-  '35',
+  '17',
   'apim-request-id',
-  'c20e75f8-57bb-4fd5-8e30-4aa68a56512c',
+  '9bf5ccc6-81fb-4bbf-96b1-c190e8728829',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Sat, 02 May 2020 19:59:54 GMT'
+  'Fri, 26 Jun 2020 21:23:15 GMT',
+  'Connection',
+  'close'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/80d87355-5d05-40b3-9dc3-1a7c384ff710')
+  .get('/formrecognizer/v2.0-preview/custom/models/97ffff4a-c78c-48e5-8a60-5644bf3f3bf0')
   .query(true)
-  .reply(200, {"modelInfo":{"modelId":"80d87355-5d05-40b3-9dc3-1a7c384ff710","status":"creating","createdDateTime":"2020-05-02T19:59:54Z","lastUpdatedDateTime":"2020-05-02T19:59:54Z"}}, [
+  .reply(200, {"modelInfo":{"modelId":"97ffff4a-c78c-48e5-8a60-5644bf3f3bf0","status":"ready","createdDateTime":"2020-06-26T21:23:09Z","lastUpdatedDateTime":"2020-06-26T21:23:20Z"},"keys":{"clusters":{"0":["Additional Notes:","Address:","Company Name:","Company Phone:","Dated As:","Details","Email:","Hero Limited","Name:","Phone:","Purchase Order","Purchase Order #:","Quantity","SUBTOTAL","Seattle, WA 93849 Phone:","Shipped From","Shipped To","TAX","TOTAL","Total","Unit Price","Vendor Name:","Website:"]}},"trainResult":{"trainingDocuments":[{"documentName":"Form_1.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_2.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_3.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_4.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_5.jpg","pages":1,"errors":[],"status":"succeeded"}],"errors":[]}}, [
   'Transfer-Encoding',
   'chunked',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-envoy-upstream-service-time',
-  '31',
+  '22',
   'apim-request-id',
-  '4622e343-6e8d-4ea9-8f86-2e009079d9fc',
+  '9366caf5-2149-4259-9a44-a61f5aa01d34',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Sat, 02 May 2020 19:59:54 GMT'
-]);
-
-nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/80d87355-5d05-40b3-9dc3-1a7c384ff710')
-  .query(true)
-  .reply(200, {"modelInfo":{"modelId":"80d87355-5d05-40b3-9dc3-1a7c384ff710","status":"creating","createdDateTime":"2020-05-02T19:59:54Z","lastUpdatedDateTime":"2020-05-02T19:59:54Z"}}, [
-  'Transfer-Encoding',
-  'chunked',
-  'Content-Type',
-  'application/json; charset=utf-8',
-  'x-envoy-upstream-service-time',
-  '26',
-  'apim-request-id',
-  'b9febf6c-4c1f-482c-ad6c-ba416f9b2201',
-  'Strict-Transport-Security',
-  'max-age=31536000; includeSubDomains; preload',
-  'x-content-type-options',
-  'nosniff',
-  'Date',
-  'Sat, 02 May 2020 19:59:58 GMT'
-]);
-
-nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0-preview/custom/models/80d87355-5d05-40b3-9dc3-1a7c384ff710')
-  .query(true)
-  .reply(200, {"modelInfo":{"modelId":"80d87355-5d05-40b3-9dc3-1a7c384ff710","status":"ready","createdDateTime":"2020-05-02T19:59:54Z","lastUpdatedDateTime":"2020-05-02T20:00:02Z"},"keys":{"clusters":{"0":["Additional Notes:","Address:","Address:","Company Name:","Company Phone:","Dated As:","Email:","Hero Limited","Name:","Phone:","Phone:","Purchase Order","Purchase Order","Purchase Order #:","SUBTOTAL","Seattle, WA 93849 Phone:","Shipped From","Shipped To","TAX","TOTAL","Vendor Name:","Website:"]}},"trainResult":{"trainingDocuments":[{"documentName":"Form_1.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_2.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_3.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_4.jpg","pages":1,"errors":[],"status":"succeeded"},{"documentName":"Form_5.jpg","pages":1,"errors":[],"status":"succeeded"}],"errors":[]}}, [
-  'Transfer-Encoding',
-  'chunked',
-  'Content-Type',
-  'application/json; charset=utf-8',
-  'x-envoy-upstream-service-time',
-  '25',
-  'apim-request-id',
-  '459fecc5-4dd2-479f-a736-d895d339aff1',
-  'Strict-Transport-Security',
-  'max-age=31536000; includeSubDomains; preload',
-  'x-content-type-options',
-  'nosniff',
-  'Date',
-  'Sat, 02 May 2020 20:00:04 GMT'
+  'Fri, 26 Jun 2020 21:23:21 GMT',
+  'Connection',
+  'close'
 ]);
