86dc33954a31b1e598b357ff55103fe3cd7eafd9
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-06-27 19:59:36 +0000

    chai assert as a named export

--- sdk/keyvault/keyvault-keys/rollup.base.config.js
+++ sdk/keyvault/keyvault-keys/rollup.base.config.js
@@ -119,6 +119,7 @@ export function browserConfig(test = false) {
       }),
       cjs({
         namedExports: {
+          chai: ["assert"],
           assert: ["ok", "equal", "strictEqual", "deepEqual"],
           "@opentelemetry/api": ["CanonicalCode", "SpanKind", "TraceFlags"]
         }
