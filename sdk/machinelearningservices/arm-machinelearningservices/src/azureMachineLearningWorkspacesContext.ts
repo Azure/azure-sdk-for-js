b4b9621947e6dd2bebecb08b1df83b38385504ff
SDK Automation <sdkautomation@microsoft.com>
2020-06-28 06:13:08 +0000

    Generated from 5838bff3a562838a5209ccba20351154d566cec3
    re-add 200

--- sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspacesContext.ts
+++ sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspacesContext.ts
@@ -44,7 +44,7 @@ export class AzureMachineLearningWorkspacesContext extends msRestAzure.AzureServ
 
     super(credentials, options);
 
-    this.apiVersion = '2019-05-01';
+    this.apiVersion = '2020-04-01';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
