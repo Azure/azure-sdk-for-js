2e92eff0878b5e3f53412f449a6dcc5725a9291e
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-07-01 16:54:39 -0700

    Re-generate to fix missing nextLink

--- sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/recoveryServicesBackupClientContext.ts
+++ sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/recoveryServicesBackupClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-recoveryservicesbackup";
-const packageVersion = "6.0.0";
+const packageVersion = "6.1.0";
 
 export class RecoveryServicesBackupClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
