53bf8a6d49b77e7ac1e02fcda5d3fd8ded737ac4
Ramya Achutha Rao <ramyar@microsoft.com>
2020-07-05 13:57:30 -0700

    [Identity] Fix warning on unresolved dependencies when building tests

--- sdk/identity/identity/rollup.base.config.js
+++ sdk/identity/identity/rollup.base.config.js
@@ -41,7 +41,7 @@ export function nodeConfig(test = false) {
     baseConfig.output.file = "test-dist/index.js";
 
     // mark assert as external
-    baseConfig.external.push("assert");
+    baseConfig.external.push("assert", "path");
 
     // Disable tree-shaking of test code.  In rollup-plugin-node-resolve@5.0.0, rollup started respecting
     // the "sideEffects" field in package.json.  Since our package.json sets "sideEffects=false", this also
