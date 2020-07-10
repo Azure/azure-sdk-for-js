e8cbb13c1d5c367796138b11fd6c2f2c14ec7300
SDK Automation <sdkautomation@microsoft.com>
2020-07-10 06:05:13 +0000

    Generated from 022152e8a5160786bcda0b4b12b54a2440c6dd2f
    Remove creationTime

--- sdk/postgresql/arm-postgresql/src/models/serverKeysMappers.ts
+++ sdk/postgresql/arm-postgresql/src/models/serverKeysMappers.ts
@@ -13,6 +13,8 @@ export {
   CloudError,
   Configuration,
   Database,
+  ErrorAdditionalInfo,
+  ErrorResponse,
   FirewallRule,
   LogFile,
   PrivateEndpointConnection,
@@ -26,6 +28,7 @@ export {
   Server,
   ServerAdministratorResource,
   ServerKey,
+  ServerKeyListResult,
   ServerPrivateEndpointConnection,
   ServerPrivateEndpointConnectionProperties,
   ServerPrivateLinkServiceConnectionStateProperty,
@@ -34,6 +37,5 @@ export {
   StorageProfile,
   SystemData,
   TrackedResource,
-  VirtualNetworkRule,
-  VirtualNetworkRuleListResult
+  VirtualNetworkRule
 } from "../models/mappers";
