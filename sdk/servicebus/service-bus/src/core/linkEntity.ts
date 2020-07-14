373c0ae33a728ff0b13e5079a7ef663cb4bbcd95
chradek <chradek@microsoft.com>
2020-07-14 13:17:56 -0700

    clarify comment

--- sdk/servicebus/service-bus/src/core/linkEntity.ts
+++ sdk/servicebus/service-bus/src/core/linkEntity.ts
@@ -157,7 +157,7 @@ export class LinkEntity {
    * @returns {void}
    */
   protected async _ensureTokenRenewal(): Promise<void> {
-    // Clear the existing token renewal timer in case it hasn't already fired.
+    // Clear the existing token renewal timer in case the previous timeout hasn't been reached.
     // This scenario can happen if the connection goes down and is brought back up
     // before the `nextRenewalTimeout` was reached.
     if (this._tokenRenewalTimer) {
