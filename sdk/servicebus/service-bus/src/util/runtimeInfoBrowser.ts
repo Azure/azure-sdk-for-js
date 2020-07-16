2959d77130b65e2c8711702e1e793a9170c67ec9
HarshaNalluru <sanallur@microsoft.com>
2020-07-15 18:27:06 -0700

    runtimeInfo replicas of eevent-hubs

---
+++ sdk/servicebus/service-bus/src/util/runtimeInfoBrowser.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+interface Window {}
+declare let self: Window & typeof globalThis & { navigator: Navigator };
+
+interface Navigator {
+  /**
+   * Returns a string representing the browser version info.
+   */
+  appVersion: string;
+}
+
+/**
+ * Returns information about the platform this function is being run on.
+ * @ignore
+ * @internal
+ */
+export function getRuntimeInfo(): string {
+  return `BROWSER-VERSION; Browser ${getReleaseInfo()}`;
+}
+
+function getReleaseInfo(): string {
+  if (typeof self === "undefined") {
+    return "";
+  }
+
+  const navigator = self.navigator;
+  return navigator.appVersion;
+}
