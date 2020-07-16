c5d5ad4f8ed5b7ada2ffc8eb6288aaa11168c44d
deyaaeldeen <dealmaha@microsoft.com>
2020-07-16 16:01:50 -0400

    remove recording handling configurations from karma configs

--- sdk/template/template/karma.conf.js
+++ sdk/template/template/karma.conf.js
@@ -1,12 +1,6 @@
 // https://github.com/karma-runner/karma-chrome-launcher
 process.env.CHROME_BIN = require("puppeteer").executablePath();
 require("dotenv").config();
-const {
-  jsonRecordingFilterFunction,
-  isPlaybackMode,
-  isSoftRecordMode,
-  isRecordMode
-} = require("@azure/test-utils-recorder");
 
 module.exports = function(config) {
   config.set({
@@ -27,18 +21,14 @@ module.exports = function(config) {
       "karma-env-preprocessor",
       "karma-coverage",
       "karma-remap-istanbul",
-      "karma-junit-reporter",
-      "karma-json-to-file-reporter",
-      "karma-json-preprocessor"
+      "karma-junit-reporter"
     ],
 
     // list of files / patterns to load in the browser
     files: [
       "dist-test/index.browser.js",
       { pattern: "dist-test/index.browser.js.map", type: "html", included: false, served: true }
-    ].concat(
-      isPlaybackMode() || isSoftRecordMode() ? ["recordings/browsers/**/*.json"] : []
-    ),
+    ],
 
     // list of files / patterns to exclude
     exclude: [],
@@ -46,8 +36,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "**/*.js": ["env"],
-      "recordings/browsers/**/*.json": ["json"]
+      "**/*.js": ["env"]
       // IMPORTANT: COMMENT following line if you want to debug in your browsers!!
       // Preprocess source file to calculate code coverage, however this will make source file unreadable
       // "dist-test/index.js": ["coverage"]
@@ -66,7 +55,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["mocha", "coverage", "karma-remap-istanbul", "junit", "json-to-file"],
+    reporters: ["mocha", "coverage", "karma-remap-istanbul", "junit"],
 
     coverageReporter: {
       // specify a common output directory
@@ -92,11 +81,6 @@ module.exports = function(config) {
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
       classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
       properties: {} // key value pair of properties to add to the <properties> section of the report
-    },
-
-    jsonToFileReporter: {
-      filter: jsonRecordingFilterFunction,
-      outputPath: "."
     },
 
     // web server port
@@ -133,9 +117,6 @@ module.exports = function(config) {
     browserNoActivityTimeout: 600000,
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 3,
-    browserConsoleLogOptions: {
-      terminal: !isRecordMode()
-    },
 
     client: {
       mocha: {
