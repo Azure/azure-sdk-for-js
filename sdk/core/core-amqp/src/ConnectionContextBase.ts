5d33944e3c07d12c77054f1ffb55c8852f202ce8
chradek <chradek@microsoft.com>
2020-07-21 15:17:08 -0700

    [core-amqp] make hostname and port configurable

--- sdk/core/core-amqp/src/ConnectionContextBase.ts
+++ sdk/core/core-amqp/src/ConnectionContextBase.ts
@@ -146,9 +146,9 @@ export const ConnectionContextBase = {
     const connectionOptions: ConnectionOptions = {
       transport: Constants.TLS,
       host: parameters.config.host,
-      hostname: parameters.config.host,
+      hostname: parameters.config.hostname ?? parameters.config.host,
       username: parameters.config.sharedAccessKeyName,
-      port: 5671,
+      port: parameters.config.port ?? 5671,
       reconnect: false,
       properties: {
         product: parameters.connectionProperties.product,
