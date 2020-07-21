bdd1fb76d68de19ca8bd967a4a85b9b5fd9580f1
SDK Automation <sdkautomation@microsoft.com>
2020-07-21 03:06:15 +0000

    Generated from ee57324bf35a286b432daecf1c5a56289f4e94ef
    Fix duplicate parameters for CDN API versions with WAF.

--- sdk/cdn/arm-cdn/src/cdnManagementClientContext.ts
+++ sdk/cdn/arm-cdn/src/cdnManagementClientContext.ts
@@ -44,7 +44,7 @@ export class CdnManagementClientContext extends msRestAzure.AzureServiceClient {
 
     super(credentials, options);
 
-    this.apiVersion = '2017-10-12';
+    this.apiVersion = '2020-04-15';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
