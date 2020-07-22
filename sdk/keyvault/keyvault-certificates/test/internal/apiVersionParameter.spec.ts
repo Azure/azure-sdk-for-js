9f6ca774dd59bee1c49ee28f0ce634027b07fa04
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-07-22 21:50:34 +0000

    using the httpClient pipeline parameter, and LATEST_API_VERSION

--- sdk/keyvault/keyvault-certificates/test/internal/apiVersionParameter.spec.ts
+++ sdk/keyvault/keyvault-certificates/test/internal/apiVersionParameter.spec.ts
@@ -4,50 +4,30 @@
 import * as assert from "assert";
 import { createSandbox } from "sinon";
 import { CertificateClient } from "../../src";
-import { ServiceClient } from "@azure/core-http";
+import { LATEST_API_VERSION } from "../../src/certificatesModels";
+import { HttpClient, WebResourceLike, HttpOperationResponse, HttpHeaders } from "@azure/core-http";
 import { ClientSecretCredential } from "@azure/identity";
 import { env } from "@azure/test-utils-recorder";
-import { testPollerProperties } from "../utils/recorderUtils";
 
 describe("The Certificates client should set the apiVersion", () => {
   const keyVaultUrl = `https://keyVaultName.vault.azure.net`;
-
-  const basicCertificatePolicy = {
-    issuerName: "Self",
-    subject: "cn=MyCert"
+  const mockHttpClient: HttpClient = {
+    async sendRequest(httpRequest: WebResourceLike): Promise<HttpOperationResponse> {
+      return {
+        status: 200,
+        headers: new HttpHeaders(),
+        request: httpRequest,
+        parsedBody: {
+          id: `${keyVaultUrl}/certificates/certificateName/id`,
+          attributes: {}
+        }
+      };
+    }
   };
 
   it("it should default to the latest API version", async function() {
     const sandbox = createSandbox();
-    const spy = sandbox.spy(ServiceClient.prototype, "sendRequest");
-
-    const credential = await new ClientSecretCredential(
-      env.AZURE_TENANT_ID!,
-      env.AZURE_CLIENT_ID!,
-      env.AZURE_CLIENT_SECRET!
-    );
-    const client = new CertificateClient(keyVaultUrl, credential);
-    try {
-      const poller = await client.beginCreateCertificate(
-        "certificateName",
-        basicCertificatePolicy,
-        testPollerProperties
-      );
-      await poller.pollUntilDone();
-    } catch {}
-
-    const calls = spy.getCalls();
-    assert.equal(
-      calls[0].args[0].url,
-      "https://keyVaultName.vault.azure.net/certificates/certificateName/create?api-version=7.1-preview"
-    );
-
-    sandbox.restore();
-  });
-
-  it("it should allow us to specify an API version", async function() {
-    const sandbox = createSandbox();
-    const spy = sandbox.spy(ServiceClient.prototype, "sendRequest");
+    const spy = sandbox.spy(mockHttpClient, "sendRequest");
 
     const credential = await new ClientSecretCredential(
       env.AZURE_TENANT_ID!,
@@ -55,22 +35,38 @@ describe("The Certificates client should set the apiVersion", () => {
       env.AZURE_CLIENT_SECRET!
     );
     const client = new CertificateClient(keyVaultUrl, credential, {
-      apiVersion: "7.0"
+      httpClient: mockHttpClient
     });
-
-    try {
-      const poller = await client.beginCreateCertificate(
-        "certificateName",
-        basicCertificatePolicy,
-        testPollerProperties
-      );
-      await poller.pollUntilDone();
-    } catch {}
+    await client.getCertificate("certificateName");
 
     const calls = spy.getCalls();
     assert.equal(
       calls[0].args[0].url,
-      "https://keyVaultName.vault.azure.net/certificates/certificateName/create?api-version=7.0"
+      `https://keyVaultName.vault.azure.net/certificates/certificateName/?api-version=${LATEST_API_VERSION}`
+    );
+
+    sandbox.restore();
+  });
+
+  it("it should allow us to specify an API version", async function() {
+    const sandbox = createSandbox();
+    const spy = sandbox.spy(mockHttpClient, "sendRequest");
+
+    const credential = await new ClientSecretCredential(
+      env.AZURE_TENANT_ID!,
+      env.AZURE_CLIENT_ID!,
+      env.AZURE_CLIENT_SECRET!
+    );
+    const client = new CertificateClient(keyVaultUrl, credential, {
+      apiVersion: "7.0",
+      httpClient: mockHttpClient
+    });
+    await client.getCertificate("certificateName");
+
+    const calls = spy.getCalls();
+    assert.equal(
+      calls[0].args[0].url,
+      "https://keyVaultName.vault.azure.net/certificates/certificateName/?api-version=7.0"
     );
 
     sandbox.restore();
--- sdk/keyvault/keyvault-keys/test/internal/apiVersionParameter.spec.ts
+++ sdk/keyvault/keyvault-keys/test/internal/apiVersionParameter.spec.ts
@@ -4,39 +4,31 @@
 import * as assert from "assert";
 import { createSandbox } from "sinon";
 import { KeyClient } from "../../src";
-import { ServiceClient } from "@azure/core-http";
+import { LATEST_API_VERSION } from "../../src/keysModels";
+import { HttpClient, HttpOperationResponse, WebResourceLike, HttpHeaders } from "@azure/core-http";
 import { ClientSecretCredential } from "@azure/identity";
 import { env } from "@azure/test-utils-recorder";
 
 describe("The Keys client should set the apiVersion", () => {
   const keyVaultUrl = `https://keyVaultName.vault.azure.net`;
+  const mockHttpClient: HttpClient = {
+    async sendRequest(httpRequest: WebResourceLike): Promise<HttpOperationResponse> {
+      return {
+        status: 200,
+        headers: new HttpHeaders(),
+        request: httpRequest,
+        parsedBody: {
+          key: {
+            kid: `${keyVaultUrl}/keys/keyName/id`
+          }
+        }
+      };
+    }
+  };
 
   it("it should default to the latest API version", async function() {
     const sandbox = createSandbox();
-    const spy = sandbox.spy(ServiceClient.prototype, "sendRequest");
-
-    const credential = await new ClientSecretCredential(
-      env.AZURE_TENANT_ID!,
-      env.AZURE_CLIENT_ID!,
-      env.AZURE_CLIENT_SECRET!
-    );
-    const client = new KeyClient(keyVaultUrl, credential);
-    try {
-      await client.createKey("keyName", "RSA");
-    } catch {}
-
-    const calls = spy.getCalls();
-    assert.equal(
-      calls[0].args[0].url,
-      "https://keyVaultName.vault.azure.net/keys/keyName/create?api-version=7.1-preview"
-    );
-
-    sandbox.restore();
-  });
-
-  it("it should allow us to specify an API version", async function() {
-    const sandbox = createSandbox();
-    const spy = sandbox.spy(ServiceClient.prototype, "sendRequest");
+    const spy = sandbox.spy(mockHttpClient, "sendRequest");
 
     const credential = await new ClientSecretCredential(
       env.AZURE_TENANT_ID!,
@@ -44,12 +36,33 @@ describe("The Keys client should set the apiVersion", () => {
       env.AZURE_CLIENT_SECRET!
     );
     const client = new KeyClient(keyVaultUrl, credential, {
-      apiVersion: "7.0"
+      httpClient: mockHttpClient
     });
+    await client.createKey("keyName", "RSA");
 
-    try {
-      await client.createKey("keyName", "RSA");
-    } catch {}
+    const calls = spy.getCalls();
+    assert.equal(
+      calls[0].args[0].url,
+      `https://keyVaultName.vault.azure.net/keys/keyName/create?api-version=${LATEST_API_VERSION}`
+    );
+
+    sandbox.restore();
+  });
+
+  it("it should allow us to specify an API version", async function() {
+    const sandbox = createSandbox();
+    const spy = sandbox.spy(mockHttpClient, "sendRequest");
+
+    const credential = await new ClientSecretCredential(
+      env.AZURE_TENANT_ID!,
+      env.AZURE_CLIENT_ID!,
+      env.AZURE_CLIENT_SECRET!
+    );
+    const client = new KeyClient(keyVaultUrl, credential, {
+      apiVersion: "7.0",
+      httpClient: mockHttpClient
+    });
+    await client.createKey("keyName", "RSA");
 
     const calls = spy.getCalls();
     assert.equal(
