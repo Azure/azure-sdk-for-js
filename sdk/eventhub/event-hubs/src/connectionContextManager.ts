0e839a04ca2a2197bdc001cac738351410539d38
chradek <chradek@microsoft.com>
2020-07-21 17:46:52 -0700

    missed a copyright

--- sdk/eventhub/event-hubs/src/connectionContextManager.ts
+++ sdk/eventhub/event-hubs/src/connectionContextManager.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { ConnectionContext, ConnectionContextConfig } from "./connectionContext";
 import {
   ConnectionConfig,
