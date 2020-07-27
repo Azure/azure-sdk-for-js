dfeea3bae677c62a3d9084da5c97b4db571d506b
deyaaeldeen <dealmaha@microsoft.com>
2020-07-26 23:34:26 -0400

    [Text Analytics] bump version number to 5.0.0

--- sdk/textanalytics/ai-text-analytics/src/generated/generatedClientContext.ts
+++ sdk/textanalytics/ai-text-analytics/src/generated/generatedClientContext.ts
@@ -10,7 +10,7 @@ import * as coreHttp from "@azure/core-http";
 import { GeneratedClientOptionalParams } from "./models";
 
 const packageName = "@azure/ai-text-analytics";
-const packageVersion = "1.0.1";
+const packageVersion = "5.0.0";
 
 export class GeneratedClientContext extends coreHttp.ServiceClient {
   endpoint: string;
