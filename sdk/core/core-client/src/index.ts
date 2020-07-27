a5fda325bdeb1ef5b866a2b52a4670884276609e
Jeff Fisher <jeffish@microsoft.com>
2020-07-27 13:28:11 -0700

    [core-client] Updated ServiceClient implementation to work with core-https (#9854)
    First cut of ServiceClient to support new codegen

--- sdk/core/core-client/src/index.ts
+++ sdk/core/core-client/src/index.ts
@@ -1,6 +1,42 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export function helloWorld(): string {
-  return "Hello world!";
-}
+export { createSerializer, MapperTypeNames } from "./serializer";
+export { ServiceClient, ServiceClientOptions } from "./serviceClient";
+export {
+  OperationSpec,
+  OperationArguments,
+  OperationOptions,
+  OperationResponseMap,
+  OperationParameter,
+  OperationQueryParameter,
+  OperationURLParameter,
+  Serializer,
+  BaseMapper,
+  Mapper,
+  MapperType,
+  SimpleMapperType,
+  EnumMapper,
+  EnumMapperType,
+  SequenceMapper,
+  SequenceMapperType,
+  DictionaryMapper,
+  DictionaryMapperType,
+  CompositeMapper,
+  CompositeMapperType,
+  MapperConstraints,
+  OperationRequest,
+  OperationRequestOptions,
+  OperationRequestInfo,
+  QueryCollectionFormat,
+  ParameterPath,
+  OperationResponse,
+  FullOperationResponse,
+  PolymorphicDiscriminator
+} from "./interfaces";
+export {
+  deserializationPolicy,
+  deserializationPolicyName,
+  DeserializationPolicyOptions,
+  DeserializationContentTypes
+} from "./deserializationPolicy";
