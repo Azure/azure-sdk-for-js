ebfcf54e0ca740f4879ae77a0c747701f112608d
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2020-07-28 11:27:02 -0400

    [Key Vault] formatting (#10265)

--- sdk/keyvault/keyvault-keys/src/index.ts
+++ sdk/keyvault/keyvault-keys/src/index.ts
@@ -239,10 +239,7 @@ export class KeyClient {
     };
 
     const pipeline = createPipelineFromOptions(internalPipelineOptions, authPolicy);
-    this.client = new KeyVaultClient(
-      pipelineOptions.apiVersion || LATEST_API_VERSION,
-      pipeline
-    );
+    this.client = new KeyVaultClient(pipelineOptions.apiVersion || LATEST_API_VERSION, pipeline);
   }
 
   /**
