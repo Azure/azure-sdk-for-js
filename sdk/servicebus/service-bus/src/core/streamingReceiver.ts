8cecfe5d11925af50c12e5340bb2ed75d27f8a68
Richard Park <richard.park@microsoft.com>
2020-08-05 14:18:57 -0700

    * Removing unneeded this.isReceivingMessages set. * Removing code that is literally gated to only run in batching but is only used in streamingreceiver.

--- sdk/servicebus/service-bus/src/core/streamingReceiver.ts
+++ sdk/servicebus/service-bus/src/core/streamingReceiver.ts
@@ -568,23 +568,11 @@ export class StreamingReceiver extends MessageReceiver {
     }
 
     this._isDetaching = true;
-    this.isReceivingMessages = false;
 
     try {
       // Clears the token renewal timer. Closes the link and its session if they are open.
       // Removes the link and its session if they are present in rhea's cache.
       await this._closeLink(this._receiver);
-
-      if (this.receiverType === ReceiverType.batching) {
-        log.error(
-          "[%s] Receiver '%s' with address '%s' is a Batching Receiver, so we will not be " +
-            "re-establishing the receiver link.",
-          connectionId,
-          this.name,
-          this.address
-        );
-        return;
-      }
 
       const translatedError = receiverError ? translate(receiverError) : receiverError;
 
