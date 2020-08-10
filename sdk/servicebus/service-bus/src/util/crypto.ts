a9f7b739d39332a2d68da9f643ac3e4bbb371a29
Harsha Nalluru <sanallur@microsoft.com>
2020-08-10 15:22:08 -0700

    Update sdk/servicebus/service-bus/src/util/crypto.ts
    Co-authored-by: Ramya Rao <ramya.rao.a@outlook.com>

--- sdk/servicebus/service-bus/src/util/crypto.ts
+++ sdk/servicebus/service-bus/src/util/crypto.ts
@@ -9,8 +9,6 @@ import crypto from "crypto";
 /**
  * @internal
  * @ignore
- * @param {string} secret
- * @param {string} stringToSign
  */
 export async function generateKey(secret: string, stringToSign: string) {
   const result = encodeURIComponent(
