1b4653fd0bce456b38bd8631e577305886adb913
Will Temple <witemple@microsoft.com>
2020-08-10 13:50:09 -0700

    [ai-form-recognizer] Final changes for 3.0.0-preview.1 (#10538)
    * [ai-form-recognizer] Final changes for 3.0.0-preview.1
    
    * Fixed actual package versions

--- sdk/formrecognizer/ai-form-recognizer/src/constants.ts
+++ sdk/formrecognizer/ai-form-recognizer/src/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const SDK_VERSION: string = "3.0.0";
+export const SDK_VERSION: string = "3.0.0-preview.1";
 
 export const DEFAULT_COGNITIVE_SCOPE = "https://cognitiveservices.azure.com/.default";
 
