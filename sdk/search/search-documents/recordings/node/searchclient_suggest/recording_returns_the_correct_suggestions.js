75cd79a3abbd05f05e801b67dc1993659a2b808e
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-08-10 02:53:32 -0700

    Added recordings

---
+++ sdk/search/search-documents/recordings/node/searchclient_suggest/recording_returns_the_correct_suggestions.js
@@ -0,0 +1,35 @@
+let nock = require('nock');
+
+module.exports.hash = "f8943716b2474071efe9757c56a01bfc";
+
+module.exports.testInfo = {"uniqueName":{},"newDate":{}}
+
+nock('https://endpoint:443', {"encodedQueryParams":true})
+  .post('/indexes(%27hotel-live-test%27)/docs/search.post.suggest', {"search":"wifi","suggesterName":"sg"})
+  .query(true)
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef1479759b9ce3f7af4bd5ffcd1efd9e4593d9d8fdbfc5dfbd1a38f5e679779ba5ea56d95dedff9ddd3eafc3c6deb6c56b445b5ccca745eb579d98cd3f4599de7e9778b67c528bda8f3ac4dcb6a9aa151ba2478e9acba5ab6f4ff517abe2ecbf46dd14ee739fd759535f3bc4e7f613aabaff37a94eeeddf7d9036ebd5aaaadb513aa9aeca6279916665995fd3ab45bbcc9b269de6cb965eca96b37451d5f9f8a3d1478cc7d98c10deddf9e8977cff97fc3f4bd5b195d2000000"], [ 'Cache-Control',
+  'no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Type',
+  'application/json; odata.metadata=none',
+  'Content-Encoding',
+  'gzip',
+  'Expires',
+  '-1',
+  'Vary',
+  'Accept-Encoding',
+  'request-id',
+  '7e85fa5a-2637-48e3-bfeb-f0475b0635e0',
+  'elapsed-time',
+  '19',
+  'OData-Version',
+  '4.0',
+  'Preference-Applied',
+  'odata.include-annotations="*"',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains',
+  'Date',
+  'Mon, 10 Aug 2020 10:01:39 GMT',
+  'Content-Length',
+  '295' ]);
