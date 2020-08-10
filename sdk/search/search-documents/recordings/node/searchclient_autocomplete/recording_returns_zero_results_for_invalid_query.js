50e2d1296b1e21274add106670494d359b57e1b7
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-08-10 14:56:35 -0700

    Added Test cases for Search Client (#10530)
    * Added Test cases for Search Client
    
    * Added recordings
    
    * Test with Harmony Flag
    
    * Update sdk/search/search-documents/test/node/searchIndexClient.spec.ts
    
    Co-authored-by: Jeff Fisher <xirzec@xirzec.com>
    
    * Code changes for PR comments
    
    Co-authored-by: Jeff Fisher <xirzec@xirzec.com>

---
+++ sdk/search/search-documents/recordings/node/searchclient_autocomplete/recording_returns_zero_results_for_invalid_query.js
@@ -0,0 +1,35 @@
+let nock = require('nock');
+
+module.exports.hash = "bca146556b72037d1a99cb733d1c9896";
+
+module.exports.testInfo = {"uniqueName":{},"newDate":{}}
+
+nock('https://endpoint:443', {"encodedQueryParams":true})
+  .post('/indexes(%27hotel-live-test%27)/docs/search.post.autocomplete', {"search":"garbxyz","suggesterName":"sg"})
+  .query(true)
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef1479759b9ce3f7af4bdefff92ff0742ea40440c000000"], [ 'Cache-Control',
+  'no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Type',
+  'application/json; odata.metadata=none',
+  'Content-Encoding',
+  'gzip',
+  'Expires',
+  '-1',
+  'Vary',
+  'Accept-Encoding',
+  'request-id',
+  '209086c5-5b15-4043-8c66-006bda9804bb',
+  'elapsed-time',
+  '5',
+  'OData-Version',
+  '4.0',
+  'Preference-Applied',
+  'odata.include-annotations="*"',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains',
+  'Date',
+  'Mon, 10 Aug 2020 10:01:29 GMT',
+  'Content-Length',
+  '133' ]);
---
+++ sdk/search/search-documents/recordings/node/searchclient_mergedocuments/recording_modify__merge_an_existing_document.js
@@ -0,0 +1,95 @@
+let nock = require('nock');
+
+module.exports.hash = "ed05cf680d464de9c52d439341d217a1";
+
+module.exports.testInfo = {"uniqueName":{},"newDate":{}}
+
+nock('https://endpoint:443', {"encodedQueryParams":true})
+  .get('/indexes(%27hotel-live-test%27)/docs(%276%27)')
+  .query(true)
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147f3aacdcbb3d9478f3efaf4a391fcf5225be41f3d5aaecb72f4d12c6fa675b16a8b6a494d5eafeb555d34c5f2a2bc4ef377ab7cd91497f938fda29ae565daac8b366fd2797699a7d932ada679b6dcbe2cf2ab3141f6003dab0df469d6e617557d6dfe6eb38be6a347dffbfee8a35556bfa56ece96d3723dcb093d69d02c2a7c7c5c96d595fbb4cc9af655beac2e33807f4a40cd37357db2bc307f951575482dccdfd96c56e70df5287fd655b5a03fbef7fd5ff2ff00978a208417010000"], [ 'Cache-Control',
+  'no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Type',
+  'application/json; odata.metadata=none',
+  'Content-Encoding',
+  'gzip',
+  'Expires',
+  '-1',
+  'Vary',
+  'Accept-Encoding',
+  'request-id',
+  'f1d30661-2b8f-4336-b666-b54b6fcdedb4',
+  'elapsed-time',
+  '8',
+  'OData-Version',
+  '4.0',
+  'Preference-Applied',
+  'odata.include-annotations="*"',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains',
+  'Date',
+  'Mon, 10 Aug 2020 10:02:15 GMT',
+  'Content-Length',
+  '301' ]);
+
+nock('https://endpoint:443', {"encodedQueryParams":true})
+  .post('/indexes(%27hotel-live-test%27)/docs/search.index', {"value":[{"@search.action":"merge","hotelId":"6","hotelName":null,"description":"Modified Description","descriptionFr":null,"category":null,"tags":[],"parkingIncluded":null,"smokingAllowed":null,"lastRenovationDate":null,"rating":null,"location":null,"address":null,"rooms":[]}]})
+  .query(true)
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef1479759b9ce3f7af4bd5ffcd1dbfcfaa3471f7dfad1e8a3a6cdda75f3d1a3b65ee7a38ff2baaeea2ff2a6c92ea8e1725d96a6c54935a34ff676767ec9f77fc9ff037255c7d34a000000"], [ 'Cache-Control',
+  'no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Type',
+  'application/json; odata.metadata=none',
+  'Content-Encoding',
+  'gzip',
+  'Expires',
+  '-1',
+  'Vary',
+  'Accept-Encoding',
+  'request-id',
+  'd8447298-e06c-469d-bc15-3f1923e131b0',
+  'elapsed-time',
+  '24',
+  'OData-Version',
+  '4.0',
+  'Preference-Applied',
+  'odata.include-annotations="*"',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains',
+  'Date',
+  'Mon, 10 Aug 2020 10:02:15 GMT',
+  'Content-Length',
+  '184' ]);
+
+nock('https://endpoint:443', {"encodedQueryParams":true})
+  .get('/indexes(%27hotel-live-test%27)/docs(%276%27)')
+  .query(true)
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147f3aacdcbb3d9478f3efaf4a391fcf5225be41f3d5aaecb72f4d12c6fa675b16a8b6a494dbea866c57991cfd2a7dec741a367b579739ab5f945555f9bbfdbeca2f9e8d1f7be3ffa6895d56f8be5c5d9725aae6739752d0d9a45858f8fcbb2ba729f9659d3beca97d56506f04f09a8f9a6a64f9617e6afb2a20ea985f93b9bcdeabca11ee5cfbaaa16f4c7f7beff4bfe1fcc8e7273f3000000"], [ 'Cache-Control',
+  'no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Type',
+  'application/json; odata.metadata=none',
+  'Content-Encoding',
+  'gzip',
+  'Expires',
+  '-1',
+  'Vary',
+  'Accept-Encoding',
+  'request-id',
+  'c103918c-b352-4e30-8bf9-30357430d523',
+  'elapsed-time',
+  '6',
+  'OData-Version',
+  '4.0',
+  'Preference-Applied',
+  'odata.include-annotations="*"',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains',
+  'Date',
+  'Mon, 10 Aug 2020 10:02:17 GMT',
+  'Content-Length',
+  '264' ]);
