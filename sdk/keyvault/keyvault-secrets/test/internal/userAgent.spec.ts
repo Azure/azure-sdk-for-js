5cacf83ab3c619f5d7873f452fd75a241a045a2e
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-08-11 22:53:54 +0000

    SDK_VERSION and packageVersion should match in the browsers too

--- sdk/keyvault/keyvault-secrets/test/internal/userAgent.spec.ts
+++ sdk/keyvault/keyvault-secrets/test/internal/userAgent.spec.ts
@@ -5,20 +5,19 @@ import * as assert from "assert";
 import { SDK_VERSION } from "../../src/generated/utils/constants";
 import { packageVersion } from "../../src/generated/keyVaultClientContext";
 import { isNode } from '@azure/core-http';
-import * as fs from "fs";
+import fs from "fs";
 
 describe("Secrets client's user agent (only in Node, because of fs)", () => {
-  if (!isNode) {
-    return;
-  }
-
-  const { version } = JSON.parse(fs.readFileSync(__dirname + "/../package.json", { encoding: "utf-8" }))
-
-  it("the version at constants should be as expected", async function() {
-    assert.equal(version, SDK_VERSION);
+  it("SDK_VERSION and packageVersion should match", async function() {
+    assert.equal(SDK_VERSION, packageVersion);
   });
 
-  it("the version at KeyVaultClientContext should be as expected", async function() {
+  it("the version should also match with the one available in the package.json  (only in Node, because of fs)", async function() {
+    if (!isNode) {
+      this.skip();
+      return;
+    }
+    const { version } = JSON.parse(fs.readFileSync(__dirname + "/../package.json", { encoding: "utf-8" }))
     assert.equal(version, packageVersion);
   });
 });
