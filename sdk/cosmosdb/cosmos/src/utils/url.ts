36e16d82108b1e208e67b9b2e968a4126c3f4d11
Steve Faulkner <southpolesteve@gmail.com>
2020-08-12 18:16:43 -0500

    Add TODO

--- sdk/cosmosdb/cosmos/src/utils/url.ts
+++ sdk/cosmosdb/cosmos/src/utils/url.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 export function checkURL(testString: string): URL {
   let _URL;
+  // TODO: Remove this when we drop Node 8 support. URL is global in Node 10+
   if (typeof URL === "undefined") {
     // tslint:disable-next-line:no-var-requires
     _URL = require("url").URL;
