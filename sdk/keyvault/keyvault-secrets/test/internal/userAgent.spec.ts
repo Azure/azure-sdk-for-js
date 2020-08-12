ccb00a18fb43823d135a68c4f26b9b94a26fd9fe
Daniel Rodríguez <sadasant@users.noreply.github.com>
2020-08-11 20:13:01 -0400

    [Key Vault] Fixing bad dist-esm folders on the resulting artifacts (#10576)
    * fixing the bad dist-esm folders on the resulting artifacts
    
    By loading the package.json in the test files, the dist-esm folder was
    including a copy of the package.json, breaking the whole folder (thus
    also source maps, etc).
    
    * SDK_VERSION and packageVersion should match in the browsers too
    
    * formatting
    
    * using path instead of concatenations

--- sdk/keyvault/keyvault-secrets/test/internal/userAgent.spec.ts
+++ sdk/keyvault/keyvault-secrets/test/internal/userAgent.spec.ts
@@ -2,18 +2,25 @@
 // Licensed under the MIT license.
 
 import * as assert from "assert";
-import { version } from "../../package.json";
 import { SDK_VERSION } from "../../src/generated/utils/constants";
 import { packageVersion } from "../../src/generated/keyVaultClientContext";
+import { isNode } from "@azure/core-http";
+import path from "path";
+import fs from "fs";
 
-describe("Secrets client's user agent", () => {
-  // The tests follow
-
-  it("the version at constants should be as expected", async function() {
-    assert.equal(version, SDK_VERSION);
+describe("Secrets client's user agent (only in Node, because of fs)", () => {
+  it("SDK_VERSION and packageVersion should match", async function() {
+    assert.equal(SDK_VERSION, packageVersion);
   });
 
-  it("the version at KeyVaultClientContext should be as expected", async function() {
+  it("the version should also match with the one available in the package.json  (only in Node, because of fs)", async function() {
+    if (!isNode) {
+      this.skip();
+      return;
+    }
+    const { version } = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../package.json"), { encoding: "utf-8" })
+    );
     assert.equal(version, packageVersion);
   });
 });
