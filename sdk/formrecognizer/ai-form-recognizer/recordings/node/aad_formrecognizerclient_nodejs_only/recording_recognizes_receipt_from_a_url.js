b58608dff6b67e54f488138c2b6c727e7a596d09
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-08-13 22:05:14 +0000

    updates after fixing conflicts

--- sdk/formrecognizer/ai-form-recognizer/recordings/node/aad_formrecognizerclient_nodejs_only/recording_recognizes_receipt_from_a_url.js
+++ sdk/formrecognizer/ai-form-recognizer/recordings/node/aad_formrecognizerclient_nodejs_only/recording_recognizes_receipt_from_a_url.js
@@ -11,6 +11,8 @@ nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
   'no-store, no-cache',
   'Pragma',
   'no-cache',
+  'Content-Length',
+  '1417',
   'Content-Type',
   'application/json; charset=utf-8',
   'Expires',
@@ -22,19 +24,17 @@ nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
   'P3P',
   'CP="DSP CUR OTPi IND OTRi ONL FIN"',
   'x-ms-request-id',
-  '9d9bb083-1a35-423f-af18-9ec7511d1e00',
+  '3cb431e7-31ab-437a-a7df-e21da3734001',
   'x-ms-ests-server',
-  '2.1.10922.14 - WUS2 ProdSlices',
+  '2.1.10922.14 - SAN ProdSlices',
   'Set-Cookie',
-  'fpc=Aq2N0b5lmWxNh4O31UGv_Br0CyfMAQAAAL43vdYOAAAA; expires=Fri, 04-Sep-2020 23:29:02 GMT; path=/; secure; HttpOnly; SameSite=None',
+  'fpc=AoPJ8EoLo1xEg2FbIpX38_P0CyfMAQAAAO-tx9YOAAAA; expires=Sat, 12-Sep-2020 21:55:59 GMT; path=/; secure; HttpOnly; SameSite=None',
   'Set-Cookie',
   'x-ms-gateway-slice=prod; path=/; secure; samesite=none; httponly',
   'Set-Cookie',
   'stsservicecookie=ests; path=/; secure; samesite=none; httponly',
   'Date',
-  'Wed, 05 Aug 2020 23:29:02 GMT',
-  'Content-Length',
-  '1417'
+  'Thu, 13 Aug 2020 21:55:59 GMT'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
@@ -43,72 +43,171 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Content-Length',
   '0',
   'Operation-Location',
-  'https://endpoint/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/df67d45f-07f3-48e8-9ed4-580d608e1873',
+  'https://endpoint/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/d254705d-a6e6-4dc0-a818-3d777499dd81',
   'x-envoy-upstream-service-time',
-  '162',
+  '886',
   'apim-request-id',
-  'df67d45f-07f3-48e8-9ed4-580d608e1873',
+  'd254705d-a6e6-4dc0-a818-3d777499dd81',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Wed, 05 Aug 2020 23:29:02 GMT'
+  'Thu, 13 Aug 2020 21:56:00 GMT'
+]);
+
+nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
+  .post('/azure_tenant_id/oauth2/v2.0/token', "response_type=token&grant_type=client_credentials&client_id=azure_client_id&client_secret=azure_client_secret&scope=https%3A%2F%2Fcognitiveservices.azure.com%2F.default")
+  .reply(200, {"token_type":"Bearer","expires_in":3599,"ext_expires_in":3599,"access_token":"access_token"}, [
+  'Cache-Control',
+  'no-store, no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Length',
+  '1417',
+  'Content-Type',
+  'application/json; charset=utf-8',
+  'Expires',
+  '-1',
+  'Strict-Transport-Security',
+  'max-age=31536000; includeSubDomains',
+  'X-Content-Type-Options',
+  'nosniff',
+  'P3P',
+  'CP="DSP CUR OTPi IND OTRi ONL FIN"',
+  'x-ms-request-id',
+  '662fd98c-66d6-439d-9d72-82df3f894601',
+  'x-ms-ests-server',
+  '2.1.10922.14 - SAN ProdSlices',
+  'Set-Cookie',
+  'fpc=AoPJ8EoLo1xEg2FbIpX38_P0CyfMAgAAAO-tx9YOAAAA; expires=Sat, 12-Sep-2020 21:56:01 GMT; path=/; secure; HttpOnly; SameSite=None',
+  'Set-Cookie',
+  'x-ms-gateway-slice=prod; path=/; secure; samesite=none; httponly',
+  'Set-Cookie',
+  'stsservicecookie=ests; path=/; secure; samesite=none; httponly',
+  'Date',
+  'Thu, 13 Aug 2020 21:56:01 GMT'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/df67d45f-07f3-48e8-9ed4-580d608e1873')
-  .reply(200, {"status":"running","createdDateTime":"2020-08-05T23:29:03Z","lastUpdatedDateTime":"2020-08-05T23:29:03Z"}, [
+  .get('/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/d254705d-a6e6-4dc0-a818-3d777499dd81')
+  .reply(200, {"status":"running","createdDateTime":"2020-08-13T21:56:00Z","lastUpdatedDateTime":"2020-08-13T21:56:01Z"}, [
   'Transfer-Encoding',
   'chunked',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-envoy-upstream-service-time',
-  '17',
+  '66',
   'apim-request-id',
-  '5191489b-dfac-453b-b435-e3155658da5e',
+  '35cf59b3-7d71-45ac-9589-3bbb29039dcc',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Wed, 05 Aug 2020 23:29:03 GMT'
+  'Thu, 13 Aug 2020 21:56:00 GMT'
+]);
+
+nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
+  .post('/azure_tenant_id/oauth2/v2.0/token', "response_type=token&grant_type=client_credentials&client_id=azure_client_id&client_secret=azure_client_secret&scope=https%3A%2F%2Fcognitiveservices.azure.com%2F.default")
+  .reply(200, {"token_type":"Bearer","expires_in":3599,"ext_expires_in":3599,"access_token":"access_token"}, [
+  'Cache-Control',
+  'no-store, no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Length',
+  '1417',
+  'Content-Type',
+  'application/json; charset=utf-8',
+  'Expires',
+  '-1',
+  'Strict-Transport-Security',
+  'max-age=31536000; includeSubDomains',
+  'X-Content-Type-Options',
+  'nosniff',
+  'P3P',
+  'CP="DSP CUR OTPi IND OTRi ONL FIN"',
+  'x-ms-request-id',
+  '1693dbd3-d5f9-4eb2-aef4-894020535301',
+  'x-ms-ests-server',
+  '2.1.10922.14 - SAN ProdSlices',
+  'Set-Cookie',
+  'fpc=AoPJ8EoLo1xEg2FbIpX38_P0CyfMAwAAAO-tx9YOAAAA; expires=Sat, 12-Sep-2020 21:56:01 GMT; path=/; secure; HttpOnly; SameSite=None',
+  'Set-Cookie',
+  'x-ms-gateway-slice=prod; path=/; secure; samesite=none; httponly',
+  'Set-Cookie',
+  'stsservicecookie=ests; path=/; secure; samesite=none; httponly',
+  'Date',
+  'Thu, 13 Aug 2020 21:56:01 GMT'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/df67d45f-07f3-48e8-9ed4-580d608e1873')
-  .reply(200, {"status":"running","createdDateTime":"2020-08-05T23:29:03Z","lastUpdatedDateTime":"2020-08-05T23:29:03Z"}, [
+  .get('/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/d254705d-a6e6-4dc0-a818-3d777499dd81')
+  .reply(200, {"status":"running","createdDateTime":"2020-08-13T21:56:00Z","lastUpdatedDateTime":"2020-08-13T21:56:01Z"}, [
   'Transfer-Encoding',
   'chunked',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-envoy-upstream-service-time',
-  '13',
+  '131',
   'apim-request-id',
-  'efad5340-e541-406f-8c36-05b6fa421c67',
+  '937ac6e9-e131-4ac0-85ca-12f3e56601b2',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Wed, 05 Aug 2020 23:29:03 GMT'
+  'Thu, 13 Aug 2020 21:56:00 GMT'
+]);
+
+nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
+  .post('/azure_tenant_id/oauth2/v2.0/token', "response_type=token&grant_type=client_credentials&client_id=azure_client_id&client_secret=azure_client_secret&scope=https%3A%2F%2Fcognitiveservices.azure.com%2F.default")
+  .reply(200, {"token_type":"Bearer","expires_in":3599,"ext_expires_in":3599,"access_token":"access_token"}, [
+  'Cache-Control',
+  'no-store, no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Length',
+  '1417',
+  'Content-Type',
+  'application/json; charset=utf-8',
+  'Expires',
+  '-1',
+  'Strict-Transport-Security',
+  'max-age=31536000; includeSubDomains',
+  'X-Content-Type-Options',
+  'nosniff',
+  'P3P',
+  'CP="DSP CUR OTPi IND OTRi ONL FIN"',
+  'x-ms-request-id',
+  '662fd98c-66d6-439d-9d72-82df6e8a4601',
+  'x-ms-ests-server',
+  '2.1.10922.14 - SAN ProdSlices',
+  'Set-Cookie',
+  'fpc=AoPJ8EoLo1xEg2FbIpX38_P0CyfMBAAAAO-tx9YOAAAA; expires=Sat, 12-Sep-2020 21:56:06 GMT; path=/; secure; HttpOnly; SameSite=None',
+  'Set-Cookie',
+  'x-ms-gateway-slice=prod; path=/; secure; samesite=none; httponly',
+  'Set-Cookie',
+  'stsservicecookie=ests; path=/; secure; samesite=none; httponly',
+  'Date',
+  'Thu, 13 Aug 2020 21:56:06 GMT'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/df67d45f-07f3-48e8-9ed4-580d608e1873')
-  .reply(200, {"status":"succeeded","createdDateTime":"2020-08-05T23:29:03Z","lastUpdatedDateTime":"2020-08-05T23:29:04Z","analyzeResult":{"version":"2.0.0","readResults":[{"page":1,"angle":0.6893,"width":1688,"height":3000,"unit":"pixel","language":"en"}],"documentResults":[{"docType":"prebuilt:receipt","pageRange":[1,1],"fields":{"ReceiptType":{"type":"string","valueString":"Itemized","confidence":0.692},"MerchantName":{"type":"string","valueString":"Contoso Contoso","text":"Contoso Contoso","boundingBox":[378.2,292.4,1117.7,468.3,1035.7,812.7,296.3,636.8],"page":1,"confidence":0.613},"MerchantAddress":{"type":"string","valueString":"123 Main Street Redmond, WA 98052","text":"123 Main Street Redmond, WA 98052","boundingBox":[302,675.8,848.1,793.7,809.9,970.4,263.9,852.5],"page":1,"confidence":0.99},"MerchantPhoneNumber":{"type":"phoneNumber","valuePhoneNumber":"+19876543210","text":"987-654-3210","boundingBox":[278,1004,656.3,1054.7,646.8,1125.3,268.5,1074.7],"page":1,"confidence":0.99},"TransactionDate":{"type":"date","valueDate":"2019-06-10","text":"6/10/2019","boundingBox":[265.1,1228.4,525,1247,518.9,1332.1,259,1313.5],"page":1,"confidence":0.99},"TransactionTime":{"type":"time","valueTime":"13:59:00","text":"13:59","boundingBox":[541,1248,677.3,1261.5,668.9,1346.5,532.6,1333],"page":1,"confidence":0.977},"Items":{"type":"array","valueArray":[{"type":"object","valueObject":{"Quantity":{"type":"number","text":"1","boundingBox":[245.1,1581.5,300.9,1585.1,295,1676,239.2,1672.4],"page":1,"confidence":0.92},"Name":{"type":"string","valueString":"Cappuccino","text":"Cappuccino","boundingBox":[322,1586,654.2,1601.1,650,1693,317.8,1678],"page":1,"confidence":0.923},"TotalPrice":{"type":"number","valueNumber":2.2,"text":"$2.20","boundingBox":[1107.7,1584,1263,1574,1268.3,1656,1113,1666],"page":1,"confidence":0.918}}},{"type":"object","valueObject":{"Quantity":{"type":"number","text":"1","boundingBox":[232,1834,286.6,1835,285,1921,230.4,1920],"page":1,"confidence":0.858},"Name":{"type":"string","valueString":"BACON & EGGS","text":"BACON & EGGS","boundingBox":[308,1836,746,1841.4,745,1925.4,307,1920],"page":1,"confidence":0.916},"TotalPrice":{"type":"number","text":"$9.5","boundingBox":[1133.9,1955,1257,1952,1259.1,2036,1136,2039],"page":1,"confidence":0.916}}}]},"Subtotal":{"type":"number","valueNumber":11.7,"text":"11.70","boundingBox":[1146,2221,1297.3,2223,1296,2319,1144.7,2317],"page":1,"confidence":0.955},"Tax":{"type":"number","valueNumber":1.17,"text":"1.17","boundingBox":[1190,2359,1304,2359,1304,2456,1190,2456],"page":1,"confidence":0.979},"Tip":{"type":"number","valueNumber":1.63,"text":"1.63","boundingBox":[1094,2479,1267.7,2485,1264,2591,1090.3,2585],"page":1,"confidence":0.941},"Total":{"type":"number","valueNumber":14.5,"text":"$14.50","boundingBox":[1034.2,2617,1387.5,2638.2,1380,2763,1026.7,2741.8],"page":1,"confidence":0.985}}}]}}, [
+  .get('/formrecognizer/v2.0/prebuilt/receipt/analyzeResults/d254705d-a6e6-4dc0-a818-3d777499dd81')
+  .reply(200, {"status":"succeeded","createdDateTime":"2020-08-13T21:56:00Z","lastUpdatedDateTime":"2020-08-13T21:56:02Z","analyzeResult":{"version":"2.0.0","readResults":[{"page":1,"angle":0.6893,"width":1688,"height":3000,"unit":"pixel","language":"en"}],"documentResults":[{"docType":"prebuilt:receipt","pageRange":[1,1],"fields":{"ReceiptType":{"type":"string","valueString":"Itemized","confidence":0.692},"MerchantName":{"type":"string","valueString":"Contoso Contoso","text":"Contoso Contoso","boundingBox":[378.2,292.4,1117.7,468.3,1035.7,812.7,296.3,636.8],"page":1,"confidence":0.613},"MerchantAddress":{"type":"string","valueString":"123 Main Street Redmond, WA 98052","text":"123 Main Street Redmond, WA 98052","boundingBox":[302,675.8,848.1,793.7,809.9,970.4,263.9,852.5],"page":1,"confidence":0.99},"MerchantPhoneNumber":{"type":"phoneNumber","valuePhoneNumber":"+19876543210","text":"987-654-3210","boundingBox":[278,1004,656.3,1054.7,646.8,1125.3,268.5,1074.7],"page":1,"confidence":0.99},"TransactionDate":{"type":"date","valueDate":"2019-06-10","text":"6/10/2019","boundingBox":[265.1,1228.4,525,1247,518.9,1332.1,259,1313.5],"page":1,"confidence":0.99},"TransactionTime":{"type":"time","valueTime":"13:59:00","text":"13:59","boundingBox":[541,1248,677.3,1261.5,668.9,1346.5,532.6,1333],"page":1,"confidence":0.977},"Items":{"type":"array","valueArray":[{"type":"object","valueObject":{"Quantity":{"type":"number","text":"1","boundingBox":[245.1,1581.5,300.9,1585.1,295,1676,239.2,1672.4],"page":1,"confidence":0.92},"Name":{"type":"string","valueString":"Cappuccino","text":"Cappuccino","boundingBox":[322,1586,654.2,1601.1,650,1693,317.8,1678],"page":1,"confidence":0.923},"TotalPrice":{"type":"number","valueNumber":2.2,"text":"$2.20","boundingBox":[1107.7,1584,1263,1574,1268.3,1656,1113,1666],"page":1,"confidence":0.918}}},{"type":"object","valueObject":{"Quantity":{"type":"number","text":"1","boundingBox":[232,1834,286.6,1835,285,1921,230.4,1920],"page":1,"confidence":0.858},"Name":{"type":"string","valueString":"BACON & EGGS","text":"BACON & EGGS","boundingBox":[308,1836,746,1841.4,745,1925.4,307,1920],"page":1,"confidence":0.916},"TotalPrice":{"type":"number","text":"$9.5","boundingBox":[1133.9,1955,1257,1952,1259.1,2036,1136,2039],"page":1,"confidence":0.916}}}]},"Subtotal":{"type":"number","valueNumber":11.7,"text":"11.70","boundingBox":[1146,2221,1297.3,2223,1296,2319,1144.7,2317],"page":1,"confidence":0.955},"Tax":{"type":"number","valueNumber":1.17,"text":"1.17","boundingBox":[1190,2359,1304,2359,1304,2456,1190,2456],"page":1,"confidence":0.979},"Tip":{"type":"number","valueNumber":1.63,"text":"1.63","boundingBox":[1094,2479,1267.7,2485,1264,2591,1090.3,2585],"page":1,"confidence":0.941},"Total":{"type":"number","valueNumber":14.5,"text":"$14.50","boundingBox":[1034.2,2617,1387.5,2638.2,1380,2763,1026.7,2741.8],"page":1,"confidence":0.985}}}]}}, [
   'Transfer-Encoding',
   'chunked',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-envoy-upstream-service-time',
-  '24',
+  '61',
   'apim-request-id',
-  '7f75b652-e40d-46d5-9acf-50fbd1b2a2ca',
+  '07def35d-b5bd-4f28-b891-a9d627c7e2b5',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Wed, 05 Aug 2020 23:29:07 GMT'
+  'Thu, 13 Aug 2020 21:56:05 GMT'
 ]);
