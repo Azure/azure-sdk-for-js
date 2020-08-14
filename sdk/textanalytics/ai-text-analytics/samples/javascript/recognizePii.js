86a2b6ec0b678b26c3782882c502062fc8109834
deyaaeldeen <dealmaha@microsoft.com>
2020-08-14 15:21:24 -0400

    fix samples

--- sdk/textanalytics/ai-text-analytics/samples/javascript/recognizePii.js
+++ sdk/textanalytics/ai-text-analytics/samples/javascript/recognizePii.js
@@ -5,7 +5,7 @@
  * detects personally-identifiable information
  */
 
-const { TextAnalyticsClient, TextAnalyticsApiKeyCredential } = require("@azure/ai-text-analytics");
+const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
 
 // Load the .env file if it exists
 require("dotenv").config();
@@ -17,7 +17,7 @@ async function main() {
   const endpoint = process.env["ENDPOINT"] || "<cognitive services endpoint>";
   const apiKey = process.env["TEXT_ANALYTICS_API_KEY"] || "<api key>";
 
-  const client = new TextAnalyticsClient(endpoint, new TextAnalyticsApiKeyCredential(apiKey));
+  const client = new TextAnalyticsClient(endpoint, new AzureKeyCredential(apiKey));
 
   const [result] = await client.recognizePiiEntities(["My phone number is 555-5555"]);
 
