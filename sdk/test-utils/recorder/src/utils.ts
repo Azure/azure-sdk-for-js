1f8f9feab9978e0c2f85c53ddaf243fde6cc205d
HarshaNalluru <sanallur@microsoft.com>
2020-08-19 23:49:20 -0700

    fix isHex

--- sdk/test-utils/recorder/src/utils.ts
+++ sdk/test-utils/recorder/src/utils.ts
@@ -508,7 +508,7 @@ export const windowLens: {
  */
 export function isHex(value: string): boolean {
   // TODO: Add tests to validate regex
-  if (/[0-9A-Fa-f]/g.test(value)) {
+  if (/^[0-9a-fA-F]+$/.test(value)) {
     return true;
   }
   return false;
