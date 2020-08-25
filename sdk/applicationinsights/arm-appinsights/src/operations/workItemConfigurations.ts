ec24d6495c62a99962ea2305da9980d0eaae4660
SDK Automation <sdkautomation@microsoft.com>
2020-08-25 01:41:50 +0000

    Generated from 2b737d2955d27c9c29181edfa8e9e0a1f780c0a0
    mark userid as a read only field, remove it and time generated from samples that do updates

--- sdk/applicationinsights/arm-appinsights/src/operations/workItemConfigurations.ts
+++ sdk/applicationinsights/arm-appinsights/src/operations/workItemConfigurations.ts
@@ -28,20 +28,20 @@ export class WorkItemConfigurations {
 
   /**
    * Gets the list work item configurations that exist for the application
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param [options] The optional parameters
    * @returns Promise<Models.WorkItemConfigurationsListResponse>
    */
   list(resourceGroupName: string, resourceName: string, options?: msRest.RequestOptionsBase): Promise<Models.WorkItemConfigurationsListResponse>;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param callback The callback
    */
   list(resourceGroupName: string, resourceName: string, callback: msRest.ServiceCallback<Models.WorkItemConfigurationsListResult>): void;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param options The optional parameters
    * @param callback The callback
@@ -60,7 +60,7 @@ export class WorkItemConfigurations {
 
   /**
    * Create a work item configuration for an Application Insights component.
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param workItemConfigurationProperties Properties that need to be specified to create a work
    * item configuration of a Application Insights component.
@@ -69,7 +69,7 @@ export class WorkItemConfigurations {
    */
   create(resourceGroupName: string, resourceName: string, workItemConfigurationProperties: Models.WorkItemCreateConfiguration, options?: msRest.RequestOptionsBase): Promise<Models.WorkItemConfigurationsCreateResponse>;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param workItemConfigurationProperties Properties that need to be specified to create a work
    * item configuration of a Application Insights component.
@@ -77,7 +77,7 @@ export class WorkItemConfigurations {
    */
   create(resourceGroupName: string, resourceName: string, workItemConfigurationProperties: Models.WorkItemCreateConfiguration, callback: msRest.ServiceCallback<Models.WorkItemConfiguration>): void;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param workItemConfigurationProperties Properties that need to be specified to create a work
    * item configuration of a Application Insights component.
@@ -99,20 +99,20 @@ export class WorkItemConfigurations {
 
   /**
    * Gets default work item configurations that exist for the application
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param [options] The optional parameters
    * @returns Promise<Models.WorkItemConfigurationsGetDefaultResponse>
    */
   getDefault(resourceGroupName: string, resourceName: string, options?: msRest.RequestOptionsBase): Promise<Models.WorkItemConfigurationsGetDefaultResponse>;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param callback The callback
    */
   getDefault(resourceGroupName: string, resourceName: string, callback: msRest.ServiceCallback<Models.WorkItemConfiguration>): void;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param options The optional parameters
    * @param callback The callback
@@ -130,33 +130,33 @@ export class WorkItemConfigurations {
   }
 
   /**
-   * Delete an workitem configuration of an Application Insights component.
-   * @param resourceGroupName The name of the resource group.
+   * Delete a work item configuration of an Application Insights component.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
    * of connector as defined in connector configuration
    * @param [options] The optional parameters
-   * @returns Promise<Models.WorkItemConfigurationsDeleteMethodResponse>
+   * @returns Promise<msRest.RestResponse>
    */
-  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, options?: msRest.RequestOptionsBase): Promise<Models.WorkItemConfigurationsDeleteMethodResponse>;
+  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, options?: msRest.RequestOptionsBase): Promise<msRest.RestResponse>;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
    * of connector as defined in connector configuration
    * @param callback The callback
    */
-  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, callback: msRest.ServiceCallback<any>): void;
+  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, callback: msRest.ServiceCallback<void>): void;
   /**
-   * @param resourceGroupName The name of the resource group.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param resourceName The name of the Application Insights component resource.
    * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
    * of connector as defined in connector configuration
    * @param options The optional parameters
    * @param callback The callback
    */
-  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, options: msRest.RequestOptionsBase, callback: msRest.ServiceCallback<any>): void;
-  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, options?: msRest.RequestOptionsBase | msRest.ServiceCallback<any>, callback?: msRest.ServiceCallback<any>): Promise<Models.WorkItemConfigurationsDeleteMethodResponse> {
+  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, options: msRest.RequestOptionsBase, callback: msRest.ServiceCallback<void>): void;
+  deleteMethod(resourceGroupName: string, resourceName: string, workItemConfigId: string, options?: msRest.RequestOptionsBase | msRest.ServiceCallback<void>, callback?: msRest.ServiceCallback<void>): Promise<msRest.RestResponse> {
     return this.client.sendOperationRequest(
       {
         resourceGroupName,
@@ -165,7 +165,92 @@ export class WorkItemConfigurations {
         options
       },
       deleteMethodOperationSpec,
-      callback) as Promise<Models.WorkItemConfigurationsDeleteMethodResponse>;
+      callback);
+  }
+
+  /**
+   * Gets specified work item configuration for an Application Insights component.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param resourceName The name of the Application Insights component resource.
+   * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
+   * of connector as defined in connector configuration
+   * @param [options] The optional parameters
+   * @returns Promise<Models.WorkItemConfigurationsGetItemResponse>
+   */
+  getItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, options?: msRest.RequestOptionsBase): Promise<Models.WorkItemConfigurationsGetItemResponse>;
+  /**
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param resourceName The name of the Application Insights component resource.
+   * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
+   * of connector as defined in connector configuration
+   * @param callback The callback
+   */
+  getItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, callback: msRest.ServiceCallback<Models.WorkItemConfiguration>): void;
+  /**
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param resourceName The name of the Application Insights component resource.
+   * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
+   * of connector as defined in connector configuration
+   * @param options The optional parameters
+   * @param callback The callback
+   */
+  getItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, options: msRest.RequestOptionsBase, callback: msRest.ServiceCallback<Models.WorkItemConfiguration>): void;
+  getItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, options?: msRest.RequestOptionsBase | msRest.ServiceCallback<Models.WorkItemConfiguration>, callback?: msRest.ServiceCallback<Models.WorkItemConfiguration>): Promise<Models.WorkItemConfigurationsGetItemResponse> {
+    return this.client.sendOperationRequest(
+      {
+        resourceGroupName,
+        resourceName,
+        workItemConfigId,
+        options
+      },
+      getItemOperationSpec,
+      callback) as Promise<Models.WorkItemConfigurationsGetItemResponse>;
+  }
+
+  /**
+   * Update a work item configuration for an Application Insights component.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param resourceName The name of the Application Insights component resource.
+   * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
+   * of connector as defined in connector configuration
+   * @param workItemConfigurationProperties Properties that need to be specified to update a work
+   * item configuration for this Application Insights component.
+   * @param [options] The optional parameters
+   * @returns Promise<Models.WorkItemConfigurationsUpdateItemResponse>
+   */
+  updateItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, workItemConfigurationProperties: Models.WorkItemCreateConfiguration, options?: msRest.RequestOptionsBase): Promise<Models.WorkItemConfigurationsUpdateItemResponse>;
+  /**
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param resourceName The name of the Application Insights component resource.
+   * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
+   * of connector as defined in connector configuration
+   * @param workItemConfigurationProperties Properties that need to be specified to update a work
+   * item configuration for this Application Insights component.
+   * @param callback The callback
+   */
+  updateItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, workItemConfigurationProperties: Models.WorkItemCreateConfiguration, callback: msRest.ServiceCallback<Models.WorkItemConfiguration>): void;
+  /**
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param resourceName The name of the Application Insights component resource.
+   * @param workItemConfigId The unique work item configuration Id. This can be either friendly name
+   * of connector as defined in connector configuration
+   * @param workItemConfigurationProperties Properties that need to be specified to update a work
+   * item configuration for this Application Insights component.
+   * @param options The optional parameters
+   * @param callback The callback
+   */
+  updateItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, workItemConfigurationProperties: Models.WorkItemCreateConfiguration, options: msRest.RequestOptionsBase, callback: msRest.ServiceCallback<Models.WorkItemConfiguration>): void;
+  updateItem(resourceGroupName: string, resourceName: string, workItemConfigId: string, workItemConfigurationProperties: Models.WorkItemCreateConfiguration, options?: msRest.RequestOptionsBase | msRest.ServiceCallback<Models.WorkItemConfiguration>, callback?: msRest.ServiceCallback<Models.WorkItemConfiguration>): Promise<Models.WorkItemConfigurationsUpdateItemResponse> {
+    return this.client.sendOperationRequest(
+      {
+        resourceGroupName,
+        resourceName,
+        workItemConfigId,
+        workItemConfigurationProperties,
+        options
+      },
+      updateItemOperationSpec,
+      callback) as Promise<Models.WorkItemConfigurationsUpdateItemResponse>;
   }
 }
 
@@ -180,7 +265,7 @@ const listOperationSpec: msRest.OperationSpec = {
     Parameters.resourceName
   ],
   queryParameters: [
-    Parameters.apiVersion
+    Parameters.apiVersion0
   ],
   headerParameters: [
     Parameters.acceptLanguage
@@ -205,7 +290,7 @@ const createOperationSpec: msRest.OperationSpec = {
     Parameters.resourceName
   ],
   queryParameters: [
-    Parameters.apiVersion
+    Parameters.apiVersion0
   ],
   headerParameters: [
     Parameters.acceptLanguage
@@ -237,7 +322,7 @@ const getDefaultOperationSpec: msRest.OperationSpec = {
     Parameters.resourceName
   ],
   queryParameters: [
-    Parameters.apiVersion
+    Parameters.apiVersion0
   ],
   headerParameters: [
     Parameters.acceptLanguage
@@ -263,19 +348,71 @@ const deleteMethodOperationSpec: msRest.OperationSpec = {
     Parameters.workItemConfigId
   ],
   queryParameters: [
-    Parameters.apiVersion
+    Parameters.apiVersion0
+  ],
+  headerParameters: [
+    Parameters.acceptLanguage
+  ],
+  responses: {
+    200: {},
+    default: {
+      bodyMapper: Mappers.CloudError
+    }
+  },
+  serializer
+};
+
+const getItemOperationSpec: msRest.OperationSpec = {
+  httpMethod: "GET",
+  path: "subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Insights/components/{resourceName}/WorkItemConfigs/{workItemConfigId}",
+  urlParameters: [
+    Parameters.resourceGroupName,
+    Parameters.subscriptionId,
+    Parameters.resourceName,
+    Parameters.workItemConfigId
+  ],
+  queryParameters: [
+    Parameters.apiVersion0
   ],
   headerParameters: [
     Parameters.acceptLanguage
   ],
   responses: {
     200: {
-      bodyMapper: {
-        serializedName: "parsedResponse",
-        type: {
-          name: "Object"
-        }
-      }
+      bodyMapper: Mappers.WorkItemConfiguration
+    },
+    default: {
+      bodyMapper: Mappers.CloudError
+    }
+  },
+  serializer
+};
+
+const updateItemOperationSpec: msRest.OperationSpec = {
+  httpMethod: "PATCH",
+  path: "subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Insights/components/{resourceName}/WorkItemConfigs/{workItemConfigId}",
+  urlParameters: [
+    Parameters.resourceGroupName,
+    Parameters.subscriptionId,
+    Parameters.resourceName,
+    Parameters.workItemConfigId
+  ],
+  queryParameters: [
+    Parameters.apiVersion0
+  ],
+  headerParameters: [
+    Parameters.acceptLanguage
+  ],
+  requestBody: {
+    parameterPath: "workItemConfigurationProperties",
+    mapper: {
+      ...Mappers.WorkItemCreateConfiguration,
+      required: true
+    }
+  },
+  responses: {
+    200: {
+      bodyMapper: Mappers.WorkItemConfiguration
     },
     default: {
       bodyMapper: Mappers.CloudError
