b58255aa1fb6d3f0bed13bcd1b8bf90338cc59cb
SDK Automation <sdkautomation@microsoft.com>
2020-08-25 07:18:08 +0000

    Generated from efcb7ecf4722c919d5528c53671b124046badcdb
    update netapp readme.java.md

--- sdk/netapp/arm-netapp/src/azureNetAppFilesManagementClientContext.ts
+++ sdk/netapp/arm-netapp/src/azureNetAppFilesManagementClientContext.ts
@@ -45,7 +45,7 @@ export class AzureNetAppFilesManagementClientContext extends msRestAzure.AzureSe
 
     super(credentials, options);
 
-    this.apiVersion = '2020-02-01';
+    this.apiVersion = '2020-06-01';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
