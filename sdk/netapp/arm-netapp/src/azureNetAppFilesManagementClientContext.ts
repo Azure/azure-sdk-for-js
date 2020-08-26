02a2fe6e69790a199a45f5ff4a8b915530e9d69c
colawwj <v-sigangzhou@microsoft.com>
2020-08-26 15:07:37 +0800

    release arm-netapp

--- sdk/netapp/arm-netapp/src/azureNetAppFilesManagementClientContext.ts
+++ sdk/netapp/arm-netapp/src/azureNetAppFilesManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-netapp";
-const packageVersion = "11.0.0";
+const packageVersion = "11.1.0";
 
 export class AzureNetAppFilesManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
