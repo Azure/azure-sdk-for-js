33889bfd846be925e6c135b5cd99f2101b421193
Will Temple <witemple@microsoft.com>
2020-08-31 09:24:18 -0700

    Fixed typeck error

--- common/tools/dev-tool/src/ambient.d.ts
+++ common/tools/dev-tool/src/ambient.d.ts
@@ -1,3 +1,20 @@
-declare module "rollup-plugin-sourcemaps" { 
-  export default function sourcemaps() : any;
+type Plugin = import("rollup").Plugin;
+
+declare module "rollup-plugin-sourcemaps" {
+  export default function(): Plugin;
+}
+
+declare module "rollup-plugin-node-globals" {
+  export default function(): Plugin;
+}
+
+declare module "rollup-plugin-node-builtins" {
+  export default function(): Plugin;
+}
+
+declare module "@rollup/plugin-multi-entry" {
+  interface MultiEntryOptions {
+    exports: boolean;
+  }
+  export default function(opts: MultiEntryOptions): Plugin;
 }
