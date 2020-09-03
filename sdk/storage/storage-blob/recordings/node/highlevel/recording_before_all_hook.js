c94cc0ac61fef067f413f49d784fe1f505e4b9f0
Lin Jian <1215122919@qq.com>
2020-09-03 14:24:17 +0800

    [storage-blob] fix uploadStream to support 4GB blockSize (#10650)
    * wip
    
    * re-write with pooled buffer
    
    * test
    
    * remove .only
    
    * add browser shim
    
    * record test
    
    * format
    
    * windows node10 allocation fail
    
    * me
    
    * fix bug
    
    * me
    
    * debug windows memory allocation failure
    
    * skip test
    
    * remove debug log
    
    * add comments and changelog
    
    * fix comments
    
    Co-authored-by: Lin Jian <ljian3377@gmail.com>

--- sdk/storage/storage-blob/recordings/node/highlevel/recording_before_all_hook.js
+++ sdk/storage/storage-blob/recordings/node/highlevel/recording_before_all_hook.js
@@ -1,5 +1,5 @@
 let nock = require('nock');
 
-module.exports.hash = "8fc0f0f8be4cfc4b1ab29277203cf2bc";
+module.exports.hash = "a517c9a2f9f67f81c0cd8de67eb9a59a";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
