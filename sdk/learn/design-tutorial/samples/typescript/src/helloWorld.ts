074ea5f598c2b7a89aa3b81a19baf79e8635a2ee
Jeff Fisher <jeffish@microsoft.com>
2020-09-02 18:05:00 -0700

    simplify the samples

---
+++ sdk/learn/design-tutorial/samples/typescript/src/helloWorld.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export async function main() {
+  console.log("== Hello World ==");
+  // TODO
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
