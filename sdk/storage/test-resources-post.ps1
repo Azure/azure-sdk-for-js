70b6634cdb54e4135925d90d27b7876b721b5a4e
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2020-09-25 11:51:27 -0700

    update script to create SAS for datalake also

--- sdk/storage/test-resources-post.ps1
+++ sdk/storage/test-resources-post.ps1
@@ -16,15 +16,8 @@ param (
   [string] $TestApplicationSecret
 )
 
-# Run any post deployment script and set any additional keys to set in Env
-$AdditionalEnvKeys = @{}
-
 $storageAccountName = $DeploymentOutputs['ACCOUNT_NAME']
 $context = New-AzStorageContext -StorageAccountName $storageAccountName
-# Create SAS for storage account with additional permissions that are not supported by SRP deployment
-Write-Host "Creating SAS for storage account $storageAccountName"
-$storageSas = New-AzStorageAccountSASToken -ResourceType Service, Container, Object -Service Blob, File, Queue, Table -Permission "rwdxftlacup" -Context $context
-$AdditionalEnvKeys["ACCOUNT_SAS"] = $storageSas
 
 # https://docs.microsoft.com/en-us/powershell/module/az.storage/set-azstoragecorsrule?view=azps-3.3.0
 $corsRules = (@{
@@ -44,15 +37,31 @@ $corsRules = (@{
   })
 
 Set-AzStorageCORSRule -ServiceType 'Queue' -CorsRules $corsRules -Context $context
+
 Write-Verbose "CORS rule set for $storageAccountName"
+
 
 $datalakeStorageAccountName = $DeploymentOutputs['DFS_ACCOUNT_NAME']
 $context = New-AzStorageContext -StorageAccountName $datalakeStorageAccountName
 Set-AzStorageCORSRule -ServiceType 'Blob' -CorsRules $corsRules -Context $context
 Write-Verbose "CORS rule set for $datalakeStorageAccountName"
-Write-Host "Creating SAS for datalake storage account $datalakeStorageAccountName"
-$storageSas = New-AzStorageAccountSASToken -ResourceType Service, Container, Object -Service Blob, File, Queue, Table -Permission "rwdxftlacup" -Context $context
+
+# Run any post deployment script and set any additional keys to set in Env
+$AdditionalEnvKeys = @{}
+
+# Create SAS for storage account with additional permissions that are not supported by SRP deployment
+$storageAccount = $DeploymentOutputs['ACCOUNT_NAME']
+Write-Host "Creating SAS for storage account $storageAccount"
+$storageContext = New-AzStorageContext -StorageAccountName $storageAccount -StorageAccountKey $DeploymentOutputs['ACCOUNT_KEY']
+$storageSas = New-AzStorageAccountSASToken -ResourceType Service, Container, Object -Service Blob, File, Queue, Table -Permission "rwdxftlacup" -Context $storageContext
+$AdditionalEnvKeys["ACCOUNT_SAS"] = $storageSas
+
+$datalakeStorageAccount = $DeploymentOutputs['DFS_ACCOUNT_NAME']
+Write-Host "Creating SAS for datalake storage account $datalakeStorageAccount"
+$storageContext = New-AzStorageContext -StorageAccountName $datalakeStorageAccount -StorageAccountKey $DeploymentOutputs['DFS_ACCOUNT_KEY']
+$storageSas = New-AzStorageAccountSASToken -ResourceType Service, Container, Object -Service Blob, File, Queue, Table -Permission "rwdxftlacup" -Context $storageContext
 $AdditionalEnvKeys["DFS_ACCOUNT_SAS"] = $storageSas
+
 
 # Try to detect the shell based on the parent process name (e.g. launch via shebang).
 $shell, $shellExportFormat = if (($parentProcessName = (Get-Process -Id $PID).Parent.ProcessName) -and $parentProcessName -eq 'cmd') {
