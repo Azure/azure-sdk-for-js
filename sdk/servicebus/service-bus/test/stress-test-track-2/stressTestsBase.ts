09ce3dac0c5aca225e91039759514b2f944d50a9
HarshaNalluru <sanallur@microsoft.com>
2020-09-29 02:46:32 -0700

    package.json and tsconfig.json

---
+++ sdk/servicebus/service-bus/test/stress-test-track-2/stressTestsBase.ts
@@ -0,0 +1,125 @@
+import {
+  ServiceBusMessage,
+  ServiceBusReceivedMessage,
+  ServiceBusReceivedMessageWithLock,
+  ServiceBusReceiver,
+  ServiceBusSender
+} from "@azure/service-bus";
+
+export class SBStressTestsBase {
+  messagesSent: ServiceBusMessage[] = [];
+  messagesReceived: ServiceBusMessage[] = [];
+  snapshotTimer: NodeJS.Timer;
+  startedAt: Date | undefined;
+  // Send metrics
+  numberOfSuccessfulSends = 0;
+  numberOfFailedSends = 0;
+  errorsInSending: any[] = [];
+  // Receive metrics
+  numberOfSuccessfulReceives = 0;
+  numberOfFailedReceives = 0;
+  errorsInReceiving: any[] = [];
+  // Message Lock Renewal
+  numberOfSuccessfulMessageLockRenewals = 0;
+  numberOfFailedMessageLockRenewals = 0;
+  errorsInMessageLockRenewal: any[] = [];
+  messageLockRenewalTimers: NodeJS.Timer[] = [];
+  renewalCount: { [key: string]: number } = {}; // key - messageId, value - number of renewals
+
+  constructor(
+    snapshotIntervalInMs = 5000 //Snapshots are taken every 5s
+  ) {
+    this.messagesSent = [];
+    this.snapshotTimer = setInterval(this.snapshot.bind(this), snapshotIntervalInMs);
+  }
+
+  public async sendMessages(senders: ServiceBusSender[], numberOfMessages = 10) {
+    for (const sender of senders) {
+      try {
+        const messages: ServiceBusMessage[] = [];
+        for (let i = 0; i < numberOfMessages; i++) {
+          messages.push({ body: `message ${i} ${Math.random()}` });
+        }
+        await sender.sendMessages(messages);
+        this.numberOfSuccessfulSends++;
+      } catch (error) {
+        this.numberOfFailedSends++;
+        this.errorsInSending.push(error);
+        console.error("Error in sending: ", error);
+      }
+    }
+  }
+
+  public async receiveMessages<ReceivedMessageT extends ServiceBusReceivedMessage>(
+    receiver: ServiceBusReceiver<ReceivedMessageT>,
+    maxMsgCount = 10
+  ): Promise<ReceivedMessageT[]> {
+    try {
+      const messages = await receiver.receiveMessages(maxMsgCount, {
+        maxWaitTimeInMs: 10000
+      });
+      this.messagesReceived = this.messagesReceived.concat(messages as ServiceBusReceivedMessage[]);
+      this.numberOfSuccessfulReceives++;
+      return messages;
+    } catch (error) {
+      this.numberOfFailedReceives++;
+      this.errorsInReceiving.push(error);
+      console.error("Error in receiving: ", error);
+    }
+  }
+
+  public async renewMessageLock(message: ServiceBusReceivedMessageWithLock) {
+    this.messageLockRenewalTimers.push(
+      setTimeout(async () => {
+        try {
+          await message.renewLock();
+          this.numberOfSuccessfulMessageLockRenewals++;
+          const currentRenewalCount = this.renewalCount[message.messageId as string];
+          this.renewalCount[message.messageId as string] =
+            currentRenewalCount === undefined ? 1 : currentRenewalCount + 1;
+          this.renewMessageLock(message);
+        } catch (error) {
+          this.numberOfFailedMessageLockRenewals++;
+          this.errorsInMessageLockRenewal.push(error);
+          console.error("Error in message lock renewal: ", error);
+        }
+      }, message.lockedUntilUtc.valueOf() - new Date().valueOf() - 10000)
+    );
+  }
+
+  public snapshot(): void {
+    if (!this.startedAt) this.startedAt = new Date();
+    console.log("Time : ", new Date());
+    const elapsedTimeInSeconds = (new Date().valueOf() - this.startedAt.valueOf()) / 1000;
+    console.log("Elapsed time in seconds: ", elapsedTimeInSeconds);
+    console.log("Number of messages sent so far : ", this.messagesSent.length);
+    console.log("Number of successful sends so far : ", this.numberOfSuccessfulSends);
+    console.log("Number of failed sends so far : ", this.numberOfFailedSends);
+    console.log(
+      "(Avg)Number of sends per sec: ",
+      this.numberOfSuccessfulSends / elapsedTimeInSeconds
+    );
+    console.log("Number of messages received so far : ", this.messagesReceived.length);
+    console.log("Number of successful receives so far : ", this.numberOfSuccessfulReceives);
+    console.log("Number of failed receives so far : ", this.numberOfFailedReceives);
+    console.log(
+      "(Avg)Number of receives per sec: ",
+      this.numberOfSuccessfulReceives / elapsedTimeInSeconds
+    );
+    console.log(
+      "Number of successful lock renewals so far : ",
+      this.numberOfSuccessfulMessageLockRenewals
+    );
+    console.log("Number of failed lock renewals so far : ", this.numberOfFailedMessageLockRenewals);
+    console.log(
+      "(Avg)Number of lock renewals per sec: ",
+      this.numberOfSuccessfulMessageLockRenewals / elapsedTimeInSeconds
+    );
+    console.log("\n");
+  }
+
+  public end() {
+    clearInterval(this.snapshotTimer);
+    this.messageLockRenewalTimers.map((timer) => clearTimeout(timer));
+  }
+}
