fdc2c8da227458ed911b3874ceb628967aa1be93
HarshaNalluru <sanallur@microsoft.com>
2020-09-30 03:50:41 -0700

    Renew session lock helper and info interface

--- sdk/servicebus/service-bus/test/stress-test-track-2/stressTestsBase.ts
+++ sdk/servicebus/service-bus/test/stress-test-track-2/stressTestsBase.ts
@@ -6,6 +6,7 @@ import {
   ServiceBusReceiver,
   ServiceBusSender
 } from "@azure/service-bus";
+import { ServiceBusSessionReceiver } from "../../src";
 
 interface ReceiveInfo {
   numberOfSuccessfulReceives: number;
@@ -17,16 +18,16 @@ interface SendInfo {
   numberOfFailedSends: number;
   errorsInSending: any[];
 }
-interface MessageLockRenewalInfo {
-  numberOfSuccessfulMessageLockRenewals: number;
-  numberOfFailedMessageLockRenewals: number;
-  errorsInMessageLockRenewal: any[];
+interface LockRenewalInfo {
+  numberOfSuccessfulLockRenewals: number;
+  numberOfFailedLockRenewals: number;
+  errorsInLockRenewal: any[];
   /**
-   * key - messageId, value - next renewal timer meant for the message
+   * key - id, value - next renewal timer meant for the message/session-receiver
    */
-  messageLockRenewalTimers: { [key: string]: NodeJS.Timer };
+  lockRenewalTimers: { [key: string]: NodeJS.Timer };
   /**
-   * key - messageId, value - number of renewals
+   * key - id, value - number of renewals
    */
   renewalCount: { [key: string]: number };
 }
@@ -52,11 +53,19 @@ export class SBStressTestsBase {
     errorsInReceiving: []
   };
   // Message Lock Renewal
-  messageLockRenewalInfo: MessageLockRenewalInfo = {
-    numberOfSuccessfulMessageLockRenewals: 0,
-    numberOfFailedMessageLockRenewals: 0,
-    errorsInMessageLockRenewal: [],
-    messageLockRenewalTimers: {},
+  messageLockRenewalInfo: LockRenewalInfo = {
+    numberOfSuccessfulLockRenewals: 0,
+    numberOfFailedLockRenewals: 0,
+    errorsInLockRenewal: [],
+    lockRenewalTimers: {},
+    renewalCount: {}
+  };
+  // Session Lock Renewal
+  sessionLockRenewalInfo: LockRenewalInfo = {
+    numberOfSuccessfulLockRenewals: 0,
+    numberOfFailedLockRenewals: 0,
+    errorsInLockRenewal: [],
+    lockRenewalTimers: {},
     renewalCount: {}
   };
   // Queue Management
@@ -82,12 +91,19 @@ export class SBStressTestsBase {
     await this.serviceBusAdministrationClient.createQueue(this.queueName);
   }
 
-  public async sendMessages(senders: ServiceBusSender[], numberOfMessages = 1) {
+  public async sendMessages(
+    senders: ServiceBusSender[],
+    numberOfMessages = 1,
+    useSessions = false
+  ) {
     for (const sender of senders) {
       try {
         const messages: ServiceBusMessage[] = [];
         for (let i = 0; i < numberOfMessages; i++) {
-          messages.push({ body: `message ${i} ${Math.random()}` });
+          messages.push({
+            body: `message ${i} ${Math.random()}`,
+            sessionId: useSessions ? `session-${Math.ceil(Math.random() * 10000)}` : undefined
+          });
         }
         await sender.sendMessages(messages);
         this.sendInfo.numberOfSuccessfulSends++;
@@ -127,11 +143,11 @@ export class SBStressTestsBase {
   public renewMessageLockUntil(message: ServiceBusReceivedMessageWithLock, duration: number) {
     // TODO: pass in max number of lock renewals? and add settlement at the end of max??
     const startTime = new Date();
-    this.messageLockRenewalInfo.messageLockRenewalTimers[message.messageId as string] = setTimeout(
+    this.messageLockRenewalInfo.lockRenewalTimers[message.messageId as string] = setTimeout(
       async () => {
         try {
           await message.renewLock();
-          this.messageLockRenewalInfo.numberOfSuccessfulMessageLockRenewals++;
+          this.messageLockRenewalInfo.numberOfSuccessfulLockRenewals++;
           const currentRenewalCount = this.messageLockRenewalInfo.renewalCount[
             message.messageId as string
           ];
@@ -141,17 +157,49 @@ export class SBStressTestsBase {
           if (duration - elapsedTime > 0) {
             this.renewMessageLockUntil(message, duration - elapsedTime);
           } else {
-            // This is reached after the duration given has passed by
+            // Code reaches here only after the duration given has passed by
             // TODO: Settle the message maybe?
           }
         } catch (error) {
-          this.messageLockRenewalInfo.numberOfFailedMessageLockRenewals++;
-          this.messageLockRenewalInfo.errorsInMessageLockRenewal.push(error);
+          this.messageLockRenewalInfo.numberOfFailedLockRenewals++;
+          this.messageLockRenewalInfo.errorsInLockRenewal.push(error);
           console.error("Error in message lock renewal: ", error);
         }
       },
       message.lockedUntilUtc!.valueOf() - startTime.valueOf() - 10000
     );
+  }
+
+  /**
+   * @param {ServiceBusSessionReceiver<ServiceBusReceivedMessage>} receiver
+   * @param {number} duration Duration until which the lock is renewed
+   */
+  public renewSessionLockUntil(
+    receiver: ServiceBusSessionReceiver<ServiceBusReceivedMessage>,
+    duration: number
+  ) {
+    // TODO: pass in max number of lock renewals? and close the receiver at the end of max??
+    const startTime = new Date();
+    this.sessionLockRenewalInfo.lockRenewalTimers[receiver.sessionId] = setTimeout(async () => {
+      try {
+        await receiver.renewSessionLock();
+        this.sessionLockRenewalInfo.numberOfSuccessfulLockRenewals++;
+        const currentRenewalCount = this.sessionLockRenewalInfo.renewalCount[receiver.sessionId];
+        this.sessionLockRenewalInfo.renewalCount[receiver.sessionId] =
+          currentRenewalCount === undefined ? 1 : currentRenewalCount + 1;
+        const elapsedTime = new Date().valueOf() - startTime.valueOf();
+        if (duration - elapsedTime > 0) {
+          this.renewSessionLockUntil(receiver, duration - elapsedTime);
+        } else {
+          // Code reaches here only after the duration given has passed by
+          // TODO: Close the receiver maybe?
+        }
+      } catch (error) {
+        this.sessionLockRenewalInfo.numberOfFailedLockRenewals++;
+        this.sessionLockRenewalInfo.errorsInLockRenewal.push(error);
+        console.error("Error in message lock renewal: ", error);
+      }
+    }, receiver.sessionLockedUntilUtc!.valueOf() - startTime.valueOf() - 10000);
   }
 
   public snapshot(): void {
@@ -179,16 +227,28 @@ export class SBStressTestsBase {
       this.receiveInfo.numberOfSuccessfulReceives / elapsedTimeInSeconds
     );
     console.log(
-      "Number of successful lock renewals so far : ",
-      this.messageLockRenewalInfo.numberOfSuccessfulMessageLockRenewals
+      "Number of successful message lock renewals so far : ",
+      this.messageLockRenewalInfo.numberOfSuccessfulLockRenewals
     );
     console.log(
-      "Number of failed lock renewals so far : ",
-      this.messageLockRenewalInfo.numberOfFailedMessageLockRenewals
+      "Number of failed message lock renewals so far : ",
+      this.messageLockRenewalInfo.numberOfFailedLockRenewals
     );
     console.log(
-      "(Avg)Number of lock renewals per sec: ",
-      this.messageLockRenewalInfo.numberOfSuccessfulMessageLockRenewals / elapsedTimeInSeconds
+      "(Avg)Number of message lock renewals per sec: ",
+      this.messageLockRenewalInfo.numberOfSuccessfulLockRenewals / elapsedTimeInSeconds
+    );
+    console.log(
+      "Number of successful session lock renewals so far : ",
+      this.sessionLockRenewalInfo.numberOfSuccessfulLockRenewals
+    );
+    console.log(
+      "Number of failed session lock renewals so far : ",
+      this.sessionLockRenewalInfo.numberOfFailedLockRenewals
+    );
+    console.log(
+      "(Avg)Number of session lock renewals per sec: ",
+      this.sessionLockRenewalInfo.numberOfSuccessfulLockRenewals / elapsedTimeInSeconds
     );
     console.log("\n");
   }
@@ -199,8 +259,11 @@ export class SBStressTestsBase {
     // TODO: Have a copy of sentMessages and match them with receivedMessages, have the message-id's in the logged file maybe
     // TODO: Add an argument to "end()" to not delete the resource
     clearInterval(this.snapshotTimer);
-    for (const id in this.messageLockRenewalInfo.messageLockRenewalTimers) {
-      clearTimeout(this.messageLockRenewalInfo.messageLockRenewalTimers[id]);
+    for (const id in this.messageLockRenewalInfo.lockRenewalTimers) {
+      clearTimeout(this.messageLockRenewalInfo.lockRenewalTimers[id]);
+    }
+    for (const id in this.sessionLockRenewalInfo.lockRenewalTimers) {
+      clearTimeout(this.sessionLockRenewalInfo.lockRenewalTimers[id]);
     }
     await this.serviceBusAdministrationClient.deleteQueue(this.queueName);
   }
