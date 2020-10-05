bd03237d78853324e08c634cee4172f87632e3d8
Nick Guerrera <nicholg@microsoft.com>
2020-10-05 14:32:41 -0700

    Update recordings

--- sdk/schemaregistry/schema-registry/recordings/node/schemaregistryclient/recording_fails_to_get_schema_by_id_when_given_invalid_id.js
+++ sdk/schemaregistry/schema-registry/recordings/node/schemaregistryclient/recording_fails_to_get_schema_by_id_when_given_invalid_id.js
@@ -1,5 +1,5 @@
 let nock = require('nock');
 
-module.exports.hash = "eb54ecf2058c7d4917b656b697b37131";
+module.exports.hash = "7e5d35a42c93ab32fc6ec0332173c2f6";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
