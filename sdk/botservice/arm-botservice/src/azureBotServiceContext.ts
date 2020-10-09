b4b539306aced20da99046ef0b14f7308db80d54
SDK Automation <sdkautomation@microsoft.com>
2020-10-09 05:53:50 +0000

    Generated from b0ab4b1c3849dbf30b1759fb4697d1913bab990e

--- sdk/botservice/arm-botservice/src/azureBotServiceContext.ts
+++ sdk/botservice/arm-botservice/src/azureBotServiceContext.ts
@@ -44,7 +44,7 @@ export class AzureBotServiceContext extends msRestAzure.AzureServiceClient {
 
     super(credentials, options);
 
-    this.apiVersion = '2018-07-12';
+    this.apiVersion = '2020-06-02';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
