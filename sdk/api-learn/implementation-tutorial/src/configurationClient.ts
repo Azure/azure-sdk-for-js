fcea89f02fd0fb386f78add31a395a7beeed6855
Jeff Fisher <jeffish@microsoft.com>
2020-10-08 18:25:46 -0700

    in progress

--- sdk/api-learn/implementation-tutorial/src/configurationClient.ts
+++ sdk/api-learn/implementation-tutorial/src/configurationClient.ts
@@ -1,8 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { PipelineOptions, TokenCredential, OperationOptions } from "@azure/core-http";
-import { ConfigurationSetting } from "./generated/models";
+import {
+  PipelineOptions,
+  TokenCredential,
+  OperationOptions,
+  bearerTokenAuthenticationPolicy,
+  createPipelineFromOptions,
+  InternalPipelineOptions
+} from "@azure/core-http";
+import { SDK_VERSION } from "./constants";
+import { logger } from "./logger";
+import { ConfigurationSetting, GeneratedClient } from "./generated";
 
 // re-export generated types that are used as public interfaces.
 export { ConfigurationSetting };
@@ -16,14 +25,42 @@ export interface ConfigurationClientOptions extends PipelineOptions {
 }
 
 export class ConfigurationClient {
+  private client: GeneratedClient;
+
   constructor(
-    // These have a leading underscore to prevent TS from telling us
-    // they are not used anywhere.
-    _endpointUrl: string,
-    _credential: TokenCredential,
-    _options: ConfigurationClientOptions = {}
+    endpointUrl: string,
+    credential: TokenCredential,
+    options: ConfigurationClientOptions = {}
   ) {
-    throw new Error("Not yet implemented.");
+    // The below code helps us set a proper User-Agent header on all requests
+    const libInfo = `azsdk-js-api-learn-implementationtutorial/${SDK_VERSION}`;
+    if (!options.userAgentOptions) {
+      options.userAgentOptions = {};
+    }
+    if (options.userAgentOptions.userAgentPrefix) {
+      options.userAgentOptions.userAgentPrefix = `${options.userAgentOptions.userAgentPrefix} ${libInfo}`;
+    } else {
+      options.userAgentOptions.userAgentPrefix = libInfo;
+    }
+
+    // The AAD scope for an API is usually the baseUri + "/.default", but it may
+    // be different for your service.
+    const authPolicy = bearerTokenAuthenticationPolicy(credential, "{endpoint}/.default");
+
+    const internalPipelineOptions: InternalPipelineOptions = {
+      ...options,
+      ...{
+        loggingOptions: {
+          logger: logger.info,
+          // This array contains header names we want to log that are not already
+          // included as safe. Unknown/unsafe headers are logged as "<REDACTED>".
+          allowedHeaderNames: ["x-ms-correlation-request-id"]
+        }
+      }
+    };
+    const pipeline = createPipelineFromOptions(internalPipelineOptions, authPolicy);
+
+    this.client = new GeneratedClient(endpointUrl, pipeline);
   }
 
   public async getConfigurationSetting(
