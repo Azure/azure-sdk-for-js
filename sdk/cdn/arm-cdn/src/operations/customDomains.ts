1182f313ee529999fc49a0d2229e39eeb32eedb1
Chris Klug <chris@59north.com>
2020-10-12 15:54:49 +0200

    Updated default customDomainHttpsParameters

--- sdk/cdn/arm-cdn/src/operations/customDomains.ts
+++ sdk/cdn/arm-cdn/src/operations/customDomains.ts
@@ -14,6 +14,7 @@ import * as Models from "../models";
 import * as Mappers from "../models/customDomainsMappers";
 import * as Parameters from "../models/parameters";
 import { CdnManagementClientContext } from "../cdnManagementClientContext";
+import { Profiles } from "./profiles";
 
 /** Class representing a CustomDomains. */
 export class CustomDomains {
@@ -114,7 +115,7 @@ export class CustomDomains {
    * @returns Promise<Models.CustomDomainsCreateResponse>
    */
   create(resourceGroupName: string, profileName: string, endpointName: string, customDomainName: string, hostName: string, options?: msRest.RequestOptionsBase): Promise<Models.CustomDomainsCreateResponse> {
-    return this.beginCreate(resourceGroupName,profileName,endpointName,customDomainName,hostName,options)
+    return this.beginCreate(resourceGroupName, profileName, endpointName, customDomainName, hostName, options)
       .then(lroPoller => lroPoller.pollUntilFinished()) as Promise<Models.CustomDomainsCreateResponse>;
   }
 
@@ -128,7 +129,7 @@ export class CustomDomains {
    * @returns Promise<Models.CustomDomainsDeleteMethodResponse>
    */
   deleteMethod(resourceGroupName: string, profileName: string, endpointName: string, customDomainName: string, options?: msRest.RequestOptionsBase): Promise<Models.CustomDomainsDeleteMethodResponse> {
-    return this.beginDeleteMethod(resourceGroupName,profileName,endpointName,customDomainName,options)
+    return this.beginDeleteMethod(resourceGroupName, profileName, endpointName, customDomainName, options)
       .then(lroPoller => lroPoller.pollUntilFinished()) as Promise<Models.CustomDomainsDeleteMethodResponse>;
   }
 
@@ -200,35 +201,40 @@ export class CustomDomains {
    */
   enableCustomHttps(resourceGroupName: string, profileName: string, endpointName: string, customDomainName: string, options: Models.CustomDomainsEnableCustomHttpsOptionalParams, callback: msRest.ServiceCallback<Models.CustomDomain>): void;
   enableCustomHttps(resourceGroupName: string, profileName: string, endpointName: string, customDomainName: string, options?: Models.CustomDomainsEnableCustomHttpsOptionalParams | msRest.ServiceCallback<Models.CustomDomain>, callback?: msRest.ServiceCallback<Models.CustomDomain>): Promise<Models.CustomDomainsEnableCustomHttpsResponse> {
+    let newOptions: Models.CustomDomainsEnableCustomHttpsOptionalParams = {};
+
     if (typeof options === "function") {
       callback = options;
-      options = undefined;
+    } else {
+      newOptions = options as Models.CustomDomainsEnableCustomHttpsOptionalParams;
     }
 
-    if (!options) {
-      options = {};
+    if (!newOptions) {
+      newOptions = {};
     }
-    
-    if (!options.customDomainHttpsParameters) {
-      options.customDomainHttpsParameters = {
-                        certificateSource: "Cdn",
-                        certificateSourceParameters: {
-                            certificateType: "Dedicated"
-                        },
-                        protocolType: "ServerNameIndication"
-                    }
+
+    let optionsPreparationPromise = Promise.resolve(options);
+
+    if (!newOptions.customDomainHttpsParameters) {
+      let profiles = new Profiles(this.client);
+      optionsPreparationPromise = profiles.get(resourceGroupName, profileName).then(profile => {
+        newOptions.customDomainHttpsParameters = getDefaultCustomDomainHttpsParameters(profile);
+        return newOptions;
+      })
     }
-    
-    return this.client.sendOperationRequest(
-      {
-        resourceGroupName,
-        profileName,
-        endpointName,
-        customDomainName,
-        options
-      },
-      enableCustomHttpsOperationSpec,
-      callback) as Promise<Models.CustomDomainsEnableCustomHttpsResponse>;
+
+    return optionsPreparationPromise.then(options =>
+      this.client.sendOperationRequest(
+        {
+          resourceGroupName,
+          profileName,
+          endpointName,
+          customDomainName,
+          options
+        },
+        enableCustomHttpsOperationSpec,
+        callback) as Promise<Models.CustomDomainsEnableCustomHttpsResponse>
+    );
   }
 
   /**
@@ -303,6 +309,43 @@ export class CustomDomains {
       },
       listByEndpointNextOperationSpec,
       callback) as Promise<Models.CustomDomainsListByEndpointNextResponse>;
+  }
+}
+
+class SkuNames {
+  public static get standard_microsoft() { return "Standard_Microsoft"; }
+  public static get standard_verizon() { return "Standard_Verizon"; }
+  public static get standard_akamai() { return "Standard_Akamai"; }
+}
+
+function getDefaultCustomDomainHttpsParameters(profile: Models.Profile): Models.CdnManagedHttpsParameters | undefined {
+  switch (profile.sku.name) {
+    case SkuNames.standard_microsoft:
+      return {
+        certificateSource: "Cdn",
+        certificateSourceParameters: {
+          certificateType: "Dedicated"
+        },
+        protocolType: "ServerNameIndication"
+      }
+    case SkuNames.standard_akamai:
+      return {
+        certificateSource: "Cdn",
+        certificateSourceParameters: {
+          certificateType: "Shared"
+        },
+        protocolType: "ServerNameIndication"
+      }
+    case SkuNames.standard_verizon:
+      return {
+        certificateSource: "Cdn",
+        certificateSourceParameters: {
+          certificateType: "Shared"
+        },
+        protocolType: "IPBased"
+      }
+    default:
+      return undefined;
   }
 }
 
