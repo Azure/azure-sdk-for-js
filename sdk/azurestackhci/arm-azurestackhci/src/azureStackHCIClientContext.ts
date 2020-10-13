8458826b74b41341cdb26ed8457ac0969217bd55
colawwj <v-sigangzhou@microsoft.com>
2020-10-13 14:33:28 +0800

    update

--- sdk/azurestackhci/arm-azurestackhci/src/azureStackHCIClientContext.ts
+++ sdk/azurestackhci/arm-azurestackhci/src/azureStackHCIClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-azurestackhci";
-const packageVersion = "1.1.0";
+const packageVersion = "1.0.0";
 
 export class AzureStackHCIClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
