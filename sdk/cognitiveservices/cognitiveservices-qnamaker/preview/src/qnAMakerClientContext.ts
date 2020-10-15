8446f191f08833955976a479278882cf8ea9ea4f
SDK Automation <sdkautomation@microsoft.com>
2020-10-15 18:21:47 +0000

    Generated from 8eed9d09c35b97bcf179f2a068a060ab2aaf94cf

--- sdk/cognitiveservices/cognitiveservices-qnamaker/preview/src/qnAMakerClientContext.ts
+++ sdk/cognitiveservices/cognitiveservices-qnamaker/preview/src/qnAMakerClientContext.ts
@@ -19,8 +19,8 @@ export class QnAMakerClientContext extends msRest.ServiceClient {
 
   /**
    * Initializes a new instance of the QnAMakerClientContext class.
-   * @param endpoint Supported Cognitive Services endpoint (e.g., https://< qnamaker-resource-name>
-   * .api.cognitiveservices.azure.com).
+   * @param endpoint Supported Cognitive Services endpoint (e.g., https://< qnamaker-resource-name
+   * >.api.cognitiveservices.azure.com).
    * @param credentials Subscription credentials which uniquely identify client subscription.
    * @param [options] The parameter options
    */
@@ -43,7 +43,7 @@ export class QnAMakerClientContext extends msRest.ServiceClient {
 
     super(credentials, options);
 
-    this.baseUri = "{Endpoint}/qnamaker/v4.0";
+    this.baseUri = "{Endpoint}/qnamaker/v5.0-preview.1";
     this.requestContentType = "application/json; charset=utf-8";
     this.endpoint = endpoint;
     this.credentials = credentials;
