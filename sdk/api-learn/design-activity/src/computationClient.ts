f1a85529325b1dfb8752b6b5ea4e372c9fc1fca6
Chris Whilar <chwhilar@microsoft.com>
2020-10-22 16:20:56 -0700

    implementation tutorial

--- sdk/api-learn/design-activity/src/computationClient.ts
+++ sdk/api-learn/design-activity/src/computationClient.ts
@@ -1,25 +1,28 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+
+/// <reference lib="esnext.asynciterable" />
+import { PipelineOptions, TokenCredential } from "@azure/core-http";
+import { PagedAsyncIterableIterator } from "../../../core/core-paging";
 import {
-  PipelineOptions,
-  TokenCredential
-} from "@azure/core-http";
-import { PagedAsyncIterableIterator } from '../../../core/core-paging';
-import { GeneratedClient, LinuxComputeNode, PageOfComputeNodes, WindowsComputeNode } from "./generated";
-import { PollerLike } from '../../../core/core-lro/src/poller';
+  GeneratedClient,
+  LinuxComputeNode,
+  PageOfComputeNodes,
+  WindowsComputeNode
+} from "./generated";
+import { PollerLike } from "../../../core/core-lro/src/poller";
 import { PollOperationState } from "../../../core/core-lro/src/pollOperation";
 
 export interface ComputationClientOptions extends PipelineOptions {
   // any custom options go here.
 }
 
-
 export type ComputeNode = LinuxComputeNode | WindowsComputeNode;
 
 export type PiResult = number;
 
 export class ComputationClient {
-  private generatedClient: GeneratedClient;
+  private generatedClient: GeneratedClient | undefined;
 
   constructor(connectionString: string, options?: ComputationClientOptions);
   constructor(
@@ -30,6 +33,7 @@ export class ComputationClient {
     _options: ComputationClientOptions
   );
   constructor() {
+    console.log(this.generatedClient);
     throw new Error("Not yet implemented.");
   }
 
@@ -37,31 +41,47 @@ export class ComputationClient {
     throw new Error("Not yet implemented.");
   }
 
-  public async listComputeNodeAsync(): Promise<PagedAsyncIterableIterator<ComputeNode, PageOfComputeNodes>> {
+  public async listComputeNodeAsync(): Promise<
+    PagedAsyncIterableIterator<ComputeNode, PageOfComputeNodes>
+  > {
     throw new Error("Not yet implemented.");
   }
 
   public getComputeNode(nodeName: string): ComputeNode {
+    console.log(nodeName);
     throw new Error("Not yet implemented.");
   }
 
   public async getComputeNodeAsync(nodeName: string): Promise<ComputeNode> {
+    console.log(nodeName);
     throw new Error("Not yet implemented.");
   }
 
   public createComputeNode(node: ComputeNode): ComputeNode {
+    console.log(node);
     throw new Error("Not yet implemented.");
   }
 
   public async createComputeNodeAsync(node: ComputeNode): Promise<ComputeNode> {
+    console.log(node);
     throw new Error("Not yet implemented.");
   }
 
-  public beginComputePi(node: ComputeNode, precision: number): PollerLike<PollOperationState<PiResult>, PiResult> {
+  public beginComputePi(
+    node: ComputeNode,
+    precision: number
+  ): PollerLike<PollOperationState<PiResult>, PiResult> {
+    console.log(node);
+    console.log(precision);
     throw new Error("Not yet implemented.");
   }
 
-  public async beginComputePiAsync(node: ComputeNode, precision: number): Promise<PollerLike<PollOperationState<PiResult>, PiResult>> {
+  public async beginComputePiAsync(
+    node: ComputeNode,
+    precision: number
+  ): Promise<PollerLike<PollOperationState<PiResult>, PiResult>> {
+    console.log(node);
+    console.log(precision);
     throw new Error("Not yet implemented.");
   }
 }
--- sdk/api-learn/implementation-tutorial/src/configurationClient.ts
+++ sdk/api-learn/implementation-tutorial/src/configurationClient.ts
@@ -1,29 +1,79 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { PipelineOptions, TokenCredential, OperationOptions } from "@azure/core-http";
+import {
+  PipelineOptions,
+  TokenCredential,
+  bearerTokenAuthenticationPolicy,
+  createPipelineFromOptions,
+  InternalPipelineOptions,
+  OperationOptions
+} from "@azure/core-http";
+import { SDK_VERSION } from "./constants";
+import { logger } from "./logger";
 import { ConfigurationSetting } from "./generated/models";
-
-// re-export generated types that are used as public interfaces.
-export { ConfigurationSetting };
-
-export interface GetConfigurationSettingOptions extends OperationOptions {
-  onlyIfChanged?: boolean;
-}
+import { GeneratedClient } from './generated';
+import { quoteETag } from './util';
+import { createSpan } from "./tracing";
+import { CanonicalCode } from "@opentelemetry/api";
 
 export interface ConfigurationClientOptions extends PipelineOptions {
   // any custom options go here.
 }
 
+export interface GetConfigurationSettingOptions extends OperationOptions {
+  onlyIfChanged?: boolean;
+}
+
 export class ConfigurationClient {
+  private client: GeneratedClient;
+
   constructor(
-    // These have a leading underscore to prevent TS from telling us
-    // they are not used anywhere.
-    _endpointUrl: string,
-    _credential: TokenCredential,
-    _options: ConfigurationClientOptions = {}
+    endpointUrl: string,
+    credential: TokenCredential,
+    options: ConfigurationClientOptions = {}
   ) {
-    throw new Error("Not yet implemented.");
+    // The below code helps us set a proper User-Agent header on all requests
+    const libInfo = `azsdk-js-api-learn-implementationtutorial/${SDK_VERSION}`;
+    if (!options.userAgentOptions) {
+      options.userAgentOptions = {};
+    }
+    if (options.userAgentOptions.userAgentPrefix) {
+      options.userAgentOptions.userAgentPrefix = `${options.userAgentOptions.userAgentPrefix} ${libInfo}`;
+    } else {
+      options.userAgentOptions.userAgentPrefix = libInfo;
+    }
+
+    // The AAD scope for an API is usually the baseUri + "/.default", but it may
+    // be different for your service.
+    const authPolicy = bearerTokenAuthenticationPolicy(credential, `${endpointUrl}/.default`);
+
+    const internalPipelineOptions: InternalPipelineOptions = {
+      ...options,
+      deserializationOptions: {
+        expectedContentTypes: {
+          // This helps the protocol layer decode the MIME types
+          // send by the service correctly.
+          json: [
+            "application/vnd.microsoft.appconfig.kvset+json",
+            "application/vnd.microsoft.appconfig.kv+json",
+            "application/vnd.microsoft.appconfig.kvs+json",
+            "application/vnd.microsoft.appconfig.keyset+json",
+            "application/vnd.microsoft.appconfig.revs+json"
+          ]
+        }
+      },
+      ...{
+        loggingOptions: {
+          logger: logger.info,
+          // This array contains header names we want to log that are not already
+          // included as safe. Unknown/unsafe headers are logged as "<REDACTED>".
+          allowedHeaderNames: ["x-ms-correlation-request-id"]
+        }
+      }
+    };
+    const pipeline = createPipelineFromOptions(internalPipelineOptions, authPolicy);
+    this.client = new GeneratedClient(endpointUrl, pipeline);
   }
 
   public async getConfigurationSetting(
@@ -35,9 +85,47 @@ export class ConfigurationClient {
     options?: GetConfigurationSettingOptions
   ): Promise<ConfigurationSetting>;
   public async getConfigurationSetting(
-    _keyOrSetting: string | ConfigurationSetting,
-    _options: GetConfigurationSettingOptions = {}
+    keyOrSetting: string | ConfigurationSetting,
+    options: GetConfigurationSettingOptions = {}
   ): Promise<ConfigurationSetting> {
-    throw new Error("Not yet implemented.");
+    let key: string;
+    let ifNoneMatch: string | undefined;
+
+    if (typeof keyOrSetting === "string") {
+      key = keyOrSetting;
+      if (options.onlyIfChanged) {
+        throw new RangeError(
+          "You must pass a ConfigurationSetting instead of a key to perform a conditional fetch."
+        );
+      }
+    } else {
+      key = keyOrSetting.key;
+      const etag = keyOrSetting.etag;
+      if (options.onlyIfChanged) {
+        ifNoneMatch = quoteETag(etag);
+      }
+    }
+    const { span, updatedOptions } = createSpan(
+      // Here you set the name of the span, usually clientName-operationName
+      "ConfigurationClient-getConfigurationSetting",
+      options
+    );
+    try {
+      const result = await this.client.getKeyValue(key, {
+        ...updatedOptions,
+        ifNoneMatch
+      });
+      return result;
+    } catch (e) {
+      // There are different standard codes available for different errors:
+      // https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/api.md#status
+      span.setStatus({
+        code: CanonicalCode.UNKNOWN,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
   }
 }
