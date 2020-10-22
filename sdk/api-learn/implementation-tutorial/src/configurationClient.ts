907a741187da0c69fdee93b87527ffbfa3769870
Dominik Messinger <domessin@microsoft.com>
2020-10-22 14:54:48 -0700

    implement first service method

--- sdk/api-learn/implementation-tutorial/src/configurationClient.ts
+++ sdk/api-learn/implementation-tutorial/src/configurationClient.ts
@@ -1,8 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { PipelineOptions, TokenCredential, OperationOptions } from "@azure/core-http";
+import {
+  PipelineOptions,
+  TokenCredential,
+  OperationOptions,
+  InternalPipelineOptions,
+  createPipelineFromOptions,
+  bearerTokenAuthenticationPolicy
+} from "@azure/core-http";
+import { CanonicalCode } from "@opentelemetry/api";
+import { SDK_VERSION } from "./constants";
+import { GeneratedClient } from "./generated";
 import { ConfigurationSetting } from "./generated/models";
+import { logger } from "./logger";
+import { createSpan } from "./tracing";
+import { quoteETag } from "./util";
 
 // re-export generated types that are used as public interfaces.
 export { ConfigurationSetting };
@@ -16,14 +29,54 @@ export interface ConfigurationClientOptions extends PipelineOptions {
 }
 
 export class ConfigurationClient {
+  private client: GeneratedClient;
+
   constructor(
-    // These have a leading underscore to prevent TS from telling us
-    // they are not used anywhere.
-    _endpointUrl: string,
-    _credential: TokenCredential,
-    _options: ConfigurationClientOptions = {}
+    endpointUrl: string,
+    credential: TokenCredential,
+    options: ConfigurationClientOptions = {}
   ) {
-    throw new Error("Not yet implemented.");
+    const libInfo = `azsdk-js-api-lean-implementationtutorial/${SDK_VERSION}`;
+    if (!options.userAgentOptions) {
+      options.userAgentOptions = {};
+    }
+    if (options.userAgentOptions.userAgentPrefix) {
+      options.userAgentOptions.userAgentPrefix = `${options.userAgentOptions.userAgentPrefix} ${libInfo}`;
+    } else {
+      options.userAgentOptions.userAgentPrefix = libInfo;
+    }
+
+    // The AAD scope for an API is usually the baseUri + "/.default", but it may
+    // be different for your service
+    const authPolicy = bearerTokenAuthenticationPolicy(credential, `${endpointUrl}./.default`);
+
+    const internalPipelineOptions: InternalPipelineOptions = {
+      ...options,
+      deserializationOptions: {
+        expectedContentTypes: {
+          // This helps the protocol layer decode the MIME types
+          // send by the service correctly.
+          json: [
+            "application/vnd.microsoft.appconfig.kvset+json",
+            "application/vnd.microsoft.appconfig.kv+json",
+            "application/vnd.microsoft.appconfig.kvs+json",
+            "application/vnd.microsoft.appconfig.keyset+json",
+            "application/vnd.microsoft.appconfig.revs+json"
+          ]
+        }
+      },
+      ...{
+        loggingOptions: {
+          logger: logger.info,
+          // This array contains header names we want to log that are not already
+          // included as safe. Unknown/unsafe headers are logged as "<REDACTED>".
+          allowedHeaderNames: ["x-ms-correlation-request-id"]
+        }
+      }
+    };
+    const pipeline = createPipelineFromOptions(internalPipelineOptions, authPolicy);
+
+    this.client = new GeneratedClient(endpointUrl, pipeline);
   }
 
   public async getConfigurationSetting(
@@ -35,9 +88,41 @@ export class ConfigurationClient {
     options?: GetConfigurationSettingOptions
   ): Promise<ConfigurationSetting>;
   public async getConfigurationSetting(
-    _keyOrSetting: string | ConfigurationSetting,
-    _options: GetConfigurationSettingOptions = {}
+    keyOrSetting: string | ConfigurationSetting,
+    options: GetConfigurationSettingOptions = {}
   ): Promise<ConfigurationSetting> {
-    throw new Error("Not yet implemented.");
+    let key: string;
+    let ifNoneMatch: string | undefined;
+
+    if (typeof keyOrSetting === "string") {
+      key = keyOrSetting;
+      if (options.onlyIfChanged) {
+        throw new RangeError(
+          "You must pass a ConfigurationSetting instead of a key to perform a conditional fetch."
+        );
+      }
+    } else {
+      key = keyOrSetting.key;
+      const etag = keyOrSetting.etag;
+      if (options.onlyIfChanged) {
+        ifNoneMatch = quoteETag(etag);
+      }
+    }
+    const { span, updatedOptions } = createSpan(
+      "ConfigurationClient-getConfigurationSetting",
+      options
+    );
+    try {
+      const result = await this.client.getKeyValue(key, { ...updatedOptions, ifNoneMatch });
+      return result;
+    } catch (e) {
+      span.setStatus({
+        code: CanonicalCode.UNKNOWN,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
   }
 }
