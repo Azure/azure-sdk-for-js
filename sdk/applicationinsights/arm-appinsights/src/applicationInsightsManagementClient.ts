9e7263dbe42cabc03de79b0906084b8a07d6d85a
SDK Automation <sdkautomation@microsoft.com>
2020-10-22 02:00:16 +0000

    Generated from fff94e486159253b602a435dd6682ea0092820e0

--- sdk/applicationinsights/arm-appinsights/src/applicationInsightsManagementClient.ts
+++ sdk/applicationinsights/arm-appinsights/src/applicationInsightsManagementClient.ts
@@ -18,46 +18,18 @@ import { ApplicationInsightsManagementClientContext } from "./applicationInsight
 class ApplicationInsightsManagementClient extends ApplicationInsightsManagementClientContext {
   // Operation groups
   operations: operations.Operations;
-  annotations: operations.Annotations;
-  aPIKeys: operations.APIKeys;
-  exportConfigurations: operations.ExportConfigurations;
-  componentCurrentBillingFeatures: operations.ComponentCurrentBillingFeatures;
-  componentQuotaStatus: operations.ComponentQuotaStatus;
-  componentFeatureCapabilities: operations.ComponentFeatureCapabilities;
-  componentAvailableFeatures: operations.ComponentAvailableFeatures;
-  proactiveDetectionConfigurations: operations.ProactiveDetectionConfigurations;
-  components: operations.Components;
-  workItemConfigurations: operations.WorkItemConfigurations;
-  favorites: operations.Favorites;
-  webTestLocations: operations.WebTestLocations;
   webTests: operations.WebTests;
-  analyticsItems: operations.AnalyticsItems;
-  workbooks: operations.Workbooks;
 
   /**
    * Initializes a new instance of the ApplicationInsightsManagementClient class.
    * @param credentials Credentials needed for the client to connect to Azure.
-   * @param subscriptionId The Azure subscription ID.
+   * @param subscriptionId The ID of the target subscription.
    * @param [options] The parameter options
    */
   constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.ApplicationInsightsManagementClientOptions) {
     super(credentials, subscriptionId, options);
     this.operations = new operations.Operations(this);
-    this.annotations = new operations.Annotations(this);
-    this.aPIKeys = new operations.APIKeys(this);
-    this.exportConfigurations = new operations.ExportConfigurations(this);
-    this.componentCurrentBillingFeatures = new operations.ComponentCurrentBillingFeatures(this);
-    this.componentQuotaStatus = new operations.ComponentQuotaStatus(this);
-    this.componentFeatureCapabilities = new operations.ComponentFeatureCapabilities(this);
-    this.componentAvailableFeatures = new operations.ComponentAvailableFeatures(this);
-    this.proactiveDetectionConfigurations = new operations.ProactiveDetectionConfigurations(this);
-    this.components = new operations.Components(this);
-    this.workItemConfigurations = new operations.WorkItemConfigurations(this);
-    this.favorites = new operations.Favorites(this);
-    this.webTestLocations = new operations.WebTestLocations(this);
     this.webTests = new operations.WebTests(this);
-    this.analyticsItems = new operations.AnalyticsItems(this);
-    this.workbooks = new operations.Workbooks(this);
   }
 }
 
