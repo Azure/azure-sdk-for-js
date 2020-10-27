06f98d4890ab9a1be516db8903d834d76a0d0eaa
SDK Automation <sdkautomation@microsoft.com>
2020-10-27 03:31:24 +0000

    Generated from 02d3677a017daf5b96b78cee612015f249f55fc6
    fix java readme

--- sdk/netapp/arm-netapp/src/models/backupsMappers.ts
+++ sdk/netapp/arm-netapp/src/models/backupsMappers.ts
@@ -12,12 +12,16 @@ export {
   BackupPatch,
   BackupPolicy,
   BackupPolicyDetails,
+  BackupPolicyPatch,
   BackupsList,
   BaseResource,
   CapacityPool,
   CapacityPoolPatch,
   CloudError,
+  DailySchedule,
   ExportPolicyRule,
+  HourlySchedule,
+  MonthlySchedule,
   MountTargetProperties,
   NetAppAccount,
   NetAppAccountPatch,
@@ -33,5 +37,6 @@ export {
   VolumePatchPropertiesExportPolicy,
   VolumePropertiesDataProtection,
   VolumePropertiesExportPolicy,
-  VolumeSnapshotProperties
+  VolumeSnapshotProperties,
+  WeeklySchedule
 } from "../models/mappers";
