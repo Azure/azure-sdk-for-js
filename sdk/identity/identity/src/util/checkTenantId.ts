ee66c5b18998d0f4a45678d746ef552856df8255
Jonathan Turner <joturner@microsoft.com>
2020-10-29 13:13:18 +1300

    fix lint

--- sdk/identity/identity/src/util/checkTenantId.ts
+++ sdk/identity/identity/src/util/checkTenantId.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { CredentialLogger, formatError } from "../util/logging";
 
 export function checkTenantId(logger: CredentialLogger, tenantId: string) {
