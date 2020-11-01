b853e0d6ad61fb82c912a847cda99e20c1aaaa95
Daniel Rodríguez <sadasant@users.noreply.github.com>
2020-11-01 14:35:47 -0500

    [Identity] VSCode credential default cloud and authority hosts (#11780)
    If I understood this correctly, the solution to #11778 is to: default to AzureCloud on this credential.
    
    This PR means that there will be no notion of "Azure" as a cloud name. However, if that credential exists (and only that exists) in the system, and no cloud name is officially set on the VSCode configuration files, it might still be taken, since we take whatever is available. That shouldn't be a problem on users that are authenticated through the VSCode extension, since they will have a concrete cloud name.
    
    **IMPORTANT**:
    After speaking with @g2vinay , he helped me realize that the VSCode credential still needs to map to the correct cloud based on the cloud name we get from the configurations! So I'll add that change to this PR.
    
    Feedback appreciated.
    
    Fixes #11778

--- sdk/identity/identity/src/credentials/visualStudioCodeCredential.ts
+++ sdk/identity/identity/src/credentials/visualStudioCodeCredential.ts
@@ -16,6 +16,7 @@ try {
 
 import { CredentialUnavailable } from "../client/errors";
 import { credentialLogger, formatSuccess, formatError } from "../util/logging";
+import { AzureAuthorityHosts } from "../constants";
 import { checkTenantId } from "../util/checkTenantId";
 
 const CommonTenantId = "common";
@@ -35,6 +36,15 @@ function checkUnsupportedTenant(tenantId: string): void {
     throw new CredentialUnavailable(unsupportedTenantError);
   }
 }
+
+type VSCodeCloudNames = "AzureCloud" | "AzureChina" | "AzureGermanCloud" | "AzureUSGovernment";
+
+const mapVSCodeAuthorityHosts: Record<VSCodeCloudNames, string> = {
+  AzureCloud: AzureAuthorityHosts.AzurePublicCloud,
+  AzureChina: AzureAuthorityHosts.AzureChina,
+  AzureGermanCloud: AzureAuthorityHosts.AzureGermany,
+  AzureUSGovernment: AzureAuthorityHosts.AzureGovernment
+};
 
 /**
  * Attempts to load a specific property from the VSCode configurations of the current OS.
@@ -89,6 +99,7 @@ export interface VisualStudioCodeCredentialOptions extends TokenCredentialOption
 export class VisualStudioCodeCredential implements TokenCredential {
   private identityClient: IdentityClient;
   private tenantId: string;
+  private cloudName: VSCodeCloudNames;
 
   /**
    * Creates an instance of VisualStudioCodeCredential to use for automatically authenticating via VSCode.
@@ -96,7 +107,18 @@ export class VisualStudioCodeCredential implements TokenCredential {
    * @param options Options for configuring the client which makes the authentication request.
    */
   constructor(options?: VisualStudioCodeCredentialOptions) {
-    this.identityClient = new IdentityClient(options);
+    // We want to make sure we use the one assigned by the user on the VSCode settings.
+    // Or just `AzureCloud` by default.
+    this.cloudName = (getPropertyFromVSCode("azure.cloud") || "AzureCloud") as VSCodeCloudNames;
+
+    // Picking an authority host based on the cloud name.
+    const authorityHost = mapVSCodeAuthorityHosts[this.cloudName];
+
+    this.identityClient = new IdentityClient({
+      authorityHost,
+      ...options
+    });
+
     if (options && options.tenantId) {
       checkTenantId(logger, options.tenantId);
 
@@ -177,13 +199,9 @@ export class VisualStudioCodeCredential implements TokenCredential {
     // ]
     const credentials = await keytar.findCredentials(VSCodeUserName);
 
-    // We want to make sure we use the one assigned by the user on the VSCode settings.
-    // Or just `Azure` by default.
-    const cloudName = getPropertyFromVSCode("azure.cloud") || "Azure";
-
     // If we can't find the credential based on the name, we'll pick the first one available.
     const { password } =
-      credentials.find((cred: { account: string }) => cred.account === cloudName) ||
+      credentials.find((cred: { account: string }) => cred.account === this.cloudName) ||
       credentials[0] ||
       {};
 
