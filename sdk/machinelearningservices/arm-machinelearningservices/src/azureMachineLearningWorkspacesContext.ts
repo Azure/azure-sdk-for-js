930934af477e8612c65374788225dedc8b93434b
qiaozha <qiaozha@microsoft.com>
2020-11-03 13:42:30 +0800

    version bump

--- sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspacesContext.ts
+++ sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspacesContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-machinelearningservices";
-const packageVersion = "3.0.0";
+const packageVersion = "4.0.0";
 
 export class AzureMachineLearningWorkspacesContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
