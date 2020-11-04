750902be0e7f5d10bab750fa6e4bba7781f137a4
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-11-04 18:13:43 +0000

    [Data plane][Azure Cognitive Search] Add encryptionKey property to indexer, datasource and skillset metadata (#12275)
    * Result of Regeneration Command
    
    * Changes to the custom layer
    
    * API Changes
    
    * Update Recordings

--- sdk/search/search-documents/recordings/node/searchindexclient_synonymmaps/recording_gets_the_list_of_synonymmaps.js
+++ sdk/search/search-documents/recordings/node/searchindexclient_synonymmaps/recording_gets_the_list_of_synonymmaps.js
@@ -7,7 +7,7 @@ module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 nock('https://endpoint:443', {"encodedQueryParams":true})
   .get('/synonymmaps')
   .query(true)
-  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195efff1e67a592daf178b6cd57c34fae8322bd7f9478fbe67a153b30b02fdfb7eb4f3eee0e9c1a77bfb0f0e769edd7f76f2f4e9effb11b55f660b6afed1e27a3bfbc1baceb71db4ed5dfafabcaa1719506baab2a6bff5eb863ef96a59b4f92c7ddd6684ee280dfe4cabf3f47891d7c5344b3f3b4abf7a7dfcfb2ebf9b35f36279d156cb518adfc7f8e6bbc704345f4eebeb555b54cbdf2bbffee8d1725d96bf64b479004fefefee9dde34803dfafae76c00dfff25ff0fe90dbeb7e1010000"], [ 'Cache-Control',
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195efff1e67a592daf178b6cd57c34fae8322bd7f9478fbe67a153b30b02fdfb7eb4f3eee0e9c1c1cec3e34f3f3d38d8db7f76eff7fd88da2fb30535ff6871bd9dfd605de7db0edaf62e7d7d5ed58b0ca8355559d3dffa75439f7cb52cda7c96be6e33427794067fa6d5797abcc8eb629aa59f1da55fbd3efe7d97dfcd9a79b1bc68abe528c5ef637cf3dd63029a2fa7f5f5aa2daae5ef955f7ff468b92ecb5f32da3c80673bf79e3db969007bf4f5cfd900beff4bfe1f0031ff3be1010000"], [ 'Cache-Control',
   'no-cache',
   'Pragma',
   'no-cache',
@@ -20,9 +20,9 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Vary',
   'Accept-Encoding',
   'request-id',
-  '0217db9d-dece-4301-a156-845a5926a771',
+  '5abbb118-66be-4865-a2ad-42ecebcf6c0a',
   'elapsed-time',
-  '16',
+  '28',
   'OData-Version',
   '4.0',
   'Preference-Applied',
@@ -30,6 +30,6 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Strict-Transport-Security',
   'max-age=15724800; includeSubDomains',
   'Date',
-  'Sat, 26 Sep 2020 18:10:54 GMT',
+  'Wed, 04 Nov 2020 08:19:55 GMT',
   'Content-Length',
   '356' ]);
--- sdk/search/search-documents/recordings/node/searchindexerclient_datasourceconnections/recording_modify_and_updates_the_datasourceconnection_object.js
+++ sdk/search/search-documents/recordings/node/searchindexerclient_datasourceconnections/recording_modify_and_updates_the_datasourceconnection_object.js
@@ -7,7 +7,7 @@ module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 nock('https://endpoint:443', {"encodedQueryParams":true})
   .get('/datasources(%27my-data-source-1%27)')
   .query(true)
-  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195eff71fcd354eb7a9a37777fb77cd916edf54723039b1a5d10e0dff7a39d77074f0f3edddb7ff06cffc1c9a7fba73bbfef47d46a992d72fa7a71bd8dd6db02667b97be99e5cdb42e566d512d3f7ab45c97e5e8a3f67a85c6d3aa5954cd6c428d9af5443e9406d33a9f0181ac6c3e7af48ba9e172994f01e1755b174bc203cd7e09b5a30167c532afd14a5198576d4eaf8d3efa45ebbcbeb64d81d6c93c5b5ee44ff35680bdacca62aa2da4c1d3bcccf1cd40937c39adaf7924bf57ae1ffe92ff07934980927d010000"], [ 'Cache-Control',
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195eff71fcd354eb7a9a37777fb77cd916edf54723039b1a5d10e0dff7a39d77074f0f0e761e3ed97d72f26ce7fecebddff7236ab5cc16397dbdb8de46eb6d01b3bd4bdfccf2665a17abb6a8961f3d5aaecb72f4517bbd42e369d52caa6636a146cd7a221f4a83699dcf804056361f3dfac5d470b9cca780f0baad8b25e18166bf84dad180b36299d768a528ccab36a7d7461ffda2755e5fdba640eb649e2d2ff2a7792bc05e566531d516d2e0695ee6f866a049be9cd6d73c92df2bd70f7fc9ff031a3cd54f7d010000"], [ 'Cache-Control',
   'no-cache',
   'Pragma',
   'no-cache',
@@ -18,13 +18,13 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Expires',
   '-1',
   'ETag',
-  'W/"0x8D86247F47C64E0"',
+  'W/"0x8D8809B1BCF0503"',
   'Vary',
   'Accept-Encoding',
   'request-id',
-  '876e5bb0-7f92-436a-b554-4e38d09b1be7',
+  'e3d11984-6018-41b3-bc99-0ec54696c4b7',
   'elapsed-time',
-  '7',
+  '11',
   'OData-Version',
   '4.0',
   'Preference-Applied',
@@ -32,14 +32,14 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Strict-Transport-Security',
   'max-age=15724800; includeSubDomains',
   'Date',
-  'Sat, 26 Sep 2020 18:14:13 GMT',
+  'Wed, 04 Nov 2020 08:25:02 GMT',
   'Content-Length',
   '367' ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .put('/datasources(%27my-data-source-1%27)', {"name":"my-data-source-1","description":"my-data-source-1","type":"cosmosdb","credentials":{"connectionString":"<unchanged>"},"container":{"name":"my-container-2","query":null},"dataChangeDetectionPolicy":null,"dataDeletionDetectionPolicy":null,"@odata.etag":"\"0x8D86247F47C64E0\""})
+  .put('/datasources(%27my-data-source-1%27)', {"name":"my-data-source-1","description":"my-data-source-1","type":"cosmosdb","credentials":{"connectionString":null},"container":{"name":"my-container-2","query":null},"dataChangeDetectionPolicy":null,"dataDeletionDetectionPolicy":null,"@odata.etag":"\"0x8D8809B1BCF0503\"","encryptionKey":null})
   .query(true)
-  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195eff71fcd354eb7a9a37777fb77cd916edf54723039b1a5d10e0dff7a39d77074f0f3edddb7ff0ece1bda7cf9eeddefb7d3fa256cb6c91d3d78beb6db4de1630dbbbf4cd2c6fa675b16a8b6a196fd05eaff0eab46a1655339bd027cd7a221f2ed76539fa685ae733a09395cd478f7e31355c2ef329e0bd6eeb624958a1d92fa17634fcac58e6355a3984ecc7db7b04fb17adf3fadabe025c4ee6d9f2227f9ab702f4655516536d210d9ee6658e6f069ae4cb697dcde3fbbd72fdf097fc3ff8e5263393010000"], [ 'Cache-Control',
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195eff71fcd354eb7a9a37777fb77cd916edf54723039b1a5d10e0dff7a39d77074f0f0e761e3ed9db39dedb3b3e7df2fb7e44ad96d922a7af17d7db68bd2d60b677e99b59de4ceb62d516d532dea0bd5ee1d569d52caa6636a14f9af5443e5caecb72f4d1b4ce6740272b9b8f1efd626ab85ce653c07bddd6c592b042b35f42ed68f859b1cc6bb47208d98fb7f708f62f5ae7f5b57d05b89cccb3e545fe346f05e8cbaa2ca6da421a3ccdcb1cdf0c34c997d3fa9ac7f77be5fae12ff97f0074fec97693010000"], [ 'Cache-Control',
   'no-cache',
   'Pragma',
   'no-cache',
@@ -50,13 +50,13 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Expires',
   '-1',
   'ETag',
-  'W/"0x8D86247F93DFF13"',
+  'W/"0x8D8809B20A22AEB"',
   'Vary',
   'Accept-Encoding',
   'request-id',
-  '4bb4a2d4-8a73-4097-aba0-860b88592845',
+  '1cb0759f-3e0c-4428-9f40-e54ab1b993a4',
   'elapsed-time',
-  '57',
+  '112',
   'OData-Version',
   '4.0',
   'Preference-Applied',
@@ -64,14 +64,14 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Strict-Transport-Security',
   'max-age=15724800; includeSubDomains',
   'Date',
-  'Sat, 26 Sep 2020 18:14:13 GMT',
+  'Wed, 04 Nov 2020 08:25:02 GMT',
   'Content-Length',
   '364' ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
   .get('/datasources(%27my-data-source-1%27)')
   .query(true)
-  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195eff71fcd354eb7a9a37777fb77cd916edf54723039b1a5d10e0dff7a39d77074f0f3edddb7ff0ece1bda7cf9eeddefb7d3fa256cb6c91d3d78beb6db4de1630dbbbf4cd2c6fa675b16a8b6a196fd05eaff0eab46a1655339bd027cd7a221f2ed76539fa685ae733a09395cd478f7e31355c2ef329e0bd6eeb624958a1d92fa17634fcac58e6355a3984ecc7db7b04fb17adf3fadabe025c4ee6d9f2227f9ab702f4655516536d210d9ee6658e6f069ae4cb697dcde3fbbd72fdf097fc3ff8e5263393010000"], [ 'Cache-Control',
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef147bf6735cbda6c3cad966dfeaefde8d147f3b65d358feede6df3a66df2ac9ecea759933763f97d7c552c67d555335ee6eddddf6d91b7195eff71fcd354eb7a9a37777fb77cd916edf54723039b1a5d10e0dff7a39d77074f0f0e761e3ed9db39dedb3b3e7df2fb7e44ad96d922a7af17d7db68bd2d60b677e99b59de4ceb62d516d532dea0bd5ee1d569d52caa6636a14f9af5443e5caecb72f4d1b4ce6740272b9b8f1efd626ab85ce653c07bddd6c592b042b35f42ed68f859b1cc6bb47208d98fb7f708f62f5ae7f5b57d05b89cccb3e545fe346f05e8cbaa2ca6da421a3ccdcb1cdf0c34c997d3fa9ac7f77be5fae12ff97f0074fec97693010000"], [ 'Cache-Control',
   'no-cache',
   'Pragma',
   'no-cache',
@@ -82,13 +82,13 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Expires',
   '-1',
   'ETag',
-  'W/"0x8D86247F93DFF13"',
+  'W/"0x8D8809B20A22AEB"',
   'Vary',
   'Accept-Encoding',
   'request-id',
-  '1ba334bd-6f55-4892-bc8d-b2987a2e8a2a',
+  '6da12f2e-a094-4faa-8685-b7e526b5f92f',
   'elapsed-time',
-  '8',
+  '35',
   'OData-Version',
   '4.0',
   'Preference-Applied',
@@ -96,6 +96,6 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Strict-Transport-Security',
   'max-age=15724800; includeSubDomains',
   'Date',
-  'Sat, 26 Sep 2020 18:14:13 GMT',
+  'Wed, 04 Nov 2020 08:25:02 GMT',
   'Content-Length',
   '364' ]);
