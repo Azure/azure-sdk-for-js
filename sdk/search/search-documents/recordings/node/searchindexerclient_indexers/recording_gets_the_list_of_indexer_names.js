6f14a8948f0c5219be6abfa90eb75fc148437034
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-11-04 00:28:15 -0800

    Update Recordings

--- sdk/search/search-documents/recordings/node/searchindexerclient_indexers/recording_gets_the_list_of_indexer_names.js
+++ sdk/search/search-documents/recordings/node/searchindexerclient_indexers/recording_gets_the_list_of_indexer_names.js
@@ -20,7 +20,7 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Vary',
   'Accept-Encoding',
   'request-id',
-  '1f712844-c8c7-4644-ae57-2f131069308e',
+  'dd4075ff-f06a-45a7-9c2d-98e4f6885c83',
   'elapsed-time',
   '7',
   'OData-Version',
@@ -30,6 +30,6 @@ nock('https://endpoint:443', {"encodedQueryParams":true})
   'Strict-Transport-Security',
   'max-age=15724800; includeSubDomains',
   'Date',
-  'Sat, 26 Sep 2020 18:12:14 GMT',
+  'Wed, 04 Nov 2020 08:22:38 GMT',
   'Content-Length',
   '233' ]);
