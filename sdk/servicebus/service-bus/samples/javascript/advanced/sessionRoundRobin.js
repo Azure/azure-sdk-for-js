82ff76f4681521ad86eb5bb4a9b8853c70ffabdb
Harsha Nalluru <sanallur@microsoft.com>
2020-11-04 22:01:36 -0800

    [Service Bus] Fix samples (#12289)

--- sdk/servicebus/service-bus/samples/javascript/advanced/sessionRoundRobin.js
+++ sdk/servicebus/service-bus/samples/javascript/advanced/sessionRoundRobin.js
@@ -83,7 +83,7 @@ async function receiveFromNextSession(serviceBusClient) {
   } catch (err) {
     if (
       isServiceBusError(err) &&
-      (err.reason === "SessionCannotBeLocked" || err.reason === "OperationTimeout")
+      (err.reason === "SessionCannotBeLocked" || err.reason === "ServiceTimeout")
     ) {
       console.log(`INFO: no available sessions, sleeping for ${delayOnErrorMs}`);
     } else {
