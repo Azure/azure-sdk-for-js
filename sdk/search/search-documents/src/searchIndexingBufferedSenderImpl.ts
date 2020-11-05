f9fa19864d2c991d1e9e8a1ad9451b4786dabe17
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-11-05 14:25:24 -0800

    Format

--- sdk/search/search-documents/src/searchIndexingBufferedSenderImpl.ts
+++ sdk/search/search-documents/src/searchIndexingBufferedSenderImpl.ts
@@ -382,9 +382,9 @@ class SearchIndexingBufferedSenderImpl<T> implements SearchIndexingBufferedSende
       this.emitter.emit("batchSucceeded", result);
     } catch (e) {
       if (this.isRetryAbleError(e) && retryAttempt <= this.maxRetries) {
-        const jitterValue = Math.floor(Math.random() * 10) + 1;
+        const jitterValue = Math.floor(Math.random() * 1000) + 1;
         const timeToWait = Math.min(
-          Math.pow(2, (retryAttempt * this.retryDelayInMs) + jitterValue),
+          Math.pow(2, retryAttempt * this.retryDelayInMs + jitterValue),
           this.maxRetryDelayInMs
         );
         await delay(timeToWait + jitterValue);
