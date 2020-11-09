58e14e9eb53b176e190235b890b3919d758a5f51
HarshaNalluru <sanallur@microsoft.com>
2020-11-09 15:39:12 -0800

    remove TODO since fixed

--- sdk/servicebus/service-bus/test/stress-test-track-2/scenarioRenewSessionLock.ts
+++ sdk/servicebus/service-bus/test/stress-test-track-2/scenarioRenewSessionLock.ts
@@ -48,7 +48,6 @@ function sanitizeOptions(args: string[]): Required<ScenarioRenewSessionLockOptio
 }
 
 // TODO: max lock renewal duration to be 70% of testDuration instead of 100%
-// TODO: stop sending messages after a 70% of test duration
 // TODO: Upon ending max lock renewal duration, pass an option to complete/ignore the message
 export async function scenarioRenewSessionLock() {
   const testOptions = sanitizeOptions(process.argv);
