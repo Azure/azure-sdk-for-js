ac7c38cac2c60429f1907468589714fee3c2a73c
Jeff Fisher <jeffish@microsoft.com>
2020-11-13 15:50:27 -0800

    Setup api-extractor

--- sdk/monitor/opentelemetry-exporter-azure-monitor/src/index.ts
+++ sdk/monitor/opentelemetry-exporter-azure-monitor/src/index.ts
@@ -2,3 +2,7 @@
 // Licensed under the MIT license.
 
 export * from "./export/trace";
+export * from "./export/exporter";
+export * from "./types";
+export { TelemetryItem, ContextTagKeys, MonitorBase, MonitorDomain } from "./generated";
+export { AzureExporterConfig } from "./config";
