4bbf954d149df3b1f38b38bcfa079fc9bfac0514
Chidozie Ononiwu <chononiw@microsoft.com>
2020-11-18 11:31:20 -0800

    Switch from using BuildNumber to BuildID for test versions

--- eng/scripts/SetTestPipelineVersion.ps1
+++ eng/scripts/SetTestPipelineVersion.ps1
@@ -3,7 +3,7 @@
 
 param (
   [Parameter(mandatory = $true)]
-  $BuildNumber
+  $BuildID
 )
 
 . "${PSScriptRoot}\..\common\scripts\common.ps1"
@@ -24,7 +24,7 @@ LogDebug "Last Published Version $($semVarsSorted[0])"
 
 $newVersion = [AzureEngSemanticVersion]::ParseVersionString($semVarsSorted[0])
 $newVersion.PrereleaseLabel = "beta"
-$newVersion.PrereleaseNumber = $BuildNumber
+$newVersion.PrereleaseNumber = $BuildID
 
 $packageFileContent = Get-Content -Path $templatePackageFile | ConvertFrom-Json
 LogDebug "Version in Source $($packageFileContent.version)"
