5e995a4b296f93af6a4ec61dab8d6277bdb9434e
colawwj <70128817+colawwj@users.noreply.github.com>
2020-11-18 10:45:56 +0800

    cdn-release (#12509)
    * cdn-release
    
    * update

--- sdk/cdn/arm-cdn/src/cdnManagementClientContext.ts
+++ sdk/cdn/arm-cdn/src/cdnManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-cdn";
-const packageVersion = "5.1.0";
+const packageVersion = "5.2.0";
 
 export class CdnManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
