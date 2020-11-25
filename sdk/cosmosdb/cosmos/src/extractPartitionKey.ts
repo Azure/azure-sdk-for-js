01a0757bf2dfe8e0593f218f3873667b8cacbc1a
deyaaeldeen <dealmaha@microsoft.com>
2020-11-25 18:23:41 +0000

    fix strictness issue in src/extractPartitionKey.ts

--- sdk/cosmosdb/cosmos/src/extractPartitionKey.ts
+++ sdk/cosmosdb/cosmos/src/extractPartitionKey.ts
@@ -12,29 +12,26 @@ export function extractPartitionKey(
   document: any,
   partitionKeyDefinition: PartitionKeyDefinition
 ): PartitionKey[] {
-  if (
-    partitionKeyDefinition &&
-    partitionKeyDefinition.paths &&
-    partitionKeyDefinition.paths.length > 0
-  ) {
-    const partitionKey: PartitionKey[] = [];
-    partitionKeyDefinition.paths.forEach((path: string) => {
-      const pathParts = parsePath(path);
-      let obj = document;
-      for (const part of pathParts) {
-        if (!(typeof obj === "object" && part in obj)) {
-          obj = undefined;
-          break;
-        }
-        obj = obj[part];
-      }
-      partitionKey.push(obj);
-    });
-    if (partitionKey.length === 1 && partitionKey[0] === undefined) {
-      return undefinedPartitionKey(partitionKeyDefinition);
-    }
-    return partitionKey;
+  if (partitionKeyDefinition.paths.length === 0) {
+    throw new Error("Expects a non empty array of paths");
   }
+  const partitionKey: PartitionKey[] = [];
+  partitionKeyDefinition.paths.forEach((path: string) => {
+    const pathParts = parsePath(path);
+    let obj = document;
+    for (const part of pathParts) {
+      if (!(typeof obj === "object" && part in obj)) {
+        obj = undefined;
+        break;
+      }
+      obj = obj[part];
+    }
+    partitionKey.push(obj);
+  });
+  if (partitionKey.length === 1 && partitionKey[0] === undefined) {
+    return undefinedPartitionKey(partitionKeyDefinition);
+  }
+  return partitionKey;
 }
 /**
  * @ignore
