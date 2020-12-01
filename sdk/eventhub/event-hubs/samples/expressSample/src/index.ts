586d02cd9484a9bca14f472bca3293fb3365e7c5
chradek <chradek@microsoft.com>
2020-11-30 16:56:40 -0800

    [event-hubs] express sample with sendBatch

--- sdk/eventhub/event-hubs/samples/expressSample/src/index.ts
+++ sdk/eventhub/event-hubs/samples/expressSample/src/index.ts
@@ -1,20 +1,23 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT Licence.
+
 import { v4 as uuid } from "uuid";
-import { EventProducer } from "./eventProducer";
+import { AsyncBatchingProducer } from "./asyncBatchingProducer";
 import bodyParser from "body-parser";
 
-var express = require("express");
-var app = express();
+import express from "express";
+import { EventHubProducerClient } from "@azure/event-hubs";
+const app = express();
 
-const eventHubConnectionString = "your eventHub connection string";
-const eventHubName = "your eventHub name";
-const batchSendSize = 20;
+const eventHubConnectionString = "my connection string";
+const eventHubName = "my event hub name";
+const batchSendSize = 3;
 const timeIntervalSeconds = 10;
-const eventProducer = new EventProducer(
-  eventHubConnectionString,
-  eventHubName,
-  batchSendSize,
-  timeIntervalSeconds
-);
+const eventProducer = new AsyncBatchingProducer({
+  producer: new EventHubProducerClient(eventHubConnectionString, eventHubName),
+  maxWaitTimeInSeconds: timeIntervalSeconds,
+  maxBatchSize: batchSendSize
+});
 const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,12 +25,17 @@ app.use(bodyParser.json());
 // respond with requestId
 app.post("/ingest", async (req, res) => {
   const requestId = uuid();
-  await eventProducer.send(requestId, req.body);
+  await eventProducer.send({
+    properties: {
+      request_id: requestId
+    },
+    body: req.body
+  });
   res.send(`ingested event. requestId: ${requestId}`);
 });
 
 // enable timeTrigger for eventHub.
-eventProducer.timeScan();
+eventProducer.start();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
