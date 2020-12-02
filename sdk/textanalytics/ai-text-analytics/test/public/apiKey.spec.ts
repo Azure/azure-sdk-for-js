2e70fdf60bf1c5dc16a71f4d894fa684096ea4ce
Will Temple <witemple@microsoft.com>
2020-12-02 16:10:48 -0500

    Repaired textanalytics build

--- sdk/textanalytics/ai-text-analytics/test/public/apiKey.spec.ts
+++ sdk/textanalytics/ai-text-analytics/test/public/apiKey.spec.ts
@@ -5,7 +5,7 @@ import { assert, use as chaiUse } from "chai";
 import chaiPromises from "chai-as-promised";
 chaiUse(chaiPromises);
 
-import { isRecordMode, Recorder } from "@azure/test-utils-recorder";
+import { env, isRecordMode, Recorder } from "@azure/test-utils-recorder";
 
 import { createRecordedClient, testEnv } from "../utils/recordedClient";
 import { TextAnalyticsClient, AzureKeyCredential } from "../../src";
@@ -80,7 +80,7 @@ describe("[API Key] TextAnalyticsClient", function() {
 
   describe("#health", () => {
     let pollingInterval = 2000;
-    if (isRecordMode() || process.env.TEST_MODE === "live") {
+    if (isRecordMode() || env.TEST_MODE === "live") {
       // eslint-disable-next-line no-invalid-this
       this.timeout(1000000);
     } else {
