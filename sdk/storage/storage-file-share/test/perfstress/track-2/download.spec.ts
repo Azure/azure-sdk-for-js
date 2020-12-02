4f9ceaa273273b0133aea4777128f630f787439d
HarshaNalluru <sanallur@microsoft.com>
2020-12-02 12:57:18 -0800

    download test

---
+++ sdk/storage/storage-file-share/test/perfstress/track-2/download.spec.ts
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { PerfStressTest, PerfStressOptionDictionary } from "@azure/test-utils-perfstress";
+
+import { ShareServiceClient, StorageSharedKeyCredential } from "../../../src";
+
+// Expects the .env file at the same level as the "test" folder
+import * as dotenv from "dotenv";
+dotenv.config();
+
+interface StorageFileShareDownloadTestOptions {
+  size: number;
+}
+
+const account = process.env.ACCOUNT_NAME || "";
+const accountKey = process.env.ACCOUNT_KEY || "";
+
+const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
+
+const shareServiceClient = new ShareServiceClient(
+  // When using AnonymousCredential, following url should include a valid SAS or support public access
+  `https://${account}.file.core.windows.net`,
+  sharedKeyCredential
+);
+const shareName = `newshare${new Date().getTime()}`;
+const directoryName = `newdirectory${new Date().getTime()}`;
+const fileName = `newfile${new Date().getTime()}`;
+const shareClient = shareServiceClient.getShareClient(shareName);
+const directoryClient = shareClient.getDirectoryClient(directoryName);
+const fileClient = directoryClient.getFileClient(fileName);
+
+export class StorageFileShareDownloadTest extends PerfStressTest<
+  StorageFileShareDownloadTestOptions
+> {
+  public options: PerfStressOptionDictionary<StorageFileShareDownloadTestOptions> = {
+    size: {
+      required: true,
+      description: "Size in bytes",
+      shortName: "sz",
+      longName: "size",
+      defaultValue: 10
+    }
+  };
+
+  public async globalSetup() {
+    const createShareResponse = await shareClient.create();
+    console.log(`Created share ${shareName} successfully`, createShareResponse.requestId);
+
+    const createDirectoryResponse = await directoryClient.create();
+    console.log(
+      `Created directory ${directoryName} successfully`,
+      createDirectoryResponse.requestId
+    );
+    await fileClient.uploadData(Buffer.alloc(this.parsedOptions.size.value!));
+  }
+
+  public async globalCleanup() {
+    const deleteShareResponse = await shareClient.delete();
+    console.log(`Deleted share ${shareName} successfully`, deleteShareResponse.requestId);
+  }
+
+  async runAsync(): Promise<void> {
+    const downloadResponse = await fileClient.download(0);
+    console.log(`Downloaded file ${fileName} successfully`, downloadResponse.requestId);
+  }
+}
