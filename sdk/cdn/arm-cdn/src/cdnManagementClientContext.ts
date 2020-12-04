a6ee2365baae24d2d9cb5792457d410d7feacfee
colawwj <v-sigangzhou@microsoft.com>
2020-12-04 10:34:17 +0800

    update

--- sdk/cdn/arm-cdn/src/cdnManagementClientContext.ts
+++ sdk/cdn/arm-cdn/src/cdnManagementClientContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-cdn";
-const packageVersion = "5.2.0";
+const packageVersion = "6.0.0";
 
 export class CdnManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
