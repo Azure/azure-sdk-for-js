aacb068bad6a13e3d1358f6e829add3021a02a85
HarshaNalluru <sanallur@microsoft.com>
2020-12-03 20:51:31 -0800

    StorageBlobUploadFileTest

--- sdk/storage/storage-blob/test/perfstress/track-2/uploadFromFile.spec.ts
+++ sdk/storage/storage-blob/test/perfstress/track-2/uploadFromFile.spec.ts
@@ -1,9 +1,5 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-
-import { PerfStressTest, PerfStressOptionDictionary } from "@azure/test-utils-perfstress";
-
-import { BlobServiceClient, StorageSharedKeyCredential } from "../../../src";
 import fs from "fs";
 import util from "util";
 const writeFile = util.promisify(fs.writeFile);
@@ -13,61 +9,25 @@ const deleteFile = util.promisify(fs.unlink);
 
 // Expects the .env file at the same level as the "test" folder
 import * as dotenv from "dotenv";
+import { StorageBlobUploadTest } from "./upload.spec";
 dotenv.config();
 
-interface StorageBlobUploadTestOptions {
-  size: number;
-}
-
-const account = process.env.ACCOUNT_NAME || "";
-const accountKey = process.env.ACCOUNT_KEY || "";
-
-const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
-
-const blobServiceClient = new BlobServiceClient(
-  `https://${account}.blob.core.windows.net`,
-  sharedKeyCredential
-);
-const containerName = `newcontainer${new Date().getTime()}`;
-const blobName = `newblob${new Date().getTime()}`;
-const containerClient = blobServiceClient.getContainerClient(containerName);
-const blockBlobClient = blobServiceClient
-  .getContainerClient(containerName)
-  .getBlockBlobClient(blobName);
 const dirName = "temp";
 const fileName = `${dirName}/upload-from-test-temp-file.txt`;
 
-export class StorageBlobUploadFileTest extends PerfStressTest<StorageBlobUploadTestOptions> {
-  public options: PerfStressOptionDictionary<StorageBlobUploadTestOptions> = {
-    size: {
-      required: true,
-      description: "Size of the file in bytes to be created",
-      shortName: "sz",
-      longName: "size",
-      defaultValue: 10
-    }
-  };
-
+export class StorageBlobUploadFileTest extends StorageBlobUploadTest {
   public async globalSetup() {
-    const createContainerResponse = await containerClient.create();
-    console.log(
-      `Create container ${containerName} successfully`,
-      createContainerResponse.requestId
-    );
+    await super.globalSetup();
     if (!(await fileExists(dirName))) await mkdir(dirName);
     await writeFile(fileName, Buffer.alloc(this.parsedOptions.size.value!));
   }
 
   public async globalCleanup() {
-    const deleteContainerResponse = await containerClient.delete();
-    console.log(
-      `Deleted container ${containerName} successfully`,
-      deleteContainerResponse.requestId
-    );
+    await super.globalCleanup();
     await deleteFile(fileName);
   }
 
   async runAsync(): Promise<void> {
-    await blockBlobClient.uploadFile(fileName);
+    await StorageBlobUploadFileTest.blockBlobClient.uploadFile(fileName);
   }
 }
