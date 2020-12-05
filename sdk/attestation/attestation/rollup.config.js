ac685305f7495f5823e4084616d3a2827709f497
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-12-04 18:38:49 -0800

    Response to PR Comments

--- sdk/attestation/attestation/rollup.config.js
+++ sdk/attestation/attestation/rollup.config.js
@@ -15,15 +15,14 @@ import sourcemaps from "rollup-plugin-sourcemaps";
  */
 const config = {
   input: "./esm/attestationClient.js",
-  external: ["@azure/core-http", "@azure/core-arm"],
+  external: ["@azure/core-http"],
   output: {
     file: "./dist/attestation.js",
     format: "umd",
     name: "Azure.Attestation",
     sourcemap: true,
     globals: {
-      "@azure/core-http": "coreHttp",
-      "@azure/core-arm": "coreArm"
+      "@azure/core-http": "coreHttp"
     },
     banner: `/*
  * Copyright (c) Microsoft Corporation. All rights reserved.
