77d69285494160eede1e2a9544eada4f45ed373b
Karishma Ghiya <kaghiya@microsoft.com>
2020-12-07 17:03:26 -0800

    fix links for package.json version test

--- sdk/eventhub/event-hubs/test/internal/node/packageInfo.spec.ts
+++ sdk/eventhub/event-hubs/test/internal/node/packageInfo.spec.ts
@@ -11,7 +11,7 @@ import { packageJsonInfo } from "../../../src/util/constants";
 // following test is in place to ensure the values in package.json and in this file are consistent
 describe("Ensure package name and version are consistent in SDK and package.json", function(): void {
   it("Ensure constants.ts file is consistent with package.json", () => {
-    const packageJsonFilePath = path.join(__dirname, "..", "..", "..", "package.json");
+    const packageJsonFilePath = path.join(__dirname, "..", "..", "package.json");
     const rawFileContents = fs.readFileSync(packageJsonFilePath, { encoding: "utf-8" });
     const packageJsonContents = JSON.parse(rawFileContents);
 
