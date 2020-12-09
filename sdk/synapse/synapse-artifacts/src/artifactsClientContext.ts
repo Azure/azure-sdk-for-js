98e1dcf5cd401b62e05ba057011012957641d459
Jonathan Turner <joturner@microsoft.com>
2020-12-10 12:14:37 +1300

    Fix synapse versions

--- sdk/synapse/synapse-artifacts/src/artifactsClientContext.ts
+++ sdk/synapse/synapse-artifacts/src/artifactsClientContext.ts
@@ -5,7 +5,7 @@ import { ArtifactsClientOptionalParams } from "./models";
 import { lroPolicy } from "./lro";
 
 const packageName = "@azure/synapse-artifacts";
-const packageVersion = "1.0.0";
+const packageVersion = "1.0.0-beta.1";
 
 export class ArtifactsClientContext extends coreHttp.ServiceClient {
   endpoint: string;
