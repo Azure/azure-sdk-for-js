01803e5187fc3b0d7e3afcc72952f837e70b9cbe
Richard Park <richard.park@microsoft.com>
2020-12-09 17:47:17 -0800

    UNDO this at some point - just dumping a teeny bit more information into the thrown exception. Something interesting is happening with the keyvault samples when it runs in the smoketest pipeline.

--- sdk/keyvault/keyvault-common/src/challengeBasedAuthenticationPolicy.ts
+++ sdk/keyvault/keyvault-common/src/challengeBasedAuthenticationPolicy.ts
@@ -205,7 +205,9 @@ export class ChallengeBasedAuthenticationPolicy extends BaseRequestPolicy {
   public async sendRequest(webResource: WebResource): Promise<HttpOperationResponse> {
     // Ensure that we're about to use a secure connection.
     if (!webResource.url.startsWith("https:")) {
-      throw new Error("The resource address for authorization must use the 'https' protocol.");
+      throw new Error(
+        `The resource address for authorization must use the 'https' protocol.: url:'${webResource.url}'`
+      );
     }
 
     // The next request will happen differently whether we have a challenge or not.
