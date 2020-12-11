12a78065b247ecff687d4ccfda9a54b66df29631
Jeff Fisher <jeffish@microsoft.com>
2020-12-10 21:31:54 -0800

    [monitor] Change package namespace back to @azure (#12843)
    Change namespace back to @azure

--- sdk/monitor/opentelemetry-exporter-azure-monitor/samples/grpc/tracer.js
+++ sdk/monitor/opentelemetry-exporter-azure-monitor/samples/grpc/tracer.js
@@ -3,7 +3,7 @@
 const opentelemetry = require("@opentelemetry/api");
 const { NodeTracerProvider } = require("@opentelemetry/node");
 const { BatchSpanProcessor } = require("@opentelemetry/tracing");
-const { AzureMonitorTraceExporter } = require("@microsoft/opentelemetry-exporter-azure-monitor");
+const { AzureMonitorTraceExporter } = require("@azure/opentelemetry-exporter-azure-monitor");
 const { ConsoleLogger, LogLevel } = require("@opentelemetry/core");
 
 module.exports = () => {
