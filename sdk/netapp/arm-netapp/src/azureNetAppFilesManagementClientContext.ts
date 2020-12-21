b2f2bffe665049b3bce833ecee1e6b62e10f1fe7
colawwj <v-sigangzhou@microsoft.com>
2020-12-21 15:53:16 +0800

    arm-netapp-release

--- sdk/netapp/arm-netapp/src/azureNetAppFilesManagementClientContext.ts
+++ sdk/netapp/arm-netapp/src/azureNetAppFilesManagementClientContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-netapp";
-const packageVersion = "12.1.0";
+const packageVersion = "12.2.0";
 
 export class AzureNetAppFilesManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
@@ -37,7 +37,7 @@ export class AzureNetAppFilesManagementClientContext extends msRestAzure.AzureSe
     if (!options) {
       options = {};
     }
-    if(!options.userAgent) {
+    if (!options.userAgent) {
       const defaultUserAgent = msRestAzure.getDefaultUserAgentValue();
       options.userAgent = `${packageName}/${packageVersion} ${defaultUserAgent}`;
     }
@@ -52,10 +52,10 @@ export class AzureNetAppFilesManagementClientContext extends msRestAzure.AzureSe
     this.credentials = credentials;
     this.subscriptionId = subscriptionId;
 
-    if(options.acceptLanguage !== null && options.acceptLanguage !== undefined) {
+    if (options.acceptLanguage !== null && options.acceptLanguage !== undefined) {
       this.acceptLanguage = options.acceptLanguage;
     }
-    if(options.longRunningOperationRetryTimeout !== null && options.longRunningOperationRetryTimeout !== undefined) {
+    if (options.longRunningOperationRetryTimeout !== null && options.longRunningOperationRetryTimeout !== undefined) {
       this.longRunningOperationRetryTimeout = options.longRunningOperationRetryTimeout;
     }
   }
