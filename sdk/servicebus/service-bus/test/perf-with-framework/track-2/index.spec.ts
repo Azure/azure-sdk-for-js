58d975e40241d07dc858ebf1f749cab13fdc1fa4
HarshaNalluru <sanallur@microsoft.com>
2020-12-22 15:30:54 -0800

    update index file

--- sdk/servicebus/service-bus/test/perf-with-framework/track-2/index.spec.ts
+++ sdk/servicebus/service-bus/test/perf-with-framework/track-2/index.spec.ts
@@ -2,13 +2,10 @@
 // Licensed under the MIT license.
 
 import { PerfStressProgram, selectPerfStressTest } from "@azure/test-utils-perfstress";
-import { SimpleSendTest } from "./send.spec";
 import { BatchSendTest } from "./sendBatch.spec";
 
 console.log("=== Starting the perfStress test ===");
 
-const perfStressProgram = new PerfStressProgram(
-  selectPerfStressTest([SimpleSendTest, BatchSendTest])
-);
+const perfStressProgram = new PerfStressProgram(selectPerfStressTest([BatchSendTest]));
 
 perfStressProgram.run();
