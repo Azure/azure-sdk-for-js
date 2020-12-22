7ecf3ca3bf61d312ad726ddce54c5a382f174949
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2020-12-22 12:46:09 -0500

    [Text Analytics] Tests read env vars only after they were read by the recorder (#12991)
    Use the API Key from the corresponding environment var only after it was read by the recorder. 
    Also does the following:
    - split `createRecordedClient` into `createRecorder` and `createClient` with better arguments.
    - get rid of `testEnv`.
    - use `createClient` everywhere.
    
    Fixes #12990 and part of https://github.com/Azure/azure-sdk-for-js/issues/12819.

--- sdk/textanalytics/ai-text-analytics/test/public/pipelineOptions.spec.ts
+++ sdk/textanalytics/ai-text-analytics/test/public/pipelineOptions.spec.ts
@@ -3,22 +3,16 @@
 
 import { assert } from "chai";
 
-import {
-  TextAnalyticsClient,
-  DetectLanguageResultArray,
-  DetectLanguageSuccessResult,
-  AzureKeyCredential
-} from "../../src";
-import { testEnv } from "../utils/recordedClient";
+import { DetectLanguageResultArray, DetectLanguageSuccessResult } from "../../src";
+import { createClient } from "../utils/recordedClient";
 
 import { WebResource, HttpOperationResponse, HttpHeaders } from "@azure/core-http";
 
-describe("TextAnalyticsClient Custom PipelineOptions", function() {
-  const credential = new AzureKeyCredential(testEnv.TEXT_ANALYTICS_API_KEY);
 
+describe("TextAnalyticsClient Custom PipelineOptions", function() {
   it("use custom HTTPClient", async () => {
     const pipelineTester = new Promise<DetectLanguageResultArray>((resolve) => {
-      const client = new TextAnalyticsClient(testEnv.ENDPOINT, credential, {
+      const client = createClient("APIKey", {
         httpClient: {
           sendRequest: async (request: WebResource): Promise<HttpOperationResponse> => ({
             status: 200,
