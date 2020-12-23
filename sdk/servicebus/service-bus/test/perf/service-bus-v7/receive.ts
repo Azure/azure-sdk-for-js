d339fb2753b0431beb309a9a6759e9ff82b921fd
Harsha Nalluru <sanallur@microsoft.com>
2020-12-22 16:33:48 -0800

    Update sdk/servicebus/service-bus/test/perf/service-bus-v7/receive.ts
    Co-authored-by: chradek <51000525+chradek@users.noreply.github.com>

--- sdk/servicebus/service-bus/test/perf/service-bus-v7/receive.ts
+++ sdk/servicebus/service-bus/test/perf/service-bus-v7/receive.ts
@@ -7,8 +7,8 @@ Measures the maximum throughput of `receiver.receive()` in package `@azure/servi
 2. Create a queue inside the namespace.
 3. Set env vars `SERVICE_BUS_CONNECTION_STRING` and `SERVICE_BUS_QUEUE_NAME`.
 4. This test presumes that there are messages in the queue.
-4. `ts-node receive.ts [maxConcurrentCalls] [totalMessages] [isReceiveAndDelete]`
-5. Example: `ts-node receive.ts 1000 1000000 false`
+5. `ts-node receive.ts [maxConcurrentCalls] [totalMessages] [isReceiveAndDelete]`
+6. Example: `ts-node receive.ts 1000 1000000 false`
  */
 
 import { ProcessErrorArgs, ServiceBusClient, ServiceBusReceivedMessage } from "@azure/service-bus";
