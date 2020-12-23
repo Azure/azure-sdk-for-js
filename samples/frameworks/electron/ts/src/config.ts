659473f4f1ab75fbf67e9388e9142fda9846ac34
Maor Leger <malege@microsoft.com>
2020-12-23 11:45:58 -0800

    fixup! Remove env vars

--- samples/frameworks/electron/ts/src/config.ts
+++
@@ -1,6 +0,0 @@
-/*
-  Copyright (c) Microsoft Corporation.
-  Licensed under the MIT license.
-*/
-import dotenv from "dotenv";
-dotenv.config();
--- samples/frameworks/electron/ts/src/main.ts
+++ samples/frameworks/electron/ts/src/main.ts
@@ -5,7 +5,6 @@
   The main process is responsible for renderer window management
   as well as integration between a renderer and the authentication provider.
 */
-import "./config";
 import { app, BrowserWindow, ipcMain } from "electron";
 import { IPC_MESSAGES } from "./constants";
 import AuthProvider from "./authProvider";
