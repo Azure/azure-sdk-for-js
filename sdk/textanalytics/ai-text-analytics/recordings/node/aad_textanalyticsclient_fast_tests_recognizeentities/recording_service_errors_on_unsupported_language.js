1094dac04f9ff774072c2adfd756633a3b70585d
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2020-12-22 20:11:34 -0500

    [Text Analytics] Fix timeout issues (#13006)
    * [Text Analytics] Fix timeout issues
    
    * add recordings

---
+++ sdk/textanalytics/ai-text-analytics/recordings/node/aad_textanalyticsclient_fast_tests_recognizeentities/recording_service_errors_on_unsupported_language.js
@@ -0,0 +1,58 @@
+let nock = require('nock');
+
+module.exports.hash = "27d0310d285fe930d29d601dd509a876";
+
+module.exports.testInfo = {"uniqueName":{},"newDate":{}}
+
+nock('https://login.microsoftonline.com:443', {"encodedQueryParams":true})
+  .post('/azure_tenant_id/oauth2/v2.0/token', "response_type=token&grant_type=client_credentials&client_id=azure_client_id&client_secret=azure_client_secret&scope=https%3A%2F%2Fcognitiveservices.azure.com%2F.default")
+  .reply(200, {"token_type":"Bearer","expires_in":86399,"ext_expires_in":86399,"access_token":"access_token"}, [
+  'Cache-Control',
+  'no-store, no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Type',
+  'application/json; charset=utf-8',
+  'Expires',
+  '-1',
+  'Strict-Transport-Security',
+  'max-age=31536000; includeSubDomains',
+  'X-Content-Type-Options',
+  'nosniff',
+  'P3P',
+  'CP="DSP CUR OTPi IND OTRi ONL FIN"',
+  'x-ms-request-id',
+  '15f57cb6-90be-47e9-932d-bb6b138fdf00',
+  'x-ms-ests-server',
+  '2.1.11328.13 - EUS ProdSlices',
+  'Set-Cookie',
+  'fpc=An2vLCwQjZ5Hp9TGCmi-aCJz_bg1AQAAAE1JdNcOAAAA; expires=Thu, 21-Jan-2021 20:08:45 GMT; path=/; secure; HttpOnly; SameSite=None',
+  'Set-Cookie',
+  'x-ms-gateway-slice=estsfd; path=/; secure; samesite=none; httponly',
+  'Set-Cookie',
+  'stsservicecookie=estsfd; path=/; secure; samesite=none; httponly',
+  'Date',
+  'Tue, 22 Dec 2020 20:08:45 GMT',
+  'Content-Length',
+  '1331'
+]);
+
+nock('https://endpoint', {"encodedQueryParams":true})
+  .post('/text/analytics/v3.1-preview.3/entities/recognition/general', {"documents":[{"id":"0","text":"This is some text, but it doesn't matter.","language":"notalanguage"}]})
+  .query(true)
+  .reply(200, {"documents":[],"errors":[{"id":"0","error":{"code":"InvalidArgument","message":"Invalid Language Code.","innererror":{"code":"UnsupportedLanguageCode","message":"Invalid language code. Supported languages: ar,cs,da,de,en,es,fi,fr,hu,it,ja,ko,nl,no,pl,pt-BR,pt-PT,ru,sv,tr,zh-Hans"}}}],"modelVersion":"2020-04-01"}, [
+  'Transfer-Encoding',
+  'chunked',
+  'Content-Type',
+  'application/json; charset=utf-8',
+  'x-envoy-upstream-service-time',
+  '3',
+  'apim-request-id',
+  'b16e4aaf-e820-4277-bfda-0206b013d752',
+  'Strict-Transport-Security',
+  'max-age=31536000; includeSubDomains; preload',
+  'x-content-type-options',
+  'nosniff',
+  'Date',
+  'Tue, 22 Dec 2020 20:08:45 GMT'
+]);
