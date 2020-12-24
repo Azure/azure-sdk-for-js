2d26744a64ca356484815edd2d6fc10b1005695d
HarshaNalluru <sanallur@microsoft.com>
2020-12-23 16:40:09 -0800

    perf folder reorganize

---
+++ sdk/servicebus/service-bus/test/perf/track-2/sbBase.spec.ts
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { PerfStressTest } from "@azure/test-utils-perfstress";
+import { ServiceBusAdministrationClient, ServiceBusClient } from "../../../src";
+
+// Expects the .env file at the same level as the "test" folder
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const connectionString = getEnvVar("SERVICEBUS_CONNECTION_STRING");
+const sbClient = new ServiceBusClient(connectionString);
+
+export abstract class ServiceBusTest<TOptions> extends PerfStressTest<TOptions> {
+  sbClient: ServiceBusClient;
+  sbAdminClient: ServiceBusAdministrationClient;
+  static queueName = `newqueue-${Math.ceil(Math.random() * 1000)}`;
+
+  constructor() {
+    super();
+    this.sbClient = sbClient;
+    this.sbAdminClient = new ServiceBusAdministrationClient(connectionString);
+  }
+
+  public async globalSetup() {
+    await this.sbAdminClient.createQueue(ServiceBusTest.queueName);
+  }
+
+  public async globalCleanup() {
+    await this.sbAdminClient.deleteQueue(ServiceBusTest.queueName);
+    await this.sbClient.close();
+  }
+}
+
+function getEnvVar(name: string) {
+  const val = process.env[name];
+  if (!val) {
+    throw `Environment variable ${name} is not defined.`;
+  }
+  return val;
+}
