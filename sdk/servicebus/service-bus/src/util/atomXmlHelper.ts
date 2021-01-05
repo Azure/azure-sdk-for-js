84494ea4139dfdd968c513be7214c037d8c6e398
HarshaNalluru <sanallur@microsoft.com>
2021-01-05 12:04:40 -0800

    new helper method to sanitize the serializable object

--- sdk/servicebus/service-bus/src/util/atomXmlHelper.ts
+++ sdk/servicebus/service-bus/src/util/atomXmlHelper.ts
@@ -92,6 +92,26 @@ export async function executeAtomXmlOperation(
 }
 
 /**
+ * The key value pairs having undefined/null as the values are removed recursively from the object in order to address the following issues
+ * - ATOM based management operations throw a "Bad Request" error if empty tags are included in the xml request body at top level.
+ * - At the inner levels, Service assigns the empty strings as values to the empty tags instead of throwing.
+ *
+ * @param {{ [key: string]: any }} resource
+ */
+function sanitizeSerializableObject(resource: { [key: string]: any }) {
+  Object.keys(resource).forEach(function(property) {
+    if (resource[property] == undefined) {
+      delete resource[property];
+    } else if (
+      typeof resource[property] === "object" &&
+      resource[property].constructor === {}.constructor
+    ) {
+      sanitizeSerializableObject(resource[property]);
+    }
+  });
+}
+
+/**
  * @internal
  * @hidden
  * Serializes input information to construct the Atom XML request
@@ -103,15 +123,8 @@ export async function executeAtomXmlOperation(
 export function serializeToAtomXmlRequest(resourceName: string, resource: any): object {
   const content: any = {};
 
-  // The top level key value pairs having undefined/null as the value are removed in order to address issue where the Service Bus'
-  // ATOM based management operations throw a "Bad Request" error if empty tags are included in the xml request body at top level.
-  const processedResource = Object.assign({}, resource);
-  Object.keys(processedResource).forEach(function(property) {
-    if (processedResource[property] == undefined) {
-      delete processedResource[property];
-    }
-  });
-  content[resourceName] = processedResource;
+  content[resourceName] = Object.assign({}, resource);
+  sanitizeSerializableObject(content[resourceName]);
 
   content[resourceName][Constants.XML_METADATA_MARKER] = {
     xmlns: "http://schemas.microsoft.com/netservices/2010/10/servicebus/connect",
