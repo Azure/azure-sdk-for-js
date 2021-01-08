ec3f02a31bf310586ee661ee2b6d35b91058a072
colawwj <v-sigangzhou@microsoft.com>
2021-01-08 10:17:18 +0800

    version update

--- sdk/machinelearningcompute/arm-machinelearningcompute/src/machineLearningComputeManagementClientContext.ts
+++ sdk/machinelearningcompute/arm-machinelearningcompute/src/machineLearningComputeManagementClientContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-machinelearningcompute";
-const packageVersion = "1.2.0";
+const packageVersion = "2.0.0";
 
 export class MachineLearningComputeManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
