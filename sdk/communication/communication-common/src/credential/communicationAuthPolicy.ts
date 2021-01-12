9ee5228405326d2c81a0cf1fe5188fd89c68e971
Josh Lai <joshlai@microsoft.com>
2021-01-12 15:05:57 -0500

    [format] rush format fixes

--- sdk/communication/communication-common/src/credential/communicationAuthPolicy.ts
+++ sdk/communication/communication-common/src/credential/communicationAuthPolicy.ts
@@ -2,20 +2,17 @@ import { isTokenCredential, KeyCredential, TokenCredential } from "@azure/core-a
 import { bearerTokenAuthenticationPolicy, RequestPolicyFactory } from "@azure/core-http";
 import { createCommunicationAccessKeyCredentialPolicy } from "./communicationAccessKeyCredentialPolicy";
 /**
- * Creates a pipeline policy to authenticate request based 
+ * Creates a pipeline policy to authenticate request based
  * on the credential passed in
- * 
+ *
  * @param {KeyCredential | TokenCredential} credential The key credential
  */
 export const createCommunicationAuthPolicy = (
-    credential: KeyCredential | TokenCredential
+  credential: KeyCredential | TokenCredential
 ): RequestPolicyFactory => {
-    if (isTokenCredential(credential)) {
-        return bearerTokenAuthenticationPolicy(
-            credential,
-            "https://communication.azure.com//.default"
-          );
-    } else {
-        return createCommunicationAccessKeyCredentialPolicy(credential);
-    };
+  if (isTokenCredential(credential)) {
+    return bearerTokenAuthenticationPolicy(credential, "https://communication.azure.com//.default");
+  } else {
+    return createCommunicationAccessKeyCredentialPolicy(credential);
+  }
 };
