78f1ec380a9d26e59118b4cec3570dc1b798efc1
zolvarga <zolvarga@microsoft.com>
2021-01-12 14:34:53 -0800

    Remove comments

--- sdk/digitaltwins/digital-twins-core/test/utils/testClient.ts
+++ sdk/digitaltwins/digital-twins-core/test/utils/testClient.ts
@@ -1,12 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-// import { testPollerProperties } from "./recorderUtils";
 import { DigitalTwinsClient } from "../../src";
-// import { PollerLike, PollOperationState } from "@azure/core-lro";
-// import { operationOptionsToRequestOptionsBase } from "@azure/core-http";
-// import { RestoreKeyBackupPoller } from "./lro/restore/poller";
-// import { BeginRestoreKeyBackupOptions } from "./lro/restore/operation";
 
 export default class TestClient {
   public readonly client: DigitalTwinsClient;
