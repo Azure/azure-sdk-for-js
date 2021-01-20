b5092d14706b5c18e55a18b962a25d7018f1f21f
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2021-01-20 10:18:58 -0800

    [app-configuration] Fix selecting ConfigurationSetting fields when they contain an underscore (#13258)
    Fixing problem where some fields could not be properly `select`'d for ConfigurationSettings because they had an underscore.
    
    This revealed some other underlying issues (like inconsistency in how the `select` field was populated in GetConfigurationSetting vs the List*Setting operations). Tests have been added, code refactored and hopefully things look better after this.
    
    Fixes #13126

--- sdk/appconfiguration/app-configuration/recordings/node/appconfigurationclient_listconfigurationsettings/recording_using_select_via_fields.js
+++ sdk/appconfiguration/app-configuration/recordings/node/appconfigurationclient_listconfigurationsettings/recording_using_select_via_fields.js
@@ -1,17 +1,17 @@
 let nock = require('nock');
 
-module.exports.hash = "9f1a26e42b4ccda6d902230edda7a504";
+module.exports.hash = "a77d228b6b443b2f94c3987f26ac6769";
 
-module.exports.testInfo = {"uniqueName":{"listConfigSetting2A":"listConfigSetting2A161110360930802770","listConfigSetting2B":"listConfigSetting2B161110360930800398","listConfigSettingsLabel":"listConfigSettingsLabel161110360930807497"},"newDate":{}}
+module.exports.testInfo = {"uniqueName":{"listConfigSetting5A":"listConfigSetting5A161110361705808128","listConfigSetting5B":"listConfigSetting5B161110361705904485","listConfigSettingsLabel":"listConfigSettingsLabel161110361705903095"},"newDate":{}}
 
 nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
-  .put('/kv/listConfigSetting2A161110360930802770', {"key":"listConfigSetting2A161110360930802770","label":"listConfigSettingsLabel161110360930807497","content_type":"a content type","value":"[A] production value"})
+  .put('/kv/listConfigSetting5A161110361705808128', {"key":"listConfigSetting5A161110361705808128","label":"listConfigSettingsLabel161110361705903095","content_type":"a content type","value":"[A] production value"})
   .query(true)
-  .reply(200, {"etag":"EpyLPRS7AZAXXXwskRHgAYYUiBj","key":"listConfigSetting2A161110360930802770","label":"listConfigSettingsLabel161110360930807497","content_type":"a content type","value":"[A] production value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:50+00:00"}, [
+  .reply(200, {"etag":"gZFox7MfUiKUzQcNqfa9sK4QoVp","key":"listConfigSetting5A161110361705808128","label":"listConfigSettingsLabel161110361705903095","content_type":"a content type","value":"[A] production value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:57+00:00"}, [
   'Server',
   'openresty/1.17.8.2',
   'Date',
-  'Wed, 20 Jan 2021 00:46:52 GMT',
+  'Wed, 20 Jan 2021 00:46:07 GMT',
   'Content-Type',
   'application/vnd.microsoft.appconfig.kv+json; charset=utf-8',
   'Transfer-Encoding',
@@ -19,15 +19,15 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   'Connection',
   'close',
   'Last-Modified',
-  'Wed, 20 Jan 2021 00:46:50 GMT',
+  'Wed, 20 Jan 2021 00:46:57 GMT',
   'ETag',
-  '"EpyLPRS7AZAXXXwskRHgAYYUiBj"',
+  '"gZFox7MfUiKUzQcNqfa9sK4QoVp"',
   'Sync-Token',
-  'zAJw6V16=NDo1IzM4NTI3MDA=;sn=3852700',
+  'zAJw6V16=NDo1IzM4NTI3MTk=;sn=3852719',
   'x-ms-request-id',
-  '1169ee45-dc63-4ff3-9e63-e741a24c08c9',
+  '02f82cef-af7d-4321-9b69-03500485e8bc',
   'x-ms-correlation-request-id',
-  '1169ee45-dc63-4ff3-9e63-e741a24c08c9',
+  '02f82cef-af7d-4321-9b69-03500485e8bc',
   'x-ms-tenant-name',
   'riparkdev',
   'Access-Control-Allow-Origin',
@@ -41,13 +41,13 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
 ]);
 
 nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
-  .put('/locks/listConfigSetting2A161110360930802770')
+  .put('/locks/listConfigSetting5A161110361705808128')
   .query(true)
-  .reply(200, {"etag":"PTJaLoG22bzRRl6zUHwnCwrU0lF","key":"listConfigSetting2A161110360930802770","label":"listConfigSettingsLabel161110360930807497","content_type":"a content type","value":"[A] production value","tags":{},"locked":true,"last_modified":"2021-01-20T00:46:50+00:00"}, [
+  .reply(200, {"etag":"0AnJCS7NmwYPfze8fGHYFZcwQle","key":"listConfigSetting5A161110361705808128","label":"listConfigSettingsLabel161110361705903095","content_type":"a content type","value":"[A] production value","tags":{},"locked":true,"last_modified":"2021-01-20T00:46:58+00:00"}, [
   'Server',
   'openresty/1.17.8.2',
   'Date',
-  'Wed, 20 Jan 2021 00:46:00 GMT',
+  'Wed, 20 Jan 2021 00:47:00 GMT',
   'Content-Type',
   'application/vnd.microsoft.appconfig.kv+json; charset=utf-8',
   'Transfer-Encoding',
@@ -55,15 +55,15 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   'Connection',
   'close',
   'Last-Modified',
-  'Wed, 20 Jan 2021 00:46:50 GMT',
+  'Wed, 20 Jan 2021 00:46:58 GMT',
   'ETag',
-  '"PTJaLoG22bzRRl6zUHwnCwrU0lF"',
+  '"0AnJCS7NmwYPfze8fGHYFZcwQle"',
   'Sync-Token',
-  'zAJw6V16=NDo1IzM4NTI3MDE=;sn=3852701',
+  'zAJw6V16=NDo1IzM4NTI3MjA=;sn=3852720',
   'x-ms-request-id',
-  '4153d2fc-e961-4943-b91d-385367cc0828',
+  '1c9f4321-4359-45a6-b892-7133a7e52b07',
   'x-ms-correlation-request-id',
-  '4153d2fc-e961-4943-b91d-385367cc0828',
+  '1c9f4321-4359-45a6-b892-7133a7e52b07',
   'x-ms-tenant-name',
   'riparkdev',
   'Access-Control-Allow-Origin',
@@ -77,13 +77,13 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
 ]);
 
 nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
-  .put('/kv/listConfigSetting2A161110360930802770', {"key":"listConfigSetting2A161110360930802770","value":"[A] value"})
+  .put('/kv/listConfigSetting5A161110361705808128', {"key":"listConfigSetting5A161110361705808128","value":"[A] value"})
   .query(true)
-  .reply(200, {"etag":"gXdXdEdgQFIEuvrM6eG54lfWPpB","key":"listConfigSetting2A161110360930802770","label":null,"content_type":null,"value":"[A] value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:50+00:00"}, [
+  .reply(200, {"etag":"fDrxTVRZZ3hwoVXdWpWztiFbdCo","key":"listConfigSetting5A161110361705808128","label":null,"content_type":null,"value":"[A] value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:58+00:00"}, [
   'Server',
   'openresty/1.17.8.2',
   'Date',
-  'Wed, 20 Jan 2021 00:46:53 GMT',
+  'Wed, 20 Jan 2021 00:46:08 GMT',
   'Content-Type',
   'application/vnd.microsoft.appconfig.kv+json; charset=utf-8',
   'Transfer-Encoding',
@@ -91,15 +91,15 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   'Connection',
   'close',
   'Last-Modified',
-  'Wed, 20 Jan 2021 00:46:50 GMT',
+  'Wed, 20 Jan 2021 00:46:58 GMT',
   'ETag',
-  '"gXdXdEdgQFIEuvrM6eG54lfWPpB"',
+  '"fDrxTVRZZ3hwoVXdWpWztiFbdCo"',
   'Sync-Token',
-  'zAJw6V16=NDo1IzM4NTI3MDI=;sn=3852702',
+  'zAJw6V16=NDo1IzM4NTI3MjE=;sn=3852721',
   'x-ms-request-id',
-  '3cb0c73a-419f-49e1-927e-c0ab68ae7574',
+  '3291cbb0-0a6a-4233-a0cb-b47bee0f1e62',
   'x-ms-correlation-request-id',
-  '3cb0c73a-419f-49e1-927e-c0ab68ae7574',
+  '3291cbb0-0a6a-4233-a0cb-b47bee0f1e62',
   'x-ms-tenant-name',
   'riparkdev',
   'Access-Control-Allow-Origin',
@@ -113,13 +113,13 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
 ]);
 
 nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
-  .put('/kv/listConfigSetting2B161110360930800398', {"key":"listConfigSetting2B161110360930800398","label":"listConfigSettingsLabel161110360930807497","value":"[B] production value"})
+  .put('/kv/listConfigSetting5B161110361705904485', {"key":"listConfigSetting5B161110361705904485","label":"listConfigSettingsLabel161110361705903095","value":"[B] production value"})
   .query(true)
-  .reply(200, {"etag":"OXPYVSgDZW1p5B6AulDSHc4IEL3","key":"listConfigSetting2B161110360930800398","label":"listConfigSettingsLabel161110360930807497","content_type":null,"value":"[B] production value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:51+00:00"}, [
+  .reply(200, {"etag":"kBmGVNZQxzlcl2ugNEmF78hNeVi","key":"listConfigSetting5B161110361705904485","label":"listConfigSettingsLabel161110361705903095","content_type":null,"value":"[B] production value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:58+00:00"}, [
   'Server',
   'openresty/1.17.8.2',
   'Date',
-  'Wed, 20 Jan 2021 00:46:01 GMT',
+  'Wed, 20 Jan 2021 00:47:01 GMT',
   'Content-Type',
   'application/vnd.microsoft.appconfig.kv+json; charset=utf-8',
   'Transfer-Encoding',
@@ -127,15 +127,15 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   'Connection',
   'close',
   'Last-Modified',
-  'Wed, 20 Jan 2021 00:46:51 GMT',
+  'Wed, 20 Jan 2021 00:46:58 GMT',
   'ETag',
-  '"OXPYVSgDZW1p5B6AulDSHc4IEL3"',
+  '"kBmGVNZQxzlcl2ugNEmF78hNeVi"',
   'Sync-Token',
-  'zAJw6V16=NDo1IzM4NTI3MDQ=;sn=3852704',
+  'zAJw6V16=NDo1IzM4NTI3MjI=;sn=3852722',
   'x-ms-request-id',
-  '14cd29b3-be3b-413b-bf46-37bf0b14bbd0',
+  '3eebcac3-2f5b-4035-93f3-c46f915c929a',
   'x-ms-correlation-request-id',
-  '14cd29b3-be3b-413b-bf46-37bf0b14bbd0',
+  '3eebcac3-2f5b-4035-93f3-c46f915c929a',
   'x-ms-tenant-name',
   'riparkdev',
   'Access-Control-Allow-Origin',
@@ -149,13 +149,13 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
 ]);
 
 nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
-  .put('/kv/listConfigSetting2B161110360930800398', {"key":"listConfigSetting2B161110360930800398","value":"[B] value"})
+  .put('/kv/listConfigSetting5B161110361705904485', {"key":"listConfigSetting5B161110361705904485","value":"[B] value"})
   .query(true)
-  .reply(200, {"etag":"rqsiOLHqE5KqY9SiKJONwoahuc5","key":"listConfigSetting2B161110360930800398","label":null,"content_type":null,"value":"[B] value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:51+00:00"}, [
+  .reply(200, {"etag":"cdZcqpz8neWU2kmWkk0VxjmeiMs","key":"listConfigSetting5B161110361705904485","label":null,"content_type":null,"value":"[B] value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:59+00:00"}, [
   'Server',
   'openresty/1.17.8.2',
   'Date',
-  'Wed, 20 Jan 2021 00:46:54 GMT',
+  'Wed, 20 Jan 2021 00:46:09 GMT',
   'Content-Type',
   'application/vnd.microsoft.appconfig.kv+json; charset=utf-8',
   'Transfer-Encoding',
@@ -163,15 +163,15 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   'Connection',
   'close',
   'Last-Modified',
-  'Wed, 20 Jan 2021 00:46:51 GMT',
+  'Wed, 20 Jan 2021 00:46:59 GMT',
   'ETag',
-  '"rqsiOLHqE5KqY9SiKJONwoahuc5"',
+  '"cdZcqpz8neWU2kmWkk0VxjmeiMs"',
   'Sync-Token',
-  'zAJw6V16=NDo1IzM4NTI3MDU=;sn=3852705',
+  'zAJw6V16=NDo1IzM4NTI3MjM=;sn=3852723',
   'x-ms-request-id',
-  'c6b5c860-d0d4-4572-a5a1-2c385d799d82',
+  'a4a9d9fb-c469-4b88-a882-2aaa15d4855e',
   'x-ms-correlation-request-id',
-  'c6b5c860-d0d4-4572-a5a1-2c385d799d82',
+  'a4a9d9fb-c469-4b88-a882-2aaa15d4855e',
   'x-ms-tenant-name',
   'riparkdev',
   'Access-Control-Allow-Origin',
@@ -187,11 +187,11 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
 nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   .get('/kv')
   .query(true)
-  .reply(200, {"items":[{"etag":"PTJaLoG22bzRRl6zUHwnCwrU0lF","key":"listConfigSetting2A161110360930802770","label":"listConfigSettingsLabel161110360930807497","content_type":"a content type","value":"[A] production value","tags":{},"locked":true,"last_modified":"2021-01-20T00:46:50+00:00"},{"etag":"OXPYVSgDZW1p5B6AulDSHc4IEL3","key":"listConfigSetting2B161110360930800398","label":"listConfigSettingsLabel161110360930807497","content_type":null,"value":"[B] production value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:51+00:00"}]}, [
+  .reply(200, {"items":[{"label":"listConfigSettingsLabel161110361705903095","content_type":"a content type","locked":true,"last_modified":"2021-01-20T00:46:58+00:00"}]}, [
   'Server',
   'openresty/1.17.8.2',
   'Date',
-  'Wed, 20 Jan 2021 00:46:54 GMT',
+  'Wed, 20 Jan 2021 00:47:02 GMT',
   'Content-Type',
   'application/vnd.microsoft.appconfig.kvset+json; charset=utf-8',
   'Transfer-Encoding',
@@ -199,11 +199,11 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   'Connection',
   'close',
   'Sync-Token',
-  'zAJw6V16=NDo1IzM4NTI3MDU=;sn=3852705',
+  'zAJw6V16=NDo1IzM4NTI3MjM=;sn=3852723',
   'x-ms-request-id',
-  '5cc6fe63-ade0-4a72-89d6-5b0aa0bacc5f',
+  '9b357e0c-9e30-4bc0-ac61-54e325de4153',
   'x-ms-correlation-request-id',
-  '5cc6fe63-ade0-4a72-89d6-5b0aa0bacc5f',
+  '9b357e0c-9e30-4bc0-ac61-54e325de4153',
   'x-ms-tenant-name',
   'riparkdev',
   'Access-Control-Allow-Origin',
@@ -219,11 +219,11 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
 nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   .get('/kv')
   .query(true)
-  .reply(200, {"items":[{"etag":"PTJaLoG22bzRRl6zUHwnCwrU0lF","key":"listConfigSetting2A161110360930802770","label":"listConfigSettingsLabel161110360930807497","content_type":"a content type","value":"[A] production value","tags":{},"locked":true,"last_modified":"2021-01-20T00:46:50+00:00"},{"etag":"OXPYVSgDZW1p5B6AulDSHc4IEL3","key":"listConfigSetting2B161110360930800398","label":"listConfigSettingsLabel161110360930807497","content_type":null,"value":"[B] production value","tags":{},"locked":false,"last_modified":"2021-01-20T00:46:51+00:00"}]}, [
+  .reply(200, {"items":[{"label":"listConfigSettingsLabel161110361705903095","content_type":"a content type","locked":true,"last_modified":"2021-01-20T00:46:58+00:00"}]}, [
   'Server',
   'openresty/1.17.8.2',
   'Date',
-  'Wed, 20 Jan 2021 00:46:02 GMT',
+  'Wed, 20 Jan 2021 00:46:10 GMT',
   'Content-Type',
   'application/vnd.microsoft.appconfig.kvset+json; charset=utf-8',
   'Transfer-Encoding',
@@ -231,11 +231,75 @@ nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
   'Connection',
   'close',
   'Sync-Token',
-  'zAJw6V16=NDo1IzM4NTI3MDU=;sn=3852705',
+  'zAJw6V16=NDo1IzM4NTI3MjM=;sn=3852723',
   'x-ms-request-id',
-  'afa1ca44-3095-494b-bf4b-df06f0018427',
+  '2f9560a9-22cd-42e4-81c4-807c95188466',
   'x-ms-correlation-request-id',
-  'afa1ca44-3095-494b-bf4b-df06f0018427',
+  '2f9560a9-22cd-42e4-81c4-807c95188466',
+  'x-ms-tenant-name',
+  'riparkdev',
+  'Access-Control-Allow-Origin',
+  '*',
+  'Access-Control-Allow-Credentials',
+  'true',
+  'Access-Control-Expose-Headers',
+  'DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Authorization, x-ms-client-request-id, x-ms-useragent, x-ms-content-sha256, x-ms-date, host, Accept, Accept-Datetime, Date, If-Match, If-None-Match, Sync-Token, x-ms-return-client-request-id, ETag, Last-Modified, Link, Memento-Datetime, retry-after-ms, x-ms-request-id, x-ms-client-session-id, x-ms-effective-locale, WWW-Authenticate',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains'
+]);
+
+nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
+  .get('/kv')
+  .query(true)
+  .reply(200, {"items":[{"key":"listConfigSetting5A161110361705808128","label":null,"value":"[A] value"},{"key":"listConfigSetting5A161110361705808128","label":"listConfigSettingsLabel161110361705903095","value":"[A] production value"}]}, [
+  'Server',
+  'openresty/1.17.8.2',
+  'Date',
+  'Wed, 20 Jan 2021 00:47:03 GMT',
+  'Content-Type',
+  'application/vnd.microsoft.appconfig.kvset+json; charset=utf-8',
+  'Transfer-Encoding',
+  'chunked',
+  'Connection',
+  'close',
+  'Sync-Token',
+  'zAJw6V16=NDo1IzM4NTI3MjQ=;sn=3852724',
+  'x-ms-request-id',
+  'f2a04889-0461-41c1-9520-d26c5ea1129a',
+  'x-ms-correlation-request-id',
+  'f2a04889-0461-41c1-9520-d26c5ea1129a',
+  'x-ms-tenant-name',
+  'riparkdev',
+  'Access-Control-Allow-Origin',
+  '*',
+  'Access-Control-Allow-Credentials',
+  'true',
+  'Access-Control-Expose-Headers',
+  'DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Authorization, x-ms-client-request-id, x-ms-useragent, x-ms-content-sha256, x-ms-date, host, Accept, Accept-Datetime, Date, If-Match, If-None-Match, Sync-Token, x-ms-return-client-request-id, ETag, Last-Modified, Link, Memento-Datetime, retry-after-ms, x-ms-request-id, x-ms-client-session-id, x-ms-effective-locale, WWW-Authenticate',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains'
+]);
+
+nock('https://myappconfig.azconfig.io:443', {"encodedQueryParams":true})
+  .get('/kv')
+  .query(true)
+  .reply(200, {"items":[{"key":"listConfigSetting5A161110361705808128","label":null,"value":"[A] value"},{"key":"listConfigSetting5A161110361705808128","label":"listConfigSettingsLabel161110361705903095","value":"[A] production value"}]}, [
+  'Server',
+  'openresty/1.17.8.2',
+  'Date',
+  'Wed, 20 Jan 2021 00:46:10 GMT',
+  'Content-Type',
+  'application/vnd.microsoft.appconfig.kvset+json; charset=utf-8',
+  'Transfer-Encoding',
+  'chunked',
+  'Connection',
+  'close',
+  'Sync-Token',
+  'zAJw6V16=NDo1IzM4NTI3MjQ=;sn=3852724',
+  'x-ms-request-id',
+  '0b5aa786-d828-433a-8e88-881475e00bce',
+  'x-ms-correlation-request-id',
+  '0b5aa786-d828-433a-8e88-881475e00bce',
   'x-ms-tenant-name',
   'riparkdev',
   'Access-Control-Allow-Origin',
