43dacc4a6edc4f0c161f2f4ce55461f97ca2614e
Karishma Ghiya <kaghiya@microsoft.com>
2021-01-22 12:36:57 -0800

    debug tests

--- sdk/eventhub/event-hubs/test/internal/eventProcessor.spec.ts
+++ sdk/eventhub/event-hubs/test/internal/eventProcessor.spec.ts
@@ -573,7 +573,7 @@ describe("Event Processor", function(): void {
     }
   });
 
-  it.only("errors thrown from the user's handlers are reported to processError()", async () => {
+  it("errors thrown from the user's handlers are reported to processError()", async () => {
     const errors = new Set<Error>();
     const partitionIds = await consumerClient.getPartitionIds();
 
@@ -624,7 +624,11 @@ describe("Event Processor", function(): void {
         name: "waiting for errors thrown from user's handlers",
         timeBetweenRunsMs: 1000,
         maxTimes: 30,
-        until: async () => errors.size >= partitionIds.length * 3
+        until: async () => {
+          console.log(partitionIds.length);
+          console.dir(errors);
+          return errors.size >= partitionIds.length * 3; 
+        }
       });
       console.log("event processor loop completed");
       const messages = [...errors].map((e) => e.message);
