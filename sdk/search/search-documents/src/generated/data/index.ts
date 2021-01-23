cd4ad88f8509b63da78d9e34b5e1962773d8ad9e
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-01-22 21:02:03 -0800

    Search documents migration (#12829)
    * Modify the Configuration Markup Files
    
    * Regenerated Changes
    
    * Custom Code Changes
    
    * Test Code and API Changes
    
    * Updated Recordings Files
    
    * Update Test Code

--- sdk/search/search-documents/src/generated/data/index.ts
+++ sdk/search/search-documents/src/generated/data/index.ts
@@ -6,4 +6,6 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./documents";
+export * from "./models";
+export { SearchClient } from "./searchClient";
+export { SearchClientContext } from "./searchClientContext";
