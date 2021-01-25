c8c46bd0e45b4495c7a728e623a26a2846b9579e
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-01-25 14:18:36 -0800

    Fixed all the scripts in package.json

---
+++ sdk/quantum/quantum-jobs/test/public/quantumJobClient.spec.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as assert from "assert";
+import { QuantumJobClient } from "../../src";
+
+describe("Quantum Job Client tests", function() {
+  it("can create the client object successfully", () => {
+    const obj: QuantumJobClient = new QuantumJobClient(
+      "sample-subscription-id",
+      "sample-resource-group-name",
+      "sample-workspace-name"
+    );
+    assert.notEqual(obj, null);
+  });
+});
