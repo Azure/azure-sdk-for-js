9a0c714c8891b0455eca1265b64474c2197f6a35
0rlanand0Wats0n <orwatson@microsoft.com>
2021-01-26 11:28:15 -0800

    update test

--- sdk/communication/communication-phone-numbers/test/ctor.spec.ts
+++ sdk/communication/communication-phone-numbers/test/ctor.spec.ts
@@ -2,35 +2,37 @@
 // Licensed under the MIT license.
 
 import { AzureKeyCredential } from "@azure/core-auth";
-import { ClientSecretCredential } from "@azure/identity";
+import { isNode } from "@azure/core-http";
+import { DefaultAzureCredential } from "@azure/identity";
 import { assert } from "chai";
 import { PhoneNumbersClient } from "../src";
 
-describe("PhoneNumbersClient Constructor", () => {
+describe("PhoneNumbersClient Constructor", function() {
   const endpoint = "https://contoso.spool.azure.local";
   const accessKey = "banana";
 
-  it("successfully instantiates with valid connection string", () => {
+  it("successfully instantiates with valid connection string", function() {
     const client = new PhoneNumbersClient(`endpoint=${endpoint};accesskey=${accessKey}`);
     assert.instanceOf(client, PhoneNumbersClient);
   });
 
-  it("throws with invalid connection string", () => {
+  it("throws with invalid connection string", function() {
     assert.throws(() => {
       new PhoneNumbersClient(`endpoints=${endpoint};accesskey=${accessKey}`);
     });
   });
 
-  it("successfully instantiates with with endpoint and access key", () => {
+  it("successfully instantiates with with endpoint and access key", function() {
     const client = new PhoneNumbersClient(endpoint, new AzureKeyCredential(accessKey));
     assert.instanceOf(client, PhoneNumbersClient);
   });
 
-  it("successfully instantiates with with endpoint and managed identity", () => {
-    const client = new PhoneNumbersClient(
-      endpoint,
-      new ClientSecretCredential("<azure_tenant_id>", "<azure_client_id>", "<azure_client_secret>")
-    );
+  it("successfully instantiates with with endpoint and managed identity", function() {
+    if (!isNode) {
+      this.skip();
+    }
+
+    const client = new PhoneNumbersClient(endpoint, new DefaultAzureCredential());
     assert.instanceOf(client, PhoneNumbersClient);
   });
 });
