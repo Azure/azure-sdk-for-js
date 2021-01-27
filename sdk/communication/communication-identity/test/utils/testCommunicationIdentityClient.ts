2044df69769a14b6b2ad3c41a529e3654ce82179
Dominik <domessin@microsoft.com>
2021-01-26 16:29:47 -0800

    [communication] Extract identity client into its own communication-identity package (#13415)

---
+++ sdk/communication/communication-identity/test/utils/testCommunicationIdentityClient.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { OperationOptions, RestResponse } from "@azure/core-http";
+import { CommunicationUserIdentifier } from "@azure/communication-common";
+import {
+  CommunicationIdentityClient,
+  TokenScope,
+  IssueTokenResponse,
+  CreateUserResponse
+} from "../../src";
+import {
+  issueTokenHttpClient,
+  createUserHttpClient,
+  revokeTokensHttpClient
+} from "./mockHttpClients";
+
+export class TestCommunicationIdentityClient {
+  private connectionString: string = "endpoint=https://contoso.spool.azure.local;accesskey=banana";
+
+  public async issueTokenTest(
+    user: CommunicationUserIdentifier,
+    scopes: TokenScope[],
+    options: OperationOptions = {}
+  ): Promise<IssueTokenResponse> {
+    const client = new CommunicationIdentityClient(this.connectionString, {
+      httpClient: issueTokenHttpClient
+    });
+    return client.issueToken(user, scopes, options);
+  }
+
+  public async revokeTokensTest(
+    user: CommunicationUserIdentifier,
+    options: OperationOptions = {}
+  ): Promise<RestResponse> {
+    const client = new CommunicationIdentityClient(this.connectionString, {
+      httpClient: revokeTokensHttpClient
+    });
+    return client.revokeTokens(user, options);
+  }
+
+  public async createUserTest(options: OperationOptions = {}): Promise<CreateUserResponse> {
+    const client = new CommunicationIdentityClient(this.connectionString, {
+      httpClient: createUserHttpClient
+    });
+    return client.createUser(options);
+  }
+}
