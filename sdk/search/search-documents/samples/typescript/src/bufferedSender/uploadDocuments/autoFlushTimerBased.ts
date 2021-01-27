a207efe3963fcfd69726ddacbd40a21130d1a7ae
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-01-27 13:54:39 -0800

    Update Samples

--- sdk/search/search-documents/samples/typescript/src/bufferedSender/uploadDocuments/autoFlushTimerBased.ts
+++ sdk/search/search-documents/samples/typescript/src/bufferedSender/uploadDocuments/autoFlushTimerBased.ts
@@ -80,7 +80,10 @@ export async function main() {
       parkingIncluded: false,
       lastRenovationDate: new Date(2010, 5, 27),
       rating: 5,
-      location: new GeographyPoint(47.678581, -122.131577)
+      location: new GeographyPoint({
+        longitude: -122.131577,
+        latitude: 47.678581
+      })
     }
   ]);
 
