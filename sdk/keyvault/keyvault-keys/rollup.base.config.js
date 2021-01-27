ddd557b27fcb98adf3848b61aa3a48cc4026c1a1
Ramya Achutha Rao <ramyar@microsoft.com>
2021-01-27 12:54:39 -0800

    [Key Vault] Remove unused fs-extra dependency

--- sdk/keyvault/keyvault-keys/rollup.base.config.js
+++ sdk/keyvault/keyvault-keys/rollup.base.config.js
@@ -124,12 +124,12 @@ export function browserConfig(test = false) {
     ]
   };
 
-  baseConfig.external = ["fs-extra", "path"];
+  baseConfig.external = ["path"];
   if (test) {
     baseConfig.input = ["dist-esm/**/*.spec.js"];
     baseConfig.plugins.unshift(multiEntry({ exports: false }));
     baseConfig.output.file = "dist-test/index.browser.js";
-    // mark fs-extra as external
+
     baseConfig.context = "null";
 
     // Disable tree-shaking of test code.  In rollup-plugin-node-resolve@5.0.0, rollup started respecting
