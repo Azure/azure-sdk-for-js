f57437151ecad0a867a2a0cd6d09d695aca68c97
colawwj <v-sigangzhou@microsoft.com>
2021-01-28 14:00:02 +0800

    arm-operationalinsights

--- sdk/operationalinsights/arm-operationalinsights/src/operationalInsightsManagementClientContext.ts
+++ sdk/operationalinsights/arm-operationalinsights/src/operationalInsightsManagementClientContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-operationalinsights";
-const packageVersion = "5.0.0";
+const packageVersion = "6.0.0";
 
 export class OperationalInsightsManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
@@ -36,7 +36,7 @@ export class OperationalInsightsManagementClientContext extends msRestAzure.Azur
     if (!options) {
       options = {};
     }
-    if(!options.userAgent) {
+    if (!options.userAgent) {
       const defaultUserAgent = msRestAzure.getDefaultUserAgentValue();
       options.userAgent = `${packageName}/${packageVersion} ${defaultUserAgent}`;
     }
@@ -51,10 +51,10 @@ export class OperationalInsightsManagementClientContext extends msRestAzure.Azur
     this.credentials = credentials;
     this.subscriptionId = subscriptionId;
 
-    if(options.acceptLanguage !== null && options.acceptLanguage !== undefined) {
+    if (options.acceptLanguage !== null && options.acceptLanguage !== undefined) {
       this.acceptLanguage = options.acceptLanguage;
     }
-    if(options.longRunningOperationRetryTimeout !== null && options.longRunningOperationRetryTimeout !== undefined) {
+    if (options.longRunningOperationRetryTimeout !== null && options.longRunningOperationRetryTimeout !== undefined) {
       this.longRunningOperationRetryTimeout = options.longRunningOperationRetryTimeout;
     }
   }
