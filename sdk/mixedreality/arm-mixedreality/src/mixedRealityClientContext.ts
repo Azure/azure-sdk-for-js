c67b7526aa19f59d6d7e2a6516ee54069cd89ecc
colawwj <v-sigangzhou@microsoft.com>
2021-02-02 17:06:21 +0800

    arm-mixedrelity-release

--- sdk/mixedreality/arm-mixedreality/src/mixedRealityClientContext.ts
+++ sdk/mixedreality/arm-mixedreality/src/mixedRealityClientContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-mixedreality";
-const packageVersion = "2.1.0";
+const packageVersion = "3.0.0";
 
 export class MixedRealityClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
