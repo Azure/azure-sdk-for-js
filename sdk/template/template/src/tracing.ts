45a25229400f90b152e11676aeba02c2093c859e
Will Temple <witemple@microsoft.com>
2021-02-04 16:24:07 -0500

    Use createSpanFunction instead of defining the span function manually.

--- sdk/template/template/src/tracing.ts
+++ sdk/template/template/src/tracing.ts
@@ -1,11 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-
-import { getTracer } from "@azure/core-tracing";
-import { Span, SpanOptions, SpanKind } from "@opentelemetry/api";
-import { OperationOptions } from "@azure/core-http";
-
-type OperationTracingOptions = OperationOptions["tracingOptions"];
+//
+import { createSpanFunction } from "@azure/core-http";
 
 /**
  * Creates a span using the global tracer.
@@ -15,47 +11,7 @@ type OperationTracingOptions = OperationOptions["tracingOptions"];
  *
  * @internal
  */
-export function createSpan<T extends OperationOptions>(
-  operationName: string,
-  operationOptions: T
-): { span: Span; updatedOptions: T } {
-  const tracer = getTracer();
-  const tracingOptions = operationOptions.tracingOptions || {};
-  const spanOptions: SpanOptions = {
-    ...tracingOptions.spanOptions,
-    kind: SpanKind.INTERNAL
-  };
-
-  const span = tracer.startSpan(`Azure.Learn.ApiLearn.${operationName}`, spanOptions);
-
-  // Resource providers are documented here:
-  // https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/azure-services-resource-providers
-  span.setAttribute("az.namespace", "Microsoft.Learn");
-
-  let newSpanOptions = tracingOptions.spanOptions || {};
-  if (span.isRecording()) {
-    newSpanOptions = {
-      ...tracingOptions.spanOptions,
-      parent: span.context(),
-      attributes: {
-        ...spanOptions.attributes,
-        "az.namespace": "Microsoft.Learn"
-      }
-    };
-  }
-
-  const newTracingOptions: OperationTracingOptions = {
-    ...tracingOptions,
-    spanOptions: newSpanOptions
-  };
-
-  const newOperationOptions: T = {
-    ...operationOptions,
-    tracingOptions: newTracingOptions
-  };
-
-  return {
-    span,
-    updatedOptions: newOperationOptions
-  };
-}
+export const createSpan = createSpanFunction({
+  namespace: "Microsoft.Learn",
+  packagePrefix: "Azure.Learn.ApiLearn"
+});
