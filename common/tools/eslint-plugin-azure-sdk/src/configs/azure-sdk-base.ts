438fe05cea09aad7724a2e029463a5f623c0bc23
deyaaeldeen <dealmaha@microsoft.com>
2021-02-08 22:04:33 +0000

    [ESLint Plugin] No no-extraneous-dependencies in imports

--- common/tools/eslint-plugin-azure-sdk/src/configs/azure-sdk-base.ts
+++ common/tools/eslint-plugin-azure-sdk/src/configs/azure-sdk-base.ts
@@ -11,7 +11,7 @@ export default {
     sourceType: "module",
     extraFileExtensions: [".json"]
   },
-  plugins: ["@typescript-eslint", "no-only-tests", "promise", "eslint-plugin-tsdoc"],
+  plugins: ["@typescript-eslint", "no-only-tests", "promise", "eslint-plugin-tsdoc", "import"],
   extends: [
     "plugin:@typescript-eslint/recommended",
     "eslint:recommended",
@@ -29,6 +29,14 @@ export default {
     curly: ["error", "multi-line"],
     "eol-last": ["error", "always"],
     eqeqeq: ["error", "always", { null: "ignore" }],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: ["test/**/*.ts", "samples/**/*.ts", "samples/**/*.js"],
+        optionalDependencies: false,
+        peerDependencies: false
+      }
+    ],
     "no-console": "off",
     "no-dupe-class-members": "off",
     "no-empty": "error",
