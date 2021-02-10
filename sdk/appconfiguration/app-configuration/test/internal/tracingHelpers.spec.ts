64d039c800e7d404064e6eb9f52b9dc1f8c2c6b7
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2021-02-10 04:08:25 +0000

    Fight of the formatters on my machine

--- sdk/appconfiguration/app-configuration/test/internal/tracingHelpers.spec.ts
+++ sdk/appconfiguration/app-configuration/test/internal/tracingHelpers.spec.ts
@@ -9,31 +9,43 @@ import sinon from "sinon";
 
 describe("tracingHelpers", () => {
   it("trace OK", async () => {
-    let setStatusStub: (sinon.SinonStub<[status: Status], Span>) | undefined;
+    let setStatusStub: sinon.SinonStub<[Status], Span> | undefined;
 
-    await trace("addConfigurationSetting", {
-      tracingOptions: {
-        context: otContext.active()
-      }
-    }, async (_newOptions, span) => {
-      setStatusStub = sinon.stub(span, 'setStatus');
-    });
-
-    assert.equal(setStatusStub?.called, false, "if nothing fails we don't explicitly set the status (uses the default status)");
-  });
-
-  it("trace ERROR", async () => {
-    let setStatusStub: (sinon.SinonStub<[status: Status], Span>) | undefined;
-
-    try {
-      await trace("addConfigurationSetting", {
+    await trace(
+      "addConfigurationSetting",
+      {
         tracingOptions: {
           context: otContext.active()
         }
-      }, async (_options: any, span: Span) => {
-        setStatusStub = sinon.stub(span, 'setStatus');
-        throw new Error("Purposefully thrown error")
-      });
+      },
+      async (_newOptions, span) => {
+        setStatusStub = sinon.stub(span, "setStatus");
+      }
+    );
+
+    assert.equal(
+      setStatusStub?.called,
+      false,
+      "if nothing fails we don't explicitly set the status (uses the default status)"
+    );
+  });
+
+  it("trace ERROR", async () => {
+    let setStatusStub: sinon.SinonStub<[Status], Span> | undefined;
+
+    try {
+      await trace(
+        "addConfigurationSetting",
+        {
+          tracingOptions: {
+            context: otContext.active()
+          }
+        },
+        async (_options: any, span: Span) => {
+          setStatusStub = sinon.stub(span, "setStatus");
+          throw new Error("Purposefully thrown error");
+        }
+      );
 
       assert.fail("Exception should have been thrown from `trace` since the inner action threw");
     } catch (err) {
@@ -41,6 +53,10 @@ describe("tracingHelpers", () => {
     }
 
     assert.ok(setStatusStub, "setStatus should have been called");
-    assert.equal(setStatusStub?.args[0][0].code, StatusCode.ERROR, "Any thrown exception causes the span to be set to StatusCode.ERROR");
+    assert.equal(
+      setStatusStub?.args[0][0].code,
+      StatusCode.ERROR,
+      "Any thrown exception causes the span to be set to StatusCode.ERROR"
+    );
   });
 });
