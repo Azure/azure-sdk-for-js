763179d722b02c30315f8bd9e2e9cb27b65435aa
Hector Hernandez Guzman <hectorh@microsoft.com>
2021-02-16 11:56:55 -0800

    OpenTelemetry Exporter arch board feedback

--- sdk/monitor/monitor-opentelemetry-exporter/src/export/trace.ts
+++ sdk/monitor/monitor-opentelemetry-exporter/src/export/trace.ts
@@ -29,13 +29,13 @@ export class AzureMonitorTraceExporter implements SpanExporter {
 
   private readonly _options: AzureExporterInternalConfig;
 
-  constructor(options: Partial<AzureExporterConfig> = {}) {
+  constructor(options: AzureExporterConfig = {}) {
     const connectionString = options.connectionString || process.env[ENV_CONNECTION_STRING];
     this._logger = new ConsoleLogger(LogLevel.ERROR);
     this._options = {
       ...DEFAULT_EXPORTER_CONFIG
     };
-    this._options.serviceApiVersion = options.serviceApiVersion ?? this._options.serviceApiVersion;
+    this._options.apiVersion = options.apiVersion ?? this._options.apiVersion;
 
     if (connectionString) {
       const parsedConnectionString = ConnectionStringParser.parse(connectionString, this._logger);
@@ -64,9 +64,9 @@ export class AzureMonitorTraceExporter implements SpanExporter {
       return success
         ? { code: ExportResultCode.SUCCESS }
         : {
-            code: ExportResultCode.FAILED,
-            error: new Error("Failed to persist envelope in disk.")
-          };
+          code: ExportResultCode.FAILED,
+          error: new Error("Failed to persist envelope in disk.")
+        };
     } catch (ex) {
       return { code: ExportResultCode.FAILED, error: ex };
     }
