026c906657bc58bb689f2d3a1a2e7c05b69c4651
Maor Leger <malege@microsoft.com>
2021-02-18 11:16:11 -0800

    Add a linter rule to prevent usage of `window`

---
+++ common/tools/eslint-plugin-azure-sdk/src/rules/ts-no-window.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @file Rule to forbid usage of window.
+ * @author Maor Leger
+ */
+
+import { Rule } from "eslint";
+import { getRuleMetaData } from "../utils";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+export = {
+  meta: getRuleMetaData("ts-no-window", "forbid usage of window", "code"),
+  create: (context: Rule.RuleContext): Rule.RuleListener =>
+    ({
+      // callback functions
+
+      // report on any window identifiers
+      "Identifier[name=window]": (node: any): void => {
+        context.report({
+          node: node,
+          message: "`window` should not be used, please use `self` instead.",
+          fix: (fixer: Rule.RuleFixer): Rule.Fix =>
+            fixer.replaceTextRange([node.range[0], node.range[0] + "window".length], "self")
+        });
+      }
+    } as Rule.RuleListener)
+};
