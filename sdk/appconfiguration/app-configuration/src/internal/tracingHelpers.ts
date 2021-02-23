1af5c091f305d88eb52938a40c7ae0344cb9ff85
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2021-02-23 18:50:37 +0000

    Update appconfig to use core-tracing/createSpan, rather than starting it's own spans. - Reduced Spanner<T> to a single method (trace) - Testing simplified - Prep for otel 0.16+ upgrade that's coming.

--- sdk/appconfiguration/app-configuration/src/internal/tracingHelpers.ts
+++ sdk/appconfiguration/app-configuration/src/internal/tracingHelpers.ts
@@ -1,100 +1,54 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { getTracer } from "@azure/core-tracing";
-import { Span, SpanKind, CanonicalCode } from "@opentelemetry/api";
-import { SpanOptions } from "@azure/core-tracing";
+import { Span, CanonicalCode } from "@opentelemetry/api";
 
-import { RestError } from "@azure/core-http";
+import { RestError, OperationOptions } from "@azure/core-http";
+import { createSpanFunction } from "@azure/core-tracing";
+import { AppConfigurationClient } from "../appConfigurationClient";
+
+/** @internal */
+export const createSpan = createSpanFunction({
+  namespace: "Microsoft.AppConfiguration",
+  packagePrefix: "Azure.Data.AppConfiguration"
+});
 
 /**
+ * Traces an operation and properly handles reporting start, end and errors for a given span
+ *
+ * @param operationName - Name of a method in the TClient type
+ * @param options - An options class, typically derived from \@azure/core-http/RequestOptionsBase
+ * @param fn - The function to call with an options class that properly propagates the span context
+ *
  * @internal
  */
-export interface Spannable {
-  spanOptions?: SpanOptions;
+export async function trace<ReturnT>(
+  operationName: keyof AppConfigurationClient,
+  options: OperationOptions,
+  fn: (options: OperationOptions, span: Span) => Promise<ReturnT>,
+  createSpanFn = createSpan
+): Promise<ReturnT> {
+  const { updatedOptions, span } = createSpanFn(
+    operationName,
+    options
+  );
+
+  try {
+    // NOTE: we really do need to await on this function here so we can handle any exceptions thrown and properly
+    // close the span.
+    return await fn(updatedOptions, span);
+  } catch (err) {
+    span.setStatus({
+      code: CanonicalCode.INTERNAL,     // TODO: StatusCode.ERROR in otel 0.16+
+      message: err.message
+    });
+    throw err;
+  } finally {
+    span.end();
+  }
 }
 
-/**
- * @internal
- */
-export class Spanner<TClient> {
-  constructor(private baseOperationName: string) {}
-
-  /**
-   * Traces an operation and properly handles reporting start, end and errors for a given span
-   *
-   * @param operationName - Name of a method in the TClient type
-   * @param options - An options class, typically derived from \@azure/core-http/RequestOptionsBase
-   * @param fn - The function to call with an options class that properly propagates the span context
-   * @param translateToCanonicalCodeFn - An optional function to translate thrown errors into a CanonicalCode for the span
-   */
-  async trace<OptionsT extends Spannable, ReturnT>(
-    operationName: keyof TClient,
-    options: OptionsT,
-    fn: (options: OptionsT, span: Span) => Promise<ReturnT>,
-    translateToCanonicalCodeFn: (err: Error) => CanonicalCode = Spanner.getCanonicalCode
-  ): Promise<ReturnT> {
-    const { newOptions, span } = this.createSpan<OptionsT>(options, operationName);
-
-    try {
-      return await fn(newOptions, span);
-    } catch (err) {
-      span.setStatus({
-        code: translateToCanonicalCodeFn(err),
-        message: err.message
-      });
-      throw err;
-    } finally {
-      span.end();
-    }
-  }
-
-  private createSpan<T extends Spannable>(options: T, operationName: keyof TClient) {
-    const span = getTracer().startSpan(`${this.baseOperationName}.${operationName}`, {
-      ...options.spanOptions,
-      kind: SpanKind.INTERNAL
-    });
-    span.setAttribute("az.namespace", "Microsoft.AppConfiguration");
-
-    let newOptions = options;
-
-    if (span.isRecording()) {
-      newOptions = Spanner.addParentToOptions<T>(options, span);
-    }
-    return { span, newOptions };
-  }
-
-  static getCanonicalCode(err: Error): CanonicalCode {
-    if (Spanner.isRestError(err)) {
-      switch (err.statusCode) {
-        case 401:
-          return CanonicalCode.PERMISSION_DENIED;
-        case 404:
-          return CanonicalCode.NOT_FOUND;
-        case 412:
-          return CanonicalCode.FAILED_PRECONDITION;
-      }
-    }
-
-    return CanonicalCode.UNKNOWN;
-  }
-
-  static isRestError(err: Error): err is RestError {
-    return err instanceof RestError;
-  }
-
-  static addParentToOptions<T extends Spannable>(options: T, span: Span): T {
-    const spanOptions = options.spanOptions || {};
-    return {
-      ...options,
-      spanOptions: {
-        ...spanOptions,
-        parent: span.context(),
-        attributes: {
-          ...spanOptions.attributes,
-          "az.namespace": "Microsoft.AppConfiguration"
-        }
-      }
-    };
-  }
+/** @internal */
+export function isRestError(err: Error): err is RestError {
+  return err instanceof RestError;
 }
