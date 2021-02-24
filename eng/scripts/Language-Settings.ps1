8f39bd3c054b0253ae2b6179cebf6af06645b014
Chidozie Ononiwu <chononiw@microsoft.com>
2021-02-24 10:51:17 -0800

    Add alternative code paths depending on parameters.

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -6,6 +6,15 @@ $packagePattern = "*.tgz"
 $MetadataUri = "https://raw.githubusercontent.com/Azure/azure-sdk/master/_data/releases/latest/js-packages.csv"
 $BlobStorageUrl = "https://azuresdkdocs.blob.core.windows.net/%24web?restype=container&comp=list&prefix=javascript%2F&delimiter=%2F"
 
+function Confirm-NodeInstallation
+{
+  if ((Get-Command npm | Measure-Object).Count -eq 0 ) 
+  {
+    LogError "Could not locate npm. Install NodeJS (includes npm and npx) https://nodejs.org/en/download"
+    exit 1
+  }
+}
+
 function Get-javascript-PackageInfoFromRepo ($pkgPath, $serviceDirectory, $pkgName)
 {
   $projectPath = Join-Path $pkgPath "package.json"
@@ -13,18 +22,19 @@ function Get-javascript-PackageInfoFromRepo ($pkgPath, $serviceDirectory, $pkgNa
   {
     $projectJson = Get-Content $projectPath | ConvertFrom-Json
     $jsStylePkgName = $projectJson.name.Replace("@", "").Replace("/", "-")
-    if ($pkgName -eq "$jsStylePkgName" -or $pkgName -eq $projectJson.name)
+    if ($pkgName -and ($pkgName -ne $jsStylePkgName -and $pkgName -ne $projectJson.name))
     {
-      $pkgProp = [PackageProps]::new($projectJson.name, $projectJson.version, $pkgPath, $serviceDirectory)
-      $pkgProp.SdkType = $projectJson.psobject.properties['sdk-type'].value
-      if ($projectJson.name.StartsWith("@azure/arm"))
-      {
-        $pkgProp.SdkType = "mgmt"
-      }
-      $pkgProp.IsNewSdk = $pkgProp.SdkType -eq "client"
-      $pkgProp.ArtifactName = $jsStylePkgName
-      return $pkgProp
+      return $null
     }
+    $pkgProp = [PackageProps]::new($projectJson.name, $projectJson.version, $pkgPath, $serviceDirectory)
+    $pkgProp.SdkType = $projectJson.psobject.properties['sdk-type'].value
+    if ($projectJson.name.StartsWith("@azure/arm"))
+    {
+      $pkgProp.SdkType = "mgmt"
+    }
+    $pkgProp.IsNewSdk = $pkgProp.SdkType -eq "client"
+    $pkgProp.ArtifactName = $jsStylePkgName
+    return $pkgProp
   }
   return $null
 }
@@ -32,6 +42,7 @@ function Get-javascript-PackageInfoFromRepo ($pkgPath, $serviceDirectory, $pkgNa
 # Returns the npm publish status of a package id and version.
 function IsNPMPackageVersionPublished ($pkgId, $pkgVersion)
 {
+  Confirm-NodeInstallation
   $npmVersions = (npm show $pkgId versions)
   if ($LastExitCode -ne 0)
   {
@@ -256,6 +267,7 @@ function SetPackageVersion ($PackageName, $Version, $ServiceDirectory = $null, $
     $ReleaseDate = Get-Date -Format "yyyy-MM-dd"
   }
   Push-Location "$EngDir/tools/versioning"
+  Confirm-NodeInstallation
   npm install
   $artifactName = $PackageName.Replace("@", "").Replace("/", "-")
   node ./set-version.js --artifact-name $artifactName --new-version $Version --release-date $ReleaseDate --repo-root $RepoRoot
