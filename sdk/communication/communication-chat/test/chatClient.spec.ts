3de15e95ff5bf8eca9e4009e3796a99d72c9f428
Kim <10719831+knvsl@users.noreply.github.com>
2021-02-24 19:04:10 -0800

    Re-write notification tests

--- sdk/communication/communication-chat/test/chatClient.spec.ts
+++ sdk/communication/communication-chat/test/chatClient.spec.ts
@@ -1,11 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Recorder } from "@azure/test-utils-recorder";
+import { isLiveMode, Recorder } from "@azure/test-utils-recorder";
 import { assert } from "chai";
 import { ChatClient, ChatThreadClient } from "../src";
 import { createTestUser, createRecorder, createChatClient } from "./utils/recordedClient";
 import { CommunicationUserIdentifier } from "@azure/communication-common";
+import { isNode } from "@azure/core-http";
+import sinon from "sinon";
 
 describe("ChatClient", function() {
   let threadId: string;
@@ -22,47 +24,176 @@ describe("ChatClient", function() {
     // await deleteTestUser(testUser3);
   });
 
-  beforeEach(function() {
-    recorder = createRecorder(this);
+  describe("Chat Operations", function() {
+    beforeEach(function() {
+      recorder = createRecorder(this);
+    });
+
+    afterEach(async function() {
+      if (!this.currentTest?.isPending()) {
+        await recorder.stop();
+      }
+    });
+
+    it("successfully creates a thread", async function() {
+      const communicationUserToken = await createTestUser();
+
+      testUser = communicationUserToken.user;
+      chatClient = createChatClient(communicationUserToken.token);
+
+      testUser2 = (await createTestUser()).user;
+
+      const request = {
+        topic: "test topic",
+        participants: [{ user: testUser }, { user: testUser2 }]
+      };
+
+      const chatThreadResult = await chatClient.createChatThread(request);
+
+      const chatThread = chatThreadResult.chatThread;
+      if (chatThread) {
+        threadId = chatThread.id!;
+      }
+
+      assert.isDefined(chatThread);
+      assert.isDefined(chatThread?.id);
+    }).timeout(8000);
+
+    it("successfully retrieves a thread client", async function() {
+      chatThreadClient = await chatClient.getChatThreadClient(threadId);
+      assert.isNotNull(chatThreadClient);
+      assert.equal(chatThreadClient.threadId, threadId);
+    });
+
+    it("successfully deletes a thread", async function() {
+      await chatClient.deleteChatThread(threadId);
+    });
+
+    // TODO add list threads test
   });
 
-  afterEach(async function() {
-    if (!this.currentTest?.isPending()) {
-      await recorder.stop();
-    }
-  });
+  describe("Realtime Notifications", function() {
+    before(async function() {
+      // Realtime notifications are browser only
+      if (isNode || !isLiveMode()) {
+        this.skip();
+      }
 
-  it("successfully creates a thread", async function() {
-    const communicationUserToken = await createTestUser();
+      // Create a thread
+      const request = {
+        topic: "notifcation tests",
+        participants: [{ user: testUser }]
+      };
+      const chatThreadResult = await chatClient.createChatThread(request);
+      threadId = chatThreadResult.chatThread?.id!;
 
-    testUser = communicationUserToken.user;
-    chatClient = createChatClient(communicationUserToken.token);
+      // Create ChatThreadClient
+      chatThreadClient = await chatClient.getChatThreadClient(threadId);
+    });
 
-    testUser2 = (await createTestUser()).user;
+    beforeEach(async function() {
+      // Start notifications
+      await chatClient.startRealtimeNotifications();
+    })
 
-    const request = {
-      topic: "test topic",
-      participants: [{ user: testUser }, { user: testUser2 }]
-    };
+    it("successfully stops realtime notifications", async function() {
+      const listener = sinon.spy();
 
-    const chatThreadResult = await chatClient.createChatThread(request);
+      chatClient.on("typingIndicatorReceived", listener);
+      await chatClient.stopRealtimeNotifications();
+      await chatThreadClient.sendTypingNotification();
 
-    const chatThread = chatThreadResult.chatThread;
-    if (chatThread) {
-      threadId = chatThread.id!;
-    }
+      // Allow time to see if notification comes in
+      await new Promise(resolve => setTimeout(resolve, 5000));
 
-    assert.isDefined(chatThread);
-    assert.isDefined(chatThread?.id);
-  }).timeout(8000);
+      sinon.assert.notCalled(listener);
+    });
 
-  it("successfully retrieves a thread client", async function() {
-    chatThreadClient = await chatClient.getChatThreadClient(threadId);
-    assert.isNotNull(chatThreadClient);
-    assert.equal(chatThreadClient.threadId, threadId);
-  });
+    it("successfully unsubscribes a listener", function(done) {
+      function listener() {
+        assert.fail();
+      }
 
-  it("successfully deletes a thread", async function() {
-    await chatClient.deleteChatThread(threadId);
+      chatClient.on("typingIndicatorReceived", listener);
+      chatClient.off("typingIndicatorReceived", listener);
+
+      // Send typing notification
+      chatThreadClient.sendTypingNotification();
+
+      // Wait a bit and fail if the notification comes in
+      setTimeout(done, 5000);
+    }).timeout(8000)
+
+    it("successfully listens to typingIndicatorReceivedEvents", function(done) {
+      function listener() {
+        done();
+      }
+
+      chatClient.on("typingIndicatorReceived", listener);
+
+      // Send typing notification
+      chatThreadClient.sendTypingNotification();
+
+    }).timeout(8000);
+
+    it("successfully listens to chatMessageEditedEvents", function(done) {
+      function listener() {
+        done();
+      }
+
+      chatClient.on("chatMessageEdited", listener);
+
+      // Edit message
+      const message = { content: "content" };
+      chatThreadClient.sendMessage(message).then((result) => {
+        chatThreadClient.updateMessage(result.id, {
+          content: "new content"
+        });
+      })
+    }).timeout(8000);
+
+    it("successfully listens to chatMessageReceivedEvents", function(done) {
+      function listener() {
+        done();
+      }
+
+      chatClient.on("chatMessageReceived", listener);
+
+      // Send message
+      const message = { content: `content` };
+      chatThreadClient.sendMessage(message);
+    }).timeout(8000);
+
+    it("successfully listens to chatMessageDeletedEvents", function(done) {
+      function listener() {
+        done();
+      }
+
+      chatClient.on("chatMessageDeleted", listener);
+
+      // Delete message
+      const message = { content: `content` };
+      chatThreadClient.sendMessage(message).then((result) => {
+        chatThreadClient.deleteMessage(result.id);
+      })
+    }).timeout(8000);
+
+    it("successfully listens to readReceiptReceivedEvents", function(done) {
+      function listener() {
+        done();
+      }
+
+      chatClient.on("readReceiptReceived", listener);
+
+      // Send read receipt
+      const message = { content: `content` };
+      chatThreadClient.sendMessage(message).then((result) => {
+        chatThreadClient.sendReadReceipt({
+          chatMessageId: result.id
+        });
+      })
+    }).timeout(8000);
+
+    // TODO thread level events
   });
 });
