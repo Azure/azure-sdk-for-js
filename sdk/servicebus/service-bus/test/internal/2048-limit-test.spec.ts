226726ee7cedd1fbd81af3d451624e18a4d2b3ef
HarshaNalluru <sanallur@microsoft.com>
2021-02-26 14:52:34 -0800

    2048-limit-test file

---
+++ sdk/servicebus/service-bus/test/internal/2048-limit-test.spec.ts
@@ -0,0 +1,138 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { ServiceBusAdministrationClient, ServiceBusSender } from "../../src";
+import { TestClientType } from "../public/utils/testUtils";
+import { ServiceBusReceiver } from "../../src/receivers/receiver";
+import {
+  ServiceBusClientForTests,
+  createServiceBusClientForTests,
+  EntityName,
+  getConnectionString
+} from "../public/utils/testutils2";
+import { verifyMessageCount } from "../public/utils/managementUtils";
+
+chai.use(chaiAsPromised);
+
+let serviceBusClient: ServiceBusClientForTests;
+let entityName: EntityName;
+let sender: ServiceBusSender;
+let receiver: ServiceBusReceiver;
+const testClientTypes = [
+  TestClientType.PartitionedQueue,
+  // TestClientType.PartitionedQueueWithSessions,
+  TestClientType.UnpartitionedQueue
+  // TestClientType.UnpartitionedQueueWithSessions
+];
+
+async function beforeEachTest(
+  entityType: TestClientType,
+  receiveMode: "peekLock" | "receiveAndDelete" = "peekLock"
+): Promise<void> {
+  entityName = await serviceBusClient.test.createTestEntities(entityType);
+  if (receiveMode === "receiveAndDelete") {
+    receiver = await serviceBusClient.test.createReceiveAndDeleteReceiver(entityName);
+  } else {
+    receiver = await serviceBusClient.test.createPeekLockReceiver(entityName);
+  }
+
+  sender = serviceBusClient.test.addToCleanup(
+    serviceBusClient.createSender(entityName.queue ?? entityName.topic!)
+  );
+}
+
+function afterEachTest(): Promise<void> {
+  return serviceBusClient.test.afterEach();
+}
+
+describe("2048 scenarios - receiveBatch in a loop", function(): void {
+  const numberOfMessagesToSend = 3000;
+
+  before(() => {
+    serviceBusClient = createServiceBusClientForTests();
+  });
+
+  after(() => {
+    return serviceBusClient.test.after();
+  });
+
+  afterEach(async () => {
+    await afterEachTest();
+  });
+
+  async function sendMessages() {
+    let current = 0;
+    while (current < numberOfMessagesToSend) {
+      const batch = await sender.createMessageBatch();
+      while (
+        batch.tryAddMessage({ body: `message-${current}` }) &&
+        current++ &&
+        current < numberOfMessagesToSend
+      );
+      await sender.sendMessages(batch);
+    }
+    console.log(
+      `Sent ${
+        (
+          await new ServiceBusAdministrationClient(getConnectionString()).getQueueRuntimeProperties(
+            entityName.queue!
+          )
+        ).activeMessageCount
+      } messages!!!`
+    );
+  }
+
+  async function receiveMessages(receiveMode: "peekLock" | "receiveAndDelete" = "peekLock") {
+    const startedAt = new Date();
+    // const testDurationInMs = 50000;
+    let maxMessageCount = 50;
+    let maxWaitTimeInMs = 3000;
+    let numberOfMessagesReceived = 0;
+    let elapsedTime = new Date().valueOf() - startedAt.valueOf();
+    let numberOfMessagesToReceive = numberOfMessagesToSend;
+    if (receiveMode === "peekLock") {
+      numberOfMessagesToReceive = 2047;
+    }
+    while (numberOfMessagesReceived < numberOfMessagesToReceive) {
+      console.log(`New receive started... ${elapsedTime / 1000} seconds`);
+      numberOfMessagesReceived += (
+        await receiver.receiveMessages(maxMessageCount, { maxWaitTimeInMs })
+      ).length;
+      elapsedTime = new Date().valueOf() - startedAt.valueOf();
+      console.log(
+        `Receive ended... ${elapsedTime /
+          1000} seconds. Total received so far = ${numberOfMessagesReceived}`
+      );
+    }
+  }
+
+  describe("receiveAndDelete", () => {
+    testClientTypes.forEach((clientType) => {
+      it(
+        clientType + ": would be able to receive more than 2048 messages",
+        async function(): Promise<void> {
+          await beforeEachTest(clientType, "receiveAndDelete");
+          await sendMessages();
+          await receiveMessages();
+          await verifyMessageCount(0, entityName);
+        }
+      );
+    });
+  });
+
+  describe("peekLock", () => {
+    testClientTypes.forEach((clientType) => {
+      it(
+        clientType + ": can receive upto 2048 messages without message loss",
+        async function(): Promise<void> {
+          await beforeEachTest(clientType);
+          await sendMessages();
+          await receiveMessages();
+          await verifyMessageCount(numberOfMessagesToSend, entityName);
+        }
+      );
+    });
+  });
+});
