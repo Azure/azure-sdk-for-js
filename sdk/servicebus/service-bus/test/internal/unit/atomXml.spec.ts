af35ab66f2a39dd687967836385191267bf4bf0e
Jeremy Meng <jeremy.ymeng@gmail.com>
2021-03-01 19:58:13 +0000

    [service-bus][test] extract key values into constants
    CredScan is computing hashes to identify locations of possible sensitive data.
    Changes unrelated to the key values can cause the hashes to change. This PR
    extract key values into constants in a separate file so the scan results remain
    stable.

--- sdk/servicebus/service-bus/test/internal/unit/atomXml.spec.ts
+++ sdk/servicebus/service-bus/test/internal/unit/atomXml.spec.ts
@@ -20,6 +20,7 @@ import { TopicResourceSerializer } from "../../../src/serializers/topicResourceS
 import { SubscriptionResourceSerializer } from "../../../src/serializers/subscriptionResourceSerializer";
 import { RuleResourceSerializer } from "../../../src/serializers/ruleResourceSerializer";
 import { getXMLNSPrefix, isJSONLikeObject } from "../../../src/util/utils";
+import { TestConstants } from "./testConstants";
 
 const queueProperties = [
   Constants.LOCK_DURATION,
@@ -210,8 +211,8 @@ describe("ATOM Serializers", () => {
               accessRights: ["Manage", "Send", "Listen"]
             },
             keyName: "allClaims_v2",
-            primaryKey: "pNSRzKKm2vfdbCuTXMa9gOMHD66NwCTxJi4KWJX/TDc=",
-            secondaryKey: "UreXLPWiP6Murmsq2HYiIXs23qAvWa36ZOL3gb9rXLs="
+            primaryKey: TestConstants.primaryKey,
+            secondaryKey: TestConstants.secondaryKey
           },
           {
             claimType: "SharedAccessKey",
@@ -219,8 +220,8 @@ describe("ATOM Serializers", () => {
               accessRights: ["Manage", "Send", "Listen"]
             },
             keyName: "allClaims_v3",
-            primaryKey: "pNSRzKKm2vfdbCuTXMa9gOMHD66NwCTxJi4KWJX/TDc=",
-            secondaryKey: "UreXLPWiP6Murmsq2HYiIXs23qAvWa36ZOL3gb9rXLs="
+            primaryKey: TestConstants.primaryKey,
+            secondaryKey: TestConstants.secondaryKey
           }
         ],
         enablePartitioning: true
@@ -267,8 +268,8 @@ describe("ATOM Serializers", () => {
               accessRights: ["Manage", "Send", "Listen"]
             },
             keyName: "allClaims_v2",
-            primaryKey: "pNSRzKKm2vfdbCuTXMa9gOMHD66NwCTxJi4KWJX/TDc=",
-            secondaryKey: "UreXLPWiP6Murmsq2HYiIXs23qAvWa36ZOL3gb9rXLs="
+            primaryKey: TestConstants.primaryKey,
+            secondaryKey: TestConstants.secondaryKey
           },
           {
             claimType: "SharedAccessKey",
@@ -276,8 +277,8 @@ describe("ATOM Serializers", () => {
               accessRights: ["Manage", "Send", "Listen"]
             },
             keyName: "allClaims_v3",
-            primaryKey: "pNSRzKKm2vfdbCuTXMa9gOMHD66NwCTxJi4KWJX/TDc=",
-            secondaryKey: "UreXLPWiP6Murmsq2HYiIXs23qAvWa36ZOL3gb9rXLs="
+            primaryKey: TestConstants.primaryKey,
+            secondaryKey: TestConstants.secondaryKey
           }
         ]
       };
---
+++ sdk/servicebus/service-bus/test/public/testConstants.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export const TestConstants = {
+  primaryKey: "pNSRzKKm2vfdbCuTXMa9gOMHD66NwCTxJi4KWJX/TDc=",
+  secondaryKey: "UreXLPWiP6Murmsq2HYiIXs23qAvWa36ZOL3gb9rXLs="
+};
