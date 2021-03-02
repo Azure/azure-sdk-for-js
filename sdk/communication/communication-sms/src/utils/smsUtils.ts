0b53600105035283f642eb142f1a29cf43b08e04
Bert Ong <823691+beltr0n@users.noreply.github.com>
2021-03-01 19:02:49 -0800

    formatting

--- sdk/communication/communication-sms/src/utils/smsUtils.ts
+++ sdk/communication/communication-sms/src/utils/smsUtils.ts
@@ -4,29 +4,29 @@ import { Uuid } from "./uuid";
 import { SmsSendOptions as InternalOptions } from "../generated/src/models";
 
 export function generateSendMessageRequest(
-    smsRequest:SmsSendRequest, 
-    options:SmsSendOptions = {}
+  smsRequest: SmsSendRequest,
+  options: SmsSendOptions = {}
 ): SendMessageRequest {
-    let _smsSendOptions:InternalOptions = {
-        enableDeliveryReport: options.enableDeliveryReport ?? false
-    }
-    if(options.tag) {
-        _smsSendOptions["tag"] = options.tag;
-    }
+  let _smsSendOptions: InternalOptions = {
+    enableDeliveryReport: options.enableDeliveryReport ?? false
+  };
+  if (options.tag) {
+    _smsSendOptions["tag"] = options.tag;
+  }
 
-    return {
-        from: smsRequest.from,
-        smsRecipients: smsRequest.to.map((phoneNumberStr) => {
-            return {
-            to: phoneNumberStr,
-            repeatabilityFirstSent: new Date(Date.now()).toUTCString(),
-            repeatabilityRequestId: Uuid.generateUuid()
-            };
-        }),
-        message: smsRequest.message,
-        smsSendOptions: {
-            enableDeliveryReport: options.enableDeliveryReport ?? false,
-            ...options.tag && { tag: options.tag }
-        }
-    };
+  return {
+    from: smsRequest.from,
+    smsRecipients: smsRequest.to.map((phoneNumberStr) => {
+      return {
+        to: phoneNumberStr,
+        repeatabilityFirstSent: new Date(Date.now()).toUTCString(),
+        repeatabilityRequestId: Uuid.generateUuid()
+      };
+    }),
+    message: smsRequest.message,
+    smsSendOptions: {
+      enableDeliveryReport: options.enableDeliveryReport ?? false,
+      ...(options.tag && { tag: options.tag })
+    }
+  };
 }
