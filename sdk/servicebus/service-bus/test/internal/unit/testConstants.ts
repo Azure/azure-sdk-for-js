d24f7d5dd0a157f8425900487c7ff705e34aa1ca
Jeremy Meng <jeremy.ymeng@gmail.com>
2021-03-02 00:55:29 +0000

    Keep just one testConstents.ts under test/public/

--- sdk/servicebus/service-bus/test/internal/unit/testConstants.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export const TestConstants = {
-  primaryKey: "pNSRzKKm2vfdbCuTXMa9gOMHD66NwCTxJi4KWJX/TDc=",
-  secondaryKey: "UreXLPWiP6Murmsq2HYiIXs23qAvWa36ZOL3gb9rXLs="
-};
