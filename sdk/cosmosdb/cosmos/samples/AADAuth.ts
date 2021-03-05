5409b167c82266eab273eb8d2159050308bcceb5
Zach <notzachfoster@gmail.com>
2021-03-05 13:48:50 -0500

    adds aad sample

--- sdk/cosmosdb/cosmos/samples/AADAuth.ts
+++ sdk/cosmosdb/cosmos/samples/AADAuth.ts
@@ -3,18 +3,41 @@ import { UsernamePasswordCredential } from "@azure/identity";
 import { CosmosClient } from "../dist";
 import { handleError, finish, logStep } from "./Shared/handleError";
 
+const endpoint = "your-endpoint"
+const masterKey = "your-master-key"
+const existingContainerId = "your-container-id"
+
 async function run() {
+  logStep('Create credential object from @azure/identity')
   const credentials = new UsernamePasswordCredential(
     "fake-tenant-id",
     "fake-client-id",
     "fakeUsername",
     "fakePassword"
   );
-  const client = new CosmosClient({
+  logStep('Pass credentials to client object with key aadCredentials')
+  const aadClient = new CosmosClient({
     endpoint,
     aadCredentials: credentials
   });
-  await client.databases.readAll().fetchAll();
+
+  const genericClient = new CosmosClient({
+    endpoint,
+    key: masterKey
+  });
+  
+  logStep('Only certain operations are authorized. Reading databases/containers will throw errors, but reading items will work')
+
+  // fails
+  await aadClient.databases.readAll().fetchAll();
+  // succeeds
+  await genericClient.databases.readAll().fetchAll();
+
+  // succeeds
+  await aadClient.container(existingContainerId).items();
+  // succeeds
+  await genericClient.container(existingContainerId).items();
+  
   await finish();
 }
 
