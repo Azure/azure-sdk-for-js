031438edea7d910e7a6b6c56c84787e12aa16aa0
Zach <notzachfoster@gmail.com>
2021-03-05 11:49:22 -0500

    Fix lint

--- sdk/cosmosdb/cosmos/samples/Bulk.ts
+++ sdk/cosmosdb/cosmos/samples/Bulk.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { handleError, finish, logStep } from "./Shared/handleError";
 import { getTestContainer, addEntropy } from "../test/public/common/TestHelpers";
 import { BulkOperationType } from "../src";
