2a52927bfc51d9a594f95555618cb83c95fa2829
Harsha Nalluru <sanallur@microsoft.com>
2021-03-09 21:17:03 +0000

    import test names from declare tests

--- sdk/test-utils/perfstress/test/index.spec.ts
+++ sdk/test-utils/perfstress/test/index.spec.ts
@@ -2,30 +2,10 @@
 // Licensed under the MIT license.
 
 import { PerfStressProgram, selectPerfStressTest } from "../src";
-
-// Tests:
-import { NoOp } from "./noop.spec";
-import { OptionsTest } from "./options.spec";
-import { SetupCleanupTest } from "./setupCleanup.spec";
-import { Delay500ms } from "./delay.spec";
-import { Exception } from "./exception.spec";
-import { PerfStressPolicyTest } from "./perfStressPolicy.spec";
-import { SleepTest } from "./sleep.spec";
-import { NodeFetchTest } from "./nodeFetch.spec";
+import { testClasses } from "./declareTests";
 
 console.log("=== Starting the perfStress test ===");
 
-const perfStressProgram = new PerfStressProgram(
-  selectPerfStressTest([
-    NoOp,
-    OptionsTest,
-    SetupCleanupTest,
-    Delay500ms,
-    Exception,
-    PerfStressPolicyTest,
-    SleepTest,
-    NodeFetchTest
-  ])
-);
+const perfStressProgram = new PerfStressProgram(selectPerfStressTest(testClasses));
 
 perfStressProgram.run();
