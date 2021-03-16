7d22e961366a4c11d10d8362c34a9f097ad923fb
Matt Ellis <matell@microsoft.com>
2021-03-16 10:18:11 -0700

    [EventGrid] Add Additional ACS Events, Address Arch-Board Feedback for GA (#14136)
    * [EventGrid] Regenerate Code
    
    Re-run the code generation based on the latest README.md in the rest
    specs repository. This picks up some changes to some Azure
    Communication Services event types that the service has changed.
    
    * [EventGrid] Updates for GA
    
    Adopt feedback from architecture board review, the changes here are
    focused on the consumption side:
    
    - We decided that we did not want to apply AutoRest style mapping for
     system event data payloads, instead we would keep the `data` value as
     is. This allows us to remove a bunch of our custom logic around
     invoking the mappers. One implication here is that we need to remove
     the `format` directive for model types in our swagger before
     generating code with auto-rest, so that the interfaces defined for
     custom events use `string` as the type instead of `Date`.
    
    - Some light renaming of interface names tied to System Event
      Data. There was a common convention from some services to use a
      `Properties` suffix in their schema name, which caused us to
      generate interfaces with the suffix `Properties` which was not very
      idiomatic.
    
    - Mark properties in shapes from System Events as present, based on
      the sample data that is published by the EventGrid service team on
      docs.microsoft.com.
    
    Fixes #10992
    
    * [EventGrid] Add test to lock in endpoint behavior
    
    We require that the user inclide the `/api/events` path as part of the
    endpoint when creating a client. This is so that we remain flexable
    and allow the event grid service to evolve the structure of their
    endpoint URLs.
    
    We already had this behavior, but so just add a test to lock the new
    behavior in.
    
    * [EventGrid] Update Recordings

--- sdk/eventgrid/eventgrid/recordings/node/eventgridpublisherclient_send_eventgrid_schema/recording_sends_multiple_events.js
+++ sdk/eventgrid/eventgrid/recordings/node/eventgridpublisherclient_send_eventgrid_schema/recording_sends_multiple_events.js
@@ -2,10 +2,10 @@ let nock = require('nock');
 
 module.exports.hash = "ad571fe06ccea51458a22486b720e15a";
 
-module.exports.testInfo = {"uniqueName":{"multiEventId1":"multiEventId1161257785259006866","multiEventId2":"multiEventId2161257785259003349"},"newDate":{"multiEventDate1":"2021-02-06T02:17:32.590Z","multiEventDate2":"2021-02-06T02:17:32.590Z"}}
+module.exports.testInfo = {"uniqueName":{"multiEventId1":"multiEventId1161541017700703368","multiEventId2":"multiEventId2161541017700701734"},"newDate":{"multiEventDate1":"2021-03-10T21:02:57.007Z","multiEventDate2":"2021-03-10T21:02:57.007Z"}}
 
 nock('https://endpoint', {"encodedQueryParams":true})
-  .post('/api/events', [{"id":"multiEventId1161257785259006866","subject":"Multiple 1","data":{"hello":"world"},"eventType":"Azure.Sdk.TestEvent1","eventTime":"2021-02-06T02:17:32.590Z","dataVersion":"1.0"},{"id":"multiEventId2161257785259003349","subject":"Multiple 2","data":{"hello":"world"},"eventType":"Azure.Sdk.TestEvent1","eventTime":"2021-02-06T02:17:32.590Z","dataVersion":"1.0"}])
+  .post('/api/events', [{"id":"multiEventId1161541017700703368","subject":"Multiple 1","data":{"hello":"world"},"eventType":"Azure.Sdk.TestEvent1","eventTime":"2021-03-10T21:02:57.007Z","dataVersion":"1.0"},{"id":"multiEventId2161541017700701734","subject":"Multiple 2","data":{"hello":"world"},"eventType":"Azure.Sdk.TestEvent1","eventTime":"2021-03-10T21:02:57.007Z","dataVersion":"1.0"}])
   .query(true)
   .reply(200, "", [
   'Content-Length',
@@ -17,7 +17,7 @@ nock('https://endpoint', {"encodedQueryParams":true})
   'api-supported-versions',
   '2018-01-01',
   'x-ms-request-id',
-  '2d51c28d-48e0-499d-ac7d-54f569537f73',
+  '6e7cb7f6-a74a-4809-b0e2-cb901b04054e',
   'Date',
-  'Sat, 06 Feb 2021 02:17:32 GMT'
+  'Wed, 10 Mar 2021 21:02:56 GMT'
 ]);
