4430cef0c7b56ca02cee2f85ed2797501587f953
Jeremy Meng <yumeng@microsoft.com>
2021-03-17 09:56:58 -0700

    Fix sample build error

--- sdk/containerregistry/container-registry/samples/typescript/src/containerRegistryClient.ts
+++ sdk/containerregistry/container-registry/samples/typescript/src/containerRegistryClient.ts
@@ -14,7 +14,7 @@ export async function main() {
   const endpoint = process.env.ENDPOINT ?? "<endpoint>";
   const client = new ContainerRegistryClient(endpoint, new DefaultAzureCredential());
   await listRepositories(client);
-  // await deleteRepository(client);
+  await deleteRepository(client);
 }
 
 async function listRepositories(client: ContainerRegistryClient) {
