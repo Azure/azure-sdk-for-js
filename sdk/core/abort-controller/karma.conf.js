671a2c475e3b1fbf674b4f460ed74102810e3b86
Ramya Achutha Rao <ramyar@microsoft.com>
2021-03-16 17:39:46 -0700

    [Core] Run format

--- sdk/core/abort-controller/karma.conf.js
+++ sdk/core/abort-controller/karma.conf.js
@@ -2,7 +2,7 @@
 process.env.CHROME_BIN = require("puppeteer").executablePath();
 require("dotenv").config({ path: "../.env" });
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: "./",
