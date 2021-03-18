e0091fb4838a00e384df9a5a9428f80c242ccd07
Maor Leger <malege@microsoft.com>
2021-03-18 16:41:56 -0700

    add multi-version support

--- sdk/keyvault/keyvault-certificates/test/public/lro.operation.spec.ts
+++ sdk/keyvault/keyvault-certificates/test/public/lro.operation.spec.ts
@@ -13,89 +13,92 @@ import {
 import { testPollerProperties } from "../utils/recorderUtils";
 import { authenticate } from "../utils/testAuthentication";
 import TestClient from "../utils/testClient";
+import { versionsToTest } from "@azure/test-utils-multi-version";
+import { serviceVersions } from "../utils/utils.common";
 
-describe("Certificates client - LRO - certificate operation", () => {
-  const certificatePrefix = `lroOperation${env.CERTIFICATE_NAME || "CertificateName"}`;
-  let certificateSuffix: string;
-  let client: CertificateClient;
-  let testClient: TestClient;
-  let recorder: Recorder;
+versionsToTest(serviceVersions, {}, (serviceVersion) => {
+  describe("Certificates client - LRO - certificate operation", () => {
+    const certificatePrefix = `lroOperation${env.CERTIFICATE_NAME || "CertificateName"}`;
+    let certificateSuffix: string;
+    let client: CertificateClient;
+    let testClient: TestClient;
+    let recorder: Recorder;
 
-  beforeEach(
-    /** @this Mocha.Context */ async function() {
-      const authentication = await authenticate(this);
+    beforeEach(async function() {
+      const authentication = await authenticate(this, serviceVersion);
       certificateSuffix = authentication.suffix;
       client = authentication.client;
       testClient = authentication.testClient;
       recorder = authentication.recorder;
-    }
-  );
-
-  afterEach(async function() {
-    await recorder.stop();
-  });
-
-  // The tests follow
-
-  it("can wait until a certificate is created by getting the poller from getCertificateOperation", /** @this Mocha.Context */ async function() {
-    const certificateName = testClient.formatName(
-      `${certificatePrefix}-${this!.test!.title}-${certificateSuffix}`
-    );
-    const createPoller = await client.beginCreateCertificate(
-      certificateName,
-      DefaultCertificatePolicy,
-      testPollerProperties
-    );
-    createPoller.stopPolling();
-    const poller = await client.getCertificateOperation(certificateName, testPollerProperties);
-    assert.ok(poller.getOperationState().isStarted);
-
-    // The pending certificate operation can be obtained this way:
-    assert.equal(poller.getOperationState().certificateOperation!.status, "inProgress");
-
-    const completeCertificate: KeyVaultCertificateWithPolicy = await poller.pollUntilDone();
-    assert.equal(completeCertificate.name, certificateName);
-
-    const operation: CertificateOperation = poller.getOperationState().certificateOperation!;
-    assert.equal(operation.status, "completed");
-    assert.ok(poller.getOperationState().isCompleted);
-
-    // The final certificate operation can also be obtained this way:
-    assert.equal(poller.getOperationState().certificateOperation!.status, "completed");
-
-    await testClient.flushCertificate(certificateName);
-  });
-
-  it("can resume from a stopped poller", /** @this Mocha.Context */ async function() {
-    const certificateName = testClient.formatName(
-      `${certificatePrefix}-${this!.test!.title}-${certificateSuffix}`
-    );
-    const createPoller = await client.beginCreateCertificate(
-      certificateName,
-      DefaultCertificatePolicy,
-      testPollerProperties
-    );
-
-    createPoller.stopPolling();
-
-    const poller = await client.getCertificateOperation(certificateName, testPollerProperties);
-    assert.ok(poller.getOperationState().isStarted);
-
-    const serialized = poller.toString();
-
-    const resumePoller = await client.getCertificateOperation(certificateName, {
-      resumeFrom: serialized,
-      ...testPollerProperties
     });
-    assert.ok(resumePoller.getOperationState().isStarted);
 
-    const completeCertificate: KeyVaultCertificateWithPolicy = await resumePoller.pollUntilDone();
-    assert.equal(completeCertificate.name, certificateName);
+    afterEach(async function() {
+      await recorder.stop();
+    });
 
-    const operation: CertificateOperation = resumePoller.getOperationState().certificateOperation!;
-    assert.equal(operation.status, "completed");
-    assert.ok(resumePoller.getOperationState().isCompleted);
+    // The tests follow
 
-    await testClient.flushCertificate(certificateName);
+    it("can wait until a certificate is created by getting the poller from getCertificateOperation", async function() {
+      const certificateName = testClient.formatName(
+        `${certificatePrefix}-${this!.test!.title}-${certificateSuffix}`
+      );
+      const createPoller = await client.beginCreateCertificate(
+        certificateName,
+        DefaultCertificatePolicy,
+        testPollerProperties
+      );
+      createPoller.stopPolling();
+      const poller = await client.getCertificateOperation(certificateName, testPollerProperties);
+      assert.ok(poller.getOperationState().isStarted);
+
+      // The pending certificate operation can be obtained this way:
+      assert.equal(poller.getOperationState().certificateOperation!.status, "inProgress");
+
+      const completeCertificate: KeyVaultCertificateWithPolicy = await poller.pollUntilDone();
+      assert.equal(completeCertificate.name, certificateName);
+
+      const operation: CertificateOperation = poller.getOperationState().certificateOperation!;
+      assert.equal(operation.status, "completed");
+      assert.ok(poller.getOperationState().isCompleted);
+
+      // The final certificate operation can also be obtained this way:
+      assert.equal(poller.getOperationState().certificateOperation!.status, "completed");
+
+      await testClient.flushCertificate(certificateName);
+    });
+
+    it("can resume from a stopped poller", async function() {
+      const certificateName = testClient.formatName(
+        `${certificatePrefix}-${this!.test!.title}-${certificateSuffix}`
+      );
+      const createPoller = await client.beginCreateCertificate(
+        certificateName,
+        DefaultCertificatePolicy,
+        testPollerProperties
+      );
+
+      createPoller.stopPolling();
+
+      const poller = await client.getCertificateOperation(certificateName, testPollerProperties);
+      assert.ok(poller.getOperationState().isStarted);
+
+      const serialized = poller.toString();
+
+      const resumePoller = await client.getCertificateOperation(certificateName, {
+        resumeFrom: serialized,
+        ...testPollerProperties
+      });
+      assert.ok(resumePoller.getOperationState().isStarted);
+
+      const completeCertificate: KeyVaultCertificateWithPolicy = await resumePoller.pollUntilDone();
+      assert.equal(completeCertificate.name, certificateName);
+
+      const operation: CertificateOperation = resumePoller.getOperationState()
+        .certificateOperation!;
+      assert.equal(operation.status, "completed");
+      assert.ok(resumePoller.getOperationState().isCompleted);
+
+      await testClient.flushCertificate(certificateName);
+    });
   });
 });
