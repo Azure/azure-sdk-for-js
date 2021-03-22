e1083301bdf09cdf8e180ff54d92f0df2e4dd73b
Jeremy Meng <yumeng@microsoft.com>
2021-03-22 12:25:29 -0700

    Fix linting error

--- sdk/containerregistry/container-registry/src/utils.ts
+++ sdk/containerregistry/container-registry/src/utils.ts
@@ -3,10 +3,11 @@
 
 /**
  * Extract the path part from the next link value returned by the service,
- * which is of form
- *    </acr/v1/name/...&n=2&orderby=>; rel="next"
  * @internal
  */
 export function extractNextLink(value: string | undefined): string | undefined {
+  // The link value has this pattern
+  //     `</acr/v1/name/...&n=2&orderby=>; rel="next"`
+  // and we only want the part inside of <...>
   return value?.substr(1, value.indexOf(">") - 1);
 }
