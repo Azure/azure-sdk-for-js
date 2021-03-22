c13522e42ba409680e14d31e7b11bc64bb660cc9
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2021-03-22 17:35:59 +0000

    node 15 CI workaround while we wait for MSAL

--- sdk/identity/identity/src/tokenCache/nodeVersion.ts
+++ sdk/identity/identity/src/tokenCache/nodeVersion.ts
@@ -8,5 +8,5 @@
 export const isNode8 = process.versions.node[0] === "8";
 export const Node8NotSupportedError = new Error("Node 8 does not support persistence caching.");
 
-export const isNode15 = process.versions.node[0] === "15";
+export const isNode15 = process.versions.node.slice(0, 2) === "15";
 export const Node15NotSupportedError = new Error("Node 15 does not support persistence caching.");
