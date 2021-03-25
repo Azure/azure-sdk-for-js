f61fe284c772677f9f9fb06018211528595b0387
colawwj <v-sigangzhou@microsoft.com>
2021-03-25 13:44:46 +0800

    arm-commeunication-release

--- sdk/communication/arm-communication/src/communicationServiceManagementClientContext.ts
+++ sdk/communication/arm-communication/src/communicationServiceManagementClientContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-communication";
-const packageVersion = "1.1.0";
+const packageVersion = "2.0.0";
 
 export class CommunicationServiceManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
@@ -36,7 +36,7 @@ export class CommunicationServiceManagementClientContext extends msRestAzure.Azu
     if (!options) {
       options = {};
     }
-    if(!options.userAgent) {
+    if (!options.userAgent) {
       const defaultUserAgent = msRestAzure.getDefaultUserAgentValue();
       options.userAgent = `${packageName}/${packageVersion} ${defaultUserAgent}`;
     }
@@ -51,10 +51,10 @@ export class CommunicationServiceManagementClientContext extends msRestAzure.Azu
     this.credentials = credentials;
     this.subscriptionId = subscriptionId;
 
-    if(options.acceptLanguage !== null && options.acceptLanguage !== undefined) {
+    if (options.acceptLanguage !== null && options.acceptLanguage !== undefined) {
       this.acceptLanguage = options.acceptLanguage;
     }
-    if(options.longRunningOperationRetryTimeout !== null && options.longRunningOperationRetryTimeout !== undefined) {
+    if (options.longRunningOperationRetryTimeout !== null && options.longRunningOperationRetryTimeout !== undefined) {
       this.longRunningOperationRetryTimeout = options.longRunningOperationRetryTimeout;
     }
   }
