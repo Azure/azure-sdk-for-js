d6dd0d37854928887a7e29552ccecb21a84fdfd7
Will Temple <witemple@microsoft.com>
2021-03-26 15:46:41 -0700

    [dev-tool] Fix relative import of prettier options. (#14560)

--- common/tools/dev-tool/src/commands/samples/tsToJs.ts
+++ common/tools/dev-tool/src/commands/samples/tsToJs.ts
@@ -18,8 +18,9 @@ export const commandInfo = makeCommandInfo(
   "ts-to-js",
   "convert a TypeScript sample to a JavaScript equivalent using our conventions for samples"
 );
+
 const prettierOptions: prettier.Options = {
-  ...(require("../eslint-plugin-azure-sdk/prettier.json") as prettier.Options),
+  ...(require("../../../../eslint-plugin-azure-sdk/prettier.json") as prettier.Options),
   parser: "typescript"
 };
 
