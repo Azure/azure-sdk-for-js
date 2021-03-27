2a601344a7d486ee252a4f72fbc9fd564faf9e9b
Richard Park <ripark@microsoft.com>
2021-03-26 20:24:26 -0700

    Adding in named exports.

--- sdk/identity/identity/rollup.base.config.js
+++ sdk/identity/identity/rollup.base.config.js
@@ -30,7 +30,11 @@ export function nodeConfig(test = false) {
       }),
       nodeResolve({ preferBuiltins: true }),
       json(),
-      cjs()
+      cjs({
+        namedExports: {
+          ...openTelemetryCommonJs
+        }
+      })
     ]
   };
 
