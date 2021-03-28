1777bacacbf894734fbf350d24bc837a0d5e46b3
Richard Park <ripark@microsoft.com>
2021-03-27 19:05:37 -0700

    Fixing lint errors

--- sdk/core/core-auth/src/tracing.ts
+++ sdk/core/core-auth/src/tracing.ts
@@ -61,22 +61,22 @@ export interface Context {
   /**
    * Get a value from the context.
    *
-   * @param key key which identifies a context value
+   * @param key - key which identifies a context value
    */
   getValue(key: symbol): unknown;
   /**
    * Create a new context which inherits from this context and has
    * the given key set to the given value.
    *
-   * @param key context key for which to set the value
-   * @param value value to set for the given key
+   * @param key - context key for which to set the value
+   * @param value - value to set for the given key
    */
   setValue(key: symbol, value: unknown): Context;
   /**
    * Return a new context which inherits from this context but does
    * not contain a value for the given key.
    *
-   * @param key context key for which to clear a value
+   * @param key - context key for which to clear a value
    */
   deleteValue(key: symbol): Context;
 }
