83f362fc2ca497aa16c60e8d492992c2ca73e366
Karishma Ghiya <kghiya8@gmail.com>
2021-04-04 23:02:33 -0700

    undo core-tracing output file changes

--- sdk/core/core-tracing/rollup.base.config.js
+++ sdk/core/core-tracing/rollup.base.config.js
@@ -38,7 +38,7 @@ export function nodeConfig(test = false) {
     baseConfig.plugins.unshift(multiEntry({ exports: false }));
 
     // different output file
-    baseConfig.output.file = "dist-test/index.js";
+    baseConfig.output.file = "test-dist/index.js";
 
     // mark assert as external
     baseConfig.external.push("assert");
