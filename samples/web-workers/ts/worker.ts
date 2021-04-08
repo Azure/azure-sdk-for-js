264afa2e157539b9340bc3842a92ab1844e3fcea
Maor Leger <malege@microsoft.com>
2021-04-08 09:53:20 -0700

    working sample

--- samples/web-workers/ts/worker.ts
+++ samples/web-workers/ts/worker.ts
@@ -2,11 +2,7 @@
 import "./polyfill.worker";
 import { BlobClient, ContainerClient } from "@azure/storage-blob";
 
-import * as dotenv from "dotenv"
-dotenv.config()
-
-
-const containerClient = new ContainerClient(process.env.STORAGE_BLOB_SAS, "/default/blobs")
+const containerClient = new ContainerClient(process.env.AZURE_STORAGE_BLOB_CONNECTION_STRING, "default/blobs")
 const blockBlobClient = containerClient.getBlockBlobClient("sample.txt")
 
 const data = "Hello, Web Workers!"
