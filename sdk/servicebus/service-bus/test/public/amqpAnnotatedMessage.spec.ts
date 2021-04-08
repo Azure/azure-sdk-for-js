4f016bbe593a5f2d57f721486a17f774c4ae5124
HarshaNalluru <sanallur@microsoft.com>
2021-04-08 15:47:22 -0700

    live test to match all the sent props

--- sdk/servicebus/service-bus/test/public/amqpAnnotatedMessage.spec.ts
+++ sdk/servicebus/service-bus/test/public/amqpAnnotatedMessage.spec.ts
@@ -3,48 +3,66 @@
 
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { ServiceBusSender, ServiceBusReceivedMessage } from "../../src";
-import { TestClientType, TestMessage } from "../public/utils/testUtils";
+import { ServiceBusSender } from "../../src";
 import { ServiceBusReceiver } from "../../src/receivers/receiver";
 import {
   ServiceBusClientForTests,
   createServiceBusClientForTests,
   testPeekMsgsLength,
   EntityName,
-  getRandomTestClientTypeWithNoSessions
+  getRandomTestClientType
 } from "../public/utils/testutils2";
 import { AmqpAnnotatedMessage } from "@azure/core-amqp";
+import { generateUuid } from "@azure/core-http";
 
 const should = chai.should();
 chai.use(chaiAsPromised);
 const assert = chai.assert;
 
-const noSessionTestClientType = getRandomTestClientTypeWithNoSessions();
-// const anyRandomTestClientType = getRandomTestClientType();
+const anyRandomTestClientType = getRandomTestClientType();
 
 let serviceBusClient: ServiceBusClientForTests;
 let entityNames: EntityName;
 let sender: ServiceBusSender;
 let receiver: ServiceBusReceiver;
-
-async function beforeEachTest(
-  entityType: TestClientType,
-  receiveMode: "peekLock" | "receiveAndDelete" = "peekLock"
-): Promise<void> {
-  entityNames = await serviceBusClient.test.createTestEntities(entityType);
-  if (receiveMode === "receiveAndDelete") {
-    receiver = await serviceBusClient.test.createReceiveAndDeleteReceiver(entityNames);
-  } else {
-    receiver = await serviceBusClient.test.createPeekLockReceiver(entityNames);
-  }
-
-  sender = serviceBusClient.test.addToCleanup(
-    serviceBusClient.createSender(entityNames.queue ?? entityNames.topic!)
-  );
-}
+const sessionId = "session-1";
 
 function afterEachTest(): Promise<void> {
   return serviceBusClient.test.afterEach();
+}
+
+function getSampleAmqpAnnotatedMessage(randomTag?: string): AmqpAnnotatedMessage {
+  if (randomTag == null) {
+    randomTag = Math.random().toString();
+  }
+
+  return {
+    body: `message body ${randomTag}`,
+    bodyType: "data",
+    header: {
+      deliveryCount: 10,
+      durable: false,
+      firstAcquirer: false,
+      priority: 20,
+      timeToLive: 100000
+    },
+    applicationProperties: {
+      propOne: 1,
+      propTwo: "two",
+      propThree: true,
+      propFour: Date()
+    },
+    // deliveryAnnotations - TODO: should this be removed for sending?
+    footer: {
+      propFooter: "foot"
+    },
+    messageAnnotations: { propMsgAnnotate: "annotation" },
+    properties: {
+      contentEncoding: "application/json; charset=utf-8",
+      correlationId: randomTag,
+      messageId: generateUuid() // Add more fields }
+    }
+  };
 }
 
 describe("AmqpAnnotatedMessage", function(): void {
@@ -60,15 +78,16 @@ describe("AmqpAnnotatedMessage", function(): void {
     await afterEachTest();
   });
 
-  async function sendReceiveMsg(
-    testMessage: AmqpAnnotatedMessage
-  ): Promise<ServiceBusReceivedMessage> {
-    await sender.sendMessages(testMessage);
+  async function receiveMsg(testMessage: AmqpAnnotatedMessage) {
+    receiver = await serviceBusClient.test.createReceiveAndDeleteReceiver({
+      ...entityNames,
+      sessionId
+    });
     const msgs = await receiver.receiveMessages(1);
 
     should.equal(Array.isArray(msgs), true, "`ReceivedMessages` is not an array");
     should.equal(msgs.length, 1, "Unexpected number of messages");
-    should.equal(msgs[0].body, testMessage.body, "Unecpected body on the received message");
+    should.equal(msgs[0].body, testMessage.body, "Unexpected body on the received message");
     should.equal(
       msgs[0]._rawAmqpMessage.messageAnnotations!["propMsgAnnotate"],
       testMessage.messageAnnotations!["propMsgAnnotate"],
@@ -77,36 +96,46 @@ describe("AmqpAnnotatedMessage", function(): void {
     assert.deepEqualExcluding(
       msgs[0]._rawAmqpMessage,
       testMessage,
-      ["deliveryAnnotations", "body", "messageAnnotations", "header"],
-      "Unexpected AmqpAnnotatedMessage"
+      ["deliveryAnnotations", "body", "messageAnnotations", "header", "properties"],
+      "Unexpected on the AmqpAnnotatedMessage"
     );
     assert.deepEqualExcluding(
       msgs[0]._rawAmqpMessage.header!,
       testMessage.header!,
       ["deliveryCount"],
-      "Unexpected header for AmqpAnnotatedMessage"
+      "Unexpected header on the AmqpAnnotatedMessage"
     );
-
-    return msgs[0];
+    console.log(msgs[0]._rawAmqpMessage.properties!.messageId, testMessage.properties!.messageId);
+    assert.deepEqualExcluding(
+      msgs[0]._rawAmqpMessage.properties!,
+      testMessage.properties!,
+      ["creationTime", "absoluteExpiryTime", "groupId"],
+      "Unexpected properties on the AmqpAnnotatedMessage"
+    );
+    assert.equal(
+      msgs[0]._rawAmqpMessage.properties!.groupId,
+      testMessage.properties!.groupId,
+      "Unexpected session-id on the AmqpAnnotatedMessage"
+    );
   }
 
-  async function testComplete(): Promise<void> {
-    const testMessages: AmqpAnnotatedMessage = {
-      ...TestMessage.getSampleForAmqpAnnotatedMessage(),
-      properties: { groupId: entityNames.usesSessions ? "session-1" : undefined }
-    };
-    const msg = await sendReceiveMsg(testMessages);
-
-    await receiver.completeMessage(msg);
-
-    await testPeekMsgsLength(receiver, 0);
-  }
-  // TODO: Make it anyTestClientType
   it(
-    noSessionTestClientType + ": send, receive, verify props, and complete()",
+    anyRandomTestClientType + ": send, receive, verify props, and complete()",
     async function(): Promise<void> {
-      await beforeEachTest(noSessionTestClientType);
-      await testComplete();
+      entityNames = await serviceBusClient.test.createTestEntities(anyRandomTestClientType);
+
+      const testMessage: AmqpAnnotatedMessage = getSampleAmqpAnnotatedMessage();
+      testMessage.properties = {
+        ...testMessage.properties,
+        groupId: entityNames.usesSessions ? sessionId : undefined
+      };
+      sender = serviceBusClient.test.addToCleanup(
+        serviceBusClient.createSender(entityNames.queue ?? entityNames.topic!)
+      );
+      await sender.sendMessages(testMessage);
+      await receiveMsg(testMessage);
+
+      await testPeekMsgsLength(receiver, 0);
     }
   );
 });
