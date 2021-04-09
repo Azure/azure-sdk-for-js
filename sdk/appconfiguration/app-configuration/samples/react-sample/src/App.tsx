33ff3029cede08ad0e8c90ed18245dfd866f855c
HarshaNalluru <sanallur@microsoft.com>
2021-04-08 23:18:40 -0700

    starter code react sample

---
+++ sdk/appconfiguration/app-configuration/samples/react-sample/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
