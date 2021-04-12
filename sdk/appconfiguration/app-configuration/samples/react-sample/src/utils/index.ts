9cea077d7332234c2359dfdab220a11f1867466d
HarshaNalluru <sanallur@microsoft.com>
2021-04-12 13:39:43 -0700

    remove AAD

--- sdk/appconfiguration/app-configuration/samples/react-sample/src/utils/index.ts
+++ sdk/appconfiguration/app-configuration/samples/react-sample/src/utils/index.ts
@@ -2,8 +2,6 @@
   Copyright (c) Microsoft Corporation.
   Licensed under the MIT license.
 */
-import { InteractiveBrowserCredential } from "@azure/identity";
-
 export function getEnvironmentVariable(name: string): string {
   const value = process.env[name.toUpperCase()] || process.env[name.toLowerCase()];
   if (!value) {
@@ -11,8 +9,3 @@ export function getEnvironmentVariable(name: string): string {
   }
   return value;
 }
-
-const clientId = getEnvironmentVariable("REACT_APP_AZURE_CLIENT_ID");
-const tenantId = getEnvironmentVariable("REACT_APP_AZURE_TENANT_ID");
-
-export const credential = new InteractiveBrowserCredential({ clientId, tenantId });
