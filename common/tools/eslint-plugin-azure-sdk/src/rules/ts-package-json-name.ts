5a3492dee714d84107fd95b8fc0add74085f1f2f
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-04-12 22:53:17 +0000

    Remove eslint changes

--- common/tools/eslint-plugin-azure-sdk/src/rules/ts-package-json-name.ts
+++ common/tools/eslint-plugin-azure-sdk/src/rules/ts-package-json-name.ts
@@ -18,7 +18,7 @@ import { stripFileName } from "../utils/verifiers";
 export = {
   meta: getRuleMetaData(
     "ts-package-json-name",
-    "force package.json's name value to be set to @azure/<service> or @azure-<subscope>/<service>"
+    "force package.json's name value to be set to @azure/<service>"
   ),
   create: (context: Rule.RuleContext): Rule.RuleListener => {
     const verifiers = getVerifiers(context, {
@@ -32,24 +32,24 @@ export = {
           // check to see if name exists at the outermost level
           "ExpressionStatement > ObjectExpression": verifiers.existsInFile,
 
-          // check the node corresponding to name to see if its value is @azure/<service> or @azure-<subscope>/<service>
+          // check the node corresponding to name to see if its value is @azure/<service>
           "ExpressionStatement > ObjectExpression > Property[key.value='name']": (
             node: Property
           ): void => {
             const nodeValue = node.value as Literal;
             const name = nodeValue.value as string;
 
-            if (!/^@azure(-[a-z]+)?\//.test(name)) {
+            if (!name.startsWith("@azure/")) {
               context.report({
                 node: nodeValue,
-                message: "name is not set to @azure/<service> or @azure-<subscope>/<service>"
+                message: "name is not set to @azure/<service>"
               });
               return;
             }
 
             const packageDirectory = stripPath(stripFileName(fileName));
             const packageBaseName = stripPath(name);
-            if (!/^@azure(-[a-z]+)?\/([a-z]+-)*[a-z]+$/.test(name)) {
+            if (!/^@azure\/([a-z]+-)*[a-z]+$/.test(name)) {
               context.report({
                 node: nodeValue,
                 message: "service name is not in kebab-case (lowercase and separated by hyphens)"
