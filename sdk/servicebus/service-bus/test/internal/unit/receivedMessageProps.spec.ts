cf7be2509d5d28624d553ff36a627fa4f7e1f500
HarshaNalluru <sanallur@microsoft.com>
2021-04-14 14:12:09 -0700

    move to unit test folder

--- sdk/servicebus/service-bus/test/internal/unit/receivedMessageProps.spec.ts
+++ sdk/servicebus/service-bus/test/internal/unit/receivedMessageProps.spec.ts
@@ -3,9 +3,9 @@
 
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { ServiceBusMessage } from "../../src";
-import { TestMessage } from "../public/utils/testUtils";
-import { fromRheaMessage, toRheaMessage } from "../../src/serviceBusMessage";
+import { ServiceBusMessage } from "../../../src";
+import { TestMessage } from "../../public/utils/testUtils";
+import { fromRheaMessage, toRheaMessage } from "../../../src/serviceBusMessage";
 import { Message as RheaMessage } from "rhea-promise";
 import { Constants } from "@azure/core-amqp";
 
