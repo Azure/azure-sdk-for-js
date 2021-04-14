4db984ac60f1fa54c2cda80abbc22a78241f01ff
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-04-14 01:35:03 +0000

    Add sendRequest tests

---
+++ sdk/core-rest/core-client/test/sendRequest.spec.ts
@@ -0,0 +1,92 @@
+import { sendRequest } from "../src/sendRequest";
+import { assert } from "chai";
+import {
+  createEmptyPipeline,
+  createHttpHeaders,
+  Pipeline,
+  PipelineResponse,
+} from "@azure/core-rest-pipeline";
+describe("sendRequest", () => {
+  const mockBaseUrl = "https://example.org";
+
+  it("should send request with json body", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    const expectedBody = { foo: "foo" };
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.body, JSON.stringify(expectedBody));
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { body: expectedBody });
+  });
+
+  it("should send request with undefined body", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.body, undefined);
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { body: undefined });
+  });
+
+  it("should set custom content-type", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.headers.get("content-type"), "testContent");
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { contentType: "testContent", body: {} });
+  });
+
+  it("should not set content-type if no body is present", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.headers.get("content-type"), undefined);
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { contentType: "testContent" });
+  });
+
+  it("should set custom accept", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.headers.get("accept"), "testContent");
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { accept: "testContent" });
+  });
+
+  it("should set custom headers", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.headers.get("foo"), "foo");
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { headers: { foo: "foo" } });
+  });
+
+  it("should set octet-stream when binary body", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.headers.get("content-type"), "application/octet-stream");
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { body: new Uint8Array() });
+  });
+
+  it("should set application/json by default if not binary", async () => {
+    const mockPipeline: Pipeline = createEmptyPipeline();
+    mockPipeline.sendRequest = async (_client, request) => {
+      assert.equal(request.headers.get("content-type"), "application/json; charset=UTF-8");
+      return { headers: createHttpHeaders() } as PipelineResponse;
+    };
+
+    await sendRequest("POST", mockBaseUrl, mockPipeline, { body: "test" });
+  });
+});
