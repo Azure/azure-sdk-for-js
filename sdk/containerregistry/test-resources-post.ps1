8782ee60fcb88921dccc43c4cb3aa2caceb13ef2
Jeremy Meng <yumeng@microsoft.com>
2021-04-16 12:23:09 -0700

    Fix error
    - add missing line continuation '`'

--- sdk/containerregistry/test-resources-post.ps1
+++ sdk/containerregistry/test-resources-post.ps1
@@ -9,5 +9,5 @@ Import-AzContainerRegistryImage `
     -ResourceGroupName $DeploymentOutputs['CONTAINERREGISTRY_RESOURCE_GROUP'] `
     -RegistryName $DeploymentOutputs['CONTAINERREGISTRY_REGISTRY_NAME'] `
     -SourceImage 'library/hello-world' -SourceRegistryUri 'registry.hub.docker.com' `
-    -Mode 'Force'
+    -Mode 'Force' `
     -TargetTag 'library/hello-world:latest','library/hello-world:test1','library/hello-world:test-delete'
