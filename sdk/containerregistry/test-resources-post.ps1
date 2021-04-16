b2a9ba2a9f174a90c5b45ab974e9789d822fd5d6
Jeremy Meng <yumeng@microsoft.com>
2021-04-16 11:14:11 -0700

    [ContainerRegistry] Update resource deployment scripts

--- sdk/containerregistry/test-resources-post.ps1
+++ sdk/containerregistry/test-resources-post.ps1
@@ -7,6 +7,7 @@ param (
 
 Import-AzContainerRegistryImage `
     -ResourceGroupName $DeploymentOutputs['CONTAINERREGISTRY_RESOURCE_GROUP'] `
-    -RegistryName $DeploymentOutputs['CONTAINERREGISTRY_USERNAME'] `
+    -RegistryName $DeploymentOutputs['CONTAINERREGISTRY_REGISTRY_NAME'] `
     -SourceImage 'library/hello-world' -SourceRegistryUri 'registry.hub.docker.com' `
     -Mode 'Force'
+    -TargetTag 'library/hello-world:latest','library/hello-world:test1','library/hello-world:test-delete'
