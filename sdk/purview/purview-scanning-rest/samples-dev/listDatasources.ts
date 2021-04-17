535e7a44838c05d2e9188d652cb80037f2ce5c66
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-04-17 01:57:26 +0000

    Add samples

---
+++ sdk/purview/purview-scanning-rest/samples-dev/listDatasources.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * This sample demonstrates how get a list of datasources
+ *
+ * @summary gets a list of datasources
+ * @azsdk-weight 40
+ */
+
+import PurviewScanning from "@azure-rest/purview-scanning";
+import { DefaultAzureCredential } from "@azure/identity";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const endpoint = process.env["SCANNING_ENDPOINT"] || "";
+
+async function main() {
+  console.log("== List dataSources sample ==");
+  const client = PurviewScanning(endpoint, new DefaultAzureCredential());
+
+  const dataSources = await client.path("/datasources").get();
+
+  if (dataSources.status !== "200") {
+    throw dataSources;
+  }
+
+  console.log(dataSources.body.value?.map((ds) => ds.name).join("\n"));
+}
+
+main().catch(console.error);
--- sdk/purview/purview-scanning-rest/swagger/README.md
+++ sdk/purview/purview-scanning-rest/swagger/README.md
@@ -12,7 +12,7 @@ generate-metadata: false
 license-header: MICROSOFT_MIT_NO_VERSION
 output-folder: ../
 source-code-folder-path: ./src
-input-file: pathToSwagger
+input-file: https://raw.githubusercontent.com/parvsaxena/azure-rest-api-specs-pr/fb3047d3c260f0e9b91baf2f9217de364e1e5906/specification/purview/data-plane/Microsoft.Purview/preview/2018-12-01-preview/scanningService.json?token=AFBBZGOLJM6CRWISWYAOF23AQNXQI
 package-version: 1.0.0-beta.1
 rest-level-client: true
 add-credentials: true
