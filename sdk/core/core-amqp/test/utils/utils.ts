9c256590d9ac401bfd6cbf42e1656b12c177483b
Christopher Radek <chradek@microsoft.com>
2021-04-19 15:20:08 -0700

    [core-amqp] fix flaky test in node 15

--- sdk/core/core-amqp/test/utils/utils.ts
+++ sdk/core/core-amqp/test/utils/utils.ts
@@ -6,6 +6,12 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
  */
 export async function settleAllTasks(tasks: Promise<any>[]): Promise<any[]> {
+  // add no-op catch to each task to prevent unhandled promise rejection in node 15+
+  for (const task of tasks) {
+    task.catch(() => {
+      /* no-op */
+    });
+  }
   const results = [];
   for (const task of tasks) {
     try {
