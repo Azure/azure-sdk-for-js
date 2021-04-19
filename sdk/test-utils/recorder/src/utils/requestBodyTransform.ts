68a90af5b0e427d71be1e9cd1bb9ff9dd5e986be
HarshaNalluru <sanallur@microsoft.com>
2021-04-19 13:38:51 -0700

    more docs

--- sdk/test-utils/recorder/src/utils/requestBodyTransform.ts
+++ sdk/test-utils/recorder/src/utils/requestBodyTransform.ts
@@ -11,6 +11,10 @@ export type RequestBodyTransformsType = {
  */
 export const defaultRequestBodyTransforms: RequestBodyTransformsType = {
   stringTransforms: [
+    // Identity v2 with the new msal, has unique request-ids in request body, to be able to
+    // match the ids in playback we apply this method on the request body
+    // 1. before saving the recording
+    // 2. add it as a filter on the new requests to be able to match the request bodies
     (body: string) =>
       body.replace(/client-request-id=[^&]*/g, "client-request-id=client-request-id")
   ],
