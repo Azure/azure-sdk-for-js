2a3535f6ed7837335360f0fea8afdc846fabb4c3
g2vinay <vinay_work@hotmail.com>
2021-04-20 14:59:13 -0700

    update

--- sdk/identity/identity/src/credentials/azurePowerShellCredential.ts
+++ sdk/identity/identity/src/credentials/azurePowerShellCredential.ts
@@ -6,6 +6,7 @@ import { createSpan } from "../util/tracing";
 import { AuthenticationErrorName, CredentialUnavailable } from "../client/errors";
 import { CanonicalCode } from "@opentelemetry/api";
 import { credentialLogger, formatSuccess, formatError } from "../util/logging";
+import * as childProcess from "child_process";
 
 const logger = credentialLogger("AzurePowerShellCredential");
 
@@ -36,11 +37,39 @@ export class AzurePowerShellCredential implements TokenCredential {
    * Gets the access token from Azure Power Shell
    * @param resource The resource to use when getting the token
    */
-  protected async getAzurePowerShellAccessToken(resource: string) {
-    return new Promise((resolve, reject) => {
-
+  protected async getAzurePowerShellAccessToken(resource: string): Promise<{ Token: string; ExpiresOn: string }> {
       //Add calls to get token from Azure Power shell via node-powershell library.
-    });
+      //"Import-Module Az.Accounts -MinimumVersion 2.2.0 -PassThru"
+    //  "Get-AzAccessToken -ResourceUrl \"https://vault.azure.net\" | ConvertTo-Json"
+    
+
+      try {
+        let output: string[] = [];
+        let error: string[] = [];
+        var child = childProcess.spawn("powershell.exe", ["-Command", "-"]);
+
+        child.stdout.on("data", function(data) {
+          output.push(data.toString());
+        });
+        child.stderr.on("data", function(data) {
+          error.push(data.toString());
+        });
+
+        const commands = ["Import-Module Az.Accounts -MinimumVersion 2.2.0 -PassThru",
+        "Get-AzAccessToken -ResourceUrl \"https://vault.azure.net\" | ConvertTo-Json"];
+    
+        commands.forEach(function(cmd){
+            child.stdin.write(cmd+ '\n');
+        });
+        console.log({output, error})
+        if (error.length) {
+          throw new Error(error[0])
+        }
+        return JSON.parse(output[1]);
+        
+      } catch (err) {
+        throw err;
+      }
   }
 
   /**
@@ -57,7 +86,6 @@ export class AzurePowerShellCredential implements TokenCredential {
     scopes: string | string[],
     options?: GetTokenOptions
   ): Promise<AccessToken | null> {
-    return new Promise((resolve, reject) => {
       const scope = typeof scopes === "string" ? scopes : scopes[0];
       logger.getToken.info(`Using the scope ${scope}`);
 
@@ -73,52 +101,50 @@ export class AzurePowerShellCredential implements TokenCredential {
       let responseData = "";
 
       const { span } = createSpan("AzurePowerShellCredential-getToken", options);
-      this.getAzurePowerShellAccessToken(resource)
-        .then((obj: any) => {
-          if (obj.stderr) {
-            const isLoginError = obj.stderr.match("(.*)Run Connect-AzAccount to login(.*)");
-            const isNotInstallError =
-              obj.stderr.match("The specified module 'Az.Accounts' with version '2.2.0' was not loaded because no valid module file was found in any module directory")  
-            if (isNotInstallError) {
-              const error = new CredentialUnavailable(
-                "Az.Account module >= 2.2.0 is not installed."
-              );
-              logger.getToken.info(formatError(error));
-              throw error;
-            } else if (isLoginError) {
-              const error = new CredentialUnavailable(
-                "Please run 'az login' from a command prompt to authenticate before using this credential."
-              );
-              logger.getToken.info(formatError(error));
-              throw error;
-            }
-            const error = new CredentialUnavailable(obj.stderr);
+      try {
+        const response = await this.getAzurePowerShellAccessToken(resource);
+        logger.getToken.info(formatSuccess(scopes));
+        const returnValue = {
+          token: response.Token,
+          expiresOnTimestamp: new Date(response.ExpiresOn).getTime()
+        };
+        return returnValue;
+      } catch (err) {
+        // PowerShell Azure User not logged in error check.
+          const isLoginError = err.message.match("(.*)Run Connect-AzAccount to login(.*)");
+
+          // Az Module not Installed in Azure PowerShell check.
+          const isNotInstallError =
+            err.message.match("The specified module 'Az.Accounts' with version '2.2.0' was not loaded because no valid module file was found in any module directory");   
+          if (isNotInstallError) {
+            const error = new CredentialUnavailable(
+              "Az.Account module >= 2.2.0 is not installed."
+            );
             logger.getToken.info(formatError(error));
             throw error;
-          } else {
-            responseData = obj.stdout;
-            const response: { Token: string; ExpiresOn: string } = JSON.parse(responseData);
-            logger.getToken.info(formatSuccess(scopes));
-            const returnValue = {
-              token: response.Token,
-              expiresOnTimestamp: new Date(response.ExpiresOn).getTime()
-            };
-            resolve(returnValue);
-            return returnValue;
+
+            // Azure user not logged in check in Azure Power Shell.
+          } else if (isLoginError) {
+            const error = new CredentialUnavailable(
+              "Please run 'az login' from a command prompt to authenticate before using this credential."
+            );
+            logger.getToken.info(formatError(error));
+            throw error;
           }
-        })
-        .catch((err) => {
+          const error = new CredentialUnavailable(err);
+          logger.getToken.info(formatError(error));
+          throw error;
+
           const code =
-            err.name === AuthenticationErrorName
-              ? CanonicalCode.UNAUTHENTICATED
-              : CanonicalCode.UNKNOWN;
-          span.setStatus({
-            code,
-            message: err.message
-          });
-          logger.getToken.info(formatError(err));
-          reject(err);
+          err.name === AuthenticationErrorName
+            ? CanonicalCode.UNAUTHENTICATED
+            : CanonicalCode.UNKNOWN;
+        span.setStatus({
+          code,
+          message: err.message
         });
-    });
+        logger.getToken.info(formatError(err));
+        throw err;
+      }
   }
 }
