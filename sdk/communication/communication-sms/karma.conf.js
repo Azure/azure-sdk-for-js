f350925e45425328dfd47d70426d50b7400a0170
Thiago Diniz Maia <thdinizm@microsoft.com>
2021-04-21 16:10:26 -0700

    Add karma --no-sandbox config

--- sdk/communication/communication-sms/karma.conf.js
+++ sdk/communication/communication-sms/karma.conf.js
@@ -112,7 +112,14 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // 'ChromeHeadless', 'Chrome', 'Firefox', 'Edge', 'IE'
-    browsers: ["ChromeHeadless"],
+    browsers: ["HeadlessChrome"],
+    
+    customLaunchers: {
+      HeadlessChrome: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"]
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
