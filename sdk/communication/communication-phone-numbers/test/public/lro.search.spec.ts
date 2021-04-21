2c262191483534de015a5b72d0785eb88cbeacff
Bert Ong <823691+beltr0n@users.noreply.github.com>
2021-04-20 19:24:22 -0700

    move utils into public, add min workaround

--- sdk/communication/communication-phone-numbers/test/public/lro.search.spec.ts
+++ sdk/communication/communication-phone-numbers/test/public/lro.search.spec.ts
@@ -5,12 +5,12 @@ import { Recorder } from "@azure/test-utils-recorder";
 import { assert } from "chai";
 import { Context } from "mocha";
 import { PhoneNumbersClient, SearchAvailablePhoneNumbersRequest } from "../../src";
-import { matrix } from "../utils/matrix";
+import { matrix } from "./utils/matrix";
 import {
   canCreateRecordedClientWithToken,
   createRecordedClient,
   createRecordedClientWithToken
-} from "../utils/recordedClient";
+} from "./utils/recordedClient";
 
 matrix([[true, false]], async function(useAad) {
   describe(`PhoneNumbersClient - lro - search${useAad ? " [AAD]" : ""}`, function() {
