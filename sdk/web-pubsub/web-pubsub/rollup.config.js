c0b240bad4b9db381e1cd05ba4a008582ebd7bac
Brian Terlson <brian.terlson@microsoft.com>
2021-04-21 12:31:08 -0700

    Remove browser rollup configs

--- sdk/web-pubsub/web-pubsub/rollup.config.js
+++ sdk/web-pubsub/web-pubsub/rollup.config.js
@@ -1,14 +1,3 @@
 import * as base from "./rollup.base.config";
 
-const inputs = [];
-
-if (!process.env.ONLY_BROWSER) {
-  inputs.push(base.nodeConfig());
-}
-
-if (!process.env.ONLY_NODE) {
-  // Browser is not supported
-  // inputs.push(base.browserConfig());
-}
-
-export default inputs;
+export default [base.nodeConfig()];
