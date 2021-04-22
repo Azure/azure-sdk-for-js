e481f2b02a9985149c1576fea0b9e15e250c020d
Richard Park <ripark@microsoft.com>
2021-04-22 12:46:43 -0700

    - Use envsubst as per Bengsys's recommendation. - Rename all files to have .yaml extension (I think that's more typical)

--- sdk/servicebus/service-bus/test/stress/testRun.sh
+++ sdk/servicebus/service-bus/test/stress/testRun.sh
@@ -8,17 +8,19 @@ if [ "$1" == "" ] || [ "$2" == "" ]; then
 fi
 
 # don't forget to fill out the .env file (use sample.env as your base)
+set -a      # export variables from .env
 source .env
+set +a
 
 # figure out the proper image name, based on the git commit
 # and the state of the current clone
-SERVICEBUS_PACKAGE_VERSION=$1
-SCENARIO_JS_BASE_NAME=$2
-KUBERNETES_ENTITY="${SCENARIO_JS_BASE_NAME,,}-$SERVICEBUS_PACKAGE_VERSION"
+export SERVICEBUS_PACKAGE_VERSION=$1
+export SCENARIO_JS_BASE_NAME=$2
+export KUBERNETES_ENTITY="${SCENARIO_JS_BASE_NAME,,}-$SERVICEBUS_PACKAGE_VERSION"
 
-DOCKER_IMAGE_NAME="$REGISTRY_NAME/$DOCKER_REPOSITORY_NAME:$SERVICEBUS_PACKAGE_VERSION"
-K8S_POD_NAME="$KUBERNETES_ENTITY"
-K8S_SECRET_NAME="$KUBERNETES_ENTITY"
+export DOCKER_IMAGE_NAME="$REGISTRY_NAME/$DOCKER_REPOSITORY_NAME:$SERVICEBUS_PACKAGE_VERSION"
+export K8S_POD_NAME="$KUBERNETES_ENTITY"
+export K8S_SECRET_NAME="$KUBERNETES_ENTITY"
 
 echo "Will deploy"
 echo "  docker image:${DOCKER_IMAGE_NAME}"
@@ -29,42 +31,13 @@ echo "  Using your default context and namespace set in kubectl"
 
 createKubernetesSecret() {
     echo "Creating Kubernetes secret"
-
-    # create/recreate our secrets.yml
-    SECRETS_YAML=$(<secrets.yml)
-
-    # all the secrets we'll replace (marked with %value% in our secrets.yaml)
-    VARIABLES_TO_INJECT="
-        K8S_SECRET_NAME
-        SERVICEBUS_CONNECTION_STRING
-        APPINSIGHTS_INSTRUMENTATIONKEY
-    "
-
-    for name in $VARIABLES_TO_INJECT; do
-        SECRETS_YAML=${SECRETS_YAML//"%$name%"/${!name}}
-    done
-
-    echo "$SECRETS_YAML" | kubectl replace --force -f -
+    SECRET_YAML=$(envsubst < secrets.yaml)
+    echo $SECRET_YAML | kubectl replace --force -f -
 }
 
 createKubernetesPod() {
     echo "Creating Kubernetes pod"
-
-    # recreate the pod
-    POD_YAML=$(<pod.yaml)
-
-    VARIABLES_TO_INJECT="
-        DOCKER_IMAGE_NAME
-        K8S_SECRET_NAME
-        K8S_POD_NAME
-        KUBERNETES_NAMESPACE
-        SCENARIO_JS_BASE_NAME
-    "
-
-    for name in $VARIABLES_TO_INJECT; do
-        POD_YAML=${POD_YAML//"%$name%"/${!name}}
-    done
-
+    POD_YAML=$(envsubst < pod.yaml)
     echo "$POD_YAML" | kubectl replace --force -f -
 }
 
