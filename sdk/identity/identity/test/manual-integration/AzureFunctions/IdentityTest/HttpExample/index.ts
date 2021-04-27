e03b4963af502ad58d255b69b67b03b469bb39be
Karishma Ghiya <kghiya8@gmail.com>
2021-04-26 18:55:24 -0700

    azure functions identity

---
+++ sdk/identity/identity/test/manual-integration/AzureFunctions/IdentityTest/HttpExample/index.ts
@@ -0,0 +1,32 @@
+import { SecretClient } from "@azure/keyvault-secrets";
+import { DefaultAzureCredential } from "@azure/identity";
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+
+const httpTrigger: AzureFunction = async function(
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    const credential = new DefaultAzureCredential();
+
+    const vaultUri = process.env.KEYVAULT_URI;
+
+    if (!vaultUri) {
+      throw new Error("Missing KEYVAULT_URI environment variable.");
+    }
+    const client = new SecretClient(vaultUri, credential);
+
+    await client.setSecret("secret-name-system", "secret-value-system");
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: "Successfully authenticated with keyvault"
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message
+    };
+  }
+};
+
+export default httpTrigger;
