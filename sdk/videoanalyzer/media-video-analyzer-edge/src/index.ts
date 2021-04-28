4b23660c52ee3af2e30e755cfb66803d32027d20
hivyas <hivyas@microsoft.com>
2021-04-28 09:39:32 -0700

    saving progresS

--- sdk/videoanalyzer/media-video-analyzer-edge/src/index.ts
+++ sdk/videoanalyzer/media-video-analyzer-edge/src/index.ts
@@ -1,12 +1,10 @@
 export * from "./generated/models"; //add omit
 import { PipelineTopology, LivePipeline } from "./generated/models";
-import {
-  MethodRequest
-} from "./generated/models/mappers";
+import { MethodRequest } from "./generated/models/mappers";
 
 export interface Request<T> {
   methodName: string;
-  payload: T & {"@apiVersion": string;};
+  payload: T & { "@apiVersion": string };
 }
 
 interface NameObject {
@@ -27,19 +25,37 @@ export type RequestType =
 
 const apiVersion = MethodRequest.type.modelProperties!.apiVersion.defaultValue;
 
-export function createRequest(request: "pipelineTopologySet", payload: PipelineTopology): Request <PipelineTopology>;
-export function createRequest(request: "pipelineTopologyGet", payload: string): Request <NameObject>;
+export function createRequest(
+  request: "pipelineTopologySet",
+  payload: PipelineTopology
+): Request<PipelineTopology>;
+export function createRequest(request: "pipelineTopologyGet", payload: string): Request<NameObject>;
 export function createRequest(request: "pipelineTopologyList"): Request<{}>;
-export function createRequest(request: "pipelineTopologyDelete", payload: string): Request<NameObject>;
-export function createRequest(request: "livePipelineSet", payload: LivePipeline): Request<LivePipeline>;
+export function createRequest(
+  request: "pipelineTopologyDelete",
+  payload: string
+): Request<NameObject>;
+export function createRequest(
+  request: "livePipelineSet",
+  payload: LivePipeline
+): Request<LivePipeline>;
 export function createRequest(request: "livePipelineGet", payload: string): Request<NameObject>;
 export function createRequest(request: "livePipelineList"): Request<{}>;
 export function createRequest(request: "livePipelineDelete", payload: string): Request<NameObject>;
-export function createRequest(request: "livePipelineActivate", payload: string): Request<NameObject>;
-export function createRequest(request: "livePipelineDeactivate", payload: string): Request<NameObject>;
+export function createRequest(
+  request: "livePipelineActivate",
+  payload: string
+): Request<NameObject>;
+export function createRequest(
+  request: "livePipelineDeactivate",
+  payload: string
+): Request<NameObject>;
 export function createRequest<T extends object>(request: RequestType, payload?: any): Request<T> {
   return {
     methodName: request,
-    payload: {...(typeof payload === "string" ? { name: payload } : payload ?? {}), "@apiVersion": apiVersion}
+    payload: {
+      ...(typeof payload === "string" ? { name: payload } : payload ?? {}),
+      "@apiVersion": apiVersion
+    }
   };
 }
