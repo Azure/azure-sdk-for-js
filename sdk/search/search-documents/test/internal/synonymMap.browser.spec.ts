345c20463d963fdbbce2ae008af394de5b36f35f
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-05-05 13:34:22 -0700

    Response to PR Comments II

--- sdk/search/search-documents/test/internal/synonymMap.browser.spec.ts
+++ sdk/search/search-documents/test/internal/synonymMap.browser.spec.ts
@@ -2,9 +2,16 @@
 // Licensed under the MIT license.
 
 import { assert } from "chai";
+import { createSynonymMapFromFile } from "../../src/synonymMapHelper.browser";
 
 describe("synonymmap", () => {
-  it.skip("create synonymmap from file", async function() {
-    assert(true);
+  it("create synonymmap from file", async function() {
+    let errorThrown = false;
+    try {
+      await createSynonymMapFromFile("my-synonym-map-1", "./test/internal/synonymMap.txt");
+    } catch (ex) {
+      errorThrown = true;
+    }
+    assert.isTrue(errorThrown, "Expected createSynonymMapFromFile to fail with an exception");
   });
 });
