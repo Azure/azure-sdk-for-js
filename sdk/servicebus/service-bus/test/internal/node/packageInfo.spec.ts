4878a862a4644bbf49445f8b7b59f5377590fe28
Richard Park <ripark@microsoft.com>
2021-05-06 15:38:59 -0700

    Comment on the reason we support two different paths to the same file in our version check.

--- sdk/servicebus/service-bus/test/internal/node/packageInfo.spec.ts
+++ sdk/servicebus/service-bus/test/internal/node/packageInfo.spec.ts
@@ -11,6 +11,8 @@ import { packageJsonInfo } from "../../../src/util/constants";
 // following test is in place to ensure the values in package.json and in this file are consistent
 describe("Ensure package name and version are consistent in SDK and package.json", function(): void {
   it("Ensure constants.ts file is consistent with package.json", () => {
+    // if you use ts-node then the folder structure is not as deep. There's only ever one of these files
+    // so we'll just check both spots and make it easy to swap back and forth between dist and ts-node.
     const packageJsonFilePath = ["../../../../package.json", "../../../package.json"]
       .map((pj) => path.join(__dirname, pj))
       .filter((pj) => fs.existsSync(pj))[0];
