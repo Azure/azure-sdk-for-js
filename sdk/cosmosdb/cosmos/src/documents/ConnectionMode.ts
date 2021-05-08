2764acff7b527c2ef80f257c5e5e12fd1e2949e7
Kevin <skiimilk@gmail.com>
2021-05-08 09:44:52 +0800

    Fix spelling errors in ConnectionMode.ts

--- sdk/cosmosdb/cosmos/src/documents/ConnectionMode.ts
+++ sdk/cosmosdb/cosmos/src/documents/ConnectionMode.ts
@@ -2,6 +2,6 @@
 // Licensed under the MIT license.
 /** Determines the connection behavior of the CosmosClient. Note, we currently only support Gateway Mode. */
 export enum ConnectionMode {
-  /** Gateway mode talks to a intermediate gateway which handles the direct communicationi with your individual partitions. */
+  /** Gateway mode talks to an intermediate gateway which handles the direct communication with your individual partitions. */
   Gateway = 0
 }
