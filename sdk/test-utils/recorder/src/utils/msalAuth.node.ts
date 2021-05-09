c51a2c56d36ada13ad59342da57a557099396f20
HarshaNalluru <sanallur@microsoft.com>
2021-05-09 02:02:28 -0700

    persist for keeping the request open to allow mocking multiple times

--- sdk/test-utils/recorder/src/utils/msalAuth.node.ts
+++ sdk/test-utils/recorder/src/utils/msalAuth.node.ts
@@ -7,6 +7,7 @@ export type NockType = typeof import("nock");
 export function mockMsalAuth(nock: NockType) {
   if (env.AZURE_TENANT_ID) {
     nock("https://login.microsoftonline.com:443")
+      .persist()
       .post(`/${env.AZURE_TENANT_ID}/oauth2/v2.0/token`)
       .reply(200, {
         token_type: "Bearer",
