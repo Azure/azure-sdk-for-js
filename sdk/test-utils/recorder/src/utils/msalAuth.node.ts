2b350781751fbdbdbe3c37fcf3c2e9e446ba4b92
HarshaNalluru <sanallur@microsoft.com>
2021-05-09 04:17:33 -0700

    Attempt fixing idenitty tests, update path to a function, add id_token and client_info from msal tests

--- sdk/test-utils/recorder/src/utils/msalAuth.node.ts
+++ sdk/test-utils/recorder/src/utils/msalAuth.node.ts
@@ -1,19 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { env } from ".";
 
 export type NockType = typeof import("nock");
 
 export function mockMsalAuth(nock: NockType) {
-  if (env.AZURE_TENANT_ID) {
-    nock("https://login.microsoftonline.com:443")
-      .persist()
-      .post(`/${env.AZURE_TENANT_ID}/oauth2/v2.0/token`)
-      .reply(200, {
-        token_type: "Bearer",
-        expires_in: 86399,
-        ext_expires_in: 86399,
-        access_token: "access_token"
-      });
-  }
+  nock("https://login.microsoftonline.com:443")
+    .persist()
+    .post((uri) => uri.includes("/oauth2/v2.0/token")) // Path can either be "{tenant-id}/oauth2/v2.0/token" or "/organizations/oauth2/v2.0/token"
+    .reply(200, {
+      token_type: "Bearer",
+      expires_in: 86399,
+      ext_expires_in: 86399,
+      access_token: "access_token",
+      refresh_token: "refresh_token",
+      id_token:
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjFMVE16YWtpaGlSbGFfOHoyQkVKVlhlV01xbyJ9.eyJ2ZXIiOiIyLjAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vOTE4ODA0MGQtNmM2Ny00YzViLWIxMTItMzZhMzA0YjY2ZGFkL3YyLjAiLCJzdWIiOiJBQUFBQUFBQUFBQUFBQUFBQUFBQUFJa3pxRlZyU2FTYUZIeTc4MmJidGFRIiwiYXVkIjoiNmNiMDQwMTgtYTNmNS00NmE3LWI5OTUtOTQwYzc4ZjVhZWYzIiwiZXhwIjoxNTM2MzYxNDExLCJpYXQiOjE1MzYyNzQ3MTEsIm5iZiI6MTUzNjI3NDcxMSwibmFtZSI6IkFiZSBMaW5jb2xuIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiQWJlTGlAbWljcm9zb2Z0LmNvbSIsIm9pZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC02NmYzLTMzMzJlY2E3ZWE4MSIsInRpZCI6IjMzMzgwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZCIsIm5vbmNlIjoiMTIzNTIzIiwiYWlvIjoiRGYyVVZYTDFpeCFsTUNXTVNPSkJjRmF0emNHZnZGR2hqS3Y4cTVnMHg3MzJkUjVNQjVCaXN2R1FPN1lXQnlqZDhpUURMcSFlR2JJRGFreXA1bW5PcmNkcUhlWVNubHRlcFFtUnA2QUlaOGpZIn0=.1AFWW-Ck5nROwSlltm7GzZvDwUkqvhSQpm55TQsmVo9Y59cLhRXpvB8n-55HCr9Z6G_31_UbeUkoz612I2j_Sm9FFShSDDjoaLQr54CreGIJvjtmS3EkK9a7SJBbcpL1MpUtlfygow39tFjY7EVNW9plWUvRrTgVk7lYLprvfzw-CIqw3gHC-T7IK_m_xkr08INERBtaecwhTeN4chPC4W3jdmw_lIxzC48YoQ0dB1L9-ImX98Egypfrlbm0IBL5spFzL6JDZIRRJOu8vecJvj1mq-IUhGt0MacxX8jdxYLP-KUu2d9MbNKpCKJuZ7p8gwTL5B7NlUdh_dmSviPWrw",
+      client_info: "eyJ1aWQiOiIxMjMtdGVzdC11aWQiLCJ1dGlkIjoiNDU2LXRlc3QtdXRpZCJ9"
+    });
+  // Above id_token and client_info are straight from the msal tests
+  // Reference - https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/926f1c2ba0598575e23dfd8cdd8b79fa3a3d19ff/lib/msal-browser/test/utils/BrowserProtocolUtils.spec.ts#L73
 }
