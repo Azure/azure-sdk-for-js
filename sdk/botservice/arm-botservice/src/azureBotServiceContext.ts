3b9bf7986408618f174b351b6a9353755a6e44d4
Hai Liu <hailiu@microsoft.com>
2021-05-10 16:21:47 -0700

    update botservice api version to 2021-03-01

--- sdk/botservice/arm-botservice/src/azureBotServiceContext.ts
+++ sdk/botservice/arm-botservice/src/azureBotServiceContext.ts
@@ -44,7 +44,7 @@ export class AzureBotServiceContext extends msRestAzure.AzureServiceClient {
 
     super(credentials, options);
 
-    this.apiVersion = '2020-06-02';
+    this.apiVersion = '2021-03-01';
     this.acceptLanguage = 'en-US';
     this.longRunningOperationRetryTimeout = 30;
     this.baseUri = options.baseUri || this.baseUri || "https://management.azure.com";
