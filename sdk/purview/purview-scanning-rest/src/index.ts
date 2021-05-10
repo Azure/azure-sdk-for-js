ddd7fa8eea847f73e94479abfd7a0858ddf77e2e
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-05-10 15:27:46 -0700

    Purview Scanning Rest Client (#14918)
    * Initial commit for PurviewScanning
    
    * Add samples
    
    * Add sanity test
    
    * Fix broken links
    
    * Fix links
    
    * remove broken link
    
    * Fix endpoint replacement
    
    * fix url
    
    * delete unused recording
    
    * Re-generate latest swagger
    
    * Add purview ci
    
    * Re-generate purview scanning
    
    * update readme
    
    * Fix broken links
    
    * Update samples
    
    * update test and broken link
    
    * Update Karma timeout
    
    * Address feedback
    
    * update pnpm and rebase
    
    * update package json

---
+++ sdk/purview/purview-scanning-rest/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import PurviewScanning from "./purviewScanning";
+
+export * from "./purviewScanning";
+export * from "./models";
+export * from "./parameters";
+export * from "./responses";
+
+export default PurviewScanning;
