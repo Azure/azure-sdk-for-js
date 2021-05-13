1343c8af7a443c5fdcbcbb0749b329349f5d3103
Emma Zhu <emmazhu@microsoft.com>
2021-05-13 17:36:31 +0800

    Support version 2020-08-04 in storage-blob package.

--- sdk/storage/storage-blob/src/generated/src/storageClientContext.ts
+++ sdk/storage/storage-blob/src/generated/src/storageClientContext.ts
@@ -47,6 +47,6 @@ export class StorageClientContext extends coreHttp.ServiceClient {
     this.url = url;
 
     // Assigning values to Constant parameters
-    this.version = options.version || "2020-06-12";
+    this.version = options.version || "2020-08-04";
   }
 }
