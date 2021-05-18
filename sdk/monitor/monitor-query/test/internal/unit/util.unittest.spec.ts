a303658cab656f2454281a148718239b7ecd300a
richardpark-msft <ripark@microsoft.com>
2021-05-18 15:32:17 -0700

    Fixing formatting (and thus fixing lint breakages!)

--- sdk/monitor/monitor-query/test/internal/unit/util.unittest.spec.ts
+++ sdk/monitor/monitor-query/test/internal/unit/util.unittest.spec.ts
@@ -5,19 +5,22 @@ import { assert } from "chai";
 import { formatPreferHeader } from "../../../src/internal/util";
 
 describe("utils unit tests", () => {
-    type PreferHeadersArg = Parameters<typeof formatPreferHeader>[0];
+  type PreferHeadersArg = Parameters<typeof formatPreferHeader>[0];
 
-    const headers: [PreferHeadersArg, ReturnType<typeof formatPreferHeader>][] = [
-        [undefined, undefined],
-        [{ includeQueryStatistics: true }, { Prefer: "include-statistics=true" }],
-        [{ serverTimeoutInSeconds: 7 }, { Prefer: "wait=7" }],
-        [{ serverTimeoutInSeconds: 7, includeQueryStatistics: true }, { Prefer: "wait=7,include-statistics=true" }],
-    ];
-    
-    headers.forEach(([options, expectedResult]) => {
-        it(`formatPreferHeader(${JSON.stringify(headers)})`, () => {
-            const actualResult = formatPreferHeader(options);
-            assert.deepEqual(actualResult, expectedResult);
-        });
+  const headers: [PreferHeadersArg, ReturnType<typeof formatPreferHeader>][] = [
+    [undefined, undefined],
+    [{ includeQueryStatistics: true }, { Prefer: "include-statistics=true" }],
+    [{ serverTimeoutInSeconds: 7 }, { Prefer: "wait=7" }],
+    [
+      { serverTimeoutInSeconds: 7, includeQueryStatistics: true },
+      { Prefer: "wait=7,include-statistics=true" }
+    ]
+  ];
+
+  headers.forEach(([options, expectedResult]) => {
+    it(`formatPreferHeader(${JSON.stringify(headers)})`, () => {
+      const actualResult = formatPreferHeader(options);
+      assert.deepEqual(actualResult, expectedResult);
     });
+  });
 });
