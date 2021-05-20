339efec5d21cd859e7afba19596c8ae43b601e31
0rlanand0Wats0n <orwatson@microsoft.com>
2021-05-20 08:13:40 -0700

    .

--- sdk/communication/communication-sms/samples/v1/typescript/src/usingAadAuth.ts
+++ sdk/communication/communication-sms/samples/v1/typescript/src/usingAadAuth.ts
@@ -19,9 +19,9 @@ export async function main() {
 
   // You will need to set this environment variable or edit the following values
   const endpoint =
-    process.env.COMMUNICATION_ENDPOINT 
-    || parseConnectionString(process.env.COMMUNICATION_SAMPLES_CONNECTION_STRING!).endpoint
-    || "https://<resource-name>.communication.azure.com";
+    process.env.COMMUNICATION_ENDPOINT ||
+    parseConnectionString(process.env.COMMUNICATION_SAMPLES_CONNECTION_STRING!).endpoint ||
+    "https://<resource-name>.communication.azure.com";
 
   // Azure AD Credential information is required to run this sample:
   if (
