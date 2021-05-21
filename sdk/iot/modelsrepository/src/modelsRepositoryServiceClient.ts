e86368cb2bc7e3915c1c7f982abea2338798d31f
Yoseph Maguire <yoseph.maguire@microsoft.com>
2021-05-21 12:46:22 -0700

    Create Device Models Repository Client (#14863)
    Create new package for Device Models Repository, see README for details.

---
+++ sdk/iot/modelsrepository/src/modelsRepositoryServiceClient.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft.
+// Licensed under the MIT license.
+
+import { ServiceClientOptions, ServiceClient } from "@azure/core-client";
+import { DEFAULT_API_VERSION } from "./constants";
+
+interface IoTModelsRepositoryServiceClientOptions extends ServiceClientOptions {
+  // API Version to be used during HTTP Calls.
+  version?: string;
+  // Endpoint that will be base of URLs for HTTP calls.
+  endpoint?: string;
+}
+
+/**
+ * @internal
+ */
+export class IoTModelsRepositoryServiceClient extends ServiceClient {
+  url: string;
+  version: string;
+
+  /**
+   * Initializes a new instance of the IoTModelsRepositoryServiceClient class.
+   * @param url The URL of the service account or table that is the target of the desired operation.
+   * @param options The parameter options
+   */
+  constructor(url: string, options: IoTModelsRepositoryServiceClientOptions = {}) {
+    const defaults: IoTModelsRepositoryServiceClientOptions = {
+      baseUri: `${url}`,
+      requestContentType: "application/json; charset=utf-8"
+    };
+
+    const optionsWithDefaults = {
+      ...defaults,
+      ...options
+    };
+
+    super(optionsWithDefaults);
+
+    this.url = url;
+    this.version = options.version || DEFAULT_API_VERSION;
+  }
+}
