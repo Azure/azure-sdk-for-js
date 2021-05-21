80ac35fe2085a2136a6e0c9fd014d942374037d0
Ari Zavala <arzavala@microsoft.com>
2021-05-21 14:34:55 -0700

    Update of PR

--- sdk/communication/communication-network-traversal/src/models.ts
+++ sdk/communication/communication-network-traversal/src/models.ts
@@ -3,23 +3,11 @@
 
 // delete this pipeline options
 import { PipelineOptions } from "@azure/core-http";
-import { CommunicationTurnServer } from "../src/generated/src/models";
+import { CommunicationTurnCredentialsResponse } from "../src/generated/src/models";
+
+export type CommunicationRelayConfiguration = CommunicationTurnCredentialsResponse;
 
 /**
  * Client options used to configure the CommunicationNetworkTraversal API requests.
  */
 export interface CommunicationRelayClientOptions extends PipelineOptions {}
-
-/**
- * A TURN credentials response.
- */
-export interface CommunicationRelayConfiguration {
-  /**
-   * The date for which the username and credentials are not longer valid.
-   */
-  expiresOn: Date;
-  /**
-   * An array representing the credentials and the TURN server URL.
-   */
-  turnServers: CommunicationTurnServer[];
-}
