3437c8e67fc5d07445843a0fa30209f31f4a1980
hivyas <61890270+hivyas@users.noreply.github.com>
2021-05-20 17:49:42 -0700

    [Media] LVA SDK addition (#14004)
    * initial commit for tutorial
    
    * saving progress
    
    * initial commit
    
    * saving progress
    
    * manually replaced type in index.ts
    
    * finalized customizations
    
    * deleting tutorial files
    
    * removing strings
    
    * changes based off PR comments
    
    * renaming sample file
    
    * added js sample
    
    * update package dependencies
    
    * resolved dependency issues
    
    * removing client export
    
    * updating pnpm-lock file
    
    * resolving karma dependency
    
    * fixing import statement
    
    * temp connection string
    
    * adding azure-iothub dev dependency
    
    * adding azure-iothub to devDependency:
    
    * saving progress
    
    * saving progress before merge
    
    * regenerated and renamed to pipeline terminology
    
    * saving progress
    
    * updated test
    
    * updated sample
    
    * updating dependencies
    
    * moving library to videoanalyzer
    
    * removing old library
    
    * generated new javascript sample
    
    * linking samples
    
    * updating correct README
    
    * updating readme in javascript samples
    
    * saving progress
    
    * added createRequest
    
    * updated swagger
    
    * saving progresS
    
    * removing asset sink
    
    * change var name
    
    * updating api extractor
    
    * updating readme
    
    * updating readme
    
    * fixing links
    
    * dropping media from name
    
    * removing old terminology
    
    * saving progress
    
    * addressing pr comments
    
    * addressing PR comments
    
    * ran rush updatE
    
    * addressing pr changes
    
    * pr comments
    
    * adding ci file
    
    * Update sdk/videoanalyzer/video-analyzer-edge/README.md
    
    Co-authored-by: Jeremy Meng <yumeng@microsoft.com>
    
    * fixing lint issues
    
    * fixing formatting
    
    Co-authored-by: Jeremy Meng <yumeng@microsoft.com>

---
+++ sdk/videoanalyzer/video-analyzer-edge/test/payloadCreation.spec.ts
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import {
+  PipelineTopology,
+  RtspSource,
+  UnsecuredEndpoint,
+  NodeInput,
+  IotHubMessageSink,
+  createRequest
+} from "../src";
+
+describe("test", () => {
+  it("creates a pipeline topology and calls createRequest to ensure apiVersion is added", () => {
+    const rtspSource: RtspSource = {
+      name: "rtspSource",
+      endpoint: {
+        url: "${rtspUrl}",
+        "@type": "#Microsoft.VideoAnalyzer.UnsecuredEndpoint",
+        credentials: {
+          username: "${rtspUserName}",
+          password: "${rtspPassword}",
+          "@type": "#Microsoft.VideoAnalyzer.UsernamePasswordCredentials"
+        }
+      } as UnsecuredEndpoint,
+      "@type": "#Microsoft.VideoAnalyzer.RtspSource"
+    };
+
+    const nodeInput: NodeInput = {
+      nodeName: "rtspSource"
+    };
+
+    const msgSink: IotHubMessageSink = {
+      name: "msgSink",
+      inputs: [nodeInput],
+      hubOutputName: "${hubSinkOutputName}",
+      "@type": "#Microsoft.VideoAnalyzer.IotHubMessageSink"
+    };
+
+    const pipelineTopology: PipelineTopology = {
+      name: "jsTestTopology",
+      properties: {
+        description: "description for jsTestTopology",
+        parameters: [
+          { name: "rtspUserName", type: "String", default: "dummyUsername" },
+          { name: "rtspPassword", type: "SecretString", default: "dummyPassword" },
+          { name: "rtspUrl", type: "String" },
+          { name: "hubSinkOutputName", type: "String" }
+        ],
+        sources: [rtspSource],
+        sinks: [msgSink]
+      }
+    };
+
+    const pipelineTopologySetRequest = createRequest("pipelineTopologySet", pipelineTopology);
+    assert.strictEqual(pipelineTopologySetRequest.payload["@apiVersion"], "1.0");
+  });
+});
