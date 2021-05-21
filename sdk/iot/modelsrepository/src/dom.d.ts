a80e4a1dbc803da4e7309a43fb501bf6d9a6df2c
Yoseph Maguire <yoseph.maguire@gmail.com>
2021-05-21 11:44:13 -0700

    add comments for dom

--- sdk/iot/modelsrepository/src/dom.d.ts
+++ sdk/iot/modelsrepository/src/dom.d.ts
@@ -1,4 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+// This file avoids adding dom to the tsconfig.
+// We still need to reference the dom for ts compiliation, since
+// in the browser url shim it references the URL dom.
 /// <reference lib="dom" />
