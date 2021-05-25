627117399c961716e613cb7e1bc811a051316628
colawwj <v-sigangzhou@microsoft.com>
2021-05-25 16:18:22 +0800

    version-update

--- sdk/storage/arm-storage/src/storageManagementClientContext.ts
+++ sdk/storage/arm-storage/src/storageManagementClientContext.ts
@@ -12,7 +12,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-storage";
-const packageVersion = "16.1.0";
+const packageVersion = "17.0.0";
 
 export class StorageManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
