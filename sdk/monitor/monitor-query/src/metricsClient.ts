3fc41dc88b0909ec4b0f8adc4664a854d44f0dd9
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2021-05-28 11:19:40 -0700

    [monitor-query] Create monitor query track 2 package (#15112)
    * The initial version of the @azure/monitor-query package.
    
    This package lets you query the Azure Monitor query APIs for
    logs and metrics.

---
+++ sdk/monitor/monitor-query/src/metricsClient.ts
@@ -0,0 +1,120 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import { TokenCredential } from "@azure/core-auth";
+import {
+  bearerTokenAuthenticationPolicy,
+  createPipelineFromOptions,
+  PipelineOptions
+} from "@azure/core-http";
+
+import {
+  GetMetricDefinitionsOptions,
+  GetMetricDefinitionsResponse,
+  GetMetricNamespacesOptions,
+  GetMetricNamespacesResponse,
+  QueryMetricsOptions,
+  QueryMetricsResponse
+} from "./models/publicMetricsModels";
+
+import {
+  KnownApiVersion20170501Preview as MetricsApiVersion,
+  MonitorManagementClient as GeneratedMetricsClient
+} from "./generated/metrics/src";
+import {
+  KnownApiVersion20170501Preview as MetricDefinitionsApiVersion,
+  MetricsDefinitionsClient as GeneratedMetricsDefinitionsClient
+} from "./generated/metricsdefinitions/src";
+import {
+  KnownApiVersion20171201Preview as MetricNamespacesApiVersion,
+  MetricsNamespacesClient as GeneratedMetricsNamespacesClient
+} from "./generated/metricsnamespaces/src";
+import {
+  convertRequestForMetrics,
+  convertRequestOptionsForMetricsDefinitions,
+  convertResponseForMetricNamespaces,
+  convertResponseForMetrics,
+  convertResponseForMetricsDefinitions
+} from "./internal/modelConverters";
+
+export interface MetricsClientOptions extends PipelineOptions {
+  /** Overrides client endpoint. */
+  endpoint?: string;
+}
+
+export class MetricsClient {
+  private _metricsClient: GeneratedMetricsClient;
+  private _definitionsClient: GeneratedMetricsDefinitionsClient;
+  private _namespacesClient: GeneratedMetricsNamespacesClient;
+
+  constructor(tokenCredential: TokenCredential, options?: MetricsClientOptions) {
+    const bearerTokenPolicy = bearerTokenAuthenticationPolicy(
+      tokenCredential,
+      formatScope(options?.endpoint)
+    );
+
+    const serviceClientOptions = {
+      ...createPipelineFromOptions(options || {}, bearerTokenPolicy),
+      $host: options?.endpoint,
+      endpoint: options?.endpoint
+    };
+
+    this._metricsClient = new GeneratedMetricsClient(
+      MetricsApiVersion.TwoThousandSeventeen0501Preview,
+      serviceClientOptions
+    );
+
+    this._definitionsClient = new GeneratedMetricsDefinitionsClient(
+      MetricDefinitionsApiVersion.TwoThousandSeventeen0501Preview,
+      serviceClientOptions
+    );
+
+    this._namespacesClient = new GeneratedMetricsNamespacesClient(
+      MetricNamespacesApiVersion.TwoThousandSeventeen1201Preview,
+      serviceClientOptions
+    );
+  }
+
+  async queryMetrics(
+    resourceUri: string,
+    options?: QueryMetricsOptions
+  ): Promise<QueryMetricsResponse> {
+    const response = await this._metricsClient.metrics.list(
+      resourceUri,
+      convertRequestForMetrics(options)
+    );
+
+    return convertResponseForMetrics(response);
+  }
+
+  async getMetricDefinitions(
+    resourceUri: string,
+    options?: GetMetricDefinitionsOptions
+  ): Promise<GetMetricDefinitionsResponse> {
+    const response = await this._definitionsClient.metricDefinitions.list(
+      resourceUri,
+      convertRequestOptionsForMetricsDefinitions(options)
+    );
+
+    return convertResponseForMetricsDefinitions(response);
+  }
+
+  async getMetricNamespaces(
+    resourceUri: string,
+    options?: GetMetricNamespacesOptions
+  ): Promise<GetMetricNamespacesResponse> {
+    const response = await this._namespacesClient.metricNamespaces.list(resourceUri, options);
+    return convertResponseForMetricNamespaces(response);
+  }
+}
+
+function formatScope(endpoint: string | undefined): string {
+  if (endpoint) {
+    if (endpoint.endsWith("/")) {
+      endpoint += "/";
+    }
+
+    return `${endpoint}/.default`;
+  } else {
+    return "https://management.azure.com/.default";
+  }
+}
