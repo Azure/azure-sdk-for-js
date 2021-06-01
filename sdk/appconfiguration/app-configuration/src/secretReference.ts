d2a8505c5c0542d8df4ba6d4711c465a846be48c
HarshaNalluru <sanallur@microsoft.com>
2021-06-01 11:55:04 -0700

    remove the typeof check

--- sdk/appconfiguration/app-configuration/src/secretReference.ts
+++ sdk/appconfiguration/app-configuration/src/secretReference.ts
@@ -31,7 +31,7 @@ export const SecretReferenceHelper = {
   toConfigurationSettingParam: (
     secretReference: ConfigurationSettingParam<SecretReferenceValue>
   ): ConfigurationSettingParam => {
-    if (!secretReference.value || typeof secretReference.value.secretId !== "string") {
+    if (!secretReference.value) {
       throw new TypeError(`SecretReference has an unexpected value - ${secretReference.value}`);
     }
 
