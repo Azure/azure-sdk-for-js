be32035f409caed08ec4d44f24348fc80e3d6ab7
0rlanand0Wats0n <orwatson@microsoft.com>
2021-06-01 09:29:13 -0700

    update to identity v2

--- sdk/communication/communication-phone-numbers/recordings/node/phonenumbersclient__lro__update/recording_update_throws_when_phone_number_isnt_owned.js
+++ sdk/communication/communication-phone-numbers/recordings/node/phonenumbersclient__lro__update/recording_update_throws_when_phone_number_isnt_owned.js
@@ -4,22 +4,24 @@ module.exports.hash = "a029440b94d74f4bb37d8021fa55d003";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
-nock('https://endpoint', {"encodedQueryParams":true})
+nock('https://endpoint', {"encodedQueryParams":false})
   .patch('/phoneNumbers/%2B14155550100/capabilities', {"calling":"none","sms":"outbound"})
   .query(true)
   .reply(404, "", [
   'Request-Context',
   'appId=',
   'MS-CV',
-  'hco6GfFnwkSfsj+6NNbOAg.0',
+  'A1uTBbq3nk2rB2RHx2xxiQ.0',
   'api-supported-versions',
   '2021-03-07',
   'X-Processing-Time',
-  '440ms',
+  '277ms',
+  'X-Cache',
+  'CONFIG_NOCACHE',
   'X-Azure-Ref',
-  '024+RYAAAAAD+xy9k1TFlSaHfMTHC5rKrWVZSMzBFREdFMDMxNgA5ZmM3YjUxOS1hOGNjLTRmODktOTM1ZS1jOTE0OGFlMDllODE=',
+  '0blm2YAAAAAApYfcZTdxvQ4um59ELvNWHWVZSMzBFREdFMDMxNAA5ZmM3YjUxOS1hOGNjLTRmODktOTM1ZS1jOTE0OGFlMDllODE=',
   'Date',
-  'Tue, 04 May 2021 18:18:04 GMT',
+  'Tue, 01 Jun 2021 15:59:42 GMT',
   'Content-Length',
   '0'
 ]);
