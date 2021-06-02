81b399a416e96d7137be48cf8c0808aa35d9eca9
praveenkuttappan <prmarott@microsoft.com>
2021-06-02 13:57:13 -0400

    Handle all packages in script

--- eng/scripts/Copy-Artifacts.ps1
+++ eng/scripts/Copy-Artifacts.ps1
@@ -2,11 +2,17 @@ param (
   [Parameter(mandatory = $true)]
   $serviceDirectory,
   [Parameter(mandatory = $true)]
-  $artifactName,
+  $artifacts,
   [Parameter(mandatory = $true)]
   $targetPath
 )
 
-New-Item -Type Directory -Name $artifactName -Path $targetPath
-Copy-Item $serviceDirectory/**/$artifactName*.tgz $targetPath/$artifactName
-Copy-Item $serviceDirectory/**/browser/$artifactName*.zip $targetPath/$artifactName
+foreach ($artifact in $artifacts)
+{
+  Write-Host $artifact
+  $artifactName = $artifact.Name
+  Write-Host "Copying artifact for $artifactName"
+  New-Item -Type Directory -Name $artifactName -Path $targetPath
+  Copy-Item $serviceDirectory/**/$artifactName-*.tgz $targetPath/$artifactName
+  Copy-Item $serviceDirectory/**/browser/$artifactName-*.zip $targetPath/$artifactName
+}
