47b66e53c6ed17fcdeb9d5e5f318cdec47fa3417
praveenkuttappan <prmarott@microsoft.com>
2021-06-02 17:13:21 -0400

    Copy packages to artifact folder

--- eng/scripts/Copy-Artifacts.ps1
+++ eng/scripts/Copy-Artifacts.ps1
@@ -2,19 +2,11 @@ param (
   [Parameter(mandatory = $true)]
   $serviceDirectory,
   [Parameter(mandatory = $true)]
-  $artifactsJson,
+  $artifactName,
   [Parameter(mandatory = $true)]
   $targetPath
 )
 
-$artifacts = ConvertFrom-Json $artifactsJson
-Write-Host $artifacts
-
-foreach ($artifact in $artifacts)
-{
-  $artifactName = $artifact.Name
-  Write-Host "Copying artifact for $artifactName"
-  New-Item -Type Directory -Name $artifactName -Path $targetPath
-  Copy-Item $serviceDirectory/**/$artifactName-*.tgz $targetPath/$artifactName
-  Copy-Item $serviceDirectory/**/browser/$artifactName-*.zip $targetPath/$artifactName
-}
+New-Item -Type Directory -Name $artifactName -Path $targetPath
+Copy-Item $serviceDirectory/**/$artifactName-*.tgz $targetPath/$artifactName
+Copy-Item $serviceDirectory/**/browser/$artifactName-*.zip $targetPath/$artifactName
