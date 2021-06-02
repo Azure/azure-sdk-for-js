9b095e4d780d42d1b759d88b5cd8cdbb48f6bc12
Will Temple <witemple@microsoft.com>
2021-06-02 11:12:36 -0700

    Improved identity-persistence samples

--- sdk/identity/identity-persistence/samples-dev/basic.ts
+++ sdk/identity/identity-persistence/samples-dev/basic.ts
@@ -1,28 +1,41 @@
 // Copyright (c) Microsoft Corporation
 // Licensed under the MIT license
 
-import { useIdentityExtension, DeviceCodeCredential, AccessToken } from "@azure/identity";
+/**
+ * This sample shows how to add a persistent token cache to `@azure/identity`
+ * using the persistence extension. Once the persistence extension is added
+ * through `useIdentityExtension`, some credentials, such as
+ * `DeviceCodeCredential`, will be able to retrieve tokens from the cache rather
+ * than requesting new tokens from the Azure Active Directory token endpoint.
+ *
+ * @summary import and use the persistence extension
+ */
+
+import { useIdentityExtension, DeviceCodeCredential } from "@azure/identity";
 
 import persistence from "@azure/identity-persistence";
-
 useIdentityExtension(persistence);
 
 import dotenv from "dotenv";
 dotenv.config();
 
-async function logToken(getToken: () => Promise<AccessToken>) {
-  console.log((await getToken()).token);
-}
-
 async function main() {
-  const credential = new DeviceCodeCredential({ tokenCachePersistenceOptions: {} });
+  const credential = new DeviceCodeCredential();
 
-  const getToken = () => credential.getToken("https://witemple-appconfig.azconfig.io/.default");
+  // This is the scope we will use to get a token from the AAD token endpoint.
+  // By default, we'll use the Microsoft Graph scope as an example, but when
+  // you use the credential with an Azure SDK package, it will configure the
+  // scope for you automatically.
+  const scope = process.env.AAD_TEST_SCOPE ?? "https://graph.microsoft.com/.default";
 
-  console.log(1);
-  await logToken(getToken);
-  console.log(2);
-  await logToken(getToken);
+  // A little helper function to print an access_token
+  const logToken = async () => console.log(await (await credential.getToken(scope)).token);
+
+  // You should observe that the same token is printed twice. if not, then caching isn't working properly.
+  console.log("Calling getToken()", 1);
+  await logToken();
+  console.log("Calling getToken()", 2);
+  await logToken();
 }
 
 main().catch((error) => {
--- sdk/identity/identity-persistence/samples-dev/basic_async.ts
+++ sdk/identity/identity-persistence/samples-dev/basic_async.ts
@@ -4,31 +4,37 @@
 import {
   useIdentityExtension,
   DeviceCodeCredential,
-  AccessToken,
   IdentityExtensionModule
 } from "@azure/identity";
+
+async function getPersistence(): Promise<IdentityExtensionModule> {
+  return require("@azure/identity-persistence");
+}
 
 import dotenv from "dotenv";
 dotenv.config();
 
-async function logToken(getToken: () => Promise<AccessToken>) {
-  console.log((await getToken()).token);
-}
-
 async function main() {
-  await useIdentityExtension(
-    new Promise<IdentityExtensionModule>((resolve) =>
-      resolve(require("@azure/identity-persistence"))
-    )
-  );
+  // If your environment supports it (ES2020 or newer), you can use
+  // `import("@azure/identity-persistence")` instead.
+  await useIdentityExtension(getPersistence());
+
   const credential = new DeviceCodeCredential({ tokenCachePersistenceOptions: {} });
 
-  const getToken = () => credential.getToken("https://witemple-appconfig.azconfig.io/.default");
+  // This is the scope we will use to get a token from the AAD token endpoint.
+  // By default, we'll use the Microsoft Graph scope as an example, but when
+  // you use the credential with an Azure SDK package, it will configure the
+  // scope for you automatically.
+  const scope = process.env.AAD_TEST_SCOPE ?? "https://graph.microsoft.com/.default";
 
-  console.log(1);
-  await logToken(getToken);
-  console.log(2);
-  await logToken(getToken);
+  // A little helper function to print an access_token
+  const logToken = async () => console.log(await (await credential.getToken(scope)).token);
+
+  // You should observe that the same token is printed twice. if not, then caching isn't working properly.
+  console.log("Calling getToken()", 1);
+  await logToken();
+  console.log("Calling getToken()", 2);
+  await logToken();
 }
 
 main().catch((error) => {
