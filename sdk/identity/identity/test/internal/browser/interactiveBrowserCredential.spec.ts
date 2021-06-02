2cc79b82831ddb4a8bbb42ca2374cdc6972c646a
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2021-06-02 17:59:40 +0000

    placing this test on the browser set of tests

--- sdk/identity/identity/test/internal/browser/interactiveBrowserCredential.spec.ts
+++ sdk/identity/identity/test/internal/browser/interactiveBrowserCredential.spec.ts
@@ -7,12 +7,13 @@
 import Sinon from "sinon";
 import assert from "assert";
 import { Context } from "mocha";
+import * as msalBrowser from "@azure/msal-browser";
 import { env } from "@azure/test-utils-recorder";
 import { InteractiveBrowserCredential } from "../../../src";
 import { MsalTestCleanup, msalNodeTestSetup } from "../../msalTestUtils";
-import { interactiveBrowserMockable } from "../../../src/msal/nodeFlows/msalOpenBrowser";
 
 import { URL } from "url";
+import { delay } from "@azure/core-http";
 
 declare global {
   namespace NodeJS {
@@ -37,30 +38,42 @@ describe("InteractiveBrowserCredential (internal)", function() {
 
   const scope = "https://vault.azure.net/.default";
 
-  it("Throws an expected error if no browser is available", async function(this: Context) {
+  it.only("Passes properties correctly to MSAL", async function(this: Context) {
     // On Node 8, URL is not defined. We use URL on the msalOpenBrowser.ts file.
     if (process.version.startsWith("v8.")) {
       global.URL = URL;
     }
 
-    // The SinonStub type does not include this second parameter to throws().
-    const testErrorMessage = "No browsers available on this test.";
-    (sandbox.stub(interactiveBrowserMockable, "open") as any).throws("TestError", testErrorMessage);
+    const msalSpy = sandbox.spy(msalBrowser.PublicClientApplication.prototype, "loginRedirect");
 
     const credential = new InteractiveBrowserCredential({
       redirectUri: "http://localhost:8081",
       tenantId: env.AZURE_TENANT_ID,
-      clientId: env.AZURE_CLIENT_ID
+      clientId: env.AZURE_CLIENT_ID,
+      loginClaims: "login-claims",
+      loginDomainHint: "login-domain-hint",
+      loginExtraQueryParameters: { parameter: "value" },
+      loginOnRedirectNavigate: () => undefined,
+      loginRedirectStartPage: "",
+      loginNonce: "login-nonce",
+      loginState: "login-state",
+      loginStyle: "popup"
     });
 
-    let error: Error | undefined;
+    const timeout = 500;
     try {
-      await credential.getToken(scope);
+      credential.getToken(scope, {
+        requestOptions: {
+          timeout
+        }
+      });
+      await delay(timeout);
     } catch (e) {
-      error = e;
+      // Nothing to do here
     }
 
-    assert.equal(error?.name, "CredentialUnavailableError");
-    assert.equal(error?.message, `Could not open a browser window. Error: ${testErrorMessage}`);
+    // Now we check the spy
+
+    assert.equal(msalSpy.callCount, 1);
   });
 });
