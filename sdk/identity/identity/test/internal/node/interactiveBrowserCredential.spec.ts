766f0a657b9dac3b88463e0a76a8f237a5c68f39
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2021-06-02 16:57:52 +0000

    wip

--- sdk/identity/identity/test/internal/node/interactiveBrowserCredential.spec.ts
+++ sdk/identity/identity/test/internal/node/interactiveBrowserCredential.spec.ts
@@ -7,6 +7,7 @@
 import Sinon from "sinon";
 import assert from "assert";
 import { Context } from "mocha";
+import * as msalBrowser from "@azure/msal-browser";
 import { env } from "@azure/test-utils-recorder";
 import { InteractiveBrowserCredential } from "../../../src";
 import { MsalTestCleanup, msalNodeTestSetup } from "../../msalTestUtils";
@@ -22,22 +23,22 @@ declare global {
   }
 }
 
-describe("InteractiveBrowserCredential (internal)", function() {
+describe("InteractiveBrowserCredential (internal)", function () {
   let cleanup: MsalTestCleanup;
   let sandbox: Sinon.SinonSandbox;
 
-  beforeEach(function(this: Context) {
+  beforeEach(function (this: Context) {
     const setup = msalNodeTestSetup(this);
     sandbox = setup.sandbox;
     cleanup = setup.cleanup;
   });
-  afterEach(async function() {
+  afterEach(async function () {
     await cleanup();
   });
 
   const scope = "https://vault.azure.net/.default";
 
-  it("Throws an expected error if no browser is available", async function(this: Context) {
+  it("Throws an expected error if no browser is available", async function (this: Context) {
     // On Node 8, URL is not defined. We use URL on the msalOpenBrowser.ts file.
     if (process.version.startsWith("v8.")) {
       global.URL = URL;
@@ -62,5 +63,45 @@ describe("InteractiveBrowserCredential (internal)", function() {
 
     assert.equal(error?.name, "CredentialUnavailableError");
     assert.equal(error?.message, `Could not open a browser window. Error: ${testErrorMessage}`);
+  });
+
+  it.only("Passes properties correctly to MSAL", async function (this: Context) {
+    // On Node 8, URL is not defined. We use URL on the msalOpenBrowser.ts file.
+    if (process.version.startsWith("v8.")) {
+      global.URL = URL;
+    }
+
+    // The SinonStub type does not include this second parameter to throws().
+    const testErrorMessage = "No browsers available on this test.";
+    (sandbox.stub(interactiveBrowserMockable, "open") as any).throws("TestError", testErrorMessage);
+
+    const msalSpy = sandbox.spy(msalBrowser.PublicClientApplication.prototype, "loginRedirect");
+
+    const credential = new InteractiveBrowserCredential({
+      redirectUri: "http://localhost:8081",
+      tenantId: env.AZURE_TENANT_ID,
+      clientId: env.AZURE_CLIENT_ID,
+      loginClaims: "login-claims",
+      loginDomainHint: "login-domain-hint",
+      loginExtraQueryParameters: { parameter: "value" },
+      loginOnRedirectNavigate: () => undefined,
+      loginRedirectStartPage: "",
+      loginNonce: "login-nonce",
+      loginState: "login-state",
+    });
+
+    let error: Error | undefined;
+    try {
+      await credential.getToken(scope);
+    } catch (e) {
+      error = e;
+    }
+
+    assert.equal(error?.name, "CredentialUnavailableError");
+    assert.equal(error?.message, `Could not open a browser window. Error: ${testErrorMessage}`);
+
+    // Now we check the spy
+
+    assert.equal(msalSpy.callCount, 1);
   });
 });
