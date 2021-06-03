77bc49b19b143bbaf890d5243ced7db3edda1879
HarshaNalluru <sanallur@microsoft.com>
2021-06-03 14:21:08 -0700

    test

---
+++ sdk/test-utils/recorder-v2/test/test.spec.ts
@@ -0,0 +1,31 @@
+import { ServiceClient, WebResource } from "@azure/core-http";
+import { isLiveMode, isPlaybackMode, env } from "@azure/test-utils-recorder";
+import { RecordingHttpClient } from "../src";
+
+describe("Tests", () => {
+  it("Sample test", async function() {
+    const file = `file_path.json`;
+    env.TEST_MODE = "record";
+    const client = new ServiceClient(undefined, {
+      httpClient: !isLiveMode() ? new RecordingHttpClient(file, isPlaybackMode()) : undefined
+    });
+    const url = "https://randomuser.me/api/";
+    const webResource = new WebResource(
+      url,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      false, // streamResponseBody
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      true // keepAlive
+    );
+    const response = await client.sendRequest(webResource);
+    console.log(response.parsedBody);
+  });
+});
