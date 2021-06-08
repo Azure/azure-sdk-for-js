b41e07accddba07463bf273238e19c329dce75bd
Christopher Radek <chradek@microsoft.com>
2021-06-08 14:54:33 -0700

    [core-amqp] remove AsyncLock!

--- sdk/core/core-amqp/test/cbs.spec.ts
+++ sdk/core/core-amqp/test/cbs.spec.ts
@@ -3,7 +3,7 @@
 
 import { assert } from "chai";
 import { AbortController } from "@azure/abort-controller";
-import { CbsClient, defaultLock, TokenType } from "../src";
+import { CbsClient, defaultCancellableLock, TokenType } from "../src";
 import { createConnectionStub } from "./utils/createConnectionStub";
 import { Connection } from "rhea-promise";
 import { stub } from "sinon";
@@ -38,14 +38,14 @@ describe("CbsClient", function() {
 
       // Make the existing `init` invocation wait until the abortSignal
       // is aborted before acquiring it's lock.
-      await defaultLock.acquire(lock, () => {
+      await defaultCancellableLock.acquire(lock, () => {
         return new Promise<void>((resolve) => {
           setTimeout(() => {
             controller.abort();
             resolve();
           }, 0);
         });
-      });
+      }, {abortSignal: undefined, timeoutInMs: undefined});
 
       try {
         await cbsClient.init({ abortSignal: signal });
