9f2722fe8a99156b9e0a223b77ac97ccae3d39d6
Praveen Kuttappan <praveen.kuttappan@gmail.com>
2021-06-09 20:23:24 -0400

    Additional changes

--- eng/tools/ci-pipeline-optimizer/modify-config.js
+++ eng/tools/ci-pipeline-optimizer/modify-config.js
@@ -1,11 +1,5 @@
 let argv = require("yargs")
   .options({
-    "test-mode": {
-      type: "string",
-      describe:
-        "node test or browser test. value should be either node or browser",
-      demandOption: true
-    },
     "repo-root": {
       type: "string",
       default: "../../../",
@@ -19,31 +13,26 @@ const path = require("path");
 const packageUtils = require("eng-package-utils");
 
 async function main(argv) {
-  const artifactName = argv["test-mode"];
   const repoRoot = argv["repo-root"];
+  const configChangeExclusion = ['@azure/cosmos'];
+  const apiExtractCommand = "api-extractor run --local";
 
   const rushSpec = await packageUtils.getRushSpec(repoRoot);
   let count = 0;
+  // Replace API-Extractor command with dummy echo statement for all projects in rush
   for (const project of rushSpec.projects) {
-    const targetPackagePath = path.join(repoRoot, project.projectFolder);
-    const packageJsonLocation = path.join(targetPackagePath, "package.json");
-    const packageJsonContents = await packageUtils.readFileJson(packageJsonLocation);
+    if (project.packageName.includes("@azure/") && configChangeExclusion.indexOf(project.packageName) < 0) {
+      const targetPackagePath = path.join(repoRoot, project.projectFolder);
+      const packageJsonLocation = path.join(targetPackagePath, "package.json");
+      const packageJsonContents = await packageUtils.readFileJson(packageJsonLocation);
 
-    for (let [name, value] of Object.entries(packageJsonContents.scripts)) {
-      if (name === "extract-api") {
-        console.log(`${packageJsonContents.name} : ${name} : ${value}`);
-        value = `echo skipped`;
-        packageJsonContents.scripts[name] = value;
+      for (let [name, value] of Object.entries(packageJsonContents.scripts)) {
+        if (value.includes(apiExtractCommand)) {
+          packageJsonContents.scripts[name] = value.replace(apiExtractCommand, "echo skipped");;
+        }
       }
-
-      if (value.includes("api-extractor run --local")) {
-        console.log(`${packageJsonContents.name} : ${name} : ${value}`);
-        packageJsonContents.scripts[name] = value.replace("api-extractor run --local", "echo skipped");
-        console.log(`${packageJsonContents.name} : ${packageJsonContents.scripts[name]}`);
-      }
+      await packageUtils.writePackageJson(packageJsonLocation, packageJsonContents);
     }
-
-    await packageUtils.writePackageJson(packageJsonLocation, packageJsonContents);
   }
 }
 
