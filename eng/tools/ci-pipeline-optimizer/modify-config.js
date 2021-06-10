78da02a6fa87fe9800409f61f197e4e0c71fe16c
Praveen Kuttappan <praveen.kuttappan@gmail.com>
2021-06-10 10:15:41 -0400

    Skip build browser for node test

--- eng/tools/ci-pipeline-optimizer/modify-config.js
+++ eng/tools/ci-pipeline-optimizer/modify-config.js
@@ -2,8 +2,14 @@ let argv = require("yargs")
   .options({
     "repo-root": {
       type: "string",
-      default: "../../../",
-      describe: "root of the repository (e.g. ../../../)",
+      default: ".",
+      describe: "root of the repository (e.g. ../../)",
+      demandOption: true
+    },
+    "testType": {
+      type: "string",
+      default: "node",
+      describe: "mode of test. possible values: node or browser",
       demandOption: true
     }
   })
@@ -14,24 +20,27 @@ const packageUtils = require("eng-package-utils");
 
 async function main(argv) {
   const repoRoot = argv["repo-root"];
+  const testType = argv["testType"];
   const configChangeExclusion = ['@azure/cosmos'];
   const apiExtractCommand = "api-extractor run --local";
 
-  const rushSpec = await packageUtils.getRushSpec(repoRoot);
-  let count = 0;
-  // Replace API-Extractor command with dummy echo statement for all projects in rush
-  for (const project of rushSpec.projects) {
-    if (project.packageName.includes("@azure/") && configChangeExclusion.indexOf(project.packageName) < 0) {
-      const targetPackagePath = path.join(repoRoot, project.projectFolder);
-      const packageJsonLocation = path.join(targetPackagePath, "package.json");
-      const packageJsonContents = await packageUtils.readFileJson(packageJsonLocation);
+  if (testType === "node") {
+    const rushSpec = await packageUtils.getRushSpec(repoRoot);
+    let count = 0;
+    // Replace API-Extractor command with dummy echo statement for all projects in rush
+    for (const project of rushSpec.projects) {
+      if (project.packageName.includes("@azure/") && configChangeExclusion.indexOf(project.packageName) < 0) {
+        const targetPackagePath = path.join(repoRoot, project.projectFolder);
+        const packageJsonLocation = path.join(targetPackagePath, "package.json");
+        const packageJsonContents = await packageUtils.readFileJson(packageJsonLocation);
 
-      for (let [name, value] of Object.entries(packageJsonContents.scripts)) {
-        if (value.includes(apiExtractCommand)) {
-          packageJsonContents.scripts[name] = value.replace(apiExtractCommand, "echo skipped");;
+        for (let [name, value] of Object.entries(packageJsonContents.scripts)) {
+          if (name === "build:browser") {
+            packageJsonContents.scripts[name] = "echo skipped";
+          }
         }
+        await packageUtils.writePackageJson(packageJsonLocation, packageJsonContents);
       }
-      await packageUtils.writePackageJson(packageJsonLocation, packageJsonContents);
     }
   }
 }
