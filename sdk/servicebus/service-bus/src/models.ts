ca75c511b0d21c6035c27d6e064d2ee6699de1d7
richardpark-msft <ripark@microsoft.com>
2021-06-09 18:08:51 -0700

    format

--- sdk/servicebus/service-bus/src/models.ts
+++ sdk/servicebus/service-bus/src/models.ts
@@ -150,13 +150,13 @@ export interface GetMessageIteratorOptions extends OperationOptionsBase {}
  */
 export interface SubscribeOptions extends OperationOptionsBase {
   /**
-   * Indicates whether the message should be settled automatically based on the result from the 
-   * user provided `processMessage` callback. 
-   * 
-   * - If an error is thrown from the `processMessage` callback the message will be abandoned 
-   *   using `receiver.abandonMessage()`. Doing so will make the message available again from the 
+   * Indicates whether the message should be settled automatically based on the result from the
+   * user provided `processMessage` callback.
+   *
+   * - If an error is thrown from the `processMessage` callback the message will be abandoned
+   *   using `receiver.abandonMessage()`. Doing so will make the message available again from the
    *   queue/subscription and the delivery count will be incremented.
-   * - If NO error is thrown from `processMessage` the message will be completed 
+   * - If NO error is thrown from `processMessage` the message will be completed
    *   using `receiver.completeMessage()`. Doing so removes the message from the queue/subscription.
    *
    * This option is ignored if messages are received in the `receiveAndDelete` receive mode or if
