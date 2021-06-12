e565a9d70961823312a52faa2b73c3981469bdda
KarishmaGhiya <kaghiya@microsoft.com>
2021-06-12 01:54:48 +0000

    tenant id

--- sdk/template/template/test/public/configurationClient.spec.ts
+++ sdk/template/template/test/public/configurationClient.spec.ts
@@ -16,7 +16,7 @@ const replaceableVariables: Record<string, string> = {
   APPCONFIG_ENDPOINT: "https://myappconfig.azconfig.io",
   APPCONFIG_TEST_SETTING_KEY: "test-key",
   APPCONFIG_TEST_SETTING_EXPECTED_VALUE: "test-value",
-  AZURE_TENANT_ID: "azure_tenant_id",
+  AZURE_TENANT_ID: "12345678-1234-1234-1234-123456789012",
   AZURE_CLIENT_ID: "azure_client_id",
   AZURE_CLIENT_SECRET: "azure_client_secret"
 };
@@ -43,7 +43,7 @@ function createConfigurationClient(): ConfigurationClient {
 // You want to give the test suite a descriptive name. Here, I add [AAD] to
 // indicate that the tests are authenticating with the service using Azure
 // Active Directory.
-describe("[AAD] ConfigurationClient functional tests", function() {
+describe("[AAD] ConfigurationClient functional tests", function () {
   // Declare the client and recorder instances.  We will set them using the
   // beforeEach hook.
   let client: ConfigurationClient;
@@ -52,7 +52,7 @@ describe("[AAD] ConfigurationClient functional tests", function() {
   // NOTE: use of "function" and not ES6 arrow-style functions with the
   // beforeEach hook is IMPORTANT due to the use of `this` in the function
   // body.
-  beforeEach(function(this: Context) {
+  beforeEach(function (this: Context) {
     // The recorder has some convenience methods, and we need to store a
     // reference to it so that we can `stop()` the recorder later in the
     // `afterEach` hook.
@@ -82,7 +82,7 @@ describe("[AAD] ConfigurationClient functional tests", function() {
   });
 
   // After each test, we need to stop the recording.
-  afterEach(async function() {
+  afterEach(async function () {
     await recorder.stop();
   });
 
