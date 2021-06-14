b7816b2abcbf51c542e3a5e4c9de9ed1825c608e
Lu Chen <luc@microsoft.com>
2021-06-14 14:18:23 -0700

    update recordings

--- sdk/communication/communication-chat/recordings/node/chatthreadclient/recording_successfully_deletes_a_message.js
+++ sdk/communication/communication-chat/recordings/node/chatthreadclient/recording_successfully_deletes_a_message.js
@@ -1,25 +1,25 @@
 let nock = require('nock');
 
-module.exports.hash = "99dab1eee1f2015b9c781625ce8891cd";
+module.exports.hash = "1a814ddcbc4190c3adefc3fc8633ba57";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
 nock('https://endpoint', {"encodedQueryParams":true})
-  .delete('/chat/threads/19%3A-SU2gzDrzE720bWocJYfAkee6As1QmtdJW3bcNJWyEw1%40thread.v2/messages/1620757462303')
+  .delete('/chat/threads/19%3AoXeDm2YWQ-rjbfG0ilcCtPs5GBwZEeVt5ssFsYc0Ehc1%40thread.v2/messages/1623705316364')
   .query(true)
   .reply(204, "", [
   'MS-CV',
-  'Z1zgqLsMd0227eztOoQ88Q.0',
+  'IsmezrBOm0aR7XWAEYO3hw.0',
   'Strict-Transport-Security',
   'max-age=2592000',
   'api-supported-versions',
   '2020-09-21-preview2, 2020-11-01-preview3, 2021-01-27-preview4, 2021-03-01-preview5, 2021-03-07, 2021-04-05-preview6',
   'X-Processing-Time',
-  '392ms',
+  '458ms',
   'X-Cache',
   'CONFIG_NOCACHE',
   'X-Azure-Ref',
-  '02MuaYAAAAABFLRpKRe18Touj+mVa/VyGV1NURURHRTA4MTkANzA1NDM5NWUtY2UxZC00NTVlLWFlNWQtMjMzY2E4MzkwNTU0',
+  '05sbHYAAAAACyeSEmq3qzQJlSTIdK+Y3xUERYMzFFREdFMDIxNgA3MDU0Mzk1ZS1jZTFkLTQ1NWUtYWU1ZC0yMzNjYTgzOTA1NTQ=',
   'Date',
-  'Tue, 11 May 2021 18:24:24 GMT'
+  'Mon, 14 Jun 2021 21:15:18 GMT'
 ]);
