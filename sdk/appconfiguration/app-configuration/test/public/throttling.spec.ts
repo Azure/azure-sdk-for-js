6579a13de895966cf40bf98aa4a45683791e15e5
HarshaNalluru <sanallur@microsoft.com>
2021-06-15 14:37:48 -0700

    test

--- sdk/appconfiguration/app-configuration/test/public/throttling.spec.ts
+++ sdk/appconfiguration/app-configuration/test/public/throttling.spec.ts
@@ -23,28 +23,41 @@ describe("AppConfigurationClient", () => {
   describe.only("simple usages", () => {
     it("Add and query a setting without a label", async () => {
       const key = recorder.getUniqueName("noLabelTests");
-      const numberOfSettings = 2000;
-      const promises = [];
-      try {
-        for (let index = 0; index < numberOfSettings; index++) {
-          promises.push(
-            client.addConfigurationSetting(
-              {
-                key: key + " " + +index,
-                value: "added"
-              },
-              {
-                abortSignal: AbortController.timeout(10000)
-              }
-            )
-          );
+      const numberOfSettings = 200;
+      const times = 1000;
+      for (let time = 0; time < times; time++) {
+        const promises = [];
+        try {
+          for (let index = 0; index < numberOfSettings; index++) {
+            promises.push(
+              client.addConfigurationSetting(
+                {
+                  key: key + " " + +index,
+                  value: "added"
+                },
+                {
+                  abortSignal: AbortController.timeout(10000)
+                }
+              )
+            );
+          }
+          await Promise.all(promises);
+        } catch (error) {
+          console.log(error);
         }
-        await Promise.all(promises);
-      } catch (error) {
-        console.log(error);
       }
 
-      await client.deleteConfigurationSetting({ key });
+      await cleanupSampleValues([key], client);
     });
   });
 });
+
+async function cleanupSampleValues(keys: string[], client: AppConfigurationClient) {
+  const settingsIterator = client.listConfigurationSettings({
+    keyFilter: keys.join(",")
+  });
+
+  for await (const setting of settingsIterator) {
+    await client.deleteConfigurationSetting({ key: setting.key, label: setting.label });
+  }
+}
