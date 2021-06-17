96f200b04bd8b63acfe14c2b36d72adbd1d1daa0
Daniel Rodríguez <sadasant@users.noreply.github.com>
2021-06-17 13:19:31 -0400

    Update sdk/identity/identity/src/credentials/clientSecretCredentialOptions.ts
    Co-authored-by: Scott Schaab <sschaab@microsoft.com>

--- sdk/identity/identity/src/credentials/clientSecretCredentialOptions.ts
+++ sdk/identity/identity/src/credentials/clientSecretCredentialOptions.ts
@@ -10,7 +10,7 @@ export interface ClientSecretCredentialOptions extends TokenCredentialOptions {
   /**
    * Specifies a regional authority. Please refer to the {@link RegionalAuthority} type for the accepted values.
    * If {@link RegionalAuthority.AutoDiscoverRegion} is specified, we will try to discover the regional authority endpoint.
-   * If the property is not specified, uses a non-regional authority endpoint.
+   * If the property is not specified, the credential uses the global authority endpoint.
    */
   regionalAuthority?: string;
 }
