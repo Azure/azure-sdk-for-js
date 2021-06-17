a969a0427c61473389dcdebf2065768d4c608089
Will Temple <witemple@microsoft.com>
2021-06-17 12:46:23 -0700

    Switch persistence tests from 12 to 14.

--- sdk/identity/identity-cache-persistence/test/internal/node/setup.spec.ts
+++ sdk/identity/identity-cache-persistence/test/internal/node/setup.spec.ts
@@ -4,19 +4,20 @@
 // We need to set up the extension for the tests!
 
 import { useIdentityExtension } from "../../../../identity/src";
-import { cachePersistenceExtension as extension } from "../../../src";
 
 // The persistence tests have to run on the same version of Node that's used to
-// install dependencies, currently 12.
+// install dependencies, currently 14.
 // TODO: We need a better solution for this, but this will do for now.
-if (!process.versions.node.startsWith("12")) {
+if (!process.versions.node.startsWith("14")) {
   console.warn(
     "WARNING: skipping persistence tests on incompatible node version:",
     process.version
   );
-  console.warn("Persistence tests are only compatible with Node 12.");
+  console.warn("Persistence tests are only compatible with Node 14.");
   process.exit(0);
 }
+
+import { cachePersistenceExtension as extension } from "../../../src";
 
 before(function() {
   useIdentityExtension(extension);
