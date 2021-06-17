7a1111650fa7bd2775584e3719a1daf9fb235f71
HarshaNalluru <sanallur@microsoft.com>
2021-06-17 14:56:11 -0700

    keep only the storage test

--- sdk/test-utils/recorder-v2/test/test.spec.ts
+++ sdk/test-utils/recorder-v2/test/test.spec.ts
@@ -1,4 +1,3 @@
-import { ServiceClient, ServiceClientOptions, WebResource } from "@azure/core-http";
 import { isLiveMode, isPlaybackMode, env } from "@azure/test-utils-recorder";
 import { BlobServiceClient, StoragePipelineOptions } from "@azure/storage-blob";
 import { RecordingHttpClient } from "../src";
@@ -25,78 +24,4 @@ describe("Tests", () => {
     await client.createContainer("harshan-1043");
     await recorder.stop();
   });
-
-  it("Sample test", async function() {
-    const file = `file_path.json`;
-    // env.TEST_MODE = "record";
-    env.TEST_MODE = "playback";
-    // env.TEST_MODE = "playback";
-    const recorder = new RecordingHttpClient(file, isPlaybackMode());
-    const options: ServiceClientOptions = {};
-    if (!isLiveMode()) {
-      options.httpClient = recorder;
-    }
-
-    const client = new ServiceClient(undefined, options);
-    const url = "https://randomuser.me/api/";
-    const webResource = new WebResource(
-      url,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      false, // streamResponseBody
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      true, // keepAlive
-      true // decompress response
-    );
-    await client.sendRequest(webResource);
-    // console.log(response.parsedBody);
-    await recorder.stop();
-  });
-
-  // it("Sample test 2 ", async function() {
-  //   const file = `file_path_2.json`;
-  //   env.TEST_MODE = "record";
-  //   // env.TEST_MODE = "playback";
-  //   // env.TEST_MODE = "playback";
-  //   const recorder = new RecordingHttpClient(file, isPlaybackMode());
-  //   // const client = new ServiceClient(undefined, {
-  //   //   httpClient: !isLiveMode() ? recorder : undefined
-  //   // });
-  //   const url = "https://randomuser.me/api/";
-
-  //   const client = createDefaultHttpClient();
-  //   client.request = createPipelineRequest({
-  //     url,
-  //     streamResponseStatusCodes: new Set([200, 206])
-  //   });
-  //   const response = await client.sendRequest(this.request);
-  //   console.log(response.parsedBody);
-
-  //   const webResource = new WebResource(
-  //     url,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     false, // streamResponseBody
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     true, // keepAlive
-  //     true // decompress response
-  //   );
-  //   const response = await client.sendRequest(webResource);
-  //   console.log(response.parsedBody);
-  //   await recorder.stop();
-  // });
 });
