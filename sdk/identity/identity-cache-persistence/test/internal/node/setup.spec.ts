32240e1a7842173ef15fd08f02511d289d9cc90f
Will Temple <witemple@microsoft.com>
2021-06-16 17:23:24 -0700

    Disable persistence tests on node !== 12

--- sdk/identity/identity-cache-persistence/test/internal/node/setup.spec.ts
+++ sdk/identity/identity-cache-persistence/test/internal/node/setup.spec.ts
@@ -6,6 +6,18 @@
 import { useIdentityExtension } from "../../../../identity/src";
 import extension from "../../../src";
 
+// The persistence tests have to run on the same version of Node that's used to
+// install dependencies, currently 12.
+// TODO: We need a better solution for this, but this will do for now.
+if (!process.versions.node.startsWith("12")) {
+  console.warn(
+    "WARNING: skipping persistence tests on incompatible node version:",
+    process.version
+  );
+  console.warn("Persistence tests are only compatible with Node 12.");
+  process.exit(0);
+}
+
 before(function() {
   useIdentityExtension(extension);
 });
