bdff63d2f44f030218aae96ea3f067de30caef92
Maor Leger <maorleger@users.noreply.github.com>
2021-06-18 15:08:03 -0700

    [KeyVault] - Add networkAcls to MHSM ARM template (#15830)
    This PR makes three changes to our deployment template:
    
    - Add networkAcls to the Managed HSM properties
    - Remove the 60 second sleep after activation
    - Speed up challenge auth tests for KV Keys by using methods that don't require creating and deleting keys.
    
    The first is now needed, and without it deployment will fail.
    The second is no longer needed, since it looks like the az cli appropriately waits before coming back.
    Finally, the third is something I noticed - there's no reason to create and purge keys which takes a long time in order to test CAE.

--- sdk/keyvault/test-resources-post.ps1
+++ sdk/keyvault/test-resources-post.ps1
@@ -107,11 +107,9 @@ az keyvault security-domain download --hsm-name $hsmName --security-domain-file 
 
 Log "Security domain downloaded to '$sdPath'; Managed HSM is now active at '$hsmUrl'"
 
-# Force a sleep to wait for Managed HSM activation to propagate through Cosmos replication. Issue tracked in AzDo.
-Log "Sleeping for 60 seconds to allow activation to propagate..."
-Start-Sleep -Seconds 60
+$testApplicationOid = $DeploymentOutputs["CLIENT_OBJECT_ID"]
 
 Log "Creating additional required role assignments for resource access."
-New-AzKeyVaultRoleAssignment -HsmName $hsmName -RoleDefinitionName "Managed HSM Crypto Officer" -ObjectID $DeploymentOutputs["CLIENT_OBJECT_ID"]
-New-AzKeyVaultRoleAssignment -HsmName $hsmName -RoleDefinitionName "Managed HSM Crypto User" -ObjectID $DeploymentOutputs["CLIENT_OBJECT_ID"]
-Log "Done."
+New-AzKeyVaultRoleAssignment -HsmName $hsmName -RoleDefinitionName "Managed HSM Crypto Officer" -ObjectID $testApplicationOid
+New-AzKeyVaultRoleAssignment -HsmName $hsmName -RoleDefinitionName "Managed HSM Crypto User" -ObjectID $testApplicationOid
+Log "Role assignments created for '$testApplicationOid'"
