86231c22b1312d379db466037c196c85d93c4a5f
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-06-21 16:11:45 -0700

    initial commit

---
+++ sdk/core/core-client-lro-rest/src/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * A helper library which implements Autorest x-ms-pageable spec for pagination
+ *
+ * @packageDocumentation
+ */
+export { paginateResponse, PaginateOptions } from "./paginate";
