1a6555b60dd7d218883d361bb5d3a68246968f8a
Harsha Nalluru <sanallur@microsoft.com>
2021-06-24 22:28:41 +0000

    HttpMockFacade

--- sdk/core/core-http/test/policies/throttlingRetryPolicy.node.ts
+++ sdk/core/core-http/test/policies/throttlingRetryPolicy.node.ts
@@ -1,48 +1,60 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import nock from "nock";
-import { Constants, ServiceClient } from "../../src/coreHttp";
+import { getHttpMock, HttpMockFacade } from "../mockHttp";
+import * as sinon from "sinon";
+import { DefaultHttpClient } from "../../src/defaultHttpClient";
+import { CommonResponse } from "../../src/fetchHttpClient";
+import { Constants, ServiceClient, WebResource } from "../../src/coreHttp";
 import { AbortController } from "@azure/abort-controller";
 import { assert } from "chai";
 
 describe("Throttling retry policy", () => {
-  let client: ServiceClient;
-
-  beforeEach(function() {
-    if (!nock.isActive()) {
-      nock.activate();
-    }
-    nock("https://fakeservice.io:443")
-      .persist()
-      .put(/.*/g)
-      .reply(
-        Constants.HttpConstants.StatusCodes.TooManyRequests,
-        {
-          type: "https://fakeservice.io/errors/too-many-requests",
-          title: "Resource utilization has surpassed the assigned quota",
-          policy: "Total Requests",
-          status: Constants.HttpConstants.StatusCodes.TooManyRequests
-        },
-        ["Retry-After", "10000"]
-      );
-    client = new ServiceClient();
+  let httpMock: HttpMockFacade;
+  beforeEach(() => {
+    httpMock = getHttpMock();
+    httpMock.setup();
   });
+  afterEach(() => httpMock.teardown());
+  after(() => httpMock.teardown());
 
-  afterEach(async function() {
-    nock.restore();
-    nock.cleanAll();
-    nock.enableNetConnect();
-  });
+  function getMockedHttpClient(): DefaultHttpClient {
+    const httpClient = new DefaultHttpClient();
+    sinon.stub(httpClient, "fetch").callsFake(async (input, init) => {
+      const response = await httpMock.getFetch()!(input, init);
+      return (response as unknown) as CommonResponse;
+    });
 
+    return httpClient;
+  }
   it("Should not retry forever (honors the abort signal passed)", async () => {
+    httpMock.get("https://fakeservice.io/", {
+      status: Constants.HttpConstants.StatusCodes.TooManyRequests,
+      body: {
+        type: "https://fakeservice.io/errors/too-many-requests",
+        title: "Resource utilization has surpassed the assigned quota",
+        policy: "Total Requests",
+        status: Constants.HttpConstants.StatusCodes.TooManyRequests
+      },
+      headers: {
+        "Retry-After": "10000"
+      }
+    });
+
     let errorWasThrown = false;
     try {
-      await client.sendRequest({
-        url: "https://fakeservice.io/ABCD",
-        abortSignal: AbortController.timeout(100),
-        method: "PUT"
-      });
+      const client = new ServiceClient(undefined, { httpClient: getMockedHttpClient() });
+      const webResource = new WebResource(
+        "https://fakeservice.io",
+        "GET",
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        AbortController.timeout(100)
+      );
+      await client.sendRequest(webResource);
     } catch (error) {
       errorWasThrown = true;
       assert.equal((error as any).name, "AbortError", "Unexpected error thrown");
