3d93d4990ec6dbd4a3fd734272dfe8d163bdb87c
Ramya Achutha Rao <ramyar@microsoft.com>
2021-06-26 12:19:38 -0700

    Fix typing issue for fsStat returning unknown

--- sdk/storage/storage-file-datalake/src/utils/utils.node.ts
+++ sdk/storage/storage-file-datalake/src/utils/utils.node.ts
@@ -3,7 +3,6 @@
 
 import * as fs from "fs";
 import * as util from "util";
-import { isNode } from "@azure/core-http";
 
 /**
  * Reads a readable stream into buffer. Fill the buffer from offset to end.
@@ -110,6 +109,6 @@ export async function streamToBuffer2(
  *
  * Promisified version of fs.stat().
  */
-export const fsStat = util.promisify(isNode ? fs.stat : function stat() {});
+export const fsStat = util.promisify(fs.stat);
 
 export const fsCreateReadStream = fs.createReadStream;
