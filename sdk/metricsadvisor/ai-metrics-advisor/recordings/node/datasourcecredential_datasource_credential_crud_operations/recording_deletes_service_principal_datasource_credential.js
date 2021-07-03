be9b832e64e3bf530b8ee1673a07fcd731bbecac
Karishma Ghiya <kghiya8@gmail.com>
2021-07-02 19:59:29 -0700

    re-recorded datasource cred tests

--- sdk/metricsadvisor/ai-metrics-advisor/recordings/node/datasourcecredential_datasource_credential_crud_operations/recording_deletes_service_principal_datasource_credential.js
+++ sdk/metricsadvisor/ai-metrics-advisor/recordings/node/datasourcecredential_datasource_credential_crud_operations/recording_deletes_service_principal_datasource_credential.js
@@ -1,49 +1,45 @@
 let nock = require('nock');
 
-module.exports.hash = "9d0b55ff5fc32dddbf48ccce45e475b5";
+module.exports.hash = "d98981d9bf602515a1154516d3524da1";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .delete('/metricsadvisor/v1.0/credentials/3ee2df85-919c-4e6a-b0b4-dca40b2d7b10')
+  .delete('/metricsadvisor/v1.0/credentials/d1916ee9-be4b-453c-a9c3-38ccdad5672a')
   .reply(204, "", [
   'Content-Length',
   '0',
   'x-request-id',
-  '210fcf3a-7c7b-41a9-85e4-95389cceadc0',
+  '05e30b21-7a6d-4eee-bc9a-c9cff1984742',
   'x-envoy-upstream-service-time',
-  '245',
+  '334',
   'apim-request-id',
-  '210fcf3a-7c7b-41a9-85e4-95389cceadc0',
+  '05e30b21-7a6d-4eee-bc9a-c9cff1984742',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Fri, 04 Jun 2021 18:52:20 GMT',
-  'Connection',
-  'close'
+  'Sat, 03 Jul 2021 02:26:59 GMT'
 ]);
 
 nock('https://endpoint:443', {"encodedQueryParams":true})
-  .get('/metricsadvisor/v1.0/credentials/3ee2df85-919c-4e6a-b0b4-dca40b2d7b10')
+  .get('/metricsadvisor/v1.0/credentials/d1916ee9-be4b-453c-a9c3-38ccdad5672a')
   .reply(404, {"code":"404 NOT_FOUND","message":"credentialId is invalid."}, [
   'Content-Length',
   '61',
   'Content-Type',
   'application/json; charset=utf-8',
   'x-request-id',
-  '527e44eb-fb28-4a81-b722-51784d6ecf30',
+  '00a33d52-545d-4e61-9c9c-1e293108f142',
   'x-envoy-upstream-service-time',
-  '75',
+  '131',
   'apim-request-id',
-  '527e44eb-fb28-4a81-b722-51784d6ecf30',
+  '00a33d52-545d-4e61-9c9c-1e293108f142',
   'Strict-Transport-Security',
   'max-age=31536000; includeSubDomains; preload',
   'x-content-type-options',
   'nosniff',
   'Date',
-  'Fri, 04 Jun 2021 18:52:20 GMT',
-  'Connection',
-  'close'
+  'Sat, 03 Jul 2021 02:26:59 GMT'
 ]);
