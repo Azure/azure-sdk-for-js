c26b50e66a86bc6f1c4f0ef6baacfd1a48220111
colawwj <v-sigangzhou@microsoft.com>
2021-07-07 15:43:58 +0800

    identity support 50

--- sdk/policy/arm-policy/src/policyClient.ts
+++ sdk/policy/arm-policy/src/policyClient.ts
@@ -8,6 +8,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -23,12 +24,17 @@ class PolicyClient extends PolicyClientContext {
 
   /**
    * Initializes a new instance of the PolicyClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId The ID of the target subscription.
    * @param [options] The parameter options
    */
   constructor(
-    credentials: msRest.ServiceClientCredentials,
+    credentials: msRest.ServiceClientCredentials | TokenCredential,
     subscriptionId: string,
     options?: Models.PolicyClientOptions
   ) {
