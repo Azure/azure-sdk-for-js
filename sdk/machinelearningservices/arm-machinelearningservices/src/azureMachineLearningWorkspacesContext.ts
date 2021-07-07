478e70c6942dc05055f689cd0138879c1cc48120
colawwj <v-sigangzhou@microsoft.com>
2021-07-07 15:41:25 +0800

    identity support 30

--- sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspacesContext.ts
+++ sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspacesContext.ts
@@ -9,23 +9,29 @@
 
 import * as Models from "./models";
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-machinelearningservices";
-const packageVersion = "4.0.0";
+const packageVersion = "4.1.0";
 
 export class AzureMachineLearningWorkspacesContext extends msRestAzure.AzureServiceClient {
-  credentials: msRest.ServiceClientCredentials;
+  credentials: msRest.ServiceClientCredentials | TokenCredential;
   subscriptionId: string;
   apiVersion?: string;
 
   /**
    * Initializes a new instance of the AzureMachineLearningWorkspaces class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId Azure subscription identifier.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.AzureMachineLearningWorkspacesOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, subscriptionId: string, options?: Models.AzureMachineLearningWorkspacesOptions) {
     if (credentials == undefined) {
       throw new Error('\'credentials\' cannot be null.');
     }
