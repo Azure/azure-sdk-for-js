aa45d78e590cf756227f732b821174b472e12993
Zachary Foster <notzachfoster@gmail.com>
2021-07-07 19:49:52 -0400

    format

--- sdk/cosmosdb/cosmos/src/common/constants.ts
+++ sdk/cosmosdb/cosmos/src/common/constants.ts
@@ -220,7 +220,7 @@ export const Constants = {
 
   /**
    * @deprecated Use EffectivePartitionKeyConstants instead
-  */
+   */
   EffectiveParitionKeyConstants: {
     MinimumInclusiveEffectivePartitionKey: "",
     MaximumExclusiveEffectivePartitionKey: "FF"
