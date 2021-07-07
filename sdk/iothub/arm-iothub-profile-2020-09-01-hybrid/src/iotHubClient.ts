50e35b1a2a7fc34a7081f91b8c3ba76790a74d30
colawwj <v-sigangzhou@microsoft.com>
2021-07-07 15:34:18 +0800

    ten identity

--- sdk/iothub/arm-iothub-profile-2020-09-01-hybrid/src/iotHubClient.ts
+++ sdk/iothub/arm-iothub-profile-2020-09-01-hybrid/src/iotHubClient.ts
@@ -9,6 +9,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -25,11 +26,16 @@ class IotHubClient extends IotHubClientContext {
 
   /**
    * Initializes a new instance of the IotHubClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId The subscription identifier.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.IotHubClientOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, subscriptionId: string, options?: Models.IotHubClientOptions) {
     super(credentials, subscriptionId, options);
     this.operations = new operations.Operations(this);
     this.iotHubResource = new operations.IotHubResource(this);
--- sdk/iotspaces/arm-iotspaces/src/ioTSpacesClient.ts
+++ sdk/iotspaces/arm-iotspaces/src/ioTSpacesClient.ts
@@ -9,6 +9,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -22,11 +23,16 @@ class IoTSpacesClient extends IoTSpacesClientContext {
 
   /**
    * Initializes a new instance of the IoTSpacesClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId The subscription identifier.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.IoTSpacesClientOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, subscriptionId: string, options?: Models.IoTSpacesClientOptions) {
     super(credentials, subscriptionId, options);
     this.ioTSpaces = new operations.IoTSpaces(this);
     this.operations = new operations.Operations(this);
