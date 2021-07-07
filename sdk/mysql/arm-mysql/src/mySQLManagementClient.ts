70efa6ab18b153f1e748fe6d1c508378b71bc9e9
colawwj <v-sigangzhou@microsoft.com>
2021-07-07 15:42:47 +0800

    identity support 40

--- sdk/mysql/arm-mysql/src/mySQLManagementClient.ts
+++ sdk/mysql/arm-mysql/src/mySQLManagementClient.ts
@@ -9,6 +9,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -31,11 +32,16 @@ class MySQLManagementClient extends MySQLManagementClientContext {
 
   /**
    * Initializes a new instance of the MySQLManagementClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId The subscription ID that identifies an Azure subscription.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.MySQLManagementClientOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, subscriptionId: string, options?: Models.MySQLManagementClientOptions) {
     super(credentials, subscriptionId, options);
     this.servers = new operations.Servers(this);
     this.replicas = new operations.Replicas(this);
