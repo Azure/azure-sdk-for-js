43c5bc43ee49d053c55a38085dd286c588abeecb
colawwj <70128817+colawwj@users.noreply.github.com>
2021-07-08 09:07:31 +0800

    identity support 40 (#16255)

--- sdk/network/arm-network-profile-2019-03-01-hybrid/src/networkManagementClient.ts
+++ sdk/network/arm-network-profile-2019-03-01-hybrid/src/networkManagementClient.ts
@@ -9,6 +9,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -43,12 +44,17 @@ class NetworkManagementClient extends NetworkManagementClientContext {
 
   /**
    * Initializes a new instance of the NetworkManagementClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId The subscription credentials which uniquely identify the Microsoft Azure
    * subscription. The subscription ID forms part of the URI for every service call.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.NetworkManagementClientOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, subscriptionId: string, options?: Models.NetworkManagementClientOptions) {
     super(credentials, subscriptionId, options);
     this.loadBalancers = new operations.LoadBalancers(this);
     this.loadBalancerBackendAddressPools = new operations.LoadBalancerBackendAddressPools(this);
