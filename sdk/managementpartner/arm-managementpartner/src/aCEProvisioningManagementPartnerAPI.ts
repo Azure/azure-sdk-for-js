c6fb10731925788b7ee4a9be5d63011ec9595eea
colawwj <70128817+colawwj@users.noreply.github.com>
2021-07-08 09:07:16 +0800

    identity support 30 (#16254)

--- sdk/managementpartner/arm-managementpartner/src/aCEProvisioningManagementPartnerAPI.ts
+++ sdk/managementpartner/arm-managementpartner/src/aCEProvisioningManagementPartnerAPI.ts
@@ -9,6 +9,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -22,10 +23,15 @@ class ACEProvisioningManagementPartnerAPI extends ACEProvisioningManagementPartn
 
   /**
    * Initializes a new instance of the ACEProvisioningManagementPartnerAPI class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, options?: Models.ACEProvisioningManagementPartnerAPIOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, options?: Models.ACEProvisioningManagementPartnerAPIOptions) {
     super(credentials, options);
     this.partner = new operations.Partner(this);
     this.operation = new operations.Operation(this);
