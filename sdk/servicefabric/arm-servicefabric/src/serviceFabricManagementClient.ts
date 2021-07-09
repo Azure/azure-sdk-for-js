5565230074f768cd2d67707d67edd3a3dc06743b
colawwj <70128817+colawwj@users.noreply.github.com>
2021-07-09 08:55:36 +0800

    70 rp identity support (#16294)

--- sdk/servicefabric/arm-servicefabric/src/serviceFabricManagementClient.ts
+++ sdk/servicefabric/arm-servicefabric/src/serviceFabricManagementClient.ts
@@ -9,6 +9,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -30,11 +31,16 @@ class ServiceFabricManagementClient extends ServiceFabricManagementClientContext
 
   /**
    * Initializes a new instance of the ServiceFabricManagementClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId The customer subscription identifier.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.ServiceFabricManagementClientOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, subscriptionId: string, options?: Models.ServiceFabricManagementClientOptions) {
     super(credentials, subscriptionId, options);
     this.clusters = new operations.Clusters(this);
     this.clusterVersions = new operations.ClusterVersions(this);
--- sdk/servicefabricmesh/arm-servicefabricmesh/src/serviceFabricMeshManagementClient.ts
+++ sdk/servicefabricmesh/arm-servicefabricmesh/src/serviceFabricMeshManagementClient.ts
@@ -9,6 +9,7 @@
  */
 
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as Models from "./models";
 import * as Mappers from "./models/mappers";
 import * as operations from "./operations";
@@ -30,11 +31,16 @@ class ServiceFabricMeshManagementClient extends ServiceFabricMeshManagementClien
 
   /**
    * Initializes a new instance of the ServiceFabricMeshManagementClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param subscriptionId The customer subscription identifier
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, subscriptionId: string, options?: Models.ServiceFabricMeshManagementClientOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, subscriptionId: string, options?: Models.ServiceFabricMeshManagementClientOptions) {
     super(credentials, subscriptionId, options);
     this.operations = new operations.Operations(this);
     this.secret = new operations.Secret(this);
