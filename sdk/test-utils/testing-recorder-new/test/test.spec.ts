40c10742de72a1d59087fac5482dd5bdb02a4f34
Harsha Nalluru <sanallur@microsoft.com>
2021-07-12 20:36:12 +0000

    test file with logs

--- sdk/test-utils/testing-recorder-new/test/test.spec.ts
+++ sdk/test-utils/testing-recorder-new/test/test.spec.ts
@@ -2,18 +2,22 @@ import { isLiveMode, isPlaybackMode, env } from "@azure/test-utils-recorder";
 import { QueueServiceClient, StoragePipelineOptions } from "@azure/storage-queue";
 import { RecordingHttpClient } from "@azure/test-utils-recorder-new";
 import { config } from "dotenv";
+import { isNode } from "@azure/core-util";
 config();
 
 describe("Tests", () => {
   it("storage test", async function() {
     const file = `file_path.json`;
+    console.log(`env.TEST_MODE = ${env.TEST_MODE}`);
     const recorder = new RecordingHttpClient(file, isPlaybackMode());
     const options: StoragePipelineOptions = {};
     if (!isLiveMode()) {
       options.httpClient = recorder;
     }
     const client = new QueueServiceClient(env.STORAGE_SAS_URL, undefined, options);
-    await client.createQueue("queue-" + `${Math.ceil(Math.random() * 1000) + 1000}`);
+    await client.createQueue(
+      (isNode ? "node" : "browser") + `-${Math.ceil(Math.random() * 1000) + 1000}`
+    );
     await recorder.stop();
   });
 });
