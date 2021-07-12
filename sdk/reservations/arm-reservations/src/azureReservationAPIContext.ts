24c4d4688bcfc5a93df4f181403ffabe207f2a72
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2021-07-12 08:53:59 +0800

    bugfix (#16329)

--- sdk/reservations/arm-reservations/src/azureReservationAPIContext.ts
+++ sdk/reservations/arm-reservations/src/azureReservationAPIContext.ts
@@ -9,20 +9,26 @@
 
 import * as Models from "./models";
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-reservations";
-const packageVersion = "6.0.0";
+const packageVersion = "6.1.0";
 
 export class AzureReservationAPIContext extends msRestAzure.AzureServiceClient {
-  credentials: msRest.ServiceClientCredentials;
+  credentials: msRest.ServiceClientCredentials | TokenCredential;
 
   /**
    * Initializes a new instance of the AzureReservationAPI class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, options?: Models.AzureReservationAPIOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, options?: Models.AzureReservationAPIOptions) {
     if (credentials == undefined) {
       throw new Error('\'credentials\' cannot be null.');
     }
--- sdk/subscription/arm-subscriptions/src/subscriptionClientContext.ts
+++ sdk/subscription/arm-subscriptions/src/subscriptionClientContext.ts
@@ -10,20 +10,26 @@
 
 import * as Models from "./models";
 import * as msRest from "@azure/ms-rest-js";
+import { TokenCredential } from "@azure/core-auth";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/arm-subscriptions";
-const packageVersion = "3.0.0";
+const packageVersion = "3.1.0";
 
 export class SubscriptionClientContext extends msRestAzure.AzureServiceClient {
-  credentials: msRest.ServiceClientCredentials;
+  credentials: msRest.ServiceClientCredentials | TokenCredential;
 
   /**
    * Initializes a new instance of the SubscriptionClient class.
-   * @param credentials Credentials needed for the client to connect to Azure.
+   * @param credentials Credentials needed for the client to connect to Azure. Credentials
+   * implementing the TokenCredential interface from the @azure/identity package are recommended. For
+   * more information about these credentials, see
+   * {@link https://www.npmjs.com/package/@azure/identity}. Credentials implementing the
+   * ServiceClientCredentials interface from the older packages @azure/ms-rest-nodeauth and
+   * @azure/ms-rest-browserauth are also supported.
    * @param [options] The parameter options
    */
-  constructor(credentials: msRest.ServiceClientCredentials, options?: Models.SubscriptionClientOptions) {
+  constructor(credentials: msRest.ServiceClientCredentials | TokenCredential, options?: Models.SubscriptionClientOptions) {
     if (credentials == undefined) {
       throw new Error('\'credentials\' cannot be null.');
     }
