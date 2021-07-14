78d4fe4368db85a56959daec8c1a24a641533407
t-bntiamoah@microsoft.com <t-bntiamoah@microsoft.com>
2021-07-14 00:15:53 -0400

    deleted a folder

--- sdk/template/template/samples/v1/typescript/src/eventhubs-checkpointstore-blob/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-/// <reference lib="esnext.asynciterable" />
-
-export { BlobCheckpointStore } from "./blobCheckpointStore";
-export { logger } from "./log";
