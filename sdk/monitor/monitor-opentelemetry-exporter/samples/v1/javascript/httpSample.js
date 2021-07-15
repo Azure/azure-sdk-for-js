eeb263b0bdd05c0e8b3720b5cc0a34dbdf993e06
xiao-lix <lixiao900921@gmail.com>
2021-07-15 15:35:23 -0700

    stop server

--- sdk/monitor/monitor-opentelemetry-exporter/samples/v1/javascript/httpSample.js
+++ sdk/monitor/monitor-opentelemetry-exporter/samples/v1/javascript/httpSample.js
@@ -43,9 +43,10 @@ const http = require("http");
  *  HTTP SERVER SETUP
  **********************************************************************/
 /** Starts a HTTP server that receives requests on sample server port. */
+let server;
 function startServer(port) {
   // Creates a server
-  const server = http.createServer(handleRequest);
+  server = http.createServer(handleRequest);
   // Starts the server
   server.listen(port, () => {
     console.log(`Node HTTP listening on ${port}`);
@@ -74,10 +75,8 @@ function handleRequest(request, response) {
     setTimeout(() => {
       span.end();
       response.end("Hello World!");
-      setTimeout(() => {
-        // terminate the process to stop CI pipeline from running forever
-        process.kill(process.pid, "SIGTERM");
-      }, 2000);
+      // terminate the process to stop CI pipeline from running forever
+      server.close();
     }, 2000);
   });
 }
