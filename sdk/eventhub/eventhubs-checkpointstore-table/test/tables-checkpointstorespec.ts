e18062d2df2835507561c3a3c909f56148d3e5b2
t-bntiamoah@microsoft.com <t-bntiamoah@microsoft.com>
2021-07-21 12:42:12 -0400

    run rush update

--- sdk/eventhub/eventhubs-checkpointstore-table/test/tables-checkpointstorespec.ts
+++ sdk/eventhub/eventhubs-checkpointstore-table/test/tables-checkpointstorespec.ts
@@ -4,9 +4,7 @@ import chai from "chai";
 const should = chai.should();
 import { TableCheckpointStore } from "../src";
 
-
-
-/*test to show that test framework is set up well*/
+/* test to show that test framework is set up well */
 describe("TableCheckpointStore", () => {
   it("is exported from the package", () => {
     should.exist(TableCheckpointStore, "Expected TableCheckpointStore to be exported.");
