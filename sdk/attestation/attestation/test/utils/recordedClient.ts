1166a361d06846ac26ecb01f40c85d1fc9660558
Larry Osterman <LarryOsterman@users.noreply.github.com>
2021-07-26 16:35:44 -0700

    Updated recordings

--- sdk/attestation/attestation/test/utils/recordedClient.ts
+++ sdk/attestation/attestation/test/utils/recordedClient.ts
@@ -112,14 +112,14 @@ export function createRecordedClient(
         expectedIssuer: getAttestationUri(endpointType)
       }
     };
-  }
-  if (authenticatedClient !== undefined && authenticatedClient) {
-    options.credentials = new ClientSecretCredential(
-      env.AZURE_TENANT_ID,
-      env.AZURE_CLIENT_ID,
-      env.AZURE_CLIENT_SECRET
-    );
-  }
+    if (authenticatedClient !== undefined && authenticatedClient) {
+      options.credentials = new ClientSecretCredential(
+        env.AZURE_TENANT_ID,
+        env.AZURE_CLIENT_ID,
+        env.AZURE_CLIENT_SECRET
+      );
+    }
+}
 
   return new AttestationClient(getAttestationUri(endpointType), options);
 }
