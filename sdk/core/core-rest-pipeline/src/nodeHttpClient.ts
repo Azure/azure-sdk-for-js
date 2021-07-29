e999c81c63d164adcb600b8256acd5a28cf3abbf
Harsha Nalluru <sanallur@microsoft.com>
2021-07-29 01:53:30 +0000

    rushx format

--- sdk/core/core-rest-pipeline/src/nodeHttpClient.ts
+++ sdk/core/core-rest-pipeline/src/nodeHttpClient.ts
@@ -208,9 +208,7 @@ class NodeHttpClient implements HttpClient {
     };
 
     return new Promise<http.IncomingMessage>((resolve, reject) => {
-      const req = isInsecure
-        ? http.request(options, resolve)
-        : https.request(options, resolve);
+      const req = isInsecure ? http.request(options, resolve) : https.request(options, resolve);
 
       req.once("error", (err) => {
         reject(new RestError(err.message, { code: RestError.REQUEST_SEND_ERROR, request }));
