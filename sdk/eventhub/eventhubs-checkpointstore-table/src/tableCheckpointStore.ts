35ead8b865f8a132ce1b959fac5411b4cd326bf3
t-bntiamoah@microsoft.com <t-bntiamoah@microsoft.com>
2021-07-30 18:10:09 -0400

    updated tests

--- sdk/eventhub/eventhubs-checkpointstore-table/src/tableCheckpointStore.ts
+++ sdk/eventhub/eventhubs-checkpointstore-table/src/tableCheckpointStore.ts
@@ -5,19 +5,15 @@ import { CheckpointStore, PartitionOwnership, Checkpoint } from "@azure/event-hu
 
 import { odata, TableClient } from "@azure/data-tables";
 
-
 export interface customCheckpoint extends Checkpoint {
-  partitionKey : string,
-  rowKey : string,
-  
+  partitionKey: string;
+  rowKey: string;
 }
 
 export interface customPartition extends PartitionOwnership {
-  partitionKey : string,
-  rowKey : string
-
+  partitionKey: string;
+  rowKey: string;
 }
-
 
 /**
  * An implementation of CheckpointStore that uses Azure Table Storage to persist checkpoint data.
@@ -43,36 +39,33 @@ export class TableCheckpointStore implements CheckpointStore {
    *  - `tracingOptions`: Options for configuring tracing.
    * @returns Partition ownership details of all the partitions that have had an owner.
    */
-   
-   async listOwnership(
+
+  async listOwnership(
     fullyQualifiedNamespace: string,
     eventHubName: string,
-    consumerGroup: string): Promise<PartitionOwnership[]> {
-    let PARTITIONKEY = eventHubName + " "+  fullyQualifiedNamespace + " "+ consumerGroup + " "+ "Ownership";
+    consumerGroup: string
+  ): Promise<PartitionOwnership[]> {
+    let PARTITIONKEY =
+      eventHubName + " " + fullyQualifiedNamespace + " " + consumerGroup + " " + "Ownership";
     const partitionOwnershipArray: PartitionOwnership[] = [];
-    let entitiesIter = this._tableClient.listEntities<PartitionOwnership >({
-      queryOptions: { filter: odata`PartitionKey eq ${PARTITIONKEY}`}
+    let entitiesIter = this._tableClient.listEntities<PartitionOwnership>({
+      queryOptions: { filter: odata`PartitionKey eq ${PARTITIONKEY}` }
     });
-     
-        for await (const entity of entitiesIter) {
-                const partitionOwnership : PartitionOwnership = {
-                    fullyQualifiedNamespace,
-                    eventHubName,
-                    consumerGroup,
-                    ownerId: entity.ownerId,
-                    partitionId: entity.partitionId,
-                    lastModifiedTimeInMs: Number(entity.timestamp),
-                    etag: entity.etag
-                };
-                partitionOwnershipArray.push(partitionOwnership);
-                
-        }
-        return partitionOwnershipArray;
-         
 
+    for await (const entity of entitiesIter) {
+      const partitionOwnership: PartitionOwnership = {
+        fullyQualifiedNamespace,
+        eventHubName,
+        consumerGroup,
+        ownerId: entity.ownerId,
+        partitionId: entity.partitionId,
+        lastModifiedTimeInMs: entity.lastModifiedTimeInMs,
+        etag: entity.etag
+      };
+      partitionOwnershipArray.push(partitionOwnership);
+    }
+    return partitionOwnershipArray;
   }
-    
-  
 
   /**
    * Claim ownership of a list of partitions. This will return the list of partitions that were
@@ -84,16 +77,12 @@ export class TableCheckpointStore implements CheckpointStore {
    *  - `tracingOptions`: Options for configuring tracing.
    * @returns A list partitions this instance successfully claimed ownership.
    */
-  async claimOwnership( partitionOwnership: PartitionOwnership[]):
-  Promise<PartitionOwnership[]>
-    {
-      const partitionOwnershipArray: PartitionOwnership[] = [];
-      for (const ownership of partitionOwnership) { 
-        ownership.ownerId = "1"
-      }
-      return partitionOwnershipArray;
-  
-
+  async claimOwnership(partitionOwnership: PartitionOwnership[]): Promise<PartitionOwnership[]> {
+    const partitionOwnershipArray: PartitionOwnership[] = [];
+    for (const ownership of partitionOwnership) {
+      ownership.ownerId = "1";
+    }
+    return partitionOwnershipArray;
   }
 
   /**
@@ -107,34 +96,32 @@ export class TableCheckpointStore implements CheckpointStore {
    *  - `abortSignal`: A signal used to request operation cancellation.
    *  - `tracingOptions`: Options for configuring tracing.
    */
-   async listCheckpoints(fullyQualifiedNamespace: string, 
-    eventHubName: string, 
-    consumerGroup: string) {
+  async listCheckpoints(
+    fullyQualifiedNamespace: string,
+    eventHubName: string,
+    consumerGroup: string
+  ) {
+    let PARTITIONKEY =
+      eventHubName + " " + fullyQualifiedNamespace + " " + consumerGroup + " " + "Checkpoint";
+    const checkpoints: Checkpoint[] = [];
 
-        let PARTITIONKEY = eventHubName + " "+ fullyQualifiedNamespace+ " " + consumerGroup + " "+ "Checkpoint";
-        const checkpoints: Checkpoint[] = [];
-        
-        let entitiesIter = this._tableClient.listEntities<Checkpoint>({
-          queryOptions: { filter: odata`PartitionKey eq ${PARTITIONKEY}`}
+    let entitiesIter = this._tableClient.listEntities<Checkpoint>({
+      queryOptions: { filter: odata`PartitionKey eq ${PARTITIONKEY}` }
+    });
+    for await (const entity of entitiesIter) {
+      if (entity.hasOwnProperty("offset")) {
+        checkpoints.push({
+          consumerGroup,
+          eventHubName,
+          fullyQualifiedNamespace,
+          partitionId: entity.partitionId,
+          offset: entity.offset,
+          sequenceNumber: entity.sequenceNumber
         });
-        for await (const entity of entitiesIter) {
-            
-            if (entity.hasOwnProperty('offset')) {
-              checkpoints.push({
-                consumerGroup,
-                eventHubName,
-                fullyQualifiedNamespace,
-                partitionId: entity.partitionId,
-                offset : entity.offset,
-                sequenceNumber : entity.sequenceNumber
-              });
-            
-            
-        }
+      }
     }
     return checkpoints;
-
-}
+  }
 
   /**
    * Updates the checkpoint in the data store for a partition.
@@ -145,42 +132,48 @@ export class TableCheckpointStore implements CheckpointStore {
    *  - `tracingOptions`: Options for configuring tracing.
    * @returns A promise that resolves when the checkpoint has been updated.
    */
-   async updateCheckpoint(checkpoint: Checkpoint){
-    let PARTITIONKEY = checkpoint.eventHubName+ checkpoint.fullyQualifiedNamespace+ checkpoint.consumerGroup+"checkpoint"
-     
-     const entity = {partitionKey: PARTITIONKEY, 
-     rowKey: checkpoint.partitionId, offset: 5890, sequenceNumber: 19};
- 
-     let entitiesIter = this._tableClient.listEntities<Checkpoint>({
-       queryOptions: { filter: odata`PartitionKey eq ${PARTITIONKEY}`}
-     });
-     let i = 0;
-     for await (const entity of entitiesIter) {
-       const checkpointArray: Checkpoint[] = [];
-       checkpointArray.push(entity);
-       i++;
-     }
- 
-     if (i > 0 ) {
-       await this._tableClient.updateEntity(entity);
-     }
-     else {
-      const entity1 : customCheckpoint = {
-        partitionKey : PARTITIONKEY,
-        rowKey : entity.rowKey,
-        consumerGroup : checkpoint.consumerGroup,
-        fullyQualifiedNamespace : checkpoint.fullyQualifiedNamespace,
+  async updateCheckpoint(checkpoint: Checkpoint) {
+    let PARTITIONKEY =
+      checkpoint.eventHubName +
+      " " +
+      checkpoint.fullyQualifiedNamespace +
+      " " +
+      checkpoint.consumerGroup +
+      " " +
+      "Checkpoint";
+    const entity = {
+      partitionKey: PARTITIONKEY,
+      rowKey: checkpoint.partitionId,
+      offset: 5890,
+      sequenceNumber: 19
+    };
+
+    let entitiesIter = this._tableClient.listEntities<Checkpoint>({
+      queryOptions: { filter: odata`PartitionKey eq ${PARTITIONKEY}` }
+    });
+    let i = 0;
+    for await (const entity of entitiesIter) {
+      const checkpointArray: Checkpoint[] = [];
+      checkpointArray.push(entity);
+      i++;
+    }
+
+    if (i > 0) {
+      await this._tableClient.updateEntity(entity);
+    } else {
+      const entity1: customCheckpoint = {
+        partitionKey: PARTITIONKEY,
+        rowKey: entity.rowKey,
+        consumerGroup: checkpoint.consumerGroup,
+        fullyQualifiedNamespace: checkpoint.fullyQualifiedNamespace,
         eventHubName: checkpoint.eventHubName,
-        sequenceNumber : entity.sequenceNumber,
-        offset : entity.offset,
-        partitionId : checkpoint.partitionId
-    
+        sequenceNumber: entity.sequenceNumber,
+        offset: entity.offset,
+        partitionId: checkpoint.partitionId
       };
       await this._tableClient.createEntity(entity1);
-      
     }
-    
+
     return;
-  
   }
 }
--- sdk/eventhub/eventhubs-checkpointstore-table/test/tables-checkpointstore.spec.ts
+++ sdk/eventhub/eventhubs-checkpointstore-table/test/tables-checkpointstore.spec.ts
@@ -5,13 +5,12 @@ import * as dotenv from "dotenv";
 const should = chai.should();
 import { TableCheckpointStore } from "../src";
 
-import { customCheckpoint , customPartition} from "../src/tableCheckpointStore";
+import { customCheckpoint, customPartition } from "../src/tableCheckpointStore";
 
 import { EnvVarKeys, getEnvVars } from "./utils/testUtils";
-import { TableServiceClient, AzureNamedKeyCredential , TableClient} from "@azure/data-tables";
+import { TableServiceClient, AzureNamedKeyCredential, TableClient } from "@azure/data-tables";
 const env = getEnvVars();
 dotenv.config();
-
 
 /* test to show that test framework is set up well */
 describe("TableCheckpointStore", () => {
@@ -27,179 +26,148 @@ const service = {
   tableName: env[EnvVarKeys.TABLENAME]
 };
 
-
-const credential = new AzureNamedKeyCredential(service.storageAccountName, service.storageAccountKey);
+const credential = new AzureNamedKeyCredential(
+  service.storageAccountName,
+  service.storageAccountKey
+);
 const serviceClient = new TableServiceClient(
-  `https://${service.storageAccountName}.table.core.windows.net`,credential);
+  `https://${service.storageAccountName}.table.core.windows.net`,
+  credential
+);
 
-  
 describe("TableCheckpointStore", function(): void {
-  
-  
-  describe("Runs tests on table with no entities" , function(){
-    const table_name = `table${new Date().getTime()}`;
-   
-    beforeEach("creating table" ,async () => {
-    
-      await serviceClient.createTable(table_name); 
-    
+  let client: TableClient;
+  let table_name: string;
+
+  describe("Runs tests on table with no entities", function() {
+    table_name = `table${new Date().getTime()}`;
+    beforeEach("creating table", async () => {
+      await serviceClient.createTable(table_name);
     });
 
     const client = new TableClient(
       `https://${service.storageAccountName}.table.core.windows.net`,
-    table_name,credential);
-  
-  afterEach(async () => {
-    await client.deleteTable();
-  });
+      table_name,
+      credential
+    );
 
-
-
-
-      
-
-
-
-describe("listOwnership", function() {
-it("listOwnership should return an empty array", async function(): Promise<void> {
-  
-  const checkpointStore = new TableCheckpointStore(client);
-  const listOwnership = await checkpointStore.listOwnership(
-    "test.servicebus.windows.net",
-    "testHub",
-    "testConsumerGroup"
-  );
-  should.equal(listOwnership.length, 0);
-  
-  
-});
-/*
-
-it("listOwnership is empty but length is greater than 0", async function(): Promise<void> {
-  
-  const checkpointStore = new TableCheckpointStore(client);
-  const listOwnership = await checkpointStore.listOwnership(
-    "test.servicebus.windows.net",
-    "testHub",
-    "testConsumerGroup"
-  );
-  should.not.equal(listOwnership.length, 5);
-  
-});
-*/
-
-});
-
-
-});
-
-describe("Runs tests on a populated table" , function(){
-  
-  
-    
-    let client : TableClient;
-    let table_name = `table${new Date().getTime()}`;
-  beforeEach("creating table" ,async () => {
-  
-  
-    await serviceClient.createTable(table_name);
-    const namespaceArray = ["red.servicebus.windows.net","blue.service.bus.net","green.servicebus.net"];
-    const eventHubArray = ["redHub","blueHub", "greenHub"];
-     /*Checkpoint*/
-  const checkpoint_entity : customCheckpoint = {
-   partitionKey : "0",
-   rowKey : "0",
-   consumerGroup : "$default",
-   fullyQualifiedNamespace : "baffy.servicebus.windows.net" ,
-   eventHubName: "baffy",
-   sequenceNumber : 0,
-   offset : 42694,      
-   partitionId : "0"
- };
- for (let i = 0; i < 3; ++i){
-  checkpoint_entity.fullyQualifiedNamespace = namespaceArray[i];
-  checkpoint_entity.eventHubName = eventHubArray[i];
-  checkpoint_entity.sequenceNumber = 100 + i;
-  checkpoint_entity.partitionId = i.toString();
-  checkpoint_entity.rowKey = checkpoint_entity.partitionId;
-  checkpoint_entity.partitionKey = checkpoint_entity.eventHubName + " " + checkpoint_entity.fullyQualifiedNamespace + " " + checkpoint_entity.consumerGroup + " " + "Checkpoint";
-  checkpoint_entity.offset = 1023 + i;
-  await client.createEntity(checkpoint_entity);
-}
-
-  /*Ownership*/
-    const ownership_entity : customPartition =  {
-      partitionKey : "",
-      rowKey: "",
-      consumerGroup : "$default",
-      fullyQualifiedNamespace : "baffy",
-      eventHubName: "yellow",
-      partitionId: "3",
-      ownerId : "11"
-
-    };
-
-    for (let i = 0; i < 3; ++i){
-      ownership_entity.fullyQualifiedNamespace = namespaceArray[i];
-      ownership_entity.eventHubName = eventHubArray[i];
-      ownership_entity.partitionId = i.toString();
-      ownership_entity.rowKey = ownership_entity.partitionId;
-      ownership_entity.partitionKey = ownership_entity.eventHubName + " " + ownership_entity.fullyQualifiedNamespace + " " + ownership_entity.consumerGroup + " " + "Ownership";
-      ownership_entity.ownerId = "Id" + i
-      
-
-      await client.createEntity(ownership_entity);
-    }
-
-  });
-   
-  client = new TableClient(
-    `https://${service.storageAccountName}.table.core.windows.net`,
-    table_name,credential);
-  
-  
-  afterEach(async () => {
-    await client.deleteTable();
-  });
-
-
-
-
-
-  describe("listOwnership", function() {
-    it("listOwnership should print an array of ownerships", async function() {
-      
-      const checkpointStore = new TableCheckpointStore(client);
-      const listOwnership = await checkpointStore.listOwnership(
-        "red.servicebus.windows.net",
-        "redHub",
-        "$default"
-      );
-      console.log(listOwnership);
-      
-      
+    afterEach(async () => {
+      await client.deleteTable();
     });
 
-    describe("listCheckpoints",function(){
-      it("listCheckpoints should print out an array of checkpoints" , async function() {
+    describe("listOwnership", function() {
+      it("listOwnership should return an empty array", async function(): Promise<void> {
         const checkpointStore = new TableCheckpointStore(client);
-        const listCheckpoint = await checkpointStore.listCheckpoints(
+        const listOwnership = await checkpointStore.listOwnership(
+          "test.servicebus.windows.net",
+          "testHub",
+          "testConsumerGroup"
+        );
+        should.equal(listOwnership.length, 0);
+      });
+    });
+  });
+
+  describe("Runs tests on a populated table", function() {
+    beforeEach("creating table", async () => {
+      table_name = `table${new Date().getTime()}`;
+      client = new TableClient(
+        `https://${service.storageAccountName}.table.core.windows.net`,
+        table_name,
+        credential
+      );
+      await serviceClient.createTable(table_name);
+      const namespaceArray = [
+        "red.servicebus.windows.net",
+        "blue.service.bus.net",
+        "green.servicebus.net"
+      ];
+      const eventHubArray = ["redHub", "blueHub", "greenHub"];
+      /*Checkpoint*/
+      const checkpoint_entity: customCheckpoint = {
+        partitionKey: "0",
+        rowKey: "0",
+        consumerGroup: "$default",
+        fullyQualifiedNamespace: "baffy.servicebus.windows.net",
+        eventHubName: "baffy",
+        sequenceNumber: 0,
+        offset: 42694,
+        partitionId: "0"
+      };
+      for (let i = 0; i < 3; ++i) {
+        checkpoint_entity.fullyQualifiedNamespace = namespaceArray[i];
+        checkpoint_entity.eventHubName = eventHubArray[i];
+        checkpoint_entity.sequenceNumber = 100 + i;
+        checkpoint_entity.partitionId = i.toString();
+        checkpoint_entity.rowKey = checkpoint_entity.partitionId;
+        checkpoint_entity.partitionKey =
+          checkpoint_entity.eventHubName +
+          " " +
+          checkpoint_entity.fullyQualifiedNamespace +
+          " " +
+          checkpoint_entity.consumerGroup +
+          " " +
+          "Checkpoint";
+        checkpoint_entity.offset = 1023 + i;
+        await client.createEntity(checkpoint_entity);
+      }
+
+      /*Ownership*/
+      const ownership_entity: customPartition = {
+        partitionKey: "",
+        rowKey: "",
+        consumerGroup: "$default",
+        fullyQualifiedNamespace: "baffy",
+        eventHubName: "yellow",
+        partitionId: "3",
+        ownerId: "11"
+      };
+
+      for (let i = 0; i < 3; ++i) {
+        ownership_entity.fullyQualifiedNamespace = namespaceArray[i];
+        ownership_entity.eventHubName = eventHubArray[i];
+        ownership_entity.partitionId = i.toString();
+        ownership_entity.rowKey = ownership_entity.partitionId;
+        ownership_entity.partitionKey =
+          ownership_entity.eventHubName +
+          " " +
+          ownership_entity.fullyQualifiedNamespace +
+          " " +
+          ownership_entity.consumerGroup +
+          " " +
+          "Ownership";
+        ownership_entity.ownerId = "Id" + i;
+
+        await client.createEntity(ownership_entity);
+      }
+    });
+
+    afterEach(async () => {
+      await client.deleteTable();
+    });
+
+    describe("listOwnership", function() {
+      it("listOwnership should print an array of ownerships", async function() {
+        const checkpointStore = new TableCheckpointStore(client);
+        const listOwnership = await checkpointStore.listOwnership(
           "red.servicebus.windows.net",
           "redHub",
           "$default"
         );
-        console.log(listCheckpoint);
+        console.log(listOwnership);
+      });
 
+      describe("listCheckpoints", function() {
+        it("listCheckpoints should print out an array of checkpoints", async function() {
+          const checkpointStore = new TableCheckpointStore(client);
+          const listCheckpoint = await checkpointStore.listCheckpoints(
+            "red.servicebus.windows.net",
+            "redHub",
+            "$default"
+          );
+          console.log(listCheckpoint);
+        });
       });
     });
-
-
-  
-
-
-
-});
-
-});
-
+  });
 });
