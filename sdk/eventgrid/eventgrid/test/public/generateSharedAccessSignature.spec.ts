e808fec27fa455bb64747edf385d7148cd1d1fbb
Harsha Nalluru <sanallur@microsoft.com>
2021-08-02 21:48:52 +0000

    use btoa in browser

--- sdk/eventgrid/eventgrid/test/public/generateSharedAccessSignature.spec.ts
+++ sdk/eventgrid/eventgrid/test/public/generateSharedAccessSignature.spec.ts
@@ -3,12 +3,14 @@
 
 import { assert } from "chai";
 import { AzureKeyCredential, generateSharedAccessSignature } from "../../src";
+import { isNode } from "@azure/test-utils";
 
-describe("generateSharedAccessSignature", function() {
+describe("generateSharedAccessSignature", function () {
   it("generates the correct signiture", async () => {
     // This is not a real key, it's the base64 encoding of "this is not a real EventGrid key", which happens to be the same
     // number of bytes as an actual EventGrid Access Key.
-    const key = Buffer.from("this is not a real EventGrid key").toString("base64");
+    const keyAsString = "this is not a real EventGrid key";
+    const key = isNode ? Buffer.from(keyAsString).toString("base64") : btoa(keyAsString);
     const topicUrl = "https://eg-topic.westus-2.eventgrid.azure.net/api/events";
 
     const sig = await generateSharedAccessSignature(
