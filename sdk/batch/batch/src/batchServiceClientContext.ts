02ca8f3ef8160b1f99254e7d927cc2196807b34e
Deyaaeldeen Almahallawi <diaa6510@gmail.com>
2021-08-12 14:33:22 -0400

    Update sdk/batch/batch/src/batchServiceClientContext.ts
    Co-authored-by: Ramya Rao <ramya.rao.a@outlook.com>

--- sdk/batch/batch/src/batchServiceClientContext.ts
+++ sdk/batch/batch/src/batchServiceClientContext.ts
@@ -11,7 +11,7 @@ import * as msRest from "@azure/ms-rest-js";
 import * as msRestAzure from "@azure/ms-rest-azure-js";
 
 const packageName = "@azure/batch";
-const packageVersion = "9.1.0";
+const packageVersion = "10.0.0";
 
 export class BatchServiceClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials;
