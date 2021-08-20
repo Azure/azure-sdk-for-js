0f2fef6d1cca58dbe0e6c517bfebbeb423249f25
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-08-20 10:05:18 -0700

    [REST Paging] Allow setting different `itemName` and `nextLinkName` (#16956)
    * Allow setting different itemName and nextLinkName
    
    * Update name and types
    
    * only loop properties once

--- sdk/core/core-client-paging-rest/src/paginate.ts
+++ sdk/core/core-client-paging-rest/src/paginate.ts
@@ -28,11 +28,11 @@ export interface PaginateOptions {
    * Note: if nextLinkName is set to `null` only the first page is returned, no additional
    * requests are made.
    */
-  nextLinkName?: string | null;
+  nextLinkNames?: string[] | null;
   /**
    * Indicates the name of the property in which the set of values is found. Default: `value`
    */
-  itemName?: string;
+  itemNames?: string[];
 }
 
 /**
@@ -48,20 +48,21 @@ export function paginateResponse<TElement>(
   options: PaginateOptions = {}
 ): PagedAsyncIterableIterator<TElement> {
   let firstRun = true;
+  const { itemName, nextLinkName } = getPaginationProperties(initialResponse, options);
   const pagedResult: PagedResult<TElement[]> = {
     firstPageLink: "",
     async getPage(pageLink: string) {
       const result = firstRun ? initialResponse : await client.pathUnchecked(pageLink).get();
       firstRun = false;
       checkPagingRequest(result);
-      const nextLink = getNextLink(result.body, options);
-      const values = getElements<TElement>(result.body, options);
+      const nextLink = getNextLink(result.body, nextLinkName);
+      const values = getElements<TElement>(result.body, itemName);
       return {
         page: values,
-        // According to x-ms-pageable is the nextLinkName is set to null we should only
+        // According to x-ms-pageable is the nextLinkNames is set to null we should only
         // return the first page and skip any additional queries even if the initial response
         // contains a nextLink.
-        nextPageLink: options.nextLinkName === null ? undefined : nextLink,
+        nextPageLink: options.nextLinkNames === null ? undefined : nextLink,
       };
     },
   };
@@ -81,29 +82,73 @@ function checkPagingRequest(response: PathUncheckedResponse): void {
 }
 
 /**
- * Gets for the value of nextLink in the body. If a custom nextLinkName was provided, it will be used instead of default
+ * Extracts the itemName and nextLinkName from the initial response to use them for pagination
  */
-function getNextLink(body: unknown, paginateOptions: PaginateOptions = {}): string | undefined {
-  const nextLinkName = paginateOptions.nextLinkName ?? DEFAULT_NEXTLINK;
-  const nextLink = (body as Record<string, unknown>)[nextLinkName];
+function getPaginationProperties(initialResponse: HttpResponse, options: PaginateOptions = {}) {
+  // Build a set with the passed custom nextLinkNames
+  const nextLinkNames = new Set(options.nextLinkNames ?? DEFAULT_NEXTLINK);
+  // Add the default nextLinkName if it doesn't exist yet
+  nextLinkNames.add(DEFAULT_NEXTLINK);
 
-  if (typeof nextLink !== "string" && typeof nextLink !== "undefined") {
-    throw new Error(`Body Property ${nextLinkName} should be a string or undefined`);
+  // Build a set with the passed custom set of itemNames
+  const itemNames = new Set(options.itemNames ?? DEFAULT_VALUES);
+  // Add the default itemName if it doesn't exist yet
+  itemNames.add(DEFAULT_VALUES);
+
+  let nextLinkName: string | undefined;
+  let itemName: string | undefined;
+
+  for (const name of nextLinkNames) {
+    const nextLink = (initialResponse.body as Record<string, unknown>)[name] as string;
+    if (nextLink) {
+      nextLinkName = name;
+      break;
+    }
   }
+
+  for (const name of itemNames) {
+    const item = (initialResponse.body as Record<string, unknown>)[name] as string;
+    if (item) {
+      itemName = name;
+      break;
+    }
+  }
+
+  if (!itemName) {
+    throw new Error(
+      `Couldn't paginate response\n Body doesn't contain an array property with name: ${[
+        ...itemNames,
+      ].join(" OR ")}`
+    );
+  }
+
+  return { itemName, nextLinkName };
+}
+
+/**
+ * Gets for the value of nextLink in the body. If a custom nextLinkNames was provided, it will be used instead of default
+ */
+function getNextLink(body: unknown, nextLinkName?: string): string | undefined {
+  // It is possible to get an undefined for nextLinkName, in the scenario where the initial response contains the last page.
+  const nextLink =
+    nextLinkName === undefined
+      ? undefined
+      : ((body as Record<string, unknown>)[nextLinkName] as string);
 
   return nextLink;
 }
 
 /**
  * Gets the elements of the current request in the body. By default it will look in the `value` property unless
- * a different value for itemName has been provided as part of the options.
+ * a different value for itemNames has been provided as part of the options.
  */
-function getElements<T = unknown>(body: unknown, paginateOptions: PaginateOptions = {}): T[] {
-  const valueName = paginateOptions?.itemName ?? DEFAULT_VALUES;
-  const value = (body as Record<string, unknown>)[valueName];
+function getElements<T = unknown>(body: unknown, itemName: string): T[] {
+  const value = (body as Record<string, unknown>)[itemName];
 
   if (!Array.isArray(value)) {
-    throw new Error(`Body Property ${valueName} is not an array`);
+    throw new Error(
+      `Couldn't paginate response\n Body doesn't contain an array property with name: ${itemName}`
+    );
   }
 
   return (value as T[]) ?? [];
