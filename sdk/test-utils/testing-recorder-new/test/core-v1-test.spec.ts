eee1249d0dc97b0fc0ca22ac1c64b03d76ebec70
Harsha Nalluru <sanallur@microsoft.com>
2021-08-21 00:14:00 +0000

    update tests

--- sdk/test-utils/testing-recorder-new/test/core-v1-test.spec.ts
+++ sdk/test-utils/testing-recorder-new/test/core-v1-test.spec.ts
@@ -1,21 +1,24 @@
 import { isLiveMode, isPlaybackMode, env } from "@azure/test-utils-recorder";
 import { QueueServiceClient, StoragePipelineOptions } from "@azure/storage-queue";
-import { RecordingHttpClient } from "@azure/test-utils-recorder-new";
+import { TestProxyHttpClientCoreV1 } from "@azure/test-utils-recorder-new";
 import { config } from "dotenv";
 import { isNode } from "@azure/core-util";
 config();
 
 describe("Tests", () => {
   it("storage test", async function() {
-    const file = (isNode ? "node_" : "browser_") + `file_path.json`;
+    const file = (isNode ? "node_" : "browser_") + `core_v1_file_path.json`;
     console.log(`env.TEST_MODE = ${env.TEST_MODE}`);
-    const recorder = new RecordingHttpClient(file, isPlaybackMode());
+    const recorder = new TestProxyHttpClientCoreV1(file, isPlaybackMode());
     const options: StoragePipelineOptions = {};
     if (!isLiveMode()) {
       options.httpClient = recorder;
     }
     const client = new QueueServiceClient(env.STORAGE_SAS_URL, undefined, options);
-    await client.createQueue((isNode ? "node_" : "browser_") + 1320);
-    await recorder.stop();
+    if (!isLiveMode()) await recorder.start();
+
+    await client.createQueue((isNode ? "node-" : "browser-") + "1320");
+
+    if (!isLiveMode()) await recorder.stop();
   });
 });
