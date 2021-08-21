937be42713ca475302515f3c895d8cbfd7e4843c
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2021-08-21 01:44:27 +0000

    comment improvements

--- sdk/identity/identity/src/credentials/onBehalfOfCredential.ts
+++ sdk/identity/identity/src/credentials/onBehalfOfCredential.ts
@@ -12,20 +12,29 @@ import { OnBehalfOfCredentialOptions } from "./onBehalfOfCredentialOptions";
 const logger = credentialLogger("OnBehalfOfCredential");
 
 /**
- * Enables authentication to Azure Active Directory using the On Behalf Of flow.:
- * https://docs.microsoft.com/azure/active-directory/develop/v2-oauth2-on-behalf-of-flow
+ * Enables authentication to Azure Active Directory using the [On Behalf Of flow](https://docs.microsoft.com/azure/active-directory/develop/v2-oauth2-on-behalf-of-flow).
  */
 export class OnBehalfOfCredential implements TokenCredential {
   private msalFlows: WeakMap<UserAssertion, MsalFlow>;
 
   /**
-   * Creates an instance of the OnBehalfOfCredential with the details
+   * Creates an instance of the {@link OnBehalfOfCredential} with the details
    * needed to authenticate against Azure Active Directory with a client
    * secret, and an user assertion.
    *
    * The user assertion needs to be sent through the `getToken` method.
    * Azure SDK clients will be able to specify the user assertion through
    * the `authenticationOptions` in the optional parameters of the SDK client methods.
+   *
+   * Example using the `KeyClient` from [\@azure/keyvault-keys](https://www.npmjs.com/package/\@azure/keyvault-keys):
+   *
+   * ```ts
+   * const tokenCredential = new OnBehalfOfCredential(tenantId, clientId, clientSecret);
+   * const client = new KeyClient("vault-url", tokenCredential);
+   *
+   * const userAssertions = createUserAssertion("access-token");
+   * await client.getKey("key-name", { authenticationOptions: { userAssertion } });
+   * ```
    *
    * @param tenantId - The Azure Active Directory tenant (directory) ID.
    * @param clientId - The client (application) ID of an App Registration in the tenant.
@@ -45,8 +54,11 @@ export class OnBehalfOfCredential implements TokenCredential {
    * Authenticates with Azure Active Directory and returns an access token if successful.
    * If authentication fails, a {@link CredentialUnavailableError} will be thrown with the details of the failure.
    *
-   * The Azure SDK clients that support TokenCredentials will allow passing an `authenticationOptions` object
-   * through the optional parameters of their methods. There, the user assertion can be passed, as follows:
+   * The user assertion needs to be sent through the `getToken` method.
+   * Azure SDK clients will be able to specify the user assertion through
+   * the `authenticationOptions` in the optional parameters of the SDK client methods.
+   *
+   * Example using the `KeyClient` from [\@azure/keyvault-keys](https://www.npmjs.com/package/\@azure/keyvault-keys):
    *
    * ```ts
    * const tokenCredential = new OnBehalfOfCredential(tenantId, clientId, clientSecret);
