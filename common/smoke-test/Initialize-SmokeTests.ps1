d288eaccac80bab9277aeee202dba654030d4791
Albert Cheng <albertcheng@microsoft.com>
2021-08-24 12:53:34 -0700

    Smoke test isolation per package

--- common/smoke-test/Initialize-SmokeTests.ps1
+++ common/smoke-test/Initialize-SmokeTests.ps1
@@ -91,6 +91,10 @@ function Set-EnvironmentVariables {
 function New-DeployManifest {
   Write-Verbose "Detecting samples..."
   $javascriptSamples = Get-ChildItem -Path "$repoRoot/sdk/$ServiceDirectory/*/samples/javascript/" -Recurse -Include package.json
+  tree $repoRoot/sdk/$ServiceDirectory /F
+  Write-Host "xxxxxx"
+  dir $repoRoot/sdk/$ServiceDirectory/*/samples/javascript/*/package.json
+  Write-Host "xxxxxx"
   Write-Host "Found samples:"
   Write-Host $($javascriptSamples)
   #(Get-ChildItem -Path "$repoRoot/sdk/$ServiceDirectory/*/samples/javascript/" -Directory
