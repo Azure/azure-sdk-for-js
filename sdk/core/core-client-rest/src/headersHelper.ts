118ef750b254d3b3c5f55396870cededbf6cd28b
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-08-25 16:45:14 -0700

    use String(value)

--- sdk/core/core-client-rest/src/headersHelper.ts
+++ sdk/core/core-client-rest/src/headersHelper.ts
@@ -5,12 +5,12 @@ import { RawHttpHeaders as CoreRawHeaders } from "@azure/core-rest-pipeline";
  * Converts a REST client headers to core-rest-pipeline compatible headers
  * REST clients allow headers to be number, boolean or strings, this function
  * stringifies these headers
- * @returns
+ * @returns all headers values converted to strings
  */
 export function toCoreRawHeaders(headers: RawHttpHeaders = {}): CoreRawHeaders {
   let coreHeaders: CoreRawHeaders = {};
   for (const [name, value] of Object.entries(headers)) {
-    coreHeaders[name] = value.toString();
+    coreHeaders[name] = String(value);
   }
   return coreHeaders;
 }
