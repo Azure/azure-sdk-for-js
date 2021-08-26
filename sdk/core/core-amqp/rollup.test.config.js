c74346d9ac3fdd8e3ccb03371ef2975279191eff
Jeremy Meng <jeremy.ymeng@gmail.com>
2021-08-26 20:34:03 +0000

    Remove rollup.test.config.js
    as test rollup configs are now in rollup.config.js

--- sdk/core/core-amqp/rollup.test.config.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import * as base from "./rollup.base.config";
-
-// Node tests are run via ts-node
-export default [base.browserConfig(true)];
