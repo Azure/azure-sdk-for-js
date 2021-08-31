9b91597adea0fcf9c3f30ab25c9e1cef080028fc
Albert Cheng <albertcheng@microsoft.com>
2021-08-30 22:44:23 -0700

    Fix

--- common/smoke-test/run.js
+++ common/smoke-test/run.js
@@ -14,7 +14,7 @@ async function main() {
     console.log(`Importing samples for ${entry.Name}...`);
 
     // Read configuration from package.json's //sampleConfiguration field
-    const packageJson = require(`${entry.PackageDirectory}v1/javascript/package.json`);
+    const packageJson = require(`${entry.SamplesDirectory}/package.json`);
     const smokeTestConfig = packageJson["//sampleConfiguration"] || {};
 
     if (smokeTestConfig.skipFolder) {
