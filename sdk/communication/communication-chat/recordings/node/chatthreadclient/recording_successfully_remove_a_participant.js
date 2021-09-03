84f8c500c865c89b57924bf83ce0c04f39982b57
Lu Chen <luc@microsoft.com>
2021-09-03 12:30:08 -0700

    update recordings

--- sdk/communication/communication-chat/recordings/node/chatthreadclient/recording_successfully_remove_a_participant.js
+++ sdk/communication/communication-chat/recordings/node/chatthreadclient/recording_successfully_remove_a_participant.js
@@ -5,21 +5,21 @@ module.exports.hash = "3f3e581d127d4ee7534cf66acf4944a1";
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
 nock('https://endpoint', {"encodedQueryParams":true})
-  .post('/chat/threads/19%3AoXeDm2YWQ-rjbfG0ilcCtPs5GBwZEeVt5ssFsYc0Ehc1%40thread.v2/participants/:remove', {"communicationUser":{"id":"8:acs:1b5cc06b-f352-4571-b1e6-d9b259b7c776_0000000a-ac42-820c-7679-5b3a0d000827"}})
+  .post('/chat/threads/19%3AGJhzTT5V5GxxOF-a6Z84_zITnb7xu-IRf-8431aqf8w1%40thread.v2/participants/:remove', {"communicationUser":{"id":"8:acs:fa5c4fc3-a269-43e2-9eb6-0ca17b388993_0000000c-4d02-cfd1-8e0e-454822000d96"}})
   .query(true)
   .reply(204, "", [
   'MS-CV',
-  'qFO+14I8ZUqaoGOVHSuw/w.0',
+  'tIHXBD0bxEi0OIEj11c6hw.0',
   'Strict-Transport-Security',
   'max-age=2592000',
   'api-supported-versions',
-  '2020-11-01-preview3, 2021-01-27-preview4, 2021-03-01-preview5, 2021-03-07, 2021-04-05-preview6',
+  '2020-11-01-preview3, 2021-01-27-preview4, 2021-03-01-preview5, 2021-03-07, 2021-04-05-preview6, 2021-09-07',
   'X-Processing-Time',
-  '514ms',
+  '211ms',
   'X-Cache',
   'CONFIG_NOCACHE',
   'X-Azure-Ref',
-  '06MbHYAAAAADOHuUDKsoARoYH96bjACYyUERYMzFFREdFMDIxNgA3MDU0Mzk1ZS1jZTFkLTQ1NWUtYWU1ZC0yMzNjYTgzOTA1NTQ=',
+  '0GHcyYQAAAADFnPaj6s4NTIzeyUOKZRhqUERYMzFFREdFMDIxOQA5ZmM3YjUxOS1hOGNjLTRmODktOTM1ZS1jOTE0OGFlMDllODE=',
   'Date',
-  'Mon, 14 Jun 2021 21:15:20 GMT'
+  'Fri, 03 Sep 2021 19:27:20 GMT'
 ]);
