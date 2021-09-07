13b312ee6cf12a0e9ed2e76ba8fdce767905136c
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2021-09-07 08:53:38 -0700

    Remove workaround for quotes

--- sdk/test-utils/recorder/src/defaultCustomizations.ts
+++ sdk/test-utils/recorder/src/defaultCustomizations.ts
@@ -1,6 +1,5 @@
 import {
   decodeHexEncodingIfExistsInNockFixture,
-  handleSingleQuotesInUrlPath,
   isBrowser,
   maskAccessTokenInBrowserRecording,
   maskAccessTokenInNockFixture,
@@ -10,10 +9,6 @@ import {
 export const defaultCustomizationsForNodeRecordings = [
   // Decodes "hex" strings in the response from the recorded fixture if any exists.
   decodeHexEncodingIfExistsInNockFixture,
-  // Nock bug: Single quotes in the path of the url are not handled by nock.
-  // (Link to the bug 🐛: https://github.com/nock/nock/issues/2136)
-  // The following is the workaround we use in the recorder until nock fixes it.
-  handleSingleQuotesInUrlPath,
   // Masks "access_token"s in the json response from the recording if any exists.
   maskAccessTokenInNockFixture,
   // Sanitizes the scope url in the request bodies to clean false positives in cred-scan reports
