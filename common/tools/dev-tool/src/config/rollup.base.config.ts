1a593e5b183daa62c94146c525306f8ea51446f5
Jeremy Meng <yumeng@microsoft.com>
2021-09-15 11:49:03 -0700

    Bump pnpm version to 6.14.2 (#17017)
    pnpm 6.13.0 includes fix to the "tar" security vulnerability issue.
    
    * pnpm v6 rename pnpmfile.js to .pnpmfile.cjs
    
    See https://github.com/pnpm/pnpm/releases/tag/v6.0.0 for details.
    
    * Add changes done by `rush update`
    
    * rush update --recheck
    
    * bump pnpm version to current latest version 6.14.2
    
    * Adapt to pnpm change
    
    to reduce directory nesting in virtual store, pnpm v6 replaces '/' in package
    name with '+'.
    
    https://github.com/pnpm/pnpm/pull/3117
    https://github.com/pnpm/pnpm/pull/3314
    
    This breaks `communication-chat` which has an
    indirect dependency on earlier version of @opentelemetry/api that doesn't
    provide `esm` support thus needs the commonjs plugin's named exports when
    rolling up for browser.
    
    * Fix native deps installation script

--- common/tools/dev-tool/src/config/rollup.base.config.ts
+++ common/tools/dev-tool/src/config/rollup.base.config.ts
@@ -44,7 +44,7 @@ export function openTelemetryCommonJs(): Record<string, string[]> {
   for (const version of releasedOpenTelemetryVersions) {
     namedExports[
       // working around a limitation in the rollup common.js plugin - it's not able to resolve these modules so the named exports listed above will not get applied. We have to drill down to the actual path.
-      `../../../common/temp/node_modules/.pnpm/@opentelemetry/api@${version}/node_modules/@opentelemetry/api/build/src/index.js`
+      `../../../common/temp/node_modules/.pnpm/@opentelemetry+api@${version}/node_modules/@opentelemetry/api/build/src/index.js`
     ] = [
       "SpanKind",
       "TraceFlags",
