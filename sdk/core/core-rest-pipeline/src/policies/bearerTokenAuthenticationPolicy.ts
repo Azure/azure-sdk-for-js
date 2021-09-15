4b36f5a4ed21875531b57e009456f090902eaac7
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2021-09-15 02:28:04 +0000

    unnecessary change undone

--- sdk/core/core-rest-pipeline/src/policies/bearerTokenAuthenticationPolicy.ts
+++ sdk/core/core-rest-pipeline/src/policies/bearerTokenAuthenticationPolicy.ts
@@ -90,9 +90,8 @@ export interface BearerTokenAuthenticationPolicyOptions {
 
 /**
  * Default authorize request handler
- * @internal
  */
-export async function defaultAuthorizeRequest(options: AuthorizeRequestOptions): Promise<void> {
+async function defaultAuthorizeRequest(options: AuthorizeRequestOptions): Promise<void> {
   const { scopes, getAccessToken, request } = options;
   const getTokenOptions: GetTokenOptions = {
     abortSignal: request.abortSignal,
