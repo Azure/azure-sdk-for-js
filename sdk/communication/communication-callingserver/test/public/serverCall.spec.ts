6975f3a0a9c706c2a44d00ae63268fda34d8eaab
Franko Morales <fmorales@microsoft.com>
2021-09-24 14:32:07 -0700

    First test case

---
+++ sdk/communication/communication-callingserver/test/public/serverCall.spec.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { CallingServerClient, ServerCall } from "@azure/communication-callingserver"
+
+describe("ServerCall", function() {
+    let callingServerClient: CallingServerClient;
+
+    describe("Recording", function() {
+        it("Start recording with relative url fails", function() {
+            let server_call: ServerCall;
+
+            const connectionString = "endpoint=https://REDACTED.communication.azure.com/;accesskey=eyJhbG==";
+            const serverCallId = "aHR0cHM6Ly9jb252LXVzd2UtMDguY29udi5za3lwZS5jb20vY29udi8tby1FWjVpMHJrS3RFTDBNd0FST1J3P2k9ODgmZT02Mzc1Nzc0MTY4MDc4MjQyOTM";
+            callingServerClient = new CallingServerClient(connectionString);
+            server_call = callingServerClient.initializeServerCall(serverCallId);
+
+            server_call.startRecording("/not/absolute/uri");
+        })
+    })
+})
