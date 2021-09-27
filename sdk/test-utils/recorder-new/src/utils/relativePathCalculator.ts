6cb397cfe3697a80b16218d04308dd4941d52d0e
Harsha Nalluru <sanallur@microsoft.com>
2021-09-27 00:13:22 +0000

    relativeRecordingsPathForNode

--- sdk/test-utils/recorder-new/src/utils/relativePathCalculator.ts
+++ sdk/test-utils/recorder-new/src/utils/relativePathCalculator.ts
@@ -10,10 +10,19 @@ export function relativeRecordingsPathForNode(testContext: Mocha.Test) {
   if (!testAbsolutePath) {
     throw new RecorderError("Unable to grab the file path from the test run");
   }
-  // TO generate "sdk/test-utils/testing-recorder-new/recordings"
-  console.log(findRecordingsFolderPath(testAbsolutePath));
-  // Calculate and return
-  return testAbsolutePath;
+
+  const recordingsPath = findRecordingsFolderPath(testAbsolutePath);
+  const recordingsFolder = path.basename(recordingsPath);
+  const projectFolder = path.basename(path.resolve(recordingsPath, ".."));
+  const serviceFolder = path.basename(path.resolve(recordingsPath, "..", ".."));
+  const sdk = path.basename(path.resolve(recordingsPath, "..", "..", ".."));
+  if (sdk !== "sdk") {
+    throw new Error("Unexpected location for recordings, please fix the location.");
+  }
+  return path
+    .join(sdk, serviceFolder, projectFolder, recordingsFolder)
+    .split(path.sep)
+    .join(path.posix.sep);
 }
 
 export function relativeRecordingsPathForBrowser() {
