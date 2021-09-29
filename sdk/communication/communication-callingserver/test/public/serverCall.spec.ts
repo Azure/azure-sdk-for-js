09bc9298511c208677bf3359a25ae56815dc3fa4
Franko Morales <fmorales@microsoft.com>
2021-09-29 16:55:26 -0700

    Refactored Live Testing

--- sdk/communication/communication-callingserver/test/public/serverCall.spec.ts
+++ sdk/communication/communication-callingserver/test/public/serverCall.spec.ts
@@ -2,10 +2,10 @@
 // Licensed under the MIT license.
 
 import { record, Recorder, RecorderEnvironmentSetup } from "@azure-tools/test-recorder";
-import { env, isLiveMode, isRecordMode } from "@azure/test-utils-recorder";
-import { v4 as uuidv4, v5 as uuidv5 } from "uuid";
-import { CommunicationIdentityClient } from "@azure/communication-identity";
-import { CallingServerClient, MediaType, EventSubscriptionType } from "../../src";
+import { env } from "@azure/test-utils-recorder";
+import { CallingServerClient } from "../../src";
+import { CALLBACK_URI  } from "./utils/constants";
+import { TestUtils } from "./utils/testUtils";
 import assert from "assert";
 
 const replaceableVariables: { [k: string]: string } = {
@@ -14,37 +14,15 @@ const replaceableVariables: { [k: string]: string } = {
 
 const environmentSetup: RecorderEnvironmentSetup = {
     replaceableVariables,
-    customizationsOnRecordings: [
-      (recording: string): string => recording.replace(/"token"\s?:\s?"[^"]*"/g, `"token":"token"`),
-      (recording: string): string => recording.replace(/(https:\/\/)([^/',]*)/, "$1endpoint"),
-      (recording: string): string => recording.replace("endpoint:443", "endpoint")
-    ],
+    customizationsOnRecordings: [],
     queryParametersToSkip: []
   };
 
 describe("Server Call", function() {
-    let communicationIdentityClient = new CommunicationIdentityClient(env.COMMUNICATION_LIVETEST_DYNAMIC_CONNECTION_STRING);
 
     describe("Recording Operations", function() {
 
         let recorder: Recorder;
-        function delay(ms: number) {
-            return new Promise( resolve => setTimeout(resolve, ms) );
-        }
-        
-        async function getUserId(userName: string) {
-          if (isLiveMode()){
-            return (await communicationIdentityClient.createUser()).communicationUserId;
-          }
-        
-          return "8:acs:" + env.AZURE_TENANT_ID + "_" + uuidv5(userName, "6ba7b812-9dad-11d1-80b4-00c04fd430c8");
-        }
-
-        async function delayIfLive() {
-          if (isLiveMode() || isRecordMode()) {
-            await delay(10000);
-          }
-        }
 
         beforeEach(async function() {
             recorder = record(this, environmentSetup);
@@ -58,46 +36,35 @@ describe("Server Call", function() {
 
         it("Run all client recording operations", async function() {
             this.timeout(0);
-            var groupId;
-            if (isLiveMode()) {
-              groupId = uuidv4();
-            } else {
-              groupId = uuidv5("Run all client recording operations", "6ba7b812-9dad-11d1-80b4-00c04fd430c8");
-            }
-            var fromUser = await getUserId("fromUser");
-            var toUser = await getUserId("toUser");
-            // var fromUser = "8:acs:016a7064-0581-40b9-be73-6dde64d69d72_f30d3598-6a48-4390-9324-6f14ffbf33c8";
-            // var toUser = "8:acs:016a7064-0581-40b9-be73-6dde64d69d72_b8cec067-f0b8-4ada-8987-25286a303880";
-            var callingServer = new CallingServerClient(env.COMMUNICATION_LIVETEST_DYNAMIC_CONNECTION_STRING);
-            var joinCallOptions = {
-                    callbackUri: "https://bot.contoso.io/callback",
-                    requestedMediaTypes: [MediaType.Audio],
-                    requestedCallEvents: [EventSubscriptionType.ParticipantsUpdated]
-                };
+            var groupId = TestUtils.getGroupId("Run all client recording operations");
+            
+            var fromUser = await TestUtils.getUserId("fromUser");
+            var toUser = await TestUtils.getUserId("toUser");
 
             var connections = [];
             var recordingId = "";
             var serverCall = null;
 
+            var callingServer = new CallingServerClient(env.COMMUNICATION_LIVETEST_DYNAMIC_CONNECTION_STRING);
+
             try {
-              connections.push(await callingServer.joinCall(groupId, {communicationUserId: fromUser}, joinCallOptions));
-              connections.push(await callingServer.joinCall(groupId, {communicationUserId: toUser}, joinCallOptions));
-              
+              connections = await TestUtils.createCallConnections(callingServer, groupId, fromUser, toUser);
+
               serverCall = callingServer.initializeServerCall(groupId);
-              var startCallRecordingResult = await serverCall.startRecording("https://bot.contoso.io/callback");
+              var startCallRecordingResult = await serverCall.startRecording(CALLBACK_URI);
               recordingId = startCallRecordingResult.recordingId!;
               assert.notStrictEqual(serverCall.serverCallId, undefined);
-              await delayIfLive();
+              await TestUtils.delayIfLive();
               var recordingState = await serverCall.getRecordingState(recordingId!);
               assert.strictEqual(recordingState.recordingState, "active");
               
               await serverCall.pauseRecording(recordingId!);
-              await delayIfLive();
+              await TestUtils.delayIfLive();
               var recordingState = await serverCall.getRecordingState(recordingId!);
               assert.strictEqual(recordingState.recordingState, "inactive");
 
               await serverCall.resumeRecording(recordingId!);
-              await delayIfLive();
+              await TestUtils.delayIfLive();
               var recordingState = await serverCall.getRecordingState(recordingId!);
               assert.strictEqual(recordingState.recordingState, "active");
 
@@ -113,9 +80,7 @@ describe("Server Call", function() {
               }
             }
             
-            connections.forEach(async (connection) => {
-              await connection.hangUp();
-            });
+            TestUtils.cleanCallConnections(connections);
         })
     })
 })
---
+++ sdk/communication/communication-callingserver/test/public/utils/testUtils.ts
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { env, isLiveMode, isRecordMode } from "@azure/test-utils-recorder";
+import { v4 as uuidv4, v5 as uuidv5 } from "uuid";
+import { CallingServerClient, MediaType, EventSubscriptionType, CallConnection } from "../../../src";
+import { CommunicationIdentityClient } from "@azure/communication-identity";
+import  * as Constants from "../utils/constants";
+
+export class TestUtils {
+    private static delay(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms) );
+    }
+    
+    public static async getUserId(userName: string) {
+      var communicationIdentityClient = new CommunicationIdentityClient(env.COMMUNICATION_LIVETEST_DYNAMIC_CONNECTION_STRING);
+      if (isLiveMode()){
+        return (await communicationIdentityClient.createUser()).communicationUserId;
+      }
+    
+      return "8:acs:" + env.AZURE_TENANT_ID + "_" + uuidv5(userName, Constants.NAMESPACE_UUID);
+    }
+
+    public static getGroupId(testName: string) {
+      if (isLiveMode()) {
+        return uuidv4();
+      }
+      return uuidv5(testName, Constants.NAMESPACE_UUID);
+    }
+
+    public static async createCallConnections(
+      callingServerClient: CallingServerClient, 
+      groupId: string,
+      fromUser: string,
+      toUser: string) {
+      
+      const joinCallOptions = {
+          callbackUri: Constants.CALLBACK_URI,
+          requestedMediaTypes: [MediaType.Audio],
+          requestedCallEvents: [EventSubscriptionType.ParticipantsUpdated]
+      };
+      var callConnections = [];
+      callConnections.push(await callingServerClient.joinCall(groupId, {communicationUserId: fromUser}, joinCallOptions));
+      callConnections.push(await callingServerClient.joinCall(groupId, {communicationUserId: toUser}, joinCallOptions));
+
+      return callConnections;
+    }
+
+    public static async delayIfLive() {
+      if (isLiveMode() || isRecordMode()) {
+        await this.delay(10000);
+      }
+    }
+
+    public static async cleanCallConnections(callConnections: Array<CallConnection>) {
+      callConnections.forEach(async (connection) => {
+        await connection.hangUp();
+      });
+    }
+}
