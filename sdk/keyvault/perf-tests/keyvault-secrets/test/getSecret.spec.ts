223e2853b7d8dd435a5659bb6a32e22d94102187
Mike Harder <mharder@microsoft.com>
2021-10-04 14:51:15 -0700

    [Perf] Update keyvault-secrets test to match .NET (#17799)
    - Move single-secret code from SecretTest to GetSecretTest
    - Ensure vault contains 0 secrets before ListSecretsTest
    - Delete and purge all created secrets

--- sdk/keyvault/perf-tests/keyvault-secrets/test/getSecret.spec.ts
+++ sdk/keyvault/perf-tests/keyvault-secrets/test/getSecret.spec.ts
@@ -1,9 +1,20 @@
 import { SecretTest } from "./secretTest";
+import { v4 as uuid } from "uuid";
 
 export class GetSecretTest extends SecretTest {
+  static secretName = `s-${uuid()}`;
+
   public options = {};
 
+  async globalSetup() {
+    await this.secretClient.setSecret(GetSecretTest.secretName, "value");
+  }
+
   async runAsync(): Promise<void> {
-    await this.secretClient.getSecret(SecretTest.secretName);
+    await this.secretClient.getSecret(GetSecretTest.secretName);
+  }
+
+  async globalCleanup() {
+    await this.deleteAndPurgeSecrets(GetSecretTest.secretName);
   }
 }
