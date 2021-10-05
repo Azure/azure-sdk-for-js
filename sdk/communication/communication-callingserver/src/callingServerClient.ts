1177fe380d27e7c16fa42cb8e37f71010fa3e59f
Ravi Thanneeru <v-ratann@microsoft.com>
2021-10-05 23:52:56 +0000

    Call recording APIs initial check in

--- sdk/communication/communication-callingserver/src/callingServerClient.ts
+++ sdk/communication/communication-callingserver/src/callingServerClient.ts
@@ -10,7 +10,12 @@ import {
   PlayAudioOptions,
   AddParticipantOptions,
   RemoveParticipantOptions,
-  CancelMediaOperationOptions
+  CancelMediaOperationOptions,
+  StartRecordingOptions,
+  PauseRecordingOptions,
+  ResumeRecordingOptions,
+  StopRecordingOptions,
+  GetRecordingPropertiesOptions
 } from "./models";
 import { CallConnections, ServerCalls } from "./generated/src/operations";
 import {
@@ -23,7 +28,11 @@ import {
   AddParticipantWithCallLocatorRequest,
   RemoveParticipantWithCallLocatorRequest,
   CancelMediaOperationWithCallLocatorRequest,
-  CancelParticipantMediaOperationWithCallLocatorRequest
+  CancelParticipantMediaOperationWithCallLocatorRequest,
+  StartCallRecordingRequest,
+  StartCallRecordingResult,
+  StartCallRecordingWithCallLocatorRequest,
+  CallRecordingProperties
 } from "./generated/src/models";
 import { TokenCredential } from "@azure/core-auth";
 
@@ -38,7 +47,8 @@ import {
   PipelineOptions,
   InternalPipelineOptions,
   createPipelineFromOptions,
-  operationOptionsToRequestOptionsBase
+  operationOptionsToRequestOptionsBase,
+  RestResponse
 } from "@azure/core-http";
 import { SpanStatusCode } from "@azure/core-tracing";
 import { CallingServerApiClient } from "./generated/src/callingServerApiClient";
@@ -46,11 +56,12 @@ import { SDK_VERSION } from "./constants";
 import { createSpan } from "./tracing";
 import { logger } from "./logger";
 import { extractOperationOptions } from "./extractOperationOptions";
+import { CallingServerUtils } from './utils/utils'
 
 /**
  * Client options used to configure CallingServer Client API requests.
  */
-export interface CallingServerClientOptions extends PipelineOptions {}
+export interface CallingServerClientOptions extends PipelineOptions { }
 
 /**
  * Checks whether the type of a value is CallingServerClientOptions or not.
@@ -88,7 +99,7 @@ export class CallingServerClient {
     connectionStringOrUrl: string,
     credentialOrOptions?: TokenCredential | CallingServerClientOptions,
     maybeOptions: CallingServerClientOptions = {}
-    ) {
+  ) {
     const { url, credential } = parseClientArguments(connectionStringOrUrl, credentialOrOptions);
     const options = isCallingServerClientOptions(credentialOrOptions) ? credentialOrOptions : maybeOptions;
     const libInfo = `azsdk-js-communication-callingserver/${SDK_VERSION}`;
@@ -137,7 +148,7 @@ export class CallingServerClient {
     source: CommunicationIdentifier,
     targets: CommunicationIdentifier[],
     options: CreateCallOptions
-    ): Promise<CallConnection> {
+  ): Promise<CallConnection> {
     const { operationOptions, restOptions } = extractOperationOptions(options);
     const { span, updatedOptions } = createSpan(
       "CallConnectionRestClient-CreateCallConnection",
@@ -151,7 +162,7 @@ export class CallingServerClient {
       requestedMediaTypes: restOptions.requestedMediaTypes,
       requestedCallEvents: restOptions.requestedCallEvents,
       alternateCallerId:
-      restOptions.alternateCallerId == null
+        restOptions.alternateCallerId == null
           ? undefined
           : { value: restOptions.alternateCallerId.phoneNumber },
       subject: restOptions.subject
@@ -178,13 +189,13 @@ export class CallingServerClient {
     }
   }
 
-   /**
-   * Join the call using callLocator.
-   *
-   * @param callLocator - The callLocator contains call id.
-   * @param source - The source identity.
-   * @param options - Additional request options contains joinCall api options.
-   */
+  /**
+  * Join the call using callLocator.
+  *
+  * @param callLocator - The callLocator contains call id.
+  * @param source - The source identity.
+  * @param options - Additional request options contains joinCall api options.
+  */
   public async joinCall(
     callLocator: CallLocator,
     source: CommunicationIdentifier,
@@ -235,10 +246,21 @@ export class CallingServerClient {
     callLocator: CallLocator,
     audioFileUri: string,
     options: PlayAudioOptions
-    ): Promise<PlayAudioResult> {
+  ): Promise<PlayAudioResult> {
     const { operationOptions, restOptions } = extractOperationOptions(options);
     const { span, updatedOptions } = createSpan("ServerCallRestClient-playAudio", operationOptions);
-
+    if (!CallingServerUtils.isValidUrl(audioFileUri)) {
+      throw new Error('audioFileUri is invalid.')
+    }
+    if (typeof options.audioFileId === 'undefined' || !options.audioFileId || !options.audioFileId.trim()) {
+      throw new Error('audioFileId is invalid.')
+    }
+    if (!CallingServerUtils.isValidUrl(String(options.callbackUri))) {
+      throw new Error('callbackUri is invalid.')
+    }
+    if (typeof options.operationContext === 'undefined' || !options.operationContext || !options.operationContext.trim()) {
+      throw new Error('operationContext can not be null.')
+    }
     const request: PlayAudioWithCallLocatorRequest = {
       callLocator: callLocator,
       playAudioRequest: {
@@ -280,7 +302,7 @@ export class CallingServerClient {
     participant: CommunicationIdentifier,
     audioFileUri: string,
     options: PlayAudioOptions
-    ): Promise<PlayAudioResult> {
+  ): Promise<PlayAudioResult> {
     const { operationOptions, restOptions } = extractOperationOptions(options);
     const { span, updatedOptions } = createSpan("ServerCallRestClient-playAudio", operationOptions);
 
@@ -330,7 +352,7 @@ export class CallingServerClient {
     alternateCallerId?: string,
     operationContext?: string,
     options: AddParticipantOptions = {}
-    ): Promise<AddParticipantResult> {
+  ): Promise<AddParticipantResult> {
     const { span, updatedOptions } = createSpan("ServerCallRestClient-playAudio", options);
     var alternate_caller_id = typeof alternateCallerId === "undefined" ? alternateCallerId : serializeCommunicationIdentifier({ phoneNumber: alternateCallerId }).phoneNumber;
 
@@ -368,11 +390,11 @@ export class CallingServerClient {
    * @param participant - The identifier of the participant.
    * @param options - Additional request options contains removeParticipant api options.
    */
-   public async removeParticipant(
+  public async removeParticipant(
     callLocator: CallLocator,
     participant: CommunicationIdentifier,
     options: RemoveParticipantOptions = {}
-    ): Promise<void> {
+  ): Promise<void> {
     const { span, updatedOptions } = createSpan("ServerCallRestClient-removeParticipant", options);
 
     const request: RemoveParticipantWithCallLocatorRequest = {
@@ -405,11 +427,11 @@ export class CallingServerClient {
    * @param mediaOperationId - The operationId of the media operation to cancel.
    * @param options - Additional request options contains cancelMediaOperation api options.
    */
-   public async cancelMediaOperation(
+  public async cancelMediaOperation(
     callLocator: CallLocator,
     mediaOperationId: string,
     options: CancelMediaOperationOptions = {}
-    ): Promise<void> {
+  ): Promise<void> {
     const { span, updatedOptions } = createSpan("ServerCallRestClient-cancelMediaOperation", options);
 
     const request: CancelMediaOperationWithCallLocatorRequest = {
@@ -443,12 +465,12 @@ export class CallingServerClient {
    * @param mediaOperationId - The operationId of the media operation to cancel.
    * @param options - Additional request options contains cancelMediaOperation api options.
    */
-   public async cancelParticipantMediaOperation(
+  public async cancelParticipantMediaOperation(
     callLocator: CallLocator,
     participant: CommunicationIdentifier,
     mediaOperationId: string,
     options: CancelMediaOperationOptions = {}
-    ): Promise<void> {
+  ): Promise<void> {
     const { span, updatedOptions } = createSpan("ServerCallRestClient-cancelParticipantMediaOperation", options);
 
     const request: CancelParticipantMediaOperationWithCallLocatorRequest = {
@@ -464,6 +486,185 @@ export class CallingServerClient {
         request,
         operationOptionsToRequestOptionsBase(updatedOptions)
       );
+    } catch (e) {
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
+  }
+
+  /**
+   * Start recording operation using call locator.
+   *
+   * @param callLocator - The callLocator contains server call id.
+   * @param recordingStateCallbackUri - The call back uri for recording state.
+   * @param options - Additional request options contains StartRecording api options.
+   */
+  public async startRecording(
+    callLocator: CallLocator,
+    recordingStateCallbackUri: string,
+    options: StartRecordingOptions = {}
+  ): Promise<StartCallRecordingResult> {
+    const { span, updatedOptions } = createSpan("ServerCallRestClient-StartRecording", options);
+
+    if (typeof callLocator === 'undefined' || !callLocator) {
+      throw new Error('callLocator is invalid.');
+    }
+    if (typeof recordingStateCallbackUri !== 'undefined' ||
+      !recordingStateCallbackUri ||
+      !CallingServerUtils.isValidUrl(recordingStateCallbackUri)) {
+      throw new Error('recordingStateCallbackUri is invalid.');
+    }
+
+    var startCallRecordingRequest: StartCallRecordingRequest = { recordingStateCallbackUri: recordingStateCallbackUri };
+
+    var startCallRecordingWithCallLocatorRequest: StartCallRecordingWithCallLocatorRequest = {
+      callLocator: callLocator,
+      'startCallRecordingRequest': startCallRecordingRequest
+    };
+
+    try {
+      const result = await this.serverCallRestClient.startRecording(
+        startCallRecordingWithCallLocatorRequest,
+        operationOptionsToRequestOptionsBase(updatedOptions)
+      );
+      return result;
+    } catch (e) {
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
+  }
+
+  /**
+   * Pause recording operation using recording id.
+   *
+   * @param recordingId - The recording id of the ongoing recording.
+   * @param options - Additional request options contains PauseRecording api options.
+   */
+  public async pauseRecording(
+    recordingId: string,
+    options: PauseRecordingOptions = {}
+  ): Promise<RestResponse> {
+    const { span, updatedOptions } = createSpan("ServerCallRestClient-PauseRecording", options);
+
+    if (typeof recordingId === 'undefined' || !recordingId || !recordingId.trim()) {
+      throw new Error('recordingId is invalid.')
+    }
+
+    try {
+      const result = await this.serverCallRestClient.pauseRecording(
+        recordingId,
+        operationOptionsToRequestOptionsBase(updatedOptions)
+      );
+      return result;
+    } catch (e) {
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
+  }
+
+  /**
+   * Resume recording operation using recording id.
+   *
+   * @param recordingId - The recording id of the ongoing recording.
+   * @param options - Additional request options contains ResumeRecording api options.
+   */
+  public async resumeRecording(
+    recordingId: string,
+    options: ResumeRecordingOptions = {}
+  ): Promise<RestResponse> {
+    const { span, updatedOptions } = createSpan("ServerCallRestClient-ResumeRecording", options);
+
+    if (typeof recordingId === 'undefined' || !recordingId || !recordingId.trim()) {
+      throw new Error('recordingId is invalid.')
+    }
+
+    try {
+      const result = await this.serverCallRestClient.resumeRecording(
+        recordingId,
+        operationOptionsToRequestOptionsBase(updatedOptions)
+      );
+      return result;
+    } catch (e) {
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
+  }
+
+  /**
+   * Stop recording operation using recording id.
+   *
+   * @param recordingId - The recording id of the ongoing recording.
+   * @param options - Additional request options contains StopRecording api options.
+   */
+  public async stopRecording(
+    recordingId: string,
+    options: StopRecordingOptions = {}
+  ): Promise<RestResponse> {
+    const { span, updatedOptions } = createSpan("ServerCallRestClient-StopRecording", options);
+
+    if (typeof recordingId === 'undefined' || !recordingId || !recordingId.trim()) {
+      throw new Error('recordingId is invalid.')
+    }
+
+    try {
+      const result = await this.serverCallRestClient.stopRecording(
+        recordingId,
+        operationOptionsToRequestOptionsBase(updatedOptions)
+      );
+      return result;
+    } catch (e) {
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: e.message
+      });
+      throw e;
+    } finally {
+      span.end();
+    }
+  }
+
+  /**
+   * Get recording properties using recording id.
+   *
+   * @param recordingId - The recording id of the ongoing recording.
+   * @param options - Additional request options contains GetRecordingState api options.
+   */
+  public async getRecordingProperties(
+    recordingId: string,
+    options: GetRecordingPropertiesOptions = {}
+  ): Promise<CallRecordingProperties> {
+    const { span, updatedOptions } = createSpan("ServerCallRestClient-Recording", options);
+
+    if (typeof recordingId === 'undefined' || !recordingId || !recordingId.trim()) {
+      throw new Error('recordingId is invalid.')
+    }
+
+    try {
+      const result = await this.serverCallRestClient.getRecordingProperties(
+        recordingId,
+        operationOptionsToRequestOptionsBase(updatedOptions)
+      );
+      return result;
     } catch (e) {
       span.setStatus({
         code: SpanStatusCode.ERROR,
