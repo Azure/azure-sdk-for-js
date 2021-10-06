a3d86121bb73ff87157aa66329bfc06c96dc30ed
KarishmaGhiya <kaghiya@microsoft.com>
2021-10-06 11:17:08 -0700

    Perf tests for monitor query (#18063)

---
+++ sdk/monitor/perf-tests/monitor-query/test/logQuery.spec.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import { PerfStressOptionDictionary, getEnvVar } from "@azure/test-utils-perfstress";
+import { MonitorQueryLog } from "./monitorQueryLog.spec";
+
+type MonitorQueryTestOptions = Record<string, unknown>;
+
+export class LogQueryTest extends MonitorQueryLog<MonitorQueryTestOptions> {
+  workspaceId: string;
+  query: string;
+  public options: PerfStressOptionDictionary<MonitorQueryTestOptions> = {};
+  constructor() {
+    super();
+    this.workspaceId = getEnvVar("MONITOR_WORKSPACE_ID");
+    this.query =
+      "AppRequests | summarize avgRequestDuration=avg(DurationMs) by bin(TimeGenerated, 10m), _ResourceId";
+  }
+
+  async runAsync(): Promise<void> {
+    await this.client.queryWorkspace(this.workspaceId, this.query, {
+      startTime: new Date("2021-07-25"),
+      endTime: new Date("2021-07-26")
+    });
+  }
+}
