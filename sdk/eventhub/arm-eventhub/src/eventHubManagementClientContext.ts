abcac45d80087f3380c437db9d29f410fd1f865d
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2021-10-07 15:29:54 +0800

    Update eventHubManagementClientContext.ts

--- sdk/eventhub/arm-eventhub/src/eventHubManagementClientContext.ts
+++ sdk/eventhub/arm-eventhub/src/eventHubManagementClientContext.ts
@@ -13,7 +13,7 @@ import * as msRestAzure from "@azure/ms-rest-azure-js";
 import { TokenCredential } from "@azure/core-auth";
 
 const packageName = "@azure/arm-eventhub";
-const packageVersion = "3.3.1";
+const packageVersion = "4.0.0";
 
 export class EventHubManagementClientContext extends msRestAzure.AzureServiceClient {
   credentials: msRest.ServiceClientCredentials | TokenCredential;
