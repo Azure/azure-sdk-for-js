c2c5b045aa34a071dc715992e5d6a922edb17b93
Harsha Nalluru <sanallur@microsoft.com>
2021-10-08 23:18:26 +0000

    add more routes to the server

--- sdk/test-utils/recorder-new/test/utils/server.ts
+++ sdk/test-utils/recorder-new/test/utils/server.ts
@@ -5,13 +5,23 @@ import express from "express";
 const app = express();
 const port = 8080;
 const TEST_SERVER_URL = `http://localhost:${port}`;
+app.use(express.json());
+app.use(express.text());
 
 app.get("/", (_, res) => {
   res.send("Hello world!");
 });
 
 app.get("/sample_response", (_, res) => {
-  res.send({ abc: "def" });
+  res.send({ val: "abc" });
+});
+
+app.get(`/sample_response/:secret_info`, (_, res) => {
+  res.send({ val: "I am the answer!" });
+});
+
+app.post("/api/sample_request_body", function(req, res) {
+  res.send({ bodyProvided: req.body });
 });
 
 app.listen(port, () => {
