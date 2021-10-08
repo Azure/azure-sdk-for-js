cf55b16657ef3274acfac1e8f3224ef972881977
Lu Chen <luc@microsoft.com>
2021-10-08 13:28:51 -0700

    export models from signaling client

---
+++ sdk/communication/communication-chat/src/models/events.ts
@@ -0,0 +1,27 @@
+import {
+  ChatEventId,
+  ChatMessageReceivedEvent,
+  ChatMessageEditedEvent,
+  ChatMessageDeletedEvent,
+  ReadReceiptReceivedEvent,
+  TypingIndicatorReceivedEvent,
+  ChatThreadCreatedEvent,
+  ChatThreadDeletedEvent,
+  ChatThreadPropertiesUpdatedEvent,
+  ParticipantsAddedEvent,
+  ParticipantsRemovedEvent
+} from "@azure/communication-signaling";
+
+export {
+  ChatEventId,
+  ChatMessageReceivedEvent,
+  ChatMessageEditedEvent,
+  ChatMessageDeletedEvent,
+  ReadReceiptReceivedEvent,
+  TypingIndicatorReceivedEvent,
+  ChatThreadCreatedEvent,
+  ChatThreadDeletedEvent,
+  ChatThreadPropertiesUpdatedEvent,
+  ParticipantsAddedEvent,
+  ParticipantsRemovedEvent
+};
