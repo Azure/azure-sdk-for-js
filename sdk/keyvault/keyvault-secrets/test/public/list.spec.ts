01be10edb1d8d3603a73c9e2eef1b1be272058df
zzhxiaofeng <v-zihz@microsoft.com>
2021-10-12 17:24:43 +0800

    update key vault secrets

--- sdk/keyvault/keyvault-secrets/test/public/list.spec.ts
+++ sdk/keyvault/keyvault-secrets/test/public/list.spec.ts
@@ -41,10 +41,11 @@ describe("Secret client - list secrets in various ways", () => {
   // This test is only useful while developing locally.
   it("can purge all secrets", async function(this: Context): Promise<void> {
     console.log("------------------");
-    var aa = process.env["OID"]!.toString();
-    for (var i = 0; i < aa.length; i++) {
-        console.log(aa[i]);
-    }
+    var aa = process.env["ENABLEHSM"];
+    console.log(aa);
+    // for (var i = 0; i < aa.length; i++) {
+    //     console.log(aa[i]);
+    // }
     console.log("-------------------");
     // WARNING: When TEST_MODE equals "record", all of the secrets in the indicated KEYVAULT_URI will be deleted as part of this test.
     if (!isRecordMode()) {
