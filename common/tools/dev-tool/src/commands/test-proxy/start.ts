f5a9992e104fff7909f57d51d6b8070efeca9e57
Harsha Nalluru <sanallur@microsoft.com>
2021-10-20 21:58:32 +0000

    adding in the requirements

--- common/tools/dev-tool/src/commands/test-proxy/start.ts
+++ common/tools/dev-tool/src/commands/test-proxy/start.ts
@@ -6,7 +6,7 @@ import fs from "fs";
 import { leafCommand, makeCommandInfo } from "../../framework/command";
 import { createPrinter } from "../../util/printer";
 
-const log = createPrinter("help");
+const log = createPrinter("info");
 
 export const commandInfo = makeCommandInfo(
   "resolve",
@@ -28,14 +28,32 @@ export const commandInfo = makeCommandInfo(
 );
 
 export default leafCommand(commandInfo, async (_options) => {
-  // Add options to enable/disable logs
   // Allow providing port
   // Volume will be hard-coded - will be evalated to be the root of the repo
   // Runnign this script will create a background process which would end when called "docker stop"
   // dev-tool command will be used in the respective SDKs to run this script before the tests are run for both node and browser
   // Create a script to docker stop which will be run after the test scripts
-  const out = fs.openSync("./out.log", "a");
-  const err = fs.openSync("./out.log", "a");
+  // Should not start the container in live mode, this is only meant for record and playback modes
+  //  - We should load the .env and check the TEST_MODE variable before starting
+  // Check http://localhost:5000 & https://localhost:5001
+  // - Only start docker run if they are not active
+  // After starting http://localhost:5000 & https://localhost:5001
+  // - Make sure http://localhost:5000 & https://localhost:5001 are active
+  // Helpful commands
+  // - dev-tool test-proxy start
+  // - dev-tool test-proxy stop // figure out how to do
+  // - dev-tool test-proxy isRunning // logs true
+
+  // docker run command should run in background
+  // - allow putting logs into a file if a flag --debug is specified
+  // If docker run fails, throw the error
+  const outFileName = "test-proxy-output.log";
+  const out = fs.openSync(`./${outFileName}`, "a");
+  const err = fs.openSync(`./${outFileName}`, "a");
+  log.info(`Attempting to start test proxy at http://localhost:5000 & https://localhost:5001. 
+  
+            Check the output file ${outFileName} for test-proxy logs.
+  `);
   const subprocess = spawn(
     "docker run -v /workspaces/azure-sdk-for-js/:/etc/testproxy -p 5001:5001 -p 5000:5000 --add-host host.docker.internal:host-gateway azsdkengsys.azurecr.io/engsys/testproxy-lin:latest",
     [],
@@ -46,6 +64,5 @@ export default leafCommand(commandInfo, async (_options) => {
     }
   );
   subprocess.unref();
-  log.info("Test proxy is running at http://localhost:5000 & https://localhost:5001");
   return true;
 });
