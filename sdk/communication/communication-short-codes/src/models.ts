7c1f46030be961cac380aa1bf81686b7633963f2
Alonso Mondal <alonsomondal@microsoft.com>
2021-10-20 16:55:09 -0600

    rush.json modified and rush update-build ran for communication-short-codes. Imported samples and ran dev-tool for sample creation.

---
+++ sdk/communication/communication-short-codes/src/models.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { OperationOptions } from "@azure/core-http";
+
+/**
+ * The result of the phone numbers purchase operation.
+ */
+export interface PurchasePhoneNumbersResult { }
+
+/**
+ * The result of the phone number release operation.
+ */
+export interface ReleasePhoneNumberResult { }
+
+/**
+ * Additional options for the get phone number request.
+ */
+export type GetPurchasedPhoneNumberOptions = OperationOptions;
+
+/**
+ * Additional options that can be passed to the list phone numbers request.
+ */
+export interface ListPurchasedPhoneNumbersOptions extends OperationOptions { }
+
+export {
+  ShortCode,
+  USProgramBrief,
+  NumberType,
+  ProgramBriefStatus,
+  Note,
+  ShortCodeCost,
+  ProgramDetails,
+  CompanyInformation,
+  MessageDetails,
+  TrafficDetails,
+  ShortCodesGetShortCodesOptionalParams,
+  ShortCodesUpsertUSProgramBriefOptionalParams,
+  BillingFrequency,
+  ProgramSignUpType,
+  ContactInformation,
+  CustomerCareInformation,
+  MessageType,
+  Recurrence,
+  ContentType,
+  UseCase
+} from "./generated/src/models/";
