0f69bec21bcf6305a3a628fc498ef2f41d5eea13
Maor Leger <malege@microsoft.com>
2021-10-25 12:34:06 -0700

    [KeyVault] - Use 7.3 for certificates and secrets

--- sdk/keyvault/keyvault-certificates/src/certificatesModels.ts
+++ sdk/keyvault/keyvault-certificates/src/certificatesModels.ts
@@ -6,13 +6,13 @@ import {
   DeletionRecoveryLevel,
   KeyUsageType,
   JsonWebKeyType as CertificateKeyType,
-  JsonWebKeyCurveName as CertificateKeyCurveName
+  JsonWebKeyCurveName as CertificateKeyCurveName,
 } from "./generated/models";
 
 /**
  * The latest supported KeyVault service API version
  */
-export const LATEST_API_VERSION = "7.2";
+export const LATEST_API_VERSION = "7.3-preview";
 
 /**
  * The optional parameters accepted by the KeyVault's KeyClient
@@ -21,7 +21,7 @@ export interface CertificateClientOptions extends coreHttp.PipelineOptions {
   /**
    * The accepted versions of the KeyVault's service API.
    */
-  serviceVersion?: "7.0" | "7.1" | "7.2";
+  serviceVersion?: "7.0" | "7.1" | "7.2" | "7.3-preview";
 }
 
 /**
@@ -192,7 +192,7 @@ export enum WellKnownIssuerNames {
   /**
    * For certificates whose issuer will be defined later
    */
-  Unknown = "Unknown"
+  Unknown = "Unknown",
 }
 
 /**
@@ -382,7 +382,7 @@ export type ImportCertificatePolicy = CertificatePolicyProperties &
  */
 export const DefaultCertificatePolicy = {
   issuerName: "Self",
-  subject: "cn=MyCert"
+  subject: "cn=MyCert",
 };
 
 /**
@@ -831,7 +831,7 @@ export enum KnownCertificateKeyCurveNames {
   /**
    * P-256K Key Curve.
    */
-  P256K = "P-256K"
+  P256K = "P-256K",
 }
 
 /** Known values of {@link CertificateKeyType} that the service accepts. */
@@ -859,7 +859,7 @@ export enum KnownCertificateKeyTypes {
   /**
    * oct-HSM Key Type
    */
-  OctHSM = "oct-HSM"
+  OctHSM = "oct-HSM",
 }
 
 /** Known values of {@link KeyUsageType} that the service accepts. */
@@ -899,5 +899,5 @@ export const enum KnownKeyUsageTypes {
   /**
    * DecipherOnly Usage Type.
    */
-  DecipherOnly = "decipherOnly"
+  DecipherOnly = "decipherOnly",
 }
