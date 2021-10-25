acbbb1f862c356c5fc7b9c56189163716a240455
Jeremy Meng <yumeng@microsoft.com>
2021-10-25 13:36:31 -0700

    [core-http] `HttpHeaders.clone()` should keep raw header name casing
    - add a failing test

---
+++ sdk/core/core-http/test/httpHeadersTests.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import { HttpHeaders } from "../src/httpHeaders";
+
+describe.only("HttpHeaders", () => {
+  it("clone() should keep the original header names", () => {
+    const rawHeaders = {
+      lowercase: "lower case value",
+      camelCase: "camel case value",
+      ALLUPPERCASE: "all upper case value"
+    };
+    const headers = new HttpHeaders(rawHeaders);
+    const cloned = headers.clone();
+
+    assert.deepStrictEqual(cloned.headersArray(), headers.headersArray());
+  });
+});
