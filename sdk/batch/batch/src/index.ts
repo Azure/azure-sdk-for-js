583150dca1ed7ae1ebd5e3f74b48047d489510cb
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2021-11-01 17:28:25 -0400

    [Batch] Export all types (#18469)
    * [Batch] Export all types
    
    * bump version

---
+++ sdk/batch/batch/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+export * from "./batchServiceClient"
+export * from "./operations";
+export * from "./models";
+export * from "./batchSharedKeyCredentials"
