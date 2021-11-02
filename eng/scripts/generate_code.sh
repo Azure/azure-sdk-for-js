98edd8726a08a86f06c25c41533054e7856bbbcb
dw511214992 <dw511214992@gmail.com>
2021-11-02 14:10:49 +0800

    install dependency

--- eng/scripts/generate_code.sh
+++ eng/scripts/generate_code.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 npm install -g dwtest-release-tools
+npm install -g autorest
 cd $3
 README=$1/$2/resource-manager/readme.md
 track2-codegen-automation-for-sdk-generation-pipeline --task=generateCode --use=@autorest/typescript@6.0.0-beta.14 --readme=$README
+if [ $? != 0 ]; then
+  exit 1
+fi
