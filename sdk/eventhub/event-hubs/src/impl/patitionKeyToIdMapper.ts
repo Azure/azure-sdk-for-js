f899ae48ea274cca6f02462ce4efeccc7a77d93b
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2021-11-03 16:45:49 +0000

    simplified implementation

--- sdk/eventhub/event-hubs/src/impl/patitionKeyToIdMapper.ts
+++ sdk/eventhub/event-hubs/src/impl/patitionKeyToIdMapper.ts
@@ -14,14 +14,6 @@ function readUInt32(data: Buffer, offset: number): number {
     return os.endianness() === "BE" ? data.readUInt32BE(offset) : data.readUInt32LE(offset);
 }
 
-function addUInt32(n1: number, n2: number): number {
-    return (n1 + n2) % 4294967296;
-}
-
-function castToUInt32(n: number): number {
-    return (new Uint32Array([n]))[0];
-}
-
 function castToInt16(n: number): number {
     return (new Int16Array([n]))[0];
 }
@@ -29,38 +21,38 @@ function castToInt16(n: number): number {
 function computeHash(data: Buffer, seed1: number = 0, seed2: number = 0): any {
     let a: number, b: number, c: number;
 
-    a = b = c = castToUInt32((0xdeadbeef + data.length + seed1));
-    c = addUInt32(c, seed2);
+    a = b = c = 0xdeadbeef + data.length + seed1;
+    c += seed2;
 
     let index = 0, size = data.length;
     while (size > 12) {
-        a = addUInt32(a, readUInt32(data, index));
-        b = addUInt32(b, readUInt32(data, index + 4));
-        c = addUInt32(c, readUInt32(data, index + 8));
+        a += readUInt32(data, index);
+        b += readUInt32(data, index + 4);
+        c += readUInt32(data, index + 8);
 
         a -= c;
-        a ^= (c << 4) | (c >> 28);
-        c = addUInt32(c, b);
+        a ^= (c << 4) | (c >>> 28);
+        c += b;
 
         b -= a;
-        b ^= (a << 6) | (a >> 26);
-        a = addUInt32(a, c);
+        b ^= (a << 6) | (a >>> 26);
+        a += c;
 
         c -= b;
-        c ^= (b << 8) | (b >> 24);
-        b = addUInt32(b, a);
+        c ^= (b << 8) | (b >>> 24);
+        b += a;
 
         a -= c;
-        a ^= (c << 16) | (c >> 16);
-        c = addUInt32(c, b);
+        a ^= (c << 16) | (c >>> 16);
+        c += b;
 
         b -= a;
-        b ^= (a << 19) | (a >> 13);
-        a = addUInt32(a, c);
+        b ^= (a << 19) | (a >>> 13);
+        a += c;
 
         c -= b;
-        c ^= (b << 4) | (b >> 28);
-        b = addUInt32(b, a);
+        c ^= (b << 4) | (b >>> 28);
+        b += a;
 
         index += 12;
         size -= 12;
@@ -69,68 +61,68 @@ function computeHash(data: Buffer, seed1: number = 0, seed2: number = 0): any {
     let curr = size;
     switch (curr) {
         case 12:
-            a = addUInt32(a, readUInt32(data, index));
-            b = addUInt32(b, readUInt32(data, index + 4));
-            c = addUInt32(c, readUInt32(data, index + 8));
+            a += readUInt32(data, index);
+            b += readUInt32(data, index + 4);
+            c += readUInt32(data, index + 8);
             break;
         case 11:
-            c = addUInt32(c, castToUInt32(data[index + 10]) << 16);
+            c += data[index + 10] << 16;
             curr = 10;
         case 10:
-            c = addUInt32(c, castToUInt32(data[index + 9]) << 8);
+            c += data[index + 9] << 8;
             curr = 9;
         case 9:
-            c = addUInt32(c, castToUInt32(data[index + 8]));
+            c += data[index + 8];
             curr = 8;
         case 8:
-            b = addUInt32(b, readUInt32(data, index + 4));
-            a = addUInt32(a, readUInt32(data, index));
+            b += readUInt32(data, index + 4);
+            a += readUInt32(data, index);
             break;
         case 7:
-            b = addUInt32(b, castToUInt32(data[index + 6]) << 16);
+            b += data[index + 6] << 16;
             curr = 6;
         case 6:
-            b = addUInt32(b, castToUInt32(data[index + 5]) << 8);
+            b += data[index + 5] << 8;
             curr = 5;
         case 5:
-            b = addUInt32(b, castToUInt32(data[index + 4]));
+            b += data[index + 4];
             curr = 4;
         case 4:
-            a = addUInt32(a, readUInt32(data, index));
+            a += readUInt32(data, index);
             break;
         case 3:
-            a = addUInt32(a, castToUInt32(data[index + 2]) << 16);
+            a += data[index + 2] << 16;
             curr = 2;
         case 2:
-            a = addUInt32(a, castToUInt32(data[index + 1]) << 8);
+            a += data[index + 1] << 8;
             curr = 1;
         case 1:
-            a = addUInt32(a, castToUInt32(data[index]));
+            a += data[index];
             break;
         case 0:
-            return { b, c };
+            return { b: b >>> 0, c: c >>> 0 };
     }
 
-    c = castToUInt32(c ^ b);
-    c = castToUInt32(c - ((b << 14) | (b >>> 18)));
+    c ^= b;
+    c -= (b << 14) | (b >>> 18);
 
-    a = castToUInt32(a ^ c);
-    a = castToUInt32(a - ((c << 11) | (c >>> 21)));
+    a ^= c;
+    a -= (c << 11) | (c >>> 21);
 
-    b = castToUInt32(b ^ a);
-    b = castToUInt32(b - ((a << 25) | (a >>> 7)));
+    b ^= a;
+    b -= (a << 25) | (a >>> 7);
 
-    c = castToUInt32(c ^ b);
-    c = castToUInt32(c - ((b << 16) | (b >>> 16)));
+    c ^= b;
+    c -= (b << 16) | (b >>> 16);
 
-    a = castToUInt32(a ^ c);
-    a = castToUInt32(a - ((c << 4) | (c >>> 28)));
+    a ^= c;
+    a -= (c << 4) | (c >>> 28);
 
-    b = castToUInt32(b ^ a);
-    b = castToUInt32(b - ((a << 14) | (a >>> 18)));
+    b ^= a;
+    b -= (a << 14) | (a >>> 18);
 
-    c = castToUInt32(c ^ b);
-    c = castToUInt32(c - ((b << 24) | (b >>> 8)));
+    c ^= b;
+    c -= (b << 24) | (b >>> 8);
 
-    return { b, c };
+    return { b: b >>> 0, c: c >>> 0 };
 }
--- sdk/eventhub/event-hubs/test/internal/impl/mapPartitionKeyToId.spec.ts
+++ sdk/eventhub/event-hubs/test/internal/impl/mapPartitionKeyToId.spec.ts
@@ -5,7 +5,19 @@ import { assert } from "chai";
 import { mapPartitionKeyToId } from "../../../src/impl/patitionKeyToIdMapper";
 
 describe.only("mapPartitionKeyToId", () => {
-  it("add", () => {
+  it("short key, small partitions count", () => {
+    assert.equal(mapPartitionKeyToId("alphabet", 3), 0);
+  });
+
+  it("short key, large partitions count", () => {
     assert.equal(mapPartitionKeyToId("alphabet", 11), 4);
+  });
+
+  it("long key, small partitions count", () => {
+    assert.equal(mapPartitionKeyToId("TheBestParitionEver", 4), 2);
+  });
+
+  it("long key, large partitions count", () => {
+    assert.equal(mapPartitionKeyToId("TheWorstParitionEver", 15), 1);
   });
 });
