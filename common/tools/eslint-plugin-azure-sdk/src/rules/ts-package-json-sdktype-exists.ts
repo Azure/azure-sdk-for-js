7146fb54a1d9a1e40705822aa304d18def8a168b
bzhang0 <bzhang0@cs.washington.edu>
2021-11-07 10:51:51 -0800

    add a linter rule to check if  exists in package.json

---
+++ common/tools/eslint-plugin-azure-sdk/src/rules/ts-package-json-sdktype-exists.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * @file Rule to check if package.json includes 'sdk-type'
+ * @author Ben Zhang
+ */
+
+import { Rule } from "eslint";
+import { getRuleMetaData, getVerifiers, stripPath } from "../utils";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+export = {
+  meta: getRuleMetaData(
+    "ts-package-json-sdktype-exists",
+    "check if package.json includes 'sdk-type'",
+    "code"
+  ),
+  create: (context: Rule.RuleContext): Rule.RuleListener => {
+    const verifiers = getVerifiers(context, {
+      outer: "sdk-type",
+    });
+    return stripPath(context.getFilename()) === "package.json"
+      ? ({
+          // callback functions
+
+          // check to see if package.json includes 'sdk-type'
+          "ExpressionStatement > ObjectExpression": verifiers.existsInFile,
+        } as Rule.RuleListener)
+      : {};
+  }
+};
