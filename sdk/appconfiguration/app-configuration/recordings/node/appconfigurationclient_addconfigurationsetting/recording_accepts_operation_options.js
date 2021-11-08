6a7c6f68b2e128e4eba2263492d309d8e85c4ffb
Harsha Nalluru <sanallur@microsoft.com>
2021-11-08 20:59:56 +0000

    delete recordings

--- sdk/appconfiguration/app-configuration/recordings/node/appconfigurationclient_addconfigurationsetting/recording_accepts_operation_options.js
+++
@@ -1,5 +0,0 @@
-let nock = require('nock');
-
-module.exports.hash = "35a74fd449126e45b315b3ab1ec53864";
-
-module.exports.testInfo = {"uniqueName":{"addConfigTestTwice":"addConfigTestTwice158696680824002773"},"newDate":{}}
--- sdk/appconfiguration/app-configuration/recordings/node/appconfigurationclient_setconfigurationsetting/recording_accepts_operation_options.js
+++
@@ -1,5 +0,0 @@
-let nock = require('nock');
-
-module.exports.hash = "79146c6240cfa8030bc2371c5048d2f7";
-
-module.exports.testInfo = {"uniqueName":{"setConfigTestNA":"setConfigTestNA158696685579202551"},"newDate":{}}
