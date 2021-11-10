ca5db9a9242ec058d18d6fddaa476429bd85f79d
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2021-11-10 15:21:51 -0800

    [dev-tool] drop node10 support in samples run

--- common/tools/dev-tool/src/commands/samples/checkNodeVersions.ts
+++ common/tools/dev-tool/src/commands/samples/checkNodeVersions.ts
@@ -224,7 +224,7 @@ export const commandInfo = makeCommandInfo(
     "node-versions": {
       kind: "string",
       description: "A comma separated list of node versions to use",
-      default: "10,12,14,15"
+      default: "12,14,15,16"
     },
     "node-version": {
       kind: "string",
