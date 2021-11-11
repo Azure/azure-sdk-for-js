460d092302d7304870d9ddc93c31af86012656aa
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-11-11 10:46:28 -0800

    Update sdk/core/core-client/src/urlHelpers.ts
    Co-authored-by: Jeff Fisher <xirzec@xirzec.com>

--- sdk/core/core-client/src/urlHelpers.ts
+++ sdk/core/core-client/src/urlHelpers.ts
@@ -291,7 +291,7 @@ export function appendQueryParams(
   for (const [name, value] of combinedParams) {
     if (typeof value === "string") {
       searchPieces.push(`${name}=${value}`);
-    } else {
+    } else if(Array.isArray(value)) {
       if (value) {
         // QUIRK: If we get an array of values, include multiple key/value pairs
         for (const subValue of value) {
