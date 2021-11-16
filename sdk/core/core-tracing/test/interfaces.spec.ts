bf3edf6027849871efc1ce38ddfe7cc44012b88e
Maor Leger <malege@microsoft.com>
2021-11-16 11:45:36 -0800

    add interface compat test

---
+++ sdk/core/core-tracing/test/interfaces.spec.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import { createTracingContext } from "../src/tracingContext";
+import * as coreTracing from "../src";
+import * as coreAuth from "@azure/core-auth";
+
+describe("Interface compatibility", () => {
+  describe("OperationTracingOptions", () => {
+    it("is compatible with core-auth", () => {
+      const tracingOptions: coreTracing.OperationTracingOptions = {
+        tracingContext: createTracingContext({})
+      };
+      const authOptions: coreAuth.GetTokenOptions = {
+        tracingOptions
+      };
+      assert.ok(authOptions.tracingOptions);
+    });
+  });
+});
