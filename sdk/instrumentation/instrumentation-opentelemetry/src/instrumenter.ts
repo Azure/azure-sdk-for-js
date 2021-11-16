4adf45a5178f857aea42df048ec382109b491375
Maor Leger <malege@microsoft.com>
2021-11-16 15:49:22 -0800

    request headers

--- sdk/instrumentation/instrumentation-opentelemetry/src/instrumenter.ts
+++ sdk/instrumentation/instrumentation-opentelemetry/src/instrumenter.ts
@@ -9,7 +9,7 @@ import {
   TracingSpanContext
 } from "@azure/core-tracing";
 
-import { SpanContext } from "@opentelemetry/api";
+import { SpanContext, TraceFlags, TraceState } from "@opentelemetry/api";
 const VERSION = "00";
 export class OpenTelemetryInstrumenter implements Instrumenter {
   startSpan(
@@ -53,7 +53,63 @@ export class OpenTelemetryInstrumenter implements Instrumenter {
 
     return spanContext;
   }
-  createRequestHeaders(_spanId: TracingSpanContext): Record<string, string> {
-    throw new Error("Method not implemented.");
+
+  createRequestHeaders(spanContext: TracingSpanContext): Record<string, string> {
+    const headers: Record<string, string> = {};
+    const traceparentHeader = createTraceparentHeader(spanContext);
+    if (traceparentHeader) {
+      headers["traceparent"] = traceparentHeader;
+
+      // if tracestate is set, traceparent MUST be set, so only set tracestate after traceparent
+      const tracestateHeader = createTracestateHeader(spanContext);
+      if (tracestateHeader) {
+        headers["tracestate"] = tracestateHeader;
+      }
+    }
+
+    return headers;
   }
+}
+
+function createTraceparentHeader(spanContext: TracingSpanContext): string | undefined {
+  const missingFields: string[] = [];
+  if (!spanContext.traceId) {
+    missingFields.push("traceId");
+  }
+  if (!spanContext.spanId) {
+    missingFields.push("spanId");
+  }
+
+  if (missingFields.length) {
+    return;
+  }
+
+  const flags = spanContext.traceFlags || TraceFlags.NONE;
+  const hexFlags = flags.toString(16);
+  const traceFlags = hexFlags.length === 1 ? `0${hexFlags}` : hexFlags;
+
+  // https://www.w3.org/TR/trace-context/#traceparent-header-field-values
+  return `${VERSION}-${spanContext.traceId}-${spanContext.spanId}-${traceFlags}`;
+}
+
+function createTracestateHeader(spanContext: TracingSpanContext): string | undefined {
+  const traceState = spanContext.traceState;
+
+  if (traceState === undefined || traceState === null) {
+    return;
+  }
+
+  if (typeof traceState !== "object") {
+    return;
+  }
+
+  if (typeof (traceState as TraceState).serialize !== "function") {
+    return;
+  }
+
+  const serializedTraceState = (traceState as TraceState).serialize();
+
+  // https://www.w3.org/TR/trace-context/#tracestate-header-field-values
+  // Return undefined instead of an empty string to indicate that the tracestate header should not be set.
+  return serializedTraceState || undefined;
 }
--- sdk/instrumentation/instrumentation-opentelemetry/test/public/instrumenter.spec.ts
+++ sdk/instrumentation/instrumentation-opentelemetry/test/public/instrumenter.spec.ts
@@ -1,6 +1,8 @@
 import { assert } from "chai";
 import { OpenTelemetryInstrumenter } from "../../src/instrumenter";
-import { TraceFlags } from "@opentelemetry/api";
+import { TraceFlags, SpanContext } from "@opentelemetry/api";
+import { TracingSpanContext } from "@azure/core-tracing";
+import { TraceState } from "./util/traceState";
 
 describe("OpenTelemetryInstrumenter", () => {
   const instrumenter = new OpenTelemetryInstrumenter();
@@ -11,6 +13,7 @@ describe("OpenTelemetryInstrumenter", () => {
       const spanId = "2222222222222222";
       const flags = "01";
       const traceParentHeader = `00-${traceId}-${spanId}-${flags}`;
+
       it("should extract a TracingSpanIdentifier from a properly formatted header", () => {
         const tracingSpanIdentifier = instrumenter.parseTraceparentHeader(traceParentHeader);
         assert.deepEqual(tracingSpanIdentifier, {
@@ -19,11 +22,147 @@ describe("OpenTelemetryInstrumenter", () => {
           traceFlags: TraceFlags.SAMPLED
         });
       });
+
+      describe("with an invalid Traceparent header", () => {
+        it("returns undefined when version is unknown", () => {
+          const traceId = "11111111111111111111111111111111";
+          const spanId = "2222222222222222";
+          const flags = "00";
+          const traceParentHeader = `99-${traceId}-${spanId}-${flags}`;
+
+          const spanContext = instrumenter.parseTraceparentHeader(traceParentHeader);
+
+          assert.strictEqual(
+            spanContext,
+            undefined,
+            "Invalid traceparent version should return undefined spanContext."
+          );
+        });
+
+        it("returns undefined when traceparent is malformed", () => {
+          const traceParentHeader = `123abc`;
+
+          const spanContext = instrumenter.parseTraceparentHeader(traceParentHeader);
+
+          assert.strictEqual(
+            spanContext,
+            undefined,
+            "Malformed traceparent should return undefined spanContext."
+          );
+        });
+      });
     });
   });
+
   describe("#createRequestHeaders", () => {
-    describe("with a valid span identifier", () => {
-      it("should return a valid Traceparent header");
+    describe("with a valid span context", () => {
+      it("should return a valid Traceparent header", () => {
+        const spanContext: TracingSpanContext = {
+          spanId: "2222222222222222",
+          traceId: "11111111111111111111111111111111",
+          traceFlags: TraceFlags.SAMPLED
+        };
+        const expectedTraceParentHeader = `00-11111111111111111111111111111111-2222222222222222-01`;
+
+        const headers = instrumenter.createRequestHeaders(spanContext);
+        assert.deepEqual(headers, {
+          traceparent: expectedTraceParentHeader
+        });
+      });
+
+      describe("when traceState is provided", () => {
+        it("should return a traceState header", () => {
+          const spanContext: SpanContext = {
+            spanId: "2222222222222222",
+            traceId: "11111111111111111111111111111111",
+            traceFlags: TraceFlags.NONE,
+            traceState: new TraceState()
+          };
+          spanContext.traceState = spanContext.traceState!.set("foo", "bar");
+
+          const expectedTraceParentHeader = `00-11111111111111111111111111111111-2222222222222222-00`;
+          const expectedTraceStateHeader = `foo=bar`;
+
+          const headers = instrumenter.createRequestHeaders(spanContext);
+
+          assert.deepEqual(headers, {
+            traceparent: expectedTraceParentHeader,
+            tracestate: expectedTraceStateHeader
+          });
+        });
+      });
+
+      describe("when a traceState is not provided", () => {
+        it("does not include it in the result set", () => {
+          const spanContext: SpanContext = {
+            spanId: "2222222222222222",
+            traceId: "11111111111111111111111111111111",
+            traceFlags: TraceFlags.NONE
+          };
+
+          const headers = instrumenter.createRequestHeaders(spanContext);
+
+          assert.notExists(headers.tracestate);
+        });
+      });
+
+      describe("when a traceState is not empty", () => {
+        it("does not include it in the result set", () => {
+          const spanContext: SpanContext = {
+            spanId: "2222222222222222",
+            traceId: "11111111111111111111111111111111",
+            traceFlags: TraceFlags.NONE,
+            traceState: new TraceState()
+          };
+
+          const headers = instrumenter.createRequestHeaders(spanContext);
+
+          assert.notExists(headers.tracestate);
+        });
+      });
+    });
+
+    describe("with an incomplete span context", () => {
+      it("returns an empty collection when traceId is missing", () => {
+        const spanContext: SpanContext = {
+          spanId: "2222222222222222",
+          traceId: "",
+          traceFlags: TraceFlags.NONE,
+          traceState: new TraceState()
+        };
+
+        const headers = instrumenter.createRequestHeaders(spanContext);
+
+        assert.isEmpty(headers);
+      });
+      it("returns an empty collection when spanId is missing", () => {
+        const spanContext: SpanContext = {
+          spanId: "",
+          traceId: "11111111111111111111111111111111",
+          traceFlags: TraceFlags.NONE,
+          traceState: new TraceState()
+        };
+
+        const headers = instrumenter.createRequestHeaders(spanContext);
+
+        assert.isEmpty(headers);
+      });
+
+      describe("when a traceState is provided", () => {
+        it("does not include it when traceparent is invalid", () => {
+          const spanContext: SpanContext = {
+            spanId: "",
+            traceId: "11111111111111111111111111111111",
+            traceFlags: TraceFlags.NONE,
+            traceState: new TraceState()
+          };
+          spanContext.traceState = spanContext.traceState!.set("foo", "bar");
+
+          const headers = instrumenter.createRequestHeaders(spanContext);
+
+          assert.isEmpty(headers);
+        });
+      });
     });
   });
 });
