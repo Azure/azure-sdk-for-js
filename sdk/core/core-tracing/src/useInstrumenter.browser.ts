e70d3b50dfd3a80ece44535341945438e1059253
Maor Leger <malege@microsoft.com>
2021-11-18 13:01:52 -0800

    fixup! add browser support for instrumentation

--- sdk/core/core-tracing/src/useInstrumenter.browser.ts
+++ sdk/core/core-tracing/src/useInstrumenter.browser.ts
@@ -8,8 +8,6 @@ globalThis.instrumenterImplementation = new NoOpInstrumenter();
  * Extends the Azure SDK with support for a given instrumenter implementation.
  *
  * @param instrumenter - The instrumenter implementation to use.
- *
- * This function is meant to be used by the \@azure/instrumentation-opentelemetry package.
  */
 export function useInstrumenter(instrumenter: Instrumenter): void {
   globalThis.instrumenterImplementation = instrumenter;
--- sdk/core/core-tracing/src/useInstrumenter.ts
+++ sdk/core/core-tracing/src/useInstrumenter.ts
@@ -8,16 +8,6 @@ export let instrumenterImplementation: Instrumenter = new NoOpInstrumenter();
  * Extends the Azure SDK with support for a given instrumenter implementation.
  *
  * @param instrumenter - The instrumenter implementation to use.
- *
- * Example:
- *
- * ```ts
- * import { openTelemetryInstrumenter } from "@azure/core-tracing-opentelemetry";
- * import { MyClient } from "@azure/package-name"
- * useInstrumenter(openTelemetryInstrumenter);
- *
- * const client = new MyClient(); // uses the OpenTelemetry instrumenter
- * ```
  */
 export function useInstrumenter(instrumenter: Instrumenter): void {
   instrumenterImplementation = instrumenter;
