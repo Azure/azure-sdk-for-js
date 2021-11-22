ad1ff4df644da8b20fc65df170f5160bd75c5111
Will Temple <witemple@microsoft.com>
2021-11-22 15:39:30 -0500

    More improvements

--- common/tools/dev-tool/src/util/samples/processor.ts
+++ common/tools/dev-tool/src/util/samples/processor.ts
@@ -3,7 +3,9 @@
 
 import nodeBuiltins from "builtin-modules";
 import fs from "fs-extra";
+import { EOL } from "os";
 import path from "path";
+import { cwd } from "process";
 import * as ts from "typescript";
 import { convert } from "../../commands/samples/tsToJs";
 import { createPrinter } from "../printer";
@@ -16,17 +18,53 @@ import {
   VALID_AZSDK_META_TAGS,
 } from "./generation";
 
-const log = createPrinter("transpile:samples");
+const log = createPrinter("samples:processor");
 
+/**
+ * Tests for syntax compatibility.
+ *
+ * This is a Map from Category -> Name -> Test, where Test is a predicate that determines if a ts.Node is an instance
+ * of the syntax form. For example:
+ *
+ * ES2020 -> NullishCoalesce -> ts.isNullishCoalesce
+ * Category: ES2020
+ * Name: NullishCoalesce
+ * Test: ts.isNullishCoalesce (TS API built-in helper)
+ *
+ * If the test returns true, then the syntax is considered unsupported by the compiler, and an error will be generated
+ * during sample publication.
+ */
 const SYNTAX_VIABILITY_TESTS = {
+  CommonJS: {
+    // require(<not a literal string>)
+    DynamicRequire: (node: ts.Node) =>
+      ts.isCallExpression(node) &&
+      ts.isIdentifier(node.expression) &&
+      node.expression.text === "require" &&
+      !ts.isStringLiteral(node.arguments[0]),
+  },
+  ESModule: {
+    // TODO: this is difficult to support well, but we could do it by enforcing that the RHS is a static require call
+    ImportEquals: ts.isImportEqualsDeclaration,
+    // These are possible to support, but samples that really need them should probably have their own unique setup
+    // export { ... }, export foo from "bar", export * as foo from "bar", and export { ... } from "foo";
+    ExportDeclaration: ts.isExportDeclaration,
+    // export = { ... }
+    ExportAssignment: ts.isExportAssignment,
+  },
   ES2020: {
+    // 1n
     BigInt: ts.isBigIntLiteral,
+    // import("foo")
     ImportExpression: (node: ts.Node) =>
       ts.isCallExpression(node) && node.expression.kind === ts.SyntaxKind.ImportKeyword,
+    // foo ?? bar
     NullishCoalesce: ts.isNullishCoalesce,
+    // foo?.bar, foo?.(bar), and foo?.[bar]
     OptionalChain: ts.isOptionalChain,
   },
   ES2021: {
+    // x ??= y, x ||= y, and x &&= y
     ShorthandAssignment: (node: ts.Node) =>
       ts.isBinaryExpression(node) &&
       [
@@ -34,28 +72,89 @@ const SYNTAX_VIABILITY_TESTS = {
         ts.SyntaxKind.BarBarEqualsToken,
         ts.SyntaxKind.QuestionQuestionEqualsToken,
       ].includes(node.operatorToken.kind),
+    // 1_000_000
     NumericSeparator: (node: ts.Node) =>
       ts.isNumericLiteral(node) && !!node.getText().includes("_"),
   },
   ES2022: {
+    // This is well-supported in TypeScript but is emitted as an assignment rather than a `static`
+    // static foo = "bar"
     StaticField: (node: ts.Node) =>
       ts.isPropertyDeclaration(node) &&
       node.modifiers?.some((modifier) => modifier.kind === ts.SyntaxKind.StaticKeyword),
+    // #foo
     PrivateIdentifier: ts.isPrivateIdentifier,
+    // static { ... }
     StaticInitializer: ts.isClassStaticBlockDeclaration,
   },
 } as const;
 
-function isForbiddenEcmaSyntax(node: ts.Node): string | false {
-  for (const [esVersion, tests] of Object.entries(SYNTAX_VIABILITY_TESTS)) {
-    for (const [syntaxName, test] of Object.entries(tests)) {
+/**
+ * Converts a union type U of types [T1, T2, ... Tn] into an intersection of the same types.
+ */
+type UnionToIntersection<Union> =
+  // Evil distributive magic. The first conditional distributes U into a union of functions, then the second conditional
+  // infers an intersection due to variance inversion.
+  (Union extends unknown ? (k: Union) => void : never) extends (k: infer Intersection) => void
+    ? Intersection
+    : never;
+
+type SyntaxTest = (node: ts.Node) => boolean;
+
+type SyntaxName = keyof UnionToIntersection<
+  typeof SYNTAX_VIABILITY_TESTS[keyof typeof SYNTAX_VIABILITY_TESTS]
+>;
+
+const SUGGEST_SYNTAX: {
+  [K in SyntaxName]?: (node: /* makes the type system happy */ ts.Node) => string;
+} = {
+  DynamicRequire: () => "use static import/exports in samples for the best TypeScript experience",
+  ExportAssignment: () => "export each symbol individually instead of using `export =`",
+  ExportDeclaration: () =>
+    "export each symbol individually where it is declared instead of using a blanket export declaration",
+  OptionalChain: (node) => {
+    const chain = node as ts.OptionalChain;
+    const expressionText = chain.expression.getText().replace(/\?\./g, ".");
+    // We need some text for the right-hand side
+    const rhs = (
+      ts.isPropertyAccessExpression(chain)
+        ? "." + chain.name.text
+        : ts.isElementAccessExpression(chain)
+        ? "[" + chain.argumentExpression.getText() + "]"
+        : ts.isCallExpression(chain)
+        ? "(" + chain.arguments.map((node) => node.getText()).join(", ") + ")"
+        : (() => {
+            log.warn(
+              "[Internal Error] Unknown right-hand-side of Optional Chain:",
+              chain.getText()
+            );
+            return "<unknown>";
+          })()
+    ).replace(/\?\./g, ".");
+    return `try \`${expressionText} && ${expressionText}${rhs}\` if it does not affect runtime behavior, or use an explicit \`if\` block to handle the nullish case`;
+  },
+  NullishCoalesce: (node) => {
+    const nullish = node as ts.BinaryExpression;
+    return `try \`${nullish.left.getText()} || ${nullish.right.getText()}\` if it does not affect runtime behavior, or use an explicit \`if\` block to handle the nullish case`;
+  },
+};
+
+interface SyntaxSupportError {
+  message: string;
+  suggest?: string;
+}
+
+function testSyntax(node: ts.Node): SyntaxSupportError | undefined {
+  for (const [category, tests] of Object.entries(SYNTAX_VIABILITY_TESTS)) {
+    for (const [syntaxName, test] of Object.entries(tests) as [SyntaxName, SyntaxTest][]) {
       if (test(node)) {
-        return `forbidden ${esVersion} "${syntaxName}" syntax (samples must support ES2019)`;
+        return {
+          message: `unsupported ${category} "${syntaxName}" syntax`,
+          suggest: SUGGEST_SYNTAX[syntaxName]?.(node as never),
+        };
       }
     }
   }
-
-  return false;
 }
 
 export async function processSources(
@@ -63,6 +162,46 @@ export async function processSources(
   sources: string[],
   fail: (...values: unknown[]) => never
 ): Promise<ModuleInfo[]> {
+  const diagnosticHost = {
+    getNewLine: () => EOL,
+    getCanonicalFileName: (name: string) => name,
+    getCurrentDirectory: () => cwd(),
+  };
+
+  let hadUnsupportedSyntax = false;
+
+  const emitError = (
+    message: string,
+    node: ts.Node,
+    sourceFile: ts.SourceFile,
+    suggest?: string
+  ) => {
+    hadUnsupportedSyntax = true;
+    const [start, end] = [node.getStart(sourceFile), node.getEnd()];
+    const diagnostic: ts.Diagnostic = {
+      category: ts.DiagnosticCategory.Error,
+      // I am intentionally lying to the compiler here to bypass the error code
+      code: "-AZURE" as never,
+      file: sourceFile,
+      start,
+      length: end - start,
+      messageText: message,
+    };
+    if (suggest) {
+      diagnostic.relatedInformation = [
+        {
+          category: ts.DiagnosticCategory.Message,
+          code: 0,
+          messageText: "Suggestion: " + suggest,
+          file: undefined,
+          start: undefined,
+          length: undefined,
+        },
+      ];
+    }
+    console.error(ts.formatDiagnosticsWithColorAndContext([diagnostic], diagnosticHost));
+  };
+
   const jobs = sources.map(async (source) => {
     const sourceText = (await fs.readFile(source)).toString("utf8");
 
@@ -90,29 +229,13 @@ export async function processSources(
           );
 
         const visitor: ts.Visitor = (node: ts.Node) => {
-          const ecmaSupportError = isForbiddenEcmaSyntax(node);
-          if (ecmaSupportError) {
-            log.error(
-              `${relativeSourcePath}: ${ecmaSupportError}: \`${node.getText(sourceFile)}\``
-            );
-
-            fail("Unsupported JavaScript/TypeScript syntax is not allowed in samples.");
+          const syntaxSupportError = testSyntax(node);
+          if (syntaxSupportError) {
+            emitError(syntaxSupportError.message, node, sourceFile, syntaxSupportError.suggest);
           }
 
           if (ts.isImportDeclaration(node)) {
             importedModules.push((node.moduleSpecifier as ts.StringLiteral).text);
-          } else if (ts.isImportEqualsDeclaration(node)) {
-            log.error(
-              `${relativeSourcePath}: unsupported \`import =\` declaration: \`${node.getText(
-                sourceFile
-              )}\`.`
-            );
-            fail(
-              [
-                "`import =` is not supported when targeting ECMAScript modules.",
-                "This dependency will NOT be included in your package's dependencies.",
-              ].join(" ")
-            );
           } else if (ts.isCallExpression(node)) {
             const {
               expression,
@@ -124,15 +247,6 @@ export async function processSources(
             ) {
               if (ts.isStringLiteralLike(firstArgument)) {
                 importedModules.push(firstArgument.text);
-              } else {
-                log.error(
-                  `${relativeSourcePath}: unsupported dynamic import: \`${node.getText(
-                    sourceFile
-                  )}\``
-                );
-                fail(
-                  "Dynamic imports (`import` expressions or `require` calls) cannot be statically analyzed."
-                );
               }
             }
           } else if (
@@ -251,7 +365,16 @@ export async function processSources(
     };
   });
 
-  return Promise.all(jobs);
+  return Promise.all(jobs).then((results) => {
+    // Only fail once at the end, so that we don't drown you with tons of red messages
+    if (hadUnsupportedSyntax) {
+      fail(
+        "Samples must support the latest Node LTS well. See the errors above for more information."
+      );
+    }
+
+    return results;
+  });
 }
 
 /**
