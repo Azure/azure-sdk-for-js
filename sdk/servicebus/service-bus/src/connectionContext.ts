88db4613f088dc4771cd0faa5816778925026958
Jeremy Meng <jeremy.ymeng@gmail.com>
2021-11-23 01:13:02 +0000

    Update to the new rule name

--- sdk/servicebus/service-bus/src/connectionContext.ts
+++ sdk/servicebus/service-bus/src/connectionContext.ts
@@ -244,7 +244,7 @@ function getNumberOfReceivers(
 /**
  * @internal
  */
-// eslint-disable-next-line @azure/azure-sdk/ts-no-namespaces
+// eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace ConnectionContext {
   export function create(
     config: ConnectionConfig,
