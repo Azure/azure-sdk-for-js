93fa01e39cd1982b30148ac3070dae7404eabd5a
dw511214992 <dw511214992@gmail.com>
2021-11-26 15:28:30 +0800

    save

--- .scripts/sdk_init.sh
+++ .scripts/sdk_init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo ${PATH}
 npm install -g @microsoft/rush
-# rush install
-# # install release tools
-# npm install -g dw-azure-track2-js-sdk-release-tools
+rush update
+# install release tools
+npm install -g dw-azure-track2-js-sdk-release-tools
