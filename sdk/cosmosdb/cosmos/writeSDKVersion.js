0d865b362848f03548dfcb914d0e3aa14a6d9df8
sajeetharan <sajeefx@gmail.com>
2021-11-29 16:41:46 +0530

    fix integration tests - v3.15.0

--- sdk/cosmosdb/cosmos/writeSDKVersion.js
+++ sdk/cosmosdb/cosmos/writeSDKVersion.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const version = require("./package.json").version;
-const fileName = "./dist-esm/src/common/constants.js";
+const fileName = "./src/common/constants.ts";
 let contents = fs.readFileSync(fileName, "utf8");
 contents = contents.replace("REPLACE_SDK_VERSION", version);
 fs.writeFileSync(fileName, contents);
