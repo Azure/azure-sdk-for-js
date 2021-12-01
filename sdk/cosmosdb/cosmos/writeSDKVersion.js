701805b6406d9ea4d62458675dd3f3765b1e8741
sajeetharan <sajeefx@gmail.com>
2021-12-01 15:59:42 +0530

    update version check and remove writeSDKVersion.js

--- sdk/cosmosdb/cosmos/writeSDKVersion.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const version = require("./package.json").version;
-const fileName = "./src/common/constants.ts";
-let contents = fs.readFileSync(fileName, "utf8");
-contents = contents.replace("REPLACE_SDK_VERSION", version);
-fs.writeFileSync(fileName, contents);
