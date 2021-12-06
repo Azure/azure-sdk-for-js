d14a8b2d9101714f564ba10bfa8b0f39365194e9
colawwj <70128817+colawwj@users.noreply.github.com>
2021-12-06 19:00:20 +0800

    network-track2-ga (#18956)

--- sdk/network/arm-network/src/operations/peerExpressRouteCircuitConnections.ts
+++ sdk/network/arm-network/src/operations/peerExpressRouteCircuitConnections.ts
@@ -11,7 +11,7 @@ import { PeerExpressRouteCircuitConnections } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
 import * as Parameters from "../models/parameters";
-import { NetworkManagementClientContext } from "../networkManagementClientContext";
+import { NetworkManagementClient } from "../networkManagementClient";
 import {
   PeerExpressRouteCircuitConnection,
   PeerExpressRouteCircuitConnectionsListNextOptionalParams,
@@ -26,13 +26,13 @@ import {
 /** Class containing PeerExpressRouteCircuitConnections operations. */
 export class PeerExpressRouteCircuitConnectionsImpl
   implements PeerExpressRouteCircuitConnections {
-  private readonly client: NetworkManagementClientContext;
+  private readonly client: NetworkManagementClient;
 
   /**
    * Initialize a new instance of the class PeerExpressRouteCircuitConnections class.
    * @param client Reference to the service client
    */
-  constructor(client: NetworkManagementClientContext) {
+  constructor(client: NetworkManagementClient) {
     this.client = client;
   }
 
