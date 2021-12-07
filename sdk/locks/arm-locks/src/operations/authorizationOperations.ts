7162289961ada623d5b3ebfe0f23d6b54d3beb97
colawwj <70128817+colawwj@users.noreply.github.com>
2021-12-07 17:12:24 +0800

    locks-track2-ga (#18996)
    * locks-track2-ga
    
    * update

--- sdk/locks/arm-locks/src/operations/authorizationOperations.ts
+++ sdk/locks/arm-locks/src/operations/authorizationOperations.ts
@@ -6,33 +6,30 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import "@azure/core-paging";
 import { PagedAsyncIterableIterator } from "@azure/core-paging";
 import { AuthorizationOperations } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
 import * as Parameters from "../models/parameters";
-import { ManagementLockClientContext } from "../managementLockClientContext";
+import { ManagementLockClient } from "../managementLockClient";
 import {
   Operation,
   AuthorizationOperationsListNextOptionalParams,
   AuthorizationOperationsListOptionalParams,
-  AuthorizationOperationsListNextNextOptionalParams,
   AuthorizationOperationsListResponse,
-  AuthorizationOperationsListNextResponse,
-  AuthorizationOperationsListNextNextResponse
+  AuthorizationOperationsListNextResponse
 } from "../models";
 
 /// <reference lib="esnext.asynciterable" />
-/** Class representing a AuthorizationOperations. */
+/** Class containing AuthorizationOperations operations. */
 export class AuthorizationOperationsImpl implements AuthorizationOperations {
-  private readonly client: ManagementLockClientContext;
+  private readonly client: ManagementLockClient;
 
   /**
    * Initialize a new instance of the class AuthorizationOperations class.
    * @param client Reference to the service client
    */
-  constructor(client: ManagementLockClientContext) {
+  constructor(client: ManagementLockClient) {
     this.client = client;
   }
 
@@ -79,52 +76,6 @@ export class AuthorizationOperationsImpl implements AuthorizationOperations {
   }
 
   /**
-   * ListNext
-   * @param nextLink The nextLink from the previous successful call to the List method.
-   * @param options The options parameters.
-   */
-  public listNext(
-    nextLink: string,
-    options?: AuthorizationOperationsListNextOptionalParams
-  ): PagedAsyncIterableIterator<Operation> {
-    const iter = this.listNextPagingAll(nextLink, options);
-    return {
-      next() {
-        return iter.next();
-      },
-      [Symbol.asyncIterator]() {
-        return this;
-      },
-      byPage: () => {
-        return this.listNextPagingPage(nextLink, options);
-      }
-    };
-  }
-
-  private async *listNextPagingPage(
-    nextLink: string,
-    options?: AuthorizationOperationsListNextOptionalParams
-  ): AsyncIterableIterator<Operation[]> {
-    let result = await this._listNext(nextLink, options);
-    yield result.value || [];
-    let continuationToken = result.nextLink;
-    while (continuationToken) {
-      result = await this._listNextNext(continuationToken, options);
-      continuationToken = result.nextLink;
-      yield result.value || [];
-    }
-  }
-
-  private async *listNextPagingAll(
-    nextLink: string,
-    options?: AuthorizationOperationsListNextOptionalParams
-  ): AsyncIterableIterator<Operation> {
-    for await (const page of this.listNextPagingPage(nextLink, options)) {
-      yield* page;
-    }
-  }
-
-  /**
    * Lists all of the available Microsoft.Authorization REST API operations.
    * @param options The options parameters.
    */
@@ -148,21 +99,6 @@ export class AuthorizationOperationsImpl implements AuthorizationOperations {
       listNextOperationSpec
     );
   }
-
-  /**
-   * ListNextNext
-   * @param nextLink The nextLink from the previous successful call to the ListNext method.
-   * @param options The options parameters.
-   */
-  private _listNextNext(
-    nextLink: string,
-    options?: AuthorizationOperationsListNextNextOptionalParams
-  ): Promise<AuthorizationOperationsListNextNextResponse> {
-    return this.client.sendOperationRequest(
-      { nextLink, options },
-      listNextNextOperationSpec
-    );
-  }
 }
 // Operation Specifications
 const serializer = coreClient.createSerializer(Mappers, /* isXml */ false);
@@ -181,19 +117,6 @@ const listOperationSpec: coreClient.OperationSpec = {
   serializer
 };
 const listNextOperationSpec: coreClient.OperationSpec = {
-  path: "{nextLink}",
-  httpMethod: "GET",
-  responses: {
-    200: {
-      bodyMapper: Mappers.OperationListResult
-    }
-  },
-  queryParameters: [Parameters.apiVersion],
-  urlParameters: [Parameters.$host, Parameters.nextLink],
-  headerParameters: [Parameters.accept],
-  serializer
-};
-const listNextNextOperationSpec: coreClient.OperationSpec = {
   path: "{nextLink}",
   httpMethod: "GET",
   responses: {
