3df24962698e0897ccc580a534a2e8aaa51d2486
hamsas <hamsas@cs.washington.edu>
2021-12-07 19:26:53 -0800

    removed excluding code

--- common/tools/eslint-plugin-azure-sdk/src/rules/ts-doc-internal-private-member.ts
+++ common/tools/eslint-plugin-azure-sdk/src/rules/ts-doc-internal-private-member.ts
@@ -10,9 +10,6 @@ import { ParserServices, TSESTree } from "@typescript-eslint/experimental-utils"
 import { ParserWeakMapESTreeToTSNode } from "@typescript-eslint/typescript-estree/dist/parser-options";
 import { Rule } from "eslint";
 import { Node } from "estree";
-import { readFileSync } from "fs";
-import { sync as globSync } from "glob";
-import { relative } from "path";
 import { Modifier, SyntaxKind } from "typescript";
 import { getRuleMetaData } from "../utils";
 
@@ -47,12 +44,13 @@ const reportInternal = (
       TSDocTags = TSDocTags.concat(
         TSDocComment.tags !== undefined
           ? TSDocComment.tags.map((TSDocTag: any): string => TSDocTag.tagName.escapedText)
+            .filter((TSDocTag: any) => TSDocTag.match(/internal/))
           : []
       );
     });
 
     // see if any tags match internal
-    if (TSDocTags.some((TSDocTag: string): boolean => /(internal)/.test(TSDocTag))) {
+    if (TSDocTags.length > 0) {
       context.report({
         node: node,
         message: "private class members should not include an @internal tag"
@@ -61,46 +59,12 @@ const reportInternal = (
   }
 };
 
-/**
- * Determine whether this rule should examine a given file
- * @param fileName the filename of the file in question
- * @param exclude the list of files excluded by TypeDoc (other than those in node_modues)
- * @returns false if not in src or is excluded by TypeDoc
- */
-const shouldExamineFile = (fileName: string, exclude: string[]): boolean => {
-  if (!/src/.test(fileName)) {
-    return false;
-  }
-  const relativePath = relative("", fileName).replace(/\\/g, "/");
-  return !exclude.includes(relativePath);
-};
-
-let exclude: string[] = [];
-try {
-  const typeDocText = readFileSync("typedoc.json", "utf8");
-  const typeDoc = JSON.parse(typeDocText);
-
-  // if typeDoc.exclude exists, add all files matching the glob patterns to exclude
-  if (typeDoc.exclude !== undefined) {
-    typeDoc.exclude.forEach((excludedGlob: string): void => {
-      exclude = exclude.concat(
-        globSync(excludedGlob).filter(
-          (excludeFile: string): boolean => !/node_modules/.test(excludeFile)
-        )
-      );
-    });
-  }
-} catch (err) {
-  exclude = [];
-}
-
 export = {
   meta: getRuleMetaData(
     "ts-doc-internal-private-member",
     "requires TSDoc comments to not include an '@internal' tag if the object is private"
   ),
   create: (context: Rule.RuleContext): Rule.RuleListener => {
-    const fileName = context.getFilename();
 
     const parserServices = context.parserServices as ParserServices;
     if (
@@ -112,20 +76,18 @@ export = {
 
     const converter = parserServices.esTreeNodeToTSNodeMap;
 
-    return shouldExamineFile(fileName, exclude)
-      ? {
-        // callback functions
+    return {
+      // callback functions
 
-        // container declarations
-        ":matches(TSInterfaceDeclaration, ClassDeclaration, TSModuleDeclaration)": (
-          node: Node
-        ): void => reportInternal(node, context, converter),
+      // container declarations
+      ":matches(TSInterfaceDeclaration, ClassDeclaration, TSModuleDeclaration)": (
+        node: Node
+      ): void => reportInternal(node, context, converter),
 
-        // functions
-        ":function": (node: Node): void => {
-          reportInternal(node, context, converter);
-        }
+      // functions
+      ":function": (node: Node): void => {
+        reportInternal(node, context, converter);
       }
-      : {};
+    };
   }
 };
