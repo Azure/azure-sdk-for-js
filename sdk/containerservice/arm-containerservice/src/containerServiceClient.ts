6ba618c2ed6d7675ad82ccc5a2fed5e51ccdf0a7
colawwj <v-sigangzhou@microsoft.com>
2021-12-09 10:44:44 +0800

    GA

--- sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
+++ sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
@@ -63,7 +63,7 @@ export class ContainerServiceClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-containerservice/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-containerservice/15.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
