0dfa2ec86007a812283a3a25f99bd97dc6596de5
SDKAuto <sdkautomation@microsoft.com>
2021-12-10 03:09:58 +0000

    CodeGen from PR 17000 in Azure/azure-rest-api-specs [Go] Track2 modify readme.go.md 9 (#17000)
    * [Go] Track2 modify readme.go.md 9
    
    * fix

--- sdk/support/arm-support/src/index.ts
+++ sdk/support/arm-support/src/index.ts
@@ -6,8 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./operations";
-export * from "./services";
-export * from "./problemClassifications";
-export * from "./supportTickets";
-export * from "./communications";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { MicrosoftSupport } from "./microsoftSupport";
+export * from "./operationsInterfaces";
