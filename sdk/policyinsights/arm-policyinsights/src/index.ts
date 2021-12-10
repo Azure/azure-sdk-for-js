15aa63764c9def88e26851a629b7f396e9165455
colawwj <v-sigangzhou@microsoft.com>
2021-12-10 10:44:17 +0800

    policyinsights-track2

--- sdk/policyinsights/arm-policyinsights/src/index.ts
+++ sdk/policyinsights/arm-policyinsights/src/index.ts
@@ -6,11 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export {
-  PolicyDetails,
-  PolicyTrackedResource,
-  PolicyTrackedResourcesQueryResults,
-  QueryFailure,
-  QueryFailureError,
-  TrackedResourceModificationDetails
-} from "../models/mappers";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { PolicyInsightsClient } from "./policyInsightsClient";
+export * from "./operationsInterfaces";
