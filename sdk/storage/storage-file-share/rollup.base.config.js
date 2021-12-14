6ce8cc2e3ca15090296d9efdd66492e1b33dd84d
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-12-14 15:00:25 -0800

    Ignore chai circular dependency in storage-file-share rollup base config

--- sdk/storage/storage-file-share/rollup.base.config.js
+++ sdk/storage/storage-file-share/rollup.base.config.js
@@ -64,6 +64,14 @@ export function nodeConfig(test = false) {
         return;
       }
 
+      if (
+        warning.code === "CIRCULAR_DEPENDENCY" &&
+        warning.importer.indexOf(path.normalize("node_modules/chai")) >= 0
+      ) {
+        // Ignore Chai circular dependency
+        return;
+      }
+
       if (warning.code === "CIRCULAR_DEPENDENCY") {
         throw new Error(warning.message);
       }
@@ -154,6 +162,14 @@ export function browserConfig(test = false) {
       ) {
         // opentelemetry contains circular references but it doesn't cause issues.
         // Tracked in https://github.com/open-telemetry/opentelemetry-js-api/issues/87
+        return;
+      }
+
+      if (
+        warning.code === "CIRCULAR_DEPENDENCY" &&
+        warning.importer.indexOf(path.normalize("node_modules/chai")) >= 0
+      ) {
+        // Ignore Chai circular dependency
         return;
       }
 
