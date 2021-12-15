2155635af3e2c9626e6930d931a4855b5f0ec99a
Alonso Mondal <alonsomondal@microsoft.com>
2021-12-15 14:58:03 -0600

    fixing mocked client

--- sdk/communication/communication-short-codes/test/public/manageUSProgramBriefs.spec.ts
+++ sdk/communication/communication-short-codes/test/public/manageUSProgramBriefs.spec.ts
@@ -97,5 +97,5 @@ describe(`ShortCodesClient - creates, gets, updates, lists, and deletes US Progr
       await doesProgramBriefExist(client, uspb.id),
       "Delete program brief was unsuccessful, program brief is still returned"
     );
-  }).timeout(15000);
+  }).timeout(25000);
 });
--- sdk/communication/communication-short-codes/test/public/utils/mockHttpClients.ts
+++ sdk/communication/communication-short-codes/test/public/utils/mockHttpClients.ts
@@ -3,14 +3,17 @@
 
 import { HttpClient, PipelineRequest, PipelineResponse } from "@azure/core-rest-pipeline";
 
-export const createMockHttpClient = (status: number = 200, bodyAsText?: string): HttpClient => {
+export const createMockHttpClient = <T = Record<string, unknown>>(
+  status: number = 200,
+  parsedBody?: T
+): HttpClient => {
   return {
     async sendRequest(request: PipelineRequest): Promise<PipelineResponse> {
       return {
         status,
         request,
         headers: request.headers,
-        bodyAsText
+        bodyAsText: JSON.stringify(parsedBody)
       };
     },
   };
