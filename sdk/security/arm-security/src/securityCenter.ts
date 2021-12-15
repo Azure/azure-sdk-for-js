c1f26c74fbb52363458fb7eab1dee29cadfdf39f
colawwj <v-sigangzhou@microsoft.com>
2021-12-15 13:38:52 +0800

    version update

--- sdk/security/arm-security/src/securityCenter.ts
+++ sdk/security/arm-security/src/securityCenter.ts
@@ -150,7 +150,7 @@ export class SecurityCenter extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-security/4.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-security/4.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
