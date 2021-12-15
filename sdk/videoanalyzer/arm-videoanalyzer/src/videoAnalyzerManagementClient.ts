6f980596d9b4bf255c18b41c6df712fd8327616e
colawwj <70128817+colawwj@users.noreply.github.com>
2021-12-15 17:08:02 +0800

    videoanalyzer-track2-ga (#19321)

--- sdk/videoanalyzer/arm-videoanalyzer/src/videoAnalyzerManagementClient.ts
+++ sdk/videoanalyzer/arm-videoanalyzer/src/videoAnalyzerManagementClient.ts
@@ -6,6 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
+import * as coreClient from "@azure/core-client";
 import * as coreAuth from "@azure/core-auth";
 import {
   EdgeModulesImpl,
@@ -45,12 +46,15 @@ import {
   Videos,
   AccessPolicies
 } from "./operationsInterfaces";
-import { VideoAnalyzerContext } from "./videoAnalyzerContext";
-import { VideoAnalyzerOptionalParams } from "./models";
+import { VideoAnalyzerManagementClientOptionalParams } from "./models";
 
-export class VideoAnalyzer extends VideoAnalyzerContext {
+export class VideoAnalyzerManagementClient extends coreClient.ServiceClient {
+  $host: string;
+  subscriptionId: string;
+  apiVersion: string;
+
   /**
-   * Initializes a new instance of the VideoAnalyzer class.
+   * Initializes a new instance of the VideoAnalyzerManagementClient class.
    * @param credentials Subscription credentials which uniquely identify client subscription.
    * @param subscriptionId The ID of the target subscription.
    * @param options The parameter options
@@ -58,9 +62,48 @@ export class VideoAnalyzer extends VideoAnalyzerContext {
   constructor(
     credentials: coreAuth.TokenCredential,
     subscriptionId: string,
-    options?: VideoAnalyzerOptionalParams
+    options?: VideoAnalyzerManagementClientOptionalParams
   ) {
-    super(credentials, subscriptionId, options);
+    if (credentials === undefined) {
+      throw new Error("'credentials' cannot be null");
+    }
+    if (subscriptionId === undefined) {
+      throw new Error("'subscriptionId' cannot be null");
+    }
+
+    // Initializing default values for options
+    if (!options) {
+      options = {};
+    }
+    const defaults: VideoAnalyzerManagementClientOptionalParams = {
+      requestContentType: "application/json; charset=utf-8",
+      credential: credentials
+    };
+
+    const packageDetails = `azsdk-js-arm-videoanalyzer/1.0.0-beta.2`;
+    const userAgentPrefix =
+      options.userAgentOptions && options.userAgentOptions.userAgentPrefix
+        ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
+        : `${packageDetails}`;
+
+    if (!options.credentialScopes) {
+      options.credentialScopes = ["https://management.azure.com/.default"];
+    }
+    const optionsWithDefaults = {
+      ...defaults,
+      ...options,
+      userAgentOptions: {
+        userAgentPrefix
+      },
+      baseUri: options.endpoint || "https://management.azure.com"
+    };
+    super(optionsWithDefaults);
+    // Parameter assignments
+    this.subscriptionId = subscriptionId;
+
+    // Assigning values to Constant parameters
+    this.$host = options.$host || "https://management.azure.com";
+    this.apiVersion = options.apiVersion || "2021-11-01-preview";
     this.edgeModules = new EdgeModulesImpl(this);
     this.pipelineTopologies = new PipelineTopologiesImpl(this);
     this.livePipelines = new LivePipelinesImpl(this);
