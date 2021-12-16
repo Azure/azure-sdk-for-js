1120e3173106d047a1f725dd6af66deebb3da1ce
SDKAuto <sdkautomation@microsoft.com>
2021-12-16 06:02:15 +0000

    CodeGen from PR 17111 in Azure/azure-rest-api-specs Add batch tag for release track1 (#17111)

--- sdk/recoveryservices/arm-recoveryservices/src/recoveryServicesClient.ts
+++ sdk/recoveryservices/arm-recoveryservices/src/recoveryServicesClient.ts
@@ -72,7 +72,7 @@ export class RecoveryServicesClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-recoveryservices/5.0.0`;
+    const packageDetails = `azsdk-js-arm-recoveryservices/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
