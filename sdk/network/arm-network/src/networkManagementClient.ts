c4fa602890790c508c77eb2a9fd9340549428726
SDKAuto <sdkautomation@microsoft.com>
2021-12-16 06:01:03 +0000

    CodeGen from PR 17111 in Azure/azure-rest-api-specs Add batch tag for release track1 (#17111)

--- sdk/network/arm-network/src/networkManagementClient.ts
+++ sdk/network/arm-network/src/networkManagementClient.ts
@@ -307,7 +307,7 @@ export class NetworkManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-network/26.0.0`;
+    const packageDetails = `azsdk-js-arm-network/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
