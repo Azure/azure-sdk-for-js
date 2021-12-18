1ba0ffe2e86f6a41b1f8c0e87b22fa82fdec1ee2
Harsha Nalluru <sanallur@microsoft.com>
2021-12-18 00:43:27 +0000

    fix imports

--- sdk/test-utils/recorder-new/src/index.ts
+++ sdk/test-utils/recorder-new/src/index.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export { recorderHttpPolicy, RecorderClient } from "./recorder";
+export { RecorderClient } from "./recorder";
 export { relativeRecordingsPath } from "./utils/relativePathCalculator";
 export { SanitizerOptions, RecorderStartOptions } from "./utils/utils";
 export { env } from "./utils/env";
