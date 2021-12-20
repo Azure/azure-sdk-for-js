ffd3b1a3fd1c44a9a2ce54818486a9cdfb09d8db
Harsha Nalluru <sanallur@microsoft.com>
2021-12-20 22:51:00 +0000

    docs for formatPath

--- sdk/test-utils/recorder-new/src/utils/filePathGenerator.ts
+++ sdk/test-utils/recorder-new/src/utils/filePathGenerator.ts
@@ -1,6 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/**
+ * This method is used while generating the file/folder path using the describe/it block titles.
+ *
+ * Since those titles may contain symbols such as `<`, "=" or even ' ', we'll replace them
+ * with strings representing those symbols or with something that reads better as a file name.
+ *
+ * If a test has some special character that is not being considered here,
+ * feel free to add the symbol and its replacement.
+ */
 export function formatPath(path: string): string {
   return path
     .toLowerCase()
