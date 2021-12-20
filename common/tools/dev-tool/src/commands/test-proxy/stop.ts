bbb1c79e566dd60495ad015335cfdcb615785aae
Timo van Veenendaal <timov@microsoft.com>
2021-12-20 14:25:17 -0800

    Stop the test proxy container explicitly when tests finish

---
+++ common/tools/dev-tool/src/commands/test-proxy/stop.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { leafCommand, makeCommandInfo } from "../../framework/command";
+import { config } from "dotenv";
+import { stopProxyTool } from "../../util/testProxyUtils";
+config();
+
+export const commandInfo = makeCommandInfo(
+  "test-proxy",
+  "stops the test proxy that was started with test-proxy start, if it was running",
+  {}
+);
+
+export default leafCommand(commandInfo, async () => {
+  await stopProxyTool();
+  return true;
+});
