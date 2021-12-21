95a0a64b8b7bc0bfb82cc8a8e6a2bbb42935c1dd
colawwj <70128817+colawwj@users.noreply.github.com>
2021-12-21 11:54:18 +0800

    cognitiveservices-track2-ga (#19465)

--- sdk/cognitiveservices/arm-cognitiveservices/src/cognitiveServicesManagementClient.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/src/cognitiveServicesManagementClient.ts
@@ -72,7 +72,7 @@ export class CognitiveServicesManagementClient extends coreClient.ServiceClient 
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-cognitiveservices/7.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-cognitiveservices/7.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
