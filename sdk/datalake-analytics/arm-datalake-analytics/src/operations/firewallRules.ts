11b12c965f2dcc5fee900fb052d6e048c10adaa5
colawwj <70128817+colawwj@users.noreply.github.com>
2021-12-23 11:10:50 +0800

    datalake-analytics-track2-ga (#19531)
    * datalake-analytics-track2-ga
    
    * update readme.md

--- sdk/datalake-analytics/arm-datalake-analytics/src/operations/firewallRules.ts
+++ sdk/datalake-analytics/arm-datalake-analytics/src/operations/firewallRules.ts
@@ -11,7 +11,7 @@ import { FirewallRules } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
 import * as Parameters from "../models/parameters";
-import { DataLakeAnalyticsAccountManagementClientContext } from "../dataLakeAnalyticsAccountManagementClientContext";
+import { DataLakeAnalyticsAccountManagementClient } from "../dataLakeAnalyticsAccountManagementClient";
 import {
   FirewallRule,
   FirewallRulesListByAccountNextOptionalParams,
@@ -31,13 +31,13 @@ import {
 /// <reference lib="esnext.asynciterable" />
 /** Class containing FirewallRules operations. */
 export class FirewallRulesImpl implements FirewallRules {
-  private readonly client: DataLakeAnalyticsAccountManagementClientContext;
+  private readonly client: DataLakeAnalyticsAccountManagementClient;
 
   /**
    * Initialize a new instance of the class FirewallRules class.
    * @param client Reference to the service client
    */
-  constructor(client: DataLakeAnalyticsAccountManagementClientContext) {
+  constructor(client: DataLakeAnalyticsAccountManagementClient) {
     this.client = client;
   }
 
