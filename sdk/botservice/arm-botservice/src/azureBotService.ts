d8699eb8d9c2a3db828625460ea26ffff13f21e9
colawwj <v-sigangzhou@microsoft.com>
2021-12-27 10:42:16 +0800

    update2

--- sdk/botservice/arm-botservice/src/azureBotService.ts
+++ sdk/botservice/arm-botservice/src/azureBotService.ts
@@ -64,7 +64,7 @@ export class AzureBotService extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-botservice/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-botservice/4.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
