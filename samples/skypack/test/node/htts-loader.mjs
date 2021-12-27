04383956415a383abe1043b396585d1df2477ca8
Jeremy Meng <yumeng@microsoft.com>
2021-12-27 14:51:44 -0800

    Add a minimal nodejs test
    It only imports blob from skypack at the moment because there are
    still skypack issues blocking running

---
+++ samples/skypack/test/node/htts-loader.mjs
@@ -0,0 +1,44 @@
+// https-loader.mjs
+import { get } from 'https';
+
+export function resolve(specifier, context, defaultResolve) {
+  const { parentURL = null } = context;
+
+  // Normally Node.js would error on specifiers starting with 'https://', so
+  // this hook intercepts them and converts them into absolute URLs to be
+  // passed along to the later hooks below.
+  if (specifier.startsWith('https://')) {
+    return {
+      url: specifier
+    };
+  } else if (parentURL && parentURL.startsWith('https://')) {
+    return {
+      url: new URL(specifier, parentURL).href
+    };
+  }
+
+  // Let Node.js handle all other specifiers.
+  return defaultResolve(specifier, context, defaultResolve);
+}
+
+export function load(url, context, defaultLoad) {
+  // For JavaScript to be loaded over the network, we need to fetch and
+  // return it.
+  if (url.startsWith('https://')) {
+    return new Promise((resolve, reject) => {
+      get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => data += chunk);
+        res.on('end', () => resolve({
+          // This example assumes all network-provided JavaScript is ES module
+          // code.
+          format: 'module',
+          source: data,
+        }));
+      }).on('error', (err) => reject(err));
+    });
+  }
+
+  // Let Node.js handle all other URLs.
+  return defaultLoad(url, context, defaultLoad);
+}
---
+++ samples/skypack/test/node/storageBlob.spec.mjs
@@ -0,0 +1,3 @@
+import { BlobClient } from "https://cdn.skypack.dev/@azure/storage-blob";
+
+console.log(BlobClient);
