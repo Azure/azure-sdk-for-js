61d5a7582b9b2df279f1c7869a1fec896703d000
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-12-27 17:01:59 -0600

    Update WebPubSub

--- sdk/web-pubsub/web-pubsub-express/test/user.spec.ts
+++ sdk/web-pubsub/web-pubsub-express/test/user.spec.ts
@@ -34,16 +34,16 @@ function mockBody(req: IncomingMessage, body: string): void {
   req.emit("end");
 }
 
-describe("Can handle user event", function() {
+describe("Can handle user event", function () {
   let req: IncomingMessage;
   let res: ServerResponse;
 
-  beforeEach(function() {
+  beforeEach(function () {
     req = new IncomingMessage(new Socket());
     res = new ServerResponse(req);
   });
 
-  it("Should not handle the request if request is not cloud events", async function() {
+  it("Should not handle the request if request is not cloud events", async function () {
     const endSpy = sinon.spy(res.end);
 
     const dispatcher = new CloudEventsDispatcher("hub1");
@@ -52,7 +52,7 @@ describe("Can handle user event", function() {
     assert.isTrue(endSpy.notCalled);
   });
 
-  it("Should not handle the request if hub does not match", async function() {
+  it("Should not handle the request if hub does not match", async function () {
     const endSpy = sinon.spy(res.end);
     buildRequest(req, "hub", "conn1");
 
@@ -62,7 +62,7 @@ describe("Can handle user event", function() {
     assert.isTrue(endSpy.notCalled);
   });
 
-  it("Should response with 200 when option is not specified", async function() {
+  it("Should response with 200 when option is not specified", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
@@ -73,7 +73,7 @@ describe("Can handle user event", function() {
     assert.equal(200, res.statusCode, "should be 200");
   });
 
-  it("Should response with 200 when handler is not specified", async function() {
+  it("Should response with 200 when handler is not specified", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
@@ -84,14 +84,14 @@ describe("Can handle user event", function() {
     assert.equal(200, res.statusCode, "should be 200");
   });
 
-  it("Should response with error when handler returns error", async function() {
+  it("Should response with error when handler returns error", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
     const dispatcher = new CloudEventsDispatcher("hub", {
       handleUserEvent: async (_, response) => {
         response.fail(500);
-      }
+      },
     });
     const process = dispatcher.handleRequest(req, res);
     mockBody(req, JSON.stringify({}));
@@ -101,14 +101,14 @@ describe("Can handle user event", function() {
     assert.equal(500, res.statusCode, "should be error");
   });
 
-  it("Should response with success when handler returns success", async function() {
+  it("Should response with success when handler returns success", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
     const dispatcher = new CloudEventsDispatcher("hub", {
       handleUserEvent: async (_, response) => {
         response.success();
-      }
+      },
     });
     const process = dispatcher.handleRequest(req, res);
     mockBody(req, JSON.stringify({}));
@@ -118,14 +118,14 @@ describe("Can handle user event", function() {
     assert.equal(200, res.statusCode, "should be success");
   });
 
-  it("Should response with success when returns success binary", async function() {
+  it("Should response with success when returns success binary", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
     const dispatcher = new CloudEventsDispatcher("hub", {
       handleUserEvent: async (_, response) => {
         response.success("a");
-      }
+      },
     });
     const process = dispatcher.handleRequest(req, res);
     mockBody(req, JSON.stringify({}));
@@ -136,14 +136,14 @@ describe("Can handle user event", function() {
     assert.equal("application/octet-stream", res.getHeader("content-type"), "should be binary");
   });
 
-  it("Should response with success when returns success text", async function() {
+  it("Should response with success when returns success text", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
     const dispatcher = new CloudEventsDispatcher("hub", {
       handleUserEvent: async (_, response) => {
         response.success("a", "text");
-      }
+      },
     });
     const process = dispatcher.handleRequest(req, res);
     mockBody(req, JSON.stringify({}));
@@ -154,14 +154,14 @@ describe("Can handle user event", function() {
     assert.equal("text/plain; charset=utf-8", res.getHeader("content-type"), "should be text");
   });
 
-  it("Should response with success when returns success json", async function() {
+  it("Should response with success when returns success json", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
     const dispatcher = new CloudEventsDispatcher("hub", {
       handleUserEvent: async (_, response) => {
         response.success("a", "json");
-      }
+      },
     });
     const process = dispatcher.handleRequest(req, res);
     mockBody(req, JSON.stringify({}));
@@ -176,7 +176,7 @@ describe("Can handle user event", function() {
     );
   });
 
-  it("Should be able to set connection state", async function() {
+  it("Should be able to set connection state", async function () {
     const endSpy = sinon.spy(res, "end");
     buildRequest(req, "hub", "conn1");
 
@@ -187,7 +187,7 @@ describe("Can handle user event", function() {
         response.setState("key1", "val3");
         response.setState("key3", "");
         response.success();
-      }
+      },
     });
     const process = dispatcher.handleRequest(req, res);
     mockBody(req, JSON.stringify({}));
@@ -201,7 +201,7 @@ describe("Can handle user event", function() {
         JSON.stringify({
           key1: "val3",
           key2: "val2",
-          key3: ""
+          key3: "",
         })
       ).toString("base64"),
       res.getHeader("ce-connectionState"),
