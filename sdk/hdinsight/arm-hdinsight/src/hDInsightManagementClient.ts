70ebddd4a8841250ad44a555453026590175b2e8
colawwj <v-sigangzhou@microsoft.com>
2021-12-29 16:25:36 +0800

    changelog update

--- sdk/hdinsight/arm-hdinsight/src/hDInsightManagementClient.ts
+++ sdk/hdinsight/arm-hdinsight/src/hDInsightManagementClient.ts
@@ -69,7 +69,7 @@ export class HDInsightManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-hdinsight/1.0.0.beta.1`;
+    const packageDetails = `azsdk-js-arm-hdinsight/1.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
