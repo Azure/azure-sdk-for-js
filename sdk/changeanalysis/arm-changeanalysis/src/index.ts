7d1a1dfee75b75a6df076db19234e44cf3e930d2
colawwj <70128817+colawwj@users.noreply.github.com>
2021-12-30 18:52:16 +0800

    changeanalysis-track2 (#19598)
    * changeanalysis-track2
    
    * update

--- sdk/changeanalysis/arm-changeanalysis/src/index.ts
+++ sdk/changeanalysis/arm-changeanalysis/src/index.ts
@@ -6,6 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./operations";
-export * from "./resourceChanges";
-export * from "./changes";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { AzureChangeAnalysisManagementClient } from "./azureChangeAnalysisManagementClient";
+export * from "./operationsInterfaces";
