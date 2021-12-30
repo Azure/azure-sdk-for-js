338d5c93cdc28edcd2484a2ab5204710d91deddf
colawwj <v-sigangzhou@microsoft.com>
2021-12-30 15:07:53 +0800

    update

--- sdk/azurestack/arm-azurestack/test/sampleTest.ts
+++ sdk/azurestack/arm-azurestack/test/sampleTest.ts
@@ -10,7 +10,9 @@ import {
   env,
   record,
   RecorderEnvironmentSetup,
-  Recorder
+  Recorder,
+  delay,
+  isPlaybackMode
 } from "@azure-tools/test-recorder";
 import * as assert from "assert";
 
@@ -29,6 +31,10 @@ const recorderEnvSetup: RecorderEnvironmentSetup = {
       )
   ],
   queryParametersToSkip: []
+};
+
+export const testPollingOptions = {
+  updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
 describe("My test", () => {
