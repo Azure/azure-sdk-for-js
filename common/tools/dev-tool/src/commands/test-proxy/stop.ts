0852e137718c74824b281bec70f3fec8571ec827
Timo van Veenendaal <timov@microsoft.com>
2022-01-04 11:33:59 -0800

    Remove unnecessary .env

--- common/tools/dev-tool/src/commands/test-proxy/stop.ts
+++ common/tools/dev-tool/src/commands/test-proxy/stop.ts
@@ -2,14 +2,12 @@
 // Licensed under the MIT license.
 
 import { leafCommand, makeCommandInfo } from "../../framework/command";
-import { config } from "dotenv";
 import { stopProxyTool } from "../../util/testProxyUtils";
-config();
 
 export const commandInfo = makeCommandInfo(
   "test-proxy",
   "stops the test proxy that was started with test-proxy start, if it was running",
-  {},
+  {}
 );
 
 export default leafCommand(commandInfo, async () => {
