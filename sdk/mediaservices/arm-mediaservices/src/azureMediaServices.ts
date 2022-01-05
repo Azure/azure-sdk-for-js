76ca25f14939f448e58368a77317e548e483362a
SDKAuto <sdkautomation@microsoft.com>
2022-01-05 07:50:29 +0000

    CodeGen from PR 16431 in Azure/azure-rest-api-specs Add track2 batch 4 mysql-->cognitiveservices (#16431)
    * Add track2 batch 4 mysql-->cognitiveservices
    
    * resourcehealth-->sqlvirtualmachine

--- sdk/mediaservices/arm-mediaservices/src/azureMediaServices.ts
+++ sdk/mediaservices/arm-mediaservices/src/azureMediaServices.ts
@@ -78,7 +78,7 @@ export class AzureMediaServices extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-mediaservices/1.0.0`;
+    const packageDetails = `azsdk-js-arm-mediaservices/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
