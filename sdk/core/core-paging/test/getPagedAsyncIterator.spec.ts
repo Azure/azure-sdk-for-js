1a77b3c65efc97a01b1cc3fe174b93299f3e3d29
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2022-01-04 19:35:12 -0500

    [core-paging] Use a TS v3.4-friendly iterator protocol (#19507)
    * [core-paging] Use a TS v3.4-friendly iterator protocol
    
    * update sample to check intellisense experience
    
    * update samples with type checks for customer experience
    
    * address feedback and convert sample to tests

--- sdk/core/core-paging/test/getPagedAsyncIterator.spec.ts
+++ sdk/core/core-paging/test/getPagedAsyncIterator.spec.ts
@@ -122,4 +122,88 @@ describe("getPagedAsyncIterator", function () {
     assert.equal(pagesCount, Math.ceil(collection.length / maxPageSize));
     assert.deepEqual([].concat(...receivedPages), collection);
   });
+
+  describe("Strong typing experience", function () {
+    type IsAny<T> = boolean extends (T extends never ? true : false) ? true : false;
+    function assertNotAny<T extends IsAny<T> extends true ? never : any>(_: T): void {}
+    function assertAny<T extends IsAny<T> extends false ? never : any>(_: T): void {}
+
+    const totalElementsCount = 100;
+    const collection = Array.from(Array(totalElementsCount), (_, i) => i + 1);
+    const pagedResult: PagedResult<number[], PageSettings, number> = {
+      firstPageLink: 0,
+      async getPage(pageLink, maxPageSize) {
+        const top = maxPageSize || 5;
+        if (pageLink < collection.length) {
+          return Promise.resolve({
+            page: collection.slice(pageLink, Math.min(pageLink + top, collection.length)),
+            nextPageLink: top < collection.length - pageLink ? pageLink + top : undefined,
+          });
+        } else {
+          assert.fail("should not get here");
+        }
+      },
+    };
+    // instantiate the generic type variables so that TElement does not default to unknown
+    const iterator = getPagedAsyncIterator<number, number[], PageSettings, number>(pagedResult);
+
+    describe("using for-await", function () {
+      it("elements and pages are not typed as any", async function () {
+        for await (const page of iterator.byPage({ maxPageSize: 5 })) {
+          assertNotAny(page);
+          for (const element of page) {
+            assertNotAny(element);
+          }
+        }
+      });
+
+      it("elements are not typed as any", async function () {
+        for await (const element of iterator) {
+          assertNotAny(element);
+        }
+      });
+    });
+
+    describe("calling next", function () {
+      it("elements and pages are not typed as any", async function () {
+        const pageIterator = iterator.byPage({ maxPageSize: 5 });
+        let page = await pageIterator.next();
+        while (!page.done) {
+          assertNotAny(page.value);
+          for (const element of page.value) {
+            assertNotAny(element);
+          }
+          page = await pageIterator.next();
+        }
+        assertAny(page.value);
+        if (page.value !== undefined) {
+          assert.fail(`next() should not return a value but it returned ${page.value}`);
+        }
+      });
+
+      it("elements are not typed as any", async function () {
+        let element = await iterator.next();
+        while (!element.done) {
+          assertNotAny(element);
+          element = await iterator.next();
+        }
+        assertAny(element.value);
+        if (element.value !== undefined) {
+          assert.fail(`next() should not return a value but it returned ${element.value}`);
+        }
+      });
+
+      it("elements are typed as any if done is not checked", async function () {
+        let element = await iterator.next();
+        while (element.value !== undefined) {
+          // without checking the `done` field, the type of the `value` field will be `any`.
+          // The reason being that the typechecker does not know whether `value` was yielded (T)
+          // or returned (any).
+          // see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-6.html#stricter-generators
+          assertAny(element.value);
+          element = await iterator.next();
+        }
+      });
+    });
+  });
 });
