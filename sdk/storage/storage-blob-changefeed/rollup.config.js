0d8ef3ea6c3ecf18948206dbcf5d89fde3491cb1
Jeremy Meng <yumeng@microsoft.com>
2022-01-05 12:56:36 -0800

    format

--- sdk/storage/storage-blob-changefeed/rollup.config.js
+++ sdk/storage/storage-blob-changefeed/rollup.config.js
@@ -5,5 +5,5 @@ import { makeConfig } from "@azure/dev-tool/shared-config/rollup";
 
 export default makeConfig(require("./package.json"), {
   // Disable this until we are ready to run rollup for the browser.
-  disableBrowserBundle: true
+  disableBrowserBundle: true,
 });
