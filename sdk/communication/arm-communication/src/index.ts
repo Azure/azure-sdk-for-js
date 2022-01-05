0de1d9f5da165c8972b58f91c37c3e03a84ea6bf
SDKAuto <sdkautomation@microsoft.com>
2022-01-05 06:08:38 +0000

    CodeGen from PR 16870 in Azure/azure-rest-api-specs Track2 modify readme.go.md (#16870)

--- sdk/communication/arm-communication/src/index.ts
+++ sdk/communication/arm-communication/src/index.ts
@@ -6,9 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export {
-  ErrorAdditionalInfo,
-  ErrorDetail,
-  ErrorResponse,
-  OperationStatus
-} from "../models/mappers";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { CommunicationServiceManagementClient } from "./communicationServiceManagementClient";
+export * from "./operationsInterfaces";
