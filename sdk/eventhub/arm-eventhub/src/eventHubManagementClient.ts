6045f105ca35cab77906184b17e95bfe067053de
SDKAuto <sdkautomation@microsoft.com>
2022-01-05 11:36:06 +0000

    CodeGen from PR 16780 in Azure/azure-rest-api-specs Add 2020-09-01-hybrid profile overrides for Java tags (#16780)
    * Add network overrides for java hybrid-2020-09-01
    
    * Add authorization overrides for java hybrid-2020-09-01
    
    * Add compute overrides for java hybrid-2020-09-01
    
    * Add eventhubs overrides for java hybrid-2020-09-01
    
    * Add storage overrides for java hybrid-2020-09-01
    
    * Add containerservice overrides for java hybrid-2020-09-01
    
    Some extra changes were needed because locations.json was being added
    everywhere and this was conflicting with the version supported for
    Azure Stack Hub.
    
    * appservice - Adding a tag for 2020-09-01-hybrid profile
    
    Co-authored-by: bganapa <bganapa@microsoft.com>

--- sdk/eventhub/arm-eventhub/src/eventHubManagementClient.ts
+++ sdk/eventhub/arm-eventhub/src/eventHubManagementClient.ts
@@ -67,7 +67,7 @@ export class EventHubManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-eventhub/5.0.0`;
+    const packageDetails = `azsdk-js-arm-eventhub/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
