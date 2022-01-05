1cb78b909fd3aa52e8a4cfe5c30bc94791791194
SDKAuto <sdkautomation@microsoft.com>
2022-01-05 07:53:18 +0000

    CodeGen from PR 16431 in Azure/azure-rest-api-specs Add track2 batch 4 mysql-->cognitiveservices (#16431)
    * Add track2 batch 4 mysql-->cognitiveservices
    
    * resourcehealth-->sqlvirtualmachine

--- sdk/operationsmanagement/arm-operations/src/operationsManagementClient.ts
+++ sdk/operationsmanagement/arm-operations/src/operationsManagementClient.ts
@@ -73,7 +73,7 @@ export class OperationsManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-operations/4.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-operations/4.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
