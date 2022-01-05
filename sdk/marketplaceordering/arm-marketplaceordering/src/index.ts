afb4c19be6b4d01512db9dfb7e06f00fc54dc47b
SDKAuto <sdkautomation@microsoft.com>
2022-01-05 07:36:37 +0000

    CodeGen from PR 16949 in Azure/azure-rest-api-specs Track2 modify readme.go.md 6 (#16949)
    * Track2 modify readme.go.md 6
    
    * mariadb derective description

--- sdk/marketplaceordering/arm-marketplaceordering/src/index.ts
+++ sdk/marketplaceordering/arm-marketplaceordering/src/index.ts
@@ -6,5 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./marketplaceAgreements";
-export * from "./operations";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { MarketplaceOrderingAgreements } from "./marketplaceOrderingAgreements";
+export * from "./operationsInterfaces";
