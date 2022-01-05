32a924233d84ccdb8f6a8f84f1cce035cb65a716
SDKAuto <sdkautomation@microsoft.com>
2022-01-05 08:27:43 +0000

    CodeGen from PR 16886 in Azure/azure-rest-api-specs Track2 modify readme.go.md 3 (#16886)
    * Track2 modify readme.go.md
    
    * directive modelerfour.lenient-model-deduplication=true

--- sdk/desktopvirtualization/arm-desktopvirtualization/src/desktopVirtualizationAPIClient.ts
+++ sdk/desktopvirtualization/arm-desktopvirtualization/src/desktopVirtualizationAPIClient.ts
@@ -74,7 +74,7 @@ export class DesktopVirtualizationAPIClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-desktopvirtualization/1.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-desktopvirtualization/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
