6bcc9aedbbe7ddae5375f9138190ce400e91c33e
SDKAuto <sdkautomation@microsoft.com>
2022-01-05 07:37:16 +0000

    CodeGen from PR 16902 in Azure/azure-rest-api-specs Track2 modify readme.go.md 4 (#16902)
    * Track2 modify readme.go.md 4
    
    * devops

--- sdk/dns/arm-dns/src/index.ts
+++ sdk/dns/arm-dns/src/index.ts
@@ -6,6 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./recordSets";
-export * from "./zones";
-export * from "./dnsResourceReferenceOperations";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { DnsManagementClient } from "./dnsManagementClient";
+export * from "./operationsInterfaces";
