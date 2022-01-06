3110f8b2fbdf6920bd9f20985b9b4194733babf9
SDKAuto <sdkautomation@microsoft.com>
2022-01-06 00:38:03 +0000

    CodeGen from PR 17196 in Azure/azure-rest-api-specs add missing Configuration Tags to operationalinsights readme (#17196)

--- sdk/operationalinsights/loganalytics/src/index.ts
+++ sdk/operationalinsights/loganalytics/src/index.ts
@@ -6,5 +6,6 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./query";
-export * from "./metadata";
+export * from "./models";
+export { OperationalInsightsDataClient } from "./operationalInsightsDataClient";
+export * from "./operationsInterfaces";
