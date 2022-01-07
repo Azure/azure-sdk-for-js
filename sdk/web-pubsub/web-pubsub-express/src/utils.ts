7063001c9995e09cb63e1d02fee668a1642706dd
Liangying.Wei <lianwei@microsoft.com>
2022-01-07 17:29:28 +0800

    Update utils.ts

--- sdk/web-pubsub/web-pubsub-express/src/utils.ts
+++ sdk/web-pubsub/web-pubsub-express/src/utils.ts
@@ -68,7 +68,7 @@ export async function convertHttpToEvent(request: IncomingMessage): Promise<Clou
   return receivedEvent;
 }
 
-export function isJsonData(event: CloudEvent) {
+export function isJsonData(event: CloudEvent): boolean {
   return event.datacontenttype?.startsWith("application/json;");
 }
 
