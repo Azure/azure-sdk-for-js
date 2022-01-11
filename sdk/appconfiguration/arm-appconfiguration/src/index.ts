37ee4bda47a099440206b1e76040d8bb89ca96c3
colawwj <70128817+colawwj@users.noreply.github.com>
2022-01-11 16:35:48 +0800

    appconfiguration-track2-ga (#19772)

--- sdk/appconfiguration/arm-appconfiguration/src/index.ts
+++ sdk/appconfiguration/arm-appconfiguration/src/index.ts
@@ -6,8 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./configurationStores";
-export * from "./operations";
-export * from "./privateEndpointConnections";
-export * from "./privateLinkResources";
-export * from "./keyValues";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { AppConfigurationManagementClient } from "./appConfigurationManagementClient";
+export * from "./operationsInterfaces";
