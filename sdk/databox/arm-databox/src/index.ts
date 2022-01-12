b29570d5d733b66581da23c54d170ce40796f6e3
colawwj <v-sigangzhou@microsoft.com>
2022-01-12 14:46:08 +0800

    databox-track2-ga

--- sdk/databox/arm-databox/src/index.ts
+++ sdk/databox/arm-databox/src/index.ts
@@ -6,6 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./operations";
-export * from "./jobs";
-export * from "./service";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { DataBoxManagementClient } from "./dataBoxManagementClient";
+export * from "./operationsInterfaces";
