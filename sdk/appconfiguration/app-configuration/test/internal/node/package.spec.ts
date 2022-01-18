ab706c598c22801ab6654dd3eaa024a4de683302
Jeremy Meng <yumeng@microsoft.com>
2022-01-18 09:28:58 -0800

    Fix unit test

--- sdk/appconfiguration/app-configuration/test/internal/node/package.spec.ts
+++ sdk/appconfiguration/app-configuration/test/internal/node/package.spec.ts
@@ -24,7 +24,7 @@ describe("packagejson related tests", () => {
     } catch (e) {
       // For unit tests
       packageJsonContents = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../../package.json"), { encoding: "utf-8" })
+        fs.readFileSync(path.join(__dirname, "../../../package.json"), { encoding: "utf-8" })
       );
     }
 
