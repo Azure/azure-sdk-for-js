1478d9996a3020fe9a9be5621d8e9f2f069bb787
colawwj <v-sigangzhou@microsoft.com>
2022-01-20 14:22:07 +0800

    powerbidedicated-release

--- sdk/powerbidedicated/arm-powerbidedicated/src/index.ts
+++ sdk/powerbidedicated/arm-powerbidedicated/src/index.ts
@@ -6,6 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./capacities";
-export * from "./operations";
-export * from "./autoScaleVCores";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { PowerBIDedicated } from "./powerBIDedicated";
+export * from "./operationsInterfaces";
