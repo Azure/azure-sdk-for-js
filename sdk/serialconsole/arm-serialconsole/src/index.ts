04111b8935f953903c018ee66b81c45392e40318
colawwj <v-sigangzhou@microsoft.com>
2022-01-21 09:40:42 +0800

    serialconsole-release

--- sdk/serialconsole/arm-serialconsole/src/index.ts
+++ sdk/serialconsole/arm-serialconsole/src/index.ts
@@ -6,4 +6,6 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./serialPorts";
+export * from "./models";
+export { MicrosoftSerialConsoleClient } from "./microsoftSerialConsoleClient";
+export * from "./operationsInterfaces";
