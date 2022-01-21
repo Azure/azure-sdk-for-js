bfdbce2d543c2f847bc258800473281a6b7cb5a5
colawwj <v-sigangzhou@microsoft.com>
2022-01-21 10:03:35 +0800

    storagecache

--- sdk/storagecache/arm-storagecache/src/index.ts
+++ sdk/storagecache/arm-storagecache/src/index.ts
@@ -6,11 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export {
-  CloudError,
-  ResourceSku,
-  ResourceSkuCapabilities,
-  ResourceSkuLocationInfo,
-  ResourceSkusResult,
-  Restriction
-} from "../models/mappers";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { StorageCacheManagementClient } from "./storageCacheManagementClient";
+export * from "./operationsInterfaces";
