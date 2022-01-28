1eaffbe0da16db10aaeff467ac670e17c7c2b9d7
tmachala-ms <tomasmachala@microsoft.com>
2022-01-28 15:10:46 +0100

    Increase the version number

--- sdk/communication/communication-sip-routing/src/constants.ts
+++ sdk/communication/communication-sip-routing/src/constants.ts
@@ -1,4 +1,4 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const SDK_VERSION: string = "1.0.0-alpha.20210825.3";
+export const SDK_VERSION: string = "1.0.0-alpha.20220128.1";
