9a779978357ef416f30737b2918502c31f8efc45
Sarangan Rajamanickam <sarajama@microsoft.com>
2022-01-31 12:17:38 -0800

    Revert "[Search Documents] Migrating search-documents to the new recorder (#20125)"
    This reverts commit e7c20eadc2e3dbaa1e0d696d18316c57419ea296.

---
+++ sdk/search/search-documents/recordings/node/searchclient_tests/recording_autocomplete_returns_zero_results_for_invalid_query.js
@@ -0,0 +1,37 @@
+let nock = require('nock');
+
+module.exports.hash = "24d5222a36c4b91ba2f86374fb2fb30a";
+
+module.exports.testInfo = {"uniqueName":{},"newDate":{}}
+
+nock('https://endpoint:443', {"encodedQueryParams":true})
+  .post(`/indexes('hotel-live-test1')/docs/search.post.autocomplete`, {"search":"garbxyz","suggesterName":"sg"})
+  .query(true)
+  .reply(200, ["1f8b0800000000000400edbd07601c499625262f6dca7b7f4af54ad7e074a10880601324d8904010ecc188cde692ec1d69472329ab2a81ca6556655d661640cced9dbcf7de7befbdf7de7befbdf7ba3b9d4e27f7dfff3f5c6664016cf6ce4adac99e2180aac81f3f7e7c1f3f227ef1479759b9ce3f7af4bdefff92ff0742ea40440c000000"], [
+  'Cache-Control',
+  'no-cache',
+  'Pragma',
+  'no-cache',
+  'Content-Type',
+  'application/json; odata.metadata=none',
+  'Content-Encoding',
+  'gzip',
+  'Expires',
+  '-1',
+  'Vary',
+  'Accept-Encoding',
+  'request-id',
+  '89237a00-faa9-4bfe-b35f-be0c4773b63c',
+  'elapsed-time',
+  '18',
+  'OData-Version',
+  '4.0',
+  'Preference-Applied',
+  'odata.include-annotations="*"',
+  'Strict-Transport-Security',
+  'max-age=15724800; includeSubDomains',
+  'Date',
+  'Fri, 07 Jan 2022 23:38:56 GMT',
+  'Content-Length',
+  '133'
+]);
