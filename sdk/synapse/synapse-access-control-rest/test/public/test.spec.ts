ebc03cae5fc26a26ff076525696954d749a0e103
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2022-01-31 13:51:58 -0600

    Migrate to new recorder

--- sdk/synapse/synapse-access-control-rest/test/public/test.spec.ts
+++ sdk/synapse/synapse-access-control-rest/test/public/test.spec.ts
@@ -1,36 +1,28 @@
 import { AccessControlRestClient } from "../../src/accessControl";
-import { paginate } from "../../src/paginateHelper";
-import { isLiveMode, Recorder } from "@azure-tools/test-recorder";
-import { assert } from "chai";
-import { createClient, createRecorder, getWorkspaceName } from "./utils/recordedClient";
-import { v4 as uuid } from "uuid";
-import { isNode } from "@azure/core-util";
+import { Recorder } from "@azure-tools/test-recorder";
 import { RoleAssignmentDetails } from "../../src";
+import { assert } from "chai";
+import { createClient } from "./utils/recordedClient";
+import { isNode } from "@azure/core-util";
+import { paginate } from "../../src/paginateHelper";
 
 describe("Access Control smoke", () => {
   let recorder: Recorder;
   let client: AccessControlRestClient;
   // When re-recording tests generate 4 new guids and replace roleAssignmentId and principalId
   let roleAssignmentId = isNode
-    ? "50f3ab49-a50e-4e81-bdca-92672423ee03"
-    : "fce80a60-785c-4e22-b341-57e3a5171ac0";
+    ? "63d7a4ea-5ee9-46de-acf4-76945d2760c9"
+    : "e7b22cf8-773d-4d4e-bcce-c3796e722394";
   let principalId = isNode
-    ? "2c819cbc-ff7b-4ea8-9d69-541281ae2184"
-    : "b895aef7-f11e-47bc-9329-bf6a89c59d8b";
-  let scope = "workspaces/xysynapsetest";
+    ? "126a182d-42fe-444d-9855-dbec15594bfc"
+    : "cb3a6666-3467-4455-8de4-2159465409f2";
+  let scope = "workspaces/joheredisyn";
   const roleId = "2a385764-43e8-416c-9825-7b18d05a2c4b";
 
-  before(() => {
-    if (isLiveMode()) {
-      roleAssignmentId = uuid();
-      principalId = uuid();
-      scope = getWorkspaceName();
-    }
-  });
-
-  beforeEach(function() {
-    recorder = createRecorder(this);
-    client = createClient();
+  beforeEach(async function() {
+    console.log(JSON.stringify(this.currentTest?.title))
+    recorder = new Recorder(this.currentTest);
+    client = await createClient(recorder);
   });
 
   afterEach(async () => {
