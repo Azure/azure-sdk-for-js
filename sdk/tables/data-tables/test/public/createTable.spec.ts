e57606a39ce9e786e3a548394d4f4f0b997810e0
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2022-02-01 18:36:17 +0000

    First set of tests

--- sdk/tables/data-tables/test/public/createTable.spec.ts
+++ sdk/tables/data-tables/test/public/createTable.spec.ts
@@ -5,22 +5,21 @@ import { RestError, TableClient, TableServiceClient } from "../../src";
 import {
   createTableClient,
   createTableServiceClient,
-  recordedEnvironmentSetup,
 } from "./utils/recordedClient";
-import { env, isPlaybackMode } from "@azure-tools/test-recorder";
+
 import { TableServiceErrorResponse } from "../../src/utils/errorHelpers";
 import { assert } from "chai";
 import { createHttpHeaders } from "@azure/core-rest-pipeline";
 
 describe("TableClient CreationHandling", () => {
-  let client: TableClient;
-  beforeEach(function () {
-    client = createTableClient("testTable");
+  let unrecordedClient: TableClient;
+  beforeEach(async function () {
+    unrecordedClient = await createTableClient("testTable", "SASConnectionString");
   });
 
   it("should not thorw if table already exists", async function () {
     // Mock core-client throwing on error to verify consistenty that don't throw the error
-    client.pipeline.addPolicy({
+    unrecordedClient.pipeline.addPolicy({
       name: "TableAlreadyExists",
       sendRequest: async (req) => {
         const mockedResponse: TableServiceErrorResponse = {
@@ -41,7 +40,7 @@ describe("TableClient CreationHandling", () => {
       },
     });
 
-    await client.createTable({
+    await unrecordedClient.createTable({
       onResponse: (response) => {
         assert.equal(response.status, 409);
       },
@@ -50,7 +49,7 @@ describe("TableClient CreationHandling", () => {
 
   it("should throw when 409 and not TableAlreadyExists", async function () {
     // Mock core-client throwing on error to verify consistenty that we surface the error
-    client.pipeline.addPolicy({
+    unrecordedClient.pipeline.addPolicy({
       name: "Other409Error",
       sendRequest: async (req) => {
         const mockedResponse: TableServiceErrorResponse = {
@@ -65,7 +64,7 @@ describe("TableClient CreationHandling", () => {
     });
 
     try {
-      await client.createTable();
+      await unrecordedClient.createTable();
       assert.fail("Expected error");
     } catch (error) {
       assert.equal((error as RestError).statusCode, 409);
@@ -74,19 +73,16 @@ describe("TableClient CreationHandling", () => {
 });
 
 describe("TableServiceClient CreationHandling", () => {
-  let client: TableServiceClient;
-  beforeEach(function () {
-    if (isPlaybackMode()) {
-      env.SAS_CONNECTION_STRING =
-        recordedEnvironmentSetup.replaceableVariables["SAS_CONNECTION_STRING"];
-    }
-    client = createTableServiceClient();
+  let unrecordedClient: TableServiceClient;
+
+  beforeEach(async function () {
+    unrecordedClient = await createTableServiceClient("SASConnectionString");
   });
 
   it("should not thorw if table already exists", async function () {
     const tableName = `tableExists`;
     // Mock core-client throwing on error to verify consistenty that don't throw the error
-    client.pipeline.addPolicy({
+    unrecordedClient.pipeline.addPolicy({
       name: "TableAlreadyExists",
       sendRequest: async (req) => {
         const mockedResponse: TableServiceErrorResponse = {
@@ -107,7 +103,7 @@ describe("TableServiceClient CreationHandling", () => {
       },
     });
 
-    await client.createTable(tableName, {
+    await unrecordedClient.createTable(tableName, {
       onResponse: (response) => {
         assert.equal(response.status, 409);
       },
@@ -117,7 +113,7 @@ describe("TableServiceClient CreationHandling", () => {
   it("should throw when 409 and not TableAlreadyExists", async function () {
     const tableName = `throwError`;
     // Mock core-client throwing on error to verify consistenty that we surface the error
-    client.pipeline.addPolicy({
+    unrecordedClient.pipeline.addPolicy({
       name: "Other409Error",
       sendRequest: async (req) => {
         const mockedResponse: TableServiceErrorResponse = {
@@ -138,7 +134,7 @@ describe("TableServiceClient CreationHandling", () => {
       },
     });
     try {
-      await client.createTable(tableName);
+      await unrecordedClient.createTable(tableName);
       assert.fail("Expected error");
     } catch (error) {
       assert.equal((error as RestError).statusCode, 409);
