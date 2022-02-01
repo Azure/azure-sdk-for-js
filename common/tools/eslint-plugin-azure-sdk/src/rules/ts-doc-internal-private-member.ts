70a40cad5b96ed18c99921ca12df2afb0c16a525
hamsas <hamsas@cs.washington.edu>
2022-01-31 22:30:37 -0800

    fixed import sorting

--- common/tools/eslint-plugin-azure-sdk/src/rules/ts-doc-internal-private-member.ts
+++ common/tools/eslint-plugin-azure-sdk/src/rules/ts-doc-internal-private-member.ts
@@ -6,11 +6,11 @@
  * @author Hamsa Shankar
  */
 
-import { SyntaxKind } from "typescript";
 import { ParserServices, TSESTree } from "@typescript-eslint/experimental-utils";
 import { Node } from "estree";
 import { ParserWeakMapESTreeToTSNode } from "@typescript-eslint/typescript-estree/dist/parser-options";
 import { Rule } from "eslint";
+import { SyntaxKind } from "typescript";
 import { getRuleMetaData } from "../utils";
 
 //------------------------------------------------------------------------------
