0215d5d1b2f2bbfd9773da20590924c9e0034f74
Harsha Nalluru <sanallur@microsoft.com>
2022-02-07 02:25:32 +0000

    format

--- sdk/tables/perf-tests/data-tables/test/tables.spec.ts
+++ sdk/tables/perf-tests/data-tables/test/tables.spec.ts
@@ -7,9 +7,11 @@ export abstract class TablesTest<TOptions = Record<string, unknown>> extends Per
   constructor(tableName: string) {
     super();
     const connectionString = getEnvVar("SAS_CONNECTION_STRING");
-    this.client =
-      TableClient.fromConnectionString(connectionString, tableName, this.configureClientOptions({}))
-
+    this.client = TableClient.fromConnectionString(
+      connectionString,
+      tableName,
+      this.configureClientOptions({})
+    );
   }
 
   public async globalSetup() {
