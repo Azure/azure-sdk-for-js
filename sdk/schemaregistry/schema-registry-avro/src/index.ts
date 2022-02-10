a6a534cb1eef257dad86e604942baa3337e00b82
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2022-02-10 17:05:29 -0500

    [Schema Registry Avro] Prepare release (#20315)
    * [Schema Registry Avro] Prepare release
    
    * rename to AvroEncoder
    
    * update changelog entry
    
    * more renames

--- sdk/schemaregistry/schema-registry-avro/src/index.ts
+++ sdk/schemaregistry/schema-registry-avro/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export { SchemaRegistryAvroEncoder } from "./schemaRegistryAvroEncoder";
+export { AvroEncoder } from "./schemaRegistryAvroEncoder";
 
 export * from "./models";
