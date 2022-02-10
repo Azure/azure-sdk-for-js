37606568a8d0c33a9f9190565273a9d5a083649b
Jeremy Meng <yumeng@microsoft.com>
2022-02-10 10:17:51 -0800

    [core-http,core-rest-pipeline] allow logging `WWW-Authenticate` header (#20288)
    This PR adds "WWW-Authenticate" to the allowed logged header list.  It doesn't
    contain sensitive or privacy information.

--- sdk/core/core-http/src/util/sanitizer.ts
+++ sdk/core/core-http/src/util/sanitizer.ts
@@ -63,6 +63,7 @@ const defaultAllowedHeaderNames = [
   "Server",
   "Transfer-Encoding",
   "User-Agent",
+  "WWW-Authenticate",
 ];
 
 const defaultAllowedQueryParameters: string[] = ["api-version"];
