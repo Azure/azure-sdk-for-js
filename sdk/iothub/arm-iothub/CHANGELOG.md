9249294cbf1f1d5b2eb7841a5e21734e3ba72e97
azure-sdk <azuresdk@microsoft.com>
2022-02-15 06:40:42 +0000

    Post release automated changes for azure-arm-iothub

--- sdk/iothub/arm-iothub/CHANGELOG.md
+++ sdk/iothub/arm-iothub/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
-    
+
+## 6.1.1 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 6.1.0 (2022-02-15)
     
 **Features**
--- sdk/iothub/arm-iothub/src/iotHubClient.ts
+++ sdk/iothub/arm-iothub/src/iotHubClient.ts
@@ -60,7 +60,7 @@ export class IotHubClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-iothub/6.1.0`;
+    const packageDetails = `azsdk-js-arm-iothub/6.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
