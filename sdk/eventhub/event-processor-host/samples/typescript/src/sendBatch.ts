8119af4a40352d760f609b9ffb340a05e1bf6ec9
praveenkuttappan <prmarott@microsoft.com>
2022-02-16 16:05:57 -0500

    Delete event-processor-host samples

--- sdk/eventhub/event-processor-host/samples/typescript/src/sendBatch.ts
+++
@@ -1,8 +0,0 @@
-/*
-  Copyright (c) Microsoft Corporation. All rights reserved.
-  Licensed under the MIT Licence.
-
-  The sample to populate your Event Hubs instance with events before you try
-  any of the other samples that show how to receive the events from Event Hubs is moved to
-  https://github.com/Azure/azure-sdk-for-js/blob/main/sdk/eventhub/event-hubs/samples/typescript/src/sendEvents.ts
-*/
