7d18dd7ab02fc70f72a65b356c62c0cdc9faf7bb
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2022-02-16 21:07:30 +0000

    fixed RegExp typo

--- sdk/identity/identity/src/msal/utils.ts
+++ sdk/identity/identity/src/msal/utils.ts
@@ -56,7 +56,7 @@ export function getAuthority(tenantId: string, host?: string): string {
   if (!host) {
     host = DefaultAuthorityHost;
   }
-  if (new ReExp(`${tenantId}/?$`).test(host)) {
+  if (new RegExp(`${tenantId}/?$`).test(host)) {
     return host;
   }
   if (host.endsWith("/")) {
