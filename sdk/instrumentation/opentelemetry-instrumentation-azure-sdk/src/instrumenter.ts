ff38eda6f433fb630b5ebea1c187283b97143b88
Maor Leger <malege@microsoft.com>
2022-02-18 15:53:13 -0800

    Remove console.log

--- sdk/instrumentation/opentelemetry-instrumentation-azure-sdk/src/instrumenter.ts
+++ sdk/instrumentation/opentelemetry-instrumentation-azure-sdk/src/instrumenter.ts
@@ -26,7 +26,6 @@ export class OpenTelemetryInstrumenter implements Instrumenter {
       .startSpan(name, toSpanOptions(spanOptions));
 
     let ctx = spanOptions?.tracingContext || context.active();
-    console.log(ctx);
 
     // COMPAT: remove when core-rest-pipeline has upgraded to core-tracing 1.0
     const newNamespaceKey = Symbol.for("@azure/core-tracing namespace");
