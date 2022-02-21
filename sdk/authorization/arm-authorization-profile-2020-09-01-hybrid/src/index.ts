886c05df8e4f7eefe6b0742486f187faf7161c0e
colawwj <v-sigangzhou@microsoft.com>
2022-02-21 15:56:11 +0800

    hybrid-for-authorization

--- sdk/authorization/arm-authorization-profile-2020-09-01-hybrid/src/index.ts
+++ sdk/authorization/arm-authorization-profile-2020-09-01-hybrid/src/index.ts
@@ -6,8 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./permissions";
-export * from "./roleDefinitions";
-export * from "./providerOperationsMetadataOperations";
-export * from "./globalAdministrator";
-export * from "./roleAssignments";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { AuthorizationManagementClient } from "./authorizationManagementClient";
+export * from "./operationsInterfaces";
