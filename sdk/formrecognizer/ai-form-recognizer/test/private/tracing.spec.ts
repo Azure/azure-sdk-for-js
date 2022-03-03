64c942d5b1f6663b724796cb26e2fa9204e6850a
Will Temple <witemple@microsoft.com>
2022-03-03 15:39:55 -0500

    Fixed copyright header.

--- sdk/formrecognizer/ai-form-recognizer/test/private/tracing.spec.ts
+++ sdk/formrecognizer/ai-form-recognizer/test/private/tracing.spec.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { AzureKeyCredential } from "@azure/core-auth";
 import { DocumentAnalysisClient } from "../../src/documentAnalysisClient";
 import { DocumentModelAdministrationClient } from "../../src/documentModelAdministrationClient";
