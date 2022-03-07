746530479e571806b2730439bdc1963f807dd4e3
Daniel Ortega <danielortega@microsoft.com>
2022-03-07 13:05:03 -0800

    Remove `phoneNumbersLroPolicy`
    Require version 2.2.4 of @azure/core-lro, which implements a fix to the LRO implementation. This renders the workaround implemented in the phone-numbers package as unnecessary.

--- sdk/communication/communication-phone-numbers/test/internal/customPipelinePolicies.spec.ts
+++ sdk/communication/communication-phone-numbers/test/internal/customPipelinePolicies.spec.ts
@@ -3,16 +3,12 @@
 
 import { FullOperationResponse } from "@azure/core-client";
 import {
-  HttpHeaders,
   PipelineRequest,
   PipelineResponse,
   createHttpHeaders,
 } from "@azure/core-rest-pipeline";
 import { assert } from "chai";
-import {
-  createPhoneNumbersPagingPolicy,
-  phoneNumbersLroPolicy,
-} from "../../src/utils/customPipelinePolicies";
+import { createPhoneNumbersPagingPolicy } from "../../src/utils/customPipelinePolicies";
 
 describe("phoneNumbersPagingPolicy", function () {
   const endpoint = "https://contoso.spool.azure.local";
@@ -77,41 +73,5 @@ describe("phoneNumbersPagingPolicy", function () {
       response.parsedBody.nextLink,
       "https://contoso.spool.azure.local/phoneNumbers?top=100&skip=100"
     );
-  });
-});
-
-describe("phoneNumbersLroPolicy", function () {
-  const request: PipelineRequest = {
-    url: "https://contoso.spool.azure.local/availablePhoneNumbers/countries/{countryCode}/:search",
-    method: "POST",
-    headers: createHttpHeaders(),
-    timeout: 0,
-    withCredentials: false,
-    requestId: "any-id",
-  };
-
-  async function createMockResponse(headers: HttpHeaders) {
-    return Promise.resolve({ headers }) as unknown as PipelineResponse;
-  }
-
-  it("sets azure-asyncoperation header", async function () {
-    const headers = createHttpHeaders();
-    headers.set("operation-location", "/phoneNumbers/operations/search_12345");
-    const response = await phoneNumbersLroPolicy.sendRequest(request, (_request) =>
-      createMockResponse(headers)
-    );
-    assert.exists(response.headers.get("azure-asyncoperation"));
-    assert.equal(
-      response.headers.get("azure-asyncoperation"),
-      "/phoneNumbers/operations/search_12345"
-    );
-  });
-
-  it("does not mutate headers without operation-location", async function () {
-    const response = await phoneNumbersLroPolicy.sendRequest(request, (_request) =>
-      createMockResponse(createHttpHeaders())
-    );
-    assert.isUndefined(response.headers.get("azure-asyncoperation"));
-    assert.deepEqual(response.headers, createHttpHeaders());
   });
 });
--- sdk/communication/communication-phone-numbers/test/internal/phoneNumbersClientPolicies.spec.ts
+++ sdk/communication/communication-phone-numbers/test/internal/phoneNumbersClientPolicies.spec.ts
@@ -3,11 +3,9 @@
 
 import { AzureKeyCredential } from "@azure/core-auth";
 import { assert } from "chai";
-import { SearchAvailablePhoneNumbersRequest } from "../../src/models";
 import { PhoneNumbersClient } from "../../src/phoneNumbersClient";
 import {
   mockListPhoneNumbersHttpClient,
-  mockSearchHttpClient,
 } from "../public/utils/mockHttpClients";
 
 describe("PhoneNumbersClient - custom policies ", function () {
@@ -15,34 +13,6 @@ describe("PhoneNumbersClient - custom policies ", function () {
   const accessKey = "banana";
   let client: PhoneNumbersClient;
 
-  it("applies the phoneNumbersLroPolicy", async function () {
-    client = new PhoneNumbersClient(endpoint, new AzureKeyCredential(accessKey), {
-      httpClient: mockSearchHttpClient,
-    });
-
-    const searchRequest: SearchAvailablePhoneNumbersRequest = {
-      countryCode: "US",
-      phoneNumberType: "tollFree",
-      assignmentType: "application",
-      capabilities: {
-        sms: "none",
-        calling: "outbound",
-      },
-    };
-
-    const poller = await client.beginSearchAvailablePhoneNumbers(searchRequest, {
-      onResponse: (_response) => {
-        assert.isDefined(_response.headers.get("azure-asyncoperation"));
-        assert.equal(
-          _response.headers.get("azure-asyncopearation"),
-          _response.headers.get("opearation-location")
-        );
-      },
-    });
-
-    // It is only required to poll once to check if the initial request contains the header
-    await poller.poll();
-  });
 
   it("applies the phoneNumbersPagingPolicy", async function () {
     client = new PhoneNumbersClient(endpoint, new AzureKeyCredential(accessKey), {
