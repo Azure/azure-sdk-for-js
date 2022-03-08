65e9dada933f08da1ae95949d4a84f8ad1860f21
colawwj <v-sigangzhou@microsoft.com>
2022-03-08 16:35:36 +0800

    [AzureStack][Mgmt]hybrid-profile-2020-09-01-for-policy

--- sdk/policy/arm-policy-profile-2020-09-01-hybrid/src/index.ts
+++ sdk/policy/arm-policy-profile-2020-09-01-hybrid/src/index.ts
@@ -6,5 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./policyDefinitions";
-export * from "./policyAssignments";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { PolicyClient } from "./policyClient";
+export * from "./operationsInterfaces";
