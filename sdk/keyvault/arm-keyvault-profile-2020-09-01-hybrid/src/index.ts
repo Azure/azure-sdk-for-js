40e157066b73ed04b97d1f8f7c506b5271580c49
colawwj <70128817+colawwj@users.noreply.github.com>
2022-03-09 16:39:11 +0800

    [AzureStack][Mgmt]hybrid-profile-2020-09-01-for-keyvaul (#20744)

--- sdk/keyvault/arm-keyvault-profile-2020-09-01-hybrid/src/index.ts
+++ sdk/keyvault/arm-keyvault-profile-2020-09-01-hybrid/src/index.ts
@@ -6,8 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./vaults";
-export * from "./privateEndpointConnections";
-export * from "./privateLinkResources";
-export * from "./operations";
-export * from "./secrets";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { KeyVaultManagementClient } from "./keyVaultManagementClient";
+export * from "./operationsInterfaces";
