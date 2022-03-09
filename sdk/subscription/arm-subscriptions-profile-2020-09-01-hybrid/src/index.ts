1711b851eb05f7b44af5604fae4faaf98a893330
colawwj <70128817+colawwj@users.noreply.github.com>
2022-03-09 11:28:08 +0800

    [AzureStack][Mgmt]hybrid-profile-2020-09-01-for-subscriptions (#20740)

--- sdk/subscription/arm-subscriptions-profile-2020-09-01-hybrid/src/index.ts
+++ sdk/subscription/arm-subscriptions-profile-2020-09-01-hybrid/src/index.ts
@@ -6,6 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./operations";
-export * from "./subscriptions";
-export * from "./tenants";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { SubscriptionClient } from "./subscriptionClient";
+export * from "./operationsInterfaces";
