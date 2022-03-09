95d605e5ea6415476b4d6cab8a0923c9f053bd70
azure-sdk <azuresdk@microsoft.com>
2022-03-09 11:13:16 +0000

    Post release automated changes for azure-arm-network-profile-2020-09-01-hybrid

--- sdk/network/arm-network-profile-2020-09-01-hybrid/src/networkManagementClient.ts
+++ sdk/network/arm-network-profile-2020-09-01-hybrid/src/networkManagementClient.ts
@@ -97,7 +97,7 @@ export class NetworkManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-network-profile-2020-09-01-hybrid/2.0.0`;
+    const packageDetails = `azsdk-js-arm-network-profile-2020-09-01-hybrid/2.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
