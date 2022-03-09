eceaf57c0f12d8cd1a08628cb7ea857803703994
azure-sdk <azuresdk@microsoft.com>
2022-03-09 09:18:28 +0000

    Post release automated changes for azure-arm-azurestackhci

--- sdk/azurestackhci/arm-azurestackhci/src/azureStackHCIClient.ts
+++ sdk/azurestackhci/arm-azurestackhci/src/azureStackHCIClient.ts
@@ -54,7 +54,7 @@ export class AzureStackHCIClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-azurestackhci/2.1.0`;
+    const packageDetails = `azsdk-js-arm-azurestackhci/2.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
