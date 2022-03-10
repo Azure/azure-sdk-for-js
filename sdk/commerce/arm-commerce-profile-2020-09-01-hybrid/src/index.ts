b9c22aa4fd32bc812ca76bc5f332fcaf915c545a
colawwj <70128817+colawwj@users.noreply.github.com>
2022-03-10 11:20:31 +0800

    [AzureStack][Mgmt]hybrid-profile-2020-09-01-for-commerce (#20754)
    * [AzureStack][Mgmt]hybrid-profile-2020-09-01-for-commerce
    
    * update link

--- sdk/commerce/arm-commerce-profile-2020-09-01-hybrid/src/index.ts
+++ sdk/commerce/arm-commerce-profile-2020-09-01-hybrid/src/index.ts
@@ -6,5 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./usageAggregates";
-export * from "./rateCard";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { UsageManagementClient } from "./usageManagementClient";
+export * from "./operationsInterfaces";
