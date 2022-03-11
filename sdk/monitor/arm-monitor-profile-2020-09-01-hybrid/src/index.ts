8dc10018e47bcc1a0cd67614d2718ea3b69660ec
colawwj <v-sigangzhou@microsoft.com>
2022-03-11 10:32:11 +0800

    [AzureStack][Mgmt]hybrid-profile-2020-09-01-for-monitor

--- sdk/monitor/arm-monitor-profile-2020-09-01-hybrid/src/index.ts
+++ sdk/monitor/arm-monitor-profile-2020-09-01-hybrid/src/index.ts
@@ -6,9 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./metricDefinitions";
-export * from "./metrics";
-export * from "./diagnosticSettings";
-export * from "./diagnosticSettingsCategory";
-export * from "./eventCategories";
-export * from "./operations";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { MonitorClient } from "./monitorClient";
+export * from "./operationsInterfaces";
