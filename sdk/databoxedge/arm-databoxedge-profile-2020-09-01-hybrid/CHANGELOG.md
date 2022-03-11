52a7de39cd0d5e115f04d894438c8cb62859aa3d
azure-sdk <azuresdk@microsoft.com>
2022-03-11 06:01:24 +0000

    Post release automated changes for azure-arm-databoxedge-profile-2020-09-01-hybrid

--- sdk/databoxedge/arm-databoxedge-profile-2020-09-01-hybrid/CHANGELOG.md
+++ sdk/databoxedge/arm-databoxedge-profile-2020-09-01-hybrid/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
-    
+
+## 2.0.1 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 2.0.0 (2022-03-11)
 
 The package of @azure/arm-databoxedge-profile-2020-09-01-hybrid is using our next generation design principles since version 2.0.0, which contains breaking changes.
--- sdk/databoxedge/arm-databoxedge-profile-2020-09-01-hybrid/src/dataBoxEdgeManagementClient.ts
+++ sdk/databoxedge/arm-databoxedge-profile-2020-09-01-hybrid/src/dataBoxEdgeManagementClient.ts
@@ -78,7 +78,7 @@ export class DataBoxEdgeManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-databoxedge-profile-2020-09-01-hybrid/2.0.0`;
+    const packageDetails = `azsdk-js-arm-databoxedge-profile-2020-09-01-hybrid/2.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
