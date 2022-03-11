8c859a765fa47810374f3cfae979babdb53b8cf5
Harsha Nalluru <sanallur@microsoft.com>
2022-03-11 02:42:23 +0000

    update jsdoc

--- sdk/core/core-rest-pipeline/src/util/helpers.ts
+++ sdk/core/core-rest-pipeline/src/util/helpers.ts
@@ -14,12 +14,12 @@ const StandardAbortMessage = "The operation was aborted.";
 
 /**
  * A wrapper for setTimeout that resolves a promise after delayInMs milliseconds.
- * @param delayInMs - The number of milliseconds to be delayed.
- * @param value - The value to be resolved with after a timeout of t milliseconds.
- * @param options - The options for delay - currently abort options
- *   @param abortSignal - The abortSignal associated with containing operation.
- *   @param abortErrorMsg - The abort error message associated with containing operation.
- * @returns - Resolved promise
+ * @param delayInMs The number of milliseconds to be delayed.
+ * @param value The value to be resolved with after a timeout of t milliseconds.
+ * @param options The options for delay - currently abort options
+ *                  - abortSignal - The abortSignal associated with containing operation.
+ *                  - abortErrorMsg - The abort error message associated with containing operation.
+ * @returns Resolved promise
  */
 export function delay<T>(
   delayInMs: number,
