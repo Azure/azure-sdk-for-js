366d41af2e153db080a5a832c5020c30fcbe3d2c
SDKAuto <sdkautomation@microsoft.com>
2022-03-15 08:22:03 +0000

    CodeGen from PR 18208 in Azure/azure-rest-api-specs update readme (#18208)
    * update readme
    
    * Update readme.python.md
    
    * Update readme.python.md
    
    * Update readme.python.md

--- sdk/machinelearningcompute/arm-machinelearningcompute/src/machineLearningComputeManagementClient.ts
+++ sdk/machinelearningcompute/arm-machinelearningcompute/src/machineLearningComputeManagementClient.ts
@@ -50,7 +50,7 @@ export class MachineLearningComputeManagementClient extends coreClient.ServiceCl
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-machinelearningcompute/3.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-machinelearningcompute/3.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
