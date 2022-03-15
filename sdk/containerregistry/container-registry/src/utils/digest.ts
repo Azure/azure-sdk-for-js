a795f6bff3e72fd7676ebc47354eda9ab44697ca
Timo van Veenendaal <timov@microsoft.com>
2022-03-15 14:55:53 -0700

    Fix stream digest calculation

--- sdk/containerregistry/container-registry/src/utils/digest.ts
+++ sdk/containerregistry/container-registry/src/utils/digest.ts
@@ -1,20 +1,20 @@
 import crypto from "crypto";
 
-
 export function calculateDigest(buffer: Buffer): Promise<string>;
 
 export function calculateDigest(stream: NodeJS.ReadableStream): Promise<string>;
 
-export function calculateDigest(bufferOrStream: Buffer | NodeJS.ReadableStream): Promise<string>;
-
 export function calculateDigest(bufferOrStream: NodeJS.ReadableStream | Buffer): Promise<string> {
   const hash = crypto.createHash("sha256");
-  if(Buffer.isBuffer(bufferOrStream)) {
-    return Promise.resolve(`sha256:${hash.update(bufferOrStream).digest('hex')}`);
+  if (Buffer.isBuffer(bufferOrStream)) {
+    return Promise.resolve(`sha256:${hash.update(bufferOrStream).digest("hex")}`);
   } else {
     bufferOrStream.pipe(hash);
-    return new Promise(resolve => hash.on('end', () => {
-      resolve(`sha256:${hash.digest('hex')}`);
-    }));
+    return new Promise((resolve) =>
+      bufferOrStream.on("end", () => {
+        hash.end();
+        resolve(`sha256:${hash.digest("hex")}`);
+      })
+    );
   }
 }
