3065dcfc18f9089576a969a6e0d3ec90889aea20
SDKAuto <sdkautomation@microsoft.com>
2022-03-15 08:36:08 +0000

    CodeGen from PR 18223 in Azure/azure-rest-api-specs update readme (#18223)
    * update readme
    
    * Update readme.python.md

--- sdk/avs/arm-avs/src/azureVMwareSolutionAPI.ts
+++ sdk/avs/arm-avs/src/azureVMwareSolutionAPI.ts
@@ -78,7 +78,7 @@ export class AzureVMwareSolutionAPI extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-avs/3.0.0`;
+    const packageDetails = `azsdk-js-arm-avs/0.1.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
