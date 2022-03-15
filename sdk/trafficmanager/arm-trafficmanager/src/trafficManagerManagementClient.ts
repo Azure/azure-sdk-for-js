af0ceffd468b1382434b8aebe08bcfe107ef0aef
SDKAuto <sdkautomation@microsoft.com>
2022-03-15 08:13:40 +0000

    CodeGen from PR 18191 in Azure/azure-rest-api-specs update readme 2_5 (#18191)
    * update readme 2_5
    
    * Update readme.python.md

--- sdk/trafficmanager/arm-trafficmanager/src/trafficManagerManagementClient.ts
+++ sdk/trafficmanager/arm-trafficmanager/src/trafficManagerManagementClient.ts
@@ -57,7 +57,7 @@ export class TrafficManagerManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-trafficmanager/6.0.0`;
+    const packageDetails = `azsdk-js-arm-trafficmanager/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
