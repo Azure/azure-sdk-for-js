9a0a7577b587b94a5d0b0c7504b4c7060f9063c0
--global <alankashiwa@gmail.com>
2022-03-16 17:44:12 +0800

    maps-render: first draft

---
+++ sdk/maps/maps-render/src/models/results.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export interface MapTile {
+  /** The content-type for the response. */
+  contentType?: string;
+  /**
+   * BROWSER ONLY
+   *
+   * The response body as a browser Blob.
+   * Always `undefined` in node.js.
+   */
+  blobBody?: Promise<Blob>;
+  /**
+   * NODEJS ONLY
+   *
+   * The response body as a node.js Readable stream.
+   * Always `undefined` in the browser.
+   */
+  readableStreamBody?: NodeJS.ReadableStream;
+}
