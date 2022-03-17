e95393462360a069e03a592e70359e1dbad00ece
Renan Lordello de Aguiar <renan.lord.de.aguiar@gmail.com>
2022-03-17 19:14:39 -0300

    [phone number sdk] Published AZURE_USERAGENT_OVERRIDE variable into the browser tests

--- sdk/communication/communication-phone-numbers/test/public/utils/recordedClient.ts
+++ sdk/communication/communication-phone-numbers/test/public/utils/recordedClient.ts
@@ -40,6 +40,7 @@ const replaceableVariables: { [k: string]: string } = {
   AZURE_TENANT_ID: "SomeTenantId",
   AZURE_PHONE_NUMBER: "+14155550100",
   COMMUNICATION_SKIP_INT_PHONENUMBERS_TESTS: "false",
+  AZURE_USERAGENT_OVERRIDE: "fake-useragent",
 };
 
 export const environmentSetup: RecorderEnvironmentSetup = {
