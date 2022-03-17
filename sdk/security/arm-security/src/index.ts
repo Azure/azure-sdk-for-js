321fd690122b83db4d508dcb2157379e96cb0a91
zsgwwj <v-sigangzhou@microsoft.com>
2022-03-17 15:02:10 +0800

    regenerate

--- sdk/security/arm-security/src/index.ts
+++ sdk/security/arm-security/src/index.ts
@@ -6,6 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
+/// <reference lib="esnext.asynciterable" />
 export * from "./models";
 export { SecurityCenter } from "./securityCenter";
 export * from "./operationsInterfaces";
