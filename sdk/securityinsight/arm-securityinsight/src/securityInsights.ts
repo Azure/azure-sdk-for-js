c9b45c1b16e3056d2ee139b5946d01c62f46cf11
azure-sdk <azuresdk@microsoft.com>
2022-03-18 09:54:24 +0000

    Post release automated changes for azure-arm-securityinsight

--- sdk/securityinsight/arm-securityinsight/src/securityInsights.ts
+++ sdk/securityinsight/arm-securityinsight/src/securityInsights.ts
@@ -110,7 +110,7 @@ export class SecurityInsights extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-securityinsight/1.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-securityinsight/1.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
