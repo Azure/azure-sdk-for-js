20d6ac3c3268a9041aefd5807b17549f630bde3f
azure-sdk <azuresdk@microsoft.com>
2022-03-23 08:33:11 +0000

    Post release automated changes for azure-arm-cdn

--- sdk/cdn/arm-cdn/src/cdnManagementClient.ts
+++ sdk/cdn/arm-cdn/src/cdnManagementClient.ts
@@ -105,7 +105,7 @@ export class CdnManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-cdn/7.0.0`;
+    const packageDetails = `azsdk-js-arm-cdn/7.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
