6b0702ef903b329c742d70c6d04389fe0af3addb
zsgwwj <v-sigangzhou@microsoft.com>
2022-03-24 19:52:11 +0800

    update

--- sdk/advisor/arm-advisor/src/advisorManagementClient.ts
+++ sdk/advisor/arm-advisor/src/advisorManagementClient.ts
@@ -56,7 +56,7 @@ export class AdvisorManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-advisor/3.0.2`;
+    const packageDetails = `azsdk-js-arm-advisor/3.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
