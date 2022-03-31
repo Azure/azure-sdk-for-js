4fc3a00f786ea1be09608ec6405b1559efdf638f
Harsha Nalluru <sanallur@microsoft.com>
2022-03-31 19:27:19 +0000

    1.4.0-beta.1

--- sdk/appconfiguration/app-configuration/src/appConfigurationClient.ts
+++ sdk/appconfiguration/app-configuration/src/appConfigurationClient.ts
@@ -60,7 +60,7 @@ import { PipelinePolicy, bearerTokenAuthenticationPolicy } from "@azure/core-res
 /**
  * @internal
  */
-export const packageVersion = "1.4.0";
+export const packageVersion = "1.4.0-beta.1";
 const apiVersion = "1.0";
 const ConnectionStringRegex = /Endpoint=(.*);Id=(.*);Secret=(.*)/;
 const deserializationContentTypes = {
