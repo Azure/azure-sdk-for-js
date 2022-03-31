4d89620d72bd4dba59cf865f842a9e346c94aa36
zsgwwj <v-sigangzhou@microsoft.com>
2022-03-31 14:30:36 +0800

    update

--- sdk/policyinsights/arm-policyinsights/samples-dev/timeRangeSortSelectAndLimit.ts
+++ sdk/policyinsights/arm-policyinsights/samples-dev/timeRangeSortSelectAndLimit.ts
@@ -20,7 +20,7 @@ import { DefaultAzureCredential } from "@azure/identity";
 async function timeRangeSortSelectAndLimit() {
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const policyStatesResource = "latest";
-  const subscriptionId = "fffedd8f-ffff-fffd-fffd-fffed2f84852";
+  const subscriptionId2 = "fffedd8f-ffff-fffd-fffd-fffed2f84852";
   const top = 2;
   const orderBy =
     "Timestamp desc, PolicyAssignmentId asc, SubscriptionId asc, ResourceGroup asc, ResourceId";
@@ -40,8 +40,8 @@ async function timeRangeSortSelectAndLimit() {
   const resArray = new Array();
   for await (let item of client.policyStates.listQueryResultsForSubscription(
     policyStatesResource,
-    subscriptionId,
-    options
+    subscriptionId2,
+    { queryOptions: options }
   )) {
     resArray.push(item);
   }
