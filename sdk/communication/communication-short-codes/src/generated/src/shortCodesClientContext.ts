75052418c393070f283d4a46c69d52105fefa20b
azure-sdk <azuresdk@microsoft.com>
2022-04-01 01:23:46 +0000

    Post release automated changes for azure-tools-communication-short-codes

--- sdk/communication/communication-short-codes/src/generated/src/shortCodesClientContext.ts
+++ sdk/communication/communication-short-codes/src/generated/src/shortCodesClientContext.ts
@@ -31,7 +31,7 @@ export class ShortCodesClientContext extends coreClient.ServiceClient {
       requestContentType: "application/json; charset=utf-8"
     };
 
-    const packageDetails = `azsdk-js-communication-short-codes/1.0.0-beta.2`;
+    const packageDetails = `azsdk-js-communication-short-codes/1.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
