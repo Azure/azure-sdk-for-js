59afd9e7db6fc3f1250099f2495f661af1732bfd
Timo van Veenendaal <timov@microsoft.com>
2022-04-01 16:44:20 -0700

    Add helper to throw when required property is not present

--- sdk/containerregistry/container-registry/src/blob/containerRegistryBlobClient.ts
+++ sdk/containerregistry/container-registry/src/blob/containerRegistryBlobClient.ts
@@ -4,6 +4,7 @@
 import {
   InternalPipelineOptions,
   bearerTokenAuthenticationPolicy,
+  RestError,
 } from "@azure/core-rest-pipeline";
 import { TokenCredential } from "@azure/core-auth";
 import { GeneratedClient } from "../generated";
@@ -38,6 +39,14 @@ enum KnownManifestMediaType {
 
 function isReadableStream(body: any): body is NodeJS.ReadableStream {
   return body && typeof body.pipe === "function";
+}
+
+function withRequiredProperty<T, U extends keyof T>(property: U, obj: T): T & Required<Pick<T, U>> {
+  if (!Object.prototype.hasOwnProperty.call(obj, property)) {
+    throw new RestError(`Expected property ${property} to be defined.`);
+  }
+
+  return obj as T & Required<Pick<T, U>>;
 }
 
 /**
@@ -198,14 +207,17 @@ export class ContainerRegistryBlobClient {
 
       const tagOrDigest = options?.tag ?? (await calculateDigest(manifestBody));
 
-      const { dockerContentDigest } = await this.client.containerRegistry.createManifest(
-        this.repositoryName,
-        tagOrDigest,
-        manifestBody,
-        { contentType: KnownManifestMediaType.OciManifestMediaType, ...updatedOptions }
+      const { dockerContentDigest } = withRequiredProperty(
+        "dockerContentDigest",
+        await this.client.containerRegistry.createManifest(
+          this.repositoryName,
+          tagOrDigest,
+          manifestBody,
+          { contentType: KnownManifestMediaType.OciManifestMediaType, ...updatedOptions }
+        )
       );
 
-      return { digest: dockerContentDigest! };
+      return { digest: dockerContentDigest };
     } catch (e) {
       span.setStatus({ code: SpanStatusCode.ERROR, message: e.message });
       throw e;
@@ -316,8 +328,9 @@ export class ContainerRegistryBlobClient {
     const { span } = createSpan("ContainerRegistryBlobClient-uploadBlob", undefined);
 
     try {
-      const startUploadResult = await this.client.containerRegistryBlob.startUpload(
-        this.repositoryName
+      const startUploadResult = withRequiredProperty(
+        "location",
+        await this.client.containerRegistryBlob.startUpload(this.repositoryName)
       );
 
       let requestBody: NodeJS.ReadableStream | Buffer;
@@ -331,15 +344,18 @@ export class ContainerRegistryBlobClient {
         digest = await calculateDigest(requestBody);
       }
 
-      const uploadChunkResult = await this.client.containerRegistryBlob.uploadChunk(
-        startUploadResult.location!.substring(1),
-        requestBody
+      const uploadChunkResult = withRequiredProperty(
+        "location",
+        await this.client.containerRegistryBlob.uploadChunk(
+          startUploadResult.location.substring(1),
+          requestBody
+        )
       );
 
       const { dockerContentDigest: digestFromResponse } =
         await this.client.containerRegistryBlob.completeUpload(
           digest,
-          uploadChunkResult.location!.substring(1)
+          uploadChunkResult.location.substring(1)
         );
 
       if (digest !== digestFromResponse) {
