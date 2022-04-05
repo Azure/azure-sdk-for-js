bdb24daeb1539cad320fbdb3e7aba09060f6ce9a
SDKAuto <sdkautomation@microsoft.com>
2022-04-05 08:28:31 +0000

    CodeGen from PR 17972 in Azure/azure-rest-api-specs add xms-ids for PowerBIEmbedded (#17972)

--- sdk/powerbiembedded/arm-powerbiembedded/src/powerBIEmbeddedManagementClient.ts
+++ sdk/powerbiembedded/arm-powerbiembedded/src/powerBIEmbeddedManagementClient.ts
@@ -51,7 +51,7 @@ export class PowerBIEmbeddedManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-powerbiembedded/2.0.0`;
+    const packageDetails = `azsdk-js-arm-powerbiembedded/2.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
