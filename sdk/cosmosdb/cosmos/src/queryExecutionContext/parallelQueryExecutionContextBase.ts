9ff8ad5db7339f290f79687d7c77875a5f11224a
Jason <71847735+jay-most@users.noreply.github.com>
2022-04-06 08:45:58 -0700

    ParallelQueryExecutionContextBase breaks use of abortSignal (#18544)
    * ParallelQueryExecutionContextBase breaks use of abortSignal in FeedOptions
    
    * update object syntax
    
    Co-authored-by: Jason Amos <digitalamerica@gmail.com>

--- sdk/cosmosdb/cosmos/src/queryExecutionContext/parallelQueryExecutionContextBase.ts
+++ sdk/cosmosdb/cosmos/src/queryExecutionContext/parallelQueryExecutionContextBase.ts
@@ -471,7 +471,7 @@ export abstract class ParallelQueryExecutionContextBase implements ExecutionCont
       sqlQuerySpec["query"] = rewrittenQuery;
     }
 
-    const options = JSON.parse(JSON.stringify(this.options));
+    const options = { ...this.options };
     options.continuationToken = continuationToken;
 
     return new DocumentProducer(
