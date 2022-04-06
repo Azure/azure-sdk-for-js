8f0fe90c1bf01b15ba5f0a32d7d66c29833bba40
colawwj <70128817+colawwj@users.noreply.github.com>
2022-04-06 12:05:04 +0800

    cognitiverservices-track2-release (#21221)

--- sdk/cognitiveservices/arm-cognitiveservices/src/operations/privateEndpointConnections.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/src/operations/privateEndpointConnections.ts
@@ -151,10 +151,12 @@ export class PrivateEndpointConnectionsImpl
       },
       createOrUpdateOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
@@ -247,10 +249,12 @@ export class PrivateEndpointConnectionsImpl
       },
       deleteOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
