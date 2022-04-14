0b058c33ef48e39b08db36f47e0d66beca09297f
zsgwwj <v-sigangzhou@microsoft.com>
2022-04-14 10:37:43 +0800

    update

--- sdk/containerregistry/arm-containerregistry/samples/v10-beta/javascript/webhooksCreateSample.js
+++ sdk/containerregistry/arm-containerregistry/samples/v10-beta/javascript/webhooksCreateSample.js
@@ -25,7 +25,7 @@ async function webhookCreate() {
   const webhookCreateParameters = {
     actions: ["push"],
     customHeaders: {
-      authorization: "Basic 000000000000000000000000000000000000000000000000000",
+      authorization: "",
     },
     location: "westus",
     scope: "myRepository",
