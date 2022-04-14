ef205eca85019c04de6b046b178a25d1213e6c58
azure-sdk <azuresdk@microsoft.com>
2022-04-14 04:41:21 +0000

    Post release automated changes for azure-arm-appplatform

--- sdk/appplatform/arm-appplatform/src/appPlatformManagementClient.ts
+++ sdk/appplatform/arm-appplatform/src/appPlatformManagementClient.ts
@@ -93,7 +93,7 @@ export class AppPlatformManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-appplatform/2.0.0-beta.5`;
+    const packageDetails = `azsdk-js-arm-appplatform/2.0.0-beta.6`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
