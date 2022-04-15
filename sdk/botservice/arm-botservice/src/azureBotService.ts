64054804fe0dcfbe619fd5990d3547f45ecee932
azure-sdk <azuresdk@microsoft.com>
2022-04-15 02:32:40 +0000

    Post release automated changes for azure-arm-botservice

--- sdk/botservice/arm-botservice/src/azureBotService.ts
+++ sdk/botservice/arm-botservice/src/azureBotService.ts
@@ -64,7 +64,7 @@ export class AzureBotService extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-botservice/4.0.0-beta.3`;
+    const packageDetails = `azsdk-js-arm-botservice/4.0.0-beta.4`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
