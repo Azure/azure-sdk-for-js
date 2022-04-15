bb9aa847279ed90c43b2909f6410a33da5001dc3
azure-sdk <azuresdk@microsoft.com>
2022-04-15 10:20:30 +0000

    Post release automated changes for azure-arm-consumption

--- sdk/consumption/arm-consumption/src/consumptionManagementClient.ts
+++ sdk/consumption/arm-consumption/src/consumptionManagementClient.ts
@@ -80,7 +80,7 @@ export class ConsumptionManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-consumption/9.0.1`;
+    const packageDetails = `azsdk-js-arm-consumption/9.0.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
