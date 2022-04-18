c400c2a028b42b07d4a17ba6a93ebee0ae531b10
SDKAuto <sdkautomation@microsoft.com>
2022-04-18 06:25:01 +0000

    CodeGen from PR 18685 in Azure/azure-rest-api-specs [Resources] config readme.python (#18685)
    * config readme.python
    
    * Update readme.python.md
    
    * Update readme.python.md
    
    * Update readme.python.md
    
    * Update readme.python.md
    
    Co-authored-by: Yuchao Yan <yuchaoyan@microsoft.com>

--- sdk/resources/arm-resources/src/resourceManagementClient.ts
+++ sdk/resources/arm-resources/src/resourceManagementClient.ts
@@ -62,7 +62,7 @@ export class ResourceManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-resources/5.0.0`;
+    const packageDetails = `azsdk-js-arm-resources/5.0.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
