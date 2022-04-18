03a407331a377a813983281ae266f2b362abe6ea
colawwj <70128817+colawwj@users.noreply.github.com>
2022-04-18 11:02:34 +0800

    Refresh 6 (#21432)
    * cosmosdb
    
    * customer-insights
    
    * databox
    
    * databoxedge
    
    * databricks
    
    * update sample-dev
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- sdk/cosmosdb/arm-cosmosdb/CHANGELOG.md
+++ sdk/cosmosdb/arm-cosmosdb/CHANGELOG.md
@@ -1,16 +1,6 @@
 # Release History
-
-## 16.0.0-beta.2 (Unreleased)
-
-### Features Added
-
-### Breaking Changes
-
-### Bugs Fixed
-
-### Other Changes
-
-## 16.0.0-beta.1 (2022-02-21)
+    
+## 16.0.0-beta.2 (2022-04-15)
     
 **Features**
 
@@ -235,11 +225,7 @@
   - Added Enum KnownServiceStatus
   - Added Enum KnownServiceType
   - Enum KnownAuthenticationMethod has a new value Ldap
-
-**Breaking Changes**
-
-  - Type Alias DatabaseAccountCreateUpdateParameters no longer has parameter identity
-  - Type Alias DatabaseAccountGetResults no longer has parameter identity
+  - Bug fix
     
     
 ## 15.0.0 (2021-12-09)
--- sdk/cosmosdb/arm-cosmosdb/src/operations/notebookWorkspaces.ts
+++ sdk/cosmosdb/arm-cosmosdb/src/operations/notebookWorkspaces.ts
@@ -211,10 +211,12 @@ export class NotebookWorkspacesImpl implements NotebookWorkspaces {
       },
       createOrUpdateOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
@@ -300,10 +302,12 @@ export class NotebookWorkspacesImpl implements NotebookWorkspaces {
       { resourceGroupName, accountName, notebookWorkspaceName, options },
       deleteOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
@@ -404,10 +408,12 @@ export class NotebookWorkspacesImpl implements NotebookWorkspaces {
       { resourceGroupName, accountName, notebookWorkspaceName, options },
       regenerateAuthTokenOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
@@ -489,10 +495,12 @@ export class NotebookWorkspacesImpl implements NotebookWorkspaces {
       { resourceGroupName, accountName, notebookWorkspaceName, options },
       startOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
