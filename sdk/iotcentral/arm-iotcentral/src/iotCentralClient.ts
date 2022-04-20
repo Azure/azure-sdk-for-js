8b66620ef8b05735ae18713085ab7ad84c435f89
azure-sdk <azuresdk@microsoft.com>
2022-04-20 06:15:32 +0000

    Post release automated changes for azure-arm-iotcentral

--- sdk/iotcentral/arm-iotcentral/src/iotCentralClient.ts
+++ sdk/iotcentral/arm-iotcentral/src/iotCentralClient.ts
@@ -54,7 +54,7 @@ export class IotCentralClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-iotcentral/7.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-iotcentral/7.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
