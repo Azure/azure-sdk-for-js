5b5d787833ed3357c99d4bc36cb093a0dfd72f64
colawwj <70128817+colawwj@users.noreply.github.com>
2022-04-20 16:17:25 +0800

    synapse-release (#21502)

--- sdk/synapse/arm-synapse/src/operations/sqlPoolWorkloadClassifier.ts
+++ sdk/synapse/arm-synapse/src/operations/sqlPoolWorkloadClassifier.ts
@@ -236,10 +236,12 @@ export class SqlPoolWorkloadClassifierImpl
       },
       createOrUpdateOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
@@ -341,10 +343,12 @@ export class SqlPoolWorkloadClassifierImpl
       },
       deleteOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
--- sdk/synapse/arm-synapse/src/operations/workspaceManagedSqlServerEncryptionProtector.ts
+++ sdk/synapse/arm-synapse/src/operations/workspaceManagedSqlServerEncryptionProtector.ts
@@ -192,10 +192,12 @@ export class WorkspaceManagedSqlServerEncryptionProtectorImpl
       },
       createOrUpdateOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
@@ -299,10 +301,12 @@ export class WorkspaceManagedSqlServerEncryptionProtectorImpl
       { resourceGroupName, workspaceName, encryptionProtectorName, options },
       revalidateOperationSpec
     );
-    return new LroEngine(lro, {
+    const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
+    await poller.poll();
+    return poller;
   }
 
   /**
