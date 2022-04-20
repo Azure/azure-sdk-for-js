3560de9659363361833e0588cce084b332573f24
Jeff Fisher <jeffish@microsoft.com>
2022-04-20 15:35:16 -0700

    To clean up old, insecure dependencies, this removes our old autorest generation code for track 1.
    I created a tag that predates this change in case we have to branch and hotfix: https://github.com/Azure/azure-sdk-for-js/releases/tag/track1-servicing

--- .scripts/automation_generate.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@6.0.0-alpha.16.20220105.1
--- .scripts/automation_init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-npm install -g @microsoft/rush
-npm install -g @azure-tools/js-sdk-release-tools
