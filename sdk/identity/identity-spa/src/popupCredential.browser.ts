f0363ff6613282644b4acfe0e2366b7a363edd48
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2022-04-21 22:05:18 +0000

    formatting

--- sdk/identity/identity-spa/src/popupCredential.browser.ts
+++ sdk/identity/identity-spa/src/popupCredential.browser.ts
@@ -66,13 +66,17 @@ export class PopupCredential implements TokenCredential {
    *                TokenCredential implementation might make.
    */
   async getToken(scopes: string | string[], options: GetTokenOptions = {}): Promise<AccessToken> {
-    return tracingClient.withSpan(`${this.constructor.name}.getToken`, options, async (newOptions) => {
-      const arrayScopes = Array.isArray(scopes) ? scopes : [scopes];
-      return this.msalFlow.getToken(arrayScopes, {
-        ...newOptions,
-        disableAutomaticAuthentication: true,
-      });
-    });
+    return tracingClient.withSpan(
+      `${this.constructor.name}.getToken`,
+      options,
+      async (newOptions) => {
+        const arrayScopes = Array.isArray(scopes) ? scopes : [scopes];
+        return this.msalFlow.getToken(arrayScopes, {
+          ...newOptions,
+          disableAutomaticAuthentication: true,
+        });
+      }
+    );
   }
 
   /**
@@ -89,11 +93,15 @@ export class PopupCredential implements TokenCredential {
     scopes: string | string[],
     options: GetTokenOptions = {}
   ): Promise<AuthenticationRecord | undefined> {
-    return tracingClient.withSpan(`${this.constructor.name}.authenticate`, options, async (newOptions) => {
-      const arrayScopes = Array.isArray(scopes) ? scopes : [scopes];
-      await this.msalFlow.getToken(arrayScopes, newOptions);
-      return this.msalFlow.getActiveAccount();
-    });
+    return tracingClient.withSpan(
+      `${this.constructor.name}.authenticate`,
+      options,
+      async (newOptions) => {
+        const arrayScopes = Array.isArray(scopes) ? scopes : [scopes];
+        await this.msalFlow.getToken(arrayScopes, newOptions);
+        return this.msalFlow.getActiveAccount();
+      }
+    );
   }
 
   /**
