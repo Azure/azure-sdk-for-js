61e345865a8e538d24f2bd5e8286ed7802b17a9f
Aigerim Beishenbekova <aigerimb@microsoft.com>
2022-04-26 16:58:01 +0200

    update&added tests, recordings

--- sdk/communication/communication-identity/test/public/node/getTokenForTeamsUser.node.spec.ts
+++ sdk/communication/communication-identity/test/public/node/getTokenForTeamsUser.node.spec.ts
@@ -19,17 +19,17 @@ matrix([[true, false]], async function (useAad) {
     let client: CommunicationIdentityClient;
     const sanitizedValue = "sanitized";
     let params = {
-        teamsToken: sanitizedValue,
-        appId: sanitizedValue,
-        userId: sanitizedValue 
-      };
+      teamsToken: sanitizedValue,
+      appId: sanitizedValue,
+      userId: sanitizedValue,
+    };
 
     before(async function (this: Context) {
       const skipTests = env.SKIP_INT_IDENTITY_EXCHANGE_TOKEN_TEST === "true";
       if (skipTests) {
         this.skip();
       }
-      if(!isPlaybackMode()){
+      if (!isPlaybackMode()) {
         params = await fetchParamsForGetTokenForTeamsUser();
       }
     });
@@ -48,23 +48,23 @@ matrix([[true, false]], async function (useAad) {
       }
     });
 
-    async function fetchParamsForGetTokenForTeamsUser(){
+    async function fetchParamsForGetTokenForTeamsUser() {
       const msalConfig = {
-          auth: {
-            clientId: env.COMMUNICATION_M365_APP_ID ?? "",
-            authority: env.COMMUNICATION_M365_AAD_AUTHORITY + "/" + env.COMMUNICATION_M365_AAD_TENANT,
-          },
-        };
-        const msalInstance = new PublicClientApplication(msalConfig);
-        const usernamePasswordRequest = {
-          scopes: [env.COMMUNICATION_M365_SCOPE ?? ""],
-          username: env.COMMUNICATION_MSAL_USERNAME ?? "",
-          password: env.COMMUNICATION_MSAL_PASSWORD ?? "",
-        };
-        const response = await msalInstance.acquireTokenByUsernamePassword(usernamePasswordRequest);
-        params.teamsToken = response!.accessToken;
-        params.appId = env.COMMUNICATION_M365_APP_ID ?? "";
-        params.userId = response!.account!.homeAccountId.split(".")[0];
+        auth: {
+          clientId: env.COMMUNICATION_M365_APP_ID ?? "",
+          authority: env.COMMUNICATION_M365_AAD_AUTHORITY + "/" + env.COMMUNICATION_M365_AAD_TENANT,
+        },
+      };
+      const msalInstance = new PublicClientApplication(msalConfig);
+      const usernamePasswordRequest = {
+        scopes: [env.COMMUNICATION_M365_SCOPE ?? ""],
+        username: env.COMMUNICATION_MSAL_USERNAME ?? "",
+        password: env.COMMUNICATION_MSAL_PASSWORD ?? "",
+      };
+      const response = await msalInstance.acquireTokenByUsernamePassword(usernamePasswordRequest);
+      params.teamsToken = response!.accessToken;
+      params.appId = env.COMMUNICATION_M365_APP_ID ?? "";
+      params.userId = response!.account!.homeAccountId.split(".")[0];
       return params;
     }
 
@@ -79,13 +79,15 @@ matrix([[true, false]], async function (useAad) {
     }).timeout(5000);
 
     given([
-        { teamsToken: '',       description: 'an empty Teams User AAD token' },
-        { teamsToken: 'invalid', description: 'an invalid Teams User AAD token' },
-        { teamsToken: env.COMMUNICATION_EXPIRED_TEAMS_TOKEN ?? "", description: 'an expired Teams User AAD token' },
-    ]).
-    it("throws an error when attempting to exchange", async function (input) {
+      { teamsToken: "", description: "an empty Teams User AAD token" },
+      { teamsToken: "invalid", description: "an invalid Teams User AAD token" },
+      {
+        teamsToken: env.COMMUNICATION_EXPIRED_TEAMS_TOKEN ?? "",
+        description: "an expired Teams User AAD token",
+      },
+    ]).it("throws an error when attempting to exchange", async function (input) {
       try {
-        if(isPlaybackMode()){
+        if (isPlaybackMode()) {
           input.teamsToken = sanitizedValue;
         }
         await client.getTokenForTeamsUser(input.teamsToken, params.appId, params.userId);
@@ -98,13 +100,12 @@ matrix([[true, false]], async function (useAad) {
     });
 
     given([
-        { appId: '',       description: 'an empty Client ID of an Azure AD application' },
-        { appId: 'invalid', description: 'an invalid Client ID of an Azure AD application' },
-        { appId: params.userId, description: 'a wrong Client ID of an Azure AD application' },
-    ]).
-    it("throws an error when attempting to exchange", async function (input) {
+      { appId: "", description: "an empty Client ID of an Azure AD application" },
+      { appId: "invalid", description: "an invalid Client ID of an Azure AD application" },
+      { appId: params.userId, description: "a wrong Client ID of an Azure AD application" },
+    ]).it("throws an error when attempting to exchange", async function (input) {
       try {
-        if(isPlaybackMode()){
+        if (isPlaybackMode()) {
           input.appId = sanitizedValue;
         }
         await client.getTokenForTeamsUser(params.teamsToken, input.appId, params.userId);
@@ -117,13 +118,12 @@ matrix([[true, false]], async function (useAad) {
     });
 
     given([
-        { userId: '',       description: 'an empty Object ID of an Azure AD user (Teams User)' },
-        { userId: 'invalid', description: 'an invalid Object ID of an Azure AD user (Teams User)' },
-        { userId: params.appId, description: 'a wrong Object ID of an Azure AD user (Teams User)' },
-    ]).
-    it("throws an error when attempting to exchange", async function (input) {
+      { userId: "", description: "an empty Object ID of an Azure AD user (Teams User)" },
+      { userId: "invalid", description: "an invalid Object ID of an Azure AD user (Teams User)" },
+      { userId: params.appId, description: "a wrong Object ID of an Azure AD user (Teams User)" },
+    ]).it("throws an error when attempting to exchange", async function (input) {
       try {
-        if(isPlaybackMode()){
+        if (isPlaybackMode()) {
           input.userId = sanitizedValue;
         }
         await client.getTokenForTeamsUser(params.teamsToken, params.appId, input.userId);
@@ -134,6 +134,5 @@ matrix([[true, false]], async function (useAad) {
 
       assert.fail("Should have thrown an error");
     });
-
   });
 });
