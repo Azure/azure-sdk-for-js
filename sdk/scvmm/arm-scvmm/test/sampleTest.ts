fe15971f27b6dc7b08ca825078a2c14430276e8b
zsgwwj <v-sigangzhou@microsoft.com>
2022-04-27 17:31:26 +0800

    update

--- sdk/scvmm/arm-scvmm/test/sampleTest.ts
+++ sdk/scvmm/arm-scvmm/test/sampleTest.ts
@@ -7,37 +7,42 @@
  */
 
 import {
-  Recorder,
-  RecorderStartOptions,
-  env
+  env,
+  record,
+  RecorderEnvironmentSetup,
+  Recorder
 } from "@azure-tools/test-recorder";
 import * as assert from "assert";
-import { Context } from "mocha";
 
-const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
-};
-
-const recorderOptions: RecorderStartOptions = {
-  envSetupForPlayback: replaceableVariables
+const recorderEnvSetup: RecorderEnvironmentSetup = {
+  replaceableVariables: {
+    AZURE_CLIENT_ID: "azure_client_id",
+    AZURE_CLIENT_SECRET: "azure_client_secret",
+    AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+    SUBSCRIPTION_ID: "azure_subscription_id"
+  },
+  customizationsOnRecordings: [
+    (recording: any): any =>
+      recording.replace(
+        /"access_token":"[^"]*"/g,
+        `"access_token":"access_token"`
+      )
+  ],
+  queryParametersToSkip: []
 };
 
 describe("My test", () => {
   let recorder: Recorder;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
+  beforeEach(async function() {
+    recorder = record(this, recorderEnvSetup);
   });
 
-  afterEach(async function () {
+  afterEach(async function() {
     await recorder.stop();
   });
 
-  it("sample test", async function () {
+  it("sample test", async function() {
     console.log("Hi, I'm a test!");
   });
 });
