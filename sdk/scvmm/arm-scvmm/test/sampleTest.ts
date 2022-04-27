693e9815c4a2d24c16923013aa4828b92fcada2b
zsgwwj <v-sigangzhou@microsoft.com>
2022-04-27 15:41:12 +0800

    update

--- sdk/scvmm/arm-scvmm/test/sampleTest.ts
+++ sdk/scvmm/arm-scvmm/test/sampleTest.ts
@@ -11,7 +11,7 @@ import {
   RecorderStartOptions,
   env
 } from "@azure-tools/test-recorder";
-import { assert } from "chai";
+import * as assert from "assert";
 import { Context } from "mocha";
 
 const replaceableVariables: Record<string, string> = {
@@ -28,16 +28,16 @@ const recorderOptions: RecorderStartOptions = {
 describe("My test", () => {
   let recorder: Recorder;
 
-  beforeEach(async function(this: Context) {
+  beforeEach(async function (this: Context) {
     recorder = new Recorder(this.currentTest);
     await recorder.start(recorderOptions);
   });
 
-  afterEach(async function() {
+  afterEach(async function () {
     await recorder.stop();
   });
 
-  it("sample test", async function() {
+  it("sample test", async function () {
     console.log("Hi, I'm a test!");
   });
 });
