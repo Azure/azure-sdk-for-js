Azure/azure-sdk-for-js

commit aa3b625dfc98ac50d15f5b573b0d673d0db9240a

[core-amqp] Remove explicit `any` catch variables (#21485)
* [core-amqp] Remove explicit `any` catch variables

Also remove the @typescript-eslint/no-explicit-any rule

* Use `isError()` from core-util

Format

* Address CR feedback

* revert back to old run time behavior of retrying

sdk/core/core-amqp/src/errors.ts
@@ -5,6 +5,7 @@
 import { AmqpError, AmqpResponseStatusCode, isAmqpError as rheaIsAmqpError } from "rhea-promise";
 import { isDefined, isObjectWithProperties } from "./util/typeGuards";
 import { isNode, isNumber, isString } from "../src/util/utils";
+import { isError } from "@azure/core-util";
 
 /**
  * Maps the conditions to the numeric AMQP Response status codes.
@@ -676,9 +677,9 @@ export function translate(err: unknown): MessagingError | Error {
     return error;
   }
 
-  if (err instanceof Error && err.name === "MessagingError") {
+  if (err instanceof MessagingError) {
     // already translated
-    return err as MessagingError;
+    return err;
   }
 
   if (isSystemError(err)) {
@@ -710,7 +711,7 @@ export function translate(err: unknown): MessagingError | Error {
 
   // Some errors come from rhea-promise and need to be converted to MessagingError.
   // A subset of these are also retryable.
-  if (err instanceof Error && rheaPromiseErrors.indexOf(err.name) !== -1) {
+  if (isError(err) && rheaPromiseErrors.indexOf(err.name) !== -1) {
     const error = new MessagingError(err.message, err);
     error.code = err.name;
     if (error.code && retryableErrors.indexOf(error.code) === -1) {
@@ -720,7 +721,7 @@ export function translate(err: unknown): MessagingError | Error {
     return error;
   }
 
-  return err instanceof Error ? err : new Error(String(err));
+  return isError(err) ? err : new Error(String(err));
 }
 
 /**

sdk/core/core-amqp/src/cbs.ts
@@ -18,6 +18,7 @@ import { RequestResponseLink } from "./requestResponseLink";
 import { StandardAbortMessage } from "./util/constants";
 import { TokenType } from "./auth/token";
 import { defaultCancellableLock } from "./util/utils";
+import { isError } from "@azure/core-util";
 import { translate } from "./errors";
 
 /**
@@ -153,7 +154,7 @@ export class CbsClient {
           this._cbsSenderReceiverLink!.receiver.name
         );
       }
-    } catch (err: any) {
+    } catch (err) {
       const translatedError = translate(err);
       logger.warning(
         "[%s] An error occurred while establishing the cbs links: %s",
@@ -234,11 +235,11 @@ export class CbsClient {
       });
       logger.verbose("[%s] The CBS response is: %O", this.connection.id, responseMessage);
       return this._fromRheaMessageResponse(responseMessage);
-    } catch (err: any) {
+    } catch (err) {
       logger.warning(
         "[%s] An error occurred while negotiating the cbs claim: %s",
         this.connection.id,
-        `${err?.name}: ${err?.message}`
+        isError(err) ? `${err.name}: ${err.message}` : String(err)
       );
       logErrorStackTrace(err);
       throw err;
@@ -258,9 +259,9 @@ export class CbsClient {
         await cbsLink!.close();
         logger.verbose("[%s] Successfully closed the cbs session.", this.connection.id);
       }
-    } catch (err: any) {
+    } catch (err) {
       const msg = `An error occurred while closing the cbs link: ${
-        err.stack || JSON.stringify(err)
+        isError(err) && err.stack ? err.stack : JSON.stringify(err)
       }.`;
       logger.verbose("[%s] %s", this.connection.id, msg);
       throw new Error(msg);
@@ -279,9 +280,9 @@ export class CbsClient {
         cbsLink!.remove();
         logger.verbose("[%s] Successfully removed the cbs session.", this.connection.id);
       }
-    } catch (err: any) {
+    } catch (err) {
       const msg = `An error occurred while removing the cbs link: ${
-        err.stack || JSON.stringify(err)
+        isError(err) && err.stack ? err.stack : JSON.stringify(err)
       }.`;
       logger.verbose("[%s] %s", this.connection.id, msg);
       throw new Error(msg);

sdk/core/core-amqp/src/retry.ts
@@ -224,14 +224,14 @@ export async function retry<T>(config: RetryConfig<T>): Promise<T> {
       const err = translate(_err);
 
       if (
-        !(err as MessagingError).retryable &&
+        !(err as any).retryable &&
         err.name === "ServiceCommunicationError" &&
         config.connectionHost
       ) {
         const isConnected = await checkNetworkConnection(config.connectionHost);
         if (!isConnected) {
           err.name = "ConnectionLostError";
-          (err as MessagingError).retryable = true;
+          (err as any).retryable = true;
         }
       }
       logger.verbose(
@@ -243,7 +243,7 @@ export async function retry<T>(config: RetryConfig<T>): Promise<T> {
       );
 
       lastError = err;
-      if ((lastError as MessagingError).retryable && totalNumberOfAttempts > i) {
+      if ((lastError as any).retryable && totalNumberOfAttempts > i) {
         const targetDelayInMs = calculateDelay(
           i,
           config.retryOptions.retryDelayInMs,

sdk/core/core-amqp/test/cbs.spec.ts
@@ -6,6 +6,7 @@ import { AbortController } from "@azure/abort-controller";
 import { Connection } from "rhea-promise";
 import { assert } from "chai";
 import { createConnectionStub } from "./utils/createConnectionStub";
+import { isError } from "@azure/core-util";
 import { stub } from "sinon";
 
 describe("CbsClient", function () {
@@ -23,8 +24,9 @@ describe("CbsClient", function () {
       try {
         await cbsClient.init({ abortSignal: signal });
         throw new Error(TEST_FAILURE);
-      } catch (err: any) {
-        assert.equal(err.name, "AbortError");
+      } catch (err) {
+        assert.ok(isError(err));
+        assert.equal((err as Error).name, "AbortError");
       }
     });
 
@@ -54,8 +56,9 @@ describe("CbsClient", function () {
       try {
         await cbsClient.init({ abortSignal: signal });
         throw new Error(TEST_FAILURE);
-      } catch (err: any) {
-        assert.equal(err.name, "AbortError");
+      } catch (err) {
+        assert.ok(isError(err));
+        assert.equal((err as Error).name, "AbortError");
       }
     });
 
@@ -74,8 +77,9 @@ describe("CbsClient", function () {
       try {
         await cbsClient.init({ abortSignal: signal });
         throw new Error(TEST_FAILURE);
-      } catch (err: any) {
-        assert.equal(err.name, "AbortError");
+      } catch (err) {
+        assert.ok(isError(err));
+        assert.equal((err as Error).name, "AbortError");
       }
     });
   });
@@ -88,9 +92,10 @@ describe("CbsClient", function () {
       try {
         await cbsClient.negotiateClaim("audience", "token", TokenType.CbsTokenTypeSas);
         throw new Error(TEST_FAILURE);
-      } catch (err: any) {
+      } catch (err) {
+        assert.ok(isError(err));
         assert.equal(
-          err.message,
+          (err as Error).message,
           "Attempted to negotiate a claim but the CBS link does not exist."
         );
       }
@@ -112,8 +117,9 @@ describe("CbsClient", function () {
             abortSignal: signal,
           });
           throw new Error(TEST_FAILURE);
-        } catch (err: any) {
-          assert.equal(err.name, "AbortError");
+        } catch (err) {
+          assert.ok(isError(err));
+          assert.equal((err as Error).name, "AbortError");
         }
       });
 
@@ -134,8 +140,9 @@ describe("CbsClient", function () {
             abortSignal: signal,
           });
           throw new Error(TEST_FAILURE);
-        } catch (err: any) {
-          assert.equal(err.name, "AbortError");
+        } catch (err) {
+          assert.ok(isError(err));
+          assert.equal((err as Error).name, "AbortError");
         }
       });
     });

sdk/core/core-amqp/test/lock.spec.ts
@@ -46,8 +46,9 @@ describe("CancellableAsyncLock", function () {
           { timeoutInMs: undefined, abortSignal: undefined }
         );
         throw new Error(TEST_FAILURE);
-      } catch (err: any) {
-        should.equal(err.message, "I break things!");
+      } catch (err) {
+        should.equal(true, err instanceof Error);
+        should.equal((err as Error).message, "I break things!");
       }
     });
 
@@ -274,7 +275,7 @@ describe("CancellableAsyncLock", function () {
       while (tasks.length) {
         try {
           await Promise.race(tasks);
-        } catch (err: any) {
+        } catch (err) {
           /* no-op */
         }
       }
@@ -359,7 +360,7 @@ describe("CancellableAsyncLock", function () {
       while (tasks.length) {
         try {
           await Promise.race(tasks);
-        } catch (err: any) {
+        } catch (err) {
           /* no-op */
         }
       }

sdk/core/core-amqp/test/requestResponse.spec.ts
@@ -21,6 +21,7 @@ import { SinonSpy, fake, stub } from "sinon";
 import EventEmitter from "events";
 import { assert } from "chai";
 import { createConnectionStub } from "./utils/createConnectionStub";
+import { isError } from "@azure/core-util";
 
 interface Window {}
 declare let self: Window & typeof globalThis;
@@ -65,8 +66,9 @@ describe("RequestResponseLink", function () {
       try {
         await RequestResponseLink.create(connection, {}, {}, { abortSignal: signal });
         throw new Error(TEST_FAILURE);
-      } catch (err: any) {
-        assert.equal(err.name, "AbortError");
+      } catch (err) {
+        assert.ok(isError(err));
+        assert.equal((err as Error).name, "AbortError");
       }
     });
 
@@ -81,8 +83,9 @@ describe("RequestResponseLink", function () {
       try {
         await RequestResponseLink.create(connection, {}, {}, { abortSignal: signal });
         throw new Error(TEST_FAILURE);
-      } catch (err: any) {
-        assert.equal(err.name, "AbortError");
+      } catch (err) {
+        assert.ok(isError(err));
+        assert.equal((err as Error).name, "AbortError");
       }
     });
   });
@@ -232,7 +235,7 @@ describe("RequestResponseLink", function () {
       await link.sendRequest(request1, {
         timeoutInMs: 2000,
       });
-    } catch (error: any) {
+    } catch (error) {
       assert.equal(
         request1.message_id === undefined,
         false,
@@ -312,8 +315,9 @@ describe("RequestResponseLink", function () {
     try {
       await failedRequest;
       throw new Error("Test failure");
-    } catch (err: any) {
-      err.message.should.not.equal("Test failure");
+    } catch (err) {
+      assert.ok(isError(err));
+      (err as Error).message.should.not.equal("Test failure");
     }
 
     // ensure the other request succeeds
@@ -449,9 +453,15 @@ describe("RequestResponseLink", function () {
       setTimeout(controller.abort.bind(controller), 100);
       await link.sendRequest(request, { abortSignal: signal, requestName: "foo" });
       throw new Error(`Test failure`);
-    } catch (err: any) {
-      assert.equal(err.name, "AbortError", `Error name ${err.name} is not as expected`);
-      assert.equal(err.message, StandardAbortMessage, `Incorrect error received "${err.message}"`);
+    } catch (err) {
+      assert.ok(isError(err));
+      const error = err as Error;
+      assert.equal(error.name, "AbortError", `Error name ${error.name} is not as expected`);
+      assert.equal(
+        error.message,
+        StandardAbortMessage,
+        `Incorrect error received "${error.message}"`
+      );
     }
     assertItemsLengthInResponsesMap(link["_responsesMap"], 0);
   });
@@ -513,9 +523,15 @@ describe("RequestResponseLink", function () {
         requestName: "foo",
       });
       throw new Error(`Test failure`);
-    } catch (err: any) {
-      assert.equal(err.name, "AbortError", `Error name ${err.name} is not as expected`);
-      assert.equal(err.message, StandardAbortMessage, `Incorrect error received "${err.message}"`);
+    } catch (err) {
+      assert.ok(isError(err));
+      const error = err as Error;
+      assert.equal(error.name, "AbortError", `Error name ${error.name} is not as expected`);
+      assert.equal(
+        error.message,
+        StandardAbortMessage,
+        `Incorrect error received "${error.message}"`
+      );
     }
     // Final state of the map
     assertItemsLengthInResponsesMap(link["_responsesMap"], 0);
@@ -568,9 +584,15 @@ describe("RequestResponseLink", function () {
       controller.abort();
       await link.sendRequest(request, { abortSignal: signal, requestName: "foo" });
       throw new Error(`Test failure`);
-    } catch (err: any) {
-      assert.equal(err.name, "AbortError", `Error name ${err.name} is not as expected`);
-      assert.equal(err.message, StandardAbortMessage, `Incorrect error received "${err.message}"`);
+    } catch (err) {
+      assert.ok(isError(err));
+      const error = err as Error;
+      assert.equal(error.name, "AbortError", `Error name ${error.name} is not as expected`);
+      assert.equal(
+        error.message,
+        StandardAbortMessage,
+        `Incorrect error received "${error.message}"`
+      );
     }
     assertItemsLengthInResponsesMap(link["_responsesMap"], 0);
   });
@@ -637,8 +659,10 @@ describe("RequestResponseLink", function () {
       try {
         await link.sendRequest(request, { timeoutInMs: 120000, requestName: "foo" });
         throw new Error(testFailureMessage);
-      } catch (err: any) {
-        assert.notEqual(err.message, testFailureMessage);
+      } catch (err) {
+        assert.ok(isError(err));
+        const error = err as Error;
+        assert.notEqual(error.message, testFailureMessage);
       }
       assertItemsLengthInResponsesMap(link["_responsesMap"], 0);
       assert.equal(clearTimeoutCalledCount, 1, "Expected clearTimeout to be called once.");

sdk/core/core-amqp/test/retry.spec.ts
@@ -42,7 +42,7 @@ const should = chai.should();
         result.code.should.equal(200);
         result.description.should.equal("OK");
         counter.should.equal(1);
-      } catch (err: any) {
+      } catch (err) {
         debug("An error occurred in a test that should have succeeded: %O", err);
         throw err;
       }
@@ -65,10 +65,10 @@ const should = chai.should();
           retryOptions: { retryDelayInMs: 15000, mode: mode },
         };
         await retry(config);
-      } catch (err: any) {
+      } catch (err) {
         should.exist(err);
         should.equal(true, err instanceof MessagingError);
-        err.message.should.equal("I would like to fail, not retryable.");
+        (err as MessagingError).message.should.equal("I would like to fail, not retryable.");
         counter.should.equal(1);
       }
     });
@@ -100,7 +100,7 @@ const should = chai.should();
         result.code.should.equal(200);
         result.description.should.equal("OK");
         counter.should.equal(2);
-      } catch (err: any) {
+      } catch (err) {
         debug("An error occurred in a test that should have succeeded: %O", err);
         throw err;
       }
@@ -136,7 +136,7 @@ const should = chai.should();
         result.code.should.equal(200);
         result.description.should.equal("OK");
         counter.should.equal(3);
-      } catch (err: any) {
+      } catch (err) {
         debug("An error occurred in a test that should have succeeded: %O", err);
         throw err;
       }
@@ -170,10 +170,10 @@ const should = chai.should();
           retryOptions: { maxRetries: 2, retryDelayInMs: 500, mode: mode },
         };
         await retry(config);
-      } catch (err: any) {
+      } catch (err) {
         should.exist(err);
         should.equal(true, err instanceof MessagingError);
-        err.message.should.equal("I would like to fail.");
+        (err as MessagingError).message.should.equal("I would like to fail.");
         counter.should.equal(3);
       }
     });
@@ -194,10 +194,10 @@ const should = chai.should();
           retryOptions: { maxRetries: 4, retryDelayInMs: 500, mode: mode },
         };
         await retry(config);
-      } catch (err: any) {
+      } catch (err) {
         should.exist(err);
         should.equal(true, err instanceof MessagingError);
-        err.message.should.equal("I would always like to fail, keep retrying.");
+        (err as MessagingError).message.should.equal("I would always like to fail, keep retrying.");
         counter.should.equal(5);
       }
     });
@@ -226,10 +226,10 @@ const should = chai.should();
         await Promise.race([retry(config), delay(10000, delayAbortController.signal)]);
         // If we get here, `delay` won :-(
         throw new Error("TestFailure: 'retry' took longer than expected to return.");
-      } catch (err: any) {
+      } catch (err) {
         should.exist(err);
-        err.message.should.equal("I would always like to fail, keep retrying.");
         should.equal(true, err instanceof MessagingError);
+        (err as MessagingError).message.should.equal("I would always like to fail, keep retrying.");
         counter.should.equal(1);
         // Clear delay's setTimeout...we don't need it anymore.
         delayAbortController.abort();
@@ -261,10 +261,10 @@ const should = chai.should();
         await Promise.race([retry(config), delay(1500, delayAbortController.signal)]);
         // If we get here, `delay` won :-(
         throw new Error("TestFailure: 'retry' took longer than expected to return.");
-      } catch (err: any) {
+      } catch (err) {
         should.exist(err);
-        err.message.should.equal("I would always like to fail, keep retrying.");
         should.equal(true, err instanceof MessagingError);
+        (err as MessagingError).message.should.equal("I would always like to fail, keep retrying.");
         counter.should.equal(2);
         // Clear delay's setTimeout...we don't need it anymore.
         delayAbortController.abort();
@@ -291,9 +291,10 @@ const should = chai.should();
           retryOptions: { maxRetries: 4, retryDelayInMs: 500, mode: mode },
         };
         await retry(config);
-      } catch (err: any) {
+      } catch (err) {
         should.exist(err);
-        should.equal(true, err.name === "AbortError");
+        should.equal(true, err instanceof Error);
+        should.equal(true, (err as Error).name === "AbortError");
         counter.should.equal(1, "It should retry only once");
       }
     });
@@ -319,7 +320,7 @@ const should = chai.should();
           result.code.should.equal(200);
           result.description.should.equal("OK");
           counter.should.equal(1);
-        } catch (err: any) {
+        } catch (err) {
           debug("An error occurred in a test that should have succeeded: %O", err);
           throw err;
         }
@@ -342,10 +343,10 @@ const should = chai.should();
             retryOptions: { maxRetries: Infinity, retryDelayInMs: 500, mode: mode },
           };
           await retry(config);
-        } catch (err: any) {
+        } catch (err) {
           should.exist(err);
           should.equal(true, err instanceof MessagingError);
-          err.message.should.equal("I would like to fail, not retryable.");
+          (err as MessagingError).message.should.equal("I would like to fail, not retryable.");
           counter.should.equal(1);
         }
       });
@@ -377,7 +378,7 @@ const should = chai.should();
           result.code.should.equal(200);
           result.description.should.equal("OK");
           counter.should.equal(2);
-        } catch (err: any) {
+        } catch (err) {
           debug("An error occurred in a test that should have succeeded: %O", err);
           throw err;
         }
@@ -413,7 +414,7 @@ const should = chai.should();
           result.code.should.equal(200);
           result.description.should.equal("OK");
           counter.should.equal(3);
-        } catch (err: any) {
+        } catch (err) {
           debug("An error occurred in a test that should have succeeded: %O", err);
           throw err;
         }
@@ -451,10 +452,10 @@ const should = chai.should();
             },
           };
           await retry(config);
-        } catch (err: any) {
+        } catch (err) {
           should.exist(err);
           should.equal(true, err instanceof MessagingError);
-          err.message.should.equal("I would like to fail.");
+          (err as MessagingError).message.should.equal("I would like to fail.");
           counter.should.equal(3);
         }
       });

sdk/core/core-amqp/test/utils/utils.ts
@@ -17,7 +17,7 @@ export async function settleAllTasks(tasks: Promise<any>[]): Promise<any[]> {
     try {
       const result = await task;
       results.push(result);
-    } catch (err: any) {
+    } catch (err) {
       results.push(err);
     }
   }

commit 28c7a4e982b87c6f2fcea058a853771235ed9c29

[core-amqp] Fix a regression
by reverting to the old runtime behavior of checking whether a message has
already been translated.

The regression was introduced in PR #21485 and caused a Service Bus live test to
fail.

sdk/core/core-amqp/src/errors.ts
@@ -677,7 +677,7 @@ export function translate(err: unknown): MessagingError | Error {
     return error;
   }
 
-  if (err instanceof MessagingError) {
+  if (err instanceof Error && err.name === "MessagingError") {
     // already translated
     return err;
   }