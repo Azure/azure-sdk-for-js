64eb18a83526915eb395f7c81041a8dd7b98eea5
Jeremy Meng <yumeng@microsoft.com>
2022-04-29 19:58:56 +0000

    Fix linter error

--- sdk/servicebus/service-bus/test/internal/unit/client.spec.ts
+++ sdk/servicebus/service-bus/test/internal/unit/client.spec.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { ServiceBusClient, TokenCredential } from "../../../src";
 
 describe("ServiceBusClient unit tests", function(): void {
