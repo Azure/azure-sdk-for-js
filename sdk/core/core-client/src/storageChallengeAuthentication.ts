b38ecde6b4071dcf00ba0e01c257a51034b6b22f
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2022-04-29 19:04:19 +0000

    Add information about challenge process

--- sdk/core/core-client/src/storageChallengeAuthentication.ts
+++ sdk/core/core-client/src/storageChallengeAuthentication.ts
@@ -26,7 +26,9 @@ const Constants = {
 };
 
 /**
- * Defines a callback to handle auth challenge for Storage APIs
+ * Defines a callback to handle auth challenge for Storage APIs.
+ * This implements the bearer challenge process described here: https://docs.microsoft.com/rest/api/storageservices/authorize-with-azure-active-directory#bearer-challenge
+ * Handling has specific features for storage that departs to the general AAD challenge docs.
  **/
 export const storageAuthorizeRequestOnChallenge: (
   challengeOptions: AuthorizeRequestOnChallengeOptions
