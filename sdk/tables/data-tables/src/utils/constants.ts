8df587f0b7cb6b5d5c2305de0d5a38f64b16358c
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2022-04-29 18:59:06 +0000

    revert unwanted change in service version

--- sdk/tables/data-tables/src/utils/constants.ts
+++ sdk/tables/data-tables/src/utils/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const SERVICE_VERSION = "2020-12-06";
+export const SERVICE_VERSION = "2019-02-02";
 export const TRANSACTION_HTTP_VERSION_1_1 = "HTTP/1.1";
 export const TRANSACTION_HTTP_LINE_ENDING = "\r\n";
 
