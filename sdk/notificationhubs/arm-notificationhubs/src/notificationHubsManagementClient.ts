4f886c0aa5cf58cafc914733d88955d17b70b50d
azure-sdk <azuresdk@microsoft.com>
2022-04-29 09:17:12 +0000

    Post release automated changes for azure-arm-notificationhubs

--- sdk/notificationhubs/arm-notificationhubs/src/notificationHubsManagementClient.ts
+++ sdk/notificationhubs/arm-notificationhubs/src/notificationHubsManagementClient.ts
@@ -54,7 +54,7 @@ export class NotificationHubsManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-notificationhubs/2.0.1`;
+    const packageDetails = `azsdk-js-arm-notificationhubs/2.0.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
