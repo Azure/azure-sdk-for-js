374d34cb670d48fbab73da28d67d75fe69e4040d
--global <alankashiwa@gmail.com>
2022-05-01 15:40:58 +0800

    Update Route comment/interface

--- sdk/maps/maps-route/recordings/node/subscriptionkey_mapsrouteclient_fast_tests_getroutedirections/recording_should_accept_latlon_and_return_route_directions.js
+++ sdk/maps/maps-route/recordings/node/subscriptionkey_mapsrouteclient_fast_tests_getroutedirections/recording_should_accept_latlon_and_return_route_directions.js
@@ -1,13 +1,13 @@
 let nock = require('nock');
 
-module.exports.hash = "7afd9623ba1ed79f160a1ce6497f5d77";
+module.exports.hash = "5e9fdf19544aaa8601bcdf175d59c889";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
 nock('https://atlas.microsoft.com:443', {"encodedQueryParams":true})
   .get('/route/directions/json')
   .query(true)
-  .reply(200, {"formatVersion":"0.0.12","routes":[{"summary":{"lengthInMeters":1147,"travelTimeInSeconds":137,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-03-15T06:37:07+01:00","arrivalTime":"2022-03-15T06:39:24+01:00"},"legs":[{"summary":{"lengthInMeters":1147,"travelTimeInSeconds":137,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-03-15T06:37:07+01:00","arrivalTime":"2022-03-15T06:39:24+01:00"},"points":[{"latitude":52.5093,"longitude":13.42937},{"latitude":52.50904,"longitude":13.42913},{"latitude":52.50895,"longitude":13.42904},{"latitude":52.50868,"longitude":13.4288},{"latitude":52.5084,"longitude":13.42857},{"latitude":52.50816,"longitude":13.42839},{"latitude":52.50791,"longitude":13.42825},{"latitude":52.50757,"longitude":13.42772},{"latitude":52.50752,"longitude":13.42785},{"latitude":52.50742,"longitude":13.42809},{"latitude":52.50735,"longitude":13.42824},{"latitude":52.5073,"longitude":13.42837},{"latitude":52.50696,"longitude":13.4291},{"latitude":52.50673,"longitude":13.42961},{"latitude":52.50619,"longitude":13.43092},{"latitude":52.50608,"longitude":13.43116},{"latitude":52.50574,"longitude":13.43195},{"latitude":52.50564,"longitude":13.43218},{"latitude":52.50528,"longitude":13.43299},{"latitude":52.50513,"longitude":13.43336},{"latitude":52.505,"longitude":13.43366},{"latitude":52.50464,"longitude":13.43451},{"latitude":52.50451,"longitude":13.43482},{"latitude":52.50444,"longitude":13.43499},{"latitude":52.50418,"longitude":13.43564},{"latitude":52.50364,"longitude":13.4369},{"latitude":52.50343,"longitude":13.43738},{"latitude":52.5033,"longitude":13.43767},{"latitude":52.50275,"longitude":13.43873}]}],"sections":[{"startPointIndex":0,"endPointIndex":28,"sectionType":"TRAVEL_MODE","travelMode":"car"}]}]}, [
+  .reply(200, {"formatVersion":"0.0.12","routes":[{"summary":{"lengthInMeters":1147,"travelTimeInSeconds":133,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-05-01T09:30:54+02:00","arrivalTime":"2022-05-01T09:33:06+02:00"},"legs":[{"summary":{"lengthInMeters":1147,"travelTimeInSeconds":133,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-05-01T09:30:54+02:00","arrivalTime":"2022-05-01T09:33:06+02:00"},"points":[{"latitude":52.5093,"longitude":13.42937},{"latitude":52.50904,"longitude":13.42913},{"latitude":52.50895,"longitude":13.42904},{"latitude":52.50868,"longitude":13.4288},{"latitude":52.5084,"longitude":13.42857},{"latitude":52.50816,"longitude":13.42839},{"latitude":52.50791,"longitude":13.42825},{"latitude":52.50757,"longitude":13.42772},{"latitude":52.50752,"longitude":13.42785},{"latitude":52.50742,"longitude":13.42809},{"latitude":52.50735,"longitude":13.42824},{"latitude":52.5073,"longitude":13.42837},{"latitude":52.50696,"longitude":13.4291},{"latitude":52.50673,"longitude":13.42961},{"latitude":52.50619,"longitude":13.43092},{"latitude":52.50608,"longitude":13.43116},{"latitude":52.50574,"longitude":13.43195},{"latitude":52.50564,"longitude":13.43218},{"latitude":52.50528,"longitude":13.43299},{"latitude":52.50513,"longitude":13.43336},{"latitude":52.505,"longitude":13.43366},{"latitude":52.50464,"longitude":13.43451},{"latitude":52.50451,"longitude":13.43482},{"latitude":52.50444,"longitude":13.43499},{"latitude":52.50418,"longitude":13.43564},{"latitude":52.50364,"longitude":13.4369},{"latitude":52.50343,"longitude":13.43738},{"latitude":52.5033,"longitude":13.43767},{"latitude":52.50275,"longitude":13.43874}]}],"sections":[{"startPointIndex":0,"endPointIndex":28,"sectionType":"TRAVEL_MODE","travelMode":"car"}]}]}, [
   'Cache-Control',
   'no-transform, no-cache',
   'Pragma',
@@ -27,7 +27,7 @@ nock('https://atlas.microsoft.com:443', {"encodedQueryParams":true})
   'X-Cache',
   'CONFIG_NOCACHE',
   'X-MSEdge-Ref',
-  'Ref A: F1E97320E5004D1AB91F4DC26A702CD1 Ref B: TPE30EDGE0421 Ref C: 2022-03-15T05:37:07Z',
+  'Ref A: C1421DCC1B0B4612AF614D00AB0C98D9 Ref B: TPE30EDGE0508 Ref C: 2022-05-01T07:30:54Z',
   'Date',
-  'Tue, 15 Mar 2022 05:37:07 GMT'
+  'Sun, 01 May 2022 07:30:54 GMT'
 ]);
--- sdk/maps/maps-route/recordings/node/subscriptionkey_mapsrouteclient_fast_tests_requestroutematrix/recording_should_accept_routematrixquery_and_return_expected_number_of_results.js
+++ sdk/maps/maps-route/recordings/node/subscriptionkey_mapsrouteclient_fast_tests_requestroutematrix/recording_should_accept_routematrixquery_and_return_expected_number_of_results.js
@@ -1,15 +1,15 @@
 let nock = require('nock');
 
-module.exports.hash = "5cffbe6e6329042ad57c107cbb739fa6";
+module.exports.hash = "1fd6b6a87665e467f297cf147628171e";
 
 module.exports.testInfo = {"uniqueName":{},"newDate":{}}
 
 nock('https://atlas.microsoft.com:443', {"encodedQueryParams":true})
   .post('/route/matrix/sync/json', {"origins":{"type":"MultiPoint","coordinates":[[4.85106,52.36006],[4.85056,52.36187]]},"destinations":{"type":"MultiPoint","coordinates":[[4.85003,52.36241],[13.42937,52.50931]]}})
   .query(true)
-  .reply(200, {"formatVersion":"0.0.1","matrix":[[{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":495,"travelTimeInSeconds":130,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-03-15T06:37:11+01:00","arrivalTime":"2022-03-15T06:39:20+01:00"}}},{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":674676,"travelTimeInSeconds":22155,"trafficDelayInSeconds":121,"trafficLengthInMeters":4125,"departureTime":"2022-03-15T06:37:11+01:00","arrivalTime":"2022-03-15T12:46:26+01:00"}}}],[{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":338,"travelTimeInSeconds":109,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-03-15T06:37:11+01:00","arrivalTime":"2022-03-15T06:38:59+01:00"}}},{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":674519,"travelTimeInSeconds":22111,"trafficDelayInSeconds":121,"trafficLengthInMeters":4125,"departureTime":"2022-03-15T06:37:11+01:00","arrivalTime":"2022-03-15T12:45:41+01:00"}}}]],"summary":{"successfulRoutes":4,"totalRoutes":4}}, [
+  .reply(200, {"formatVersion":"0.0.1","matrix":[[{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":495,"travelTimeInSeconds":133,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-05-01T09:30:58+02:00","arrivalTime":"2022-05-01T09:33:11+02:00"}}},{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":666389,"travelTimeInSeconds":21021,"trafficDelayInSeconds":161,"trafficLengthInMeters":13202,"departureTime":"2022-05-01T09:30:58+02:00","arrivalTime":"2022-05-01T15:21:19+02:00"}}}],[{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":338,"travelTimeInSeconds":109,"trafficDelayInSeconds":0,"trafficLengthInMeters":0,"departureTime":"2022-05-01T09:30:58+02:00","arrivalTime":"2022-05-01T09:32:47+02:00"}}},{"statusCode":200,"response":{"routeSummary":{"lengthInMeters":666232,"travelTimeInSeconds":20976,"trafficDelayInSeconds":161,"trafficLengthInMeters":13202,"departureTime":"2022-05-01T09:30:58+02:00","arrivalTime":"2022-05-01T15:20:33+02:00"}}}]],"summary":{"successfulRoutes":4,"totalRoutes":4}}, [
   'Content-Length',
-  '1045',
+  '1047',
   'Content-Type',
   'application/json; charset=utf-8',
   'Vary',
@@ -23,7 +23,7 @@ nock('https://atlas.microsoft.com:443', {"encodedQueryParams":true})
   'X-Cache',
   'CONFIG_NOCACHE',
   'X-MSEdge-Ref',
-  'Ref A: 48153C75A50847B0B552FEEF64575635 Ref B: TPE30EDGE0421 Ref C: 2022-03-15T05:37:10Z',
+  'Ref A: 19BC92B887DC4CCEA58702336F8EF463 Ref B: TPE30EDGE0508 Ref C: 2022-05-01T07:30:57Z',
   'Date',
-  'Tue, 15 Mar 2022 05:37:11 GMT'
+  'Sun, 01 May 2022 07:30:58 GMT'
 ]);
