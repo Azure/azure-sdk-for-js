9c1c04494ba4401f5ea971bcc6ebb3f9b5c4e35b
azure-sdk <azuresdk@microsoft.com>
2022-05-06 13:21:10 +0000

    Post release automated changes for azure-arm-servicefabric

--- sdk/servicefabric/arm-servicefabric/src/serviceFabricManagementClient.ts
+++ sdk/servicefabric/arm-servicefabric/src/serviceFabricManagementClient.ts
@@ -61,7 +61,7 @@ export class ServiceFabricManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-servicefabric/2.0.1`;
+    const packageDetails = `azsdk-js-arm-servicefabric/2.0.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
