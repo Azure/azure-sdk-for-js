494da3254c5bcf41b2248148fc1a4dd684ae6ab7
azure-sdk <azuresdk@microsoft.com>
2022-05-07 03:06:23 +0000

    Post release automated changes for azure-arm-streamanalytics

--- sdk/streamanalytics/arm-streamanalytics/src/streamAnalyticsManagementClient.ts
+++ sdk/streamanalytics/arm-streamanalytics/src/streamAnalyticsManagementClient.ts
@@ -65,7 +65,7 @@ export class StreamAnalyticsManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-streamanalytics/4.0.1`;
+    const packageDetails = `azsdk-js-arm-streamanalytics/4.0.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
