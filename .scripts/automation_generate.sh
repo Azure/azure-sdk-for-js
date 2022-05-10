f1693a7b228eee645284c84315e23228d68cfff7
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2022-05-10 15:11:20 +0800

    use latest codegen (#21797)

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@6.0.0-alpha.16.20220105.1
+code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@6.0.0-alpha.19.20220425.1
