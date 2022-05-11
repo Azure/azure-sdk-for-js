8f97b0de032e1b8159aea7f58e69fb24bdff49f9
hamsas <hamsas@cs.washington.edu>
2022-05-10 17:15:07 -0700

    added sample

---
+++ sdk/servicebus/service-bus/samples-dev/exceedMaxDeliveryCount.ts
@@ -0,0 +1,67 @@
+import { ServiceBusClient } from "@azure/service-bus";
+
+// Load the .env file if it exists
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Define connection string and related Service Bus entity names here
+const connectionString = process.env.SERVICEBUS_CONNECTION_STRING || "<connection string>";
+const queueName = process.env.QUEUE_NAME || "<queue name>";
+
+const sbClient: ServiceBusClient = new ServiceBusClient(connectionString);
+
+export async function main() {
+  try {
+    await exceedMaxDelivery();
+  } finally {
+    await sbClient.close();
+  }
+}
+
+async function exceedMaxDelivery() {
+  const receiver = sbClient.createReceiver(queueName);
+
+  while (true) {
+    // Ask the broker to return any message readily available or return with no
+    // result after 2 seconds (allowing for clients with great network latency)
+    var msg = await receiver.receiveMessages(1, {
+      maxWaitTimeInMs: 2 * 1000,
+    });
+    if (msg != null && msg[0] != undefined) {
+      // Now we immediately abandon the message, which increments the DeliveryCount
+      console.log("Picked up message; DeliveryCount " + msg[0].deliveryCount);
+      await receiver.abandonMessage(msg[0])
+    }
+    else {
+      // Once the system moves the message to the DLQ, the main queue is empty
+      // and the loop exits as ReceiveAsync returns null.
+      break;
+    }
+  }
+
+  // For picking up the message from a DLQ, we make a receiver just like for a
+  // regular queue.
+  const deadletterReceiver = sbClient.createReceiver(queueName, { subQueueType: "deadLetter" });
+  while (true) {
+    // receive a message
+    var msg = await deadletterReceiver.receiveMessages(1, {
+      maxWaitTimeInMs: 10 * 1000,
+    });
+    if (msg != null && msg[0] != undefined) {
+      // write out the message
+      console.log("Deadletter message: " + msg[0].body);
+
+      // complete and therefore remove the message from the DLQ
+      await deadletterReceiver.completeMessage(msg[0]);
+    }
+    else {
+      // DLQ was empty on last receive attempt
+      break;
+    }
+  }
+}
+
+main().catch((err) => {
+  console.log("Moving from DLQ Sample - Error occurred: ", err);
+  process.exit(1);
+});
