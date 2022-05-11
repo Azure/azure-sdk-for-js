4342a48c172e50dc116d41cd5187a2cf38a37cc3
azure-sdk <azuresdk@microsoft.com>
2022-05-11 09:42:42 +0000

    Post release automated changes for azure-arm-vmwarecloudsimple

--- sdk/vmwarecloudsimple/arm-vmwarecloudsimple/src/vMwareCloudSimple.ts
+++ sdk/vmwarecloudsimple/arm-vmwarecloudsimple/src/vMwareCloudSimple.ts
@@ -69,7 +69,7 @@ export class VMwareCloudSimple extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-vmwarecloudsimple/3.0.0`;
+    const packageDetails = `azsdk-js-arm-vmwarecloudsimple/3.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
