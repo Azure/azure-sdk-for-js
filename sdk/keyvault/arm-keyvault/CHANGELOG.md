d1e9c7af9ce03139507203cea5de62492b734264
SDKAuto <sdkautomation@microsoft.com>
2022-05-12 03:38:38 +0000

    CodeGen from PR 19039 in Azure/azure-rest-api-specs [Go]Keyvault add batch tag: package-2021-10 (#19039)

--- sdk/keyvault/arm-keyvault/CHANGELOG.md
+++ sdk/keyvault/arm-keyvault/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release History
     
-## 2.1.0-beta.1 (2022-04-24)
+## 2.1.0-beta.2 (2022-05-12)
     
 **Features**
 
--- sdk/keyvault/arm-keyvault/src/keyVaultManagementClient.ts
+++ sdk/keyvault/arm-keyvault/src/keyVaultManagementClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   KeysImpl,
@@ -66,7 +71,7 @@ export class KeyVaultManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-keyvault/2.1.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-keyvault/2.1.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -125,6 +130,35 @@ export class KeyVaultManagementClient extends coreClient.ServiceClient {
     this.mhsmPrivateLinkResources = new MhsmPrivateLinkResourcesImpl(this);
     this.operations = new OperationsImpl(this);
     this.secrets = new SecretsImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   keys: Keys;
