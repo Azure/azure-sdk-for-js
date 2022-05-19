79bd5aefe6d284835aebfebbc2d23e4c996cc29c
azure-sdk <azuresdk@microsoft.com>
2022-05-19 11:19:17 +0000

    Post release automated changes for azure-arm-eventgrid

--- sdk/eventgrid/arm-eventgrid/src/eventGridManagementClient.ts
+++ sdk/eventgrid/arm-eventgrid/src/eventGridManagementClient.ts
@@ -94,7 +94,7 @@ export class EventGridManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-eventgrid/14.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-eventgrid/14.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
