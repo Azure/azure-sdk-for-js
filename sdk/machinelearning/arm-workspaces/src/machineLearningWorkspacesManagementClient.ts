1e284995d4ebde7651f6220227fec5a784efc1a4
SDKAuto <sdkautomation@microsoft.com>
2022-05-25 08:43:23 +0000

    CodeGen from PR 18645 in Azure/azure-rest-api-specs [Hub Generated] Review request for Microsoft.MachineLearningServices to add version stable/2022-05-01 (#18645)
    * Adds base for updating Microsoft.MachineLearning from version stable/2019-10-01 to version 2022-05-01
    
    * Updates readme
    
    * Updates API version in new specs and examples
    
    * add 2022-05-01 mfe.json
    
    * add 2022-02-01-preview mls.json
    
    * add workspaceFeatures.json from 2022-02-01-preview
    
    * add example files
    
    * update readme
    
    * Update mfe spec
    
    - Update discriminator values from PascalCase to snake_case
    - Remove job schedules
    - Add MLTable
    
    * Add ScheduleBase types to machineLearningServices.json
    
    * Updated custom-words.txt
    
    * Update examples
    
    * Update readme
    
    * add workspace connection v2 changes
    
    * Update readme
    
    * Update readme
    
    * fix example validation error
    
    * Update mfe swagger
    
    * Updated examples
    
    * Moved 2022-05-01 specs from machinelearning to machinelearningservices
    
    * Update readme
    
    * port 2022-02-01-preview mfe.json Schedule to machineLearningServices.json
    
    * update example w/ provisioningState
    
    * Rename workspace connection related auth type
    
    * description change to try to trigger pipelines
    
    * Revert "description change to try to trigger pipelines"
    
    This reverts commit 899cb59290814f98b56bc777cf4f369eb2ed78c8.
    
    * update missed common-types v2 -> v3
    
    * Remove unnecessary Credentials properties
    
    * fix propertyBag schema
    
    * add x-ms-secret
    
    * remove checkNameAvailability api
    
    * remove traffic property from PartialOnlineEndpoint
    
    * prettier
    
    * Revert "port 2022-02-01-preview mfe.json Schedule to machineLearningServices.json"
    
    This reverts commit 4268210b92105c3c7ba89b2d45d7bcd0c56c2864.
    
    * add v1LegacyMode to workpsace properties.
    
    * update description and add default value.
    
    * update description
    
    * [MLC] label sslConfiguration and virtualMachineImage as nullable
    
    * reduce PATCH to bare minimum
    
    * update examples w/ new spec
    
    * [MLC] Mark as null-able for properties Getting API
    
    * TEST enum as x-nullable
    
    * Revert "TEST enum as x-nullable"
    
    This reverts commit 616291c334fa825ac07a8b589797478699c7f6b6.
    
    * [MLC] Mark as null-able for properties of List API
    
    * remove x-nullable from properties with defaults
    
    * description update
    
    * enum ordering
    
    * fix XmsEnumValidation error for MLC
    
    * change x-ms-enum name of SSLConfiguration
    
    * update descriptions
    
    * change x-ms-client-name 'Details' to 'Properties'
    
    * removed x-ms-client-name 'Data' suffix
    
    * reorder SecretsType enum
    
    Co-authored-by: Teddy <thtodoro@microsoft.com>
    Co-authored-by: zhaomuzhi <zhaoweixiong521@gmail.com>
    Co-authored-by: Xi Jin <jinxi@microsoft.com>
    Co-authored-by: ZhidaLiu <86350902+ZhidaLiu@users.noreply.github.com>
    Co-authored-by: Teddy Todorov <forteddyt@gmail.com>
    Co-authored-by: Bingchen Li <bingchenli@microsoft.com>

--- sdk/machinelearning/arm-workspaces/src/machineLearningWorkspacesManagementClient.ts
+++ sdk/machinelearning/arm-workspaces/src/machineLearningWorkspacesManagementClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import { OperationsImpl, WorkspacesImpl } from "./operations";
 import { Operations, Workspaces } from "./operationsInterfaces";
@@ -45,7 +50,7 @@ export class MachineLearningWorkspacesManagementClient extends coreClient.Servic
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-workspaces/1.0.2`;
+    const packageDetails = `azsdk-js-arm-workspaces/1.0.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -95,6 +100,35 @@ export class MachineLearningWorkspacesManagementClient extends coreClient.Servic
     this.apiVersion = options.apiVersion || "2019-10-01";
     this.operations = new OperationsImpl(this);
     this.workspaces = new WorkspacesImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   operations: Operations;
