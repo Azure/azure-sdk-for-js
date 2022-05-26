2d3751b6b8d40ab63c1c31daa5099fb1de74cf49
colawwj <70128817+colawwj@users.noreply.github.com>
2022-05-26 11:27:36 +0800

    containerregistry-track2-release (#21910)
    * containerregistry-track2-release
    
    * update

--- sdk/containerregistry/arm-containerregistry/samples/v10-beta/typescript/src/replicationsCreateSample.ts
+++ sdk/containerregistry/arm-containerregistry/samples/v10-beta/typescript/src/replicationsCreateSample.ts
@@ -18,7 +18,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * This sample demonstrates how to Creates a replication for a container registry with the specified parameters.
  *
  * @summary Creates a replication for a container registry with the specified parameters.
- * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2021-12-01-preview/examples/ReplicationCreate.json
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2022-02-01-preview/examples/ReplicationCreate.json
  */
 async function replicationCreate() {
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
@@ -49,7 +49,7 @@ replicationCreate().catch(console.error);
  * This sample demonstrates how to Creates a replication for a container registry with the specified parameters.
  *
  * @summary Creates a replication for a container registry with the specified parameters.
- * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2021-12-01-preview/examples/ReplicationCreateZoneRedundant.json
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2022-02-01-preview/examples/ReplicationCreateZoneRedundant.json
  */
 async function replicationCreateZoneRedundant() {
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
--- sdk/containerregistry/arm-containerregistry/src/containerRegistryManagementClient.ts
+++ sdk/containerregistry/arm-containerregistry/src/containerRegistryManagementClient.ts
@@ -7,6 +7,7 @@
  */
 
 import * as coreClient from "@azure/core-client";
+import * as coreRestPipeline from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   ConnectedRegistriesImpl,
@@ -94,6 +95,29 @@ export class ContainerRegistryManagementClient extends coreClient.ServiceClient 
         options.endpoint ?? options.baseUri ?? "https://management.azure.com"
     };
     super(optionsWithDefaults);
+
+    if (options?.pipeline && options.pipeline.getOrderedPolicies().length > 0) {
+      const pipelinePolicies: coreRestPipeline.PipelinePolicy[] = options.pipeline.getOrderedPolicies();
+      const bearerTokenAuthenticationPolicyFound = pipelinePolicies.some(
+        (pipelinePolicy) =>
+          pipelinePolicy.name ===
+          coreRestPipeline.bearerTokenAuthenticationPolicyName
+      );
+      if (!bearerTokenAuthenticationPolicyFound) {
+        this.pipeline.removePolicy({
+          name: coreRestPipeline.bearerTokenAuthenticationPolicyName
+        });
+        this.pipeline.addPolicy(
+          coreRestPipeline.bearerTokenAuthenticationPolicy({
+            scopes: `${optionsWithDefaults.baseUri}/.default`,
+            challengeCallbacks: {
+              authorizeRequestOnChallenge:
+                coreClient.authorizeRequestOnClaimChallenge
+            }
+          })
+        );
+      }
+    }
     // Parameter assignments
     this.subscriptionId = subscriptionId;
 
