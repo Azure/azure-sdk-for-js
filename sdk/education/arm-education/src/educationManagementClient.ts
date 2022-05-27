0bfe9357c10d01131d6e7b30dc397701a5e9dc04
azure-sdk <azuresdk@microsoft.com>
2022-05-27 02:42:16 +0000

    Post release automated changes for azure-arm-education

--- sdk/education/arm-education/src/educationManagementClient.ts
+++ sdk/education/arm-education/src/educationManagementClient.ts
@@ -59,7 +59,7 @@ export class EducationManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-education/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-education/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
