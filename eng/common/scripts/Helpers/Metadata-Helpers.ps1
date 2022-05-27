c4473b20f6e55e3d3177cf3e85243dec7ed51834
sima-zhu <sizhu@microsoft.com>
2022-05-26 23:38:43 -0700

    f

--- eng/common/scripts/Helpers/Metadata-Helpers.ps1
+++ eng/common/scripts/Helpers/Metadata-Helpers.ps1
@@ -102,19 +102,19 @@ function GenerateDocsMsMetadata($language, $langTitle = "", $serviceName, $tenan
   # Github url for source code: e.g. https://github.com/Azure/azure-sdk-for-js
   $serviceBaseName = $serviceName.ToLower().Replace(' ', '').Replace('/', '-')
   $author = GetPrimaryCodeOwner -TargetDirectory "/sdk/$serviceBaseName/"
-  $msauthor = "sizhu"
-  # if (!$author) {
-  #   LogError "Cannot fetch the author from CODEOWNER file."
-  #   return
-  # }
-  # elseif ($TenantId -and $ClientId -and $ClientSecret) {
-  #   $msauthor = GetMsAliasFromGithub -TenantId $tenantId -ClientId $clientId -ClientSecret $clientSecret -GithubUser $author
-  # }
-  # # Default value
-  # if (!$msauthor) {
-  #   LogError "No ms.author found for $author. "
-  #   $msauthor = $author
-  # }
+  $msauthor = ""
+  if (!$author) {
+    LogError "Cannot fetch the author from CODEOWNER file."
+    return
+  }
+  elseif ($TenantId -and $ClientId -and $ClientSecret) {
+    $msauthor = GetMsAliasFromGithub -TenantId $tenantId -ClientId $clientId -ClientSecret $clientSecret -GithubUser $author
+  }
+  # Default value
+  if (!$msauthor) {
+    LogError "No ms.author found for $author. "
+    $msauthor = $author
+  }
   $date = Get-Date -Format "MM/dd/yyyy"
   $header = @"
 ---
