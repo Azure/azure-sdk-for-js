48d26a73c8a128c1fd556f699466dd35d4c37b57
zsgwwj <v-sigangzhou@microsoft.com>
2022-05-27 10:34:29 +0800

    update

--- sdk/deviceprovisioningservices/arm-deviceprovisioningservices/src/iotDpsClient.ts
+++ sdk/deviceprovisioningservices/arm-deviceprovisioningservices/src/iotDpsClient.ts
@@ -8,11 +8,6 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
-import {
-  PipelineRequest,
-  PipelineResponse,
-  SendRequest
-} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   OperationsImpl,
@@ -109,35 +104,6 @@ export class IotDpsClient extends coreClient.ServiceClient {
     this.operations = new OperationsImpl(this);
     this.dpsCertificate = new DpsCertificateImpl(this);
     this.iotDpsResource = new IotDpsResourceImpl(this);
-    this.addCustomApiVersionPolicy(options.apiVersion);
-  }
-
-  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
-  private addCustomApiVersionPolicy(apiVersion?: string) {
-    if (!apiVersion) {
-      return;
-    }
-    const apiVersionPolicy = {
-      name: "CustomApiVersionPolicy",
-      async sendRequest(
-        request: PipelineRequest,
-        next: SendRequest
-      ): Promise<PipelineResponse> {
-        const param = request.url.split("?");
-        if (param.length > 1) {
-          const newParams = param[1].split("&").map((item) => {
-            if (item.indexOf("api-version") > -1) {
-              return item.replace(/(?<==).*$/, apiVersion);
-            } else {
-              return item;
-            }
-          });
-          request.url = param[0] + "?" + newParams.join("&");
-        }
-        return next(request);
-      }
-    };
-    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   operations: Operations;
