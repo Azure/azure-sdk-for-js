b0edae835eced58f7b7fc8ebf17efff2a9af5d51
Karishma Ghiya <kaghiya@microsoft.com>
2022-05-31 15:36:36 -0700

    generated client

---
+++ sdk/monitor/data-collection/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ * 
+ */
+export { GeneratedDataCollectionClient as DataCollectionClient }  from "./generated";
+export { GeneratedDataCollectionClientOptionalParams as DataCollectionClientOptions } from "./generated";
+export { DataCollectionRule } from "./generated";
