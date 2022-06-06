26437c8e4fa07c62cd4cb03dd01683f6ec6481f3
Jeremy Meng <yumeng@microsoft.com>
2022-06-06 19:04:37 +0000

    use `any` as the parameter type
    as in this test we save and restore the original `clearTimeout` function and
    don't care about the parameter type.

--- sdk/core/core-amqp/test/requestResponse.spec.ts
+++ sdk/core/core-amqp/test/requestResponse.spec.ts
@@ -604,7 +604,7 @@ describe("RequestResponseLink", function () {
 
     beforeEach(() => {
       clearTimeoutCalledCount = 0;
-      _global.clearTimeout = (tid: NodeJS.Timeout | string | number | undefined) => {
+      _global.clearTimeout = (tid: any) => {
         clearTimeoutCalledCount++;
         return originalClearTimeout(tid);
       };
