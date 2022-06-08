f36d6b5ce3dd027bd5a4068b29ddb6a60adec051
KarishmaGhiya <kghiya8@gmail.com>
2022-06-08 16:45:00 -0700

    [Monitor Query] move to tracing 1.0.0 (#21260)

--- sdk/monitor/monitor-query/test/internal/unit/logsQueryClient.unittest.spec.ts
+++ sdk/monitor/monitor-query/test/internal/unit/logsQueryClient.unittest.spec.ts
@@ -3,31 +3,31 @@
 
 import { Durations, LogsQueryClient } from "../../../src";
 import { AccessToken, GetTokenOptions, TokenCredential } from "@azure/core-auth";
-import { assert } from "chai";
+import { assert } from "@azure/test-utils";
 
 describe("LogsQueryClient unit tests", () => {
   /**
    * Custom scopes and endpoints are needed if you're connecting to
    * a government cloud, for instance.
    */
+  const scopesPassed: string[] = [];
+
+  const tokenCredential: TokenCredential = {
+    async getToken(
+      scopes: string | string[],
+      _options?: GetTokenOptions
+    ): Promise<AccessToken | null> {
+      if (Array.isArray(scopes)) {
+        scopesPassed.push(...scopes);
+      } else {
+        scopesPassed.push(scopes);
+      }
+
+      throw new Error("Shortcircuit auth exception");
+    },
+  };
+
   it("using custom scopes and endpoints", async () => {
-    const scopesPassed: string[] = [];
-
-    const tokenCredential: TokenCredential = {
-      async getToken(
-        scopes: string | string[],
-        _options?: GetTokenOptions
-      ): Promise<AccessToken | null> {
-        if (Array.isArray(scopes)) {
-          scopesPassed.push(...scopes);
-        } else {
-          scopesPassed.push(scopes);
-        }
-
-        throw new Error("Shortcircuit auth exception");
-      },
-    };
-
     const client = new LogsQueryClient(tokenCredential, {
       endpoint: "https://customEndpoint1",
     });
@@ -43,5 +43,36 @@ describe("LogsQueryClient unit tests", () => {
         message: "Shortcircuit auth exception",
       });
     }
+  });
+
+  it("verify tracing", async () => {
+    const client = new LogsQueryClient(tokenCredential, {
+      endpoint: "https://customEndpoint1",
+    });
+    await assert.supportsTracing(
+      async (options) => {
+        const promises: Promise<any>[] = [
+          client.queryWorkspace(
+            "workspaceId",
+            "query",
+            { duration: Durations.fiveMinutes },
+            options
+          ),
+          client.queryBatch(
+            [
+              {
+                workspaceId: "monitorWorkspaceId",
+                query: "AppEvents | project TimeGenerated, Name, AppRoleInstance | limit 1",
+                timespan: { duration: "P1D" },
+              },
+            ],
+            options
+          ),
+        ];
+        // We don't care about errors, only that we created (and closed) the appropriate spans.
+        await Promise.all(promises.map((p) => p.catch(() => undefined)));
+      },
+      ["LogsQueryClient.queryWorkspace", "LogsQueryClient.queryBatch"]
+    );
   });
 });
