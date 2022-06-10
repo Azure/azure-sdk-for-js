a70e6d87d01f34ec84a772578542fad1c7dd6729
SDKAuto <sdkautomation@microsoft.com>
2022-06-10 07:00:41 +0000

    CodeGen from PR 19389 in Azure/azure-rest-api-specs [Go] Add go track2 config (#19389)
    * [Go] Add go track2 config
    
    * add powerplatform multi-api
    
    * other
    
    * fix
    
    * fix
    
    * cancel devspace
    
    * cancel iotspaces

--- sdk/customer-insights/arm-customerinsights/src/customerInsightsManagementClient.ts
+++ sdk/customer-insights/arm-customerinsights/src/customerInsightsManagementClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   OperationsImpl,
@@ -82,7 +87,7 @@ export class CustomerInsightsManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-customerinsights/4.0.2`;
+    const packageDetails = `azsdk-js-arm-customerinsights/4.0.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -147,6 +152,35 @@ export class CustomerInsightsManagementClient extends coreClient.ServiceClient {
     this.roleAssignments = new RoleAssignmentsImpl(this);
     this.images = new ImagesImpl(this);
     this.predictions = new PredictionsImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   operations: Operations;
