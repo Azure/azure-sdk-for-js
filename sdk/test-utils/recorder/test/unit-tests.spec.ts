85a3d4890372dce78cd81be662925db5c7f9487e
Harsha Nalluru <sanallur@microsoft.com>
2022-06-15 21:42:24 +0000

    handleTestProxyErrors test

---
+++ sdk/test-utils/recorder/test/unit-tests.spec.ts
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { RecorderError, RecordingStateManager } from "../src/utils/utils";
+import { expect } from "chai";
+import { Recorder } from "../src/recorder";
+import { createHttpHeaders, createPipelineRequest } from "@azure/core-rest-pipeline";
+
+describe("State Manager", function () {
+  it("throws error if started twice", function () {
+    const manager = new RecordingStateManager();
+    manager.state = "started";
+    try {
+      manager.state = "started";
+      throw new Error("should not have reached here, previous assignment should have failed");
+    } catch (error: any) {
+      expect((error as RecorderError).name).to.equal("RecorderError");
+      expect((error as RecorderError).message).to.equal(
+        "Already started, should not have called start again."
+      );
+    }
+  });
+
+  it("throws error if stopped twice", function () {
+    const manager = new RecordingStateManager();
+    try {
+      manager.state = "stopped";
+      throw new Error("should not have reached here, previous assignment should have failed");
+    } catch (error: any) {
+      expect((error as RecorderError).name).to.equal("RecorderError");
+      expect((error as RecorderError).message).to.equal(
+        "Already stopped, should not have called stop again."
+      );
+    }
+  });
+});
+
+describe("handleTestProxyErrors", function () {
+  let recorder: Recorder;
+  beforeEach(function () {
+    recorder = new Recorder(this.currentTest);
+  });
+
+  it("x-request-mismatch header", function () {
+    const headers = createHttpHeaders();
+    headers.set("x-request-mismatch", true);
+    headers.set("x-request-mismatch-error", btoa("this is a mismatch error"));
+    recorder["handleTestProxyErrors"]({
+      request: createPipelineRequest({ url: "abcd" }),
+      status: 401,
+      headers,
+    });
+  });
+});
