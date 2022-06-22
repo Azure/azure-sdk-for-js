78042b9def3f8a0a84bb8d475453da8e8219054b
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-06-22 10:44:47 +0800

    update labservices package (#22293)
    * update labservices package
    
    * update labservices package
    
    Co-authored-by: ZiWei Chen (WICRESOFT NORTH AMERICA LTD) <v-ziweichen@microsoft.com>

--- sdk/labservices/arm-labservices/src/labServicesClient.ts
+++ sdk/labservices/arm-labservices/src/labServicesClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   ImagesImpl,
@@ -67,7 +72,7 @@ export class LabServicesClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-labservices/3.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-labservices/3.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -125,6 +130,35 @@ export class LabServicesClient extends coreClient.ServiceClient {
     this.virtualMachines = new VirtualMachinesImpl(this);
     this.usages = new UsagesImpl(this);
     this.skus = new SkusImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   images: Images;
