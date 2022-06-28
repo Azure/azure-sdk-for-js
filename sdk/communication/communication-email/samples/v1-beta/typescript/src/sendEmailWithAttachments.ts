a7880f072bc3d6295358bd0d22034d74fdade9f8
Yogesh Mohanraj <ymohanraj@microsoft.com>
2022-06-28 15:22:22 -0700

    Adding Email SDK updates

--- sdk/communication/communication-email/samples/v1-beta/typescript/src/sendEmailWithAttachments.ts
+++ sdk/communication/communication-email/samples/v1-beta/typescript/src/sendEmailWithAttachments.ts
@@ -5,7 +5,7 @@
  * @summary Sends an email with a txt file attachment
  */
 
-import { EmailClient, EmailMessage } from "@azure/communication-email"
+import { EmailClient, EmailMessage } from "@azure/communication-email";
 
 // Load the .env file (you will need to set these environment variables)
 import * as dotenv from "dotenv";
@@ -29,10 +29,7 @@ const sendEmailWithAttachments = async (): Promise<void> => {
     },
     recipients: {
       to: [
-        {
-          email: recipientAddress,
-          displayName: "Customer Name",
-        },
+        { email: recipientAddress, displayName: "Customer Name" },
       ],
     },
     attachments: [
