cd4652c76dba912de9a09512776ded79cfc6cc6a
Mary Gao <yanmeigao1210@gmail.com>
2022-07-04 12:32:08 +0800

    Do not append customized api-version if url already has one e.g LRO/paging URL returned by the service

--- sdk/core/core-client-rest/src/apiVersionPolicy.ts
+++ sdk/core/core-client-rest/src/apiVersionPolicy.ts
@@ -17,7 +17,10 @@ export function apiVersionPolicy(options: ClientOptions): PipelinePolicy {
     sendRequest: (req, next) => {
       if (options.apiVersion) {
         const url = new URL(req.url);
-        url.searchParams.append("api-version", options.apiVersion);
+        // Do not append customized api-version if url already has one e.g LRO/paging URL returned by the service 
+        if(!url.searchParams.has("api-version")) {
+          url.searchParams.append("api-version", options.apiVersion);
+        }
         req.url = url.toString();
       }
 
