05b10df86b0a50f5a64f3692cb4f082ea17093a8
ZiWei Chen (WICRESOFT NORTH AMERICA LTD) <v-ziweichen@microsoft.com>
2022-07-05 13:45:21 +0800

    refresh storageimportexport package

--- sdk/storageimportexport/arm-storageimportexport/CHANGELOG.md
+++ sdk/storageimportexport/arm-storageimportexport/CHANGELOG.md
@@ -1,14 +1,10 @@
 # Release History
 
-## 2.0.2 (Unreleased)
+## 2.0.2 (2022-07-05)
 
-### Features Added
+**Features**
 
-### Breaking Changes
-
-### Bugs Fixed
-
-### Other Changes
+  - Bug fix
 
 ## 2.0.1 (2022-05-01)
 
--- sdk/storageimportexport/arm-storageimportexport/src/storageImportExport.ts
+++ sdk/storageimportexport/arm-storageimportexport/src/storageImportExport.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   LocationsImpl,
@@ -62,9 +67,6 @@ export class StorageImportExport extends coreClient.ServiceClient {
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
         : `${packageDetails}`;
 
-    if (!options.credentialScopes) {
-      options.credentialScopes = ["https://management.azure.com/.default"];
-    }
     const optionsWithDefaults = {
       ...defaults,
       ...options,
@@ -108,6 +110,35 @@ export class StorageImportExport extends coreClient.ServiceClient {
     this.jobs = new JobsImpl(this);
     this.bitLockerKeys = new BitLockerKeysImpl(this);
     this.operations = new OperationsImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return "api-version=" + apiVersion;
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   locations: Locations;
