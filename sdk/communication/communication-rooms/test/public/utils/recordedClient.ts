014d6fc9c6d94844cda74cf6a3fd027b7bbb7263
Calvin Tang <calvintang@microsoft.com>
2022-07-05 14:49:25 -0700

    Rooms RecordedClient

--- sdk/communication/communication-rooms/test/public/utils/recordedClient.ts
+++ sdk/communication/communication-rooms/test/public/utils/recordedClient.ts
@@ -5,13 +5,17 @@ import {
   Recorder,
   SanitizerOptions,
   env,
-  RecorderStartOptions
+  RecorderStartOptions,
+  isPlaybackMode
 } from "@azure-tools/test-recorder";
-import { Test } from "mocha";
-import { RoomsClient } from "../../../src"
+import { TokenCredential } from "@azure/core-auth";
+import { parseConnectionString } from "@azure/communication-common";
+import { createTestCredential } from "@azure-tools/test-credential";
+import { Context, Test } from "mocha";
+import { RoomsClient } from "../../../src";
 
-export interface RecordedRoomsClient {
-  client: RoomsClient;
+export interface RecordedClient<T> {
+  client: T;
   recorder: Recorder;
 }
 
@@ -37,4 +41,46 @@ export async function createRecorder(context: Test | undefined): Promise<Recorde
   const recorder = new Recorder(context);
   await recorder.start(recorderOptions);
   return recorder;
+}
+
+export async function createRecordedRoomsClient(
+  context: Context
+): Promise<RecordedClient<RoomsClient>> {
+  const recorder = await createRecorder(context.currentTest);
+
+  const client = new RoomsClient(
+    env.COMMUNICATION_LIVETEST_DYNAMIC_CONNECTION_STRING ?? "",
+    recorder.configureClientOptions({})
+  );
+  return {
+    client,
+    recorder,
+  }
+}
+
+export async function createRecordedRoomsClientWithToken(
+  context: Context
+): Promise<RecordedClient<RoomsClient>> {
+  const recorder = await createRecorder(context.currentTest);
+
+  let credential: TokenCredential;
+  const endpoint = parseConnectionString(
+    env.COMMUNICATION_LIVETEST_DYNAMIC_CONNECTION_STRING ?? ""
+  ).endpoint;
+
+  if (isPlaybackMode()) {
+    credential = {
+      getToken: async (_scopes: any) => {
+        return { token: "testToken", expiresOnTimestamp: 11111 };
+      },
+    };
+  } else {
+    credential = createTestCredential();
+  }
+
+  const client = new RoomsClient(endpoint, credential, recorder.configureClientOptions({}));
+  return {
+    client,
+    recorder,
+  }
 }
