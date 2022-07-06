1994730741d3024bd9d1d8b37d92b4c6c830b81d
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-07-06 14:14:03 +0800

    modify links in sdk repo (#22464)
    Co-authored-by: ZiWei Chen (WICRESOFT NORTH AMERICA LTD) <v-ziweichen@microsoft.com>

--- sdk/databricks/arm-databricks/samples/v3-beta/javascript/outboundNetworkDependenciesEndpointsListSample.js
+++ sdk/databricks/arm-databricks/samples/v3-beta/javascript/outboundNetworkDependenciesEndpointsListSample.js
@@ -12,9 +12,9 @@ const { AzureDatabricksManagementClient } = require("@azure/arm-databricks");
 const { DefaultAzureCredential } = require("@azure/identity");
 
 /**
- * This sample demonstrates how to Gets the list of endpoints that VNET Injected Workspace calls Azure Databricks Control Plane. You must configure outbound access with these endpoints. For more information, see https://docs.microsoft.com/en-us/azure/databricks/administration-guide/cloud-configurations/azure/udr
+ * This sample demonstrates how to Gets the list of endpoints that VNET Injected Workspace calls Azure Databricks Control Plane. You must configure outbound access with these endpoints. For more information, see https://docs.microsoft.com/azure/databricks/administration-guide/cloud-configurations/azure/udr
  *
- * @summary Gets the list of endpoints that VNET Injected Workspace calls Azure Databricks Control Plane. You must configure outbound access with these endpoints. For more information, see https://docs.microsoft.com/en-us/azure/databricks/administration-guide/cloud-configurations/azure/udr
+ * @summary Gets the list of endpoints that VNET Injected Workspace calls Azure Databricks Control Plane. You must configure outbound access with these endpoints. For more information, see https://docs.microsoft.com/azure/databricks/administration-guide/cloud-configurations/azure/udr
  * x-ms-original-file: specification/databricks/resource-manager/Microsoft.Databricks/preview/2021-04-01-preview/examples/OutboundNetworkDependenciesEndpointsList.json
  */
 async function listOutboundNetworkDependenciesEndpointsByWorkspace() {
