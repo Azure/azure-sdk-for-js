c7187201ebc3555986f59ded0b25611005eb0a21
Calvin Tang <calvintang@microsoft.com>
2022-07-05 17:03:43 -0700

    Initial Test Commit

--- sdk/communication/communication-rooms/test/public/roomsClient.spec.ts
+++ sdk/communication/communication-rooms/test/public/roomsClient.spec.ts
@@ -1,2 +1,38 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+
+import { Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
+import { createRecordedRoomsClient } from "./utils/recordedClient";
+import { assert } from "chai";
+import { Context } from "mocha";
+import sinon from "sinon";
+import { RoomsClient } from "../../src/roomsClient";
+
+describe("RoomsClient", function () {
+  let recorder: Recorder;
+  let client: RoomsClient;
+
+  describe("Room Operations", function () {
+    beforeEach(async function (this: Context) {
+      ({ client, recorder } = await createRecordedRoomsClient(this));
+    });
+
+    afterEach(async function () {
+      if (!this.currentTest?.isPending()) {
+        await recorder.stop();
+      }
+      if (isPlaybackMode()) {
+        sinon.restore();
+      }
+    });
+
+    it("successfully creates a room", async function () {
+      const request = {};
+      const createRoomResult = await client
+        .createRoom(request)
+        .catch((error) => console.error(error));
+
+      assert.isDefined(createRoomResult);
+    });
+  });
+});
