ad2941d915e87e253f94f84e76b8160c5240e385
azure-sdk <azuresdk@microsoft.com>
2022-07-08 08:51:28 +0000

    Post release automated changes for azure-arm-marketplaceordering

--- sdk/marketplaceordering/arm-marketplaceordering/src/marketplaceOrderingAgreements.ts
+++ sdk/marketplaceordering/arm-marketplaceordering/src/marketplaceOrderingAgreements.ts
@@ -50,7 +50,7 @@ export class MarketplaceOrderingAgreements extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-marketplaceordering/3.1.0`;
+    const packageDetails = `azsdk-js-arm-marketplaceordering/3.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
