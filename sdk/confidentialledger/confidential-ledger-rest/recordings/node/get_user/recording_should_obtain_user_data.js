537552a118873a3c81bf6e588673cbfc89d4c79d
Emily Louden <51801155+deer-prudence@users.noreply.github.com>
2022-07-11 21:13:05 +0000

    test update

--- sdk/confidentialledger/confidential-ledger-rest/recordings/node/get_user/recording_should_obtain_user_data.js
+++ sdk/confidentialledger/confidential-ledger-rest/recordings/node/get_user/recording_should_obtain_user_data.js
@@ -8,7 +8,7 @@ nock('https://identity.confidential-ledger.core.azure.com:443', {"encodedQueryPa
   .get('/ledgerIdentity/FAKE_CERT')
   .reply(200, {"ledgerTlsCertificate":"-----BEGIN CERTIFICATE-----\nMIIBejCCASCgAwIBAgIQMNwF270tS2Ex6jsW6jP46TAKBggqhkjOPQQDAjAWMRQw\nEgYDVQQDDAtDQ0YgTmV0d29yazAeFw0yMjA3MDYyMTA0NDBaFw0yMjEwMDQyMTA0\nMzlaMBYxFDASBgNVBAMMC0NDRiBOZXR3b3JrMFkwEwYHKoZIzj0CAQYIKoZIzj0D\nAQcDQgAEDUWr/JYiEUnNS+4Ndfcci6yGRXhVWSnabgvShqrdxW4RBmsKZ+qsAWJP\nnavsVjf8Zgd8gghMm1y4Zl4PoHzTxKNQME4wDAYDVR0TBAUwAwEB/zAdBgNVHQ4E\nFgQUiIlVb/2YkHp4mXRhBuLaadG82zYwHwYDVR0jBBgwFoAUiIlVb/2YkHp4mXRh\nBuLaadG82zYwCgYIKoZIzj0EAwIDSAAwRQIgfYFw63rQ8RrH0BBs6yWbYbm+OWCq\nwyWR8oAT90gwHtACIQDNJ3eIewMJNDtUSJaRYhOIOu10evuW63wBLP/kftLAmw==\n-----END CERTIFICATE-----\n","ledgerId":"FAKE_CERT"}, [
   'Date',
-  'Mon, 11 Jul 2022 20:28:41 GMT',
+  'Mon, 11 Jul 2022 21:12:39 GMT',
   'Content-Type',
   'application/json; charset=utf-8',
   'Server',
@@ -16,11 +16,11 @@ nock('https://identity.confidential-ledger.core.azure.com:443', {"encodedQueryPa
   'Transfer-Encoding',
   'chunked',
   'x-ms-correlation-request-id',
-  'b4b8eea4-cc39-47bf-a6b1-ff3a1cd9bcdc',
+  '421454e9-d972-40f9-a1d7-1aaa3975dc04',
   'x-ms-client-request-id',
-  '5d6ce643-c031-472f-8749-08929f161f02',
+  '2dffc8c8-66b6-4ced-ac94-fd15803ff963',
   'x-ms-machineName',
-  'identityservice-555869b65b-t46r5',
+  'identityservice-555869b65b-9pfc9',
   'x-ms-image-digest',
   'sha256:882aaa027c6a13390418cc070e29736d7255c85700ea0a3bde9eb7f85430c4b8',
   'x-ms-image-tag',
@@ -28,17 +28,17 @@ nock('https://identity.confidential-ledger.core.azure.com:443', {"encodedQueryPa
 ]);
 
 nock('https://endpoint', {"encodedQueryParams":true})
-  .get('/app/users/ec667af1-0642-45f0-be8a-b76758a35dde')
+  .get('/app/users/00000000-0000-0000-0000-000000000000')
   .query(true)
-  .reply(200, {"assignedRole":"Administrator","userId":"ec667af1-0642-45f0-be8a-b76758a35dde"}, [
+  .reply(200, {"assignedRole":"Administrator","userId":"00000000-0000-0000-0000-000000000000"}, [
   'content-length',
   '80',
   'content-type',
   'application/json',
   'x-ms-ccf-transaction-id',
-  '2.16777',
+  '2.16779',
   'x-ms-client-request-id',
-  'ccc26df1-174e-40af-8738-86bc42404f8b',
+  '037049f4-6ec7-4978-844d-4aa60e1bb8d7',
   'x-ms-request-id',
-  '121609897'
+  '337336436'
 ]);
