dc5cf718b9db7a8d49080f7543779a6c5500caec
Jan Mach <janmach1@microsoft.com>
2022-07-12 19:28:26 +0200

    sdk-type + @packageDocumentation updated

--- sdk/apimanagement/api-management-custom-widgets-scaffolder/src/index.ts
+++ sdk/apimanagement/api-management-custom-widgets-scaffolder/src/index.ts
@@ -2,13 +2,7 @@
 // Licensed under the MIT license.
 
 /**
- * This is the ModelsRepositoryClient Library for JavaScript.
- *
- * @remarks
- * TODO - add description
- * @example
- * TODO - add example fo how this API will be used programmatically
- * @packageDocumentation
+ * @packageDocumentation https://aka.ms/apimdocs/portal/customwidgets
  */
 
 export {
