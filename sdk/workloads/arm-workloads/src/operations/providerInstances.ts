b2417e89aa9b464afa7de60702495059ce9e3ee5
ZiWei Chen (WICRESOFT NORTH AMERICA LTD) <v-ziweichen@microsoft.com>
2022-07-14 10:07:36 +0800

    update files

--- sdk/workloads/arm-workloads/src/operations/providerInstances.ts
+++ sdk/workloads/arm-workloads/src/operations/providerInstances.ts
@@ -212,8 +212,7 @@ export class ProviderInstancesImpl implements ProviderInstances {
     );
     const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
-      intervalInMs: options?.updateIntervalInMs,
-      lroResourceLocationConfig: "azure-async-operation"
+      intervalInMs: options?.updateIntervalInMs
     });
     await poller.poll();
     return poller;
