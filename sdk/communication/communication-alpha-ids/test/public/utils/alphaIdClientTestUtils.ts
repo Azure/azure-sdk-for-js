761f018d30e19c9390bd48f8b31dcd042a11c841
Guilherme Luiz <guilhermeluiz.sp@outlook.com>
2022-07-18 16:38:08 -0300

    Add log when api errors out

--- sdk/communication/communication-alpha-ids/test/public/utils/alphaIdClientTestUtils.ts
+++ sdk/communication/communication-alpha-ids/test/public/utils/alphaIdClientTestUtils.ts
@@ -13,6 +13,7 @@ export async function ignoreSubscriptionNotEligibleError(
     assert.isOk(configuration);
   } catch (error) {
     if (error instanceof RestError) {
+      console.log(error.response?.bodyAsText);
       if (
         error?.statusCode === 403 &&
         error?.response?.bodyAsText?.includes("is not eligible for Alpha IDs usage")
