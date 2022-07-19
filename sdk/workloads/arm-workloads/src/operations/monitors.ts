2943cae8c25b7a947625d5122c5338ce0eb4482f
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-07-19 17:37:54 +0800

    release workloads package (#22563)
    * release workloads package
    
    * update files
    
    * update files
    
    * update files
    
    * update files
    
    * update files
    
    * update files
    
    * update file
    
    * update files
    
    * update files
    
    Co-authored-by: ZiWei Chen (WICRESOFT NORTH AMERICA LTD) <v-ziweichen@microsoft.com>

--- sdk/workloads/arm-workloads/src/operations/monitors.ts
+++ sdk/workloads/arm-workloads/src/operations/monitors.ts
@@ -251,8 +251,7 @@ export class MonitorsImpl implements Monitors {
     );
     const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
-      intervalInMs: options?.updateIntervalInMs,
-      lroResourceLocationConfig: "azure-async-operation"
+      intervalInMs: options?.updateIntervalInMs
     });
     await poller.poll();
     return poller;
