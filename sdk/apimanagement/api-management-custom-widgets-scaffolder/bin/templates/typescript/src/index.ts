6b5f28031dbe9049b86ec4e554f8afad976e665c
Jan Mach <janmach1@microsoft.com>
2022-07-19 17:04:59 +0200

    bin regenerated

--- sdk/apimanagement/api-management-custom-widgets-scaffolder/bin/templates/typescript/src/index.ts
+++ sdk/apimanagement/api-management-custom-widgets-scaffolder/bin/templates/typescript/src/index.ts
@@ -1,18 +1,33 @@
-import {getEditorData} from "@azure/api-management-custom-widgets-tools"
+import {askForSecrets, getEditorData, TSecrets} from "@azure/api-management-custom-widgets-tools"
 import {valuesDefault} from "./values"
 
-function initialize() {
-  const editorData = getEditorData(valuesDefault)
+class app {
+  private readonly editorData
+  private secrets: TSecrets | undefined
 
-  Object.entries(editorData).forEach(([key, value]) => {
-    const element = document.getElementById(key)
-    if (element) element.innerText = (value ?? "").toString()
-  })
+  constructor() {
+    this.editorData = getEditorData(valuesDefault)
 
-  Object.entries(editorData.values).forEach(([key, value]) => {
-    const element = document.getElementById(key)
-    if (element) element.innerText = value
-  })
+    Object.entries(this.editorData).forEach(([key, value]) => {
+      const element = document.getElementById(key)
+      if (element) element.innerText = (value ?? "").toString()
+    })
+
+    Object.entries(this.editorData.values).forEach(([key, value]) => {
+      const element = document.getElementById(key)
+      if (element) element.innerText = value
+    })
+
+    askForSecrets("app", this.editorData)
+      .then(secrets => this.secrets = secrets)
+      .catch(e => console.error("Failed to retrieve secrets from the Developer Portal. The app might not work as expected!", e))
+  }
+
+  public async sendData(data: Record<string, unknown>) {
+    if (!this.secrets) throw new Error()
+
+    console.log({data, authorization: this.secrets})
+  }
 }
 
-export default initialize
+export default app
