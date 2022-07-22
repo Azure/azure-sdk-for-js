2c144874baff2ebf9263eee94c6e376f4e2aa658
Matthew Podwysocki <matthewp@microsoft.com>
2022-07-22 13:11:18 -0400

    Updating to module for template

--- sdk/template/template/src/index.ts
+++ sdk/template/template/src/index.ts
@@ -3,4 +3,12 @@
 
 export * from "./configurationClient.js";
 
-//import { Installation } from "@azure/notification-hubs/models/installation";
+import { AppleInstallation } from "@azure/notification-hubs/models/installation";
+
+const x: AppleInstallation = {
+  platform: "apns",
+  pushChannel: "abc",
+  installationId: "abc",
+};
+
+console.log(x);
