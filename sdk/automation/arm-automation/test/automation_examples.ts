0c02a6201c7aa3c525c833823a4536239b07e51c
ZiWei Chen (WICRESOFT NORTH AMERICA LTD) <v-ziweichen@microsoft.com>
2022-07-28 13:49:07 +0800

    update files

--- sdk/automation/arm-automation/test/automation_examples.ts
+++ sdk/automation/arm-automation/test/automation_examples.ts
@@ -36,7 +36,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe("ContainerInstance test", () => {
+describe("Automation test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: AutomationClient;
