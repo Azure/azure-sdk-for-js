5f8e1b04aba8b9e13fc59ed01455ebdb2ba8182b
Fabian Meiswinkel <fabian@meiswinkel.com>
2022-07-29 19:35:49 +0000

    Fixing issues for containers and databases with ids with white spaces or special characters

--- sdk/cosmosdb/cosmos/src/common/helper.ts
+++ sdk/cosmosdb/cosmos/src/common/helper.ts
@@ -5,7 +5,7 @@ import { OperationType, ResourceType } from "./constants";
 
 const trimLeftSlashes = new RegExp("^[/]+");
 const trimRightSlashes = new RegExp("[/]+$");
-const illegalResourceIdCharacters = new RegExp("[/\\\\?#]");
+const illegalResourceIdCharacters = new RegExp("[/\\\\?#]"); 
 
 /** @hidden */
 export function jsonStringifyAndEscapeNonASCII(arg: unknown): string {
@@ -187,6 +187,35 @@ export function parsePath(path: string): string[] {
  * @hidden
  */
 export function isResourceValid(resource: { id?: string }, err: { message?: string }): boolean {
+  // TODO: fix strictness issues so that caller contexts respects the types of the functions
+  if (resource.id) {
+    if (typeof resource.id !== "string") {
+      err.message = "Id must be a string.";
+      return false;
+    }
+
+    if (
+      resource.id.indexOf("/") !== -1 ||
+      resource.id.indexOf("\\") !== -1 ||
+      resource.id.indexOf("?") !== -1 ||
+      resource.id.indexOf("#") !== -1
+    ) {
+      err.message = "Id contains illegal chars.";
+      return false;
+    }
+
+    if (resource.id[resource.id.length - 1] === " ") {
+      err.message = "Id ends with a space.";
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * @hidden
+ */
+ export function isItemResourceValid(resource: { id?: string }, err: { message?: string }): boolean {
   // TODO: fix strictness issues so that caller contexts respects the types of the functions
   if (resource.id) {
     if (typeof resource.id !== "string") {
