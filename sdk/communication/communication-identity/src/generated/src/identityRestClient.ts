3a6eb9ba8585d38453b2450bb368386c80d32808
Petr Švihlík <petrsvihlik@microsoft.com>
2022-08-11 10:06:42 +0200

    Update sdk/communication/communication-identity/src/generated/src/identityRestClient.ts

--- sdk/communication/communication-identity/src/generated/src/identityRestClient.ts
+++ sdk/communication/communication-identity/src/generated/src/identityRestClient.ts
@@ -38,7 +38,7 @@ export class IdentityRestClient extends coreClient.ServiceClient {
       requestContentType: "application/json; charset=utf-8"
     };
 
-    const packageDetails = `azsdk-js-communication-identity/1.1.2`;
+    const packageDetails = `azsdk-js-communication-identity/1.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
