44215061b688a5e7ec81391ccea14b2f75723f92
Sharath Malladi <msharath@live.com>
2022-08-16 13:50:26 -0700

    Accepting linter corrections

---
+++ sdk/personalizer/ai-personalizer-rest/test/public/multiSlotEventTests.spec.ts
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { Recorder } from "@azure-tools/test-recorder";
+import { createRecorder } from "./utils/recordedClient";
+import { Context } from "mocha";
+import Personalizer, { GeneratedClient } from "../../src";
+import { env } from "process";
+import { assert } from "chai";
+import { enableMultiSlotAsync, isMultiSlotEnabledAsync } from "./helpers";
+
+describe("Multi-Slot Event Tests", () => {
+  let recorder: Recorder;
+  let client: GeneratedClient;
+
+  before(async function (this: Context) {
+    client = Personalizer(env["PERSONALIZER_ENDPOINT_MULTI_SLOT"] ?? "", {
+      key: env["PERSONALIZER_API_KEY_MULTI_SLOT"] ?? "",
+    });
+    if (!(await isMultiSlotEnabledAsync(client))) {
+      await enableMultiSlotAsync(client);
+    }
+  });
+
+  beforeEach(async function (this: Context) {
+    recorder = await createRecorder(this);
+  });
+
+  afterEach(async function () {
+    await recorder.stop();
+  });
+
+  it("multi-slot activate Test", async function () {
+    const eventId: string = "123456789";
+    const response = await client.path("/multislot/events/{eventId}/activate", eventId).post();
+    assert.equal(response.status, "204");
+  });
+
+  it("multi-slot reward Test", async function () {
+    const eventId: string = "123456789";
+    const response = await client
+      .path("/multislot/events/{eventId}/reward", eventId)
+      .post({ body: { reward: [{ slotId: "Main Article", value: 1 }] } });
+    assert.equal(response.status, "204");
+  });
+});
---
+++ sdk/personalizer/ai-personalizer-rest/test/public/multiSlotRankTests.spec.ts
@@ -0,0 +1,107 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { Recorder } from "@azure-tools/test-recorder";
+import { createRecorder } from "./utils/recordedClient";
+import { Context } from "mocha";
+import Personalizer, {
+  GeneratedClient,
+  MultiSlotRankResponseOutput,
+  RankableAction,
+  SlotRequest,
+  SlotResponseOutput,
+} from "../../src";
+import { env } from "process";
+import { assert } from "chai";
+import { enableMultiSlotAsync, isMultiSlotEnabledAsync } from "./helpers";
+
+describe("Multi-Slot Rank Tests", () => {
+  let recorder: Recorder;
+  let client: GeneratedClient;
+
+  before(async function (this: Context) {
+    client = Personalizer(env["PERSONALIZER_ENDPOINT_MULTI_SLOT"] ?? "", {
+      key: env["PERSONALIZER_API_KEY_MULTI_SLOT"] ?? "",
+    });
+    if (!(await isMultiSlotEnabledAsync(client))) {
+      await enableMultiSlotAsync(client);
+    }
+  });
+
+  beforeEach(async function (this: Context) {
+    recorder = await createRecorder(this);
+  });
+
+  afterEach(async function () {
+    await recorder.stop();
+  });
+
+  it("rank with no context features", async function () {
+    const actions = getActions();
+    const slots = getSlots();
+    const eventId = "123456789";
+    const response = await client
+      .path("/multislot/rank")
+      .post({ body: { actions: actions, slots: slots, eventId: eventId } });
+    assert.equal(response.status, "201");
+    const responseBody = response.body as MultiSlotRankResponseOutput;
+    const responseSlots = responseBody.slots as Array<SlotResponseOutput>;
+    assert.equal(responseBody.eventId, eventId);
+    assert.equal(slots.length, responseSlots.length);
+    assert.equal("NewsArticle", responseSlots[0].rewardActionId);
+    assert.equal("SportsArticle", responseSlots[1].rewardActionId);
+  });
+
+  it("rank with context features", async function () {
+    const actions = getActions();
+    const slots = getSlots();
+    const response = await client
+      .path("/multislot/rank")
+      .post({ body: { actions: actions, slots: slots, contextFeatures: getContextFeatures() } });
+    assert.equal(response.status, "201");
+    const responseBody = response.body as MultiSlotRankResponseOutput;
+    const responseSlots = responseBody.slots as Array<SlotResponseOutput>;
+    assert.equal(slots.length, responseSlots.length);
+    assert.equal("NewsArticle", responseSlots[0].rewardActionId);
+    assert.equal("SportsArticle", responseSlots[1].rewardActionId);
+  });
+});
+
+function getActions(): RankableAction[] {
+  return [
+    { id: "NewsArticle", features: [{ type: "News" }] },
+    { id: "SportsArticle", features: [{ type: "Sports" }] },
+    { id: "EntertainmentArticle", features: [{ type: "Entertainment" }] },
+  ];
+}
+
+function getContextFeatures() {
+  return [
+    { User: { ProfileType: "AnonymousUser", LatLong: "47.6,-122.1" } },
+    { Environment: { DayOfMonth: "28", MonthOfYear: "8", Weather: "Sunny" } },
+    { Device: { Mobile: true, Windows: true } },
+    { RecentActivity: { ItemsInCart: 3 } },
+  ];
+}
+
+function getSlots(): SlotRequest[] {
+  return [getSlotRequest1(), getSlotRequest2()];
+}
+
+function getSlotRequest1(): SlotRequest {
+  return {
+    id: "Main Article",
+    baselineAction: "NewsArticle",
+    features: [{ Size: "Large", Position: "Top Middle" }],
+    excludedActions: ["SportsArticle", "EntertainmentArticle"],
+  };
+}
+
+function getSlotRequest2(): SlotRequest {
+  return {
+    id: "Side Bar",
+    baselineAction: "SportsArticle",
+    features: [{ Size: "Small", Position: "Bottom Right" }],
+    excludedActions: ["EntertainmentArticle"],
+  };
+}
