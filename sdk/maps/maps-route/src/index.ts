9cea83bba6de887105a6bd25691bf5fb3e5b69dc
Andrew Kao <andykao1213@gmail.com>
2022-08-25 06:30:56 +0000

    Remove route & render

--- sdk/maps/maps-route/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export { AzureKeyCredential } from "@azure/core-auth";
-export * from "./mapsRouteClient";
-export * from "./models/options";
-export * from "./models/models";
-export * from "./models/results";
