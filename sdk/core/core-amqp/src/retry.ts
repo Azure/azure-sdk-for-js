9eb62bea105a2b94ba5f9186340bdee5dc081fc9
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2022-08-26 22:51:51 +0000

    [core-amqp] update format

--- sdk/core/core-amqp/src/retry.ts
+++ sdk/core/core-amqp/src/retry.ts
@@ -263,11 +263,12 @@ export async function retry<T>(config: RetryConfig<T>): Promise<T> {
           targetDelayInMs,
           updatedConfig.operationType
         );
-        await delay(targetDelayInMs, undefined, {
-          abortSignal: updatedConfig.abortSignal,
-          abortErrorMsg: `The retry operation has been cancelled by the user.`,
-        });
-
+        await delay(
+          targetDelayInMs, undefined, {
+            abortSignal: updatedConfig.abortSignal,
+            abortErrorMsg: `The retry operation has been cancelled by the user.`,
+          });
+          
         continue;
       } else {
         break;
