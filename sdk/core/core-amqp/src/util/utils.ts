cf61427a6cc8f1f9146781d7cc6189c7b7485e68
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2022-08-26 18:26:03 +0000

    [core-amqp] Unduplicate delay method

--- sdk/core/core-amqp/src/util/utils.ts
+++ sdk/core/core-amqp/src/util/utils.ts
@@ -1,11 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { AbortError, AbortSignalLike } from "@azure/abort-controller";
 import { CancellableAsyncLock, CancellableAsyncLockImpl } from "./lock";
-import { StandardAbortMessage } from "./constants";
 import { WebSocketImpl } from "rhea-promise";
-import { isDefined } from "@azure/core-util";
+import { delay } from "@azure/core-util";
 
 /**
  * @internal
@@ -168,56 +166,7 @@ export class Timeout {
   }
 }
 
-/**
- * A wrapper for setTimeout that resolves a promise after t milliseconds.
- * @param delayInMs - The number of milliseconds to be delayed.
- * @param abortSignal - The abortSignal associated with containing operation.
- * @param abortErrorMsg - The abort error message associated with containing operation.
- * @param value - The value to be resolved with after a timeout of t milliseconds.
- * @returns - Resolved promise
- */
-export function delay<T>(
-  delayInMs: number,
-  abortSignal?: AbortSignalLike,
-  abortErrorMsg?: string,
-  value?: T
-): Promise<T | void> {
-  return new Promise((resolve, reject) => {
-    let timer: ReturnType<typeof setTimeout> | undefined = undefined;
-    let onAborted: (() => void) | undefined = undefined;
-
-    const rejectOnAbort = (): void => {
-      return reject(new AbortError(abortErrorMsg ? abortErrorMsg : StandardAbortMessage));
-    };
-
-    const removeListeners = (): void => {
-      if (abortSignal && onAborted) {
-        abortSignal.removeEventListener("abort", onAborted);
-      }
-    };
-
-    onAborted = (): void => {
-      if (isDefined(timer)) {
-        clearTimeout(timer);
-      }
-      removeListeners();
-      return rejectOnAbort();
-    };
-
-    if (abortSignal && abortSignal.aborted) {
-      return rejectOnAbort();
-    }
-
-    timer = setTimeout(() => {
-      removeListeners();
-      resolve(value);
-    }, delayInMs);
-
-    if (abortSignal) {
-      abortSignal.addEventListener("abort", onAborted);
-    }
-  });
-}
+export { delay };
 
 /**
  * @internal
