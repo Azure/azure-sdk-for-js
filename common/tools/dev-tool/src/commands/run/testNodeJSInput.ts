2f99100176bdab42c749afd529c8cfa4c3e4daf0
Jeremy Meng <yumeng@microsoft.com>
2022-08-30 16:08:02 +0000

    Don't mutate the original options

--- common/tools/dev-tool/src/commands/run/testNodeJSInput.ts
+++ common/tools/dev-tool/src/commands/run/testNodeJSInput.ts
@@ -12,14 +12,10 @@ export const commandInfo = makeCommandInfo(
 export default leafCommand(commandInfo, async (options) => {
   const defaultMochaArgs =
     "-r esm --require source-map-support/register --reporter ../../../common/tools/mocha-multi-reporter.js --full-trace";
-  for (let i = 0; i < (options["--"]?.length ?? 0); i++) {
-    const opt = options["--"]![i];
-    if (opt.includes("**") && !opt.startsWith("'") && !opt.startsWith('"')) {
-      options["--"]![i] = `"${opt}"`
-    }
-  }
-  const mochaArgs = options["--"]?.length
-    ? options["--"]?.join(" ")
+  const updatedArgs = options["--"]?.map(opt =>
+    opt.includes("**") && !opt.startsWith("'") && !opt.startsWith('"') ? `"${opt}"` : opt)
+  const mochaArgs = updatedArgs?.length
+    ? updatedArgs?.join(" ")
     : '--timeout 5000000 "dist-esm/test/{,!(browser)/**/}/*.spec.js"';
   return runTestsWithProxyTool({
     command: `nyc mocha ${defaultMochaArgs} ${mochaArgs}`,
