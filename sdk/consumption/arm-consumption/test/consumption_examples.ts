fe5f5f2e9a680259ede4c73fc5a958678fb26aa4
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-09-05 17:40:36 +0800

    update files

--- sdk/consumption/arm-consumption/test/consumption_examples.ts
+++ sdk/consumption/arm-consumption/test/consumption_examples.ts
@@ -8,31 +8,25 @@
 
 import {
   env,
-  record,
-  RecorderEnvironmentSetup,
   Recorder,
+  RecorderStartOptions,
   delay,
-  isPlaybackMode
+  isPlaybackMode,
 } from "@azure-tools/test-recorder";
-import * as assert from "assert";
-import { ClientSecretCredential } from "@azure/identity";
+import { createTestCredential } from "@azure-tools/test-credential";
+import { assert } from "chai";
+import { Context } from "mocha";
 import { ConsumptionManagementClient } from "../src/consumptionManagementClient";
 
-const recorderEnvSetup: RecorderEnvironmentSetup = {
-  replaceableVariables: {
-    AZURE_CLIENT_ID: "azure_client_id",
-    AZURE_CLIENT_SECRET: "azure_client_secret",
-    AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-    SUBSCRIPTION_ID: "azure_subscription_id"
-  },
-  customizationsOnRecordings: [
-    (recording: any): any =>
-      recording.replace(
-        /"access_token":"[^"]*"/g,
-        `"access_token":"access_token"`
-      )
-  ],
-  queryParametersToSkip: []
+const replaceableVariables: Record<string, string> = {
+  AZURE_CLIENT_ID: "azure_client_id",
+  AZURE_CLIENT_SECRET: "azure_client_secret",
+  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+  SUBSCRIPTION_ID: "azure_subscription_id"
+};
+
+const recorderOptions: RecorderStartOptions = {
+  envSetupForPlayback: replaceableVariables
 };
 
 export const testPollingOptions = {
@@ -49,21 +43,18 @@ describe("Consumption test", () => {
   let vmName: string;
   let scope: string;
 
-  beforeEach(async function () {
-    recorder = record(this, recorderEnvSetup);
-    subscriptionId = env.SUBSCRIPTION_ID;
+  beforeEach(async function (this: Context) {
+    recorder = new Recorder(this.currentTest);
+    await recorder.start(recorderOptions);
+    subscriptionId = env.SUBSCRIPTION_ID || '';
     // This is an example of how the environment variables are used
-    const credential = new ClientSecretCredential(
-      env.AZURE_TENANT_ID,
-      env.AZURE_CLIENT_ID,
-      env.AZURE_CLIENT_SECRET
-    );
-    client = new ConsumptionManagementClient(credential, subscriptionId);
+    const credential = createTestCredential();
+    client = new ConsumptionManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
     location = "eastus";
     resourceGroup = "myjstest";
     budgetName = "mybudgetxxxy";
     vmName = "myvmxxx";
-    scope = "/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroup;
+    scope = "subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroup;
   });
 
   afterEach(async function () {
@@ -76,8 +67,8 @@ describe("Consumption test", () => {
       amount: 100,
       timeGrain: "Monthly",
       timePeriod: {
-        startDate: new Date("2021-12-01T00:00:00Z"),
-        endDate: new Date("2021-12-31T00:00:00Z")
+        startDate: new Date("2022-09-01T00:00:00Z"),
+        endDate: new Date("2022-09-31T00:00:00Z")
       },
       filter: {
         and: [
@@ -86,7 +77,7 @@ describe("Consumption test", () => {
               name: "ResourceId",
               operator: "In",
               values: [
-                "/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroup + "/providers/Microsoft.Compute/virtualMachines/" + vmName
+                "subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroup + "/providers/Microsoft.Compute/virtualMachines/" + vmName
               ]
             }
           },
