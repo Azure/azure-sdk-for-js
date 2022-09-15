9f90d814213d57aa5c38709d334e5a0133302937
Jeff Fisher <jeffish@microsoft.com>
2022-09-15 17:55:01 -0500

    extra robustness for expires_on logic

--- sdk/identity/identity/src/credentials/managedIdentityCredential/utils.ts
+++ sdk/identity/identity/src/credentials/managedIdentityCredential/utils.ts
@@ -51,14 +51,25 @@ export interface TokenResponseParsedBody {
 export function parseExpiresOn(body: TokenResponseParsedBody): number {
   if (typeof body.expires_on === "number") {
     return body.expires_on * 1000;
-  } else if (typeof body.expires_on === "string") {
+  }
+
+  if (typeof body.expires_on === "string") {
     const asNumber = +body.expires_on;
     if (!isNaN(asNumber)) {
       return asNumber * 1000;
-    } else {
-      return Date.parse(body.expires_on);
     }
-  } else {
+
+    const asDate = Date.parse(body.expires_on);
+    if (!isNaN(asDate)) {
+      return asDate;
+    }
+  }
+
+  if (typeof body.expires_in === "number") {
     return Date.now() + body.expires_in * 1000;
   }
+
+  throw new Error(
+    `Failed to parse token expiration from body. expires_in="${body.expires_in}", expires_on="${body.expires_on}"`
+  );
 }
