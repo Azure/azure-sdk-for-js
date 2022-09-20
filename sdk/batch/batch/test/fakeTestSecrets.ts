23ddc1d7c9b1ea9046f3faa48067a3ec7712cac4
Jeremy Meng <yumeng@microsoft.com>
2022-09-20 23:21:30 +0000

    [batch] move test password into fakeTestSecrets.ts and suppress it

---
+++ sdk/batch/batch/test/fakeTestSecrets.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// Centralized location for test/fake secrets so that suppressions
+// are easier to maintain properly
+export const fakeTestPassword1 = "nodesdk";
+export const fakeTestPassword2 = "kt#_gahr!@aGERDXA";
+export const fakeTestPassword3 = "liilef#$DdRGSa_ewkjh";
