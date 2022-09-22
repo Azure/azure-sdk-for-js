4516d0ba4706f7b67fcfd0b5bb1225d7430b175e
azure-sdk <azuresdk@microsoft.com>
2022-09-22 03:01:20 +0000

    Post release automated changes for azure-arm-netapp

--- sdk/netapp/arm-netapp/CHANGELOG.md
+++ sdk/netapp/arm-netapp/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
-    
+
+## 17.0.1 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 17.0.0 (2022-09-21)
     
 **Features**
--- sdk/netapp/arm-netapp/src/netAppManagementClient.ts
+++ sdk/netapp/arm-netapp/src/netAppManagementClient.ts
@@ -83,7 +83,7 @@ export class NetAppManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-netapp/17.0.0`;
+    const packageDetails = `azsdk-js-arm-netapp/17.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
