7a77a0704d6b649de9ed346314757e99db64f97d
azure-sdk <azuresdk@microsoft.com>
2022-09-23 09:04:15 +0000

    Post release automated changes for azure-arm-sqlvirtualmachine

--- sdk/sqlvirtualmachine/arm-sqlvirtualmachine/src/sqlVirtualMachineManagementClient.ts
+++ sdk/sqlvirtualmachine/arm-sqlvirtualmachine/src/sqlVirtualMachineManagementClient.ts
@@ -60,7 +60,7 @@ export class SqlVirtualMachineManagementClient extends coreClient.ServiceClient 
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-sqlvirtualmachine/5.0.0-beta.6`;
+    const packageDetails = `azsdk-js-arm-sqlvirtualmachine/5.0.0-beta.7`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
