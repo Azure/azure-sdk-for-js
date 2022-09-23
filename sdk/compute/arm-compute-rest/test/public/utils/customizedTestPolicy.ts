e86897b4e4661d115d073e6dced1bdbc11ca8211
Mary Gao <yanmeigao1210@gmail.com>
2022-09-23 13:04:41 +0800

    Update test customized test policy

---
+++ sdk/compute/arm-compute-rest/test/public/utils/customizedTestPolicy.ts
@@ -0,0 +1,19 @@
+import { PipelinePolicy } from "@azure/core-rest-pipeline";
+export const apiVersionPolicyName = "CustomizedApiVersionPolicy";
+
+/**
+ * The test recorder will change underlying url so we need to revert change when send new calls
+ * @returns customized policy in testing
+ */
+export function customizedTestPolicy(): PipelinePolicy {
+  return {
+    name: apiVersionPolicyName,
+    sendRequest: (req, next) => {
+      const isLocalUrlReg: RegExp = /http:\/\/localhost:([0-9.\-A-Za-z]+)\//;
+      if (isLocalUrlReg.test(req.url)) {
+        req.url = req.url.replace(isLocalUrlReg, "https://management.azure.com");
+      }
+      return next(req);
+    },
+  };
+}
