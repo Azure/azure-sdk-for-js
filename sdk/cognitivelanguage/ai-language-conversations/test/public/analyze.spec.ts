c474b6a0e27877c816f29867239b1f1cd4d48834
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2022-09-26 16:34:07 -0700

    clean up env var loading

--- sdk/cognitivelanguage/ai-language-conversations/test/public/analyze.spec.ts
+++ sdk/cognitivelanguage/ai-language-conversations/test/public/analyze.spec.ts
@@ -4,9 +4,12 @@
 import { AuthMethod, createClient, startRecorder } from "./utils/recordedClient";
 import { Context, Suite } from "mocha";
 import { assert, matrix } from "@azure/test-utils";
-import { conv1, conv2, conv3, conv4, conv5, conv6 } from "./inputs";
-import { ConversationAnalysisClient } from "../../src";
-import { Recorder } from "@azure-tools/test-recorder";
+import {
+  AnalyzeConversationJobsInput,
+  ConversationalTask,
+  ConversationAnalysisClient,
+} from "../../src";
+import { Recorder, assertEnvironmentVariable } from "@azure-tools/test-recorder";
 
 matrix([["APIKey"]] as const, async (authMethod: AuthMethod) => {
   describe(`[${authMethod}] ConversationAnalysisClient`, function (this: Suite) {
@@ -26,6 +29,82 @@ matrix([["APIKey"]] as const, async (authMethod: AuthMethod) => {
     });
 
     describe("#sync", () => {
+      const conv1: ConversationalTask = {
+        kind: "Conversation",
+        analysisInput: {
+          conversationItem: {
+            participantId: "1",
+            id: "1",
+            modality: "text",
+            language: "en",
+            text: "Send an email to Carol about the tomorrow's demo",
+          },
+        },
+        parameters: {
+          projectName: assertEnvironmentVariable("LANGUAGE_CLU_PROJECT_NAME"),
+          deploymentName: assertEnvironmentVariable("LANGUAGE_CLU_DEPLOYMENT_NAME"),
+          verbose: true,
+          isLoggingEnabled: false,
+        },
+      };
+
+      const conv2: ConversationalTask = {
+        kind: "Conversation",
+        analysisInput: {
+          conversationItem: {
+            participantId: "1",
+            id: "1",
+            modality: "text",
+            language: "en",
+            text: "Send an email to Carol about the tomorrow's demo",
+          },
+        },
+        parameters: {
+          projectName: assertEnvironmentVariable("LANGUAGE_ORCHESTRATION_PROJECT_NAME"),
+          deploymentName: assertEnvironmentVariable("LANGUAGE_ORCHESTRATION_DEPLOYMENT_NAME"),
+          verbose: true,
+          isLoggingEnabled: false,
+        },
+      };
+
+      const conv3: ConversationalTask = {
+        kind: "Conversation",
+        analysisInput: {
+          conversationItem: {
+            participantId: "1",
+            id: "1",
+            modality: "text",
+            language: "en",
+            text: "Reserve a table for 2 at the Italian restaurant",
+          },
+        },
+        parameters: {
+          projectName: assertEnvironmentVariable("LANGUAGE_ORCHESTRATION_PROJECT_NAME"),
+          deploymentName: assertEnvironmentVariable("LANGUAGE_ORCHESTRATION_DEPLOYMENT_NAME"),
+          verbose: true,
+          isLoggingEnabled: false,
+        },
+      };
+
+      const conv4: ConversationalTask = {
+        kind: "Conversation",
+        analysisInput: {
+          conversationItem: {
+            participantId: "1",
+            id: "1",
+            modality: "text",
+            language: "en",
+            text: "How are you?",
+          },
+        },
+        parameters: {
+          projectName: assertEnvironmentVariable("LANGUAGE_ORCHESTRATION_PROJECT_NAME"),
+          deploymentName: assertEnvironmentVariable("LANGUAGE_ORCHESTRATION_DEPLOYMENT_NAME"),
+          verbose: true,
+          isLoggingEnabled: false,
+        },
+      };
+
       it("Test Conversation App", async function () {
         const message = await client.analyzeConversation(conv1);
         // Assert prediction type
@@ -139,6 +218,99 @@ matrix([["APIKey"]] as const, async (authMethod: AuthMethod) => {
     });
 
     describe("#async", () => {
+      const conv5: AnalyzeConversationJobsInput = {
+        displayName: "Analyze PII in conversation",
+        analysisInput: {
+          conversations: [
+            {
+              conversationItems: [
+                {
+                  id: "1",
+                  participantId: "0",
+                  modality: "transcript",
+                  text: "It is john doe.",
+                  lexical: "It is john doe",
+                  itn: "It is john doe",
+                  maskedItn: "It is john doe",
+                },
+                {
+                  id: "2",
+                  participantId: "1",
+                  modality: "transcript",
+                  text: "Yes, 633-27-8199 is my phone",
+                  lexical: "yes six three three two seven eight one nine nine is my phone",
+                  itn: "yes 633278199 is my phone",
+                  maskedItn: "yes 633278199 is my phone",
+                },
+                {
+                  id: "3",
+                  participantId: "1",
+                  modality: "transcript",
+                  text: "j.doe@yahoo.com is my email",
+                  lexical: "j dot doe at yahoo dot com is my email",
+                  maskedItn: "j.doe@yahoo.com is my email",
+                  itn: "j.doe@yahoo.com is my email",
+                },
+              ],
+              modality: "transcript",
+              id: "1",
+              language: "en",
+            },
+          ],
+        },
+        tasks: [
+          {
+            kind: "ConversationalPIITask",
+            parameters: {
+              redactionSource: "lexical",
+              piiCategories: ["all"],
+            },
+          },
+        ],
+      };
+
+      const conv6: AnalyzeConversationJobsInput = {
+        displayName: "Analyze conversations from xxx",
+        analysisInput: {
+          conversations: [
+            {
+              conversationItems: [
+                {
+                  text: "Hello, how can I help you?",
+                  modality: "text",
+                  id: "1",
+                  participantId: "Agent",
+                },
+                {
+                  text: "How to upgrade Office? I am getting error messages the whole day.",
+                  modality: "text",
+                  id: "2",
+                  participantId: "Customer",
+                },
+                {
+                  text: "Press the upgrade button please. Then sign in and follow the instructions.",
+                  modality: "text",
+                  id: "3",
+                  participantId: "Agent",
+                },
+              ],
+              modality: "text",
+              id: "conversation1",
+              language: "en",
+            },
+          ],
+        },
+        tasks: [
+          {
+            taskName: "analyze 1",
+            kind: "ConversationalSummarizationTask",
+            parameters: {
+              summaryAspects: ["Issue, Resolution"],
+            },
+          },
+        ],
+      };
+
       it("Test Conversation App PII transcript", async function () {
         const poller = await client.beginConversationAnalysis(conv5);
         const message = await poller.pollUntilDone();
