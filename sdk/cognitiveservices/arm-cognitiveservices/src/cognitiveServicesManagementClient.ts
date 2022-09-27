1ff12d8b718368a188dd43b1fd0614baaa7f4854
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-09-27 16:00:35 +0800

    update files

--- sdk/cognitiveservices/arm-cognitiveservices/src/cognitiveServicesManagementClient.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/src/cognitiveServicesManagementClient.ts
@@ -78,7 +78,7 @@ export class CognitiveServicesManagementClient extends coreClient.ServiceClient 
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-cognitiveservices/7.2.0`;
+    const packageDetails = `azsdk-js-arm-cognitiveservices/7.2.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
