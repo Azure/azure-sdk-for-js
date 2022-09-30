745ae8f00631cac39b6c7e64053dc2a133355192
azure-sdk <azuresdk@microsoft.com>
2022-09-30 09:13:38 +0000

    Post release automated changes for azure-arm-appplatform

--- sdk/appplatform/arm-appplatform/CHANGELOG.md
+++ sdk/appplatform/arm-appplatform/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
 
+## 2.1.0-beta.3 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 2.1.0-beta.2 (2022-09-30)
 
 **Bugs Fixed**
--- sdk/appplatform/arm-appplatform/src/appPlatformManagementClient.ts
+++ sdk/appplatform/arm-appplatform/src/appPlatformManagementClient.ts
@@ -99,7 +99,7 @@ export class AppPlatformManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-appplatform/2.1.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-appplatform/2.1.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
