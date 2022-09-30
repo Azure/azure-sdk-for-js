37def5882bd78d30d0270320e503bae4945fe371
azure-sdk <azuresdk@microsoft.com>
2022-09-30 06:18:03 +0000

    Post release automated changes for azure-arm-network

--- sdk/network/arm-network/src/networkManagementClient.ts
+++ sdk/network/arm-network/src/networkManagementClient.ts
@@ -358,7 +358,7 @@ export class NetworkManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-network/30.0.0`;
+    const packageDetails = `azsdk-js-arm-network/30.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
