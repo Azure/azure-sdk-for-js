96ca71eec35921859f94e9d720c21f9daaa9bba3
azure-sdk <azuresdk@microsoft.com>
2022-09-30 08:22:33 +0000

    Post release automated changes for azure-arm-appconfiguration

--- sdk/appconfiguration/arm-appconfiguration/src/appConfigurationManagementClient.ts
+++ sdk/appconfiguration/arm-appconfiguration/src/appConfigurationManagementClient.ts
@@ -62,7 +62,7 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-appconfiguration/3.0.1`;
+    const packageDetails = `azsdk-js-arm-appconfiguration/3.0.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
