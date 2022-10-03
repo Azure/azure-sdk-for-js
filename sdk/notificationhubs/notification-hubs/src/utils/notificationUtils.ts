d496a8d4b1064839aebfb32dc5b2f6477dfe02a4
Matthew Podwysocki <matthewp@microsoft.com>
2022-10-03 18:40:26 -0400

    [notification hubs] fixing lint issues

--- sdk/notificationhubs/notification-hubs/src/utils/notificationUtils.ts
+++ sdk/notificationhubs/notification-hubs/src/utils/notificationUtils.ts
@@ -11,12 +11,12 @@ export function createMultipartDirectNotification(
   deviceHandles: string[]
 ): string {
   return "--nh-batch-multipart-boundary\r\n" +
-    "Content-type: "+ notification.contentType+"\r\n" +
+    "Content-type: " + notification.contentType + "\r\n" +
     "Content-Disposition: inline; name=notification\r\n\r\n" +
     notification.body + "\r\n" +
-    "--nh-batch-multipart-boundary\r\n"+
+    "--nh-batch-multipart-boundary\r\n" +
     "Content-type: application/json\r\n" +
     "Content-Disposition: inline; name=devices\r\n\r\n" +
-    JSON.stringify(deviceHandles) + "\r\n"+
+    JSON.stringify(deviceHandles) + "\r\n" +
     "--nh-batch-multipart-boundary--";
 }
