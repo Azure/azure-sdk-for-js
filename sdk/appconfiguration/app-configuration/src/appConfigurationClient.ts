e6ad5fe0d652ea88c6aaa5daaf28c88f3bd8ae0f
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2022-10-11 17:37:11 +0000

    [app-config]migrate to the new core-paging method

--- sdk/appconfiguration/app-configuration/src/appConfigurationClient.ts
+++ sdk/appconfiguration/app-configuration/src/appConfigurationClient.ts
@@ -37,10 +37,7 @@ import {
   deserializationPolicy,
   deserializationPolicyName,
 } from "@azure/core-client";
-<<<<<<< HEAD
 import { PagedAsyncIterableIterator, PagedResult, getPagedAsyncIterator } from "@azure/core-paging";
-=======
->>>>>>> 4dabab3cf1 (Fix eslint warnings)
 import { PipelinePolicy, bearerTokenAuthenticationPolicy } from "@azure/core-rest-pipeline";
 import { SyncTokens, syncTokenPolicy } from "./internal/synctokenpolicy";
 import { TokenCredential, isTokenCredential } from "@azure/core-auth";
@@ -346,108 +343,6 @@ export class AppConfigurationClient {
   }
 
   /**
-   * Lists settings using the core paging method for the iterator
-   * @param options - Optional parameters for the request.
-   */
-  listConfigurationSettingsWithCorePaging(
-    options: ListConfigurationSettingsOptions = {}
-  ): PagedAsyncIterableIterator<ConfigurationSetting, ListConfigurationSettingPage, PageSettings> {
-
-    const pagedResult: PagedResult<ListConfigurationSettingPage, PageSettings, string | undefined> = {
-      firstPageLink: undefined,
-      getPage: async (pageLink: string | undefined) => {
-        const response = await (this.sendRequest(options, pageLink));
-        let currentResponse = {
-          ... response,
-          items: response.items != null ? response.items?.map(transformKeyValue): [],
-          continuationToken: response.nextLink
-            ? extractAfterTokenFromNextLink(response.nextLink)
-            : undefined,
-        }
-        
-        return {
-          page: currentResponse,
-          nextPageLink: currentResponse.nextLink,
-        };
-      }
-      
-    }
-    return getPagedAsyncIterator(pagedResult);
-  }
-
-  private async sendRequest(
-    options: ListConfigurationSettingsOptions & PageSettings = {},
-    pageLink: string | undefined
-  ): Promise<GetKeyValuesResponse & HttpResponseField<AppConfigurationGetKeyValuesHeaders>> {
-    return tracingClient.withSpan(
-      "AppConfigurationClient.listConfigurationSettings",
-      options,
-      async (updatedOptions) => {
-        const response = await this.client.getKeyValues({
-          ...updatedOptions,
-          ...formatAcceptDateTime(options),
-          ...formatFiltersAndSelect(options),
-          after: pageLink,
-        });
-
-        return response as GetKeyValuesResponse &
-          HttpResponseField<AppConfigurationGetKeyValuesHeaders>;
-      }
-    );
-  }
-
-  /**
-   * Lists settings using the core paging method for the iterator
-   * @param options - Optional parameters for the request.
-   */
-  listConfigurationSettingsWithCorePaging(
-    options: ListConfigurationSettingsOptions = {}
-  ): PagedAsyncIterableIterator<ConfigurationSetting, ListConfigurationSettingPage, PageSettings> {
-
-    const pagedResult: PagedResult<ListConfigurationSettingPage, PageSettings, string | undefined> = {
-      firstPageLink: undefined,
-      getPage: async (pageLink: string | undefined) => {
-        const response = await (this.sendRequest(options, pageLink));
-        let currentResponse = {
-          ... response,
-          items: response.items != null ? response.items?.map(transformKeyValue): [],
-          continuationToken: response.nextLink
-            ? extractAfterTokenFromNextLink(response.nextLink)
-            : undefined,
-        }
-        // let itemList = currentResponse.items;
-        return {
-          page: currentResponse,
-          nextPageLink: currentResponse.nextLink,
-        };
-      }
-      
-    }
-    return getPagedAsyncIterator(pagedResult);
-  }
-
-  private async sendRequest(
-    options: ListConfigurationSettingsOptions & PageSettings = {},
-    pageLink: string | undefined
-  ): Promise<GetKeyValuesResponse & HttpResponseField<AppConfigurationGetKeyValuesHeaders>> {
-    return tracingClient.withSpan(
-      "AppConfigurationClient.listConfigurationSettings",
-      options,
-      async (updatedOptions) => {
-        const response = await this.client.getKeyValues({
-          ...updatedOptions,
-          ...formatAcceptDateTime(options),
-          ...formatFiltersAndSelect(options),
-          after: pageLink,
-        });
-
-        return response as GetKeyValuesResponse &
-          HttpResponseField<AppConfigurationGetKeyValuesHeaders>;
-      }
-    );
-  }
-
-  /**
    * Lists revisions of a set of keys, optionally filtered by key names,
    * labels and accept datetime.
    *
@@ -501,44 +396,6 @@ export class AppConfigurationClient {
           HttpResponseField<AppConfigurationGetRevisionsHeaders>;
       }
     );
-<<<<<<< HEAD
-=======
-
-    yield* this.createListRevisionsPageFromResponse(currentResponse);
-
-    while (currentResponse.nextLink) {
-      currentResponse = (await tracingClient.withSpan(
-        "AppConfigurationClient.listRevisions",
-        options,
-        (updatedOptions) => {
-          return this.client.getRevisions({
-            ...updatedOptions,
-            ...formatAcceptDateTime(options),
-            ...formatFiltersAndSelect(options),
-            after: extractAfterTokenFromNextLink(currentResponse.nextLink!),
-          });
-        }
-      )) as GetRevisionsResponse & HttpResponseField<AppConfigurationGetRevisionsHeaders>;
-
-      if (!currentResponse.items) {
-        break;
-      }
-
-      yield* this.createListRevisionsPageFromResponse(currentResponse);
-    }
-  }
-
-  private *createListRevisionsPageFromResponse(
-    currentResponse: GetKeyValuesResponse & HttpResponseField<AppConfigurationGetKeyValuesHeaders>
-  ): Generator<ListRevisionsPage> {
-    yield {
-      ...currentResponse,
-      items: currentResponse.items != null ? currentResponse.items.map(transformKeyValue) : [],
-      continuationToken: currentResponse.nextLink
-        ? extractAfterTokenFromNextLink(currentResponse.nextLink)
-        : undefined,
-    };
->>>>>>> 4dabab3cf1 (Fix eslint warnings)
   }
 
   /**
