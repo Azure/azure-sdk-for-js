cd42543fc4cabd83f2d70d119b3ac4165cb52c46
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-10-12 12:32:04 +0800

    Appcontainer re (#23484)
    * appcontainer release
    
    * update files
    
    * release appcontainers

--- sdk/appcontainers/arm-appcontainers/src/containerAppsAPIClient.ts
+++ sdk/appcontainers/arm-appcontainers/src/containerAppsAPIClient.ts
@@ -20,12 +20,21 @@ import {
   ContainerAppsRevisionsImpl,
   ContainerAppsRevisionReplicasImpl,
   DaprComponentsImpl,
+  ContainerAppsDiagnosticsImpl,
+  ManagedEnvironmentDiagnosticsImpl,
+  ManagedEnvironmentsDiagnosticsImpl,
   OperationsImpl,
   ManagedEnvironmentsImpl,
   CertificatesImpl,
   NamespacesImpl,
   ManagedEnvironmentsStoragesImpl,
-  ContainerAppsSourceControlsImpl
+  ContainerAppsSourceControlsImpl,
+  ConnectedEnvironmentsImpl,
+  ConnectedEnvironmentsCertificatesImpl,
+  ConnectedEnvironmentsDaprComponentsImpl,
+  ConnectedEnvironmentsStoragesImpl,
+  AvailableWorkloadProfilesImpl,
+  BillingMetersImpl
 } from "./operations";
 import {
   ContainerAppsAuthConfigs,
@@ -33,12 +42,21 @@ import {
   ContainerAppsRevisions,
   ContainerAppsRevisionReplicas,
   DaprComponents,
+  ContainerAppsDiagnostics,
+  ManagedEnvironmentDiagnostics,
+  ManagedEnvironmentsDiagnostics,
   Operations,
   ManagedEnvironments,
   Certificates,
   Namespaces,
   ManagedEnvironmentsStorages,
-  ContainerAppsSourceControls
+  ContainerAppsSourceControls,
+  ConnectedEnvironments,
+  ConnectedEnvironmentsCertificates,
+  ConnectedEnvironmentsDaprComponents,
+  ConnectedEnvironmentsStorages,
+  AvailableWorkloadProfiles,
+  BillingMeters
 } from "./operationsInterfaces";
 import { ContainerAppsAPIClientOptionalParams } from "./models";
 
@@ -74,7 +92,7 @@ export class ContainerAppsAPIClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-appcontainers/1.1.3`;
+    const packageDetails = `azsdk-js-arm-appcontainers/2.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -128,7 +146,7 @@ export class ContainerAppsAPIClient extends coreClient.ServiceClient {
 
     // Assigning values to Constant parameters
     this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2022-03-01";
+    this.apiVersion = options.apiVersion || "2022-06-01-preview";
     this.containerAppsAuthConfigs = new ContainerAppsAuthConfigsImpl(this);
     this.containerApps = new ContainerAppsImpl(this);
     this.containerAppsRevisions = new ContainerAppsRevisionsImpl(this);
@@ -136,6 +154,13 @@ export class ContainerAppsAPIClient extends coreClient.ServiceClient {
       this
     );
     this.daprComponents = new DaprComponentsImpl(this);
+    this.containerAppsDiagnostics = new ContainerAppsDiagnosticsImpl(this);
+    this.managedEnvironmentDiagnostics = new ManagedEnvironmentDiagnosticsImpl(
+      this
+    );
+    this.managedEnvironmentsDiagnostics = new ManagedEnvironmentsDiagnosticsImpl(
+      this
+    );
     this.operations = new OperationsImpl(this);
     this.managedEnvironments = new ManagedEnvironmentsImpl(this);
     this.certificates = new CertificatesImpl(this);
@@ -146,6 +171,18 @@ export class ContainerAppsAPIClient extends coreClient.ServiceClient {
     this.containerAppsSourceControls = new ContainerAppsSourceControlsImpl(
       this
     );
+    this.connectedEnvironments = new ConnectedEnvironmentsImpl(this);
+    this.connectedEnvironmentsCertificates = new ConnectedEnvironmentsCertificatesImpl(
+      this
+    );
+    this.connectedEnvironmentsDaprComponents = new ConnectedEnvironmentsDaprComponentsImpl(
+      this
+    );
+    this.connectedEnvironmentsStorages = new ConnectedEnvironmentsStoragesImpl(
+      this
+    );
+    this.availableWorkloadProfiles = new AvailableWorkloadProfilesImpl(this);
+    this.billingMeters = new BillingMetersImpl(this);
     this.addCustomApiVersionPolicy(options.apiVersion);
   }
 
@@ -182,10 +219,19 @@ export class ContainerAppsAPIClient extends coreClient.ServiceClient {
   containerAppsRevisions: ContainerAppsRevisions;
   containerAppsRevisionReplicas: ContainerAppsRevisionReplicas;
   daprComponents: DaprComponents;
+  containerAppsDiagnostics: ContainerAppsDiagnostics;
+  managedEnvironmentDiagnostics: ManagedEnvironmentDiagnostics;
+  managedEnvironmentsDiagnostics: ManagedEnvironmentsDiagnostics;
   operations: Operations;
   managedEnvironments: ManagedEnvironments;
   certificates: Certificates;
   namespaces: Namespaces;
   managedEnvironmentsStorages: ManagedEnvironmentsStorages;
   containerAppsSourceControls: ContainerAppsSourceControls;
+  connectedEnvironments: ConnectedEnvironments;
+  connectedEnvironmentsCertificates: ConnectedEnvironmentsCertificates;
+  connectedEnvironmentsDaprComponents: ConnectedEnvironmentsDaprComponents;
+  connectedEnvironmentsStorages: ConnectedEnvironmentsStorages;
+  availableWorkloadProfiles: AvailableWorkloadProfiles;
+  billingMeters: BillingMeters;
 }
