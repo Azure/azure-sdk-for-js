502d20b701bc322bcf9ab2594529d17f9d2900c1
Olena Stoliarova <ostoliarova@microsoft.com>
2022-10-13 15:32:39 +0300

    fix formatting

--- sdk/communication/communication-common/src/identifierModels.ts
+++ sdk/communication/communication-common/src/identifierModels.ts
@@ -4,265 +4,265 @@
 /**
  * Identifies a communication participant.
  */
- export type CommunicationIdentifier =
- | CommunicationUserIdentifier
- | PhoneNumberIdentifier
- | MicrosoftTeamsUserIdentifier
- | UnknownIdentifier;
+export type CommunicationIdentifier =
+  | CommunicationUserIdentifier
+  | PhoneNumberIdentifier
+  | MicrosoftTeamsUserIdentifier
+  | UnknownIdentifier;
 
 /**
-* An Azure Communication user.
-*/
+ * An Azure Communication user.
+ */
 export interface CommunicationUserIdentifier {
- /**
-  * Id of the CommunicationUser as returned from the Communication Service.
-  */
- communicationUserId: string;
+  /**
+   * Id of the CommunicationUser as returned from the Communication Service.
+   */
+  communicationUserId: string;
 }
 
 /**
-* A phone number.
-*/
+ * A phone number.
+ */
 export interface PhoneNumberIdentifier {
- /**
-  * Optional raw id of the phone number.
-  */
- rawId?: string;
- /**
-  * The phone number in E.164 format.
-  */
- phoneNumber: string;
+  /**
+   * Optional raw id of the phone number.
+   */
+  rawId?: string;
+  /**
+   * The phone number in E.164 format.
+   */
+  phoneNumber: string;
 }
 
 /**
-* A Microsoft Teams user.
-*/
+ * A Microsoft Teams user.
+ */
 export interface MicrosoftTeamsUserIdentifier {
- /**
-  * Optional raw id of the Microsoft Teams user.
-  */
- rawId?: string;
+  /**
+   * Optional raw id of the Microsoft Teams user.
+   */
+  rawId?: string;
 
- /**
-  * Id of the Microsoft Teams user. If the user isn't anonymous, the id is the AAD object id of the user.
-  */
- microsoftTeamsUserId: string;
+  /**
+   * Id of the Microsoft Teams user. If the user isn't anonymous, the id is the AAD object id of the user.
+   */
+  microsoftTeamsUserId: string;
 
- /**
-  * True if the user is anonymous, for example when joining a meeting with a share link. If missing, the user is not anonymous.
-  */
- isAnonymous?: boolean;
+  /**
+   * True if the user is anonymous, for example when joining a meeting with a share link. If missing, the user is not anonymous.
+   */
+  isAnonymous?: boolean;
 
- /**
-  * The cloud that the Microsoft Teams user belongs to. If missing, the cloud is "public".
-  */
- cloud?: "public" | "dod" | "gcch";
+  /**
+   * The cloud that the Microsoft Teams user belongs to. If missing, the cloud is "public".
+   */
+  cloud?: "public" | "dod" | "gcch";
 }
 
 /**
-* An unknown identifier that doesn't fit any of the other identifier types.
-*/
+ * An unknown identifier that doesn't fit any of the other identifier types.
+ */
 export interface UnknownIdentifier {
- /**
-  * Id of the UnknownIdentifier.
-  */
- id: string;
+  /**
+   * Id of the UnknownIdentifier.
+   */
+  id: string;
 }
 
 /**
-* Tests an Identifier to determine whether it implements CommunicationUserIdentifier.
-*
-* @param identifier - The assumed CommunicationUserIdentifier to be tested.
-*/
+ * Tests an Identifier to determine whether it implements CommunicationUserIdentifier.
+ *
+ * @param identifier - The assumed CommunicationUserIdentifier to be tested.
+ */
 export const isCommunicationUserIdentifier = (
- identifier: CommunicationIdentifier
+  identifier: CommunicationIdentifier
 ): identifier is CommunicationUserIdentifier => {
- return typeof (identifier as any).communicationUserId === "string";
+  return typeof (identifier as any).communicationUserId === "string";
 };
 
 /**
-* Tests an Identifier to determine whether it implements PhoneNumberIdentifier.
-*
-* @param identifier - The assumed PhoneNumberIdentifier to be tested.
-*/
+ * Tests an Identifier to determine whether it implements PhoneNumberIdentifier.
+ *
+ * @param identifier - The assumed PhoneNumberIdentifier to be tested.
+ */
 export const isPhoneNumberIdentifier = (
- identifier: CommunicationIdentifier
+  identifier: CommunicationIdentifier
 ): identifier is PhoneNumberIdentifier => {
- return typeof (identifier as any).phoneNumber === "string";
+  return typeof (identifier as any).phoneNumber === "string";
 };
 
 /**
-* Tests an Identifier to determine whether it implements MicrosoftTeamsUserIdentifier.
-*
-* @param identifier - The assumed available to be tested.
-*/
+ * Tests an Identifier to determine whether it implements MicrosoftTeamsUserIdentifier.
+ *
+ * @param identifier - The assumed available to be tested.
+ */
 export const isMicrosoftTeamsUserIdentifier = (
- identifier: CommunicationIdentifier
+  identifier: CommunicationIdentifier
 ): identifier is MicrosoftTeamsUserIdentifier => {
- return typeof (identifier as any).microsoftTeamsUserId === "string";
+  return typeof (identifier as any).microsoftTeamsUserId === "string";
 };
 
 /**
-* Tests an Identifier to determine whether it implements UnknownIdentifier.
-*
-* @param identifier - The assumed UnknownIdentifier to be tested.
-*/
+ * Tests an Identifier to determine whether it implements UnknownIdentifier.
+ *
+ * @param identifier - The assumed UnknownIdentifier to be tested.
+ */
 export const isUnknownIdentifier = (
- identifier: CommunicationIdentifier
+  identifier: CommunicationIdentifier
 ): identifier is UnknownIdentifier => {
- return typeof (identifier as any).id === "string";
+  return typeof (identifier as any).id === "string";
 };
 
 /**
-* The CommunicationIdentifierKind is a discriminated union that adds a property `kind` to an Identifier.
-*/
+ * The CommunicationIdentifierKind is a discriminated union that adds a property `kind` to an Identifier.
+ */
 export type CommunicationIdentifierKind =
- | CommunicationUserKind
- | PhoneNumberKind
- | MicrosoftTeamsUserKind
- | UnknownIdentifierKind;
+  | CommunicationUserKind
+  | PhoneNumberKind
+  | MicrosoftTeamsUserKind
+  | UnknownIdentifierKind;
 
 /**
-* IdentifierKind for a CommunicationUserIdentifier.
-*/
+ * IdentifierKind for a CommunicationUserIdentifier.
+ */
 export interface CommunicationUserKind extends CommunicationUserIdentifier {
- /**
-  * The identifier kind.
-  */
- kind: "communicationUser";
+  /**
+   * The identifier kind.
+   */
+  kind: "communicationUser";
 }
 
 /**
-* IdentifierKind for a PhoneNumberIdentifier.
-*/
+ * IdentifierKind for a PhoneNumberIdentifier.
+ */
 export interface PhoneNumberKind extends PhoneNumberIdentifier {
- /**
-  * The identifier kind.
-  */
- kind: "phoneNumber";
+  /**
+   * The identifier kind.
+   */
+  kind: "phoneNumber";
 }
 
 /**
-* IdentifierKind for a MicrosoftTeamsUserIdentifier.
-*/
+ * IdentifierKind for a MicrosoftTeamsUserIdentifier.
+ */
 export interface MicrosoftTeamsUserKind extends MicrosoftTeamsUserIdentifier {
- /**
-  * The identifier kind.
-  */
- kind: "microsoftTeamsUser";
+  /**
+   * The identifier kind.
+   */
+  kind: "microsoftTeamsUser";
 }
 
 /**
-* IdentifierKind for UnknownIdentifier.
-*/
+ * IdentifierKind for UnknownIdentifier.
+ */
 export interface UnknownIdentifierKind extends UnknownIdentifier {
- /**
-  * The identifier kind.
-  */
- kind: "unknown";
+  /**
+   * The identifier kind.
+   */
+  kind: "unknown";
 }
 
 /**
-* Returns the CommunicationIdentifierKind for a given CommunicationIdentifier. Returns undefined if the kind couldn't be inferred.
-*
-* @param identifier - The identifier whose kind is to be inferred.
-*/
+ * Returns the CommunicationIdentifierKind for a given CommunicationIdentifier. Returns undefined if the kind couldn't be inferred.
+ *
+ * @param identifier - The identifier whose kind is to be inferred.
+ */
 export const getIdentifierKind = (
- identifier: CommunicationIdentifier
+  identifier: CommunicationIdentifier
 ): CommunicationIdentifierKind => {
- if (isCommunicationUserIdentifier(identifier)) {
-   return { ...identifier, kind: "communicationUser" };
- }
- if (isPhoneNumberIdentifier(identifier)) {
-   return { ...identifier, kind: "phoneNumber" };
- }
- if (isMicrosoftTeamsUserIdentifier(identifier)) {
-   return { ...identifier, kind: "microsoftTeamsUser" };
- }
- return { ...identifier, kind: "unknown" };
+  if (isCommunicationUserIdentifier(identifier)) {
+    return { ...identifier, kind: "communicationUser" };
+  }
+  if (isPhoneNumberIdentifier(identifier)) {
+    return { ...identifier, kind: "phoneNumber" };
+  }
+  if (isMicrosoftTeamsUserIdentifier(identifier)) {
+    return { ...identifier, kind: "microsoftTeamsUser" };
+  }
+  return { ...identifier, kind: "unknown" };
 };
 
 /**
-* Returns the rawId for a given CommunicationIdentifier. You can use the rawId for encoding the identifier and then use it as a key in a database.
-*
-* @param identifier - The identifier to be translated to its rawId.
-*/
+ * Returns the rawId for a given CommunicationIdentifier. You can use the rawId for encoding the identifier and then use it as a key in a database.
+ *
+ * @param identifier - The identifier to be translated to its rawId.
+ */
 export const getIdentifierRawId = (identifier: CommunicationIdentifier): string => {
- const identifierKind = getIdentifierKind(identifier);
- switch (identifierKind.kind) {
-   case "communicationUser":
-     return identifierKind.communicationUserId;
-   case "microsoftTeamsUser": {
-     const { microsoftTeamsUserId, rawId, cloud, isAnonymous } = identifierKind;
-     if (rawId) return rawId;
-     if (isAnonymous) return `8:teamsvisitor:${microsoftTeamsUserId}`;
-     switch (cloud) {
-       case "dod":
-         return `8:dod:${microsoftTeamsUserId}`;
-       case "gcch":
-         return `8:gcch:${microsoftTeamsUserId}`;
-       case "public":
-         return `8:orgid:${microsoftTeamsUserId}`;
-     }
-     return `8:orgid:${microsoftTeamsUserId}`;
-   }
-   case "phoneNumber": {
-     const { phoneNumber, rawId } = identifierKind;
-     if (rawId) return rawId;
-     // strip the leading +. We just assume correct E.164 format here because validation should only happen server-side, not client-side.
-     return `4:${phoneNumber.replace(/^\+/, "")}`;
-   }
-   case "unknown": {
-     return identifierKind.id;
-   }
- }
+  const identifierKind = getIdentifierKind(identifier);
+  switch (identifierKind.kind) {
+    case "communicationUser":
+      return identifierKind.communicationUserId;
+    case "microsoftTeamsUser": {
+      const { microsoftTeamsUserId, rawId, cloud, isAnonymous } = identifierKind;
+      if (rawId) return rawId;
+      if (isAnonymous) return `8:teamsvisitor:${microsoftTeamsUserId}`;
+      switch (cloud) {
+        case "dod":
+          return `8:dod:${microsoftTeamsUserId}`;
+        case "gcch":
+          return `8:gcch:${microsoftTeamsUserId}`;
+        case "public":
+          return `8:orgid:${microsoftTeamsUserId}`;
+      }
+      return `8:orgid:${microsoftTeamsUserId}`;
+    }
+    case "phoneNumber": {
+      const { phoneNumber, rawId } = identifierKind;
+      if (rawId) return rawId;
+      // strip the leading +. We just assume correct E.164 format here because validation should only happen server-side, not client-side.
+      return `4:${phoneNumber.replace(/^\+/, "")}`;
+    }
+    case "unknown": {
+      return identifierKind.id;
+    }
+  }
 };
 
 /**
-* Creates a CommunicationIdentifierKind from a given rawId. When storing rawIds use this function to restore the identifier that was encoded in the rawId.
-*
-* @param rawId - The rawId to be translated to its identifier representation.
-*/
+ * Creates a CommunicationIdentifierKind from a given rawId. When storing rawIds use this function to restore the identifier that was encoded in the rawId.
+ *
+ * @param rawId - The rawId to be translated to its identifier representation.
+ */
 export const createIdentifierFromRawId = (rawId: string): CommunicationIdentifierKind => {
- if (rawId.startsWith("4:")) {
-   return { kind: "phoneNumber", phoneNumber: `+${rawId.substring("4:".length)}` };
- }
+  if (rawId.startsWith("4:")) {
+    return { kind: "phoneNumber", phoneNumber: `+${rawId.substring("4:".length)}` };
+  }
 
- const segments = rawId.split(":");
- if (segments.length < 3) return { kind: "unknown", id: rawId };
+  const segments = rawId.split(":");
+  if (segments.length < 3) return { kind: "unknown", id: rawId };
 
- const prefix = `${segments[0]}:${segments[1]}:`;
- const suffix = rawId.substring(prefix.length);
+  const prefix = `${segments[0]}:${segments[1]}:`;
+  const suffix = rawId.substring(prefix.length);
 
- switch (prefix) {
-   case "8:teamsvisitor:":
-     return { kind: "microsoftTeamsUser", microsoftTeamsUserId: suffix, isAnonymous: true };
-   case "8:orgid:":
-     return {
-       kind: "microsoftTeamsUser",
-       microsoftTeamsUserId: suffix,
-       isAnonymous: false,
-       cloud: "public",
-     };
-   case "8:dod:":
-     return {
-       kind: "microsoftTeamsUser",
-       microsoftTeamsUserId: suffix,
-       isAnonymous: false,
-       cloud: "dod",
-     };
-   case "8:gcch:":
-     return {
-       kind: "microsoftTeamsUser",
-       microsoftTeamsUserId: suffix,
-       isAnonymous: false,
-       cloud: "gcch",
-     };
-   case "8:acs:":
-   case "8:spool:":
-   case "8:dod-acs:":
-   case "8:gcch-acs:":
-     return { kind: "communicationUser", communicationUserId: rawId };
- }
- return { kind: "unknown", id: rawId };
+  switch (prefix) {
+    case "8:teamsvisitor:":
+      return { kind: "microsoftTeamsUser", microsoftTeamsUserId: suffix, isAnonymous: true };
+    case "8:orgid:":
+      return {
+        kind: "microsoftTeamsUser",
+        microsoftTeamsUserId: suffix,
+        isAnonymous: false,
+        cloud: "public",
+      };
+    case "8:dod:":
+      return {
+        kind: "microsoftTeamsUser",
+        microsoftTeamsUserId: suffix,
+        isAnonymous: false,
+        cloud: "dod",
+      };
+    case "8:gcch:":
+      return {
+        kind: "microsoftTeamsUser",
+        microsoftTeamsUserId: suffix,
+        isAnonymous: false,
+        cloud: "gcch",
+      };
+    case "8:acs:":
+    case "8:spool:":
+    case "8:dod-acs:":
+    case "8:gcch-acs:":
+      return { kind: "communicationUser", communicationUserId: rawId };
+  }
+  return { kind: "unknown", id: rawId };
 };
