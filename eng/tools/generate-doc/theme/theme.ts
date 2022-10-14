a4e70d5ce18db54ffb16b1231af181f0fb6ee038
Jeff Fisher <jeffish@microsoft.com>
2022-10-14 17:14:06 -0500

    Add version picker back
    Port the old version picker forward and remove the old theme files

--- eng/tools/generate-doc/theme/theme.ts
+++ eng/tools/generate-doc/theme/theme.ts
@@ -3,12 +3,39 @@ import {
   DefaultTheme,
   Options,
   DefaultThemeRenderContext,
+  RendererEvent,
 } from "typedoc";
 import { toolbar } from "./toolbar.js";
+import { versionPicker, versionPickerScript } from "./versionPicker.js";
+import * as path from "path";
+import * as fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function copyAssetsSync(source: string, dest: string) {
+  const stat = fs.statSync(source);
+
+  if (stat.isDirectory()) {
+    for (const item of fs.readdirSync(source)) {
+      copyAssetsSync(path.join(source, item), path.join(dest, item));
+    }
+  } else if (stat.isFile()) {
+    fs.mkdirSync(path.dirname(dest), { recursive: true });
+    fs.copyFileSync(source, dest);
+  }
+}
 
 export function loadTheme(app: TypeDocApplication) {
   app.renderer.defineTheme("azureSdk", AzureSdkTheme);
-  // app.renderer.hooks.on("")
+  app.renderer.hooks.on("content.begin", versionPicker);
+  app.renderer.hooks.on("body.end", versionPickerScript);
+  app.renderer.on(RendererEvent.END, (event: RendererEvent) => {
+    const dest = path.join(event.outputDirectory, "assets");
+    const source = path.join(__dirname, "..", "..", "theme", "assets");
+    copyAssetsSync(source, dest);
+  });
 }
 
 /**
