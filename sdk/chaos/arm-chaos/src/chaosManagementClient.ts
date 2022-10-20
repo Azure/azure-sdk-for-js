50e10c04b136db6828c505430be7f76bd508bf28
azure-sdk <azuresdk@microsoft.com>
2022-10-20 06:53:50 +0000

    Post release automated changes for azure-arm-chaos

--- sdk/chaos/arm-chaos/src/chaosManagementClient.ts
+++ sdk/chaos/arm-chaos/src/chaosManagementClient.ts
@@ -64,7 +64,7 @@ export class ChaosManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-chaos/1.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-chaos/1.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
