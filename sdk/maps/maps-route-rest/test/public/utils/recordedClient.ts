ccd165bc96de0ae7be82603bab11dba8f68f632b
Andrew Kao <andykao1213@gmail.com>
2022-10-20 07:20:15 +0000

    Add test

--- sdk/maps/maps-route-rest/test/public/utils/recordedClient.ts
+++ sdk/maps/maps-route-rest/test/public/utils/recordedClient.ts
@@ -2,8 +2,13 @@
 // Licensed under the MIT license.
 
 import { Context } from "mocha";
-import { Recorder, RecorderStartOptions } from "@azure-tools/test-recorder";
+import { Recorder, RecorderStartOptions, env } from "@azure-tools/test-recorder";
 import "./env";
+import { createClientLogger } from "@azure/logger";
+import { AzureKeyCredential } from "@azure/core-auth";
+import createMapsRouteClient from "../../../src/mapsRouteClient";
+import { ClientOptions } from "@azure-rest/core-client";
+import { MapsRouteClient } from "../../../src/generated";
 
 const envSetupForPlayback: Record<string, string> = {
   ENDPOINT: "https://endpoint",
@@ -26,4 +31,11 @@ export async function createRecorder(context: Context): Promise<Recorder> {
   const recorder = new Recorder(context.currentTest);
   await recorder.start(recorderEnvSetup);
   return recorder;
+}
+
+export const testLogger = createClientLogger("route-test");
+
+export function createClient(options?: ClientOptions): MapsRouteClient {
+  const credential = new AzureKeyCredential(env["MAPS_SUBSCRIPTION_KEY"] ?? "");
+  return createMapsRouteClient(credential, options);
 }
