af9ee8fc9e3a274bb5e26d742e864712b4df0c95
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-11-03 10:53:14 +0800

    cognitiveservice release (#23648)
    * cognitiveservice release
    
    * update files

--- sdk/cognitiveservices/arm-cognitiveservices/samples/v7/javascript/resourceSkusListSample.js
+++ sdk/cognitiveservices/arm-cognitiveservices/samples/v7/javascript/resourceSkusListSample.js
@@ -15,7 +15,7 @@ const { DefaultAzureCredential } = require("@azure/identity");
  * This sample demonstrates how to Gets the list of Microsoft.CognitiveServices SKUs available for your Subscription.
  *
  * @summary Gets the list of Microsoft.CognitiveServices SKUs available for your Subscription.
- * x-ms-original-file: specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2022-03-01/examples/GetSkus.json
+ * x-ms-original-file: specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2022-10-01/examples/GetSkus.json
  */
 async function regenerateKeys() {
   const subscriptionId = "f1c637e4-72ec-4f89-8d2b-0f933c036002";
