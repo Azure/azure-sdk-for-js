9158f92b033f3e748f5bbd14586dff13322bd0de
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2022-11-04 21:34:38 -0700

    [Anomaly Detector] Enable CI and add tests (#23725)
    * Enable CI
    
    * Add changelog
    
    * Fix format
    
    * Fix package pipeline name
    
    * Set correct version
    
    * Fix env variable name
    
    * Re-record

--- sdk/anomalydetector/ai-anomaly-detector-rest/samples-dev/sample_multivariate_detection.ts
+++ sdk/anomalydetector/ai-anomaly-detector-rest/samples-dev/sample_multivariate_detection.ts
@@ -23,7 +23,6 @@ dotenv.config();
 // You will need to set this environment variables or edit the following values
 const apiKey = process.env["ANOMALY_DETECTOR_API_KEY"] || "";
 const endpoint = process.env["ANOMALY_DETECTOR_ENDPOINT"] || "";
-const apiVersion = "v1.1";
 const dataSource = "<your data source>";
 
 function sleep(time: number): Promise<NodeJS.Timer> {
@@ -33,7 +32,7 @@ function sleep(time: number): Promise<NodeJS.Timer> {
 export async function main() {
   // create client
   const credential = new AzureKeyCredential(apiKey);
-  const client = AnomalyDetector(endpoint, credential, { apiVersion });
+  const client = AnomalyDetector(endpoint, credential);
 
   // Already available models
   const options: ListMultivariateModelsParameters = {
