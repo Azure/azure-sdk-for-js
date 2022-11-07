4e64dd2a96bbd63d0f58b79d4666bfe6f7750de2
Matthew Podwysocki <matthewp@microsoft.com>
2022-11-07 12:55:56 -0500

    [kusto data] Initial commit

---
+++ sdk/kusto/kusto-data/src/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import KustoClient from "./source/client";
+import ClientRequestProperties from "./source/clientRequestProperties";
+import KustoConnectionStringBuilder from "./source/connectionBuilder";
+import * as KustoDataErrors from "./source/errors";
+
+export { KustoClient as Client, ClientRequestProperties, KustoConnectionStringBuilder, KustoDataErrors };
