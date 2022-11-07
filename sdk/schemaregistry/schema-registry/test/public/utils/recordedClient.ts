890a223d6bbeda9a5d7b91206ec52be1d66ce751
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2022-11-07 13:25:39 -0800

    re-record

--- sdk/schemaregistry/schema-registry/test/public/utils/recordedClient.ts
+++ sdk/schemaregistry/schema-registry/test/public/utils/recordedClient.ts
@@ -9,21 +9,23 @@ import {
 import { SchemaRegistryClient } from "../../../src";
 import { createTestCredential } from "@azure-tools/test-credential";
 
-export const recorderOptions: RecorderStartOptions = {
-  envSetupForPlayback: {
-    AZURE_CLIENT_ID: "azure_client_id",
-    AZURE_CLIENT_SECRET: "azure_client_secret",
-    AZURE_TENANT_ID: "azuretenantid",
-    SCHEMA_REGISTRY_ENDPOINT: "https://endpoint",
-    SCHEMA_REGISTRY_GROUP: "group-1",
-  },
-};
-
 export type Format = "Avro" | "json" | "custom";
 
 function getFQNSVarName(format: Format): string {
   return `SCHEMAREGISTRY_${format.toUpperCase()}_FULLY_QUALIFIED_NAMESPACE`;
 }
+
+export const recorderOptions: RecorderStartOptions = {
+  envSetupForPlayback: {
+    AZURE_CLIENT_ID: "azure_client_id",
+    AZURE_CLIENT_SECRET: "azure_client_secret",
+    AZURE_TENANT_ID: "azuretenantid",
+    [getFQNSVarName("Avro")]: "https://endpoint",
+    [getFQNSVarName("json")]: "https://endpoint",
+    [getFQNSVarName("custom")]: "https://endpoint",
+    SCHEMA_REGISTRY_GROUP: "group-1",
+  },
+};
 
 export function createRecordedClient(inputs: {
   recorder: Recorder;
