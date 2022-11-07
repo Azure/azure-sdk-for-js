8d4b3963fa386b9acd9d071b6b439c97b7db42ab
sima-zhu <sizhu@microsoft.com>
2022-11-07 12:48:09 -0800

    continue on error

--- eng/scripts/validate-docs-package.ps1
+++ eng/scripts/validate-docs-package.ps1
@@ -84,7 +84,7 @@ function DockerValidation() {
 
 function ValidateInRexTool() {
   $tempFolder = Join-Path ([System.IO.Path]::GetTempPath()) "rexOutput"
-  & 'type2docfx' $package $tempFolder
+  & 'type2docfx' $package $tempFolder -ErrorAction Continue
   $checkyamlFiles = Get-ChildItem $tempFolder
   for ($i=1; $i -le 10; $i++) {
     Write-Host $checkyamlFiles[$i].FullName
