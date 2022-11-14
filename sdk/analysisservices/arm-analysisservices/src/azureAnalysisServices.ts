ae1bb73c6c0bcd1c9ae72c3261d25e46438f0340
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-11-14 11:17:24 +0800

    update files

--- sdk/analysisservices/arm-analysisservices/src/azureAnalysisServices.ts
+++ sdk/analysisservices/arm-analysisservices/src/azureAnalysisServices.ts
@@ -57,16 +57,13 @@ export class AzureAnalysisServices extends coreClient.ServiceClient {
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
         : `${packageDetails}`;
 
-    if (!options.credentialScopes) {
-      options.credentialScopes = ["https://management.azure.com/.default"];
-    }
     const optionsWithDefaults = {
       ...defaults,
       ...options,
       userAgentOptions: {
         userAgentPrefix
       },
-      baseUri:
+      endpoint:
         options.endpoint ?? options.baseUri ?? "https://management.azure.com"
     };
     super(optionsWithDefaults);
@@ -92,7 +89,9 @@ export class AzureAnalysisServices extends coreClient.ServiceClient {
       this.pipeline.addPolicy(
         coreRestPipeline.bearerTokenAuthenticationPolicy({
           credential: credentials,
-          scopes: `${optionsWithDefaults.credentialScopes}`,
+          scopes:
+            optionsWithDefaults.credentialScopes ??
+            `${optionsWithDefaults.endpoint}/.default`,
           challengeCallbacks: {
             authorizeRequestOnChallenge:
               coreClient.authorizeRequestOnClaimChallenge
