edec5a62150ae2844fef0cc823a707a1e5a28ec8
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-11-18 17:14:56 +0800

    recording refresh 2 (#23884)
    * recording refresh 2
    
    * update files
    
    * update files
    
    * update files
    
    * update files
    
    * update files
    
    * update files
    
    * update files

--- sdk/botservice/arm-botservice/src/index.ts
+++ sdk/botservice/arm-botservice/src/index.ts
@@ -7,6 +7,7 @@
  */
 
 /// <reference lib="esnext.asynciterable" />
+export { getContinuationToken } from "./pagingHelper";
 export * from "./models";
 export { AzureBotService } from "./azureBotService";
 export * from "./operationsInterfaces";
