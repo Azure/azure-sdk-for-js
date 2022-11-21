7a6bdd519d239a62c3497b2ac79277860567ee16
azure-sdk <azuresdk@microsoft.com>
2022-11-21 06:19:48 +0000

    Post release automated changes for azure-arm-devcenter

--- sdk/devcenter/arm-devcenter/src/devCenterClient.ts
+++ sdk/devcenter/arm-devcenter/src/devCenterClient.ts
@@ -88,7 +88,7 @@ export class DevCenterClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-devcenter/1.0.0-beta.3`;
+    const packageDetails = `azsdk-js-arm-devcenter/1.0.0-beta.4`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
