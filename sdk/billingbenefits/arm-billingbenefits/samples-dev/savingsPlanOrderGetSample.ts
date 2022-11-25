b9da769f2bcc7b4a908d58124952e67840a2be1a
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-11-25 10:08:23 +0800

    billingbenefits release

--- sdk/billingbenefits/arm-billingbenefits/samples-dev/savingsPlanOrderGetSample.ts
+++ sdk/billingbenefits/arm-billingbenefits/samples-dev/savingsPlanOrderGetSample.ts
@@ -34,9 +34,10 @@ savingsPlanOrderGet().catch(console.error);
  * x-ms-original-file: specification/billingbenefits/resource-manager/Microsoft.BillingBenefits/stable/2022-11-01/examples/SavingsPlanOrderExpandedGet.json
  */
 async function savingsPlanOrderWithExpandedPaymentsGet() {
+  const expand = "schedule";
   const savingsPlanOrderId = "20000000-0000-0000-0000-000000000000";
   const credential = new DefaultAzureCredential();
-  const client = new BillingBenefitsRP(credential);
+  const client = new BillingBenefitsRP(credential, expand);
   const result = await client.savingsPlanOrder.get(savingsPlanOrderId);
   console.log(result);
 }
