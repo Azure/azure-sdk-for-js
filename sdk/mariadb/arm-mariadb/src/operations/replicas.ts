1353c1a21a87a40e7bba8254e3d8e45916bef798
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-11-29 16:53:21 +0800

    update files

--- sdk/mariadb/arm-mariadb/src/operations/replicas.ts
+++ sdk/mariadb/arm-mariadb/src/operations/replicas.ts
@@ -6,7 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { PagedAsyncIterableIterator } from "@azure/core-paging";
+import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
 import { Replicas } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
@@ -54,11 +54,15 @@ export class ReplicasImpl implements Replicas {
       [Symbol.asyncIterator]() {
         return this;
       },
-      byPage: () => {
+      byPage: (settings?: PageSettings) => {
+        if (settings?.maxPageSize) {
+          throw new Error("maxPageSize is not supported by this operation.");
+        }
         return this.listByServerPagingPage(
           resourceGroupName,
           serverName,
-          options
+          options,
+          settings
         );
       }
     };
@@ -67,13 +71,11 @@ export class ReplicasImpl implements Replicas {
   private async *listByServerPagingPage(
     resourceGroupName: string,
     serverName: string,
-    options?: ReplicasListByServerOptionalParams
+    options?: ReplicasListByServerOptionalParams,
+    _settings?: PageSettings
   ): AsyncIterableIterator<Server[]> {
-    let result = await this._listByServer(
-      resourceGroupName,
-      serverName,
-      options
-    );
+    let result: ReplicasListByServerResponse;
+    result = await this._listByServer(resourceGroupName, serverName, options);
     yield result.value || [];
   }
 
