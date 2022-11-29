dd0d1fd50f73c41e2e7c3a86ba51b50c88760979
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-11-29 11:08:52 +0800

    Devcenter re (#23964)
    * devcenter release
    
    * update files

--- sdk/devcenter/arm-devcenter/samples/v1-beta/javascript/projectEnvironmentTypesUpdateSample.js
+++ sdk/devcenter/arm-devcenter/samples/v1-beta/javascript/projectEnvironmentTypesUpdateSample.js
@@ -15,13 +15,13 @@ const { DefaultAzureCredential } = require("@azure/identity");
  * This sample demonstrates how to Partially updates a project environment type.
  *
  * @summary Partially updates a project environment type.
- * x-ms-original-file: specification/devcenter/resource-manager/Microsoft.DevCenter/preview/2022-10-12-preview/examples/ProjectEnvironmentTypes_Patch.json
+ * x-ms-original-file: specification/devcenter/resource-manager/Microsoft.DevCenter/preview/2022-11-11-preview/examples/ProjectEnvironmentTypes_Patch.json
  */
 async function projectEnvironmentTypesUpdate() {
-  const subscriptionId = "{subscriptionId}";
+  const subscriptionId = "0ac520ee-14c0-480f-b6c9-0a90c58ffff";
   const resourceGroupName = "rg1";
   const projectName = "ContosoProj";
-  const environmentTypeName = "{environmentTypeName}";
+  const environmentTypeName = "DevTest";
   const body = {
     deploymentTargetId: "/subscriptions/00000000-0000-0000-0000-000000000000",
     identity: {
