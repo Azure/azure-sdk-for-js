38bb2305f38a3eae9df9fab7a38bd8641db7cde0
azure-sdk <azuresdk@microsoft.com>
2022-11-30 06:15:53 +0000

    Post release automated changes for azure-arm-hybridkubernetes

--- sdk/hybridkubernetes/arm-hybridkubernetes/src/connectedKubernetesClient.ts
+++ sdk/hybridkubernetes/arm-hybridkubernetes/src/connectedKubernetesClient.ts
@@ -50,7 +50,7 @@ export class ConnectedKubernetesClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-hybridkubernetes/2.1.0`;
+    const packageDetails = `azsdk-js-arm-hybridkubernetes/2.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
