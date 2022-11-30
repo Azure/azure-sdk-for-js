94a501028f4fe98f6f3e4ce21f34475c0a4c6653
azure-sdk <azuresdk@microsoft.com>
2022-11-30 06:12:56 +0000

    Post release automated changes for azure-arm-healthcareapis

--- sdk/healthcareapis/arm-healthcareapis/src/healthcareApisManagementClient.ts
+++ sdk/healthcareapis/arm-healthcareapis/src/healthcareApisManagementClient.ts
@@ -78,7 +78,7 @@ export class HealthcareApisManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-healthcareapis/2.2.0`;
+    const packageDetails = `azsdk-js-arm-healthcareapis/2.2.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
