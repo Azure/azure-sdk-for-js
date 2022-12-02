21572d62bf5ac9667cc878b392b1f9343550dd55
Andrew Kao <andrewkao@microsoft.com>
2022-12-02 15:21:17 +0800

    [Maps] Onboarding Maps Geolocation (#23956)
    * Generate code through AutoRest
    
    * Add authentication layer
    
    * Add tests
    
    * Add built samples
    
    * Add recording
    
    * Refine README
    
    * Finalize
    
    * Fix several issues in README
    
    * Disable MS docs in samples

---
+++ sdk/maps/maps-geolocation-rest/src/generated/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import MapsGeolocationClient from "./mapsGeolocationClient";
+
+export * from "./mapsGeolocationClient";
+export * from "./parameters";
+export * from "./responses";
+export * from "./clientDefinitions";
+export * from "./isUnexpected";
+export * from "./outputModels";
+
+export default MapsGeolocationClient;
