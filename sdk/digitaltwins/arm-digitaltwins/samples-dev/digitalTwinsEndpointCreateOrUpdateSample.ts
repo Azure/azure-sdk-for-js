50ed951888383a3aed93b57be5f146ebee1786a9
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-12-05 10:10:45 +0800

    Digitaltwins re (#24061)
    * digitaltwins release
    
    * update files
    
    * update files

--- sdk/digitaltwins/arm-digitaltwins/samples-dev/digitalTwinsEndpointCreateOrUpdateSample.ts
+++ sdk/digitaltwins/arm-digitaltwins/samples-dev/digitalTwinsEndpointCreateOrUpdateSample.ts
@@ -18,7 +18,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * This sample demonstrates how to Create or update DigitalTwinsInstance endpoint.
  *
  * @summary Create or update DigitalTwinsInstance endpoint.
- * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-05-31/examples/DigitalTwinsEndpointPut_example.json
+ * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/DigitalTwinsEndpointPut_example.json
  */
 async function putADigitalTwinsEndpointResource() {
   const subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
@@ -55,7 +55,7 @@ putADigitalTwinsEndpointResource().catch(console.error);
  * This sample demonstrates how to Create or update DigitalTwinsInstance endpoint.
  *
  * @summary Create or update DigitalTwinsInstance endpoint.
- * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-05-31/examples/DigitalTwinsEndpointPut_WithIdentity_example.json
+ * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/DigitalTwinsEndpointPut_WithIdentity_example.json
  */
 async function putADigitalTwinsEndpointResourceWithIdentity() {
   const subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
@@ -85,3 +85,43 @@ async function putADigitalTwinsEndpointResourceWithIdentity() {
 }
 
 putADigitalTwinsEndpointResourceWithIdentity().catch(console.error);
+
+/**
+ * This sample demonstrates how to Create or update DigitalTwinsInstance endpoint.
+ *
+ * @summary Create or update DigitalTwinsInstance endpoint.
+ * x-ms-original-file: specification/digitaltwins/resource-manager/Microsoft.DigitalTwins/stable/2022-10-31/examples/DigitalTwinsEndpointPut_WithUserIdentity_example.json
+ */
+async function putADigitalTwinsEndpointResourceWithUserAssignedIdentity() {
+  const subscriptionId = "50016170-c839-41ba-a724-51e9df440b9e";
+  const resourceGroupName = "resRg";
+  const resourceName = "myDigitalTwinsService";
+  const endpointName = "myServiceBus";
+  const endpointDescription: DigitalTwinsEndpointResource = {
+    properties: {
+      authenticationType: "IdentityBased",
+      endpointType: "ServiceBus",
+      endpointUri: "sb://mysb.servicebus.windows.net/",
+      entityPath: "mysbtopic",
+      identity: {
+        type: "UserAssigned",
+        userAssignedIdentity:
+          "/subscriptions/50016170-c839-41ba-a724-51e9df440b9e/resourceGroups/testrg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/testidentity"
+      }
+    }
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new AzureDigitalTwinsManagementClient(
+    credential,
+    subscriptionId
+  );
+  const result = await client.digitalTwinsEndpoint.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    resourceName,
+    endpointName,
+    endpointDescription
+  );
+  console.log(result);
+}
+
+putADigitalTwinsEndpointResourceWithUserAssignedIdentity().catch(console.error);
