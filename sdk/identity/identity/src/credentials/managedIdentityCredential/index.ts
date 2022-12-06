0294db7282d7494a4b1ad4b3a68106057f54360a
Karishma Ghiya <kaghiya@microsoft.com>
2022-12-05 16:23:17 -0800

    remove token logging and update changelog

--- sdk/identity/identity/src/credentials/managedIdentityCredential/index.ts
+++ sdk/identity/identity/src/credentials/managedIdentityCredential/index.ts
@@ -252,7 +252,6 @@ export class ManagedIdentityCredential implements TokenCredential {
 
             if (resultToken) {
               logger.info(`SetAppTokenProvider has saved the token in cache`);
-              logger.info(`token = ${resultToken.token}`);
 
               const expiresInSeconds = resultToken?.expiresOnTimestamp
                 ? Math.floor((resultToken.expiresOnTimestamp - Date.now()) / 1000)
