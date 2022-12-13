556b46cfda4b115c770912cb19830e2299fce6b2
Natalija Ostojic <nostojic@microsoft.com>
2022-12-13 16:05:27 +0100

    Updating and adding tests for sip routing

--- sdk/communication/communication-phone-numbers/test/public/siprouting/deleteTrunk.spec.ts
+++ sdk/communication/communication-phone-numbers/test/public/siprouting/deleteTrunk.spec.ts
@@ -49,18 +49,12 @@ matrix([[true, false]], async function (useAad) {
         sipSignalingPort: 5678,
         enabled: true
       };
-      console.log("============ can delete an existing trunk");
       const storedTrunk = await client.setTrunk(trunk);
-      console.log("============ trunk set");
       assert.deepEqual(storedTrunk, trunk);
-      console.log("============ trunk deep equal");
       assert.exists((await client.getTrunks()).find((value) => value.fqdn === trunk.fqdn));
-      console.log("============ trunk exists");
-
       await client.deleteTrunk(testFqdn);
 
       assert.notExists((await client.getTrunks()).find((value) => value.fqdn === trunk.fqdn));
-      console.log("============ trunk doesn't exists");
     });
 
     it("cannot delete a not existing trunk but succeeds", async () => {
