0c180fc2f0d8e74fa002d043e9c6214f66f8230f
azure-sdk <azuresdk@microsoft.com>
2022-12-14 09:04:07 +0000

    Post release automated changes for azure-arm-serialconsole

--- sdk/serialconsole/arm-serialconsole/src/microsoftSerialConsoleClient.ts
+++ sdk/serialconsole/arm-serialconsole/src/microsoftSerialConsoleClient.ts
@@ -63,7 +63,7 @@ export class MicrosoftSerialConsoleClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-serialconsole/2.1.0`;
+    const packageDetails = `azsdk-js-arm-serialconsole/2.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
