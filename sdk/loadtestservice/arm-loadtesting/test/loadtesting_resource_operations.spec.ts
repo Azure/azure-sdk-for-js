8476d7c155336f729a90309e838eef92b4b0e08e
Venkat Ragavan <mail2venkat21@gmail.com>
2022-12-19 12:01:02 +0530

    updated js tests

--- sdk/loadtestservice/arm-loadtesting/test/loadtesting_resource_operations.spec.ts
+++ sdk/loadtestservice/arm-loadtesting/test/loadtesting_resource_operations.spec.ts
@@ -8,87 +8,136 @@
 
 import {
   env,
-  Recorder,
   RecorderStartOptions,
-  delay,
-  isPlaybackMode,
+  Recorder,
 } from "@azure-tools/test-recorder";
-import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
 import { Context } from "mocha";
 import { LoadTestClient } from "../src/loadTestClient";
+import { createTestCredential } from "@azure-tools/test-credential";
+import {
+  LoadTestResource,
+  LoadTestResourcePatchRequestBody,
+  QuotaBucketRequest,
+  QuotaBucketRequestPropertiesDimensions
+} from "../src/models";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
   AZURE_CLIENT_SECRET: "azure_client_secret",
   AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "00000000-0000-0000-0000-000000000000"
 };
 
 const recorderOptions: RecorderStartOptions = {
   envSetupForPlayback: replaceableVariables
 };
 
-export const testPollingOptions = {
-  updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
-};
-
-describe("LoadTesting test", () => {
+describe("Load Testing Resource Operations", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: LoadTestClient;
   let location: string;
-  let resourceGroup: string;
-  let loadTestName: string;
+  let resourceGroupName: string;
+  let loadTestResourceName: string;
+  let loadTestResourceCreatePayload: LoadTestResource;
+  let loadTestResourcePatchPayload: LoadTestResourcePatchRequestBody;
+
+  before(function () {
+    // Load test resource create payload
+    loadTestResourceCreatePayload = {
+      description: "New Load test resource from SDK.",
+      location: "westus2",
+      tags: { team: "Azure Load Testing SDK" },
+    };
+
+    // Load test resource patch payload
+    loadTestResourcePatchPayload = {
+      identity: {
+        type: 'SystemAssigned'
+      }
+    };
+
+    // Set the global variables to be used in the tests
+    subscriptionId = env.SUBSCRIPTION_ID || '00000000-0000-0000-0000-000000000000';
+    location = env.LOCATION || "westus2";
+    resourceGroupName = env.RESOURCE_GROUP || "js-sdk-test-rg";
+    loadTestResourceName = "loadtestsResource";
+  })
 
   beforeEach(async function (this: Context) {
     recorder = new Recorder(this.currentTest);
     await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
     const credential = createTestCredential();
     client = new LoadTestClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus";
-    resourceGroup = "myjstest";
-    loadTestName = "myLoadTest"
   });
 
   afterEach(async function () {
     await recorder.stop();
   });
 
-  it("loadTests create test", async function () {
-    const res = await client.loadTests.beginCreateOrUpdateAndWait(
-      resourceGroup,
-      loadTestName,
-      {
-        description: "This is new load test resource",
-        location,
-        tags: { team: "Dev Exp" }
-      },
-      testPollingOptions);
-    assert.equal(res.name, loadTestName);
+
+  it("create resource", async function () {
+    // Create a load test resource
+    const resource = await client.loadTests.beginCreateOrUpdateAndWait(
+      resourceGroupName,
+      loadTestResourceName,
+      loadTestResourceCreatePayload
+    );
+
+    // Verify the response
+    assert.equal(resource.provisioningState, "Succeeded");
+    assert.equal(resource.name, loadTestResourceName);
+    assert.equal(resource.location, location);
+    assert.equal(resource.tags?.team, loadTestResourceCreatePayload.tags?.team);
+    assert.equal(resource.description, loadTestResourceCreatePayload.description);
+    assert.equal(resource.identity?.type, "None");
   });
 
-  it("loadTests get test", async function () {
-    const res = await client.loadTests.get(resourceGroup, loadTestName);
-    assert.equal(res.name, loadTestName);
+  it("get resource", async function () {
+    // Get the load test resource
+    const resource = await client.loadTests.get(
+      resourceGroupName,
+      loadTestResourceName
+    );
+
+    // Verify the response
+    assert.equal(resource.provisioningState, "Succeeded");
+    assert.equal(resource.name, loadTestResourceName);
+    assert.equal(resource.location, location);
+    assert.equal(resource.tags?.team, loadTestResourceCreatePayload.tags?.team);
+    assert.equal(resource.description, loadTestResourceCreatePayload.description);
+    assert.equal(resource.identity?.type, "None");
   });
 
-  it("loadTests list test", async function () {
-    const resArray = new Array();
-    for await (let item of client.loadTests.listByResourceGroup(resourceGroup)) {
-      resArray.push(item);
-    }
-    assert.equal(resArray.length, 1);
+  it("patch resource", async function () {
+    // Patch the load test resource
+    const result = await client.loadTests.beginUpdateAndWait(
+      resourceGroupName,
+      loadTestResourceName,
+      loadTestResourcePatchPayload
+    );
+
+    // Get the load test resource
+    const patchedResource = await client.loadTests.get(
+      resourceGroupName,
+      loadTestResourceName
+    );
+
+    // Verify the response
+    assert.equal(patchedResource.provisioningState, "Succeeded");
+    assert.equal(patchedResource.name, loadTestResourceName);
+    assert.equal(patchedResource.location, location);
+    assert.equal(patchedResource.tags?.team, loadTestResourceCreatePayload.tags?.team);
+    assert.equal(patchedResource.description, loadTestResourceCreatePayload.description);
+    assert.equal(patchedResource.identity?.type, loadTestResourcePatchPayload.identity?.type);
   });
 
-  it("loadTests delete test", async function () {
-    const resArray = new Array();
-    const res = await client.loadTests.beginDeleteAndWait(resourceGroup, loadTestName)
-    for await (let item of client.loadTests.listByResourceGroup(resourceGroup)) {
-      resArray.push(item);
-    }
-    assert.equal(resArray.length, 0);
+  it("delete resource", async function () {
+    // Delete the load test resource
+    const result = await client.loadTests.beginDelete(
+      resourceGroupName,
+      loadTestResourceName
+    );
   });
-})
+});
