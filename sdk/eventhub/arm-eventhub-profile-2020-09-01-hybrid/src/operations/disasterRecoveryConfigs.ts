350424fcda2be5dae90720fab936afa5161dddf5
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-12-21 16:54:39 +0800

    recording refresh 22 (#24286)
    * recording refresh 22
    
    * update files
    
    * update files
    
    * update files
    
    * update files
    
    * update files

--- sdk/eventhub/arm-eventhub-profile-2020-09-01-hybrid/src/operations/disasterRecoveryConfigs.ts
+++ sdk/eventhub/arm-eventhub-profile-2020-09-01-hybrid/src/operations/disasterRecoveryConfigs.ts
@@ -6,7 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { PagedAsyncIterableIterator } from "@azure/core-paging";
+import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
+import { setContinuationToken } from "../pagingHelper";
 import { DisasterRecoveryConfigs } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
@@ -63,12 +64,16 @@ export class DisasterRecoveryConfigsImpl implements DisasterRecoveryConfigs {
       [Symbol.asyncIterator]() {
         return this;
       },
-      byPage: () => {
+      byPage: (settings?: PageSettings) => {
+        if (settings?.maxPageSize) {
+          throw new Error("maxPageSize is not supported by this operation.");
+        }
         return this.listAuthorizationRulesPagingPage(
           resourceGroupName,
           namespaceName,
           alias,
-          options
+          options,
+          settings
         );
       }
     };
@@ -78,16 +83,23 @@ export class DisasterRecoveryConfigsImpl implements DisasterRecoveryConfigs {
     resourceGroupName: string,
     namespaceName: string,
     alias: string,
-    options?: DisasterRecoveryConfigsListAuthorizationRulesOptionalParams
+    options?: DisasterRecoveryConfigsListAuthorizationRulesOptionalParams,
+    settings?: PageSettings
   ): AsyncIterableIterator<AuthorizationRule[]> {
-    let result = await this._listAuthorizationRules(
-      resourceGroupName,
-      namespaceName,
-      alias,
-      options
-    );
-    yield result.value || [];
-    let continuationToken = result.nextLink;
+    let result: DisasterRecoveryConfigsListAuthorizationRulesResponse;
+    let continuationToken = settings?.continuationToken;
+    if (!continuationToken) {
+      result = await this._listAuthorizationRules(
+        resourceGroupName,
+        namespaceName,
+        alias,
+        options
+      );
+      let page = result.value || [];
+      continuationToken = result.nextLink;
+      setContinuationToken(page, continuationToken);
+      yield page;
+    }
     while (continuationToken) {
       result = await this._listAuthorizationRulesNext(
         resourceGroupName,
@@ -97,7 +109,9 @@ export class DisasterRecoveryConfigsImpl implements DisasterRecoveryConfigs {
         options
       );
       continuationToken = result.nextLink;
-      yield result.value || [];
+      let page = result.value || [];
+      setContinuationToken(page, continuationToken);
+      yield page;
     }
   }
 
@@ -296,7 +310,6 @@ const listAuthorizationRulesNextOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.ErrorResponse
     }
   },
-  queryParameters: [Parameters.apiVersion1],
   urlParameters: [
     Parameters.$host,
     Parameters.subscriptionId,
