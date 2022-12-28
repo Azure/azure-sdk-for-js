513306343bb197ada58e4d6a9b974e650cb93185
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2022-12-28 10:20:08 +0800

    update files

--- sdk/avs/arm-avs/samples/v3/typescript/src/datastoresGetSample.ts
+++ sdk/avs/arm-avs/samples/v3/typescript/src/datastoresGetSample.ts
@@ -10,6 +10,9 @@
 // Licensed under the MIT License.
 import { AzureVMwareSolutionAPI } from "@azure/arm-avs";
 import { DefaultAzureCredential } from "@azure/identity";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * This sample demonstrates how to Get a datastore in a private cloud cluster
@@ -18,8 +21,10 @@ import { DefaultAzureCredential } from "@azure/identity";
  * x-ms-original-file: specification/vmware/resource-manager/Microsoft.AVS/stable/2022-05-01/examples/Datastores_Get.json
  */
 async function datastoresGet() {
-  const subscriptionId = "00000000-0000-0000-0000-000000000000";
-  const resourceGroupName = "group1";
+  const subscriptionId =
+    process.env["AVS_SUBSCRIPTION_ID"] ||
+    "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName = process.env["AVS_RESOURCE_GROUP"] || "group1";
   const privateCloudName = "cloud1";
   const clusterName = "cluster1";
   const datastoreName = "datastore1";
@@ -34,4 +39,8 @@ async function datastoresGet() {
   console.log(result);
 }
 
-datastoresGet().catch(console.error);
+async function main() {
+  datastoresGet();
+}
+
+main().catch(console.error);
