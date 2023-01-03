d1c0566ccc19ed15bd093bb9be26959aadc5d66f
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-01-03 10:21:14 +0800

    update files

--- sdk/consumption/arm-consumption/src/operations/usageDetails.ts
+++ sdk/consumption/arm-consumption/src/operations/usageDetails.ts
@@ -6,7 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { PagedAsyncIterableIterator } from "@azure/core-paging";
+import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
+import { setContinuationToken } from "../pagingHelper";
 import { UsageDetails } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
@@ -69,23 +70,35 @@ export class UsageDetailsImpl implements UsageDetails {
       [Symbol.asyncIterator]() {
         return this;
       },
-      byPage: () => {
-        return this.listPagingPage(scope, options);
+      byPage: (settings?: PageSettings) => {
+        if (settings?.maxPageSize) {
+          throw new Error("maxPageSize is not supported by this operation.");
+        }
+        return this.listPagingPage(scope, options, settings);
       }
     };
   }
 
   private async *listPagingPage(
     scope: string,
-    options?: UsageDetailsListOptionalParams
+    options?: UsageDetailsListOptionalParams,
+    settings?: PageSettings
   ): AsyncIterableIterator<UsageDetailUnion[]> {
-    let result = await this._list(scope, options);
-    yield result.value || [];
-    let continuationToken = result.nextLink;
+    let result: UsageDetailsListResponse;
+    let continuationToken = settings?.continuationToken;
+    if (!continuationToken) {
+      result = await this._list(scope, options);
+      let page = result.value || [];
+      continuationToken = result.nextLink;
+      setContinuationToken(page, continuationToken);
+      yield page;
+    }
     while (continuationToken) {
       result = await this._listNext(scope, continuationToken, options);
       continuationToken = result.nextLink;
-      yield result.value || [];
+      let page = result.value || [];
+      setContinuationToken(page, continuationToken);
+      yield page;
     }
   }
 
@@ -204,14 +217,6 @@ const listNextOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.ErrorResponse
     }
   },
-  queryParameters: [
-    Parameters.expand,
-    Parameters.filter,
-    Parameters.skiptoken,
-    Parameters.top,
-    Parameters.apiVersion,
-    Parameters.metric
-  ],
   urlParameters: [Parameters.$host, Parameters.scope, Parameters.nextLink],
   headerParameters: [Parameters.accept],
   serializer
