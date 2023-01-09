a22ff3426040cd2bf775810369877069657f3ddc
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-01-09 16:22:01 +0800

    update files

--- sdk/dnsresolver/arm-dnsresolver/samples/v1/typescript/src/dnsForwardingRulesetsGetSample.ts
+++ sdk/dnsresolver/arm-dnsresolver/samples/v1/typescript/src/dnsForwardingRulesetsGetSample.ts
@@ -10,6 +10,9 @@
 // Licensed under the MIT License.
 import { DnsResolverManagementClient } from "@azure/arm-dnsresolver";
 import { DefaultAzureCredential } from "@azure/identity";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * This sample demonstrates how to Gets a DNS forwarding ruleset properties.
@@ -18,8 +21,11 @@ import { DefaultAzureCredential } from "@azure/identity";
  * x-ms-original-file: specification/dnsresolver/resource-manager/Microsoft.Network/stable/2022-07-01/examples/DnsForwardingRuleset_Get.json
  */
 async function retrieveDnsForwardingRuleset() {
-  const subscriptionId = "abdd4249-9f34-4cc6-8e42-c2e32110603e";
-  const resourceGroupName = "sampleResourceGroup";
+  const subscriptionId =
+    process.env["DNSRESOLVER_SUBSCRIPTION_ID"] ||
+    "abdd4249-9f34-4cc6-8e42-c2e32110603e";
+  const resourceGroupName =
+    process.env["DNSRESOLVER_RESOURCE_GROUP"] || "sampleResourceGroup";
   const dnsForwardingRulesetName = "sampleDnsForwardingRuleset";
   const credential = new DefaultAzureCredential();
   const client = new DnsResolverManagementClient(credential, subscriptionId);
@@ -30,4 +36,8 @@ async function retrieveDnsForwardingRuleset() {
   console.log(result);
 }
 
-retrieveDnsForwardingRuleset().catch(console.error);
+async function main() {
+  retrieveDnsForwardingRuleset();
+}
+
+main().catch(console.error);
