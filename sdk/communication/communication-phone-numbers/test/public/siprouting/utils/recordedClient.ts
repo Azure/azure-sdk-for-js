da7089df99387c6bded08ffa38096c77b2672d3a
Natalija Ostojic <nostojic@microsoft.com>
2023-01-12 15:57:11 +0100

    Update broweser test recordings

--- sdk/communication/communication-phone-numbers/test/public/siprouting/utils/recordedClient.ts
+++ sdk/communication/communication-phone-numbers/test/public/siprouting/utils/recordedClient.ts
@@ -58,8 +58,14 @@ const sanitizerOptions: SanitizerOptions = {
       regex: true,
       target: `[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}`,
       value: `sanitized`,
-    },
+    },   
   ],
+  headerSanitizers: [
+    {
+      key: "Sec-Fetch-Dest",
+      value: "empty",
+    },
+  ]
 };
 
 const recorderOptions: RecorderStartOptions = {
