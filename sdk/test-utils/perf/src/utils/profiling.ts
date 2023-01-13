7992e009033e78beda0dcb501cea472ab17034e0
Harsha Nalluru <sanallur@microsoft.com>
2023-01-13 09:56:35 +0000

    more yml testing - profile name

--- sdk/test-utils/perf/src/utils/profiling.ts
+++ sdk/test-utils/perf/src/utils/profiling.ts
@@ -1,6 +1,14 @@
 import { Session } from "node:inspector";
 import fs from "fs";
 
+if (process.env.PREFIX_VALUE == undefined) {
+  process.env.PREFIX_VALUE = "1";
+  console.log(`PREFIX_VALUE: ${process.env.PREFIX_VALUE}`);
+} else {
+  process.env.PREFIX_VALUE = "2";
+  console.log(`PREFIX_VALUE: ${process.env.PREFIX_VALUE}`);
+}
+
 export async function runWithCpuProfile(functionToProfile: () => Promise<void>) {
   const session = new Session();
   session.connect();
@@ -12,7 +20,7 @@ export async function runWithCpuProfile(functionToProfile: () => Promise<void>) 
       session.post("Profiler.stop", (err, { profile }) => {
         // Write profile to disk, upload, etc.
         if (!err) {
-          const profileName = `./${getFormattedDate()}-perfProgram.cpuprofile`;
+          const profileName = `./${process.env.PREFIX_VALUE}-perfProgram.cpuprofile`;
           fs.writeFileSync(profileName, JSON.stringify(profile));
           console.log(`...CPUProfile saved to ${profileName}...`);
         } else {
@@ -22,5 +30,3 @@ export async function runWithCpuProfile(functionToProfile: () => Promise<void>) 
     });
   });
 }
-
-const getFormattedDate = () => { return new Date().toISOString().replaceAll(/[:\-.]/g, "_") }
