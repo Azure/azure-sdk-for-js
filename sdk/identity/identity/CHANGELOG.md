7362af1417c3f7642c4489fa5c0349853f7a7742
azure-sdk <azuresdk@microsoft.com>
2023-01-13 00:59:41 +0000

    Post release automated changes for azure-identity

--- sdk/identity/identity/CHANGELOG.md
+++ sdk/identity/identity/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
 
+## 3.1.4 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 3.1.3 (2023-01-12)
 
 ### Other Changes
--- sdk/identity/identity/src/constants.ts
+++ sdk/identity/identity/src/constants.ts
@@ -5,7 +5,7 @@
  * Current version of the `@azure/identity` package.
  */
 
-export const SDK_VERSION = `3.1.2`;
+export const SDK_VERSION = `3.1.4`;
 
 /**
  * The default client ID for authentication
