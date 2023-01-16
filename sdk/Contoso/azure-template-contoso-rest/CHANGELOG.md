6e5d4f8fb105195c733c20656a470c06e5f1e109
azure-sdk <no-reply@azure-sdk.com>
2023-01-16 09:22:29 +0000

    [auto]create by sdk generation

---
+++ sdk/Contoso/azure-template-contoso-rest/CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.0.0-beta.1 (2023-01-16)
+
+  - Initial Release
---
+++ sdk/Contoso/azure-template-contoso-rest/src/outputModels.ts
@@ -0,0 +1,89 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/** A widget. */
+export interface WidgetOutput {
+  /** The widget name. */
+  readonly name: string;
+  /** The ID of the manufacturer. */
+  manufacturerId: string;
+  /** The faked shared model. */
+  sharedModel?: FakedSharedModelOutput;
+}
+
+/** Faked shared model */
+export interface FakedSharedModelOutput {
+  /** The tag. */
+  tag: string;
+  /** The created date. */
+  createdDate: string;
+}
+
+/** A response containing error details. */
+export interface ErrorResponseOutput {
+  /** The error object. */
+  error: ErrorModelOutput;
+}
+
+/** The error object. */
+export interface ErrorModelOutput {
+  /** One of a server-defined set of error codes. */
+  code: string;
+  /** A human-readable representation of the error. */
+  message: string;
+  /** The target of the error. */
+  target?: string;
+  /** An array of details about specific errors that led to this reported error. */
+  details: Array<ErrorModelOutput>;
+  /** An object containing more specific information than the current object about the error. */
+  innererror?: InnerErrorOutput;
+}
+
+/** An object containing more specific information about the error. As per Microsoft One API guidelines - https://github.com/Microsoft/api-guidelines/blob/vNext/Guidelines.md#7102-error-condition-responses. */
+export interface InnerErrorOutput {
+  /** One of a server-defined set of error codes. */
+  code: string;
+  /** Inner error. */
+  innererror?: InnerErrorOutput;
+}
+
+/** Provides status details for long running operations. */
+export interface ResourceOperationStatusOutput {
+  /** The unique ID of the operation. */
+  id: string;
+  /**
+   * The status of the operation
+   *
+   * Possible values: InProgress, Succeeded, Failed, Canceled
+   */
+  status: string;
+  /** Error object that describes the error when status is "Failed". */
+  error?: ErrorModelOutput;
+  /** The result of the operation. */
+  result?: WidgetOutput;
+}
+
+/** Provides status details for long running operations. */
+export interface OperationStatusOutput {
+  /** The unique ID of the operation. */
+  id: string;
+  /**
+   * The status of the operation
+   *
+   * Possible values: InProgress, Succeeded, Failed, Canceled
+   */
+  status: string;
+  /** Error object that describes the error when status is "Failed". */
+  error?: ErrorModelOutput;
+}
+
+/** Provides the most common query parameters for list operations. */
+export interface StandardListQueryParametersOutput {}
+
+/** Paged collection of Widget items */
+export interface WidgetListOutput {
+  /** The Widget items on this page */
+  value: Array<WidgetOutput>;
+  /** The link to the next page of items */
+  nextLink?: string;
+}
