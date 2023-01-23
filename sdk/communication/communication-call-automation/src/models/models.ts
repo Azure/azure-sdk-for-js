80122a42878493526c27ee589ad615ce82d67560
juntuchen <juntuchen@microsoft.com>
2023-01-23 21:43:01 +0800

    added main client methods

--- sdk/communication/communication-call-automation/src/models/models.ts
+++ sdk/communication/communication-call-automation/src/models/models.ts
@@ -1,2 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+
+import { CommunicationIdentifier, PhoneNumberIdentifier } from "@azure/communication-common";
+
+/** The caller. */
+export interface CallSource {
+    /**
+    * The source caller Id, a phone number, that's shown to the PSTN participant being invited.
+    * Required only when calling a PSTN callee.
+    */
+    callerId?: PhoneNumberIdentifier;
+    /** Display name of the call if dialing out to a pstn number */
+    displayName?: string;
+    /** The identifier of the source of the call */
+    identifier: CommunicationIdentifier;
+}
--- sdk/communication/communication-call-automation/src/models/options.ts
+++ sdk/communication/communication-call-automation/src/models/options.ts
@@ -5,6 +5,29 @@ import { OperationOptions } from "@azure/core-client";
 import { CallRejectReason } from "../generated/src/models/index";
 
 /**
+ * Options to create a call.
+ */
+export interface CreateCallOptions extends OperationOptions {
+    /** The operation context. */
+    operationContext?: string;
+    /** The Azure cognitive services end point url. */
+    azureCognitiveServicesEndpointUrl?: string;
+}
+
+/**
+ * Options to answer a call.
+ */
+export interface AnswerCallOptions extends OperationOptions {
+    /** The Azure cognitive services end point url. */
+    azureCognitiveServicesEndpointUrl?: string;
+}
+
+/**
+ * Options to redirect call.
+ */
+export type RedirectCallOptions = OperationOptions;
+
+/**
  * Options to reject call.
  */
 export interface RejectCallOptions extends OperationOptions {
