66c5b05cef80da4fe1316759a76476d6abe5f09f
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-01-29 16:32:51 +0800

    update files

--- sdk/monitor/arm-monitor/samples-dev/diagnosticSettingsCategoryGetSample.ts
+++ sdk/monitor/arm-monitor/samples-dev/diagnosticSettingsCategoryGetSample.ts
@@ -10,6 +10,9 @@
 // Licensed under the MIT License.
 import { MonitorClient } from "@azure/arm-monitor";
 import { DefaultAzureCredential } from "@azure/identity";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * This sample demonstrates how to Gets the diagnostic settings category for the specified resource.
@@ -18,7 +21,9 @@ import { DefaultAzureCredential } from "@azure/identity";
  * x-ms-original-file: specification/monitor/resource-manager/Microsoft.Insights/preview/2021-05-01-preview/examples/getDiagnosticSettingsCategory.json
  */
 async function getsTheDiagnosticSetting() {
-  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const subscriptionId =
+    process.env["MONITOR_SUBSCRIPTION_ID"] ||
+    "00000000-0000-0000-0000-000000000000";
   const resourceUri =
     "subscriptions/1a66ce04-b633-4a0b-b2bc-a912ec8986a6/resourcegroups/viruela1/providers/microsoft.logic/workflows/viruela6";
   const name = "WorkflowRuntime";
@@ -28,4 +33,8 @@ async function getsTheDiagnosticSetting() {
   console.log(result);
 }
 
-getsTheDiagnosticSetting().catch(console.error);
+async function main() {
+  getsTheDiagnosticSetting();
+}
+
+main().catch(console.error);
