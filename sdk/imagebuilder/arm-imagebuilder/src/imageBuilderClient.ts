5c1341466158baa4c2a160c2cd0d561bbce0fa3d
azure-sdk <azuresdk@microsoft.com>
2023-01-29 10:27:03 +0000

    Post release automated changes for azure-arm-imagebuilder

--- sdk/imagebuilder/arm-imagebuilder/src/imageBuilderClient.ts
+++ sdk/imagebuilder/arm-imagebuilder/src/imageBuilderClient.ts
@@ -54,7 +54,7 @@ export class ImageBuilderClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-imagebuilder/2.1.0`;
+    const packageDetails = `azsdk-js-arm-imagebuilder/2.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
