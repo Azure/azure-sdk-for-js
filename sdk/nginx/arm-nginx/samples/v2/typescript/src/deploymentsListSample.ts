ec4a84fcd96dd6692af71e04a46e2e3273fa342b
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-01-30 10:03:50 +0800

    refresh 13

--- sdk/nginx/arm-nginx/samples/v2/typescript/src/deploymentsListSample.ts
+++ sdk/nginx/arm-nginx/samples/v2/typescript/src/deploymentsListSample.ts
@@ -10,6 +10,9 @@
 // Licensed under the MIT License.
 import { NginxManagementClient } from "@azure/arm-nginx";
 import { DefaultAzureCredential } from "@azure/identity";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * This sample demonstrates how to List the Nginx deployments resources
@@ -18,7 +21,9 @@ import { DefaultAzureCredential } from "@azure/identity";
  * x-ms-original-file: specification/nginx/resource-manager/NGINX.NGINXPLUS/stable/2022-08-01/examples/Deployments_List.json
  */
 async function deploymentsList() {
-  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const subscriptionId =
+    process.env["NGINX_SUBSCRIPTION_ID"] ||
+    "00000000-0000-0000-0000-000000000000";
   const credential = new DefaultAzureCredential();
   const client = new NginxManagementClient(credential, subscriptionId);
   const resArray = new Array();
@@ -28,4 +33,8 @@ async function deploymentsList() {
   console.log(resArray);
 }
 
-deploymentsList().catch(console.error);
+async function main() {
+  deploymentsList();
+}
+
+main().catch(console.error);
