0ee923520ec15ef4d678d8e604729d82029b22aa
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-01-31 16:34:24 +0800

    update files

--- sdk/recoveryservices/arm-recoveryservices/samples/v5/typescript/src/privateLinkResourcesListSample.ts
+++ sdk/recoveryservices/arm-recoveryservices/samples/v5/typescript/src/privateLinkResourcesListSample.ts
@@ -10,6 +10,9 @@
 // Licensed under the MIT License.
 import { RecoveryServicesClient } from "@azure/arm-recoveryservices";
 import { DefaultAzureCredential } from "@azure/identity";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * This sample demonstrates how to Returns the list of private link resources that need to be created for Backup and SiteRecovery
@@ -18,8 +21,11 @@ import { DefaultAzureCredential } from "@azure/identity";
  * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2022-04-01/examples/ListPrivateLinkResources.json
  */
 async function listPrivateLinkResources() {
-  const subscriptionId = "6c48fa17-39c7-45f1-90ac-47a587128ace";
-  const resourceGroupName = "petesting";
+  const subscriptionId =
+    process.env["RECOVERYSERVICES_SUBSCRIPTION_ID"] ||
+    "6c48fa17-39c7-45f1-90ac-47a587128ace";
+  const resourceGroupName =
+    process.env["RECOVERYSERVICES_RESOURCE_GROUP"] || "petesting";
   const vaultName = "pemsi-ecy-rsv2";
   const credential = new DefaultAzureCredential();
   const client = new RecoveryServicesClient(credential, subscriptionId);
@@ -33,4 +39,8 @@ async function listPrivateLinkResources() {
   console.log(resArray);
 }
 
-listPrivateLinkResources().catch(console.error);
+async function main() {
+  listPrivateLinkResources();
+}
+
+main().catch(console.error);
--- sdk/recoveryservices/arm-recoveryservices/src/index.ts
+++ sdk/recoveryservices/arm-recoveryservices/src/index.ts
@@ -7,6 +7,7 @@
  */
 
 /// <reference lib="esnext.asynciterable" />
+export { getContinuationToken } from "./pagingHelper";
 export * from "./models";
 export { RecoveryServicesClient } from "./recoveryServicesClient";
 export * from "./operationsInterfaces";
