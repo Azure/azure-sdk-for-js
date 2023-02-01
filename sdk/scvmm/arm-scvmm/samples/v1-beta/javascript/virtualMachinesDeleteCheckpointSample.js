4ca80a8ddf1a92433c3699d4dd077d18954d746a
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-02-01 14:13:49 +0800

    update files

--- sdk/scvmm/arm-scvmm/samples/v1-beta/javascript/virtualMachinesDeleteCheckpointSample.js
+++ sdk/scvmm/arm-scvmm/samples/v1-beta/javascript/virtualMachinesDeleteCheckpointSample.js
@@ -10,6 +10,7 @@
 // Licensed under the MIT License.
 const { Scvmm } = require("@azure/arm-scvmm");
 const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv").config();
 
 /**
  * This sample demonstrates how to Deletes a checkpoint in virtual machine.
@@ -18,8 +19,9 @@ const { DefaultAzureCredential } = require("@azure/identity");
  * x-ms-original-file: specification/scvmm/resource-manager/Microsoft.ScVmm/preview/2020-06-05-preview/examples/DeleteCheckpointVirtualMachine.json
  */
 async function deleteCheckpointVirtualMachine() {
-  const subscriptionId = "fd3c3665-1729-4b7b-9a38-238e83b0f98b";
-  const resourceGroupName = "testrg";
+  const subscriptionId =
+    process.env["SCVMM_SUBSCRIPTION_ID"] || "fd3c3665-1729-4b7b-9a38-238e83b0f98b";
+  const resourceGroupName = process.env["SCVMM_RESOURCE_GROUP"] || "testrg";
   const virtualMachineName = "DemoVM";
   const body = { id: "Demo CheckpointID" };
   const options = { body };
@@ -33,4 +35,8 @@ async function deleteCheckpointVirtualMachine() {
   console.log(result);
 }
 
-deleteCheckpointVirtualMachine().catch(console.error);
+async function main() {
+  deleteCheckpointVirtualMachine();
+}
+
+main().catch(console.error);
