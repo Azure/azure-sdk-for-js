6a76e6ac3f67fdf7b2d22c7b3a242538b0c60f54
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-02-02 10:13:33 +0800

    refresh 14 (#24625)
    * refresh 14
    
    * update files
    
    * update files
    
    * update files
    
    * update files

--- sdk/recoveryservices/arm-recoveryservices/CHANGELOG.md
+++ sdk/recoveryservices/arm-recoveryservices/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
     
+## 5.1.1 (2023-01-31)
+
+**Features**
+
+  - Exposes `getContinuationToken` helper function to extract continuation token
+  
+**Bugs Fixed**
+
+  - A series of small bug fixs relevant to authentication and apiVersion policy
+
 ## 5.1.0 (2022-07-22)
     
 **Features**
--- sdk/recoveryservices/arm-recoveryservices/samples/v5/javascript/vaultsDeleteSample.js
+++ sdk/recoveryservices/arm-recoveryservices/samples/v5/javascript/vaultsDeleteSample.js
@@ -10,6 +10,7 @@
 // Licensed under the MIT License.
 const { RecoveryServicesClient } = require("@azure/arm-recoveryservices");
 const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv").config();
 
 /**
  * This sample demonstrates how to Deletes a vault.
@@ -18,8 +19,10 @@ const { DefaultAzureCredential } = require("@azure/identity");
  * x-ms-original-file: specification/recoveryservices/resource-manager/Microsoft.RecoveryServices/stable/2022-04-01/examples/DeleteVault.json
  */
 async function deleteRecoveryServicesVault() {
-  const subscriptionId = "77777777-b0c6-47a2-b37c-d8e65a629c18";
-  const resourceGroupName = "Default-RecoveryServices-ResourceGroup";
+  const subscriptionId =
+    process.env["RECOVERYSERVICES_SUBSCRIPTION_ID"] || "77777777-b0c6-47a2-b37c-d8e65a629c18";
+  const resourceGroupName =
+    process.env["RECOVERYSERVICES_RESOURCE_GROUP"] || "Default-RecoveryServices-ResourceGroup";
   const vaultName = "swaggerExample";
   const credential = new DefaultAzureCredential();
   const client = new RecoveryServicesClient(credential, subscriptionId);
@@ -27,4 +30,8 @@ async function deleteRecoveryServicesVault() {
   console.log(result);
 }
 
-deleteRecoveryServicesVault().catch(console.error);
+async function main() {
+  deleteRecoveryServicesVault();
+}
+
+main().catch(console.error);
