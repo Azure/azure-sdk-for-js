e6ee8551d977243fd3e3d135dc7fddfa5d2bef75
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-02-02 15:31:41 +0800

    update files

--- sdk/support/arm-support/src/operations/supportTickets.ts
+++ sdk/support/arm-support/src/operations/supportTickets.ts
@@ -6,7 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { PagedAsyncIterableIterator } from "@azure/core-paging";
+import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
+import { setContinuationToken } from "../pagingHelper";
 import { SupportTickets } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
@@ -18,10 +19,10 @@ import {
   SupportTicketDetails,
   SupportTicketsListNextOptionalParams,
   SupportTicketsListOptionalParams,
+  SupportTicketsListResponse,
   CheckNameAvailabilityInput,
   SupportTicketsCheckNameAvailabilityOptionalParams,
   SupportTicketsCheckNameAvailabilityResponse,
-  SupportTicketsListResponse,
   SupportTicketsGetOptionalParams,
   SupportTicketsGetResponse,
   UpdateSupportTicket,
@@ -64,22 +65,34 @@ export class SupportTicketsImpl implements SupportTickets {
       [Symbol.asyncIterator]() {
         return this;
       },
-      byPage: () => {
-        return this.listPagingPage(options);
+      byPage: (settings?: PageSettings) => {
+        if (settings?.maxPageSize) {
+          throw new Error("maxPageSize is not supported by this operation.");
+        }
+        return this.listPagingPage(options, settings);
       }
     };
   }
 
   private async *listPagingPage(
-    options?: SupportTicketsListOptionalParams
+    options?: SupportTicketsListOptionalParams,
+    settings?: PageSettings
   ): AsyncIterableIterator<SupportTicketDetails[]> {
-    let result = await this._list(options);
-    yield result.value || [];
-    let continuationToken = result.nextLink;
+    let result: SupportTicketsListResponse;
+    let continuationToken = settings?.continuationToken;
+    if (!continuationToken) {
+      result = await this._list(options);
+      let page = result.value || [];
+      continuationToken = result.nextLink;
+      setContinuationToken(page, continuationToken);
+      yield page;
+    }
     while (continuationToken) {
       result = await this._listNext(continuationToken, options);
       continuationToken = result.nextLink;
-      yield result.value || [];
+      let page = result.value || [];
+      setContinuationToken(page, continuationToken);
+      yield page;
     }
   }
 
@@ -422,7 +435,6 @@ const listNextOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.ExceptionResponse
     }
   },
-  queryParameters: [Parameters.apiVersion, Parameters.top, Parameters.filter],
   urlParameters: [
     Parameters.$host,
     Parameters.subscriptionId,
