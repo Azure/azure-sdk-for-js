1dd647fb3891d06a4b98afb68a9adda972dd817a
Jeremy Meng <yumeng@microsoft.com>
2023-02-03 00:23:52 +0000

    [engsys] do not build the `ts-genapi`
    The installed one is ready for use. Trying to build it would result in compiler
    errors as its devDependencies are not installed.

--- eng/scripts/Generate-APIView-CodeFile.ps1
+++ eng/scripts/Generate-APIView-CodeFile.ps1
@@ -35,7 +35,6 @@ if (!(Test-Path -Path $installedPath))
 
 Write-Host "Setting working directory to $($installedPath)"
 Set-Location $installedPath
-npm run-script build
 
 $apiFiles = Get-ChildItem -Path $ArtifactPath -Recurse -Filter "*.api.json"
 foreach ($apiPkgFile in $apiFiles)
