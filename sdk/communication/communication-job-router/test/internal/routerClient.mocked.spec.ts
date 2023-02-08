3e66e4890ae990a490bef03461c249070fc11ac3
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-02-08 17:06:13 -0600

    fix: remove mocha arrow functions

--- sdk/communication/communication-job-router/test/internal/routerClient.mocked.spec.ts
+++ sdk/communication/communication-job-router/test/internal/routerClient.mocked.spec.ts
@@ -6,12 +6,12 @@ import { AzureCommunicationTokenCredential } from "@azure/communication-common";
 import { RouterClient } from "../../src";
 import { baseUri, generateToken } from "../public/utils/connectionUtils";
 
-describe("[Mocked] RouterClient", async () => {
-  afterEach(() => {
+describe("[Mocked] RouterClient", async function() {
+  afterEach(function() {
     sinon.restore();
   });
 
-  it("can instantiate", async () => {
+  it("can instantiate", async function() {
     new RouterClient(baseUri, new AzureCommunicationTokenCredential(generateToken()));
   });
 });
