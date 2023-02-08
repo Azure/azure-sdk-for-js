8e0ae2e8730101c6f6e84be7f5ad1cd73535f075
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2023-02-08 17:15:29 +0000

    update expectations for healthcare

--- sdk/cognitivelanguage/ai-language-text/test/public/analyzeBatch.spec.ts
+++ sdk/cognitivelanguage/ai-language-text/test/public/analyzeBatch.spec.ts
@@ -408,7 +408,7 @@ matrix([["APIKey", "AAD"]] as const, async (authMethod: AuthMethod) => {
             await assertActionsResults(await poller.pollUntilDone(), expectation29);
           });
 
-          it.only("abstractive summarization", async function () {
+          it("abstractive summarization", async function () {
             const docs = [windows365ArticlePart1, windows365ArticlePart2];
             const poller = await client.beginAnalyzeBatch(
               [
