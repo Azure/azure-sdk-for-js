9ab7ae672bb065105ffb4120b19d17cd51b11c79
Jing Wang <jiwang6@microsoft.com>
2023-02-11 23:14:18 +0800

    Add more test case

--- sdk/purview/purview-workflow-rest/test/public/workflow.spec.ts
+++ sdk/purview/purview-workflow-rest/test/public/workflow.spec.ts
@@ -3,11 +3,12 @@
 
 // import { PurviewWorkflowClient } from "@azure-rest/purview-workflow";
 import { Recorder } from "@azure-tools/test-recorder";
-import { assert } from "chai";
 import { createClient } from "./utils/recordedClient";
 
 import { Context } from "mocha";
 import { PurviewWorkflowClient } from "../../src/clientDefinitions";
+import { CreateOrReplaceWorkflowParameters } from "../../src/parameters";
+import { isUnexpected } from "../../src/isUnexpected";
 
 describe("Get a workflow", () => {
   let recorder: Recorder;
@@ -17,17 +18,105 @@ describe("Get a workflow", () => {
   beforeEach(async function(this: Context) {
     recorder = new Recorder(this.currentTest);
     client = await createClient(recorder);
-    workflowId = "669ab714-1900-47a9-8346-060358f26623";
+    workflowId = "e3467b48-a9d8-11ed-afa1-0242ac120002";
   });
 
   afterEach(async function() {
     await recorder.stop();
   });
 
+  it("should create a workflow", async function() {
+    const options: CreateOrReplaceWorkflowParameters = {
+    body: {
+      name: "Create glossary term workflow",
+      description: "",
+      actionDag: {
+        actions: {
+          Condition: {
+            type: "If",
+            actions: {
+              "Create glossary term": { type: "CreateTerm", runAfter: {} },
+              "Send email notification": {
+                type: "EmailNotification",
+                inputs: {
+                  parameters: {
+                    emailMessage:
+                      "Your request for Glossary Term @{triggerBody()['request']['term']['name']} is approved.",
+                    emailRecipients: [
+                      "@{triggerBody()['request']['requestor']}"
+                    ],
+                    emailSubject: "Glossary Term Create - APPROVED"
+                  }
+                },
+                runAfter: { "Create glossary term": ["Succeeded"] }
+              }
+            },
+            else: {
+              actions: {
+                "Send reject email notification": {
+                  type: "EmailNotification",
+                  inputs: {
+                    parameters: {
+                      emailMessage:
+                        "Your request for Glossary Term @{triggerBody()['request']['term']['name']} is rejected.",
+                      emailRecipients: [
+                        "@{triggerBody()['request']['requestor']}"
+                      ],
+                      emailSubject: "Glossary Term Create - REJECTED"
+                    }
+                  },
+                  runAfter: {}
+                }
+              }
+            },
+            expression: {
+              and: [
+                {
+                  equals: [
+                    "@outputs('Start and wait for an approval')['body/outcome']",
+                    "Approved"
+                  ]
+                }
+              ]
+            },
+            runAfter: { "Start and wait for an approval": ["Succeeded"] }
+          },
+          "Start and wait for an approval": {
+            type: "Approval",
+            inputs: {
+              parameters: {
+                approvalType: "PendingOnAll",
+                assignedTo: ["83ffc40f-f670-49b0-a17e-30c7f74f209a"],
+                title: "Approval Request for Create Glossary Term"
+              }
+            },
+            runAfter: {}
+          }
+        }
+      },
+      isEnabled: true,
+      triggers: [
+        {
+          type: "when_term_creation_is_requested",
+          underGlossaryHierarchy:
+            "/glossaries/5dae5e5b-5aa6-48f1-9e46-26fe7328de71"
+        }
+      ]
+    }
+  };
+  const createResult = await client
+  .path("/workflows/{workflowId}", workflowId)
+  .put(options);
+
+      if (isUnexpected(createResult)) {
+      throw createResult.body.error;
+    }
+  });
+
   it("should get a workflow", async function() {
-    const result = await client.path("/workflows/{workflowId}", workflowId).get();
-    if (result.status !== "200") {
-      assert.fail(`GET "/workflows/{workflowId} failed with ${result.status}`);
+    const getResult = await client.path("/workflows/{workflowId}", workflowId).get();
+          if (isUnexpected(getResult)) {
+      throw getResult.body.error;
     }
   });
 });
