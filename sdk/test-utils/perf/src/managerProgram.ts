2c92f5ac6d9406c0a02dccc7526d818e6dc0edf6
Harsha Nalluru <sanallur@microsoft.com>
2023-02-14 01:14:32 +0000

    format

--- sdk/test-utils/perf/src/managerProgram.ts
+++ sdk/test-utils/perf/src/managerProgram.ts
@@ -95,10 +95,10 @@ export class ManagerPerfProgram implements PerfProgram {
       `Completed ${totalOperations.toLocaleString(undefined, {
         maximumFractionDigits: 0,
       })} ` +
-      `operations in a weighted-average of ` +
-      `${formatNumber(weightedAverage, 4)}s ` +
-      `(${formatNumber(operationsPerSecond, 4)} ops/s, ` +
-      `${formatNumber(secondsPerOperation, 4)} s/op)`
+        `operations in a weighted-average of ` +
+        `${formatNumber(weightedAverage, 4)}s ` +
+        `(${formatNumber(operationsPerSecond, 4)} ops/s, ` +
+        `${formatNumber(secondsPerOperation, 4)} s/op)`
     );
   }
 
@@ -120,7 +120,7 @@ export class ManagerPerfProgram implements PerfProgram {
     const parallels = this.parsedOptions.parallel.value ?? 1;
 
     if (this.parsedOptions.profile.value) {
-      // Overriding to 1 core 
+      // Overriding to 1 core
       // since there is no point in observing profiling artifacts of all the cores that do the same thing
       cpuOption = 1;
     }
@@ -166,7 +166,8 @@ export class ManagerPerfProgram implements PerfProgram {
     // of operations running.
     const millisecondsToLog = Number(this.parsedOptions["milliseconds-to-log"].value);
     console.log(
-      `\n=== ${title} mode, iteration ${iterationIndex + 1}. Logs every ${millisecondsToLog / 1000
+      `\n=== ${title} mode, iteration ${iterationIndex + 1}. Logs every ${
+        millisecondsToLog / 1000
       }s ===`
     );
     console.log(`ElapsedTime\tCurrent\t\tTotal\t\tAverage`);
