2b13a045f4d04214548d94a57827d235aed592e6
Matthew Podwysocki <matthewp@microsoft.com>
2023-02-15 14:28:53 -0500

    [identity] Add WorkloadIdentityCredential

---
+++ sdk/identity/identity/samples-dev/workloadIdentityCredential.ts
@@ -0,0 +1,36 @@
+import { 
+  DefaultAzureCredential, 
+  WorkloadIdentityCredential 
+} from "@azure/identity";
+
+// Load the .env file if it exists
+require("dotenv").config();
+
+async function testDefaultCredential() {
+  const credential = new DefaultAzureCredential();
+
+  const token = await credential.getToken("https://storage.azure.com/.default");
+
+  console.log(token);
+}
+
+async function testWorkloadCredential() {
+  const credential = new WorkloadIdentityCredential(
+    process.env.AZURE_TENANT_ID!,
+    process.env.AZURE_CLIENT_ID!,
+    process.env.AZURE_FEDERATED_TOKEN_FILE_PATH!
+  );
+
+  const token = await credential.getToken("https://storage.azure.com/.default");
+
+  console.log(token);
+}
+
+async function main() {
+  await testDefaultCredential();
+  await testWorkloadCredential();
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
