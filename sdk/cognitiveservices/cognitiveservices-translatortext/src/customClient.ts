857b7ab528db483ba05e24c0ab45f39e748deeab
Michal Materna <mimat@microsoft.com>
2023-02-20 13:08:10 -0800

    Initial commit

---
+++ sdk/cognitiveservices/cognitiveservices-translatortext/src/customClient.ts
@@ -0,0 +1,96 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { getClient, ClientOptions } from "@azure-rest/core-client";
+import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
+import { TextTranslationClient } from "./generated/clientDefinitions";
+import {
+  TranslatorCredential,
+  TranslatorAuthenticationPolicy,
+  TranslatorCustomEndpoint,
+  TranslatorAzureKeyAuthenticationPolicy
+} from "./authentication";
+import {
+  AzureKeyCredential,
+  TokenCredential
+} from "@azure/core-auth";
+
+const DEFAULT_SCOPE = "https://cognitiveservices.azure.com/.default";
+
+/**
+ * Initialize a new instance of `TextTranslationClient`
+ * @param endpoint type: string, Supported Text Translation endpoints (protocol and hostname, for example:
+ *     https://api.cognitive.microsofttranslator.com).
+ * @param options type: ClientOptions, the parameter for all optional parameters
+ */
+export default function createClient(
+  endpoint: string | TranslatorCustomEndpoint,
+  credential: undefined | TranslatorCredential | AzureKeyCredential | TokenCredential = undefined,
+  options: ClientOptions = {}
+): TextTranslationClient {
+  let serviceEndpoint: string;
+  if (endpoint instanceof TranslatorCustomEndpoint) {
+    serviceEndpoint = `${endpoint.endpoint}/translator/text/v3.0`;
+  } else {
+    serviceEndpoint = endpoint;
+  }
+
+  const baseUrl = options.baseUrl ?? `${serviceEndpoint}`;
+
+  const userAgentInfo = `azsdk-js-cognitiveservices-translator-rest/1.0.0-beta.1`;
+  const userAgentPrefix =
+    options.userAgentOptions && options.userAgentOptions.userAgentPrefix
+      ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
+      : `${userAgentInfo}`;
+  options = {
+    ...options,
+    userAgentOptions: {
+      userAgentPrefix,
+    },
+  };
+
+  const client = getClient(baseUrl, options) as TextTranslationClient;
+  client.pipeline.addPolicy(apiVersionPolicy);
+
+  if (credential instanceof TranslatorCredential) {
+    const mtAuthneticationPolicy = new TranslatorAuthenticationPolicy(credential as TranslatorCredential);
+    client.pipeline.addPolicy(mtAuthneticationPolicy);
+  } else if (credential instanceof AzureKeyCredential) {
+    const mtKeyAuthenticationPolicy = new TranslatorAzureKeyAuthenticationPolicy(credential as AzureKeyCredential);
+    client.pipeline.addPolicy(mtKeyAuthenticationPolicy);
+  } else if (credential) {
+    client.pipeline.addPolicy(
+      coreRestPipeline.bearerTokenAuthenticationPolicy({
+        credential: credential as TokenCredential,
+        scopes: DEFAULT_SCOPE
+      })
+    );
+  }
+
+  return client;
+}
+
+/** Policy that sets the api-version (or equivalent) to reflect the library version. */
+const apiVersionPolicy = {
+  name: "MTApiVersionPolicy",
+  async sendRequest(
+    request: PipelineRequest,
+    next: SendRequest
+  ): Promise<PipelineResponse> {
+    const param = request.url.split("?");
+    if (param.length > 1) {
+      const newParams = param[1].split("&");
+      newParams.push("api-version=3.0");
+      request.url = param[0] + "?" + newParams.join("&");
+    } else {
+      // no query parameters in request url
+      request.url = param[0] + "?api-version=3.0";
+    }
+    return next(request);
+  }
+};
---
+++ sdk/cognitiveservices/cognitiveservices-translatortext/src/generated/clientDefinitions.ts
@@ -0,0 +1,103 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  GetLanguagesParameters,
+  TranslateParameters,
+  TransliterateParameters,
+  DetectParameters,
+  BreakSentenceParameters,
+  DictionaryLookupParameters,
+  DictionaryExamplesParameters,
+} from "./parameters";
+import {
+  GetLanguages200Response,
+  GetLanguagesDefaultResponse,
+  Translate200Response,
+  TranslateDefaultResponse,
+  Transliterate200Response,
+  TransliterateDefaultResponse,
+  Detect200Response,
+  DetectDefaultResponse,
+  BreakSentence200Response,
+  BreakSentenceDefaultResponse,
+  DictionaryLookup200Response,
+  DictionaryLookupDefaultResponse,
+  DictionaryExamples200Response,
+  DictionaryExamplesDefaultResponse,
+} from "./responses";
+import { Client, StreamableMethod } from "@azure-rest/core-client";
+
+export interface GetLanguages {
+  /** Gets the set of languages currently supported by other operations of the Translator. */
+  get(
+    options?: GetLanguagesParameters
+  ): StreamableMethod<GetLanguages200Response | GetLanguagesDefaultResponse>;
+}
+
+export interface Translate {
+  /** Translate Text */
+  post(
+    options: TranslateParameters
+  ): StreamableMethod<Translate200Response | TranslateDefaultResponse>;
+}
+
+export interface Transliterate {
+  /** Transliterate Text */
+  post(
+    options: TransliterateParameters
+  ): StreamableMethod<Transliterate200Response | TransliterateDefaultResponse>;
+}
+
+export interface Detect {
+  /** Detect Languages */
+  post(
+    options: DetectParameters
+  ): StreamableMethod<Detect200Response | DetectDefaultResponse>;
+}
+
+export interface BreakSentence {
+  /** Break Sentence */
+  post(
+    options: BreakSentenceParameters
+  ): StreamableMethod<BreakSentence200Response | BreakSentenceDefaultResponse>;
+}
+
+export interface DictionaryLookup {
+  /** Dictionary Lookup */
+  post(
+    options: DictionaryLookupParameters
+  ): StreamableMethod<
+    DictionaryLookup200Response | DictionaryLookupDefaultResponse
+  >;
+}
+
+export interface DictionaryExamples {
+  /** Dictionary Examples */
+  post(
+    options: DictionaryExamplesParameters
+  ): StreamableMethod<
+    DictionaryExamples200Response | DictionaryExamplesDefaultResponse
+  >;
+}
+
+export interface Routes {
+  /** Resource for '/languages' has methods for the following verbs: get */
+  (path: "/languages"): GetLanguages;
+  /** Resource for '/translate' has methods for the following verbs: post */
+  (path: "/translate"): Translate;
+  /** Resource for '/transliterate' has methods for the following verbs: post */
+  (path: "/transliterate"): Transliterate;
+  /** Resource for '/detect' has methods for the following verbs: post */
+  (path: "/detect"): Detect;
+  /** Resource for '/breaksentence' has methods for the following verbs: post */
+  (path: "/breaksentence"): BreakSentence;
+  /** Resource for '/dictionary/lookup' has methods for the following verbs: post */
+  (path: "/dictionary/lookup"): DictionaryLookup;
+  /** Resource for '/dictionary/examples' has methods for the following verbs: post */
+  (path: "/dictionary/examples"): DictionaryExamples;
+}
+
+export type TextTranslationClient = Client & {
+  path: Routes;
+};
