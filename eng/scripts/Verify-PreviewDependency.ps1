ed822cfd504a9f0909188414bb7acae8cea08361
Albert Cheng <albertcheng@microsoft.com>
2023-02-23 14:01:02 -0800

    also prohibit pre-release packages

--- eng/scripts/Verify-PreviewDependency.ps1
+++ eng/scripts/Verify-PreviewDependency.ps1
@@ -10,8 +10,8 @@ foreach ($packagePath in $packagePaths) {
   $pkgContent = Get-Content $packagePath | ConvertFrom-Json
   foreach ($dependency in $pkgContent.dependencies.PSObject.properties) {
     $depVer = $dependency.Value
-    if ($depVer -match "\D0.\d.\d") {
-      throw "Found dependency on preview package $($dependency.Name) with version range $depVer. Please pin dependencies to preview packages."
+    if (($depVer -match "\D0.\d.\d") -or ($depVer -match "\D\d.\d.\d-\w")) {
+      throw "Found dependency on preview/pre-release package $($dependency.Name) with version range $depVer. Please pin dependencies to preview packages."
     }
   }
 }
