5d13c39e8c0425c928868e419fb07ccd03fd1684
Bhargav Kansagara <kansagara.bhargav1997@gmail.com>
2023-02-24 03:24:21 +0530

    [AgriFood] Updating client for 2022-11-01-preview API version (#23189)
    ### Packages impacted by this PR
     @azure-rest/agrifood-farming
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    - Updating clients to support 2022-11-01-preview API version
    
    ### What are the possible designs available to address the problem? If there are more than one possible design, why was the one in this PR chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    - Yes
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so, create an Issue in the [Autorest/typescript](https://github.com/Azure/autorest.typescript) repository and link it here)_
    - [ ] Added a changelog (if necessary)

---
+++ sdk/agrifood/agrifood-farming-rest/samples/v1-beta/typescript/src/listParties.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * This sample demonstrates how get a list of parties and how to get all pages in case the service returns a paged result
+ *
+ * @summary gets a list of parties
+ */
+
+import FarmBeats, { isUnexpected, PartyOutput, paginate } from "@azure-rest/agrifood-farming";
+import { DefaultAzureCredential } from "@azure/identity";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const endpoint = process.env["FARMBEATS_ENDPOINT"] || "";
+
+async function main() {
+  const farming = FarmBeats(endpoint, new DefaultAzureCredential());
+  const response = await farming.path("/parties").get();
+
+  if (isUnexpected(response)) {
+    throw response.body.error;
+  }
+
+  const parties = paginate(farming, response);
+
+  // Lof each party id
+  for await (const party of parties) {
+    const partyOutput = party;
+    console.log(partyOutput.id);
+  }
+}
+
+main().catch(console.error);
