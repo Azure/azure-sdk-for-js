c2e693913992dbf5d9c24a6d09731751804fc478
yogeshmo <46582228+yogeshmo@users.noreply.github.com>
2023-02-23 16:07:21 -0800

    [Communication][Email] Addressing SDK Feedback for GA (#24378)
    Changes Made:
    
    SDK was regenerated with new swagger version
    Archboard Feedback was addressed
    
    ---------
    
    Co-authored-by: Yogesh Mohanraj <ymohanraj@microsoft.com>

--- sdk/communication/communication-email/samples-dev/sendEmailWithAttachments.ts
+++ sdk/communication/communication-email/samples-dev/sendEmailWithAttachments.ts
@@ -20,30 +20,32 @@ const sendEmailWithAttachments = async (): Promise<void> => {
   const emailClient: EmailClient = new EmailClient(connectionString);
 
   // Create the Email Message to be sent
-  const emailMessage: EmailMessage = {
-    sender: senderAddress,
+  const message: EmailMessage = {
+    senderAddress: senderAddress,
     content: {
       subject: "This is the subject",
       plainText: "This is the body",
       html: "<html><h1>This is the body</h1></html>",
     },
     recipients: {
-      to: [{ email: recipientAddress, displayName: "Customer Name" }],
+      to: [{ address: recipientAddress, displayName: "Customer Name" }],
     },
     attachments: [
       {
         name: "readme.txt",
-        attachmentType: "txt",
-        contentBytesBase64: "ZW1haWwgdGVzdCBhdHRhY2htZW50",
+        contentType: "text/plain",
+        contentInBase64: "ZW1haWwgdGVzdCBhdHRhY2htZW50",
       },
     ],
   };
 
   try {
     // Send the email message
-    const response = await emailClient.send(emailMessage);
+    const poller = await emailClient.beginSend(message);
+    const response = await poller.pollUntilDone();
 
-    console.log("Message ID: " + response.messageId);
+    // Get the OperationId so that it can be used for tracking the message for troubleshooting
+    console.log("Operation ID: " + response.id);
   } catch (error) {
     console.log(error);
   }
