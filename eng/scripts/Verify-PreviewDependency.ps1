e8c4e1f0f90a6c6a058937cb0068f30b6d5caab5
Albert Cheng <albertcheng@microsoft.com>
2023-03-02 14:11:13 -0800

    using semver.ps1

--- eng/scripts/Verify-PreviewDependency.ps1
+++ eng/scripts/Verify-PreviewDependency.ps1
@@ -4,14 +4,20 @@ param (
   [string]$PackagePath
 )
 
+."$PSScriptRoot\..\common\scripts\SemVer.ps1"
+
 $packagePaths = Get-ChildItem -Path "$PackagePath/*/package.json"
 
 foreach ($packagePath in $packagePaths) {
   $pkgContent = Get-Content $packagePath | ConvertFrom-Json
   foreach ($dependency in $pkgContent.dependencies.PSObject.properties) {
     $depVer = $dependency.Value
-    if (($depVer -match "\D0.\d.\d") -or ($depVer -match "\D\d.\d.\d-\w")) {
-      throw "Found dependency on preview/pre-release package $($dependency.Name) with version range $depVer. Please pin dependencies to preview packages."
+    if ($depVer -match "\D\d.\d.\d") {
+      $baseVer = $depVer.replace("~","").replace("^","")
+      $version = [AzureEngSemanticVersion]::new($baseVer)
+      if ($version.IsPrerelease) {
+        throw "Found dependency on preview/pre-release package $($dependency.Name) with version range $depVer. Please pin dependencies to preview packages."
+      }
     }
   }
 }
