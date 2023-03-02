90b194ff7110e866edfd8e042cf866221b266c4c
Min Woo Lee 🧊 <77083090+minwoolee-ms@users.noreply.github.com>
2023-03-01 18:14:42 -0800

    Adding template samples

---
+++ sdk/communication/communication-call-automation/samples/v1-beta/typescript/src/getConfigurationSetting.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * @summary TODO: build samples
+ */
+
+// Load the .env file if it exists
+import * as dotenv from "dotenv";
+dotenv.config();
+
+async function main() {
+  console.log("TODO: built sample here");
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
