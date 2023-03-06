d8895ad6d543de401bdc5b59d32f6b1b9f03ddea
Natalija Ostojic <nostojic@microsoft.com>
2023-03-06 15:52:52 +0100

    Add AAD tests

--- sdk/communication/communication-domain-verification/test/public/createDomainOwnershipChallenge.spec.ts
+++ sdk/communication/communication-domain-verification/test/public/createDomainOwnershipChallenge.spec.ts
@@ -11,50 +11,56 @@ import { assert } from "chai";
 import { Context } from "mocha";
 import { Recorder } from "@azure-tools/test-recorder";
 import { DomainVerificationClient } from "../../src";
-import { createRecordedClient } from "./utils/recordedClient";
+import { createRecordedClient, createRecordedClientWithToken } from "./utils/recordedClient";
+import { matrix } from "@azure/test-utils";
 
-describe("Domain Verification Client - Create Domain Ownership Challenge", () => {
-  let recorder: Recorder;
-  let client: DomainVerificationClient;
-
-  beforeEach(async function (this: Context) {
-    ({ client, recorder } = await createRecordedClient(this));
-  });
-
-  afterEach(async function () {
-    if (!this.currentTest?.isPending()) {
-      await recorder.stop();
-    }
-  });
-
-  it("Can create challenge", async function () {
-    const result = await client.createDomainOwnershipChallenge(getDomain());
-    assert.isNotEmpty(result.value);
-    assert.equal(result.value, getVerificationValue());
+matrix([[true, false]], async function (useAad) {
+  describe(`Domain Verification Client - Create Domain Ownership Challenge${useAad ? " [AAD]" : ""}`, () => {
+    let recorder: Recorder;
+    let client: DomainVerificationClient;
+  
+    beforeEach(async function (this: Context) {
+      ({ client, recorder } = useAad
+        ? await createRecordedClientWithToken(this)
+        : await createRecordedClient(this));
+    });
+  
+    afterEach(async function () {
+      if (!this.currentTest?.isPending()) {
+        await recorder.stop();
+      }
+    });
+  
+    it("Can create challenge", async function () {
+      const result = await client.createDomainOwnershipChallenge(getDomain());
+      assert.isNotEmpty(result.value);
+      assert.equal(result.value, getVerificationValue());
+    }).timeout(15000);
+  
+    it("Error if domain is empty on create challenge", async function () {
+      try {
+        await client.createDomainOwnershipChallenge(getEmptyDomain());
+      } catch (error: any) {
+        assert.equal(error.statusCode, 400);
+        assert.strictEqual(
+          getErrorDescription(error.message),
+          "A non-empty request body is required."
+        );
+      }
+    }).timeout(15000);
+  
+    it("Error if domain has invalid format create challenge", async function () {
+      try {
+        await client.createDomainOwnershipChallenge(getInvalidDomain());
+      } catch (error: any) {
+        assert.equal(error.statusCode, 422);
+        assert.strictEqual(error.code, "UnprocessableConfiguration");
+        assert.strictEqual(error.message, "One or more request inputs are not valid.");
+      }
+    });
   }).timeout(15000);
+});
 
-  it("Error if domain is empty on create challenge", async function () {
-    try {
-      await client.createDomainOwnershipChallenge(getEmptyDomain());
-    } catch (error: any) {
-      assert.equal(error.statusCode, 400);
-      assert.strictEqual(
-        getErrorDescription(error.message),
-        "A non-empty request body is required."
-      );
-    }
-  }).timeout(15000);
-
-  it("Error if domain has invalid format create challenge", async function () {
-    try {
-      await client.createDomainOwnershipChallenge(getInvalidDomain());
-    } catch (error: any) {
-      assert.equal(error.statusCode, 422);
-      assert.strictEqual(error.code, "UnprocessableConfiguration");
-      assert.strictEqual(error.message, "One or more request inputs are not valid.");
-    }
-  });
-}).timeout(15000);
 
 export function getErrorDescription(error: string): string {
   const innerError = JSON.parse(error);
