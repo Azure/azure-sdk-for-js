be76af01335736bd0a6c37efebb3408b14c4c243
Natalija Ostojic <nostojic@microsoft.com>
2023-03-06 15:52:52 +0100

    Add AAD tests

--- sdk/communication/communication-domain-verification/test/public/verifyDomainOwnership.spec.ts
+++ sdk/communication/communication-domain-verification/test/public/verifyDomainOwnership.spec.ts
@@ -11,47 +11,52 @@ import { assert } from "chai";
 import { Context } from "mocha";
 import { Recorder } from "@azure-tools/test-recorder";
 import { DomainVerificationClient } from "../../src";
-import { createRecordedClient } from "./utils/recordedClient";
+import { createRecordedClient, createRecordedClientWithToken } from "./utils/recordedClient";
+import { matrix } from "@azure/test-utils";
 
-describe("Domain Verification - Verify Domain Ownership", () => {
-  let recorder: Recorder;
-  let client: DomainVerificationClient;
-
-  beforeEach(async function (this: Context) {
-    ({ client, recorder } = await createRecordedClient(this));
-  });
-
-  afterEach(async function () {
-    if (!this.currentTest?.isPending()) {
-      await recorder.stop();
-    }
-  });
-
-  it("Can verify domain ownership", async function () {
-    const domain = getDomain();
-    const result = await client.verifyDomainOwnership(domain);
-    assert.isNotEmpty(result.status);
-    assert.equal(result.status, getDomainStatus());
-  }).timeout(15000);
-
-  it("Error if domain is empty on verify domain ownership", async function () {
-    try {
-      const domain = getEmptyDomain();
-      await client.verifyDomainOwnership(domain);
-    } catch (error: any) {
-      assert.equal(error.statusCode, 400);
-      assert.strictEqual(error.message, '"Domain is not provided"');
-    }
-  }).timeout(15000);
-
-  it("Error if domain has invalid format on verify domain ownership", async function () {
-    try {
-      const domain = getInvalidDomain();
-      await client.verifyDomainOwnership(domain);
-    } catch (error: any) {
-      assert.equal(error.statusCode, 422);
-      assert.strictEqual(error.code, "UnprocessableConfiguration");
-      assert.strictEqual(error.message, "One or more request inputs are not valid.");
-    }
-  }).timeout(15000);
+matrix([[true, false]], async function (useAad) {
+  describe(`Domain Verification - Verify Domain Ownership${useAad ? " [AAD]" : ""}`, () => {
+    let recorder: Recorder;
+    let client: DomainVerificationClient;
+  
+    beforeEach(async function (this: Context) {
+      ({ client, recorder } = useAad
+        ? await createRecordedClientWithToken(this)
+        : await createRecordedClient(this));
+    });
+  
+    afterEach(async function () {
+      if (!this.currentTest?.isPending()) {
+        await recorder.stop();
+      }
+    });
+  
+    it("Can verify domain ownership", async function () {
+      const domain = getDomain();
+      const result = await client.verifyDomainOwnership(domain);
+      assert.isNotEmpty(result.status);
+      assert.equal(result.status, getDomainStatus());
+    }).timeout(15000);
+  
+    it("Error if domain is empty on verify domain ownership", async function () {
+      try {
+        const domain = getEmptyDomain();
+        await client.verifyDomainOwnership(domain);
+      } catch (error: any) {
+        assert.equal(error.statusCode, 400);
+        assert.strictEqual(error.message, '"Domain is not provided"');
+      }
+    }).timeout(15000);
+  
+    it("Error if domain has invalid format on verify domain ownership", async function () {
+      try {
+        const domain = getInvalidDomain();
+        await client.verifyDomainOwnership(domain);
+      } catch (error: any) {
+        assert.equal(error.statusCode, 422);
+        assert.strictEqual(error.code, "UnprocessableConfiguration");
+        assert.strictEqual(error.message, "One or more request inputs are not valid.");
+      }
+    }).timeout(15000);
+  });  
 });
