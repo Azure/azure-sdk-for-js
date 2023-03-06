50b5d62f3ce662d0004245f4beb58b50981bf854
Edward Ho <89552441+edwardho-ms@users.noreply.github.com>
2023-03-06 11:03:11 -0800

    Unit Test Setup (#25051)
    - setting up first unit test using chai/sinon
    - adding ESLint config for VSCode
    
    ### Packages impacted by this PR
    
    ### Issues associated with this PR
    
    ### Describe the problem that is addressed by this PR
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    ### Provide a list of related PRs _(if any)_
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [] Added impacted package name to the issue description
    - [] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [] Added a changelog (if necessary)

--- sdk/communication/communication-call-automation/test/public/utils/connectionUtils.ts
+++ sdk/communication/communication-call-automation/test/public/utils/connectionUtils.ts
@@ -5,6 +5,20 @@ import { isNode } from "@azure/core-util";
 
 export const baseUri = "https://contoso.api.fake";
 
+export const MOCK_ENDPOINT = "https://REDACTED.communication.azure.com/";
+export const MOCK_CONNECTION_STRING = `endpoint=${MOCK_ENDPOINT};accesskey=eyJhbG`;
+export const CALL_CONNECTION_ID = "callConnectionId";
+export const CALL_SERVER_CALL_ID = "serverCallId";
+export const CALL_CALLER_ID = "callerId";
+export const CALL_CALLER_DISPLAY_NAME = "callerDisplayName";
+export const CALL_TARGET_ID = "targetId";
+export const CALL_CONNECTION_STATE = "connected";
+export const CALL_SUBJECT = "subject";
+export const CALL_CALLBACK_URL = "https://REDACTED.com/events";
+export const CALL_INCOMING_CALL_CONTEXT = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.REDACTED";
+export const CALL_OPERATION_CONTEXT = "operationContext";
+export const MEDIA_SUBSCRIPTION_ID = "mediaSubscriptionId";
+
 declare function btoa(stringToEncode: string): string;
 
 export const generateToken = (): string => {
