87e9ff63f9359499116fd9886ed2f9b076db2523
Will Temple <witemple@microsoft.com>
2023-03-09 16:39:41 -0500

    Unstage migration for asset sync

--- common/tools/dev-tool/src/migrations/onboard/test-proxy-asset-sync.ts
+++ common/tools/dev-tool/src/migrations/onboard/test-proxy-asset-sync.ts
@@ -1,28 +1,28 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT license.
 
-import { createMigration } from "../../util/migrations";
-import { runMigrationScript } from "../../util/testProxyUtils";
+// import { createMigration } from "../../util/migrations";
+// import { runMigrationScript } from "../../util/testProxyUtils";
 
-// The migration itself should be default-exported so that other tools can reference it easily.
-export default createMigration(
-  "onboard/test_proxy_asset_sync", // unique ID
-  "2023-03-08T18:36:03+00:00Z", // ISO timestamp that the migration becomes effective
-  "enables the `dev-tool check` command (for example)", // short description of the migration
-  {
-    // Optional URL to more information
-    url: "https://github.com/azure/azure-sdk-for-js/tree/main/sdk/test-utils/recorder/ASSET_SYNC_MIGRATION.md",
+// // The migration itself should be default-exported so that other tools can reference it easily.
+// export default createMigration(
+//   "onboard/test_proxy_asset_sync", // unique ID
+//   "2023-03-08T18:36:03+00:00Z", // ISO timestamp that the migration becomes effective
+//   "enables the `dev-tool check` command (for example)", // short description of the migration
+//   {
+//     // Optional URL to more information
+//     url: "https://github.com/azure/azure-sdk-for-js/tree/main/sdk/test-utils/recorder/ASSET_SYNC_MIGRATION.md",
 
-    // Optional validation
-    async validate(_project) {
-      /* the validation fails if this function throws an error. */
-    },
+//     // Optional validation
+//     async validate(_project) {
+//       /* the validation fails if this function throws an error. */
+//     },
 
-    // Optional automated execution
-    async execute(project) {
-      // TODO: test if package has already been migrated.
+//     // Optional automated execution
+//     async execute(project) {
+//       // TODO: test if package has already been migrated.
 
-      await runMigrationScript(project, true);
-    },
-  }
-);
+//       await runMigrationScript(project, true);
+//     },
+//   }
+// );
