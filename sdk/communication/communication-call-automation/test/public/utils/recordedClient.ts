b98064c41bb34178fdeada0e8fc38cf093dbcc3c
juntuchen <juntuchen@microsoft.com>
2023-03-09 18:47:35 -0500

    checked in init infra

--- sdk/communication/communication-call-automation/test/public/utils/recordedClient.ts
+++ sdk/communication/communication-call-automation/test/public/utils/recordedClient.ts
@@ -3,16 +3,19 @@
 
 import * as dotenv from "dotenv";
 import { isNode } from "@azure/core-util";
-import { Recorder, RecorderStartOptions, env } from "@azure-tools/test-recorder";
+import { Recorder, RecorderStartOptions, env, assertEnvironmentVariable } from "@azure-tools/test-recorder";
 import { Test } from "mocha";
 import { generateToken } from "./connectionUtils";
+import { CommunicationIdentityClient } from "@azure/communication-identity";
+import { CommunicationUserIdentifier } from "@azure/communication-common";
+import { CallAutomationClient } from "../../../src";
 
 if (isNode) {
   dotenv.config();
 }
 
 const envSetupForPlayback: { [k: string]: string } = {
-  COMMUNICATION_CONNECTION_STRING: "endpoint=https://endpoint/;accesskey=banana",
+  COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING: "endpoint=https://endpoint/;accesskey=redacted",
 };
 
 const fakeToken = generateToken();
@@ -21,8 +24,8 @@ export const recorderOptions: RecorderStartOptions = {
   sanitizerOptions: {
     connectionStringSanitizers: [
       {
-        fakeConnString: envSetupForPlayback["COMMUNICATION_CONNECTION_STRING"],
-        actualConnString: env["COMMUNICATION_CONNECTION_STRING"] || undefined,
+        fakeConnString: envSetupForPlayback["COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING"],
+        actualConnString: env["COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING"] || undefined,
       },
     ],
     bodyKeySanitizers: [{ jsonPath: "$.accessToken.token", value: fakeToken }],
@@ -34,4 +37,30 @@ export async function createRecorder(context: Test | undefined): Promise<Recorde
   await recorder.start(recorderOptions);
   await recorder.setMatcher("HeaderlessMatcher");
   return recorder;
+}
+
+export async function createTestUser(recorder: Recorder): Promise<CommunicationUserIdentifier> {
+  const identityClient = new CommunicationIdentityClient(
+    assertEnvironmentVariable("COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING"),
+    recorder.configureClientOptions({})
+  );
+  return identityClient.createUser();
+}
+
+export async function deleteTestUser(testUser: CommunicationUserIdentifier): Promise<void> {
+  if (testUser) {
+    const identityClient = new CommunicationIdentityClient(
+      assertEnvironmentVariable("COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING")
+    );
+    await identityClient.deleteUser(testUser);
+  }
+}
+
+export function createCallAutomationClient(recorder: Recorder): CallAutomationClient {
+  const connectionString = assertEnvironmentVariable("COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING");
+
+  return new CallAutomationClient(
+    connectionString,
+    recorder.configureClientOptions({})
+  );
 }
