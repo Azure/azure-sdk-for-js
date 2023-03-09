d454598a54e741e5e1cfa4d6854ac421871d8e49
Will Temple <witemple@microsoft.com>
2023-03-09 16:51:25 -0500

    Added migration for test proxy asset sync

--- common/tools/dev-tool/src/migrations/onboard/test-proxy-asset-sync.ts
+++ common/tools/dev-tool/src/migrations/onboard/test-proxy-asset-sync.ts
@@ -1,17 +1,17 @@
-// // Copyright (c) Microsoft Corporation.
-// // Licensed under the MIT license.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
-// import { createMigration } from "../../util/migrations";
-// import { runMigrationScript } from "../../util/testProxyUtils";
+import { readFile, pathExists } from "fs-extra";
+import path from "path";
+import { createMigration } from "../../util/migrations";
+import { runMigrationScript } from "../../util/testProxyUtils";
 
-// // The migration itself should be default-exported so that other tools can reference it easily.
-// export default createMigration(
-//   "onboard/test_proxy_asset_sync", // unique ID
-//   "2023-03-08T18:36:03+00:00Z", // ISO timestamp that the migration becomes effective
-//   "enables the `dev-tool check` command (for example)", // short description of the migration
-//   {
-//     // Optional URL to more information
-//     url: "https://github.com/azure/azure-sdk-for-js/tree/main/sdk/test-utils/recorder/ASSET_SYNC_MIGRATION.md",
+interface AssetsJson {
+  AssetsRepo: "Azure/azure-sdk-assets";
+  AssetsRepoPrefixPath: "js";
+  TagPrefix: string;
+  Tag: string;
+}
 
 // The migration itself should be default-exported so that other tools can reference it easily.
 export default createMigration(
@@ -22,11 +22,30 @@ export default createMigration(
     // Optional URL to more information
     url: "https://github.com/azure/azure-sdk-for-js/tree/main/sdk/test-utils/recorder/ASSET_SYNC_MIGRATION.md",
 
-//     // Optional automated execution
-//     async execute(project) {
-//       // TODO: test if package has already been migrated.
+    // Optional validation
+    async validate(project) {
+      const assetsJson = JSON.parse(
+        (await readFile(path.join(project.path, "assets.json"))).toString("utf-8")
+      ) as AssetsJson;
 
-//       await runMigrationScript(project, true);
-//     },
-//   }
-// );
+      if (
+        !assetsJson.AssetsRepo ||
+        !assetsJson.AssetsRepoPrefixPath ||
+        !assetsJson.TagPrefix ||
+        !assetsJson.Tag ||
+        assetsJson.Tag === "" ||
+        assetsJson.TagPrefix === ""
+      ) {
+        throw new Error("assets.json is missing required fields.");
+      }
+    },
+
+    // Optional automated execution
+    async execute(project) {
+      // If the `assets.json` file exists, this package is already migrated.
+      if (await pathExists(path.join(project.path, "assets.json"))) return;
+
+      await runMigrationScript(project, true);
+    },
+  }
+);
