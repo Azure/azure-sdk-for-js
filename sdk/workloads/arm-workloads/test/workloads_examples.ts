df5b6187427bea48357bc13310f4712b6b54d9b7
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-03-10 15:39:44 +0800

    update files

--- sdk/workloads/arm-workloads/test/workloads_examples.ts
+++ sdk/workloads/arm-workloads/test/workloads_examples.ts
@@ -34,7 +34,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe.skip("workloads test", () => {
+describe("workloads test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: WorkloadsClient;
