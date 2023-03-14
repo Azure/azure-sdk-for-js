a6fe3ff7eb4b712caaa770140c63de30c4416940
Jiri Burant <83883278+jiriburant@users.noreply.github.com>
2023-03-14 10:56:16 +0100

    Jiriburant/siprouting ga version (#25166)
    # Description
    
    SIP routing GA version.
    
    Regenerated SIP routing functionality in PhoneNumbers SDK to accomodate
    for GA version.
    
    Rest API swagger:
    https://github.com/Azure/azure-rest-api-specs/pull/22747
    
    If an SDK is being regenerated based on a new swagger spec, a link to
    the pull request containing these swagger spec changes has been included
    above.
    
    ### Packages impacted by this PR
    PhoneNumbers SDK
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    Updating the underlying API to GA version.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/communication/communication-phone-numbers/src/generated/src/siprouting/operationsInterfaces/index.ts
+++ sdk/communication/communication-phone-numbers/src/generated/src/siprouting/operationsInterfaces/index.ts
@@ -6,7 +6,4 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./models";
-export { SipRoutingClient } from "./sipRoutingClient";
-export { SipRoutingClientContext } from "./sipRoutingClientContext";
-export * from "./operationsInterfaces";
+export * from "./sipRouting";
