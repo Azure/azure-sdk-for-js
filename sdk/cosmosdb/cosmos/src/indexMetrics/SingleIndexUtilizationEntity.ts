c9cfd883dc90f3d237548c6eed7a35be2563021f
Manik Khandelwal <113669638+topshot99@users.noreply.github.com>
2023-03-20 13:45:43 +0530

    Enable index metrics feature  (#24955)
    ### Packages impacted by this PR
    azure-cosmos-js-sdk
    
    ### Issues associated with this PR
    https://github.com/Azure/azure-sdk-for-js/issues/20194
    
    ### Describe the problem that is addressed by this PR
    Currently cosmos db node sdk doesn't support index metrics feature this
    change would enable the same.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    N/A
    
    ### Are there test cases added in this PR? _(If not, why?)_
    yes
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Manik Khandelwal <mkhandelwal@microsoft.com>

---
+++ sdk/cosmosdb/cosmos/src/indexMetrics/SingleIndexUtilizationEntity.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * Query index utilization data for single index (sub-structure of the Index Utilization metrics) in the Azure Cosmos database service.
+ */
+export interface SingleIndexUtilizationEntity {
+  FilterExpression?: string;
+  IndexSpec?: string; // IndexDocumentExpression
+  FilterPreciseSet?: boolean; // FilterExpressionPrecision
+  IndexPreciseSet?: boolean; // IndexPlanFullFidelity
+  IndexImpactScore?: string;
+}
