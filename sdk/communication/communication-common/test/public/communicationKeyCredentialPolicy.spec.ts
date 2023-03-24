ca54f09c57cd67fdfbba6b1ece6f581b33a32065
ikumarapeli <ikumarapeli@microsoft.com>
2023-03-23 23:57:50 -0700

    update unit tests

--- sdk/communication/communication-common/test/public/communicationKeyCredentialPolicy.spec.ts
+++ sdk/communication/communication-common/test/public/communicationKeyCredentialPolicy.spec.ts
@@ -12,174 +12,82 @@ import { KeyCredential } from "@azure/core-auth";
 import { assert } from "chai";
 import { createCommunicationAccessKeyCredentialPolicy } from "../../src";
 import { isNode } from "@azure/core-util";
-import { shaHMAC, shaHash } from "../../src/credential/cryptoUtils";
+import { set } from 'mockdate'
+
+const date = '2022-04-13T18:09:12.451Z'
+set(date) // Any request to Date will return this date
 
 describe("CommunicationKeyCredentialPolicy", function () {
   it("signs the request", async function () {
-    const credential = new MockKeyCredential("pw==");
-    const communicationKeyCredentialPolicy =
-      createCommunicationAccessKeyCredentialPolicy(credential);
-
-    const pipelineRequest = createPipelineRequest({ url: "https://example.com" });
-
-    const responses: PipelineResponse[] = [
-      {
-        headers: createHttpHeaders(),
-        request: pipelineRequest,
-        status: 200,
-      },
-    ];
-
-    const testHttpsClient: HttpClient = {
-      sendRequest: async (req) => {
-        if (responses.length) {
-          const response = responses.shift()!;
-          response.request = req;
-          return response;
-        }
-        throw new Error("No responses found");
-      },
-    };
-
-    const pipeline = createEmptyPipeline();
-    pipeline.addPolicy(communicationKeyCredentialPolicy);
-    const pipelineResponse = await pipeline.sendRequest(testHttpsClient, pipelineRequest);
-
-    const authHeader = pipelineResponse.request.headers.get("Authorization");
-    const dateHeader = pipelineResponse.request.headers.get("x-ms-date");
-    const hashHeader = pipelineResponse.request.headers.get("x-ms-content-sha256");
-    const hostHeader = pipelineResponse.request.headers.get("Host");
-
-    assert.isNotEmpty(authHeader);
-    assert.isNotEmpty(dateHeader);
-    assert.isNotEmpty(hashHeader);
-    if (isNode) {
-      assert.isNotEmpty(hostHeader);
-    }
+    const authHeader = await verifyHeadersForUrlReturnAuthHeader("https://example.com")
+    assert.isNotEmpty(authHeader)
   });
 });
 
 describe("CommunicationKeyCredentialPolicy", function () {
   it("signs the request correctly with path and query params", async function () {
-    const credential = new MockKeyCredential("pw==");
-    const communicationKeyCredentialPolicy =
-      createCommunicationAccessKeyCredentialPolicy(credential);
-
-    const pipelineRequest = createPipelineRequest({
-      url: "https://example.com/testPath?testQuery",
-    });
-
-    const responses: PipelineResponse[] = [
-      {
-        headers: createHttpHeaders(),
-        request: pipelineRequest,
-        status: 200,
-      },
-    ];
-
-    const testHttpsClient: HttpClient = {
-      sendRequest: async (req) => {
-        if (responses.length) {
-          const response = responses.shift()!;
-          response.request = req;
-          return response;
-        }
-        throw new Error("No responses found");
-      },
-    };
-
-    const pipeline = createEmptyPipeline();
-    pipeline.addPolicy(communicationKeyCredentialPolicy);
-    const pipelineResponse = await pipeline.sendRequest(testHttpsClient, pipelineRequest);
-
-    const authHeader = pipelineResponse.request.headers.get("Authorization");
-    const dateHeader = pipelineResponse.request.headers.get("x-ms-date");
-    const hashHeader = pipelineResponse.request.headers.get("x-ms-content-sha256");
-    const hostHeader = pipelineResponse.request.headers.get("Host");
-
-    const authHeaderExpected = await generateAuthHeader(
-      pipelineRequest.method.toLocaleUpperCase(),
-      dateHeader || "",
-      credential.key,
-      "/testPath?testQuery=",
-      "example.com"
-    );
-
-    assert.equal(authHeader, authHeaderExpected);
-    assert.isNotEmpty(dateHeader);
-    assert.isNotEmpty(hashHeader);
-    if (isNode) {
-      assert.isNotEmpty(hostHeader);
-    }
+    const authHeader = await verifyHeadersForUrlReturnAuthHeader("https://example.com/testPath?testQuery=test")
+    assert.equal(authHeader, "HMAC-SHA256 SignedHeaders=x-ms-date;host;x-ms-content-sha256&Signature=DGdgwggJWnQyc6EHjR/Vbqg1ES64KpD6U2XwTDDj3tU=")
   });
 });
 
 describe("CommunicationKeyCredentialPolicy", function () {
-  it("signs the request correctly with path and no query", async function () {
-    const credential = new MockKeyCredential("pw==");
-    const communicationKeyCredentialPolicy =
-      createCommunicationAccessKeyCredentialPolicy(credential);
-
-    const pipelineRequest = createPipelineRequest({ url: "https://example.com/testPath" });
-
-    const responses: PipelineResponse[] = [
-      {
-        headers: createHttpHeaders(),
-        request: pipelineRequest,
-        status: 200,
-      },
-    ];
-
-    const testHttpsClient: HttpClient = {
-      sendRequest: async (req) => {
-        if (responses.length) {
-          const response = responses.shift()!;
-          response.request = req;
-          return response;
-        }
-        throw new Error("No responses found");
-      },
-    };
-
-    const pipeline = createEmptyPipeline();
-    pipeline.addPolicy(communicationKeyCredentialPolicy);
-    const pipelineResponse = await pipeline.sendRequest(testHttpsClient, pipelineRequest);
-
-    const authHeader = pipelineResponse.request.headers.get("Authorization");
-    const dateHeader = pipelineResponse.request.headers.get("x-ms-date");
-    const hashHeader = pipelineResponse.request.headers.get("x-ms-content-sha256");
-    const hostHeader = pipelineResponse.request.headers.get("Host");
-
-    const authHeaderExpected = await generateAuthHeader(
-      pipelineRequest.method.toLocaleUpperCase(),
-      dateHeader || "",
-      credential.key,
-      "/testPath",
-      "example.com"
-    );
-
-    assert.equal(authHeader, authHeaderExpected);
-    assert.isNotEmpty(dateHeader);
-    assert.isNotEmpty(hashHeader);
-    if (isNode) {
-      assert.isNotEmpty(hostHeader);
-    }
+  it("signs the request correctly with path and no query param", async function () {
+    const authHeader = await verifyHeadersForUrlReturnAuthHeader("https://example.com/testPath")
+    assert.equal(authHeader, "HMAC-SHA256 SignedHeaders=x-ms-date;host;x-ms-content-sha256&Signature=+6tWkg3lNKVjQHHmxkdGQcJjUgzclsWTMebnuCz1ngU=")
   });
 });
 
-async function generateAuthHeader(
-  verb: string,
-  dateHeader: string,
-  key: string,
-  pathAndQuery: string,
-  hostAndPort: string
+
+async function verifyHeadersForUrlReturnAuthHeader(
+  urlToTest: string,
+
 ): Promise<string> {
-  const signedHeaders = `x-ms-date;host;x-ms-content-sha256`;
-  const contentHash = await shaHash("");
-  const stringToSign = `${verb}\n${pathAndQuery}\n${dateHeader};${hostAndPort};${contentHash}`;
-  const signature = await shaHMAC(key, stringToSign);
-  return `HMAC-SHA256 SignedHeaders=${signedHeaders}&Signature=${signature}`;
+  const credential = new MockKeyCredential("pw==");
+  const communicationKeyCredentialPolicy =
+    createCommunicationAccessKeyCredentialPolicy(credential);
+
+  const pipelineRequest = createPipelineRequest({
+    url: urlToTest,
+  });
+
+  const responses: PipelineResponse[] = [
+    {
+      headers: createHttpHeaders(),
+      request: pipelineRequest,
+      status: 200,
+    },
+  ];
+
+  const testHttpsClient: HttpClient = {
+    sendRequest: async (req) => {
+      if (responses.length) {
+        const response = responses.shift()!;
+        response.request = req;
+        return response;
+      }
+      throw new Error("No responses found");
+    },
+  };
+
+  const pipeline = createEmptyPipeline();
+  pipeline.addPolicy(communicationKeyCredentialPolicy);
+  const pipelineResponse = await pipeline.sendRequest(testHttpsClient, pipelineRequest);
+
+  const authHeader = pipelineResponse.request.headers.get("Authorization") || "";
+  const dateHeader = pipelineResponse.request.headers.get("x-ms-date");
+  const hashHeader = pipelineResponse.request.headers.get("x-ms-content-sha256");
+  const hostHeader = pipelineResponse.request.headers.get("Host");
+
+  assert.equal(dateHeader, "Wed, 13 Apr 2022 18:09:12 GMT");
+  assert.equal(hashHeader, "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=");
+  if (isNode) {
+    assert.isNotEmpty(hostHeader);
+  }
+  return authHeader;
+
 }
+
 
 class MockKeyCredential implements KeyCredential {
   key: string;
