d2124a1e45d5da782bd1bc6f79bc09e26888570e
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-03-28 15:17:17 +0800

    update files

--- sdk/workloads/arm-workloads/test/workloads_examples.ts
+++ sdk/workloads/arm-workloads/test/workloads_examples.ts
@@ -78,31 +78,6 @@ describe("workloads test", () => {
     assert.equal(res.name, monitorName);
   });
 
-  it("providerInstances create test", async function () {
-    const res = await client.providerInstances.beginCreateAndWait(
-      resourceGroup,
-      monitorName,
-      "myProviderInstance",
-      {
-        providerSettings: {
-          dbName: "dbName",
-          dbPassword: "password",
-          dbPasswordUri: "",
-          dbPort: "dbPort",
-          dbUsername: "username",
-          hostname: "hostname",
-          providerType: "Db2",
-          sapSid: "SID",
-          sslCertificateUri:
-            "https://storageaccount.blob.core.windows.net/containername/filename",
-          sslPreference: "ServerCertificate"
-        }
-      },
-      testPollingOptions
-    );
-    assert.equal(res.name, monitorName);
-  });
-
   //create svi
   it.skip("svi create test", async function () {
     const subnetId = "/subscriptions/" + subscriptionId + "/resourceGroups/myjstest/providers/Microsoft.Networks/virtualNetworks/networknamex/subnets/subnetworknamex"
@@ -179,15 +154,6 @@ describe("workloads test", () => {
       resArray.push(item);
     }
     assert.equal(resArray.length, 1);
-  });
-
-  it("providerInstances delete test", async function () {
-    const res = await client.providerInstances.beginDeleteAndWait(resourceGroup, monitorName, "myProviderInstance");
-    const resArray = new Array();
-    for await (let item of client.providerInstances.list(resourceGroup, monitorName)) {
-      resArray.push(item);
-    }
-    assert.equal(resArray.length, 0);
   });
 
   //delete monitors
