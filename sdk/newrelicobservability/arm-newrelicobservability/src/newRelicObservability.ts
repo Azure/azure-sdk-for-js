17e4bd1ff620ce296ec5dedd46bbf0735daa92ef
SDKAuto <sdkautomation@microsoft.com>
2023-03-29 06:57:10 +0000

    CodeGen from PR 23246 in Azure/azure-rest-api-specs update newrelic typescript.md (#23246)

--- sdk/newrelicobservability/arm-newrelicobservability/src/newRelicObservability.ts
+++ sdk/newrelicobservability/arm-newrelicobservability/src/newRelicObservability.ts
@@ -64,7 +64,7 @@ export class NewRelicObservability extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-newrelicobservability/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-newrelicobservability/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
