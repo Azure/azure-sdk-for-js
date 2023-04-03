f483fc7debc829c0c94d325c7ef8a26a73df22cc
hamshavathimunibyraiah <125092972+hamshavathimunibyraiah@users.noreply.github.com>
2023-04-03 12:56:53 -0700

    Hamsha/adding initial tests (#2)
    * Adding a basic translate test
    
    * Testing
    
    * Reverting a small change
    
    * Adding all test cases and they are working in NodeJS
    
    * Trying to make the tests work in browser mode
    
    * Investigating test failures in browser mode by adding loggings
    
    * Tests are working in browser mode
    
    * Investigation on translate token test continues
    
    * Investigating Translate token test run in browser mode
    
    * Making the translate with token test to run in Live and record mode only
    
    * Reverting src file changes
    
    ---------
    
    Co-authored-by: Hamshavathi Munibyraiah (Murphy V-hmunibyrai Associates Inc) <v-hmunibyrai@microsoft.com>

--- sdk/translation/ai-translation-text/test/public/utils/recordedClient.ts
+++ sdk/translation/ai-translation-text/test/public/utils/recordedClient.ts
@@ -2,28 +2,114 @@
 // Licensed under the MIT license.
 
 import { Context } from "mocha";
-import { Recorder, RecorderStartOptions } from "@azure-tools/test-recorder";
-import "./env";
+import { Recorder, RecorderStartOptions, env, isPlaybackMode } from "@azure-tools/test-recorder";
+import {StaticAccessTokenCredential} from "../StaticAccessTokenCredential";
+import TextTranslationFactory, { TextTranslationClient, TranslatorCredential } from "../../../src";
+import { ClientOptions } from "@azure-rest/core-client";
+import {
+  createDefaultHttpClient,
+  createPipelineRequest,
+} from "@azure/core-rest-pipeline";
+import { TokenCredential } from "@azure/core-auth";
 
 const envSetupForPlayback: Record<string, string> = {
-  ENDPOINT: "https://endpoint",
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id",
+  TEXT_TRANSLATION_API_KEY: "fakeapikey",
+  TEXT_TRANSLATION_ENDPOINT: "https://fakeEndpoint.cognitive.microsofttranslator.com",
+  TEXT_TRANSLATION_CUSTOM_ENDPOINT: "https://fakeCustomEndpoint.cognitiveservices.azure.com",
+  TEXT_TRANSLATION_REGION: "fakeregion",
 };
 
 const recorderEnvSetup: RecorderStartOptions = {
   envSetupForPlayback,
 };
 
-/**
- * creates the recorder and reads the environment variables from the `.env` file.
- * Should be called first in the test suite to make sure environment variables are
- * read before they are being used.
- */
-export async function createRecorder(context: Context): Promise<Recorder> {
+export async function startRecorder(context: Context): Promise<Recorder> {
   const recorder = new Recorder(context.currentTest);
   await recorder.start(recorderEnvSetup);
   return recorder;
+}
+
+export async function createTranslationClient(options: {
+  recorder?: Recorder;
+  clientOptions?: ClientOptions;
+}): Promise<TextTranslationClient> {
+  const { recorder, clientOptions = {} } = options;
+  const updatedOptions = recorder ? recorder.configureClientOptions(clientOptions) : clientOptions;
+  const endpoint =
+    env.TEXT_TRANSLATION_ENDPOINT || "https://fakeEndpoint.cognitive.microsofttranslator.com";
+  const apikey = env.TEXT_TRANSLATION_API_KEY || "fakeApikey";
+  const region = env.TEXT_TRANSLATION_REGION || "fakeRegion";
+
+  const translatorCredential = new TranslatorCredential(apikey, region);
+  const client = TextTranslationFactory(endpoint, translatorCredential, updatedOptions);
+  return client;
+}
+
+export async function createCustomTranslationClient(options: {
+  recorder?: Recorder;
+  clientOptions?: ClientOptions;
+}): Promise<TextTranslationClient> {
+  const { recorder, clientOptions = {} } = options;
+  const updatedOptions = recorder ? recorder.configureClientOptions(clientOptions) : clientOptions;
+  const customEndpoint =
+    env.TEXT_TRANSLATION_CUSTOM_ENDPOINT ||
+    "https://fakeCustomEndpoint.cognitiveservices.azure.com";
+  const apikey = env.TEXT_TRANSLATION_API_KEY || "fakeApikey";
+  const region = env.TEXT_TRANSLATION_REGION || "fakeRegion";
+
+  const translatorCredential = new TranslatorCredential(apikey, region);
+  const client = TextTranslationFactory(customEndpoint, translatorCredential, updatedOptions);
+  return client;
+}
+
+export async function createLanguageClient(options: {
+  recorder?: Recorder;
+  clientOptions?: ClientOptions;
+}): Promise<TextTranslationClient> {
+  const { recorder, clientOptions = {} } = options;
+  const updatedOptions = recorder ? recorder.configureClientOptions(clientOptions) : clientOptions;
+  const endpoint =
+    env.TEXT_TRANSLATION_ENDPOINT || "https://fakeEndpoint.cognitive.microsofttranslator.com";
+  const client = TextTranslationFactory(endpoint, undefined, updatedOptions);
+  return client;
+}
+
+export async function createTokenTranslationClient(options: {  
+  recorder?: Recorder;
+  clientOptions?: ClientOptions;
+}): Promise<TextTranslationClient> {
+  const { recorder, clientOptions = {} } = options;
+  const updatedOptions = recorder ? recorder.configureClientOptions(clientOptions) : clientOptions;
+  const endpoint = env.TEXT_TRANSLATION_ENDPOINT || "https://fakeEndpoint.cognitive.microsofttranslator.com";
+  const apikey = env.TEXT_TRANSLATION_API_KEY || "fakeApikey";
+  const region = env.TEXT_TRANSLATION_REGION || "fakeRegion"; 
+  
+  const issueTokenURL: string = "https://" + region + ".api.cognitive.microsoft.com/sts/v1.0/issueToken?Subscription-Key=" + apikey;
+  let credential: TokenCredential;
+  if (isPlaybackMode()) {
+    credential = createMockToken();
+  }
+  else {  
+    const tokenClient = createDefaultHttpClient();
+    const request = createPipelineRequest({
+      url: issueTokenURL,
+      method: "POST"
+    });
+    request.allowInsecureConnection = true;
+    const response = await tokenClient.sendRequest(request);
+    const token:string = response.bodyAsText!; 
+    credential = new StaticAccessTokenCredential(token);
+  }
+  const client = TextTranslationFactory (endpoint, credential, updatedOptions);    
+  return client;
+}
+
+export function createMockToken(): {
+  getToken: (_scopes: string) => Promise<{ token: string; expiresOnTimestamp: number }>;
+} {
+  return {
+    getToken: async (_scopes: string) => {
+      return { token: "testToken", expiresOnTimestamp: 11111 };
+    },
+  };
 }
