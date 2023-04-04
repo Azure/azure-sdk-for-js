114bc518f4b4a942f4c4b509bfb6c5dec9cb2307
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-04-04 11:58:33 +0800

    credscan fix (#25435)
    https://github.com/Azure/azure-sdk-for-js/issues/25414

--- sdk/containerregistry/arm-containerregistry/samples/v10/typescript/src/webhooksUpdateSample.ts
+++ sdk/containerregistry/arm-containerregistry/samples/v10/typescript/src/webhooksUpdateSample.ts
@@ -34,7 +34,7 @@ async function webhookUpdate() {
   const webhookUpdateParameters: WebhookUpdateParameters = {
     actions: ["push"],
     customHeaders: {
-      authorization: "Basic 000000000000000000000000000000000000000000000000000"
+      authorization: "******"
     },
     scope: "myRepository",
     serviceUri: "http://myservice.com",
