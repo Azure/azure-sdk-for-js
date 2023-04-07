25b105b8a28b06145bdc4a55488d5a9426e8b500
juntuchen <juntuchen@microsoft.com>
2023-04-06 23:59:10 -0400

    changed createCall implementation and signature

--- sdk/communication/communication-call-automation/src/callAutomationClient.ts
+++ sdk/communication/communication-call-automation/src/callAutomationClient.ts
@@ -159,47 +159,10 @@ export class CallAutomationClient {
       : undefined;
   }
 
-  /**
-   * Create an outgoing call from source to target identities.
-   * @param target - Either a single target or a group of target identities.
-   * @param callbackUri - The callback url.
-   * @param options - Additional request options contains createCallConnection api options.
-   */
-  public async createCall(
-    target: CallInvite | CommunicationIdentifier[],
-    callbackUri: string,
-    options: CreateCallOptions = {}
+  private async createCallInternal(
+    request: CreateCallRequest,
+    options?: CreateCallOptions
   ): Promise<CreateCallResult> {
-    const request: CreateCallRequest = {
-      sourceIdentity: this.sourceIdentity,
-      targets:
-        target instanceof CallInvite
-          ? [communicationIdentifierModelConverter(target.target)]
-          : target.map((m) => communicationIdentifierModelConverter(m)),
-      callbackUri: callbackUri,
-      operationContext: options.operationContext,
-      azureCognitiveServicesEndpointUrl: options.azureCognitiveServicesEndpointUrl,
-      mediaStreamingConfiguration: options.mediaStreamingConfiguration,
-      customContext: {
-        sipHeaders:
-          target instanceof CallInvite ? target.sipHeaders : options.sipHeaders ?? undefined,
-        voipHeaders:
-          target instanceof CallInvite ? target.voipHeaders : options.voipHeaders ?? undefined,
-      },
-      sourceCallerIdNumber:
-        target instanceof CallInvite
-          ? PhoneNumberIdentifierModelConverter(target.sourceCallIdNumber)
-          : options.sourceCallIdNumber
-          ? PhoneNumberIdentifierModelConverter(options.sourceCallIdNumber)
-          : undefined,
-      sourceDisplayName:
-        target instanceof CallInvite
-          ? target.sourceDisplayName
-          : options.sourceDisplayName
-          ? options.sourceDisplayName
-          : undefined,
-    };
-
     const result = await this.callAutomationApiClient.createCall(request, options);
 
     if (result?.callConnectionId) {
@@ -227,6 +190,65 @@ export class CallAutomationClient {
       return createCallResult;
     }
     throw "callConnectionProperties / callConnectionId is missing in createCall result";
+
+  }
+
+  /**
+   * Create an outgoing call from source to a target identity.
+   * @param target - A single target.
+   * @param callbackUrl - The callback url.
+   * @param options - Additional request options contains createCallConnection api options.
+   */
+  public async createCall(
+    target: CallInvite,
+    callbackUrl: string,
+    options: CreateCallOptions = {}
+  ): Promise<CreateCallResult> {
+    const request: CreateCallRequest = {
+      sourceIdentity: this.sourceIdentity,
+      targets: [communicationIdentifierModelConverter(target.target)],
+      callbackUri: callbackUrl,
+      operationContext: options.operationContext,
+      azureCognitiveServicesEndpointUrl: options.azureCognitiveServicesEndpointUrl,
+      mediaStreamingConfiguration: options.mediaStreamingConfiguration,
+      customContext: {
+        sipHeaders: target.sipHeaders,
+        voipHeaders: target.voipHeaders,
+      },
+      sourceCallerIdNumber: PhoneNumberIdentifierModelConverter(target.sourceCallIdNumber),
+      sourceDisplayName: target.sourceDisplayName
+    };
+
+    return this.createCallInternal(request, options);
+  }
+
+  /**
+   * Create an outgoing call from source to a group of targets identities.
+   * @param targets - A group of targets identities.
+   * @param callbackUrl - The callback url.
+   * @param options - Additional request options contains createCallConnection api options.
+   */
+  public async createGroupCall(
+    targets: CommunicationIdentifier[],
+    callbackUrl: string,
+    options: CreateCallOptions = {}
+  ): Promise<CreateCallResult> {
+    const request: CreateCallRequest = {
+      sourceIdentity: this.sourceIdentity,
+      targets: targets.map((target) => communicationIdentifierModelConverter(target)),
+      callbackUri: callbackUrl,
+      operationContext: options.operationContext,
+      azureCognitiveServicesEndpointUrl: options.azureCognitiveServicesEndpointUrl,
+      mediaStreamingConfiguration: options.mediaStreamingConfiguration,
+      customContext: {
+        sipHeaders: options.sipHeaders,
+        voipHeaders: options.voipHeaders,
+      },
+      sourceCallerIdNumber: PhoneNumberIdentifierModelConverter(options.sourceCallIdNumber),
+      sourceDisplayName: options.sourceDisplayName
+    };
+
+    return this.createCallInternal(request, options);
   }
 
   /**
