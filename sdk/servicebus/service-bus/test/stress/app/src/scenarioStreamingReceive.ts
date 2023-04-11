2be146a5409382a2f9c311b35eaafee4da45b075
Harsha Nalluru <sanallur@microsoft.com>
2023-04-11 17:30:48 +0000

    15 days test

--- sdk/servicebus/service-bus/test/stress/app/src/scenarioStreamingReceive.ts
+++ sdk/servicebus/service-bus/test/stress/app/src/scenarioStreamingReceive.ts
@@ -37,15 +37,15 @@ function sanitizeOptions(args: string[]): Required<ScenarioStreamingReceiveOptio
     },
   });
   return {
-    testDurationInMs: options.testDurationInMs || 60 * 60 * 1000, // Default = 60 minutes
+    testDurationInMs: options.testDurationInMs || 15 * 24 * 60 * 60 * 1000, // Default = 15 days
     receiveMode: options.receiveMode || "peekLock",
     autoComplete: !!options.autoComplete,
     maxConcurrentCalls: options.maxConcurrentCalls || 100,
     maxAutoRenewLockDurationInMs: options.maxAutoRenewLockDurationInMs || 0,
     manualLockRenewal: options.manualLockRenewal,
-    numberOfMessagesPerSend: options.numberOfMessagesPerSend || 1,
+    numberOfMessagesPerSend: options.numberOfMessagesPerSend || 0,
     delayBetweenSendsInMs: options.delayBetweenSendsInMs || 0,
-    totalNumberOfMessagesToSend: options.totalNumberOfMessagesToSend || Infinity,
+    totalNumberOfMessagesToSend: options.totalNumberOfMessagesToSend || 0,
     completeMessageAfterDuration: !!options.completeMessageAfterDuration,
     settleMessageOnReceive: !!options.settleMessageOnReceive,
   };
@@ -72,7 +72,6 @@ export async function scenarioStreamingReceive() {
 
   const stressBase = new ServiceBusStressTester({
     testName: "streamingReceive",
-    snapshotFocus: ["send-info", "receive-info", "message-lock-renewal-info"],
   });
 
   const operation = async (sbClient: ServiceBusClient) => {
@@ -116,7 +115,7 @@ export async function scenarioStreamingReceive() {
   };
 
   return stressBase.runStressTest(operation, {
-    additionalEventProperties: testOptions,
+    additionalEventProperties: testOptions
   });
 }
 
