76c88afe9057369f3340633298606162e083aa88
Jeremy Meng <jeremy.ymeng@gmail.com>
2023-04-21 15:20:15 -0700

    [samples][web-worker] use SAS url in browsers (#25620)
    Due to security reason we don't want to have secrets in the browsers.
    This PR updates the web-worker sample to use container SAS url to
    construct storage blob clients.

--- samples/web-workers/ts/worker.ts
+++ samples/web-workers/ts/worker.ts
@@ -7,26 +7,13 @@ import "./polyfill.worker";
 import { ContainerClient } from "@azure/storage-blob";
 
 async function uploadToStorageBlob() {
-  const azureStorageBlobConnectionString = process.env.AZURE_STORAGE_BLOB_CONNECTION_STRING;
-  if (!azureStorageBlobConnectionString) {
-    throw new Error(
-      "Required environment variable AZURE_STORAGE_BLOB_CONNECTION_STRING is either missing or empty."
-    );
-  }
-
-  const azureStorageBlobContainerName = process.env.AZURE_STORAGE_BLOB_CONTAINER_NAME;
-  if (!azureStorageBlobContainerName) {
-    throw new Error(
-      "Required environment variable AZURE_STORAGE_BLOB_CONTAINER_NAME is either missing or empty."
-    );
-  }
+  // For example, in Azure public cloud, a container SAS URL has the form of
+  // `https://${account}.blob.core.windows.net/${containerName}?${sasToken}`
+  const containerSasUrl = "<container SAS url>";
 
   const data = "Hello, Web Workers!";
 
-  const containerClient = new ContainerClient(
-    azureStorageBlobConnectionString,
-    azureStorageBlobContainerName
-  );
+  const containerClient = new ContainerClient(containerSasUrl);
   const blockBlobClient = containerClient.getBlockBlobClient("sample.txt");
 
   await blockBlobClient.upload(data, data.length);
