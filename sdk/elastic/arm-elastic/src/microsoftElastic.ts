a41b62c9092dd68e2383314c2cb95007e52603ea
azure-sdk <azuresdk@microsoft.com>
2023-04-24 03:04:45 +0000

    Post release automated changes for azure-arm-elastic

--- sdk/elastic/arm-elastic/src/microsoftElastic.ts
+++ sdk/elastic/arm-elastic/src/microsoftElastic.ts
@@ -93,7 +93,7 @@ export class MicrosoftElastic extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-elastic/1.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-elastic/1.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
