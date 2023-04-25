2887e9195399ea14f681a3b50fbc8bd9372855a0
Karishma Ghiya <kaghiya@microsoft.com>
2023-04-25 10:45:26 -0700

    format

--- sdk/identity/identity/src/credentials/authorityValidationOptions.ts
+++ sdk/identity/identity/src/credentials/authorityValidationOptions.ts
@@ -8,7 +8,7 @@
  */
 export interface AuthorityValidationOptions {
   /**
-   * The field determines whether instance discovery is performed when attempting to authenticate. 
+   * The field determines whether instance discovery is performed when attempting to authenticate.
    * Setting this to `true` will completely disable both instance discovery and authority validation.
    * As a result, it's crucial to ensure that the configured authority host is valid and trustworthy.
    * This functionality is intended for use in scenarios where the metadata endpoint cannot be reached, such as in private clouds or Azure Stack.
