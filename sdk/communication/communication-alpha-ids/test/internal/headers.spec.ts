e7db1326c7e2eb04d97bb9c86ef95528a8e0f5b0
Roberto Trevino <rtrevino@microsoft.com>
2023-04-26 13:21:20 -0600

    include generated client test

--- sdk/communication/communication-alpha-ids/test/internal/headers.spec.ts
+++ sdk/communication/communication-alpha-ids/test/internal/headers.spec.ts
@@ -3,7 +3,7 @@
 
 import { AzureKeyCredential } from "@azure/core-auth";
 import { Context } from "mocha";
-import { PipelineRequest, PipelinePolicy, createEmptyPipeline } from "@azure/core-rest-pipeline";
+import { PipelineRequest } from "@azure/core-rest-pipeline";
 import { SDK_VERSION } from "../../src/utils/constants";
 import { AlphaIdsClient } from "../../src";
 import { TokenCredential } from "@azure/identity";
@@ -12,20 +12,6 @@ import { createMockToken } from "../public/utils/recordedClient";
 import { configurationHttpClient } from "../public/utils/mockHttpClients";
 import { isNode } from "@azure/test-utils";
 import sinon from "sinon";
-
-export const userAgentPolicy: (policyName: string, customHeader: string) => PipelinePolicy = (
-  customHeader: string,
-  policyName: string
-) => {
-  return {
-    name: policyName,
-    sendRequest: async (req, next) => {
-      const userAgentHeader = isNode ? "user-agent" : "x-ms-useragent";
-      req.headers.set(userAgentHeader, customHeader);
-      return next(req);
-    },
-  };
-};
 
 describe("AlphaIdsClient - headers", function () {
   const endpoint = "https://contoso.spool.azure.local";
@@ -127,37 +113,5 @@ describe("AlphaIdsClient - headers", function () {
       request.headers.get(userAgentHeader) as string,
       new RegExp(`alphaidsclient-headers-test azsdk-js-communication-alpha-ids/${SDK_VERSION}`, "g")
     );
-  });
-
-  it("can set custom user-agent prefix through additional policies", async function () {
-    const testPipeline = createEmptyPipeline();
-    const customPolicyName = "custom-header-policy";
-    const customHeader = "alphaidsclient-headers-test-additional";
-    const customApiVersion = "alphaids-custom-api-version";
-    testPipeline.addPolicy(userAgentPolicy(customHeader, customPolicyName));
-    client = new AlphaIdsClient(`endpoint=${endpoint};accessKey=${accessKey}`, {
-      httpClient: configurationHttpClient,
-      apiVersion: customApiVersion,
-      pipeline: testPipeline,
-    } as never);
-
-    const spy = sinon.spy(configurationHttpClient, "sendRequest");
-    await client.getConfiguration();
-    sinon.assert.calledOnce(spy);
-    request = spy.getCall(0).args[0];
-
-    const userAgentHeader = isNode ? "user-agent" : "x-ms-useragent";
-    assert.match(request.headers.get(userAgentHeader) as string, new RegExp(customHeader, "g"));
-    assert.match(request.url, new RegExp(customApiVersion, "g"));
-  });
-
-  it("set undefined endpoint and test the error from client", async function () {
-    const undefinedConnection: unknown = null;
-    assert.throws(() => {
-      new AlphaIdsClient(undefinedConnection as string);
-    });
-    //assert client is created correctly without options
-    const client = new AlphaIdsClient(`endpoint=${endpoint};accessKey=${accessKey}`);
-    assert.instanceOf(client, AlphaIdsClient);
   });
 });
