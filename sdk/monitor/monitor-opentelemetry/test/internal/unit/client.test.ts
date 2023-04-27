fc81e50b36d1afe31b3a1c85207c0e2ada817fc3
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2023-04-27 16:34:02 -0700

    Fix test

--- sdk/monitor/monitor-opentelemetry/test/internal/unit/client.test.ts
+++ sdk/monitor/monitor-opentelemetry/test/internal/unit/client.test.ts
@@ -3,10 +3,15 @@
 
 import * as assert from "assert";
 import { AzureMonitorOpenTelemetryClient } from "../../../src/client";
+import { AzureMonitorOpenTelemetryConfig } from "../../../src/shared";
 
 describe("AzureMonitorOpenTelemetryClient", () => {
   it("constructor", () => {
-    const client = new AzureMonitorOpenTelemetryClient();
+    let config = new AzureMonitorOpenTelemetryConfig();
+    config.azureMonitorExporterConfig = {
+      connectionString: "InstrumentationKey=00000000-0000-0000-0000-000000000000",
+    };
+    const client = new AzureMonitorOpenTelemetryClient(config);
     assert.notStrictEqual(client, true);
     // TODO: Add tests
   });
