ecf37fa92f34568cabb5db4ae0b114b6bd5713b0
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2023-04-28 16:15:39 -0700

    Add @azure/monitor-opentelemetry project (#25621)
    New project used to host OpenTelemetry Azure Monitor Distro, allowing
    customers to quickly start using OpenTelemetry and send data to Azure
    Monitor

---
+++ sdk/monitor/monitor-opentelemetry/README.md
@@ -0,0 +1,70 @@
+# Azure Monitor OpenTelemetry client library for JavaScript
+
+[![npm version](https://badge.fury.io/js/%40azure%2Fmonitor-opentelemetry.svg)](https://badge.fury.io/js/%40azure%2Fmonitor-opentelemetry)
+
+## Getting started
+
+### Install the package
+
+`npm install @azure/monitor-opentelemetry`
+
+### Currently supported environments
+
+- [LTS versions of Node.js](https://github.com/nodejs/release#release-schedule)
+
+See our [support policy](https://github.com/Azure/azure-sdk-for-js/blob/main/SUPPORT.md) for more details.
+
+### Prerequisites
+
+- An [Azure subscription](https://azure.microsoft.com/free/)
+- An [Application Insights workspace](https://docs.microsoft.com/azure/azure-monitor/app/app-insights-overview/)
+
+### Enable Azure Monitor OpenTelemetry Client
+
+> *Important:* `AzureMonitorOpenTelemetryClient` must be setup *and* started *before* you import anything else. There may be resulting telemetry loss if other libraries are imported first.
+
+
+```typescript
+const { AzureMonitorOpenTelemetryClient, AzureMonitorOpenTelemetryConfig } = require("@azure/monitor-opentelemetry");
+
+const config = new AzureMonitorOpenTelemetryConfig();
+config.connectionString = "<YOUR_CONNECTION_STRING>";
+const client = new AzureMonitorOpenTelemetryClient(config);
+```
+
+* Connection String could be set using the environment variable APPLICATIONINSIGHTS\_CONNECTION\_STRING
+
+## Examples
+
+For complete samples of a few champion scenarios, see the [`samples/`](https://github.com/Azure/azure-sdk-for-js/tree/main/sdk/monitor/monitor-opentelemetry/samples-dev/) folder.
+
+## Key concepts
+
+For more information on the OpenTelemetry project, please review the [**OpenTelemetry Specifications**](https://github.com/open-telemetry/opentelemetry-specification#opentelemetry-specification).
+
+## Troubleshooting
+
+### Enable debug logging
+
+You can enable debug logging by changing the logging level of your provider.
+
+```js
+const { DiagConsoleLogger, DiagLogLevel, diag } = require("@opentelemetry/api");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+
+const provider = new NodeTracerProvider();
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+provider.register();
+```
+
+### Plugin Registry
+
+To see if a plugin has already been made for a library you are using, please check out the [OpenTelemetry Registry](https://opentelemetry.io/registry/).
+
+If you cannot your library in the registry, feel free to suggest a new plugin request at [`opentelemetry-js-contrib`](https://github.com/open-telemetry/opentelemetry-js-contrib).
+
+## Contributing
+
+If you'd like to contribute to this library, please read the [contributing guide](https://github.com/Azure/azure-sdk-for-js/blob/main/CONTRIBUTING.md) to learn more about how to build and test the code.
+
+![Impressions](https://azure-sdk-impressions.azurewebsites.net/api/impressions/azure-sdk-for-js/sdk/monitor/monitor-opentelemetry/README.png)
---
+++ sdk/monitor/monitor-opentelemetry/src/traces/handler.ts
@@ -0,0 +1,201 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import { RequestOptions } from "http";
+import { createAzureSdkInstrumentation } from "@azure/opentelemetry-instrumentation-azure-sdk";
+import {
+  ApplicationInsightsSampler,
+  AzureMonitorTraceExporter,
+} from "@azure/monitor-opentelemetry-exporter";
+import { NodeTracerProvider, NodeTracerConfig } from "@opentelemetry/sdk-trace-node";
+import {
+  BatchSpanProcessor,
+  BufferConfig,
+  SpanProcessor,
+  Tracer,
+} from "@opentelemetry/sdk-trace-base";
+import { TracerProvider } from "@opentelemetry/api";
+import { Instrumentation } from "@opentelemetry/instrumentation";
+import {
+  HttpInstrumentation,
+  HttpInstrumentationConfig,
+  IgnoreOutgoingRequestFunction,
+} from "@opentelemetry/instrumentation-http";
+import { MongoDBInstrumentation } from "@opentelemetry/instrumentation-mongodb";
+import { MySQLInstrumentation } from "@opentelemetry/instrumentation-mysql";
+import { PgInstrumentation } from "@opentelemetry/instrumentation-pg";
+import { RedisInstrumentation } from "@opentelemetry/instrumentation-redis";
+import { RedisInstrumentation as Redis4Instrumentation } from "@opentelemetry/instrumentation-redis-4";
+
+import { AzureMonitorOpenTelemetryConfig } from "../shared/config";
+import { MetricHandler } from "../metrics/handler";
+import { ignoreOutgoingRequestHook } from "../utils/common";
+import { AzureMonitorSpanProcessor } from "./spanProcessor";
+
+/**
+ * Azure Monitor OpenTelemetry Trace Handler
+ */
+export class TraceHandler {
+  private _spanProcessor: BatchSpanProcessor;
+  private _tracerProvider: NodeTracerProvider;
+  private _tracer: Tracer;
+  private _exporter: AzureMonitorTraceExporter;
+  private _instrumentations: Instrumentation[];
+  private _httpInstrumentation?: Instrumentation;
+  private _azureSdkInstrumentation?: Instrumentation;
+  private _mongoDbInstrumentation?: Instrumentation;
+  private _mySqlInstrumentation?: Instrumentation;
+  private _postgressInstrumentation?: Instrumentation;
+  private _redisInstrumentation?: Instrumentation;
+  private _redis4Instrumentation?: Instrumentation;
+
+  /**
+   * Initializes a new instance of the TraceHandler class.
+   * @param _config - Configuration.
+   * @param _metricHandler - MetricHandler.
+   */
+  constructor(
+    private _config: AzureMonitorOpenTelemetryConfig,
+    private _metricHandler?: MetricHandler
+  ) {
+    this._instrumentations = [];
+    const aiSampler = new ApplicationInsightsSampler(this._config.samplingRatio);
+    const tracerConfig: NodeTracerConfig = {
+      sampler: aiSampler,
+      resource: this._config.resource,
+      forceFlushTimeoutMillis: 30000,
+    };
+    this._tracerProvider = new NodeTracerProvider(tracerConfig);
+    this._exporter = new AzureMonitorTraceExporter(this._config.azureMonitorExporterConfig);
+    const bufferConfig: BufferConfig = {
+      maxExportBatchSize: 512,
+      scheduledDelayMillis: 5000,
+      exportTimeoutMillis: 30000,
+      maxQueueSize: 2048,
+    };
+    this._spanProcessor = new BatchSpanProcessor(this._exporter, bufferConfig);
+    this._tracerProvider.addSpanProcessor(this._spanProcessor);
+    this._tracerProvider.register();
+    this._tracer = this._tracerProvider.getTracer("AzureMonitorTracer");
+    if (this._metricHandler) {
+      const azureSpanProcessor = new AzureMonitorSpanProcessor(this._metricHandler);
+      this._tracerProvider.addSpanProcessor(azureSpanProcessor);
+    }
+    this._initializeInstrumentations();
+  }
+
+  /**
+   *Get OpenTelemetry TracerProvider
+   */
+  public getTracerProvider(): TracerProvider {
+    return this._tracerProvider;
+  }
+
+  /**
+   *Get OpenTelemetry Tracer
+   */
+  public getTracer(): Tracer {
+    return this._tracer;
+  }
+
+  /**
+   * Shutdown handler, all Tracer providers will return no-op Tracers
+   */
+  public async shutdown(): Promise<void> {
+    await this._tracerProvider.shutdown();
+  }
+
+  /**
+   * Force flush Tracer Provider
+   */
+  public async flush(): Promise<void> {
+    return this._tracerProvider.forceFlush();
+  }
+
+  /**
+   * Add OpenTelemetry Span Processor
+   */
+  public addSpanProcessor(spanProcessor: SpanProcessor) {
+    this._tracerProvider.addSpanProcessor(spanProcessor);
+  }
+
+  /**
+   * Add OpenTelemetry Instrumentation, should be called before calling start
+   */
+  public addInstrumentation(instrumentation?: Instrumentation) {
+    if (instrumentation) {
+      this._instrumentations.push(instrumentation);
+    }
+  }
+
+  /**
+   * Disable all OpenTelemetry Instrumentations
+   */
+  public disableInstrumentations() {
+    this._instrumentations.forEach((instrumentation) => {
+      instrumentation.disable();
+    });
+  }
+
+  /**
+   * Start auto collection of telemetry
+   */
+  private _initializeInstrumentations() {
+    if (!this._httpInstrumentation) {
+      const httpInstrumentationConfig = this._config.instrumentations
+        .http as HttpInstrumentationConfig;
+      const providedIgnoreOutgoingRequestHook = httpInstrumentationConfig.ignoreOutgoingRequestHook;
+      const mergedIgnoreOutgoingRequestHook: IgnoreOutgoingRequestFunction = (
+        request: RequestOptions
+      ) => {
+        const result = ignoreOutgoingRequestHook(request);
+        if (!result) {
+          // Not internal call
+          if (providedIgnoreOutgoingRequestHook) {
+            // Provided hook in config
+            return providedIgnoreOutgoingRequestHook(request);
+          }
+        }
+        return result;
+      };
+      httpInstrumentationConfig.ignoreOutgoingRequestHook = mergedIgnoreOutgoingRequestHook;
+      this._httpInstrumentation = new HttpInstrumentation(this._config.instrumentations.http);
+      this.addInstrumentation(this._httpInstrumentation);
+    }
+    if (!this._azureSdkInstrumentation) {
+      this._azureSdkInstrumentation = createAzureSdkInstrumentation(
+        this._config.instrumentations.azureSdk
+      ) as any;
+      this.addInstrumentation(this._azureSdkInstrumentation);
+    }
+    if (!this._mongoDbInstrumentation) {
+      this._mongoDbInstrumentation = new MongoDBInstrumentation(
+        this._config.instrumentations.mongoDb
+      );
+      this.addInstrumentation(this._mongoDbInstrumentation);
+    }
+    if (!this._mySqlInstrumentation) {
+      this._mySqlInstrumentation = new MySQLInstrumentation(this._config.instrumentations.mySql);
+      this.addInstrumentation(this._mySqlInstrumentation);
+    }
+    if (!this._postgressInstrumentation) {
+      this._postgressInstrumentation = new PgInstrumentation(
+        this._config.instrumentations.postgreSql
+      );
+      this.addInstrumentation(this._postgressInstrumentation);
+    }
+    if (!this._redisInstrumentation) {
+      this._redisInstrumentation = new RedisInstrumentation(this._config.instrumentations.redis);
+      this.addInstrumentation(this._redisInstrumentation);
+    }
+    if (!this._redis4Instrumentation) {
+      this._redis4Instrumentation = new Redis4Instrumentation(this._config.instrumentations.redis4);
+      this.addInstrumentation(this._redis4Instrumentation);
+    }
+    this._instrumentations.forEach((instrumentation) => {
+      instrumentation.setTracerProvider(this._tracerProvider);
+      if (instrumentation.getConfig().enabled) {
+        instrumentation.enable();
+      }
+    });
+  }
+}
