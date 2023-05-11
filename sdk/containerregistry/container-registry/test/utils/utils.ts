7679764f2c46c666eeec3ed8bd27ce63aee0c7cf
Timo van Veenendaal <timov@microsoft.com>
2023-05-12 08:17:01 +1200

    Format

--- sdk/containerregistry/container-registry/test/utils/utils.ts
+++ sdk/containerregistry/container-registry/test/utils/utils.ts
@@ -51,8 +51,8 @@ export const recorderStartOptions: RecorderStartOptions = {
       {
         regex: true,
         target: `sig=([^&]+)`,
-        value: `sig=sanitized`
-      }
+        value: `sig=sanitized`,
+      },
     ],
     bodyKeySanitizers: [
       {
