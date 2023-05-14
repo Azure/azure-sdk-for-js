2b271f1cdb9dc25b60c055961d77c58cce52fc34
Oleg Demchenko <oldemche@microsoft.com>
2023-05-14 13:08:03 -0700

    1.0.0-beta.3 -> 1.0.0 rename

--- sdk/devcenter/developer-devcenter-rest/src/generated/azureDevCenter.ts
+++ sdk/devcenter/developer-devcenter-rest/src/generated/azureDevCenter.ts
@@ -24,7 +24,7 @@ export default function createClient(
     }
   };
 
-  const userAgentInfo = `azsdk-js-developer-devcenter-rest/1.0.0-beta.3`;
+  const userAgentInfo = `azsdk-js-developer-devcenter-rest/1.0.0`;
   const userAgentPrefix =
     options.userAgentOptions && options.userAgentOptions.userAgentPrefix
       ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
