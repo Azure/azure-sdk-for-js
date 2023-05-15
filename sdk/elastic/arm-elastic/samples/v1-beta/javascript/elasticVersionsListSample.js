fe70fcfb8185fb29aa74392cca59eb87bfea9968
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-05-15 16:24:09 +0800

    elastic release

--- sdk/elastic/arm-elastic/samples/v1-beta/javascript/elasticVersionsListSample.js
+++ sdk/elastic/arm-elastic/samples/v1-beta/javascript/elasticVersionsListSample.js
@@ -13,22 +13,26 @@ const { DefaultAzureCredential } = require("@azure/identity");
 require("dotenv").config();
 
 /**
- * This sample demonstrates how to Fetch User API Key from internal database, if it was generated and stored while creating the Elasticsearch Organization.
+ * This sample demonstrates how to Get a list of available versions for a region.
  *
- * @summary Fetch User API Key from internal database, if it was generated and stored while creating the Elasticsearch Organization.
- * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples/Organizations_GetApiKey.json
+ * @summary Get a list of available versions for a region.
+ * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/preview/2023-02-01-preview/examples/ElasticVersions_List.json
  */
-async function organizationsGetApiKey() {
+async function elasticVersionsList() {
   const subscriptionId =
     process.env["ELASTIC_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const region = "myregion";
   const credential = new DefaultAzureCredential();
   const client = new MicrosoftElastic(credential, subscriptionId);
-  const result = await client.organizations.getApiKey();
-  console.log(result);
+  const resArray = new Array();
+  for await (let item of client.elasticVersions.list(region)) {
+    resArray.push(item);
+  }
+  console.log(resArray);
 }
 
 async function main() {
-  organizationsGetApiKey();
+  elasticVersionsList();
 }
 
 main().catch(console.error);
