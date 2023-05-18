177029064995511fb7bdf35202cb935ceec65db4
Babacar Ndoye <babacarndoye@microsoft.com>
2023-05-18 11:18:54 -0500

    SDK for tiering

---
+++ sdk/communication/communication-tiering/src/mapper.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export { AssetDetails as AssetDetailsMapper } from "./generated/src/models/mappers";
+export { AssetDetailsTierInfo as AssetDetailsTierInfoMapper } from "./generated/src/models/mappers";
---
+++ sdk/communication/communication-tiering/src/models.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  NumberAllotmentGetAcquiredNumberLimitsOptionalParams,
+  NumberAllotmentGetAcquiredNumberLimitsResponse,
+  AssetDetails,
+  AssetDetailsTierInfo,
+  TieringClientOptionalParams,
+} from "./generated/src/models";
