6ef9b588f2ded23fdcef17205617539a6982df33
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2023-05-19 04:56:00 +0000

    update dev-tool

--- common/tools/dev-tool/src/commands/run/testNodeTSInput.ts
+++ common/tools/dev-tool/src/commands/run/testNodeTSInput.ts
@@ -2,24 +2,18 @@
 // Licensed under the MIT license
 
 import { leafCommand, makeCommandInfo } from "../../framework/command";
+import { resolveProject } from "../../util/resolveProject";
 import { runTestsWithProxyTool } from "../../util/testUtils";
 
 export const commandInfo = makeCommandInfo(
   "test:node-ts-input",
-  "runs the node tests using mocha with the default and the provided options; starts the proxy-tool in record and playback modes",
-  {
-    modular: {
-      kind: "boolean",
-      description: "Whether the library follows the modular design pattern",
-      default: false,
-      allowMultiple: false,
-    },
-  }
+  "runs the node tests using mocha with the default and the provided options; starts the proxy-tool in record and playback modes"
 );
 
-export default leafCommand(commandInfo, async ({ modular, ...options }) => {
+export default leafCommand(commandInfo, async (options) => {
+  const projectInfo = await resolveProject(process.cwd());
   const defaultMochaArgs = `${
-    modular ? "" : "-r esm "
+    projectInfo.packageJson.type === "module" ? "" : "-r esm "
   }-r ts-node/register --reporter ../../../common/tools/mocha-multi-reporter.js --full-trace`;
   const updatedArgs = options["--"]?.map((opt) =>
     opt.includes("**") && !opt.startsWith("'") && !opt.startsWith('"') ? `"${opt}"` : opt
