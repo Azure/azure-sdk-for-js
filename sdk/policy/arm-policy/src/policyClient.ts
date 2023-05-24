a2e73e86bf20d2702135468d0fcadb6dbeb61e55
azure-sdk <azuresdk@microsoft.com>
2023-05-24 08:08:52 +0000

    Post release automated changes for azure-arm-policy

--- sdk/policy/arm-policy/src/policyClient.ts
+++ sdk/policy/arm-policy/src/policyClient.ts
@@ -71,7 +71,7 @@ export class PolicyClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-policy/5.1.0`;
+    const packageDetails = `azsdk-js-arm-policy/5.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
