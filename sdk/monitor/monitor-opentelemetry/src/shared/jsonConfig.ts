476fd9537be1d960fa9ff1ab2f22516ef46831da
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2023-05-24 10:25:33 -0700

    format

--- sdk/monitor/monitor-opentelemetry/src/shared/jsonConfig.ts
+++ sdk/monitor/monitor-opentelemetry/src/shared/jsonConfig.ts
@@ -1,9 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import {
-  AzureMonitorOpenTelemetryOptions,
-  InstrumentationOptions,
-} from "./types";
+import { AzureMonitorOpenTelemetryOptions, InstrumentationOptions } from "./types";
 import { AzureMonitorExporterOptions } from "@azure/monitor-opentelemetry-exporter";
 import { Logger } from "./logging";
 
