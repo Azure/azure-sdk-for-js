21db4e1ddcff163c4aee4dd74420a3ea2cdb02a2
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-05-29 11:20:39 +0800

    subscriptions release (#25783)
    https://github.com/Azure/sdk-release-request/issues/4063
    Recording:
    https://github.com/Azure/azure-sdk-assets/tree/js/resources-subscriptions/arm-resources-subscriptions_c4231ede92

--- sdk/resources-subscriptions/arm-resources-subscriptions/samples/v2/javascript/subscriptionsCheckZonePeersSample.js
+++ sdk/resources-subscriptions/arm-resources-subscriptions/samples/v2/javascript/subscriptionsCheckZonePeersSample.js
@@ -13,21 +13,25 @@ const { DefaultAzureCredential } = require("@azure/identity");
 require("dotenv").config();
 
 /**
- * This sample demonstrates how to Gets details about a specified subscription.
+ * This sample demonstrates how to Compares a subscriptions logical zone mapping
  *
- * @summary Gets details about a specified subscription.
- * x-ms-original-file: specification/resources/resource-manager/Microsoft.Resources/stable/2022-12-01/examples/GetSubscription.json
+ * @summary Compares a subscriptions logical zone mapping
+ * x-ms-original-file: specification/resources/resource-manager/Microsoft.Resources/stable/2022-12-01/examples/PostCheckZonePeers.json
  */
-async function getASingleSubscription() {
-  const subscriptionId = "291bba3f-e0a5-47bc-a099-3bdcb2a50a05";
+async function getLogicalZoneMapping() {
+  const subscriptionId = "8d65815f-a5b6-402f-9298-045155da7d74";
+  const parameters = {
+    location: "eastus",
+    subscriptionIds: ["subscriptions/11111111-1111-1111-1111-111111111111"],
+  };
   const credential = new DefaultAzureCredential();
   const client = new SubscriptionClient(credential);
-  const result = await client.subscriptions.get(subscriptionId);
+  const result = await client.subscriptions.checkZonePeers(subscriptionId, parameters);
   console.log(result);
 }
 
 async function main() {
-  getASingleSubscription();
+  getLogicalZoneMapping();
 }
 
 main().catch(console.error);
