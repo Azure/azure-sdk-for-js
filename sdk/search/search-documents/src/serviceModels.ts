5d024de01cc4458302912eb2960e80ee83daf779
Daniel Getu <danielgetu@microsoft.com>
2023-05-31 23:37:47 +0000

    Fix comment formatting

--- sdk/search/search-documents/src/serviceModels.ts
+++ sdk/search/search-documents/src/serviceModels.ts
@@ -741,13 +741,25 @@ export type LexicalTokenizer =
   | LuceneStandardTokenizer
   | UaxUrlEmailTokenizer;
 
-/** Definition of additional projections to azure blob, table, or files, of enriched data. */
+/**
+ *  Definition of additional projections to azure blob, table, or files, of enriched data.
+ */
 export interface SearchIndexerKnowledgeStore {
-  /** The connection string to the storage account projections will be stored in. */
+  /**
+   * The connection string to the storage account projections will be stored in.
+   */
   storageConnectionString: string;
-  /** A list of additional projections to perform during indexing. */
+  /**
+   * A list of additional projections to perform during indexing.
+   */
   projections: SearchIndexerKnowledgeStoreProjection[];
-  /** The user-assigned managed identity used for connections to Azure Storage when writing knowledge store projections. If the connection string indicates an identity (ResourceId) and it's not specified, the system-assigned managed identity is used. On updates to the indexer, if the identity is unspecified, the value remains unchanged. If set to "none", the value of this property is cleared. */
+  /**
+   * The user-assigned managed identity used for connections to Azure Storage when writing
+   * knowledge store projections. If the connection string indicates an identity (ResourceId) and
+   * it's not specified, the system-assigned managed identity is used. On updates to the indexer,
+   * if the identity is unspecified, the value remains unchanged. If set to "none", the value of
+   * this property is cleared.
+   */
   identity?: SearchIndexerDataIdentity;
 }
 
