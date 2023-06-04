c9efe56e58751d915d6a6c775bc1ace2f0c82240
Harsha Nalluru <sanallur@microsoft.com>
2023-06-03 17:46:19 -0700

    [Event Hubs] Add stress tests to event-hubs SDK (#25661)
    # Stress tests for Event Hubs
    - Adds the stress testing framework to the Event Hubs SDK.
    - Adds the relevant tests from
    https://gist.github.com/HarshaNalluru/2f1931ea30e8335678310053aa342db0
    that have helped in the recent memory-leak investigations.
    - Follows the patterns from the service-bus stress tests
    - In sync with SB after
    https://github.com/Azure/azure-sdk-for-js/pull/25658
    - tracks message count
    - tracks memory metrics
    
    ## EventHubsStressTester 
    - Sets up Application Insights for logging and tracking events.
    - `EventHubsStressTester` class has methods for initializing the test,
    taking snapshots of test progress, and ending the test.
    - `loopForever` takes a function fn, a delay duration, and an optional
    abort signal as arguments. The function enters a loop that calls fn
    repeatedly with a delay between each call until the abort signal is
    triggered or times out.
    
    ## noActivity test
    - **Initialization**: The main function `scenarioNoActivity` creates
    instances of Event Hubs consumer and producer clients and subscribes to
    all partitions of the consumer client with handlers for processing
    events, errors, initialization, and closing.
    - **Execution**: The producer sends a batch of two events and starts a
    loop that checks if the test duration has been reached or if any of the
    subscribers have stopped running.
    - **Cleanup**: If either condition is met, the loop exits and the
    producer and consumer clients are closed. The stress test is ended by
    calling the `endTest` method on the `stressBase` instance.
    
    ## getRuntimeProperties test
    - **Initialization**: The main function `scenarioGetRuntimeProperties`
    creates instances of Event Hubs consumer and producer clients and a
    `stressBase` instance for stress testing.
    - **Execution**: The function defines an asynchronous function `func`
    that enters a loop and repeatedly calls the `getEventHubProperties`
    method on the consumer client with a random delay between each call. The
    function then creates an array of 100 promises that resolve when `func`
    completes and waits for all promises to resolve.
    - **Cleanup**: Once all promises have resolved, the producer and
    consumer clients are closed and the stress test is ended by calling the
    `endTest` method on the `stressBase` instance.
    
    ## checkpoint store test
    - **Initialization**: The main function `scenarioCheckpointStore`
    creates instances of Event Hubs consumer and producer clients, a
    `stressBase` instance for stress testing, and a `BlobCheckpointStore`
    instance for storing checkpoints. The consumer client subscribes to the
    default consumer group with handlers for processing events, errors,
    initialization, and closing.
    - **Execution**: The function enters a loop that sends a batch of random
    events using the producer client and waits for a random delay between
    each iteration. The loop checks if the test duration has been reached or
    if the subscription has stopped running.
    - **Cleanup**: If either condition is met, the loop exits and the
    producer and consumer clients are closed. The stress test is ended by
    calling the `endTest` method on the `stressBase` instance.
    
    ---------
    
    Co-authored-by: Deyaaeldeen Almahallawi <dealmaha@microsoft.com>

---
+++ sdk/eventhub/event-hubs/test/stress/app/src/eventHubsStressTester.ts
@@ -0,0 +1,98 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import util from "util";
+import { SnapshotOptions } from "./utils";
+import * as appInsights from "applicationinsights";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: process.env.ENV_FILE || ".env" });
+
+appInsights.setup().setAutoCollectConsole(true).setUseDiskRetryCaching(true).start();
+
+export const defaultClientAppInsights = appInsights.defaultClient;
+
+export interface StressTestInitOptions {
+  /**
+   * Additional custom properties to add to the 'start' event reported to Monitor.
+   */
+  additionalEventProperties?: Record<string, string | number | boolean>;
+  eventHubName: string;
+}
+
+export function captureConsoleOutputToAppInsights() {
+  const debug = require("debug");
+
+  debug.log = (...args: any[]) => {
+    // for some reason the appinsights console.log hook doesn't seem to be firing for me (or at least
+    // it's inconsistent). For now I'll just add a hook in here and send the events myself.
+    defaultClientAppInsights.trackTrace({
+      message: util.format(...args),
+    });
+  };
+}
+
+export class EventHubsStressTester {
+  public eventsSentCount = 0;
+  public eventsReceivedCount = 0;
+  private snapshotTimer: NodeJS.Timer;
+  private startedAt!: Date;
+  public _numErrors = 0;
+  public eventProperties: Record<string, string | boolean | number> = {};
+
+  constructor(private snapshotOptions: SnapshotOptions) {
+    const snapshotIntervalMs = !this.snapshotOptions.snapshotIntervalInMs
+      ? 5000
+      : this.snapshotOptions.snapshotIntervalInMs;
+
+    this.startedAt = new Date();
+    this.snapshotTimer = setInterval(this.snapshot.bind(this), snapshotIntervalMs);
+  }
+
+  public async init(options?: StressTestInitOptions) {
+    defaultClientAppInsights.commonProperties = {
+      // these will be reported with each event
+      testName: this.snapshotOptions.testName,
+    };
+
+    defaultClientAppInsights.trackEvent({
+      name: "start",
+      properties: {
+        ...options?.additionalEventProperties,
+        eventHubsName: options?.eventHubName,
+      },
+    });
+  }
+
+  public async snapshot(): Promise<void> {
+    const elapsedTimeInSeconds = (new Date().valueOf() - this.startedAt.valueOf()) / 1000;
+
+    this.eventProperties["elapsedTimeInSeconds"] = elapsedTimeInSeconds;
+    this.eventProperties["events.sent"] = this.eventsSentCount;
+    this.eventProperties["events.received"] = this.eventsReceivedCount;
+    this.eventProperties["errorCount"] = this._numErrors;
+    const { arrayBuffers, rss, heapUsed } = process.memoryUsage();
+    this.eventProperties["memory.arrayBuffers"] = arrayBuffers;
+    this.eventProperties["memory.rss"] = rss;
+    this.eventProperties["memory.heapUsed"] = heapUsed;
+    this._numErrors = 0;
+
+    defaultClientAppInsights.trackEvent({
+      name: "summary",
+      properties: this.eventProperties,
+    });
+    defaultClientAppInsights.flush();
+
+    if (this.snapshotOptions.writeSnapshotInfoToConsole) {
+      console.log(JSON.stringify(this.eventProperties, undefined, 2));
+    }
+  }
+
+  public async endTest() {
+    console.log(`[BEGIN]: ending test...`);
+    await this.snapshot();
+    clearInterval(this.snapshotTimer);
+    // TODO: Log tracked messages in JSON
+    console.log(`[END]: ending test...`);
+  }
+}
