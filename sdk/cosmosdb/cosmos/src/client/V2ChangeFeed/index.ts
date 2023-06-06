2b7b8b6adae478ff5f80fac601c162fb558f4318
Aman Rao <amanrao18613@gmail.com>
2023-06-06 13:28:19 +0530

    Implement ChangeFeedV2 method

---
+++ sdk/cosmosdb/cosmos/src/client/V2ChangeFeed/index.ts
@@ -0,0 +1,5 @@
+export { ChangeFeedEpkRange } from "./ChangeFeedEpkRange";
+export { ChangeFeedOptionsV2 } from "./ChangeFeedOptions";
+export { ChangeFeedForPartitionKey } from "./ChangeFeedForPartitionKey";
+export { ChangeFeedResponseV2 } from "./ChangeFeedResponse";
+export { ChangeFeedIteratorV2 } from "./ChangeFeedIterator";
