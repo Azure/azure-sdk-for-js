7eb03e9aaed6d9c9f276dbe69954288ffb5840cb
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-06-08 14:47:52 +0800

    sqlvirtualmachine release

--- sdk/sqlvirtualmachine/arm-sqlvirtualmachine/samples-dev/sqlVirtualMachineTroubleshootSample.ts
+++ sdk/sqlvirtualmachine/arm-sqlvirtualmachine/samples-dev/sqlVirtualMachineTroubleshootSample.ts
@@ -8,39 +8,49 @@
 
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-import { SqlVirtualMachineManagementClient } from "@azure/arm-sqlvirtualmachine";
+import {
+  SqlVmTroubleshooting,
+  SqlVirtualMachineManagementClient
+} from "@azure/arm-sqlvirtualmachine";
 import { DefaultAzureCredential } from "@azure/identity";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 /**
- * This sample demonstrates how to Gets a SQL virtual machine.
+ * This sample demonstrates how to Starts SQL virtual machine troubleshooting.
  *
- * @summary Gets a SQL virtual machine.
- * x-ms-original-file: specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-08-01-preview/examples/GetSqlVirtualMachine.json
+ * @summary Starts SQL virtual machine troubleshooting.
+ * x-ms-original-file: specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-08-01-preview/examples/TroubleshootSqlVirtualMachine.json
  */
-async function getsASqlVirtualMachine() {
+async function startSqlVirtualMachineTroubleshootingOperation() {
   const subscriptionId =
     process.env["SQLVIRTUALMACHINE_SUBSCRIPTION_ID"] ||
     "00000000-1111-2222-3333-444444444444";
   const resourceGroupName =
     process.env["SQLVIRTUALMACHINE_RESOURCE_GROUP"] || "testrg";
   const sqlVirtualMachineName = "testvm";
+  const parameters: SqlVmTroubleshooting = {
+    endTimeUtc: new Date("2022-07-09T22:10:00Z"),
+    properties: { unhealthyReplicaInfo: { availabilityGroupName: "AG1" } },
+    startTimeUtc: new Date("2022-07-09T17:10:00Z"),
+    troubleshootingScenario: "UnhealthyReplica"
+  };
   const credential = new DefaultAzureCredential();
   const client = new SqlVirtualMachineManagementClient(
     credential,
     subscriptionId
   );
-  const result = await client.sqlVirtualMachines.get(
+  const result = await client.sqlVirtualMachineTroubleshoot.beginTroubleshootAndWait(
     resourceGroupName,
-    sqlVirtualMachineName
+    sqlVirtualMachineName,
+    parameters
   );
   console.log(result);
 }
 
 async function main() {
-  getsASqlVirtualMachine();
+  startSqlVirtualMachineTroubleshootingOperation();
 }
 
 main().catch(console.error);
