c1457b492488f24b8413fefbb80e7fb0bd324b6a
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2023-06-08 21:45:09 +0000

    fix ci

--- sdk/openai/openai/scripts/copyTypeSpecConfig.ts
+++ sdk/openai/openai/scripts/copyTypeSpecConfig.ts
@@ -4,5 +4,5 @@
 import * as fs from "fs";
 
 const dest = "TempTypeSpecFiles/OpenAI.Inference";
-fs.copyFileSync("typespec/package.json", `${dest}/package.json`);
-fs.copyFileSync("typespec/tspconfig.yaml", `${dest}/tspconfig.yaml`);
+fs.copyFileSync("customts/package.json", `${dest}/package.json`);
+fs.copyFileSync("customts/tspconfig.yaml", `${dest}/tspconfig.yaml`);
