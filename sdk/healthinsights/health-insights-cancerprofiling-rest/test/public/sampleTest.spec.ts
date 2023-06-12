c1ec98a0d9e7817928bc65dce6860c248bc027e9
reutgross <reutgross@microsoft.com>
2023-06-12 17:29:17 +0300

    sample

--- sdk/healthinsights/health-insights-cancerprofiling-rest/test/public/sampleTest.spec.ts
+++ sdk/healthinsights/health-insights-cancerprofiling-rest/test/public/sampleTest.spec.ts
@@ -3,21 +3,79 @@
 
 import { Recorder } from "@azure-tools/test-recorder";
 import { assert } from "chai";
-import { createRecorder } from "./utils/recordedClient";
+import {createClient, createRecorder} from "./utils/recordedClient";
 import { Context } from "mocha";
+import {
+  CancerProfilingClient, DocumentContent, InferCancerProfileBodyParam,
+  OncoPhenotypeData,
+  OncoPhenotypeModelConfiguration,
+  PatientDocument,
+  PatientInfo,
+  PatientRecord
+} from "../../src";
+import {getLongRunningPoller} from "../../../health-insights-clinicalmatching-rest/src";
+
+const patientInfo: PatientInfo = {
+        sex: "FEMALE",
+        birthDate: new Date(1979, 10, 8), // Note: Months are zero-based (11 represents December)
+    };
+
+    const patient1: PatientRecord = {
+        id: "patient_id",
+        info: patientInfo,
+    };
+
+    let doc1 = "15.8.2021"
+        "Jane Doe 091175-8967"
+        "42 year old female, married with 3 children, works as a nurse. "
+        "Healthy, no medications taken on a regular basis.";
+
+    const docContent: DocumentContent = {
+        sourceType: "INLINE",
+        value: doc1
+    };
+
+    const patientDoc1: PatientDocument = {
+        type: "NOTE",
+        id: "doc1",
+        content: docContent,
+        clinicalType: "IMAGING",
+        language: "en",
+        createdDateTime: new Date(2021, 8, 15)
+    };
+
+    const patientDocList: PatientDocument[] = [patientDoc1];
+    patient1.data = patientDocList;
+
+    const configuration: OncoPhenotypeModelConfiguration = {includeEvidence: true};
+
+    const cancerProfilingData: OncoPhenotypeData = {
+        patients: [patient1],
+        configuration: configuration
+    };
+
+    const parameters: InferCancerProfileBodyParam = {
+        body: cancerProfilingData
+    };
+
 
 describe("My test", () => {
   let recorder: Recorder;
+  let client: CancerProfilingClient;
 
   beforeEach(async function (this: Context) {
     recorder = await createRecorder(this);
+    client = await createClient(recorder)
   });
 
   afterEach(async function () {
     await recorder.stop();
   });
 
-  it("sample test", async function () {
-    assert.equal(1, 1);
+  it("cancer profiling test", async function () {
+      const result = await client.path("/oncophenotype/jobs").post(parameters);
+      const poller = await getLongRunningPoller(client, result);
+    const res = await poller.pollUntilDone();
+    assert.equal(res.status, '200');
   });
 });
--- sdk/healthinsights/health-insights-cancerprofiling-rest/test/public/utils/recordedClient.ts
+++ sdk/healthinsights/health-insights-cancerprofiling-rest/test/public/utils/recordedClient.ts
@@ -4,11 +4,12 @@
 import { Context } from "mocha";
 import { Recorder, RecorderStartOptions } from "@azure-tools/test-recorder";
 import "./env";
+import {AzureKeyCredential} from "@azure/core-auth";
+import CancerProfiling, {CancerProfilingClient} from "../../../src";
 
 const envSetupForPlayback: Record<string, string> = {
-  ENDPOINT: "https://endpoint",
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
+  HEALTH_INSIGHTS_ENDPOINT: "***",
+  HEALTH_INSIGHTS_KEY: "***",
   AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
   SUBSCRIPTION_ID: "azure_subscription_id",
 };
@@ -26,4 +27,14 @@ export async function createRecorder(context: Context): Promise<Recorder> {
   const recorder = new Recorder(context.currentTest);
   await recorder.start(recorderEnvSetup);
   return recorder;
+}
+
+export async function createClient(recorder: Recorder): Promise<CancerProfilingClient> {
+/*  const endpoint = assertEnvironmentVariable("HEALTH_INSIGHTS_ENDPOINT");
+  const key = assertEnvironmentVariable("HEALTH_INSIGHTS_KEY");*/
+/*  const credential = new AzureKeyCredential(key);
+  return CancerProfiling(endpoint, credential, recorder.configureClientOptions({}));*/
+  const credential = new AzureKeyCredential("");
+  //const credential = createTestCredential();
+  return CancerProfiling("https://eastus.api.cognitive.microsoft.com", credential, recorder.configureClientOptions({}));
 }
