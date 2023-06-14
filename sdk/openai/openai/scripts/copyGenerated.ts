c5372c92dc116f8296cff1a1423d2a85034426cb
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2023-06-14 00:49:29 +0000

    remove custom stuff

--- sdk/openai/openai/scripts/copyGenerated.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import * as fs from "fs";
-import * as fsextra from "fs-extra";
-
-await fsextra.move("TempTypeSpecFiles/OpenAI.Inference/generated/src", "sources/generated", { overwrite: true});
-fs.rmSync("TempTypeSpecFiles/OpenAI.Inference/generated", { recursive: true, force: true });
--- sdk/openai/openai/scripts/copyTypeSpecConfig.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import * as fs from "fs";
-
-fs.copyFileSync("typespec/package.json", "TempTypeSpecFiles/OpenAI.Inference/package.json");
