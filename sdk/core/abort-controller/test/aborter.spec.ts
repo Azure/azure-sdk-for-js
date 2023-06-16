9cad5460d4e5e4448ac29a990d8d08e9a279da63
Matthew Podwysocki <matthewp@microsoft.com>
2023-06-16 18:19:23 -0400

    [core] Fix abort-controller

--- sdk/core/abort-controller/test/aborter.spec.ts
+++ sdk/core/abort-controller/test/aborter.spec.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { AbortController, AbortError, AbortSignal } from "../src";
+import { AbortController, AbortError, AbortSignal } from "../src/index.js";
 import { assert } from "chai";
 
 describe("AbortController", () => {
