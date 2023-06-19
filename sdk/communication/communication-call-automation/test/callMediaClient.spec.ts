a420556eb88ce0c805697cd8dbbf919db67aa223
vivekmore@microsoft.com <vivekmore@microsoft.com>
2023-06-19 14:54:23 +0530

    Renamed test name

--- sdk/communication/communication-call-automation/test/callMediaClient.spec.ts
+++ sdk/communication/communication-call-automation/test/callMediaClient.spec.ts
@@ -457,7 +457,7 @@ describe("Call Media Client Live Tests", function () {
     assert.isDefined(callDisconnectedEvent);
   }).timeout(60000);
   
-  it("Trigger DTMF operations", async function () {
+  it("Trigger DTMF actions", async function () {
     testName = this.test?.fullTitle()
       ? this.test?.fullTitle().replace(/ /g, "_")
       : "create_call_and_trigger_dtmf_actions_then_hang_up";
