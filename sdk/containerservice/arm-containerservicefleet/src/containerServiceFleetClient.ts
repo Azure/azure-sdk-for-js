b448a97065ebe605189cc12fd00a8eedda5c42b4
azure-sdk <azuresdk@microsoft.com>
2023-06-21 02:58:39 +0000

    Post release automated changes for azure-arm-containerservicefleet

--- sdk/containerservice/arm-containerservicefleet/src/containerServiceFleetClient.ts
+++ sdk/containerservice/arm-containerservicefleet/src/containerServiceFleetClient.ts
@@ -60,7 +60,7 @@ export class ContainerServiceFleetClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-containerservicefleet/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-containerservicefleet/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
