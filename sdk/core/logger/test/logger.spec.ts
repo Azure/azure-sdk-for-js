b115286bc5e635ec303176689df1bca400c01f7b
Matthew Podwysocki <matthewp@microsoft.com>
2023-06-23 14:36:22 -0400

    [core] Update logger

--- sdk/core/logger/test/logger.spec.ts
+++ sdk/core/logger/test/logger.spec.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as Logger from "../src";
+import * as Logger from "../src/index.js";
 import { assert } from "chai";
 
 const testLogger = Logger.createClientLogger("test");
