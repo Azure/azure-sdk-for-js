6be4af121e05040a4277626857cb55657f2e07bc
Rykov Dmitry <aikava.kizuna@gmail.com>
2023-06-27 14:37:16 +0200

    private release preview 2023-06-26 (#26328)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    Co-authored-by: Rykov Dmitrii <rykovdmitrii@microsoft.com>

--- sdk/communication/communication-phone-numbers/src/generated/src/phoneNumbersClient.ts
+++ sdk/communication/communication-phone-numbers/src/generated/src/phoneNumbersClient.ts
@@ -40,7 +40,7 @@ export class PhoneNumbersClient extends coreClient.ServiceClient {
       requestContentType: "application/json; charset=utf-8"
     };
 
-    const packageDetails = `azsdk-js-communication-phone-numbers/1.3.0-alpha.20230517.1`;
+    const packageDetails = `azsdk-js-communication-phone-numbers/1.3.0-alpha.20230626.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
