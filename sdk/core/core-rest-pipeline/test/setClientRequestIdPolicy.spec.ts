8a44e72e4796939beb305c0d50f45518d538a498
Mary Gao <yanmeigao1210@gmail.com>
2023-07-07 15:14:24 +0800

    lint fix

--- sdk/core/core-rest-pipeline/test/setClientRequestIdPolicy.spec.ts
+++ sdk/core/core-rest-pipeline/test/setClientRequestIdPolicy.spec.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { assert } from "chai";
 import * as sinon from "sinon";
 import { PipelineResponse, SendRequest, createHttpHeaders, createPipelineRequest, setClientRequestIdPolicy } from "../src";
