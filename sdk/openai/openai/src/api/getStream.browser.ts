e66c0b4b5b86dcd74516a5e779e9609496c0c78e
Jeff Fisher <jeffish@microsoft.com>
2023-07-07 19:13:56 +0000

    fix lint issues in generated output

--- sdk/openai/openai/src/api/getStream.browser.ts
+++ sdk/openai/openai/src/api/getStream.browser.ts
@@ -11,6 +11,8 @@
 
 import { StreamableMethod } from "@azure-rest/core-client";
 
+declare const TextDecoder: any;
+
 export async function* getStream<TResponse>(
   response: StreamableMethod<TResponse>
 ): AsyncIterable<string> {
@@ -26,5 +28,3 @@ export async function* getStream<TResponse>(
     yield encoder.decode(value);
   }
 }
-
-declare const TextDecoder: any;
