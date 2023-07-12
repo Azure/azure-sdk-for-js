fa71b8197a1dae87d06c0a868bf6421ae3456bb0
vivekmore@microsoft.com <vivekmore@microsoft.com>
2023-07-12 16:02:10 +0530

    Fixing duplicated lib info fix

--- sdk/communication/communication-call-automation/src/callAutomationClient.ts
+++ sdk/communication/communication-call-automation/src/callAutomationClient.ts
@@ -11,7 +11,6 @@ import {
   CommunicationUserIdentifier,
 } from "@azure/communication-common";
 import { logger } from "./models/logger";
-import { SDK_VERSION } from "./models/constants";
 import {
   AnswerCallRequest,
   CallAutomationApiClient,
@@ -94,16 +93,9 @@ export class CallAutomationClient {
     const options = isCallAutomationClientOptions(credentialOrOptions)
       ? credentialOrOptions
       : maybeOptions;
-    const libInfo = `azsdk-js-communication-call-automation/${SDK_VERSION}`;
 
     if (!options?.userAgentOptions) {
       options.userAgentOptions = {};
-    }
-
-    if (options?.userAgentOptions?.userAgentPrefix) {
-      options.userAgentOptions.userAgentPrefix = `${options.userAgentOptions.userAgentPrefix} ${libInfo}`;
-    } else {
-      options.userAgentOptions.userAgentPrefix = libInfo;
     }
 
     this.internalPipelineOptions = {
