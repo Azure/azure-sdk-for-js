b7d74b6118b3f71061ebefc3274df8b74880b556
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-07-24 15:55:03 +0800

    update file

--- sdk/cognitiveservices/arm-cognitiveservices/test/cognitiveservices_congitiveservice.spec.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/test/cognitiveservices_congitiveservice.spec.ts
@@ -29,7 +29,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe("CognitiveServices test", () => {
+describe.only("CognitiveServices test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: CognitiveServicesManagementClient;
