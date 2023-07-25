c08a9593c0610a1e4a5c17a4f801eb14e01cfa82
rtrevino89 <99822668+rtrevino89@users.noreply.github.com>
2023-07-25 14:55:57 -0600

    add msuseragent policy to short codes (#26469)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/communication/communication-short-codes/test/public/utils/recordedClient.ts
+++ sdk/communication/communication-short-codes/test/public/utils/recordedClient.ts
@@ -14,6 +14,7 @@ import { Context } from "mocha";
 import { ShortCodesClient } from "../../../src";
 import { isNode } from "@azure/test-utils";
 import { parseConnectionString } from "@azure/communication-common";
+import { createMSUserAgentPolicy } from "./msUserAgentPolicy";
 
 if (isNode) {
   dotenv.config();
@@ -29,6 +30,7 @@ const envSetupForPlayback: { [k: string]: string } = {
   AZURE_CLIENT_ID: "SomeClientId",
   AZURE_CLIENT_SECRET: "azure_client_secret",
   AZURE_TENANT_ID: "SomeTenantId",
+  AZURE_USERAGENT_OVERRIDE: "fake-useragent",
 };
 
 export const recorderOptions: RecorderStartOptions = {
@@ -63,7 +65,14 @@ export async function createRecordedClient(
   return {
     client: new ShortCodesClient(
       assertEnvironmentVariable("COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING"),
-      recorder.configureClientOptions({})
+      recorder.configureClientOptions({
+        additionalPolicies: [
+          {
+            policy: createMSUserAgentPolicy(),
+            position: "perCall",
+          },
+        ],
+      })
     ),
     recorder,
   };
@@ -95,7 +104,18 @@ export async function createRecordedClientWithToken(
 
     // casting is a workaround to enable min-max testing
     return {
-      client: new ShortCodesClient(endpoint, credential, recorder.configureClientOptions({})),
+      client: new ShortCodesClient(
+        endpoint,
+        credential,
+        recorder.configureClientOptions({
+          additionalPolicies: [
+            {
+              policy: createMSUserAgentPolicy(),
+              position: "perCall",
+            },
+          ],
+        })
+      ),
       recorder,
     };
   }
@@ -112,7 +132,18 @@ export async function createRecordedClientWithToken(
 
   // casting is a workaround to enable min-max testing
   return {
-    client: new ShortCodesClient(endpoint, credential, recorder.configureClientOptions({})),
+    client: new ShortCodesClient(
+      endpoint,
+      credential,
+      recorder.configureClientOptions({
+        additionalPolicies: [
+          {
+            policy: createMSUserAgentPolicy(),
+            position: "perCall",
+          },
+        ],
+      })
+    ),
     recorder,
   };
 }
