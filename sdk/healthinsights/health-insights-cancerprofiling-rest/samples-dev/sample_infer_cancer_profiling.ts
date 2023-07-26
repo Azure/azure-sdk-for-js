9318362d89e294d72238df7c19b3dcecc534ffc0
reutgross <reutgross@microsoft.com>
2023-07-26 17:35:23 +0300

    sample

--- sdk/healthinsights/health-insights-cancerprofiling-rest/samples-dev/sample_infer_cancer_profiling.ts
+++ sdk/healthinsights/health-insights-cancerprofiling-rest/samples-dev/sample_infer_cancer_profiling.ts
@@ -98,41 +98,6 @@ export async function main() {
         createdDateTime: new Date(2021, 8, 15)
     };
 
- const doc1 = `15.8.2021
-        Jane Doe 091175-8967
-        42 year old female, married with 3 children, works as a nurse. 
-        Healthy, no medications taken on a regular basis.
-        PMHx is significant for migraines with aura, uses Mirena for contraception.
-        Smoking history of 10 pack years (has stopped and relapsed several times).
-        She is in c/o 2 weeks of productive cough and shortness of breath.
-        She has a fever of 37.8 and general weakness. 
-        Denies night sweats and rash. She denies symptoms of rhinosinusitis, asthma, and heartburn. 
-        On PE:
-        GENERAL: mild pallor, no cyanosis. Regular breathing rate. 
-        LUNGS: decreased breath sounds on the base of the right lung. Vesicular breathing.
-         No crackles, rales, and wheezes. Resonant percussion. 
-        PLAN: 
-        Will be referred for a chest x-ray. 
-        ======================================
-        CXR showed mild nonspecific opacities in right lung base. 
-        PLAN:
-        Findings are suggestive of a working diagnosis of pneumonia. The patient is referred to a 
-        follow-up CXR in 2 weeks. `;
-
-    const docContent = {
-        sourceType: "INLINE",
-        value: doc1
-    };
-
-    const patientDoc1 = {
-        type: "NOTE",
-        id: "doc1",
-        content: docContent,
-        clinicalType: "IMAGING",
-        language: "en",
-        createdDateTime: new Date(2021, 8, 15)
-    };
-
     const doc2 = `Oncology Clinic 
        20.10.2021
        Jane Doe 091175-8967
@@ -159,6 +124,41 @@ export async function main() {
        Stage 4 triple negative breast adenocarcinoma. 
        Could benefit from biological therapy. 
        Different treatment options were explained- the patient wants to get a second opinion.`;
+
+    const docContent2 = {
+        sourceType: "INLINE",
+        value: doc2
+    };
+
+    const patientDoc2 = {
+        type: "NOTE",
+        id: "doc2",
+        content: docContent2,
+        clinicalType: "PATHOLOGY",
+        language: "en",
+        createdDateTime: new Date(2021, 10, 20)
+    };
+
+    const doc3 = `PATHOLOGY REPORT
+                             Clinical Information
+   Ultrasound-guided biopsy; A. 18 mm mass; most likely diagnosis based on imaging:  IDC
+                                  Diagnosis
+    A.  BREAST, LEFT AT 2:00 4 CM FN; ULTRASOUND-GUIDED NEEDLE CORE BIOPSIES:
+    - Invasive carcinoma of no special type (invasive ductal carcinoma), grade 1
+    Nottingham histologic grade:  1/3 (tubules 2; nuclear grade 2; mitotic rate 1; 
+    total score; 5/9)
+    Fragments involved by invasive carcinoma:  2
+    Largest measurement of invasive carcinoma on a single fragment:  7 mm
+    Ductal carcinoma in situ (DCIS):  Present
+    Architectural pattern:  Cribriform
+    Nuclear grade:  2-
+                     -intermediate
+    Necrosis:  Not identified
+    Fragments involved by DCIS:  1
+    Largest measurement of DCIS on a single fragment:  Span 2 mm
+    Microcalcifications:  Present in benign breast tissue and invasive carcinoma
+    Blocks with invasive carcinoma:  A1
+    Special studies: Pending`;
 
     const docContent3 = {
         sourceType: "INLINE",
