a89df68bc773bc4bc3802b24fc89c36357b25a37
azure-sdk <azuresdk@microsoft.com>
2023-07-27 14:44:40 +0000

    Post release automated changes for azure-arm-networkcloud

--- sdk/networkcloud/arm-networkcloud/src/networkCloud.ts
+++ sdk/networkcloud/arm-networkcloud/src/networkCloud.ts
@@ -90,7 +90,7 @@ export class NetworkCloud extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-networkcloud/1.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-networkcloud/1.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
