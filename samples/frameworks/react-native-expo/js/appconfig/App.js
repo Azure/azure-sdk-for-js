64d65f0e61698e20db90dc717410d1497a074822
Jeremy Meng <jeremy.ymeng@gmail.com>
2023-08-07 10:29:20 -0700

    Add appconfig basic sample

--- samples/frameworks/react-native-expo/js/appconfig/App.js
+++ samples/frameworks/react-native-expo/js/appconfig/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import './shims';
+import {runAppConfigSample} from './appConfigSample';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click me!" onPress={() => runAppConfigSample()} />
       <StatusBar style="auto" />
     </View>
   );
--- samples/frameworks/react-native-expo/js/appconfig/babel.config.js
+++ samples/frameworks/react-native-expo/js/appconfig/babel.config.js
@@ -2,5 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      "@babel/plugin-proposal-async-generator-functions",
+      ["inline-dotenv", { unsafe: true }],
+    ],
   };
 };
