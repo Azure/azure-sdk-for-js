70342c0a57c171e317750b9cc85779fb782f88b8
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2023-08-07 23:50:44 +0000

    add chunked events tests

--- sdk/openai/openai/test/internal/sse.ts
+++ sdk/openai/openai/test/internal/sse.ts
@@ -5,6 +5,7 @@ import { Client } from "@azure-rest/core-client";
 import { getSSEs } from "../../src/api/getSSEs.js";
 import {
   assertAsyncIterable,
+  genChunks,
   createChunkedEvent,
   createDataEvent,
   createDataLine,
@@ -26,28 +27,30 @@ export function buildSseTests<StreamT>(
 ): Mocha.Suite {
   return describe(`[${rtName}] Server-sent Events`, () => {
     matrix([[0, 1, 2, 100000]], async function (count: number) {
-      it(`handles ${count} events`, async () => {
-        const client = createClient(() =>
-          createStream((write) => {
-            for (const event of genEvents(genStrs(count))) {
-              write(event);
+      matrix([[1, 3, 10]], async function (chunkLen: number) {
+        it(`handles ${count} events chunked into chunks of length ${chunkLen}`, async () => {
+          const client = createClient(() =>
+            createStream((write) => {
+              for (const c of genChunks(genEvents(genStrs(count)), 3)) {
+                write(c);
+              }
+            })
+          );
+          const iter = genStrs(count);
+          await assertAsyncIterable(
+            await getSSEs(client.pathUnchecked("/foo").get()),
+            count,
+            (event) => {
+              const { value, done } = iter.next();
+              if (done) return;
+              assert.deepEqual(event.data, decoder.decode(value));
             }
-          })
-        );
-        const iter = genStrs(count);
-        await assertAsyncIterable(
-          await getSSEs(client.pathUnchecked("/foo").get()),
-          count,
-          (event) => {
-            const { value, done } = iter.next();
-            if (done) return;
-            assert.deepEqual(event.data, decoder.decode(value));
-          }
-        );
+          );
+        });
       });
     });
 
-    it("handles non-ASCII text", async function () {
+    it("handles non-ASCII text event across multiple chunks", async function () {
       const txt = `OpenAI（开放人工智能研究中心）是一個美国人工智能研究實驗室，由非營利組織OpenAI Inc，和其營利組織子公司OpenAI LP所組成。OpenAI 進行 AI 研究的目的是促進和发展友好的人工智能，使人类整体受益。 OpenAI 系統運行在微軟基於 Azure 的超級計算平台上。該組織於2015年由萨姆·阿尔特曼、里德·霍夫曼、Jessica Livingston、伊隆·马斯克、伊爾亞·蘇茨克維、沃伊切赫·扎伦巴 (Wojciech Zaremba)、彼得·泰爾 等人在旧金山成立，他們共同認捐了$10億美元。 微軟在2019年向 OpenAI LP 提供了$10億美元的投資，並在2023年1月向其提供了第二筆多年投資，據報導為$100億美元， 用於獨家訪問GPT-4，這將為微軟自己的Bing Prometheus 模型提供支持`;
       const client = createClient(() =>
         createStream((write) => {
--- sdk/openai/openai/test/internal/util.ts
+++ sdk/openai/openai/test/internal/util.ts
@@ -53,7 +53,7 @@ export function createType(data: Uint8Array): Uint8Array {
   return concatBuffer(eventPrefix, concatBuffer(data, lineEnd));
 }
 
-export function* createChunkedEvent(str: string, chunkLen: number): Generator<Uint8Array> {
+export function* createChunkedEvent(str: string, chunkLen: number): Iterable<Uint8Array> {
   const bytes = encoder.encode(str);
   yield dataPrefix;
   for (let i = 0; i < bytes.length; i += chunkLen) {
@@ -63,13 +63,39 @@ export function* createChunkedEvent(str: string, chunkLen: number): Generator<Ui
   yield eventEnd;
 }
 
-export function* genEvents(iter: Generator<Uint8Array>): Iterable<Uint8Array> {
+export function* genEvents(iter: Iterable<Uint8Array>): Iterable<Uint8Array> {
   for (const value of iter) {
     yield createDataEvent(value);
   }
 }
 
-export function* genLines(iter: Generator<Uint8Array>): Iterable<Uint8Array> {
+function* take<T>(iter: Iterable<T>, count: number): Iterable<T[]> {
+  let res: T[] = [];
+  for (const item of iter) {
+    if (res.length === count) {
+      yield res;
+      res = [];
+    }
+    res.push(item);
+  }
+  if (res.length > 0) {
+    yield res;
+  }
+}
+
+function* stream(iter: Iterable<Uint8Array>): Iterable<number> {
+  for (const value of iter) {
+    yield* value;
+  }
+}
+
+export function* genChunks(iter: Iterable<Uint8Array>, chunkLen: number): Iterable<Uint8Array> {
+  for (const value of take(stream(iter), chunkLen)) {
+    yield Uint8Array.from(value);
+  }
+}
+
+export function* genLines(iter: Iterable<Uint8Array>): Iterable<Uint8Array> {
   for (const value of iter) {
     yield createDataLine(value);
   }
@@ -82,7 +108,7 @@ export function* genStrs(count: number): Generator<Uint8Array> {
   }
 }
 
-export function* genComments(iter: Generator<Uint8Array>): Generator<Uint8Array> {
+export function* genComments(iter: Iterable<Uint8Array>): Iterable<Uint8Array> {
   for (const value of iter) {
     yield createComment(value);
   }
