49c265fad4c85e169d35a2775f499dfaa9ff51c9
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-08-14 10:24:54 +0800

    resourceconnector release

--- sdk/resourceconnector/arm-resourceconnector/samples/v1/javascript/appliancesListOperationsSample.js
+++ sdk/resourceconnector/arm-resourceconnector/samples/v1/javascript/appliancesListOperationsSample.js
@@ -19,10 +19,8 @@ require("dotenv").config();
  * x-ms-original-file: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples/AppliancesListOperations.json
  */
 async function listAppliancesOperations() {
-  const subscriptionId =
-    process.env["RESOURCECONNECTOR_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
   const credential = new DefaultAzureCredential();
-  const client = new ResourceConnectorManagementClient(credential, subscriptionId);
+  const client = new ResourceConnectorManagementClient(credential);
   const resArray = new Array();
   for await (let item of client.appliances.listOperations()) {
     resArray.push(item);
