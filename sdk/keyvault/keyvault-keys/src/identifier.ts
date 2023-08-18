445488531bb81adc24e05236d6dff91886b27641
Timo van Veenendaal <timov@microsoft.com>
2023-08-18 15:16:29 -0700

    [keyvault] Keys: depend on keyvault-common package

--- sdk/keyvault/keyvault-keys/src/identifier.ts
+++ sdk/keyvault/keyvault-keys/src/identifier.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { parseKeyVaultIdentifier } from "../../keyvault-common/src";
+import { parseKeyVaultIdentifier } from "@azure/keyvault-common";
 
 /**
  * Represents the segments that compose a Key Vault Key Id.
