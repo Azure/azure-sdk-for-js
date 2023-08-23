a38ed07b943f948ab68c754e1b069f18b2450f9c
azure-sdk <azuresdk@microsoft.com>
2023-08-23 09:39:21 +0000

    Post release automated changes for azure-arm-maps

--- sdk/maps/arm-maps/src/azureMapsManagementClient.ts
+++ sdk/maps/arm-maps/src/azureMapsManagementClient.ts
@@ -64,7 +64,7 @@ export class AzureMapsManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-maps/3.1.0`;
+    const packageDetails = `azsdk-js-arm-maps/3.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
