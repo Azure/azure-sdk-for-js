de42f59d3b92bb48032e7ef9287c8e7c402acc65
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2023-08-25 17:54:40 +0000

    fix test

--- sdk/openai/openai/test/public/openai.spec.ts
+++ sdk/openai/openai/test/public/openai.spec.ts
@@ -387,7 +387,7 @@ describe("OpenAI", function () {
       });
 
       describe("getImages", function () {
-        it("get images test", async function () {
+        it("gets images with the expected dimensions", async function () {
           const prompt = "monkey eating banana";
           const numberOfImages = 2;
           const height = 256;
