3fbef06f9f4906be9e4fe403d3891ad9a4df5b39
azure-sdk <azuresdk@microsoft.com>
2023-08-25 08:55:57 +0000

    Post release automated changes for azure-arm-apicenter

--- sdk/apicenter/arm-apicenter/src/azureAPICenter.ts
+++ sdk/apicenter/arm-apicenter/src/azureAPICenter.ts
@@ -50,7 +50,7 @@ export class AzureAPICenter extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-apicenter/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-apicenter/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
