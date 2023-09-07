c406c774778db088b75572153854c6caf3360c41
azure-sdk <azuresdk@microsoft.com>
2023-09-07 07:36:09 +0000

    Post release automated changes for azure-arm-hybridconnectivity

--- sdk/hybridconnectivity/arm-hybridconnectivity/src/hybridConnectivityManagementAPI.ts
+++ sdk/hybridconnectivity/arm-hybridconnectivity/src/hybridConnectivityManagementAPI.ts
@@ -52,7 +52,7 @@ export class HybridConnectivityManagementAPI extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-hybridconnectivity/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-hybridconnectivity/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
