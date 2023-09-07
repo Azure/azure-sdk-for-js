b99018b08641451edfffdb45ad84ff05860843da
vikas singh <viki.singhk@gmail.com>
2023-09-07 15:39:02 +0530

    Adding Diagnostic for change feed (#27031)
    ### Packages impacted by this PR
    @azure/cosmos
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/cosmosdb/cosmos/src/CosmosDiagnostics.ts
+++ sdk/cosmosdb/cosmos/src/CosmosDiagnostics.ts
@@ -7,24 +7,25 @@ import { DiagnosticNodeInternal } from "./diagnostics/DiagnosticNodeInternal";
 import { ConsistencyLevel } from "./documents";
 
 /**
- * This is a Cosmos Diagnostic type that holds diagnostic collected information during client operations. ie. Item.read(), Containes.create().
+ *  * This is a Cosmos Diagnostic type that holds collected diagnostic information during a client operations. ie. Item.read(), Container.create().
+ * It has three members -
  * 1. `clientSideRequestStatistics` member contains aggregate diagnostic information, including -
  *   - metadata lookups. Here all the server requests, apart from the final intended resource are considered as metadata calls.
  *    i.e. for item.read(id), if the client makes server call to discover endpoints it would be considered as metadata call.
  *   - retries
  *   - endpoints contacted.
  *   - request, response payload stats.
- *   - gatewayStatistics. information corresponding to main operation. For example during Item.read(), the client might perform many operations
+ *   - gatewayStatistics - Information corresponding to main operation. For example during Item.read(), the client might perform many operations
  *    i.e. metadata lookup etc, but gatewayStatistics represents the diagnostics information for actual read operation.
  *
- * 2. diagnosticNode: Is a tree like structure which captures detailed diagnostic information. By default it is disabled, and is inteded to be
+ * 2. diagnosticNode - Is a tree like structure which captures detailed diagnostic information. By default it is disabled, and is intended to be
  * used only for debugging on non production environments. The kind of details captured in diagnosticNode is controlled by `CosmosDbDiagnosticLevel`.
- * - CosmosDbDiagnosticLevel.silent - is default value. In this level only clientSideRequestStatistics are captured. Is meant for production environments.
- * - CosmosDbDiagnosticLevel.debug - captures diagnosticNode and clientConfig. No request and response payloads are captured. Is not meant to be used
+ * - CosmosDbDiagnosticLevel.info - Is default value. In this level only clientSideRequestStatistics are captured. Is is meant for production environments.
+ * - CosmosDbDiagnosticLevel.debug - Captures diagnosticNode and clientConfig. No request and response payloads are captured. Is not meant to be used
  * in production environment.
- * - CosmosDbDiagnosticLevel.debug-unsafe - in addition to data captured in CosmosDbDiagnosticLevel.debug, also captures request and response payloads.
+ * - CosmosDbDiagnosticLevel.debug-unsafe - In addition to data captured in CosmosDbDiagnosticLevel.debug, also captures request and response payloads.
  * Is not meant to be used in production environment.
- * 3. clientConfig: captures information related to Client's config.
+ * 3. clientConfig - Captures information related to how client was configured during initialization.
  */
 export class CosmosDiagnostics {
   public readonly clientSideRequestStatistics: ClientSideRequestStatistics;
@@ -45,7 +46,7 @@ export class CosmosDiagnostics {
 }
 
 /**
- * This type holds information related to initilization of `CosmosClient`
+ * This type holds information related to initialization of `CosmosClient`
  */
 export type ClientConfigDiagnostic = {
   /**
@@ -163,10 +164,9 @@ export interface FailedRequestAttemptDiagnostic {
  */
 export enum MetadataLookUpType {
   PartitionKeyRangeLookUp = "PARTITION_KEY_RANGE_LOOK_UP",
-  ServiceEndpointResolution = "SERVICE_ENDPOINT_RESOLUTION",
-  PartitionKeyDefinition = "PARTITION_KEY_DEFINITION",
   DatabaseAccountLookUp = "DATABASE_ACCOUNT_LOOK_UP",
   QueryPlanLookUp = "QUERY_PLAN_LOOK_UP",
+  DatabaseLookUp = "DATABASE_LOOK_UP",
   ContainerLookUp = "CONTAINER_LOOK_UP",
 }
 
@@ -179,7 +179,7 @@ export type ClientSideRequestStatistics = {
    */
   requestStartTimeUTCInMs: number;
   /**
-   * This is the duration in Milli seconds taken by client operation.
+   * This is the duration in milli seconds taken by client operation.
    */
   requestDurationInMs: number;
   /**
