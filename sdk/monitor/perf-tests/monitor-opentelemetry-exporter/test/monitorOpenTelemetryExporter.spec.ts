9326600f57a74831fda5d28b56b913d0a1d6e42d
Jackson Weber <47067795+JacksonWeber@users.noreply.github.com>
2023-09-11 22:31:24 -0700

    Update functional test.

--- sdk/monitor/perf-tests/monitor-opentelemetry-exporter/test/monitorOpenTelemetryExporter.spec.ts
+++ sdk/monitor/perf-tests/monitor-opentelemetry-exporter/test/monitorOpenTelemetryExporter.spec.ts
@@ -1,7 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { PerfTest, getEnvVar } from "@azure/test-utils-perf";
+import { PerfTest } from "@azure/test-utils-perf";
+import * as opentelemetry from "@opentelemetry/api";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { BasicTracerProvider, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { AzureMonitorTraceExporter } from "@azure/monitor-opentelemetry-exporter";
 
 import * as dotenv from "dotenv";
@@ -9,11 +13,22 @@ dotenv.config();
 
 export abstract class MonitorOpenTelemetryExporterTest<TOptions> extends PerfTest<TOptions> {
   client: AzureMonitorTraceExporter;
+  tracer: opentelemetry.Tracer;
   
   constructor() {
     super();
     this.client = new AzureMonitorTraceExporter({
-      connectionString: getEnvVar("APPLICATIONINSIGHTS_CONNECTION_STRING")
+      connectionString: // getEnvVar("APPLICATIONINSIGHTS_CONNECTION_STRING")
+      "InstrumentationKey=b59d565e-da91-4140-8671-6c79b6938b4d;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/"
     });
+    const provider = new BasicTracerProvider({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: "basic-service",
+      }),
+    });
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(this.client as any));
+    provider.register();
+    this.tracer = opentelemetry.trace.getTracer("example-basic-tracer-node");
   }
 }
--- sdk/monitor/perf-tests/monitor-opentelemetry-exporter/test/spanExport.spec.ts
+++ sdk/monitor/perf-tests/monitor-opentelemetry-exporter/test/spanExport.spec.ts
@@ -4,9 +4,6 @@
 import { PerfOptionDictionary } from "@azure/test-utils-perf";
 import { MonitorOpenTelemetryExporterTest } from "./monitorOpenTelemetryExporter.spec";
 import * as opentelemetry from "@opentelemetry/api";
-import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { BasicTracerProvider, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { AzureMonitorTraceExporter } from "@azure/monitor-opentelemetry-exporter";
 
 type MonitorOpenTelemetryExporterTestOptions = Record<string, unknown>;
@@ -18,35 +15,25 @@ export class SpanExportTest extends MonitorOpenTelemetryExporterTest<MonitorOpen
   }
 
   async run(): Promise<void> {
-    const provider = new BasicTracerProvider({
-      resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: "basic-service",
-      }),
-    });
-
-    provider.addSpanProcessor(new SimpleSpanProcessor(this.client as any));
-    provider.register();
-    const tracer = opentelemetry.trace.getTracer("example-basic-tracer-node");
-
-    async function main(client: AzureMonitorTraceExporter) {
+    async function main(client: AzureMonitorTraceExporter, tracer: opentelemetry.Tracer) {
       const parentSpan = tracer.startSpan("main");
-      for (let i = 0; i < 10; i += 1) {
-        doWork(parentSpan);
+      for (let i = 0; i < 1; i += 1) {
+        doWork(parentSpan, tracer);
       }
       parentSpan.end();
       client.shutdown();
     }
 
-    function doWork(parent: opentelemetry.Span) {
+    function doWork(parent: opentelemetry.Span, tracer: opentelemetry.Tracer) {
       const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), parent);
       const span = tracer.startSpan("doWork", undefined, ctx);
-      for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {}
+      for (let i = 0; i <= 1; i += 1) {}
       span.setAttribute("key", "value");
       span.addEvent("invoking doWork");
       span.end();
     }
 
-    main(this.client).catch((error) => {
+    main(this.client, this.tracer).catch((error) => {
       console.error("Error running perf test:", error.message);
       process.exit(1);
     });
