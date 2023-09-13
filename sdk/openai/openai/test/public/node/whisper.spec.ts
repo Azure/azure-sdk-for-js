5bbf3016eb1651795a549c96f6c0e563bbfb0c6c
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2023-09-13 23:22:13 +0000

    Add mpga format

--- sdk/openai/openai/test/public/node/whisper.spec.ts
+++ sdk/openai/openai/test/public/node/whisper.spec.ts
@@ -37,11 +37,10 @@ describe("OpenAI", function () {
         }
       });
 
-      // To-do: add tests for ['mpga']"
       matrix(
         [
           ["json", "verbose_json", "srt", "vtt", "text"],
-          ["m4a", "mp3", "wav", "ogg", "flac", 'webm', 'mp4', 'mpeg', 'oga'],
+          ["m4a", "mp3", "wav", "ogg", "flac", "webm", "mp4", "mpeg", "oga"],
         ] as const,
         async function (format: AudioResultFormat, extension: string) {
           describe("getAudioTranscription", function () {
