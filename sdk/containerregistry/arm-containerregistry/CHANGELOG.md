4ed60827e7462e530b512eeddb1ff2f83db75df3
azure-sdk <azuresdk@microsoft.com>
2023-09-19 04:52:55 +0000

    Post release automated changes for azure-arm-containerregistry

--- sdk/containerregistry/arm-containerregistry/CHANGELOG.md
+++ sdk/containerregistry/arm-containerregistry/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
-    
+
+## 11.0.0-beta.2 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 11.0.0-beta.1 (2023-09-07)
     
 **Features**
--- sdk/containerregistry/arm-containerregistry/src/containerRegistryManagementClient.ts
+++ sdk/containerregistry/arm-containerregistry/src/containerRegistryManagementClient.ts
@@ -72,7 +72,7 @@ export class ContainerRegistryManagementClient extends coreClient.ServiceClient 
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-containerregistry/11.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-containerregistry/11.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
