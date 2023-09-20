f96f241a11958ffdd45eb52de4e12f2d024ab007
azure-sdk <azuresdk@microsoft.com>
2023-09-20 11:14:43 +0000

    Post release automated changes for azure-arm-communication

--- sdk/communication/arm-communication/src/communicationServiceManagementClient.ts
+++ sdk/communication/arm-communication/src/communicationServiceManagementClient.ts
@@ -62,7 +62,7 @@ export class CommunicationServiceManagementClient extends coreClient.ServiceClie
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-communication/4.1.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-communication/4.1.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
