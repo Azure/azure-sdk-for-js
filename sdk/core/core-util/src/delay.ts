b2c1fff6dffbcbe31c4401af6cbf0f21b11e64b6
Matthew Podwysocki <matthewp@microsoft.com>
2023-09-29 12:30:27 -0400

    [core] update from main

--- sdk/core/core-util/src/delay.ts
+++ sdk/core/core-util/src/delay.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { AbortSignalLike } from "@azure/abort-controller";
 import { createAbortablePromise } from "./createAbortablePromise.js";
 import { AbortOptions } from "./aborterUtils.js";
 
--- sdk/core/core-util/test/public/aborterUtils.spec.ts
+++ sdk/core/core-util/test/public/aborterUtils.spec.ts
@@ -5,7 +5,7 @@ import * as sinon from "sinon";
 import { AbortController, AbortSignalLike } from "@azure/abort-controller";
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { cancelablePromiseRace, createAbortablePromise } from "../../src";
+import { cancelablePromiseRace, createAbortablePromise } from "../../src/index.js";
 
 chai.use(chaiAsPromised);
 const { assert } = chai;
