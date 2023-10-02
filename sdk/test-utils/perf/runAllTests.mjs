0416873c128a04df1de8360c720da5ca9d35efaa
Matthew Podwysocki <matthewp@microsoft.com>
2023-10-02 16:19:22 -0700

    [core] fix perf-tests

--- sdk/test-utils/perf/runAllTests.mjs
+++ sdk/test-utils/perf/runAllTests.mjs
@@ -1,7 +1,7 @@
 import { spawnSync } from "child_process";
-import { allTests } from "./test/declareTests";
+import { allTests } from "./dist-esm/test/declareTests.js";
 
-function spawn(command: string) {
+function spawn(command) {
   const ret = spawnSync(command, { shell: true, stdio: "inherit" });
 
   if (ret.status !== 0) {
@@ -9,7 +9,7 @@ function spawn(command: string) {
   }
 }
 
-function runTest(testClassName: string, options: string = "") {
+function runTest(testClassName, options = "") {
   console.log("\n");
   spawn(
     `node ./dist-esm/test/index.spec.js ${testClassName} --warmup 0 --iterations 1 --duration 1 ${options}`
