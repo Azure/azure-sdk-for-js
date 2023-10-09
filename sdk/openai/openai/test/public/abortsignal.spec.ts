af3e102ea3adbaedffe3ad5eec6d1f7d7bef4664
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2023-10-09 20:40:59 +0000

    remove skip node 14

--- sdk/openai/openai/test/public/abortsignal.spec.ts
+++ sdk/openai/openai/test/public/abortsignal.spec.ts
@@ -21,13 +21,6 @@ describe("AbortSignal", () => {
   });
 
   it("Abort signal test for streaming method", async function () {
-    // Skip test for Node 14. See https://github.com/Azure/azure-sdk-for-js/issues/27353
-    if (typeof process === "object") {
-      const [major] = process.versions.node.split(".").map(Number);
-      if (major === 14) {
-        this.skip();
-      }
-    }
     const messages = [
       { role: "system", content: "You are a helpful assistant. You will talk like a pirate." },
       { role: "user", content: "Can you help me?" },
