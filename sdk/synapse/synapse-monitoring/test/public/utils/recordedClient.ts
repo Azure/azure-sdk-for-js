df974bd2c1a5ba9b65ab4b18e21bb0d3d874cf95
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2023-10-10 21:14:03 +0000

    [Synapze monitoring] Modernizing

--- sdk/synapse/synapse-monitoring/test/public/utils/recordedClient.ts
+++ sdk/synapse/synapse-monitoring/test/public/utils/recordedClient.ts
@@ -1,58 +1,36 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Context } from "mocha";
+import { Test } from "mocha";
 
-import { env, Recorder, record, RecorderEnvironmentSetup } from "@azure-tools/test-recorder";
-import { TokenCredential, ClientSecretCredential } from "@azure/identity";
+import {
+  Recorder,
+  RecorderStartOptions,
+  assertEnvironmentVariable,
+} from "@azure-tools/test-recorder";
+import { createTestCredential } from "@azure-tools/test-credential";
 
 import { MonitoringClient, MonitoringClientOptionalParams } from "../../../src";
-import "./env";
 
-const replaceableVariables: { [k: string]: string } = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+const envSetupForPlayback: { [k: string]: string } = {
   ENDPOINT: "https://testaccount.dev.azuresynapse.net"
 };
 
-export const environmentSetup: RecorderEnvironmentSetup = {
-  replaceableVariables,
-  customizationsOnRecordings: [
-    (recording: string): string =>
-      recording.replace(/"access_token"\s?:\s?"[^"]*"/g, `"access_token":"access_token"`),
-    // If we put ENDPOINT in replaceableVariables above, it will not capture
-    // the endpoint string used with nock, which will be expanded to
-    // https://<endpoint>:443/ and therefore will not match, so we have to do
-    // this instead.
-    (recording: string): string => {
-      const replaced = recording.replace(
-        "testaccount.dev.azuresynapse.net:443",
-        "testaccount.dev.azuresynapse.net"
-      );
-      return replaced;
-    }
-  ],
-  queryParametersToSkip: []
+const recorderStartOptions: RecorderStartOptions = {
+  envSetupForPlayback,
 };
 
-export function createClient(options?: MonitoringClientOptionalParams): MonitoringClient {
-  let credential: TokenCredential;
-
-  credential = new ClientSecretCredential(
-    env.AZURE_TENANT_ID,
-    env.AZURE_CLIENT_ID,
-    env.AZURE_CLIENT_SECRET
-  );
-
-  return new MonitoringClient(credential, env.ENDPOINT, options);
+export function createClient(options?: {
+  recorder?: Recorder;
+  clientOptions?: MonitoringClientOptionalParams;
+}): MonitoringClient {
+  const { recorder, clientOptions = {} } = options || {};
+  const updatedOptions = recorder ? recorder.configureClientOptions(clientOptions) : clientOptions;
+  return new MonitoringClient(createTestCredential(), assertEnvironmentVariable("ENDPOINT"), updatedOptions);
 }
 
-/**
- * creates the recorder and reads the environment variables from the `.env` file.
- * Should be called first in the test suite to make sure environment variables are
- * read before they are being used.
- */
-export function createRecorder(context: Context): Recorder {
-  return record(context, environmentSetup);
+export async function createRecorder(currentTest?: Test): Promise<Recorder> {
+  const recorder = new Recorder(currentTest);
+  await recorder.start(recorderStartOptions);
+  return recorder;
 }
