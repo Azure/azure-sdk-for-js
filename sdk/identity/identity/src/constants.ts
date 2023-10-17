d1b3478b7cac5973198734f28aedb5a545421b44
azure-sdk <azuresdk@microsoft.com>
2023-10-17 20:22:50 +0000

    Post release automated changes for azure-identity

--- sdk/identity/identity/src/constants.ts
+++ sdk/identity/identity/src/constants.ts
@@ -5,7 +5,7 @@
  * Current version of the `@azure/identity` package.
  */
 
-export const SDK_VERSION = `3.3.2`;
+export const SDK_VERSION = `3.3.3`;
 
 /**
  * The default client ID for authentication
