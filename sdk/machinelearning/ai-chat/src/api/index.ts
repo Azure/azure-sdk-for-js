ea53b7d85b7b74492729c3e94449a714e65a33a0
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2023-10-17 19:37:04 -0700

    stale dir

--- sdk/machinelearning/ai-chat/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export { createChat, ChatClientOptions, ChatContext } from "./ChatContext.js";
-export { create, createStreaming } from "./operations.js";
--- sdk/machinelearning/ai-chat/src/models/options.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-import { OperationOptions } from "@azure-rest/core-client";
-
-export interface CreateOptions extends OperationOptions {}
-
-export interface CreateStreamingOptions extends OperationOptions {}
