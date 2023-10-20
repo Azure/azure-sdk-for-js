37e322f941e121446fb9c47a6b891342d3246144
Timo van Veenendaal <timov@microsoft.com>
2023-10-20 08:28:11 -0700

    [core-util] Add support for hex strings to `uint8ArrayToString` (#27441)
    ### Packages impacted by this PR
    
    - `@azure/core-util`
    
    ### Describe the problem that is addressed by this PR
    
    Key Vault needs a util to convert byte arrays to a hex string for an
    upcoming convenience property. This adds that support to the existing
    `core-util` `uint8ArrayToString` helper.
    
    In this PR I also simplified the logic for `uint8ArrayToString` in node,
    and also removed the now-redundant Base64 and hex helpers that aren't
    exported but are used elsewhere in `core-util`.
    
    This PR also fixes a bug with the browser Base64 `uint8ArrayToString`
    implementation where the conversion would throw upon encountering bytes
    which are not valid ISO-8859-1 (Latin1) code points.

--- sdk/core/core-util/src/bytesEncoding.browser.ts
+++ sdk/core/core-util/src/bytesEncoding.browser.ts
@@ -8,7 +8,7 @@ declare global {
 }
 
 /** The supported character encoding type */
-export type EncodingType = "utf-8" | "base64" | "base64url";
+export type EncodingType = "utf-8" | "base64" | "base64url" | "hex";
 
 /**
  * The helper that transforms bytes with specific character encoding into string
@@ -24,6 +24,8 @@ export function uint8ArrayToString(bytes: Uint8Array, format: EncodingType): str
       return uint8ArrayToBase64(bytes);
     case "base64url":
       return uint8ArrayToBase64Url(bytes);
+    case "hex":
+      return uint8ArrayToHexString(bytes);
   }
 }
 
@@ -41,6 +43,8 @@ export function stringToUint8Array(value: string, format: EncodingType): Uint8Ar
       return base64ToUint8Array(value);
     case "base64url":
       return base64UrlToUint8Array(value);
+    case "hex":
+      return hexStringToUint8Array(value);
   }
 }
 
@@ -48,10 +52,8 @@ export function stringToUint8Array(value: string, format: EncodingType): Uint8Ar
  * Decodes a Uint8Array into a Base64 string.
  * @internal
  */
-export function uint8ArrayToBase64(uint8Array: Uint8Array): string {
-  const decoder = new TextDecoder();
-  const dataString = decoder.decode(uint8Array);
-  return btoa(dataString);
+export function uint8ArrayToBase64(bytes: Uint8Array): string {
+  return btoa([...bytes].map((x) => String.fromCharCode(x)).join(""));
 }
 
 /**
@@ -66,10 +68,18 @@ export function uint8ArrayToBase64Url(bytes: Uint8Array): string {
  * Decodes a Uint8Array into a javascript string.
  * @internal
  */
-export function uint8ArrayToUtf8String(uint8Array: Uint8Array): string {
+export function uint8ArrayToUtf8String(bytes: Uint8Array): string {
   const decoder = new TextDecoder();
-  const dataString = decoder.decode(uint8Array);
+  const dataString = decoder.decode(bytes);
   return dataString;
+}
+
+/**
+ * Decodes a Uint8Array into a hex string
+ * @internal
+ */
+export function uint8ArrayToHexString(bytes: Uint8Array): string {
+  return [...bytes].map((x) => x.toString(16).padStart(2, "0")).join("");
 }
 
 /**
@@ -95,4 +105,27 @@ export function base64ToUint8Array(value: string): Uint8Array {
 export function base64UrlToUint8Array(value: string): Uint8Array {
   const base64String = value.replace(/-/g, "+").replace(/_/g, "/");
   return base64ToUint8Array(base64String);
+}
+
+const hexDigits = new Set("0123456789abcdefABCDEF");
+
+/**
+ * Encodes a hex string into a Uint8Array
+ * @internal
+ */
+export function hexStringToUint8Array(value: string): Uint8Array {
+  // If value has odd length, the last character will be ignored, consistent with NodeJS Buffer behavior
+  const bytes = new Uint8Array(value.length / 2);
+  for (let i = 0; i < value.length / 2; ++i) {
+    const highNibble = value[2 * i];
+    const lowNibble = value[2 * i + 1];
+    if (!hexDigits.has(highNibble) || !hexDigits.has(lowNibble)) {
+      // Replicate Node Buffer behavior by exiting early when we encounter an invalid byte
+      return bytes.slice(0, i);
+    }
+
+    bytes[i] = parseInt(`${highNibble}${lowNibble}`, 16);
+  }
+
+  return bytes;
 }
