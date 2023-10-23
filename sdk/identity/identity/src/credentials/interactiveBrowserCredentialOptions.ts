1ebbf50cb13b138f284fdbf9c1392410e9f10ff2
KarishmaGhiya <kghiya8@gmail.com>
2023-10-23 12:28:25 -0700

    Update sdk/identity/identity/src/credentials/interactiveBrowserCredentialOptions.ts

--- sdk/identity/identity/src/credentials/interactiveBrowserCredentialOptions.ts
+++ sdk/identity/identity/src/credentials/interactiveBrowserCredentialOptions.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { BrowserCustomizationOptions } from "./browserCustomizatioOptions";
+import { BrowserCustomizationOptions } from "./browserCustomizationOptions";
 import { CredentialPersistenceOptions } from "./credentialPersistenceOptions";
 import { InteractiveCredentialOptions } from "./interactiveCredentialOptions";
 
