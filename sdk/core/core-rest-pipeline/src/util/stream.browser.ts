7b5d0aa576b7af88016c811f5fc84d0d0fcab5b4
Timo van Veenendaal <timov@microsoft.com>
2023-10-24 13:24:39 -0700

    nit

--- sdk/core/core-rest-pipeline/src/util/stream.browser.ts
+++ sdk/core/core-rest-pipeline/src/util/stream.browser.ts
@@ -75,8 +75,8 @@ export function concatenateStreams(
   }
 
   return new ReadableStream({
-    async pull(controller) {
-      await doPull(controller);
+    pull(controller) {
+      return doPull(controller);
     },
   });
 }
