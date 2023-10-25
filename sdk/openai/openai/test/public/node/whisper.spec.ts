4f9220e64cfd108f39076fef5f6cd7a1c5987b25
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2023-10-25 14:25:27 -0700

    [OpenAI] Upgrade formdata-node and form-data-encoder (#27532)
    ### Packages impacted by this PR
    @azure/openai
    
    ### Issues associated with this PR
    Fixes https://github.com/Azure/azure-sdk-for-js/issues/27528
    
    ### Describe the problem that is addressed by this PR
    Upgrades formdata-node and form-data-encoder to latest release which
    restores cjs support
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    N/A
    
    ### Are there test cases added in this PR? _(If not, why?)_
    N/A
    
    ### Provide a list of related PRs _(if any)_
    N/A
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [x] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/openai/openai/test/public/node/whisper.spec.ts
+++ sdk/openai/openai/test/public/node/whisper.spec.ts
@@ -23,6 +23,11 @@ describe("OpenAI", function () {
     describe(`[${authMethod}] Client`, () => {
       let recorder: Recorder;
       let client: OpenAIClient;
+      before(async function (this: Context) {
+        if (process.version.startsWith("v16")) {
+          this.skip();
+        }
+      });
 
       beforeEach(async function (this: Context) {
         recorder = await startRecorder(this.currentTest);
