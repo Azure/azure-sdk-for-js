6d1e4dbe6779697e8567a11c5ae981c16b937c44
Jeff Fisher <jeffish@microsoft.com>
2023-10-25 19:33:50 +0000

    [ts-http-runtime] Port core-util fixes
    Port changes from PR #27441 and #27515

--- sdk/core/ts-http-runtime/test/bytesEncoding.spec.ts
+++ sdk/core/ts-http-runtime/test/bytesEncoding.spec.ts
@@ -1,21 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { isBrowser, isNode } from "../../src";
-import { stringToUint8Array, uint8ArrayToString } from "../../src/util/bytesEncoding";
+import { stringToUint8Array, uint8ArrayToString } from "../src/util/bytesEncoding";
 import { assert } from "chai";
 
-describe("Base64", function () {
-  describe("isBrowser (node)", function () {
-    it("should return true", async function () {
-      assert.isTrue(isBrowser);
-    });
-  });
-  describe("isNode (node)", function () {
-    it("should return false", async function () {
-      assert.isFalse(isNode);
-    });
-  });
+describe("bytesEncoding", function () {
   describe("base64ToBytes", function () {
     it("converts a base64 string to bytes", function () {
       const input = "YXp1cmU="; // 'azure' in base64.
@@ -26,6 +15,12 @@ describe("Base64", function () {
         new Uint8Array([97, 122, 117, 114, 101]),
         "Incorrect conversion of base64 to bytes."
       );
+    });
+
+    it("converts base64-encoded data containing an emoji (multibyte character) to bytes", function () {
+      const input = "8J+RjQ=="; // thumbs up emoji, hex f0 9f 91 8d
+      const output = stringToUint8Array(input, "base64");
+      assert.deepEqual(output, new Uint8Array([0xf0, 0x9f, 0x91, 0x8d]));
     });
 
     it("converts a utf-8 string to bytes", function () {
@@ -110,6 +105,46 @@ describe("Base64", function () {
         const output = uint8ArrayToString(scenario.bytes, "base64url");
         assert.equal(output, scenario.expected, "Incorrect conversion of bytes to base64.");
       }
+    });
+  });
+
+  describe("bytes to hex", function () {
+    it("encodes a byte", function () {
+      const bytes = new Uint8Array(256).map((_, index) => index);
+      bytes.forEach((_, index) => {
+        const hex = uint8ArrayToString(bytes.slice(index, index + 1), "hex");
+        assert.equal(hex.length, 2, "Unexpected length for hex value.");
+        assert.equal(parseInt(hex, 16), index, "Unexpected hex value.");
+      });
+    });
+
+    it("encodes bytes", function () {
+      const bytes = new Uint8Array([97, 122, 117, 114, 101]); // 'azure' in utf8
+      const hex = uint8ArrayToString(bytes, "hex");
+      assert.equal(hex, "617a757265", "Unexpected hex value.");
+    });
+  });
+
+  describe("hex to bytes", function () {
+    it("decodes bytes", function () {
+      const hex = "617a757265";
+      const bytes = stringToUint8Array(hex, "hex");
+
+      assert.sameOrderedMembers([...bytes], [0x61, 0x7a, 0x75, 0x72, 0x65]);
+    });
+
+    it("truncates with odd number of characters", function () {
+      const invalid = "abc";
+      const bytes = stringToUint8Array(invalid, "hex");
+
+      assert.sameOrderedMembers([...bytes], [0xab]);
+    });
+
+    it("truncates upon encountering invalid hex digits", function () {
+      const invalid = "0102xx03";
+      const bytes = stringToUint8Array(invalid, "hex");
+
+      assert.sameOrderedMembers([...bytes], [0x01, 0x02]);
     });
   });
 });
