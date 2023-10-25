5dc36a720b26cf5b6c0fe33215305c149b92f118
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-10-25 16:50:25 +0800

    [mgmt] webpubsub release (#27366)
    https://github.com/Azure/sdk-release-request/issues/4618

--- sdk/web-pubsub/arm-webpubsub/samples-dev/webPubSubCheckNameAvailabilitySample.ts
+++ sdk/web-pubsub/arm-webpubsub/samples-dev/webPubSubCheckNameAvailabilitySample.ts
@@ -21,7 +21,7 @@ dotenv.config();
  * This sample demonstrates how to Checks that the resource name is valid and is not already in use.
  *
  * @summary Checks that the resource name is valid and is not already in use.
- * x-ms-original-file: specification/webpubsub/resource-manager/Microsoft.SignalRService/preview/2023-06-01-preview/examples/WebPubSub_CheckNameAvailability.json
+ * x-ms-original-file: specification/webpubsub/resource-manager/Microsoft.SignalRService/preview/2023-08-01-preview/examples/WebPubSub_CheckNameAvailability.json
  */
 async function webPubSubCheckNameAvailability() {
   const subscriptionId =
