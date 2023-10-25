735677407c4fbbceea95200f6d6de00e29804740
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-10-25 16:47:04 +0800

    [mgmt] datadog release (#27348)
    https://github.com/Azure/sdk-release-request/issues/4609

--- sdk/datadog/arm-datadog/samples/v3/typescript/src/marketplaceAgreementsListSample.ts
+++ sdk/datadog/arm-datadog/samples/v3/typescript/src/marketplaceAgreementsListSample.ts
@@ -10,15 +10,20 @@
 // Licensed under the MIT License.
 import { MicrosoftDatadogClient } from "@azure/arm-datadog";
 import { DefaultAzureCredential } from "@azure/identity";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * This sample demonstrates how to List Datadog marketplace agreements in the subscription.
  *
  * @summary List Datadog marketplace agreements in the subscription.
- * x-ms-original-file: specification/datadog/resource-manager/Microsoft.Datadog/stable/2021-03-01/examples/MarketplaceAgreements_List.json
+ * x-ms-original-file: specification/datadog/resource-manager/Microsoft.Datadog/stable/2023-01-01/examples/MarketplaceAgreements_List.json
  */
 async function marketplaceAgreementsList() {
-  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const subscriptionId =
+    process.env["DATADOG_SUBSCRIPTION_ID"] ||
+    "00000000-0000-0000-0000-000000000000";
   const credential = new DefaultAzureCredential();
   const client = new MicrosoftDatadogClient(credential, subscriptionId);
   const resArray = new Array();
@@ -28,4 +33,8 @@ async function marketplaceAgreementsList() {
   console.log(resArray);
 }
 
-marketplaceAgreementsList().catch(console.error);
+async function main() {
+  marketplaceAgreementsList();
+}
+
+main().catch(console.error);
