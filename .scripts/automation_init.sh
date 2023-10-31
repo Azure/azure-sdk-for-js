da39097751627453c62102e7f8a76bcc76651f8c
Mary Gao <yanmeigao1210@gmail.com>
2023-10-31 17:41:13 +0800

    Update the node version

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-node -v
 npm install --force -g node@18.14.0
-node -v
 npm install -g @microsoft/rush@5.92.0
-npm install -g @marygao/js-sdk-release-tools
+npm install -g @azure-tools/js-sdk-release-tools
