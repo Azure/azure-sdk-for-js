bfb4e2a44076f2d7fbb23a95a3b35e60c3ebb32b
Jeremy Meng <jeremy.ymeng@gmail.com>
2023-10-31 11:04:31 -0700

    remove old headers

--- sdk/template/template-dpg/src/api/index.ts
+++ sdk/template/template-dpg/src/api/index.ts
@@ -1,30 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-/**
- * THIS IS AN AUTO-GENERATED FILE - DO NOT EDIT!
- *
- * Any changes you make here may be lost.
- *
- * If you need to make changes, please do so in the original source file, \{project-root\}/sources/custom
- */
-
-/**
- * THIS IS AN AUTO-GENERATED FILE - DO NOT EDIT!
- *
- * Any changes you make here may be lost.
- *
- * If you need to make changes, please do so in the original source file, \{project-root\}/sources/custom
- */
-
-/**
- * THIS IS AN AUTO-GENERATED FILE - DO NOT EDIT!
- *
- * Any changes you make here may be lost.
- *
- * If you need to make changes, please do so in the original source file, \{project-root\}/sources/custom
- */
-
 export { Widget, ColorType, AnalyzeResult } from "./models.js";
 export {
   listWidgets,
--- sdk/template/template-dpg/src/rest/responses.ts
+++ sdk/template/template-dpg/src/rest/responses.ts
@@ -1,30 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-/**
- * THIS IS AN AUTO-GENERATED FILE - DO NOT EDIT!
- *
- * Any changes you make here may be lost.
- *
- * If you need to make changes, please do so in the original source file, \{project-root\}/sources/custom
- */
-
-/**
- * THIS IS AN AUTO-GENERATED FILE - DO NOT EDIT!
- *
- * Any changes you make here may be lost.
- *
- * If you need to make changes, please do so in the original source file, \{project-root\}/sources/custom
- */
-
-/**
- * THIS IS AN AUTO-GENERATED FILE - DO NOT EDIT!
- *
- * Any changes you make here may be lost.
- *
- * If you need to make changes, please do so in the original source file, \{project-root\}/sources/custom
- */
-
 import { HttpResponse } from "@azure-rest/core-client";
 import { WidgetOutput, WidgetErrorOutput, AnalyzeResultOutput } from "./outputModels.js";
 
