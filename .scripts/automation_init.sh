9f783536b79057d273c1ebde38826ae385e47b2c
Mary Gao <yanmeigao1210@gmail.com>
2023-10-31 23:59:17 +0800

    Quick workaround before the ci is upgrading to node 18

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-npm install --force -g node@18.14.0
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install v18.15.0 
+nvm alias default node
 npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
