86cf92204526f9dd7d5d44311a0df77ef052a384
Mary Gao <yanmeigao1210@gmail.com>
2023-10-31 17:32:09 +0800

    npm install -g node@18.14.0

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 node -v
-npm install -g node@18.14.0
+npm install --force -g node@18.14.0
 node -v
 npm install -g @microsoft/rush@5.92.0
 npm install -g @marygao/js-sdk-release-tools
