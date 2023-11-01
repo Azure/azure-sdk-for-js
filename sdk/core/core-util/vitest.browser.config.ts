028b20824f0923c76be35f1f4fe425559328bdd1
Jeff Fisher <jeffish@microsoft.com>
2023-11-01 17:33:48 +0000

    remove the need for adding more browser maps

--- sdk/core/core-util/vitest.browser.config.ts
+++ sdk/core/core-util/vitest.browser.config.ts
@@ -3,7 +3,36 @@
 
 import { defineConfig } from "vitest/config";
 
+function hasPackageCache<T extends {}>(
+  obj: T
+): obj is T & { packageCache: Map<string, { data: any }> } {
+  return "packageCache" in obj;
+}
+
+function rewriteDistPath(path: string): string {
+  return path.replace(/^\.\/dist-esm\/(\S+)\.js$/, function replacer(_match, path) {
+    return `./src/${path}`;
+  });
+}
+
 export default defineConfig({
+  plugins: [
+    {
+      name: "browser-test-config",
+      enforce: "pre",
+      configResolved: (config) => {
+        if (hasPackageCache(config)) {
+          for (const { data } of config.packageCache.values()) {
+            if (data.browser) {
+              for (const [key, value] of Object.entries<string>(data.browser)) {
+                data.browser[rewriteDistPath(key)] = rewriteDistPath(value);
+              }
+            }
+          }
+        }
+      },
+    },
+  ],
   test: {
     browser: {
       enabled: true,
