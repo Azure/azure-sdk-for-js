548c77ca1054a6df3cc622793e225f68a6b80137
Rango Meadows <v-rameadows@microsoft.com>
2023-11-02 09:18:48 -0700

    Sync and Generate from Typespec

--- sdk/translation/ai-translation-text-rest/src/serializeHelper.ts
+++ sdk/translation/ai-translation-text-rest/src/serializeHelper.ts
@@ -1,11 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export function buildMultiCollection(
-  queryParameters: string[],
-  parameterName: string
-) {
-  return queryParameters
+export function buildMultiCollection(items: string[], parameterName: string) {
+  return items
     .map((item, index) => {
       if (index === 0) {
         return item;
