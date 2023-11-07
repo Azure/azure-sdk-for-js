e76a020223bce9bcf71e031c55f27fd7451e534c
Emma Zhu <emmazhu@microsoft.com>
2023-11-07 11:34:34 +0800

    [Storage]Bump package version for STG91 GA

--- sdk/storage/storage-queue/src/utils/constants.ts
+++ sdk/storage/storage-queue/src/utils/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const SDK_VERSION: string = "12.16.0-beta.2";
+export const SDK_VERSION: string = "12.16.0";
 export const SERVICE_VERSION: string = "2023-11-03";
 
 /**
