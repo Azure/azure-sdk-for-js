73e630f611880aa4d003e5903f1abd0ae37cae4b
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2023-11-10 12:09:51 -0800

    Adding rename annotation + Adding basic export customization + Allow property modification in place.

---
+++ common/tools/dev-tool/src/util/customization/exports.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license
+
+import { SourceFile } from "ts-morph";
+
+export async function augmentExports(customFile: SourceFile, originalFile: SourceFile) {
+  const customExports = customFile.getExportDeclarations();
+  const originalExports = originalFile.getExportDeclarations();
+  for (const customExport of customExports) {
+    const [original] = originalExports.filter(
+      (e) =>
+        e.getModuleSpecifier()?.getLiteralValue() ===
+        customExport.getModuleSpecifier()?.getLiteralValue()
+    );
+    if (original) {
+      const exports = customExport.getNamedExports().map((e) => ({
+        name: e.getName(),
+        alias: e.getAliasNode()?.getText(),
+      }));
+      original.addNamedExports(exports);
+    } else {
+      originalFile.addExportDeclaration(customExport.getStructure());
+    }
+  }
+}
