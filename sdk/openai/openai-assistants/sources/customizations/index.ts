4edfd485e4f6f576de3aca4cc0f19e1085cdd440
Glenn Harper <glharper@microsoft.com>
2023-11-17 15:22:11 -0800

    add initial customized code

---
+++ sdk/openai/openai-assistants/sources/customizations/index.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * [Azure OpenAI Assistants](https://learn.microsoft.com/azure/cognitive-services/openai-assistants/overview)
+ * provides REST API access to OpenAI's Assistants service. This service allows you to
+ * use natural language to automate tasks and retrieve information.
+ * 
+ * @packageDocumentation
+ */
+
+export { AzureKeyCredential } from "@azure/core-auth";
+export { AssistantsClient } from "./AssistantsClient.js";
+export { OpenAIKeyCredential } from "./OpenAIKeyCredential.js";
