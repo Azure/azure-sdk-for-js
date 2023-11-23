832904b18988c9e9c343e2e772a2b54a02ecc6d7
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-11-23 15:57:10 +0800

    [mgmt] hybridcontainerservice release (#27751)
    https://github.com/Azure/sdk-release-request/issues/4739

--- sdk/hybridcontainerservice/arm-hybridcontainerservice/src/operationsInterfaces/operations.ts
+++ sdk/hybridcontainerservice/arm-hybridcontainerservice/src/operationsInterfaces/operations.ts
@@ -7,10 +7,7 @@
  */
 
 import { PagedAsyncIterableIterator } from "@azure/core-paging";
-import {
-  ResourceProviderOperation,
-  OperationsListOptionalParams
-} from "../models";
+import { Operation, OperationsListOptionalParams } from "../models";
 
 /// <reference lib="esnext.asynciterable" />
 /** Interface representing a Operations. */
@@ -18,5 +15,5 @@ export interface Operations {
   /** @param options The options parameters. */
   list(
     options?: OperationsListOptionalParams
-  ): PagedAsyncIterableIterator<ResourceProviderOperation>;
+  ): PagedAsyncIterableIterator<Operation>;
 }
