0aeb9ede10759151437ae9bf849cd6d680fa99c2
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2023-11-23 13:12:34 +0800

    [mgmt] containerregistry release (#27699)
    https://github.com/Azure/sdk-release-request/issues/4706

--- sdk/containerregistry/arm-containerregistry/samples/v11-beta/typescript/src/registriesListUsagesSample.ts
+++ sdk/containerregistry/arm-containerregistry/samples/v11-beta/typescript/src/registriesListUsagesSample.ts
@@ -18,7 +18,7 @@ dotenv.config();
  * This sample demonstrates how to Gets the quota usages for the specified container registry.
  *
  * @summary Gets the quota usages for the specified container registry.
- * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-08-01-preview/examples/RegistryListUsages.json
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/preview/2023-11-01-preview/examples/RegistryListUsages.json
  */
 async function registryListUsages() {
   const subscriptionId =
