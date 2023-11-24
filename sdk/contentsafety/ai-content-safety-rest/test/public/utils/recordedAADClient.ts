e78757fbe4d296192dfb022c1abb3b188bb8bdc0
MinjueWu <597922968@qq.com>
2023-11-24 15:20:52 +0800

    Add test cases for aad auth

--- sdk/contentsafety/ai-content-safety-rest/test/public/utils/recordedAADClient.ts
+++ sdk/contentsafety/ai-content-safety-rest/test/public/utils/recordedAADClient.ts
@@ -9,12 +9,16 @@ import {
 } from "@azure-tools/test-recorder";
 import "./env";
 import ContentSafety, { ContentSafetyClient } from "../../../src";
-import { AzureKeyCredential } from "@azure/core-auth";
+import { createTestCredential } from "@azure-tools/test-credential";
+// import { AzureKeyCredential } from "@azure/core-auth";
 // import { ClientOptions } from "@azure-rest/core-client";
 
 const envSetupForPlayback: Record<string, string> = {
   CONTENT_SAFETY_ENDPOINT: "https://endpoint/",
-  CONTENT_SAFETY_API_KEY: "fake_key",
+  AZURE_CLIENT_ID: "azure_client_id",
+  AZURE_CLIENT_SECRET: "azure_client_secret",
+  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+  AZURE_SUBSCRIPTION_ID: "azure_subscription_id",
 };
 
 const recorderEnvSetup: RecorderStartOptions = {
@@ -26,16 +30,15 @@ const recorderEnvSetup: RecorderStartOptions = {
  * Should be called first in the test suite to make sure environment variables are
  * read before they are being used.
  */
-export async function createRecorder(context: Context): Promise<Recorder> {
+export async function createAADRecorder(context: Context): Promise<Recorder> {
   const recorder = new Recorder(context.currentTest);
   await recorder.start(recorderEnvSetup);
   return recorder;
 }
 
-export function createClient(recorder: Recorder): ContentSafetyClient {
+export function createAADClient(recorder: Recorder): ContentSafetyClient {
   const endpoint = assertEnvironmentVariable("CONTENT_SAFETY_ENDPOINT");
-  const key = assertEnvironmentVariable("CONTENT_SAFETY_API_KEY");
-  const credential = new AzureKeyCredential(key);
+  const credential = createTestCredential();
   const client = ContentSafety(endpoint, credential, recorder.configureClientOptions({}));
   return client;
 }
