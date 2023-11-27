74d3af69e7879a27d035ab409d9e92147076757c
Andrew Kao <andykao1213@gmail.com>
2023-11-27 08:00:50 +0000

    Add support for SAS token authentication for maps-geolocation-rest

--- sdk/maps/maps-geolocation-rest/src/MapsGeolocation.ts
+++ sdk/maps/maps-geolocation-rest/src/MapsGeolocation.ts
@@ -2,7 +2,13 @@
 // Licensed under the MIT license.
 
 import { ClientOptions } from "@azure-rest/core-client";
-import { AzureKeyCredential, TokenCredential, isTokenCredential } from "@azure/core-auth";
+import {
+  AzureKeyCredential,
+  AzureSASCredential,
+  TokenCredential,
+  isSASCredential,
+  isTokenCredential,
+} from "@azure/core-auth";
 import { createMapsClientIdPolicy } from "@azure/maps-common";
 import { MapsGeolocationClient } from "./generated";
 import createClient from "./generated";
@@ -48,8 +54,27 @@ export default function MapsGeolocation(
   mapsAccountClientId: string,
   options?: ClientOptions
 ): MapsGeolocationClient;
+/**
+ * Creates an instance of MapsGeolocation from an Azure Identity `AzureSASCredential`.
+ *
+ * @example
+ * ```ts
+ * import MapsGeolocation from "@azure/maps-geo-location";
+ * import { AzureSASCredential } from "@azure/core-auth";
+ *
+ * const credential = new AzureSASCredential("<SAS Token>");
+ * const client = MapsGeolocation(credential);
+ * ```
+ *
+ * @param credential - An AzureSASCredential instance used to authenticate requests to the service
+ * @param options - Options used to configure the Geolocation Client
+ */
 export default function MapsGeolocation(
-  credential: TokenCredential | AzureKeyCredential,
+  credential: AzureSASCredential,
+  options?: ClientOptions
+): MapsGeolocationClient;
+export default function MapsGeolocation(
+  credential: TokenCredential | AzureKeyCredential | AzureSASCredential,
   clientIdOrOptions: string | ClientOptions = {},
   maybeOptions: ClientOptions = {}
 ): MapsGeolocationClient {
@@ -75,5 +100,18 @@ export default function MapsGeolocation(
     client.pipeline.addPolicy(createMapsClientIdPolicy(clientId));
     return client;
   }
+
+  if (isSASCredential(credential)) {
+    const client = createClient(undefined as any, options);
+    client.pipeline.addPolicy({
+      name: "mapsSASCredentialPolicy",
+      async sendRequest(request, next) {
+        request.headers.set("Authorization", `jwt-sas ${credential.signature}`);
+        return next(request);
+      },
+    });
+    return client;
+  }
+
   return createClient(credential, options);
 }
