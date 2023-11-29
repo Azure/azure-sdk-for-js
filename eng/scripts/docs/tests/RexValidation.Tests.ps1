cf8ecd3bc78bd596c1c773b6068c741386b55861
Daniel Jurek <djurek@microsoft.com>
2023-11-29 17:48:55 +0000

    Use original schema for rex-tool-tests.json, formatting

--- eng/scripts/docs/tests/RexValidation.Tests.ps1
+++ eng/scripts/docs/tests/RexValidation.Tests.ps1
@@ -1,30 +1,32 @@
-<#
-```
-Invoke-Pester -Output Detailed $PSScriptRoot/Validation.Tests.ps1
-We are testing the ValidatePackagesForDocs function in Language-Settings.sp1
-```
-#>
 Import-Module Pester
+
 BeforeDiscovery {
   $RexToolSpecs = Get-Content "$PSScriptRoot/rex-tool-tests.json" | ConvertFrom-Json -AsHashtable
 }
 
 BeforeAll {
   . $PSScriptRoot/../../../common/scripts/common.ps1
-}
-function GetPackageSpecs() {
-  Get-Content "$PSScriptRoot/rex-tool-tests.json" | ConvertFrom-Json
+
+  # Returns 
+  function getPackageInfo($packageName) { 
+    $output = @{ Name = $packageName; Version = '' }
+
+    if ($packageName -match '^(?<pkgName>.+?)(@(?<pkgVersion>.+))?$') { 
+      $pkgName = $Matches['pkgName']
+      $pkgVersion = $Matches['pkgVersion']
+      $output.Name = $pkgName
+      $output.Version = $pkgVersion
+    }
+
+    return $output
+  }
+
 }
 
-# Test plan:
-# 1. Tests on a list of packages which some of the packages pass the validation and the rest fail the validation.
-Describe "Validate-javascript-DocMsPackages" -Tag "UnitTest" {
-  It "Package <Name>@<Version> returns result <ExpectedResult>" -ForEach $RexToolSpecs {
+Describe "Validate-javascript-DocMsPackages" -Tag "LiveTests" {
+  It "Package <Package> returns result <ExpectedResult>" -ForEach $RexToolSpecs {
     
-    $packageInfo = [PSCustomObject]@{
-      Name    = $Name; 
-      Version = $Version;
-    }
+    $packageInfo = getPackageInfo($Package)
 
     $output = Validate-javascript-DocMsPackages -PackageInfo $packageInfo
     $output | Should -Be $ExpectedResult
