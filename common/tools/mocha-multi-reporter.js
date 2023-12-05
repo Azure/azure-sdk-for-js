142f16477a76b54e62705731bcbfa8e84eae0754
Jeremy Meng <jeremy.ymeng@gmail.com>
2023-12-05 12:27:56 -0800

    update comments

--- common/tools/mocha-multi-reporter.js
+++ common/tools/mocha-multi-reporter.js
@@ -6,9 +6,9 @@ const Mocha = require("mocha");
 
 /**
  * Usage :
- * - Meant to be used to leverage Mocha's Spec reporter as well as mocha-junit-reporter for the test runs
- * - Provide the relative path to this file from your `sdk/service/package-folder/` as the reporter options to mocha
- * - Example - `--reporter ../../../common/tools/mocha-multi-reporter.js`
+ * - Meant to be used to leverage Mocha's buitin Spec and XUnit reporters
+ * - Provide the relative path to this file from your `sdk/service/package-folder/` to mocha
+ * - Example - `--reporter ../../../common/tools/mocha-multi-reporter.js --report-option ...`
  *
  * Refer the following docs for more customizations on reporters
  * https://mochajs.org/api/tutorial-custom-reporter.html
@@ -17,8 +17,6 @@ const Mocha = require("mocha");
  */
 class MultiReporter {
   constructor(runner, options) {
-    // Spec reporter is provided as part of mocha library
-    // Invoking the spec reporter with the runner
     new Mocha.reporters.Spec(runner, options);
     new Mocha.reporters.XUnit(runner, options);
   }
