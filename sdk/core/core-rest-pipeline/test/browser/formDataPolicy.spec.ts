a0d23e2f75ee5fc4553f1a4a62384a71f6842b46
Timo van Veenendaal <timov@microsoft.com>
2024-01-09 14:29:10 +1300

    Add browser test

---
+++ sdk/core/core-rest-pipeline/test/browser/formDataPolicy.spec.ts
@@ -0,0 +1,49 @@
+import sinon from "sinon";
+import { createHttpHeaders } from "../../src/httpHeaders";
+import { PipelineResponse, SendRequest } from "../../src/interfaces";
+import { createPipelineRequest } from "../../src/pipelineRequest";
+import { formDataPolicy } from "../../src/policies/formDataPolicy";
+import { assert } from "chai";
+
+describe("formDataPolicy (browser-only)", function() {
+  it("prepares multipart/form-data form data correctly", async function() {
+    const request = createPipelineRequest({
+      url: "https://bing.com",
+      headers: createHttpHeaders({
+        "Content-Type": "multipart/form-data",
+      }),
+    });
+
+    const file = new File([new Uint8Array([1, 2, 3])], 'test.txt', { type: 'text/plain' });
+    request.formData = { a: "va", b: "v:b", c: file };
+    const successResponse: PipelineResponse = {
+      headers: createHttpHeaders(),
+      request,
+      status: 200,
+    };
+    const next = sinon.stub<Parameters<SendRequest>, ReturnType<SendRequest>>();
+    next.resolves(successResponse);
+
+    const policy = formDataPolicy();
+
+    const result = await policy.sendRequest(request, next);
+
+    assert.isUndefined(result.request.formData);
+    const body = result.request.body as any;
+    assert.ok(body, "expecting valid body");
+    let length = 0;
+    for (const [fieldName, value] of body.entries()) {
+      length++;
+      if (fieldName === "a") {
+        assert.strictEqual(value, "va");
+      } else if (fieldName === "b") {
+        assert.strictEqual(value, "v:b");
+      } else if (fieldName === "c") {
+        assert.strictEqual(value, file);
+      } else {
+        assert.fail(`unexpected form data key ${fieldName}`);
+      }
+    }
+    assert.strictEqual(length, 3, "expecting three entries in form data");
+  });
+})
