3491b8db99723c4a3d0a686fcd54a18cafcd84d3
Harsha Nalluru <sanallur@microsoft.com>
2024-01-24 01:01:54 +0000

    regenerate and format

--- sdk/appconfiguration/app-configuration/src/models.ts
+++ sdk/appconfiguration/app-configuration/src/models.ts
@@ -318,7 +318,10 @@ export interface ListConfigurationSettingsForSnapshotOptions
  * Also provides `fields` which allows you to selectively choose which fields are populated in the
  * result.
  */
-export interface ListConfigurationSettingsOptions extends OperationOptions, ListSettingsOptions {}
+export interface ListConfigurationSettingsOptions
+  extends OperationOptions,
+    ListSettingsOptions,
+    HttpOnlyIfChangedField {}
 
 /**
  * Common options for 'list' style APIs in AppConfig used to specify wildcards as well as
