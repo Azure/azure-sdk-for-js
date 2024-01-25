0fd75587ada0646d7b9ebf7bd3250a693331ad24
Matthew Podwysocki <matthewp@microsoft.com>
2024-01-25 17:51:10 -0500

    [test-utils] formatting

--- sdk/test-utils/recorder/src/index.ts
+++ sdk/test-utils/recorder/src/index.ts
@@ -16,13 +16,13 @@ export {
 export { env } from "./utils/env.js";
 export { delay } from "./utils/delay.js";
 export { CustomMatcherOptions } from "./matcher.js";
-export { 
-  TestInfo, 
-  MochaTest, 
+export {
+  TestInfo,
+  MochaTest,
   MochaTestSuite,
-  VitestTestContext, 
+  VitestTestContext,
   VitestTask,
   VitestSuite,
-  isMochaTest, 
-  isVitestTestContext 
+  isMochaTest,
+  isVitestTestContext,
 } from "./testInfo.js";
