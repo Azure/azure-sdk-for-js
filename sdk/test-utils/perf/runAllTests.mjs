9f087ed958d5d63edf6e11731be8b1ee9ff5901a
Matthew Podwysocki <matthewp@microsoft.com>
2024-01-25 14:42:50 -0500

    [perf] Fix perf tests

--- sdk/test-utils/perf/runAllTests.mjs
+++ sdk/test-utils/perf/runAllTests.mjs
@@ -1,5 +1,5 @@
 import { spawnSync } from "child_process";
-import { allTests } from "./dist-esm/test/declareTests.js";
+import { allTests } from "./dist-test/node/test/declareTests.js";
 
 function spawn(command) {
   const ret = spawnSync(command, { shell: true, stdio: "inherit" });
@@ -12,7 +12,7 @@ function spawn(command) {
 function runTest(testClassName, options = "") {
   console.log("\n");
   spawn(
-    `node ./dist-esm/test/index.spec.js ${testClassName} --warmup 0 --iterations 1 --duration 1 ${options}`
+    `node ./dist-test/node/test/index.spec.js ${testClassName} --warmup 0 --iterations 1 --duration 1 ${options}`
   );
 }
 
--- sdk/test-utils/perf/src/utils/utils.ts
+++ sdk/test-utils/perf/src/utils/utils.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-/* eslint-disable-next-line @typescript-eslint/triple-slash-reference */
+
 /// <reference types="node" />
 
 import { IncomingMessage, RequestOptions } from "http";
@@ -9,9 +9,6 @@ import http from "http";
 
 /**
  * Returns the environment variable, throws an error if not defined.
- *
- * @export
- * @param {string} name
  */
 export function getEnvVar(name: string): string {
   const val = process.env[name];
@@ -24,9 +21,6 @@ export function getEnvVar(name: string): string {
 let cachedHttpsAgent: https.Agent;
 /**
  * Returns https Agent to allow connecting to the proxy tool with "https" protocol.
- *
- * @export
- * @param {string} name
  */
 export const getCachedHttpsAgent = (insecure: boolean): https.Agent => {
   if (!cachedHttpsAgent) {
@@ -44,9 +38,7 @@ export const getCachedHttpsAgent = (insecure: boolean): https.Agent => {
 
 /**
  * Reads a readable stream. Doesn't save to a buffer.
- *
- * @export
- * @param stream A Node.js Readable stream
+ * @param stream - A Node.js Readable stream
  */
 export async function drainStream(stream: NodeJS.ReadableStream): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -94,7 +86,7 @@ export function isDefined<T>(thing: T | undefined | null): thing is T {
 /**
  * Formats a duration/time span (e.g. elapsed time) into mm:ss format.
  *
- * @param durationMilliseconds Duration in milliseconds
+ * @param durationMilliseconds - Duration in milliseconds
  */
 export function formatDuration(durationMilliseconds: number): string {
   const totalSeconds = Math.floor(durationMilliseconds / 1000);
@@ -108,13 +100,14 @@ export function formatDuration(durationMilliseconds: number): string {
  * Formats a number with a minimum number of significant digits.
  * Digits to the left of the decimal point are always significant.
  * Examples:
- *  - formatNumber(0, 4) -> "0.000"
- *  - formatNumber(12345, 4) -> "12,345"
- *  - formatNumber(1.2345, 4) -> "1.235"
- *  - formatNumber(0.00012345, 4) -> "0.0001235"
+ *  - formatNumber(0, 4) -&gt; "0.000"
+ *  - formatNumber(12345, 4) -&gt; "12,345"
+ *  - formatNumber(1.2345, 4) -&gt; "1.235"
+ *  - formatNumber(0.00012345, 4) -&gt; "0.0001235"
  */
-export function formatNumber(value: number, minSignificantDigits: number) {
+export function formatNumber(value: number, minSignificantDigits: number): string {
   // Special case since log(0) is undefined
+  // eslint-disable-next-line eqeqeq
   if (value == 0) {
     return value.toLocaleString(undefined, {
       minimumSignificantDigits: minSignificantDigits,
@@ -131,6 +124,6 @@ export function formatNumber(value: number, minSignificantDigits: number) {
   });
 }
 
-export function getFormattedDate() {
+export function getFormattedDate(): string {
   return new Date().toISOString().replace(/[:\-.]/g, "_");
 }
