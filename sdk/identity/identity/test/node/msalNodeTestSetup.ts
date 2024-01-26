f41851b6c0cb5b419d85b8c035611c0f473281ea
Maor Leger <maorleger@users.noreply.github.com>
2024-01-26 19:56:43 +0000

    wrap core-util's uuid for tests

--- sdk/identity/identity/test/node/msalNodeTestSetup.ts
+++ sdk/identity/identity/test/node/msalNodeTestSetup.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as coreUtil from "@azure/core-util";
-
 import {
   AuthenticationResult,
   ConfidentialClientApplication,
@@ -13,6 +11,7 @@ import Sinon, { createSandbox } from "sinon";
 import { PlaybackTenantId } from "../msalTestUtils";
 import { Recorder } from "@azure-tools/test-recorder";
 import { Test } from "mocha";
+import * as utils from "../../src/msal/utils";
 
 export type MsalTestCleanup = () => Promise<void>;
 
@@ -46,7 +45,7 @@ export async function msalNodeTestSetup(
 
   const sandbox = createSandbox();
 
-  const stub = sandbox.stub(coreUtil, "randomUUID");
+  const stub = sandbox.stub(utils, "randomUUID");
   stub.returns(playbackValues.correlationId);
 
   if (testContextOrStubbedToken instanceof Test || testContextOrStubbedToken === undefined) {
