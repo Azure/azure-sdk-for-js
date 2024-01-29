fed8fa8c85d68b7a131848022100d16c2725e078
Jackson Weber <jacksonweber@microsoft.com>
2024-01-28 23:27:11 -0800

    Update fileSystemPersist.test.ts

--- sdk/monitor/monitor-opentelemetry-exporter/test/internal/fileSystemPersist.test.ts
+++ sdk/monitor/monitor-opentelemetry-exporter/test/internal/fileSystemPersist.test.ts
@@ -46,7 +46,9 @@ const assertFirstFile = async (tempDir: string, expectation: unknown): Promise<v
 
   // Read the first file in tempDir
   const origFiles = await readdirAsync(tempDir);
-  const files = origFiles.filter((f) => path.basename(f).includes(`${process.pid}${FileSystemPersist.FILENAME_SUFFIX}`));
+  const files = origFiles.filter((f) =>
+    path.basename(f).includes(`${process.pid}${FileSystemPersist.FILENAME_SUFFIX}`),
+  );
   assert.ok(files.length > 0);
 
   // Assert file matches expectation
