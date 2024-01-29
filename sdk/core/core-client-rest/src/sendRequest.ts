96faafab3c6e3351fa7f33e809520231998f6c77
Timo van Veenendaal <timov@microsoft.com>
2024-01-29 12:35:22 -0800

    [core] Avoid use of web streams in multipart/form-data (#28294)
    ### Packages impacted by this PR
    
    - `@azure/core-rest-pipeline`
    
    ### Issues associated with this PR
    
    - #28102
    - Fixes https://github.com/Azure/autorest.typescript/issues/2177
    
    ### Describe the problem that is addressed by this PR
    
    
    - Added private "raw content" property to `createFile`-created `File`s
    to work around a couple of Node bugs (one where `Blob#stream` doesn't
    work in certain versions of Node 20, and another where
    `Readable.fromWeb` doesn't work in certain versions of Node 18). The
    "raw" property is used to get the original stream instead of the
    `stream` method. This property is used when accessing the stream so we
    don't have to convert to/from web streams, cirvumventing the two issues.
    - In browser, multipartPolicy now concatenates `Blob` objects using the
    `Blob` constructor instead of concatenating `ReadableStream`s. This
    works around the fact that `ReadableStream` is not a supported request
    body type when using HTTP/1.1 in the browser (see #28102).
    - We are still using the browser-mapped formDataPolicy for now, but this
    should unblock us returning to the isomorphic multipartPolicy-based
    formDataPolicy if it provides a benefit in future.
    - See this doc for some interesting reading on the design of the `Blob`
    API as it is implemented by Chromium:
    https://chromium.googlesource.com/chromium/src/+/HEAD/storage/browser/blob/README.md.
    Notably, large `Blob`s may be put on disk when created, and creating a
    new `Blob` by concatenating other `Blob`s does not involve an expensive
    copy.

--- sdk/core/core-client-rest/src/sendRequest.ts
+++ sdk/core/core-client-rest/src/sendRequest.ts
@@ -11,6 +11,7 @@ import {
   PipelineResponse,
   RequestBodyType,
   RestError,
+  createFile,
   createHttpHeaders,
   createPipelineRequest,
 } from "@azure/core-rest-pipeline";
@@ -187,7 +188,7 @@ function isRLCFormDataInput(body: unknown): body is RLCFormDataInput {
 }
 
 function processFormDataValue(value: RLCFormDataValue): FormDataValue {
-  return value instanceof Uint8Array ? new Blob([value]) : value;
+  return value instanceof Uint8Array ? createFile(value, "blob") : value;
 }
 
 /**
