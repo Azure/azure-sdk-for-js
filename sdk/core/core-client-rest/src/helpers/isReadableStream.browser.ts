d7e1976f9f6de5e2179b284261af2afae8ca1dfc
Timo van Veenendaal <timov@microsoft.com>
2024-01-29 21:24:41 +0000

    allow readablestream/node stream for file upload

--- sdk/core/core-client-rest/src/helpers/isReadableStream.browser.ts
+++ sdk/core/core-client-rest/src/helpers/isReadableStream.browser.ts
@@ -5,10 +5,6 @@
  * Checks if the body is a ReadableStream supported by browsers
  * @internal
  */
-export function isReadableStream(body: unknown): body is ReadableStream {
-  return Boolean(
-    body &&
-      typeof (body as ReadableStream).getReader === "function" &&
-      typeof (body as ReadableStream).tee === "function",
-  );
+export function isReadableStream(body: unknown): body is ReadableStream | NodeJS.ReadableStream {
+  return body instanceof ReadableStream;
 }
--- sdk/core/core-client-rest/src/helpers/isReadableStream.ts
+++ sdk/core/core-client-rest/src/helpers/isReadableStream.ts
@@ -5,6 +5,11 @@
  * Checks if the body is a ReadableStream supported by Node
  * @internal
  */
-export function isReadableStream(body: unknown): body is NodeJS.ReadableStream {
-  return Boolean(body) && typeof (body as any).pipe === "function";
+export function isReadableStream(
+  body: unknown,
+): body is NodeJS.ReadableStream | ReadableStream<Uint8Array> {
+  return (
+    (Boolean(body) && typeof (body as any).pipe === "function") ||
+    (typeof ReadableStream !== "undefined" && body instanceof ReadableStream)
+  );
 }
