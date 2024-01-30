e79c98d746fa0f9bedd9e751280894935573f630
Matthew Podwysocki <matthewp@microsoft.com>
2024-01-30 17:42:35 -0500

    [core] fix export types

--- sdk/core/abort-controller/src/index.ts
+++ sdk/core/abort-controller/src/index.ts
@@ -1,5 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+declare global {
+  interface Event {}
+}
+
 export { AbortError } from "./AbortError.js";
 export { AbortSignalLike } from "./AbortSignalLike.js";
