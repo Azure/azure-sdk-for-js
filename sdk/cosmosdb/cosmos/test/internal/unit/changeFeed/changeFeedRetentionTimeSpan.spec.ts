b8e50e058b1246f49defa4e1316e0f300c083da5
Aman Rao <amanrao18613@gmail.com>
2024-01-31 17:27:10 +0530

    update test

--- sdk/cosmosdb/cosmos/test/internal/unit/changeFeed/changeFeedRetentionTimeSpan.spec.ts
+++ sdk/cosmosdb/cosmos/test/internal/unit/changeFeed/changeFeedRetentionTimeSpan.spec.ts
@@ -7,8 +7,7 @@ import assert from "assert";
 describe("test ChangeFeedRetentionTimeSpan", function () {
   it("timeSpan should be positive number", async function () {
     try {
-      const timeSpan = ChangeFeedRetentionTimeSpan.fromMinutes(-5);
-      console.log(timeSpan);
+      ChangeFeedRetentionTimeSpan.fromMinutes(-5);
       assert.fail("should throw exception");
     } catch (err) {
       assert.equal(err.message, "ChangeFeedRetentionTimeSpan must be a positive number.");
@@ -16,11 +15,14 @@ describe("test ChangeFeedRetentionTimeSpan", function () {
   });
   it("timeSpan granularity should be minutes", async function () {
     try {
-      const timeSpan = ChangeFeedRetentionTimeSpan.fromMinutes(5.5);
-      console.log(timeSpan);
+      ChangeFeedRetentionTimeSpan.fromMinutes(5.5);
       assert.fail("should throw exception");
     } catch (err) {
       assert.equal(err.message, "Retention's granularity is minutes.");
     }
+  });
+  it("timeSpan should work correctly", async function () {
+    const timeSpan = ChangeFeedRetentionTimeSpan.fromMinutes(5);
+    assert.equal(timeSpan.getRetentionInMinutes(), 5);
   });
 });
