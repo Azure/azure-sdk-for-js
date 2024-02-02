1a16edf1092f4424922f401a4a2bb18066119a6f
Maor Leger <maorleger@microsoft.com>
2024-02-02 14:12:20 -0800

    initial function

--- sdk/identity/identity/src/msal/msalClient.ts
+++ sdk/identity/identity/src/msal/msalClient.ts
@@ -1,7 +1,25 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export interface MsalClient {}
+import { AccessToken, GetTokenOptions } from "@azure/core-auth";
+
+export interface MsalClient {
+  getTokenByClientSecret(
+    scopes: string[],
+    clientSecret: string,
+    options?: GetTokenOptions,
+  ): Promise<AccessToken>;
+}
+
 export function createMsalClient(_clientId: string, _tenantId: string): MsalClient {
-  return {};
+  return {
+    getTokenByClientSecret(
+      _scopes: string[],
+      _clientSecret: string,
+      _options?: GetTokenOptions,
+    ): Promise<AccessToken> {
+      throw new Error();
+      // implementation goes here
+    },
+  };
 }
--- sdk/identity/identity/test/internal/msalClient.spec.ts
+++ sdk/identity/identity/test/internal/msalClient.spec.ts
@@ -1,17 +1,47 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import * as msalClient from "../../src/msal/msalClient";
+
 import { assert } from "@azure/test-utils";
-import { createMsalClient } from "../../src/msal/msalClient";
+import { msalNodeTestSetup } from "../node/msalNodeTestSetup";
 
 describe.only("MsalClient", function () {
+  let cleanup: any;
+  afterEach(async function () {
+    if (cleanup) {
+      await cleanup();
+    }
+  });
+  beforeEach(async function () {
+    const setup = await msalNodeTestSetup(this.currentTest);
+    cleanup = setup.cleanup;
+  });
+
   describe("#createMsalClient", function () {
     it("can create an msal client with minimal configuration", function () {
       const clientId = "client-id";
       const tenantId = "tenant-id";
 
-      const msalClient = createMsalClient(clientId, tenantId);
-      assert.exists(msalClient);
+      const client = msalClient.createMsalClient(clientId, tenantId);
+      assert.exists(client);
+    });
+  });
+
+  describe("#getTokenByClientCredential", function () {
+    it("is supported", async function () {
+      const clientId = "client-id";
+      const tenantId = "tenant-id";
+
+      const client = msalClient.createMsalClient(clientId, tenantId);
+      const scopes = ["scope"];
+      const clientSecret = process.env.AZURE_CLIENT_SECRET!;
+
+      const accessToken = await client.getTokenByClientSecret(scopes, clientSecret);
+      assert.deepEqual(accessToken, {
+        token: "token",
+        expiresOnTimestamp: 0,
+      });
     });
   });
 });
