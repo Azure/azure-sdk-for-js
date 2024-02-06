7d820deca2e9b2ca674d7baedcc858e3a266dd26
Matthew Podwysocki <matthewp@microsoft.com>
2024-02-06 14:00:31 -0500

    [core] Formatting

--- sdk/core/ts-http-runtime/src/util/userAgentPlatform.ts
+++ sdk/core/ts-http-runtime/src/util/userAgentPlatform.ts
@@ -7,6 +7,14 @@ import * as process from "node:process";
 /**
  * @internal
  */
+interface ExtendedPlatformVersions extends NodeJS.ProcessVersions {
+  bun?: string;
+  deno?: string;
+}
+
+/**
+ * @internal
+ */
 export function getHeaderName(): string {
   return "User-Agent";
 }
@@ -15,7 +23,7 @@ export function getHeaderName(): string {
  * @internal
  */
 export function setPlatformSpecificData(map: Map<string, string>): void {
-  const versions = process.versions as any;
+  const versions = process.versions as ExtendedPlatformVersions;
   if (versions.bun) {
     map.set("Bun", versions.bun);
   } else if (versions.deno) {
