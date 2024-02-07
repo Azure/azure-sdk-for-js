16402f7c266bb3d25e3947952621dd1b065c321e
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2024-02-07 14:40:27 -0800

    Update

--- sdk/monitor/monitor-opentelemetry-exporter/src/utils/spanUtils.ts
+++ sdk/monitor/monitor-opentelemetry-exporter/src/utils/spanUtils.ts
@@ -3,7 +3,7 @@
 
 import { URL } from "url";
 import { ReadableSpan, TimedEvent } from "@opentelemetry/sdk-trace-base";
-import { addHrTimes, hrTime, hrTimeDuration, hrTimeToNanoseconds, hrTimeToTimeStamp } from "@opentelemetry/core";
+import { addHrTimes, hrTime, hrTimeToTimeStamp } from "@opentelemetry/core";
 import { diag, SpanKind, SpanStatusCode, Link, Attributes } from "@opentelemetry/api";
 import { SemanticAttributes, DbSystemValues } from "@opentelemetry/semantic-conventions";
 
@@ -48,13 +48,14 @@ function createTagsFromSpan(span: ReadableSpan): Tags {
       const httpUrl = span.attributes[SemanticAttributes.HTTP_URL];
       tags[KnownContextTagKeys.AiOperationName] = span.name; // Default
       if (httpRoute) {
-        tags[KnownContextTagKeys.AiOperationName] = `${httpMethod as string} ${httpRoute as string
-          }`;
+        tags[KnownContextTagKeys.AiOperationName] = `${httpMethod as string} ${
+          httpRoute as string
+        }`;
       } else if (httpUrl) {
         try {
           const url = new URL(String(httpUrl));
           tags[KnownContextTagKeys.AiOperationName] = `${httpMethod} ${url.pathname}`;
-        } catch (ex: any) { }
+        } catch (ex: any) {}
       }
       if (httpClientIp) {
         tags[KnownContextTagKeys.AiLocationIp] = String(httpClientIp);
@@ -148,7 +149,7 @@ function createDependencyData(span: ReadableSpan): RemoteDependencyData {
       try {
         const dependencyUrl = new URL(String(httpUrl));
         remoteDependencyData.name = `${httpMethod} ${dependencyUrl.pathname}`;
-      } catch (ex: any) { }
+      } catch (ex: any) {}
     }
     remoteDependencyData.type = DependencyTypes.Http;
     remoteDependencyData.data = getUrl(span.attributes);
@@ -173,7 +174,7 @@ function createDependencyData(span: ReadableSpan): RemoteDependencyData {
             target = res[1] + res[2] + res[4];
           }
         }
-      } catch (ex: any) { }
+      } catch (ex: any) {}
       remoteDependencyData.target = `${target}`;
     }
   }
