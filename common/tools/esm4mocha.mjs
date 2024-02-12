2de7d89bb2de38561d30ad7a6bfddfc3fc37b093
Jeremy Meng <jeremy.ymeng@gmail.com>
2024-02-12 11:32:57 -0800

    [EngSys] add a workaround to load ESM for Mocha
    Our '*.js' tests and sources under dist-esm/ are currently transformed by `esm`
    package before feeding them to Mocha because our package.json has `"type":
    "commonjs"` thus Mocha treats `.js` files as CommonJS.
    
    This PR takes another approach by telling Mocha that the loaded modules are ESM.
    `".js"` extension is added to relative imports/exports on the fly.

---
+++ common/tools/esm4mocha.mjs
@@ -0,0 +1,97 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { readFile, stat, constants } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { Project } from "./dev-tool/node_modules/ts-morph/dist/ts-morph.js";
+
+function consoleLog(...args) {
+  if (process.env.DEBUG) {
+    console.log(args);
+  }
+}
+
+function consoleDir(...args) {
+  if (process.env.DEBUG) {
+    console.dir(args);
+  }
+}
+
+export async function resolve(specifier, context, defaultResolve) {
+  consoleLog("resolving...");
+  consoleDir({specifier, context});
+  const resolved = await defaultResolve(specifier, context, defaultResolve);
+  consoleDir({resolved});
+  if (resolved.url.includes("dist-esm/")) {
+    resolved.format = "module";
+  }
+  return resolved;
+}
+
+async function updateSpecifierValueIfRelative(declaration, base) {
+    const specifierValue = declaration.getModuleSpecifierValue();
+    consoleDir({base, specifierValue});
+    if (specifierValue?.startsWith(".")) {
+      const sourcePath = join(base, specifierValue);
+      try {
+        const s = await stat(sourcePath, constants.R_OK);
+        consoleDir({sourcePath});
+        if (s.isDirectory()) {
+          declaration.setModuleSpecifier(`${specifierValue}/index.js`);
+        } else {
+          declaration.setModuleSpecifier(`${specifierValue}.js`);
+        }
+      } catch (ex) {
+        consoleDir({error: ex});
+        declaration.setModuleSpecifier(`${specifierValue}.js`);
+      }
+      consoleLog(`  specifier updated to ${declaration.getModuleSpecifierValue()}`);
+    }  
+}
+
+async function addJsExtensionToRelativeModules(source, path) {
+  const base = dirname(path);
+  const project = new Project({ useInMemoryFileSystem: true });
+  const text =  source.toString("utf-8");
+  const f = project.createSourceFile("file.ts", text);
+  const imports = f.getImportDeclarations();
+  const exports = f.getExportDeclarations();
+  consoleDir({l: "input",
+               path,
+               head: text.substring(0, 1200),
+               imports: imports.map(i => i.getText()),
+               exports: exports.map(e => e.getText()),
+              });
+  for (const i of imports) {
+    await updateSpecifierValueIfRelative(i, base);
+  }
+  for (const e of exports) {
+    await updateSpecifierValueIfRelative(e, base);
+  }
+
+  consoleDir({l:"output", head: f.getFullText().substring(0, 1200)});
+  return f.getFullText();
+}
+
+export async function load(url, context, defaultLoad) {
+  consoleLog("loading...");
+  consoleDir({url, context});
+
+  if (url.includes("dist-esm/")) {
+    const path = url.replace("file://", "");
+    const source = await readFile(path);
+    const transformed = await addJsExtensionToRelativeModules(source, path);
+    return {
+      format: context.format,
+      source: transformed,
+      shortCircuit: true
+    };
+  }
+  const { source } = await defaultLoad(
+    url,
+    context,
+    defaultLoad
+  );
+
+  return { format: context.format, source, shortCircuit: true };
+}
