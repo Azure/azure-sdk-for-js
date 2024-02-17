a967df0c5b2a44130108eb66395ffbfae05a15bc
Maor Leger <maorleger@microsoft.com>
2024-02-17 00:18:37 -0800

    working recorded tests

--- sdk/identity/identity/test/public/node/multiTenantAuthentication.spec.ts
+++ sdk/identity/identity/test/public/node/multiTenantAuthentication.spec.ts
@@ -2,45 +2,52 @@
 // Licensed under the MIT license.
 
 import { MsalTestCleanup, msalNodeTestSetup } from "../../node/msalNodeTestSetup";
-import { createDefaultHttpClient, createHttpHeaders } from "@azure/core-rest-pipeline";
+import { Recorder, env } from "@azure-tools/test-recorder";
+import { createHttpHeaders, createPipelineRequest } from "@azure/core-rest-pipeline";
 
 import { ClientSecretCredential } from "../../../src/credentials/clientSecretCredential";
 import { Context } from "mocha";
+import { IdentityClient } from "../../../src/client/identityClient";
+import { assert } from "@azure/test-utils";
 
 describe("MultiTenantAuthentication", function () {
   let cleanup: MsalTestCleanup;
+  let recorder: Recorder;
 
   beforeEach(async function (this: Context) {
     const setup = await msalNodeTestSetup(this.currentTest);
     cleanup = setup.cleanup;
+    recorder = setup.recorder;
   });
 
   afterEach(async function () {
     await cleanup();
   });
 
-  it.only("supports calling graph with client secret", async function () {
+  it("supports calling graph with client secret", async function () {
     // TODO: validate that env vars exist
     const credential = new ClientSecretCredential(
-      process.env.AZURE_IDENTITY_MULTI_TENANT_TENANT_ID!,
-      process.env.AZURE_IDENTITY_MULTI_TENANT_CLIENT_ID!,
-      process.env.AZURE_IDENTITY_MULTI_TENANT_CLIENT_SECRET!,
+      env.AZURE_IDENTITY_MULTI_TENANT_TENANT_ID!,
+      env.AZURE_IDENTITY_MULTI_TENANT_CLIENT_ID!,
+      env.AZURE_IDENTITY_MULTI_TENANT_CLIENT_SECRET!,
+      recorder.configureClientOptions({}),
     );
 
     const token = await credential.getToken("https://graph.microsoft.com/.default");
-    const client = createDefaultHttpClient();
-    const response = await client.sendRequest({
+    const client = new IdentityClient(recorder.configureClientOptions({}));
+
+    const request = createPipelineRequest({
+      url: "https://graph.microsoft.com/v1.0/applications/$count",
+      method: "GET",
       headers: createHttpHeaders({
         Authorization: `Bearer ${token.token}`,
         ConsistencyLevel: "eventual",
       }),
-      method: "GET",
-      requestId: "request-id",
-      timeout: 0,
-      url: "https://graph.microsoft.com/v1.0/applications/$count",
-      withCredentials: false,
     });
 
-    console.log(response);
+    const response = await client.sendRequest(request);
+    assert.equal(response.status, 200);
+    assert.isNotEmpty(response.bodyAsText);
+    assert.isAbove(Number.parseInt(response.bodyAsText!, 10), 0);
   });
 });
