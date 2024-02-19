891cd591e95c27c8b62dfd94e4df48297144fc76
Karolien Gellynck <138193722+KarolGel@users.noreply.github.com>
2024-02-19 14:12:47 +0100

    Update sample_critical_result_inference_async.js
    add paths to @azure-/health-insights-radiologyinsights

--- sdk/healthinsights/health-insights-radiologyinsights-rest/samples/v1-beta/javascript/sample_critical_result_inference_async.js
+++ sdk/healthinsights/health-insights-radiologyinsights-rest/samples/v1-beta/javascript/sample_critical_result_inference_async.js
@@ -6,8 +6,8 @@
  */
 
 const dotenv = require("dotenv");
-const AzureHealthInsightsClient = require('../../../src').default,
-  { getLongRunningPoller, isUnexpected } = require('../../../src');
+const AzureHealthInsightsClient = require("@azure-/health-insights-radiologyinsights").default,
+  { getLongRunningPoller, isUnexpected } = require("@azure-/health-insights-radiologyinsights");
 const { AzureKeyCredential } = require("@azure/core-auth");
 
 dotenv.config();
