31933218bc0ebf0562d75d4d7edec2e011f9be36
yifan-zhou922 <yifanzhou@microsoft.com>
2024-02-21 13:52:27 +0800

    update to support multipart form data

--- sdk/purview/purview-datamap-rest/src/parameters.ts
+++ sdk/purview/purview-datamap-rest/src/parameters.ts
@@ -353,7 +353,7 @@ export type EntityGetSampleBusinessMetadataTemplateParameters =
   RequestParameters;
 
 export interface EntityImportBusinessMetadataBodyParam {
-  body?: { file: string };
+  body?: { file: Uint8Array | File };
 }
 
 export interface EntityImportBusinessMetadataMediaTypesParam {
