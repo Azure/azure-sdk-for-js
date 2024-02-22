9a5d859fac2cb7c6a3ddcfc852fc0a452b8523ab
Karolien Gellynck <138193722+KarolGel@users.noreply.github.com>
2024-02-22 09:56:48 +0100

    Update isUnexpected.ts formatting

--- sdk/healthinsights/health-insights-radiologyinsights-rest/src/isUnexpected.ts
+++ sdk/healthinsights/health-insights-radiologyinsights-rest/src/isUnexpected.ts
@@ -19,11 +19,8 @@ export function isUnexpected(
   response: GetJob200Response | GetJobDefaultResponse,
 ): response is GetJobDefaultResponse;
 export function isUnexpected(
-  response:
-    | CreateJob202Response
-    | CreateJobLogicalResponse
-    | CreateJobDefaultResponse,
-): response is CreateJobDefaultResponse;
+ response: CreateJob202Response | CreateJobLogicalResponse | CreateJobDefaultResponse,
+):  response is CreateJobDefaultResponse;
 export function isUnexpected(
   response:
     | GetJob200Response
@@ -63,25 +60,18 @@ function getParametrizedPathSuccess(method: string, path: string): string[] {
     const candidateParts = candidatePath.split("/");
 
     // track if we have found a match to return the values found.
-    let found = true;
-    for (
-      let i = candidateParts.length - 1, j = pathParts.length - 1;
-      i >= 1 && j >= 1;
-      i--, j--
-    ) {
-      if (
-        candidateParts[i]?.startsWith("{") &&
-        candidateParts[i]?.indexOf("}") !== -1
-      ) {
+   let found = true;
+    for (let i = candidateParts.length - 1, j = pathParts.length - 1; i >= 1 && j >= 1; i--, j--) {
+      if (candidateParts[i]?.startsWith("{") && candidateParts[i]?.indexOf("}") !== -1) {
         const start = candidateParts[i]!.indexOf("}") + 1,
           end = candidateParts[i]?.length;
         // If the current part of the candidate is a "template" part
         // Try to use the suffix of pattern to match the path
         // {guid} ==> $
         // {guid}:export ==> :export$
-        const isMatched = new RegExp(
-          `${candidateParts[i]?.slice(start, end)}`,
-        ).test(pathParts[j] || "");
+         const isMatched = new RegExp(`${candidateParts[i]?.slice(start, end)}`).test(
+          pathParts[j] || "",
+        );
 
         if (!isMatched) {
           found = false;
