269871d058d1d9f300ab1ad57635d3f8184d0f37
Maor Leger <maorleger@microsoft.com>
2024-02-21 21:31:14 -0800

    fix vitest tests for core-tracing

--- sdk/core/core-tracing/src/state-cjs.cts
+++ sdk/core/core-tracing/src/state-cjs.cts
@@ -4,17 +4,8 @@
 /**
  * @internal
  *
- * The tracing instrumenter that can be replaced at runtime by OpenTelemetry instrumentation.
- *
- * Unknown is used to avoid importing `interfaces` from the CJS module, which would require dynamic imports.
- */
-let instrumenterImplementation: unknown | undefined;
-
-/**
- * @internal
- *
  * Holds the singleton instrumenter, to be shared across CJS and ESM imports.
  */
 export const state = {
-  instrumenterImplementation,
+  instrumenterImplementation: undefined,
 };
--- sdk/core/core-tracing/src/state.ts
+++ sdk/core/core-tracing/src/state.ts
@@ -4,11 +4,11 @@
 import { Instrumenter } from "./interfaces.js";
 // @ts-expect-error The recommended approach to sharing module state between ESM and CJS.
 // See https://github.com/isaacs/tshy/blob/main/README.md#module-local-state for additional information.
-import cjsState from "../commonjs/state.js";
+import { state as cjsState } from "../commonjs/state.js";
 
 /**
  * Defines the shared state between CJS and ESM by re-exporting the CJS state.
  */
-export const state = {
-  instrumenterImplementation: cjsState.state.instrumenterImplementation as Instrumenter | undefined,
+export const state = cjsState as {
+  instrumenterImplementation: Instrumenter | undefined;
 };
