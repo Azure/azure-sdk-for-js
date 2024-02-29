cb9f53149a118661d4ec4cb501b39e3b7607f859
Minh-Anh Phan <minhanhphan@microsoft.com>
2024-02-29 22:05:58 +0000

    format samples

--- sdk/appconfiguration/app-configuration/samples-dev/listConfigurationSettingsWithEtags.ts
+++ sdk/appconfiguration/app-configuration/samples-dev/listConfigurationSettingsWithEtags.ts
@@ -18,12 +18,12 @@ export async function main() {
   const connectionString = process.env["APPCONFIG_CONNECTION_STRING"] || "<connection string>";
   const client = new AppConfigurationClient(connectionString);
 
-  const key ="sample key"
+  const key = "sample key";
   const pageSize = 100;
   // this number is chosen to create 2 full page an an empty 3 page
   const expectedNumberOfLabels = pageSize * 2;
 
-  console.log("Creating 2 full pages of settings")
+  console.log("Creating 2 full pages of settings");
   for (let i = 0; i < expectedNumberOfLabels; i++) {
     client.addConfigurationSetting({
       key,
@@ -32,17 +32,17 @@ export async function main() {
     });
   }
 
-  console.log("Get the current page Etags into a list")
+  console.log("Get the current page Etags into a list");
   let iterator = client.listConfigurationSettings({ keyFilter: key }).byPage();
   const etags: string[] = [];
   for await (const page of iterator) {
-    console.log(`  The current page etag is ${page.etag}`)
+    console.log(`  The current page etag is ${page.etag}`);
     etags.push(page.etag ?? "");
   }
 
   // This number is arbitrarily chosen to add new setting to the 3rd page
-  console.log("Add additional etags to create a 3rd page")
-  const additionalNumberOfLabels = 50; 
+  console.log("Add additional etags to create a 3rd page");
+  const additionalNumberOfLabels = 50;
   for (let i = expectedNumberOfLabels; i < expectedNumberOfLabels + additionalNumberOfLabels; i++) {
     client.addConfigurationSetting({
       key,
@@ -52,7 +52,7 @@ export async function main() {
   }
 
   // Second run with added settings
-  console.log("Checking for updated pages")
+  console.log("Checking for updated pages");
   iterator = client.listConfigurationSettings({ keyFilter: key, pageEtags: etags }).byPage();
 
   for await (const page of iterator) {
@@ -61,7 +61,7 @@ export async function main() {
       console.log("No updates for this page");
     } else if (statusCode === 200) {
       console.log("Updates available for this page");
-      console.log(`  The new page etag is ${page.etag}`)
+      console.log(`  The new page etag is ${page.etag}`);
     }
   }
 
