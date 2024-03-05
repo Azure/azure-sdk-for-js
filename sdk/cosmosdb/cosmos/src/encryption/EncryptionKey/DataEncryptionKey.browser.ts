de769fb580edd0ee5450eaf974b9f5f7077c05af
Aman Rao <amanrao@microsoft.com>
2024-03-05 18:34:44 +0530

    format code

--- sdk/cosmosdb/cosmos/src/encryption/EncryptionKey/DataEncryptionKey.browser.ts
+++ sdk/cosmosdb/cosmos/src/encryption/EncryptionKey/DataEncryptionKey.browser.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export abstract class DataEncryptionKey {};
+export abstract class DataEncryptionKey {}
 
 export function createHmac(_algorithm: string, _key: Buffer): Promise<Buffer> {
   throw new Error("Client-side hmac generator not supported in browser environment");
