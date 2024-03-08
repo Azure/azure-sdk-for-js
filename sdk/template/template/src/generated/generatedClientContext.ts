cf7381d0ab5b5c9537935f34c8a0e8fc1fdf3875
azure-sdk <azuresdk@microsoft.com>
2024-03-08 21:33:43 +0000

    Post release automated changes for azure-template

--- sdk/template/template/src/generated/generatedClientContext.ts
+++ sdk/template/template/src/generated/generatedClientContext.ts
@@ -33,7 +33,7 @@ export class GeneratedClientContext extends coreClient.ServiceClient {
       requestContentType: "application/json; charset=utf-8"
     };
 
-    const packageDetails = `azsdk-js-template/1.0.11-beta.1`;
+    const packageDetails = `azsdk-js-template/1.0.11-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
