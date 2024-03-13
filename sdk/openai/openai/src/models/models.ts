b99eab54674bb76202056eae1dcd8ed8cc2d7a62
Minh-Anh Phan <minhanhphan@microsoft.com>
2024-03-13 23:31:49 +0000

    add index property

--- sdk/openai/openai/src/models/models.ts
+++ sdk/openai/openai/src/models/models.ts
@@ -490,6 +490,8 @@ export interface ChatCompletionsFunctionToolCall {
   function: FunctionCall;
   /** The ID of the tool call. */
   id: string;
+  /** The index of the tool calls. */
+  index?: number;
 }
 
 /**
