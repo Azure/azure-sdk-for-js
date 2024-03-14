c92e99dfa3e59a0851aab9f69aa250f436e3078e
KarishmaGhiya <kghiya8@gmail.com>
2024-03-13 19:15:15 -0700

    Update azureWebAppsTest.spec.ts

--- sdk/identity/identity/test/integration/azureWebAppsTest.spec.ts
+++ sdk/identity/identity/test/integration/azureWebAppsTest.spec.ts
@@ -8,23 +8,8 @@ import { Context } from "mocha";
 import { isLiveMode } from "@azure-tools/test-recorder";
 
 describe("AzureWebApps Integration test", function () {
-  it("test the Azure Web Apps endpoint where the '/' MI credential is used.", async function (this: Context) {
+  it("test the Azure Web Apps endpoint where the MI credential is used.", async function (this: Context) {
     if (!isLiveMode()) {
-      this.skip();
-    }
-    const baseUri = baseUrlSync();
-    const client = new ServiceClient({ baseUri: baseUri });
-    const pipelineRequest = createPipelineRequest({
-      url: baseUri,
-      method: "GET",
-    });
-    const response = await client.sendRequest(pipelineRequest);
-    console.log(response.bodyAsText);
-    assert.equal(response.status, 200, `Expected status 200. Received ${response.status}`);
-  });
-
-  it("test the Azure Web Apps endpoint where the '/sync' MI credential is used.", async function (this: Context) {
-    if (!isLiveMode) {
       this.skip();
     }
     const baseUri = baseUrl();
@@ -38,15 +23,6 @@ describe("AzureWebApps Integration test", function () {
     assert.equal(response.status, 200, `Expected status 200. Received ${response.status}`);
   });
 });
-
-function baseUrlSync(): string {
-  const webAppName = process.env.IDENTITY_WEBAPP_NAME;
-  if (!webAppName) {
-    console.log("IDENTITY_WEBAPP_NAME is not set");
-    throw new Error("IDENTITY_WEBAPP_NAME is not set");
-  }
-  return `https://${webAppName}.azurewebsites.net/`;
-}
 
 function baseUrl(): string {
   const webAppName = process.env.IDENTITY_WEBAPP_NAME;
