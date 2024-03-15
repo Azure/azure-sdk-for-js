fdd1c26450042a1b178ac6a5a36b41d1d1389a86
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-03-15 14:09:52 +0800

    [mgmt] batch release (#28900)
    https://github.com/Azure/sdk-release-request/issues/5016

--- sdk/batch/arm-batch/samples/v9/javascript/batchAccountListDetectorsSample.js
+++ sdk/batch/arm-batch/samples/v9/javascript/batchAccountListDetectorsSample.js
@@ -16,7 +16,7 @@ require("dotenv").config();
  * This sample demonstrates how to Gets information about the detectors available for a given Batch account.
  *
  * @summary Gets information about the detectors available for a given Batch account.
- * x-ms-original-file: specification/batch/resource-manager/Microsoft.Batch/stable/2023-11-01/examples/DetectorList.json
+ * x-ms-original-file: specification/batch/resource-manager/Microsoft.Batch/stable/2024-02-01/examples/DetectorList.json
  */
 async function listDetectors() {
   const subscriptionId = process.env["BATCH_SUBSCRIPTION_ID"] || "subid";
