2626ba41aa86489f159443e85a713a9cfa73caa8
Matthew Podwysocki <matthewp@microsoft.com>
2024-03-18 12:56:32 -0400

    [notification-hubs] Fix config path issue

--- sdk/notificationhubs/notification-hubs/vitest.int.config.ts
+++ sdk/notificationhubs/notification-hubs/vitest.int.config.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "./vitest.shared.config.ts";
+import viteConfig from "../../../vitest.shared.config.ts";
 
 export default mergeConfig(
   viteConfig,
