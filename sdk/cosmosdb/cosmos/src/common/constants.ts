cdeba9c6007c882b01e375be3566d66101872ac0
Aman Rao <amanrao@microsoft.com>
2024-03-20 01:51:14 +0530

    fix readClientEncryptionKeys

--- sdk/cosmosdb/cosmos/src/common/constants.ts
+++ sdk/cosmosdb/cosmos/src/common/constants.ts
@@ -293,7 +293,7 @@ export enum ResourceType {
   item = "docs",
   pkranges = "pkranges",
   partitionkey = "partitionKey",
-  clientencryptionkey = "clientEncryptionKeys",
+  clientencryptionkey = "clientencryptionkeys",
 }
 
 /**
