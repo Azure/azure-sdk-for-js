158098c02bb0795ce27ef692929aa1c28670ec21
Albert Cheng <albertcheng@microsoft.com>
2024-03-21 12:00:22 -0700

    parameter changes

--- eng/tools/publish-to-npm.ps1
+++ eng/tools/publish-to-npm.ps1
@@ -155,9 +155,9 @@ try {
         }
         elseif ($p.Publish -and $publishToNpm) {
             if ($additionalTag -eq "") {
-                write-host "Copy $($p.TarGz) to $pipelineWorkspace"
-                New-Item -ItemType File -Path $pipelineWorkspace -Force
-                Copy-Item -Path $($p.TarGz) -Destination $pipelineWorkspace -Force
+                write-host "Copy $($p.TarGz) to $packagesToPublishPath"
+                New-Item -ItemType File -Path $packagesToPublishPath -Force
+                Copy-Item -Path $($p.TarGz) -Destination $packagesToPublishPath -Force
             } elseif ($additionalTag -ne $tag) {
               npm dist-tag add "$($p.Project.name)@$($p.Project)" $additionalTag
               .$(Build.SourcesDirectory)/eng/scripts/npm-admin-tasks.ps1 -taskType AddTag -packageName $p.Project.name -pkgVersion $p.Project.version -tagName $AdditionalTag -npmToken $(azure-sdk-npm-token)
