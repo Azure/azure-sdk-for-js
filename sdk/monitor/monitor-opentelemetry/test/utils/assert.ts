d3235c5331e1e6a89e44981d8e4f2053ba059463
Jackson Weber <jacksonweber@microsoft.com>
2024-03-28 12:02:00 -0700

    Handle parsing nested objects in integration tests.

--- sdk/monitor/monitor-opentelemetry/test/utils/assert.ts
+++ sdk/monitor/monitor-opentelemetry/test/utils/assert.ts
@@ -19,11 +19,21 @@ export const assertData = (actual: MonitorBase, expected: MonitorBase): void => 
   assert.ok(actual.baseData);
   for (const [key, value] of Object.entries(expected.baseData!)) {
     const serializedKey = (EnvelopeMapper.type as any).modelProperties![key]?.serializedName ?? key;
-    assert.deepStrictEqual(
-      actual.baseData[serializedKey],
-      value,
-      `baseData.${serializedKey} should be equal\nActual: ${actual.baseData[serializedKey]}\nExpected: ${value}`,
-    );
+    if (typeof(value) === "object") {
+      for (const [nestedKey, nestedValue] of Object.entries(value)) {
+        assert.deepStrictEqual(
+          expected.baseData![serializedKey][nestedKey],
+          nestedValue,
+          `baseData.${serializedKey}.${nestedKey} should be equal\nActual: ${JSON.stringify(actual.baseData[serializedKey][nestedKey])}\nExpected: ${JSON.stringify(nestedValue)}`,
+        );
+      }
+    } else {
+      assert.deepStrictEqual(
+        actual.baseData[serializedKey],
+        value,
+        `baseData.${serializedKey} should be equal\nActual: ${JSON.stringify(actual.baseData[serializedKey])}\nExpected: ${JSON.stringify(value)}`,
+      );
+    }
   }
 };
 
