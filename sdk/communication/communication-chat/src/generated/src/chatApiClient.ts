ca11ece719f972dd6781dfbbf0b2eafb355777dc
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2024-03-28 09:24:13 -0700

    Update to 1.5.0-beta.2 and 2024-03-01-preview versions

--- sdk/communication/communication-chat/src/generated/src/chatApiClient.ts
+++ sdk/communication/communication-chat/src/generated/src/chatApiClient.ts
@@ -38,7 +38,7 @@ export class ChatApiClient extends coreClient.ServiceClient {
       requestContentType: "application/json; charset=utf-8"
     };
 
-    const packageDetails = `azsdk-js-communication-chat/1.5.0-beta.1`;
+    const packageDetails = `azsdk-js-communication-chat/1.5.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -57,7 +57,7 @@ export class ChatApiClient extends coreClient.ServiceClient {
     this.endpoint = endpoint;
 
     // Assigning values to Constant parameters
-    this.apiVersion = options.apiVersion || "2023-11-15-preview";
+    this.apiVersion = options.apiVersion || "2024-03-01-preview";
     this.chatThread = new ChatThreadImpl(this);
     this.chat = new ChatImpl(this);
     this.addCustomApiVersionPolicy(options.apiVersion);
