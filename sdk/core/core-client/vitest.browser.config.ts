8e2386394001f6038f29227a507e47efcd6c353a
Harsha Nalluru <sanallur@microsoft.com>
2024-04-01 14:39:14 -0700

    Update sdk/core/core-client/vitest.browser.config.ts

--- sdk/core/core-client/vitest.browser.config.ts
+++ sdk/core/core-client/vitest.browser.config.ts
@@ -15,11 +15,6 @@ export default defineConfig({
       name: "chromium",
       provider: "playwright",
       slowHijackESM: false,
-      providerOptions: {
-        launch: {
-          args: ["--disable-web-security"]
-        }
-      }
     },
     fakeTimers: {
       toFake: ["setTimeout"],
