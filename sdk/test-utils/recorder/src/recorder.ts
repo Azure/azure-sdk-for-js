bc08460d5a8a718b75c4ea243c59e8c5a565ce4e
Harsha Nalluru <sanallur@microsoft.com>
2024-04-16 21:04:53 +0000

    format

--- sdk/test-utils/recorder/src/recorder.ts
+++ sdk/test-utils/recorder/src/recorder.ts
@@ -265,8 +265,9 @@ export class Recorder {
     logger.info(`[Recorder#start] Starting the recorder in ${getTestMode()} mode`);
     this.stateManager.state = "started";
     if (this.recordingId === undefined) {
-      const startUri = `${Recorder.url}${isPlaybackMode() ? paths.playback : paths.record}${paths.start
-        }`;
+      const startUri = `${Recorder.url}${isPlaybackMode() ? paths.playback : paths.record}${
+        paths.start
+      }`;
 
       const req = createRecordingRequest(
         startUri,
@@ -379,8 +380,9 @@ export class Recorder {
     this.stateManager.state = "stopped";
     if (this.recordingId !== undefined) {
       logger.info("[Recorder#stop] Stopping recording", this.recordingId);
-      const stopUri = `${Recorder.url}${isPlaybackMode() ? paths.playback : paths.record}${paths.stop
-        }`;
+      const stopUri = `${Recorder.url}${isPlaybackMode() ? paths.playback : paths.record}${
+        paths.stop
+      }`;
 
       const req = createRecordingRequest(stopUri, undefined, this.recordingId);
       req.headers.set("x-recording-save", "true");
--- sdk/test-utils/recorder/src/utils/fallbackSanitizers.ts
+++ sdk/test-utils/recorder/src/utils/fallbackSanitizers.ts
@@ -5,41 +5,93 @@ import { HttpClient } from "@azure/core-rest-pipeline";
 import { addSanitizers } from "../sanitizer.js";
 import { BodyKeySanitizer, FindReplaceSanitizer, HeaderSanitizer } from "./utils.js";
 
-const JSON_BODY_KEYS_TO_REDACT = ["authHeader", "accountKey",
-  "accessToken", "accountName", "applicationId", "apiKey", "client_secret", "connectionString", "url", "host",
-  "password", "userName", "applicationSecret", "aliasSecondaryConnectionString", "aliasPrimaryConnectionString",
-  "primaryKey", "secondaryKey", "adminPassword.value", "administratorLoginPassword", "runAsPassword",
-  "adminPassword", "accessSAS", "WEBSITE_AUTH_ENCRYPTION_KEY", "decryptionKey", "primaryMasterKey",
-  "primaryReadonlyMasterKey", "secondaryMasterKey", "secondaryReadonlyMasterKey", "certificatePassword",
-  "clientSecret", "keyVaultClientSecret", "authHeader", "httpHeader", "encryptedCredential", "appkey",
-  "functionKey", "atlasKafkaPrimaryEndpoint", "atlasKafkaSecondaryEndpoint", "certificatePassword",
-  "storageAccountPrimaryKey", "privateKey", "fencingClientPassword", "acrToken", "scriptUrlSasToken",
-  "azureBlobSource.containerUrl", "properties.DOCKER_REGISTRY_SEVER_PASSWORD"];
+const JSON_BODY_KEYS_TO_REDACT = [
+  "authHeader",
+  "accountKey",
+  "accessToken",
+  "accountName",
+  "applicationId",
+  "apiKey",
+  "client_secret",
+  "connectionString",
+  "url",
+  "host",
+  "password",
+  "userName",
+  "applicationSecret",
+  "aliasSecondaryConnectionString",
+  "aliasPrimaryConnectionString",
+  "primaryKey",
+  "secondaryKey",
+  "adminPassword.value",
+  "administratorLoginPassword",
+  "runAsPassword",
+  "adminPassword",
+  "accessSAS",
+  "WEBSITE_AUTH_ENCRYPTION_KEY",
+  "decryptionKey",
+  "primaryMasterKey",
+  "primaryReadonlyMasterKey",
+  "secondaryMasterKey",
+  "secondaryReadonlyMasterKey",
+  "certificatePassword",
+  "clientSecret",
+  "keyVaultClientSecret",
+  "authHeader",
+  "httpHeader",
+  "encryptedCredential",
+  "appkey",
+  "functionKey",
+  "atlasKafkaPrimaryEndpoint",
+  "atlasKafkaSecondaryEndpoint",
+  "certificatePassword",
+  "storageAccountPrimaryKey",
+  "privateKey",
+  "fencingClientPassword",
+  "acrToken",
+  "scriptUrlSasToken",
+  "azureBlobSource.containerUrl",
+  "properties.DOCKER_REGISTRY_SEVER_PASSWORD",
+];
 
-const BODY_REGEXES_TO_REDACT
-  = ["(?:(Password|User ID)=)(?<secret>.*)(?:;)", "client_secret=(?<secret>[^&]+)",
-    "<PrimaryKey>(?<secret>.*?)</PrimaryKey>", "<SecondaryKey>(?<secret>.*?)</SecondaryKey>",
-    "<UserDelegationKey>.*?<SignedOid>(?<secret>.*?)</SignedOid>.*?</UserDelegationKey>",
-    "<UserDelegationKey>.*?<SignedTid>(?<secret>.*?)</SignedTid>.*?</UserDelegationKey>",
-    "<UserDelegationKey>.*?<Value>(?<secret>.*?)</Value>.*?</UserDelegationKey>",
-    "SharedAccessKey=(?<secret>[^;\\\"]+)", "AccountKey=(?<secret>[^;\\\"]+)", "accesskey=(?<secret>[^;\\\"]+)",
-    "AccessKey=(?<secret>[^;\\\"]+)", "Secret=(?<secret>[^;\\\"]+)", "access_token=(?<secret>.*?)(?=&|$)",
-    "refresh_token=(?<secret>.*?)(?=&|$)", "(?:(sv|sig|se|srt|ss|sp)=)(?<secret>[^&\\\"]*)"];
+const BODY_REGEXES_TO_REDACT = [
+  "(?:(Password|User ID)=)(?<secret>.*)(?:;)",
+  "client_secret=(?<secret>[^&]+)",
+  "<PrimaryKey>(?<secret>.*?)</PrimaryKey>",
+  "<SecondaryKey>(?<secret>.*?)</SecondaryKey>",
+  "<UserDelegationKey>.*?<SignedOid>(?<secret>.*?)</SignedOid>.*?</UserDelegationKey>",
+  "<UserDelegationKey>.*?<SignedTid>(?<secret>.*?)</SignedTid>.*?</UserDelegationKey>",
+  "<UserDelegationKey>.*?<Value>(?<secret>.*?)</Value>.*?</UserDelegationKey>",
+  'SharedAccessKey=(?<secret>[^;\\"]+)',
+  'AccountKey=(?<secret>[^;\\"]+)',
+  'accesskey=(?<secret>[^;\\"]+)',
+  'AccessKey=(?<secret>[^;\\"]+)',
+  'Secret=(?<secret>[^;\\"]+)',
+  "access_token=(?<secret>.*?)(?=&|$)",
+  "refresh_token=(?<secret>.*?)(?=&|$)",
+  '(?:(sv|sig|se|srt|ss|sp)=)(?<secret>[^&\\"]*)',
+];
 
 const URL_REGEX = "(?<=http://|https://)([^/?]+)";
 
-const HEADER_KEYS_TO_REDACT = ["Ocp-Apim-Subscription-Key", "api-key",
-  "x-api-key", "subscription-key", "x-ms-encryption-key", "sshPassword"];
+const HEADER_KEYS_TO_REDACT = [
+  "Ocp-Apim-Subscription-Key",
+  "api-key",
+  "x-api-key",
+  "subscription-key",
+  "x-ms-encryption-key",
+  "sshPassword",
+];
 
 export async function fallbackSanitizers(
   httpClient: HttpClient,
   url: string,
-  recordingId: string): Promise<void> {
-  const bodyKeySanitizers: BodyKeySanitizer[] = JSON_BODY_KEYS_TO_REDACT
-    .map((prop) => ({
-      jsonPath: `$..${prop}`, // Handles the request body
-      value: "REDACTED",
-    }));
+  recordingId: string,
+): Promise<void> {
+  const bodyKeySanitizers: BodyKeySanitizer[] = JSON_BODY_KEYS_TO_REDACT.map((prop) => ({
+    jsonPath: `$..${prop}`, // Handles the request body
+    value: "REDACTED",
+  }));
 
   const generalSanitizers: FindReplaceSanitizer[] = BODY_REGEXES_TO_REDACT.map((regex) => ({
     value: "REDACTED",
@@ -49,14 +101,27 @@ export async function fallbackSanitizers(
   }));
 
   const headerSanitizers: HeaderSanitizer[] = [
-    { key: "Operation-location", groupForReplace: "secret", regex: true, target: URL_REGEX, value: "REDACTED" },
     {
-      key: "ServiceBusDlqSupplementaryAuthorization", groupForReplace: "secret", regex: true, target:
-        "(?:(sv|sig|se|srt|ss|sp)=)(?<secret>[^&\\\"]+)", value: "REDACTED"
+      key: "Operation-location",
+      groupForReplace: "secret",
+      regex: true,
+      target: URL_REGEX,
+      value: "REDACTED",
     },
     {
-      key: "ServiceBusSupplementaryAuthorization", groupForReplace: "secret", regex: true, target: "(?:(sv|sig|se|srt|ss|sp)=)(?<secret>[^&\\\"]+)", value: "REDACTED"
-    }
+      key: "ServiceBusDlqSupplementaryAuthorization",
+      groupForReplace: "secret",
+      regex: true,
+      target: '(?:(sv|sig|se|srt|ss|sp)=)(?<secret>[^&\\"]+)',
+      value: "REDACTED",
+    },
+    {
+      key: "ServiceBusSupplementaryAuthorization",
+      groupForReplace: "secret",
+      regex: true,
+      target: '(?:(sv|sig|se|srt|ss|sp)=)(?<secret>[^&\\"]+)',
+      value: "REDACTED",
+    },
   ];
 
   const headersForRemoval: string[] = HEADER_KEYS_TO_REDACT;
@@ -64,6 +129,6 @@ export async function fallbackSanitizers(
     bodyKeySanitizers,
     generalSanitizers,
     removeHeaderSanitizer: { headersForRemoval },
-    headerSanitizers
+    headerSanitizers,
   });
 }
