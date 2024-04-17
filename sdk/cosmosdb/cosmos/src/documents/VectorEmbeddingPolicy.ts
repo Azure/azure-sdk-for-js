f6d6e78f0e93e5705fcc4f081f67884792ad7376
Manik Khandelwal <mkhandelwal@microsoft.com>
2024-04-17 15:27:45 +0530

    update enum

--- sdk/cosmosdb/cosmos/src/documents/VectorEmbeddingPolicy.ts
+++ sdk/cosmosdb/cosmos/src/documents/VectorEmbeddingPolicy.ts
@@ -17,35 +17,9 @@ export interface VectorEmbedding {
   /**
    * The data type of the vector.
    */
-  dataType: VectorDataType;
+  dataType: "float16" | "float32" | "uint8" | "int8";
   /**
    * The distance function to use for distance calculation in between vectors.
    */
-  distanceFunction: DistanceFunction;
-  /**
-   * Additional properties for the vector embedding.
-   * This contains additional values for scenarios where the SDK is not aware of new fields.
-   * This ensures that if resource is read and updated none of the fields will be lost in the process.
-   */
-  additionalProperties?: { [key: string]: any };
-}
-
-/**
- * Represents the data type for vector embeddings.
- */
-export enum VectorDataType {
-  Float16 = "float16",
-  Float32 = "float32",
-  Uint8 = "uint8",
-  Int8 = "int8",
-}
-
-/**
- * Defines the distance function for a vector index specification in the
- * Azure Cosmos DB service.
- */
-export enum DistanceFunction {
-  Euclidean = "euclidean",
-  Cosine = "cosine",
-  DotProduct = "dotproduct",
+  distanceFunction: "euclidean" | "cosine" | "dotproduct";
 }
