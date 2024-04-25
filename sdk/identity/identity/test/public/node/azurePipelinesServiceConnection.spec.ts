f66b82fbab90a8c0ad398e3454a44b7dadb3660a
Karishma Ghiya <kaghiya@microsoft.com>
2024-04-24 17:19:46 -0700

    update service connection

--- sdk/identity/identity/test/public/node/azurePipelinesServiceConnection.spec.ts
+++ sdk/identity/identity/test/public/node/azurePipelinesServiceConnection.spec.ts
@@ -23,16 +23,20 @@ describe("AzurePipelinesServiceConnectionCredential", function () {
 
   const scope = "https://vault.azure.net/.default";
   const tenantId = env.IDENTITY_SP_TENANT_ID || env.AZURE_TENANT_ID!;
-  const clientId = env.IDENTITY_SP_CLIENT_ID || env.AZURE_CLIENT_ID!;
+  //const clientId = env.IDENTITY_SP_CLIENT_ID || env.AZURE_CLIENT_ID!;
 
   it("authenticates with a valid service connection", async function () {
     if (!isLiveMode()) {
       this.skip();
     }
+    // this serviceConnection corresponds to the Azure SDK Test Resources - LiveTestSecrets service
+    const existingServiceConnectionId = "0dec29c2-a766-4121-9c2e-1894f5aca5cb";
+    // clientId for above service connection
+    const clientId = "203c27cb-6778-4ecc-9bfd-9f03a61f3408";
     const credential = new AzurePipelinesServiceConnectionCredential(
       clientId,
       tenantId,
-      "0dec29c2-a766-4121-9c2e-1894f5aca5cb",
+      existingServiceConnectionId
     );
     try {
       const token = await credential.getToken(scope);
