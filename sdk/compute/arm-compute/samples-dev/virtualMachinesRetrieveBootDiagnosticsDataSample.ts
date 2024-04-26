6dded1bcdea36815ef7ab7fc6a0f6cc625bbcaf5
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-04-26 09:29:49 +0800

    [mgmt] compute release (#29270)
    https://github.com/Azure/sdk-release-request/issues/5069

--- sdk/compute/arm-compute/samples-dev/virtualMachinesRetrieveBootDiagnosticsDataSample.ts
+++ sdk/compute/arm-compute/samples-dev/virtualMachinesRetrieveBootDiagnosticsDataSample.ts
@@ -21,7 +21,7 @@ dotenv.config();
  * This sample demonstrates how to The operation to retrieve SAS URIs for a virtual machine's boot diagnostic logs.
  *
  * @summary The operation to retrieve SAS URIs for a virtual machine's boot diagnostic logs.
- * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-09-01/examples/virtualMachineExamples/VirtualMachine_RetrieveBootDiagnosticsData.json
+ * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples/virtualMachineExamples/VirtualMachine_RetrieveBootDiagnosticsData.json
  */
 async function retrieveBootDiagnosticsDataOfAVirtualMachine() {
   const subscriptionId =
--- sdk/compute/arm-compute/samples/v21/javascript/virtualMachineExtensionsCreateOrUpdateSample.js
+++ sdk/compute/arm-compute/samples/v21/javascript/virtualMachineExtensionsCreateOrUpdateSample.js
@@ -16,7 +16,7 @@ require("dotenv").config();
  * This sample demonstrates how to The operation to create or update the extension.
  *
  * @summary The operation to create or update the extension.
- * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-09-01/examples/virtualMachineExamples/VirtualMachineExtension_CreateOrUpdate_MaximumSet_Gen.json
+ * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples/virtualMachineExamples/VirtualMachineExtension_CreateOrUpdate_MaximumSet_Gen.json
  */
 async function virtualMachineExtensionCreateOrUpdateMaximumSetGen() {
   const subscriptionId = process.env["COMPUTE_SUBSCRIPTION_ID"] || "{subscription-id}";
@@ -74,7 +74,7 @@ async function virtualMachineExtensionCreateOrUpdateMaximumSetGen() {
  * This sample demonstrates how to The operation to create or update the extension.
  *
  * @summary The operation to create or update the extension.
- * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2023-09-01/examples/virtualMachineExamples/VirtualMachineExtension_CreateOrUpdate_MinimumSet_Gen.json
+ * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/ComputeRP/stable/2024-03-01/examples/virtualMachineExamples/VirtualMachineExtension_CreateOrUpdate_MinimumSet_Gen.json
  */
 async function virtualMachineExtensionCreateOrUpdateMinimumSetGen() {
   const subscriptionId = process.env["COMPUTE_SUBSCRIPTION_ID"] || "{subscription-id}";
