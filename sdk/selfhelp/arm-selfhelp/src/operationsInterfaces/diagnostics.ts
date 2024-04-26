4d300c43190c59daa61037c1247353561e7308fd
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-04-26 11:45:22 +0800

    [mgmt] selfhelp release (#29315)
    https://github.com/Azure/sdk-release-request/issues/5110

--- sdk/selfhelp/arm-selfhelp/src/operationsInterfaces/diagnostics.ts
+++ sdk/selfhelp/arm-selfhelp/src/operationsInterfaces/diagnostics.ts
@@ -11,17 +11,15 @@ import {
   DiagnosticsCreateOptionalParams,
   DiagnosticsCreateResponse,
   DiagnosticsGetOptionalParams,
-  DiagnosticsGetResponse
+  DiagnosticsGetResponse,
 } from "../models";
 
 /** Interface representing a Diagnostics. */
 export interface Diagnostics {
   /**
-   * Creates a diagnostic for the specific resource using solutionId and requiredInputs* from discovery
-   * solutions. <br/>Diagnostics are powerful solutions that access product resources or other relevant
-   * data and provide the root cause of the issue and the steps to address the issue.<br/><br/> <b>Note:
-   * </b> ‘requiredInputs’ from Discovery solutions response must be passed via ‘additionalParameters’ as
-   * an input to Diagnostics API.
+   * Creates a diagnostic for the specific resource using solutionId from discovery solutions.
+   * <br/>Diagnostics are powerful solutions that access product resources or other relevant data and
+   * provide the root cause of the issue and the steps to address the issue.<br/><br/>
    * @param scope scope = resourceUri of affected resource.<br/> For example:
    *              /subscriptions/0d0fcd2e-c4fd-4349-8497-200edb3923c6/resourcegroups/myresourceGroup/providers/Microsoft.KeyVault/vaults/test-keyvault-non-read
    *
@@ -31,7 +29,7 @@ export interface Diagnostics {
   beginCreate(
     scope: string,
     diagnosticsResourceName: string,
-    options?: DiagnosticsCreateOptionalParams
+    options?: DiagnosticsCreateOptionalParams,
   ): Promise<
     SimplePollerLike<
       OperationState<DiagnosticsCreateResponse>,
@@ -39,11 +37,9 @@ export interface Diagnostics {
     >
   >;
   /**
-   * Creates a diagnostic for the specific resource using solutionId and requiredInputs* from discovery
-   * solutions. <br/>Diagnostics are powerful solutions that access product resources or other relevant
-   * data and provide the root cause of the issue and the steps to address the issue.<br/><br/> <b>Note:
-   * </b> ‘requiredInputs’ from Discovery solutions response must be passed via ‘additionalParameters’ as
-   * an input to Diagnostics API.
+   * Creates a diagnostic for the specific resource using solutionId from discovery solutions.
+   * <br/>Diagnostics are powerful solutions that access product resources or other relevant data and
+   * provide the root cause of the issue and the steps to address the issue.<br/><br/>
    * @param scope scope = resourceUri of affected resource.<br/> For example:
    *              /subscriptions/0d0fcd2e-c4fd-4349-8497-200edb3923c6/resourcegroups/myresourceGroup/providers/Microsoft.KeyVault/vaults/test-keyvault-non-read
    *
@@ -53,7 +49,7 @@ export interface Diagnostics {
   beginCreateAndWait(
     scope: string,
     diagnosticsResourceName: string,
-    options?: DiagnosticsCreateOptionalParams
+    options?: DiagnosticsCreateOptionalParams,
   ): Promise<DiagnosticsCreateResponse>;
   /**
    * Get the diagnostics using the 'diagnosticsResourceName' you chose while creating the diagnostic.
@@ -66,6 +62,6 @@ export interface Diagnostics {
   get(
     scope: string,
     diagnosticsResourceName: string,
-    options?: DiagnosticsGetOptionalParams
+    options?: DiagnosticsGetOptionalParams,
   ): Promise<DiagnosticsGetResponse>;
 }
