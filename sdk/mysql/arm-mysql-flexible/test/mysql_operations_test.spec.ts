2537298b64632b9253671289ec24eb01f1ae6a8e
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-04-29 15:15:26 +0800

    update

--- sdk/mysql/arm-mysql-flexible/test/mysql_operations_test.spec.ts
+++ sdk/mysql/arm-mysql-flexible/test/mysql_operations_test.spec.ts
@@ -20,7 +20,7 @@ import { DefaultAzureCredential } from "@azure/identity";
 import { MySQLManagementFlexibleServerClient } from "../src/mySQLManagementFlexibleServerClient";
 
 const replaceableVariables: Record<string, string> = {
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
 };
 
 const recorderOptions: RecorderStartOptions = {
