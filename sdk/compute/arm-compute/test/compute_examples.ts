b84aa961a9c7f6a0bee981c13f818fbfe8c2a495
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-04-30 10:55:02 +0800

    update

--- sdk/compute/arm-compute/test/compute_examples.ts
+++ sdk/compute/arm-compute/test/compute_examples.ts
@@ -21,10 +21,7 @@ import { NetworkManagementClient, VirtualNetwork, Subnet, NetworkInterface } fro
 import { DefaultAzureCredential } from "@azure/identity";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
 };
 
 const recorderOptions: RecorderStartOptions = {
