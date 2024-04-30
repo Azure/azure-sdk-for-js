83daecab0c418f2a7b2dc27ee9ccabd56bf1daea
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-04-30 17:31:25 +0800

    update

--- sdk/datafactory/arm-datafactory/test/datafactory_examples.spec.ts
+++ sdk/datafactory/arm-datafactory/test/datafactory_examples.spec.ts
@@ -13,18 +13,16 @@ import {
   delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
-import { createTestCredential } from "@azure-tools/test-credential";
+import { NoOpCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
 import { Context } from "mocha";
 import { DataFactoryManagementClient } from "../src/dataFactoryManagementClient";
 import { Factory, PipelineResource } from "../src/models";
 import { dataFlow } from "../src/models/parameters";
+import { DefaultAzureCredential } from "@azure/identity";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -35,8 +33,15 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
+export function createTestCredential() {
+  return isPlaybackMode()
+    ? new NoOpCredential()
+    : new
+      DefaultAzureCredential()
+    ;
+}
 
-describe.skip("Datafactory test", () => {
+describe("Datafactory test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: DataFactoryManagementClient;
