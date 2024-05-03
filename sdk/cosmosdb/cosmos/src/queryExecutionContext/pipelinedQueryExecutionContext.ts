e9a68ed743129eba9735b88863e884874168da5c
Manik Khandelwal <113669638+topshot99@users.noreply.github.com>
2024-05-03 17:43:59 +0530

    Feature/cosmosdb/ru cap + vector search (#29570)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Manik Khandelwal <mkhandelwal@microsoft.com>

--- sdk/cosmosdb/cosmos/src/queryExecutionContext/pipelinedQueryExecutionContext.ts
+++ sdk/cosmosdb/cosmos/src/queryExecutionContext/pipelinedQueryExecutionContext.ts
@@ -18,6 +18,7 @@ import { SqlQuerySpec } from "./SqlQuerySpec";
 import { DiagnosticNodeInternal } from "../diagnostics/DiagnosticNodeInternal";
 import { RUCapPerOperationExceededErrorCode } from "../request/RUCapPerOperationExceededError";
 import { RUConsumedManager } from "../common";
+import { NonStreamingOrderByEndpointComponent } from "./EndpointComponent/NonStreamingOrderByEndpointComponent";
 
 /** @hidden */
 export class PipelinedQueryExecutionContext implements ExecutionContext {
@@ -38,7 +39,6 @@ export class PipelinedQueryExecutionContext implements ExecutionContext {
     if (this.pageSize === undefined) {
       this.pageSize = PipelinedQueryExecutionContext.DEFAULT_PAGE_SIZE;
     }
-
     // Pick between Nonstreaming and streaming endpoints
     const nonStreamingOrderBy = partitionedQueryExecutionInfo.queryInfo.hasNonStreamingOrderBy;
 
@@ -46,7 +46,7 @@ export class PipelinedQueryExecutionContext implements ExecutionContext {
     const sortOrders = partitionedQueryExecutionInfo.queryInfo.orderBy;
 
     if (nonStreamingOrderBy) {
-      //TODO: if non order by, throw error.
+      // TODO: if non order by, throw error.
       const distinctType = partitionedQueryExecutionInfo.queryInfo.distinctType;
       const context: ExecutionContext = new ParallelQueryExecutionContext(
         this.clientContext,
@@ -56,8 +56,8 @@ export class PipelinedQueryExecutionContext implements ExecutionContext {
         this.partitionedQueryExecutionInfo,
       );
 
-      if (distinctType == "None") {
-        // this.endpoint = new NonStreamingOrderByEndpointComponent();
+      if (distinctType === "None") {
+        this.endpoint = new NonStreamingOrderByEndpointComponent(context, sortOrders);
       } else {
         // this.endpoint = new NonStreamingOrderByDistinctEndpointComponent(context, partitionedQueryExecutionInfo.queryInfo);
       }
@@ -182,8 +182,10 @@ export class PipelinedQueryExecutionContext implements ExecutionContext {
           return { result: temp, headers: this.fetchMoreRespHeaders };
         }
       } else {
-        // append the result
-        this.fetchBuffer.push(item);
+        if (Object.keys(item).length !== 0) {
+          // discard empty JSON and append the result
+          this.fetchBuffer.push(item);
+        }
         if (this.fetchBuffer.length >= this.pageSize) {
           // fetched enough results
           const temp = this.fetchBuffer.slice(0, this.pageSize);
