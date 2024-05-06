12102d937ad8c83ccafa1ceddb3c888b1d973244
Chung Sheng Fu <Chung.Fu@microsoft.com>
2024-05-06 15:48:00 +0800

    Update liveness detection samples

--- sdk/face/ai-vision-face-rest/samples-dev/liveness.ts
+++ sdk/face/ai-vision-face-rest/samples-dev/liveness.ts
@@ -7,6 +7,7 @@ import createFaceClient, {
     DeleteLivenessSession200Response,
     GetLivenessSessionAuditEntries200Response,
     GetLivenessSessionResult200Response,
+    GetLivenessSessions200Response,
 } from '@azure-rest/ai-vision-face';
 
 /**
@@ -27,13 +28,33 @@ const pressAnyKeyToContinue = () => new Promise<void>(resolve => {
     });
 });
 
+const waitForLivenessRequest = async () => {
+    // Wait for request from client device.
+};
+
+const sendTokenToClientDevices = async (token: string) => {
+    // Send the token to client devices.
+};
+
+const waitForLivenessSessionComplete = async () => {
+    console.log('Please refer to https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness and use the mobile client SDK to perform liveness detection on your mobile application.');
+    console.log('Press any key to continue when you complete these steps to run sample to get session results...');
+    await pressAnyKeyToContinue();
+};
+
 async function main() {
+    // This sample follows the documentation: https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness
+    // We will follow the steps in https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness#orchestrate-the-liveness-solution to demo the sample code in app server.
+
     const endpoint = process.env['FACE_ENDPOINT'] ?? '<endpoint>';
     const apikey = process.env['FACE_APIKEY'] ?? '<apikey>';
     const credential = new AzureKeyCredential(apikey);
     const client = createFaceClient(endpoint, credential);
 
-    // Create session.
+    // 1. A client device will send a request to start liveness check to us.
+    await waitForLivenessRequest();
+
+    // 2.Send a request to Face API to create a liveness detection session.
     const createLivenessSessionResponse = await client.path('/detectLiveness/singleModal/sessions').post({
         body: {
             livenessOperationMode: 'Passive',
@@ -45,28 +66,30 @@ async function main() {
     console.log('Create liveness session:');
     console.log(JSON.stringify(createLivenessSessionResponse.body, null, 2));
 
-    const { sessionId } = createLivenessSessionResponse.body;
+    const { sessionId, authToken } = createLivenessSessionResponse.body;
 
+    // 3. Pass the AuthToken to client device.
+    // Client device will process the step 4, 5, 6 in the documentation 'Orchestrate the liveness solution'
+    await sendTokenToClientDevices(authToken);
+
+    // 7. wait for client device notifyus us that liveness session completed.
+    await waitForLivenessSessionComplete();
+
+    // 8. After client devices perform the action, we can get the result from the following APIs.
     // Get session results.
-    const getLivenessSessionResponse1 = await client.path('/detectLiveness/singleModal/sessions/{sessionId}', sessionId).get() as GetLivenessSessionResult200Response;
+    const getLivenessSessionResponse = await client.path('/detectLiveness/singleModal/sessions/{sessionId}', sessionId).get() as GetLivenessSessionResult200Response;
     console.log('Get liveness detection results:');
-    console.log(JSON.stringify(getLivenessSessionResponse1.body, null, 2));
-
-    console.log();
-    console.log('Please refer to https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness to download client SDK to run session starts and detcet liveness call.');
-    console.log('Press any key to continue when you complete these steps to run sample to get session results.');
-    await pressAnyKeyToContinue();
-    console.log();
-
-    // Get session results.
-    const getLivenessSessionResponse2 = await client.path('/detectLiveness/singleModal/sessions/{sessionId}', sessionId).get() as GetLivenessSessionResult200Response;
-    console.log('Get liveness detection results:');
-    console.log(JSON.stringify(getLivenessSessionResponse2.body, null, 2));
+    console.log(JSON.stringify(getLivenessSessionResponse.body, null, 2));
 
     // Get audit entries.
     const getAuditEntryResponse = await client.path('/detectLiveness/singleModal/sessions/{sessionId}/audit', sessionId).get() as GetLivenessSessionAuditEntries200Response;
     console.log('Get audit entries:');
     console.log(JSON.stringify(getAuditEntryResponse.body, null, 2));
+
+    // We can also list all liveness sessions of this face account.
+    const getLivenessSessionsResponse = await client.path('/detectLiveness/singleModal/sessions').get() as GetLivenessSessions200Response;
+    console.log('Get liveness sessions:');
+    console.log(JSON.stringify(getLivenessSessionsResponse.body, null, 2));
 
     // Delete session.
     const deleteLivenessSessionResponse = await client.path('/detectLiveness/singleModal/sessions/{sessionId}', sessionId).delete() as DeleteLivenessSession200Response;
--- sdk/face/ai-vision-face-rest/samples-dev/livenessWithVerifyWithImage.ts
+++ sdk/face/ai-vision-face-rest/samples-dev/livenessWithVerifyWithImage.ts
@@ -9,6 +9,7 @@ import createFaceClient, {
     GetLivenessWithVerifySessionResult200Response,
     CreateLivenessWithVerifySessionContentParametersPartDescriptor,
     CreateLivenessWithVerifySessionContentVerifyImagePartDescriptor,
+    GetLivenessWithVerifySessions200Response,
 } from '@azure-rest/ai-vision-face';
 import { readFileSync } from 'fs';
 
@@ -30,13 +31,33 @@ const pressAnyKeyToContinue = () => new Promise<void>(resolve => {
     });
 });
 
+const waitForLivenessRequest = async () => {
+    // Wait for request from client device.
+};
+
+const sendTokenToClientDevices = async (token: string) => {
+    // Send the token to client devices.
+};
+
+const waitForLivenessSessionComplete = async () => {
+    console.log('Please refer to https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness and use the mobile client SDK to perform liveness detection on your mobile application.');
+    console.log('Press any key to continue when you complete these steps to run sample to get session results...');
+    await pressAnyKeyToContinue();
+};
+
 async function main() {
+    // This sample follows the documentation: https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness
+    // We will follow the steps in https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness#orchestrate-the-liveness-solution to demo the sample code in app server.
+
     const endpoint = process.env['FACE_ENDPOINT'] ?? '<endpoint>';
     const apikey = process.env['FACE_APIKEY'] ?? '<apikey>';
     const credential = new AzureKeyCredential(apikey);
     const client = createFaceClient(endpoint, credential);
 
-    // Create session.
+    // 1. A client device will send a request to start liveness check to us.
+    await waitForLivenessRequest();
+
+    // 2.Send a request to Face API to create a liveness detection session.
     const createLivenessSessionResponse = await client.path('/detectLivenessWithVerify/singleModal/sessions').post({
         contentType: 'multipart/form-data',
         body: [
@@ -58,28 +79,30 @@ async function main() {
     console.log('Create liveness session:');
     console.log(JSON.stringify(createLivenessSessionResponse.body, null, 2));
 
-    const { sessionId } = createLivenessSessionResponse.body;
+    const { sessionId, authToken } = createLivenessSessionResponse.body;
 
+    // 3. Pass the AuthToken to client device.
+    // Client device will process the step 4, 5, 6 in the documentation 'Orchestrate the liveness solution'
+    await sendTokenToClientDevices(authToken);
+
+    // 7. wait for client device notifyus us that liveness session completed.
+    await waitForLivenessSessionComplete();
+
+    // 8. After client devices perform the action, we can get the result from the following APIs.
     // Get session results.
-    const getLivenessSessionResultResponse1 = await client.path('/detectLivenessWithVerify/singleModal/sessions/{sessionId}', sessionId).get() as GetLivenessWithVerifySessionResult200Response;
+    const getLivenessSessionResultResponse = await client.path('/detectLivenessWithVerify/singleModal/sessions/{sessionId}', sessionId).get() as GetLivenessWithVerifySessionResult200Response;
     console.log('Get liveness detection results:');
-    console.log(JSON.stringify(getLivenessSessionResultResponse1.body, null, 2));
-
-    console.log();
-    console.log('Please refer to https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/tutorials/liveness to download client SDK to run session starts and detcet liveness call.');
-    console.log('Press any key to continue when you complete these steps to run sample to get session results.');
-    await pressAnyKeyToContinue();
-    console.log();
-
-    // Get session results.
-    const getLivenessSessionResultResponse2 = await client.path('/detectLivenessWithVerify/singleModal/sessions/{sessionId}', sessionId).get() as GetLivenessWithVerifySessionResult200Response;
-    console.log('Get liveness detection results:');
-    console.log(JSON.stringify(getLivenessSessionResultResponse2.body, null, 2));
+    console.log(JSON.stringify(getLivenessSessionResultResponse.body, null, 2));
 
     // Get audit entries.
     const getAuditEntryResponse = await client.path('/detectLivenessWithVerify/singleModal/sessions/{sessionId}/audit', sessionId).get() as GetLivenessWithVerifySessionAuditEntries200Response;
     console.log('Get audit entries:');
     console.log(JSON.stringify(getAuditEntryResponse.body, null, 2));
+
+    // We can also list all liveness sessions of this face account.
+    const getLivenessSessionsResponse = await client.path('/detectLivenessWithVerify/singleModal/sessions').get() as GetLivenessWithVerifySessions200Response;
+    console.log('Get liveness sessions:');
+    console.log(JSON.stringify(getLivenessSessionsResponse.body, null, 2));
 
     // Delete session.
     const deleteLivenessSessionResponse = await client.path('/detectLivenessWithVerify/singleModal/sessions/{sessionId}', sessionId).delete() as DeleteLivenessWithVerifySession200Response;
