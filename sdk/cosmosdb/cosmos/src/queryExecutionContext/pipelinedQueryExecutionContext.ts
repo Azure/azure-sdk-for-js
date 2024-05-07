47b6a69e24b9efd1d8581fb5a232b0e4172bff52
Manik Khandelwal <mkhandelwal@microsoft.com>
2024-05-07 14:19:57 +0530

    update element addition to buffer

--- sdk/cosmosdb/cosmos/src/queryExecutionContext/pipelinedQueryExecutionContext.ts
+++ sdk/cosmosdb/cosmos/src/queryExecutionContext/pipelinedQueryExecutionContext.ts
@@ -209,10 +209,7 @@ export class PipelinedQueryExecutionContext implements ExecutionContext {
           return { result: temp, headers: this.fetchMoreRespHeaders };
         }
       } else {
-        if (Object.keys(item).length !== 0) {
-          // discard empty JSON and append the result
-          this.fetchBuffer.push(item);
-        }
+        this.fetchBuffer.push(item);
         if (this.fetchBuffer.length >= this.pageSize) {
           // fetched enough results
           const temp = this.fetchBuffer.slice(0, this.pageSize);
@@ -269,7 +266,11 @@ export class PipelinedQueryExecutionContext implements ExecutionContext {
           operationOptions && operationOptions.ruCapPerOperation
             ? operationOptions.ruCapPerOperation
             : Constants.NonStreamingQueryDefaultRUThreshold;
-        if (Object.keys(item).length !== 0) this.fetchBuffer.push(item);
+        if (typeof item !== "object") {
+          this.fetchBuffer.push(item);
+        } else if (Object.keys(item).length !== 0) {
+          this.fetchBuffer.push(item);
+        }
         if (this.fetchBuffer.length >= this.pageSize) {
           // fetched enough results
           const temp = this.fetchBuffer.slice(0, this.pageSize);
