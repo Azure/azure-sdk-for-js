eebfde9b4cd69e86bfcfde21ae158fa95830ef93
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-05-07 15:28:47 +0800

    update

--- sdk/selfhelp/arm-selfhelp/test/selfhelp_operations_test.spec.ts
+++ sdk/selfhelp/arm-selfhelp/test/selfhelp_operations_test.spec.ts
@@ -13,16 +13,14 @@ import {
   delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
-import { createTestCredential } from "@azure-tools/test-credential";
+import { NoOpCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
 import { Context } from "mocha";
 import { HelpRP } from "../src/helpRP";
+import { DefaultAzureCredential } from "@azure/identity";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -33,7 +31,15 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe.skip("help test", () => {
+export function createTestCredential() {
+  return isPlaybackMode()
+    ? new NoOpCredential()
+    : new
+      DefaultAzureCredential()
+    ;
+}
+
+describe("help test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: HelpRP;
--- sdk/support/arm-support/test/support_operations_test.spec.ts
+++ sdk/support/arm-support/test/support_operations_test.spec.ts
@@ -13,15 +13,13 @@ import {
   delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
-import { createTestCredential } from "@azure-tools/test-credential";
+import { NoOpCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
 import { Context } from "mocha";
 import { MicrosoftSupport } from "../src/microsoftSupport";
+import { DefaultAzureCredential } from "@azure/identity";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
   SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
 };
 
@@ -33,7 +31,15 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe.skip("MicrosoftSupport test", () => {
+export function createTestCredential() {
+  return isPlaybackMode()
+    ? new NoOpCredential()
+    : new
+      DefaultAzureCredential()
+    ;
+}
+
+describe("MicrosoftSupport test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: MicrosoftSupport;
