28a045273a2af9073f4987564c6b11e5692f6802
Aman Rao <amanrao@microsoft.com>
2024-05-08 17:56:53 +0530

    Update Feed option field metadata

--- sdk/cosmosdb/cosmos/src/request/FeedOptions.ts
+++ sdk/cosmosdb/cosmos/src/request/FeedOptions.ts
@@ -114,7 +114,7 @@ export interface FeedOptions extends SharedOptions {
    */
   populateIndexMetrics?: boolean;
   /**
-   *
+   * Custom max buffer size for storing final results for nonStreamingOrderBy queries. If query has top/offset+limit, this is ignored.
    */
   vectorSearchBufferSize?: number;
 }
