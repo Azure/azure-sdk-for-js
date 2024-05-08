6f24123aa35846320ab25cd83762b14f838451a3
Vinothini Dharmaraj <v-vdharmaraj@microsoft.com>
2024-05-07 23:10:05 -0700

    fixing the merge change

--- sdk/communication/communication-call-automation/src/callAutomationClient.ts
+++ sdk/communication/communication-call-automation/src/callAutomationClient.ts
@@ -234,7 +234,7 @@ export class CallAutomationClient {
       callbackUri: callbackUrl,
       operationContext: options.operationContext,
       callIntelligenceOptions: options.callIntelligenceOptions,
-      mediaStreamingOptions: options.mediaStreamingOptions,
+      transcriptionOptions: options.transcriptionOptions,
       sourceCallerIdNumber: PhoneNumberIdentifierModelConverter(options.sourceCallIdNumber),
       sourceDisplayName: options.sourceDisplayName,
     };
