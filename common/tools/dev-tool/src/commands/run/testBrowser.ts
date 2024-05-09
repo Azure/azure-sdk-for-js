947a1d08a7963c750e03eefda364bfc99ca36622
Harsha Nalluru <sanallur@microsoft.com>
2024-05-08 21:31:02 -0700

    [test-credential] DAC and browser relay changes cherry-pick to v1 test-credential (#29641)
    ### Packages impacted by this PR
    `@azure-tools/test-credential@1.1.0`
    
    ### What's happening?
    
    `test-credential 2.x` is for `test-recorder 4.x`
    `test-credential 1.x` is for `test-recorder 3.x`
    
    The only difference between test-credential 2.x and test-credential 1.x
    is that it consumes the new env support with vitest
    [azure-sdk-for-js/sdk/test-utils/test-credential/CHANGELOG.md at main ·
    Azure/azure-sdk-for-js
    (github.com)](https://github.com/Azure/azure-sdk-for-js/blob/main/sdk/test-utils/test-credential/CHANGELOG.md#200-2024-04-09)
    
    New changes went in `test-credential v2` as part of the spring grove
    effort as part of #29577 and #29616.
    Cherrypicking those changes into `test-credential v1` is necessary to
    address the spring grove efforts. This PR aims to accomplish that for
    1.1.0 version of `@azure-tools/test-credential`.
    
    Next step is to publish `@azure-tools/test-credential@1.1.0` package so
    that sdks relying on recorder v3 can leverage the package.
    
    ---------
    
    Co-authored-by: Timo van Veenendaal <me@timo.nz>
    Co-authored-by: Timo van Veenendaal <timov@microsoft.com>

--- common/tools/dev-tool/src/commands/run/testBrowser.ts
+++ common/tools/dev-tool/src/commands/run/testBrowser.ts
@@ -3,19 +3,36 @@
 
 import { leafCommand, makeCommandInfo } from "../../framework/command";
 import { isModuleProject } from "../../util/resolveProject";
+import { shouldStartRelay, startRelayServer } from "../../util/browserRelayServer";
 import { runTestsWithProxyTool } from "../../util/testUtils";
 
 export const commandInfo = makeCommandInfo(
   "test:browser",
   "runs the browser tests using karma with the default and the provided options; starts the proxy-tool in record and playback modes",
+  {
+    "relay-server": {
+      description: "Start the relay server for browser credentials",
+      kind: "boolean",
+      default: true,
+    },
+  },
 );
 
 export default leafCommand(commandInfo, async (options) => {
   const karmaArgs = options["--"]?.length
     ? options["--"].join(" ")
     : `${(await isModuleProject()) ? "karma.conf.cjs" : ""} --single-run`;
-  return runTestsWithProxyTool({
-    command: `karma start ${karmaArgs}`,
-    name: "browser-tests",
-  });
+
+  const stopRelay =
+    options["relay-server"] && (await shouldStartRelay()) ? startRelayServer() : undefined;
+
+  try {
+    const result = await runTestsWithProxyTool({
+      command: `karma start ${karmaArgs}`,
+      name: "browser-tests",
+    });
+    return result;
+  } finally {
+    stopRelay?.();
+  }
 });
