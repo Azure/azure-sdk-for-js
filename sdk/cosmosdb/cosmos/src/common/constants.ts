d69199670b41d2bf8db5e4fd6e80fe7ac7c382ee
Manik Khandelwal <113669638+topshot99@users.noreply.github.com>
2024-05-16 20:28:41 +0530

    Preview feat cosmosdb ru cap+vector search (#29723)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Manik Khandelwal <mkhandelwal@microsoft.com>

--- sdk/cosmosdb/cosmos/src/common/constants.ts
+++ sdk/cosmosdb/cosmos/src/common/constants.ts
@@ -201,7 +201,7 @@ export const Constants = {
   AzureNamespace: "Azure.Cosmos",
   AzurePackageName: "@azure/cosmos",
   SDKName: "azure-cosmos-js",
-  SDKVersion: "4.0.1-beta.2",
+  SDKVersion: "4.0.1-beta.3",
 
   // Diagnostics
   CosmosDbDiagnosticLevelEnvVarName: "AZURE_COSMOSDB_DIAGNOSTICS_LEVEL",
