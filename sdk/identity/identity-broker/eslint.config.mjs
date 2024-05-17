a86f9c2b14cca55e909be8f1c26cb3e324247c2c
Jeremy Meng <yumeng@microsoft.com>
2024-05-17 10:30:19 -0700

    update identity-broker config

--- sdk/identity/identity-broker/eslint.config.mjs
+++ sdk/identity/identity-broker/eslint.config.mjs
@@ -1,7 +1,7 @@
 import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default [
-  ...azsdkEslint.default.configs.recommended,
+  ...azsdkEslint.configs.recommended,
   {
     rules: {
       "@azure/azure-sdk/ts-package-json-module": "warn",
