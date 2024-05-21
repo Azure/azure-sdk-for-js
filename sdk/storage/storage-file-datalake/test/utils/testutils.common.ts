ac8ea6ef31a77c8335c6dba94e5c0d3d86bec3db
Harsha Nalluru <sanallur@microsoft.com>
2024-05-21 07:38:55 +0000

    merge conflixt

--- sdk/storage/storage-file-datalake/test/utils/testutils.common.ts
+++ sdk/storage/storage-file-datalake/test/utils/testutils.common.ts
@@ -31,10 +31,10 @@ function getUriSanitizerForQueryParam(paramName: string) {
 
 const mockAccountName = "fakestorageaccount";
 const mockAccountKey = "aaaaa";
+const mockTenantId = "00000000-0000-0000-0000-000000000000&";
 const mockSas =
   "?sv=2015-04-05&ss=bfqt&srt=sco&sp=rwdlacup&se=2023-01-31T18%3A51%3A40.0000000Z&sig=foobar";
 
-const mockTenantId = "00000000-0000-0000-0000-000000000000";
 const sasParams = ["se", "sig", "sip", "sp", "spr", "srt", "ss", "sr", "st", "sv"];
 if (isBrowser()) {
   sasParams.push("_");
@@ -65,8 +65,8 @@ export const recorderEnvSetup: RecorderStartOptions = {
     uriSanitizers,
   },
   removeCentralSanitizers: [
-    "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
-  ],
+    "AZSDK3493"  // .name in the body is not a secret and is created as part of tests
+  ]
 };
 
 /**
