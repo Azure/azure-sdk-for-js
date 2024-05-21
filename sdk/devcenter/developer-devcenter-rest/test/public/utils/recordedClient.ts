3e7aa862cce6c0a60f71d249901b8165ec2e779f
Lei Tian <leti@microsoft.com>
2024-05-21 13:18:12 -0700

    bypass two unnecessary sanitizers

--- sdk/devcenter/developer-devcenter-rest/test/public/utils/recordedClient.ts
+++ sdk/devcenter/developer-devcenter-rest/test/public/utils/recordedClient.ts
@@ -27,7 +27,12 @@ const envSetupForPlayback: Record<string, string> = {
 };
 
 const recorderEnvSetup: RecorderStartOptions = {
-  envSetupForPlayback,
+  envSetupForPlayback: envSetupForPlayback,
+  //  https://github.com/Azure/azure-sdk-tools/blob/main/tools/test-proxy/Azure.Sdk.Tools.TestProxy/Common/SanitizerDictionary.cs
+  removeCentralSanitizers: [
+    "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
+    "AZSDK2030"
+  ]
 };
 
 /**
