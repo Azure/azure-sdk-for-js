1f56d161892e8b9f580bf45ac9bc3d9ec189a089
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-05-22 16:02:55 +0800

    [mgmt] maintenance release (#29664)
    https://github.com/Azure/sdk-release-request/issues/5160

--- sdk/maintenance/arm-maintenance/samples/v1-beta/javascript/updatesListParentSample.js
+++ sdk/maintenance/arm-maintenance/samples/v1-beta/javascript/updatesListParentSample.js
@@ -16,7 +16,7 @@ require("dotenv").config();
  * This sample demonstrates how to Get updates to resources.
  *
  * @summary Get updates to resources.
- * x-ms-original-file: specification/maintenance/resource-manager/Microsoft.Maintenance/stable/2023-04-01/examples/Updates_ListParent.json
+ * x-ms-original-file: specification/maintenance/resource-manager/Microsoft.Maintenance/preview/2023-10-01-preview/examples/Updates_ListParent.json
  */
 async function updatesListParent() {
   const subscriptionId =
@@ -36,7 +36,7 @@ async function updatesListParent() {
     resourceParentType,
     resourceParentName,
     resourceType,
-    resourceName
+    resourceName,
   )) {
     resArray.push(item);
   }
