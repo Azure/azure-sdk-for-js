bec6e474a0f56847ea2ca4d998220a50f2b82bbe
azure-sdk <azuresdk@microsoft.com>
2024-05-24 06:59:03 +0000

    Post release automated changes for azure-arm-oracledatabase

--- sdk/oracledatabase/arm-oracledatabase/src/oracleDatabaseManagementClient.ts
+++ sdk/oracledatabase/arm-oracledatabase/src/oracleDatabaseManagementClient.ts
@@ -84,7 +84,7 @@ export class OracleDatabaseManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-oracledatabase/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-oracledatabase/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
