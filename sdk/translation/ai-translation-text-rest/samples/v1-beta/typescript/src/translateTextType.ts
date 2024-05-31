16b7aa1474a2166b7fa2737434716f181420497f
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-05-31 13:46:27 +0800

    merge main

--- sdk/translation/ai-translation-text-rest/samples/v1-beta/typescript/src/translateTextType.ts
+++ sdk/translation/ai-translation-text-rest/samples/v1-beta/typescript/src/translateTextType.ts
@@ -23,7 +23,7 @@ export async function main() {
 
   const translateCedential: TranslatorCredential = {
     key: apiKey,
-    region
+    region,
   };
   const translationClient = TextTranslationClient(endpoint, translateCedential);
 
@@ -34,7 +34,8 @@ export async function main() {
       to: "cs",
       from: "en",
       textType: "html",
-  }});
+    },
+  });
 
   if (isUnexpected(translateResponse)) {
     throw translateResponse.body.error;
@@ -43,7 +44,7 @@ export async function main() {
   const translations = translateResponse.body;
   for (const translation of translations) {
     console.log(
-      `Text was translated to: '${translation?.translations[0]?.to}' and the result is: '${translation?.translations[0]?.text}'.`
+      `Text was translated to: '${translation?.translations[0]?.to}' and the result is: '${translation?.translations[0]?.text}'.`,
     );
   }
 }
