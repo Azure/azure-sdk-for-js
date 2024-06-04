0a48870f8db2aaa378f94137a47a4585577f1410
Sarangan Rajamanickam <sarajama@microsoft.com>
2024-06-03 23:11:12 -0700

    Add test

--- sdk/eventgrid/eventgrid-system-events/test/public/sample.spec.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-import { Suite } from "mocha";
-
-describe("Sample test", function (this: Suite) {
-  it("sample test", async () => {
-    // Test code
-  });
-});
