993647651e4cef4d169084bdc49199ccde6d4ecd
Karishma Ghiya <kaghiya@microsoft.com>
2024-06-04 16:02:35 -0700

    update the code

--- sdk/identity/identity/samples/sample.js
+++ sdk/identity/identity/samples/sample.js
@@ -1,16 +1,15 @@
-const { AzurePipelinesCredential } = await import("@azure/identity");
-const { SecretClient } = await import("@azure/keyvault-secrets");
+const { AzurePipelinesCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
 
 async function main(){
     const clientId = "7960e60e-aabb-47e7-9ac5-7145dd8157a4";
     const tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
     const serviceConnectionId = "59b37403-61d1-4653-bcd2-0e9f58d68f32";
-    const systemAccessToken = process.env.SYSTEM_ACCESSTOKEN;
+    //const systemAccessToken = process.env.SYSTEM_ACCESSTOKEN;
     const credential = new AzurePipelinesCredential(
       tenantId,
       clientId,
-      serviceConnectionId,
-      systemAccessToken
+      serviceConnectionId
     );  
   const client = new SecretClient("https://azuresdk-testsecrets2.vault.azure.net/", credential);
   const secretValue = await client.getSecret("secretKey");
