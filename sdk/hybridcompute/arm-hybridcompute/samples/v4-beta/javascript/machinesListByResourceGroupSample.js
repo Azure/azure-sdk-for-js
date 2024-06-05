c22412fa3a3ef780bfccb31358c958c0afd37baa
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-06-05 17:03:33 +0800

    [mgmt] hybridcompute release (#29908)
    https://github.com/Azure/sdk-release-request/issues/5229

--- sdk/hybridcompute/arm-hybridcompute/samples/v4-beta/javascript/machinesListByResourceGroupSample.js
+++ sdk/hybridcompute/arm-hybridcompute/samples/v4-beta/javascript/machinesListByResourceGroupSample.js
@@ -16,7 +16,7 @@ require("dotenv").config();
  * This sample demonstrates how to Lists all the hybrid machines in the specified resource group. Use the nextLink property in the response to get the next page of hybrid machines.
  *
  * @summary Lists all the hybrid machines in the specified resource group. Use the nextLink property in the response to get the next page of hybrid machines.
- * x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2023-10-03-preview/examples/machine/Machines_ListByResourceGroup.json
+ * x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-03-31-preview/examples/machine/Machines_ListByResourceGroup.json
  */
 async function listMachinesByResourceGroup() {
   const subscriptionId = process.env["HYBRIDCOMPUTE_SUBSCRIPTION_ID"] || "{subscription-id}";
