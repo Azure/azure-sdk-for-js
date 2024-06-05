1457d73c53d69e4703f4c82ecc0749509731b416
Karishma Ghiya <kaghiya@microsoft.com>
2024-06-04 17:31:50 -0700

    update samples and TS guide

--- sdk/identity/identity/samples/azurePipelinesCredential/sample.js
+++ sdk/identity/identity/samples/azurePipelinesCredential/sample.js
@@ -1,15 +1,17 @@
 const { AzurePipelinesCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
-async function main(){
+async function main(systemAccessToken){
+  console.log("systemaccesstoken =",systemAccessToken)
     const clientId = "CLIENT_ID";
     const tenantId = "TENANT_ID";
     const serviceConnectionId = "SERVICE_CONNECTION_ID";
-    //const systemAccessToken = process.env.SYSTEM_ACCESSTOKEN;
+    const systemAccessToken = process.env.SYSTEM_ACCESSTOKEN;
     const credential = new AzurePipelinesCredential(
       tenantId,
       clientId,
-      serviceConnectionId
+      serviceConnectionId,
+      systemAccessToken
     );  
   const client = new SecretClient("https://KEYVAULT_NAME.vault.azure.net/", credential);
   const secretValue = await client.getSecret("secretKey");
