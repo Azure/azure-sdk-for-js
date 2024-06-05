8dc2505a172b370785ef747b54e87cc879d066af
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-06-05 10:04:45 -0700

    [OpenAI] Add migration guide and update samples (#29789)
    ### Packages impacted by this PR
    @azure/openai
    
    ### Issues associated with this PR
    N/A
    
    ### Describe the problem that is addressed by this PR
    N/A
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    N/A
    
    ### Are there test cases added in this PR? _(If not, why?)_
    N/A
    
    ### Provide a list of related PRs _(if any)_
    N/A
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [x] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

---
+++ sdk/openai/openai/samples/v2-beta/javascript/completions.js
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * Demonstrates how to get completions for the provided prompt.
+ *
+ * @summary get completions.
+ */
+
+const { AzureOpenAI } = require("openai");
+const { DefaultAzureCredential, getBearerTokenProvider } = require("@azure/identity");
+
+// Set AZURE_OPENAI_ENDPOINT to the endpoint of your
+// OpenAI resource. You can find this in the Azure portal.
+// Load the .env file if it exists
+require("dotenv/config");
+
+const prompt = ["What is Azure OpenAI?"];
+
+async function main() {
+  console.log("== Get completions Sample ==");
+
+  const scope = "https://cognitiveservices.azure.com/.default";
+  const azureADTokenProvider = getBearerTokenProvider(new DefaultAzureCredential(), scope);
+  const deployment = "text-davinci-003";
+  const apiVersion = "2024-04-01-preview";
+  const client = new AzureOpenAI({ azureADTokenProvider, deployment, apiVersion });
+  const result = await client.completions.create({ prompt, model: "", max_tokens: 128 });
+
+  for (const choice of result.choices) {
+    console.log(choice.text);
+  }
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
+
+module.exports = { main };
