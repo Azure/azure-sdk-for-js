c854f5e67d2123428501cffaaaa5d19eab00f981
KarishmaGhiya <kghiya8@gmail.com>
2024-06-10 10:55:35 -0700

    Update sdk/identity/identity/src/credentials/azurePipelinesCredential.ts
    Co-authored-by: Scott Addie <10702007+scottaddie@users.noreply.github.com>

--- sdk/identity/identity/src/credentials/azurePipelinesCredential.ts
+++ sdk/identity/identity/src/credentials/azurePipelinesCredential.ts
@@ -26,7 +26,7 @@ export class AzurePipelinesCredential implements TokenCredential {
    * AzurePipelinesCredential supports Federated Identity on Azure Pipelines through Service Connections.
    * @param tenantId - tenantId associated with the service connection
    * @param clientId - clientId associated with the service connection
-   * @param serviceConnectionId - id for the service connection, as found in the querystring's resourceId key
+   * @param serviceConnectionId - Unique ID for the service connection, as found in the querystring's resourceId key
    * @param systemAccessToken - The pipeline's <see href="https://learn.microsoft.com/azure/devops/pipelines/build/variables?view=azure-devops%26tabs=yaml#systemaccesstoken">System.AccessToken</see> value.
    * @param options - The identity client options to use for authentication.
    */
