626d5b77b12b3f98740b30511c2be28a52e6753a
Minh-Anh Phan <minhanhphan@microsoft.com>
2024-06-12 18:03:08 +0000

    Update realistic timeout

--- vitest.shared.config.ts
+++ vitest.shared.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    testTimeout: 60000,
+    testTimeout: 18000,
     reporters: ["basic", "junit"],
     outputFile: {
       junit: "test-results.xml",
