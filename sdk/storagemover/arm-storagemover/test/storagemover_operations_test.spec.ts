3fd0e3a27c6a1be2f9e6d91f5f1c66e1f3969645
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-06-14 11:12:00 +0800

    update

--- sdk/storagemover/arm-storagemover/test/storagemover_operations_test.spec.ts
+++ sdk/storagemover/arm-storagemover/test/storagemover_operations_test.spec.ts
@@ -57,7 +57,7 @@ describe("storageMover test", () => {
     // This is an example of how the environment variables are used
     const credential = createTestCredential();
     client = new StorageMoverClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus2";
+    location = "eastus2euap";
     resourceGroup = "myjstest";
     storageMoverName = "storageMoverName";
     agentName = "testagent";
