7c191a69e3788958fef2155b8c19c4ab9e578908
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-06-19 11:23:26 +0800

    update

--- sdk/notificationhubs/arm-notificationhubs/test/notificationhubs_examples.ts
+++ sdk/notificationhubs/arm-notificationhubs/test/notificationhubs_examples.ts
@@ -19,14 +19,15 @@ import { Context } from "mocha";
 import { NotificationHubsManagementClient } from "../src/notificationHubsManagementClient";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
+  SUBSCRIPTION_ID: "azure_subscription_id"
 };
 
 const recorderOptions: RecorderStartOptions = {
-  envSetupForPlayback: replaceableVariables
+  envSetupForPlayback: replaceableVariables,
+  removeCentralSanitizers: [
+    "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
+    "AZSDK3430", // .id in the body is not a secret and is listed below in the beforeEach section
+  ],
 };
 
 export const testPollingOptions = {
--- sdk/redis/arm-rediscache/test/redis_examples.ts
+++ sdk/redis/arm-rediscache/test/redis_examples.ts
@@ -20,9 +20,6 @@ import { RedisManagementClient } from "../src/redisManagementClient";
 import { NetworkManagementClient, VirtualNetwork } from "@azure/arm-network";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
   SUBSCRIPTION_ID: "azure_subscription_id"
 };
 
@@ -39,7 +36,11 @@ const recorderOptions: RecorderStartOptions = {
       value: `fakeKey`,
       target: `[a-z0-9_A-z=]{40,100}`
     }]
-  }
+  },
+  removeCentralSanitizers: [
+    "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
+    "AZSDK3430", // .id in the body is not a secret and is listed below in the beforeEach section
+  ],
 };
 
 export const testPollingOptions = {
