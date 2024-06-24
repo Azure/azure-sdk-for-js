1918e400c678fd9c75ef8c1aaaa6371a6f3446bc
Maor Leger <maorleger@microsoft.com>
2024-06-24 10:44:25 -0700

    make tracerProvider node-browser compat

--- sdk/instrumentation/opentelemetry-instrumentation-azure-sdk/test/public/util/tracerProvider.ts
+++ sdk/instrumentation/opentelemetry-instrumentation-azure-sdk/test/public/util/tracerProvider.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { BasicTracerProvider, NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 
-export const tracerProvider = new NodeTracerProvider();
+import { isNodeLike } from "@azure/core-util";
+
+export const tracerProvider = isNodeLike ? new NodeTracerProvider() : new BasicTracerProvider();
