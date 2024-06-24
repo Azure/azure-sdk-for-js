4c7b848c7f3c82ce5f715b35c75466af74b3a305
azure-sdk <azuresdk@microsoft.com>
2024-06-24 09:51:38 +0000

    Post release automated changes for azure-arm-avs

--- sdk/avs/arm-avs/src/azureVMwareSolutionAPI.ts
+++ sdk/avs/arm-avs/src/azureVMwareSolutionAPI.ts
@@ -86,7 +86,7 @@ export class AzureVMwareSolutionAPI extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-avs/5.0.0`;
+    const packageDetails = `azsdk-js-arm-avs/5.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
