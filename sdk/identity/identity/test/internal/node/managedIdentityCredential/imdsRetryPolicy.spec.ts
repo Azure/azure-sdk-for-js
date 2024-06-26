99bca062db723fc627c51633377a0d7a71807fdd
Maor Leger <maorleger@microsoft.com>
2024-06-26 14:13:59 -0700

    update tests and reduce timeout

--- sdk/identity/identity/test/internal/node/managedIdentityCredential/imdsRetryPolicy.spec.ts
+++ sdk/identity/identity/test/internal/node/managedIdentityCredential/imdsRetryPolicy.spec.ts
@@ -1,21 +1,27 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { imdsRetryPolicy } from "../../../../src/credentials/managedIdentityCredential/imdsRetryPolicy";
-import { PipelineRequest, SendRequest, createHttpHeaders } from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  RestError,
+  SendRequest,
+  createHttpHeaders,
+} from "@azure/core-rest-pipeline";
+
 import { MSIConfiguration } from "../../../../src/credentials/managedIdentityCredential/models";
 import { assert } from "@azure-tools/test-utils";
+import { imdsRetryPolicy } from "../../../../src/credentials/managedIdentityCredential/imdsRetryPolicy";
 
 describe("imdsRetryPolicy", () => {
   const mockRetryConfig: MSIConfiguration["retryConfig"] = {
     maxRetries: 3,
-    startDelayInMs: 100,
-    intervalIncrement: 1000,
+    startDelayInMs: 1,
+    intervalIncrement: 1,
   };
 
   it("should retry on 404 errors", async () => {
     const policy = imdsRetryPolicy(mockRetryConfig);
-    const request: PipelineRequest = {
+    const pipelineRequest: PipelineRequest = {
       url: "https://example.com",
       method: "GET",
       headers: createHttpHeaders(),
@@ -28,59 +34,57 @@ describe("imdsRetryPolicy", () => {
 
     let sendRequestCount = 0;
 
-    const sendRequest: SendRequest = async (req) => {
+    const sendRequest: SendRequest = async (request) => {
       sendRequestCount++;
       if (sendRequestCount === 1) {
         // Simulate a 404 error on the first request
-        return {
+        const response = {
           status: 404,
           headers: createHttpHeaders(),
-          request: req,
-          body: undefined,
+          request,
         };
+        throw new RestError("Not found", { statusCode: 404, request, response });
       } else {
         // Return a successful response on subsequent requests
         return {
           status: 200,
           headers: createHttpHeaders(),
-          request: req,
+          request: request,
           body: undefined,
         };
       }
     };
 
-    await policy.sendRequest(request, sendRequest);
+    await policy.sendRequest(pipelineRequest, sendRequest);
 
     assert.strictEqual(sendRequestCount, 2); // Should retry once
   });
 
   it("should respect the maximum number of retries", async () => {
     const policy = imdsRetryPolicy(mockRetryConfig);
-    const request: PipelineRequest = {
+    const pipelineRequest: PipelineRequest = {
       url: "https://example.com",
       method: "GET",
       headers: createHttpHeaders(),
-      body: undefined,
-      abortSignal: undefined,
       timeout: 100,
       requestId: "test",
       withCredentials: false,
     };
 
     let sendRequestCount = 0;
-    const sendRequest: SendRequest = async (req) => {
+    const sendRequest: SendRequest = async (request) => {
       sendRequestCount++;
       // Simulate a 404 error on every request
-      // Simulate a 404 error on the first request
-      return {
+      const response = {
         status: 404,
         headers: createHttpHeaders(),
-        request: req,
+        request,
         body: undefined,
       };
+      throw new RestError("Not found", { statusCode: 404, request, response });
     };
 
-    await assert.isRejected(policy.sendRequest(request, sendRequest), "Not Found");
+    await assert.isRejected(policy.sendRequest(pipelineRequest, sendRequest), "Not found");
     assert.strictEqual(sendRequestCount, mockRetryConfig.maxRetries + 1); // Should retry the maximum number of times
   });
 });
