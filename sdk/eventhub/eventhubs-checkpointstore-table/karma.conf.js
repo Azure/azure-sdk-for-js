18b9c20d2f0b8a2a9aa380b9d18bef3885308a1f
sharath <29162020+tnfssc@users.noreply.github.com>
2024-06-28 10:40:28 +0530

    fix(eventhub): replace `polyfill.io` with Cloudflare equivalent

--- sdk/eventhub/eventhubs-checkpointstore-table/karma.conf.js
+++ sdk/eventhub/eventhubs-checkpointstore-table/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     files: [
       // Uncomment the cdn link below for the polyfill service to support IE11 missing features
       // Promise,String.prototype.startsWith,String.prototype.endsWith,String.prototype.repeat,String.prototype.includes,Array.prototype.includes,Object.keys
-      // "https://cdn.polyfill.io/v2/polyfill.js?features=Symbol,Promise,String.prototype.startsWith,String.prototype.endsWith,String.prototype.repeat,String.prototype.includes,Array.prototype.includes,Object.keys|always",
+      // "https://cdnjs.cloudflare.com/polyfill/v2/polyfill.js?features=Symbol,Promise,String.prototype.startsWith,String.prototype.endsWith,String.prototype.repeat,String.prototype.includes,Array.prototype.includes,Object.keys|always",
       "dist-test/index.browser.js",
       { pattern: "dist-test/index.browser.js.map", type: "html", included: false, served: true },
     ],
