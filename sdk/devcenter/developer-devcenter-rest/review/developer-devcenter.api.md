331158a7e36b3ebc6c7ddabd4296853bbc352c8a
Mary Gao <yanmeigao1210@gmail.com>
2024-06-28 13:14:09 +0800

    Update the core-lro to v3 stable

--- sdk/devcenter/developer-devcenter-rest/review/developer-devcenter.api.md
+++ sdk/devcenter/developer-devcenter-rest/review/developer-devcenter.api.md
@@ -1540,6 +1540,7 @@ export interface SimplePollerLike<TState extends OperationState<TResult>, TResul
     getOperationState(): TState;
     getResult(): TResult | undefined;
     isDone(): boolean;
+    // @deprecated
     isStopped(): boolean;
     onProgress(callback: (state: TState) => void): CancelOnProgress;
     poll(options?: {
--- sdk/devcenter/developer-devcenter-rest/src/pollingHelper.ts
+++ sdk/devcenter/developer-devcenter-rest/src/pollingHelper.ts
@@ -6,7 +6,7 @@ import { AbortSignalLike } from "@azure/abort-controller";
 import {
   CancelOnProgress,
   CreateHttpPollerOptions,
-  LongRunningOperation,
+  RunningOperation,
   OperationResponse,
   OperationState,
   createHttpPoller,
@@ -46,10 +46,6 @@ export interface SimplePollerLike<TState extends OperationState<TResult>, TResul
    * Returns true if the poller has finished polling.
    */
   isDone(): boolean;
-  /**
-   * Returns true if the poller is stopped.
-   */
-  isStopped(): boolean;
   /**
    * Returns the state of the operation.
    */
@@ -100,6 +96,12 @@ export interface SimplePollerLike<TState extends OperationState<TResult>, TResul
    * @deprecated Use abortSignal to stop polling instead.
    */
   stopPolling(): void;
+
+  /**
+   * Returns true if the poller is stopped.
+   * @deprecated Use abortSignal status to track this instead.
+   */
+  isStopped(): boolean;
 }
 
 /**
@@ -171,14 +173,14 @@ export async function getLongRunningPoller<TResult extends HttpResponse>(
   options: CreateHttpPollerOptions<TResult, OperationState<TResult>> = {},
 ): Promise<SimplePollerLike<OperationState<TResult>, TResult>> {
   const abortController = new AbortController();
-  const poller: LongRunningOperation<TResult> = {
+  const poller: RunningOperation<TResult> = {
     sendInitialRequest: async () => {
       // In the case of Rest Clients we are building the LRO poller object from a response that's the reason
       // we are not triggering the initial request here, just extracting the information from the
       // response we were provided.
       return getLroResponse(initialResponse);
     },
-    sendPollRequest: async (path, sendPollRequestOptions?: { abortSignal?: AbortSignalLike }) => {
+    sendPollRequest: async (path: string, pollOptions?: { abortSignal?: AbortSignalLike }) => {
       // This is the callback that is going to be called to poll the service
       // to get the latest status. We use the client provided and the polling path
       // which is an opaque URL provided by caller, the service sends this in one of the following headers: operation-location, azure-asyncoperation or location
@@ -186,7 +188,7 @@ export async function getLongRunningPoller<TResult extends HttpResponse>(
       function abortListener(): void {
         abortController.abort();
       }
-      const inputAbortSignal = sendPollRequestOptions?.abortSignal;
+      const inputAbortSignal = pollOptions?.abortSignal;
       const abortSignal = abortController.signal;
       if (inputAbortSignal?.aborted) {
         abortController.abort();
@@ -216,7 +218,7 @@ export async function getLongRunningPoller<TResult extends HttpResponse>(
       return httpPoller.isDone;
     },
     isStopped() {
-      return httpPoller.isStopped;
+      return abortController.signal.aborted;
     },
     getOperationState() {
       if (!httpPoller.operationState) {
