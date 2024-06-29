5facca37120b3ae776c0af5826d795f2cd12c9a9
Glenn Harper <glharper@microsoft.com>
2024-06-28 20:08:41 -0700

    match dummy env var format for AZURE_ENDPOINT

--- sdk/ai/ai-inference-rest/test/public/utils/recordedClient.ts
+++ sdk/ai/ai-inference-rest/test/public/utils/recordedClient.ts
@@ -12,7 +12,7 @@ import { ClientOptions } from "@azure-rest/core-client";
 import createClient, { ModelClient } from "../../../src/index.js";
 
 const envSetupForPlayback: Record<string, string> = {
-  AZURE_ENDPOINT: "https://endpoint",
+  AZURE_ENDPOINT: "https://endpoint.openai.azure.com/openai/deployments/model/",
   SUBSCRIPTION_ID: "azure_subscription_id",
   AZURE_CLIENT_SECRET: "azureclientsecret"
 };
