3cb69eeac69879b6b4d911c7608933043aaf35ad
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-08 14:33:43 +0800

    update

--- sdk/dataprotection/arm-dataprotection/samples/v2/javascript/operationStatusGetSample.js
+++ sdk/dataprotection/arm-dataprotection/samples/v2/javascript/operationStatusGetSample.js
@@ -16,7 +16,7 @@ require("dotenv").config();
  * This sample demonstrates how to Gets the operation status for a resource.
  *
  * @summary Gets the operation status for a resource.
- * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2023-11-01/examples/GetOperationStatus.json
+ * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2024-04-01/examples/GetOperationStatus.json
  */
 async function getOperationStatus() {
   const subscriptionId =
