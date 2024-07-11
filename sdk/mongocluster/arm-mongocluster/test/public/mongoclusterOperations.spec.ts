f6fe6f5cde26716c3875ece06bf63308f3945339
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-11 15:13:53 +0800

    update

--- sdk/mongocluster/arm-mongocluster/test/public/mongoclusterOperations.spec.ts
+++ sdk/mongocluster/arm-mongocluster/test/public/mongoclusterOperations.spec.ts
@@ -49,6 +49,15 @@ describe("MongoCluster test", () => {
     }
   });
 
+  it("operations list test", async function () {
+    const resArray = new Array();
+    for await (let item of client.operations.list()) {
+      resArray.push(item);
+    }
+    assert.notEqual(resArray.length, 0);
+  });
+
+
   it("mongoClusters create test", async function () {
     const res = await client.mongoClusters.createOrUpdate(
       resourceGroup,
