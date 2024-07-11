87a281d735a89d072034f37a0e20d48e14e40946
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-11 10:34:47 +0800

    upddate

--- sdk/computefleet/arm-computefleet/src/rest/azureFleetClient.ts
+++ sdk/computefleet/arm-computefleet/src/rest/azureFleetClient.ts
@@ -57,9 +57,8 @@ export default function createClient(
       // Append one if there is no apiVersion and we have one at client options
       const url = new URL(req.url);
       if (!url.searchParams.get("api-version") && apiVersion) {
-        req.url = `${req.url}${
-          Array.from(url.searchParams.keys()).length > 0 ? "&" : "?"
-        }api-version=${apiVersion}`;
+        req.url = `${req.url}${Array.from(url.searchParams.keys()).length > 0 ? "&" : "?"
+          }api-version=${apiVersion}`;
       }
 
       return next(req);
