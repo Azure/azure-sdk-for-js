9b9ec0cec3c96e5639e54a511e8d4ffdf8c39c6b
Ryan Hurey <rhurey@microsoft.com>
2024-07-17 15:59:10 -0400

    Regenerate client to enable EntraID auth

---
+++ sdk/vision/ai-vision-image-analysis-rest/test/public/utils/clientMethods.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { Recorder, assertEnvironmentVariable } from "@azure-tools/test-recorder";
+import "./env";
+import { AzureKeyCredential, TokenCredential } from "@azure/core-auth";
+import importedCreateClient, { ImageAnalysisClient } from "../../../src/index";
+
+export async function createClient(recorder: Recorder, credential : AzureKeyCredential | TokenCredential): Promise<ImageAnalysisClient> {
+    const endpoint = assertEnvironmentVariable("VISION_ENDPOINT");
+    return importedCreateClient(endpoint, credential, recorder.configureClientOptions({}));
+  }
+  
--- sdk/vision/ai-vision-image-analysis-rest/test/public/utils/recordedClient.ts
+++ sdk/vision/ai-vision-image-analysis-rest/test/public/utils/recordedClient.ts
@@ -2,18 +2,14 @@
 // Licensed under the MIT license.
 
 import { Context } from "mocha";
-import {
-  Recorder,
-  RecorderStartOptions,
-  assertEnvironmentVariable,
-} from "@azure-tools/test-recorder";
-import "./env";
-import importedCreateClient, { ImageAnalysisClient } from "../../../src/index";
-import { AzureKeyCredential } from "@azure/core-auth";
+import { Recorder, RecorderStartOptions } from "@azure-tools/test-recorder";
 
 const envSetupForPlayback: Record<string, string> = {
-  VISION_ENDPOINT: "https://endpoint/",
-  VISION_KEY: "***********",
+  ENDPOINT: "https://endpoint",
+  AZURE_CLIENT_ID: "azure_client_id",
+  AZURE_CLIENT_SECRET: "azure_client_secret",
+  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+  SUBSCRIPTION_ID: "azure_subscription_id",
 };
 
 const recorderEnvSetup: RecorderStartOptions = {
@@ -29,11 +25,4 @@ export async function createRecorder(context: Context): Promise<Recorder> {
   const recorder = new Recorder(context.currentTest);
   await recorder.start(recorderEnvSetup);
   return recorder;
-}
-
-export async function createClient(recorder: Recorder): Promise<ImageAnalysisClient> {
-  const endpoint = assertEnvironmentVariable("VISION_ENDPOINT");
-  const key = assertEnvironmentVariable("VISION_KEY");
-  const credential = new AzureKeyCredential(key);
-  return importedCreateClient(endpoint, credential, recorder.configureClientOptions({}));
 }
