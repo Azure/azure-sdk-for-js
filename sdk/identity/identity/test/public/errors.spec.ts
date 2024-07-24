86da2dba09b89c404afdd19b682857bc937b2584
Maor Leger <maorleger@microsoft.com>
2024-07-24 13:01:26 -0700

    wip

--- sdk/identity/identity/test/public/errors.spec.ts
+++ sdk/identity/identity/test/public/errors.spec.ts
@@ -1,8 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { AggregateAuthenticationError, CredentialUnavailableError } from "../../src";
-
+import { AggregateAuthenticationError } from "../../src";
 import { assert } from "chai";
 
 describe("AggregateAuthenticationError", function () {
@@ -13,12 +12,5 @@ describe("AggregateAuthenticationError", function () {
     ]);
 
     assert(aggregateError.message.includes("Error: Boom.\nError: Boom again."));
-  });
-});
-
-describe.only("CredentialUnavailableError", function () {
-  it("produces a message containing details of the errors it contains", async () => {
-    const err = new CredentialUnavailableError("outer error", { cause: new Error("inner error") });
-    throw err;
   });
 });
