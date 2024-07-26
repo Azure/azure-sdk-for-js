1c9c7b73a938e09556ac47c36768a10713f1a2ee
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-07-26 05:27:05 +0000

    edits

--- sdk/eventhub/event-hubs/test/utils/chai.ts
+++ sdk/eventhub/event-hubs/test/utils/chai.ts
@@ -4,8 +4,10 @@
 import { chai, should as shouldFn, assert, expect } from "vitest";
 import chaiAsPromised from "chai-as-promised";
 import chaiExclude from "chai-exclude";
+import chaiString from "chai-string";
 
 chai.use(chaiAsPromised);
 chai.use(chaiExclude);
+chai.use(chaiString);
 const should = shouldFn();
 export { should, expect, assert };
