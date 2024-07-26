247dfcaa461266fa22e602dddd8415a5c9deccf8
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-26 11:13:45 +0800

    update

--- sdk/redis/arm-rediscache/test/redis_examples.ts
+++ sdk/redis/arm-rediscache/test/redis_examples.ts
@@ -25,18 +25,6 @@ const replaceableVariables: Record<string, string> = {
 
 const recorderOptions: RecorderStartOptions = {
   envSetupForPlayback: replaceableVariables,
-  sanitizerOptions: {
-    bodySanitizers: [{
-      regex: true,
-      value: `fakeKey`,
-      target: `[a-z0-9_A-z=]{40,100}`
-    }],
-    uriSanitizers: [{
-      regex: true,
-      value: `fakeKey`,
-      target: `[a-z0-9_A-z=]{40,100}`
-    }]
-  },
   removeCentralSanitizers: [
     "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
     "AZSDK3430", // .id in the body is not a secret and is listed below in the beforeEach section
