b259be72cd0682fd0c77b60a135b512afa1d96d1
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-30 10:48:42 +0800

    update

--- sdk/mongocluster/arm-mongocluster/src/rest/documentDBClient.ts
+++ sdk/mongocluster/arm-mongocluster/src/rest/documentDBClient.ts
@@ -22,7 +22,7 @@ export default function createClient(
   { apiVersion = "2024-03-01-preview", ...options }: DocumentDBContextOptions = {},
 ): DocumentDBContext {
   const endpointUrl = options.endpoint ?? options.baseUrl ?? `https://management.azure.com`;
-  const userAgentInfo = `azsdk-js-arm-mongocluster/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-mongocluster/1.0.0-beta.2`;
   const userAgentPrefix =
     options.userAgentOptions && options.userAgentOptions.userAgentPrefix
       ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
@@ -49,9 +49,8 @@ export default function createClient(
       // Append one if there is no apiVersion and we have one at client options
       const url = new URL(req.url);
       if (!url.searchParams.get("api-version") && apiVersion) {
-        req.url = `${req.url}${
-          Array.from(url.searchParams.keys()).length > 0 ? "&" : "?"
-        }api-version=${apiVersion}`;
+        req.url = `${req.url}${Array.from(url.searchParams.keys()).length > 0 ? "&" : "?"
+          }api-version=${apiVersion}`;
       }
 
       return next(req);
