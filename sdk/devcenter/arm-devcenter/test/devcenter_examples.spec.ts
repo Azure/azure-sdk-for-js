15b03a3de4f8ee76b5c60458ce9785794a7dda22
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-07-31 16:33:29 +0800

    [mgmt] revert skipped test cases

--- sdk/devcenter/arm-devcenter/test/devcenter_examples.spec.ts
+++ sdk/devcenter/arm-devcenter/test/devcenter_examples.spec.ts
@@ -20,21 +20,22 @@ import { DevCenterClient } from "../src/devCenterClient";
 import { DevCenter } from "../src/models";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
   SUBSCRIPTION_ID: "azure_subscription_id"
 };
 
 const recorderOptions: RecorderStartOptions = {
-  envSetupForPlayback: replaceableVariables
+  envSetupForPlayback: replaceableVariables,
+  removeCentralSanitizers: [
+    "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
+    "AZSDK3430", // .id in the body is not a secret and is listed below in the beforeEach section
+  ],
 };
 
 export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe.skip("devcenter test", () => {
+describe("devcenter test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: DevCenterClient;
--- sdk/healthcareapis/arm-healthcareapis/test/healthcareapis_operations_test.spec.ts
+++ sdk/healthcareapis/arm-healthcareapis/test/healthcareapis_operations_test.spec.ts
@@ -19,21 +19,22 @@ import { Context } from "mocha";
 import { HealthcareApisManagementClient } from "../src/healthcareApisManagementClient";
 
 const replaceableVariables: Record<string, string> = {
-  AZURE_CLIENT_ID: "azure_client_id",
-  AZURE_CLIENT_SECRET: "azure_client_secret",
-  AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
   SUBSCRIPTION_ID: "azure_subscription_id"
 };
 
 const recorderOptions: RecorderStartOptions = {
-  envSetupForPlayback: replaceableVariables
+  envSetupForPlayback: replaceableVariables,
+  removeCentralSanitizers: [
+    "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
+    "AZSDK3430", // .id in the body is not a secret and is listed below in the beforeEach section
+  ],
 };
 
 export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe.skip("HealthcareApis test", () => {
+describe("HealthcareApis test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: HealthcareApisManagementClient;
