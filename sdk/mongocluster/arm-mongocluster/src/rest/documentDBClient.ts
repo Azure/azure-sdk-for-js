3b884d296fe1c2247145c13809e67685c3ced122
azure-sdk <azuresdk@microsoft.com>
2024-08-01 04:41:19 +0000

    Post release automated changes for azure-arm-mongocluster

--- sdk/mongocluster/arm-mongocluster/src/rest/documentDBClient.ts
+++ sdk/mongocluster/arm-mongocluster/src/rest/documentDBClient.ts
@@ -22,7 +22,7 @@ export default function createClient(
   { apiVersion = "2024-03-01-preview", ...options }: DocumentDBContextOptions = {},
 ): DocumentDBContext {
   const endpointUrl = options.endpoint ?? options.baseUrl ?? `https://management.azure.com`;
-  const userAgentInfo = `azsdk-js-arm-mongocluster/1.0.0-beta.2`;
+  const userAgentInfo = `azsdk-js-arm-mongocluster/1.0.0-beta.3`;
   const userAgentPrefix =
     options.userAgentOptions && options.userAgentOptions.userAgentPrefix
       ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
