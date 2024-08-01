ebeae92fec18e2732889b1c64c04939af104ac1a
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-08-01 17:29:44 +0800

    remove nvm installation for SDK automation

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install v18.15.0 
-nvm alias default node
 npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
