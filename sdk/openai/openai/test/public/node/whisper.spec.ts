62e316ba95dde96542c1454a9003b865c246e28f
Minh-Anh Phan <minhanhphan@microsoft.com>
2024-08-01 17:40:51 +0000

    update whisper deployment

--- sdk/openai/openai/test/public/node/whisper.spec.ts
+++ sdk/openai/openai/test/public/node/whisper.spec.ts
@@ -1,54 +1,37 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as fs from "fs";
+import { createReadStream } from "fs";
 import { matrix } from "@azure-tools/test-utils";
 import { describe, it, beforeAll } from "vitest";
 import { createClient } from "../utils/createClient.js";
 import OpenAI, { AzureOpenAI } from "openai";
-import {
-  APIMatrix,
-  APIVersion,
-  DeploymentInfo,
-  getDeployments,
-  withDeployments,
-} from "../utils/utils.js";
+import { APIMatrix, APIVersion } from "../utils/utils.js";
 import { assertAudioResult } from "../utils/asserts.js";
 import { AudioResultFormat } from "../utils/audioTypes.js";
-import { whisperModels } from "../utils/models.js";
 
 describe("OpenAI", function () {
   matrix([APIMatrix] as const, async function (authMethod: APIVersion) {
     describe(`[${authMethod}] Client`, () => {
       let client: AzureOpenAI | OpenAI;
-      let deployments: DeploymentInfo[] = [];
 
       beforeAll(async function () {
-        client = createClient(authMethod, "audio");
-        deployments = await getDeployments("audio");
+        client = createClient(authMethod, "audio", { deployment: "whisper" });
       });
 
       describe("getAudioTranscription", function () {
         it(`returns json transcription if responseFormat wasn't specified`, async function () {
-          const file = fs.createReadStream(`./assets/audio/countdown.mp3`);
-          await withDeployments(
-            deployments,
-            (deploymentName) => client.audio.transcriptions.create({ model: deploymentName, file }),
-            (result) => assertAudioResult("json", result),
-            whisperModels,
-          );
+          const file = createReadStream(`./assets/audio/countdown.mp3`);
+          const audio = await client.audio.transcriptions.create({ model: "", file });
+          assertAudioResult("json", audio);
         });
       });
 
       describe("getAudioTranslation", function () {
         it(`returns json translation if responseFormat wasn't specified`, async function () {
-          const file = fs.createReadStream(`./assets/audio/countdown.mp3`);
-          await withDeployments(
-            deployments,
-            (deploymentName) => client.audio.translations.create({ model: deploymentName, file }),
-            (result) => assertAudioResult("json", result),
-            whisperModels,
-          );
+          const file = createReadStream(`./assets/audio/countdown.mp3`);
+          const audio = await client.audio.translations.create({ model: "", file });
+          assertAudioResult("json", audio);
         });
       });
 
@@ -60,35 +43,25 @@ describe("OpenAI", function () {
         async function (format: AudioResultFormat, extension: string) {
           describe("getAudioTranscription", function () {
             it(`returns ${format} transcription for ${extension} files`, async function () {
-              const file = fs.createReadStream(`./assets/audio/countdown.${extension}`);
-              withDeployments(
-                deployments,
-                (deploymentName) =>
-                  client.audio.transcriptions.create({
-                    model: deploymentName,
-                    file,
-                    response_format: format,
-                  }),
-                (result) => assertAudioResult(format, result),
-                whisperModels,
-              );
+              const file = createReadStream(`./assets/audio/countdown.${extension}`);
+              const audio = await client.audio.transcriptions.create({
+                model: "",
+                file,
+                response_format: format,
+              });
+              assertAudioResult(format, audio);
             });
           });
 
           describe("getAudioTranslation", function () {
             it(`returns ${format} translation for ${extension} files`, async function () {
-              const file = fs.createReadStream(`./assets/audio/countdown.${extension}`);
-              withDeployments(
-                deployments,
-                (deploymentName) =>
-                  client.audio.translations.create({
-                    model: deploymentName,
-                    file,
-                    response_format: format,
-                  }),
-                (result) => assertAudioResult(format, result),
-                whisperModels,
-              );
+              const file = createReadStream(`./assets/audio/countdown.${extension}`);
+              const audio = await client.audio.translations.create({
+                model: "",
+                file,
+                response_format: format,
+              });
+              assertAudioResult(format, audio);
             });
           });
         },
