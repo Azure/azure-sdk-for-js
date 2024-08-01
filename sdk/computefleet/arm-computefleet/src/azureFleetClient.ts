d166c6983c2a0d2cbecacc908656c1044df5fa5c
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-08-01 10:48:02 +0800

    [mgmt] computefleet with modular (#30358)
    https://github.com/Azure/sdk-release-request/issues/5256

---
+++ sdk/computefleet/arm-computefleet/src/azureFleetClient.ts
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { TokenCredential } from "@azure/core-auth";
+import { Pipeline } from "@azure/core-rest-pipeline";
+import { getOperationsOperations, OperationsOperations } from "./classic/operations/index.js";
+import { getFleetsOperations, FleetsOperations } from "./classic/fleets/index.js";
+import {
+  createAzureFleet,
+  AzureFleetClientOptionalParams,
+  AzureFleetContext,
+} from "./api/index.js";
+
+export { AzureFleetClientOptionalParams } from "./api/azureFleetContext.js";
+
+export class AzureFleetClient {
+  private _client: AzureFleetContext;
+  /** The pipeline used by this client to make requests */
+  public readonly pipeline: Pipeline;
+
+  constructor(
+    credential: TokenCredential,
+    subscriptionId: string,
+    options: AzureFleetClientOptionalParams = {},
+  ) {
+    const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+    const userAgentPrefix = prefixFromOptions
+      ? `${prefixFromOptions} azsdk-js-client`
+      : "azsdk-js-client";
+
+    this._client = createAzureFleet(credential, {
+      ...options,
+      userAgentOptions: { userAgentPrefix },
+    });
+    this.pipeline = this._client.pipeline;
+    this.operations = getOperationsOperations(this._client);
+    this.fleets = getFleetsOperations(this._client, subscriptionId);
+  }
+
+  /** The operation groups for Operations */
+  public readonly operations: OperationsOperations;
+  /** The operation groups for Fleets */
+  public readonly fleets: FleetsOperations;
+}
