1c2b136c0f30bb908534c388238303545e877784
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-08-06 17:13:31 +0800

    [mgmt] healthdataaiservices release

---
+++ sdk/healthdataaiservices/arm-healthdataaiservices/src/api/healthDataAIServicesContext.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { TokenCredential } from "@azure/core-auth";
+import { ClientOptions } from "@azure-rest/core-client";
+import { HealthDataAIServicesContext } from "../rest/index.js";
+import getClient from "../rest/index.js";
+
+/** Optional parameters for the client. */
+export interface HealthDataAIServicesClientOptionalParams extends ClientOptions {
+  /** The API version to use for this operation. */
+  apiVersion?: string;
+}
+
+export { HealthDataAIServicesContext } from "../rest/index.js";
+
+export function createHealthDataAIServices(
+  credential: TokenCredential,
+  options: HealthDataAIServicesClientOptionalParams = {},
+): HealthDataAIServicesContext {
+  const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+  const userAgentPrefix = prefixFromOptions ? `${prefixFromOptions} azsdk-js-api` : "azsdk-js-api";
+
+  const clientContext = getClient(credential, {
+    ...options,
+    userAgentOptions: { userAgentPrefix },
+  });
+  return clientContext;
+}
