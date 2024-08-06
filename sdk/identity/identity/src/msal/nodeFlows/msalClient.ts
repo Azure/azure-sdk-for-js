b516ddd3b8494333c5d59a4586cf0912a299083d
Karishma Ghiya <kaghiya@microsoft.com>
2024-08-06 16:16:10 -0700

    update refresh on

--- sdk/identity/identity/src/msal/nodeFlows/msalClient.ts
+++ sdk/identity/identity/src/msal/nodeFlows/msalClient.ts
@@ -85,7 +85,7 @@ export interface MsalClient {
     scopes: string[],
     userAssertionToken: string,
     clientCredentials: string | CertificateParts | (() => Promise<string>),
-    options?: GetTokenOptions,
+    options?: GetTokenOptions
   ): Promise<AccessToken>;
 
   /**
@@ -96,7 +96,7 @@ export interface MsalClient {
    */
   getTokenByInteractiveRequest(
     scopes: string[],
-    options: GetTokenInteractiveOptions,
+    options: GetTokenInteractiveOptions
   ): Promise<AccessToken>;
   /**
    * Retrieves an access token by using a user's username and password.
@@ -111,7 +111,7 @@ export interface MsalClient {
     scopes: string[],
     username: string,
     password: string,
-    options?: GetTokenOptions,
+    options?: GetTokenOptions
   ): Promise<AccessToken>;
   /**
    * Retrieves an access token by prompting the user to authenticate using a device code.
@@ -124,7 +124,7 @@ export interface MsalClient {
   getTokenByDeviceCode(
     scopes: string[],
     userPromptCallback: DeviceCodePromptCallback,
-    options?: GetTokenWithSilentAuthOptions,
+    options?: GetTokenWithSilentAuthOptions
   ): Promise<AccessToken>;
   /**
    * Retrieves an access token by using a client certificate.
@@ -137,7 +137,7 @@ export interface MsalClient {
   getTokenByClientCertificate(
     scopes: string[],
     certificate: CertificateParts,
-    options?: GetTokenOptions,
+    options?: GetTokenOptions
   ): Promise<AccessToken>;
 
   /**
@@ -151,7 +151,7 @@ export interface MsalClient {
   getTokenByClientAssertion(
     scopes: string[],
     clientAssertion: () => Promise<string>,
-    options?: GetTokenOptions,
+    options?: GetTokenOptions
   ): Promise<AccessToken>;
 
   /**
@@ -165,7 +165,7 @@ export interface MsalClient {
   getTokenByClientSecret(
     scopes: string[],
     clientSecret: string,
-    options?: GetTokenOptions,
+    options?: GetTokenOptions
   ): Promise<AccessToken>;
 
   /**
@@ -185,7 +185,7 @@ export interface MsalClient {
     redirectUri: string,
     authorizationCode: string,
     clientSecret?: string,
-    options?: GetTokenWithSilentAuthOptions,
+    options?: GetTokenWithSilentAuthOptions
   ): Promise<AccessToken>;
 
   /**
@@ -260,18 +260,18 @@ export const interactiveBrowserMockable = {
 export function generateMsalConfiguration(
   clientId: string,
   tenantId: string,
-  msalClientOptions: MsalClientOptions = {},
+  msalClientOptions: MsalClientOptions = {}
 ): msal.Configuration {
   const resolvedTenant = resolveTenantId(
     msalClientOptions.logger ?? msalLogger,
     tenantId,
-    clientId,
+    clientId
   );
 
   // TODO: move and reuse getIdentityClientAuthorityHost
   const authority = getAuthority(
     resolvedTenant,
-    msalClientOptions.authorityHost ?? process.env.AZURE_AUTHORITY_HOST,
+    msalClientOptions.authorityHost ?? process.env.AZURE_AUTHORITY_HOST
   );
 
   const httpClient = new IdentityClient({
@@ -287,7 +287,7 @@ export function generateMsalConfiguration(
       knownAuthorities: getKnownAuthorities(
         resolvedTenant,
         authority,
-        msalClientOptions.disableInstanceDiscovery,
+        msalClientOptions.disableInstanceDiscovery
       ),
     },
     system: {
@@ -338,7 +338,7 @@ interface MsalClientState {
 export function createMsalClient(
   clientId: string,
   tenantId: string,
-  createMsalClientOptions: MsalClientOptions = {},
+  createMsalClientOptions: MsalClientOptions = {}
 ): MsalClient {
   const state: MsalClientState = {
     msalConfig: generateMsalConfiguration(clientId, tenantId, createMsalClientOptions),
@@ -351,7 +351,7 @@ export function createMsalClient(
 
   const publicApps: Map<string, msal.PublicClientApplication> = new Map();
   async function getPublicApp(
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<msal.PublicClientApplication> {
     const appKey = options.enableCae ? "CAE" : "default";
 
@@ -363,7 +363,7 @@ export function createMsalClient(
 
     // Initialize a new app and cache it
     state.logger.getToken.info(
-      `Creating new PublicClientApplication with CAE ${options.enableCae ? "enabled" : "disabled"}.`,
+      `Creating new PublicClientApplication with CAE ${options.enableCae ? "enabled" : "disabled"}.`
     );
 
     const cachePlugin = options.enableCae
@@ -385,14 +385,14 @@ export function createMsalClient(
 
   const confidentialApps: Map<string, msal.ConfidentialClientApplication> = new Map();
   async function getConfidentialApp(
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<msal.ConfidentialClientApplication> {
     const appKey = options.enableCae ? "CAE" : "default";
 
     let confidentialClientApp = confidentialApps.get(appKey);
     if (confidentialClientApp) {
       state.logger.getToken.info(
-        "Existing ConfidentialClientApplication found in cache, returning it.",
+        "Existing ConfidentialClientApplication found in cache, returning it."
       );
       return confidentialClientApp;
     }
@@ -401,7 +401,7 @@ export function createMsalClient(
     state.logger.getToken.info(
       `Creating new ConfidentialClientApplication with CAE ${
         options.enableCae ? "enabled" : "disabled"
-      }.`,
+      }.`
     );
 
     const cachePlugin = options.enableCae
@@ -424,11 +424,11 @@ export function createMsalClient(
   async function getTokenSilent(
     app: msal.ConfidentialClientApplication | msal.PublicClientApplication,
     scopes: string[],
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<msal.AuthenticationResult> {
     if (state.cachedAccount === null) {
       state.logger.getToken.info(
-        "No cached account found in local state, attempting to load it from MSAL cache.",
+        "No cached account found in local state, attempting to load it from MSAL cache."
       );
       const cache = app.getTokenCache();
       const accounts = await cache.getAllAccounts();
@@ -497,7 +497,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
     msalApp: msal.ConfidentialClientApplication | msal.PublicClientApplication,
     scopes: Array<string>,
     options: GetTokenWithSilentAuthOptions,
-    onAuthenticationRequired: () => Promise<msal.AuthenticationResult | null>,
+    onAuthenticationRequired: () => Promise<msal.AuthenticationResult | null>
   ): Promise<AccessToken> {
     let response: msal.AuthenticationResult | null = null;
     try {
@@ -530,17 +530,23 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
     state.cachedAccount = response?.account ?? null;
 
     state.logger.getToken.info(formatSuccess(scopes));
-
-    return {
+    const accessToken: AccessToken = {
       token: response.accessToken,
       expiresOnTimestamp: response.expiresOn.getTime(),
     };
+    if (response.refreshOn) {
+      return {
+        ...accessToken,
+        refreshesOnTimestamp: response.refreshOn.getTime(),
+      };
+    }
+    return accessToken;
   }
 
   async function getTokenByClientSecret(
     scopes: string[],
     clientSecret: string,
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<AccessToken> {
     state.logger.getToken.info(`Attempting to acquire token using client secret`);
 
@@ -556,13 +562,18 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
         claims: options?.claims,
       });
       ensureValidMsalToken(scopes, response, options);
-
       state.logger.getToken.info(formatSuccess(scopes));
-
-      return {
+      const accessToken: AccessToken = {
         token: response.accessToken,
         expiresOnTimestamp: response.expiresOn.getTime(),
       };
+      if (response.refreshOn) {
+        return {
+          ...accessToken,
+          refreshesOnTimestamp: response.refreshOn.getTime(),
+        };
+      }
+      return accessToken;
     } catch (err: any) {
       throw handleMsalError(scopes, err, options);
     }
@@ -571,7 +582,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
   async function getTokenByClientAssertion(
     scopes: string[],
     clientAssertion: () => Promise<string>,
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<AccessToken> {
     state.logger.getToken.info(`Attempting to acquire token using client assertion`);
 
@@ -591,10 +602,17 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
 
       state.logger.getToken.info(formatSuccess(scopes));
 
-      return {
+      const accessToken: AccessToken = {
         token: response.accessToken,
         expiresOnTimestamp: response.expiresOn.getTime(),
       };
+      if (response.refreshOn) {
+        return {
+          ...accessToken,
+          refreshesOnTimestamp: response.refreshOn.getTime(),
+        };
+      }
+      return accessToken;
     } catch (err: any) {
       throw handleMsalError(scopes, err, options);
     }
@@ -603,7 +621,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
   async function getTokenByClientCertificate(
     scopes: string[],
     certificate: CertificateParts,
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<AccessToken> {
     state.logger.getToken.info(`Attempting to acquire token using client certificate`);
 
@@ -621,10 +639,17 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
 
       state.logger.getToken.info(formatSuccess(scopes));
 
-      return {
+      const accessToken: AccessToken = {
         token: response.accessToken,
         expiresOnTimestamp: response.expiresOn.getTime(),
       };
+      if (response.refreshOn) {
+        return {
+          ...accessToken,
+          refreshesOnTimestamp: response.refreshOn.getTime(),
+        };
+      }
+      return accessToken;
     } catch (err: any) {
       throw handleMsalError(scopes, err, options);
     }
@@ -633,7 +658,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
   async function getTokenByDeviceCode(
     scopes: string[],
     deviceCodeCallback: DeviceCodePromptCallback,
-    options: GetTokenWithSilentAuthOptions = {},
+    options: GetTokenWithSilentAuthOptions = {}
   ): Promise<AccessToken> {
     state.logger.getToken.info(`Attempting to acquire token using device code`);
 
@@ -662,7 +687,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
     scopes: string[],
     username: string,
     password: string,
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<AccessToken> {
     state.logger.getToken.info(`Attempting to acquire token using username and password`);
 
@@ -693,7 +718,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
     redirectUri: string,
     authorizationCode: string,
     clientSecret?: string,
-    options: GetTokenWithSilentAuthOptions = {},
+    options: GetTokenWithSilentAuthOptions = {}
   ): Promise<AccessToken> {
     state.logger.getToken.info(`Attempting to acquire token using authorization code`);
 
@@ -722,7 +747,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
     scopes: string[],
     userAssertionToken: string,
     clientCredentials: string | CertificateParts | (() => Promise<string>),
-    options: GetTokenOptions = {},
+    options: GetTokenOptions = {}
   ): Promise<AccessToken> {
     msalLogger.getToken.info(`Attempting to acquire token on behalf of another user`);
 
@@ -751,11 +776,17 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
       ensureValidMsalToken(scopes, response, options);
 
       msalLogger.getToken.info(formatSuccess(scopes));
-
-      return {
+      const accessToken: AccessToken = {
         token: response.accessToken,
         expiresOnTimestamp: response.expiresOn.getTime(),
       };
+      if (response.refreshOn) {
+        return {
+          ...accessToken,
+          refreshesOnTimestamp: response.refreshOn.getTime(),
+        };
+      }
+      return accessToken;
     } catch (err: any) {
       throw handleMsalError(scopes, err, options);
     }
@@ -763,7 +794,7 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
 
   async function getTokenByInteractiveRequest(
     scopes: string[],
-    options: GetTokenInteractiveOptions = {},
+    options: GetTokenInteractiveOptions = {}
   ): Promise<AccessToken> {
     msalLogger.getToken.info(`Attempting to acquire token interactively`);
 
@@ -776,18 +807,18 @@ To work with multiple accounts for the same Client ID and Tenant ID, please prov
      * If the default broker account is not available, the method will fall back to interactive authentication.
      */
     async function getBrokeredToken(
-      useDefaultBrokerAccount: boolean,
+      useDefaultBrokerAccount: boolean
     ): Promise<msal.AuthenticationResult> {
       msalLogger.verbose("Authentication will resume through the broker");
       const interactiveRequest = createBaseInteractiveRequest();
       if (state.pluginConfiguration.broker.parentWindowHandle) {
         interactiveRequest.windowHandle = Buffer.from(
-          state.pluginConfiguration.broker.parentWindowHandle,
+          state.pluginConfiguration.broker.parentWindowHandle
         );
       } else {
         // this is a bug, as the pluginConfiguration handler should validate this case.
         msalLogger.warning(
-          "Parent window handle is not specified for the broker. This may cause unexpected behavior. Please provide the parentWindowHandle.",
+          "Parent window handle is not specified for the broker. This may cause unexpected behavior. Please provide the parentWindowHandle."
         );
       }
 
