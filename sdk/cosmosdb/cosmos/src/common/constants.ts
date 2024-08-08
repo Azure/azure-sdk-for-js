2f519ee0d3ad366535dba6942681ae9a81a163ac
azure-sdk <azuresdk@microsoft.com>
2024-08-08 15:11:48 +0000

    Post release automated changes for azure-cosmos

--- sdk/cosmosdb/cosmos/src/common/constants.ts
+++ sdk/cosmosdb/cosmos/src/common/constants.ts
@@ -204,7 +204,7 @@ export const Constants = {
   AzureNamespace: "Azure.Cosmos",
   AzurePackageName: "@azure/cosmos",
   SDKName: "azure-cosmos-js",
-  SDKVersion: "4.1.0",
+  SDKVersion: "4.1.1",
 
   // Diagnostics
   CosmosDbDiagnosticLevelEnvVarName: "AZURE_COSMOSDB_DIAGNOSTICS_LEVEL",
