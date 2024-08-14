884082ff9b7f563a8da5c4fce7e6f558dee860f9
Siddharth Singha Roy <siddharthsingharoy@gmail.com>
2024-08-14 17:33:56 +0000

    docs(): modified CHANGELOG as per azure standard

--- sdk/playwrighttesting/microsoft-playwright-testing/src/common/messages.ts
+++ sdk/playwrighttesting/microsoft-playwright-testing/src/common/messages.ts
@@ -5,8 +5,9 @@ export const ServiceErrorMessageConstants = {
   NO_AUTH_ERROR:
     "Could not authenticate with the service. Please refer to https://aka.ms/mpt/authentication for more information.", // no mpt pat set and could not fetch entra token
   NO_SERVICE_URL_ERROR: "Please set PLAYWRIGHT_SERVICE_URL in your environment variables.",
-  INVALID_MPT_PAT_ERROR: "The service PAT set in the environment variable is invalid.",
-  EXPIRED_MPT_PAT_ERROR: "The service PAT has expired. Please create a new PAT.",
+  INVALID_MPT_PAT_ERROR: "The Access Token provided in the environment variable is invalid.",
+  EXPIRED_MPT_PAT_ERROR: "The Access Token you are using is expired. Create a new token.",
   INVALID_GLOBAL_FUNCTION: "file must export a single function",
-  DUPLICATE_REPORTER: "Reporter with the same name already exists",
+  DUPLICATE_REPORTER:
+    "Another reporter with the same name already exists. Please resolve the duplicate and try again.",
 };
