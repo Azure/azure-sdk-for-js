55d340f79d3bc14c76bc13736f2c9616c2dfa039
Jeremy Meng <jeremy.ymeng@gmail.com>
2024-08-15 13:16:23 -0700

    [eslint-plugin] enable eslint-plugin-promise for our flat config
    now that the plugin has flat config support

--- common/tools/eslint-plugin-azure-sdk/src/configs/index.ts
+++ common/tools/eslint-plugin-azure-sdk/src/configs/index.ts
@@ -6,6 +6,7 @@ import typescriptEslint from "typescript-eslint";
 import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import markdown from "eslint-plugin-markdown";
+import promise from "eslint-plugin-promise";
 
 import eslintCustomized from "./eslint-customized";
 import markdownCustomized from "./markdown-customized";
@@ -35,8 +36,12 @@ function recommended(plugin: FlatConfig.Plugin) {
       plugins: {
         "@azure/azure-sdk": plugin,
         markdown,
+        promise,
       },
     },
+
+    promise.configs["flat/recommended"],
+
     // azure sdk customized
     eslintCustomized,
     ...markdownCustomized,
