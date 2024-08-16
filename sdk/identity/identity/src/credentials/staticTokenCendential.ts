4b93ef8be2cd3d05bdb4ac847a382d7c6ff54936
Tekdycaml <zhang_nan_163@163.com>
2024-08-16 09:13:40 +0000

    adding StaticTokenCredential.ts for the package

---
+++ sdk/identity/identity/src/credentials/staticTokenCendential.ts
@@ -0,0 +1,11 @@
+import { TokenCredential, AccessToken } from "@azure/core-auth";
+
+export class StaticTokenCredential implements TokenCredential {
+  // AccessToken is an object with two properties:
+  // - A "token" property with a string value.
+  // - And an "expiresOnTimestamp" property with a numeric unix timestamp as its value.
+  constructor(private accessToken: AccessToken) { }
+  async getToken(): Promise<AccessToken> {
+    return this.accessToken;
+  }
+}
