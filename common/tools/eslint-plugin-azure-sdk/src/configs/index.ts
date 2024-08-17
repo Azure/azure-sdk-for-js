0854e57c064de4148687dabb212965c649026305
Jeremy Meng <jeremy.ymeng@gmail.com>
2024-08-16 17:18:54 -0700

    [linting] move monitor-opentelemetry-exporter to use flat config (#30792)
    - add a config in eslint-plugin-azure-sdk for linting with type
    information
    - remove .eslint* and replace with eslint.config.mjs
    - remove --ext from linting NPM scripts
    - fix linting error: URL is now available in global scope on our
    supported platforms.

--- common/tools/eslint-plugin-azure-sdk/src/configs/index.ts
+++ common/tools/eslint-plugin-azure-sdk/src/configs/index.ts
@@ -15,7 +15,7 @@ import azureSdkCustomized from "./azure-sdk-customized";
 // to keep compat with old .eslintrc style usage
 import rootConfig from "./azure-sdk-base";
 
-function recommended(plugin: FlatConfig.Plugin) {
+function recommended(plugin: FlatConfig.Plugin, options: { typeChecked: boolean }) {
   return typescriptEslint.config(
     {
       ignores: ["**/generated/**", "**/*.config.{js,cjs,mjs}"],
@@ -29,7 +29,9 @@ function recommended(plugin: FlatConfig.Plugin) {
       },
     },
     eslint.configs.recommended,
-    ...typescriptEslint.configs.recommended,
+    ...(options.typeChecked
+      ? typescriptEslint.configs.recommendedTypeChecked
+      : typescriptEslint.configs.recommended),
     typescriptEslint.configs.eslintRecommended,
     eslintConfigPrettier,
     {
@@ -50,7 +52,8 @@ function recommended(plugin: FlatConfig.Plugin) {
 }
 
 export default (plugin: FlatConfig.Plugin) => ({
-  recommended: recommended(plugin),
+  recommended: recommended(plugin, { typeChecked: false }),
+  recommendedTypeChecked: recommended(plugin, { typeChecked: true }),
   "recommended-legacy": {
     plugins: ["@azure/azure-sdk"],
     env: {
