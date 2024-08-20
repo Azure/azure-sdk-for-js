0533b787d38f74a96acbf598be03dbfcbbe6c715
v-weiyding <v-weiyding@microsoft.com>
2024-08-20 16:08:08 +0800

    fix samples-dev

--- sdk/face/ai-vision-face-rest/samples-dev/livenessSessionWithVerifyWithVerifyImage.ts
+++ sdk/face/ai-vision-face-rest/samples-dev/livenessSessionWithVerifyWithVerifyImage.ts
@@ -7,6 +7,9 @@ import createFaceClient, {
 } from '@azure-rest/ai-vision-face';
 import { readFileSync } from 'fs';
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
 /**
  * This sample demonstrates how to create a liveness detection with face verification session with a verification image.
  *
