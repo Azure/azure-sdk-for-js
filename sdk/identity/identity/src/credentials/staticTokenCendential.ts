9eafc31fe6df497a90a66982502116a006a6fbef
Tekdycaml <zhang_nan_163@163.com>
2024-08-20 06:39:22 +0000

    [identity] format staticTokenCredential

--- sdk/identity/identity/src/credentials/staticTokenCendential.ts
+++ sdk/identity/identity/src/credentials/staticTokenCendential.ts
@@ -4,7 +4,7 @@ export class StaticTokenCredential implements TokenCredential {
   // AccessToken is an object with two properties:
   // - A "token" property with a string value.
   // - And an "expiresOnTimestamp" property with a numeric unix timestamp as its value.
-  constructor(private accessToken: AccessToken) { }
+  constructor(private accessToken: AccessToken) {}
   async getToken(): Promise<AccessToken> {
     return this.accessToken;
   }
