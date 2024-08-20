c2459f58ba50034c1a36e36f83e23d593719ca77
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2024-08-20 09:16:37 -0700

    [OpenAI] Add Live Test Pipeline (#30506)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    Add live test pipeline for OpenAI service
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Deyaaeldeen Almahallawi <dealmaha@microsoft.com>

---
+++ sdk/openai/openai/test/public/images.spec.ts
@@ -0,0 +1,75 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { matrix } from "@azure-tools/test-utils-vitest";
+import { createClient } from "./utils/createClient.js";
+import {
+  APIMatrix,
+  APIVersion,
+  DeploymentInfo,
+  getDeployments,
+  getSucceeded,
+  updateWithSucceeded,
+  withDeployments,
+} from "./utils/utils.js";
+import { assertImagesWithJSON, assertImagesWithURLs } from "./utils/asserts.js";
+import OpenAI, { AzureOpenAI } from "openai";
+import { describe, it, beforeAll } from "vitest";
+
+describe("Images", function () {
+  matrix([APIMatrix] as const, async function (apiVersion: APIVersion) {
+    describe(`[${apiVersion}] Client`, () => {
+      let deployments: DeploymentInfo[] = [];
+      let client: AzureOpenAI | OpenAI;
+
+      beforeAll(async function () {
+        client = createClient(apiVersion, "vision");
+        deployments = await getDeployments("vision");
+      });
+
+      describe("getImages", function () {
+        const imageGenerationDeployments: DeploymentInfo[] = [];
+        const prompt = "a flower vase on a table";
+        const numberOfImages = 1;
+        const height = 1024;
+        const width = 1024;
+        const size = `${height}x${width}`;
+
+        it("generates image URLs", async function () {
+          updateWithSucceeded(
+            await withDeployments(
+              getSucceeded(deployments, imageGenerationDeployments),
+              (deploymentName) =>
+                client.images.generate({
+                  model: deploymentName,
+                  prompt: prompt,
+                  n: numberOfImages,
+                  size,
+                }),
+              (item) => assertImagesWithURLs(item, height, width),
+            ),
+            imageGenerationDeployments,
+          );
+        });
+
+        it("generates image strings", async function () {
+          updateWithSucceeded(
+            await withDeployments(
+              getSucceeded(deployments, imageGenerationDeployments),
+              (deploymentName) =>
+                client.images.generate({
+                  model: deploymentName,
+                  prompt,
+                  n: numberOfImages,
+                  size,
+                  response_format: "b64_json",
+                }),
+              (item) => assertImagesWithJSON(item, height, width),
+            ),
+            imageGenerationDeployments,
+          );
+        });
+      });
+    });
+  });
+});
