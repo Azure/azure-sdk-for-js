4d8aec42a1facbaa32e284a608d92382996191de
Maor Leger <maorleger@microsoft.com>
2024-08-22 11:21:53 -0700

    shorter error message per feedback

--- sdk/identity/identity/test/internal/node/managedIdentityCredential/msalMsiProvider.spec.ts
+++ sdk/identity/identity/test/internal/node/managedIdentityCredential/msalMsiProvider.spec.ts
@@ -37,28 +37,28 @@ describe("ManagedIdentityCredential (MSAL)", function () {
       it("throws when both clientId and resourceId are provided", function () {
         assert.throws(
           () => new MsalMsiProvider("id", { resourceId: "id" }),
-          /At most, one of 'clientId', 'resourceId', or 'objectId' may be provided/,
+          /only one of 'clientId', 'resourceId', or 'objectId' can be provided/,
         );
       });
 
       it("throws when both clientId and resourceId are provided via options", function () {
         assert.throws(
           () => new MsalMsiProvider({ clientId: "id", resourceId: "id" }),
-          /At most, one of 'clientId', 'resourceId', or 'objectId' may be provided/,
+          /only one of 'clientId', 'resourceId', or 'objectId' can be provided/,
         );
       });
 
       it("throws when both clientId and objectId are provided", function () {
         assert.throws(
           () => new MsalMsiProvider("id", { objectId: "id" }),
-          /At most, one of 'clientId', 'resourceId', or 'objectId' may be provided/,
+          /only one of 'clientId', 'resourceId', or 'objectId' can be provided/,
         );
       });
 
       it("throws when both resourceId and objectId are provided via options", function () {
         assert.throws(
           () => new MsalMsiProvider({ resourceId: "id", objectId: "id" }),
-          /At most, one of 'clientId', 'resourceId', or 'objectId' may be provided/,
+          /only one of 'clientId', 'resourceId', or 'objectId' can be provided/,
         );
       });
     });
