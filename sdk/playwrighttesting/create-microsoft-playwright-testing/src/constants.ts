21b1956dcc4e6a7f3868594e1635b47fff5c1244
Siddharth Singha Roy <siddharthsingharoy@gmail.com>
2024-08-22 10:31:28 +0530

    Onboard @azure/microsoft-playwright-testing sdk (#30433)
    ### Packages impacted by this PR
    @azure/microsoft-playwright-testing
    @azure/create-microsoft-playwright-testing
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    - Onboard 2 packages
    - @azure/microsoft-playwright-testing: Client library used to integrate
    Playwright OSS with Microsoft Playwright Testing service
    - @azure/create-microsoft-playwright-testing: Utility library to setup
    @azure/microsoft-playwright-testing
    
    #### What does the package do
    
    - Playwright OSS client connects to the service-hosted browsers using
    the API provided by Playwright OSS. The package provides an abstraction
    over this interaction
    - Test results are published from the machine running the tests to the
    service.
     
    To integrate the service with a Playwright project, users will need to
    install a package along with Playwright OSS package. The package is
    tightly coupled with Playwright OSS package and also is responsible for
    entra id authentication.
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    Yes
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [x] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [x] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Vansh Vardhan Singh <vanshvsingh@gmail.com>

---
+++ sdk/playwrighttesting/create-microsoft-playwright-testing/src/constants.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { Language } from "./types";
+
+export const Languages = {
+  TypeScript: "TypeScript" as Language,
+  JavaScript: "JavaScript" as Language,
+};
+
+export const Extensions = {
+  TypeScript: ".ts",
+  JavaScript: ".js",
+};
+
+export const ErrorMessages = {
+  NO_CONFIGURATION_FILE_FOUND:
+    "Could not find a playwright configuration file in the current directory. Please use --config parameter to specify the configuration file.",
+  UNSUPPORTED_CONFIGURATION_FILE: "The playwright configuration file passed is not supported.",
+};
+
+export const Messages = {
+  CAN_OVERRIDE_MESSAGE:
+    "There's already a playwright service configuration file present. Do you want to overwrite it?",
+  CONFIRMATION_FOR_EXIT_MESSAGE:
+    "You won't be able to install and setup up the service without overwriting the existing configuration file. Do you still want to exit the setup process?",
+  SETUP_PROCESS_EXIT_MESSAGE: "Exiting the service setup process.",
+};
---
+++ sdk/playwrighttesting/microsoft-playwright-testing/src/utils/serviceClient.ts
@@ -0,0 +1,154 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import type { FullResult } from "@playwright/test/reporter";
+import { Constants } from "../common/constants";
+import { EnvironmentVariables } from "../common/environmentVariables";
+import { HttpService } from "../common/httpService";
+import { Shard, UploadMetadata } from "../model/shard";
+import { StorageUri } from "../model/storageUri";
+import { TestResult } from "../model/testResult";
+import { TestRun } from "../model/testRun";
+import { CIInfo } from "./cIInfoProvider";
+import ReporterUtils from "./reporterUtils";
+import { PipelineResponse } from "@azure/core-rest-pipeline";
+
+export class ServiceClient {
+  private httpService: HttpService;
+  private readonly envVariables: EnvironmentVariables;
+  private readonly reporterUtils: ReporterUtils;
+
+  // eslint-disable-next-line @azure/azure-sdk/ts-use-interface-parameters
+  constructor(envVariables: EnvironmentVariables, reporterUtils: ReporterUtils) {
+    this.httpService = new HttpService();
+    this.envVariables = envVariables;
+    this.reporterUtils = reporterUtils;
+  }
+
+  async patchTestRun(ciInfo: CIInfo): Promise<TestRun | undefined> {
+    const testRun = await this.reporterUtils.getTestRunObject(ciInfo);
+    const response: PipelineResponse = await this.httpService.callAPI(
+      "PATCH",
+      `${this.getServiceEndpoint()}/${Constants.testRunsEndpoint.replace("{workspaceId}", this.envVariables.accountId!)}/${this.envVariables.runId}?api-version=${Constants.API_VERSION}`,
+      JSON.stringify(testRun),
+      this.envVariables.accessToken,
+      this.envVariables.correlationId!,
+    );
+    if (response.status === 200) {
+      return JSON.parse(response.bodyAsText!) as TestRun;
+    } else if (response.status === 409) {
+      process.stdout.write(
+        `\n${Constants.CONFLICT_409_ERROR_MESSAGE.replace("{runId}", this.envVariables.runId)}`,
+      );
+    } else if (response.status === 403) {
+      process.stdout.write(
+        `\n${Constants.FORBIDDEN_403_ERROR_MESSAGE.replace(new RegExp("{workspaceId}", "g"), this.envVariables.accountId!)}`,
+      );
+    } else {
+      throw new Error(`Received status ${response.status} from service from PATCH TestRun call.`);
+    }
+    return; // Not all code paths return a value (warning fix).
+  }
+
+  async getTestRun(): Promise<TestRun> {
+    const response: PipelineResponse = await this.httpService.callAPI(
+      "GET",
+      `${this.getServiceEndpoint()}/${Constants.testRunsEndpoint.replace("{workspaceId}", this.envVariables.accountId!).concat(`/${this.envVariables.runId}?api-version=${Constants.API_VERSION}`)}`,
+      null,
+      this.envVariables.accessToken,
+      this.envVariables.correlationId!,
+    );
+    if (response.status === 200) {
+      return JSON.parse(response.bodyAsText!) as TestRun;
+    } else {
+      throw new Error(`Received status ${response.status} from service from GET TestRun call.`);
+    }
+  }
+
+  async patchTestRunShardStart(): Promise<Shard> {
+    const patchTestRunShardObject = this.reporterUtils.getTestRunShardStartObject();
+    const response: PipelineResponse = await this.httpService.callAPI(
+      "PATCH",
+      `${this.getServiceEndpoint()}/${Constants.testRunsShardEndpoint.replace("{workspaceId}", this.envVariables.accountId!).replace("{testRunId}", this.envVariables.runId).replace("{shardId}", this.envVariables.shardId!)}/?api-version=${Constants.API_VERSION}`,
+      JSON.stringify(patchTestRunShardObject),
+      this.envVariables.accessToken,
+      this.envVariables.correlationId!,
+    );
+    if (response.status === 200) {
+      return JSON.parse(response.bodyAsText!) as Shard;
+    } else {
+      throw new Error(
+        `Received status ${response.status} from service from PATCH TestRun Shard Start call.`,
+      );
+    }
+  }
+
+  async patchTestRunShardEnd(
+    result: FullResult,
+    // eslint-disable-next-line @azure/azure-sdk/ts-use-interface-parameters
+    shard: Shard,
+    errorMessages: string[],
+    attachmentMetadata: UploadMetadata,
+  ): Promise<TestRun> {
+    const patchTestRunShardObject = this.reporterUtils.getTestRunShardEndObject(
+      result,
+      shard,
+      errorMessages,
+      attachmentMetadata,
+    );
+    const response: PipelineResponse = await this.httpService.callAPI(
+      "PATCH",
+      `${this.getServiceEndpoint()}/${Constants.testRunsShardEndpoint.replace("{workspaceId}", this.envVariables.accountId!).replace("{testRunId}", this.envVariables.runId).replace("{shardId}", this.envVariables.shardId!)}/?api-version=${Constants.API_VERSION}`,
+      JSON.stringify(patchTestRunShardObject),
+      this.envVariables.accessToken,
+      this.envVariables.correlationId!,
+    );
+    if (response.status === 200) {
+      return JSON.parse(response.bodyAsText!) as TestRun;
+    } else {
+      throw new Error(
+        `Received status ${response.status} from service from PATCH TestRun Shard End call.`,
+      );
+    }
+  }
+
+  // eslint-disable-next-line @azure/azure-sdk/ts-use-interface-parameters
+  async postTestResults(testResults: TestResult[]): Promise<void> {
+    const payload: any = {
+      value: testResults,
+    };
+    const response: PipelineResponse = await this.httpService.callAPI(
+      "POST",
+      `${this.getServiceEndpoint()}/${Constants.testResultsEndpoint.replace("{workspaceId}", this.envVariables.accountId!)}?api-version=${Constants.API_VERSION}`,
+      JSON.stringify(payload),
+      this.envVariables.accessToken,
+      this.envVariables.correlationId!,
+    );
+    if (response.status === 200) {
+      return;
+    } else {
+      throw new Error(
+        `Received status ${response.status} from service from POST TestResults call.`,
+      );
+    }
+  }
+
+  async getStorageUri(): Promise<StorageUri> {
+    const response: PipelineResponse = await this.httpService.callAPI(
+      "GET",
+      `${this.getServiceEndpoint()}/${Constants.storageUriEndpoint.replace("{workspaceId}", this.envVariables.accountId!).replace("{testRunId}", this.envVariables.runId)}?api-version=${Constants.API_VERSION}`,
+      null,
+      this.envVariables.accessToken,
+      this.envVariables.correlationId!,
+    );
+    if (response.status === 200) {
+      return JSON.parse(response.bodyAsText!) as StorageUri;
+    } else {
+      throw new Error(`Received status ${response.status} from service from GET StorageUri call.`);
+    }
+  }
+
+  private getServiceEndpoint(): string {
+    return process.env["PLAYWRIGHT_SERVICE_REPORTING_URL"]!;
+  }
+}
