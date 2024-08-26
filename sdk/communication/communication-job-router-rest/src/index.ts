f4ad18c065c900516c1caad4076f377bab022371
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-08-26 17:42:22 +0800

    update

--- sdk/communication/communication-job-router-rest/src/index.ts
+++ sdk/communication/communication-job-router-rest/src/index.ts
@@ -3,13 +3,13 @@
 
 import AzureCommunicationRoutingServiceClient from "./azureCommunicationRoutingServiceClient";
 
-export * from "./azureCommunicationRoutingServiceClient";
-export * from "./parameters";
-export * from "./responses";
-export * from "./clientDefinitions";
-export * from "./isUnexpected";
-export * from "./models";
-export * from "./outputModels";
-export * from "./paginateHelper";
+export * from "../generated/azureCommunicationRoutingServiceClient";
+export * from "../generated/parameters";
+export * from "../generated/responses";
+export * from "../generated/clientDefinitions";
+export * from "../generated/isUnexpected";
+export * from "../generated/models";
+export * from "../generated/outputModels";
+export * from "../generated/paginateHelper";
 
 export default AzureCommunicationRoutingServiceClient;
