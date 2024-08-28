08c81d2240c68ff932f6f2730c8f65247d0a0bcc
Matthew Podwysocki <matthewp@microsoft.com>
2024-08-28 10:38:43 -0400

    [cosmos] Fix usage of process

--- sdk/cosmosdb/cosmos/src/utils/envUtils.ts
+++ sdk/cosmosdb/cosmos/src/utils/envUtils.ts
@@ -4,5 +4,7 @@
 import process from "node:process";
 import { Constants } from "../common/constants";
 
-export const disableListAndSetAggregate: boolean = process.env.DISABLE_LIST_AND_SET_AGGREGATE === "true";
-export const diagnosticLevelFromEnv: string | undefined = process.env[Constants.CosmosDbDiagnosticLevelEnvVarName];
+export const disableListAndSetAggregate: boolean =
+  process.env.DISABLE_LIST_AND_SET_AGGREGATE === "true";
+export const diagnosticLevelFromEnv: string | undefined =
+  process.env[Constants.CosmosDbDiagnosticLevelEnvVarName];
