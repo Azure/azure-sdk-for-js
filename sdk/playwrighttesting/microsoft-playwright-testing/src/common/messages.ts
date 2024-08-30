8e6648cc3ff1322af8322057fa93abf88ef7efc9
Kashish Gupta <90824921+kashish2508@users.noreply.github.com>
2024-08-30 17:19:47 +0530

    chore(playwrighttesting): added error scenarios for auth and API failures (#30952)
    ### Packages impacted by this PR
    @azure/microsoft-playwright-testing
    
    ### Issues associated with this PR
    Display informational messages to the user in case of errors during test
    run.
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Siddharth Singha Roy <ssingharoy@microsoft.com>

--- sdk/playwrighttesting/microsoft-playwright-testing/src/common/messages.ts
+++ sdk/playwrighttesting/microsoft-playwright-testing/src/common/messages.ts
@@ -4,9 +4,11 @@
 export const ServiceErrorMessageConstants = {
   NO_AUTH_ERROR:
     "Could not authenticate with the service. Please refer to https://aka.ms/mpt/authentication for more information.", // no mpt pat set and could not fetch entra token
-  NO_SERVICE_URL_ERROR: "Please set PLAYWRIGHT_SERVICE_URL in your environment variables.",
-  INVALID_MPT_PAT_ERROR: "The Access Token provided in the environment variable is invalid.",
-  EXPIRED_MPT_PAT_ERROR: "The Access Token you are using is expired. Create a new token.",
+  NO_SERVICE_URL_ERROR:
+    "The value for the PLAYWRIGHT_SERVICE_URL variable is not set correctly. Please verify the URL and try again.",
+  INVALID_MPT_PAT_ERROR:
+    "The authentication token provided is invalid. Please check the token and try again.",
+  EXPIRED_MPT_PAT_ERROR: "Your authentication token has expired. Please create a new token.",
   INVALID_GLOBAL_FUNCTION: "file must export a single function",
   DUPLICATE_REPORTER:
     "Another reporter with the same name already exists. Please resolve the duplicate and try again.",
