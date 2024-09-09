9f5503619a8f70632e0754d956e909a3df68fc33
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-09-09 20:42:58 +0000

    update version

--- sdk/openai/openai-assistants/src/assistantsClient.ts
+++ sdk/openai/openai-assistants/src/assistantsClient.ts
@@ -111,8 +111,8 @@ import { AssistantsContext } from "./rest/index.js";
 export { AssistantsClientOptions } from "./api/AssistantsContext.js";
 
 /**
- * @deprecated The Azure OpenAI Assistants client library for JavaScript beta will be retired on January 14th, 2025. Please migrate to the stable OpenAI SDK for JavaScript using the [migration guide](https://aka.ms/azjssdk_migration) before that date. 
  * Client handling assistant-related operations.
+ * @deprecated The Azure OpenAI Assistants client library for JavaScript beta will be retired on January 14th, 2025. Please migrate to the stable OpenAI SDK for JavaScript using the [migration guide](https://aka.ms/azjssdk_migration) before that date.
  */
 export class AssistantsClient {
   /** The pipeline used by this client to make requests */
