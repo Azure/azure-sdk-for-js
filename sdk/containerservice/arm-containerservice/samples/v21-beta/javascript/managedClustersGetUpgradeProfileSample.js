6cd29bb70438808c12ae3cddadd10bb755147e05
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-09-10 15:04:03 +0800

    [mgmt] containerservice preivew release

--- sdk/containerservice/arm-containerservice/samples/v21-beta/javascript/managedClustersGetUpgradeProfileSample.js
+++ sdk/containerservice/arm-containerservice/samples/v21-beta/javascript/managedClustersGetUpgradeProfileSample.js
@@ -16,7 +16,7 @@ require("dotenv").config();
  * This sample demonstrates how to Gets the upgrade profile of a managed cluster.
  *
  * @summary Gets the upgrade profile of a managed cluster.
- * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-05-02-preview/examples/ManagedClustersGetUpgradeProfile.json
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-06-02-preview/examples/ManagedClustersGetUpgradeProfile.json
  */
 async function getUpgradeProfileForManagedCluster() {
   const subscriptionId =
