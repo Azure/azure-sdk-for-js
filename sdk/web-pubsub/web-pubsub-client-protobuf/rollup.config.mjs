17104f444cf5c1082282e31fdb70856077820095
Matthew Podwysocki <matthewp@microsoft.com>
2024-09-10 10:55:31 -0400

    [web pub sub] update protobuf to ESM

--- sdk/web-pubsub/web-pubsub-client-protobuf/rollup.config.mjs
+++
@@ -1,6 +0,0 @@
-import config from "@azure/dev-tool/shared-config/rollup.js";
-import { readFile } from "node:fs/promises";
-
-const { makeConfig } = config;
-const pkg = JSON.parse(await readFile("./package.json", { encoding: "utf-8" }));
-export default makeConfig(pkg, { disableBrowserBundle: true });
