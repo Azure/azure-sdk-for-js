ebb399928ec34ab7b5ac23d0e5937679ffdb553d
Matthew Podwysocki <matthewp@microsoft.com>
2024-09-13 14:06:49 -0400

    [notification-hubs] Separate broadcast from send operations (#31102)
    ### Packages impacted by this PR
    
    - @azure/notification-hubs
    
    ### Issues associated with this PR
    
    - #30140
    
    ### Describe the problem that is addressed by this PR
    
    Separates the broadcast send and broadcast scheduled send into their own
    methods instead of having in the base `sendNotification` and
    `scheduleNotification` respectively. Creates two methods of
    `sendBroadcastNotification` and `scheduleBroadcastNotification` for each
    scenario.
    
    cc @gunzip 
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    This allows us to keep required fields for either devices or tag based
    expressions for mainline sends and tag based for scheduled sends in the
    main methods and only separate the broadcast parts which are less used.
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [x] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [x] Added a changelog (if necessary)

--- sdk/notificationhubs/notification-hubs/src/api/sendBroadcastNotification.ts
+++ sdk/notificationhubs/notification-hubs/src/api/sendBroadcastNotification.ts
@@ -1,23 +1,23 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { DirectSendNotificationOptions, SendNotificationOptions } from "../models/options.js";
+import { BroadcastSendNotificationOptions } from "../models/options.js";
 import { Notification } from "../models/notification.js";
 import { NotificationHubsClientContext } from "./index.js";
 import { NotificationHubsMessageResponse } from "../models/notificationDetails.js";
 import { sendNotificationInternal } from "./internal/_sendNotification.js";
 
 /**
- * Sends push notifications to devices that match the given tags or tag expression.
+ * Sends push notifications to all devices with a broadcast send.
  * @param context - The Notification Hubs client.
  * @param notification - The notification to send to the matching devices.
- * @param options - Options for the notification including tags, device handles and whether to enable test send.
+ * @param options - Options for the notification including whether to enable test send.
  * @returns A NotificationHubResponse with the tracking ID, correlation ID and location.
  */
-export function sendNotification(
+export function sendBroadcastNotification(
   context: NotificationHubsClientContext,
   notification: Notification,
-  options: DirectSendNotificationOptions | SendNotificationOptions,
+  options: BroadcastSendNotificationOptions = {},
 ): Promise<NotificationHubsMessageResponse> {
   return sendNotificationInternal(context, notification, options);
 }
