ac82d226e729a1fcbd2263976c65f56c18a2653c
Karishma Ghiya <kaghiya@microsoft.com>
2024-09-17 18:35:08 -0700

    update

---
+++ sdk/identity/identity-broker/test/manual/node/popTokenClient.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { CommonClientOptions, ServiceClient } from "@azure/core-client";
+
+export class PopTokenClient extends ServiceClient {
+
+}
+
+export interface PopTokenClientOptions extends CommonClientOptions {
+
+}
