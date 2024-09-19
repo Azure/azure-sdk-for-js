4451b8280fa290ca5e9ad95d2ea946f18f7c6065
Karishma Ghiya <kaghiya@microsoft.com>
2024-09-19 11:17:53 -0700

    update pop token policy

--- sdk/identity/identity-broker/test/manual/node/popTokenClient.ts
+++ sdk/identity/identity-broker/test/manual/node/popTokenClient.ts
@@ -3,10 +3,32 @@
 
 import { CommonClientOptions, ServiceClient } from "@azure/core-client";
 
-export class PopTokenClient extends ServiceClient {
+export class PopTokenClient extends ServiceClient {}
 
-}
+export interface PopTokenClientOptions extends CommonClientOptions {}
 
-export interface PopTokenClientOptions extends CommonClientOptions {
+import {
+  createEmptyPipeline,
+  bearerTokenAuthenticationPolicy,
+  createPipelineRequest,
+  createDefaultHttpClient,
+} from "@azure/core-rest-pipeline";
+import { popTokenAuthenticationPolicy } from "./popTokenAuthenticationPolicy";
 
+async function sendGraphRequest() {
+  const pipeline = createEmptyPipeline();
+  // how to create pop policy?
+  pipeline.addPolicy(popTokenAuthenticationPolicy());
+
+  const req = createPipelineRequest({
+    url: "https://graph...",
+  });
+
+  const client = createDefaultHttpClient();
+
+  const result = await pipeline.sendRequest(client, req);
+
+  // assert something on the result
+  result.status;
+  result.bodyAsText;
 }
