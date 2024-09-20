2e4a12f3a81046ec8e8bded74869a3d3788df50f
Maor Leger <maorleger@users.noreply.github.com>
2024-09-20 08:54:24 -0700

    [tracing] Add support for addEvent (#31162)
    ### Packages impacted by this PR
    
    @azure/core-tracing
    @azure/opentelemetry-instrumentation-azure-sdk
    @typespec/ts-http-runtime
    
    ### Issues associated with this PR
    
    Resolves #31158
    
    ### Describe the problem that is addressed by this PR
    
    OpenTelemetry supports adding events to a span, and part of the gen-ai
    specification expects events to be added on a span. 
    
    This PR adds support for the `addEvent` API in our core-tracing library
    as well as our otel-sdk.
    
    Adding a required field is a breaking change, so this is added as
    optional. The implementation will need to use the safe-navigation 
    operator syntax or check for the function's existence before calling it.
    
    In passing, I am updating some of our internal transformations to use
    OTel's APIs instead.

--- sdk/core/ts-http-runtime/src/tracing/interfaces.ts
+++ sdk/core/ts-http-runtime/src/tracing/interfaces.ts
@@ -209,6 +209,20 @@ export type SpanStatusError = { status: "error"; error?: Error | string };
 export type SpanStatus = SpanStatusSuccess | SpanStatusError;
 
 /**
+ * Represents options you can pass to {@link TracingSpan.addEvent}.
+ */
+export interface AddEventOptions {
+  /**
+   * A set of attributes to attach to the event.
+   */
+  attributes?: Record<string, unknown>;
+  /**
+   * The start time of the event.
+   */
+  startTime?: Date;
+}
+
+/**
  * Represents an implementation agnostic tracing span.
  */
 export interface TracingSpan {
@@ -248,6 +262,11 @@ export interface TracingSpan {
    * Depending on the span implementation, this may return false if the span is not being sampled.
    */
   isRecording(): boolean;
+
+  /**
+   * Adds an event to the span.
+   */
+  addEvent?(name: string, options?: AddEventOptions): void;
 }
 
 /** An immutable context bag of tracing values for the current operation. */
