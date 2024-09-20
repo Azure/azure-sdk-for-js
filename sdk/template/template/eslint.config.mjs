8597959155be03b8ee402c838290c488b9dbdf68
Matthew Podwysocki <matthewp@microsoft.com>
2024-09-20 19:05:10 -0400

    [dev-tool] Add snippets extraction (#31143)
    ### Packages impacted by this PR
    
    - @azure/dev-tool
    
    ### Issues associated with this PR
    
    ### Describe the problem that is addressed by this PR
    
    This PR implements a new dev-tool command: `dev-tool run
    update-snippets`.
    
    This command looks for code fences in markdown files and JSDoc comments,
    and updates them with the contents of test methods in a file named
    `snippets.spec.ts`.
    
    For example, the following fence indicates that the contents of a test
    named "new_configurationclient" should be used:
    
    ````
    ```js snippet:new_configurationclient
    ```
    ````
    
    After running `dev-tool run update-snippets`, the contents of the
    snippet will be populated:
    
    ````
    ```js snippet:new_configurationclient
    import { ConfigurationClient } from "@azure/template";
    import { DefaultAzureCredential } from "@azure/identity";
    
    const client = new ConfigurationClient(
      "<app configuration endpoint>",
      new DefaultAzureCredential()
    );
    ```
    ````
    
    To accomplish this, the command uses the TypeScript compiler API to
    extract and transpile snippets from `snippets.spec.ts`. Snippets are the
    contents of calls to the `it` function. If syntax with the shape
    `it(<literal string>, <function with block>)` appears in
    `snippets.spec.ts`, it will be considered a snippet that is valid for
    injection.
    
    ("Function with block" means either a `function () { ... }` expression
    or an arrow function with a block on the arrow side (`() => { ... }`).
    An arrow function that has an expression on the right hand side (`() =>
    (...)`) will not be recognized.)
    
    For example:
    
    ```ts
      it("new_configurationclient", function () {
        // @ts-ignore
        const client = new ConfigurationClient(
          process.env.ENDPOINT ?? "<app configuration endpoint>",
          new DefaultAzureCredential()
        );
      });
    ```
    
    The transpiler automatically "cleans" and validates the snippet using
    similar techniques as the sample transpiler. As a result, it enforces
    the same syntactic rules that the sample transpiler does. In addition to
    those, it removes references to `process.env` (if an alternative is
    specified), removes compiler pragmas like `// @ts-ignore`, and
    automatically inserts imports for symbols that the snippet uses. So in
    the above snippet, imports for `ConfigurationClient` and
    `DefaultAzureCredential` are required, automatically detected, and
    injected into the resulting snippet.
    
    Snippets without `snippet:${name}` tags are _errors_ when using this
    command, so a package must be fully migrated to use it.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    - #24536
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/template/template/eslint.config.mjs
+++ sdk/template/template/eslint.config.mjs
@@ -1,13 +1,7 @@
 import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default [
-  { ignores: ["test/manual*"] },
   ...azsdkEslint.configs.recommended,
-  {
-    rules: {
-      "@azure/azure-sdk/ts-naming-options": "warn",
-    },
-  },
   {
     files: ["test/snippets.spec.ts"],
     rules: {
