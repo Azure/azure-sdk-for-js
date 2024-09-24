56f689f99578349a9762724b7d97c7b7999b3584
Maor Leger <maorleger@microsoft.com>
2024-09-24 17:45:24 +0000

    debug windows quirk

--- common/tools/dev-tool/src/commands/run/build-package.ts
+++ common/tools/dev-tool/src/commands/run/build-package.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { concurrently, ConcurrentlyCommandInput } from "concurrently";
 import { leafCommand, makeCommandInfo } from "../../framework/command";
 import { createPrinter } from "../../util/printer";
 import { run } from "../../util/run";
@@ -11,11 +12,31 @@ export const commandInfo = makeCommandInfo("build-package", "build a package for
 
 export default leafCommand(commandInfo, async () => {
   log.info("Building package with tshy...");
+  const command: ConcurrentlyCommandInput = {
+    command: "tshy",
+  };
+
+  log.info("Building with concurrently and no args");
+  await concurrently([command]).result;
+  log.info("success");
+
+  log.info("Building with concurrently and env vars");
+  await concurrently([
+    {
+      command: "tshy",
+      env: {
+        ...process.env,
+        TSHY_VERBOSE: "2",
+      },
+    },
+  ]).result;
+  log.info("success");
+
+  log.info("Building with run and no args");
   const output = await run("tshy", {
     captureOutput: true,
-    cwd: process.cwd(),
-    env: { TSHY_VERBOSE: "2", ...process.env },
   });
+  log.info("success");
 
   log.debug("tshy output:");
   log.debug(output);
