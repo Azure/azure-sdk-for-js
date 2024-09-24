b1dc798927f5ea605ff9c40a23255696be397e6a
Matthew Podwysocki <matthewp@microsoft.com>
2024-09-24 17:27:31 -0400

    [core] Add README snippets for core

--- sdk/core/core-client/vitest.config.ts
+++ sdk/core/core-client/vitest.config.ts
@@ -3,11 +3,15 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.shared.config.ts";
+import { resolve } from "node:path";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      alias: {
+        "../commonjs/state.js": resolve("./src/state-cjs.cts"),
+      },
       include: ["test/*.spec.ts"],
       exclude: ["test/snippets.spec.ts"],
     },
