5125b567b32560f6eae1cde94dfca2e8e2b27b0b
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-09-24 14:45:26 +0800

    [mgmt] containerservice preivew release (#31054)
    https://github.com/Azure/sdk-release-request/issues/5434

--- sdk/containerservice/arm-containerservice/samples-dev/loadBalancersCreateOrUpdateSample.ts
+++ sdk/containerservice/arm-containerservice/samples-dev/loadBalancersCreateOrUpdateSample.ts
@@ -18,7 +18,7 @@ dotenv.config();
  * This sample demonstrates how to Creates or updates a load balancer in the specified managed cluster.
  *
  * @summary Creates or updates a load balancer in the specified managed cluster.
- * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-05-02-preview/examples/LoadBalancers_Create_Or_Update.json
+ * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/preview/2024-06-02-preview/examples/LoadBalancers_Create_Or_Update.json
  */
 async function createOrUpdateALoadBalancer() {
   const subscriptionId =
