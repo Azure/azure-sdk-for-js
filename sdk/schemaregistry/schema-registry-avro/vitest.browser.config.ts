a461fa4107b2393159739e049db17493db93d541
Minh-Anh Phan <minhanhphan@microsoft.com>
2024-09-24 23:06:20 +0000

    inject buffer

--- sdk/schemaregistry/schema-registry-avro/vitest.browser.config.ts
+++ sdk/schemaregistry/schema-registry-avro/vitest.browser.config.ts
@@ -4,10 +4,19 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
+import browserMap from "@azure-tools/vite-plugin-browser-test-map";
+import inject from "@rollup/plugin-inject";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    optimizeDeps: {
+      include: ["buffer"],
+    },
+    plugins: [
+      browserMap(),
+      inject({ Buffer: ["buffer", "Buffer"] }),
+    ],
     test: {
       fileParallelism: false,
       include: [
