39179471231453307ac8687389409f000565ca62
Maor Leger <maorleger@microsoft.com>
2024-09-24 18:05:47 +0000

    use concurrently

--- common/tools/dev-tool/src/commands/run/build-package.ts
+++ common/tools/dev-tool/src/commands/run/build-package.ts
@@ -1,45 +1,29 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { concurrently, ConcurrentlyCommandInput } from "concurrently";
+import { concurrently } from "concurrently";
 import { leafCommand, makeCommandInfo } from "../../framework/command";
 import { createPrinter } from "../../util/printer";
-import { run } from "../../util/run";
 
 const log = createPrinter("build-package");
 
 export const commandInfo = makeCommandInfo("build-package", "build a package for production");
 
+// // Create an output stream to capture the output of the build command
+// const outStream = new WriteStream();
+
 export default leafCommand(commandInfo, async () => {
   log.info("Building package with tshy...");
-  const command: ConcurrentlyCommandInput = {
-    command: "tshy",
-  };
 
-  log.info("Building with concurrently and no args");
-  await concurrently([command]).result;
-  log.info("success");
-
-  log.info("Building with concurrently and env vars");
-  await concurrently([
-    {
-      command: "tshy",
-      env: {
-        ...process.env,
-        TSHY_VERBOSE: "2",
+  await concurrently(
+    [
+      {
+        command: "tshy",
       },
-    },
-  ]).result;
-  log.info("success");
+    ],
+    { raw: true },
+  ).result;
 
-  log.info("Building with run and no args");
-  const output = await run("tshy", {
-    captureOutput: true,
-  });
-  log.info("success");
-
-  log.debug("tshy output:");
-  log.debug(output);
   log.info("Package built successfully.");
 
   return true;
