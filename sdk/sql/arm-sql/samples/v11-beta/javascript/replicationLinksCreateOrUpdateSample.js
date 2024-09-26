fd0f48e819765d17e09e7d8a0c4f48454b8ee525
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-09-26 11:41:22 +0800

    [mgmt] sql release (#30693)
    https://github.com/Azure/sdk-release-request/issues/5379

--- sdk/sql/arm-sql/samples/v11-beta/javascript/replicationLinksCreateOrUpdateSample.js
+++ sdk/sql/arm-sql/samples/v11-beta/javascript/replicationLinksCreateOrUpdateSample.js
@@ -13,31 +13,33 @@ const { DefaultAzureCredential } = require("@azure/identity");
 require("dotenv").config();
 
 /**
- * This sample demonstrates how to Deletes the replication link.
+ * This sample demonstrates how to Updates the replication link type.
  *
- * @summary Deletes the replication link.
- * x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2023-05-01-preview/examples/ReplicationLinkDelete.json
+ * @summary Updates the replication link type.
+ * x-ms-original-file: specification/sql/resource-manager/Microsoft.Sql/preview/2023-05-01-preview/examples/ReplicationLinkCreateOrUpdate.json
  */
-async function deleteReplicationLinkOnServer() {
+async function updatesReplicationLink() {
   const subscriptionId =
     process.env["SQL_SUBSCRIPTION_ID"] || "00000000-1111-2222-3333-444444444444";
   const resourceGroupName = process.env["SQL_RESOURCE_GROUP"] || "Default";
   const serverName = "sourcesvr";
   const databaseName = "gamma-db";
-  const linkId = "4891ca10-ebd0-47d7-9182-c722651780fb";
+  const linkId = "00000000-1111-2222-3333-666666666666";
+  const parameters = { linkType: "STANDBY" };
   const credential = new DefaultAzureCredential();
   const client = new SqlManagementClient(credential, subscriptionId);
-  const result = await client.replicationLinks.beginDeleteAndWait(
+  const result = await client.replicationLinks.beginCreateOrUpdateAndWait(
     resourceGroupName,
     serverName,
     databaseName,
     linkId,
+    parameters,
   );
   console.log(result);
 }
 
 async function main() {
-  deleteReplicationLinkOnServer();
+  updatesReplicationLink();
 }
 
 main().catch(console.error);
