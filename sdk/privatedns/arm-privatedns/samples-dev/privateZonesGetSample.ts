86b305b49c99c9d3eaa40727a487612fff84ca4c
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-09-26 13:49:43 +0800

    [mgmt] privatedns release (#31092)
    https://github.com/Azure/sdk-release-request/issues/5466
    
    ---------
    
    Co-authored-by: Mary Gao <yanmeigao1210@gmail.com>
    Co-authored-by: Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>

--- sdk/privatedns/arm-privatedns/samples-dev/privateZonesGetSample.ts
+++ sdk/privatedns/arm-privatedns/samples-dev/privateZonesGetSample.ts
@@ -18,7 +18,7 @@ dotenv.config();
  * This sample demonstrates how to Gets a Private DNS zone. Retrieves the zone properties, but not the virtual networks links or the record sets within the zone.
  *
  * @summary Gets a Private DNS zone. Retrieves the zone properties, but not the virtual networks links or the record sets within the zone.
- * x-ms-original-file: specification/privatedns/resource-manager/Microsoft.Network/stable/2020-06-01/examples/PrivateZoneGet.json
+ * x-ms-original-file: specification/privatedns/resource-manager/Microsoft.Network/stable/2024-06-01/examples/PrivateZoneGet.json
  */
 async function getPrivateDnsZone() {
   const subscriptionId =
@@ -30,7 +30,7 @@ async function getPrivateDnsZone() {
   const client = new PrivateDnsManagementClient(credential, subscriptionId);
   const result = await client.privateZones.get(
     resourceGroupName,
-    privateZoneName
+    privateZoneName,
   );
   console.log(result);
 }
