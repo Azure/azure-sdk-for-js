120f06cc344428bedaa11095b01c825f92a3eb2a
Karishma Ghiya <kaghiya@microsoft.com>
2024-10-02 12:18:52 -0700

    update

--- sdk/identity/identity/test/public/node/azurePipelinesCredential.spec.ts
+++ sdk/identity/identity/test/public/node/azurePipelinesCredential.spec.ts
@@ -22,7 +22,7 @@ describe.only("AzurePipelinesCredential", function () {
       tenantId,
       clientId,
       existingServiceConnectionId,
-      systemAccessToken,
+      systemAccessToken
     );
     const token = await credential.getToken(scope);
     assert.ok(token?.token);
@@ -41,14 +41,14 @@ describe.only("AzurePipelinesCredential", function () {
       tenantId,
       clientId,
       "existingServiceConnectionId",
-      systemAccessToken,
+      systemAccessToken
     );
     const regExp: RegExp =
       /invalid_client: Error\(s\): 700213 .* AADSTS700213: No matching federated identity record found for presented assertion subject .* Please note that the matching is done using a case-sensitive comparison. Check your federated identity credential Subject, Audience and Issuer against the presented assertion/;
     await assert.isRejected(
       credential.getToken(scope),
       regExp,
-      "error thrown doesn't match or promise not rejected",
+      "error thrown doesn't match or promise not rejected"
     );
   });
 
@@ -58,30 +58,37 @@ describe.only("AzurePipelinesCredential", function () {
     }
     // clientId for above service connection
     const clientId = process.env.AZURE_SERVICE_CONNECTION_CLIENT_ID!;
-    const existingServiceConnectionId = process.env.AZURE_SERVICE_CONNECTION_ID!
+    const existingServiceConnectionId = process.env.AZURE_SERVICE_CONNECTION_ID!;
     // const systemAccessToken = process.env.SYSTEM_ACCESSTOKEN!;
     const credential = new AzurePipelinesCredential(
       tenantId,
       clientId,
       existingServiceConnectionId,
-      "systemAccessToken",
+      "systemAccessToken"
     );
+    const regExpHeader: RegExp = /"x-vss-e2eid"/gm;
     const regExpHeader1: RegExp =
       /\["x-vss-e2eid"\] - \"[a-fA-F0-9]{8}-([a-f0-9A-F]{4}-){3}[0-9a-fA-F]{12}\"/gm;
     const regExpHeader2: RegExp = /\["x-msedge-ref"\] - "[a-zA-Z\s:0-9-]*"/gm;
+    await assert.isRejected(
+      credential.getToken(scope),
+      regExpHeader,
+      "error thrown doesn't contain expected header"
+    );
+    await assert.isRejected(
+      credential.getToken(scope),
+      regExpHeader1,
+      "error thrown doesn't contain expected header1"
+    );
+    await assert.isRejected(
+      credential.getToken(scope),
+      regExpHeader2,
+      "error thrown doesn't contain expected header2"
+    );
     try {
-     await credential.getToken(scope)
-    }
-    catch(e){
-      console.log(e)
-      const match1 = (e as string).match(regExpHeader1);
-      if(match1){
-        console.log(match1)
-      }
-      const match2 = (e as string).match(regExpHeader2);
-      if(match2){
-        console.log(match2)
-      }
+      await credential.getToken(scope);
+    } catch (e) {
+      console.log(e);
     }
   });
 
@@ -95,14 +102,14 @@ describe.only("AzurePipelinesCredential", function () {
       tenantId,
       "clientId",
       existingServiceConnectionId,
-      systemAccessToken,
+      systemAccessToken
     );
     const regExp: RegExp =
       /AADSTS700016: Application with identifier 'clientId' was not found in the directory 'Microsoft'/;
     await assert.isRejected(
       credential.getToken(scope),
       regExp,
-      "error thrown doesn't match or promise not rejected",
+      "error thrown doesn't match or promise not rejected"
     );
   });
 
@@ -116,7 +123,7 @@ describe.only("AzurePipelinesCredential", function () {
       tenantId,
       clientId,
       existingServiceConnectionId,
-      "systemAccessToken",
+      "systemAccessToken"
     );
     await assert.isRejected(credential.getToken(scope), /Status code: 401/);
   });
