c26ce0cce735bbd49862d3ae28dda25426e4a111
Matthew Podwysocki <matthewp@microsoft.com>
2024-10-04 15:20:53 -0400

    [template] Add retry/timeout to macOS tests

--- sdk/template/template/test/public/configurationClient.spec.ts
+++ sdk/template/template/test/public/configurationClient.spec.ts
@@ -80,25 +80,20 @@ describe("[AAD] ConfigurationClient functional tests", function () {
   });
 
   describe("#getConfigurationSetting", () => {
-    // TODO: This test is skipped on macOS pending investigation (see https://github.com/Azure/azure-sdk-for-js/issues/31296)
-    it.skipIf(typeof process !== "undefined" && process.platform === "darwin")(
-      "predetermined setting has expected value",
-      async () => {
-        const key = assertEnvironmentVariable("APPCONFIG_TEST_SETTING_KEY");
-        const expectedValue = assertEnvironmentVariable("APPCONFIG_TEST_SETTING_EXPECTED_VALUE");
+    it("predetermined setting has expected value", { timeout: 50000, retry: 3 }, async () => {
+      const key = assertEnvironmentVariable("APPCONFIG_TEST_SETTING_KEY");
+      const expectedValue = assertEnvironmentVariable("APPCONFIG_TEST_SETTING_EXPECTED_VALUE");
 
-        const setting = await client.getConfigurationSetting(key);
+      const setting = await client.getConfigurationSetting(key);
 
-        // Make sure the key returned is the same as the key we asked for
-        assert.equal(key, setting.key);
+      // Make sure the key returned is the same as the key we asked for
+      assert.equal(key, setting.key);
 
-        // Make sure the value of the setting is the same as the value we entered
-        // on the environment
-        assert.equal(expectedValue, setting.value);
-      },
-    );
+      // Make sure the value of the setting is the same as the value we entered
+      // on the environment
+      assert.equal(expectedValue, setting.value);
+    });
 
-    // TODO: Waiting on https://github.com/Azure/azure-sdk-for-js/issues/29287
     // The supportsTracing assertion from chaiAzure can be used to verify that
     // the `getConfigurationSetting` method is being traced correctly, that the
     // tracing span is properly parented and closed.
