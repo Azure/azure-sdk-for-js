ea2c4b0553b43213bfc1beb23daa35906f7389f8
KarishmaGhiya <kghiya8@gmail.com>
2024-10-07 10:33:27 -0700

    Update sdk/identity/identity/src/errors.ts
    Co-authored-by: Maor Leger <maorleger@users.noreply.github.com>

--- sdk/identity/identity/src/errors.ts
+++ sdk/identity/identity/src/errors.ts
@@ -140,7 +140,6 @@ export class AuthenticationError extends Error {
         errorDescription: "An unknown error occurred and no additional details are available.",
       };
     }
-    console.log(errorResponse.error);
 
     super(
       `${errorResponse.error} Status code: ${statusCode}\nMore details:\n${errorResponse.errorDescription},`,
