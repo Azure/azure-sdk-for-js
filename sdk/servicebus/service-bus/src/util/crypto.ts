5c05d8a7c6d4b6dce13e6c527a5d73bd7e3cc472
Jeremy Meng <jeremy.ymeng@gmail.com>
2024-10-08 10:38:13 -0700

    minor cleanup

--- sdk/servicebus/service-bus/src/util/crypto.ts
+++ sdk/servicebus/service-bus/src/util/crypto.ts
@@ -1,10 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-// eslint-disable-next-line @typescript-eslint/no-redeclare
 import crypto from "node:crypto";
 
 /**
