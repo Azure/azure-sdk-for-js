a9441df1ad0701cff0a9de43ae11a9b1adc512d1
Maor Leger <maorleger@microsoft.com>
2024-10-08 23:00:34 +0000

    wip

--- eng/tools/dependency-testing/templates/vitest.dependency-test.config.ts
+++ eng/tools/dependency-testing/templates/vitest.dependency-test.config.ts
@@ -1,10 +1,30 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import path from "node:path";
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../../../vitest.shared.config.ts";
 
-export default mergeConfig(
+// The idea is as follows:
+
+// 1. Try to load the package vite config from the package's vitest.config.ts file, using an empty object if it fails to load
+let packageConfig = {};
+try {
+  packageConfig = (await import("../../vitest.config.ts")).default;
+} catch (e: any) {
+  if (e.code === "ERR_MODULE_NOT_FOUND") {
+    // If the file does not exist, log a message and default to an empty config
+    console.warn(
+      `vitest.config.ts not found in expected location ${path.resolve(__dirname, "../../vitest.config.ts")} - using default min/max vitest config`,
+    );
+  } else {
+    // If there's another error, rethrow it
+    throw e;
+  }
+}
+
+// Taking the base vite config and merging it with the vitest template config we have here
+const config = mergeConfig(
   viteConfig,
   defineConfig({
     test: {
@@ -13,3 +33,6 @@ export default mergeConfig(
     },
   }),
 );
+
+// Finally, the default export mixes in the package config with the merged config, producing the final result
+export default mergeConfig(mergedTestConfig, packageConfig);
