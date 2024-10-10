7e09bb7d0bc3dcc401dadd5dbebd7f8aa5ae132f
Albert Cheng <albertcheng@microsoft.com>
2024-10-10 14:24:19 -0700

    package name

--- eng/scripts/verify-npm-tags.ps1
+++ eng/scripts/verify-npm-tags.ps1
@@ -1,21 +1,10 @@
 param (
   [Parameter(mandatory = $true)]
-  $packageArtifact,
+  $packageName,
   [Parameter(mandatory = $true)]
   $workingDirectory
 )
 
-$HelpersPath = Join-Path $PSScriptRoot "helpers"
-. (Join-Path $HelpersPath npm-helpers.ps1)
-
-$pkgProps = Get-javascript-PackageInfoFromPackageFile -pkg $packageArtifact -workingDirectory $workingDirectory
-if (!$pkgProps)
-{
-  Write-Error "Failed to parse package artifact $packageArtifact to get package name"
-  exit 1
-}
-
-$packageName = $pkgProps.PackageId
 # TODO: delete testing comments below
 #'@azure/eventhubs-checkpointstore-table' alpha, no beta, no GA
 #'@azure/openai' alpha, beta, no GA
