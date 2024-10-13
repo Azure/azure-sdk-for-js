c5f10a71c33787034348462fca052115b19705e0
Manik Khandelwal <mkhandelwal@microsoft.com>
2024-10-13 15:03:30 +0530

    add full text container policy

---
+++ sdk/cosmosdb/cosmos/src/documents/FullTextPolicy.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export interface FullTextPolicy {
+  /**
+   * The default language for the full text .
+   */
+  defaultLanguage: string;
+  /**
+   * The paths to be indexed for full text search.
+   */
+  paths: FullTextPath[];
+
+}
+
+export interface FullTextPath {
+  /**
+   * The path to be indexed for full text search.
+   */
+  path: string;
+  /**
+   * The language for the full text path.
+   */
+  language: string;
+}
