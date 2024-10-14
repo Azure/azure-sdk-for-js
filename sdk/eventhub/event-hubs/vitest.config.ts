0285188afa5ddf40061c0e8e8b0a5498b8f7457f
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-10-14 14:44:51 +0000

    set globalSetup as an absolute path

--- sdk/eventhub/event-hubs/vitest.config.ts
+++ sdk/eventhub/event-hubs/vitest.config.ts
@@ -3,6 +3,11 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.shared.config.ts";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default mergeConfig(
   viteConfig,
@@ -13,7 +18,7 @@ export default mergeConfig(
       fileParallelism: false,
       include: ["test/**/*.spec.ts"],
       exclude: ["./node_modules/**/*"],
-      globalSetup: ["./test/utils/setup.ts"],
+      globalSetup: [path.resolve(__dirname, "/test/utils/setup.ts")],
       fakeTimers: {
         toFake: [
           "setTimeout",
