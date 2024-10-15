cc530dd07750fa65754618e1bf03656448cbc9ad
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-10-15 14:20:32 +0800

    update

--- sdk/containerservice/arm-containerservicefleet/test/containerservicefleet_operations_test.spec.ts
+++ sdk/containerservice/arm-containerservicefleet/test/containerservicefleet_operations_test.spec.ts
@@ -58,14 +58,13 @@ describe("containerservicefleet test", () => {
     await recorder.stop();
   });
 
-  it.only("operations list test", async function () {
+  it("operations list test", async function () {
     const resArray = new Array();
     for await (let item of client.operations.list()) {
       resArray.push(item);
     }
     assert.notEqual(resArray.length, 0);
   });
-
 
   it("fleets create test", async function () {
     const res = await client.fleets.beginCreateOrUpdateAndWait(
