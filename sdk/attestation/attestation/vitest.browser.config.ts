b66e3bd8441c29d18e3d8408a9f300d081fee412
Maor Leger <maorleger@microsoft.com>
2024-10-16 08:09:34 -0700

    Migration: Update test config

---
+++ sdk/attestation/attestation/vitest.browser.config.ts
@@ -0,0 +1,17 @@
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "../../../vitest.browser.shared.config.ts";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      include: [
+        "dist-test/browser/test/**/*.spec.js",
+      ],
+    },
+  }),
+);
