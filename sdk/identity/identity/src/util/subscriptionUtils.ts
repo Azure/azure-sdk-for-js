738dfa03caec9ca92e55595a36212c2f6b85c3f7
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2024-10-18 11:47:33 -0700

    Update subscription check logic
    Co-authored-by: Charles Lowell <10964656+chlowell@users.noreply.github.com>

--- sdk/identity/identity/src/util/subscriptionUtils.ts
+++ sdk/identity/identity/src/util/subscriptionUtils.ts
@@ -7,7 +7,7 @@ import { CredentialLogger, formatError } from "./logging";
  * @internal
  */
 export function checkSubscription(logger: CredentialLogger, subscription: string): void {
-  if (!subscription.match(/^[0-9a-zA-Z-. ]+$/)) {
+  if (!subscription.match(/^[0-9a-zA-Z-._ ]+$/)) {
     const error = new Error(
       "Invalid subscription provided. You can locate your subscription by following the instructions listed here: https://learn.microsoft.com/azure/azure-portal/get-subscription-tenant-id.",
     );
