4a15893c534dc83f75c3b231d0e9cb93a51d1b0a
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2024-10-25 15:24:34 +0800

    [mgmt] servicefabricmanagedcluster release (#31444)
    https://github.com/Azure/sdk-release-request/issues/5554

--- sdk/servicefabricmanagedclusters/arm-servicefabricmanagedclusters/samples/v1-beta/typescript/src/managedClustersDeleteSample.ts
+++ sdk/servicefabricmanagedclusters/arm-servicefabricmanagedclusters/samples/v1-beta/typescript/src/managedClustersDeleteSample.ts
@@ -18,7 +18,7 @@ dotenv.config();
  * This sample demonstrates how to Delete a Service Fabric managed cluster resource with the specified name.
  *
  * @summary Delete a Service Fabric managed cluster resource with the specified name.
- * x-ms-original-file: specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/stable/2024-04-01/examples/ManagedClusterDeleteOperation_example.json
+ * x-ms-original-file: specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/preview/2024-06-01-preview/examples/ManagedClusterDeleteOperation_example.json
  */
 async function deleteACluster() {
   const subscriptionId =
