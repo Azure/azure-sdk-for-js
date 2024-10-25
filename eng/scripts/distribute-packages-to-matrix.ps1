cd50e715137b4f3f60c3045e3b7abeb8786a56ff
Scott Beddall <scbedd@microsoft.com>
2024-10-25 16:50:46 -0700

    try re-enabling and referencing a real environment setting

--- eng/scripts/distribute-packages-to-matrix.ps1
+++ eng/scripts/distribute-packages-to-matrix.ps1
@@ -8,52 +8,52 @@ param(
     [array]$Matrix
 )
 
-# $RepoRoot = (Resolve-Path (Join-Path $PSScriptRoot ".." ".."))
-# $BATCHSIZE = 10
+$RepoRoot = (Resolve-Path (Join-Path $PSScriptRoot ".." ".."))
+$BATCHSIZE = 10
 
-# if (!$Matrix) {
-#     Write-Error "Matrix input not found: $Matrix"
-#     exit 1
-# }
+if (!$Matrix) {
+    Write-Error "Matrix input not found: $Matrix"
+    exit 1
+}
 
-# function Split-ArrayIntoBatches {
-#     param (
-#         [Parameter(Mandatory=$true)]
-#         [object[]]$InputArray,
+function Split-ArrayIntoBatches {
+    param (
+        [Parameter(Mandatory=$true)]
+        [object[]]$InputArray,
 
-#         [Parameter(Mandatory=$true)]
-#         [int]$BatchSize
-#     )
+        [Parameter(Mandatory=$true)]
+        [int]$BatchSize
+    )
 
-#     $batches = @()
+    $batches = @()
 
-#     for ($i = 0; $i -lt $InputArray.Count; $i += $BatchSize) {
-#         $batch = $InputArray[$i..[math]::Min($i + $BatchSize - 1, $InputArray.Count - 1)]
+    for ($i = 0; $i -lt $InputArray.Count; $i += $BatchSize) {
+        $batch = $InputArray[$i..[math]::Min($i + $BatchSize - 1, $InputArray.Count - 1)]
 
-#         $batches += ,$batch
-#     }
+        $batches += ,$batch
+    }
 
-#     return ,$batches
-# }
+    return ,$batches
+}
 
-# # calculate the batches, then duplicate the entire matrix if need be
-# $packageProperties = $env:ArtifactPackageNames.Split(",")
-# $batches = Split-ArrayIntoBatches -InputArray $packageProperties -BatchSize $BATCHSIZE
+# calculate the batches, then duplicate the entire matrix if need be
+$packageProperties = $env:ARTIFACTPACKAGENAMES.Split(",")
+$batches = Split-ArrayIntoBatches -InputArray $packageProperties -BatchSize $BATCHSIZE
 
-# $ModifiedMatrix = @()
-# # to understand this iteration, one must understand that the matrix is a list of hashtables, each with a couple keys:
-# # [
-# #  { "name": "jobname", "parameters": { matrixSetting1: matrixValue1, ...} },
-# # ]
-# if($batches.Length -gt 1) {
-#   throw "This script is not prepared to handle more than one batch. We will need to duplicate the input objects."
-# }
-# else {
-#   foreach($config in $Matrix) {
-#       # we just need to iterate across them, grab the parameters hashtable, and add the new key
-#       # if there is more than one batch, we will need to add a suffix including the batch name to the job name
-#       $config["parameters"]["ArtifactPackageNames"] = ($batches -join ",")
-#   }
-# }
+$ModifiedMatrix = @()
+# to understand this iteration, one must understand that the matrix is a list of hashtables, each with a couple keys:
+# [
+#  { "name": "jobname", "parameters": { matrixSetting1: matrixValue1, ...} },
+# ]
+if($batches.Length -gt 1) {
+  throw "This script is not prepared to handle more than one batch. We will need to duplicate the input objects."
+}
+else {
+  foreach($config in $Matrix) {
+      # we just need to iterate across them, grab the parameters hashtable, and add the new key
+      # if there is more than one batch, we will need to add a suffix including the batch name to the job name
+      $config["parameters"]["ArtifactPackageNames"] = ($batches -join ",")
+  }
+}
 
 return $Matrix
