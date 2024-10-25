5a782d3b7ab1c38bafefccf216492c184afe5510
azure-sdk <azuresdk@microsoft.com>
2024-10-25 10:24:59 +0000

    Post release automated changes for azure-arm-containerservice

--- sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
+++ sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
@@ -74,7 +74,7 @@ export class ContainerServiceClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-containerservice/21.2.0`;
+    const packageDetails = `azsdk-js-arm-containerservice/21.2.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
