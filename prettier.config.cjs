b7e84dd16a38a28f649d06bb2c93966434afbc6f
Matthew Podwysocki <matthewp@microsoft.com>
2024-10-27 12:45:03 -0400

    [EngSys] Add legacy packages  ***NO_CI***

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-packagejson"],
+};
