58d3e2915dc330886c09f4a000708a841607a464
Ujjwal Soni <soniujjwal@microsoft.com>
2024-10-30 16:09:53 +0530

    refactor

--- sdk/cosmosdb/cosmos/test/public/integration/fullTextSearch.spec.ts
+++ sdk/cosmosdb/cosmos/test/public/integration/fullTextSearch.spec.ts
@@ -61,17 +61,6 @@ const expectedValues: number[][] = [
   [61, 51, 49, 54, 75, 24, 77, 76, 80, 25, 22, 2, 66],
 ];
 
-const queries1: string[] = [`SELECT c.index AS Index FROM c order by c.index`];
-const expectedValues1: number[][] = [
-  [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-    51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
-    75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
-    99, 100,
-  ],
-];
-
 describe("Validate full text search queries", function (this: Suite) {
   this.timeout(process.env.MOCHA_TIMEOUT || 20000);
 
@@ -112,18 +101,12 @@ describe("Validate full text search queries", function (this: Suite) {
   });
 
   it("should return correct expected values for all the queries", async function () {
-    for (let i = 0; i < queries1.length; i++) {
-      const queryIterator = container.items.query(queries1[i]);
+    for (let i = 0; i < queries.length; i++) {
+      const queryIterator = container.items.query(queries[i]);
       const { resources: results } = await queryIterator.fetchAll();
 
       const indexes = results.map((result) => result.Index);
-      assert.deepStrictEqual(indexes, expectedValues1[i]);
-
-      if (queries) {
-        if (expectedValues) {
-          console.log("yo");
-        }
-      }
+      assert.deepStrictEqual(indexes, expectedValues[i]);
     }
   });
 });
