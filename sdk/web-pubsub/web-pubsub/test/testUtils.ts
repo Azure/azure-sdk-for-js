fe3b5e27ffe674260990688379c5ec88f385f57e
chuongnguyen <chuongnguyen@microsoft.com>
2024-11-04 13:51:59 +0700

    fix format

--- sdk/web-pubsub/web-pubsub/test/testUtils.ts
+++ sdk/web-pubsub/web-pubsub/test/testUtils.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 export function parseJwt(token: string): any {
-  const base64Payload = token.split('.')[1];
-  const payload = Buffer.from(base64Payload, 'base64');
+  const base64Payload = token.split(".")[1];
+  const payload = Buffer.from(base64Payload, "base64");
   return JSON.parse(payload.toString());
 }
