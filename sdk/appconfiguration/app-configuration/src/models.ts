dc90a1302a11e78a521c7ad8baad2a20547bfb0b
Minh-Anh Phan <minhanhphan@microsoft.com>
2024-11-05 21:41:55 +0000

    update docs

--- sdk/appconfiguration/app-configuration/src/models.ts
+++ sdk/appconfiguration/app-configuration/src/models.ts
@@ -18,7 +18,8 @@ import type {
  */
 export interface AppConfigurationClientOptions extends CommonClientOptions {
   /**
-   * The API version to use when interacting with the service.
+   * The API version to use when interacting with the service. The default value is `2023-11-01`. 
+   * Note that overriding this default value may result in unsupported behavior.
    */
   apiVersion?: string;
 }
