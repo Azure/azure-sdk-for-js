9cfa17ae3b48f962d6d09a5e37ef31594b8be0f6
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-06 11:50:04 -0500

    Migration: Apply codemod: "replaceSinonStub"

--- sdk/communication/communication-alpha-ids/test/internal/headers.spec.ts
+++ sdk/communication/communication-alpha-ids/test/internal/headers.spec.ts
@@ -20,7 +20,7 @@ describe("AlphaIdsClient - headers", function () {
   let request: PipelineRequest;
 
   afterEach(function () {
-    sinon.restore();
+    vi.restoreAllMocks();
   });
 
   it("calls the spy", async function () {
