7ceafc70913eb39f350bb339998ecb186214c957
Bhanu Prakash Chinthalapally <bchinthalapa@microsoft.com>
2024-11-07 19:00:53 +0530

    Auto generated samples

---
+++ sdk/microsoft.app/microsoft-app-dynamicsessions-rest/samples/v1-beta/typescript/src/codeExecutionAsyncSample.ts
@@ -0,0 +1,61 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * Demonstrates how to use the DynamicSessionsClient to execute code asynchronously,
+ * get initial response and long poll till execution is finished and get result.
+ *
+ *
+ * @summary asynchronous code execution.
+ */
+
+import DynamicSessionsClient, { CodeExecutionExecuteParameters, getLongRunningPoller, SessionCodeExecutionResourceOutput } from "@azure-rest/microsoft-app-dynamicsessions-rest";
+import { DefaultAzureCredential } from "@azure/identity";
+
+async function main() {
+
+  const endpoint = "https://<REGION>.dynamicsessions.io/subscriptions/<SUBSCRIPTION_ID>/resourceGroups/<RESOURCE_GROUP>/sessionPools/<SESSION_POOL_NAME>"
+  const client = DynamicSessionsClient(endpoint, new DefaultAzureCredential());
+
+  const options: CodeExecutionExecuteParameters = {
+    headers: {
+      "operation-id": "testExecutionId",
+    },
+    queryParameters: {
+      identifier: "testSessionIdentifier",
+    },
+    body: {
+      codeInputType: "Inline",
+      executionType: "Asynchronous",
+      code: "print('Hello, world!')",
+      timeoutInSeconds: 60
+    },
+  }
+
+  // Start code execution
+  const initialResponse = await client.path("/executions").post(options);
+ 
+  // Create a poller to monitor the long-running operation
+  const poller = await getLongRunningPoller(client, initialResponse);
+
+  // Wait for the operation to complete
+  const result = await poller.pollUntilDone();
+
+  if (result.status == "200")
+  {
+    const sessionCodeExecutionResourceOutput = result.body as SessionCodeExecutionResourceOutput;
+    
+    // Possible values: "NotStarted", "Running", "Succeeded", "Failed", "Canceled"
+    const operationStatus = sessionCodeExecutionResourceOutput.status;
+
+    if (operationStatus == "Succeeded")
+    {
+      console.log("Code execution succeeded!");
+      console.log("Standard output: ", sessionCodeExecutionResourceOutput.result?.stdout);
+    }
+  }
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
