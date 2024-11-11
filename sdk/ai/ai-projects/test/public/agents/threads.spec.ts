0b3028432ae823a11adafc4b091a8300ce89385d
Grace Brigham <nbrigham@microsoft.com>
2024-11-11 09:02:45 -0800

    initial tests

---
+++ sdk/ai/ai-projects/test/public/agents/threads.spec.ts
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+ 
+import { Recorder, VitestTestContext } from "@azure-tools/test-recorder";
+import { AgentsOperations, AIProjectsClient } from "../../../src/index.js";
+import { createRecorder, createProjectsClient } from "../utils/createClient.js";
+import { assert, beforeEach, afterEach, it, describe } from "vitest";
+ 
+describe("Agents - threads", () => {
+  let recorder: Recorder;
+  let projectsClient : AIProjectsClient;
+  let agents: AgentsOperations
+ 
+  beforeEach(async function (context: VitestTestContext) {
+    recorder = await createRecorder(context);
+    projectsClient = createProjectsClient(recorder);
+    agents = projectsClient.agents
+  });
+ 
+  afterEach(async function () {
+     await recorder.stop();
+  });
+ 
+  it("client and agents operations are accessible", async function () {
+    assert.isNotNull(projectsClient);
+    assert.isNotNull(agents);
+  });
+   
+  it("should create thread", async function () {  
+    const thread = await agents.createThread()
+    assert.isNotNull(thread);
+    assert.isNotNull(thread.id);
+    agents.deleteThread(thread.id);
+  });
+ 
+  it("should retrieve thread", async function () {
+    const thread = await agents.createThread()
+    const _thread = await agents.getThread(thread.id);
+    agents.deleteThread(thread.id);
+    assert.isNotEmpty(_thread);
+    assert.equal(_thread.id, thread.id)
+  });
+
+  it("should update thread", async function () {  
+    // TODO
+  });
+ 
+  it("should delete thread", async function () {
+    const thread = await agents.createThread()
+    const deleted = await agents.deleteThread(thread.id);
+    assert.isNotNull(deleted);
+  });
+
+});
