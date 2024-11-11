a7a9f5738eb12ae44acfc62bb5a4017f26f3dda9
Ujjwal Soni <soniujjwal@microsoft.com>
2024-11-11 08:51:10 +0530

    added unit tests for hybrid query execution context

--- sdk/cosmosdb/cosmos/src/queryExecutionContext/hybridQueryExecutionContext.ts
+++ sdk/cosmosdb/cosmos/src/queryExecutionContext/hybridQueryExecutionContext.ts
@@ -284,6 +284,9 @@ export class HybridQueryExecutionContext implements ExecutionContext {
   }
 
   private sortHybridSearchResultByRRFScore(hybridSearchResult: HybridSearchQueryResult[]) {
+    if (hybridSearchResult.length === 0) {
+      return [];
+    }
     const ranksArray: { rid: string; ranks: number[] }[] = hybridSearchResult.map((item) => ({
       rid: item.rid,
       ranks: new Array(item.componentScores.length).fill(0),
@@ -403,6 +406,15 @@ export class HybridQueryExecutionContext implements ExecutionContext {
   }
 
   private replacePlaceholders(query: string, globalStats: GlobalStatistics): string {
+    // Validate globalStats object
+    if (
+      !globalStats ||
+      !globalStats.documentCount ||
+      !Array.isArray(globalStats.fullTextStatistics)
+    ) {
+      throw new Error("Invalid globalStats object provided.");
+    }
+
     // Replace total document count
     query = query.replace(
       /{documentdb-formattablehybridsearchquery-totaldocumentcount}/g,
