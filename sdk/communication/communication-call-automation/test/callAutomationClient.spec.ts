9120c3380b0bc3d03b1d97f00ff1640264fb0eba
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-11 14:29:30 -0500

    [communication] Update @azure/communication-call-automation to ESM/vitest

--- sdk/communication/communication-call-automation/test/callAutomationClient.spec.ts
+++ sdk/communication/communication-call-automation/test/callAutomationClient.spec.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import type { Recorder } from "@azure-tools/test-recorder";
 import type { CallConnectionProperties } from "../src/models/models.js";
 import type { AnswerCallResult, CreateCallResult } from "../src/models/responses.js";
 import {
@@ -10,37 +9,15 @@ import {
   CALL_TARGET_ID,
   CALL_TARGET_ID_2,
 } from "./utils/connectionUtils.js";
-import type {
-  CommunicationIdentifier,
-  CommunicationUserIdentifier,
-} from "@azure/communication-common";
-import type {
-  CallInvite,
-  CallConnection,
-  CreateCallOptions,
-  AnswerCallOptions,
-} from "../src/index.js";
-import {
-  createRecorder,
-  createTestUser,
-  dispatcherCallback,
-  serviceBusWithNewCall,
-  createCallAutomationClient,
-  waitForIncomingCallContext,
-  waitForEvent,
-  events,
-  serviceBusReceivers,
-  incomingCallContexts,
-  loadPersistedEvents,
-  persistEvents,
-} from "./utils/recordedClient.js";
+import type { CommunicationIdentifier } from "@azure/communication-common";
+import type { CallInvite, CallConnection } from "../src/index.js";
 import type {
   AnswerCallEventResult,
   CreateCallEventResult,
 } from "../src/eventprocessor/eventResponses.js";
 import { randomUUID } from "@azure/core-util";
 import type { MockedObject } from "vitest";
-import { describe, it, assert, expect, vi, beforeEach, afterEach } from "vitest";
+import { describe, it, assert, expect, vi, beforeEach } from "vitest";
 
 vi.mock(import("../src/index.js"), async (importOriginal) => {
   const mod = await importOriginal();
@@ -195,125 +172,5 @@ describe("Call Automation Client Unit Tests", () => {
     // asserts
     await promiseResult;
     expect(client.rejectCall).toHaveBeenCalledWith(CALL_INCOMING_CALL_CONTEXT);
-  });
-});
-
-describe("Call Automation Main Client Live Tests", function () {
-  let recorder: Recorder;
-  let callerCallAutomationClient: CallAutomationClient;
-  let receiverCallAutomationClient: CallAutomationClient;
-  let callConnection: CallConnection;
-  let testUser: CommunicationUserIdentifier;
-  let testUser2: CommunicationUserIdentifier;
-  let testName: string;
-
-  beforeEach(async function (ctx) {
-    recorder = await createRecorder(ctx);
-    testUser = await createTestUser(recorder);
-    testUser2 = await createTestUser(recorder);
-    callerCallAutomationClient = createCallAutomationClient(recorder, testUser);
-    receiverCallAutomationClient = createCallAutomationClient(recorder, testUser2);
-  });
-
-  afterEach(async function () {
-    persistEvents(testName);
-    serviceBusReceivers.forEach((receiver) => {
-      receiver.close();
-    });
-    events.forEach((callConnectionEvents) => {
-      callConnectionEvents.clear();
-    });
-    events.clear();
-    serviceBusReceivers.clear();
-    incomingCallContexts.clear();
-    await recorder.stop();
-    if (callConnection) {
-      try {
-        await callConnection.hangUp(true);
-      } catch {
-        return;
-      }
-    }
-  });
-
-  it("Create a call and hangup", { timeout: 60000 }, async function (ctx) {
-    const fullTitle: string | undefined =
-      ctx.task.suite && ctx.task.suite.name && ctx.task.name
-        ? `${ctx.task.suite.name} ${ctx.task.name}`
-        : undefined;
-
-    testName = fullTitle
-      ? fullTitle.replace(/ /g, "_").toLocaleLowerCase()
-      : "create_call_and_hang_up";
-    await loadPersistedEvents(testName);
-
-    const callInvite: CallInvite = { targetParticipant: testUser2 };
-    const uniqueId = await serviceBusWithNewCall(testUser, testUser2);
-    const callBackUrl: string = dispatcherCallback + `?q=${uniqueId}`;
-    const createCallOption: CreateCallOptions = { operationContext: "operationContextCreateCall" };
-
-    const result = await callerCallAutomationClient.createCall(
-      callInvite,
-      callBackUrl,
-      createCallOption,
-    );
-    const incomingCallContext = await waitForIncomingCallContext(uniqueId, 8000);
-    const callConnectionId: string = result.callConnectionProperties.callConnectionId
-      ? result.callConnectionProperties.callConnectionId
-      : "";
-    assert.isDefined(incomingCallContext);
-
-    if (incomingCallContext) {
-      const answerCallOptions: AnswerCallOptions = {
-        operationContext: "operationContextAnswerCall",
-      };
-      await receiverCallAutomationClient.answerCall(
-        incomingCallContext,
-        callBackUrl,
-        answerCallOptions,
-      );
-    }
-
-    const callConnectedEvent = await waitForEvent("CallConnected", callConnectionId, 8000);
-
-    assert.isDefined(callConnectedEvent);
-    callConnection = result.callConnection;
-
-    await callConnection.hangUp(true);
-
-    const callDisconnectedEvent = await waitForEvent("CallDisconnected", callConnectionId, 8000);
-    assert.isDefined(callDisconnectedEvent);
-  });
-
-  it("Reject call", { timeout: 60000 }, async function (ctx) {
-    const fullTitle: string | undefined =
-      ctx.task.suite && ctx.task.suite.name && ctx.task.name
-        ? `${ctx.task.suite.name} ${ctx.task.name}`
-        : undefined;
-    testName = fullTitle ? fullTitle.replace(/ /g, "_").toLocaleLowerCase() : "reject_call";
-    await loadPersistedEvents(testName);
-
-    const callInvite: CallInvite = { targetParticipant: testUser2 };
-    const uniqueId = await serviceBusWithNewCall(testUser, testUser2);
-    const callBackUrl: string = dispatcherCallback + `?q=${uniqueId}`;
-    const createCallOption: CreateCallOptions = { operationContext: "operationContextRejectCall" };
-
-    const result = await callerCallAutomationClient.createCall(
-      callInvite,
-      callBackUrl,
-      createCallOption,
-    );
-    const incomingCallContext = await waitForIncomingCallContext(uniqueId, 8000);
-    const callConnectionId: string = result.callConnectionProperties.callConnectionId
-      ? result.callConnectionProperties.callConnectionId
-      : "";
-    assert.isDefined(incomingCallContext);
-
-    if (incomingCallContext) {
-      await receiverCallAutomationClient.rejectCall(incomingCallContext);
-    }
-
-    const createCallFailedEvent = await waitForEvent("CreateCallFailed", callConnectionId, 8000);
-    assert.isDefined(createCallFailedEvent);
   });
 });
