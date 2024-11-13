c4c87ea805b8e51e59f9ce4653cf0ff3687d93ea
Ganesh Bheemarasetty <1634042+ganeshyb@users.noreply.github.com>
2024-11-13 10:15:10 -0800

    Run operations (#31747)
    Exposes API for Runs, Tests and Samples
    
    `
    createRun: (threadId: string, assistantId: string, options?:
    Omit<CreateRunOptions, "assistant_id">, requestParams?:
    OptionalRequestParameters) => Promise<ThreadRunOutput>;
    
    getRun: (threadId: string, runId: string, requestParams?:
    OptionalRequestParameters) => Promise<ThreadRunOutput>;
    
    updateRun: (threadId: string, runId: string, options?: UpdateRunOptions,
    requestParams?: OptionalRequestParameters) => Promise<ThreadRunOutput>;`

--- sdk/ai/ai-projects/src/agents/inputOutputs.ts
+++ sdk/ai/ai-projects/src/agents/inputOutputs.ts
@@ -3,6 +3,8 @@
 // Licensed under the MIT License.
 
 
-export  *  from "./streamingModels.js"
+export * from "./streamingModels.js"
 export * from "../generated/src/models.js"
 export * from "../generated/src/outputModels.js"
+export * from "./customModels.js"
+export * from "./runModels.js"
