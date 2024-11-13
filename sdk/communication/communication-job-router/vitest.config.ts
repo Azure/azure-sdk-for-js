14d7589d5db96c25617d0896881a381b41c0c804
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-13 11:56:44 -0500

    [communication] Update @azure/communication-job-router to ESM/vitest

--- sdk/communication/communication-job-router/vitest.config.ts
+++ sdk/communication/communication-job-router/vitest.config.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "../../../vitest.shared.config.ts";
+import viteConfig from "../../../vitest.shared.config.js";
 
 export default mergeConfig(
   viteConfig,
