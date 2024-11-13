d7d48c33d54745385628099a9aa0cc5f07b721f4
Lazar <163033155+Lakicar95@users.noreply.github.com>
2024-11-13 15:19:07 +0100

    Update sdk/maps/maps-route-rest/samples-dev/resumeLro.ts

--- sdk/maps/maps-route-rest/samples-dev/resumeLro.ts
+++ sdk/maps/maps-route-rest/samples-dev/resumeLro.ts
@@ -73,7 +73,7 @@ async function main(): Promise<void> {
   const initialPoller = await getLongRunningPoller(client, initialResponse);
   /* We can get a partial of the results first */
   /** Serialized the current operation for future poller */
-  const serializedState = await (await initialPoller).serialize();
+  const serializedState = await initialPoller.serialize();
   /** Use the `resumeFrom` option to rehydrate the previous operation */
   const rehydratedPoller = await getLongRunningPoller(client, initialResponse, {
     restoreFrom: serializedState,
