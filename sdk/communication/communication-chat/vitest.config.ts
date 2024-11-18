e1d93e053f0e4ca88b64c777015962eaa852f8fd
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-18 17:35:08 -0500

    [communication] Move @azure/communication-call-automation to ESM/vitest

--- sdk/communication/communication-chat/vitest.config.ts
+++ sdk/communication/communication-chat/vitest.config.ts
@@ -9,8 +9,8 @@ export default mergeConfig(
   defineConfig({
     test: {
       include: ["test/**/*.spec.ts"],
+      hookTimeout: 5000000,
+      testTimeout: 5000000,
     },
-    hookTimeout: 5000000,
-    testTimeout: 5000000,
   }),
 );
