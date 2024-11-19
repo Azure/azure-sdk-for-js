7489bd5704dbc50ab5129d37d5d400922ee74c89
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-19 11:51:52 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/template/template-dpg/test/public/widgetService.spec.ts
+++ sdk/template/template-dpg/test/public/widgetService.spec.ts
@@ -45,11 +45,11 @@ describe("WidgetServiceClient", function () {
   // NOTE: use of "function" and not ES6 arrow-style functions with the
   // beforeEach hook is IMPORTANT due to the use of `this` in the function
   // body.
-  beforeEach(async function (this: Context) {
+  beforeEach(async function (ctx) {
     // The recorder has some convenience methods, and we need to store a
     // reference to it so that we can `stop()` the recorder later in the
     // `afterEach` hook.
-    recorder = new Recorder(this.currentTest);
+    recorder = new Recorder(ctx);
 
     // Start the recorder before each test.
     await recorder.start({
