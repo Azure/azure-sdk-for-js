e48c6bc83febb48ec4e2c0e1c5e6f7e63754a24c
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-11-20 06:20:58 -0800

    fix linting

--- sdk/openai/openai/eslint.config.mjs
+++ sdk/openai/openai/eslint.config.mjs
@@ -3,13 +3,19 @@ import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 export default [
   ...azsdkEslint.configs.recommended,
   {
-    rules: {
-      "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
-      "@azure/azure-sdk/ts-package-json-module": "off",
-      "@azure/azure-sdk/ts-package-json-main-is-cjs": "off",
-      "@azure/azure-sdk/ts-package-json-types": "off",
-      "@azure/azure-sdk/ts-package-json-files-required": "off",
-      "no-restricted-imports": "warn",
+    files: ["**/*.ts", "**/*.cts", "**/*.mts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+      },
+    },
+  },
+  {
+    files: ["*.md/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: null,
+      },
     },
   },
 ];
