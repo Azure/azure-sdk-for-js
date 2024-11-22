0ce054303dbb2c91977fbf4d223e41879063f2ae
azure-sdk <azuresdk@microsoft.com>
2024-11-22 07:32:15 +0000

    Post release automated changes for azure-arm-hardwaresecuritymodules

--- sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/src/azureHSMResourceProvider.ts
+++ sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/src/azureHSMResourceProvider.ts
@@ -68,7 +68,7 @@ export class AzureHSMResourceProvider extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-hardwaresecuritymodules/2.0.0-beta.2`;
+    const packageDetails = `azsdk-js-arm-hardwaresecuritymodules/2.0.0-beta.3`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
