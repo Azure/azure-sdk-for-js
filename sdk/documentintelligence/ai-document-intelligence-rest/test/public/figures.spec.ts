c1d1c7583da7395d747dbbd21fd412b08a2cabea
Harsha Nalluru <sanallur@microsoft.com>
2024-11-22 17:12:49 -0800

    update the test

--- sdk/documentintelligence/ai-document-intelligence-rest/test/public/figures.spec.ts
+++ sdk/documentintelligence/ai-document-intelligence-rest/test/public/figures.spec.ts
@@ -5,11 +5,11 @@ import { env } from "@azure-tools/test-recorder";
 import DocumentIntelligence from "../../src/documentIntelligence.js";
 import { describe, it } from "vitest";
 import fs from "fs";
-// import path from "path";
-// import { getLongRunningPoller, isUnexpected } from "../../src/index.js";
-// import {
-//   ASSET_PATH,
-// } from "./utils/utils.js";
+import path from "path";
+import { getLongRunningPoller, isUnexpected } from "../../src/index.js";
+import {
+  ASSET_PATH,
+} from "./utils/utils.js";
 
 describe("DocumentIntelligenceClient", () => {
   describe("get AnalyzeResult methods", function () {
@@ -22,7 +22,7 @@ describe("DocumentIntelligenceClient", () => {
 
 const MODEL_ID = 'prebuilt-layout';
 
-// const FILE_PATH = path.join(ASSET_PATH, "test.pdf");;
+const FILE_PATH = path.join(ASSET_PATH, "test.pdf");
 
 const API_ENDPOINT = env.DOCUMENT_INTELLIGENCE_ENDPOINT || "";
 const API_KEY = env.DOCUMENT_INTELLIGENCE_API_KEY || "";
@@ -44,15 +44,15 @@ const client = DocumentIntelligence(API_ENDPOINT, {
 );
 
 
-// interface AnalyzeResponse {
-//   analyzeResult?: {
-//     figures?: Figure[];
-//   };
-// }
+interface AnalyzeResponse {
+  analyzeResult?: {
+    figures?: Figure[];
+  };
+}
 
-// interface Figure {
-//   id?: string;
-// }
+interface Figure {
+  id?: string;
+}
 
 async function fetchFigure(responseId: string, figureId: string): Promise<void | null> {
   console.log(`modelId: ${MODEL_ID}, responseId: ${responseId}, figureId: ${figureId}`);
@@ -64,58 +64,44 @@ async function fetchFigure(responseId: string, figureId: string): Promise<void |
   console.log(response);
   const { body } = response;
 
-  if (typeof body !== 'string') {
-    return null;
+  if (Buffer.isBuffer(body)) {
+    await fs.promises.writeFile(`./figures/${figureId}.png`, body);
   }
-
-  await fs.promises.writeFile(`./figures/${figureId}.png`, body);
-
-  // fs.writeFile(`./figures/${figureId}.png`, body, (err: NodeJS.ErrnoException | null) => {
-  //   if (err) {
-  //     console.error('Error saving image:', err);
-  //   } else {
-  //     console.log('Image saved successfully');
-  //   }
-  // })
-
-  // const buffer = Buffer.from(body, 'binary');
-
-  // await fs.promises.writeFile(`./figures/${figureId}.png`, buffer, 'binary'); // When I open this file, it says it's corrupted.
 }
 
 async function analyze(): Promise<void> {
-  // const base64Source = fs.readFileSync(FILE_PATH, { encoding: 'base64' });
+  const base64Source = fs.readFileSync(FILE_PATH, { encoding: 'base64' });
   await fs.promises.rm(`./figures`, { recursive: true }).catch(() => { });
   await fs.promises.mkdir(`./figures`);
 
   await fs.promises.rm(`./figures-updated`, { recursive: true }).catch(() => { });
   await fs.promises.mkdir(`./figures-updated`);
 
-  // const initialResponse = await client.path(`/documentModels/{modelId}:analyze`, MODEL_ID).post({
-  //   contentType: 'application/json',
-  //   body: { base64Source },
-  //   queryParameters: {
-  //     output: ['figures'],
-  //   },
-  // });
+  const initialResponse = await client.path(`/documentModels/{modelId}:analyze`, MODEL_ID).post({
+    contentType: 'application/json',
+    body: { base64Source },
+    queryParameters: {
+      output: ['figures'],
+    },
+  });
 
-  // const poller = await getLongRunningPoller(client, initialResponse);
+  const poller = await getLongRunningPoller(client, initialResponse);
 
-  // const response = await poller.pollUntilDone();
+  const response = await poller.pollUntilDone();
 
-  // if (isUnexpected(response)) {
-  //   console.log(response);
-  //   throw new Error("The response was unexpected.");
-  // }
+  if (isUnexpected(response)) {
+    console.log(response);
+    throw new Error("The response was unexpected.");
+  }
 
-  // const figures = (response.body as AnalyzeResponse).analyzeResult?.figures || [];
+  const figures = (response.body as AnalyzeResponse).analyzeResult?.figures || [];
 
-  // for (const figure of figures) {
-  //   if (figure.id) {
+  for (const figure of figures) {
+    if (figure.id) {
 
-  //     await fetchFigure(poller.getOperationId(), figure.id);
-  //   }
-  // };
+      await fetchFigure(poller.getOperationId(), figure.id);
+    }
+  };
 
-  await fetchFigure("98816098-c1b1-4b14-b001-c9433603c988", '3.2');
+  // await fetchFigure("98816098-c1b1-4b14-b001-c9433603c988", '3.2');
 }
