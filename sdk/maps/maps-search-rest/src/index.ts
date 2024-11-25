1b52c4c48b54098c89224269934bb7fa79f3c816
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-25 15:20:41 -0500

    [maps] Upgrade @azure-rest/maps-search to ESM/vitest

--- sdk/maps/maps-search-rest/src/index.ts
+++ sdk/maps/maps-search-rest/src/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
 import MapsSearch from "./MapsSearch.js";
 
-export * from "../generated/index.js";
+export * from "./generated/index.js";
 export default MapsSearch;
--- sdk/maps/maps-search-rest/test/public/utils/recordedClient.ts
+++ sdk/maps/maps-search-rest/test/public/utils/recordedClient.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-import type { RecorderStartOptions } from "@azure-tools/test-recorder";
+
+import type { RecorderStartOptions, TestInfo } from "@azure-tools/test-recorder";
 import { env, Recorder } from "@azure-tools/test-recorder";
 import "./env.js";
 import type { ClientOptions } from "@azure-rest/core-client";
@@ -21,8 +22,8 @@ const recorderEnvSetup: RecorderStartOptions = {
  * Should be called first in the test suite to make sure environment variables are
  * read before they are being used.
  */
-export async function createRecorder(context: Context): Promise<Recorder> {
-  const recorder = new Recorder(context.currentTest);
+export async function createRecorder(context: TestInfo): Promise<Recorder> {
+  const recorder = new Recorder(context);
   await recorder.start(recorderEnvSetup);
   return recorder;
 }
