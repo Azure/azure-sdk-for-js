3cc43f288be799b618531b263884826ff3b97b3b
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-26 17:46:03 -0500

    [batch] Update @azure-rest/batch to latest vitest

--- sdk/batch/batch-rest/vitest.browser.config.ts
+++ sdk/batch/batch-rest/vitest.browser.config.ts
@@ -1,50 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { defineConfig } from "vitest/config";
-import { isPlaybackMode, relativeRecordingsPath } from "@azure-tools/test-recorder";
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "../../../vitest.browser.shared.config.ts";
 
-process.env.RECORDINGS_RELATIVE_PATH = relativeRecordingsPath();
-export default defineConfig({
-  define: {
-    "process.env": process.env,
-    __env__: process.env,
-  },
-  optimizeDeps: {
-    include: ["@azure-tools/test-recorder"],
-  },
-  test: {
-    testTimeout: 600000,
-    hookTimeout: 600000,
-    reporters: ["basic", "junit"],
-    outputFile: {
-      junit: "test-results.browser.xml",
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      include: ["dist-test/browser/test/**/*.spec.js"],
+      hookTimeout: 500000,
+      testTImeout: 500000,
     },
-    browser: {
-      enabled: true,
-      headless: isPlaybackMode(),
-      name: "chromium",
-      provider: "playwright",
-      providerOptions: {
-        launch: {
-          args: ["--disable-web-security"],
-        },
-      },
-    },
-    fakeTimers: {
-      toFake: ["setTimeout", "Date"],
-    },
-    watch: false,
-    include: ["dist-test/browser/**/*.spec.js"],
-    coverage: {
-      include: ["dist-test/browser/**/*.js"],
-      exclude: [
-        "dist-test/browser/**/*./*-browser.mjs",
-        "dist-test/browser/**/*./*-react-native.mjs",
-      ],
-      provider: "istanbul",
-      reporter: ["text", "json", "html"],
-      reportsDirectory: "coverage-browser",
-    },
-  },
-});
+  }),
+);
