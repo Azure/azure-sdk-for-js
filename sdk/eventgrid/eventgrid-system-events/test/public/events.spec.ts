e67236bc228aa98bc9ae593f95916534f6192d89
Matthew Podwysocki <matthewp@microsoft.com>
2024-11-27 14:28:51 -0500

    [eventgrid] Migrate @azure/eventgrid-system-events to ESM/vitest

--- sdk/eventgrid/eventgrid-system-events/test/public/events.spec.ts
+++ sdk/eventgrid/eventgrid-system-events/test/public/events.spec.ts
@@ -3,7 +3,7 @@
 import { isSystemEvent } from "../../src/index.js";
 import { describe, it, assert } from "vitest";
 
-describe("Events tests", function (this: Suite) {
+describe("Events tests", () => {
   it("isSystemEvent test", async () => {
     const result = isSystemEvent("Microsoft.KeyVault.SecretNearExpiry", {
       eventType: "Microsoft.KeyVault.SecretNearExpiry",
