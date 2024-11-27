b75580aae8b0dcb07aa46bf4590f6425b39b3e3e
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-11-27 10:17:07 +0800

    update

--- sdk/communication/communication-job-router-rest/generated/azureCommunicationRoutingServiceClient.ts
+++ sdk/communication/communication-job-router-rest/generated/azureCommunicationRoutingServiceClient.ts
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 import { getClient, ClientOptions } from "@azure-rest/core-client";
-import { logger } from "./logger";
-import { AzureCommunicationRoutingServiceClient } from "./clientDefinitions";
+import { logger } from "./logger.js";
+import { AzureCommunicationRoutingServiceClient } from "./clientDefinitions.js";
 
 /** The optional parameters for the client */
 export interface AzureCommunicationRoutingServiceClientOptions
