84a7f4f3ae45770c976809ace173af2df50e6661
Ujjwal Soni <soniujjwal@microsoft.com>
2024-11-28 13:51:14 +0530

    removed dependency of emulator to run unit tests locally

--- sdk/cosmosdb/cosmos/test/public/common/TestHelpers.ts
+++ sdk/cosmosdb/cosmos/test/public/common/TestHelpers.ts
@@ -42,18 +42,22 @@ import { extractPartitionKeys } from "../../../src/extractPartitionKey";
 const defaultRoutingGatewayPort: string = ":8081";
 const defaultComputeGatewayPort: string = ":8903";
 
-export const defaultClient = new CosmosClient({
-  endpoint,
-  key: masterKey,
-  connectionPolicy: { enableBackgroundEndpointRefreshing: false },
-  diagnosticLevel: CosmosDbDiagnosticLevel.info,
-});
+export function getDefaultClient(): CosmosClient {
+  return new CosmosClient({
+    endpoint,
+    key: masterKey,
+    connectionPolicy: { enableBackgroundEndpointRefreshing: false },
+    diagnosticLevel: CosmosDbDiagnosticLevel.info,
+  });
+}
 
-export const defaultComputeGatewayClient = new CosmosClient({
-  endpoint: endpoint.replace(defaultRoutingGatewayPort, defaultComputeGatewayPort),
-  key: masterKey,
-  connectionPolicy: { enableBackgroundEndpointRefreshing: false },
-});
+export function getDefaultComputeGatewayClient(): CosmosClient {
+  return new CosmosClient({
+    endpoint: endpoint.replace(defaultRoutingGatewayPort, defaultComputeGatewayPort),
+    key: masterKey,
+    connectionPolicy: { enableBackgroundEndpointRefreshing: false },
+  });
+}
 
 export function addEntropy(name: string): string {
   return name + getEntropy();
@@ -63,8 +67,9 @@ export function getEntropy(): string {
   return `${Math.floor(Math.random() * 10000)}`;
 }
 
-export async function removeAllDatabases(client: CosmosClient = defaultClient): Promise<void> {
+export async function removeAllDatabases(): Promise<void> {
   try {
+    const client = getDefaultClient();
     const { resources: databases } = await client.databases.readAll().fetchAll();
     const length = databases.length;
 
@@ -422,9 +427,12 @@ function validateRequestStartTimeForDiagnostics(
 
 export async function getTestDatabase(
   testName: string,
-  client: CosmosClient = defaultClient,
+  client: CosmosClient,
   attrs?: Partial<DatabaseRequest>,
 ): Promise<Database> {
+  if (!client) {
+    client = getDefaultClient();
+  }
   const entropy = Math.floor(Math.random() * 10000);
   const id = `${testName.replace(" ", "").substring(0, 30)}${entropy}`;
   await client.databases.create({ id, ...attrs });
@@ -433,10 +441,13 @@ export async function getTestDatabase(
 
 export async function getTestContainer(
   testName: string,
-  client: CosmosClient = defaultClient,
+  client: CosmosClient,
   containerDef?: ContainerRequest,
   options?: RequestOptions,
 ): Promise<Container> {
+  if (!client) {
+    client = getDefaultClient();
+  }
   const db = await getTestDatabase(testName, client);
   const entropy = Math.floor(Math.random() * 10000);
   const id = `${testName.replace(" ", "").substring(0, 30)}${entropy}`;
