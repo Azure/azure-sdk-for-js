b2ffea6c8958ea64add091866e8f125ad2265990
azure-sdk <azuresdk@microsoft.com>
2024-11-28 06:39:52 +0000

    Post release automated changes for azure-arm-connectedcache

--- sdk/connectedcache/arm-connectedcache/src/api/connectedCacheContext.ts
+++ sdk/connectedcache/arm-connectedcache/src/api/connectedCacheContext.ts
@@ -23,7 +23,7 @@ export function createConnectedCache(
 ): ConnectedCacheContext {
   const endpointUrl = options.endpoint ?? options.baseUrl ?? `https://management.azure.com`;
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-connectedcache/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-connectedcache/1.0.0-beta.2`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
