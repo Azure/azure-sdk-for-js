7d08322c0a87f48822c0b1a66c9052af469de0b6
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-02 17:06:12 -0500

    [formrecognizer] Migrate @azure/ai-form-recognizer to ESM/vitest

--- sdk/formrecognizer/ai-form-recognizer/eslint.config.mjs
+++ sdk/formrecognizer/ai-form-recognizer/eslint.config.mjs
@@ -3,11 +3,6 @@ import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 export default [
   ...azsdkEslint.configs.recommended,
   {
-    rules: {
-      "@azure/azure-sdk/github-source-headers": "off",
-    },
-  },
-  {
     // shebang needs to come first
     files: ["src/bin/gen-model.ts"],
     rules: {
