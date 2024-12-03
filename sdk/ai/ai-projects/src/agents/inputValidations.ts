5718c83c1dc46b1bd9df1a229bf205c35be76bcb
Zachary King <zacharyking@microsoft.com>
2024-12-03 15:53:09 -0800

    Input validation for threads and streaming

--- sdk/ai/ai-projects/src/agents/inputValidations.ts
+++ sdk/ai/ai-projects/src/agents/inputValidations.ts
@@ -1,11 +1,23 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { VectorStoreDataSource } from "../generated/src/models.js";
+import { ToolDefinition, UpdateToolResourcesOptions, VectorStoreDataSource } from "../generated/src/models.js";
 
 export function validateVectorStoreDataType(data_sources: VectorStoreDataSource[]): void {
   if (!data_sources.some(value => !["uri_asset", "id_asset"].includes(value.type))) {
     throw new Error("Vector store data type must be one of 'uri_asset', 'id_asset'");
+  }
+}
+
+export function validateThreadId(threadId: string): void {
+  if (!threadId) {
+    throw new Error("Thread ID is required");
+  }
+}
+
+export function validateRunId(runId: string): void {
+  if (!runId) {
+    throw new Error("Run ID is required");
   }
 }
 
@@ -21,6 +33,22 @@ export function validateOrder(order: string): void {
   }
 }
 
+enum Tools {
+  CodeInterpreter = "code_interpreter",
+  FileSearch = "file_search",
+  Function = "function",
+  BingGrounding = "bing_grounding",
+  MicrosoftFabric = "microsoft_fabric",
+  SharepointGrounding = "sharepoint_grounding",
+  AzureAISearch = "azure_ai_search",
+}
+
+export function validateTools(value: Array<ToolDefinition>): void {
+    if (value.some(tool => !Object.values(Tools).includes(tool as unknown as Tools))) {
+      throw new Error("Tool type must be one of 'code_interpreter', 'file_search', 'function', 'bing_grounding', 'microsoft_fabric', 'sharepoint_grounding', 'azure_ai_search'");
+    }
+}
+
 export function validateMetadata(metadata: Record<string, string>): void {
   if (Object.keys(metadata).length > 16) {
       throw new Error("Only 16 key/value pairs are allowed");
@@ -30,6 +58,24 @@ export function validateMetadata(metadata: Record<string, string>): void {
   }
   if (Object.values(metadata).some(value => value.length > 512)) {
     throw new Error("Values must be less than 512 characters");
+  }
+}
+
+export function validateToolResources(toolResource: UpdateToolResourcesOptions): void {
+  if (toolResource.code_interpreter) {
+    if (toolResource.code_interpreter.file_ids && toolResource.code_interpreter.file_ids.length > 20) {
+      throw new Error("A maximum of 20 file IDs are allowed");
+    }
+  }
+  if (toolResource.file_search) {
+    if (toolResource.file_search.vector_store_ids && toolResource.file_search.vector_store_ids.length > 1) {
+      throw new Error("Only one vector store ID is allowed");
+    }
+  }
+  if (toolResource.azure_ai_search) {
+    if (toolResource.azure_ai_search.indexes && toolResource.azure_ai_search.indexes.length > 1) {
+      throw new Error("Only one index is allowed");
+    }
   }
 }
 
--- sdk/ai/ai-projects/src/agents/streaming.ts
+++ sdk/ai/ai-projects/src/agents/streaming.ts
@@ -7,6 +7,7 @@ import { AgentEventMessage, AgentEventMessageStream } from "./streamingModels.js
 import { createSseStream, EventMessageStream } from "@azure/core-sse";
 import { isNodeLike } from "@azure/core-util";
 import { IncomingMessage } from "http";
+import { validateMetadata, validateRunId, validateThreadId, validateToolResources, validateTools } from "./inputValidations.js";
 
 const expectedStatuses = ["200"];
 
@@ -51,6 +52,8 @@ export async function createRunStreaming(
   threadId: string,
   options: CreateRunParameters,
 ): Promise<AgentEventMessageStream> {
+  validateThreadId(threadId);
+  validateCreateThreadAndRunBodyParam(options);
   options.body.stream = true;
 
   return processStream(context
@@ -64,6 +67,7 @@ export async function createThreadAndRunStreaming(
   context: Client,
   options: CreateThreadAndRunBodyParam,
 ): Promise<AgentEventMessageStream> {
+  validateCreateThreadAndRunBodyParam(options);
   options.body.stream = true;
   return processStream(context
     .path("/threads/runs")
@@ -76,8 +80,43 @@ export async function submitToolOutputsToRunStreaming(
   runId: string,
   options: SubmitToolOutputsToRunParameters,
 ): Promise<AgentEventMessageStream> {
+  validateThreadId(threadId);
+  validateRunId(runId);
   options.body.stream = true;
 
   return processStream(context.path("/threads/{threadId}/runs/{runId}/submit_tool_outputs", threadId, runId)
     .post(options));
+}
+
+
+function validateCreateThreadAndRunBodyParam(options: CreateRunParameters| CreateThreadAndRunBodyParam): void {
+  if ('additional_messages' in options.body && options.body.additional_messages && options.body.additional_messages.some(value => !["user", "assistant"].includes(value.role))) {
+    throw new Error("Role must be either 'user' or 'assistant'");
+  }
+  if ( 'thread' in options.body && options.body.thread?.messages?.some(value => !["user", "assistant"].includes(value.role))) {
+    throw new Error("Role must be either 'user' or 'assistant'");
+  }
+  if (options.body.tools) {
+    validateTools(options.body.tools);
+  }
+  if ('tool_resources' in options.body && options?.body.tool_resources) {
+    validateToolResources(options.body.tool_resources);  
+  }
+  if (options.body.temperature && (options.body.temperature < 0 || options.body.temperature > 2)) {
+    throw new Error("Temperature must be between 0 and 2");
+  }
+    if (options.body.tool_choice && typeof options.body.tool_choice !== 'string') {
+    validateTools([options.body.tool_choice]);
+  }
+  if (options.body.truncation_strategy?.type && !["auto", "last_messages"].includes(options.body.truncation_strategy.type)) {
+    throw new Error("Role must be either 'auto' or 'last_messages'");
+  }
+  if (options.body.response_format){
+    if (!["json", "text"].includes(options.body.response_format as string)) {
+      throw new Error("Response format must be either 'json' or 'text'");
+    }
+  }
+  if (options?.body.metadata){
+    validateMetadata(options.body.metadata);
+  }
 }
