eb19bdd711258c4298ba08105ce55271ec2f80c6
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-12-07 10:55:28 -0800

    [eventhub] Precise Typechecking

--- sdk/eventhub/eventhubs-checkpointstore-table/eslint.config.mjs
+++ sdk/eventhub/eventhubs-checkpointstore-table/eslint.config.mjs
@@ -2,11 +2,6 @@ import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default azsdkEslint.config([
   {
-    rules: {
-      "no-restricted-imports": "warn",
-    }
-  },
-  {
     files: ["**/*.ts", "**/*.cts", "**/*.mts"],
     languageOptions: {
       parserOptions: {
