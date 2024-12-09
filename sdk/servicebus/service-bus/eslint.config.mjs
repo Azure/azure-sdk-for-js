daf783054200d606098a03b161f3f6b65b5be539
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-12-09 09:11:07 -0800

    [servicebus] Precise Typechecking

--- sdk/servicebus/service-bus/eslint.config.mjs
+++ sdk/servicebus/service-bus/eslint.config.mjs
@@ -2,11 +2,14 @@ import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default azsdkEslint.config([
   {
-    ignores: ["test/stress/"],
+    ignores: ["test/stress"],
   },
   {
-    rules: {
-      "@azure/azure-sdk/ts-package-json-types": "off",
+    files: ["**/*.ts", "**/*.cts", "**/*.mts"],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.test.json"],
+      },
     },
   },
 ]);
