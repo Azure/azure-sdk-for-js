e9034fa780f3490037c0bc20b1061f22d222b0c1
Ganesh Bheemarasetty <1634042+ganeshyb@users.noreply.github.com>
2024-12-08 20:46:35 -0800

    Adding Tracing for Agents (#32020)
    Adding Tracing for Agents
    Introduce Tracing Utility that provides utility functions for adding
    span attributes and wrap span with start and end functions that can be
    called.
    TODO :  Streaming Events.

--- sdk/ai/ai-projects/src/agents/threads.ts
+++ sdk/ai/ai-projects/src/agents/threads.ts
@@ -4,69 +4,80 @@
 import { Client, createRestError } from "@azure-rest/core-client";
 import { CreateThreadParameters, DeleteThreadParameters, GetThreadParameters, UpdateThreadParameters } from "../generated/src/parameters.js";
 import { AgentThreadOutput, ThreadDeletionStatusOutput } from "../generated/src/outputModels.js";
+import { TracingUtility } from "../tracing.js";
+import { traceEndCreateThread, traceStartCreateThread } from "./threadsTrace.js";
 import { validateMessages, validateMetadata, validateThreadId, validateToolResources } from "./inputValidations.js";
+import { traceStartAgentGeneric } from "./traceUtility.js";
 
 const expectedStatuses = ["200"];
 
 /** Creates a new thread. Threads contain messages and can be run by agents. */
 export async function createThread(
   context: Client,
-  options?: CreateThreadParameters,
+  options: CreateThreadParameters = { body: {} },
 ): Promise<AgentThreadOutput> {
   validateCreateThreadParameters(options);
-  const result = await context.path("/threads").post(options);
-  if (!expectedStatuses.includes(result.status)) {
+  return TracingUtility.withSpan("CreateThread", options, async (updatedOptions) => {
+    const result = await context.path("/threads").post(updatedOptions);
+    if (!expectedStatuses.includes(result.status)) {
       throw createRestError(result);
-  }
-  return result.body;
+    }
+    return result.body;
+  }, traceStartCreateThread, traceEndCreateThread);
 }
 
 /** Gets information about an existing thread. */
 export async function getThread(
   context: Client,
   threadId: string,
-  options?: GetThreadParameters,
+  options: GetThreadParameters = {},
 ): Promise<AgentThreadOutput> {
   validateThreadId(threadId);
-  const result = await context
-    .path("/threads/{threadId}", threadId)
-    .get(options);
-  if (!expectedStatuses.includes(result.status)) {
+  return TracingUtility.withSpan("GetThread", options, async (updatedOptions) => {
+    const result = await context
+      .path("/threads/{threadId}", threadId)
+      .get(updatedOptions);
+    if (!expectedStatuses.includes(result.status)) {
       throw createRestError(result);
-  }
-  return result.body;
+    }
+    return result.body;
+  }, (span, updatedOptions) => traceStartAgentGeneric(span, { ...updatedOptions, tracingAttributeOptions: { threadId: threadId } }));
 }
 
 /** Modifies an existing thread. */
 export async function updateThread(
   context: Client,
   threadId: string,
-  options?: UpdateThreadParameters,
+  options: UpdateThreadParameters = { body: {} },
 ): Promise<AgentThreadOutput> {
   validateUpdateThreadParameters(threadId, options);
-  const result = await context
-    .path("/threads/{threadId}", threadId)
-    .post(options);
-  if (!expectedStatuses.includes(result.status)) {
+  return TracingUtility.withSpan("UpdateThread", options, async (updatedOptions) => {
+    const result = await context
+      .path("/threads/{threadId}", threadId)
+      .post(updatedOptions);
+    if (!expectedStatuses.includes(result.status)) {
       throw createRestError(result);
-  }
-  return result.body;
+    }
+    return result.body;
+  }, (span, updatedOptions) => traceStartAgentGeneric(span, { ...updatedOptions, tracingAttributeOptions: { threadId: threadId } }));
 }
 
 /** Deletes an existing thread. */
 export async function deleteThread(
   context: Client,
   threadId: string,
-  options?: DeleteThreadParameters,
+  options: DeleteThreadParameters = {},
 ): Promise<ThreadDeletionStatusOutput> {
   validateThreadId(threadId);
-  const result = await context
-    .path("/threads/{threadId}", threadId)
-    .delete(options);
-  if (!expectedStatuses.includes(result.status)) {
+  return TracingUtility.withSpan("DeleteThread", options, async (updatedOptions) => {
+    const result = await context
+      .path("/threads/{threadId}", threadId)
+      .delete(updatedOptions);
+    if (!expectedStatuses.includes(result.status)) {
       throw createRestError(result);
-  }
-  return result.body;
+    }
+    return result.body;
+  }, (span, updatedOptions) => traceStartAgentGeneric(span, { ...updatedOptions, tracingAttributeOptions: { threadId: threadId } }));
 }
 
 
@@ -75,9 +86,9 @@ function validateCreateThreadParameters(options?: CreateThreadParameters): void 
     options.body.messages.forEach(message => validateMessages(message.role));
   }
   if (options?.body.tool_resources) {
-    validateToolResources(options.body.tool_resources);  
+    validateToolResources(options.body.tool_resources);
   }
-  if (options?.body.metadata){
+  if (options?.body.metadata) {
     validateMetadata(options.body.metadata);
   }
 }
@@ -85,9 +96,9 @@ function validateCreateThreadParameters(options?: CreateThreadParameters): void 
 function validateUpdateThreadParameters(threadId: string, options?: UpdateThreadParameters): void {
   validateThreadId(threadId);
   if (options?.body.tool_resources) {
-    validateToolResources(options.body.tool_resources);  
+    validateToolResources(options.body.tool_resources);
   }
-  if (options?.body.metadata){
+  if (options?.body.metadata) {
     validateMetadata(options.body.metadata);
   }
 }
