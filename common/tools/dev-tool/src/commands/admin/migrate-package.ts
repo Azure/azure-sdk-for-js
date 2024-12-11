895871a70d411f772c609bd78bbc643eac436d1b
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-11 17:06:58 -0500

    [perf] Migrate perf to ESM/vitest

--- common/tools/dev-tool/src/commands/admin/migrate-package.ts
+++ common/tools/dev-tool/src/commands/admin/migrate-package.ts
@@ -10,7 +10,7 @@ import { basename, dirname, resolve } from "node:path";
 import { run } from "../../util/run";
 import stripJsonComments from "strip-json-comments";
 import { codemods } from "../../util/admin/migrate-package/codemods";
-import { exists } from "fs-extra";
+import { exists } from "node:fs/promises";
 
 const log = createPrinter("migrate-package");
 
