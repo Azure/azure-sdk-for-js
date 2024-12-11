77943b76e9a1b0e22c4a1c6a7cbd5854e2dc2174
Harsha Nalluru <sanallur@microsoft.com>
2024-12-10 17:57:36 -0800

    compose model sample fix

--- sdk/documentintelligence/ai-document-intelligence-rest/samples-dev/composeModel.ts
+++ sdk/documentintelligence/ai-document-intelligence-rest/samples-dev/composeModel.ts
@@ -59,13 +59,12 @@ async function main() {
           throw initialResponse.body.error;
         }
         const poller = await getLongRunningPoller(client, initialResponse);
-        const model = (
+        const { docTypes } = (
           (await (poller).pollUntilDone()).body as DocumentModelBuildOperationDetailsOutput
         ).result!;
 
-        return model;
+        return docTypes;
       })
-      .map(async (model) => { return { modelId: (await model).modelId } })
   );
 
   // Finally, create the composed model.
@@ -74,10 +73,11 @@ async function main() {
 
   const initialResponse = await client.path("/documentModels:compose").post({
     body: {
-      description: "A composed model that classifies purchase order documents and extracts data from them.",
-      componentModels: modelIds,
       modelId: composedModelId,
-    } as any, // https://github.com/Azure/autorest.typescript/issues/2127
+      description: "A composed model that classifies purchase order documents and extracts data from them.",
+      classifierId: "classifierId", // Add the appropriate classifier ID here
+      docTypes: { model1: modelIds[0]!.modelId, model2: modelIds[1]!.modelId },
+    }
   });
 
   if (isUnexpected(initialResponse)) {
@@ -110,7 +110,7 @@ async function main() {
       console.log("    <no fields>");
       continue;
     }
-    
+
     for (const [fieldName, fieldSchema] of Object.entries(schema)) {
       console.log(`  - "${fieldName}" (${fieldSchema.type})`);
       console.log(`    ${fieldSchema.description || "<no description>"}`);
