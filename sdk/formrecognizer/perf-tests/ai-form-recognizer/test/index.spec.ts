ac21cd2d6b076a765f285a0aa6f5e2ec6ef44efd
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-11 15:01:59 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/formrecognizer/perf-tests/ai-form-recognizer/test/index.spec.ts
+++ sdk/formrecognizer/perf-tests/ai-form-recognizer/test/index.spec.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import { createPerfProgram } from "@azure-tools/test-perf";
-import { CustomModelRecognitionTest } from "./custom.spec";
+import { CustomModelRecognitionTest } from "./custom.spec.js";
 
 import dotenv from "dotenv";
 dotenv.config();
