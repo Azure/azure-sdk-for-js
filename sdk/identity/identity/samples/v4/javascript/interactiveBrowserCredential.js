99b6f79a34bf7f0082c5ce1bd35fbb22c632c1b8
KarishmaGhiya <kaghiya@microsoft.com>
2024-12-12 14:15:35 -0800

    trying some scenarios

--- sdk/identity/identity/samples/v4/javascript/interactiveBrowserCredential.js
+++ sdk/identity/identity/samples/v4/javascript/interactiveBrowserCredential.js
@@ -6,7 +6,8 @@
  */
 
 const { InteractiveBrowserCredential } = require("@azure/identity");
-
+const { setLogLevel } = require("@azure/logger");
+setLogLevel("verbose")
 require("dotenv").config();
 
 const clientId = process.env.AZURE_CLIENT_ID; // The app registration client Id in the Microsoft Entra tenant
@@ -21,10 +22,47 @@ async function main() {
     },
   });
 
-  const token = await credential.getToken("https://storage.azure.com/.default");
+  const token = await credential.getToken("https://graph.microsoft.com/.default");
+  const authenticationRecord = await credential.authenticate("https://graph.microsoft.com/.default");
+  console.log("InteractiveBrowserCredential: Authentication Record:", authenticationRecord);
   console.log(
     "InteractiveBrowserCredential: Successfully got a token with expiry time:",
     token.expiresOnTimestamp,
+  );
+
+    const cred3 = new InteractiveBrowserCredential({
+    clientId: "7960e60e-aabb-47e7-9ac5-7145dd8157a4",
+    tenantId: "72f988bf-86f1-41af-91ab-2d7cd011db47"
+  });
+  const token3 = await cred3.getToken("https://graph.microsoft.com/profile");
+  console.log(
+    "InteractiveBrowserCredential: Successfully got a token with expiry time:",
+    token3.expiresOnTimestamp,
+  );
+
+  // const cred2 = new InteractiveBrowserCredential({
+  //   clientId,
+  //   tenantId,
+  //   authenticationRecord
+  // });
+  // const token2 = await cred2.getToken("https://graph.microsoft.com/.default");
+  // console.log(
+  //   "InteractiveBrowserCredential: Successfully got a token with expiry time:",
+  //   token2.expiresOnTimestamp,
+  // );
+  // const cred3 = new InteractiveBrowserCredential({
+  //   clientId: "7960e60e-aabb-47e7-9ac5-7145dd8157a4",
+  //   tenantId: "72f988bf-86f1-41af-91ab-2d7cd011db47"
+  // });
+  // const token3 = await cred3.getToken("https://graph.microsoft.com/.default");
+  // console.log(
+  //   "InteractiveBrowserCredential: Successfully got a token with expiry time:",
+  //   token3.expiresOnTimestamp,
+  // );
+  const token4 = await credential.getToken("https://graph.microsoft.com/.default");
+  console.log(
+    "InteractiveBrowserCredential: Successfully got a token with expiry time:",
+    token4.expiresOnTimestamp,
   );
 }
 
