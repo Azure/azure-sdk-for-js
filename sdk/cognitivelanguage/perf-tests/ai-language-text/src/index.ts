0e13ee13c6a4870daecc6538dfc017b4ea9aadcf
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-17 15:50:56 -0500

    [perf] Migrate @azure-tests/perf-ai-language-text to ESM (#32208)
    ### Packages impacted by this PR
    
    - @azure-tests/perf-ai-language-text
    - @azure-tests/perf-ai-form-recognizer
    - @azure-tests/perf-service-bus
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/31338
    
    ### Describe the problem that is addressed by this PR
    
    Migrates @azure-tests/perf-ai-language-text to ESM and fixes up previous
    packages already moved to ESM
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/cognitivelanguage/perf-tests/ai-language-text/src/index.ts
+++ sdk/cognitivelanguage/perf-tests/ai-language-text/src/index.ts
@@ -2,10 +2,8 @@
 // Licensed under the MIT License.
 
 import { createPerfProgram } from "@azure-tools/test-perf";
-import { LanguageDetectionTest } from "./languageDetection.spec";
-
-import dotenv from "dotenv";
-dotenv.config();
+import { LanguageDetectionTest } from "./languageDetection.spec.js";
+import "dotenv/config";
 
 const perfProgram = createPerfProgram(LanguageDetectionTest);
 
