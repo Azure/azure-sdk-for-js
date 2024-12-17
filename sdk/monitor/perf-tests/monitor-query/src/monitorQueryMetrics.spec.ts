708295a1400cc256dab541a27890025f2e6253a7
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-17 18:17:00 -0500

    [perf] Migrate @azure-tests/perf-monitor-query to ESM (#32256)
    ### Packages impacted by this PR
    
    - @azure-tests/perf-monitor-query 
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/31338
    
    ### Describe the problem that is addressed by this PR
    
    Migrating @azure-tests/perf-monitor-query to ESM via automation.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/monitor/perf-tests/monitor-query/src/monitorQueryMetrics.spec.ts
+++ sdk/monitor/perf-tests/monitor-query/src/monitorQueryMetrics.spec.ts
@@ -4,10 +4,7 @@
 import { PerfTest } from "@azure-tools/test-perf";
 import { MetricsQueryClient } from "@azure/monitor-query";
 import { DefaultAzureCredential } from "@azure/identity";
-
-// Expects the .env file at the same level
-import * as dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 
 export abstract class MonitorQueryMetrics<TOptions> extends PerfTest<TOptions> {
   client: MetricsQueryClient;
