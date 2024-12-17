e063608e518b8e51f4c0934b1deb14888f2ec7e5
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-17 15:23:06 -0500

    [perf] Migrate @azure-tests/perf-keyvault-certificates to ESM (#32241)
    ### Packages impacted by this PR
    
    - @azure-tests/perf-keyvault-certificates
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/31338
    
    ### Describe the problem that is addressed by this PR
    
    Migrates @azure-tests/perf-keyvault-certificates to ESM via automation.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/keyvault/perf-tests/keyvault-certificates/src/index.ts
+++ sdk/keyvault/perf-tests/keyvault-certificates/src/index.ts
@@ -1,5 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { createPerfProgram } from "@azure-tools/test-perf";
-import { GetCertificateTest } from "./getCertificate.spec";
+import { GetCertificateTest } from "./getCertificate.spec.js";
 
 const perfProgram = createPerfProgram(GetCertificateTest);
 
