9e5c0c4a269f1c0f1e970e24067b7b3e8045c990
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-17 15:02:45 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/schemaregistry/perf-tests/schema-registry-avro/test/index.spec.ts
+++ sdk/schemaregistry/perf-tests/schema-registry-avro/test/index.spec.ts
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 import { createPerfProgram } from "@azure-tools/test-perf";
-import { SerializeTest } from "./serialize.spec";
-import { DeserializeTest } from "./deserialize.spec";
+import { SerializeTest } from "./serialize.spec.js";
+import { DeserializeTest } from "./deserialize.spec.js";
 
 import dotenv from "dotenv";
 dotenv.config();
