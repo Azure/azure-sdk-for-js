291d62b04df2cad5f4e0012d04355ce23f1e2627
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-12-17 13:48:41 -0800

    Fix build warnings

--- sdk/schemaregistry/schema-registry-avro/src/index.ts
+++ sdk/schemaregistry/schema-registry-avro/src/index.ts
@@ -1,6 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+declare global {
+    interface Blob {}
+  }
+  
+
 export { AvroSerializer } from "./avroSerializer.js";
 
 export * from "./models.js";
