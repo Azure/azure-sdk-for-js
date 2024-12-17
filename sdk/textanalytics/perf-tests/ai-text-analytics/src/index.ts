6ab19e86eb5d05a7e5a6cfb7b13d9fd97aff6a19
Matthew Podwysocki <matthewp@microsoft.com>
2024-12-16 19:49:58 -0500

    [perf] Migrate @azure-tests/perf-ai-text-analytics to ESM (#32212)
    ### Packages impacted by this PR
    
    - @azure-tests/perf-ai-text-analytics
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/31338
    
    ### Describe the problem that is addressed by this PR
    
    Migrates @azure-tests/perf-ai-text-analytics to ESM via automation
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/textanalytics/perf-tests/ai-text-analytics/src/index.ts
+++ sdk/textanalytics/perf-tests/ai-text-analytics/src/index.ts
@@ -2,10 +2,8 @@
 // Licensed under the MIT License.
 
 import { createPerfProgram } from "@azure-tools/test-perf";
-import { DetectLanguageTest } from "./detectLanguage.spec";
-
-import dotenv from "dotenv";
-dotenv.config();
+import { DetectLanguageTest } from "./detectLanguage.spec.js";
+import "dotenv/config";
 
 const perfProgram = createPerfProgram(DetectLanguageTest);
 
