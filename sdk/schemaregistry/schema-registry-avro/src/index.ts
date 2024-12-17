917e4c6dd2ad8ba23b097d0aaa8158d436f39a76
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-12-17 14:11:26 -0800

    remove unused Blob

--- sdk/schemaregistry/schema-registry-avro/src/index.ts
+++ sdk/schemaregistry/schema-registry-avro/src/index.ts
@@ -1,11 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-declare global {
-    interface Blob {}
-  }
-  
-
 export { AvroSerializer } from "./avroSerializer.js";
 
 export * from "./models.js";
