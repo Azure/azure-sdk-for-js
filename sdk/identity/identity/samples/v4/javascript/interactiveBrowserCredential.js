6937087823a82a54819d216dd43a51dff55624ce
KarishmaGhiya <kaghiya@microsoft.com>
2024-12-19 15:56:18 -0800

    update

--- sdk/identity/identity/samples/v4/javascript/interactiveBrowserCredential.js
+++ sdk/identity/identity/samples/v4/javascript/interactiveBrowserCredential.js
@@ -5,12 +5,8 @@
  * @summary Authenticates using Interactive Browser Credential
  */
 
-const { InteractiveBrowserCredential, useIdentityPlugin } = require("@azure/identity");
-const { cachePersistencePlugin } = require("@azure/identity-cache-persistence");
+const { InteractiveBrowserCredential } = require("@azure/identity");
 
-useIdentityPlugin(cachePersistencePlugin);
-const { setLogLevel } = require("@azure/logger");
-setLogLevel("verbose")
 require("dotenv").config();
 
 const clientId = process.env.AZURE_CLIENT_ID; // The app registration client Id in the Microsoft Entra tenant
@@ -18,65 +14,18 @@ const tenantId = process.env.AZURE_TENANT_ID; // The tenant ID in Microsoft Entr
 
 async function main() {
   const credential = new InteractiveBrowserCredential({
-    loginHint: "kghiya8@gmail.com",
-    tokenCachePersistenceOptions:
-    {
-      enabled: true,
-      name: "myCache",
-      cachePlugin: cachePersistencePlugin
-    }
+    clientId,
+    tenantId,
+    browserCustomizationOptions: {
+      successMessage: "Authentication Completed. You can close the browser.",
+    },
   });
 
- 
-  // const token = await credential.getToken("https://management.azure.com/.default");
-  // console.log(
-  //   "InteractiveBrowserCredential: Successfully got a token with expiry time:",
-  //   token.expiresOnTimestamp,
-  // );
-  const authenticationRecord = await credential.authenticate("https://management.azure.com/.default");
-  console.log("InteractiveBrowserCredential: Authentication Record:", authenticationRecord);
-  
-
-  const cred2 = new InteractiveBrowserCredential({
-        loginHint: "kaghiya@microsoft.com"
-      });
-  const authenticationRecord2 = await cred2.authenticate("https://management.azure.com/.default");
-  console.log("InteractiveBrowserCredential: Authentication Record:", authenticationRecord2);
-
-  //   const cred3 = new InteractiveBrowserCredential({
-  //   clientId: "7960e60e-aabb-47e7-9ac5-7145dd8157a4",
-  //   tenantId: "72f988bf-86f1-41af-91ab-2d7cd011db47"
-  // });
-  // const token3 = await cred3.getToken("https://graph.microsoft.com/profile");
-  // console.log(
-  //   "InteractiveBrowserCredential: Successfully got a token with expiry time:",
-  //   token3.expiresOnTimestamp,
-  // );
-
-  // const cred2 = new InteractiveBrowserCredential({
-  //   clientId,
-  //   tenantId,
-  //   authenticationRecord
-  // });
-  // const token2 = await cred2.getToken("https://graph.microsoft.com/.default");
-  // console.log(
-  //   "InteractiveBrowserCredential: Successfully got a token with expiry time:",
-  //   token2.expiresOnTimestamp,
-  // );
-  // const cred3 = new InteractiveBrowserCredential({
-  //   clientId: "7960e60e-aabb-47e7-9ac5-7145dd8157a4",
-  //   tenantId: "72f988bf-86f1-41af-91ab-2d7cd011db47"
-  // });
-  // const token3 = await cred3.getToken("https://graph.microsoft.com/.default");
-  // console.log(
-  //   "InteractiveBrowserCredential: Successfully got a token with expiry time:",
-  //   token3.expiresOnTimestamp,
-  // );
-  // const token4 = await credential.getToken("https://graph.microsoft.com/.default");
-  // console.log(
-  //   "InteractiveBrowserCredential: Successfully got a token with expiry time:",
-  //   token4.expiresOnTimestamp,
-  // );
+  const token = await credential.getToken("https://storage.azure.com/.default");
+  console.log(
+    "InteractiveBrowserCredential: Successfully got a token with expiry time:",
+    token.expiresOnTimestamp,
+  );
 }
 
 main().catch((err) => {
