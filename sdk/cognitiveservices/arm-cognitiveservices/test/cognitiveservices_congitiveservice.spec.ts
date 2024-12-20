91188523cc156709e92eb3da34d4ff7b3d4d984a
Mary Gao <yanmeigao1210@gmail.com>
2024-12-20 15:29:45 +0800

    Update sdk/cognitiveservices/arm-cognitiveservices/test/cognitiveservices_congitiveservice.spec.ts

--- sdk/cognitiveservices/arm-cognitiveservices/test/cognitiveservices_congitiveservice.spec.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/test/cognitiveservices_congitiveservice.spec.ts
@@ -33,7 +33,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe.only("CognitiveServices test", () => {
+describe("CognitiveServices test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: CognitiveServicesManagementClient;
