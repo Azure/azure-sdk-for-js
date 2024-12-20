772fb3fd8541179ba7cb4174b6ea1ee64ad29345
azure-sdk <azuresdk@microsoft.com>
2024-12-20 06:25:21 +0000

    Post release automated changes for azure-arm-playwrighttesting

--- sdk/playwrighttesting/arm-playwrighttesting/src/api/azurePlaywrightServiceContext.ts
+++ sdk/playwrighttesting/arm-playwrighttesting/src/api/azurePlaywrightServiceContext.ts
@@ -23,7 +23,7 @@ export function createAzurePlaywrightService(
 ): AzurePlaywrightServiceContext {
   const endpointUrl = options.endpoint ?? options.baseUrl ?? `https://management.azure.com`;
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-playwrighttesting/1.0.0`;
+  const userAgentInfo = `azsdk-js-arm-playwrighttesting/1.0.1`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
