255d3f1eaa9bf54d29a7dec565a6a3fb1ed6aa9c
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-12-23 15:43:29 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/network/arm-network-rest/test/public/network_rest_sample.spec.ts
+++ sdk/network/arm-network-rest/test/public/network_rest_sample.spec.ts
@@ -12,7 +12,6 @@
 import type { RecorderStartOptions } from "@azure-tools/test-recorder";
 import { Recorder, env, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import type {
   IpGroupsCreateOrUpdateParameters,
   IpGroupsDeleteParameters,
@@ -31,6 +30,7 @@ import type {
 } from "../../src/index.js";
 import { getLongRunningPoller, isUnexpected, paginate } from "../../src/index.js";
 import { createTestNetworkManagementClient } from "./utils/recordedClient.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id",
