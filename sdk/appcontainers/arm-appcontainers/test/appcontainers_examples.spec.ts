3047837f8665067c5ab54fd03206e2490992bbaa
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-12-24 15:23:41 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/appcontainers/arm-appcontainers/test/appcontainers_examples.spec.ts
+++ sdk/appcontainers/arm-appcontainers/test/appcontainers_examples.spec.ts
@@ -13,9 +13,9 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { ContainerAppsAPIClient } from "../src/containerAppsAPIClient.js";
 import { ContainerApp, ManagedEnvironment } from "../src/models/index.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
