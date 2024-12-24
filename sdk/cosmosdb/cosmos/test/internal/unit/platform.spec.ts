50dff4eea1ed58f42e9c72a35221f2580f6dd478
Ujjwal Soni <soniujjwal@microsoft.com>
2024-12-24 16:28:32 +0530

    minor change

--- sdk/cosmosdb/cosmos/test/internal/unit/platform.spec.ts
+++ sdk/cosmosdb/cosmos/test/internal/unit/platform.spec.ts
@@ -14,9 +14,11 @@ describe("getUserAgent", function () {
   });
 
   it("should contain the current node version", () => {
-    console.log(getUserAgent());
-    console.log(process.version.replace("v", ""));
-    assert(getUserAgent().includes(process.version.replace("v", "")));
+    if (globalThis.navigator && globalThis.navigator.userAgent) {
+      assert(getUserAgent().includes(globalThis.navigator.userAgent));
+    } else if (globalThis.process && globalThis.process.version) {
+      assert(getUserAgent().includes(process.version.replace("v", "")));
+    }
   });
 
   it("should allow a custom suffix", () => {
