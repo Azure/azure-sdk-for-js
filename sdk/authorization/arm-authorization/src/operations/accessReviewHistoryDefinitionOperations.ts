d035262758e6a81898038af919997efc1e520c67
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-12-25 10:30:26 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/authorization/arm-authorization/src/operations/accessReviewHistoryDefinitionOperations.ts
+++ sdk/authorization/arm-authorization/src/operations/accessReviewHistoryDefinitionOperations.ts
@@ -6,17 +6,17 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { AccessReviewHistoryDefinitionOperations } from "../operationsInterfaces";
+import { AccessReviewHistoryDefinitionOperations } from "../operationsInterfaces/index.js";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers";
-import * as Parameters from "../models/parameters";
-import { AuthorizationManagementClient } from "../authorizationManagementClient";
+import * as Mappers from "../models/mappers.js";
+import * as Parameters from "../models/parameters.js";
+import { AuthorizationManagementClient } from "../authorizationManagementClient.js";
 import {
   AccessReviewHistoryDefinitionProperties,
   AccessReviewHistoryDefinitionCreateOptionalParams,
   AccessReviewHistoryDefinitionCreateResponse,
   AccessReviewHistoryDefinitionDeleteByIdOptionalParams
-} from "../models";
+} from "../models/index.js";
 
 /** Class containing AccessReviewHistoryDefinitionOperations operations. */
 export class AccessReviewHistoryDefinitionOperationsImpl
--- sdk/authorization/arm-authorization/src/operationsInterfaces/accessReviewHistoryDefinitionInstance.ts
+++ sdk/authorization/arm-authorization/src/operationsInterfaces/accessReviewHistoryDefinitionInstance.ts
@@ -9,7 +9,7 @@
 import {
   AccessReviewHistoryDefinitionInstanceGenerateDownloadUriOptionalParams,
   AccessReviewHistoryDefinitionInstanceGenerateDownloadUriResponse
-} from "../models";
+} from "../models/index.js";
 
 /** Interface representing a AccessReviewHistoryDefinitionInstance. */
 export interface AccessReviewHistoryDefinitionInstance {
