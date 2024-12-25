01ab52b632993c4f682094596126e9dac825ef69
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-12-25 15:06:26 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/botservice/arm-botservice/test/botservice_examples.ts
+++ sdk/botservice/arm-botservice/test/botservice_examples.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { AzureBotService } from "../src/azureBotService";
+import { AzureBotService } from "../src/azureBotService.js";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id"
@@ -42,8 +40,8 @@ describe("Botservice test", () => {
   let resourceGroup: string;
   let botresourceName: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
+  beforeEach(async function (ctx) {
+    recorder = new Recorder(ctx);
     await recorder.start(recorderOptions);
     subscriptionId = env.SUBSCRIPTION_ID || '';
     // This is an example of how the environment variables are used
