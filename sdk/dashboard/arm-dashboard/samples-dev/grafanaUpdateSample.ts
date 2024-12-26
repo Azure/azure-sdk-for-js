2cb8646f7233cbcef4cee743815cd65e2bc01453
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-12-26 15:39:02 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/dashboard/arm-dashboard/samples-dev/grafanaUpdateSample.ts
+++ sdk/dashboard/arm-dashboard/samples-dev/grafanaUpdateSample.ts
@@ -13,9 +13,7 @@ import {
   DashboardManagementClient
 } from "@azure/arm-dashboard";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Update a workspace for Grafana resource.
