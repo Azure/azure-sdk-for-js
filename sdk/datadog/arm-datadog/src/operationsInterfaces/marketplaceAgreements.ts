0d3ac9905a720ca3b1d3ec2e0b6ee0c9e471f7d9
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2024-12-27 10:15:52 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/datadog/arm-datadog/src/operationsInterfaces/marketplaceAgreements.ts
+++ sdk/datadog/arm-datadog/src/operationsInterfaces/marketplaceAgreements.ts
@@ -12,7 +12,7 @@ import {
   MarketplaceAgreementsListOptionalParams,
   MarketplaceAgreementsCreateOrUpdateOptionalParams,
   MarketplaceAgreementsCreateOrUpdateResponse
-} from "../models";
+} from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Interface representing a MarketplaceAgreements. */
