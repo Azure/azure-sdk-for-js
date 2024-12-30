b9f9a6561bda065ce50676b03f13684088ed877b
Jeremy Meng <jeremy.ymeng@gmail.com>
2024-12-30 14:11:52 -0800

    better assertions

--- sdk/tables/data-tables/test/internal/isCosmosEndpoint.spec.ts
+++ sdk/tables/data-tables/test/internal/isCosmosEndpoint.spec.ts
@@ -7,36 +7,36 @@ import { isCosmosEndpoint } from "../../src/utils/isCosmosEndpoint.js";
 describe("isCosmosEndpoint", () => {
   it("returns true for cosmosdb.* host", () => {
     const result = isCosmosEndpoint("https://abc.table.cosmosdb.azure.com");
-    assert.equal(result, true);
+    assert.isTrue(result);
   });
 
   it("returns true for cosmos.* host", () => {
     const result = isCosmosEndpoint("https://abc.table.cosmos.azure.com");
-    assert.equal(result, true);
+    assert.isTrue(result);
   });
 
   it("returns true for localhost", () => {
     const result = isCosmosEndpoint("https://localhost:8092");
-    assert.equal(result, true);
+    assert.isTrue(result);
   });
 
   it("returns false for azurite default endpoint", () => {
     const result = isCosmosEndpoint("https://127.0.0.1:10002/devstoreaccount1");
-    assert.equal(result, false);
+    assert.isFalse(result);
   });
 
   it("returns false for azurite default endpoint with non-default port", () => {
     const result = isCosmosEndpoint("https://127.0.0.1:20002/devstoreaccount1");
-    assert.equal(result, false);
+    assert.isFalse(result);
   });
 
   it("returns false for azurite default localhost endpoint", () => {
     const result = isCosmosEndpoint("https://localhost:10002/devstoreaccount1");
-    assert.equal(result, false);
+    assert.isFalse(result);
   });
 
   it("returns false for azurite default localhost endpoint with non-default port", () => {
     const result = isCosmosEndpoint("https://localhost:20002/devstoreaccount1");
-    assert.equal(result, false);
+    assert.isFalse(result);
   });
 });
