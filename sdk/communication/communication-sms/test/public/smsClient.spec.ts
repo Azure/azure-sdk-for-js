76429872226c08df8bbeedeb336a7c49f5c40685
Pavlo Hermanov <117445225+phermanov-msft@users.noreply.github.com>
2025-01-02 13:47:14 +0200

    [Communication][SMS] Fix test, update recordings (#32402)
    ### Packages impacted by this PR
    SMS
    
    ### Issues associated with this PR
    https://github.com/Azure/azure-sdk-for-js/issues/32393
    
    ### Describe the problem that is addressed by this PR
    Fix the tests and upload recordings
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/communication/communication-sms/test/public/smsClient.spec.ts
+++ sdk/communication/communication-sms/test/public/smsClient.spec.ts
@@ -181,7 +181,7 @@ matrix([[false, true]], async function (useAad: boolean) {
             const results = await client.optOuts.check(fromNumber, [validToNumber]);
 
             assert.lengthOf(results, 1, "must return as many results as there were recipients");
-            assert.equal(200, results[0].httpStatusCode);
+            assert.equal(results[0].httpStatusCode, 200);
           },
         );
 
@@ -194,7 +194,7 @@ matrix([[false, true]], async function (useAad: boolean) {
             const results = await client.optOuts.add(fromNumber, [validToNumber]);
 
             assert.lengthOf(results, 1, "must return as many results as there were recipients");
-            assert.equal(200, results[0].httpStatusCode);
+            assert.equal(results[0].httpStatusCode, 200);
           },
         );
 
@@ -207,7 +207,7 @@ matrix([[false, true]], async function (useAad: boolean) {
             const results = await client.optOuts.remove(fromNumber, [validToNumber]);
 
             assert.lengthOf(results, 1, "must return as many results as there were recipients");
-            assert.equal(200, results[0].httpStatusCode);
+            assert.equal(results[0].httpStatusCode, 200);
           },
         );
 
@@ -216,11 +216,11 @@ matrix([[false, true]], async function (useAad: boolean) {
           const validToNumber = env.AZURE_PHONE_NUMBER as string;
 
           const addResults = await client.optOuts.add(fromNumber, [validToNumber]);
-          assert.equal(200, addResults[0].httpStatusCode);
+          assert.equal(addResults[0].httpStatusCode, 200);
 
           const checkResults = await client.optOuts.check(fromNumber, [validToNumber]);
-          assert.equal(200, checkResults[0].httpStatusCode);
-          assert.equal(true, checkResults[0].isOptedOut);
+          assert.equal(checkResults[0].httpStatusCode, 200);
+          assert.equal(checkResults[0].isOptedOut, true);
         });
 
         it("OptOut Remove should mark recipient as opted in", { timeout: 4000 }, async () => {
@@ -228,11 +228,11 @@ matrix([[false, true]], async function (useAad: boolean) {
           const validToNumber = env.AZURE_PHONE_NUMBER as string;
 
           const removeResults = await client.optOuts.remove(fromNumber, [validToNumber]);
-          assert.equal(200, removeResults[0].httpStatusCode);
+          assert.equal(removeResults[0].httpStatusCode, 200);
 
           const checkResults = await client.optOuts.check(fromNumber, [validToNumber]);
-          assert.equal(200, checkResults[0].httpStatusCode);
-          assert.equal(false, checkResults[0].isOptedOut);
+          assert.equal(checkResults[0].httpStatusCode, 200);
+          assert.equal(checkResults[0].isOptedOut, false);
         });
       });
     },
