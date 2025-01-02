ffb7d1ac83e988665ce77a28a6676267153dd87a
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-01-02 10:14:17 +0800

    Update paginateHelper.ts

--- sdk/purview/purview-scanning-rest/src/paginateHelper.ts
+++ sdk/purview/purview-scanning-rest/src/paginateHelper.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { PagedAsyncIterableIterator, PagedResult } from "@azure/core-paging";
+import type { PagedAsyncIterableIterator, PagedResult } from "@azure/core-paging";
 import { getPagedAsyncIterator } from "@azure/core-paging";
 import type { Client, PathUncheckedResponse } from "@azure-rest/core-client";
 import { createRestError } from "@azure-rest/core-client";
@@ -69,16 +69,16 @@ export function paginate<TResponse extends PathUncheckedResponse>(
       typeof customGetPage === "function"
         ? customGetPage
         : async (pageLink: string) => {
-          const result = firstRun ? initialResponse : await client.pathUnchecked(pageLink).get();
-          firstRun = false;
-          checkPagingRequest(result);
-          const nextLink = getNextLink(result.body, nextLinkName);
-          const values = getElements<TElement>(result.body, itemName);
-          return {
-            page: values,
-            nextPageLink: nextLink,
-          };
-        },
+            const result = firstRun ? initialResponse : await client.pathUnchecked(pageLink).get();
+            firstRun = false;
+            checkPagingRequest(result);
+            const nextLink = getNextLink(result.body, nextLinkName);
+            const values = getElements<TElement>(result.body, itemName);
+            return {
+              page: values,
+              nextPageLink: nextLink,
+            };
+          },
   };
 
   return getPagedAsyncIterator(pagedResult);
