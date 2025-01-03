9b82beac8a08edf6fba7b266a777388665682303
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-03 18:10:27 -0500

    [dev-tool] Fix snippet extraction

--- common/tools/dev-tool/src/commands/run/update-snippets.ts
+++ common/tools/dev-tool/src/commands/run/update-snippets.ts
@@ -70,6 +70,7 @@ const IGNORE_CODE_COMMENT = "// dev-tool snippets ignore";
 const IGNORE_MARKDOWN_COMMENT = "<!-- dev-tool snippets ignore -->";
 const TS_PRESERVE_WHITESPACE = /\r?\n[ ]*\/\/\s*@ts-preserve-whitespace\s*\r?\n/g;
 const TS_IGNORE = /\r?\n[ ]*\/\/\s*@ts-ignore\s*\r?\n/g;
+const UNIX_EOL = "\n";
 
 /**
  * Finds all the snippet locations in a project.
@@ -288,12 +289,12 @@ async function parseSnippetDefinitions(
         .replace(
           // Need to get rid of any ts-ignores that were added because of unused symbols
           TS_IGNORE,
-          EOL,
+          UNIX_EOL,
         )
         .replace(
           // Need to get rid of any ts-ignores that were added because of unused symbols
           TS_PRESERVE_WHITESPACE,
-          EOL + EOL,
+          UNIX_EOL + UNIX_EOL,
         )
         .trim();
 
