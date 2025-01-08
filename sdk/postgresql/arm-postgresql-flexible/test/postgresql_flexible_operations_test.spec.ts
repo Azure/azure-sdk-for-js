b739c1a4a877cba04da3a6931c3b8f694b974515
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-01-08 17:35:08 +0800

    Update sdk/postgresql/arm-postgresql-flexible/test/postgresql_flexible_operations_test.spec.ts

--- sdk/postgresql/arm-postgresql-flexible/test/postgresql_flexible_operations_test.spec.ts
+++ sdk/postgresql/arm-postgresql-flexible/test/postgresql_flexible_operations_test.spec.ts
@@ -14,7 +14,7 @@ import {
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { PostgreSQLManagementFlexibleServerClient } from "../src/postgreSQLManagementFlexibleServerClient.js";
-import { describe, it, beforeEach, afterEach } from "vitest";
+import { describe, it, beforeEach, afterEach, assert  } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id"
