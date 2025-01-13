20495b23b9783135a19f61fae99208599f60f53a
Manik Khandelwal <113669638+topshot99@users.noreply.github.com>
2025-01-13 08:29:34 +0000

    Feature/query pipeline rewrite (#32517)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Manik Khandelwal <mkhandelwal@microsoft.com>
    Co-authored-by: Aman Rao <amanrao@microsoft.com>

--- sdk/cosmosdb/cosmos/test/internal/unit/common/TestOrderbyQueryExecutionContext.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import { ExecutionContext } from "../../../../src/queryExecutionContext";
-import { OrderByQueryExecutionContext } from "../../../../src/queryExecutionContext/orderByQueryExecutionContext";
-
-export class TestOrderbyQueryExecutionContext
-  extends OrderByQueryExecutionContext
-  implements ExecutionContext {}
