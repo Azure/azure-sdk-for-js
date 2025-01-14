d28d39468122dc60bd5488e842b990becff5e9b3
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-01-14 15:46:13 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/dataprotection/arm-dataprotection/samples/v2/typescript/src/dppResourceGuardProxyGetSample.ts
+++ sdk/dataprotection/arm-dataprotection/samples/v2/typescript/src/dppResourceGuardProxyGetSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { DataProtectionClient } from "@azure/arm-dataprotection";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Returns the ResourceGuardProxy object associated with the vault, and that matches the name in the request
