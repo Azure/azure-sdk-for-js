dfc549f64ea595998f1aa5f0bcd5455192adf636
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-01-14 16:25:43 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/desktopvirtualization/arm-desktopvirtualization/samples-dev/scalingPlansListBySubscriptionSample.ts
+++ sdk/desktopvirtualization/arm-desktopvirtualization/samples-dev/scalingPlansListBySubscriptionSample.ts
@@ -13,9 +13,7 @@ import {
   DesktopVirtualizationAPIClient,
 } from "@azure/arm-desktopvirtualization";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to List scaling plans in subscription.
