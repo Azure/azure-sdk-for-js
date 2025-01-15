1540dc2279993826e27f4adeac89409cf0c8beea
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-01-15 14:48:58 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/digitaltwins/arm-digitaltwins/test/digitaltwins_examples.spec.ts
+++ sdk/digitaltwins/arm-digitaltwins/test/digitaltwins_examples.spec.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { AzureDigitalTwinsManagementClient } from "../src/azureDigitalTwinsManagementClient";
+import { AzureDigitalTwinsManagementClient } from "../src/azureDigitalTwinsManagementClient.js";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
@@ -45,21 +43,21 @@ describe("DigitalTwins test", () => {
   let resourceGroup: string;
   let resourceName: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new AzureDigitalTwinsManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus";
-    resourceGroup = "myjstest";
-    resourceName = "myDigitalTwinsService";
-  });
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new AzureDigitalTwinsManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "eastus";
+      resourceGroup = "myjstest";
+      resourceName = "myDigitalTwinsService";
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("DigitalTwins create test", async function () {
     const res = await client.digitalTwins.beginCreateOrUpdateAndWait(
@@ -85,7 +83,6 @@ describe("DigitalTwins test", () => {
 
   it("DigitalTwins delete test", async function () {
     const resArray = new Array();
-    const res = await client.digitalTwins.beginDeleteAndWait(resourceGroup, resourceName, testPollingOptions)
     for await (let item of client.digitalTwins.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
