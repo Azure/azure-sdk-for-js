f18bfe444b6742f4d96925d959f4a7a99ff91ca7
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-01-16 21:06:52 +0800

    [mgmt] migrate mgmt package to esm 10 (#32546)
    https://github.com/Azure/azure-sdk-for-js/issues/32184
    @azure/arm-datadog
    @azure/arm-datafactory
    @azure/arm-datalake-analytics
    @azure/arm-datamigration
    @azure/arm-dataprotection

--- sdk/dataprotection/arm-dataprotection/samples-dev/dataProtectionCheckFeatureSupportSample.ts
+++ sdk/dataprotection/arm-dataprotection/samples-dev/dataProtectionCheckFeatureSupportSample.ts
@@ -13,9 +13,7 @@ import {
   DataProtectionClient,
 } from "@azure/arm-dataprotection";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Validates if a feature is supported
@@ -23,7 +21,7 @@ dotenv.config();
  * @summary Validates if a feature is supported
  * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2024-04-01/examples/CheckfeatureSupport.json
  */
-async function checkAzureVMBackupFeatureSupport() {
+async function checkAzureVMBackupFeatureSupport(): Promise<void> {
   const subscriptionId =
     process.env["DATAPROTECTION_SUBSCRIPTION_ID"] ||
     "0b352192-dcac-4cc7-992e-a96190ccc68c";
@@ -41,7 +39,7 @@ async function checkAzureVMBackupFeatureSupport() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   checkAzureVMBackupFeatureSupport();
 }
 
--- sdk/dataprotection/arm-dataprotection/samples-dev/resourceGuardsGetResourcesInSubscriptionSample.ts
+++ sdk/dataprotection/arm-dataprotection/samples-dev/resourceGuardsGetResourcesInSubscriptionSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { DataProtectionClient } from "@azure/arm-dataprotection";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Returns ResourceGuards collection belonging to a subscription.
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary Returns ResourceGuards collection belonging to a subscription.
  * x-ms-original-file: specification/dataprotection/resource-manager/Microsoft.DataProtection/stable/2024-04-01/examples/ResourceGuardCRUD/GetResourceGuardsInSubscription.json
  */
-async function getResourceGuardsInSubscription() {
+async function getResourceGuardsInSubscription(): Promise<void> {
   const subscriptionId =
     process.env["DATAPROTECTION_SUBSCRIPTION_ID"] ||
     "0b352192-dcac-4cc7-992e-a96190ccc68c";
@@ -33,7 +31,7 @@ async function getResourceGuardsInSubscription() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   getResourceGuardsInSubscription();
 }
 
