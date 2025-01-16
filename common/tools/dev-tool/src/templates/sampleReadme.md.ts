Azure/azure-sdk-for-js

commit e8acf6965890b2091fedb766ce22c5f3078df39c

[dev-tool] use package scope when generating sample apiref link (#32465)
Currently the apiref link template has a hardcoded `@azure` scope so it doesn't
work for packages with `@azure-rest` scope. Those packages rely on the
customized `apiRefLink` fields in the sample's configuration section of
package.json. This PR adds package scope variable to the apiref link template as
well so the link generation works for both `@azure` and `@azure-rest` scopes.
These two scopes are the only ones that have SDK API reference pages currently.

With this change, customized `apiRefLink` for -rest packages are also removed.

common/tools/dev-tool/src/templates/sampleReadme.md.ts
@@ -5,6 +5,9 @@ import path from "node:path";
 import YAML from "yaml";
 import { SampleReadmeConfiguration } from "../util/samples/info";
 import { format } from "../util/prettier";
+import { createPrinter } from "../util/printer";
+
+const log = createPrinter("readme-template");
 
 /**
  * Renders the frontmatter of the sample README.
@@ -143,7 +146,7 @@ function exampleNodeInvocation(info: SampleReadmeConfiguration) {
 }
 
 /**
- * Create a link to the package.
+ * Creates a link to the package.
  * @param info - the README configuration
  * @returns a link to the project
  */
@@ -155,15 +158,47 @@ function createReadmeLink(info: SampleReadmeConfiguration) {
 }
 
 /**
+ * Checks whether a url exists
+ * @param url -
+ * @returns true if the url is accessible; false otherwise
+ */
+async function urlExists(url: string): Promise<boolean> {
+  const res = await fetch(url);
+  await res.text();
+  return res.ok;
+}
+
+/**
+ * Creates link to the SDK package's Api reference page on learn.microsoft.com.
+ * @param info - the README configuration
+ * @returns a link to the api reference page.
+            This methods validates the generated api link first and will return an empty
+            string if the generated link is not valid.
+            No validation is done for customized api ref link that is passed in.
+ */
+async function createApiRef(info: SampleReadmeConfiguration): Promise<string> {
+  if (info.apiRefLink) {
+    return `[apiref]: ${info.apiRefLink}`;
+  } else if (info.scope.startsWith("@azure")) {
+    const link = `https://learn.microsoft.com/javascript/api/${info.scope}/${info.baseName}${info.isBeta ? "?view=azure-node-preview" : ""}`;
+    if (await urlExists(link)) {
+      return `[apiref]: ${link}`;
+    }
+    log.warn(`failed to reach api reference ${link} so not adding it.`);
+  }
+  return "";
+}
+
+/**
  * Creates a README for a sample package from a SampleReadmeConfiguration.
  */
-export default (info: SampleReadmeConfiguration): Promise<string> => {
+export default async (info: SampleReadmeConfiguration): Promise<string> => {
   let stepCount = 1;
   const step = (content: string) => `${stepCount++}. ${content}`;
 
   const language = info.useTypeScript ? "TypeScript" : "JavaScript";
 
-  return format(
+  return await format(
     `${formatFrontmatter(info.frontmatter)}\
 # ${info.productName} client library samples for ${language}${info.isBeta ? " (Beta)" : ""}
 
@@ -241,7 +276,7 @@ Take a look at our [API Documentation][apiref] for more information about the AP
 ${info.customSnippets?.footer ?? ""}
 
 ${fileLinks(info)}
-[apiref]: ${info.apiRefLink ?? `https://learn.microsoft.com/javascript/api/@azure/${info.baseName}`}
+${await createApiRef(info)}
 [freesub]: https://azure.microsoft.com/free/
 ${resourceLinks(info)}
 [package]: ${createReadmeLink(info)}

common/tools/dev-tool/src/util/samples/generation.ts
@@ -97,8 +97,7 @@ export async function makeSampleGenerationInfo(
 
   const sampleConfiguration = getSampleConfiguration(projectInfo.packageJson);
 
-  const baseName = projectInfo.name.split("/").slice(-1)[0];
-
+  const [scope, baseName] = projectInfo.name.split("/");
   log.debug("Determined project baseName:", baseName);
 
   // A helper to handle configuration errors.
@@ -139,6 +138,7 @@ export async function makeSampleGenerationInfo(
 
   return {
     ...sampleConfiguration,
+    scope,
     baseName,
     packageKeywords:
       projectInfo.packageJson.keywords ??

common/tools/dev-tool/src/util/samples/info.ts
@@ -52,6 +52,10 @@ export const enum OutputKind {
  */
 export interface SampleGenerationInfo extends SampleConfiguration {
   /**
+   * The scope part of the package name. For example, the base part of "@azure/template" is "@azure".
+   */
+  scope: string;
+  /**
    * The base part of the package name. For example, the base part of "@azure/template" is "template".
    */
   baseName: string;

commit 414f28053f48f57e88259c542743b9a2ce3e8188

[dev-tool] fix unit tests
Sample generation tests use package names that doesn't have scope so they are broken by e8acf696589 which
assumes package names are always in the form of "@scope/packageName". This PR handles the non-scope case.

common/tools/dev-tool/src/templates/sampleReadme.md.ts
@@ -179,7 +179,7 @@ async function urlExists(url: string): Promise<boolean> {
 async function createApiRef(info: SampleReadmeConfiguration): Promise<string> {
   if (info.apiRefLink) {
     return `[apiref]: ${info.apiRefLink}`;
-  } else if (info.scope.startsWith("@azure")) {
+  } else if (info.scope?.startsWith("@azure")) {
     const link = `https://learn.microsoft.com/javascript/api/${info.scope}/${info.baseName}${info.isBeta ? "?view=azure-node-preview" : ""}`;
     if (await urlExists(link)) {
       return `[apiref]: ${link}`;

common/tools/dev-tool/src/util/samples/generation.ts
@@ -97,8 +97,14 @@ export async function makeSampleGenerationInfo(
 
   const sampleConfiguration = getSampleConfiguration(projectInfo.packageJson);
 
-  const [scope, baseName] = projectInfo.name.split("/");
-  log.debug("Determined project baseName:", baseName);
+  let scope, baseName: string | undefined;
+  [scope, baseName] = projectInfo.name.split("/");
+  if (baseName === undefined) {
+    log.debug(`unscoped package name: ${projectInfo.name}`);
+    baseName = scope;
+    scope = undefined;
+  }
+  log.debug(`Determined project scope: ${scope}, baseName: ${baseName}`);
 
   // A helper to handle configuration errors.
   function fail(...values: unknown[]): never {

common/tools/dev-tool/src/util/samples/info.ts
@@ -54,7 +54,7 @@ export interface SampleGenerationInfo extends SampleConfiguration {
   /**
    * The scope part of the package name. For example, the base part of "@azure/template" is "@azure".
    */
-  scope: string;
+  scope?: string;
   /**
    * The base part of the package name. For example, the base part of "@azure/template" is "template".
    */