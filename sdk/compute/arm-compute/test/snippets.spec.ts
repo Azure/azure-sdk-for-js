268c330de51f4a8f56c08d48731dc90636d205fa
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-16 16:17:46 -0500

    Migration: Apply codemod: "fixTestingImports"

--- sdk/compute/arm-compute/test/snippets.spec.ts
+++ sdk/compute/arm-compute/test/snippets.spec.ts
@@ -1,21 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { InteractiveBrowserCredential } from "@azure/identity";
 import { setLogLevel } from "@azure/logger";
 import { describe, it } from "vitest";
 
 describe("snippets", () => {
   it("ReadmeSampleCreateClient_Node", async () => {
-    const subscriptionId = "00000000-0000-0000-0000-000000000000";
   });
 
   it("ReadmeSampleCreateClient_Browser", async () => {
-    const subscriptionId = "00000000-0000-0000-0000-000000000000";
-    const credential = new InteractiveBrowserCredential({
-      tenantId: "<YOUR_TENANT_ID>",
-      clientId: "<YOUR_CLIENT_ID>",
-    });
   });
 
   it("SetLogLevel", async () => {
