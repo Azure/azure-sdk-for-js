ed902eef1d35ec637c287760a7454a0c1536f975
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-16 19:40:11 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/computeschedule/arm-computeschedule/test/public/computeschedule_operations_test.spec.ts
+++ sdk/computeschedule/arm-computeschedule/test/public/computeschedule_operations_test.spec.ts
@@ -34,9 +34,9 @@ describe("ComputeSchedule test", () => {
     );
   });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
   it("operations list test", async function () {
     const resArray = new Array();
     for await (let item of client.operations.list()) {
