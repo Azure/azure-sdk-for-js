e553675941e04d15c4b474759e6e91987ed7e9b5
Ujjwal Soni <soniujjwal@microsoft.com>
2025-01-17 12:13:35 +0530

    Added test cases for enableQueryControl flag in FeedOptions (#32499)
    ### Packages impacted by this PR
    @azure/cosmos

--- sdk/cosmosdb/cosmos/test/internal/unit/common/TestParallelQueryExecutionContext.ts
+++ sdk/cosmosdb/cosmos/test/internal/unit/common/TestParallelQueryExecutionContext.ts
@@ -6,6 +6,8 @@ import {
   ExecutionContext,
   ParallelQueryExecutionContextBase,
 } from "../../../../src/queryExecutionContext";
+import { Response } from "../../../../src/request";
+import { DiagnosticNodeInternal } from "../../../../src/diagnostics/DiagnosticNodeInternal";
 
 export class TestParallelQueryExecutionContext
   extends ParallelQueryExecutionContextBase
@@ -16,5 +18,16 @@ export class TestParallelQueryExecutionContext
     docProd2: DocumentProducer,
   ): number {
     return docProd1.generation - docProd2.generation;
+  }
+
+  private async bufferMore(diagnosticNode?: DiagnosticNodeInternal): Promise<void> {
+    // TODO: need to upadte headers from here, so make sure it returns it
+    await this.bufferDocumentProducers(diagnosticNode);
+    await this.fillBufferFromBufferQueue();
+  }
+
+  public async fetchMore(diagnosticNode?: DiagnosticNodeInternal): Promise<Response<any>> {
+    await this.bufferMore(diagnosticNode);
+    return this.drainBufferedItems();
   }
 }
