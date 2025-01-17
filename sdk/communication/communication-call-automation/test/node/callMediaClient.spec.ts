b5105274fd3a781d3f8e786ba585e6345cdf44ac
Vinothini Dharmaraj <v-vdharmaraj@microsoft.com>
2025-01-16 16:36:23 -0800

    addressing comments

--- sdk/communication/communication-call-automation/test/node/callMediaClient.spec.ts
+++ sdk/communication/communication-call-automation/test/node/callMediaClient.spec.ts
@@ -2324,9 +2324,6 @@ describe("Call Media Client Live Tests", function () {
       const holdAudioStartedEvent = await waitForEvent("HoldAudioStarted", callConnectionId, 8000);
       assert.isDefined(holdAudioStartedEvent);
 
-      await ((ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms)))(
-        3000,
-      );
       const participantHold: CallParticipant = await callConnection.getParticipant(testUser2);
       assert.isDefined(participantHold);
       assert.isTrue(participantHold.isOnHold);
@@ -2352,10 +2349,6 @@ describe("Call Media Client Live Tests", function () {
       const holdAudioResumedEvent = await waitForEvent("HoldAudioResumed", callConnectionId, 8000);
       assert.isDefined(holdAudioResumedEvent);
 
-      await ((ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms)))(
-        3000,
-      );
-
       await callConnection.getCallMedia().unhold(testUser2);
 
       const holdAudioCompletedEvent = await waitForEvent(
@@ -2365,9 +2358,6 @@ describe("Call Media Client Live Tests", function () {
       );
       assert.isDefined(holdAudioCompletedEvent);
 
-      await ((ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms)))(
-        3000,
-      );
       const participantUnhold = await callConnection.getParticipant(testUser2);
       assert.isDefined(participantUnhold);
       assert.isFalse(participantUnhold.isOnHold);
