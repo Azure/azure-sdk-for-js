5efd9b0b32fb48410d0adbffec45ee9cec3f0e85
Glenn Harper <64209257+glharper@users.noreply.github.com>
2025-01-21 14:58:41 -0500

    [AI] [Inference] merge generated code from latest TypeSpec (#32507)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/ai/ai-inference-rest/samples/v1-beta/javascript/imageEmbeddings.js
+++ sdk/ai/ai-inference-rest/samples/v1-beta/javascript/imageEmbeddings.js
@@ -2,15 +2,16 @@
 // Licensed under the MIT License.
 
 /**
- * Demonstrates how to get model info using the Inference SDK.
+ * Demonstrates how to get image embeddings from a model endpoint.
  *
- * @summary Get model info.
+ * @summary Get image embeddings.
  */
 
 const ModelClient = require("@azure-rest/ai-inference").default,
   { isUnexpected } = require("@azure-rest/ai-inference");
 const { AzureKeyCredential } = require("@azure/core-auth");
 const { DefaultAzureCredential } = require("@azure/identity");
+const fs = require("fs");
 
 // Load the .env file if it exists
 require("dotenv").config();
@@ -18,20 +19,31 @@ require("dotenv").config();
 // You will need to set these environment variables or edit the following values
 const endpoint = process.env["ENDPOINT"] || "<endpoint>";
 const key = process.env["KEY"];
+const modelName = process.env["MODEL_NAME"];
+const imageFilePath = "sample1.png";
+const imageFormat = "png"; //"jpeg", "png", etc.
 
 async function main() {
-  console.log("== Get Model Info Sample ==");
+  console.log("== Image Embeddings Sample ==");
 
   const client = createModelClient();
-  const response = await client.path("/info").get();
+
+  const image = getImageDataUrl(imageFilePath, imageFormat);
+
+  const response = await client.path("/images/embeddings").post({
+    body: {
+      input: [{ image }],
+      model: modelName,
+    },
+  });
 
   if (isUnexpected(response)) {
     throw response.body.error;
   }
-
-  console.log(
-    `Model name: ${response.body.model_name}, type: ${response.body.model_type}, provider name: ${response.body.model_provider_name}`,
-  );
+  for (const data of response.body.data) {
+    console.log(data);
+  }
+  console.log(response.body.usage);
 }
 
 /*
@@ -39,8 +51,7 @@ async function main() {
  */
 function createModelClient() {
   // auth scope for AOAI resources is currently https://cognitiveservices.azure.com/.default
-  // auth scope for MaaS and MaaP is currently https://ml.azure.com
-  // (Do not use for Serverless API or Managed Computer Endpoints)
+  // auth scope for MaaS and MaaP is currently https://ml.azure.com/.default
   if (key) {
     return ModelClient(endpoint, new AzureKeyCredential(key));
   } else {
@@ -53,6 +64,24 @@ function createModelClient() {
 
     const clientOptions = { credentials: { scopes } };
     return ModelClient(endpoint, new DefaultAzureCredential(), clientOptions);
+  }
+}
+
+/**
+ * Get the data URL of an image file.
+ * @param {string} imageFile - The path to the image file.
+ * @param {string} imageFormat - The format of the image file. For example: "jpeg", "png".
+ * @returns {string} The data URL of the image.
+ */
+function getImageDataUrl(imageFile, imageFormat) {
+  try {
+    const imageBuffer = fs.readFileSync(imageFile);
+    const imageBase64 = imageBuffer.toString("base64");
+    return `data:image/${imageFormat};base64,${imageBase64}`;
+  } catch (error) {
+    console.error(`Could not read '${imageFile}'.`);
+    console.error("Set the correct path to the image file before running this sample.");
+    process.exit(1);
   }
 }
 
