941feb2d391d82df533c96177fafd9d8d21c4f83
Manik Khandelwal <mkhandelwal@microsoft.com>
2025-01-23 13:20:41 +0530

    format

--- sdk/cosmosdb/cosmos/src/request/FeedOptions.ts
+++ sdk/cosmosdb/cosmos/src/request/FeedOptions.ts
@@ -134,5 +134,5 @@ export interface FeedOptions extends SharedOptions {
    * Default: false. If set to false, the query will retry until results are ready and `maxItemCount` is reached, which can take time for large partitions with relatively small data.
    * If set to true, scans partitions up to `maxDegreeOfParallelism`, adds results to the buffer, and returns what is available. If results are not ready, it returns an empty response.
    */
-enableQueryControl?: boolean;
+  enableQueryControl?: boolean;
 }
