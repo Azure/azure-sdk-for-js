b3537aee40b7421573ea097aebe4edb6b39f75ef
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-24 15:58:51 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/databox/arm-databox/test/databox_operations_test.spec.ts
+++ sdk/databox/arm-databox/test/databox_operations_test.spec.ts
@@ -40,18 +40,18 @@ describe("DataBox test", () => {
   let subscriptionId: string;
   let client: DataBoxManagementClient;
 
-  beforeEach(async function (ctx) {
-    recorder = new Recorder(ctx);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new DataBoxManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-  });
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new DataBoxManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("operations list test", async function () {
     const resArray = new Array();
