ffd3568c90bfbf789440cdbc113f25560e873778
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-27 14:32:15 -0500

    [datalake-analytics] Update datalake-analytics projects to use snippets extraction (#32744)
    ### Packages impacted by this PR
    
    - @azure/arm- datalake-analytics
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under ` datalake-analytics` to use snippets
    extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/datalake-analytics/arm-datalake-analytics/samples/v2-beta/typescript/src/storageAccountsDeleteSample.ts
+++ sdk/datalake-analytics/arm-datalake-analytics/samples/v2-beta/typescript/src/storageAccountsDeleteSample.ts
@@ -14,7 +14,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * @summary Updates the specified Data Lake Analytics account to remove an Azure Storage account.
  * x-ms-original-file: specification/datalake-analytics/resource-manager/Microsoft.DataLakeAnalytics/preview/2019-11-01-preview/examples/StorageAccounts_Delete.json
  */
-async function removesAnAzureStorageAccount() {
+async function removesAnAzureStorageAccount(): Promise<void> {
   const subscriptionId = "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
   const resourceGroupName = "contosorg";
   const accountName = "contosoadla";
