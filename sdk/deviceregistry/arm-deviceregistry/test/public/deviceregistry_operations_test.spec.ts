af0cee01249c6f383db08c7c3ef7355a411b9d15
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-29 17:41:12 -0500

    Migration: rushx format

--- sdk/deviceregistry/arm-deviceregistry/test/public/deviceregistry_operations_test.spec.ts
+++ sdk/deviceregistry/arm-deviceregistry/test/public/deviceregistry_operations_test.spec.ts
@@ -34,8 +34,8 @@ describe("DeviceRegistry test", () => {
   });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
   it("operations list test", async function () {
     const resArray = new Array();
     for await (let item of client.operations.list()) {
