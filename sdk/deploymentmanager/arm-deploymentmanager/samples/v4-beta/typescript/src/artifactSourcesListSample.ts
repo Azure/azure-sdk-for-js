6bbbcbf6f251700f9f41d6a670410aba8eaa389c
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-29 09:55:26 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/deploymentmanager/arm-deploymentmanager/samples/v4-beta/typescript/src/artifactSourcesListSample.ts
+++ sdk/deploymentmanager/arm-deploymentmanager/samples/v4-beta/typescript/src/artifactSourcesListSample.ts
@@ -14,7 +14,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * @summary Lists the artifact sources in a resource group.
  * x-ms-original-file: specification/deploymentmanager/resource-manager/Microsoft.DeploymentManager/preview/2019-11-01-preview/examples/artifactsources_list.json
  */
-async function listSteps() {
+async function listSteps(): Promise<void> {
   const subscriptionId = "caac1590-e859-444f-a9e0-62091c0f5929";
   const resourceGroupName = "myResourceGroup";
   const credential = new DefaultAzureCredential();
