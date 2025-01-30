2e79f9234edeeff2c6c08f08a110e87c12bc7f75
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-30 16:18:57 -0500

    [deviceregistry] Update deviceregistry projects to use snippets extraction (#32790)
    ### Packages impacted by this PR
    
    - @azure/arm-deviceregistry
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `deviceregistry` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/deviceregistry/arm-deviceregistry/samples/v1-beta/typescript/src/schemaRegistriesListBySubscriptionSample.ts
+++ sdk/deviceregistry/arm-deviceregistry/samples/v1-beta/typescript/src/schemaRegistriesListBySubscriptionSample.ts
@@ -10,7 +10,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * @summary list SchemaRegistry resources by subscription ID
  * x-ms-original-file: 2024-09-01-preview/List_SchemaRegistries_Subscription.json
  */
-async function listSchemaRegistriesSubscription() {
+async function listSchemaRegistriesSubscription(): Promise<void> {
   const credential = new DefaultAzureCredential();
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const client = new DeviceRegistryManagementClient(credential, subscriptionId);
@@ -22,7 +22,7 @@ async function listSchemaRegistriesSubscription() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   listSchemaRegistriesSubscription();
 }
 
