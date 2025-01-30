c434a5a2a612c4c0a02c7b026c8245e4b61d2910
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-30 10:46:01 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/devspaces/arm-devspaces/samples/v2/typescript/src/controllersListSample.ts
+++ sdk/devspaces/arm-devspaces/samples/v2/typescript/src/controllersListSample.ts
@@ -14,7 +14,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * @summary Lists all the Azure Dev Spaces Controllers with their properties in the subscription.
  * x-ms-original-file: specification/devspaces/resource-manager/Microsoft.DevSpaces/stable/2019-04-01/examples/ControllersList_example.json
  */
-async function controllersList() {
+async function controllersList(): Promise<void> {
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const credential = new DefaultAzureCredential();
   const client = new DevSpacesManagementClient(credential, subscriptionId);
