1d2f1ad1aa7eb6f1839caebdd4748e37354d3817
Matthew Podwysocki <matthewp@microsoft.com>
2025-01-30 11:22:54 -0500

    [devtestlabs] Update devtestlabs to use snippet extraction

--- common/tools/dev-tool/src/commands/admin/migrate-snippets.ts
+++ common/tools/dev-tool/src/commands/admin/migrate-snippets.ts
@@ -11,6 +11,7 @@ import { existsSync } from "node:fs";
 import { run } from "../../util/run";
 import { isWindows } from "../../util/platform";
 import vendored from "../run/vendored";
+import { Project, SourceFile } from "ts-morph";
 
 const log = createPrinter("migrate-source");
 
@@ -84,12 +85,47 @@ export default leafCommand(commandInfo, async ({ "package-name": packageName }) 
   await upgradePackageJson(resolve(projectFolder, "package.json"));
   await updateReadme(resolve(projectFolder, "README.md"));
   await addSnippetFiles(projectFolder);
+  await cleanupCode(projectFolder);
   if (!(await runCleanup())) {
     return false;
   }
 
   return true;
 });
+
+async function cleanupCode(projectFolder: string): Promise<void> {
+  const project = new Project({ tsConfigFilePath: resolve(projectFolder, "tsconfig.json") });
+  for (const sourceFile of project.getSourceFiles()) {
+    if (!sourceFile.getFilePath().endsWith(".spec.ts")) {
+      continue;
+    }
+
+    fixLegacyStatements(sourceFile);
+
+    // Clean up source file after applying the codemod
+    if (!sourceFile.getBaseName().includes("snippets.spec.ts")) {
+      sourceFile.fixUnusedIdentifiers();
+    }
+
+    await sourceFile.save();
+  }
+}
+
+function fixLegacyStatements(sourceFile: SourceFile): void {
+  for (const statement of sourceFile.getStatements()) {
+    const patternsToReplace = [
+      { pattern: /it\("([^"]+)", async function \(\) \{/g, replace: 'it("$1", async () => {' },
+      { pattern: /it\("([^"]+)", function \(\) \{/g, replace: 'it("$1", () => {' },
+    ];
+
+    // Replace the patterns in the source file
+    for (const { pattern, replace } of patternsToReplace) {
+      if (pattern.test(statement.getText())) {
+        statement.replaceWithText(statement.getText().replace(pattern, replace));
+      }
+    }
+  }
+}
 
 async function runCleanup(): Promise<boolean> {
   const lintCommand = await vendored(
