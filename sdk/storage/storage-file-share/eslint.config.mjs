e379284ae3b7373dca499c4aca79330d0a4ef771
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-01-31 14:37:46 -0800

    remove references of deleted rules

--- sdk/storage/storage-file-share/eslint.config.mjs
+++ sdk/storage/storage-file-share/eslint.config.mjs
@@ -4,8 +4,7 @@ export default azsdkEslint.config([
   {
     rules: {
       "@azure/azure-sdk/ts-naming-options": "warn",
-      "@azure/azure-sdk/ts-package-json-module": "warn",
-      "@typescript-eslint/no-redeclare": "warn",
+z      "@typescript-eslint/no-redeclare": "warn",
     },
   },
 ]);
