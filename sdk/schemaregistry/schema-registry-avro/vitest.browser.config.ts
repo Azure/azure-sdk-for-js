0e2b9c85cd2f9112ba4bc988569655481b03e112
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-03 15:06:27 -0500

    Migration: Update test config

--- sdk/schemaregistry/schema-registry-avro/vitest.browser.config.ts
+++ sdk/schemaregistry/schema-registry-avro/vitest.browser.config.ts
@@ -4,24 +4,14 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
-import browserMap from "@azure-tools/vite-plugin-browser-test-map";
-import inject from "@rollup/plugin-inject";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    optimizeDeps: {
-      include: ["process", "buffer"],
-    },
-    plugins: [
-      browserMap(),
-      inject({ process: "process", Buffer: ["buffer", "Buffer"] }),
-    ],
     test: {
-      fileParallelism: false,
-      include: [
-        "dist-test/browser/test/**/*.spec.js",
-      ],
+      include: ["dist-test/browser/test/**/*.spec.js",],
+      testTimeout: 1200000,
+      hookTimeout: 1200000,
     },
   }),
 );
