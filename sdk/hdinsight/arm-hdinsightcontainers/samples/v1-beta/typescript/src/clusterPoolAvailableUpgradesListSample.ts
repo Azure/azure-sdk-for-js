26a479fb96c48208b33b21ac94dca48693837390
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-02-05 15:10:47 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/hdinsight/arm-hdinsightcontainers/samples/v1-beta/typescript/src/clusterPoolAvailableUpgradesListSample.ts
+++ sdk/hdinsight/arm-hdinsightcontainers/samples/v1-beta/typescript/src/clusterPoolAvailableUpgradesListSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { HDInsightContainersManagementClient } from "@azure/arm-hdinsightcontainers";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to List a cluster pool available upgrade.
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary List a cluster pool available upgrade.
  * x-ms-original-file: specification/hdinsight/resource-manager/Microsoft.HDInsight/HDInsightOnAks/preview/2024-05-01-preview/examples/ListClusterPoolAvailableUpgrades.json
  */
-async function getClusterPoolAvailableUpgrade() {
+async function getClusterPoolAvailableUpgrade(): Promise<void> {
   const subscriptionId =
     process.env["HDINSIGHT_SUBSCRIPTION_ID"] ||
     "10e32bab-26da-4cc4-a441-52b318f824e6";
@@ -42,7 +40,7 @@ async function getClusterPoolAvailableUpgrade() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   getClusterPoolAvailableUpgrade();
 }
 
