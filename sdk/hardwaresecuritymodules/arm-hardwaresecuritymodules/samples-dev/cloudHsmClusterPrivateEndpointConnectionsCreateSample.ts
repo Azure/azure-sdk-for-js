f7371cc99f617dac1f5f53dcaf33430b8a5fd0e2
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-02-05 14:49:13 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/samples-dev/cloudHsmClusterPrivateEndpointConnectionsCreateSample.ts
+++ sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/samples-dev/cloudHsmClusterPrivateEndpointConnectionsCreateSample.ts
@@ -8,9 +8,7 @@
 import type { PrivateEndpointConnection } from "@azure/arm-hardwaresecuritymodules";
 import { AzureHSMResourceProvider } from "@azure/arm-hardwaresecuritymodules";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Creates or updates the private endpoint connection for the Cloud Hsm Cluster.
@@ -18,7 +16,7 @@ dotenv.config();
  * @summary Creates or updates the private endpoint connection for the Cloud Hsm Cluster.
  * x-ms-original-file: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2024-06-30-preview/examples/CloudHsmClusterPrivateEndpointConnection_Create_MaximumSet_Gen.json
  */
-async function cloudHsmClusterPrivateEndpointConnectionCreateMaximumSetGen() {
+async function cloudHsmClusterPrivateEndpointConnectionCreateMaximumSetGen(): Promise<void> {
   const subscriptionId =
     process.env["HARDWARESECURITYMODULES_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -42,7 +40,7 @@ async function cloudHsmClusterPrivateEndpointConnectionCreateMaximumSetGen() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await cloudHsmClusterPrivateEndpointConnectionCreateMaximumSetGen();
 }
 
--- sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/test/hardwaresecuritymodules_operations_test.spec.ts
+++ sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/test/hardwaresecuritymodules_operations_test.spec.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { AzureHSMResourceProvider } from "../src/azureHSMResourceProvider";
+import { AzureHSMResourceProvider } from "../src/azureHSMResourceProvider.js";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
@@ -45,22 +43,22 @@ describe("hardwaresecuritymodules test", () => {
   let resourceGroup: string;
   let resourcename: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new AzureHSMResourceProvider(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "ukwest";
-    resourceGroup = "myjstest";
-    resourcename = "resourcetest";
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new AzureHSMResourceProvider(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "ukwest";
+      resourceGroup = "myjstest";
+      resourcename = "resourcetest";
 
-  });
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("operation list test", async function () {
     const resArray = new Array();
