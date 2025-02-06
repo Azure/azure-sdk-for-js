d418e92185fe166cec57f1a5f532a47df3f9bfd5
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-06 16:28:01 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/hybridcontainerservice/arm-hybridcontainerservice/samples/v1/typescript/src/agentPoolDeleteSample.ts
+++ sdk/hybridcontainerservice/arm-hybridcontainerservice/samples/v1/typescript/src/agentPoolDeleteSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { HybridContainerServiceClient } from "@azure/arm-hybridcontainerservice";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Deletes the specified agent pool in the provisioned cluster
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary Deletes the specified agent pool in the provisioned cluster
  * x-ms-original-file: specification/hybridaks/resource-manager/Microsoft.HybridContainerService/stable/2024-01-01/examples/DeleteAgentPool.json
  */
-async function deleteAgentPool() {
+async function deleteAgentPool(): Promise<void> {
   const connectedClusterResourceUri =
     "subscriptions/fd3c3665-1729-4b7b-9a38-238e83b0f98b/resourceGroups/testrg/providers/Microsoft.Kubernetes/connectedClusters/test-hybridakscluster";
   const agentPoolName = "testnodepool";
@@ -33,7 +31,7 @@ async function deleteAgentPool() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   deleteAgentPool();
 }
 
