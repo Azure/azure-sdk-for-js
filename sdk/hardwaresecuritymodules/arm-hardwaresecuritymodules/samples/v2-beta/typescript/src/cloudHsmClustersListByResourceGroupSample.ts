af4daada8cd146c77483ca10699051b33159c882
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-06 10:52:43 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/samples/v2-beta/typescript/src/cloudHsmClustersListByResourceGroupSample.ts
+++ sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/samples/v2-beta/typescript/src/cloudHsmClustersListByResourceGroupSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { AzureHSMResourceProvider } from "@azure/arm-hardwaresecuritymodules";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to The List operation gets information about the Cloud HSM Clusters associated with the subscription and within the specified resource group.
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary The List operation gets information about the Cloud HSM Clusters associated with the subscription and within the specified resource group.
  * x-ms-original-file: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2024-06-30-preview/examples/CloudHsmCluster_ListByResourceGroup_MaximumSet_Gen.json
  */
-async function cloudHsmClusterListByResourceGroupMaximumSetGen() {
+async function cloudHsmClusterListByResourceGroupMaximumSetGen(): Promise<void> {
   const subscriptionId =
     process.env["HARDWARESECURITYMODULES_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -37,7 +35,7 @@ async function cloudHsmClusterListByResourceGroupMaximumSetGen() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   cloudHsmClusterListByResourceGroupMaximumSetGen();
 }
 
--- sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/src/operations/cloudHsmClusterRestoreStatus.ts
+++ sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/src/operations/cloudHsmClusterRestoreStatus.ts
@@ -6,15 +6,15 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { CloudHsmClusterRestoreStatus } from "../operationsInterfaces";
+import { CloudHsmClusterRestoreStatus } from "../operationsInterfaces/index.js";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers";
-import * as Parameters from "../models/parameters";
-import { AzureHSMResourceProvider } from "../azureHSMResourceProvider";
+import * as Mappers from "../models/mappers.js";
+import * as Parameters from "../models/parameters.js";
+import { AzureHSMResourceProvider } from "../azureHSMResourceProvider.js";
 import {
   CloudHsmClusterRestoreStatusGetOptionalParams,
   CloudHsmClusterRestoreStatusGetResponse,
-} from "../models";
+} from "../models/index.js";
 
 /** Class containing CloudHsmClusterRestoreStatus operations. */
 export class CloudHsmClusterRestoreStatusImpl
