fee6ce73d7bf73fe281e42e527206a831fcabb80
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-06 16:05:59 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/hybridcompute/arm-hybridcompute/samples/v4-beta/typescript/src/licensesGetSample.ts
+++ sdk/hybridcompute/arm-hybridcompute/samples/v4-beta/typescript/src/licensesGetSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { HybridComputeManagementClient } from "@azure/arm-hybridcompute";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Retrieves information about the view of a license.
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary Retrieves information about the view of a license.
  * x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-07-31-preview/examples/license/License_Get.json
  */
-async function getLicense() {
+async function getLicense(): Promise<void> {
   const subscriptionId =
     process.env["HYBRIDCOMPUTE_SUBSCRIPTION_ID"] || "{subscriptionId}";
   const resourceGroupName =
@@ -32,7 +30,7 @@ async function getLicense() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   getLicense();
 }
 
