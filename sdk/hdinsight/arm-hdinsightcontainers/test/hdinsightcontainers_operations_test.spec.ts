09c18902ad67f95db7b880d84caa05264734f2c8
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-06 11:29:34 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/hdinsight/arm-hdinsightcontainers/test/hdinsightcontainers_operations_test.spec.ts
+++ sdk/hdinsight/arm-hdinsightcontainers/test/hdinsightcontainers_operations_test.spec.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { HDInsightContainersManagementClient } from "../src/hDInsightContainersManagementClient";
+import { HDInsightContainersManagementClient } from "../src/hDInsightContainersManagementClient.js";
 
 
 const replaceableVariables: Record<string, string> = {
@@ -43,22 +41,22 @@ describe("HDInsightOnAks test", () => {
   let resourceGroup: string;
   let resourcename: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new HDInsightContainersManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus2euap";
-    resourceGroup = "myjstest";
-    resourcename = "resourcetest";
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new HDInsightContainersManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "eastus2euap";
+      resourceGroup = "myjstest";
+      resourcename = "resourcetest";
 
-  });
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("clusterPools create test", async function () {
     const res = await client.clusterPools.beginCreateOrUpdateAndWait(
@@ -98,8 +96,6 @@ describe("HDInsightOnAks test", () => {
 
   it("clusterPools delete test", async function () {
     const resArray = new Array();
-    const res = await client.clusterPools.beginDeleteAndWait(resourceGroup, resourcename, testPollingOptions
-    )
     for await (let item of client.clusterPools.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
