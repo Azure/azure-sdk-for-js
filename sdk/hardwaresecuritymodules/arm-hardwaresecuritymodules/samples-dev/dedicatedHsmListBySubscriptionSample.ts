752ed601b5d21c2731fe0c3485c9c60d56fe726e
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-06 13:05:10 -0500

    [hardwaresecuritymodules] Update hardwaresecuritymodules projects to use snippets extraction (#32901)
    ### Packages impacted by this PR
    
    - @azure/arm-hardwaresecuritymodules
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `hardwaresecuritymodules` to use snippets
    extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/samples-dev/dedicatedHsmListBySubscriptionSample.ts
+++ sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/samples-dev/dedicatedHsmListBySubscriptionSample.ts
@@ -7,9 +7,7 @@
  */
 import { AzureHSMResourceProvider } from "@azure/arm-hardwaresecuritymodules";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to The List operation gets information about the dedicated HSMs associated with the subscription.
@@ -17,7 +15,7 @@ dotenv.config();
  * @summary The List operation gets information about the dedicated HSMs associated with the subscription.
  * x-ms-original-file: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2024-06-30-preview/examples/DedicatedHsm_ListBySubscription.json
  */
-async function listDedicatedHsmDevicesInASubscription() {
+async function listDedicatedHsmDevicesInASubscription(): Promise<void> {
   const subscriptionId =
     process.env["HARDWARESECURITYMODULES_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -36,7 +34,7 @@ async function listDedicatedHsmDevicesInASubscription() {
  * @summary The List operation gets information about the dedicated HSMs associated with the subscription.
  * x-ms-original-file: specification/hardwaresecuritymodules/resource-manager/Microsoft.HardwareSecurityModules/preview/2024-06-30-preview/examples/PaymentHsm_ListBySubscription.json
  */
-async function listDedicatedHsmDevicesInASubscriptionIncludingPaymentHsm() {
+async function listDedicatedHsmDevicesInASubscriptionIncludingPaymentHsm(): Promise<void> {
   const subscriptionId =
     process.env["HARDWARESECURITYMODULES_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -49,7 +47,7 @@ async function listDedicatedHsmDevicesInASubscriptionIncludingPaymentHsm() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await listDedicatedHsmDevicesInASubscription();
   await listDedicatedHsmDevicesInASubscriptionIncludingPaymentHsm();
 }
