7874e3e0db030ac463518093483feeb8cb5d3622
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-06 22:28:55 -0500

    [hybridcompute] Migrate hybridcompute projects to use snippets extraction (#32912)
    ### Packages impacted by this PR
    
    - @azure/arm-hybridcompute
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `hybridcompute` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/hybridcompute/arm-hybridcompute/samples-dev/licensesUpdateSample.ts
+++ sdk/hybridcompute/arm-hybridcompute/samples-dev/licensesUpdateSample.ts
@@ -8,9 +8,7 @@
 import type { LicenseUpdate } from "@azure/arm-hybridcompute";
 import { HybridComputeManagementClient } from "@azure/arm-hybridcompute";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to The operation to update a license.
@@ -18,7 +16,7 @@ dotenv.config();
  * @summary The operation to update a license.
  * x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-07-31-preview/examples/license/License_Update.json
  */
-async function updateALicense() {
+async function updateALicense(): Promise<void> {
   const subscriptionId = process.env["HYBRIDCOMPUTE_SUBSCRIPTION_ID"] || "{subscriptionId}";
   const resourceGroupName = process.env["HYBRIDCOMPUTE_RESOURCE_GROUP"] || "myResourceGroup";
   const licenseName = "{licenseName}";
@@ -40,7 +38,7 @@ async function updateALicense() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await updateALicense();
 }
 
--- sdk/hybridcompute/arm-hybridcompute/samples/v4-beta/typescript/src/licenseProfilesCreateOrUpdateSample.ts
+++ sdk/hybridcompute/arm-hybridcompute/samples/v4-beta/typescript/src/licenseProfilesCreateOrUpdateSample.ts
@@ -13,9 +13,7 @@ import {
   HybridComputeManagementClient,
 } from "@azure/arm-hybridcompute";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to The operation to create or update a license profile.
@@ -23,7 +21,7 @@ dotenv.config();
  * @summary The operation to create or update a license profile.
  * x-ms-original-file: specification/hybridcompute/resource-manager/Microsoft.HybridCompute/preview/2024-07-31-preview/examples/licenseProfile/LicenseProfile_CreateOrUpdate.json
  */
-async function createOrUpdateALicenseProfile() {
+async function createOrUpdateALicenseProfile(): Promise<void> {
   const subscriptionId =
     process.env["HYBRIDCOMPUTE_SUBSCRIPTION_ID"] || "{subscriptionId}";
   const resourceGroupName =
@@ -47,7 +45,7 @@ async function createOrUpdateALicenseProfile() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   createOrUpdateALicenseProfile();
 }
 
