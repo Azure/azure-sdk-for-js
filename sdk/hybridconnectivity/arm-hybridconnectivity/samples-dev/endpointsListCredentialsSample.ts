aec676a192be3fae572861968e532fa31e80424e
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-06 22:28:42 -0500

    [hybridconnectivity] Migrate hybridconnectivity projects to use snippets extraction (#32913)
    ### Packages impacted by this PR
    
    - @azure/arm-hybridconnectivity
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `hybridconnectivity` to use snippets
    extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/hybridconnectivity/arm-hybridconnectivity/samples-dev/endpointsListCredentialsSample.ts
+++ sdk/hybridconnectivity/arm-hybridconnectivity/samples-dev/endpointsListCredentialsSample.ts
@@ -11,9 +11,7 @@ import type {
 } from "@azure/arm-hybridconnectivity";
 import { HybridConnectivityManagementAPI } from "@azure/arm-hybridconnectivity";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Gets the endpoint access credentials to the resource.
@@ -21,7 +19,7 @@ dotenv.config();
  * @summary Gets the endpoint access credentials to the resource.
  * x-ms-original-file: specification/hybridconnectivity/resource-manager/Microsoft.HybridConnectivity/stable/2023-03-15/examples/EndpointsPostListCredentials.json
  */
-async function hybridConnectivityEndpointsPostListCredentials() {
+async function hybridConnectivityEndpointsPostListCredentials(): Promise<void> {
   const resourceUri =
     "subscriptions/f5bcc1d9-23af-4ae9-aca1-041d0f593a63/resourceGroups/hybridRG/providers/Microsoft.HybridCompute/machines/testMachine";
   const endpointName = "default";
@@ -37,7 +35,7 @@ async function hybridConnectivityEndpointsPostListCredentials() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await hybridConnectivityEndpointsPostListCredentials();
 }
 
