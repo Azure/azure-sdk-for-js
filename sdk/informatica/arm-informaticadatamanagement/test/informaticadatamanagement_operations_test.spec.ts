49e13b9fb99e942fb5fa23836645293d3a0c6afe
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-07 16:22:35 -0500

    [informatica] Migrate informatica projects to use snippets extraction

--- sdk/informatica/arm-informaticadatamanagement/test/informaticadatamanagement_operations_test.spec.ts
+++ sdk/informatica/arm-informaticadatamanagement/test/informaticadatamanagement_operations_test.spec.ts
@@ -6,18 +6,14 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import {
-  env,
-  Recorder,
-  RecorderStartOptions,
-  isPlaybackMode,
-} from "@azure-tools/test-recorder";
+import type { RecorderStartOptions } from "@azure-tools/test-recorder";
+import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { InformaticaDataManagement } from "../src/informaticaDataManagement.js";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
-  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
+  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888",
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -36,32 +32,29 @@ describe("InformaticaDataManagement test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: InformaticaDataManagement;
-  let location: string;
-  let resourceGroup: string;
-  let resourcename: string;
 
   beforeEach(async (ctx) => {
-      recorder = new Recorder(ctx);
-      await recorder.start(recorderOptions);
-      subscriptionId = env.SUBSCRIPTION_ID || '';
-      // This is an example of how the environment variables are used
-      const credential = createTestCredential();
-      client = new InformaticaDataManagement(credential, subscriptionId, recorder.configureClientOptions({}));
-      location = "eastus2euap";
-      resourceGroup = "myjstest";
-      resourcename = "resourcetest";
-
-    });
+    recorder = new Recorder(ctx);
+    await recorder.start(recorderOptions);
+    subscriptionId = env.SUBSCRIPTION_ID || "";
+    // This is an example of how the environment variables are used
+    const credential = createTestCredential();
+    client = new InformaticaDataManagement(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+  });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
 
-  it("operation list test", async function () {
+  it("operation list test", async () => {
     const resArray = new Array();
-    for await (let item of client.operations.list()) {
+    for await (const item of client.operations.list()) {
       resArray.push(item);
     }
     assert.notEqual(resArray.length, 0);
   });
-})
+});
