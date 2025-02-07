806c14dcb6d8d5700876c2f6b5b8cac0ee4e6588
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-02-07 16:27:28 +0800

    update

--- sdk/eventhub/arm-eventhub/samples/v5-beta/javascript/disasterRecoveryConfigsCreateOrUpdateSample.js
+++ sdk/eventhub/arm-eventhub/samples/v5-beta/javascript/disasterRecoveryConfigsCreateOrUpdateSample.js
@@ -6,8 +6,6 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
 const { EventHubManagementClient } = require("@azure/arm-eventhub");
 const { DefaultAzureCredential } = require("@azure/identity");
 require("dotenv/config");
