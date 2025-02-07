c3db9a6aa5bda8fb066efebb99feb83ffecfe744
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-07 16:07:03 -0500

    Migration: Update package.json, tsconfig.json, and api-extractor.json

--- sdk/imagebuilder/arm-imagebuilder/test/imagebuilder.spec.ts
+++ sdk/imagebuilder/arm-imagebuilder/test/imagebuilder.spec.ts
@@ -6,12 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import {
-  env,
-  Recorder,
-  isPlaybackMode,
-  RecorderStartOptions,
-} from "@azure-tools/test-recorder";
+import type { RecorderStartOptions } from "@azure-tools/test-recorder";
+import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { ImageBuilderClient } from "../src/imageBuilderClient.js";
 import { ComputeManagementClient } from "@azure/arm-compute";
@@ -22,7 +18,7 @@ const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
   AZURE_CLIENT_SECRET: "azure_client_secret",
   AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "azure_subscription_id",
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -52,49 +48,60 @@ describe("ImageBuilder test", () => {
   let msiName: string;
 
   beforeEach(async (ctx) => {
-      recorder = new Recorder(ctx);
-      await recorder.start(recorderOptions);
-      subscriptionId = env.SUBSCRIPTION_ID || "";
-      // This is an example of how the environment variables are used
-      const credential = createTestCredential();
-      client = new ImageBuilderClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      compute_client = new ComputeManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      msi_client = new ManagedServiceIdentityClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      location = "eastus";
-      resourceGroup = "myjstest";
-      imageTemplateName = "myimageTemplatexxxz";
-      diskName = "mydiskaaa";
-      snapshotName = "mysnapshotaaa";
-      imagesName = "myimagesaaa";
-      msiName = "mymsiaaa";
-    });
+    recorder = new Recorder(ctx);
+    await recorder.start(recorderOptions);
+    subscriptionId = env.SUBSCRIPTION_ID || "";
+    // This is an example of how the environment variables are used
+    const credential = createTestCredential();
+    client = new ImageBuilderClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    compute_client = new ComputeManagementClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    msi_client = new ManagedServiceIdentityClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    location = "eastus";
+    resourceGroup = "myjstest";
+    imageTemplateName = "myimageTemplatexxxz";
+    diskName = "mydiskaaa";
+    snapshotName = "mysnapshotaaa";
+    imagesName = "myimagesaaa";
+    msiName = "mymsiaaa";
+  });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
 
-  it.only("operations list test", async function () {
+  it("operations list test", async function () {
     const resArray = new Array();
-    for await (let item of client.operations.list()) {
+    for await (const item of client.operations.list()) {
       resArray.push(item);
     }
     assert.notEqual(resArray.length, 0);
   });
 
   it("create parameter for virtualMachineImageTemplates test", async function () {
-    //create a userAssignedIdentities
-    //create a disk
-    //create a snapshots
-    //create a images
+    // create a userAssignedIdentities
+    // create a disk
+    // create a snapshots
+    // create a images
   });
 
   it("virtualMachineImageTemplates create test", async function () {
-
     if (isPlaybackMode()) {
       ctx.skip();
     }
-    //before create ,we need add msi owner authority for images in portal
-  }).timeout(3600000);;
+    // before create ,we need add msi owner authority for images in portal
+  }).timeout(3600000);
 
   it("virtualMachineImageTemplates get test", async function () {
     const res = await client.virtualMachineImageTemplates.get(resourceGroup, imageTemplateName);
@@ -103,7 +110,7 @@ describe("ImageBuilder test", () => {
 
   it("virtualMachineImageTemplates list test", async function () {
     const resArray = new Array();
-    for await (let item of client.virtualMachineImageTemplates.list()) {
+    for await (const item of client.virtualMachineImageTemplates.list()) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 1);
@@ -111,12 +118,11 @@ describe("ImageBuilder test", () => {
 
   it("virtualMachineImageTemplates delete test", async function () {
     const resArray = new Array();
-    for await (let item of client.virtualMachineImageTemplates.list()) {
+    for await (const item of client.virtualMachineImageTemplates.list()) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 0);
   });
 
-  it("delete parameter for virtualMachineImageTemplates test", async function () {
-  });
+  it("delete parameter for virtualMachineImageTemplates test", async function () {});
 });
