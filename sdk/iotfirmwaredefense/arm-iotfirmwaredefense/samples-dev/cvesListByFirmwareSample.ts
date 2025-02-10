bdabf6c3595d11b66841c133b4bf13543a5f7424
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-10 10:21:57 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/iotfirmwaredefense/arm-iotfirmwaredefense/samples-dev/cvesListByFirmwareSample.ts
+++ sdk/iotfirmwaredefense/arm-iotfirmwaredefense/samples-dev/cvesListByFirmwareSample.ts
@@ -7,9 +7,7 @@
  */
 import { IoTFirmwareDefenseClient } from "@azure/arm-iotfirmwaredefense";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Lists CVE analysis results of a firmware.
@@ -17,7 +15,7 @@ dotenv.config();
  * @summary Lists CVE analysis results of a firmware.
  * x-ms-original-file: specification/fist/resource-manager/Microsoft.IoTFirmwareDefense/stable/2024-01-10/examples/Cves_ListByFirmware_MaximumSet_Gen.json
  */
-async function cvesListByFirmwareMaximumSetGen() {
+async function cvesListByFirmwareMaximumSetGen(): Promise<void> {
   const subscriptionId =
     process.env["IOTFIRMWAREDEFENSE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
   const resourceGroupName =
@@ -43,7 +41,7 @@ async function cvesListByFirmwareMaximumSetGen() {
  * @summary Lists CVE analysis results of a firmware.
  * x-ms-original-file: specification/fist/resource-manager/Microsoft.IoTFirmwareDefense/stable/2024-01-10/examples/Cves_ListByFirmware_MinimumSet_Gen.json
  */
-async function cvesListByFirmwareMinimumSetGen() {
+async function cvesListByFirmwareMinimumSetGen(): Promise<void> {
   const subscriptionId =
     process.env["IOTFIRMWAREDEFENSE_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
   const resourceGroupName =
@@ -63,7 +61,7 @@ async function cvesListByFirmwareMinimumSetGen() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await cvesListByFirmwareMaximumSetGen();
   await cvesListByFirmwareMinimumSetGen();
 }
--- sdk/iotfirmwaredefense/arm-iotfirmwaredefense/test/iotfirmwaredefense_operations_test.spec.ts
+++ sdk/iotfirmwaredefense/arm-iotfirmwaredefense/test/iotfirmwaredefense_operations_test.spec.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { IoTFirmwareDefenseClient } from "../src/ioTFirmwareDefenseClient";
+import { IoTFirmwareDefenseClient } from "../src/ioTFirmwareDefenseClient.js";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
@@ -45,22 +43,22 @@ describe("iotfirmwaredefense test", () => {
   let resourceGroup: string;
   let resourcename: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new IoTFirmwareDefenseClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus";
-    resourceGroup = "myjstest";
-    resourcename = "resourcetest";
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new IoTFirmwareDefenseClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "eastus";
+      resourceGroup = "myjstest";
+      resourcename = "resourcetest";
 
-  });
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("workspaces create test", async function () {
     const res = await client.workspaces.create(
@@ -88,8 +86,6 @@ describe("iotfirmwaredefense test", () => {
 
   it("workspaces delete test", async function () {
     const resArray = new Array();
-    const res = await client.workspaces.delete(resourceGroup, resourcename
-    )
     for await (let item of client.workspaces.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
