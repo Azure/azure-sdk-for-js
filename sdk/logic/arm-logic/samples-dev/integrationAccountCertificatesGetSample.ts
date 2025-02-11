d8c9051669610ff9cf465555064c94da16c766d9
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 14:56:27 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/logic/arm-logic/samples-dev/integrationAccountCertificatesGetSample.ts
+++ sdk/logic/arm-logic/samples-dev/integrationAccountCertificatesGetSample.ts
@@ -7,9 +7,7 @@
  */
 import { LogicManagementClient } from "@azure/arm-logic";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Gets an integration account certificate.
@@ -17,7 +15,7 @@ dotenv.config();
  * @summary Gets an integration account certificate.
  * x-ms-original-file: specification/logic/resource-manager/Microsoft.Logic/stable/2019-05-01/examples/IntegrationAccountCertificates_Get.json
  */
-async function getCertificateByName() {
+async function getCertificateByName(): Promise<void> {
   const subscriptionId =
     process.env["LOGIC_SUBSCRIPTION_ID"] || "34adfa4f-cedf-4dc0-ba29-b6d1a69ab345";
   const resourceGroupName = process.env["LOGIC_RESOURCE_GROUP"] || "testResourceGroup";
@@ -33,7 +31,7 @@ async function getCertificateByName() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await getCertificateByName();
 }
 
--- sdk/logic/arm-logic/test/logic_examples.ts
+++ sdk/logic/arm-logic/test/logic_examples.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { LogicManagementClient } from "../src/logicManagementClient";
+import { LogicManagementClient } from "../src/logicManagementClient.js";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
@@ -45,21 +43,21 @@ describe("Logic test", () => {
   let resourceGroupName: string;
   let workflowName: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || "";
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new LogicManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus";
-    resourceGroupName = "myjstest";
-    workflowName = "myworkflowxxx";
-  });
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || "";
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new LogicManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "eastus";
+      resourceGroupName = "myjstest";
+      workflowName = "myworkflowxxx";
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("workflows create test", async function () {
     const res = await client.workflows.createOrUpdate(resourceGroupName, workflowName, {
@@ -90,7 +88,6 @@ describe("Logic test", () => {
   });
 
   it("workflows delete test", async function () {
-    const res = await client.workflows.delete(resourceGroupName, workflowName);
     const resArray = new Array();
     for await (let item of client.workflows.listByResourceGroup(resourceGroupName)) {
       resArray.push(item);
