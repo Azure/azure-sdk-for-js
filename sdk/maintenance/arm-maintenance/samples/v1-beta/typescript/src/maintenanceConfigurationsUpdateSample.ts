84490bf067ad64c25dfac737a7465c051f65baa3
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 18:50:25 -0500

    [maintenance] Migrate maintenance projects to use snippets extraction (#32990)
    ### Packages impacted by this PR
    
    - @azure/arm-maintenance
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `maintenance` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/maintenance/arm-maintenance/samples/v1-beta/typescript/src/maintenanceConfigurationsUpdateSample.ts
+++ sdk/maintenance/arm-maintenance/samples/v1-beta/typescript/src/maintenanceConfigurationsUpdateSample.ts
@@ -13,9 +13,7 @@ import {
   MaintenanceManagementClient,
 } from "@azure/arm-maintenance";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Patch configuration record
@@ -23,7 +21,7 @@ dotenv.config();
  * @summary Patch configuration record
  * x-ms-original-file: specification/maintenance/resource-manager/Microsoft.Maintenance/preview/2023-10-01-preview/examples/MaintenanceConfigurations_UpdateForResource.json
  */
-async function maintenanceConfigurationsUpdateForResource() {
+async function maintenanceConfigurationsUpdateForResource(): Promise<void> {
   const subscriptionId =
     process.env["MAINTENANCE_SUBSCRIPTION_ID"] ||
     "5b4b650e-28b9-4790-b3ab-ddbd88d727c4";
@@ -51,7 +49,7 @@ async function maintenanceConfigurationsUpdateForResource() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   maintenanceConfigurationsUpdateForResource();
 }
 
