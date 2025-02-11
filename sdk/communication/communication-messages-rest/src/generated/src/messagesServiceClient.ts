579fd4d4906f2ec2eb04d31d3d1b9ec34729bb94
arifibrahim4 <arifibrahim4@gmail.com>
2025-02-10 20:44:53 -0500

    update latest commit type spec in tsp-location.yaml

--- sdk/communication/communication-messages-rest/src/generated/src/messagesServiceClient.ts
+++ sdk/communication/communication-messages-rest/src/generated/src/messagesServiceClient.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { getClient, ClientOptions } from "@azure-rest/core-client";
+import type { ClientOptions } from "@azure-rest/core-client";
+import { getClient } from "@azure-rest/core-client";
 import { logger } from "./logger.js";
-import { TokenCredential, KeyCredential } from "@azure/core-auth";
-import { MessagesServiceClient } from "./clientDefinitions.js";
+import type { TokenCredential, KeyCredential } from "@azure/core-auth";
+import type { MessagesServiceClient } from "./clientDefinitions.js";
 
 /** The optional parameters for the client */
 export interface MessagesServiceClientOptions extends ClientOptions {
