589fff5724068b32bc9feeb7e91c70f2b9304e51
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 14:26:39 -0500

    [kusto] Migrate kusto projects to use snippets extraction (#32965)
    ### Packages impacted by this PR
    
    - @azure/arm-kusto
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `kusto` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/kusto/arm-kusto/samples-dev/clustersCheckNameAvailabilitySample.ts
+++ sdk/kusto/arm-kusto/samples-dev/clustersCheckNameAvailabilitySample.ts
@@ -8,9 +8,7 @@
 import type { ClusterCheckNameRequest } from "@azure/arm-kusto";
 import { KustoManagementClient } from "@azure/arm-kusto";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Checks that the cluster name is valid and is not already in use.
@@ -18,7 +16,7 @@ dotenv.config();
  * @summary Checks that the cluster name is valid and is not already in use.
  * x-ms-original-file: specification/azure-kusto/resource-manager/Microsoft.Kusto/stable/2023-08-15/examples/KustoClustersCheckNameAvailability.json
  */
-async function kustoClustersCheckNameAvailability() {
+async function kustoClustersCheckNameAvailability(): Promise<void> {
   const subscriptionId =
     process.env["KUSTO_SUBSCRIPTION_ID"] || "12345678-1234-1234-1234-123456789098";
   const location = "westus";
@@ -32,7 +30,7 @@ async function kustoClustersCheckNameAvailability() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await kustoClustersCheckNameAvailability();
 }
 
