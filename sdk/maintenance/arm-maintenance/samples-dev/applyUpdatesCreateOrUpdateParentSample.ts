64129255771c0c6864b007d2bcff50b0c0bf3823
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 18:19:33 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/maintenance/arm-maintenance/samples-dev/applyUpdatesCreateOrUpdateParentSample.ts
+++ sdk/maintenance/arm-maintenance/samples-dev/applyUpdatesCreateOrUpdateParentSample.ts
@@ -7,9 +7,7 @@
  */
 import { MaintenanceManagementClient } from "@azure/arm-maintenance";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Apply maintenance updates to resource with parent
@@ -17,7 +15,7 @@ dotenv.config();
  * @summary Apply maintenance updates to resource with parent
  * x-ms-original-file: specification/maintenance/resource-manager/Microsoft.Maintenance/preview/2023-10-01-preview/examples/ApplyUpdates_CreateOrUpdateParent.json
  */
-async function applyUpdatesCreateOrUpdateParent() {
+async function applyUpdatesCreateOrUpdateParent(): Promise<void> {
   const subscriptionId =
     process.env["MAINTENANCE_SUBSCRIPTION_ID"] || "5b4b650e-28b9-4790-b3ab-ddbd88d727c4";
   const resourceGroupName = process.env["MAINTENANCE_RESOURCE_GROUP"] || "examplerg";
@@ -39,7 +37,7 @@ async function applyUpdatesCreateOrUpdateParent() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await applyUpdatesCreateOrUpdateParent();
 }
 
