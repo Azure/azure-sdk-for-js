d5e5ad55bbf9fe4d34b3bf5d933a92f9dc209341
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 14:35:20 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/devhub/arm-devhub/test/devhub_examples.spec.ts
+++ sdk/devhub/arm-devhub/test/devhub_examples.spec.ts
@@ -41,19 +41,19 @@ describe("devhub test", () => {
   let client: DeveloperHubServiceClient;
   let resourceGroup: string;
 
-  beforeEach(async function (ctx) {
-    recorder = new Recorder(ctx);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new DeveloperHubServiceClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    resourceGroup = "myjstest";
-  });
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new DeveloperHubServiceClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      resourceGroup = "myjstest";
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("workflow list test", async function () {
     const resArray = new Array();
