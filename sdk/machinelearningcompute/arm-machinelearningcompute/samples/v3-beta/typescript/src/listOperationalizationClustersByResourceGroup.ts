78b94548e25bfaf89e61dbe27e48c920790cb4c0
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 16:48:28 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/machinelearningcompute/arm-machinelearningcompute/samples/v3-beta/typescript/src/listOperationalizationClustersByResourceGroup.ts
+++ sdk/machinelearningcompute/arm-machinelearningcompute/samples/v3-beta/typescript/src/listOperationalizationClustersByResourceGroup.ts
@@ -17,7 +17,7 @@
 import { MachineLearningComputeManagementClient } from "@azure/arm-machinelearningcompute";
 import { DefaultAzureCredential } from "@azure/identity";
 
-async function listOperationalizationClustersByResourceGroup() {
+async function listOperationalizationClustersByResourceGroup(): Promise<void> {
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const resourceGroupName = "myResourceGroup";
   const credential = new DefaultAzureCredential();
