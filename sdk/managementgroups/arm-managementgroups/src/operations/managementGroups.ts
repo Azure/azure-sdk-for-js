a8f8e84c387695bffae525f6246bdbe296ff026f
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 18:46:34 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/managementgroups/arm-managementgroups/src/operations/managementGroups.ts
+++ sdk/managementgroups/arm-managementgroups/src/operations/managementGroups.ts
@@ -7,14 +7,14 @@
  */
 
 import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
-import { setContinuationToken } from "../pagingHelper";
-import { ManagementGroups } from "../operationsInterfaces";
+import { setContinuationToken } from "../pagingHelper.js";
+import { ManagementGroups } from "../operationsInterfaces/index.js";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers";
-import * as Parameters from "../models/parameters";
-import { ManagementGroupsAPI } from "../managementGroupsAPI";
+import * as Mappers from "../models/mappers.js";
+import * as Parameters from "../models/parameters.js";
+import { ManagementGroupsAPI } from "../managementGroupsAPI.js";
 import { PollerLike, PollOperationState, LroEngine } from "@azure/core-lro";
-import { LroImpl } from "../lroImpl";
+import { LroImpl } from "../lroImpl.js";
 import {
   ManagementGroupInfo,
   ManagementGroupsListNextOptionalParams,
@@ -36,7 +36,7 @@ import {
   ManagementGroupsDeleteResponse,
   ManagementGroupsListNextResponse,
   ManagementGroupsGetDescendantsNextResponse
-} from "../models";
+} from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Class containing ManagementGroups operations. */
