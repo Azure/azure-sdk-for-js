b36a6d7ab3982fe0b2187305929fc2f2af225ca5
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 14:59:46 -0500

    Migration: Apply codemod: "fixTestingImports"

--- sdk/logic/arm-logic/test/logic.spec.ts
+++ sdk/logic/arm-logic/test/logic.spec.ts
@@ -10,6 +10,7 @@ import type { RecorderStartOptions } from "@azure-tools/test-recorder";
 import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { LogicManagementClient } from "../src/logicManagementClient.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
