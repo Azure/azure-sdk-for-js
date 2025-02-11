e51e40357ebcd22993f810b881e9e75fb85fde65
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-10 20:00:25 -0500

    [iothub] Migrate iothub projects to use snippets extraction (#32944)
    ### Packages impacted by this PR
    
    - @azure/arm-iothub
    - @azure/arm-iothub-profile-2020-09-01-hybrid
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `iothub` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/iothub/arm-iothub/src/operations/privateLinkResourcesOperations.ts
+++ sdk/iothub/arm-iothub/src/operations/privateLinkResourcesOperations.ts
@@ -6,17 +6,17 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { PrivateLinkResourcesOperations } from "../operationsInterfaces";
+import { PrivateLinkResourcesOperations } from "../operationsInterfaces/index.js";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers";
-import * as Parameters from "../models/parameters";
-import { IotHubClient } from "../iotHubClient";
+import * as Mappers from "../models/mappers.js";
+import * as Parameters from "../models/parameters.js";
+import { IotHubClient } from "../iotHubClient.js";
 import {
   PrivateLinkResourcesListOptionalParams,
   PrivateLinkResourcesListResponse,
   PrivateLinkResourcesGetOptionalParams,
   PrivateLinkResourcesGetResponse
-} from "../models";
+} from "../models/index.js";
 
 /** Class containing PrivateLinkResourcesOperations operations. */
 export class PrivateLinkResourcesOperationsImpl
