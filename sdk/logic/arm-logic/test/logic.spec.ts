a9ab3b11bf2d678fedd05e855810a0bff1325774
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-11 14:59:37 -0500

    Migration: Update package.json, tsconfig.json, and api-extractor.json

--- sdk/logic/arm-logic/test/logic.spec.ts
+++ sdk/logic/arm-logic/test/logic.spec.ts
@@ -6,21 +6,16 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import {
-  env,
-  Recorder,
-  RecorderStartOptions,
-  isPlaybackMode,
-} from "@azure-tools/test-recorder";
+import type { RecorderStartOptions } from "@azure-tools/test-recorder";
+import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { LogicManagementClient } from "../src/logicManagementClient.js";
-import { assert } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
   AZURE_CLIENT_SECRET: "azure_client_secret",
   AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "azure_subscription_id",
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -44,32 +39,37 @@ describe("Logic test", () => {
   let workflowName: string;
 
   beforeEach(async (ctx) => {
-      recorder = new Recorder(ctx);
-      await recorder.start(recorderOptions);
-      subscriptionId = env.SUBSCRIPTION_ID || "";
-      // This is an example of how the environment variables are used
-      const credential = createTestCredential();
-      client = new LogicManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      location = "eastus";
-      resourceGroupName = "myjstest";
-      workflowName = "myworkflowxxx";
-    });
+    recorder = new Recorder(ctx);
+    await recorder.start(recorderOptions);
+    subscriptionId = env.SUBSCRIPTION_ID || "";
+    // This is an example of how the environment variables are used
+    const credential = createTestCredential();
+    client = new LogicManagementClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    location = "eastus";
+    resourceGroupName = "myjstest";
+    workflowName = "myworkflowxxx";
+  });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
 
   it("workflows create test", async function () {
     const res = await client.workflows.createOrUpdate(resourceGroupName, workflowName, {
       location: location,
       definition: {
-        $schema: "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
+        $schema:
+          "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
         contentVersion: "1.0.0.0",
         parameters: {},
         triggers: {},
         actions: {},
-        outputs: {}
-      }
+        outputs: {},
+      },
     });
     assert.equal(res.name, workflowName);
   });
@@ -81,7 +81,7 @@ describe("Logic test", () => {
 
   it("workflows listByResourceGroup test", async function () {
     const resArray = new Array();
-    for await (let item of client.workflows.listByResourceGroup(resourceGroupName)) {
+    for await (const item of client.workflows.listByResourceGroup(resourceGroupName)) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 1);
@@ -89,7 +89,7 @@ describe("Logic test", () => {
 
   it("workflows delete test", async function () {
     const resArray = new Array();
-    for await (let item of client.workflows.listByResourceGroup(resourceGroupName)) {
+    for await (const item of client.workflows.listByResourceGroup(resourceGroupName)) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 0);
