e9bbfa9d9beddbef3a71dc9870d388070c7f603e
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-12 14:22:08 -0500

    Migration: Apply codemod: "fixTestingImports"

--- sdk/mediaservices/arm-mediaservices/test/mediaservice_examples.ts
+++ sdk/mediaservices/arm-mediaservices/test/mediaservice_examples.ts
@@ -13,9 +13,9 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { AzureMediaServices } from "../src/azureMediaServices.js";
 import { StorageManagementClient } from "@azure/arm-storage";
+import { assert } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id"
