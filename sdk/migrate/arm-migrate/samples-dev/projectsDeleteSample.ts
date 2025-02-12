4b73988a3dc6539d0a228f5f80231e4cf0138ddc
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-12 17:05:31 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/migrate/arm-migrate/samples-dev/projectsDeleteSample.ts
+++ sdk/migrate/arm-migrate/samples-dev/projectsDeleteSample.ts
@@ -7,9 +7,7 @@
  */
 import { AzureMigrateV2 } from "@azure/arm-migrate";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Delete the project. Deleting non-existent project is a no-operation.
@@ -17,7 +15,7 @@ dotenv.config();
  * @summary Delete the project. Deleting non-existent project is a no-operation.
  * x-ms-original-file: specification/migrate/resource-manager/Microsoft.Migrate/stable/2019-10-01/examples/Projects_Delete.json
  */
-async function projectsDelete() {
+async function projectsDelete(): Promise<void> {
   const subscriptionId =
     process.env["MIGRATE_SUBSCRIPTION_ID"] || "6393a73f-8d55-47ef-b6dd-179b3e0c7910";
   const resourceGroupName = process.env["MIGRATE_RESOURCE_GROUP"] || "abgoyal-westEurope";
@@ -28,7 +26,7 @@ async function projectsDelete() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await projectsDelete();
 }
 
