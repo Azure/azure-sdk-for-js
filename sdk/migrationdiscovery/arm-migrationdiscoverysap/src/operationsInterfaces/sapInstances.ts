ae76359de800999e2cbaf88521b712139da853d8
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-12 17:11:47 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/migrationdiscovery/arm-migrationdiscoverysap/src/operationsInterfaces/sapInstances.ts
+++ sdk/migrationdiscovery/arm-migrationdiscoverysap/src/operationsInterfaces/sapInstances.ts
@@ -19,7 +19,7 @@ import {
   SapInstancesUpdateOptionalParams,
   SapInstancesUpdateResponse,
   SapInstancesDeleteOptionalParams,
-} from "../models";
+} from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Interface representing a SapInstances. */
