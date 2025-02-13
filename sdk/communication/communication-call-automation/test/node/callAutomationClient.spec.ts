cd3e12d3f9d24a376e420ff0eea54926a9770345
Vinothini Dharmaraj <v-vdharmaraj@microsoft.com>
2025-02-12 18:09:11 -0800

    fixing analyze issue

--- sdk/communication/communication-call-automation/test/node/callAutomationClient.spec.ts
+++ sdk/communication/communication-call-automation/test/node/callAutomationClient.spec.ts
@@ -209,7 +209,11 @@ describe("Call Automation Client Unit Tests", () => {
     const result = await promiseResult;
 
     assert.isNotNull(result);
-    expect(client.answerCall).toHaveBeenCalledWith(CALL_INCOMING_CALL_CONTEXT, CALL_CALLBACK_URL, answerCallOptions);
+    expect(client.answerCall).toHaveBeenCalledWith(
+      CALL_INCOMING_CALL_CONTEXT,
+      CALL_CALLBACK_URL,
+      answerCallOptions,
+    );
     assert.equal(result, answerCallResultMock);
   });
 
