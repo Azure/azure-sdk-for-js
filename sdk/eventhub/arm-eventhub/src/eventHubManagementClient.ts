287b2c97802201e5387bde242b60483c6985ac48
azure-sdk <azuresdk@microsoft.com>
2025-02-13 06:50:35 +0000

    Post release automated changes for azure-arm-eventhub

--- sdk/eventhub/arm-eventhub/src/eventHubManagementClient.ts
+++ sdk/eventhub/arm-eventhub/src/eventHubManagementClient.ts
@@ -79,7 +79,7 @@ export class EventHubManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-eventhub/5.3.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-eventhub/5.3.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
