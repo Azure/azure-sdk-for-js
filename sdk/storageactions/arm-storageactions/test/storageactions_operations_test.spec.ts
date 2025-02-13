b9af7cdc8c93a84645098393394129a5c8a7e788
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-13 16:41:52 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/storageactions/arm-storageactions/test/storageactions_operations_test.spec.ts
+++ sdk/storageactions/arm-storageactions/test/storageactions_operations_test.spec.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { StorageActionsManagementClient } from "../src/storageActionsManagementClient";
+import { StorageActionsManagementClient } from "../src/storageActionsManagementClient.js";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
@@ -45,22 +43,22 @@ describe("StorageActions test", () => {
   let resourceGroup: string;
   let resourcename: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new StorageActionsManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus2euap";
-    resourceGroup = "myjstest";
-    resourcename = "resourcetest";
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new StorageActionsManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "eastus2euap";
+      resourceGroup = "myjstest";
+      resourcename = "resourcetest";
 
-  });
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("storageTasks create test", async function () {
     const res = await client.storageTasks.beginCreateAndWait(
@@ -115,8 +113,6 @@ describe("StorageActions test", () => {
 
   it("storageTasks delete test", async function () {
     const resArray = new Array();
-    const res = await client.storageTasks.beginDeleteAndWait(resourceGroup, resourcename
-    )
     for await (let item of client.storageTasks.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
