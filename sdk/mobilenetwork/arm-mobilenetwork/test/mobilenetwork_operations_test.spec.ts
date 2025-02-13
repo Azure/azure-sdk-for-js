ec98d14821f11a023cecbca06572e9f2788cf8e8
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-13 12:10:34 -0500

    [mobilenetwork] Migrate mobilenetwork projects to use snippets extraction

--- sdk/mobilenetwork/arm-mobilenetwork/test/mobilenetwork_operations_test.spec.ts
+++ sdk/mobilenetwork/arm-mobilenetwork/test/mobilenetwork_operations_test.spec.ts
@@ -6,18 +6,14 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import {
-  env,
-  Recorder,
-  RecorderStartOptions,
-  isPlaybackMode,
-} from "@azure-tools/test-recorder";
+import type { RecorderStartOptions } from "@azure-tools/test-recorder";
+import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { MobileNetworkManagementClient } from "../src/mobileNetworkManagementClient.js";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
-  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
+  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888",
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -41,61 +37,64 @@ describe("MobileNetwork test", () => {
   let resourcename: string;
 
   beforeEach(async (ctx) => {
-      recorder = new Recorder(ctx);
-      await recorder.start(recorderOptions);
-      subscriptionId = env.SUBSCRIPTION_ID || '';
-      // This is an example of how the environment variables are used
-      const credential = createTestCredential();
-      client = new MobileNetworkManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      location = "eastus";
-      resourceGroup = "myjstest";
-      resourcename = "resourcetest";
-
-    });
+    recorder = new Recorder(ctx);
+    await recorder.start(recorderOptions);
+    subscriptionId = env.SUBSCRIPTION_ID || "";
+    // This is an example of how the environment variables are used
+    const credential = createTestCredential();
+    client = new MobileNetworkManagementClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    location = "eastus";
+    resourceGroup = "myjstest";
+    resourcename = "resourcetest";
+  });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
 
-  it("mobileNetworks create test", async function () {
+  it("mobileNetworks create test", async () => {
     const res = await client.mobileNetworks.beginCreateOrUpdateAndWait(
       resourceGroup,
       resourcename,
       {
         location,
-        publicLandMobileNetworkIdentifier: { mcc: "001", mnc: "01" }
+        publicLandMobileNetworkIdentifier: { mcc: "001", mnc: "01" },
       },
-      testPollingOptions);
+      testPollingOptions,
+    );
     assert.equal(res.name, resourcename);
   });
 
-  it("mobileNetworks get test", async function () {
-    const res = await client.mobileNetworks.get(resourceGroup,
-      resourcename);
+  it("mobileNetworks get test", async () => {
+    const res = await client.mobileNetworks.get(resourceGroup, resourcename);
     assert.equal(res.name, resourcename);
   });
 
-  it("mobileNetworks list test", async function () {
+  it("mobileNetworks list test", async () => {
     const resArray = new Array();
-    for await (let item of client.mobileNetworks.listByResourceGroup(resourceGroup)) {
+    for await (const item of client.mobileNetworks.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 1);
   });
 
-  it("mobileNetworks delete test", async function () {
+  it("mobileNetworks delete test", async () => {
     const resArray = new Array();
-    for await (let item of client.mobileNetworks.listByResourceGroup(resourceGroup)) {
+    for await (const item of client.mobileNetworks.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 0);
   });
 
-  it("operation list test", async function () {
+  it("operation list test", async () => {
     const resArray = new Array();
-    for await (let item of client.operations.list()) {
+    for await (const item of client.operations.list()) {
       resArray.push(item);
     }
     assert.notEqual(resArray.length, 0);
   });
-})
+});
