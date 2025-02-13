1a892be5a0f62f69baaf9d989d91eb6a87a39cbb
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-13 23:38:19 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/sqlvirtualmachine/arm-sqlvirtualmachine/samples-dev/sqlVirtualMachineGroupsCreateOrUpdateSample.ts
+++ sdk/sqlvirtualmachine/arm-sqlvirtualmachine/samples-dev/sqlVirtualMachineGroupsCreateOrUpdateSample.ts
@@ -8,9 +8,7 @@
 import type { SqlVirtualMachineGroup } from "@azure/arm-sqlvirtualmachine";
 import { SqlVirtualMachineManagementClient } from "@azure/arm-sqlvirtualmachine";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Creates or updates a SQL virtual machine group.
@@ -18,7 +16,7 @@ dotenv.config();
  * @summary Creates or updates a SQL virtual machine group.
  * x-ms-original-file: specification/sqlvirtualmachine/resource-manager/Microsoft.SqlVirtualMachine/preview/2022-08-01-preview/examples/CreateOrUpdateSqlVirtualMachineGroup.json
  */
-async function createsOrUpdatesASqlVirtualMachineGroup() {
+async function createsOrUpdatesASqlVirtualMachineGroup(): Promise<void> {
   const subscriptionId =
     process.env["SQLVIRTUALMACHINE_SUBSCRIPTION_ID"] || "00000000-1111-2222-3333-444444444444";
   const resourceGroupName = process.env["SQLVIRTUALMACHINE_RESOURCE_GROUP"] || "testrg";
@@ -49,7 +47,7 @@ async function createsOrUpdatesASqlVirtualMachineGroup() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await createsOrUpdatesASqlVirtualMachineGroup();
 }
 
