482ba8b7e0d7397aa7b3a8275fa0c411b20bc050
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-13 22:40:58 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/storage/arm-storage/test/storage_examples.spec.ts
+++ sdk/storage/arm-storage/test/storage_examples.spec.ts
@@ -13,8 +13,8 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { StorageManagementClient } from "../src/storageManagementClient.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id"
