7fc4f7307337feb23deb695200a81bcef865941d
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-13 11:23:19 -0500

    [migrationdiscovery] Migrate migrationdiscovery projects to use snippets extraction (#33025)
    ### Packages impacted by this PR
    
    - @azure/arm-migrationdiscoverysap
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `migrationdiscovery` to use snippets
    extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/migrationdiscovery/arm-migrationdiscoverysap/samples/v1-beta/typescript/src/serverInstancesUpdateSample.ts
+++ sdk/migrationdiscovery/arm-migrationdiscoverysap/samples/v1-beta/typescript/src/serverInstancesUpdateSample.ts
@@ -13,9 +13,7 @@ import {
   WorkloadsClient,
 } from "@azure/arm-migrationdiscoverysap";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Updates the Server Instance resource. This operation on a resource by end user will return a Bad Request error.
@@ -23,7 +21,7 @@ dotenv.config();
  * @summary Updates the Server Instance resource. This operation on a resource by end user will return a Bad Request error.
  * x-ms-original-file: specification/workloads/resource-manager/Microsoft.Workloads/SAPDiscoverySites/preview/2023-10-01-preview/examples/ServerInstances_Update.json
  */
-async function updatesTheServerInstanceResource() {
+async function updatesTheServerInstanceResource(): Promise<void> {
   const subscriptionId =
     process.env["MIGRATIONDISCOVERY_SUBSCRIPTION_ID"] ||
     "6d875e77-e412-4d7d-9af4-8895278b4443";
@@ -45,7 +43,7 @@ async function updatesTheServerInstanceResource() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   updatesTheServerInstanceResource();
 }
 
