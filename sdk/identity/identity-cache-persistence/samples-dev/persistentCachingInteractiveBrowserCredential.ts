56d4895b22396adaacba221c705a3bb828f20200
KarishmaGhiya <kaghiya@microsoft.com>
2025-02-13 14:59:58 -0800

    ibc

--- sdk/identity/identity-cache-persistence/samples-dev/persistentCachingInteractiveBrowserCredential.ts
+++ sdk/identity/identity-cache-persistence/samples-dev/persistentCachingInteractiveBrowserCredential.ts
@@ -4,73 +4,68 @@
 /**
  * @summary Authenticates using persistent token caching with Interactive Browser Credential
  */
-import { InteractiveBrowserCredential, useIdentityPlugin, AuthenticationRecord } from "@azure/identity";
+import {
+  InteractiveBrowserCredential,
+  useIdentityPlugin,
+  AuthenticationRecord,
+} from "@azure/identity";
 import { cachePersistencePlugin } from "@azure/identity-cache-persistence";
-import path from 'node:path';
-import fs from 'node:fs';
-import { setLogLevel } from "@azure/logger";
-import dotenv from 'dotenv';
+import path from "path";
+import fs from "fs";
+import dotenv from "dotenv";
 
 useIdentityPlugin(cachePersistencePlugin);
-setLogLevel("verbose");
+
 dotenv.config();
-
-const clientId = "APP-REGISTRATION-CLIENT-ID"; // The app registration client Id in the Microsoft Entra tenant
-const tenantId = "TENANT-ID"; // The tenant ID in Microsoft Entra ID
-const loginHint = `EMAIL-ADDRESS`; // The email address of the user
-const authenticationRecordFolderPath = path.join(__dirname, "authenticationRecords");
-const authenticationRecordFilePath = path.join(authenticationRecordFolderPath, `cortex_notes@${loginHint}.json`);
-
-let authenticationRecord: AuthenticationRecord | undefined;
+// The app registration client Id in the Microsoft Entra tenant
+const clientId = "APP-REGISTRATION-CLIENT-ID";
+// The tenant ID in Microsoft Entra ID
+const tenantId = "TENANT-ID";
+// The email address of the user
+const loginHint = `EMAIL-ADDRESS`;
+// The file path to save the authentication record
+const authenticationRecordFilePath = path.resolve(__dirname, "authenticationRecord.json");
 
 async function main() {
+  try {
+    const fileContents = await fs.promises.readFile(authenticationRecordFilePath, "utf8");
+    const authenticationRecord = JSON.parse(fileContents);
+    await doAuthenticate(authenticationRecord);
+  } catch (e) {
+    console.error(e);
+    await doAuthenticate();
+  }
+}
+
+async function doAuthenticate(authenticationRecord?: AuthenticationRecord): Promise<void> {
+  const scopes = ["Mail.ReadWrite", "Calendars.Read"];
+  const credential = new InteractiveBrowserCredential({
+    tenantId,
+    clientId,
+    tokenCachePersistenceOptions: {
+      enabled: true,
+    },
+    loginHint: loginHint,
+    authenticationRecord: authenticationRecord,
+  });
+
+  if (!authenticationRecord) {
+    console.log("No authentication record found");
+    authenticationRecord = await credential.authenticate(scopes);
     try {
-        let jsonString: string | undefined;
-        await fs.promises.readFile(authenticationRecordFilePath, { encoding: "utf-8" })
-            .then(data => {
-                console.log(`data found: ${data}`);
-                jsonString = data;
-                authenticationRecord = JSON.parse(jsonString) as AuthenticationRecord;
-                return doAuthenticate(authenticationRecord);
-            })
-            .catch(err => {
-                console.error(err);
-            });
-        console.log(`jsonString => ${jsonString}`);
-    } catch (err) {
-        console.log(err);
+      const jsonString = JSON.stringify(authenticationRecord);
+      await fs.promises.writeFile(authenticationRecordFilePath, jsonString);
+      console.log("The file has been saved!");
+    } catch (error) {
+      console.log(error);
     }
+  } else {
+    console.log("Authenticating with authentication record");
+    await credential.getToken(scopes);
+    console.log("login successful");
+  }
 }
 
-async function doAuthenticate(authenticationRecord?: AuthenticationRecord) {
-    const scopes = ['Mail.ReadWrite', 'Calendars.Read'];
-    const credential = new InteractiveBrowserCredential({
-        tenantId,
-        clientId,
-        tokenCachePersistenceOptions: {
-            enabled: true,
-        },
-        loginHint: loginHint,
-        authenticationRecord: authenticationRecord
-    });
-
-    if (!authenticationRecord) {
-        console.log("No authentication record found");
-        authenticationRecord = await credential.authenticate(scopes);
-        try {
-            const jsonString = JSON.stringify(authenticationRecord);
-            await fs.promises.writeFile(authenticationRecordFilePath, jsonString);
-            console.log('The file has been saved!');
-        } catch (error) {
-            console.log(error);
-        }
-    } else {
-        console.log("Authenticating with authentication record");
-        await credential.getToken(scopes);
-        console.log("login successful");
-    }
-}
-
-main().catch((err) => {    
-    console.error("The sample encountered an error:", err);
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
 });
