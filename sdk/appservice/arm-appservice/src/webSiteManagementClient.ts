e638813757b6a6ff4bb66c7d59fac2834255ec03
azure-sdk <azuresdk@microsoft.com>
2025-02-13 07:15:18 +0000

    Post release automated changes for azure-arm-appservice

--- sdk/appservice/arm-appservice/src/webSiteManagementClient.ts
+++ sdk/appservice/arm-appservice/src/webSiteManagementClient.ts
@@ -191,7 +191,7 @@ export class WebSiteManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-appservice/16.0.0`;
+    const packageDetails = `azsdk-js-arm-appservice/16.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
