3591c5311f91fac2951b654a827ab7a91438770a
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-13 13:34:29 -0500

    [mobilenetwork] Migrate mobilenetwork projects to use snippets extraction (#33040)
    ### Packages impacted by this PR
    
    - @azure/arm-mobilenetwork
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `mobilenetwork` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/mobilenetwork/arm-mobilenetwork/samples-dev/packetCoreControlPlaneVersionsGetBySubscriptionSample.ts
+++ sdk/mobilenetwork/arm-mobilenetwork/samples-dev/packetCoreControlPlaneVersionsGetBySubscriptionSample.ts
@@ -7,9 +7,7 @@
  */
 import { MobileNetworkManagementClient } from "@azure/arm-mobilenetwork";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Gets information about the specified packet core control plane version.
@@ -17,7 +15,7 @@ dotenv.config();
  * @summary Gets information about the specified packet core control plane version.
  * x-ms-original-file: specification/mobilenetwork/resource-manager/Microsoft.MobileNetwork/stable/2024-04-01/examples/PacketCoreControlPlaneVersionGetBySubscription.json
  */
-async function getPacketCoreControlPlaneVersionBySubscription() {
+async function getPacketCoreControlPlaneVersionBySubscription(): Promise<void> {
   const subscriptionId =
     process.env["MOBILENETWORK_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
   const versionName = "2404.0-1";
@@ -27,7 +25,7 @@ async function getPacketCoreControlPlaneVersionBySubscription() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await getPacketCoreControlPlaneVersionBySubscription();
 }
 
