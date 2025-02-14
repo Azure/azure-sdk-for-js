c590312e95a9b267804abb89765b0a30ead40cfd
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-14 15:09:13 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/service-map/arm-servicemap/samples-dev/machinesListConnectionsSample.ts
+++ sdk/service-map/arm-servicemap/samples-dev/machinesListConnectionsSample.ts
@@ -8,9 +8,7 @@
 import type { MachinesListConnectionsOptionalParams } from "@azure/arm-servicemap";
 import { ServiceMap } from "@azure/arm-servicemap";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Returns a collection of connections terminating or originating at the specified machine
@@ -18,7 +16,7 @@ dotenv.config();
  * @summary Returns a collection of connections terminating or originating at the specified machine
  * x-ms-original-file: specification/service-map/resource-manager/Microsoft.OperationalInsights/preview/2015-11-01-preview/examples/Machines/SMMachinesListConnectionsGet.json
  */
-async function smMachinesListConnectionsGet() {
+async function smMachinesListConnectionsGet(): Promise<void> {
   const subscriptionId =
     process.env["SERVICE-MAP_SUBSCRIPTION_ID"] || "63BE4E24-FDF0-4E9C-9342-6A5D5A359722";
   const resourceGroupName = process.env["SERVICE-MAP_RESOURCE_GROUP"] || "rg-sm";
@@ -41,7 +39,7 @@ async function smMachinesListConnectionsGet() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await smMachinesListConnectionsGet();
 }
 
