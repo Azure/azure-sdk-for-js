8d3b2d666beceee17068a405da4461c9257d9b38
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-14 13:04:38 -0500

    Migration: Apply codemod: "fixTestingImports"

--- sdk/network/arm-network/test/network.spec.ts
+++ sdk/network/arm-network/test/network.spec.ts
@@ -13,8 +13,8 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { NetworkManagementClient } from "../src/networkManagementClient.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id"
