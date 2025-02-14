34449255f27734051d040c8c97e4e353e815f644
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-14 10:56:34 -0500

    Migration: Apply codemod: "fixTestingImports"

--- sdk/mysql/arm-mysql-flexible/test/mysql_operations_test.spec.ts
+++ sdk/mysql/arm-mysql-flexible/test/mysql_operations_test.spec.ts
@@ -13,8 +13,8 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { MySQLManagementFlexibleServerClient } from "../src/mySQLManagementFlexibleServerClient.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
