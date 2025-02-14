820ce972c55025a62a1f37b6cc46712dd4fc49cc
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-14 15:24:30 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/servicelinker/arm-servicelinker/test/servicelinker_operations_test.spec.ts
+++ sdk/servicelinker/arm-servicelinker/test/servicelinker_operations_test.spec.ts
@@ -13,8 +13,8 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { ServiceLinkerManagementClient } from "../src/serviceLinkerManagementClient.js"
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
