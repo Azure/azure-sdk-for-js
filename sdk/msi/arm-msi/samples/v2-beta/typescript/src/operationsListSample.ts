dff22463a4d9f6a46636b6788b404854966f05fd
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-14 10:48:39 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/msi/arm-msi/samples/v2-beta/typescript/src/operationsListSample.ts
+++ sdk/msi/arm-msi/samples/v2-beta/typescript/src/operationsListSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { ManagedServiceIdentityClient } from "@azure/arm-msi";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Lists available operations for the Microsoft.ManagedIdentity provider
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary Lists available operations for the Microsoft.ManagedIdentity provider
  * x-ms-original-file: specification/msi/resource-manager/Microsoft.ManagedIdentity/preview/2022-01-31-preview/examples/MsiOperationsList.json
  */
-async function msiOperationsList() {
+async function msiOperationsList(): Promise<void> {
   const subscriptionId =
     process.env["MSI_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -33,7 +31,7 @@ async function msiOperationsList() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   msiOperationsList();
 }
 
