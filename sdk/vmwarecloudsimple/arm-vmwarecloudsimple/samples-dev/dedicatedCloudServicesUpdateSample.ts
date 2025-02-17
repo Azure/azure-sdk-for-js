e12f7f0255e3cb16451ca84ecc502c6c2129311f
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-17 15:57:45 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/vmwarecloudsimple/arm-vmwarecloudsimple/samples-dev/dedicatedCloudServicesUpdateSample.ts
+++ sdk/vmwarecloudsimple/arm-vmwarecloudsimple/samples-dev/dedicatedCloudServicesUpdateSample.ts
@@ -8,9 +8,7 @@
 import type { PatchPayload } from "@azure/arm-vmwarecloudsimple";
 import { VMwareCloudSimple } from "@azure/arm-vmwarecloudsimple";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Patch dedicated cloud service's properties
@@ -18,7 +16,7 @@ dotenv.config();
  * @summary Patch dedicated cloud service's properties
  * x-ms-original-file: specification/vmwarecloudsimple/resource-manager/Microsoft.VMwareCloudSimple/stable/2019-04-01/examples/PatchDedicatedService.json
  */
-async function patchDedicatedService() {
+async function patchDedicatedService(): Promise<void> {
   const subscriptionId = process.env["VMWARECLOUDSIMPLE_SUBSCRIPTION_ID"] || "{subscription-id}";
   const resourceGroupName = process.env["VMWARECLOUDSIMPLE_RESOURCE_GROUP"] || "myResourceGroup";
   const dedicatedCloudServiceName = "myService";
@@ -35,7 +33,7 @@ async function patchDedicatedService() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   await patchDedicatedService();
 }
 
