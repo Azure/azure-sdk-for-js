ecd60e43d20add8d58e2b61be70bd97f6096efeb
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-18 17:58:15 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/privatedns/arm-privatedns/test/privatedns_operations_test.spec.ts
+++ sdk/privatedns/arm-privatedns/test/privatedns_operations_test.spec.ts
@@ -10,13 +10,11 @@ import {
   env,
   Recorder,
   RecorderStartOptions,
-  delay,
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { PrivateDnsManagementClient } from "../src/privateDnsManagementClient";
+import { PrivateDnsManagementClient } from "../src/privateDnsManagementClient.js";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "azure_subscription_id"
@@ -42,22 +40,22 @@ describe("PrivateDns test", () => {
   let resourceGroup: string;
   let resourcename: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new PrivateDnsManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus";
-    resourceGroup = "myjstest";
-    resourcename = "privatezone1.com";
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new PrivateDnsManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "eastus";
+      resourceGroup = "myjstest";
+      resourcename = "privatezone1.com";
 
-  });
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("privateZones create test", async function () {
     const res = await client.privateZones.beginCreateOrUpdateAndWait(
@@ -87,8 +85,6 @@ describe("PrivateDns test", () => {
 
   it("privateZones delete test", async function () {
     const resArray = new Array();
-    const res = await client.privateZones.beginDeleteAndWait(resourceGroup, resourcename, testPollingOptions
-    )
     for await (let item of client.privateZones.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
