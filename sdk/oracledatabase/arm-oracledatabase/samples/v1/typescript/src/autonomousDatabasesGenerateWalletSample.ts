bcace99e1d30b44515ce7d2c3e89b0f1a72bf8aa
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-18 12:56:25 -0500

    Migration: Apply codemod: "fixSourceFile"

--- sdk/oracledatabase/arm-oracledatabase/samples/v1/typescript/src/autonomousDatabasesGenerateWalletSample.ts
+++ sdk/oracledatabase/arm-oracledatabase/samples/v1/typescript/src/autonomousDatabasesGenerateWalletSample.ts
@@ -13,9 +13,7 @@ import {
   OracleDatabaseManagementClient,
 } from "@azure/arm-oracledatabase";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Generate wallet action on Autonomous Database
@@ -23,7 +21,7 @@ dotenv.config();
  * @summary Generate wallet action on Autonomous Database
  * x-ms-original-file: specification/oracle/resource-manager/Oracle.Database/stable/2023-09-01/examples/autonomousDatabase_generateWallet.json
  */
-async function generateWalletActionOnAutonomousDatabase() {
+async function generateWalletActionOnAutonomousDatabase(): Promise<void> {
   const subscriptionId =
     process.env["ORACLEDATABASE_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -45,7 +43,7 @@ async function generateWalletActionOnAutonomousDatabase() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   generateWalletActionOnAutonomousDatabase();
 }
 
--- sdk/oracledatabase/arm-oracledatabase/samples/v1/typescript/src/autonomousDatabasesShrinkSample.ts
+++ sdk/oracledatabase/arm-oracledatabase/samples/v1/typescript/src/autonomousDatabasesShrinkSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { OracleDatabaseManagementClient } from "@azure/arm-oracledatabase";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to This operation shrinks the current allocated storage down to the current actual used data storage.
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary This operation shrinks the current allocated storage down to the current actual used data storage.
  * x-ms-original-file: specification/oracle/resource-manager/Oracle.Database/stable/2023-09-01/examples/autonomousDatabase_shrink.json
  */
-async function performShrinkActionOnAutonomousDatabase() {
+async function performShrinkActionOnAutonomousDatabase(): Promise<void> {
   const subscriptionId =
     process.env["ORACLEDATABASE_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -36,7 +34,7 @@ async function performShrinkActionOnAutonomousDatabase() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   performShrinkActionOnAutonomousDatabase();
 }
 
