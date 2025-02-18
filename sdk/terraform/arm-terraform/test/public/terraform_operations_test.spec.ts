fc689a2189fa9fc0de60721fdb86c8f5418c9e64
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-18 17:36:31 -0500

    Migration: npm run format

--- sdk/terraform/arm-terraform/test/public/terraform_operations_test.spec.ts
+++ sdk/terraform/arm-terraform/test/public/terraform_operations_test.spec.ts
@@ -34,8 +34,8 @@ describe("AzureTerraform test", () => {
   });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
   it("operations list test", async function () {
     const resArray = new Array();
     for await (let item of client.operations.list()) {
