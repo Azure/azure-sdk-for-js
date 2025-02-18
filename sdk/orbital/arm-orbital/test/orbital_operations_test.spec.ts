ca40626097a4ae0727f88930f386439456539dd4
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-18 13:06:18 -0500

    Migration: Apply codemod: "fixTestingImports"

--- sdk/orbital/arm-orbital/test/orbital_operations_test.spec.ts
+++ sdk/orbital/arm-orbital/test/orbital_operations_test.spec.ts
@@ -5,8 +5,8 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { AzureOrbital } from "../src/azureOrbital.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
