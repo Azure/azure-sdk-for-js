d89fd31af2585d226d73f6285361127719d0b345
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-18 14:17:26 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/recoveryservicesbackup/arm-recoveryservicesbackup/test/recoveryservicesbackup_operations_test.spec.ts
+++ sdk/recoveryservicesbackup/arm-recoveryservicesbackup/test/recoveryservicesbackup_operations_test.spec.ts
@@ -13,9 +13,9 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { RecoveryServicesBackupClient } from "../src/recoveryServicesBackupClient.js";
 import { RecoveryServicesClient } from "@azure/arm-recoveryservices";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
