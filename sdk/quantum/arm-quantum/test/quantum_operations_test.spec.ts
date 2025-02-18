054d7bd22439b2a79caac544a1c5999adfb70a5a
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-18 17:07:35 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/quantum/arm-quantum/test/quantum_operations_test.spec.ts
+++ sdk/quantum/arm-quantum/test/quantum_operations_test.spec.ts
@@ -15,8 +15,7 @@ import {
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert } from "chai";
-import { Context } from "mocha";
-import { AzureQuantumManagementClient } from "../src/azureQuantumManagementClient";
+import { AzureQuantumManagementClient } from "../src/azureQuantumManagementClient.js";
 
 const replaceableVariables: Record<string, string> = {
   SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
@@ -42,22 +41,22 @@ describe("quantum test", () => {
   let resourceGroup: string;
   let resourcename: string;
 
-  beforeEach(async function (this: Context) {
-    recorder = new Recorder(this.currentTest);
-    await recorder.start(recorderOptions);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used
-    const credential = createTestCredential();
-    client = new AzureQuantumManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-    location = "eastus2euap";
-    resourceGroup = "myjstest";
-    resourcename = "resource-test1";
+  beforeEach(async (ctx) => {
+      recorder = new Recorder(ctx);
+      await recorder.start(recorderOptions);
+      subscriptionId = env.SUBSCRIPTION_ID || '';
+      // This is an example of how the environment variables are used
+      const credential = createTestCredential();
+      client = new AzureQuantumManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+      location = "eastus2euap";
+      resourceGroup = "myjstest";
+      resourcename = "resource-test1";
 
-  });
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("workspaces create test", async function () {
     const res = await client.workspaces.beginCreateOrUpdateAndWait(
@@ -97,8 +96,6 @@ describe("quantum test", () => {
 
   it("workspaces delete test", async function () {
     const resArray = new Array();
-    const res = await client.workspaces.beginDeleteAndWait(resourceGroup, resourcename, testPollingOptions
-    )
     for await (let item of client.workspaces.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
