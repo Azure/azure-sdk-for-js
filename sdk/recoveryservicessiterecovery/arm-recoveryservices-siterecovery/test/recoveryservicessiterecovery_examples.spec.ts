3873c05e55f6a900611035739d7f17df110216ef
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-18 13:49:22 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/recoveryservicessiterecovery/arm-recoveryservices-siterecovery/test/recoveryservicessiterecovery_examples.spec.ts
+++ sdk/recoveryservicessiterecovery/arm-recoveryservices-siterecovery/test/recoveryservicessiterecovery_examples.spec.ts
@@ -14,6 +14,7 @@ import {
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { SiteRecoveryManagementClient } from "../src/siteRecoveryManagementClient.js";
+import { describe, it, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
