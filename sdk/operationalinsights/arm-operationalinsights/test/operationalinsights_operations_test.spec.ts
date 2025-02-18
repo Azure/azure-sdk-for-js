045a54194aba7bbe27fdbd657280329918f5444f
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-18 11:15:13 -0500

    [operationalinsights] Migrate operationalinsights projects to use snippets extraction

--- sdk/operationalinsights/arm-operationalinsights/test/operationalinsights_operations_test.spec.ts
+++ sdk/operationalinsights/arm-operationalinsights/test/operationalinsights_operations_test.spec.ts
@@ -6,12 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import {
-  env,
-  Recorder,
-  RecorderStartOptions,
-  isPlaybackMode,
-} from "@azure-tools/test-recorder";
+import type { RecorderStartOptions } from "@azure-tools/test-recorder";
+import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { OperationalInsightsManagementClient } from "../src/operationalInsightsManagementClient.js";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
@@ -20,7 +16,7 @@ const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
   AZURE_CLIENT_SECRET: "azure_client_secret",
   AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
-  SUBSCRIPTION_ID: "azure_subscription_id"
+  SUBSCRIPTION_ID: "azure_subscription_id",
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -44,23 +40,27 @@ describe("operationalinsights test", () => {
   let workspaceName: string;
 
   beforeEach(async (ctx) => {
-      recorder = new Recorder(ctx);
-      await recorder.start(recorderOptions);
-      subscriptionId = env.SUBSCRIPTION_ID || '';
-      // This is an example of how the environment variables are used
-      const credential = createTestCredential();
-      client = new OperationalInsightsManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      location = "eastus";
-      resourceGroup = "myjstest";
-      // workspaceName = "workspacetest";
-      workspaceName = "oiautorest6685";
-    });
+    recorder = new Recorder(ctx);
+    await recorder.start(recorderOptions);
+    subscriptionId = env.SUBSCRIPTION_ID || "";
+    // This is an example of how the environment variables are used
+    const credential = createTestCredential();
+    client = new OperationalInsightsManagementClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    location = "eastus";
+    resourceGroup = "myjstest";
+    // workspaceName = "workspacetest";
+    workspaceName = "oiautorest6685";
+  });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
 
-  it("workspaces create test", async function () {
+  it("workspaces create test", async () => {
     const res = await client.workspaces.beginCreateOrUpdateAndWait(
       resourceGroup,
       workspaceName,
@@ -68,40 +68,40 @@ describe("operationalinsights test", () => {
         location,
         retentionInDays: 30,
         sku: { name: "PerGB2018" },
-        tags: { tag1: "val1" }
+        tags: { tag1: "val1" },
       },
-      testPollingOptions);
+      testPollingOptions,
+    );
     assert.equal(res.name, workspaceName);
   });
 
-  it("workspaces get test", async function () {
+  it("workspaces get test", async () => {
     const res = await client.workspaces.get(resourceGroup, workspaceName);
     assert.equal(res.name, workspaceName);
   });
 
-  it("workspaces list test", async function () {
+  it("workspaces list test", async () => {
     const resArray = new Array();
-    for await (let item of client.workspaces.listByResourceGroup(resourceGroup)) {
+    for await (const item of client.workspaces.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
     // There are two workspace work for other services on the portal, so now after creating this service ,it is 3 .Next time it will be reverted to 1
     assert.equal(resArray.length, 3);
   });
 
-  it("table list test", async function () {
-
+  it("table list test", async () => {
     const resArray = new Array();
-    for await (let item of client.tables.listByWorkspace(resourceGroup, workspaceName)) {
+    for await (const item of client.tables.listByWorkspace(resourceGroup, workspaceName)) {
       resArray.push(item);
     }
   });
 
-  it("workspaces delete test", async function () {
+  it("workspaces delete test", async () => {
     const resArray = new Array();
-    for await (let item of client.workspaces.listByResourceGroup(resourceGroup)) {
+    for await (const item of client.workspaces.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
     // There are two workspace work for other services on the portal, so it is 2. Next time it will be reverted to 0
     assert.equal(resArray.length, 2);
   });
-})
+});
