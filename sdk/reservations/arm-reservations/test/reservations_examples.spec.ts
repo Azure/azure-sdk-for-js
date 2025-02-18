09e4f3da9d954d6081b705b0cc1332a10a94a13f
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-18 12:26:54 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/reservations/arm-reservations/test/reservations_examples.spec.ts
+++ sdk/reservations/arm-reservations/test/reservations_examples.spec.ts
@@ -13,9 +13,9 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { AzureReservationAPI } from "../src/azureReservationAPI.js";
 import { CurrentQuotaLimitBase } from "../src/models/index.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
