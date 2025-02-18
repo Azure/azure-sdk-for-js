3e86120fc3e8555c3575b181107703464265d714
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-18 12:03:41 +0800

    Migration: Apply codemod: "fixTestingImports"

--- sdk/resourceconnector/arm-resourceconnector/test/resourceconnnector__operations_test.spec.ts
+++ sdk/resourceconnector/arm-resourceconnector/test/resourceconnnector__operations_test.spec.ts
@@ -13,8 +13,8 @@ import {
   isPlaybackMode,
 } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { assert } from "chai";
 import { ResourceConnectorManagementClient } from "../src/resourceConnectorManagementClient.js";
+import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
   AZURE_CLIENT_ID: "azure_client_id",
