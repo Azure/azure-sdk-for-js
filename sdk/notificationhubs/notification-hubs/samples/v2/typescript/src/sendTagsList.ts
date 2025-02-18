83a99c54f3006ca139a1a01d6babf4f273dfef85
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-18 16:39:03 -0500

    [notificationhubs] Migrate notificationhubs projects to use snippets extraction (#33073)
    ### Packages impacted by this PR
    
    - @azure/arm-notificationhubs
    - @azure/notification-hubs
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `notificationhubs` to use snippets
    extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Jeremy Meng <jeremy.ymeng@gmail.com>

---
+++ sdk/notificationhubs/notification-hubs/samples/v2/typescript/src/sendTagsList.ts
@@ -0,0 +1,98 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * This sample demonstrates how the sendNotification() method can be used to send a tags list
+ * notification using APNs.  This sends a JSON message to an APNs given device token and returns
+ * a Tracking ID which can be used for troubleshooting with the Azure Notification Hubs team.
+ *
+ * See https://learn.microsoft.com/azure/notification-hubs/notification-hubs-tags-segment-push-message
+ * to learn about Routing and Tag Expressions.
+ *
+ *
+ * @summary Demonstrates how to send tag expression notifications using Azure Notification Hubs
+ */
+
+import "dotenv/config";
+import {
+  NotificationDetails,
+  NotificationOutcomeState,
+  createAppleNotification,
+  createTagExpression,
+} from "@azure/notification-hubs/models";
+import {
+  NotificationHubsClientContext,
+  createClientContext,
+  getNotificationOutcomeDetails,
+  sendNotification,
+} from "@azure/notification-hubs/api";
+import { delay } from "@azure/core-util";
+import { isRestError } from "@azure/core-rest-pipeline";
+
+// Define connection string and hub name
+const connectionString = process.env.NOTIFICATIONHUBS_CONNECTION_STRING || "<connection string>";
+const hubName = process.env.NOTIFICATION_HUB_NAME || "<hub name>";
+
+async function main(): Promise<void> {
+  const context = createClientContext(connectionString, hubName);
+
+  const messageBody = `{ "aps" : { "alert" : "Hello" } }`;
+  const tagExpression = createTagExpression(["likes_hockey", "likes_football"]);
+
+  const notification = createAppleNotification({
+    body: messageBody,
+    headers: {
+      "apns-priority": "10",
+      "apns-push-type": "alert",
+    },
+  });
+
+  const result = await sendNotification(context, notification, {
+    enableTestSend: false,
+    tagExpression,
+  });
+
+  console.log(`Tag List send Tracking ID: ${result.trackingId}`);
+  console.log(`Tag List Correlation ID: ${result.correlationId}`);
+
+  // Only available in Standard SKU and above
+  if (result.notificationId) {
+    console.log(`Tag List send Notification ID: ${result.notificationId}`);
+
+    const results = await getNotificationDetails(context, result.notificationId);
+    if (results) {
+      console.log(JSON.stringify(results, null, 2));
+    }
+  }
+}
+
+async function getNotificationDetails(
+  context: NotificationHubsClientContext,
+  notificationId: string,
+): Promise<NotificationDetails | undefined> {
+  let state: NotificationOutcomeState = "Enqueued";
+  let count = 0;
+  let result: NotificationDetails | undefined;
+  while ((state === "Enqueued" || state === "Processing") && count++ < 10) {
+    try {
+      result = await getNotificationOutcomeDetails(context, notificationId);
+      state = result.state!;
+    } catch (e) {
+      // Possible to get 404 for when it doesn't exist yet.
+      if (isRestError(e) && e.statusCode === 404) {
+        continue;
+      } else {
+        throw e;
+      }
+    }
+
+    await delay(1000);
+  }
+
+  return result;
+}
+
+main().catch((err) => {
+  console.log("sendTagsList Sample: Error occurred: ", err);
+  process.exit(1);
+});
