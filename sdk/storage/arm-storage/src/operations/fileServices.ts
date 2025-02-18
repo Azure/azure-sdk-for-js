cb0c9e47183792b9ca681dbb7bb9310d9da564a4
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-02-18 11:18:57 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/storage/arm-storage/src/operations/fileServices.ts
+++ sdk/storage/arm-storage/src/operations/fileServices.ts
@@ -7,12 +7,12 @@
  */
 
 import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
-import { setContinuationToken } from "../pagingHelper";
-import { FileServices } from "../operationsInterfaces";
+import { setContinuationToken } from "../pagingHelper.js";
+import { FileServices } from "../operationsInterfaces/index.js";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers";
-import * as Parameters from "../models/parameters";
-import { StorageManagementClient } from "../storageManagementClient";
+import * as Mappers from "../models/mappers.js";
+import * as Parameters from "../models/parameters.js";
+import { StorageManagementClient } from "../storageManagementClient.js";
 import {
   FileServiceUsage,
   FileServicesListServiceUsagesNextOptionalParams,
@@ -28,7 +28,7 @@ import {
   FileServicesGetServiceUsageOptionalParams,
   FileServicesGetServiceUsageResponse,
   FileServicesListServiceUsagesNextResponse,
-} from "../models";
+} from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Class containing FileServices operations. */
