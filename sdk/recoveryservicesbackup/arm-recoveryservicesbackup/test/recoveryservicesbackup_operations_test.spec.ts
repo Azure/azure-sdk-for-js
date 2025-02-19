17d3933e6f9cbb5c46fa974a921d263d0a827e92
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-02-19 13:36:13 +0800

    update test case

--- sdk/recoveryservicesbackup/arm-recoveryservicesbackup/test/recoveryservicesbackup_operations_test.spec.ts
+++ sdk/recoveryservicesbackup/arm-recoveryservicesbackup/test/recoveryservicesbackup_operations_test.spec.ts
@@ -6,19 +6,14 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import {
-  env,
-  Recorder,
-  RecorderStartOptions,
-  isPlaybackMode,
-} from "@azure-tools/test-recorder";
+import { env, Recorder, RecorderStartOptions, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { RecoveryServicesBackupClient } from "../src/recoveryServicesBackupClient.js";
 import { RecoveryServicesClient } from "@azure/arm-recoveryservices";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
-  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888"
+  SUBSCRIPTION_ID: "88888888-8888-8888-8888-888888888888",
 };
 
 const recorderOptions: RecorderStartOptions = {
@@ -45,23 +40,30 @@ describe("RecoveryServicesBackup test", () => {
   let vaultsname: string;
 
   beforeEach(async (ctx) => {
-      recorder = new Recorder(ctx);
-      await recorder.start(recorderOptions);
-      subscriptionId = env.SUBSCRIPTION_ID || '';
-      // This is an example of how the environment variables are used
-      const credential = createTestCredential();
-      client = new RecoveryServicesBackupClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      rsclient = new RecoveryServicesClient(credential, subscriptionId, recorder.configureClientOptions({}));
-      location = "eastus";
-      resourceGroup = "myjstest";
-      resourcename = "resourcetest";
-      vaultsname = "vaultstest";
-
-    });
+    recorder = new Recorder(ctx);
+    await recorder.start(recorderOptions);
+    subscriptionId = env.SUBSCRIPTION_ID || "";
+    // This is an example of how the environment variables are used
+    const credential = createTestCredential();
+    client = new RecoveryServicesBackupClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    rsclient = new RecoveryServicesClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
+    location = "eastus";
+    resourceGroup = "myjstest";
+    resourcename = "resourcetest";
+    vaultsname = "vaultstest";
+  });
 
   afterEach(async () => {
-      await recorder.stop();
-    });
+    await recorder.stop();
+  });
 
   it("dependence create test", async () => {
     const res = await rsclient.vaults.beginCreateOrUpdateAndWait(
@@ -71,9 +73,10 @@ describe("RecoveryServicesBackup test", () => {
         identity: { type: "SystemAssigned" },
         location,
         properties: { publicNetworkAccess: "Enabled" },
-        sku: { name: "Standard" }
+        sku: { name: "Standard" },
       },
-      testPollingOptions);
+      testPollingOptions,
+    );
     console.log(res.name);
     assert.equal(res.name, vaultsname);
   });
@@ -85,41 +88,38 @@ describe("RecoveryServicesBackup test", () => {
       resourcename,
       {
         properties: {
-          "backupManagementType": "AzureIaasVM",
-          "policyType": "V1",
-          "instantRPDetails": {},
-          "schedulePolicy": {
-            "schedulePolicyType": "SimpleSchedulePolicy",
-            "scheduleRunFrequency": "Daily",
-            "scheduleRunTimes": [
-              new Date("2023-03-17T18:00:00Z")
-            ],
-            "scheduleWeeklyFrequency": 0
+          backupManagementType: "AzureIaasVM",
+          policyType: "V1",
+          instantRPDetails: {},
+          schedulePolicy: {
+            schedulePolicyType: "SimpleSchedulePolicy",
+            scheduleRunFrequency: "Daily",
+            scheduleRunTimes: [new Date("2023-03-17T18:00:00Z")],
+            scheduleWeeklyFrequency: 0,
           },
-          "retentionPolicy": {
-            "retentionPolicyType": "LongTermRetentionPolicy",
-            "dailySchedule": {
-              "retentionTimes": [
-                new Date("2023-03-17T18:00:00Z")
-              ],
-              "retentionDuration": {
-                "count": 180,
-                "durationType": "Days"
-              }
-            }
+          retentionPolicy: {
+            retentionPolicyType: "LongTermRetentionPolicy",
+            dailySchedule: {
+              retentionTimes: [new Date("2023-03-17T18:00:00Z")],
+              retentionDuration: {
+                count: 180,
+                durationType: "Days",
+              },
+            },
           },
-          "tieringPolicy": {
-            "ArchivedRP": {
-              "tieringMode": "DoNotTier",
-              "duration": 0,
-              "durationType": "Invalid"
-            }
+          tieringPolicy: {
+            ArchivedRP: {
+              tieringMode: "DoNotTier",
+              duration: 0,
+              durationType: "Invalid",
+            },
           },
-          "instantRpRetentionRangeInDays": 2,
-          "timeZone": "UTC",
-          "protectedItemsCount": 0
-        }
-      },);
+          instantRpRetentionRangeInDays: 2,
+          timeZone: "UTC",
+          protectedItemsCount: 0,
+        },
+      },
+    );
     assert.equal(res.name, resourcename);
   });
 
@@ -129,13 +129,20 @@ describe("RecoveryServicesBackup test", () => {
   });
 
   it("protectionPolicies delete test", async () => {
+    await client.protectionPolicies.beginDeleteAndWait(
+      vaultsname,
+      resourceGroup,
+      resourcename,
+      testPollingOptions,
+    );
   });
 
   it("dependence delete test", async () => {
     const resArray = new Array();
+    await rsclient.vaults.delete(resourceGroup, vaultsname);
     for await (let item of rsclient.vaults.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 0);
   });
-})
+});
