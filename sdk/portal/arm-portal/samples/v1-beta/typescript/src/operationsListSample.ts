c350fd488bcacb242114a96a59bc166c57eecd9b
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-18 19:19:14 -0500

    [portal] Migrate portal projects to use snippets extraction (#33115)
    ### Packages impacted by this PR
    
    - @azure/arm-portal
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `portal` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/portal/arm-portal/samples/v1-beta/typescript/src/operationsListSample.ts
+++ sdk/portal/arm-portal/samples/v1-beta/typescript/src/operationsListSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { Portal } from "@azure/arm-portal";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to The Microsoft Portal operations API.
@@ -20,7 +18,7 @@ dotenv.config();
  * @summary The Microsoft Portal operations API.
  * x-ms-original-file: specification/portal/resource-manager/Microsoft.Portal/preview/2020-09-01-preview/examples/operationsList.json
  */
-async function listThePortalOperations() {
+async function listThePortalOperations(): Promise<void> {
   const subscriptionId =
     process.env["PORTAL_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -33,7 +31,7 @@ async function listThePortalOperations() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   listThePortalOperations();
 }
 
