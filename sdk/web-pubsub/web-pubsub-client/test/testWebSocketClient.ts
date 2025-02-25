04f3eb76c1b55e64439f78c74a7740e204cb21c9
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-25 13:28:31 -0500

    [web-pubsub] Migrate web-pubsub projects to use snippets extraction

--- sdk/web-pubsub/web-pubsub-client/test/testWebSocketClient.ts
+++ sdk/web-pubsub/web-pubsub-client/test/testWebSocketClient.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import type { AbortSignalLike } from "@azure/abort-controller";
-import EventEmitter from "node:events";
+import EventEmitter from "events";
 import type { WebPubSubClient } from "../src/webPubSubClient.js";
 import type {
   WebSocketClientFactoryLike,
