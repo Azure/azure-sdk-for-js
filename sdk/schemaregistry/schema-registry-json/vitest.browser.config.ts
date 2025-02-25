286563aa289b6202def7f9936b8f0ab92ef0050e
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-25 18:04:24 -0500

    Migration: Update test config

--- sdk/schemaregistry/schema-registry-json/vitest.browser.config.ts
+++ sdk/schemaregistry/schema-registry-json/vitest.browser.config.ts
@@ -1,22 +1,17 @@
+
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
-import browserMap from "@azure-tools/vite-plugin-browser-test-map";
-import inject from "@rollup/plugin-inject";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    optimizeDeps: {
-      include: ["buffer"],
-    },
-    plugins: [browserMap(), inject({ Buffer: ["buffer", "Buffer"] })],
     test: {
-      include: ["dist-test/browser/test/**/*.spec.js"],
-      hookTimeout: 5000000,
-      testTimeout: 5000000,
+      include: ["dist-test/browser/test/**/*.spec.js",],
+      testTimeout: 1200000,
+      hookTimeout: 1200000,
     },
   }),
 );
