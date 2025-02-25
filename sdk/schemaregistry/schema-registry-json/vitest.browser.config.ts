b487ff0db767b040f249f1ec989b29f705daf7cf
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-25 18:37:47 -0500

    [schemaregistry] Migrate schemaregistry projects to use snippets extraction

--- sdk/schemaregistry/schema-registry-json/vitest.browser.config.ts
+++ sdk/schemaregistry/schema-registry-json/vitest.browser.config.ts
@@ -3,11 +3,18 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
+import browserMap from "@azure-tools/vite-plugin-browser-test-map";
+import inject from "@rollup/plugin-inject";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    optimizeDeps: {
+      include: ["buffer"],
+    },
+    plugins: [browserMap(), inject({ Buffer: ["buffer", "Buffer"] })],
     test: {
+      fileParallelism: false,
       include: ["dist-test/browser/test/**/*.spec.js"],
       testTimeout: 1200000,
       hookTimeout: 1200000,
