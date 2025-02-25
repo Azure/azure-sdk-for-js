3a59833caa3494d7351066b5841ea03abbfefde9
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-25 12:13:35 -0500

    Update sdk/tables/data-tables/test/snippets.spec.ts
    Co-authored-by: Jeremy Meng <jeremy.ymeng@gmail.com>

--- sdk/tables/data-tables/test/snippets.spec.ts
+++ sdk/tables/data-tables/test/snippets.spec.ts
@@ -26,10 +26,6 @@ describe("snippets", () => {
   });
 
   it("ReadmeSampleCreateClient_TokenCredential", async () => {
-    // DefaultAzureCredential expects the following three environment variables:
-    // - AZURE_TENANT_ID: The tenant ID in Azure Active Directory
-    // - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
-    // - AZURE_CLIENT_SECRET: The client secret for the registered application
     const credential = new DefaultAzureCredential();
     const account = "<account name>";
     // @ts-preserve-whitespace
