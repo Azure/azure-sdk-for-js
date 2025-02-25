69cbf97414be56a0c59be05563a834c8c70caa1e
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-25 11:31:40 -0500

    Migration: Update test config

--- sdk/web-pubsub/web-pubsub-client-protobuf/vitest.browser.config.ts
+++ sdk/web-pubsub/web-pubsub-client-protobuf/vitest.browser.config.ts
@@ -3,12 +3,13 @@
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "../../../vitest.shared.config.ts";
+import viteConfig from "../../../vitest.browser.shared.config.ts";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      include: ["dist-test/browser/test/**/*.spec.js",],
       testTimeout: 1200000,
       hookTimeout: 1200000,
     },
