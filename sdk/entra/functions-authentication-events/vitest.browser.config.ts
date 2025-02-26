a09b07d3150d5462e2053a5e017b5a4cd54fbd10
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-25 20:36:32 -0500

    Migration: Update test config

--- sdk/entra/functions-authentication-events/vitest.browser.config.ts
+++ sdk/entra/functions-authentication-events/vitest.browser.config.ts
@@ -1,14 +1,15 @@
+
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "../../../vitest.browser.base.config.ts";
+import viteConfig from "../../../vitest.browser.shared.config.ts";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      include: ["dist-test/browser/test/**/*.spec.js"],
+      include: ["dist-test/browser/test/**/*.spec.js",],
       testTimeout: 1200000,
       hookTimeout: 1200000,
     },
