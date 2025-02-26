7810d929a71496aa07ac94dacb8439dfba70639d
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-25 20:40:28 -0500

    [entra] Migrate entra projects to use snippets extraction

--- sdk/entra/functions-authentication-events/vitest.browser.config.ts
+++ sdk/entra/functions-authentication-events/vitest.browser.config.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "../../../vitest.browser.shared.config.ts";
+import viteConfig from "../../../vitest.browser.base.config.ts";
 
 export default mergeConfig(
   viteConfig,
