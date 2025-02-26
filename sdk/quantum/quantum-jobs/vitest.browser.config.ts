c50254b27e7d00bf37f1bbb1a3c27c8e6bfa3400
Matthew Podwysocki <matthewp@microsoft.com>
2025-02-26 15:06:13 -0500

    [quantum] Migrate quantum projects to use snippets extraction (#33198)
    ### Packages impacted by this PR
    
    - @azure/quantum-jobs
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `quantum` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/quantum/quantum-jobs/vitest.browser.config.ts
+++ sdk/quantum/quantum-jobs/vitest.browser.config.ts
@@ -2,12 +2,14 @@
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "../../../vitest.shared.config.ts";
+import viteConfig from "../../../vitest.browser.shared.config.ts";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      include: ["dist-test/browser/test/**/*.spec.js"],
+      exclude: ["dist-test/browser/test/snippets.spec.js"],
       testTimeout: 1200000,
       hookTimeout: 1200000,
     },
