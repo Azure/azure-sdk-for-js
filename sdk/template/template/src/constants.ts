9b23e6f1cbea2da0ff5103f638dc4174e295a693
azure-sdk <azuresdk@microsoft.com>
2025-02-28 20:02:00 +0000

    Post release automated changes for azure-template

--- sdk/template/template/src/constants.ts
+++ sdk/template/template/src/constants.ts
@@ -1,4 +1,4 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export const SDK_VERSION: string = "1.0.13-beta.2";
+export const SDK_VERSION: string = "1.0.13-beta.3";
