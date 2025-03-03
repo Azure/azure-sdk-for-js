257fa692b0e08a5e5558a1828f29ea8247e39e7d
Matthew Podwysocki <matthewp@microsoft.com>
2025-03-03 10:13:06 -0500

    Migration: Update test config

--- sdk/openai/openai/vitest.browser.config.ts
+++ sdk/openai/openai/vitest.browser.config.ts
@@ -4,20 +4,14 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
-import { resolve } from "node:path";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      testTimeout: 500000,
-      hookTimeout: 25000,
-      fileParallelism: false,
-      globalSetup: [resolve(__dirname, "test/utils/setup.ts")],
-      setupFiles: [resolve(__dirname, "test/utils/logging.ts")],
-      include: [
-        "dist-test/browser/test/**/*.spec.js",
-      ],
+      include: ["dist-test/browser/test/**/*.spec.js",],
+      testTimeout: 1200000,
+      hookTimeout: 1200000,
     },
   }),
 );
--- sdk/openai/openai/vitest.config.ts
+++ sdk/openai/openai/vitest.config.ts
@@ -1,22 +1,16 @@
+
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.shared.config.ts";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      testTimeout: 500000,
-      hookTimeout: 25000,
-      fileParallelism: false,
-      globalSetup: [resolve(__dirname, "test/utils/setup.ts")],
+      testTimeout: 1200000,
+      hookTimeout: 1200000,
     },
   }),
 );
