c19a89478dfa595c445ce7b294139e0b57e217e5
Matthew Podwysocki <matthewp@microsoft.com>
2025-03-03 15:58:54 -0500

    [servicebus] Migrate servicebus projects to use snippets extraction

--- sdk/servicebus/service-bus/vitest.browser.config.ts
+++ sdk/servicebus/service-bus/vitest.browser.config.ts
@@ -1,15 +1,27 @@
-
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
+import browserMap from "@azure-tools/vite-plugin-browser-test-map";
+import inject from "@rollup/plugin-inject";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    define: {
+      "process.env": process.env,
+    },
+    optimizeDeps: {
+      include: ["process", "buffer"],
+    },
+    plugins: [
+      browserMap(),
+      inject({ process: "process", Buffer: ["buffer", "Buffer"], stream: ["stream", "stream"] }),
+    ],
     test: {
-      include: ["dist-test/browser/test/**/*.spec.js",],
+      fileParallelism: false,
+      include: ["dist-test/browser/test/**/*.spec.js"],
       testTimeout: 1200000,
       hookTimeout: 1200000,
     },
--- sdk/servicebus/service-bus/vitest.config.ts
+++ sdk/servicebus/service-bus/vitest.config.ts
@@ -1,4 +1,3 @@
-
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
@@ -9,8 +8,24 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      testTimeout: 1200000,
-      hookTimeout: 1200000,
+      testTimeout: 600000,
+      hookTimeout: 60000,
+      fileParallelism: false,
+      exclude: ["test/internal/connectionManagement.spec.ts"],
+      fakeTimers: {
+        toFake: [
+          "setTimeout",
+          "clearTimeout",
+          "setImmediate",
+          "clearImmediate",
+          "setInterval",
+          "clearInterval",
+          "Date",
+        ],
+      },
+      typecheck: {
+        enabled: false,
+      },
     },
   }),
 );
