680a84f1f3ce9e5178419045530645de9f70749f
Matthew Podwysocki <matthewp@microsoft.com>
2025-03-03 11:07:22 -0500

    [openai] Migrate openai projects to use snippets extraction

--- sdk/openai/openai/vitest.browser.config.ts
+++ sdk/openai/openai/vitest.browser.config.ts
@@ -3,14 +3,21 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname((import.meta.url);
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      testTimeout: 500000,
+      hookTimeout: 25000,
+      fileParallelism: false,
+      globalSetup: [resolve(__dirname, "test/utils/setup.ts")],
+      setupFiles: [resolve(__dirname, "test/utils/logging.ts")],
       include: ["dist-test/browser/test/**/*.spec.js"],
-      testTimeout: 1200000,
-      hookTimeout: 1200000,
     },
   }),
 );
--- sdk/openai/openai/vitest.config.ts
+++ sdk/openai/openai/vitest.config.ts
@@ -3,13 +3,19 @@
 
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.shared.config.ts";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname((import.meta.url);
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      testTimeout: 1200000,
-      hookTimeout: 1200000,
+      testTimeout: 500000,
+      hookTimeout: 25000,
+      fileParallelism: false,
+      globalSetup: [resolve(__dirname, "test/utils/setup.ts")],
     },
   }),
 );
