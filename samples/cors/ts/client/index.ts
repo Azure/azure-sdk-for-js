a87dd1d47b0e575111243fc084dc66ed0ccd32c3
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-03-06 11:34:36 -0800

    [samples] update the CORS sample
    - server: remove unused dependencies
    - server: replace `ts-node` with `tsx`
    
    - client: remove dotenv and axios which depend on NodeJS
      - replace axios with fetch API
      - replace env variable with HTML input
    - client: upgrade parcel bundler to v2

--- samples/cors/ts/client/index.ts
+++ samples/cors/ts/client/index.ts
@@ -1,24 +1,25 @@
-import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-if (!process.env.AZURE_API_NAME) {
-  throw new Error("Missing AZURE_API_NAME in the local .env file");
-}
-
 let secretDisplay: HTMLSpanElement = document.getElementById("secret-display") as HTMLSpanElement;
 
 let subscriptionKey: HTMLInputElement = document.getElementById(
   "subscription-key"
 ) as HTMLInputElement;
 
+let azureApiName: HTMLInputElement = document.getElementById(
+  "api-name"
+) as HTMLInputElement;
+
+
 const getSecretFromServer = () => {
   secretDisplay.textContent = "";
-  axios
-    .get("http://localhost:4000/api/secret")
-    .then(({ data }) => {
-      secretDisplay.textContent = data.value;
+  fetch("http://localhost:4000/api/secret")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((json) => {
+      secretDisplay.textContent = json;
     })
     .catch((err) => {
       console.log("error fetching secret from the server", err);
@@ -31,14 +32,19 @@ const getSecretFromApiManagement = () => {
   if (!key) {
     alert("Azure API Management subscription key missing.");
   } else {
-    axios
-      .get(`https://${process.env.AZURE_API_NAME}.azure-api.net/get-secret`, {
+    fetch(`https://${azureApiName}.azure-api.net/get-secret`, {
         headers: {
           "Ocp-Apim-Subscription-Key": key
         }
       })
-      .then(({ data }) => {
-        secretDisplay.textContent = data.value;
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        secretDisplay.textContent = json;
       })
       .catch((err) => console.log("error fetching secret from Azure API Management", err));
   }
