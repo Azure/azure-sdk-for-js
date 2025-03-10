b1f2df5ca18b9ae42eca99e7ebeccec90a1191e3
Matthew Podwysocki <matthewp@microsoft.com>
2025-03-10 15:58:02 -0400

    Migration: Apply codemod: "replaceTestUtils"

--- sdk/storage/storage-file-share/test/directoryclient.spec.ts
+++ sdk/storage/storage-file-share/test/directoryclient.spec.ts
@@ -14,7 +14,7 @@ import { ShareDirectoryClient, FileSystemAttributes } from "../src/index.js";
 import { Recorder, isLiveMode } from "@azure-tools/test-recorder";
 import type { DirectoryCreateResponse } from "../src/generatedModels.js";
 import { truncatedISO8061Date } from "../src/utils/utils.common.js";
-import { getYieldedValue } from "@azure-tools/test-utils";
+import { getYieldedValue } from "@azure-tools/test-utils-vitest";
 import { isBrowser } from "@azure/core-util";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
 import { toSupportTracing } from "@azure-tools/test-utils-vitest";
