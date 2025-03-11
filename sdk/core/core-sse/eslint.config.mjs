c43a03e3ad59672f05f1c42474896d64e2f80467
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-03-11 15:19:25 -0700

    formatting

--- sdk/core/core-sse/eslint.config.mjs
+++ sdk/core/core-sse/eslint.config.mjs
@@ -2,11 +2,7 @@ import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default azsdkEslint.config([
   {
-    files: [
-      "src/models.ts",
-      "src/sse.ts",
-      "src/utils.ts",
-    ],
+    files: ["src/models.ts", "src/sse.ts", "src/utils.ts"],
     rules: {
       "n/no-unsupported-features/node-builtins": "off",
     },
