ccdc7a331663d3830c3c064e1d19ff1b49c8079a
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-03-13 16:19:21 +0800

    update

--- sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
+++ sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
@@ -82,7 +82,7 @@ export class ContainerServiceClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-containerservice/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-containerservice/22.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
