c78ae98aa321965438de6a8488ae254b5b5afb78
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-03-14 16:12:01 +0800

    Migration: Update test config

--- sdk/edgezones/arm-edgezones/vitest.config.ts
+++ sdk/edgezones/arm-edgezones/vitest.config.ts
@@ -1,34 +1,16 @@
+
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { defineConfig } from "vitest/config";
-import { relativeRecordingsPath } from "@azure-tools/test-recorder";
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "../../../vitest.shared.config.ts";
 
-export default defineConfig({
-  test: {
-    reporters: ["basic", "junit"],
-    outputFile: {
-      junit: "test-results.browser.xml",
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      testTimeout: 1200000,
+      hookTimeout: 1200000,
     },
-    fakeTimers: {
-      toFake: ["setTimeout", "Date"],
-    },
-    watch: false,
-    include: ["test/**/*.spec.ts"],
-    exclude: ["test/**/browser/*.spec.ts"],
-    coverage: {
-      include: ["src/**/*.ts"],
-      exclude: [
-        "src/**/*-browser.mts",
-        "src/**/*-react-native.mts",
-        "vitest*.config.ts",
-        "samples-dev/**/*.ts",
-      ],
-      provider: "istanbul",
-      reporter: ["text", "json", "html"],
-      reportsDirectory: "coverage",
-    },
-    testTimeout: 1200000,
-    hookTimeout: 1200000,
-  },
-});
+  }),
+);
