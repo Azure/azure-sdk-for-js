17fc7fac9d586ff3bd3060ccada2a4375ab8091c
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-03-18 11:28:22 +0800

    update

--- sdk/databox/arm-databox/samples/v5/javascript/jobsListByResourceGroupSample.js
+++ sdk/databox/arm-databox/samples/v5/javascript/jobsListByResourceGroupSample.js
@@ -10,13 +10,13 @@
 // Licensed under the MIT License.
 const { DataBoxManagementClient } = require("@azure/arm-databox");
 const { DefaultAzureCredential } = require("@azure/identity");
-require("dotenv").config();
+require("dotenv/config");
 
 /**
  * This sample demonstrates how to Lists all the jobs available under the given resource group.
  *
  * @summary Lists all the jobs available under the given resource group.
- * x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2022-12-01/examples/JobsListByResourceGroup.json
+ * x-ms-original-file: specification/databox/resource-manager/Microsoft.DataBox/stable/2025-02-01/examples/JobsListByResourceGroup.json
  */
 async function jobsListByResourceGroup() {
   const subscriptionId = process.env["DATABOX_SUBSCRIPTION_ID"] || "YourSubscriptionId";
@@ -24,14 +24,14 @@ async function jobsListByResourceGroup() {
   const credential = new DefaultAzureCredential();
   const client = new DataBoxManagementClient(credential, subscriptionId);
   const resArray = new Array();
-  for await (let item of client.jobs.listByResourceGroup(resourceGroupName)) {
+  for await (const item of client.jobs.listByResourceGroup(resourceGroupName)) {
     resArray.push(item);
   }
   console.log(resArray);
 }
 
 async function main() {
-  jobsListByResourceGroup();
+  await jobsListByResourceGroup();
 }
 
 main().catch(console.error);
--- sdk/databox/arm-databox/src/dataBoxManagementClient.ts
+++ sdk/databox/arm-databox/src/dataBoxManagementClient.ts
@@ -56,7 +56,7 @@ export class DataBoxManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-databox/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-databox/5.1.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
