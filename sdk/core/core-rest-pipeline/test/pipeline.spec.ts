Azure/azure-sdk-for-js

commit 4967916d8fbadad8a209a20783142573c053ddf6

[core] Support NodeJS stream cancellation (#33341)
### Packages impacted by this PR
@azure/core-rest-pipeline, @azure-rest/core-client, and @azure/core-sse

### Issues associated with this PR
https://github.com/Azure/azure-sdk-for-js/issues/33334

### Describe the problem that is addressed by this PR

The type of NodeJS streams used in our core doesn't support cancellation
even though the actual runtime object does support it. For context, this
type is being used by NodeJS to type ["old style streams" that existed
before NodeJS
v0.10](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/3a17ff00600a50db103774cfb818685c5677e815/types/node/stream.d.ts#L449),
therefore, it doesn't have the `destroy` method.

### What are the possible designs available to address the problem? If
there are more than one possible design, why was the one in this PR
chosen?

The type used in this PR is the minimalist one. Another option is to use
the full `Readable` class type but we try to not use NodeJS types
directly in our APIs.

### Are there test cases added in this PR? _(If not, why?)_

No, the changes are types only

### Provide a list of related PRs _(if any)_

N/A

### Command used to generate this PR:**_(Applicable only to SDK release
request PRs)_

### Checklists
- [x] Added impacted package name to the issue description
- [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
create an Issue in the
[Autorest/typescript](https://github.com/Azure/autorest.typescript)
repository and link it here)_
- [x] Added a changelog (if necessary)

sdk/core/core-client-rest/src/common.ts
@@ -4,6 +4,7 @@
 import type {
   HttpClient,
   LogPolicyOptions,
+  NodeJSReadableStream,
   Pipeline,
   PipelineOptions,
   PipelinePolicy,
@@ -224,7 +225,7 @@ export type HttpNodeStreamResponse = HttpResponse & {
   /**
    * Streamable body
    */
-  body?: NodeJS.ReadableStream;
+  body?: NodeJSReadableStream;
 };
 
 /**

sdk/core/core-rest-pipeline/src/index.ts
@@ -30,6 +30,7 @@ export type {
   SendRequest,
   TlsSettings,
   TransferProgressEvent,
+  NodeJSReadableStream,
 } from "./interfaces.js";
 export {
   type AddPolicyOptions as AddPipelineOptions,

sdk/core/core-rest-pipeline/src/interfaces.ts
@@ -49,6 +49,18 @@ export interface HttpHeaders extends Iterable<[string, string]> {
 }
 
 /**
+ * A Node.js Readable stream that also has a `destroy` method.
+ */
+export interface NodeJSReadableStream extends NodeJS.ReadableStream {
+  /**
+   * Destroy the stream. Optionally emit an 'error' event, and emit a
+   * 'close' event (unless emitClose is set to false). After this call,
+   * internal resources will be released.
+   */
+  destroy(error?: Error): void;
+}
+
+/**
  * A part of the request body in a multipart request.
  */
 export interface BodyPart {
@@ -286,7 +298,7 @@ export interface PipelineResponse {
    * The response body as a node.js Readable stream.
    * Always undefined in the browser.
    */
-  readableStreamBody?: NodeJS.ReadableStream;
+  readableStreamBody?: NodeJSReadableStream;
 }
 
 /**

sdk/core/core-rest-pipeline/src/nodeHttpClient.ts
@@ -9,6 +9,7 @@ import { AbortError } from "@azure/abort-controller";
 import type {
   HttpClient,
   HttpHeaders,
+  NodeJSReadableStream,
   PipelineRequest,
   PipelineResponse,
   RequestBodyType,
@@ -23,11 +24,11 @@ import { Sanitizer } from "./util/sanitizer.js";
 
 const DEFAULT_TLS_SETTINGS = {};
 
-function isReadableStream(body: any): body is NodeJS.ReadableStream {
+function isReadableStream(body: any): body is NodeJSReadableStream {
   return body && typeof body.pipe === "function";
 }
 
-function isStreamComplete(stream: NodeJS.ReadableStream): Promise<void> {
+function isStreamComplete(stream: NodeJSReadableStream): Promise<void> {
   if (stream.readable === false) {
     return Promise.resolve();
   }
@@ -121,7 +122,7 @@ class NodeHttpClient implements HttpClient {
       }
     }
 
-    let responseStream: NodeJS.ReadableStream | undefined;
+    let responseStream: NodeJSReadableStream | undefined;
     try {
       if (body && request.onUploadProgress) {
         const onUploadProgress = request.onUploadProgress;
@@ -332,7 +333,7 @@ function getResponseHeaders(res: IncomingMessage): HttpHeaders {
 function getDecodedResponseStream(
   stream: IncomingMessage,
   headers: HttpHeaders,
-): NodeJS.ReadableStream {
+): NodeJSReadableStream {
   const contentEncoding = headers.get("Content-Encoding");
   if (contentEncoding === "gzip") {
     const unzip = zlib.createGunzip();
@@ -347,7 +348,7 @@ function getDecodedResponseStream(
   return stream;
 }
 
-function streamToText(stream: NodeJS.ReadableStream): Promise<string> {
+function streamToText(stream: NodeJSReadableStream): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     const buffer: Buffer[] = [];
 

sdk/core/core-sse/src/index.ts
@@ -2,4 +2,4 @@
 // Licensed under the MIT License.
 
 export { createSseStream } from "./sse.js";
-export { EventMessage, EventMessageStream } from "./models.js";
+export { EventMessage, EventMessageStream, NodeJSReadableStream } from "./models.js";

sdk/core/core-sse/src/models.ts
@@ -25,3 +25,15 @@ export interface EventMessageStream
     AsyncIterable<EventMessage> {}
 
 export type PartialSome<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+/**
+ * A Node.js Readable stream that also has a `destroy` method.
+ */
+export interface NodeJSReadableStream extends NodeJS.ReadableStream {
+  /**
+   * Destroy the stream. Optionally emit an 'error' event, and emit a
+   * 'close' event (unless emitClose is set to false). After this call,
+   * internal resources will be released.
+   */
+  destroy(error?: Error): void;
+}

sdk/core/core-sse/src/sse.ts
@@ -2,7 +2,12 @@
 // Licensed under the MIT License.
 
 import type { IncomingMessage } from "node:http";
-import type { EventMessage, EventMessageStream, PartialSome } from "./models.js";
+import type {
+  EventMessage,
+  EventMessageStream,
+  NodeJSReadableStream,
+  PartialSome,
+} from "./models.js";
 import { createStream, ensureAsyncIterable } from "./utils.js";
 
 enum ControlChars {
@@ -15,17 +20,23 @@ enum ControlChars {
 /**
  * Processes a response stream into a stream of events.
  * @param chunkStream - A stream of Uint8Array chunks
- * @returns An async iterable of EventMessage objects
+ * @returns A stream of EventMessage objects
  */
 export function createSseStream(chunkStream: ReadableStream<Uint8Array>): EventMessageStream;
 /**
  * Processes a response stream into a stream of events.
- * @param chunkStream - An async iterable of Uint8Array chunks
- * @returns An async iterable of EventMessage objects
+ * @param chunkStream - A NodeJS HTTP response
+ * @returns A stream of EventMessage objects
  */
 export function createSseStream(chunkStream: IncomingMessage): EventMessageStream;
+/**
+ * Processes a response stream into a stream of events.
+ * @param chunkStream - A NodeJS Readable stream
+ * @returns A stream of EventMessage objects
+ */
+export function createSseStream(chunkStream: NodeJSReadableStream): EventMessageStream;
 export function createSseStream(
-  chunkStream: IncomingMessage | ReadableStream<Uint8Array>,
+  chunkStream: IncomingMessage | NodeJSReadableStream | ReadableStream<Uint8Array>,
 ): EventMessageStream {
   const { cancel, iterable } = ensureAsyncIterable(chunkStream);
   const asyncIter = toMessage(toLine(iterable));

sdk/core/core-sse/src/utils.ts
@@ -2,10 +2,7 @@
 // Licensed under the MIT License.
 
 import type { IncomingMessage } from "node:http";
-
-type Nullable<T, K extends keyof T> = {
-  [P in K]: T[P] | null;
-} & Omit<T, K>;
+import type { NodeJSReadableStream } from "./models.js";
 
 export function createStream<T>(
   asyncIter: AsyncIterableIterator<T>,
@@ -65,7 +62,7 @@ function iteratorToStream<T>(
 }
 
 export function ensureAsyncIterable(
-  stream: Nullable<IncomingMessage, "socket"> | ReadableStream<Uint8Array>,
+  stream: IncomingMessage | NodeJSReadableStream | ReadableStream<Uint8Array>,
 ): {
   cancel(): Promise<void>;
   iterable: AsyncIterable<Uint8Array>;
@@ -79,11 +76,11 @@ export function ensureAsyncIterable(
   } else {
     return {
       cancel: async () => {
-        /**
-         * socket is set to null when the stream has been consumed
-         * so we need to make sure to guard against nullability.
-         */
-        stream.socket?.end();
+        if ("socket" in stream) {
+          stream.socket.end();
+        } else {
+          stream.destroy();
+        }
       },
       iterable: stream as AsyncIterable<Uint8Array>,
     };

commit c980b1c56825916170b6674c66ddb47ea8cbfb3c

[core-rest-pipeline] add a test for pipeline response shape (#33402)
To guard against breaking changes similar to what was introduced in
https://github.com/Azure/azure-sdk-for-js/pull/33341

sdk/core/core-rest-pipeline/test/pipeline.spec.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { describe, it, assert } from "vitest";
+import { describe, it, assert, expectTypeOf } from "vitest";
+import type { HttpClient, HttpHeaders, PipelinePolicy, PipelineResponse } from "../src/index.js";
 import {
-  type HttpClient,
-  type PipelinePolicy,
+  createDefaultHttpClient,
   createEmptyPipeline,
   createHttpHeaders,
   createPipelineFromOptions,
@@ -438,5 +438,29 @@ describe("HttpsPipeline", function () {
     pipeline.addPolicy(testPolicy2, { phase: "Sign" });
     const policies = pipeline.getOrderedPolicies();
     assert.deepStrictEqual(policies, [testPolicy, testPolicy2]);
+  });
+
+  it("assert pipeline response shape", async function () {
+    const p = createEmptyPipeline();
+    p.addPolicy({
+      name: "test",
+      sendRequest: async (request) => {
+        return {
+          headers: {} as HttpHeaders,
+          status: 0 as number,
+          request,
+          bodyAsText: "" as string,
+          readableStreamBody: {} as NodeJS.ReadableStream,
+          blobBody: {} as Promise<Blob>,
+          browserStreamBody: {} as ReadableStream<Uint8Array<ArrayBufferLike>>,
+        };
+      },
+    });
+    expectTypeOf(
+      await p.sendRequest(
+        createDefaultHttpClient(),
+        createPipelineRequest({ url: "https://example.com" }),
+      ),
+    ).toEqualTypeOf<PipelineResponse>();
   });
 });