effe8d669c09b476a7b8e9e9379421382a612871
Mary Gao <yanmeigao1210@gmail.com>
2025-03-19 21:49:31 +0800

    Format code

--- sdk/synapse/synapse-artifacts/src/lroImpl.ts
+++ sdk/synapse/synapse-artifacts/src/lroImpl.ts
@@ -6,7 +6,6 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-
 import type { AbortSignalLike } from "@azure/abort-controller";
 import type { LongRunningOperation, LroResponse } from "@azure/core-lro";
 
