d698b798924defc16ecef87ec30fa943c1f5e665
Mitesh Shah <58204159+mitsha-microsoft@users.noreply.github.com>
2025-03-19 11:40:30 +0530

    Fixed formatting

--- sdk/loadtesting/load-testing-rest/test/public/testAdministration.spec.ts
+++ sdk/loadtesting/load-testing-rest/test/public/testAdministration.spec.ts
@@ -22,9 +22,6 @@ describe("Test Administration Operations", () => {
 
   beforeEach(async (ctx) => {
     recorder = await createRecorder(ctx);
-    if (!isNodeLike) {
-      ctx.skip();
-    }
     client = createClient(recorder);
   });
 
@@ -172,8 +169,8 @@ describe("Test Administration Operations", () => {
 describe("Test Profile Administration Operations", () => {
   let recorder: Recorder;
   let client: AzureLoadTestingClient;
-  const testId = "sample-sdk-testpr-202503183";
-  const testProfileId = "sample-sdk-testprofile-202503187";
+  const testId = "sample-sdk-testpr-202503184";
+  const testProfileId = "sample-sdk-testprofile-202503188";
 
   beforeEach(async (ctx) => {
     recorder = await createRecorder(ctx);
