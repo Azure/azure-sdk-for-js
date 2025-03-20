b12ebefed6a64450095eb89bc2c525b3575bde23
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-03-20 11:06:46 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/hybridkubernetes/arm-hybridkubernetes/src/operations/connectedClusterOperations.ts
+++ sdk/hybridkubernetes/arm-hybridkubernetes/src/operations/connectedClusterOperations.ts
@@ -7,18 +7,18 @@
  */
 
 import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
-import { setContinuationToken } from "../pagingHelper";
-import { ConnectedClusterOperations } from "../operationsInterfaces";
+import { setContinuationToken } from "../pagingHelper.js";
+import { ConnectedClusterOperations } from "../operationsInterfaces/index.js";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers";
-import * as Parameters from "../models/parameters";
-import { ConnectedKubernetesClient } from "../connectedKubernetesClient";
+import * as Mappers from "../models/mappers.js";
+import * as Parameters from "../models/parameters.js";
+import { ConnectedKubernetesClient } from "../connectedKubernetesClient.js";
 import {
   SimplePollerLike,
   OperationState,
   createHttpPoller,
 } from "@azure/core-lro";
-import { createLroSpec } from "../lroImpl";
+import { createLroSpec } from "../lroImpl.js";
 import {
   ConnectedCluster,
   ConnectedClusterListByResourceGroupNextOptionalParams,
@@ -40,7 +40,7 @@ import {
   ConnectedClusterListClusterUserCredentialResponse,
   ConnectedClusterListByResourceGroupNextResponse,
   ConnectedClusterListBySubscriptionNextResponse,
-} from "../models";
+} from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Class containing ConnectedClusterOperations operations. */
