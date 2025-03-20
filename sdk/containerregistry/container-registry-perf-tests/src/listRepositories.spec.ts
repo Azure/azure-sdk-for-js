8f50fe296fcd7f3a301807aa16abe804655514d5
Matthew Podwysocki <matthewp@microsoft.com>
2025-03-20 13:39:11 -0400

    [containerregistry] Move perf tests to service level directory (#33467)
    ### Packages impacted by this PR
    
    - @azure-tests/perf-container-registry
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/33456
    
    ### Describe the problem that is addressed by this PR
    
    Moves the perf tests to the service level for pnpm migration
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/containerregistry/container-registry-perf-tests/src/listRepositories.spec.ts
+++ sdk/containerregistry/container-registry-perf-tests/src/listRepositories.spec.ts
@@ -1,20 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { PerfOptionDictionary } from "@azure-tools/test-perf";
-import { ContainerRegistryTest, ContainerRegistryTestOptions } from "./client.spec.js";
+import type { PerfOptionDictionary } from "@azure-tools/test-perf";
+import type { ContainerRegistryTestOptions } from "./client.spec.js";
+import { ContainerRegistryTest } from "./client.spec.js";
 
 export class RepositoryListTest extends ContainerRegistryTest<ContainerRegistryTestOptions> {
   public options: PerfOptionDictionary<ContainerRegistryTestOptions> = {};
-  constructor() {
-    super();
-  }
 
   async run(): Promise<void> {
     const listIterator = this.client.listRepositoryNames();
 
-    // eslint-disable-next-line no-empty
     for await (const _repositoryName of listIterator) {
+      // Do nothing
     }
   }
 }
