68b0f06b344194e320a259ff5349e744af75600d
ZachhK <112514790+ZachhK@users.noreply.github.com>
2025-03-21 10:54:14 -0700

    Samples fixes (#33474)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    - fixing build errors in typescript samples
    - fixing files.ts error
    - updating dotenv config import
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Zachary King <zacharyking@microsoft.com>

--- sdk/ai/ai-projects/samples-dev/agents/batchVectorStoreWithFiles.ts
+++ sdk/ai/ai-projects/samples-dev/agents/batchVectorStoreWithFiles.ts
@@ -10,9 +10,8 @@
 
 import { AIProjectsClient } from "@azure/ai-projects";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
 import { Readable } from "stream";
-dotenv.config();
+import "dotenv/config";
 
 const connectionString =
   process.env["AZURE_AI_PROJECTS_CONNECTION_STRING"] || "<project connection string>";
