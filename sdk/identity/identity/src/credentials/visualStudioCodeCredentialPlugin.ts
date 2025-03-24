1cfe0e446123fd8f0f43cc02be11ccc095b6f9d2
Minh-Anh Phan <minhanhphan@microsoft.com>
2025-03-24 18:01:56 +0000

    update based on feedback

--- sdk/identity/identity/src/credentials/visualStudioCodeCredentialPlugin.ts
+++ sdk/identity/identity/src/credentials/visualStudioCodeCredentialPlugin.ts
@@ -10,6 +10,6 @@
  * @deprecated This credential is deprecated because the VS Code Azure Account extension on which this credential
  * relies has been deprecated. Users should use other dev-time credentials, such as {@link AzureCliCredential},
  * {@link AzureDeveloperCliCredential}, or {@link AzurePowerShellCredential} for their
- * local development needs. See Azure account extension deprecation notice [here](https://github.com/microsoft/vscode-azure-account/issues/964).
+ * local development needs. See Azure Account extension deprecation notice [here](https://github.com/microsoft/vscode-azure-account/issues/964).
  */
 export type VSCodeCredentialFinder = () => Promise<Array<{ account: string; password: string }>>;
