15f91e4f76b2ced48d43218eb14c1c907b874442
Andrea Beggiato <andrea.beggiato@geekcups.com>
2025-03-30 23:23:24 +0200

    Update sdk/identity/identity/src/credentials/environmentCredential.ts
    Co-authored-by: KarishmaGhiya <kghiya8@gmail.com>

--- sdk/identity/identity/src/credentials/environmentCredential.ts
+++ sdk/identity/identity/src/credentials/environmentCredential.ts
@@ -140,6 +140,7 @@ export class EnvironmentCredential implements TokenCredential {
       );
     }
 
+if(!this._credential)
     throw new CredentialUnavailableError(
       `${credentialName} is unavailable. No underlying credential could be used. To troubleshoot, visit https://aka.ms/azsdk/js/identity/environmentcredential/troubleshoot.`,
     );
