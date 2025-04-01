2cfcc306ddb1fb0987818b3ce9e65fbb9f4ab85f
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-04-01 14:52:45 -0700

    formatting

--- eng/tools/eng-package-utils/getPackagePath.js
+++ eng/tools/eng-package-utils/getPackagePath.js
@@ -4,30 +4,32 @@ let argv = require("yargs")
       type: "string",
       describe:
         "name of the artifact to be set (e.g. azure-keyvault-secrets), will be translated to @azure/(package) format",
-      demandOption: true
+      demandOption: true,
     },
     "repo-root": {
       type: "string",
       default: "../../../",
       describe: "root of the repository (e.g. ../../../)",
-      demandOption: true
-    }
+      demandOption: true,
+    },
   })
   .help().argv;
 
 const path = require("path");
-const {findPackages} = require('@pnpm/fs.find-packages')
+const { findPackages } = require("@pnpm/fs.find-packages");
 
 async function main(argv) {
   const packageName = argv["package-name"];
   const repoRoot = argv["repo-root"];
 
-  const pkgs = (await findPackages(repoRoot, {
-    patterns: ["sdk/*/*", "common/tools/*"]
-  })).filter(pkg => pkg.manifest.name === packageName);
+  const pkgs = (
+    await findPackages(repoRoot, {
+      patterns: ["sdk/*/*", "common/tools/*"],
+    })
+  ).filter((pkg) => pkg.manifest.name === packageName);
 
   const targetPackagePath = pkgs[0].rootDirRealPath;
-  
+
   console.log(`##vso[task.setvariable variable=PackagePath]${targetPackagePath}`);
   console.log(`Emitted variable "PackagePath" with content: ${targetPackagePath}`);
 }
