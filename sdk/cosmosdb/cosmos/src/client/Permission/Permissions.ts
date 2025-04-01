cff8f6529e2bb4b8ef75a9548ceac06e0473378c
Matthew Podwysocki <matthewp@microsoft.com>
2025-04-01 13:18:55 -0400

    [cosmos] Migrate @azure/cosmos to use snippets extraction (#33145)
    ### Packages impacted by this PR
    
    - @azure/arm-cosmos
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/32416
    
    ### Describe the problem that is addressed by this PR
    
    Updates all projects under `cosmos` to use snippets extraction.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Aman <aman@Amans-MacBook-Pro.local>
    Co-authored-by: Aditishree . <adlnu@microsoft.com>
    Co-authored-by: Ujjwal Soni <soniujjwal@microsoft.com>

--- sdk/cosmosdb/cosmos/src/client/Permission/Permissions.ts
+++ sdk/cosmosdb/cosmos/src/client/Permission/Permissions.ts
@@ -37,6 +37,24 @@ export class Permissions {
   /**
    * Query all permissions.
    * @param query - Query configuration for the operation. See {@link SqlQuerySpec} for more info on how to configure a query.
+   * @example Query permission with id.
+   * ```ts snippet:PermissionsQuery
+   * import { CosmosClient, SqlQuerySpec } from "@azure/cosmos";
+   *
+   * const endpoint = "https://your-account.documents.azure.com";
+   * const key = "<database account masterkey>";
+   * const client = new CosmosClient({ endpoint, key });
+   * const { database } = await client.databases.createIfNotExists({ id: "Test Database" });
+   *
+   * const querySpec: SqlQuerySpec = {
+   *   query: `SELECT * FROM root r WHERE r.id = @permission`,
+   *   parameters: [{ name: "@permission", value: "<permission-id>" }],
+   * };
+   * const { resources: permisssion } = await database
+   *   .user("<user-id>")
+   *   .permissions.query(querySpec)
+   *   .fetchAll();
+   * ```
    */
   public query<T>(query: SqlQuerySpec, options?: FeedOptions): QueryIterator<T>;
   public query<T>(query: SqlQuerySpec, options?: FeedOptions): QueryIterator<T> {
@@ -59,8 +77,16 @@ export class Permissions {
   /**
    * Read all permissions.
    * @example Read all permissions to array.
-   * ```typescript
-   * const {body: permissionList} = await user.permissions.readAll().fetchAll();
+   * ```ts snippet:PermissionsReadAll
+   * import { CosmosClient } from "@azure/cosmos";
+   *
+   * const endpoint = "https://your-account.documents.azure.com";
+   * const key = "<database account masterkey>";
+   * const client = new CosmosClient({ endpoint, key });
+   *
+   * const { database } = await client.databases.createIfNotExists({ id: "Test Database" });
+   *
+   * const { resources: permissionList } = await database.user("user1").permissions.readAll().fetchAll();
    * ```
    */
   public readAll(options?: FeedOptions): QueryIterator<PermissionDefinition & Resource> {
@@ -73,6 +99,23 @@ export class Permissions {
    * A permission represents a per-User Permission to access a specific resource
    * e.g. Item or Container.
    * @param body - Represents the body of the permission.
+   * @example
+   * ```ts snippet:PermissionsCreate
+   * import { CosmosClient, PermissionDefinition, PermissionMode } from "@azure/cosmos";
+   *
+   * const endpoint = "https://your-account.documents.azure.com";
+   * const key = "<database account masterkey>";
+   * const client = new CosmosClient({ endpoint, key });
+   * const { database } = await client.databases.createIfNotExists({ id: "Test Database" });
+   * const { user } = await database.users.create({ id: "<user-id>" });
+   *
+   * const permissionDefinition: PermissionDefinition = {
+   *   id: "<permission-id>",
+   *   permissionMode: PermissionMode.Read,
+   *   resource: "<resource-url>",
+   * };
+   * await user.permissions.create(permissionDefinition);
+   * ```
    */
   public async create(
     body: PermissionDefinition,
@@ -108,9 +151,25 @@ export class Permissions {
 
   /**
    * Upsert a permission.
-   *
    * A permission represents a per-User Permission to access a
    * specific resource e.g. Item or Container.
+   * @example
+   * ```ts snippet:PermissionsUpsert
+   * import { CosmosClient, PermissionDefinition, PermissionMode } from "@azure/cosmos";
+   *
+   * const endpoint = "https://your-account.documents.azure.com";
+   * const key = "<database account masterkey>";
+   * const client = new CosmosClient({ endpoint, key });
+   * const { database } = await client.databases.createIfNotExists({ id: "Test Database" });
+   * const user = database.user("<user-id>");
+   * const permissionDefinitionToUpsert: PermissionDefinition = {
+   *   id: "<permission-id>",
+   *   permissionMode: PermissionMode.Read,
+   *   resource: "<resource-url>",
+   * };
+   *
+   * await user.permissions.upsert(permissionDefinitionToUpsert);
+   * ```
    */
   public async upsert(
     body: PermissionDefinition,
