fd2eff46fcf2c365b702a91360b4d616876eab53
Bo Gou <bogou@microsoft.com>
2025-04-07 16:36:26 -0700

    skip the recorder right now

--- sdk/ai/ai-projects-1dp/test/public/connections/connections.spec.ts
+++ sdk/ai/ai-projects-1dp/test/public/connections/connections.spec.ts
@@ -1,24 +1,24 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import type { Recorder, VitestTestContext } from "@azure-tools/test-recorder";
+import type { VitestTestContext } from "@azure-tools/test-recorder";
 import type { AIProjectClient, ConnectionsOperations } from "../../../src/index.js";
-import { createRecorder, createProjectsClient } from "../utils/createClient.js";
+import { createProjectsClient } from "../utils/createClient.js";
 import { assert, beforeEach, afterEach, it, describe } from "vitest";
 
 describe("Agents - assistants", () => {
-  let recorder: Recorder;
+  // let recorder: Recorder;
   let projectsClient: AIProjectClient;
   let connections: ConnectionsOperations;
 
-  beforeEach(async function (context: VitestTestContext) {
-    recorder = await createRecorder(context);
-    projectsClient = createProjectsClient(recorder);
+  beforeEach(async function (_context: VitestTestContext) {
+    // recorder = await createRecorder(context);
+    projectsClient = createProjectsClient();
     connections = projectsClient.connections;
   });
 
   afterEach(async function () {
-    await recorder.stop();
+    // await recorder.stop();
   });
 
   it("client and connection operations are accessible", async function () {
