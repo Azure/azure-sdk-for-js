960a034941663943042037c7be614dfbd924f47a
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-04-08 10:00:23 +0800

    update

--- sdk/search/arm-search/src/searchManagementClient.ts
+++ sdk/search/arm-search/src/searchManagementClient.ts
@@ -89,7 +89,7 @@ export class SearchManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-search/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-search/3.3.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
