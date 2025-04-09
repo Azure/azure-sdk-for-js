59bcec60ad77ea481c45c2f3aaaf1a29d00c941c
Ujjwal Soni <soniujjwal@microsoft.com>
2025-04-09 14:47:37 +0530

    minor change

--- sdk/cosmosdb/cosmos/test/public/integration/session.spec.ts
+++ sdk/cosmosdb/cosmos/test/public/integration/session.spec.ts
@@ -75,7 +75,7 @@ describe("New session token", () => {
   });
 });
 
-describe.skip("Integrated Cache Staleness", async () => {
+describe("Integrated Cache Staleness", async () => {
   beforeEach(async () => {
     await removeAllDatabases();
   });
