f3c2909147fbb4d394e401c04b011046bddfb419
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-04-10 14:34:14 +0800

    update

--- sdk/search/azure-search-documents-rest/src/documentsClient.ts
+++ sdk/search/azure-search-documents-rest/src/documentsClient.ts
@@ -25,7 +25,7 @@ export default function createClient(
   { apiVersion = "2025-03-01-preview", ...options }: DocumentsClientOptions = {},
 ): DocumentsClient {
   const endpointUrl = options.endpoint ?? options.baseUrl ?? `${endpointParam}`;
-  const userAgentInfo = `azsdk-js-azure-search-documents-rest/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-@azure-rest/azure-search-documents-rest/1.0.0-beta.1`;
   const userAgentPrefix =
     options.userAgentOptions && options.userAgentOptions.userAgentPrefix
       ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
