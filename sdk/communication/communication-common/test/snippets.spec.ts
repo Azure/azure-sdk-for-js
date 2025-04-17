8961689927fc95b471ef3facfe7c22476f8766fa
Matthew Podwysocki <matthewp@microsoft.com>
2025-04-16 23:02:48 -0400

    [communication] Fix snippets extraction for communication projects (#33886)
    ### Packages impacted by this PR
    
    - @azure/communication-common
    - @azure-rest/communication-messages
    
    ### Issues associated with this PR
    
    - https://github.com/Azure/azure-sdk-for-js/issues/33880
    
    ### Describe the problem that is addressed by this PR
    
    Updates snippets to be compliant with our `update-snippets` check.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)

--- sdk/communication/communication-common/test/snippets.spec.ts
+++ sdk/communication/communication-common/test/snippets.spec.ts
@@ -1,7 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { AzureCommunicationTokenCredential } from "../src/index.js";
+import { InteractiveBrowserCredential } from "@azure/identity";
+import {
+  AzureCommunicationTokenCredential,
+  EntraCommunicationTokenCredentialOptions,
+} from "../src/index.js";
 import { setLogLevel } from "@azure/logger";
 import { describe, it } from "vitest";
 
@@ -47,6 +51,37 @@ describe("snippets", () => {
       token:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjM2MDB9.adM-ddBZZlQ1WlN3pdPBOF5G4Wh9iZpxNP_fSvpF4cWs",
     });
+  });
+
+  it("ReadmeSampleCredentialEntraUser", async () => {
+    function fetchTokenFromMyServerForUser(user: string): Promise<string> {
+      // Your custom implementation to fetch a token for the user
+      return Promise.resolve("some-unique-token-for-" + user);
+    }
+    // @ts-preserve-whitespace
+    const tokenCredential = new AzureCommunicationTokenCredential({
+      tokenRefresher: async () => fetchTokenFromMyServerForUser("bob@contoso.com"),
+      refreshProactively: true,
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjM2MDB9.adM-ddBZZlQ1WlN3pdPBOF5G4Wh9iZpxNP_fSvpF4cWs",
+    });
+  });
+
+  it("ReadmeSampleCredentialEntraUserTeamsPhoneExtensibility", async () => {
+    const options = {
+      tenantId: "<your-tenant-id>",
+      clientId: "<your-client-id>",
+      redirectUri: "<your-redirect-uri>",
+    };
+    const entraTokenCredential = new InteractiveBrowserCredential(options);
+    // @ts-preserve-whitespace
+    const entraTokenCredentialOptions: EntraCommunicationTokenCredentialOptions = {
+      resourceEndpoint: "https://<your-resource>.communication.azure.com",
+      tokenCredential: entraTokenCredential,
+      scopes: ["https://auth.msft.communication.azure.com/TeamsExtension.ManageCalls"],
+    };
+    // @ts-preserve-whitespace
+    const credential = new AzureCommunicationTokenCredential(entraTokenCredentialOptions);
   });
 
   it("SetLogLevel", async () => {
