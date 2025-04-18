b141446cbdcdd251744fb1c9969dce5dc6610170
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-04-18 15:39:26 +0800

    update

--- sdk/storage/arm-storage/src/storageManagementClient.ts
+++ sdk/storage/arm-storage/src/storageManagementClient.ts
@@ -100,7 +100,7 @@ export class StorageManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-storage/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-storage/18.5.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
