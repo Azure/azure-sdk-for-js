21cf9b4f57e9ab99d10b3721b8985d3f284517dd
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-04-18 15:17:04 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/storage/arm-storage/src/operationsInterfaces/fileServices.ts
+++ sdk/storage/arm-storage/src/operationsInterfaces/fileServices.ts
@@ -19,7 +19,7 @@ import {
   FileServicesGetServicePropertiesResponse,
   FileServicesGetServiceUsageOptionalParams,
   FileServicesGetServiceUsageResponse,
-} from "../models";
+} from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Interface representing a FileServices. */
