8a2909d8673ba626b851a3ec66b0b913cc6547e8
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-04-21 15:48:53 +0800

    Update redisenterprisecache_test.spec.ts

--- sdk/redisenterprise/arm-redisenterprisecache/test/redisenterprisecache_test.spec.ts
+++ sdk/redisenterprise/arm-redisenterprisecache/test/redisenterprisecache_test.spec.ts
@@ -48,7 +48,7 @@ describe("RedisEnterprise test", () => {
   afterEach(async () => {
     await recorder.stop();
   });
-
+  // Due to our recent policy, we don't block the SDK release if all the other resource types of this new version are available. so skip operation_list and add another list case instead
   it.skip("operations list test", async () => {
     const resArray = new Array();
     for await (let item of client.operations.list()) {
@@ -56,6 +56,7 @@ describe("RedisEnterprise test", () => {
     }
     assert.notEqual(resArray.length, 0);
   });
+
   it("redisEnterprise list test", async () => {
     const res = client.redisEnterprise.listByResourceGroup("myjstest");
     assert.ok(res);
