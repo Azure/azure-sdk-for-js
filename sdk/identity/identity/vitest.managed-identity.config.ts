a318ec71d7c88094229b03c9e7d93964c4e21363
Minh-Anh Phan <minhanhphan@microsoft.com>
2025-04-21 20:59:49 +0000

    override config

--- sdk/identity/identity/vitest.managed-identity.config.ts
+++ sdk/identity/identity/vitest.managed-identity.config.ts
@@ -4,16 +4,28 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.shared.config.ts";
 
-const config = mergeConfig(
+const mergedConfig = mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      include: ["test/integration/**/*.spec.ts"],
       hookTimeout: 500000,
       testTimeout: 500000,
     },
   }),
 );
+
+const config = {
+  ...mergedConfig,
+  test: {
+    ...mergedConfig.test,
+    include: ["test/integration/**/*.spec.ts"],
+        exclude: [
+          "test/**/browser/*.spec.ts",
+          "test/snippets.spec.ts",
+          "test/stress/**/*.ts",
+        ],
+  },
+};
 
 console.log("vitest.managed-identity.config.ts", config);
 export default config;
