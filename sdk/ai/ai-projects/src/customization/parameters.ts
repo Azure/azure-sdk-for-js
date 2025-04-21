471864c2247cb3498ec014e8d1f254ae574a8f84
ZachhK <112514790+ZachhK@users.noreply.github.com>
2025-04-21 17:19:15 -0400

    Beta 5 release of azure-ai-projects sdk (#33858)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [X] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: Zachary King <zacharyking@microsoft.com>

--- sdk/ai/ai-projects/src/customization/parameters.ts
+++ sdk/ai/ai-projects/src/customization/parameters.ts
@@ -73,6 +73,27 @@ export interface UpdateThreadBodyParam {
 export type UpdateThreadParameters = UpdateThreadBodyParam & RequestParameters;
 export type DeleteThreadParameters = RequestParameters;
 
+export interface ListThreadsQueryParamProperties {
+  /** A limit on the number of objects to be returned. Limit can range between 1 and 100, and the default is 20. */
+  limit?: number;
+  /**
+   * Sort order by the created_at timestamp of the objects. asc for ascending order and desc for descending order.
+   *
+   * Possible values: "asc", "desc"
+   */
+  order?: ListSortOrder;
+  /** A cursor for use in pagination. after is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with obj_foo, your subsequent call can include after=obj_foo in order to fetch the next page of the list. */
+  after?: string;
+  /** A cursor for use in pagination. before is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with obj_foo, your subsequent call can include before=obj_foo in order to fetch the previous page of the list. */
+  before?: string;
+}
+
+export interface ListThreadsQueryParam {
+  queryParameters?: ListThreadsQueryParamProperties;
+}
+
+export type ListThreadsParameters = ListThreadsQueryParam & RequestParameters;
+
 export interface CreateMessageBodyParam {
   body: ThreadMessageOptions;
 }
