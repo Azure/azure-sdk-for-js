d0654e65dfb62b98a4cdd4a63b9960af9c035480
Ubuntu <cloudtest@dd8df21cc000000.tkkhlgj2u51eln3mcb0lygmbye.dx.internal.cloudapp.net>
2025-04-22 03:15:29 +0000

    Migration: Update package.json, tsconfig.json, and api-extractor.json

---
+++ sdk/contoso/arm-contoso/src/classic/operations/index.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ContosoContext } from "../../api/contosoContext.js";
+import { list } from "../../api/operations/index.js";
+import { OperationsListOptionalParams } from "../../api/options.js";
+import { Operation } from "../../models/models.js";
+import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
+
+/** Interface representing a Operations operations. */
+export interface OperationsOperations {
+  /** List the operations for the provider */
+  list: (
+    options?: OperationsListOptionalParams,
+  ) => PagedAsyncIterableIterator<Operation>;
+}
+
+function _getOperations(context: ContosoContext) {
+  return {
+    list: (options?: OperationsListOptionalParams) => list(context, options),
+  };
+}
+
+export function _getOperationsOperations(
+  context: ContosoContext,
+): OperationsOperations {
+  return {
+    ..._getOperations(context),
+  };
+}
