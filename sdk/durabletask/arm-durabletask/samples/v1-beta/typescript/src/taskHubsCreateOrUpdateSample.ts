c90cb6d77d12708f06bd7a3af66bf79b5628e70a
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-04-22 16:35:35 +0800

    [mgmt] durable release (#33921)
    https://github.com/Azure/sdk-release-request/issues/6054

--- sdk/durabletask/arm-durabletask/samples/v1-beta/typescript/src/taskHubsCreateOrUpdateSample.ts
+++ sdk/durabletask/arm-durabletask/samples/v1-beta/typescript/src/taskHubsCreateOrUpdateSample.ts
@@ -8,7 +8,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * This sample demonstrates how to create or Update a Task Hub
  *
  * @summary create or Update a Task Hub
- * x-ms-original-file: 2024-10-01-preview/TaskHubs_CreateOrUpdate.json
+ * x-ms-original-file: 2025-04-01-preview/TaskHubs_CreateOrUpdate.json
  */
 async function taskHubsCreateOrUpdate(): Promise<void> {
   const credential = new DefaultAzureCredential();
