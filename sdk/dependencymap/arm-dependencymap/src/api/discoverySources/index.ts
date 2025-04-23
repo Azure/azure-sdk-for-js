4d0c0c9009c153db7a4575fef03fe9430e9394b4
Ubuntu <cloudtest@53f0f0eac000008.tkkhlgj2u51eln3mcb0lygmbye.dx.internal.cloudapp.net>
2025-04-23 06:54:09 +0000

    Migration: Update package.json, tsconfig.json, and api-extractor.json

---
+++ sdk/dependencymap/arm-dependencymap/src/api/discoverySources/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  listByMapsResource,
+  $delete,
+  update,
+  createOrUpdate,
+  get,
+} from "./operations.js";
+export {
+  DiscoverySourcesListByMapsResourceOptionalParams,
+  DiscoverySourcesDeleteOptionalParams,
+  DiscoverySourcesUpdateOptionalParams,
+  DiscoverySourcesCreateOrUpdateOptionalParams,
+  DiscoverySourcesGetOptionalParams,
+} from "./options.js";
