17ba8ca7117b096611b71f23582e163218a1a31f
azure-sdk <azuresdk@microsoft.com>
2025-04-24 09:27:00 +0000

    Post release automated changes for azure-arm-recoveryservices-siterecovery

--- sdk/recoveryservicessiterecovery/arm-recoveryservices-siterecovery/src/siteRecoveryManagementClient.ts
+++ sdk/recoveryservicessiterecovery/arm-recoveryservices-siterecovery/src/siteRecoveryManagementClient.ts
@@ -114,7 +114,7 @@ export class SiteRecoveryManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-recoveryservices-siterecovery/5.3.0`;
+    const packageDetails = `azsdk-js-arm-recoveryservices-siterecovery/5.3.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
