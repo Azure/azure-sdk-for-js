2a498f789dbeedd6e8b3c1b682ebb4e677622992
azure-sdk <azuresdk@microsoft.com>
2025-04-25 08:35:44 +0000

    Post release automated changes for azure-arm-recoveryservicesbackup

--- sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/recoveryServicesBackupClient.ts
+++ sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/recoveryServicesBackupClient.ts
@@ -174,7 +174,7 @@ export class RecoveryServicesBackupClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-recoveryservicesbackup/13.2.0`;
+    const packageDetails = `azsdk-js-arm-recoveryservicesbackup/13.2.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
