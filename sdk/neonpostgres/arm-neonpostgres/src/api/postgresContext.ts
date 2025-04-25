3121f446f1d1c6eef0f7fb6994e029ba2ca20b9d
azure-sdk <azuresdk@microsoft.com>
2025-04-25 08:45:13 +0000

    Post release automated changes for azure-arm-neonpostgres

--- sdk/neonpostgres/arm-neonpostgres/src/api/postgresContext.ts
+++ sdk/neonpostgres/arm-neonpostgres/src/api/postgresContext.ts
@@ -28,7 +28,7 @@ export function createPostgres(
 ): PostgresContext {
   const endpointUrl = options.endpoint ?? options.baseUrl ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-neonpostgres/1.0.0`;
+  const userAgentInfo = `azsdk-js-arm-neonpostgres/1.0.1`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
