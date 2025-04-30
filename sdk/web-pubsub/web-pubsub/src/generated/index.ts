a14470df846bb93891195dd653994c2189878f75
yzt <zityang@microsoft.com>
2025-04-30 13:07:04 +0800

    Update to version 1.2.0 with new API for listing group connections (#34006)
    - Updated package version to 1.2.0 in package.json and assets.json.
    - Added `WebPubSubGroup.listConnections` API to list connections in a
    group.
    - Introduced new types and interfaces for handling group connections.
    - Updated changelog to reflect the new feature.
    - Adjusted API version to 2024-12-01 across relevant files.
    - Added tests for the new list connections functionality.
    
    
    ### Packages impacted by this PR
    
    @azure/web-pubsub
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [x] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [x] Added a changelog (if necessary)

--- sdk/web-pubsub/web-pubsub/src/generated/index.ts
+++ sdk/web-pubsub/web-pubsub/src/generated/index.ts
@@ -6,6 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
+/// <reference lib="esnext.asynciterable" />
+export { getContinuationToken } from "./pagingHelper.js";
 export * from "./models/index.js";
 export { GeneratedClient } from "./generatedClient.js";
 export * from "./operationsInterfaces/index.js";
