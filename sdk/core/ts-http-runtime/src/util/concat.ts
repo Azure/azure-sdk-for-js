Azure/azure-sdk-for-js

commit 0bca843d978d692b07cf7441e04177a3df7e8037

[core] core-rest-pipeline: depend on ts-http-runtime (#33948)
### Packages impacted by this PR

- `@azure/core-rest-pipeline`
- `@typespec/ts-http-runtime`

### Issues associated with this PR

- #32698

### Describe the problem that is addressed by this PR

Updating core-rest-pipeline to depend on the unbranded package. This one
is a bit more involved than the changes to
[core-util](https://github.com/Azure/azure-sdk-for-js/pull/33721) and
[logger](https://github.com/Azure/azure-sdk-for-js/pull/33752), but
generally follows the same principles.

Specific callouts:
- `AbortSignalLike`. Since we decided that the unbranded package will
only support the native AbortSignal, there is a slight incompatability
between the unbranded and Azure `PipelineRequest`. I worked around this
by casting where necessary, but also added logic to wrap any
`AbortSignalLike` that is not in fact a native `AbortSignal` behind an
`AbortSignal` that reflects the state of the underlying AbortSignalLike.
This wrapping should keep the cast safe at runtime, in case any
unbranded policies or the unbranded HttpClient requires a native
AbortSignal for some reason. This wrapping is done in two places -- in a
new pipeline policy, to ensure that any subsequent policies receive a
native AbortSignal, and in the HttpClient sendRequest function, so that
any requests which aren't passed through a pipeline also get handled.
Would appreciate feedback on this approach.
- `RestError`. To ensure that the unbranded and Azure `RestError`
instances are compatible, the Azure RestError constructor now returns an
unbranded RestError (who knew you could return something in a
constructor? I didn't). This has the effect of making sure we don't have
two versions of RestError lying around at runtime which could cause some
weird behavior.
- User agent policy. I kept the user agent logic duplicated for now
while we figure out what we want to do about #33847. This will probably

sdk/core/ts-http-runtime/src/util/concat.ts
@@ -4,7 +4,6 @@
 import { Readable } from "stream";
 import type { ReadableStream as AsyncIterableReadableStream } from "stream/web";
 import { isBlob } from "./typeGuards.js";
-import { getRawContent } from "./file.js";
 
 async function* streamAsyncIterator(
   this: ReadableStream<Uint8Array>,
@@ -51,7 +50,7 @@ function toStream(
   if (source instanceof Uint8Array) {
     return Readable.from(Buffer.from(source));
   } else if (isBlob(source)) {
-    return toStream(getRawContent(source));
+    return toStream(source);
   } else {
     return ensureNodeStream(source);
   }

sdk/core/core-rest-pipeline/src/util/concat.ts
@@ -1,92 +1,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import { Readable } from "node:stream";
-import type { ReadableStream as AsyncIterableReadableStream } from "node:stream/web";
-import { isBlob } from "./typeGuards.js";
-import { getRawContent } from "./file.js";
-
-async function* streamAsyncIterator(
-  this: ReadableStream<Uint8Array>,
-): AsyncIterableIterator<Uint8Array> {
-  const reader = this.getReader();
-  try {
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) {
-        return;
-      }
-
-      yield value;
-    }
-  } finally {
-    reader.releaseLock();
-  }
-}
-
-function makeAsyncIterable<T>(webStream: any): asserts webStream is AsyncIterableReadableStream<T> {
-  if (!webStream[Symbol.asyncIterator]) {
-    webStream[Symbol.asyncIterator] = streamAsyncIterator.bind(webStream);
-  }
-
-  if (!webStream.values) {
-    webStream.values = streamAsyncIterator.bind(webStream);
-  }
-}
-
-function ensureNodeStream(
-  stream: ReadableStream<Uint8Array> | NodeJS.ReadableStream,
-): NodeJS.ReadableStream {
-  if (stream instanceof ReadableStream) {
-    makeAsyncIterable<Uint8Array>(stream);
-    return Readable.fromWeb(stream);
-  } else {
-    return stream;
-  }
-}
-
-function toStream(
-  source: ReadableStream<Uint8Array> | NodeJS.ReadableStream | Uint8Array | Blob,
-): NodeJS.ReadableStream {
-  if (source instanceof Uint8Array) {
-    return Readable.from(Buffer.from(source));
-  } else if (isBlob(source)) {
-    return toStream(getRawContent(source));
-  } else {
-    return ensureNodeStream(source);
-  }
-}
-
-/**
- * Accepted binary data types for concat
- *
- * @internal
- */
-export type ConcatSource = ReadableStream<Uint8Array> | NodeJS.ReadableStream | Uint8Array | Blob;
-
-/**
- * Utility function that concatenates a set of binary inputs into one combined output.
- *
- * @param sources - array of sources for the concatenation
- * @returns - in Node, a (() =\> NodeJS.ReadableStream) which, when read, produces a concatenation of all the inputs.
- *           In browser, returns a `Blob` representing all the concatenated inputs.
- *
- * @internal
- */
-export async function concat(
-  sources: (ConcatSource | (() => ConcatSource))[],
-): Promise<(() => NodeJS.ReadableStream) | Blob> {
-  return function () {
-    const streams = sources.map((x) => (typeof x === "function" ? x() : x)).map(toStream);
-
-    return Readable.from(
-      (async function* () {
-        for (const stream of streams as NodeJS.ReadableStream[]) {
-          for await (const chunk of stream) {
-            yield chunk;
-          }
-        }
-      })(),
-    );
-  };
-}

sdk/core/core-rest-pipeline/test/multipartPolicy.spec.ts
@@ -1,14 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { describe, it, assert, vi, expect } from "vitest";
 import { createHttpHeaders } from "../src/httpHeaders.js";
 import type { PipelineRequest, PipelineResponse, SendRequest } from "../src/interfaces.js";
 import { createPipelineRequest } from "../src/pipelineRequest.js";
 import { multipartPolicy } from "../src/policies/multipartPolicy.js";
-import { describe, it, assert, expect, vi } from "vitest";
 import type { PipelineRequestOptions } from "../src/pipelineRequest.js";
 import { stringToUint8Array } from "@azure/core-util";
 import { assertBodyMatches } from "./util.js";
+import { createFile, createFileFromStream } from "../src/util/file.js";
 
 export async function performRequest(
   requestOptions: Omit<PipelineRequestOptions, "url" | "method">,
@@ -64,30 +65,6 @@ describe("multipartPolicy", function () {
       request,
       originalRequest,
       "multipartPolicy touched a request that is not multipart",
-    );
-  });
-
-  it("throws if request.body is already present", async function () {
-    const request = createPipelineRequest({
-      url: "https://bing.com",
-      headers: createHttpHeaders({
-        "Content-Type": "multipart/form-data",
-      }),
-      multipartBody: { parts: [] },
-      body: "AAAAAAAAAAAA",
-    });
-    const successResponse: PipelineResponse = {
-      headers: createHttpHeaders(),
-      request,
-      status: 200,
-    };
-    const next = vi.fn<SendRequest>();
-    next.mockResolvedValue(successResponse);
-
-    const policy = multipartPolicy();
-
-    await expect(policy.sendRequest(request, next)).rejects.toThrow(
-      /multipartBody and regular body cannot be set at the same time/,
     );
   });
 
@@ -265,6 +242,28 @@ describe("multipartPolicy", function () {
       );
     });
 
+    it("supports createFile body", async function () {
+      const request = await performRequest({
+        multipartBody: {
+          boundary: "blah",
+          parts: [
+            {
+              body: createFile(stringToUint8Array("part", "utf-8"), "hello.txt"),
+              headers: createHttpHeaders(),
+            },
+          ],
+        },
+      });
+
+      const expectedBody = stringToUint8Array("--blah\r\n\r\npart\r\n--blah--\r\n\r\n", "utf-8");
+      await assertBodyMatches(request.body, expectedBody);
+      assert.equal(
+        request.headers.get("Content-Length"),
+        expectedBody.byteLength.toString(),
+        "Expected Content-Length header to equal length of body",
+      );
+    });
+
     it("Supports web ReadableStream body", async function () {
       const body = new ReadableStream({
         start(controller) {
@@ -279,6 +278,35 @@ describe("multipartPolicy", function () {
           parts: [
             {
               body,
+              headers: createHttpHeaders(),
+            },
+          ],
+        },
+      });
+
+      const expectedBody = stringToUint8Array("--blah\r\n\r\npart1\r\n--blah--\r\n\r\n", "utf-8");
+      await assertBodyMatches(request.body, expectedBody);
+      assert.isUndefined(
+        request.headers.get("Content-Length"),
+        "Content-Length value should not be inferred from a stream",
+      );
+    });
+
+    it("Supports createFileFromStream body", async function () {
+      const body = () =>
+        new ReadableStream<Uint8Array>({
+          start(controller) {
+            controller.enqueue(new TextEncoder().encode("part1"));
+            controller.close();
+          },
+        });
+
+      const request = await performRequest({
+        multipartBody: {
+          boundary: "blah",
+          parts: [
+            {
+              body: createFileFromStream(body, "hello.txt"),
               headers: createHttpHeaders(),
             },
           ],

sdk/core/core-rest-pipeline/test/node/multipartPolicy.spec.ts
@@ -1,34 +1,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import { describe, it, assert } from "vitest";
-import { createHttpHeaders } from "../../src/httpHeaders.js";
-import { isNodeLike, stringToUint8Array } from "@azure/core-util";
-import { Readable } from "stream";
-import { performRequest } from "../multipartPolicy.spec.js";
-import { assertBodyMatches } from "../util.js";
-
-describe("multipartPolicy (node-only)", function () {
-  it.runIf(isNodeLike)("supports Node ReadableStream body", async function () {
-    const body = Readable.from(Buffer.from("part1", "utf-8"));
-
-    const request = await performRequest({
-      multipartBody: {
-        boundary: "blah",
-        parts: [
-          {
-            body,
-            headers: createHttpHeaders(),
-          },
-        ],
-      },
-    });
-
-    const expectedBody = stringToUint8Array("--blah\r\n\r\npart1\r\n--blah--\r\n\r\n", "utf-8");
-    await assertBodyMatches(request.body, expectedBody);
-    assert.isUndefined(
-      request.headers.get("Content-Length"),
-      "Content-Length value should not be inferred from a stream",
-    );
-  });
-});

commit 933d1c1c4238ce0741b9a5ee77d4c097b53fa207

[core] fix stack overflow when passing a Blob for multipart part body in Node (#34160)
### Packages impacted by this PR

- `@typespec/ts-http-runtime`

### Issues associated with this PR

- Fixes bug introduced in #33948 (unreleased)

### Describe the problem that is addressed by this PR

Came across this when investigating with @HarshaNalluru yesterday
evening. I introduced this when taking the `createFile` shim and friends
out of the Unbranded package. The logic I 'fixed' results in infinite
recursion if a `Blob` is passed into `toStream` in Node. Apparently we
didn't have a test for passing a `Blob` body into `multipartPolicy` so
this wasn't caught. I added a test here to fix the gap in coverage.

Ultimately, it seems unlikely that someone would actually pass a Blob as
input here in Node, so the impact of this would probably have been
limited, but it's still a bug and it's an easy fix.

sdk/core/ts-http-runtime/src/util/concat.ts
@@ -50,7 +50,7 @@ function toStream(
   if (source instanceof Uint8Array) {
     return Readable.from(Buffer.from(source));
   } else if (isBlob(source)) {
-    return toStream(source);
+    return ensureNodeStream(source.stream());
   } else {
     return ensureNodeStream(source);
   }

sdk/core/ts-http-runtime/test/multipartPolicy.spec.ts
@@ -270,6 +270,26 @@ describe("multipartPolicy", function () {
     });
   });
 
+  it("Supports Blob body", async function () {
+    const blob = new Blob(["part1"]);
+
+    const request = await performRequest({
+      multipartBody: {
+        boundary: "blah",
+        parts: [
+          {
+            body: blob,
+            headers: createHttpHeaders(),
+          },
+        ],
+      },
+    });
+
+    const expectedBody = stringToUint8Array("--blah\r\n\r\npart1\r\n--blah--\r\n\r\n", "utf-8");
+    await assertBodyMatches(request.body, expectedBody);
+    assert.equal(request.headers.get("Content-Length"), expectedBody.byteLength.toString());
+  });
+
   describe("part headers", function () {
     it("are present when specified", async function () {
       const request = await performRequest({