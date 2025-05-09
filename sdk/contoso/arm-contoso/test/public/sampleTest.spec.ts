b39723b30c832b65c412144bbfbf0188cf3f2080
Ubuntu <cloudtest@158235aac00000E.tkkhlgj2u51eln3mcb0lygmbye.dx.internal.cloudapp.net>
2025-05-09 07:58:14 +0000

    Migration: Apply codemod: "fixSourceFile"

--- sdk/contoso/arm-contoso/test/public/sampleTest.spec.ts
+++ sdk/contoso/arm-contoso/test/public/sampleTest.spec.ts
@@ -1,19 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { createRecorder } from "./utils/recordedClient.js";
 import { assert, beforeEach, afterEach, it, describe } from "vitest";
 
 describe("My test", () => {
   // let recorder: Recorder;
 
-  beforeEach(async function () {
-    // recorder = await createRecorder(this);
-  });
+  beforeEach(async () => {
+      // recorder = await createRecorder(this);
+    });
 
-  afterEach(async function () {
-    // await recorder.stop();
-  });
+  afterEach(async () => {
+      // await recorder.stop();
+    });
 
   it("sample test", async function () {
     assert.equal(1, 1);
