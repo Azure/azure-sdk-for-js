7c0ff37ec097054fda918a00449772673312529a
v-saasomani <v-saasomani@microsoft.com>
2025-05-13 09:38:33 -0700

    Removed logging statement in callMedia.ts

--- sdk/communication/communication-call-automation/src/callMedia.ts
+++ sdk/communication/communication-call-automation/src/callMedia.ts
@@ -171,7 +171,6 @@ export class CallMedia {
         loop: false,
       };
       playRequest.interruptCallMediaOperation = options.interruptCallMediaOperation;
-      console.log(playRequest.interruptCallMediaOperation);
     }
 
     return this.callMedia.play(this.callConnectionId, playRequest, options);
