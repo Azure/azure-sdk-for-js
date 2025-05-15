5e12596c3c0cec15eb5429498f7f2961fc437255
Kyle Zhang <v-zhanh@microsoft.com>
2025-05-15 17:14:05 +0800

    test

--- eng/common/scripts/Update-GeneratedSdks.ps1
+++ eng/common/scripts/Update-GeneratedSdks.ps1
@@ -4,8 +4,19 @@ param(
   [string]$PackageDirectoriesFile
 )
 
+Write-Host "Running common.ps1 from path: $PSScriptRoot/common.ps1"
 . $PSScriptRoot/common.ps1
+
+Write-Host "Running CommandInvocation-Helpers.ps1 from path: $PSScriptRoot/Helpers/CommandInvocation-Helpers.ps1"
 . $PSScriptRoot/Helpers/CommandInvocation-Helpers.ps1
+
+
+Write-Host "Running automation_init.sh..."
+bash ".scripts/automation_init.sh"
+
+
+Write-Host "Running automation_generate.sh..."
+bash ".scripts/automation_generate.sh"
 
 $ErrorActionPreference = 'Stop'
 
