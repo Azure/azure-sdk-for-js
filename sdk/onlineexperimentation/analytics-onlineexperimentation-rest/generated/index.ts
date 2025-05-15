c93971e347be01e4dd588743f3a082f9d69e37d0
smichtch <smichtch@users.noreply.github.com>
2025-05-15 07:47:25 -0700

    Azure Online Experimentation data-plane SDK (#33812)
    ### Packages impacted by this PR
    New package `@azure-rest/analytics-onlineexperimentation`
    
    ### Issues associated with this PR
    https://github.com/Azure/azure-sdk-pr/issues/2199
    
    ### Describe the problem that is addressed by this PR
    Adding data plane client package for new resource provider
    `Microsoft.OnlineExperimentation/*`.
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    N/A, used standard process to generate SDK from TypeSpec.
    
    ### Are there test cases added in this PR? _(If not, why?)_
    Yes.
    
    ### Provide a list of related PRs _(if any)_
    https://github.com/Azure/azure-rest-api-specs/pull/33135 TypeSpec
    definitions used for code generation.
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [x] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [x] Added a changelog (if necessary)

---
+++ sdk/onlineexperimentation/analytics-onlineexperimentation-rest/generated/index.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import OnlineExperimentationClient from "./onlineExperimentationClient.js";
+
+export * from "./onlineExperimentationClient.js";
+export * from "./parameters.js";
+export * from "./responses.js";
+export * from "./clientDefinitions.js";
+export * from "./isUnexpected.js";
+export * from "./models.js";
+export * from "./outputModels.js";
+export * from "./paginateHelper.js";
+
+export default OnlineExperimentationClient;
