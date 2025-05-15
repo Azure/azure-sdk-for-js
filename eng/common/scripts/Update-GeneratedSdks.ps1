8c10afb870812c98f1ac9c12a33c63bfbbcd565b
Kyle Zhang <v-zhanh@microsoft.com>
2025-05-15 17:56:30 +0800

    up

--- eng/common/scripts/Update-GeneratedSdks.ps1
+++ eng/common/scripts/Update-GeneratedSdks.ps1
@@ -10,17 +10,10 @@ Write-Host "Running common.ps1 from path: $PSScriptRoot/common.ps1"
 Write-Host "Running CommandInvocation-Helpers.ps1 from path: $PSScriptRoot/Helpers/CommandInvocation-Helpers.ps1"
 . $PSScriptRoot/Helpers/CommandInvocation-Helpers.ps1
 
-
-Write-Host "Running automation_init.sh..."
-bash ".scripts/automation_init.sh"
-
-
-Write-Host "Running automation_generate.sh..."
-bash ".scripts/automation_generate.sh"
-
 $ErrorActionPreference = 'Stop'
 
 if (Test-Path "Function:$UpdateGeneratedSdksFn") {
+    Write-Host "Calling function $UpdateGeneratedSdksFn"
     &$UpdateGeneratedSdksFn $PackageDirectoriesFile
 } else {
     Write-Error "Function $UpdateGeneratedSdksFn not implemented in Language-Settings.ps1"
