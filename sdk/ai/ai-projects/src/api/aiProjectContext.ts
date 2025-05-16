48bf84ba5e6983f02bff54fa1cfdd14721710ead
retry-recv <goubo2012@gmail.com>
2025-05-15 17:26:31 -0700

    beta6 fix comments 2 (#34374)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: bobogogo1990 <bobobupt2014@163.com>

--- sdk/ai/ai-projects/src/api/aiProjectContext.ts
+++ sdk/ai/ai-projects/src/api/aiProjectContext.ts
@@ -42,7 +42,6 @@ export function createAIProject(
     loggingOptions: { logger: options.loggingOptions?.logger ?? logger.info },
     credentials: {
       scopes: options.credentials?.scopes ?? ["https://cognitiveservices.azure.com/.default"],
-      apiKeyHeaderName: options.credentials?.apiKeyHeaderName ?? "Ocp-Apim-Subscription-Key",
     },
   };
   const clientContext = getClient(endpointUrl, credential, updatedOptions);
