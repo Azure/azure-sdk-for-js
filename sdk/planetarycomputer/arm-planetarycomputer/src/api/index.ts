840a3e9e244bdc7f5b8c5443263395116d4ffe0b
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-05-20 14:56:46 +0800

    Migration: Update package.json, tsconfig.json, and api-extractor.json

---
+++ sdk/planetarycomputer/arm-planetarycomputer/src/api/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  createSpatio,
+  SpatioContext,
+  SpatioClientOptionalParams,
+} from "./spatioContext.js";
