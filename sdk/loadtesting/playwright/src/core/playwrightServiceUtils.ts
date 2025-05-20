bc41f12ac649e3000bd38c491558280858b002db
Siddharth Singha Roy <siddharthsingharoy@gmail.com>
2025-05-20 18:30:13 +0000

    fix(): vitest configuration issue

--- sdk/loadtesting/playwright/src/core/playwrightServiceUtils.ts
+++ sdk/loadtesting/playwright/src/core/playwrightServiceUtils.ts
@@ -2,12 +2,6 @@
 // Licensed under the MIT License.
 
 import path from "node:path";
-import url from "node:url";
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export const globalPaths = {
   setup: path.join(__dirname, "./global/playwright-service-global-setup.js"),
