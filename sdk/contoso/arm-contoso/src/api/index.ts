da0e7eea9b64c558ea07ae72848c2945026da464
Ubuntu <cloudtest@35d185dbc000002.tkkhlgj2u51eln3mcb0lygmbye.dx.internal.cloudapp.net>
2025-05-20 07:58:11 +0000

    Migration: Update package.json, tsconfig.json, and api-extractor.json

---
+++ sdk/contoso/arm-contoso/src/api/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  createContoso,
+  ContosoContext,
+  ContosoClientOptionalParams,
+} from "./contosoContext.js";
