c6d9926ca2cd8660cc8503a46191d90b89c9be5f
Siddharth Singha Roy <siddharthsingharoy@gmail.com>
2025-05-28 18:51:33 +0000

    chore(): update sample with dac by default

--- sdk/loadtesting/playwright/samples/v1/typescript/manually-connecting-to-browsers/playwright.service.config.ts
+++ sdk/loadtesting/playwright/samples/v1/typescript/manually-connecting-to-browsers/playwright.service.config.ts
@@ -1,5 +1,8 @@
 import { getServiceConfig } from "@azure/playwright";
 import { defineConfig } from "@playwright/test";
+import { DefaultAzureCredential } from "@azure/identity";
 import config from "./playwright.config.js";
 
-export default defineConfig(config, getServiceConfig(config));
+export default defineConfig(config, getServiceConfig(config, {
+    credential: new DefaultAzureCredential()
+}));
