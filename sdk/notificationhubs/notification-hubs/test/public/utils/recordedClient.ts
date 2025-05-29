b09de205370de9449ae5320d40f17071aa659c54
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-05-29 00:41:13 +0000

    [notificationhubs][test] update recorded client sanitizers
    - replace "x-ms-azsdk-telemetry" header value
    - ignore new "ServicebusNotification-Tags" header which we don't use and have undefined as value

--- sdk/notificationhubs/notification-hubs/test/public/utils/recordedClient.ts
+++ sdk/notificationhubs/notification-hubs/test/public/utils/recordedClient.ts
@@ -33,6 +33,20 @@ export async function createRecordedClientContext(
   recorder: Recorder,
 ): Promise<NotificationHubsClientContext> {
   await recorder.start(recorderOptions);
+  await recorder.addSanitizers(
+    {
+      headerSanitizers: [
+        {
+          key: "x-ms-azsdk-telemetry",
+          value: "class=REDACTED",
+        },
+      ],
+      removeHeaderSanitizer: {
+        headersForRemoval: ["ServiceBusNotification-Tags"],
+      }
+    },
+    ["record", "playback"],
+  );
   if (isBrowser) {
     // there are timestamps in the body, so do not match body
     await recorder.setMatcher("BodilessMatcher");
