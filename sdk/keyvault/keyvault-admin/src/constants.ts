6e10ef5a3fbdfefb8f87b5d66441a3fc2aef591c
Maor Leger <maorleger@microsoft.com>
2025-06-02 15:34:48 +0000

    update version to 7.6

--- sdk/keyvault/keyvault-admin/src/constants.ts
+++ sdk/keyvault/keyvault-admin/src/constants.ts
@@ -9,9 +9,9 @@ export const SDK_VERSION: string = "4.7.0-beta.2";
 /**
  * The latest supported Key Vault service API version.
  */
-export const LATEST_API_VERSION = "7.6-preview.2";
+export const LATEST_API_VERSION = "7.6";
 
 /**
  * Supported API versions
  */
-export type SUPPORTED_API_VERSIONS = "7.2" | "7.3" | "7.4" | "7.5" | "7.6-preview.2";
+export type SUPPORTED_API_VERSIONS = "7.2" | "7.3" | "7.4" | "7.5" | "7.6";
