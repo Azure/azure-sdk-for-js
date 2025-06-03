7b9c834b3f7b41ce65d746f643a615c8a146844c
Siddharth Singha Roy <siddharthsingharoy@gmail.com>
2025-06-03 06:46:39 +0000

    refactor(): run linter on samples

--- sdk/loadtesting/playwright/samples/v1/typescript/manually-connecting-to-browsers/playwright.service.config.ts
+++ sdk/loadtesting/playwright/samples/v1/typescript/manually-connecting-to-browsers/playwright.service.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "@playwright/test";
 import { DefaultAzureCredential } from "@azure/identity";
 import config from "./playwright.config.js";
 
-export default defineConfig(config, getServiceConfig(config, {
-    credential: new DefaultAzureCredential()
-}));
+export default defineConfig(
+  config,
+  getServiceConfig(config, {
+    credential: new DefaultAzureCredential(),
+  }),
+);
