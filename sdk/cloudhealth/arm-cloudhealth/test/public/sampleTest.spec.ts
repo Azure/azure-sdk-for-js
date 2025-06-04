9b629bec41aeac5ca35dd35df51b1fd7a9691783
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-06-04 15:25:29 +0800

    Migration: Apply codemod: "fixSourceFile"

--- sdk/cloudhealth/arm-cloudhealth/test/public/sampleTest.spec.ts
+++ sdk/cloudhealth/arm-cloudhealth/test/public/sampleTest.spec.ts
@@ -8,13 +8,13 @@ import { assert, beforeEach, afterEach, it, describe } from "vitest";
 describe("My test", () => {
   let recorder: Recorder;
 
-  beforeEach(async function (ctx) {
-    recorder = await createRecorder(ctx);
-  });
+  beforeEach(async (ctx) => {
+      recorder = await createRecorder(ctx);
+    });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  afterEach(async () => {
+      await recorder.stop();
+    });
 
   it("sample test", async function () {
     assert.equal(1, 1);
