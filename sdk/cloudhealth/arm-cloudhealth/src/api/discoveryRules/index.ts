d56a4326a38eaf152377882a62d0aff1417358fd
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-06-04 15:25:10 +0800

    Migration: Update package.json, tsconfig.json, and api-extractor.json

---
+++ sdk/cloudhealth/arm-cloudhealth/src/api/discoveryRules/index.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  listByHealthModel,
+  $delete,
+  createOrUpdate,
+  get,
+} from "./operations.js";
+export {
+  DiscoveryRulesListByHealthModelOptionalParams,
+  DiscoveryRulesDeleteOptionalParams,
+  DiscoveryRulesCreateOrUpdateOptionalParams,
+  DiscoveryRulesGetOptionalParams,
+} from "./options.js";
