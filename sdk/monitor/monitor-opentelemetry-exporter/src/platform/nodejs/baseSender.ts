25483b860df580a41166516c0625968c0e8f2b5b
Jackson Weber <jacksonweber@microsoft.com>
2025-06-05 14:55:40 -0700

    Add full statsbeat sender checks.

--- sdk/monitor/monitor-opentelemetry-exporter/src/platform/nodejs/baseSender.ts
+++ sdk/monitor/monitor-opentelemetry-exporter/src/platform/nodejs/baseSender.ts
@@ -94,13 +94,17 @@ export abstract class BaseSender {
           }, this.batchSendRetryIntervalMs);
           this.retryTimer.unref();
         }
-        // If we are not exportings statsbeat and statsbeat is not disabled -- count success
-        this.networkStatsbeatMetrics?.countSuccess(duration);
+        // If we are not exporting statsbeat and statsbeat is not disabled -- count success
+        if (!this.isStatsbeatSender) {
+          this.networkStatsbeatMetrics?.countSuccess(duration);
+        }
         return { code: ExportResultCode.SUCCESS };
       } else if (statusCode && isRetriable(statusCode)) {
         // Failed -- persist failed data
         if (statusCode === 429 || statusCode === 439) {
-          this.networkStatsbeatMetrics?.countThrottle(statusCode);
+          if (!this.isStatsbeatSender) {
+            this.networkStatsbeatMetrics?.countThrottle(statusCode);
+          }
           return {
             code: ExportResultCode.SUCCESS,
           };
@@ -110,7 +114,7 @@ export abstract class BaseSender {
           const breezeResponse = JSON.parse(result) as BreezeResponse;
           const filteredEnvelopes: Envelope[] = [];
           // If we have a partial success, count the succeeded envelopes
-          if (breezeResponse.itemsAccepted > 0 && statusCode === 206) {
+          if (breezeResponse.itemsAccepted > 0 && statusCode === 206 && !this.isStatsbeatSender) {
             this.networkStatsbeatMetrics?.countSuccess(duration);
           }
           // Figure out if we need to either retry or count failures
@@ -122,23 +126,29 @@ export abstract class BaseSender {
             });
           }
           if (filteredEnvelopes.length > 0) {
-            this.networkStatsbeatMetrics?.countRetry(statusCode);
+            if (!this.isStatsbeatSender) {
+              this.networkStatsbeatMetrics?.countRetry(statusCode);
+            }
             // calls resultCallback(ExportResult) based on result of persister.push
             return await this.persist(filteredEnvelopes);
           }
           // Failed -- not retriable
-          this.networkStatsbeatMetrics?.countFailure(duration, statusCode);
+          if (!this.isStatsbeatSender) {
+            this.networkStatsbeatMetrics?.countFailure(duration, statusCode);
+          }
           return {
             code: ExportResultCode.FAILED,
           };
         } else {
           // calls resultCallback(ExportResult) based on result of persister.push
-          this.networkStatsbeatMetrics?.countRetry(statusCode);
+          if (!this.isStatsbeatSender) {
+            this.networkStatsbeatMetrics?.countRetry(statusCode);
+          }
           return await this.persist(envelopes);
         }
       } else {
         // Failed -- not retriable
-        if (this.networkStatsbeatMetrics) {
+        if (this.networkStatsbeatMetrics && !this.isStatsbeatSender) {
           if (statusCode) {
             this.networkStatsbeatMetrics.countFailure(duration, statusCode);
           }
@@ -172,7 +182,9 @@ export abstract class BaseSender {
           }
         } else {
           const redirectError = new Error("Circular redirect");
-          this.networkStatsbeatMetrics?.countException(redirectError);
+          if (!this.isStatsbeatSender) {
+            this.networkStatsbeatMetrics?.countException(redirectError);
+          }
           return { code: ExportResultCode.FAILED, error: redirectError };
         }
       } else if (
@@ -199,7 +211,7 @@ export abstract class BaseSender {
         return { code: ExportResultCode.SUCCESS };
       }
       if (this.isRetriableRestError(restError)) {
-        if (restError.statusCode) {
+        if (restError.statusCode && !this.isStatsbeatSender) {
           this.networkStatsbeatMetrics?.countRetry(restError.statusCode);
         }
         if (!this.isStatsbeatSender) {
@@ -210,7 +222,9 @@ export abstract class BaseSender {
         }
         return this.persist(envelopes);
       }
-      this.networkStatsbeatMetrics?.countException(restError);
+      if (!this.isStatsbeatSender) {
+        this.networkStatsbeatMetrics?.countException(restError);
+      }
       if (!this.isStatsbeatSender) {
         diag.error(
           "Envelopes could not be exported and are not retriable. Error message:",
@@ -234,7 +248,9 @@ export abstract class BaseSender {
             error: new Error("Failed to persist envelope in disk."),
           };
     } catch (ex: any) {
-      this.networkStatsbeatMetrics?.countWriteFailure();
+      if (!this.isStatsbeatSender) {
+        this.networkStatsbeatMetrics?.countWriteFailure();
+      }
       return { code: ExportResultCode.FAILED, error: ex };
     }
   }
@@ -268,7 +284,9 @@ export abstract class BaseSender {
         await this.send(envelopes);
       }
     } catch (err: any) {
-      this.networkStatsbeatMetrics?.countReadFailure();
+      if (!this.isStatsbeatSender) {
+        this.networkStatsbeatMetrics?.countReadFailure();
+      }
       diag.warn(`Failed to fetch persisted file`, err);
     }
   }
