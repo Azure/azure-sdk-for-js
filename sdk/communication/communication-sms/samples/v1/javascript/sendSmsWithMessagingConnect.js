6229f8add477472292b5141be4b53e2fc21ba8c6
ihuseynov-msft <ihuseynov@microsoft.com>
2025-06-05 07:29:36 +0000

    addded changelog and sample; fixed recorded test uri sanitization

---
+++ sdk/communication/communication-sms/samples/v1/javascript/sendSmsWithMessagingConnect.js
@@ -0,0 +1,71 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * @summary Configure Messaging Connect options when sending a message
+ */
+
+const { SmsClient } = require("@azure/communication-sms");
+
+// Load the .env file if it exists
+require("dotenv").config();
+
+async function main() {
+  console.log("== Send SMS Message With Messaging Connect ==");
+
+  // You will need to set this environment variable or edit the following values
+  const connectionString =
+    process.env.COMMUNICATION_SAMPLES_CONNECTION_STRING ||
+    "endpoint=https://<resource-name>.communication.azure.com/;<access-key>";
+
+  // create new client
+  const client = new SmsClient(connectionString);
+
+  // construct send request
+  let phoneNumbers;
+  if (process.env.TO_PHONE_NUMBERS !== undefined) {
+    phoneNumbers = process.env.TO_PHONE_NUMBERS.split(",");
+  } else if (process.env.AZURE_PHONE_NUMBER !== undefined) {
+    phoneNumbers = [process.env.AZURE_PHONE_NUMBER];
+  } else {
+    phoneNumbers = ["<to-phone-number-1>", "<to-phone-number-2>"];
+  }
+
+  const sendRequest = {
+    from: process.env.FROM_PHONE_NUMBER || process.env.AZURE_PHONE_NUMBER || "<from-phone-number>",
+    to: phoneNumbers,
+    message: "Hello World via SMS!",
+  };
+
+  // construct send options
+  const sendOptions = {
+    messagingConnect: {
+      // Represents the API key associated with the customer's account in the Messaging Connect Partner portal.
+      apiKey: process.env.MESSAGING_CONNECT_API_KEY || "<messaging-connect-api-key>",
+      // Specifies the partner associated with the API key.
+      partner: process.env.MESSAGING_CONNECT_PARTNER || "<messaging-connect-partner>",
+    },
+  };
+
+  // send sms with request and options
+  const sendResults = await client.send(sendRequest, sendOptions);
+
+  // individual messages can encounter errors during sending
+  // use the "successful" property to verify
+  for (const sendResult of sendResults) {
+    if (sendResult.successful) {
+      console.log("Success: ", sendResult);
+    } else {
+      console.error("Something went wrong when trying to send this message: ", sendResult);
+    }
+  }
+
+  console.log("== Done: Send SMS Message With Messaging Connect Options ==");
+}
+
+main().catch((error) => {
+  console.error("Encountered an error while sending SMS: ", error);
+  process.exit(1);
+});
+
+module.exports = { main };
