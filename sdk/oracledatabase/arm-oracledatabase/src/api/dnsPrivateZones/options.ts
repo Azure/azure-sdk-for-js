35d626a4cd6271c90993bb4cff6182e22b9e2c5b
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-06-14 03:31:58 +0800

    [mgmt] Oracledatabase release (#34693)
    https://github.com/Azure/sdk-release-request/issues/6263

---
+++ sdk/oracledatabase/arm-oracledatabase/src/api/dnsPrivateZones/options.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { OperationOptions } from "@azure-rest/core-client";
+
+/** Optional parameters. */
+export interface DnsPrivateZonesListByLocationOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface DnsPrivateZonesGetOptionalParams extends OperationOptions {}
