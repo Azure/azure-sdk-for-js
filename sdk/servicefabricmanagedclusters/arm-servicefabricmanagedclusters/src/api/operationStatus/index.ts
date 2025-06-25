4937596366ce0f4c17fd3602cd2e431a80ae02b1
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-06-26 05:02:31 +0800

    [mgmt] servicefabricmanagedclusters release (#34554)
    https://github.com/Azure/sdk-release-request/issues/6225

---
+++ sdk/servicefabricmanagedclusters/arm-servicefabricmanagedclusters/src/api/operationStatus/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { get } from "./operations.js";
+export { OperationStatusGetOptionalParams } from "./options.js";
