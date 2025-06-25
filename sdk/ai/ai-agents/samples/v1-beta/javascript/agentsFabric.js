863e0c5f95053faf14fe653521c53aaf46e20df0
retry-recv <goubo2012@gmail.com>
2025-06-25 12:34:16 -0700

    simplify ai-agents samples (#34963)
    ### Packages impacted by this PR
    
    
    ### Issues associated with this PR
    
    
    ### Describe the problem that is addressed by this PR
    
    
    ### What are the possible designs available to address the problem? If
    there are more than one possible design, why was the one in this PR
    chosen?
    
    
    ### Are there test cases added in this PR? _(If not, why?)_
    
    
    ### Provide a list of related PRs _(if any)_
    
    
    ### Command used to generate this PR:**_(Applicable only to SDK release
    request PRs)_
    
    ### Checklists
    - [ ] Added impacted package name to the issue description
    - [ ] Does this PR needs any fixes in the SDK Generator?** _(If so,
    create an Issue in the
    [Autorest/typescript](https://github.com/Azure/autorest.typescript)
    repository and link it here)_
    - [ ] Added a changelog (if necessary)
    
    ---------
    
    Co-authored-by: bobogogo1990 <bobobupt2014@163.com>
    Co-authored-by: Deyaaeldeen Almahallawi <dealmaha@microsoft.com>

--- sdk/ai/ai-agents/samples/v1-beta/javascript/agentsFabric.js
+++ sdk/ai/ai-agents/samples/v1-beta/javascript/agentsFabric.js
@@ -61,14 +61,12 @@ async function main() {
 
   // Fetch and log all messages
   const messagesIterator = client.messages.list(thread.id);
-  console.log(`Messages:`);
 
   // Get the first message
   for await (const m of messagesIterator) {
     const agentMessage = m.content[0];
     if (isOutputOfType(agentMessage, "text")) {
-      const textContent = agentMessage;
-      console.log(`Text Message Content - ${textContent.text.value}`);
+      console.log(`Text Message Content - ${agentMessage.text.value}`);
     }
     break; // Only process the first message
   }
--- sdk/ai/ai-agents/samples/v1-beta/typescript/src/agentsBingGrounding.ts
+++ sdk/ai/ai-agents/samples/v1-beta/typescript/src/agentsBingGrounding.ts
@@ -64,15 +64,13 @@ export async function main(): Promise<void> {
 
   // Fetch and log all messages
   const messagesIterator = client.messages.list(thread.id);
-  console.log(`Messages:`);
 
   // Get the first message
   const firstMessage = await messagesIterator.next();
   if (!firstMessage.done && firstMessage.value) {
     const agentMessage: MessageContent = firstMessage.value.content[0];
     if (isOutputOfType<MessageTextContent>(agentMessage, "text")) {
-      const textContent = agentMessage as MessageTextContent;
-      console.log(`Text Message Content - ${textContent.text.value}`);
+      console.log(`Text Message Content - ${agentMessage.text.value}`);
     }
   }
 }
