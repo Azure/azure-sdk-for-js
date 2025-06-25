b3549414b8cf8a93f9cc0d1f0339e4d4b224a614
azure-sdk <azuresdk@microsoft.com>
2025-06-25 06:25:48 +0000

    Configurations:  'specification/onlineexperimentation/OnlineExperimentation.Management/tspconfig.yaml', API Version: v2025_08_01_preview, SDK Release Type: beta, and CommitSHA: '20d6633d3f5f97f614feef2e68133a60a03bee8b' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs'

---
+++ sdk/onlineexperimentation/arm-onlineexperimentation/src/api/privateEndpointConnections/operations.ts
@@ -0,0 +1,303 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { OnlineExperimentationContext as Client } from "../index.js";
+import {
+  errorResponseDeserializer,
+  PrivateEndpointConnection,
+  privateEndpointConnectionSerializer,
+  privateEndpointConnectionDeserializer,
+  _PrivateEndpointConnectionListResult,
+  _privateEndpointConnectionListResultDeserializer,
+} from "../../models/models.js";
+import {
+  PrivateEndpointConnectionsListOptionalParams,
+  PrivateEndpointConnectionsDeleteOptionalParams,
+  PrivateEndpointConnectionsUpdateOptionalParams,
+  PrivateEndpointConnectionsGetOptionalParams,
+} from "./options.js";
+import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
+import {
+  PagedAsyncIterableIterator,
+  buildPagedAsyncIterator,
+} from "../../static-helpers/pagingHelpers.js";
+import { expandUrlTemplate } from "../../static-helpers/urlTemplate.js";
+import {
+  StreamableMethod,
+  PathUncheckedResponse,
+  createRestError,
+  operationOptionsToRequestParameters,
+} from "@azure-rest/core-client";
+import { PollerLike, OperationState } from "@azure/core-lro";
+
+export function _listSend(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  options: PrivateEndpointConnectionsListOptionalParams = {
+    requestOptions: {},
+  },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OnlineExperimentation/workspaces/{workspaceName}/privateEndpointConnections{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      "api%2Dversion": apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _listDeserialize(
+  result: PathUncheckedResponse,
+): Promise<_PrivateEndpointConnectionListResult> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return _privateEndpointConnectionListResultDeserializer(result.body);
+}
+
+/** Gets all the private endpoint connections configured on an online experimentation workspace resource. */
+export function list(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  options: PrivateEndpointConnectionsListOptionalParams = {
+    requestOptions: {},
+  },
+): PagedAsyncIterableIterator<PrivateEndpointConnection> {
+  return buildPagedAsyncIterator(
+    context,
+    () => _listSend(context, apiVersion, resourceGroupName, workspaceName, options),
+    _listDeserialize,
+    ["200"],
+    { itemName: "value", nextLinkName: "nextLink" },
+  );
+}
+
+export function _$deleteSend(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  privateEndpointConnectionName: string,
+  options: PrivateEndpointConnectionsDeleteOptionalParams = {
+    requestOptions: {},
+  },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OnlineExperimentation/workspaces/{workspaceName}/privateEndpointConnections/{privateEndpointConnectionName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      privateEndpointConnectionName: privateEndpointConnectionName,
+      "api%2Dversion": apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).delete({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _$deleteDeserialize(result: PathUncheckedResponse): Promise<void> {
+  const expectedStatuses = ["202", "204", "200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return;
+}
+
+/** Deletes private endpoint connection for an online experimentation workspace resource. */
+/**
+ *  @fixme delete is a reserved word that cannot be used as an operation name.
+ *         Please add @clientName("clientName") or @clientName("<JS-Specific-Name>", "javascript")
+ *         to the operation to override the generated name.
+ */
+export function $delete(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  privateEndpointConnectionName: string,
+  options: PrivateEndpointConnectionsDeleteOptionalParams = {
+    requestOptions: {},
+  },
+): PollerLike<OperationState<void>, void> {
+  return getLongRunningPoller(context, _$deleteDeserialize, ["202", "204", "200"], {
+    updateIntervalInMs: options?.updateIntervalInMs,
+    abortSignal: options?.abortSignal,
+    getInitialResponse: () =>
+      _$deleteSend(
+        context,
+        apiVersion,
+        resourceGroupName,
+        workspaceName,
+        privateEndpointConnectionName,
+        options,
+      ),
+    resourceLocationConfig: "location",
+  }) as PollerLike<OperationState<void>, void>;
+}
+
+export function _updateSend(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  privateEndpointConnectionName: string,
+  resource: PrivateEndpointConnection,
+  options: PrivateEndpointConnectionsUpdateOptionalParams = {
+    requestOptions: {},
+  },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OnlineExperimentation/workspaces/{workspaceName}/privateEndpointConnections/{privateEndpointConnectionName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      privateEndpointConnectionName: privateEndpointConnectionName,
+      "api%2Dversion": apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).put({
+    ...operationOptionsToRequestParameters(options),
+    contentType: "application/json",
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+    body: privateEndpointConnectionSerializer(resource),
+  });
+}
+
+export async function _updateDeserialize(
+  result: PathUncheckedResponse,
+): Promise<PrivateEndpointConnection> {
+  const expectedStatuses = ["200", "201"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return privateEndpointConnectionDeserializer(result.body);
+}
+
+/** Updates private endpoint connection status (Approval/Rejected) for an online experimentation workspace resource. This gets invoked by resource admin. */
+export async function update(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  privateEndpointConnectionName: string,
+  resource: PrivateEndpointConnection,
+  options: PrivateEndpointConnectionsUpdateOptionalParams = {
+    requestOptions: {},
+  },
+): Promise<PrivateEndpointConnection> {
+  const result = await _updateSend(
+    context,
+    apiVersion,
+    resourceGroupName,
+    workspaceName,
+    privateEndpointConnectionName,
+    resource,
+    options,
+  );
+  return _updateDeserialize(result);
+}
+
+export function _getSend(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  privateEndpointConnectionName: string,
+  options: PrivateEndpointConnectionsGetOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OnlineExperimentation/workspaces/{workspaceName}/privateEndpointConnections/{privateEndpointConnectionName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      privateEndpointConnectionName: privateEndpointConnectionName,
+      "api%2Dversion": apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _getDeserialize(
+  result: PathUncheckedResponse,
+): Promise<PrivateEndpointConnection> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return privateEndpointConnectionDeserializer(result.body);
+}
+
+/** Gets the private endpoint connection details for an online experimentation workspace resource. */
+export async function get(
+  context: Client,
+  apiVersion: string,
+  resourceGroupName: string,
+  workspaceName: string,
+  privateEndpointConnectionName: string,
+  options: PrivateEndpointConnectionsGetOptionalParams = { requestOptions: {} },
+): Promise<PrivateEndpointConnection> {
+  const result = await _getSend(
+    context,
+    apiVersion,
+    resourceGroupName,
+    workspaceName,
+    privateEndpointConnectionName,
+    options,
+  );
+  return _getDeserialize(result);
+}
