c1e7cc4d6c01cce4cb0dbc447343811be797b22e
azure-sdk <azuresdk@microsoft.com>
2025-06-25 10:18:17 +0000

    Configurations:  'specification/contosowidgetmanager/Contoso.Management/tspconfig.yaml', API Version: X, SDK Release Type: beta, and CommitSHA: '941701bfba960d20abf49b53e6da13dc6dd737e1' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs'

---
+++ sdk/contoso/arm-contoso/src/api/contosoContext.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { logger } from "../logger.js";
+import { Client, ClientOptions, getClient } from "@azure-rest/core-client";
+import { TokenCredential } from "@azure/core-auth";
+
+/** Microsoft.Contoso Resource Provider management API. */
+export interface ContosoContext extends Client {
+  /** The ID of the target subscription. The value must be an UUID. */
+  subscriptionId: string;
+}
+
+/** Optional parameters for the client. */
+export interface ContosoClientOptionalParams extends ClientOptions {}
+
+/** Microsoft.Contoso Resource Provider management API. */
+export function createContoso(
+  credential: TokenCredential,
+  subscriptionId: string,
+  options: ContosoClientOptionalParams = {},
+): ContosoContext {
+  const endpointUrl = options.endpoint ?? options.baseUrl ?? "https://management.azure.com";
+  const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+  const userAgentInfo = `azsdk-js-arm-contoso/1.0.0-beta.1`;
+  const userAgentPrefix = prefixFromOptions
+    ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
+    : `azsdk-js-api ${userAgentInfo}`;
+  const { apiVersion: _, ...updatedOptions } = {
+    ...options,
+    userAgentOptions: { userAgentPrefix },
+    loggingOptions: { logger: options.loggingOptions?.logger ?? logger.info },
+    credentials: {
+      scopes: options.credentials?.scopes ?? [`${endpointUrl}/.default`],
+    },
+  };
+  const clientContext = getClient(endpointUrl, credential, updatedOptions);
+  clientContext.pipeline.removePolicy({ name: "ApiVersionPolicy" });
+  if (options.apiVersion) {
+    logger.warning(
+      "This client does not support client api-version, please change it at the operation level",
+    );
+  }
+  return { ...clientContext, subscriptionId } as ContosoContext;
+}
