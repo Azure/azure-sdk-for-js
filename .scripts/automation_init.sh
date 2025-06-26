3981f5c1775bf131bc8a237f7587856cfc9d2312
Kyle Zhang <v-zhanh@microsoft.com>
2025-06-26 16:59:45 +0800

    up

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -3,11 +3,9 @@ npm install -g @azure-tools/typespec-client-generator-cli@0.21.0
 npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
 
-
-
-
 echo "Before changing directory: $(pwd)"
-cd tools/js-sdk-release-tools
-echo "After changing to tools/js-sdk-release-tools: $(pwd)"
+cd ..
+cd azure-sdk-tools/tools/js-sdk-release-tools
+echo "After changing to parent directory: $(pwd)"
 npm install
 npm run build
