84769dbde33c95a06c8cf0d78453b4e6b1067734
Kyle Zhang <v-zhanh@microsoft.com>
2025-06-26 16:45:24 +0800

    up

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
-cd ../../../..
+echo "Before changing directory: $(pwd)"
+cd ../../../../..
+echo "After changing to azure-sdk-for-js root: $(pwd)"
 cd azure-sdk-for-js
+echo "After changing to azure-sdk-for-js: $(pwd)"
 node ../azure-sdk-tools/tools/js-sdk-release-tools/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
