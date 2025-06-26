72c876489eec54419d17eadc67a5f3c8fcfd3269
Kyle Zhang <v-zhanh@microsoft.com>
2025-06-26 16:36:16 +0800

    up

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -4,32 +4,10 @@ npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
 
 
-# Hardcode branch to applyLegacySettingsMapping
-branch="applyLegacySettingsMapping"
-echo "Using branch: $branch"
 
-if [ -d "azure-sdk-tools" ]; then
-    echo "Delete folder azure-sdk-tools"
-    rm -rf azure-sdk-tools
-else
-    echo "azure-sdk-tools folder does not exist."
-fi
 
-echo 'clone azure-sdk-tools'
-git clone https://github.com/Azure/azure-sdk-tools/
-
-cd azure-sdk-tools
-if [ -z "$branch" ]; then
-  echo 'branch is empty'
-else
-    git checkout -b test $branch
-    echo git checkout -b test $branch
-fi
-
-echo '-------------- git status start'
-git status
-echo '-------------- git status end'
-
+echo "Before changing directory: $(pwd)"
 cd tools/js-sdk-release-tools
+echo "After changing to tools/js-sdk-release-tools: $(pwd)"
 npm install
 npm run build
