89249966313bca75c01c0b687daf747260fbb349
Kyle Zhang <v-zhanh@microsoft.com>
2025-06-26 22:40:17 +0800

    with private branch

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
+echo "Before changing directory: $(pwd)"
+cd ../../../../../..
+echo "After changing to azure-sdk root: $(pwd)"
+cd azure-sdk
+echo "After changing to azure-sdk: $(pwd)"
+node ../azure-sdk-tools/tools/js-sdk-release-tools/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -2,3 +2,10 @@
 npm install -g @azure-tools/typespec-client-generator-cli@0.21.0
 npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
+
+echo "Before changing directory: $(pwd)"
+cd ..
+cd azure-sdk-tools/tools/js-sdk-release-tools
+echo "After changing to parent directory: $(pwd)"
+npm install
+npm run build
