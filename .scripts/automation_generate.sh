36a0a69ae3a249b6c799311328e1f47f33b84160
Kyle Zhang <v-zhanh@microsoft.com>
2025-06-26 16:28:32 +0800

    up

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,37 +1,5 @@
 #!/usr/bin/env bash
 
-cd ..
-
-# Hardcode branch to applyLegacySettingsMapping
-branch="applyLegacySettingsMapping"
-echo "Using branch: $branch"
-
-if [ -d "azure-sdk-tools" ]; then
-    echo "Delete folder azure-sdk-tools"
-    rm -rf azure-sdk-tools
-else
-    echo "azure-sdk-tools folder does not exist."
-fi
-
-echo 'clone azure-sdk-tools'
-git clone https://github.com/Azure/azure-sdk-tools/
-
-cd azure-sdk-tools
-if [ -z "$branch" ]; then
-  echo 'branch is empty'
-else
-    git checkout -b test $branch
-    echo git checkout -b test $branch
-fi
-
-echo '-------------- git status start'
-git status
-echo '-------------- git status end'
-
-cd tools/js-sdk-release-tools
-npm install
-npm run build
-cd ../../..
-
+cd ../../../..
 cd azure-sdk-for-js
 node ../azure-sdk-tools/tools/js-sdk-release-tools/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -2,3 +2,34 @@
 npm install -g @azure-tools/typespec-client-generator-cli@0.21.0
 npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
+
+
+# Hardcode branch to applyLegacySettingsMapping
+branch="applyLegacySettingsMapping"
+echo "Using branch: $branch"
+
+if [ -d "azure-sdk-tools" ]; then
+    echo "Delete folder azure-sdk-tools"
+    rm -rf azure-sdk-tools
+else
+    echo "azure-sdk-tools folder does not exist."
+fi
+
+echo 'clone azure-sdk-tools'
+git clone https://github.com/Azure/azure-sdk-tools/
+
+cd azure-sdk-tools
+if [ -z "$branch" ]; then
+  echo 'branch is empty'
+else
+    git checkout -b test $branch
+    echo git checkout -b test $branch
+fi
+
+echo '-------------- git status start'
+git status
+echo '-------------- git status end'
+
+cd tools/js-sdk-release-tools
+npm install
+npm run build
