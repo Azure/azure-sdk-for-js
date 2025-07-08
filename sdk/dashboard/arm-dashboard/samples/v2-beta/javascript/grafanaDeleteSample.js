2d805f171e5ad6cae0889a85ce3ba56b7a7c7560
azure-sdk <azuresdk@microsoft.com>
2025-07-08 04:14:07 +0000

    Configurations:  'specification/dashboard/Dashboard.Management/tspconfig.yaml', API Version: 2024-11-01-preview, SDK Release Type: beta, and CommitSHA: '2a29539fd6c52ea15ff02fc4635cc75b4fc5d600' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs'

---
+++ sdk/dashboard/arm-dashboard/samples/v2-beta/javascript/grafanaDeleteSample.js
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { DashboardManagementClient } = require("@azure/arm-dashboard");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to delete a workspace for Grafana resource.
+ *
+ * @summary delete a workspace for Grafana resource.
+ * x-ms-original-file: 2024-11-01-preview/Grafana_Delete.json
+ */
+async function grafanaDelete() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DashboardManagementClient(credential, subscriptionId);
+  await client.grafana.delete("myResourceGroup", "myWorkspace");
+}
+
+async function main() {
+  await grafanaDelete();
+}
+
+main().catch(console.error);
