9100c4ef6a70d7c24a8a153000cad99fc37c57fa
Hoppe Wang <hoppewang@microsoft.com>
2025-07-11 15:59:23 -0700

    Add snippets tests

---
+++ sdk/batch/batch-rest/test/snippets.spec.ts
@@ -0,0 +1,16 @@
+import { AzureNamedKeyCredential } from "@azure/core-auth";
+import createClient from "../src/index.js";
+import { setLogLevel } from "@azure/logger";
+import { describe, it } from "vitest";
+
+describe("snippets", function () {
+
+  it("ReadmeSampleCreateClient_Node", async function () {
+      const credential = new AzureNamedKeyCredential("<account name>", "<account key>");
+      const batchClient = createClient("<account endpoint>", credential);
+  });
+
+  it("SetLogLevel", () => {
+    setLogLevel("info");
+  });
+});
