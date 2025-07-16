7782c4e7d7e9889a7d84688811d596e5e2be7660
Ujjwal Soni <soniujjwal@microsoft.com>
2025-07-16 15:36:49 +0530

    change log

--- sdk/cosmosdb/cosmos/src/utils/time.ts
+++ sdk/cosmosdb/cosmos/src/utils/time.ts
@@ -14,6 +14,9 @@ export function getCurrentTimestampInMs(): number {
 /**
  * @hidden
  * Utility function to start a background task that runs at specified intervals.
+ * @param action - A function that returns a Promise, representing the task to be executed.
+ * @param intervalMs - The interval in milliseconds at which the task should run.
+ * @returns A NodeJS.Timeout object representing the timer for the background task.
  */
 export function startBackgroundTask(
   action: () => Promise<void>,
