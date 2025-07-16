f507c5f19a5f9c2586c38e2d6b63924d311afe18
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-07-16 18:17:23 +0800

    [mgmt] hardwaresecuritymodules release (#35110)
    https://github.com/Azure/sdk-release-request/issues/6375

---
+++ sdk/hardwaresecuritymodules/arm-hardwaresecuritymodules/src/api/cloudHsmClusters/index.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  restore,
+  validateRestoreProperties,
+  backup,
+  validateBackupProperties,
+  listBySubscription,
+  listByResourceGroup,
+  $delete,
+  update,
+  createOrUpdate,
+  get,
+} from "./operations.js";
+export {
+  CloudHsmClustersRestoreOptionalParams,
+  CloudHsmClustersValidateRestorePropertiesOptionalParams,
+  CloudHsmClustersBackupOptionalParams,
+  CloudHsmClustersValidateBackupPropertiesOptionalParams,
+  CloudHsmClustersListBySubscriptionOptionalParams,
+  CloudHsmClustersListByResourceGroupOptionalParams,
+  CloudHsmClustersDeleteOptionalParams,
+  CloudHsmClustersUpdateOptionalParams,
+  CloudHsmClustersCreateOrUpdateOptionalParams,
+  CloudHsmClustersGetOptionalParams,
+} from "./options.js";
