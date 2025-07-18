519809d436652eb29cdedc86855c382879e8b1c1
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-07-18 14:30:59 +0800

    [mgmt] storagediscovert release

---
+++ sdk/storagediscovery/arm-storagediscovery/src/classic/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { OperationsOperations } from "./operations/index.js";
+export { StorageDiscoveryWorkspacesOperations } from "./storageDiscoveryWorkspaces/index.js";
