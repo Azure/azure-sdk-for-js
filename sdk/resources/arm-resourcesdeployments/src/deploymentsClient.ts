fee998a947945d514c5da1403ec6a8fd24740e7e
azure-sdk <azuresdk@microsoft.com>
2025-07-18 05:45:49 +0000

    Post release automated changes for azure-arm-resourcesdeployments

--- sdk/resources/arm-resourcesdeployments/src/deploymentsClient.ts
+++ sdk/resources/arm-resourcesdeployments/src/deploymentsClient.ts
@@ -70,7 +70,7 @@ export class DeploymentsClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-resourcesdeployments/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-resourcesdeployments/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
