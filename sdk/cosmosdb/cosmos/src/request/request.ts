4857fad6d72bc0d3cb99ff65c1f74a70d503b16f
Ujjwal Soni <soniujjwal@microsoft.com>
2025-07-18 10:44:48 +0530

    bug fix

--- sdk/cosmosdb/cosmos/src/request/request.ts
+++ sdk/cosmosdb/cosmos/src/request/request.ts
@@ -206,6 +206,8 @@ export async function getHeaders({
 
   if (partitionKey !== undefined && !headers[Constants.HttpHeaders.PartitionKey]) {
     headers[Constants.HttpHeaders.PartitionKey] = jsonStringifyAndEscapeNonASCII(partitionKey);
+  }else if (partitionKeyRangeId !== undefined) {
+    headers[Constants.HttpHeaders.PartitionKeyRangeID] = partitionKeyRangeId;
   }
 
   if (clientOptions.key || clientOptions.tokenProvider) {
@@ -222,9 +224,7 @@ export async function getHeaders({
     headers[Constants.HttpHeaders.Accept] = JsonContentType;
   }
 
-  if (partitionKeyRangeId !== undefined) {
-    headers[Constants.HttpHeaders.PartitionKeyRangeID] = partitionKeyRangeId;
-  }
+  
 
   if (options.enableScriptLogging) {
     headers[Constants.HttpHeaders.EnableScriptLogging] = options.enableScriptLogging;
