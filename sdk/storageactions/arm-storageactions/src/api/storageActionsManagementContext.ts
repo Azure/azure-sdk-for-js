09c1b9749a1a39ad0464f03fc688af1a76882e96
azure-sdk <azuresdk@microsoft.com>
2025-07-22 06:33:54 +0000

    Configurations:  'specification/storageactions/StorageAction.Management/tspconfig.yaml', API Version: 2023-01-01, SDK Release Type: beta, and CommitSHA: '1a1984eab477a3527e10ce1c9e9ee5fd806bec62' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs'

--- sdk/storageactions/arm-storageactions/src/api/storageActionsManagementContext.ts
+++ sdk/storageactions/arm-storageactions/src/api/storageActionsManagementContext.ts
@@ -28,7 +28,7 @@ export function createStorageActionsManagement(
   subscriptionId: string,
   options: StorageActionsManagementClientOptionalParams = {},
 ): StorageActionsManagementContext {
-  const endpointUrl = options.endpoint ?? options.baseUrl ?? "https://management.azure.com";
+  const endpointUrl = options.endpoint ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
   const userAgentInfo = `azsdk-js-arm-storageactions/1.0.0-beta.1`;
   const userAgentPrefix = prefixFromOptions
