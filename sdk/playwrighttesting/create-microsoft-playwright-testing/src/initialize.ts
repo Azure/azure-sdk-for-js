a2337ad30fcead63a7b48e9338747ad43d8c4d96
Kashish Gupta <90824921+kashish2508@users.noreply.github.com>
2025-07-24 20:02:04 +0000

    update

--- sdk/playwrighttesting/create-microsoft-playwright-testing/src/initialize.ts
+++ sdk/playwrighttesting/create-microsoft-playwright-testing/src/initialize.ts
@@ -130,11 +130,8 @@ export default defineConfig(
     exposeNetwork: '<loopback>',
     timeout: 30000,
     os: ServiceOS.LINUX,
-    useCloudHostedBrowsers: true // Set to false if you want to only use reporting and not cloud hosted browsers
   }),
-  {
-  
-  }
+
 );
 `;
     return content;
