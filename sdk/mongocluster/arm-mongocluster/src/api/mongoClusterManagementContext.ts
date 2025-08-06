dad3d08c0f526b40d4406cd8d1910f68ed82b43d
azure-sdk <azuresdk@microsoft.com>
2025-08-06 03:45:12 +0000

    Post release automated changes for azure-arm-mongocluster

--- sdk/mongocluster/arm-mongocluster/src/api/mongoClusterManagementContext.ts
+++ sdk/mongocluster/arm-mongocluster/src/api/mongoClusterManagementContext.ts
@@ -30,7 +30,7 @@ export function createMongoClusterManagement(
 ): MongoClusterManagementContext {
   const endpointUrl = options.endpoint ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-mongocluster/1.1.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-mongocluster/1.1.0-beta.2`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
