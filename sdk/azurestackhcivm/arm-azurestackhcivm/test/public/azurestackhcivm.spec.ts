fdbb48cf8e8ef0bde3bd6c663f4304e1e58ac6ce
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-08-08 15:50:32 +0800

    update

---
+++ sdk/azurestackhcivm/arm-azurestackhcivm/test/public/azurestackhcivm.spec.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+* Changes may cause incorrect behavior and will be lost if the code is regenerated.
+ */
+
+import {
+  env,
+  Recorder,
+  isPlaybackMode,
+} from "@azure-tools/test-recorder";
+import { createTestCredential } from "@azure-tools/test-credential";
+import { assert, beforeEach, afterEach, it, describe } from "vitest";
+import { createRecorder } from "./utils/recordedClient.js";
+import { AzureStackHCIClient } from "../../src/azureStackHCIClient.js";
+
+export const testPollingOptions = {
+  updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
+};
+
+describe("AzureStackHCIVM test", () => {
+  let recorder: Recorder;
+  let subscriptionId: string;
+  let client: AzureStackHCIClient;
+
+  beforeEach(async (context) => {
+    process.env.SystemRoot = process.env.SystemRoot || "C:\\Windows";
+    recorder = await createRecorder(context);
+    subscriptionId = env.SUBSCRIPTION_ID || '';
+    // This is an example of how the environment variables are used
+    const credential = createTestCredential();
+    client = new AzureStackHCIClient(credential, subscriptionId, recorder.configureClientOptions({}));
+  });
+
+  afterEach(async function () {
+    await recorder.stop();
+  });
+  it("operations list test", async function () {
+    const resArray = new Array();
+    for await (let item of client.galleryImages.listAll()) {
+      resArray.push(item);
+    }
+    assert.ok(resArray.length);
+  });
+})
