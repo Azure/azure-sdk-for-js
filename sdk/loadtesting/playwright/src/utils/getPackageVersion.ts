b6e76efe6f211bf2ee8998e86d4591225bea60c3
Maor Leger <maorleger@microsoft.com>
2025-08-12 16:44:09 +0000

    apply same fix

---
+++ sdk/loadtesting/playwright/src/utils/getPackageVersion.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import path, { dirname } from "node:path";
+import { coreLogger } from "../common/logger.js";
+import { fileURLToPath } from "node:url";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+const getPackageVersionFromFolder = (folder: string): string => {
+  try {
+    const version = require(path.join(currentDir, folder, "package.json")).version;
+    return version;
+  } catch (error) {
+    coreLogger.error("Error fetching package version:", error);
+    return "";
+  }
+};
+
+export const getPackageVersion = (): string => {
+  // hacky way to get package version
+  // try from dist folder first (customer perspective)
+  const distVersion = getPackageVersionFromFolder("../../../");
+  if (distVersion) {
+    return distVersion;
+  }
+  // if not found, try from src folder (internal test suite)
+  const srcVersion = getPackageVersionFromFolder("../../");
+  if (srcVersion) {
+    return srcVersion;
+  }
+  return "unknown-version";
+};
--- sdk/loadtesting/playwright/src/utils/utils.ts
+++ sdk/loadtesting/playwright/src/utils/utils.ts
@@ -165,32 +165,6 @@ export const fetchOrValidateAccessToken = async (credential?: TokenCredential): 
   return getAccessToken()!;
 };
 
-const getPackageVersionFromFolder = (folder: string): string => {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const version = require(path.join(__dirname, folder, "package.json")).version;
-    return version;
-  } catch (error) {
-    coreLogger.error("Error fetching package version:", error);
-    return "";
-  }
-};
-
-export const getPackageVersion = (): string => {
-  // hacky way to get package version
-  // try from dist folder first (customer perspective)
-  const distVersion = getPackageVersionFromFolder("../../../");
-  if (distVersion) {
-    return distVersion;
-  }
-  // if not found, try from src folder (internal test suite)
-  const srcVersion = getPackageVersionFromFolder("../../");
-  if (srcVersion) {
-    return srcVersion;
-  }
-  return "unknown-version";
-};
-
 export const getVersionInfo = (version: string): VersionInfo => {
   const regex = /^(\d+)(?:\.(\d+))?(?:\.(\d+))?/;
   const match = version.match(regex);
