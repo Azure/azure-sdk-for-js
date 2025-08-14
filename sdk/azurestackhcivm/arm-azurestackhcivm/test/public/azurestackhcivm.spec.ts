a60373bc65b721f0bfb1f0e0fe53bb26155f0d34
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-08-14 11:07:36 +0800

    Update azurestackhcivm.spec.ts

--- sdk/azurestackhcivm/arm-azurestackhcivm/test/public/azurestackhcivm.spec.ts
+++ sdk/azurestackhcivm/arm-azurestackhcivm/test/public/azurestackhcivm.spec.ts
@@ -9,7 +9,7 @@ import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert, beforeEach, afterEach, it, describe } from "vitest";
 import { createRecorder } from "./utils/recordedClient.js";
-import { AzureStackHCIVMManagementClient } from "../../src/azureStackHCIClient.js";
+import { AzureStackHCIVMManagementClient } from "../../src/azureStackHcivmManagementClient.js";
 
 export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
