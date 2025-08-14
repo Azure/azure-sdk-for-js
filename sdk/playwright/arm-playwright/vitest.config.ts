ffc10195970bcfd74a79a737bde5c12fa1928bc3
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-08-14 16:17:04 +0800

    update

--- sdk/playwright/arm-playwright/vitest.config.ts
+++ sdk/playwright/arm-playwright/vitest.config.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.shared.config.ts";
 
 export default viteConfig;
