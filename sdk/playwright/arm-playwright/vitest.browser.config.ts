5d24dd561171adc99a000f6a889df37911a457be
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-08-14 15:05:56 +0800

    update

--- sdk/playwright/arm-playwright/vitest.browser.config.ts
+++ sdk/playwright/arm-playwright/vitest.browser.config.ts
@@ -4,13 +4,4 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.browser.shared.config.ts";
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      include: ["dist-test/browser/test/**/*.spec.js"],
-      testTimeout: 1200000,
-      hookTimeout: 1200000,
-    },
-  }),
-);
+export default viteConfig;
--- sdk/playwright/arm-playwright/vitest.config.ts
+++ sdk/playwright/arm-playwright/vitest.config.ts
@@ -4,12 +4,4 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "../../../vitest.shared.config.ts";
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      hookTimeout: 1200000,
-      testTimeout: 1200000,
-    },
-  }),
-);
+export default viteConfig;
