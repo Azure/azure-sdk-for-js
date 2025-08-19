d0af8f22a529023d22dc17120f3bf9598053b28f
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2025-08-18 21:45:39 -0700

    feat: comments only ts changes (part 34/58)

--- sdk/network/arm-network-rest/samples-dev/applicationSecurityGroupsListSample.ts
+++ sdk/network/arm-network-rest/samples-dev/applicationSecurityGroupsListSample.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
 import type { ApplicationSecurityGroupsListParameters } from "@azure-rest/arm-network";
 import createNetworkManagementClient, { paginate } from "@azure-rest/arm-network";
 import { DefaultAzureCredential } from "@azure/identity";
