cd68a239910d48df4b7d847e21beb96217e25b25
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2025-08-18 21:45:42 -0700

    feat: comments only ts changes (part 37/58)

--- sdk/network/arm-network-rest/samples-dev/webApplicationFirewallPoliciesListSample.ts
+++ sdk/network/arm-network-rest/samples-dev/webApplicationFirewallPoliciesListSample.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
 import type { WebApplicationFirewallPoliciesListParameters } from "@azure-rest/arm-network";
 import createNetworkManagementClient, { paginate } from "@azure-rest/arm-network";
 import { DefaultAzureCredential } from "@azure/identity";
