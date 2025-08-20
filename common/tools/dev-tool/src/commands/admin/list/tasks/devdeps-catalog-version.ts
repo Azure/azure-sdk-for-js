fd7f6259a57d8f574ddf239dcf604edb121a447a
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-08-20 23:48:48 +0000

    format and update

--- common/tools/dev-tool/src/commands/admin/list/tasks/devdeps-catalog-version.ts
+++ common/tools/dev-tool/src/commands/admin/list/tasks/devdeps-catalog-version.ts
@@ -17,12 +17,12 @@ export default async function listPackageCallback(
 
   const mainDeps = ["@types/node", "autorest", "eslint", "tslib", "tsx", "typescript"];
   const testingDeps = [
-    '@rollup/plugin-inject',
-    '@types/chai',
-    '@types/chai-as-promised',
-    '@vitest/browser',
-    '@vitest/coverage-istanbul',
-    '@vitest/expect',
+    "@rollup/plugin-inject",
+    "@types/chai",
+    "@types/chai-as-promised",
+    "@vitest/browser",
+    "@vitest/coverage-istanbul",
+    "@vitest/expect",
     "chai",
     "chai-as-promised",
     "chai-exclude",
@@ -40,7 +40,7 @@ export default async function listPackageCallback(
     "@azure/dev-tool",
     "@azure/eslint-plugin-azure-sdk",
   ];
-  const internalDeps = [ "@azure/identity" ];
+  const internalDeps = ["@azure/identity"];
   const armDeps = ["@azure/arm-storage", "@azure/arm-cognitiveservices", "@azure/arm-webpubsub"];
 
   const depVersionReplacement: [string[], string][] = [
@@ -51,9 +51,6 @@ export default async function listPackageCallback(
     [armDeps, "catalog:arm"],
   ];
   for (const c of depVersionReplacement) {
-    if (c[1] === "catalog:arm") {
-      continue; // skipping ARM dependencies for now due to issue
-    }
     for (const d of c[0]) {
       if (packageJson?.devDependencies?.[d]) {
         packageJson.devDependencies[d] = c[1];
@@ -66,7 +63,7 @@ export default async function listPackageCallback(
     console.log(`    updating ${packageName} ${fullProjectPath} devDependencies`);
     const packScript = packageJson?.scripts?.["pack"];
     if (packScript) {
-      packageJson.scripts["pack"] = packScript.replace("npm pack 2", "pnpm pack 2");
+      packageJson.scripts["pack"] = packScript.replace(`"npm pack 2`, `"pnpm pack 2`);
     }
     await writeFile(
       path.join(fullProjectPath, "package.json"),
