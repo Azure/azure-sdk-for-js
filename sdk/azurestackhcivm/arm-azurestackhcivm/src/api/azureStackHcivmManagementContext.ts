6a148c801f60773870878b48333612907685becc
azure-sdk <azuresdk@microsoft.com>
2025-08-20 23:44:23 +0000

    Post release automated changes for azure-arm-azurestackhcivm

--- sdk/azurestackhcivm/arm-azurestackhcivm/src/api/azureStackHcivmManagementContext.ts
+++ sdk/azurestackhcivm/arm-azurestackhcivm/src/api/azureStackHcivmManagementContext.ts
@@ -34,7 +34,7 @@ export function createAzureStackHCIVMManagement(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-azurestackhcivm/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-azurestackhcivm/1.0.0-beta.2`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
