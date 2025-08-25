600ddfc27023e9f3cb492aa771ff6014c779b6d0
azure-sdk <azuresdk@microsoft.com>
2025-08-25 22:34:45 +0000

    Post release automated changes for azure-communication-call-automation

--- sdk/communication/communication-call-automation/src/generated/src/callAutomationApiClient.ts
+++ sdk/communication/communication-call-automation/src/generated/src/callAutomationApiClient.ts
@@ -66,7 +66,7 @@ export class CallAutomationApiClient extends coreClient.ServiceClient {
       requestContentType: "application/json; charset=utf-8",
     };
 
-    const packageDetails = `azsdk-js-communication-call-automation/1.5.0`;
+    const packageDetails = `azsdk-js-communication-call-automation/1.5.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
