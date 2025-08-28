2f4cf17e0188f76a04161ccddbf426d3b6ca2641
azure-sdk <azuresdk@microsoft.com>
2025-08-28 13:14:38 +0000

    Post release automated changes for azure-arm-computeschedule

--- sdk/computeschedule/arm-computeschedule/src/api/computeScheduleContext.ts
+++ sdk/computeschedule/arm-computeschedule/src/api/computeScheduleContext.ts
@@ -34,7 +34,7 @@ export function createComputeSchedule(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-computeschedule/1.2.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-computeschedule/1.2.0-beta.2`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
