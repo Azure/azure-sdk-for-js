18f57d8db2ce312b1098139501206d6014106bb3
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-08-29 10:42:22 +0800

    update version

--- sdk/impactreporting/arm-impactreporting/src/api/impactContext.ts
+++ sdk/impactreporting/arm-impactreporting/src/api/impactContext.ts
@@ -32,7 +32,7 @@ export function createImpact(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-impactreporting/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-impactreporting/1.0.0-beta.2`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
