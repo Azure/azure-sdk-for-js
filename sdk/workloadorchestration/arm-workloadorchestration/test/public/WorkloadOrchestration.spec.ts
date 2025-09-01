2cdcef7fa80abab1a4799af2e739af4ff485a93d
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-09-01 10:26:59 +0800

    format

--- sdk/workloadorchestration/arm-workloadorchestration/test/public/WorkloadOrchestration.spec.ts
+++ sdk/workloadorchestration/arm-workloadorchestration/test/public/WorkloadOrchestration.spec.ts
@@ -1,20 +1,16 @@
-/* 
- * Copyright (c) Microsoft Corporation. 
- * Licensed under the MIT License. 
- * 
-* Changes may cause incorrect behavior and will be lost if the code is regenerated. 
-*/
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+ * Changes may cause incorrect behavior and will be lost if the code is regenerated.
+ */
 
-import {
-  env,
-  Recorder,
-  isPlaybackMode,
-} from "@azure-tools/test-recorder";
+import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 
 import { createTestCredential } from "@azure-tools/test-credential";
 import { assert, beforeEach, afterEach, it, describe } from "vitest";
 import { createRecorder } from "./utils/recordedClient.js";
-import { WorkloadOrchestrationManagementClient } from "../../src/workloadOrchestrationManagementClient.js"
+import { WorkloadOrchestrationManagementClient } from "../../src/workloadOrchestrationManagementClient.js";
 
 export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
@@ -28,10 +24,14 @@ describe("WorkloadOrchestration test", () => {
   beforeEach(async (context) => {
     process.env.SystemRoot = process.env.SystemRoot || "C:\\Windows";
     recorder = await createRecorder(context);
-    subscriptionId = env.SUBSCRIPTION_ID || '';
-    // This is an example of how the environment variables are used 
+    subscriptionId = env.SUBSCRIPTION_ID || "";
+    // This is an example of how the environment variables are used
     const credential = createTestCredential();
-    client = new WorkloadOrchestrationManagementClient(credential, subscriptionId, recorder.configureClientOptions({}));
+    client = new WorkloadOrchestrationManagementClient(
+      credential,
+      subscriptionId,
+      recorder.configureClientOptions({}),
+    );
   });
 
   afterEach(async function () {
@@ -45,4 +45,4 @@ describe("WorkloadOrchestration test", () => {
     }
     assert.ok(resArray);
   });
-})
+});
