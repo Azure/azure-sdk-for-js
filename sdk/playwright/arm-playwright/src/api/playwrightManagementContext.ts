506c439c4b973ca440cda647d995cb5c5b6800a8
azure-sdk <azuresdk@microsoft.com>
2025-09-01 08:12:23 +0000

    Post release automated changes for azure-arm-playwright

--- sdk/playwright/arm-playwright/src/api/playwrightManagementContext.ts
+++ sdk/playwright/arm-playwright/src/api/playwrightManagementContext.ts
@@ -34,7 +34,7 @@ export function createPlaywrightManagement(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-playwright/1.0.0`;
+  const userAgentInfo = `azsdk-js-arm-playwright/1.0.1`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
