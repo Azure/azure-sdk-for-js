ae5a9a02e0bceb5dfdaefc953b39bd1f0a4bce05
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2025-09-04 17:32:34 +0800

    Update Autorest TypeScript version in script

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -40,4 +40,4 @@ npm run build
 cd ../../..
 
 cd azure-sdk-for-js
-node ../azure-sdk-tools/tools/js-sdk-release-tools/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@6.0.45 --typespecEmitter=@azure-tools/typespec-ts
+node ../azure-sdk-tools/tools/js-sdk-release-tools/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
