dc52eb155b99a9a89d3703299595cbe0a342ca1d
KarishmaGhiya <kaghiya@microsoft.com>
2025-09-09 14:48:58 -0700

    update snippet

--- sdk/identity/identity/src/credentials/defaultAzureCredential.ts
+++ sdk/identity/identity/src/credentials/defaultAzureCredential.ts
@@ -73,12 +73,9 @@ export class UnavailableDefaultCredential implements TokenCredential {
  * The following example demonstrates how to use the `requiredEnvVars` option to ensure that certain environment variables are set before the `DefaultAzureCredential` is instantiated.
  * If any of the specified environment variables are missing or empty, an error will be thrown, preventing the application from continuing execution without the necessary configuration.
  * It also demonstrates how to set the `AZURE_TOKEN_CREDENTIALS` environment variable to control which credentials are included in the chain.
- *
- * @example
  * ```ts snippet:defaultazurecredential_requiredEnvVars
  * import { DefaultAzureCredential } from "\\@azure/identity";
- * import { SecretClient } from "\\@azure/keyvault-secrets";
- * // AZURE_TOKEN_CREDENTIALS can be set to `EnvironmentCredential` in the .env file in this example.
+ *
  * const credential = new DefaultAzureCredential({
  *   requiredEnvVars: ["AZURE_CLIENT_ID", "AZURE_TENANT_ID", "AZURE_CLIENT_SECRET", "AZURE_TOKEN_CREDENTIALS"]
  * });
