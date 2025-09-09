d4094225d1d804d1723ee67e17421e7ce65370f9
KarishmaGhiya <kaghiya@microsoft.com>
2025-09-09 10:55:30 -0700

    trying to fix linting

--- sdk/identity/identity/src/credentials/defaultAzureCredentialOptions.ts
+++ sdk/identity/identity/src/credentials/defaultAzureCredentialOptions.ts
@@ -69,15 +69,11 @@ export interface DefaultAzureCredentialOptions
    */
   processTimeoutInMs?: number;
   /**
-   *
    * List of environment variables that must be defined at runtime.
-   *
    * If any variable in this list is missing or set to an empty value,
    * {@link DefaultAzureCredential} constructor will throw an error.
-   *
    * Use this to enforce that your application has the necessary environment configuration before
    * continuing execution.
    */
-
   requiredEnvVars?: DefaultAzureCredentialEnvVars | DefaultAzureCredentialEnvVars[];
 }
