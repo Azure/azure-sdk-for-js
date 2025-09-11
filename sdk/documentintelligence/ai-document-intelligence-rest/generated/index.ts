f9f43356ffaf561ca0a7c3064b7613516001167d
Mary Gao <yanmeigao1210@gmail.com>
2025-09-11 14:30:24 +0800

    Update the generated code

---
+++ sdk/documentintelligence/ai-document-intelligence-rest/generated/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import DocumentIntelligence from "./documentIntelligence.js";
+
+export * from "./documentIntelligence.js";
+export * from "./parameters.js";
+export * from "./responses.js";
+export * from "./clientDefinitions.js";
+export * from "./isUnexpected.js";
+export * from "./models.js";
+export * from "./outputModels.js";
+export * from "./paginateHelper.js";
+export * from "./pollingHelper.js";
+export * from "./utils.js";
+
+export default DocumentIntelligence;
