8a517d379492754408c3f2cb4bfd31b640359c00
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-09-16 12:43:10 +0800

    fix lint

--- sdk/appconfiguration/app-configuration/samples-dev/featureFlag.ts
+++ sdk/appconfiguration/app-configuration/samples-dev/featureFlag.ts
@@ -116,7 +116,7 @@ export async function main() {
     // The feature flag will not be enabled for everyone as targeting filter is configured
     isEnabled = await featureManager.isEnabled(featureFlagName);
     console.log(`Is featureFlag enabled? ${isEnabled}`);
-   
+
     isEnabled = await featureManager.isEnabled(featureFlagName, targetingContext);
     console.log(`Is featureFlag enabled for test@contoso.com? ${isEnabled}`);
   }
