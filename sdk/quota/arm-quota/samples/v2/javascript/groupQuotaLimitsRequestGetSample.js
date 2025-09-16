543e1682fcef0b6bf1ffc38181146cbb6f751884
azure-sdk <azuresdk@microsoft.com>
2025-09-16 02:54:17 +0000

    Configurations:  'specification/quota/Quota.Management/tspconfig.yaml', API Version: 2025-09-01, SDK Release Type: stable, and CommitSHA: '3323341bd50a94ed45813bea84e57ec28408853e' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5341716 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/quota/arm-quota/samples/v2/javascript/groupQuotaLimitsRequestGetSample.js
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { AzureQuotaExtensionAPI } = require("@azure/arm-quota");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get API to check the status of a GroupQuota request by requestId.
+ *
+ * @summary get API to check the status of a GroupQuota request by requestId.
+ * x-ms-original-file: 2025-09-01/GroupQuotaLimitsRequests/GroupQuotaLimitsRequests_Get.json
+ */
+async function groupQuotaLimitsRequestsGet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-00000000000";
+  const client = new AzureQuotaExtensionAPI(credential, subscriptionId);
+  const result = await client.groupQuotaLimitsRequest.get(
+    "E7EC67B3-7657-4966-BFFC-41EFD36BAA09",
+    "groupquota1",
+    "requestId",
+  );
+  console.log(result);
+}
+
+async function main() {
+  await groupQuotaLimitsRequestsGet();
+}
+
+main().catch(console.error);
