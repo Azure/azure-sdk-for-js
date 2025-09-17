e75e42f5af03249aef431df6172f152a7ec7ffb8
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-09-17 22:07:49 +0000

    fix formatting

--- sdk/servicebus/service-bus/src/core/linkEntity.ts
+++ sdk/servicebus/service-bus/src/core/linkEntity.ts
@@ -238,7 +238,10 @@ export abstract class LinkEntity<LinkT extends Receiver | AwaitableSender | Requ
     const checkAborted = (): void => {
       if (abortSignal?.aborted) {
         this._link?.close().catch((closeErr) => {
-          this._logger.logError(closeErr, `${this._logPrefix} Error occurred while closing link during abort check`);
+          this._logger.logError(
+            closeErr,
+            `${this._logPrefix} Error occurred while closing link during abort check`,
+          );
         });
         throw new AbortError(StandardAbortMessage);
       }
