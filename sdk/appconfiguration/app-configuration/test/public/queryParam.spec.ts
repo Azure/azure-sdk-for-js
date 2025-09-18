a3c09926855b3b8b71c3380feefea69f8870a930
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-09-18 11:35:36 +0800

    fix format

--- sdk/appconfiguration/app-configuration/test/public/queryParam.spec.ts
+++ sdk/appconfiguration/app-configuration/test/public/queryParam.spec.ts
@@ -1,16 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import type {
-  AppConfigurationClient
-} from "../../src/index.js";
+import type { AppConfigurationClient } from "../../src/index.js";
 import type { Recorder } from "@azure-tools/test-recorder";
-import { isLiveMode} from "@azure-tools/test-recorder";
+import { isLiveMode } from "@azure-tools/test-recorder";
 import type { PipelinePolicy } from "@azure/core-rest-pipeline";
-import {
-  createAppConfigurationClientForTests,
-  startRecorder,
-} from "./utils/testHelpers.js";
+import { createAppConfigurationClientForTests, startRecorder } from "./utils/testHelpers.js";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 describe("request url query parameters", () => {
@@ -76,58 +71,60 @@ describe("request url query parameters", () => {
     // More details at https://github.com/Azure/azure-sdk-for-js/issues/16743
     //
     // Remove the following line if you want to hit the live service.
-    it("sort query params in alphabetical order - continuation token", /* { skip: isLiveMode() }, */ async () => {
-      const key = recorder.variable(
-        "sortQueryParamsMultiplePages",
-        `sortQueryParamsMultiplePages${Math.floor(Math.random() * 1000)}`,
-      );
-
-      const { getCapturedUrl, client } = createClientWithUrlCapturePolicy();
-
-      // this number is arbitrarily chosen to match the size of a page + 1
-      const expectedNumberOfLabels = 101;
-
-      let addSettingPromises = [];
-
-      for (let i = 0; i < expectedNumberOfLabels; i++) {
-        addSettingPromises.push(
-          client.addConfigurationSetting({
-            key,
-            value: `the value for ${i}`,
-            label: i.toString(),
-          }),
+    it(
+      "sort query params in alphabetical order - continuation token",
+      { skip: isLiveMode() },
+      async () => {
+        const key = recorder.variable(
+          "sortQueryParamsMultiplePages",
+          `sortQueryParamsMultiplePages${Math.floor(Math.random() * 1000)}`,
         );
 
-        if (i !== 0 && i % 2 === 0) {
-          await Promise.all(addSettingPromises);
-          addSettingPromises = [];
+        const { getCapturedUrl, client } = createClientWithUrlCapturePolicy();
+
+        // this number is arbitrarily chosen to match the size of a page + 1
+        const expectedNumberOfLabels = 101;
+
+        let addSettingPromises = [];
+
+        for (let i = 0; i < expectedNumberOfLabels; i++) {
+          addSettingPromises.push(
+            client.addConfigurationSetting({
+              key,
+              value: `the value for ${i}`,
+              label: i.toString(),
+            }),
+          );
+
+          if (i !== 0 && i % 2 === 0) {
+            await Promise.all(addSettingPromises);
+            addSettingPromises = [];
+          }
         }
-      }
 
-      const listResult = client.listConfigurationSettings({
-        keyFilter: key,
-      });
+        const listResult = client.listConfigurationSettings({
+          keyFilter: key,
+        });
 
-      for await (const _ of listResult.byPage()) {
-        // do nothing, just drain the iterator
-      }
+        for await (const _ of listResult.byPage()) {
+          // do nothing, just drain the iterator
+        }
 
         // Regex enforces exact ordering of query params for continuation page: after, api-version, key
-      // Note that only the request for the second page has the 'after' query param
-        const queryOrderRegex = new RegExp(
-          `\\?after=[^&]+&api-version=[^&]+&key=[^&]+$`,
+        // Note that only the request for the second page has the 'after' query param
+        const queryOrderRegex = new RegExp(`\\?after=[^&]+&api-version=[^&]+&key=[^&]+$`);
+
+        assert.match(
+          getCapturedUrl()!,
+          queryOrderRegex,
+          `Query parameters not in expected order or values. URL: ${getCapturedUrl()}`,
         );
 
-      assert.match(
-        getCapturedUrl()!,
-        queryOrderRegex,
-        `Query parameters not in expected order or values. URL: ${getCapturedUrl()}`,
-      );
-
-      for (let i = 0; i < expectedNumberOfLabels; i++) {
-        await client.deleteConfigurationSetting({ key, label: i.toString() });
-      }
-    });
+        for (let i = 0; i < expectedNumberOfLabels; i++) {
+          await client.deleteConfigurationSetting({ key, label: i.toString() });
+        }
+      },
+    );
 
     function createClientWithUrlCapturePolicy(): {
       getCapturedUrl: () => string | undefined;
