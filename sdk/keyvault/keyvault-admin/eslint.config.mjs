251f1e10c553765fbd85af0b992740591e2c405c
Maor Leger <maorleger@microsoft.com>
2025-09-19 19:00:06 +0000

    keyvault-admin: prep for customization codegen

--- sdk/keyvault/keyvault-admin/eslint.config.mjs
+++ sdk/keyvault/keyvault-admin/eslint.config.mjs
@@ -8,11 +8,21 @@ export default azsdkEslint.config([
     },
   },
   {
-    files: ["src/api/**/*.ts", "src/classic/**/*.ts", "src/models/**/*.ts"],
+    files: [
+      "src/api/**/*.ts",
+      "src/classic/**/*.ts",
+      "src/models/**/*.ts",
+      "src/static-helpers/**/*.ts",
+      "src/restorePollerHelpers.ts",
+    ],
     rules: {
       "@typescript-eslint/explicit-module-boundary-types": "off",
       "@typescript-eslint/naming-convention": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
       "tsdoc/syntax": "off",
+      "spaced-comment": "off",
+      "no-useless-escape": "off",
+      "no-unused-expressions": "off",
     },
   },
 ]);
