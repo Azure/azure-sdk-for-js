60921bd3862784b0ded01ed018d6cbcfb26ed101
Maor Leger <maorleger@microsoft.com>
2025-09-19 18:50:41 +0000

    update scripts, keyvault, etc

--- sdk/keyvault/keyvault-secrets/eslint.config.mjs
+++ sdk/keyvault/keyvault-secrets/eslint.config.mjs
@@ -2,12 +2,6 @@ import azsdkEslint from "@azure/eslint-plugin-azure-sdk";
 
 export default azsdkEslint.config([
   {
-    rules: {
-      "@typescript-eslint/no-empty-object-type": "warn",
-      "@azure/azure-sdk/ts-naming-options": "warn",
-    },
-  },
-  {
     files: ["src/api/**/*.ts", "src/classic/**/*.ts", "src/models/**/*.ts"],
     rules: {
       "@typescript-eslint/explicit-module-boundary-types": "off",
