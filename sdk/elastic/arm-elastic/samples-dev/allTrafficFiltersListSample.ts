e721c4f46ce869cef1bc9a62bed06c5e3940faea
ZiWei Chen <v-ziweichen@microsoft.com>
2025-09-19 14:00:41 +0800

    generate from origin swagger

--- sdk/elastic/arm-elastic/samples-dev/allTrafficFiltersListSample.ts
+++ sdk/elastic/arm-elastic/samples-dev/allTrafficFiltersListSample.ts
@@ -1,25 +1,29 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-/**
- * This sample demonstrates how to Get the list of all traffic filters for the account.
- *
- * @summary Get the list of all traffic filters for the account.
- * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/stable/2024-03-01/examples/AllTrafficFilters_list.json
- */
-
 import { MicrosoftElastic } from "@azure/arm-elastic";
 import { DefaultAzureCredential } from "@azure/identity";
 import "dotenv/config";
 
+/**
+ * This sample demonstrates how to List all traffic filters associated with your Elastic monitor resource, helping you manage network traffic control.
+ *
+ * @summary List all traffic filters associated with your Elastic monitor resource, helping you manage network traffic control.
+ * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples/AllTrafficFilters_list.json
+ */
 async function allTrafficFiltersList(): Promise<void> {
   const subscriptionId =
-    process.env["ELASTIC_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
-  const resourceGroupName = process.env["ELASTIC_RESOURCE_GROUP"] || "myResourceGroup";
+    process.env["ELASTIC_SUBSCRIPTION_ID"] ||
+    "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName =
+    process.env["ELASTIC_RESOURCE_GROUP"] || "myResourceGroup";
   const monitorName = "myMonitor";
   const credential = new DefaultAzureCredential();
   const client = new MicrosoftElastic(credential, subscriptionId);
-  const result = await client.allTrafficFilters.list(resourceGroupName, monitorName);
+  const result = await client.allTrafficFilters.list(
+    resourceGroupName,
+    monitorName,
+  );
   console.log(result);
 }
 
--- sdk/elastic/arm-elastic/samples-dev/monitorsCreateSample.ts
+++ sdk/elastic/arm-elastic/samples-dev/monitorsCreateSample.ts
@@ -1,25 +1,29 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-/**
- * This sample demonstrates how to Create a monitor resource.
- *
- * @summary Create a monitor resource.
- * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/stable/2024-03-01/examples/Monitors_Create.json
- */
-
 import { MicrosoftElastic } from "@azure/arm-elastic";
 import { DefaultAzureCredential } from "@azure/identity";
 import "dotenv/config";
 
+/**
+ * This sample demonstrates how to Create a new Elastic monitor resource in your Azure subscription, enabling observability and monitoring of your Azure resources through Elastic.
+ *
+ * @summary Create a new Elastic monitor resource in your Azure subscription, enabling observability and monitoring of your Azure resources through Elastic.
+ * x-ms-original-file: specification/elastic/resource-manager/Microsoft.Elastic/stable/2025-06-01/examples/Monitors_Create.json
+ */
 async function monitorsCreate(): Promise<void> {
   const subscriptionId =
-    process.env["ELASTIC_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
-  const resourceGroupName = process.env["ELASTIC_RESOURCE_GROUP"] || "myResourceGroup";
+    process.env["ELASTIC_SUBSCRIPTION_ID"] ||
+    "00000000-0000-0000-0000-000000000000";
+  const resourceGroupName =
+    process.env["ELASTIC_RESOURCE_GROUP"] || "myResourceGroup";
   const monitorName = "myMonitor";
   const credential = new DefaultAzureCredential();
   const client = new MicrosoftElastic(credential, subscriptionId);
-  const result = await client.monitors.beginCreateAndWait(resourceGroupName, monitorName);
+  const result = await client.monitors.beginCreateAndWait(
+    resourceGroupName,
+    monitorName,
+  );
   console.log(result);
 }
 
