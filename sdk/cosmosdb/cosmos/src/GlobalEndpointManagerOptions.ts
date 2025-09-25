f64bc7e06dcab037ccc8f26dcfd314d054a2c9cf
ajnovice <ajaysimple91@gmail.com>
2025-09-25 11:05:19 +0530

    add new GlobalEnpointManagerOptions.st file

---
+++ sdk/cosmosdb/cosmos/src/GlobalEndpointManagerOptions.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export interface ResolveServiceEndpointOptions {
+  startServiceEndpointIndex?: number;
+  excludedLocations?: string[];
+}
