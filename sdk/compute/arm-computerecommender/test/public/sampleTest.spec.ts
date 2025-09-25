d0df6cadeec9956336450d758deda1fcd822d22a
Jeff Fisher <jeffish@microsoft.com>
2025-09-25 20:03:48 +0000

    disable example test

--- sdk/compute/arm-computerecommender/test/public/sampleTest.spec.ts
+++ sdk/compute/arm-computerecommender/test/public/sampleTest.spec.ts
@@ -1,19 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import type { Recorder } from "@azure-tools/test-recorder";
-import { createRecorder } from "./utils/recordedClient.js";
+// import type { Recorder } from "@azure-tools/test-recorder";
+// import { createRecorder } from "./utils/recordedClient.js";
 import { assert, beforeEach, afterEach, it, describe } from "vitest";
 
 describe("My test", () => {
-  let recorder: Recorder;
+  // let recorder: Recorder;
 
   beforeEach(async function (ctx) {
-    recorder = await createRecorder(ctx);
+    // uncomment if recorded tests are added
+    // recorder = await createRecorder(ctx);
   });
 
   afterEach(async function () {
-    await recorder.stop();
+    // uncomment if recorded tests are added
+    // await recorder.stop();
   });
 
   it("sample test", async function () {
