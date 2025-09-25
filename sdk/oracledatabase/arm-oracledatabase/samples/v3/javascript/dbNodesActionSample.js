35f95f69f4d4b30ec287c6be266412570a3321d1
azure-sdk <azuresdk@microsoft.com>
2025-09-25 06:43:24 +0000

    Configurations:  'specification/oracle/Oracle.Database.Management/tspconfig.yaml', API Version: 2025-09-01, SDK Release Type: stable, and CommitSHA: '9158dabc895f6b48e4b04a84bd5070e8301caf23' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5383160 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/oracledatabase/arm-oracledatabase/samples/v3/javascript/dbNodesActionSample.js
@@ -0,0 +1,64 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { OracleDatabaseManagementClient } = require("@azure/arm-oracledatabase");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to vM actions on DbNode of VM Cluster by the provided filter
+ *
+ * @summary vM actions on DbNode of VM Cluster by the provided filter
+ * x-ms-original-file: 2025-09-01/DbNodes_Action_MaximumSet_Gen.json
+ */
+async function vmActionsOnDbNodesOfVMClusterGeneratedByMaximumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new OracleDatabaseManagementClient(credential, subscriptionId);
+  const result = await client.dbNodes.action("rgopenapi", "cloudvmcluster1", "abciderewdidsereq", {
+    action: "Start",
+  });
+  console.log(result);
+}
+
+/**
+ * This sample demonstrates how to vM actions on DbNode of VM Cluster by the provided filter
+ *
+ * @summary vM actions on DbNode of VM Cluster by the provided filter
+ * x-ms-original-file: 2025-09-01/DbNodes_Action_MinimumSet_Gen.json
+ */
+async function vmActionsOnDbNodesOfVMClusterGeneratedByMinimumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new OracleDatabaseManagementClient(credential, subscriptionId);
+  const result = await client.dbNodes.action(
+    "rgopenapi",
+    "cloudvmcluster1",
+    "adfedefeewwevkieviect",
+    { action: "Start" },
+  );
+  console.log(result);
+}
+
+/**
+ * This sample demonstrates how to vM actions on DbNode of VM Cluster by the provided filter
+ *
+ * @summary vM actions on DbNode of VM Cluster by the provided filter
+ * x-ms-original-file: 2025-09-01/dbNodes_action.json
+ */
+async function dbNodesAction() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new OracleDatabaseManagementClient(credential, subscriptionId);
+  const result = await client.dbNodes.action("rg000", "cluster1", "ocid1....aaaaaa", {
+    action: "Start",
+  });
+  console.log(result);
+}
+
+async function main() {
+  await vmActionsOnDbNodesOfVMClusterGeneratedByMaximumSetRule();
+  await vmActionsOnDbNodesOfVMClusterGeneratedByMinimumSetRule();
+  await dbNodesAction();
+}
+
+main().catch(console.error);
