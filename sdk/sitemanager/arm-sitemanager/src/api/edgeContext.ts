d63d5df086e88151637db6d1f94932b4dd51170a
azure-sdk <azuresdk@microsoft.com>
2025-09-29 06:34:33 +0000

    Post release automated changes for azure-arm-sitemanager

--- sdk/sitemanager/arm-sitemanager/src/api/edgeContext.ts
+++ sdk/sitemanager/arm-sitemanager/src/api/edgeContext.ts
@@ -34,7 +34,7 @@ export function createEdge(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-sitemanager/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-sitemanager/1.0.1`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
