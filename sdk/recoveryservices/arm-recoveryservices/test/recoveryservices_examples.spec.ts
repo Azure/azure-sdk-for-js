bd794c7cea407cbdc86f2aa2c92743ae8fe0081e
ZiWei Chen <v-ziweichen@microsoft.com>
2025-09-30 15:14:15 +0800

    update

--- sdk/recoveryservices/arm-recoveryservices/test/recoveryservices_examples.spec.ts
+++ sdk/recoveryservices/arm-recoveryservices/test/recoveryservices_examples.spec.ts
@@ -9,7 +9,7 @@
 import type { RecorderStartOptions } from "@azure-tools/test-recorder";
 import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
-import { RecoveryServicesClient } from "../../src/recoveryServicesClient.js";
+import { RecoveryServicesClient } from "../src/recoveryServicesClient.js";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
 
 const replaceableVariables: Record<string, string> = {
