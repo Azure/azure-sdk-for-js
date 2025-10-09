5d324e18598ec00af9d6074d009d87368f932f00
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-10-09 14:26:34 +0800

    Update mongoClusterManagementContext.ts

--- sdk/mongocluster/arm-mongocluster/src/api/mongoClusterManagementContext.ts
+++ sdk/mongocluster/arm-mongocluster/src/api/mongoClusterManagementContext.ts
@@ -36,7 +36,7 @@ export function createMongoClusterManagement(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-mongocluster/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-mongocluster/1.1.0-beta.2`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
