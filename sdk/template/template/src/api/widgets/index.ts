f3b73075d39084de4468603ff5ffbfe10fb85312
Maor Leger <maorleger@microsoft.com>
2025-10-09 20:12:50 +0000

    [template] Use contoso widget manager

---
+++ sdk/template/template/src/api/widgets/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  listWidgets,
+  deleteWidget,
+  createOrUpdateWidget,
+  getWidgetOperationStatus,
+  getWidget,
+} from "./operations.js";
+export {
+  WidgetsListWidgetsOptionalParams,
+  WidgetsDeleteWidgetOptionalParams,
+  WidgetsCreateOrUpdateWidgetOptionalParams,
+  WidgetsGetWidgetOperationStatusOptionalParams,
+  WidgetsGetWidgetOptionalParams,
+} from "./options.js";
