aef65ca9f2b9edc1e6425fc9aad731efab8c3d4a
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-10-14 12:11:00 +0800

    update

--- sdk/appconfiguration/app-configuration/test/public/utils/testHelpers.ts
+++ sdk/appconfiguration/app-configuration/test/public/utils/testHelpers.ts
@@ -167,18 +167,6 @@ export async function toSortedLabelsArray(
   for await (const label of pagedIterator) {
     labels.push(label);
   }
-
-  /**
-   * Comment out the sanity check. During the live test, there might be other parallel tests adding labels, which causes the sanity check to fail.
-   */
-  // let labelsViaPageIterator: SettingLabel[] = [];
-
-  // for await (const page of pagedIterator.byPage()) {
-  //   labelsViaPageIterator = labelsViaPageIterator.concat(page.items);
-  // }
-
-  // assert.deepEqual(labels, labelsViaPageIterator);
-
   labels.sort((a, b) => (compareFn ? compareFn(a, b) : `${a.name}`.localeCompare(`${b.name}`)));
 
   return labels;
