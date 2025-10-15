a0a2c72db620c979db6f34bf25f54b4624e46a13
azure-sdk <azuresdk@microsoft.com>
2025-10-15 16:50:32 +0000

    Post release automated changes for azure-arm-dataprotection

--- sdk/dataprotection/arm-dataprotection/CHANGELOG.md
+++ sdk/dataprotection/arm-dataprotection/CHANGELOG.md
@@ -1,5 +1,15 @@
 # Release History
 
+## 4.0.1 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
 ## 4.0.0 (2025-10-13)
 
 ### Features Added
--- sdk/dataprotection/arm-dataprotection/src/api/dataProtectionContext.ts
+++ sdk/dataprotection/arm-dataprotection/src/api/dataProtectionContext.ts
@@ -36,7 +36,7 @@ export function createDataProtection(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-dataprotection/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-dataprotection/4.0.1`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
