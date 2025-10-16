8170a1fbb5add6737e333fb9f4c595e05a188751
Maor Leger <maorleger@users.noreply.github.com>
2025-10-16 10:44:18 -0700

    [template] Use contoso widget analytics (#36158)
    Using modular for codegen in our templates matches our guidance to
    partners
    
    Resolves #36153

--- sdk/template/template/test/snippets.spec.ts
+++ sdk/template/template/test/snippets.spec.ts
@@ -1,20 +1,25 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { DefaultAzureCredential } from "@azure/identity";
+import { WidgetManagerClient } from "../src/index.js";
+import { DefaultAzureCredential, InteractiveBrowserCredential } from "@azure/identity";
 import { setLogLevel } from "@azure/logger";
-import { ConfigurationClient } from "../src/index.js";
 import { describe, it } from "vitest";
 
-describe("snippets", function () {
-  it("ReadmeSampleCreateClient", () => {
-    const client = new ConfigurationClient(
-      process.env.ENDPOINT ?? "<app configuration endpoint>",
-      new DefaultAzureCredential(),
-    );
+describe("snippets", () => {
+  it("ReadmeSampleCreateClient_Node", async () => {
+    const client = new WidgetManagerClient("<endpoint>", new DefaultAzureCredential());
   });
 
-  it("SetLogLevel", () => {
-    setLogLevel("verbose");
+  it("ReadmeSampleCreateClient_Browser", async () => {
+    const credential = new InteractiveBrowserCredential({
+      tenantId: "<YOUR_TENANT_ID>",
+      clientId: "<YOUR_CLIENT_ID>",
+    });
+    const client = new WidgetManagerClient("<endpoint>", credential);
+  });
+
+  it("SetLogLevel", async () => {
+    setLogLevel("info");
   });
 });
