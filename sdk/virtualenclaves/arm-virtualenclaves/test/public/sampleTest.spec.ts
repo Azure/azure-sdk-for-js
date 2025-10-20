2c0e5de2172fa87c0eeabe706d9d8bcf1a6c29c9
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-10-20 15:50:29 +0800

    Update sampleTest.spec.ts

--- sdk/virtualenclaves/arm-virtualenclaves/test/public/sampleTest.spec.ts
+++ sdk/virtualenclaves/arm-virtualenclaves/test/public/sampleTest.spec.ts
@@ -1,20 +1,26 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import type { Recorder } from "@azure-tools/test-recorder";
-import { createRecorder } from "./utils/recordedClient.js";
-import { assert, beforeEach, afterEach, it, describe } from "vitest";
+// import type { Recorder } from "@azure-tools/test-recorder";
+// import { createRecorder } from "./utils/recordedClient.js";
+import {
+  assert,
+  // beforeEach,
+  // afterEach,
+  it,
+  describe,
+} from "vitest";
 
 describe("My test", () => {
-  let recorder: Recorder;
+  // let recorder: Recorder;
 
-  beforeEach(async function (ctx) {
-    recorder = await createRecorder(ctx);
-  });
+  // beforeEach(async function (ctx) {
+  //   recorder = await createRecorder(ctx);
+  // });
 
-  afterEach(async function () {
-    await recorder.stop();
-  });
+  // afterEach(async function () {
+  //   await recorder.stop();
+  // });
 
   it("sample test", async function () {
     assert.equal(1, 1);
