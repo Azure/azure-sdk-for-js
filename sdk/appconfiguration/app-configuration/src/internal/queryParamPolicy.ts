48b980bd34d818aa31ea3aef3f78881ab37792d5
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-10-27 13:15:08 +0800

    catch TypeError if request url is invalid

--- sdk/appconfiguration/app-configuration/src/internal/queryParamPolicy.ts
+++ sdk/appconfiguration/app-configuration/src/internal/queryParamPolicy.ts
@@ -7,6 +7,7 @@ import type {
   PipelineResponse,
   SendRequest,
 } from "@azure/core-rest-pipeline";
+import { logger } from "../logger.js";
 
 /**
  * Creates a PipelinePolicy that normalizes query parameters:
@@ -18,42 +19,48 @@ export function queryParamPolicy(): PipelinePolicy {
   return {
     name: "queryParamPolicy",
     async sendRequest(request: PipelineRequest, next: SendRequest): Promise<PipelineResponse> {
-      const originalUrl: string = request.url;
-      const url = new URL(originalUrl);
+      try {
+        const originalUrl: string = request.url;
+        const url = new URL(originalUrl);
 
-      if (url.search === "") {
-        return next(request);
-      }
-
-      const params: ParamEntry[] = [];
-      for (const entry of url.search.substring(1).split("&")) {
-        if (entry === "") {
-          continue;
+        if (url.search === "") {
+          return next(request);
         }
-        const equalIndex = entry.indexOf("=");
-        const name = equalIndex === -1 ? entry : entry.substring(0, equalIndex);
-        const value = equalIndex === -1 ? "" : entry.substring(equalIndex + 1);
-        params.push({ lowercaseName: name.toLowerCase(), value });
-      }
 
-      // Modern JavaScript Array.prototype.sort is stable
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#sort_stability
-      params.sort((a, b) => {
-        if (a.lowercaseName < b.lowercaseName) {
-          return -1;
-        } else if (a.lowercaseName > b.lowercaseName) {
-          return 1;
+        const params: ParamEntry[] = [];
+        for (const entry of url.search.substring(1).split("&")) {
+          if (entry === "") {
+            continue;
+          }
+          const equalIndex = entry.indexOf("=");
+          const name = equalIndex === -1 ? entry : entry.substring(0, equalIndex);
+          const value = equalIndex === -1 ? "" : entry.substring(equalIndex + 1);
+          params.push({ lowercaseName: name.toLowerCase(), value });
         }
-        return 0;
-      });
 
-      const newSearchParams: string = params
-        .map(({ lowercaseName, value }) => `${lowercaseName}=${value}`)
-        .join("&");
+        // Modern JavaScript Array.prototype.sort is stable
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#sort_stability
+        params.sort((a, b) => {
+          if (a.lowercaseName < b.lowercaseName) {
+            return -1;
+          } else if (a.lowercaseName > b.lowercaseName) {
+            return 1;
+          }
+          return 0;
+        });
 
-      const newUrl = url.origin + url.pathname + "?" + newSearchParams + url.hash;
-      if (newUrl !== originalUrl) {
-        request.url = newUrl;
+        const newSearchParams: string = params
+          .map(({ lowercaseName, value }) => `${lowercaseName}=${value}`)
+          .join("&");
+
+        const newUrl = url.origin + url.pathname + "?" + newSearchParams + url.hash;
+        if (newUrl !== originalUrl) {
+          request.url = newUrl;
+        }
+      } catch (e) {
+        if (e instanceof TypeError) {
+          logger.warning(`"[queryParamPolicy] Could not parse URL: ${request.url}"`);
+        }
       }
 
       return next(request);
