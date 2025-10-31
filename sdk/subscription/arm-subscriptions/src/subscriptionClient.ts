dab03f64db317c28c3c163e8a803edb508d47fca
azure-sdk <azuresdk@microsoft.com>
2025-10-31 09:18:35 +0000

    Post release automated changes for azure-arm-subscriptions

--- sdk/subscription/arm-subscriptions/src/subscriptionClient.ts
+++ sdk/subscription/arm-subscriptions/src/subscriptionClient.ts
@@ -49,7 +49,7 @@ export class SubscriptionClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-subscriptions/6.0.0`;
+    const packageDetails = `azsdk-js-arm-subscriptions/6.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
