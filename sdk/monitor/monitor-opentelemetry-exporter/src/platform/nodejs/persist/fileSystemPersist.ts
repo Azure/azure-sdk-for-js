72f0219a66856c02e96db57b5dba64d71b86ab55
Radhika Gupta <guptaradhika@microsoft.com>
2025-10-31 13:58:28 -0700

    Fix formatting

--- sdk/monitor/monitor-opentelemetry-exporter/src/platform/nodejs/persist/fileSystemPersist.ts
+++ sdk/monitor/monitor-opentelemetry-exporter/src/platform/nodejs/persist/fileSystemPersist.ts
@@ -3,7 +3,7 @@
 
 import { tmpdir, userInfo } from "node:os";
 import { basename, join, dirname } from "node:path";
-import { createHash } from 'node:crypto';
+import { createHash } from "node:crypto";
 import { diag } from "@opentelemetry/api";
 import type { PersistentStorage } from "../../../types.js";
 import { FileAccessControl } from "./fileAccessControl.js";
@@ -58,6 +58,10 @@ export class FileSystemPersist implements PersistentStorage {
       );
     }
     if (this._enabled) {
+      this._tempDirectory = getStorageDirectory(
+        this._instrumentationKey,
+        this._options?.storageDirectory,
+      );
       this._tempDirectory = getStorageDirectory(
         this._instrumentationKey,
         this._options?.storageDirectory,
