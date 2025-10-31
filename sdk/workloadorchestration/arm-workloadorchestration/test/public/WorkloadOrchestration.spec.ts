073dcb3d98c86e6db78c7a737325f835ea7cf142
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2025-10-31 15:09:56 +0800

    test for workloadorchestration

--- sdk/workloadorchestration/arm-workloadorchestration/test/public/WorkloadOrchestration.spec.ts
+++ sdk/workloadorchestration/arm-workloadorchestration/test/public/WorkloadOrchestration.spec.ts
@@ -12,6 +12,10 @@ import { assert, beforeEach, afterEach, it, describe } from "vitest";
 import { createRecorder } from "./utils/recordedClient.js";
 import { WorkloadOrchestrationManagementClient } from "../../src/workloadOrchestrationManagementClient.js";
 
+import * as logger from '@azure/logger';  
+
+logger.setLogLevel('verbose');  
+
 export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
@@ -44,5 +48,60 @@ describe("WorkloadOrchestration test", () => {
       resArray.push(item);
     }
     assert.ok(resArray);
+  });
+
+  it.only("solutionTemplates createVersion test", async function () {
+    const resourceGroupName = "test-rg";
+    const solutionTemplateName = "test-solution-template";
+    const schemaName = "test-schema";
+    const schemaVersion = "1.0.0";
+    const version = "1.0.0";
+
+    const configurationsStr = `schema:
+  name: ${schemaName}
+  version: ${schemaVersion}
+configs:
+  AppName: Hotmelt
+  TemperatureRangeMax: \${{$val(TemperatureRangeMax)}}
+  ErrorThreshold: \${{$val(ErrorThreshold)}}
+  HealthCheckEndpoint: \${{$val(HealthCheckEndpoint)}}
+  EnableLocalLog: \${{$val(EnableLocalLog)}}
+  AgentEndpoint: \${{$val(AgentEndpoint)}}
+  HealthCheckEnabled: \${{$val(HealthCheckEnabled)}}
+  ApplicationEndpoint: \${{$val(ApplicationEndpoint)}}`;
+
+    const result = await client.solutionTemplates.createVersion(
+      resourceGroupName,
+      solutionTemplateName,
+      {
+        version: version,
+        solutionTemplateVersion: {
+          properties: {
+            configurations: configurationsStr,
+            specification: {
+              components: [
+                {
+                  name: "helmcomponent",
+                  type: "helm.v3",
+                  properties: {
+                    chart: {
+                      repo: "ghcr.io/eclipse-symphony/tests/helm/simple-chart",
+                      version: "0.3.0",
+                      wait: true,
+                      timeout: "5m",
+                    },
+                  },
+                },
+              ],
+            },
+            orchestratorType: "TO",
+          },
+        },
+      },
+    );
+
+    assert.ok(result);
+    assert.ok(result.properties);
+    console.log("Solution template version created successfully:", result.name);
   });
 });
