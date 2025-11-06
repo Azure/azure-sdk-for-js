c03baef333aa7ad31b9d32b0381e518d9e5d4036
azure-sdk <azuresdk@microsoft.com>
2025-11-06 18:42:41 +0000

    Configurations:  'specification/computelimit/resource-manager/Microsoft.ComputeLimit/ComputeLimit/tspconfig.yaml', API Version: 2025-08-15, SDK Release Type: stable, and CommitSHA: '257777bd5dd2cec7c658f4517b030ae4d65e0812' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5543511 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/computelimit/arm-computelimit/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Release History
+    
+## 1.0.0 (2025-11-06)
+
+### Features Added
+
+This is the first stable version with the package of @azure/arm-computelimit
---
+++ sdk/computelimit/arm-computelimit/src/models/index.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  Operation,
+  OperationDisplay,
+  KnownOrigin,
+  Origin,
+  KnownActionType,
+  ActionType,
+  ErrorResponse,
+  ErrorDetail,
+  ErrorAdditionalInfo,
+  GuestSubscription,
+  GuestSubscriptionProperties,
+  KnownResourceProvisioningState,
+  ResourceProvisioningState,
+  ProxyResource,
+  Resource,
+  SystemData,
+  KnownCreatedByType,
+  CreatedByType,
+  SharedLimit,
+  SharedLimitProperties,
+  LimitName,
+  KnownVersions,
+} from "./models.js";
