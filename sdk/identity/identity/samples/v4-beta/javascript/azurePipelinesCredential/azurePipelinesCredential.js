af026379949d2d25b9a1375a60d1682c33107ac2
azure-sdk <azuresdk@microsoft.com>
2025-11-06 21:36:08 +0000

    Post release automated changes for azure-identity

---
+++ sdk/identity/identity/samples/v4-beta/javascript/azurePipelinesCredential/azurePipelinesCredential.js
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * @summary Authenticates using AzurePipelinesCredential
+ */
+const { AzurePipelinesCredential } = require("@azure/identity");
+const { KeyClient } = require("@azure/keyvault-keys");
+
+async function main() {
+  const clientId = process.env.AZURE_SERVICE_CONNECTION_CLIENT_ID;
+  const tenantId = process.env.AZURE_SERVICE_CONNECTION_TENANT_ID;
+  const serviceConnectionId = process.env.AZURE_SERVICE_CONNECTION_ID;
+  const systemAccessToken = process.env.SYSTEM_ACCESSTOKEN;
+  if (systemAccessToken) {
+    const credential = new AzurePipelinesCredential(
+      tenantId,
+      clientId,
+      serviceConnectionId,
+      systemAccessToken,
+    );
+
+    const keyVaultUrl = `https://key-vault-name.vault.azure.net`;
+    const client = new KeyClient(keyVaultUrl, credential);
+
+    // Retrieving the properties of the existing keys in that specific Key Vault.
+    console.log(await client.listPropertiesOfKeys().next());
+  }
+}
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
---
+++ sdk/identity/identity/samples/v4-beta/javascript/interactiveBrowserCredential.js
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * @summary Authenticates using Interactive Browser Credential
+ */
+
+const { InteractiveBrowserCredential } = require("@azure/identity");
+
+require("dotenv").config();
+
+const clientId = process.env.AZURE_CLIENT_ID; // The app registration client Id in the Microsoft Entra tenant
+const tenantId = process.env.AZURE_TENANT_ID; // The tenant ID in Microsoft Entra ID
+
+async function main() {
+  const credential = new InteractiveBrowserCredential({
+    clientId,
+    tenantId,
+    browserCustomizationOptions: {
+      successMessage: "Authentication Completed. You can close the browser.",
+    },
+  });
+
+  const token = await credential.getToken("https://storage.azure.com/.default");
+  console.log(
+    "InteractiveBrowserCredential: Successfully got a token with expiry time:",
+    token.expiresOnTimestamp,
+  );
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
