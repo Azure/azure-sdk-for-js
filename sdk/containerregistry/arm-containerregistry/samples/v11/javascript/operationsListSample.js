043bf6a8659275aad8ef2a9ba05bccc1e6baece3
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-11-06 10:43:40 +0800

    [mgmt] containerregistry release with generated swagger (#36439)
    https://github.com/Azure/sdk-release-request/issues/6692

---
+++ sdk/containerregistry/arm-containerregistry/samples/v11/javascript/operationsListSample.js
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { ContainerRegistryManagementClient } = require("@azure/arm-containerregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to List the operations for the provider
+ *
+ * @summary List the operations for the provider
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/stable/2025-11-01/examples/OperationList.json
+ */
+async function operationList() {
+  const subscriptionId =
+    process.env["CONTAINERREGISTRY_SUBSCRIPTION_ID"] || "00000000-0000-0000-0000-000000000000";
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.operations.list()) {
+    resArray.push(item);
+  }
+  console.log(resArray);
+}
+
+async function main() {
+  await operationList();
+}
+
+main().catch(console.error);
