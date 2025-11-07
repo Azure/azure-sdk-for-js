d73f7969a064f231b6b059b7e0417cc358a32be0
Glenn Harper <glharper@microsoft.com>
2025-11-07 17:58:35 -0500

    feedback

--- sdk/ai/ai-projects/src/static-helpers/serialization/serialize-record.ts
+++ sdk/ai/ai-projects/src/static-helpers/serialization/serialize-record.ts
@@ -3,10 +3,10 @@
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types
 export function serializeRecord(item: any, excludes?: string[], serializer?: (item: any) => any) {
-  excludes = excludes ?? [];
+  const nextExcludes = excludes ?? [];
   const res: any = {};
   for (const key of Object.keys(item)) {
-    if (excludes.includes(key) || item[key] === undefined) {
+    if (nextExcludes.includes(key) || item[key] === undefined) {
       continue;
     }
     if (serializer) {
