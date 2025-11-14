dd29506964fed31d3436656dbadc9b0400e1a9b0
azure-sdk <azuresdk@microsoft.com>
2025-11-14 11:42:46 +0000

    Post release automated changes for azure-arm-paloaltonetworksngfw

--- sdk/paloaltonetworksngfw/arm-paloaltonetworksngfw/src/paloAltoNetworksCloudngfw.ts
+++ sdk/paloaltonetworksngfw/arm-paloaltonetworksngfw/src/paloAltoNetworksCloudngfw.ts
@@ -92,7 +92,7 @@ export class PaloAltoNetworksCloudngfw extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-paloaltonetworksngfw/1.2.0`;
+    const packageDetails = `azsdk-js-arm-paloaltonetworksngfw/1.2.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
