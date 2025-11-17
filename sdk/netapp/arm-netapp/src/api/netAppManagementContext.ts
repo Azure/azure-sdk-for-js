815d5a8a9cb7195b7ffd7e09b9df8331d50034a0
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-11-17 10:20:52 +0800

    Update user agent version in netAppManagementContext

--- sdk/netapp/arm-netapp/src/api/netAppManagementContext.ts
+++ sdk/netapp/arm-netapp/src/api/netAppManagementContext.ts
@@ -36,7 +36,7 @@ export function createNetAppManagement(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-netapp/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-netapp/22.0.0`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
