b5f9dfaa41cce346e645a0b1471d71feea46a7e0
Maor Leger <maorleger@microsoft.com>
2025-11-20 15:50:25 +0000

    update package version and such

--- sdk/search/search-documents/src/searchIndexer/api/searchIndexerContext.ts
+++ sdk/search/search-documents/src/searchIndexer/api/searchIndexerContext.ts
@@ -26,7 +26,7 @@ export function createSearchIndexer(
 ): SearchIndexerContext {
   const endpointUrl = options.endpoint ?? String(endpointParam);
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-search-documents/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-search-documents/12.3.0-beta.1`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
