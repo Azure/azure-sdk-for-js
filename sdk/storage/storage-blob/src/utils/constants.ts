2521d74d442f6ba9e156f914ed337e677f64c50b
azure-sdk <azuresdk@microsoft.com>
2025-12-01 02:34:06 +0000

    Post release automated changes for azure-storage-blob

--- sdk/storage/storage-blob/src/utils/constants.ts
+++ sdk/storage/storage-blob/src/utils/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export const SDK_VERSION: string = "12.30.0-beta.1";
+export const SDK_VERSION: string = "12.30.0-beta.2";
 export const SERVICE_VERSION: string = "2026-02-06";
 
 export const BLOCK_BLOB_MAX_UPLOAD_BLOB_BYTES: number = 256 * 1024 * 1024; // 256MB
