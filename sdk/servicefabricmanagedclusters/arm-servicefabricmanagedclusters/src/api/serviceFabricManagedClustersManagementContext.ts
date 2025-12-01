aadbe9c3b9766ad550a58c1b637bf59e35d24618
azure-sdk <azuresdk@microsoft.com>
2025-12-01 18:15:07 +0000

    Post release automated changes for azure-arm-servicefabricmanagedclusters

--- sdk/servicefabricmanagedclusters/arm-servicefabricmanagedclusters/src/api/serviceFabricManagedClustersManagementContext.ts
+++ sdk/servicefabricmanagedclusters/arm-servicefabricmanagedclusters/src/api/serviceFabricManagedClustersManagementContext.ts
@@ -36,7 +36,7 @@ export function createServiceFabricManagedClustersManagement(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-servicefabricmanagedclusters/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-servicefabricmanagedclusters/1.0.0-beta.7`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
