bf9eeaf2ad608be786c9a2543fff222447df3b52
chienyuanchang <ds.chienyuanchang@gmail.com>
2025-12-05 15:09:51 -0800

    [SDK-UPDATE] 4af871c7b6129cc1e2537259c9a73789e55c0703

--- sdk/contentunderstanding/ai-content-understanding-rest/src/models/models.ts
+++ sdk/contentunderstanding/ai-content-understanding-rest/src/models/models.ts
@@ -1333,9 +1333,9 @@ export function audioVisualContentDeserializer(item: any): AudioVisualContent {
       : item["cameraShotTimesMs"].map((p: any) => {
           return p;
         }),
-    keyFrameTimesMs: !item["KeyFrameTimesMs"]
-      ? item["KeyFrameTimesMs"]
-      : item["KeyFrameTimesMs"].map((p: any) => {
+    keyFrameTimesMs: !item["keyFrameTimesMs"]
+      ? item["keyFrameTimesMs"]
+      : item["keyFrameTimesMs"].map((p: any) => {
           return p;
         }),
     transcriptPhrases: !item["transcriptPhrases"]
@@ -1827,9 +1827,6 @@ export function contentFieldDefinitionRecordSerializer(
 export function contentFieldDefinitionRecordDeserializer(
   item: Record<string, any>,
 ): Record<string, ContentFieldDefinition> {
-  if (!item) {
-    return item;
-  }
   const result: Record<string, any> = {};
   Object.keys(item).map((key) => {
     result[key] = !item[key]
@@ -2037,16 +2034,12 @@ export interface SupportedModels {
 
 export function supportedModelsDeserializer(item: any): SupportedModels {
   return {
-    completion: !item["completion"]
-      ? item["completion"]
-      : item["completion"].map((p: any) => {
-          return p;
-        }),
-    embedding: !item["embedding"]
-      ? item["embedding"]
-      : item["embedding"].map((p: any) => {
-          return p;
-        }),
+    completion: item["completion"].map((p: any) => {
+      return p;
+    }),
+    embedding: item["embedding"].map((p: any) => {
+      return p;
+    }),
   };
 }
 
