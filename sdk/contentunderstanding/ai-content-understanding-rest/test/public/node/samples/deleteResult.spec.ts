18e4754ccbd64d66075462accbef12b21dcbe3e7
chienyuanchang <ds.chienyuanchang@gmail.com>
2025-12-11 16:41:07 -0800

    formatting

--- sdk/contentunderstanding/ai-content-understanding-rest/test/public/node/samples/deleteResult.spec.ts
+++ sdk/contentunderstanding/ai-content-understanding-rest/test/public/node/samples/deleteResult.spec.ts
@@ -39,7 +39,7 @@ describe("Sample: deleteResult", () => {
     const result = await poller.pollUntilDone();
 
     // Get the operation ID
-     
+
     const operationLocation = (poller as any).operationState?.config?.operationLocation;
     let operationId: string | undefined;
 
