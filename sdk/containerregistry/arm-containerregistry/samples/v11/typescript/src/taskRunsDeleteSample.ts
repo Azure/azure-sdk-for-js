bce3c5ee8ceb881d1e0894f057a88f93035f9a3f
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-12-18 15:39:23 -0800

    [EngSys] remove leftover `dev-tool run vendored` (#36908)
    Those dev dependencies are already included in individual package.json.

--- sdk/containerregistry/arm-containerregistry/samples/v11/typescript/src/taskRunsDeleteSample.ts
+++ sdk/containerregistry/arm-containerregistry/samples/v11/typescript/src/taskRunsDeleteSample.ts
@@ -9,7 +9,7 @@ import "dotenv/config";
  * This sample demonstrates how to Deletes a specified task run resource.
  *
  * @summary Deletes a specified task run resource.
- * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/RegistryTasks/preview/2019-06-01-preview/examples/TaskRunsDelete.json
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/Registry/preview/2019-06-01-preview/examples/TaskRunsDelete.json
  */
 async function taskRunsDelete(): Promise<void> {
   const subscriptionId =
