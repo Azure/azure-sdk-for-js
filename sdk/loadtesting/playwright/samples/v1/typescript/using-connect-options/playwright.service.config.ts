0599e5d393610c72f4556ae73c0d2c2d29268102
guptakashish <guptakashish@microsoft.com>
2025-12-23 14:13:37 +0530

    Added @azure/storage-blob dependency to @azure/playwright package

--- sdk/loadtesting/playwright/samples/v1/typescript/using-connect-options/playwright.service.config.ts
+++ sdk/loadtesting/playwright/samples/v1/typescript/using-connect-options/playwright.service.config.ts
@@ -9,17 +9,5 @@ export default defineConfig(
   config,
   createAzurePlaywrightConfig(config, {
     credential,
-    serviceAuthType: "ENTRA_ID",
   }),
-  {
-    /* 
-    Playwright service reporter is added by default.
-    This will override any reporter options specified in the base playwright config.
-    If you are using more reporters, please update your configuration accordingly.
-    */
-    reporter: [
-      ["html"], // Generate HTML report
-      ["@azure/playwright/reporter"], // Upload HTML report to Azure
-    ],
-  },
 );
