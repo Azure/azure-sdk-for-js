592b0229487fafb884de220a8d79cc73edfbfd2b
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-12-24 16:54:03 +0800

    [mgmt] containerregistry release (#36857)
    https://github.com/Azure/sdk-release-request/issues/6769

---
+++ sdk/containerregistry/arm-containerregistry/samples/v12-beta/javascript/taskRunsCreateSample.js
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { ContainerRegistryManagementClient } = require("@azure/arm-containerregistry");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Creates a task run for a container registry with the specified parameters.
+ *
+ * @summary Creates a task run for a container registry with the specified parameters.
+ * x-ms-original-file: specification/containerregistry/resource-manager/Microsoft.ContainerRegistry/RegistryTasks/preview/2025-03-01-preview/examples/TaskRunsCreate.json
+ */
+async function taskRunsCreate() {
+  const subscriptionId =
+    process.env["CONTAINERREGISTRY_SUBSCRIPTION_ID"] || "4385cf00-2d3a-425a-832f-f4285b1c9dce";
+  const resourceGroupName = process.env["CONTAINERREGISTRY_RESOURCE_GROUP"] || "myResourceGroup";
+  const registryName = "myRegistry";
+  const taskRunName = "myRun";
+  const taskRun = {
+    forceUpdateTag: "test",
+    runRequest: {
+      type: "EncodedTaskRunRequest",
+      credentials: {},
+      encodedTaskContent: "c3RlcHM6IAogIC0gY21kOiB7eyAuVmFsdWVzLmNvbW1hbmQgfX0K",
+      encodedValuesContent: "Y29tbWFuZDogYmFzaCBlY2hvIHt7LlJ1bi5SZWdpc3RyeX19Cg==",
+      platform: { architecture: "amd64", os: "Linux" },
+      values: [],
+    },
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new ContainerRegistryManagementClient(credential, subscriptionId);
+  const result = await client.taskRuns.beginCreateAndWait(
+    resourceGroupName,
+    registryName,
+    taskRunName,
+    taskRun,
+  );
+  console.log(result);
+}
+
+async function main() {
+  await taskRunsCreate();
+}
+
+main().catch(console.error);
