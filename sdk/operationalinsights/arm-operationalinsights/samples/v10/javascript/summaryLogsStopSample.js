4c279c457bc53f5cf48647497881bd63f4d57081
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2025-12-24 15:40:06 +0800

    [mgmt] operationalinsights release (#36883)
    https://github.com/Azure/sdk-release-request/issues/6734
    
    breaking link https://github.com/Azure/azure-rest-api-specs/pull/36056

---
+++ sdk/operationalinsights/arm-operationalinsights/samples/v10/javascript/summaryLogsStopSample.js
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { OperationalInsightsManagementClient } = require("@azure/arm-operationalinsights");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Stops an active Summary rule.
+ *
+ * @summary Stops an active Summary rule.
+ * x-ms-original-file: specification/operationalinsights/resource-manager/Microsoft.OperationalInsights/OperationalInsights/stable/2025-07-01/examples/SummaryLogsStop.json
+ */
+async function summaryLogsStop() {
+  const subscriptionId =
+    process.env["OPERATIONALINSIGHTS_SUBSCRIPTION_ID"] || "11111111-1111-1111-1111-111111111111";
+  const resourceGroupName = process.env["OPERATIONALINSIGHTS_RESOURCE_GROUP"] || "oiautorest6685";
+  const workspaceName = "oiautorest6685";
+  const summaryLogsName = "summarylogs1";
+  const credential = new DefaultAzureCredential();
+  const client = new OperationalInsightsManagementClient(credential, subscriptionId);
+  const result = await client.summaryLogsOperations.stop(
+    resourceGroupName,
+    workspaceName,
+    summaryLogsName,
+  );
+  console.log(result);
+}
+
+async function main() {
+  await summaryLogsStop();
+}
+
+main().catch(console.error);
---
+++ sdk/operationalinsights/arm-operationalinsights/samples/v10/javascript/workspacesReconcileNspSample.js
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { OperationalInsightsManagementClient } = require("@azure/arm-operationalinsights");
+const { DefaultAzureCredential } = require("@azure/identity");
+require("dotenv/config");
+
+/**
+ * This sample demonstrates how to Reconcile network security perimeter configuration for Workspace resource.
+ *
+ * @summary Reconcile network security perimeter configuration for Workspace resource.
+ * x-ms-original-file: specification/operationalinsights/resource-manager/Microsoft.OperationalInsights/OperationalInsights/stable/2025-07-01/examples/NSPForWorkspaces_Reconcile.json
+ */
+async function reconcileNspConfigForScheduledQueryRule() {
+  const subscriptionId =
+    process.env["OPERATIONALINSIGHTS_SUBSCRIPTION_ID"] || "00000000-1111-2222-3333-444444444444";
+  const resourceGroupName = process.env["OPERATIONALINSIGHTS_RESOURCE_GROUP"] || "exampleRG";
+  const workspaceName = "someWorkspace";
+  const networkSecurityPerimeterConfigurationName = "somePerimeterConfiguration";
+  const credential = new DefaultAzureCredential();
+  const client = new OperationalInsightsManagementClient(credential, subscriptionId);
+  const result = await client.workspaces.beginReconcileNSPAndWait(
+    resourceGroupName,
+    workspaceName,
+    networkSecurityPerimeterConfigurationName,
+  );
+  console.log(result);
+}
+
+async function main() {
+  await reconcileNspConfigForScheduledQueryRule();
+}
+
+main().catch(console.error);
