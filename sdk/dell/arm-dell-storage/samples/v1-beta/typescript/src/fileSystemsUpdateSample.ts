fb619069e1c163838455c93706d96eac016740fb
azure-sdk <azuresdk@microsoft.com>
2025-12-30 05:28:26 +0000

    Configurations:  'specification/dell/Dell.Storage.Management/tspconfig.yaml', API Version: 2025-03-21, SDK Release Type: stable, and CommitSHA: 'fb25c38a86b35dcf8bb03eaaa7844643bd92c5a2' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5709117 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/dell/arm-dell-storage/samples/v1-beta/typescript/src/fileSystemsUpdateSample.ts
+++ sdk/dell/arm-dell-storage/samples/v1-beta/typescript/src/fileSystemsUpdateSample.ts
@@ -8,17 +8,14 @@ import { DefaultAzureCredential } from "@azure/identity";
  * This sample demonstrates how to update a FileSystemResource
  *
  * @summary update a FileSystemResource
- * x-ms-original-file: 2025-03-21-preview/FileSystems_Update_MaximumSet_Gen.json
+ * x-ms-original-file: 2025-03-21/FileSystems_Update_MaximumSet_Gen.json
  */
 async function fileSystemsUpdateMaximumSetGen(): Promise<void> {
   const credential = new DefaultAzureCredential();
   const subscriptionId = "4B6E265D-57CF-4A9D-8B35-3CC68ED9D208";
   const client = new StorageClient(credential, subscriptionId);
   const result = await client.fileSystems.update("rgDell", "abcd", {
-    identity: {
-      type: "SystemAssigned,UserAssigned",
-      userAssignedIdentities: { key7645: {} },
-    },
+    identity: { type: "SystemAssigned,UserAssigned", userAssignedIdentities: { key7645: {} } },
     tags: { key6099: "ursbxlphfcguvntuevleacwq" },
     properties: {
       delegatedSubnetId: "bfpuabdz",
@@ -41,17 +38,14 @@ async function fileSystemsUpdateMaximumSetGen(): Promise<void> {
  * This sample demonstrates how to update a FileSystemResource
  *
  * @summary update a FileSystemResource
- * x-ms-original-file: 2025-03-21-preview/FileSystems_Update_MinimumSet_Gen.json
+ * x-ms-original-file: 2025-03-21/FileSystems_Update_MinimumSet_Gen.json
  */
 async function fileSystemsUpdateMinimumSetGen(): Promise<void> {
   const credential = new DefaultAzureCredential();
   const subscriptionId = "BF7E7352-2FE4-4163-9CF7-5FF8EC2E9B92";
   const client = new StorageClient(credential, subscriptionId);
   const result = await client.fileSystems.update("rgDell", "abcd", {
-    properties: {
-      delegatedSubnetId: "uqfvajvyltgmqvdnxhbrfqbpuey",
-      capacity: { current: "5" },
-    },
+    properties: { delegatedSubnetId: "uqfvajvyltgmqvdnxhbrfqbpuey", capacity: { current: "5" } },
   });
   console.log(result);
 }
