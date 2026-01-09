747c807429572f605f979457d069b575c1c4431d
azure-sdk <azuresdk@microsoft.com>
2026-01-09 22:04:57 +0000

    Configurations:  'specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml', API Version: 2022-12-01, SDK Release Type: stable, and CommitSHA: 'ea9e2ff730d93906cb1671950e2415fcec64da51' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5738343 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/contosowidgetmanager/contoso-widgetmanager/src/models/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  WidgetSuite,
+  FakedSharedModel,
+  ResourceOperationStatusWidgetSuiteWidgetSuiteError,
+  OperationState,
+  KnownVersions,
+} from "./models.js";
