b06ceba6b205cc6d8d07e38a9ba9344d1c90ca40
Minh-Anh Phan <minhanhphan@microsoft.com>
2026-01-12 15:18:36 -0800

    update post resource script

--- sdk/identity/test-resources-post.ps1
+++ sdk/identity/test-resources-post.ps1
@@ -3,6 +3,7 @@
 
 # IMPORTANT: Do not invoke this file directly. Please instead run eng/New-TestResources.ps1 from the repository root.
 
+param(
   [Parameter()]
   [hashtable] $DeploymentOutputs,
 
