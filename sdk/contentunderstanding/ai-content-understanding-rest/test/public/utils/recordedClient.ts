455aff9fa60c786ce0165027d6d19225529827c0
chienyuanchang <ds.chienyuanchang@gmail.com>
2026-01-13 15:43:16 -0800

    [Sample] add grantCopyAuth test

--- sdk/contentunderstanding/ai-content-understanding-rest/test/public/utils/recordedClient.ts
+++ sdk/contentunderstanding/ai-content-understanding-rest/test/public/utils/recordedClient.ts
@@ -8,11 +8,23 @@ import * as MOCKS from "../../utils/constants.js";
 import { EnvVarKeys } from "../../utils/constants.js";
 
 const key = process.env[EnvVarKeys.KEY];
+const sourceResourceId = process.env[EnvVarKeys.SOURCE_RESOURCE_ID];
+const sourceRegion = process.env[EnvVarKeys.SOURCE_REGION];
+const targetEndpoint = process.env[EnvVarKeys.TARGET_ENDPOINT];
+const targetResourceId = process.env[EnvVarKeys.TARGET_RESOURCE_ID];
+const targetRegion = process.env[EnvVarKeys.TARGET_REGION];
+const targetKey = process.env[EnvVarKeys.TARGET_KEY];
 
 export const recorderOptions: RecorderStartOptions = {
   envSetupForPlayback: {
     CONTENTUNDERSTANDING_ENDPOINT: MOCKS.ENDPOINT,
     ...(key ? { CONTENTUNDERSTANDING_KEY: MOCKS.KEY } : {}),
+    CONTENTUNDERSTANDING_SOURCE_RESOURCE_ID: MOCKS.SOURCE_RESOURCE_ID,
+    CONTENTUNDERSTANDING_SOURCE_REGION: MOCKS.SOURCE_REGION,
+    CONTENTUNDERSTANDING_TARGET_ENDPOINT: MOCKS.TARGET_ENDPOINT,
+    CONTENTUNDERSTANDING_TARGET_RESOURCE_ID: MOCKS.TARGET_RESOURCE_ID,
+    CONTENTUNDERSTANDING_TARGET_REGION: MOCKS.TARGET_REGION,
+    ...(targetKey ? { CONTENTUNDERSTANDING_TARGET_KEY: MOCKS.TARGET_KEY } : {}),
   },
   removeCentralSanitizers: [
     "AZSDK4001", // envSetupForPlayback handles endpoint sanitization
@@ -37,6 +49,22 @@ export const recorderOptions: RecorderStartOptions = {
       },
       {
         jsonPath: "$..sourceUrl",
+      },
+      {
+        jsonPath: "$.targetAzureResourceId",
+        value: MOCKS.TARGET_RESOURCE_ID,
+      },
+      {
+        jsonPath: "$.targetRegion",
+        value: MOCKS.TARGET_REGION,
+      },
+      {
+        jsonPath: "$.sourceAzureResourceId",
+        value: MOCKS.SOURCE_RESOURCE_ID,
+      },
+      {
+        jsonPath: "$.sourceRegion",
+        value: MOCKS.SOURCE_REGION,
       },
     ],
   },
