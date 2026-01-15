2ac4e24dfef07eab5afc9ff81c999a029cafe425
retry-recv <goubo2012@gmail.com>
2026-01-15 10:41:33 -0800

    fix comment

--- sdk/ai/ai-projects/src/static-helpers/serialization/serialize-record.ts
+++ sdk/ai/ai-projects/src/static-helpers/serialization/serialize-record.ts
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types
 export function serializeRecord(item: any, excludes?: string[], serializer?: (item: any) => any) {
-  excludes = excludes ?? [];
+  const nextExcludes = excludes ?? [];
   const res: any = {};
   for (const key of Object.keys(item)) {
-    if (excludes.includes(key) || item[key] === undefined) {
+    if (nextExcludes.includes(key) || item[key] === undefined) {
       continue;
     }
     if (serializer) {
