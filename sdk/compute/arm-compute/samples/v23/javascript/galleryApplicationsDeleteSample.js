4c2a8b6a0ecbb05a14f3b12b5adac6f135799348
ZiWei Chen <98569699+kazrael2119@users.noreply.github.com>
2026-01-21 17:23:00 +0800

    [mgmt] compute release (#37051)
    https://github.com/Azure/sdk-release-request/issues/6786

--- sdk/compute/arm-compute/samples/v23/javascript/galleryApplicationsDeleteSample.js
+++ sdk/compute/arm-compute/samples/v23/javascript/galleryApplicationsDeleteSample.js
@@ -9,7 +9,7 @@ require("dotenv/config");
  * This sample demonstrates how to Delete a gallery Application.
  *
  * @summary Delete a gallery Application.
- * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/GalleryRP/stable/2024-03-03/examples/galleryExamples/GalleryApplication_Delete.json
+ * x-ms-original-file: specification/compute/resource-manager/Microsoft.Compute/GalleryRP/stable/2025-03-03/examples/galleryExamples/GalleryApplication_Delete.json
  */
 async function deleteAGalleryApplication() {
   const subscriptionId = process.env["COMPUTE_SUBSCRIPTION_ID"] || "{subscription-id}";
