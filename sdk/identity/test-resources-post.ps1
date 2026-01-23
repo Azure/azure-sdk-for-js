f612070c11b66a351d1d31d7cd1070688176a219
Minh-Anh Phan <111523473+minhanh-phan@users.noreply.github.com>
2026-01-23 11:44:44 -0800

    [Identity] Restart VM Test (#37030)
    #35682
    Pipeline passing
    [here](https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5785829&view=logs&j=3e489e67-52af-50ac-8d79-40bc53186f53&t=f4eb0d6f-a606-5b3f-be16-91b0e2a43d8c)
    
    ---------
    
    Co-authored-by: Charles Lowell <10964656+chlowell@users.noreply.github.com>

--- sdk/identity/test-resources-post.ps1
+++ sdk/identity/test-resources-post.ps1
@@ -38,6 +38,7 @@ if (!$AdditionalParameters['deployMIResources']) {
 
 $MIClientId = $DeploymentOutputs['IDENTITY_USER_DEFINED_CLIENT_ID']
 $MIName = $DeploymentOutputs['IDENTITY_USER_DEFINED_IDENTITY_NAME']
+$MIId = $DeploymentOutputs['IDENTITY_USER_DEFINED_IDENTITY']
 $MIObjectId = $DeploymentOutputs['IDENTITY_USER_DEFINED_OBJECT_ID']
 $saAccountName = 'workload-identity-sa'
 $podName = $DeploymentOutputs['IDENTITY_AKS_POD_NAME']
@@ -91,10 +92,33 @@ docker push $image
 Write-Host "Deployed image to ACR"
 Write-Host "##[endgroup]"
 
+Write-Host "##[group]Deploying to VM"
+Write-Host "##vso[task.setvariable variable=IDENTITY_VM_PUBLIC_IP;]$($DeploymentOutputs['IDENTITY_VM_PUBLIC_IP'])"
+$uuid = [guid]::NewGuid().ToString()
+$vmScript = @"
+az login --identity --client-id $MIClientId --allow-no-subscriptions && \
+az acr login -n $($DeploymentOutputs['IDENTITY_ACR_NAME']) && \
+sudo docker run \
+-e IDENTITY_STORAGE_NAME=$storageName1 \
+-e IDENTITY_STORAGE_NAME_USER_ASSIGNED=$storageNameUserAssigned \
+-e IDENTITY_USER_DEFINED_IDENTITY=$MIId \
+-e IDENTITY_USER_DEFINED_CLIENT_ID=$MIClientId \
+-e IDENTITY_USER_DEFINED_OBJECT_ID=$MIObjectId \
+-p 80:8080 -d \
+$image && \
+/usr/bin/echo $uuid
+"@
+$output = az vm run-command invoke -g $identityResourceGroup -n $DeploymentOutputs['IDENTITY_VM_NAME'] --command-id RunShellScript --scripts "$vmScript" | Out-String
+Write-Host $output
+if (-not $output.Contains($uuid)) {
+  throw "couldn't start container on VM"
+}
+Write-Host "##[endgroup]"
+
 Write-Host "##[group]Deploying Azure Container Instance"
 az container create -g $identityResourceGroup -n $($DeploymentOutputs['IDENTITY_CONTAINER_INSTANCE_NAME']) --image $image `
-  --acr-identity $DeploymentOutputs['IDENTITY_USER_DEFINED_IDENTITY'] `
-  --assign-identity $DeploymentOutputs['IDENTITY_USER_DEFINED_IDENTITY'] `
+  --acr-identity $MIId `
+  --assign-identity $MIId `
   --cpu "1" `
   --ports 80 `
   --ip-address "Public" `
