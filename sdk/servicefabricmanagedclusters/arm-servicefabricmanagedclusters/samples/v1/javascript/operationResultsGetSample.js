aa827431396b1bc62a7e0c4896134f8a0bddb4e5
azure-sdk <azuresdk@microsoft.com>
2026-02-24 17:48:16 +0000

    Configurations:  'specification/servicefabricmanagedclusters/resource-manager/Microsoft.ServiceFabric/ServiceFabricManagedClusters/tspconfig.yaml', API Version: 2026-02-01, SDK Release Type: stable, and CommitSHA: '57a2504930224552a409d9435d523e631a6c93e2' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5919238 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/servicefabricmanagedclusters/arm-servicefabricmanagedclusters/samples/v1/javascript/operationResultsGetSample.js
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const {
+  ServiceFabricManagedClustersManagementClient,
+} = require("@azure/arm-servicefabricmanagedclusters");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get long running operation result.
+ *
+ * @summary get long running operation result.
+ * x-ms-original-file: 2026-02-01/OperationResultsGet_example.json
+ */
+async function getLongRunningOperationResult() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ServiceFabricManagedClustersManagementClient(credential, subscriptionId);
+  await client.operationResults.get("eastus", "00000000-0000-0000-0000-000000001234");
+}
+
+async function main() {
+  await getLongRunningOperationResult();
+}
+
+main().catch(console.error);
