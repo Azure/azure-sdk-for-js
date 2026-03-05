b217f35a96834ebef53c6d7524f49533972e40ea
Jeff Fisher <jeffish@microsoft.com>
2026-03-05 14:45:50 -0600

    revert formatting changes

--- sdk/resources/arm-resources/src/resourceManagementClient.ts
+++ sdk/resources/arm-resources/src/resourceManagementClient.ts
@@ -8,7 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
-import { PipelineRequest, PipelineResponse, SendRequest } from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest,
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   OperationsImpl,
@@ -86,7 +90,8 @@ export class ResourceManagementClient extends coreClient.ServiceClient {
       userAgentOptions: {
         userAgentPrefix,
       },
-      endpoint: options.endpoint ?? options.baseUri ?? "https://management.azure.com",
+      endpoint:
+        options.endpoint ?? options.baseUri ?? "https://management.azure.com",
     };
     super(optionsWithDefaults);
 
@@ -96,7 +101,8 @@ export class ResourceManagementClient extends coreClient.ServiceClient {
         options.pipeline.getOrderedPolicies();
       bearerTokenAuthenticationPolicyFound = pipelinePolicies.some(
         (pipelinePolicy) =>
-          pipelinePolicy.name === coreRestPipeline.bearerTokenAuthenticationPolicyName,
+          pipelinePolicy.name ===
+          coreRestPipeline.bearerTokenAuthenticationPolicyName,
       );
     }
     if (
@@ -112,9 +118,11 @@ export class ResourceManagementClient extends coreClient.ServiceClient {
         coreRestPipeline.bearerTokenAuthenticationPolicy({
           credential: credentials,
           scopes:
-            optionsWithDefaults.credentialScopes ?? `${optionsWithDefaults.endpoint}/.default`,
+            optionsWithDefaults.credentialScopes ??
+            `${optionsWithDefaults.endpoint}/.default`,
           challengeCallbacks: {
-            authorizeRequestOnChallenge: coreClient.authorizeRequestOnClaimChallenge,
+            authorizeRequestOnChallenge:
+              coreClient.authorizeRequestOnClaimChallenge,
           },
         }),
       );
@@ -141,7 +149,10 @@ export class ResourceManagementClient extends coreClient.ServiceClient {
     }
     const apiVersionPolicy = {
       name: "CustomApiVersionPolicy",
-      async sendRequest(request: PipelineRequest, next: SendRequest): Promise<PipelineResponse> {
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest,
+      ): Promise<PipelineResponse> {
         const param = request.url.split("?");
         if (param.length > 1) {
           const newParams = param[1].split("&").map((item) => {
