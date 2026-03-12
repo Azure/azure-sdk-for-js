4a23df94a3276a3f4a5173eea692cbcc4cd69885
azure-sdk <azuresdk@microsoft.com>
2026-03-12 15:38:02 +0000

    Configurations:  'specification/discovery/Discovery.Management/tspconfig.yaml', API Version: 2026-02-01-preview, SDK Release Type: beta, and CommitSHA: '52f2a91499f8a39cfcb50453a9b36b8c3a681ed8' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6001912 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/discovery/arm-discovery/samples/v1-beta/javascript/supercomputersListBySubscriptionSample.js
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { DiscoveryClient } = require("@azure/arm-discovery");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to list Supercomputer resources by subscription ID
+ *
+ * @summary list Supercomputer resources by subscription ID
+ * x-ms-original-file: 2026-02-01-preview/Supercomputers_ListBySubscription_MaximumSet_Gen.json
+ */
+async function supercomputersListBySubscriptionMaximumSet() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "C058B75F-64D2-4E9D-8B66-65339DCB22C7";
+  const client = new DiscoveryClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.supercomputers.listBySubscription()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
+
+async function main() {
+  await supercomputersListBySubscriptionMaximumSet();
+}
+
+main().catch(console.error);
