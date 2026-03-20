abd5bf9a729de1bd251aaa014ff1482c6afdb8ee
azure-sdk <azuresdk@microsoft.com>
2026-03-20 03:23:18 +0000

    Post release automated changes for azure-arm-marketplace

--- sdk/marketplace/arm-marketplace/src/api/marketplaceContext.ts
+++ sdk/marketplace/arm-marketplace/src/api/marketplaceContext.ts
@@ -33,7 +33,7 @@ export function createMarketplace(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-marketplace/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-marketplace/1.0.0-beta.2`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
