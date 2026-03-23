29fbdfd4a06dd8cd3c65f8b18a16c6e355785d46
azure-sdk <azuresdk@microsoft.com>
2026-03-23 19:38:55 +0000

    Configurations:  'specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml', API Version: 2026-03-01-preview, SDK Release Type: beta, and CommitSHA: 'eabb54a20ec2e6214f756d805261dfc3312ebea8' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6049814 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/contosowidgetmanager/contoso-widgetmanager/src/api/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export type {
+  WidgetManagerContext,
+  WidgetManagerClientOptionalParams,
+} from "./widgetManagerContext.js";
+export { createWidgetManager } from "./widgetManagerContext.js";
---
+++ sdk/contosowidgetmanager/contoso-widgetmanager/src/api/widgetManagerContext.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { logger } from "../logger.js";
+import { KnownVersions } from "../models/models.js";
+import type { Client, ClientOptions } from "@azure-rest/core-client";
+import { getClient } from "@azure-rest/core-client";
+import type { TokenCredential } from "@azure/core-auth";
+
+export interface WidgetManagerContext extends Client {
+  /** The API version to use for this operation. */
+  /** Known values of {@link KnownVersions} that the service accepts. */
+  apiVersion?: string;
+}
+
+/** Optional parameters for the client. */
+export interface WidgetManagerClientOptionalParams extends ClientOptions {
+  /** The API version to use for this operation. */
+  /** Known values of {@link KnownVersions} that the service accepts. */
+  apiVersion?: string;
+}
+
+export function createWidgetManager(
+  endpointParam: string,
+  credential: TokenCredential,
+  options: WidgetManagerClientOptionalParams = {},
+): WidgetManagerContext {
+  const endpointUrl = options.endpoint ?? String(endpointParam);
+  const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+  const userAgentInfo = `azsdk-js-contoso-widgetmanager/1.0.0-beta.1`;
+  const userAgentPrefix = prefixFromOptions
+    ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
+    : `azsdk-js-api ${userAgentInfo}`;
+  const { apiVersion: _, ...updatedOptions } = {
+    ...options,
+    userAgentOptions: { userAgentPrefix },
+    loggingOptions: { logger: options.loggingOptions?.logger ?? logger.info },
+    credentials: { scopes: options.credentials?.scopes ?? ["https://contoso.azure.com/.default"] },
+  };
+  const clientContext = getClient(endpointUrl, credential, updatedOptions);
+  const apiVersion = options.apiVersion;
+  return { ...clientContext, apiVersion } as WidgetManagerContext;
+}
