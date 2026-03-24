fc86ee09e6decab8070482e7e5ba02fa7861a250
azure-sdk <azuresdk@microsoft.com>
2026-03-24 02:43:24 +0000

    Configurations:  'specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/PrometheusRuleGroups/tspconfig.yaml', API Version: 2023-03-01, SDK Release Type: stable, and CommitSHA: 'f11f9cc21ceca5b4057818487552b76ee985bf1d' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6051837 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/prometheusrulegroups/arm-prometheusrulegroups/samples/v1-beta/javascript/prometheusRuleGroupsListBySubscriptionSample.js
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { PrometheusRuleGroupsManagementClient } = require("@azure/arm-prometheusrulegroups");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to retrieve Prometheus all rule group definitions in a subscription.
+ *
+ * @summary retrieve Prometheus all rule group definitions in a subscription.
+ * x-ms-original-file: 2023-03-01/listSubscriptionPrometheusRuleGroups.json
+ */
+async function listSubscriptionResourcePrometheusRuleGroups() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "ffffffff-ffff-ffff-ffff-ffffffffffff";
+  const client = new PrometheusRuleGroupsManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.prometheusRuleGroups.listBySubscription()) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
+
+async function main() {
+  await listSubscriptionResourcePrometheusRuleGroups();
+}
+
+main().catch(console.error);
---
+++ sdk/prometheusrulegroups/arm-prometheusrulegroups/src/api/prometheusRuleGroups/options.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import type { OperationOptions } from "@azure-rest/core-client";
+
+/** Optional parameters. */
+export interface PrometheusRuleGroupsListBySubscriptionOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface PrometheusRuleGroupsListByResourceGroupOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface PrometheusRuleGroupsDeleteOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface PrometheusRuleGroupsUpdateOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface PrometheusRuleGroupsCreateOrUpdateOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface PrometheusRuleGroupsGetOptionalParams extends OperationOptions {}
