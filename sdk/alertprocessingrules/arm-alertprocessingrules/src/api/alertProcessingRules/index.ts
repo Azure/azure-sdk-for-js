f1614b401d5f0d589540c1e3e6007962c88cd352
azure-sdk <azuresdk@microsoft.com>
2026-03-27 09:14:14 +0000

    Configurations:  'specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/AlertProcessingRules/tspconfig.yaml', API Version: 2021-08-08, SDK Release Type: stable, and CommitSHA: '8454cb5ef73c1ad157090245bec57c27668c81c7' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6072514 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/alertprocessingrules/arm-alertprocessingrules/src/api/alertProcessingRules/index.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  listBySubscription,
+  listByResourceGroup,
+  $delete,
+  update,
+  createOrUpdate,
+  getByName,
+} from "./operations.js";
+export type {
+  AlertProcessingRulesListBySubscriptionOptionalParams,
+  AlertProcessingRulesListByResourceGroupOptionalParams,
+  AlertProcessingRulesDeleteOptionalParams,
+  AlertProcessingRulesUpdateOptionalParams,
+  AlertProcessingRulesCreateOrUpdateOptionalParams,
+  AlertProcessingRulesGetByNameOptionalParams,
+} from "./options.js";
