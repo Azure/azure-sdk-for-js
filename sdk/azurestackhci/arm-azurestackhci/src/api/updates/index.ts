6d043f342b0c229810d77f4f7fa41f1d43da020d
azure-sdk <azuresdk@microsoft.com>
2026-03-31 03:40:00 +0000

    Configurations:  'specification/azurestackhci/resource-manager/Microsoft.AzureStackHCI/StackHCI/tspconfig.yaml', API Version: 2026-02-01, SDK Release Type: stable, and CommitSHA: 'aa591176e14ed29f96ebf1e3899e4356df7f13dd' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6084365 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/azurestackhci/arm-azurestackhci/src/api/updates/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { post, list, $delete, put, get } from "./operations.js";
+export type {
+  UpdatesPostOptionalParams,
+  UpdatesListOptionalParams,
+  UpdatesDeleteOptionalParams,
+  UpdatesPutOptionalParams,
+  UpdatesGetOptionalParams,
+} from "./options.js";
