96dbe0d9086d379f96e92ee44394315ab30360e6
Sri Balakrishnan <srbalakr@microsoft.com>
2026-04-08 16:16:46 +0000

    address comments

--- sdk/search/search-documents/src/search/api/searchContext.ts
+++ sdk/search/search-documents/src/search/api/searchContext.ts
@@ -30,7 +30,7 @@ export function createSearch(
 ): SearchContext {
   const endpointUrl = options.endpoint ?? String(endpointParam);
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-search-documents/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-search-documents/13.0.0`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
