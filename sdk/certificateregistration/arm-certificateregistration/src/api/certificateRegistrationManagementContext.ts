467a825e4fe96bfd53fa849734bda56a53ecacad
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2026-04-17 15:47:24 +0800

    update

--- sdk/certificateregistration/arm-certificateregistration/src/api/certificateRegistrationManagementContext.ts
+++ sdk/certificateregistration/arm-certificateregistration/src/api/certificateRegistrationManagementContext.ts
@@ -34,7 +34,7 @@ export function createCertificateRegistrationManagement(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-certificateregistration/1.0.0-beta.1`;
+  const userAgentInfo = `azsdk-js-arm-certificateregistration/1.0.0`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} azsdk-js-api ${userAgentInfo}`
     : `azsdk-js-api ${userAgentInfo}`;
