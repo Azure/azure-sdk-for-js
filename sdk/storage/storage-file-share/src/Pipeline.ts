dc540a81b9aee2a98b02876503cd28a165e81358
Jeremy Meng <jeremy.ymeng@gmail.com>
2026-04-20 14:14:43 -0700

    fix user agent string

--- sdk/storage/storage-file-share/src/Pipeline.ts
+++ sdk/storage/storage-file-share/src/Pipeline.ts
@@ -277,7 +277,7 @@ export function getCoreClientOptions(pipeline: PipelineLike): ExtendedServiceCli
 
   let corePipeline: CorePipeline = (pipeline as any)._corePipeline;
   if (!corePipeline) {
-    const packageDetails = `azsdk-js-azure-storage-blob/${SDK_VERSION}`;
+    const packageDetails = `azsdk-js-storage-file-share/${SDK_VERSION}`;
     const userAgentPrefix =
       restOptions.userAgentOptions && restOptions.userAgentOptions.userAgentPrefix
         ? `${restOptions.userAgentOptions.userAgentPrefix} ${packageDetails}`
