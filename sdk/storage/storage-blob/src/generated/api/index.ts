ec201681cd405a1476c64b86652cfcc80917f513
Jeremy Meng <jeremy.ymeng@gmail.com>
2026-04-20 12:49:19 -0700

    revert blob new files

--- sdk/storage/storage-blob/src/generated/api/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export type { BlobContext, BlobClientOptionalParams } from "./blobContext.js";
-export { createBlob } from "./blobContext.js";
