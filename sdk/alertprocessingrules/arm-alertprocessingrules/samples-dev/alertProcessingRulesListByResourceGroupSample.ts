979fa2c7bc7471aa3af0c53561590cc0ba2f933c
azure-sdk <azuresdk@microsoft.com>
2026-04-22 07:40:13 +0000

    Configurations:  'specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/AlertProcessingRules/tspconfig.yaml', API Version: 2021-08-08, SDK Release Type: beta, and CommitSHA: '4850dea187a72ae7ceaf0bd61b2e4a4c4b4a3b3e' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6191237 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/alertprocessingrules/arm-alertprocessingrules/samples-dev/alertProcessingRulesListByResourceGroupSample.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { AlertProcessingRulesManagementClient } from "@azure/arm-alertprocessingrules";
+import { DefaultAzureCredential } from "@azure/identity";
+
+/**
+ * This sample demonstrates how to list all alert processing rules in a resource group.
+ *
+ * @summary list all alert processing rules in a resource group.
+ * x-ms-original-file: 2021-08-08/AlertProcessingRules_List_ResourceGroup.json
+ */
+async function getAlertProcessingRulesResourceGroupWide(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "1e3ff1c0-771a-4119-a03b-be82a51e232d";
+  const client = new AlertProcessingRulesManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.alertProcessingRules.listByResourceGroup("alertscorrelationrg")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
+
+async function main(): Promise<void> {
+  await getAlertProcessingRulesResourceGroupWide();
+}
+
+main().catch(console.error);
