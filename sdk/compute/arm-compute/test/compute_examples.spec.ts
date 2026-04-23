50f64b98c3c80f6d3f4e720874e3ceeb7d888910
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2026-04-23 14:45:56 +0800

    Update compute_examples.spec.ts

--- sdk/compute/arm-compute/test/compute_examples.spec.ts
+++ sdk/compute/arm-compute/test/compute_examples.spec.ts
@@ -28,7 +28,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe.skip("Compute test", () => {
+describe("Compute test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: ComputeManagementClient;
