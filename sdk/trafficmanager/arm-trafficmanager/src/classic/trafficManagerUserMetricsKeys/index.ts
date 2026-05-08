9bb48bbb24937baba60e5058e480b219b7890aa1
azure-sdk <azuresdk@microsoft.com>
2026-05-08 15:23:34 +0000

    Configurations:  'specification/trafficmanager/resource-manager/Microsoft.Network/TrafficManager/tspconfig.yaml', API Version: 2024-04-01-preview, SDK Release Type: beta, and CommitSHA: '28e08376806422730d28d8dd90479d0ad68397ac' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6270976 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/trafficmanager/arm-trafficmanager/src/classic/trafficManagerUserMetricsKeys/index.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { TrafficManagerManagementContext } from "../../api/trafficManagerManagementContext.js";
+import {
+  $delete,
+  createOrUpdate,
+  get,
+} from "../../api/trafficManagerUserMetricsKeys/operations.js";
+import {
+  TrafficManagerUserMetricsKeysDeleteOptionalParams,
+  TrafficManagerUserMetricsKeysCreateOrUpdateOptionalParams,
+  TrafficManagerUserMetricsKeysGetOptionalParams,
+} from "../../api/trafficManagerUserMetricsKeys/options.js";
+import { DeleteOperationResult, UserMetricsModel } from "../../models/models.js";
+
+/** Interface representing a TrafficManagerUserMetricsKeys operations. */
+export interface TrafficManagerUserMetricsKeysOperations {
+  /** Delete a subscription-level key used for Real User Metrics collection. */
+  delete: (
+    options?: TrafficManagerUserMetricsKeysDeleteOptionalParams,
+  ) => Promise<DeleteOperationResult>;
+  /** Create or update a subscription-level key used for Real User Metrics collection. */
+  createOrUpdate: (
+    options?: TrafficManagerUserMetricsKeysCreateOrUpdateOptionalParams,
+  ) => Promise<UserMetricsModel>;
+  /** Get the subscription-level key used for Real User Metrics collection. */
+  get: (options?: TrafficManagerUserMetricsKeysGetOptionalParams) => Promise<UserMetricsModel>;
+}
+
+function _getTrafficManagerUserMetricsKeys(context: TrafficManagerManagementContext) {
+  return {
+    delete: (options?: TrafficManagerUserMetricsKeysDeleteOptionalParams) =>
+      $delete(context, options),
+    createOrUpdate: (options?: TrafficManagerUserMetricsKeysCreateOrUpdateOptionalParams) =>
+      createOrUpdate(context, options),
+    get: (options?: TrafficManagerUserMetricsKeysGetOptionalParams) => get(context, options),
+  };
+}
+
+export function _getTrafficManagerUserMetricsKeysOperations(
+  context: TrafficManagerManagementContext,
+): TrafficManagerUserMetricsKeysOperations {
+  return {
+    ..._getTrafficManagerUserMetricsKeys(context),
+  };
+}
