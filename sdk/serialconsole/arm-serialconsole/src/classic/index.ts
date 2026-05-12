f57ceec0fe60f1136c2fdd4841b8be89e57c65b6
azure-sdk <azuresdk@microsoft.com>
2026-05-12 09:50:04 +0000

    Configurations:  'specification/serialconsole/resource-manager/Microsoft.SerialConsole/SerialConsole/tspconfig.yaml', API Version: 2024-07-01, SDK Release Type: beta, and CommitSHA: 'b7fdf7f42665de0a0e24b13c38e80bb09439e096' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6284853 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/serialconsole/arm-serialconsole/src/classic/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export type { SerialConsoleOperationGroupOperations } from "./serialConsoleOperationGroup/index.js";
+export type { SerialPortsOperations } from "./serialPorts/index.js";
