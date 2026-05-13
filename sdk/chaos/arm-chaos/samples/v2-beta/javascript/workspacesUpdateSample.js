f633416c80bc788b97e7e0e40b02ea377531fcd0
azure-sdk <azuresdk@microsoft.com>
2026-05-13 15:34:35 +0000

    Configurations:  'specification/chaos/resource-manager/Microsoft.Chaos/Chaos/tspconfig.yaml', API Version: 2026-05-01-preview, SDK Release Type: beta, and CommitSHA: 'fba0ddc48535c777a154e285a0708f27c188f081' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6292132 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/chaos/arm-chaos/samples/v2-beta/javascript/workspacesUpdateSample.js
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { ChaosManagementClient } = require("@azure/arm-chaos");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to the operation to update a Workspace.
+ *
+ * @summary the operation to update a Workspace.
+ * x-ms-original-file: 2026-05-01-preview/Workspaces_Update.json
+ */
+async function updateAWorkspaceInAResourceGroup() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "6b052e15-03d3-4f17-b2e1-be7f07588291";
+  const client = new ChaosManagementClient(credential, subscriptionId);
+  const result = await client.workspaces.update("exampleRG", "exampleWorkspace", {
+    identity: {
+      type: "UserAssigned",
+      userAssignedIdentities: {
+        "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/exampleResourceGroup/providers/Microsoft.ManagedIdentity/userAssignedIdentities/exampleIdentity":
+          {},
+      },
+    },
+    tags: { environment: "production", department: "engineering", project: "chaos-testing" },
+  });
+  console.log(result);
+}
+
+async function main() {
+  await updateAWorkspaceInAResourceGroup();
+}
+
+main().catch(console.error);
