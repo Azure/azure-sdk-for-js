1061660570779fefd8300508f26fa78174b9f9db
azure-sdk <azuresdk@microsoft.com>
2026-05-20 03:18:20 +0000

    Configurations:  'specification/operationalinsights/resource-manager/Microsoft.OperationalInsights/OperationalInsights/tspconfig.yaml', API Version: 2025-07-01, SDK Release Type: beta, and CommitSHA: '2283827e2d3992938b814e5b23b6b288093423d5' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6324825 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/operationalinsights/arm-operationalinsights/src/api/dataSources/operations.ts
@@ -0,0 +1,253 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { OperationalInsightsManagementContext as Client } from "../index.js";
+import {
+  errorResponseDeserializer,
+  DataSource,
+  dataSourceSerializer,
+  dataSourceDeserializer,
+  _DataSourceListResult,
+  _dataSourceListResultDeserializer,
+} from "../../models/models.js";
+import {
+  PagedAsyncIterableIterator,
+  buildPagedAsyncIterator,
+} from "../../static-helpers/pagingHelpers.js";
+import { expandUrlTemplate } from "../../static-helpers/urlTemplate.js";
+import {
+  DataSourcesListByWorkspaceOptionalParams,
+  DataSourcesDeleteOptionalParams,
+  DataSourcesCreateOrUpdateOptionalParams,
+  DataSourcesGetOptionalParams,
+} from "./options.js";
+import {
+  StreamableMethod,
+  PathUncheckedResponse,
+  createRestError,
+  operationOptionsToRequestParameters,
+} from "@azure-rest/core-client";
+
+export function _listByWorkspaceSend(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  filter: string,
+  options: DataSourcesListByWorkspaceOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{workspaceName}/dataSources{?api%2Dversion,%24filter,%24skiptoken}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      "api%2Dversion": context.apiVersion ?? "2025-07-01",
+      "%24filter": filter,
+      "%24skiptoken": options?.skiptoken,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: { accept: "application/json", ...options.requestOptions?.headers },
+  });
+}
+
+export async function _listByWorkspaceDeserialize(
+  result: PathUncheckedResponse,
+): Promise<_DataSourceListResult> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+
+    throw error;
+  }
+
+  return _dataSourceListResultDeserializer(result.body);
+}
+
+/** Gets the first page of data source instances in a workspace with the link to the next page. */
+export function listByWorkspace(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  filter: string,
+  options: DataSourcesListByWorkspaceOptionalParams = { requestOptions: {} },
+): PagedAsyncIterableIterator<DataSource> {
+  return buildPagedAsyncIterator(
+    context,
+    () => _listByWorkspaceSend(context, resourceGroupName, workspaceName, filter, options),
+    _listByWorkspaceDeserialize,
+    ["200"],
+    { itemName: "value", nextLinkName: "nextLink", apiVersion: context.apiVersion ?? "2025-07-01" },
+  );
+}
+
+export function _$deleteSend(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  dataSourceName: string,
+  options: DataSourcesDeleteOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{workspaceName}/dataSources/{dataSourceName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      dataSourceName: dataSourceName,
+      "api%2Dversion": context.apiVersion ?? "2025-07-01",
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).delete({ ...operationOptionsToRequestParameters(options) });
+}
+
+export async function _$deleteDeserialize(result: PathUncheckedResponse): Promise<void> {
+  const expectedStatuses = ["200", "204"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+
+    throw error;
+  }
+
+  return;
+}
+
+/** Deletes a data source instance. */
+export async function $delete(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  dataSourceName: string,
+  options: DataSourcesDeleteOptionalParams = { requestOptions: {} },
+): Promise<void> {
+  const result = await _$deleteSend(
+    context,
+    resourceGroupName,
+    workspaceName,
+    dataSourceName,
+    options,
+  );
+  return _$deleteDeserialize(result);
+}
+
+export function _createOrUpdateSend(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  dataSourceName: string,
+  parameters: DataSource,
+  options: DataSourcesCreateOrUpdateOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{workspaceName}/dataSources/{dataSourceName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      dataSourceName: dataSourceName,
+      "api%2Dversion": context.apiVersion ?? "2025-07-01",
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).put({
+    ...operationOptionsToRequestParameters(options),
+    contentType: "application/json",
+    headers: { accept: "application/json", ...options.requestOptions?.headers },
+    body: dataSourceSerializer(parameters),
+  });
+}
+
+export async function _createOrUpdateDeserialize(
+  result: PathUncheckedResponse,
+): Promise<DataSource> {
+  const expectedStatuses = ["200", "201"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+
+    throw error;
+  }
+
+  return dataSourceDeserializer(result.body);
+}
+
+/** Create or update a data source. */
+export async function createOrUpdate(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  dataSourceName: string,
+  parameters: DataSource,
+  options: DataSourcesCreateOrUpdateOptionalParams = { requestOptions: {} },
+): Promise<DataSource> {
+  const result = await _createOrUpdateSend(
+    context,
+    resourceGroupName,
+    workspaceName,
+    dataSourceName,
+    parameters,
+    options,
+  );
+  return _createOrUpdateDeserialize(result);
+}
+
+export function _getSend(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  dataSourceName: string,
+  options: DataSourcesGetOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{workspaceName}/dataSources/{dataSourceName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      workspaceName: workspaceName,
+      dataSourceName: dataSourceName,
+      "api%2Dversion": context.apiVersion ?? "2025-07-01",
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: { accept: "application/json", ...options.requestOptions?.headers },
+  });
+}
+
+export async function _getDeserialize(result: PathUncheckedResponse): Promise<DataSource> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+
+    throw error;
+  }
+
+  return dataSourceDeserializer(result.body);
+}
+
+/** Gets a datasource instance. */
+export async function get(
+  context: Client,
+  resourceGroupName: string,
+  workspaceName: string,
+  dataSourceName: string,
+  options: DataSourcesGetOptionalParams = { requestOptions: {} },
+): Promise<DataSource> {
+  const result = await _getSend(context, resourceGroupName, workspaceName, dataSourceName, options);
+  return _getDeserialize(result);
+}
