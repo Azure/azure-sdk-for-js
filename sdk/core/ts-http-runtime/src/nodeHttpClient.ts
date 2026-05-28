9eced93f930765309a249fa1b71c45d1d4814616
Jeremy Meng <jeremy.ymeng@gmail.com>
2026-05-28 00:22:58 +0000

    [core] send request body of ArrayBufferView subarray properly
    When the request body is an ArrayBufferView we are incorrectly sending the whole
    backing buffer of the view `Buffer.from(body.buffer)`.  This PR fixes the issue
    by sending the proper subarray.

--- sdk/core/ts-http-runtime/src/nodeHttpClient.ts
+++ sdk/core/ts-http-runtime/src/nodeHttpClient.ts
@@ -256,7 +256,7 @@ class NodeHttpClient implements HttpClient {
         if (typeof body === "string" || Buffer.isBuffer(body)) {
           req.end(body);
         } else if (isArrayBuffer(body)) {
-          req.end(ArrayBuffer.isView(body) ? Buffer.from(body.buffer) : Buffer.from(body));
+          req.end(ArrayBuffer.isView(body) ? Buffer.from(body.buffer, body.byteOffset, body.byteLength) : Buffer.from(body));
         } else {
           logger.error("Unrecognized body type", body);
           reject(new RestError("Unrecognized body type"));
