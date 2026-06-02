63013840ad69c071fe9c524ce58fa9d60cef1861
azure-sdk <azuresdk@microsoft.com>
2026-06-02 03:41:33 +0000

    Configurations:  'specification/confidentialledger/Microsoft.CodeTransparency/tspconfig.yaml', and CommitSHA: '36e6eb9bcefbb5371381d6c0ca98a6cc01703d9d' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6381747 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/confidentialledger/codetransparency-rest/src/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import CodeTransparencyClient from "./codeTransparencyClient.js";
+
+export * from "./codeTransparencyClient.js";
+export * from "./parameters.js";
+export * from "./responses.js";
+export * from "./clientDefinitions.js";
+export * from "./outputModels.js";
+export { RestError, isRestError } from "@azure/core-rest-pipeline";
+
+export default CodeTransparencyClient;
