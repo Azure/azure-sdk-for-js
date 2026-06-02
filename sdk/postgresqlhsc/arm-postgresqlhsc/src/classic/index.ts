03fd8c13a49e407aaa1d8f9f070b3dd54d307aae
azure-sdk <azuresdk@microsoft.com>
2026-06-02 01:56:28 +0000

    Configurations:  'specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/PostgresqlHsc/tspconfig.yaml', API Version: 2023-03-02-preview, SDK Release Type: beta, and CommitSHA: '16e90365f2fcc4efaa3e67e39c15f75c3712ad3d' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6381305 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/postgresqlhsc/arm-postgresqlhsc/src/classic/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export type { ClustersOperations } from "./clusters/index.js";
+export type { ConfigurationsOperations } from "./configurations/index.js";
+export type { FirewallRulesOperations } from "./firewallRules/index.js";
+export type { OperationsOperations } from "./operations/index.js";
+export type { PrivateEndpointConnectionsOperations } from "./privateEndpointConnections/index.js";
+export type { PrivateLinkResourcesOperations } from "./privateLinkResources/index.js";
+export type { RolesOperations } from "./roles/index.js";
+export type { ServersOperations } from "./servers/index.js";
