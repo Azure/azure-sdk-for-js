23f3107fde7f2f4097f8ccc5ae5a75b584de862b
azure-sdk <azuresdk@microsoft.com>
2026-06-05 01:25:25 +0000

    Configurations:  'specification/monitor/resource-manager/Microsoft.Insights/Insights/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: '4375d0c4eb36e73152199e484c0d4537ddd4c9da' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6398417 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/monitor/arm-monitor/samples-dev/autoscaleSettingsGetSample.ts
+++ sdk/monitor/arm-monitor/samples-dev/autoscaleSettingsGetSample.ts
@@ -1,25 +1,20 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-/**
- * This sample demonstrates how to Gets an autoscale setting
- *
- * @summary Gets an autoscale setting
- * x-ms-original-file: specification/monitor/resource-manager/Microsoft.Insights/stable/2022-10-01/examples/getAutoscaleSetting.json
- */
-
 import { MonitorClient } from "@azure/arm-monitor";
 import { DefaultAzureCredential } from "@azure/identity";
-import "dotenv/config";
 
+/**
+ * This sample demonstrates how to gets an autoscale setting
+ *
+ * @summary gets an autoscale setting
+ * x-ms-original-file: 2022-10-01/getAutoscaleSetting.json
+ */
 async function getAnAutoscaleSetting(): Promise<void> {
-  const subscriptionId =
-    process.env["MONITOR_SUBSCRIPTION_ID"] || "b67f7fec-69fc-4974-9099-a26bd6ffeda3";
-  const resourceGroupName = process.env["MONITOR_RESOURCE_GROUP"] || "TestingMetricsScaleSet";
-  const autoscaleSettingName = "MySetting";
   const credential = new DefaultAzureCredential();
+  const subscriptionId = "b67f7fec-69fc-4974-9099-a26bd6ffeda3";
   const client = new MonitorClient(credential, subscriptionId);
-  const result = await client.autoscaleSettings.get(resourceGroupName, autoscaleSettingName);
+  const result = await client.autoscaleSettings.get("TestingMetricsScaleSet", "MySetting");
   console.log(result);
 }
 
---
+++ sdk/monitor/arm-monitor/src/models/serviceDiagnosticsSettingsApi/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export type {
+  ServiceDiagnosticSettingsResource,
+  ServiceDiagnosticSettings,
+  MetricSettings,
+  LogSettings,
+  ServiceDiagnosticSettingsResourcePatch,
+} from "./models.js";
