8d2855aa2cca333534cc0fc292cacc3b63519ef4
azure-sdk <azuresdk@microsoft.com>
2026-06-08 08:49:56 +0000

    Configurations:  'specification/reservations/resource-manager/Microsoft.Capacity/Reservations/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: '7eedf0843327d688a23ed89a7c2921cbfc129dfe' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6406239 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/reservations/arm-reservations/test/reservations_examples.spec.ts
+++ sdk/reservations/arm-reservations/test/reservations_examples.spec.ts
@@ -6,7 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-import { env, Recorder, RecorderStartOptions, isPlaybackMode } from "@azure-tools/test-recorder";
+import type { RecorderStartOptions } from "@azure-tools/test-recorder";
+import { env, Recorder, isPlaybackMode } from "@azure-tools/test-recorder";
 import { createTestCredential } from "@azure-tools/test-credential";
 import { AzureReservationAPI } from "../src/azureReservationAPI.js";
 import { describe, it, assert, beforeEach, afterEach } from "vitest";
@@ -72,7 +73,7 @@ describe("Datafactory test", () => {
 
   it("quota list test", async () => {
     const resArray = new Array();
-    for await (let item of client.quota.list(subscriptionId, providerId, location)) {
+    for await (const item of client.quota.list(subscriptionId, providerId, location)) {
       resArray.push(item);
     }
     assert.equal(resArray.length, 108);
