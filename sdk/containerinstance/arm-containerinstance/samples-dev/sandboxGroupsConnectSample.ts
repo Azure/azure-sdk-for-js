160d0fc37c0688acd5d274eaae2eeea5f22da1ae
azure-sdk <azuresdk@microsoft.com>
2026-06-08 02:37:49 +0000

    Configurations:  'specification/containerinstance/resource-manager/Microsoft.ContainerInstance/ContainerInstance/tspconfig.yaml', API Version: 2026-06-01-preview, SDK Release Type: beta, and CommitSHA: 'd5ee6b87272dea7413a15e1403db07fe0450316a' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6405196 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/containerinstance/arm-containerinstance/samples-dev/sandboxGroupsConnectSample.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ContainerInstanceManagementClient } from "@azure/arm-containerinstance";
+import { DefaultAzureCredential } from "@azure/identity";
+
+/**
+ * This sample demonstrates how to get an access token and endpoint for connecting to the SandboxGroup.
+ *
+ * @summary get an access token and endpoint for connecting to the SandboxGroup.
+ * x-ms-original-file: 2026-06-01-preview/SandboxGroupsConnect.json
+ */
+async function connectToASandboxGroup(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new ContainerInstanceManagementClient(credential, subscriptionId);
+  const result = await client.sandboxGroups.connect("myResourceGroup", "mySandboxGroup");
+  console.log(result);
+}
+
+async function main(): Promise<void> {
+  await connectToASandboxGroup();
+}
+
+main().catch(console.error);
