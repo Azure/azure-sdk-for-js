897e663b3be0e64a848ae01b5fffc27914827c0e
retry-recv <goubo2012@gmail.com>
2026-06-10 16:35:21 -0700

    update version

--- sdk/ai/ai-projects/src/constants.ts
+++ sdk/ai/ai-projects/src/constants.ts
@@ -4,7 +4,7 @@
 /**
  * Current version of the `@azure/ai-projects` package.
  */
-export const SDK_VERSION = `2.2.1`;
+export const SDK_VERSION = `2.2.2`;
 
 /**
  * The package name of the `@azure/ai-projects` package.
