007f61b657348e71c027b0c1814677ad440d9dfc
Jiao Di (MSFT) <v-jiaodi@microsoft.com>
2026-06-11 13:55:09 +0800

    update

--- sdk/dnsresolver/arm-dnsresolver/test/dnsresolver_examples.spec.ts
+++ sdk/dnsresolver/arm-dnsresolver/test/dnsresolver_examples.spec.ts
@@ -83,15 +83,15 @@ describe("dnsresolve test", () => {
     assert.equal(res.name, dnsResolverName);
   });
 
-  it("dnsResolvers list clusters", async () => {
+  it.only("dnsResolvers list clusters", async () => {
     const resArray = new Array();
     for await (const item of client.dnsResolvers.listByResourceGroup(resourceGroup)) {
       resArray.push(item);
     }
-    assert.equal(resArray.length, 1);
+    assert.equal(resArray.length, 0);
   });
 
-  it.only("dnsResolvers delete clusters", async () => {
+  it("dnsResolvers delete clusters", async () => {
     const resArray = new Array();
     await client.dnsResolvers.beginDeleteAndWait(
       resourceGroup,
