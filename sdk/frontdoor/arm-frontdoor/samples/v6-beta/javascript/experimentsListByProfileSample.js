24893b46d87f30f509fce0a2ddcbd7ef0b7f2d76
azure-sdk <azuresdk@microsoft.com>
2026-06-16 00:02:02 +0000

    Configurations:  'specification/frontdoor/resource-manager/Microsoft.Network/FrontDoor/tspconfig.yaml', API Version: 2025-11-01, SDK Release Type: beta, and CommitSHA: '49de8edaf1b1a951a8998670ed781e911586c4a4' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6438532 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/frontdoor/arm-frontdoor/samples/v6-beta/javascript/experimentsListByProfileSample.js
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { FrontDoorManagementClient } = require("@azure/arm-frontdoor");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to gets a list of Experiments
+ *
+ * @summary gets a list of Experiments
+ * x-ms-original-file: 2025-11-01/NetworkExperimentListExperiments.json
+ */
+async function getsAListOfExperiments() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new FrontDoorManagementClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.experiments.listByProfile("MyResourceGroup", "MyProfile")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
+
+async function main() {
+  await getsAListOfExperiments();
+}
+
+main().catch(console.error);
