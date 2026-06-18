4ac89c3a278e98451d3126dc161c5a9dfb0048c1
azure-sdk <azuresdk@microsoft.com>
2026-06-18 09:25:55 +0000

    Regenerate shard mo_we_2 (emitter 0.55.0-dev.4)

--- sdk/template/template/generated/models/models.ts
+++ sdk/template/template/generated/models/models.ts
@@ -3,6 +3,12 @@
 
 import { ErrorModel } from "@azure-rest/core-client";
 
+/**
+ * This file contains only generated model types and their (de)serializers.
+ * Disable the following rules for internal models with '_' prefix and deserializers which require 'any' for raw JSON input.
+ */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /** A widget. */
 export interface WidgetSuite {
   /** The widget name. */
