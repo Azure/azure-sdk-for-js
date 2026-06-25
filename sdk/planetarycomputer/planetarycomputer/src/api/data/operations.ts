4c9e5a0dde146c36a24dc8f35c3c1b487689b95e
Karthick <karthick.narendran@microsoft.com>
2026-06-25 13:05:07 +0530

    Fix all 6 wmts capabilities deserializers

--- sdk/planetarycomputer/planetarycomputer/src/api/data/operations.ts
+++ sdk/planetarycomputer/planetarycomputer/src/api/data/operations.ts
@@ -191,7 +191,6 @@ import {
   createRestError,
   operationOptionsToRequestParameters,
 } from "@azure-rest/core-client";
-import { stringToUint8Array } from "@azure/core-util";
 
 export function _getSearchPointWithAssetsSend(
   context: Client,
@@ -1077,7 +1076,7 @@ export async function _getSearchWmtsCapabilitiesDeserialize(
   }
 
   return {
-    body: typeof result.body === "string" ? stringToUint8Array(result.body, "base64") : result.body,
+    body: typeof result.body === "string" ? new TextEncoder().encode(result.body) : result.body,
   };
 }
 
@@ -1877,7 +1876,7 @@ export async function _getSearchWmtsCapabilitiesByTmsDeserialize(
   }
 
   return {
-    body: typeof result.body === "string" ? stringToUint8Array(result.body, "base64") : result.body,
+    body: typeof result.body === "string" ? new TextEncoder().encode(result.body) : result.body,
   };
 }
 
@@ -3872,7 +3871,7 @@ export async function _getCollectionWmtsCapabilitiesByTmsDeserialize(
   }
 
   return {
-    body: typeof result.body === "string" ? stringToUint8Array(result.body, "base64") : result.body,
+    body: typeof result.body === "string" ? new TextEncoder().encode(result.body) : result.body,
   };
 }
 
@@ -3952,7 +3951,7 @@ export async function _getCollectionWmtsCapabilitiesDeserialize(
   }
 
   return {
-    body: typeof result.body === "string" ? stringToUint8Array(result.body, "base64") : result.body,
+    body: typeof result.body === "string" ? new TextEncoder().encode(result.body) : result.body,
   };
 }
 
@@ -5990,7 +5989,7 @@ export async function _getItemWmtsCapabilitiesByTmsDeserialize(
   }
 
   return {
-    body: typeof result.body === "string" ? stringToUint8Array(result.body, "base64") : result.body,
+    body: typeof result.body === "string" ? new TextEncoder().encode(result.body) : result.body,
   };
 }
 
@@ -6097,7 +6096,7 @@ export async function _getItemWmtsCapabilitiesDeserialize(
   }
 
   return {
-    body: typeof result.body === "string" ? stringToUint8Array(result.body, "base64") : result.body,
+    body: typeof result.body === "string" ? new TextEncoder().encode(result.body) : result.body,
   };
 }
 
