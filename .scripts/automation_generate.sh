315a83d8081ec41cdabf9ee0821d9a396b917b44
Kyle Zhang <v-zhanh@microsoft.com>
2026-07-21 10:46:03 +0800

    add test script

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-npm --prefix eng/tools/js-sdk-release-tools exec --no -- code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.74 --typespecEmitter=@azure-tools/typespec-ts
+
+cd ..
+
+echo 'Build js-sdk-release-tools from local source'
+pushd azure-sdk-for-js/eng/tools/js-sdk-release-tools-src
+npm install
+npm run build
+popd
+
+cd azure-sdk-for-js
+node eng/tools/js-sdk-release-tools-src/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.74 --typespecEmitter=@azure-tools/typespec-ts
