26a9feb938e061c1f356e6493f9bd28f7a427edc
Anubhav <aanubhav@microsoft.com>
2026-07-21 11:13:06 +0530

    [storage-blob] Throw descriptive error on empty Arrow list body; rename Arrow spec to lowercase convention

--- sdk/storage/storage-blob/src/utils/blobListArrowParser.ts
+++ sdk/storage/storage-blob/src/utils/blobListArrowParser.ts
@@ -228,7 +228,7 @@ async function readResponseBodyToBytes(response: {
   if (response.readableStreamBody) {
     return readNodeStreamToBytes(response.readableStreamBody);
   }
-  return new Uint8Array(0);
+  throw new RangeError("List Blobs Apache Arrow response body is empty or unavailable.");
 }
 
 function readNodeStreamToBytes(stream: NodeJS.ReadableStream): Promise<Uint8Array> {
