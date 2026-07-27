4f5520630cb4b0f75a9c1ede292a38bc8cdbb0ae
Kyle Zhang <v-zhanh@microsoft.com>
2026-07-27 18:55:37 +0800

    Refactor automation_generate.sh to use REPO_ROOT for directory navigation

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-cd ..
+REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 
 echo 'Build js-sdk-release-tools from local source'
-pushd azure-sdk-for-js/eng/tools/js-sdk-release-tools-src
+cd "$REPO_ROOT/eng/tools/js-sdk-release-tools-src"
 npm install
 npm run build
-popd
 
-cd azure-sdk-for-js
+cd "$REPO_ROOT"
 node eng/tools/js-sdk-release-tools-src/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.74 --typespecEmitter=@azure-tools/typespec-ts
