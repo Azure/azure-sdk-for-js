51b81a2185824f8694d044886a7c8160c6afba51
azure-sdk <azuresdk@microsoft.com>
2026-07-29 08:04:05 +0000

    Configurations:  'specification/eventhub/resource-manager/Microsoft.EventHub/Eventhub/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: '801a60cdad2669e8f824fedfbabbfe7f7093b940' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6632569 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/eventhub/arm-eventhub/src/api/eventHubManagementContext.ts
+++ sdk/eventhub/arm-eventhub/src/api/eventHubManagementContext.ts
@@ -34,7 +34,7 @@ export function createEventHubManagement(
   const endpointUrl =
     options.endpoint ?? getArmEndpoint(options.cloudSetting) ?? "https://management.azure.com";
   const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
-  const userAgentInfo = `azsdk-js-arm-eventhub/6.0.0`;
+  const userAgentInfo = `azsdk-js-arm-eventhub/7.0.0-beta.1`;
   const userAgentPrefix = prefixFromOptions
     ? `${prefixFromOptions} ${userAgentInfo}`
     : `${userAgentInfo}`;
