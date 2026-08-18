84552668b1f4bf5fb00ee0b643a7acf50c91c1c4
azure-sdk <azuresdk@microsoft.com>
2026-08-18 17:24:25 +0000

    Configurations:  'specification/deviceupdate/data-plane/sudeviceregistry/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: '83ee12a23d0ab5a14b990f8b5700e787eb8a0b7c' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6715276 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/deviceregistry/iot-device-registry-software-update/src/api/deviceClasses/options.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { OperationOptions } from "@azure-rest/core-client";
+
+/** Optional parameters. */
+export interface DeviceClassesDeleteOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface DeviceClassesGetDeviceClassOptionalParams extends OperationOptions {}
+
+/** Optional parameters. */
+export interface DeviceClassesListOptionalParams extends OperationOptions {}
