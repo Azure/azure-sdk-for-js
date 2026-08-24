0271c796d4ec67e4b54ccf2f2e7d8b01391aa452
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2026-08-24 15:02:45 +0800

    Update iotfirmwaredefense_operations_test.spec.ts

--- sdk/iotfirmwaredefense/arm-iotfirmwaredefense/test/public/iotfirmwaredefense_operations_test.spec.ts
+++ sdk/iotfirmwaredefense/arm-iotfirmwaredefense/test/public/iotfirmwaredefense_operations_test.spec.ts
@@ -31,7 +31,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe("iotfirmwaredefense test", () => {
+describe.skip("iotfirmwaredefense test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: IoTFirmwareDefenseClient;
