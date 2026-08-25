ad20c2c6ccc8fa00186e1a4112aa275229963b44
azure-sdk <azuresdk@microsoft.com>
2026-08-25 21:05:58 +0000

    Configurations:  'specification/deviceupdate/data-plane/sudeviceregistry/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: 'befd1687368facdf2e5f0878725c66107c2cd8d5' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6744503 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/deviceregistry/iot-device-registry-software-update/src/api/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export type {
+  DeviceRegistrySoftwareUpdateContext,
+  DeviceRegistrySoftwareUpdateClientOptionalParams,
+} from "./deviceRegistrySoftwareUpdateContext.js";
+export { createDeviceRegistrySoftwareUpdate } from "./deviceRegistrySoftwareUpdateContext.js";
---
+++ sdk/deviceregistry/iot-device-registry-software-update/src/deviceRegistrySoftwareUpdateClient.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  DeviceRegistrySoftwareUpdateContext,
+  DeviceRegistrySoftwareUpdateClientOptionalParams,
+  createDeviceRegistrySoftwareUpdate,
+} from "./api/index.js";
+import {
+  DeviceClassesOperations,
+  _getDeviceClassesOperations,
+} from "./classic/deviceClasses/index.js";
+import {
+  SoftwareUpdateOperations,
+  _getSoftwareUpdateOperations,
+} from "./classic/softwareUpdate/index.js";
+import { TokenCredential } from "@azure/core-auth";
+import { Pipeline } from "@azure/core-rest-pipeline";
+
+export type { DeviceRegistrySoftwareUpdateClientOptionalParams } from "./api/deviceRegistrySoftwareUpdateContext.js";
+
+export class DeviceRegistrySoftwareUpdateClient {
+  private _client: DeviceRegistrySoftwareUpdateContext;
+  /** The pipeline used by this client to make requests */
+  public readonly pipeline: Pipeline;
+
+  /** Software Update for Device Registry is an Azure service that enables customers to publish updates for their IoT devices to the cloud, and then deploy that update to their devices (approve updates to groups of devices managed and provisioned in Device Registry). It leverages the proven security and reliability of the Windows Update platform, optimized for IoT devices. It works globally and knows when and how to update devices, enabling customers to focus on their business goals and let Software Update for Device Registry handle the updates. */
+  constructor(
+    endpointParam: string,
+    credential: TokenCredential,
+    options: DeviceRegistrySoftwareUpdateClientOptionalParams = {},
+  ) {
+    this._client = createDeviceRegistrySoftwareUpdate(endpointParam, credential, options);
+    this.pipeline = this._client.pipeline;
+    this.softwareUpdate = _getSoftwareUpdateOperations(this._client);
+    this.deviceClasses = _getDeviceClassesOperations(this._client);
+  }
+
+  /** The operation groups for softwareUpdate */
+  public readonly softwareUpdate: SoftwareUpdateOperations;
+  /** The operation groups for deviceClasses */
+  public readonly deviceClasses: DeviceClassesOperations;
+}
