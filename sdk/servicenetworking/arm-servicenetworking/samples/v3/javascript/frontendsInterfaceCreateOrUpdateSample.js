25b56af1c332a0b65c27f98f686fa92948e7c43f
azure-sdk <azuresdk@microsoft.com>
2026-08-27 22:18:41 +0000

    Configurations:  'specification/servicenetworking/resource-manager/Microsoft.ServiceNetworking/ServiceNetworking/tspconfig.yaml', API Version: 2026-03-01, SDK Release Type: stable, and CommitSHA: '3fbff828a405632668f197b4c30466a0b8dc2cef' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6756616 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

---
+++ sdk/servicenetworking/arm-servicenetworking/samples/v3/javascript/frontendsInterfaceCreateOrUpdateSample.js
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { ServiceNetworkingManagementClient } = require("@azure/arm-servicenetworking");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to create a Frontend
+ *
+ * @summary create a Frontend
+ * x-ms-original-file: 2026-03-01/FrontendPut.json
+ */
+async function putFrontend() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new ServiceNetworkingManagementClient(credential, subscriptionId);
+  const result = await client.frontendsInterface.createOrUpdate("rg1", "tc1", "fe1", {
+    location: "NorthCentralUS",
+    properties: { publicNetworkAccess: "Enabled" },
+  });
+  console.log(result);
+}
+
+/**
+ * This sample demonstrates how to create a Frontend
+ *
+ * @summary create a Frontend
+ * x-ms-original-file: 2026-03-01/PrivateFrontendPut.json
+ */
+async function putPrivateFrontend() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new ServiceNetworkingManagementClient(credential, subscriptionId);
+  const result = await client.frontendsInterface.createOrUpdate("rg1", "tc1", "pfe1", {
+    location: "NorthCentralUS",
+    properties: {
+      publicNetworkAccess: "Disabled",
+      association: {
+        id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.ServiceNetworking/trafficControllers/tc1/associations/as1",
+      },
+    },
+  });
+  console.log(result);
+}
+
+async function main() {
+  await putFrontend();
+  await putPrivateFrontend();
+}
+
+main().catch(console.error);
