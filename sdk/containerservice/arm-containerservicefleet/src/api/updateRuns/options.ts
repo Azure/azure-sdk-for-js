852940383c9da8fcb84a0a7b57cb137fe7b102b3
azure-sdk <azuresdk@microsoft.com>
2026-09-03 19:52:32 +0000

    Configurations:  'specification/containerservice/resource-manager/Microsoft.ContainerService/fleet/tspconfig.yaml', API Version: 2026-06-02-preview, SDK Release Type: beta, and CommitSHA: '7ad5d5c9180ad584d3e0b90622233e01fa5978cf' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6786233 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/containerservice/arm-containerservicefleet/src/api/updateRuns/options.ts
+++ sdk/containerservice/arm-containerservicefleet/src/api/updateRuns/options.ts
@@ -10,6 +10,7 @@ export interface UpdateRunsSkipOptionalParams extends OperationOptions {
   /** The request should only proceed if an entity matches this string. */
   ifMatch?: string;
 }
+
 /** Optional parameters. */
 export interface UpdateRunsStopOptionalParams extends OperationOptions {
   /** Delay to wait until next poll, in milliseconds. */
@@ -17,6 +18,7 @@ export interface UpdateRunsStopOptionalParams extends OperationOptions {
   /** The request should only proceed if an entity matches this string. */
   ifMatch?: string;
 }
+
 /** Optional parameters. */
 export interface UpdateRunsStartOptionalParams extends OperationOptions {
   /** Delay to wait until next poll, in milliseconds. */
@@ -24,6 +26,7 @@ export interface UpdateRunsStartOptionalParams extends OperationOptions {
   /** The request should only proceed if an entity matches this string. */
   ifMatch?: string;
 }
+
 /** Optional parameters. */
 export interface UpdateRunsListByFleetOptionalParams extends OperationOptions {
   /** The number of result items to return. */
@@ -31,6 +34,7 @@ export interface UpdateRunsListByFleetOptionalParams extends OperationOptions {
   /** The page-continuation token to use with a paged version of this API. */
   skipToken?: string;
 }
+
 /** Optional parameters. */
 export interface UpdateRunsDeleteOptionalParams extends OperationOptions {
   /** Delay to wait until next poll, in milliseconds. */
@@ -38,6 +42,7 @@ export interface UpdateRunsDeleteOptionalParams extends OperationOptions {
   /** The request should only proceed if an entity matches this string. */
   ifMatch?: string;
 }
+
 /** Optional parameters. */
 export interface UpdateRunsCreateOrUpdateOptionalParams extends OperationOptions {
   /** Delay to wait until next poll, in milliseconds. */
@@ -47,5 +52,6 @@ export interface UpdateRunsCreateOrUpdateOptionalParams extends OperationOptions
   /** The request should only proceed if no entity matches this string. */
   ifNoneMatch?: string;
 }
+
 /** Optional parameters. */
 export interface UpdateRunsGetOptionalParams extends OperationOptions {}
