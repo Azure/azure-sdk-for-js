020b798a1a255f19dc6e41530930209f54040d64
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2026-09-04 14:14:27 +0800

    Update webpubsub_examples.spec.ts

--- sdk/web-pubsub/arm-webpubsub/test/webpubsub_examples.spec.ts
+++ sdk/web-pubsub/arm-webpubsub/test/webpubsub_examples.spec.ts
@@ -28,7 +28,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe("webPubSub test", () => {
+describe.skip("webPubSub test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: WebPubSubManagementClient;
