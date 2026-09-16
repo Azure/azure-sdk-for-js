f0a1aee92377038b9a66c09b1541d7a2a25785aa
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2026-09-16 13:59:02 +0800

    Update WorkloadOrchestration.spec.ts

--- sdk/workloadorchestration/arm-workloadorchestration/test/public/WorkloadOrchestration.spec.ts
+++ sdk/workloadorchestration/arm-workloadorchestration/test/public/WorkloadOrchestration.spec.ts
@@ -17,7 +17,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe("WorkloadOrchestration test", () => {
+describe.skip("WorkloadOrchestration test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: WorkloadOrchestrationManagementClient;
