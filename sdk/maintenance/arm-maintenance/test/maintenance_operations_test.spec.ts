9179b1c1cede6e47e3169a87f4a0e9af2e1f2350
ZiWei Chen <v-ziweichen@microsoft.com>
2026-09-24 11:24:09 +0800

    Update maintenance_operations_test.spec.ts

--- sdk/maintenance/arm-maintenance/test/maintenance_operations_test.spec.ts
+++ sdk/maintenance/arm-maintenance/test/maintenance_operations_test.spec.ts
@@ -28,7 +28,7 @@ export const testPollingOptions = {
   updateIntervalInMs: isPlaybackMode() ? 0 : undefined,
 };
 
-describe("MaintenanceManagement test", () => {
+describe.skip("MaintenanceManagement test", () => {
   let recorder: Recorder;
   let subscriptionId: string;
   let client: MaintenanceManagementClient;
